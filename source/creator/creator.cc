260514cd54abe0f4401b67508152ca2d2fe74cb1
Jaume Bellet <mauge@bixo.org>
2023-10-24 18:46:48 +0200

    implements mblender-info command line option. implmements patch discovery

--- source/creator/creator.cc
+++ source/creator/creator.cc
@@ -57,6 +57,8 @@
 #include "BKE_vfont.h"
 #include "BKE_volume.h"
 
+#include "MB_blender.h"
+
 #ifndef WITH_PYTHON_MODULE
 #  include "BLI_args.h"
 #endif
@@ -284,7 +286,7 @@ int main(int argc,
 )
 {
   bContext *C;
-
+	
 #ifndef WITH_PYTHON_MODULE
   bArgs *ba;
 #endif
@@ -488,6 +490,8 @@ int main(int argc,
 
   /* Initialize sub-systems that use `BKE_appdir.h`. */
   IMB_init();
+
+  MB_init();
 
 #ifndef WITH_PYTHON_MODULE
   /* First test for background-mode (#Global.background) */
