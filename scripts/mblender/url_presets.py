c2ddfdf09ca5fb9d43dda369f602e5ff89bf3153
Jaume Bellet <mauge@bixo.org>
2023-11-02 23:12:30 +0100

    added py file

--- scripts/mblender/url_presets.py
+++ scripts/mblender/url_presets.py
@@ -1,19 +1,17 @@
 import bpy
 
-
 def register ():
-    print ("HOLA");
+    # bpy.ops.wm.url_open_preset(type="MBLENDER")
+
     bpy.types.WM_OT_url_open_preset.preset_items.append(
-        (('MBLENDER', "Mechanicalblender.org",  "Mechanical blender's official web-site"), 
+        (('MBLENDER', "Mechanicalblender.org",  "Mechanical blender's official web-site"),
         "https://www.mechanicalblender.org")
         )
     bpy.types.WM_OT_url_open_preset.preset_items.append(
-        (('MBLENDER_DOC', "Mechanicalblender Doc",  "Mechanical blender's documentation"), 
+        (('MBLENDER_DOC', "Mechanicalblender Doc",  "Mechanical blender's documentation"),
         "https://www.mechanicalblender.org/#documentation")
         )
-    
-    
+
 def unregister():
     pass
-    
-# bpy.ops.wm.url_open_preset(type="MBLENDER")
+
