cb058ee6a823f1168c464364700f2086c4102884
Next-Lab <nextlab_xr@nxlb.org>
2024-04-26 19:57:50 +0200

    start digging on how to put the z axis value (again)

--- source/blender/windowmanager/intern/wm_event_system.cc
+++ source/blender/windowmanager/intern/wm_event_system.cc
@@ -6012,6 +6012,7 @@ void wm_event_add_xrevent(wmWindow *win, wmXrActionData *actiondata, short val)
   event.custom = EVT_DATA_XR;
   event.customdata = actiondata;
   event.customdata_free = true;
+  copy_v3_v3(event.cval, actiondata->controller_loc);
 
   wm_event_add(win, &event);
 }
