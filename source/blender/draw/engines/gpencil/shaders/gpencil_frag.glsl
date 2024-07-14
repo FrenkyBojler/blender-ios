200faa6de31238ccf46be88f3ec956b93f7bc350
Casey Bianco-Davis <caseycasey739@gmail.com>
2024-07-13 19:48:24 -0700

    Cleanup: math

--- source/blender/draw/engines/gpencil/shaders/gpencil_frag.glsl
+++ source/blender/draw/engines/gpencil/shaders/gpencil_frag.glsl
@@ -134,16 +134,16 @@ float i_to_t(float i, vec4 p1, vec4 p2){
     vec4 P2 = from_cam(p2);
     l = length(P1.xyz - P2.xyz);
 
-    float a = P1.w - P2.w;
+    float a = P2.w - P1.w;
     if(abs(a) < 0.001){
       return (i - l_start)*P1.w / l;
     }
 
-    float E = -(a - l)/(a + l);
+    float E = (l + a)/(l - a);
 
-    float E_i = exp(((i - l_start)/2.0) * log(E));
+    float E_i = pow(E, (i - l_start)/2.0);
 
-    return -(P1.w * (E_i - 1.0)) / a;
+    return P1.w * (E_i - 1.0) / a;
   }else if(placement_mode == GP_DOTS_PLACEMENT_MODE_NUMBER){
     return i / l;
   }else if(placement_mode == GP_DOTS_PLACEMENT_MODE_LENGTH){
@@ -164,13 +164,13 @@ float t_to_i(float t, vec4 p1, vec4 p2){
     vec4 P2 = from_cam(p2);
     l = length(P1.xyz - P2.xyz);
 
-    float a = P1.w - P2.w;
+    float a = P2.w - P1.w;
     if(abs(a) < 0.001){
       return t * l/P1.w + l_start;
     }
 
-    float E = -(a - l)/(a + l);
-    float E_i = -t * a / P1.w + 1.0;
+    float E = (l + a)/(l - a);
+    float E_i = t * a / P1.w + 1.0;
     
     return 2.0 * log(E_i)/log(E) + l_start;
   }else if(placement_mode == GP_DOTS_PLACEMENT_MODE_NUMBER){
