6c64318480d4944b30606a51243bf3b90a782074
Guillermo Venegas <guillermovcra@gmail.com>
2024-09-23 22:12:41 -0600

    revert ui_color_picker_rgb_to_hsv_compat back, required to keep color value

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -7121,6 +7121,11 @@ static bool ui_numedit_but_HSVCIRCLE(uiBut *but,
   rcti rect;
   BLI_rcti_rctf_copy(&rect, &but->rect);
 
+  float rgb[3];
+  ui_but_v3_get(but, rgb);
+  ui_scene_linear_to_perceptual_space(but, rgb);
+  ui_color_picker_rgb_to_hsv_compat(rgb, hsv);
+
   if (bool(flags & (ButEditFlags::Shift | ButEditFlags::ContinuousGrab))) {
     const float fac = ui_mouse_scale_warp_factor(bool(flags & ButEditFlags::Shift));
     mval[0] = (mx - float(data->draglastx)) * fac + mval[0];
@@ -7172,7 +7177,6 @@ static bool ui_numedit_but_HSVCIRCLE(uiBut *but,
     ui_color_snap_hue(snap, &hsv[0]);
   }
 
-  float rgb[3];
   ui_color_picker_hsv_to_rgb(hsv, rgb);
 
   if (cpicker->use_luminosity_lock) {
