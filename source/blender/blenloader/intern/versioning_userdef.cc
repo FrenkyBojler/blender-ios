31bc31956865ec9290c1e77fabe5d50f597b4d4d
Sean Kim <SeanCTKim@protonmail.com>
2024-12-02 09:21:05 -0800

    Support migrating removing default keymap entries

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -303,6 +303,90 @@ static bool keymap_item_update_tweak_event(wmKeyMapItem *kmi, void * /*user_data
   return false;
 }
 
+static void keymap_update_brushes_handle_add_item(
+    const blender::StringRef asset_prefix,
+    const blender::StringRef tool_property,
+    const blender::Map<blender::StringRef, blender::StringRefNull> &tool_tool_map,
+    const blender::Map<blender::StringRef, blender::StringRef> &tool_asset_map,
+    const blender::Map<int, blender::StringRef> &id_asset_map,
+    wmKeyMapItem *kmi)
+{
+  std::optional<blender::StringRef> asset_id = {};
+  std::optional<blender::StringRefNull> tool_id = {};
+  if (STREQ(kmi->idname, "WM_OT_tool_set_by_id")) {
+    IDProperty *idprop = IDP_GetPropertyFromGroup(kmi->properties, "name");
+    if (idprop && (idprop->type == IDP_STRING)) {
+      const blender::StringRef prop_val = IDP_String(idprop);
+      if (!prop_val.startswith("builtin_brush.")) {
+        return;
+      }
+      if (tool_asset_map.contains(prop_val)) {
+        asset_id = tool_asset_map.lookup(prop_val);
+      }
+      else if (tool_tool_map.contains(prop_val)) {
+        tool_id = tool_tool_map.lookup(prop_val);
+      }
+    }
+  }
+  else if (STREQ(kmi->idname, "PAINT_OT_brush_select")) {
+    IDProperty *idprop = IDP_GetPropertyFromGroup(kmi->properties, tool_property);
+    if (idprop && (idprop->type == IDP_INT)) {
+      const int prop_val = IDP_Int(idprop);
+      if (id_asset_map.contains(prop_val)) {
+        asset_id = id_asset_map.lookup(prop_val);
+      }
+    }
+  }
+
+  if (asset_id) {
+    const std::string full_path = fmt::format("{}{}", asset_prefix, *asset_id);
+
+    WM_keymap_item_properties_reset(kmi, nullptr);
+    STRNCPY(kmi->idname, "BRUSH_OT_asset_activate");
+    IDP_AddToGroup(
+        kmi->properties,
+        blender::bke::idprop::create("asset_library_type", ASSET_LIBRARY_ESSENTIALS).release());
+    IDP_AddToGroup(kmi->properties,
+                   blender::bke::idprop::create("relative_asset_identifier", full_path).release());
+  }
+  else if (tool_id) {
+    WM_keymap_item_properties_reset(kmi, nullptr);
+    IDP_AddToGroup(kmi->properties, blender::bke::idprop::create("name", *tool_id).release());
+  }
+}
+
+static void keymap_update_brushes_handle_remove_item(
+    const blender::StringRef asset_prefix,
+    const blender::StringRef tool_property,
+    const blender::Map<int, blender::StringRef> &id_asset_map,
+    wmKeyMapItem *kmi)
+{
+  std::optional<blender::StringRef> asset_id = {};
+  /* Only the paint.brush_select operator is stored in the default keymap & applicable to be
+   * updated if the user removed it in a previous version. */
+  if (STREQ(kmi->idname, "PAINT_OT_brush_select")) {
+    IDProperty *idprop = IDP_GetPropertyFromGroup(kmi->properties, tool_property);
+    if (idprop && (idprop->type == IDP_INT)) {
+      const int prop_val = IDP_Int(idprop);
+      if (id_asset_map.contains(prop_val)) {
+        asset_id = id_asset_map.lookup(prop_val);
+      }
+    }
+  }
+
+  if (asset_id) {
+    const std::string full_path = fmt::format("{}{}", asset_prefix, *asset_id);
+
+    WM_keymap_item_properties_reset(kmi, nullptr);
+    STRNCPY(kmi->idname, "BRUSH_OT_asset_activate");
+    IDP_AddToGroup(
+        kmi->properties,
+        blender::bke::idprop::create("asset_library_type", ASSET_LIBRARY_ESSENTIALS).release());
+    IDP_AddToGroup(kmi->properties,
+                   blender::bke::idprop::create("relative_asset_identifier", full_path).release());
+  }
+}
+
 static void keymap_update_brushes(
     wmKeyMap *keymap,
     const blender::StringRef asset_prefix,
@@ -312,53 +396,17 @@ static void keymap_update_brushes(
     const blender::Map<int, blender::StringRef> &id_asset_map)
 {
   LISTBASE_FOREACH (wmKeyMapDiffItem *, kmid, &keymap->diff_items) {
-    wmKeyMapItem *kmi = kmid->add_item;
-    if (!kmi) {
-      continue;
+    if (kmid->add_item) {
+      keymap_update_brushes_handle_add_item(asset_prefix,
+                                            tool_property,
+                                            tool_tool_map,
+                                            tool_asset_map,
+                                            id_asset_map,
+                                            kmid->add_item);
     }
-
-    std::optional<blender::StringRef> asset_id = {};
-    std::optional<blender::StringRefNull> tool_id = {};
-    if (STREQ(kmi->idname, "WM_OT_tool_set_by_id")) {
-      IDProperty *idprop = IDP_GetPropertyFromGroup(kmi->properties, "name");
-      if (idprop && (idprop->type == IDP_STRING)) {
-        const blender::StringRef prop_val = IDP_String(idprop);
-        if (!prop_val.startswith("builtin_brush.")) {
-          continue;
-        }
-        if (tool_asset_map.contains(prop_val)) {
-          asset_id = tool_asset_map.lookup(prop_val);
-        }
-        else if (tool_tool_map.contains(prop_val)) {
-          tool_id = tool_tool_map.lookup(prop_val);
-        }
-      }
-    }
-    else if (STREQ(kmi->idname, "PAINT_OT_brush_select")) {
-      IDProperty *idprop = IDP_GetPropertyFromGroup(kmi->properties, tool_property);
-      if (idprop && (idprop->type == IDP_INT)) {
-        const int prop_val = IDP_Int(idprop);
-        if (id_asset_map.contains(prop_val)) {
-          asset_id = id_asset_map.lookup(prop_val);
-        }
-      }
-    }
-
-    if (asset_id) {
-      const std::string full_path = fmt::format("{}{}", asset_prefix, *asset_id);
-
-      WM_keymap_item_properties_reset(kmi, nullptr);
-      STRNCPY(kmi->idname, "BRUSH_OT_asset_activate");
-      IDP_AddToGroup(
-          kmi->properties,
-          blender::bke::idprop::create("asset_library_type", ASSET_LIBRARY_ESSENTIALS).release());
-      IDP_AddToGroup(
-          kmi->properties,
-          blender::bke::idprop::create("relative_asset_identifier", full_path).release());
-    }
-    else if (tool_id) {
-      WM_keymap_item_properties_reset(kmi, nullptr);
-      IDP_AddToGroup(kmi->properties, blender::bke::idprop::create("name", *tool_id).release());
+    if (kmid->remove_item) {
+      keymap_update_brushes_handle_remove_item(
+          asset_prefix, tool_property, id_asset_map, kmid->remove_item);
     }
   }
 }
