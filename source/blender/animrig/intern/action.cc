c64e5601cce099d4ac6274f851c924d5cade1e94
Nathan Vegdahl <cessen@cessen.com>
2024-12-02 16:14:28 +0100

    Add assert

--- source/blender/animrig/intern/action.cc
+++ source/blender/animrig/intern/action.cc
@@ -403,6 +403,9 @@ void Action::slot_display_name_set(Main &bmain, Slot &slot, StringRefNull new_di
 {
   BLI_assert_msg(StringRef(new_display_name).size() >= 1,
                  "Action Slot display names must not be empty");
+  BLI_assert_msg(StringRef(slot.identifier).size() >= 2,
+                 "Action Slot's existing identifier lacks the two-character type prefix, which "
+                 "would make the display name copy meaningless due to early null termination.");
 
   BLI_strncpy_utf8(slot.identifier + 2, new_display_name.c_str(), ARRAY_SIZE(slot.identifier) - 2);
   slot_identifier_ensure_unique(*this, slot);
