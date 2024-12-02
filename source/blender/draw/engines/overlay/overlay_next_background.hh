26f90c3dd4b8941d78f6a3f973cbc816c84dfb84
Jeroen Bakker <jeroen@blender.org>
2024-12-02 15:14:38 +0100

    Cleanup: Make format

--- source/blender/draw/engines/overlay/overlay_next_background.hh
+++ source/blender/draw/engines/overlay/overlay_next_background.hh
@@ -87,7 +87,7 @@ class Background : Overlay {
     bg_ps_.bind_ubo(OVERLAY_GLOBALS_SLOT, &res.globals_buf);
     bg_ps_.bind_texture("colorBuffer", &res.color_render_tx);
     bg_ps_.bind_texture("depthBuffer", &res.depth_tx);
-    bg_ps_.push_constant("colorOverride", color_override); 
+    bg_ps_.push_constant("colorOverride", color_override);
     bg_ps_.push_constant("bgType", background_type);
     bg_ps_.draw_procedural(GPU_PRIM_TRIS, 1, 3);
   }
