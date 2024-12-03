3a788e7b3e049223143a28ad523eb1a428582274
Nicola <nicdessi@gmail.com>
2024-12-03 06:21:53 +0100

    Delete Vertex Data node

--- source/blender/nodes/NOD_static_types.h
+++ source/blender/nodes/NOD_static_types.h
@@ -463,7 +463,6 @@ DefNode(GeometryNode, GEO_NODE_SCULPT_ACTIVE_MESH, 0, "SCULPT_ACTIVE_MESH", Scul
 DefNode(GeometryNode, GEO_NODE_SCULPT_BRUSH_INFO, 0, "SCULPT_BRUSH_INFO", SculptBrushInfo, "Brush Info", "Brush Info")
 DefNode(GeometryNode, GEO_NODE_SCULPT_PEN_PRESSURE, 0, "SCULPT_PEN_PRESSURE", SculptPenPressure, "Pen Pressure", "Pen Pressure")
 DefNode(GeometryNode, GEO_NODE_SCULPT_STROKE_INFO, 0, "SCULPT_STROKE_INFO", SculptStrokeInfo, "Stroke Info", "Stroke Info")
-DefNode(GeometryNode, GEO_NODE_SCULPT_VERTEX_DATA, 0, "SCULPT_VERTEX_DATA", SculptVertexData, "Vertex Data", "Vertex Data")
 DefNode(GeometryNode, GEO_NODE_SDF_GRID_BOOLEAN, 0, "SDF_GRID_BOOLEAN", SDFGridBoolean, "SDF Grid Boolean", "Cut, subtract, or join multiple SDF volume grid inputs")
 DefNode(GeometryNode, GEO_NODE_SELF_OBJECT, 0, "SELF_OBJECT", SelfObject, "Self Object", "Retrieve the object that contains the geometry nodes modifier currently being executed")
 DefNode(GeometryNode, GEO_NODE_SEPARATE_COMPONENTS, 0, "SEPARATE_COMPONENTS",SeparateComponents, "Separate Components", "Split a geometry into a separate output for each type of data in the geometry")
