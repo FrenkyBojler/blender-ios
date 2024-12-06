8d8ddf8689a3a2aaed99c531b9a06e31b00e1076
Eitan Traurig <eitant13@gmail.com>
2024-12-05 19:50:40 -0500

    Remove comment

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -1392,7 +1392,6 @@ void blo_do_versions_userdef(UserDef *userdef)
     }
   }
   if (!USER_VERSION_ATLEAST(404, 10)) {
-    /* Rename Hide Collection to Isolate Collection */
     LISTBASE_FOREACH (wmKeyMap *, keymap, &userdef->user_keymaps) {
       LISTBASE_FOREACH (wmKeyMapDiffItem *, kmdi, &keymap->diff_items) {
         if (STREQ("OBJECT_OT_hide_collection", kmdi->remove_item->idname)) {
