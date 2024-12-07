3592371788a4555a7aa23f334777fb1f0c62b4b4
Sean <seantommurray@gmail.com>
2024-12-07 17:50:29 -0500

    Added early return and cleaned up comments

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -76,8 +76,6 @@
 #include "UI_resources.hh"
 #include <limits>
 
-#include <iostream>
-
 namespace blender::ed::greasepencil {
 
 /* -------------------------------------------------------------------- */
@@ -1187,13 +1185,15 @@ static bke::CurvesGeometry set_start_point(const bke::CurvesGeometry &curves,
 
   const OffsetIndices<int> points_by_curve = curves.points_by_curve();
   const VArray<bool> src_cyclic = curves.cyclic();
+
+  /* Early-return if no cyclic curves. */
+  if (array_utils::booleans_mix_calc(src_cyclic) == array_utils::BooleanMix::AllFalse) {
+    return curves;
+  }
+
   Array<bool> start_set_points(curves.points_num());
-
-  // early exit here
-
   mask.to_bools(start_set_points.as_mutable_span());
 
-  int curr_dst_point_id = 0;
   Array<int> dst_to_src_point(curves.points_num());
 
   threading::parallel_for(curves.curves_range(), 1024, [&](const IndexRange range) {
@@ -1206,7 +1206,6 @@ static bke::CurvesGeometry set_start_point(const bke::CurvesGeometry &curves,
 
       array_utils::fill_index_range<int>(dst_to_src_slice, points.start());
 
-      /* map 1:1 for points that aren't changing or non cyclic*/
       if (first_selected == -1 || src_cyclic[curve_i] == false) {
         array_utils::scatter<int>(dst_to_src_slice, points, dst_to_src_point);
         continue;
@@ -1220,18 +1219,18 @@ static bke::CurvesGeometry set_start_point(const bke::CurvesGeometry &curves,
     }
   });
 
-  /* New CurvesGeometry to copy to*/
+  /* New CurvesGeometry to copy to. */
   bke::CurvesGeometry dst_curves(curves.points_num(), curves.curves_num());
   BKE_defgroup_copy_list(&dst_curves.vertex_group_names, &curves.vertex_group_names);
 
-  /* Copy offsets */
+  /* Copy offsets. */
   array_utils::copy(curves.offsets(), dst_curves.offsets_for_write());
 
-  /* Attribute accessors for copying */
+  /* Attribute accessors for copying. */
   bke::MutableAttributeAccessor dst_attributes = dst_curves.attributes_for_write();
   const bke::AttributeAccessor src_attributes = curves.attributes();
 
-  /* Copy curve attrs */
+  /* Copy curve attrs. */
   bke::copy_attributes(
       src_attributes, bke::AttrDomain::Curve, bke::AttrDomain::Curve, {}, dst_attributes);
   array_utils::copy(src_cyclic, dst_curves.cyclic_for_write());
@@ -1265,8 +1264,7 @@ static int grease_pencil_set_start_point_exec(bContext *C, wmOperator *)
       return;
     }
 
-    bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
-    curves = set_start_point(curves, selection);
+    info.drawing.strokes_for_write() = set_start_point(info.drawing.strokes(), selection);
 
     info.drawing.tag_topology_changed();
     changed = true;
