5265e938fc70b36e99137d7d067559324fdbd865
Jesse Yurkovich <jesse.y@gmail.com>
2024-12-06 16:35:37 -0800

    Feedback

--- source/blender/blenkernel/BKE_duplilist.hh
+++ source/blender/blenkernel/BKE_duplilist.hh
@@ -36,7 +36,7 @@ ListBase *object_duplilist_preview(Depsgraph *depsgraph,
                                    const ViewerPath *viewer_path);
 void free_object_duplilist(ListBase *lb);
 
-inline constexpr int MAX_DUPLI_RECUR = 8;
+constexpr int MAX_DUPLI_RECUR = 8;
 
 struct DupliObject {
   DupliObject *next, *prev;
