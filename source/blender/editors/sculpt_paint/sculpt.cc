eb7db5897542a43b24dfe0060847870b7c87ad08
Sean Kim <SeanCTKim@protonmail.com>
2024-12-10 14:44:21 -0800

    Cleanup: Rename & move SCULPT_handles_colors_report

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -5234,7 +5234,9 @@ static void stroke_undo_end(const bContext *C, Brush *brush)
   }
 }
 
-bool SCULPT_handles_colors_report(const Object &object, ReportList *reports)
+namespace blender::ed::sculpt_paint {
+
+bool color_supported_check(const Object &object, ReportList *reports)
 {
   switch (blender::bke::object::pbvh_get(object)->type()) {
     case blender::bke::pbvh::Type::Mesh:
@@ -5249,8 +5251,6 @@ bool SCULPT_handles_colors_report(const Object &object, ReportList *reports)
   BLI_assert_unreachable();
   return false;
 }
-
-namespace blender::ed::sculpt_paint {
 
 static bool stroke_test_start(bContext *C, wmOperator *op, const float mval[2])
 {
@@ -5424,7 +5424,7 @@ static int sculpt_brush_stroke_invoke(bContext *C, wmOperator *op, const wmEvent
   Brush &brush = *BKE_paint_brush(&sd.paint);
 
   if (SCULPT_brush_type_is_paint(brush.sculpt_brush_type) &&
-      !SCULPT_handles_colors_report(ob, op->reports))
+      !color_supported_check(ob, op->reports))
   {
     return OPERATOR_CANCELLED;
   }
