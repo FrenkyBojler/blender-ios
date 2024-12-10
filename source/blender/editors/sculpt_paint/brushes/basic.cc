352e2441edc06deb56ddd54b5ce982d3dd275245
Nicola <nicdessi@gmail.com>
2024-12-10 07:45:54 +0100

    basic brush: use sculpt center for dist calc

--- source/blender/editors/sculpt_paint/brushes/basic.cc
+++ source/blender/editors/sculpt_paint/brushes/basic.cc
@@ -13,6 +13,7 @@
 #include "BKE_paint.hh"
 #include "BKE_paint_bvh.hh"
 #include "BKE_subdiv_ccg.hh"
+#include "BKE_brush.hh"
 
 #include "BLI_array.hh"
 #include "BLI_enumerable_thread_specific.hh"
@@ -38,6 +39,25 @@ struct LocalData {
   Vector<float3> translations;
 };
 
+static void calc_distances(const float3& center,
+  const Span<int> verts,
+  const Span<float3> positions,
+  const MutableSpan<float> distances)
+{
+  for (const int i : verts.index_range()) {
+    distances[i] = math::distance(positions[verts[i]], center);
+  }
+}
+
+static void calc_distances(const float3& center,
+  const Span<float3> positions,
+  const MutableSpan<float> distances)
+{
+  for (const int i : positions.index_range()) {
+    distances[i] = math::distance(positions[i], center);
+  }
+}
+
 static void calc_faces(const Depsgraph &depsgraph,
                        const Sculpt &sd,
                        const Brush &brush,
@@ -48,23 +68,35 @@ static void calc_faces(const Depsgraph &depsgraph,
                        LocalData &tls,
                        const PositionDeformData &position_data)
 {
-  const SculptSession &ss = *object.sculpt;
+  const SculptSession& ss = *object.sculpt;
+  const StrokeCache& cache = *ss.cache;
+
   const Span<int> verts = node.verts();
 
-  calc_factors_common_mesh_indexed(depsgraph,
-                                   brush,
-                                   object,
-                                   attribute_data,
-                                   position_data.eval,
-                                   vert_normals,
-                                   node,
-                                   tls.factors,
-                                   tls.distances);
+  tls.factors.resize(verts.size());
+  const MutableSpan<float> factors = tls.factors;
+  fill_factor_from_hide_and_mask(attribute_data.hide_vert, attribute_data.mask, verts, factors);
+  filter_region_clip_factors(ss, position_data.eval, verts, factors);
+  if (brush.flag & BRUSH_FRONTFACE) {
+    calc_front_face(cache.view_normal_symm, vert_normals, verts, factors);
+  }
+
+  tls.distances.resize(verts.size());
+  const MutableSpan<float> distances = tls.distances;
+  calc_distances(cache.sculpt_center_symm, verts, position_data.eval, distances);
+  apply_hardness_to_distances(cache.radius, cache.hardness, distances);
+  BKE_brush_calc_curve_factors(
+    eBrushCurvePreset(brush.curve_preset), brush.curve, distances, cache.radius, factors);
+
+  auto_mask::calc_vert_factors(depsgraph, object, cache.automasking.get(), node, verts, factors);
+  calc_brush_texture_factors(ss, brush, position_data.eval, verts, factors);
 
   tls.translations.resize(verts.size());
   const MutableSpan<float3> translations = tls.translations;
+  translations.fill(float3(0.0f));
   mesh_sculpt_nodes_evaluate<float3>(
       depsgraph, object, *ss.cache, position_data.eval, verts, translations);
+
   scale_translations(translations, tls.factors);
   scale_translations(translations, ss.cache->bstrength);
 
@@ -80,18 +112,34 @@ static void calc_grids(const Depsgraph &depsgraph,
                        LocalData &tls)
 {
   SculptSession &ss = *object.sculpt;
+  const StrokeCache& cache = *ss.cache;
   SubdivCCG &subdiv_ccg = *ss.subdiv_ccg;
 
   const Span<int> grids = node.grids();
   const MutableSpan<float3> positions = gather_grids_positions(subdiv_ccg, grids, tls.positions);
 
-  calc_factors_common_grids(depsgraph, brush, object, positions, node, tls.factors, tls.distances);
+  tls.factors.resize(positions.size());
+  const MutableSpan<float> factors = tls.factors;
+  fill_factor_from_hide_and_mask(subdiv_ccg, grids, factors);
+  filter_region_clip_factors(ss, positions, factors);
+  if (brush.flag & BRUSH_FRONTFACE) {
+    calc_front_face(cache.view_normal_symm, subdiv_ccg, grids, factors);
+  }
 
-  tls.translations.resize(positions.size());
+  tls.distances.resize(positions.size());
+  const MutableSpan<float> distances = tls.distances;
+  calc_distances(cache.sculpt_center_symm, positions, distances);
+  apply_hardness_to_distances(cache.radius, cache.hardness, distances);
+  BKE_brush_calc_curve_factors(
+    eBrushCurvePreset(brush.curve_preset), brush.curve, distances, cache.radius, factors);
+
+  auto_mask::calc_grids_factors(depsgraph, object, cache.automasking.get(), node, grids, factors);
+  calc_brush_texture_factors(ss, brush, positions, factors);
+
+  tls.translations.resize(factors.size());
   const MutableSpan<float3> translations = tls.translations;
-
   grids_sculpt_nodes_evaluate<float3>(
-      depsgraph, object, *ss.cache, subdiv_ccg, grids, positions, translations);
+    depsgraph, object, *ss.cache, subdiv_ccg, grids, positions, translations);
 
   scale_translations(translations, tls.factors);
   scale_translations(translations, ss.cache->bstrength);
@@ -108,15 +156,32 @@ static void calc_bmesh(const Depsgraph &depsgraph,
                        LocalData &tls)
 {
   SculptSession &ss = *object.sculpt;
-
+  const StrokeCache& cache = *ss.cache;
   const Set<BMVert *, 0> &verts = BKE_pbvh_bmesh_node_unique_verts(&node);
   const MutableSpan<float3> positions = gather_bmesh_positions(verts, tls.positions);
 
-  calc_factors_common_bmesh(depsgraph, brush, object, positions, node, tls.factors, tls.distances);
+  tls.factors.resize(positions.size());
+  const MutableSpan<float> factors = tls.factors;
+  fill_factor_from_hide_and_mask(*ss.bm, verts, factors);
+  filter_region_clip_factors(ss, positions, factors);
+  if (brush.flag & BRUSH_FRONTFACE) {
+    calc_front_face(cache.view_normal_symm, verts, factors);
+  }
 
-  tls.translations.resize(verts.size());
+  tls.distances.resize(positions.size());
+  const MutableSpan<float> distances = tls.distances;
+  calc_distances(cache.sculpt_center_symm, positions, distances);
+  apply_hardness_to_distances(cache.radius, cache.hardness, distances);
+  BKE_brush_calc_curve_factors(
+    eBrushCurvePreset(brush.curve_preset), brush.curve, distances, cache.radius, factors);
+
+  auto_mask::calc_vert_factors(depsgraph, object, cache.automasking.get(), node, verts, factors);
+  calc_brush_texture_factors(ss, brush, positions, factors);
+
+  tls.translations.resize(factors.size());
   const MutableSpan<float3> translations = tls.translations;
   bmesh_sculpt_nodes_evaluate<float3>(depsgraph, object, *ss.cache, verts, positions, translations);
+
   scale_translations(translations, tls.factors);
   scale_translations(translations, ss.cache->bstrength);
 
