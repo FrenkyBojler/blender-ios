911f0e54584462ba994e68b382ff01664094abb8
Jonas Holzman <jonas@holzman.fr>
2024-12-11 18:19:48 +0100

    Improve `Bone X-ray ` fade property name and description

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -4678,7 +4678,8 @@ static void rna_def_space_view3d_overlay(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "xray_alpha_bone", PROP_FLOAT, PROP_FACTOR);
   RNA_def_property_float_sdna(prop, nullptr, "overlay.xray_alpha_bone");
-  RNA_def_property_ui_text(prop, "Bone X-Ray Alpha", "Opacity used for bone X-Ray selection");
+  RNA_def_property_ui_text(
+      prop, "Bone X-Ray Fade", "Geometry fade strength for bone X-Ray selection");
   RNA_def_property_range(prop, 0.0f, 1.0f);
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_VIEW3D, nullptr);
