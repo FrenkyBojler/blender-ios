c6aa29fcd41b24e573dc1bcd330350ccf1c089bc
Pratik Borhade <pratikborhade302@gmail.com>
2024-12-12 12:22:05 +0530

    Revert "Fix #130853: Asset shelf popup when Spacebar mapped to different actions"
    This reverts commit 53e74497a0dd86769d4654df35383e3c0db4105e.

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -267,12 +267,7 @@ def op_panel(menu, kmi_args, kmi_data=()):
     return ("wm.call_panel", kmi_args, {"properties": [("name", menu), *kmi_data]})
 
 
-def op_asset_shelf_popup(asset_shelf, spacebar_action):
-    if spacebar_action == 'PLAY':
-        kmi_args = {"type": 'SPACE', "value": 'PRESS', "shift": True}
-    elif spacebar_action == 'TOOL':
-        kmi_args = {"type": 'SPACE', "value": 'PRESS'}
-
+def op_asset_shelf_popup(asset_shelf, kmi_args):
     return ("wm.call_asset_shelf_popover", kmi_args, {"properties": [("name", asset_shelf)]})
 
 
@@ -3795,15 +3790,14 @@ def km_grease_pencil_paint_mode(params):
         # Lasso/Box erase
         ("grease_pencil.erase_lasso", {"type": 'RIGHTMOUSE', "value": 'PRESS', "ctrl": True, "alt": True}, None),
         ("grease_pencil.erase_box", {"type": "B", "value": 'PRESS'}, {"properties": [("wait_for_input", True)]}),
+        
+        op_asset_shelf_popup(
+            "VIEW3D_AST_brush_gpencil_paint",
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
 
         *_template_items_context_panel("VIEW3D_PT_greasepencil_draw_context_menu", params.context_menu_event),
     ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
-            "VIEW3D_AST_brush_gpencil_paint",
-            params.spacebar_action
-        )])
 
     return keymap
 
@@ -4026,15 +4020,12 @@ def km_grease_pencil_sculpt_mode(params):
         op_menu("GREASE_PENCIL_MT_layer_active", {"type": 'Y', "value": 'PRESS'}),
 
         *_template_paint_radial_control("gpencil_sculpt_paint"),
-
+        op_asset_shelf_popup(
+            "VIEW3D_AST_brush_gpencil_sculpt",
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
         *_template_items_context_panel("VIEW3D_PT_greasepencil_sculpt_context_menu", params.context_menu_event),
     ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
-            "VIEW3D_AST_brush_gpencil_sculpt",
-            params.spacebar_action
-        )])
 
     return keymap
 
@@ -4100,13 +4091,12 @@ def km_grease_pencil_weight_paint(params):
 
         # Show/hide layer
         *_template_items_hide_reveal_actions("grease_pencil.layer_hide", "grease_pencil.layer_reveal"),
-    ])
 
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_gpencil_weight",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     if params.select_mouse == 'LEFTMOUSE':
         # Bone selection for combined weight paint + pose mode (Alt).
@@ -4182,13 +4172,12 @@ def km_grease_pencil_vertex_paint(params):
         *_template_paint_radial_control("gpencil_vertex_paint"),
         # Context menu
         *_template_items_context_panel("VIEW3D_PT_greasepencil_vertex_paint_context_menu", params.context_menu_event),
-    ])
 
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_gpencil_vertex",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     return keymap
 
@@ -4748,17 +4737,15 @@ def km_image_paint(params):
         ("wm.context_menu_enum", {"type": 'E', "value": 'PRESS', "alt": True},
          {"properties": [("data_path", "tool_settings.image_paint.brush.stroke_method")]}),
         *_template_items_context_panel("VIEW3D_PT_paint_texture_context_menu", params.context_menu_event),
-    ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_texture_paint",
-            params.spacebar_action
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
         ),
-            op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "IMAGE_AST_brush_paint",
-            params.spacebar_action
-        ),])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     if params.legacy:
         items.extend(_template_items_legacy_tools_from_numbers())
@@ -4808,13 +4795,11 @@ def km_vertex_paint(params):
          {"properties": [("data_path", "tool_settings.vertex_paint.brush.stroke_method")]}),
         ("paint.face_vert_reveal", {"type": 'H', "value": 'PRESS', "alt": True}, None),
         *_template_items_context_panel("VIEW3D_PT_paint_vertex_context_menu", params.context_menu_event),
-    ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_vertex_paint",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     if params.legacy:
         items.extend(_template_items_legacy_tools_from_numbers())
@@ -4870,13 +4855,11 @@ def km_weight_paint(params):
          {"properties": [("data_path", "tool_settings.weight_paint.brush.use_smooth_stroke")]}),
         op_menu_pie("VIEW3D_MT_wpaint_vgroup_lock_pie", {"type": 'K', "value": 'PRESS'}),
         *_template_items_context_panel("VIEW3D_PT_paint_weight_context_menu", params.context_menu_event),
-    ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_weight_paint",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     if params.select_mouse == 'LEFTMOUSE':
         # Bone selection for combined weight paint + pose mode (Alt).
@@ -5126,13 +5109,11 @@ def km_sculpt(params):
              ("asset_library_type", 'ESSENTIALS'),
              ("relative_asset_identifier", "brushes/essentials_brushes-mesh_sculpt.blend/Brush/Mask"),
          ]}),
-    ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_sculpt",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     # Lasso Masking.
     # Needed because of shortcut conflicts on CTRL-LMB on right click select,
@@ -5184,13 +5165,11 @@ def km_sculpt_curves(params):
         *_template_items_select_actions(params, "curves.select_all"),
         ("sculpt_curves.min_distance_edit", {"type": 'R', "value": 'PRESS'}, {}),
         ("sculpt_curves.select_grow", {"type": 'A', "value": 'PRESS', "shift": True}, {}),
-    ])
-
-    if params.spacebar_action != 'SEARCH':
-        items.extend([op_asset_shelf_popup(
+        op_asset_shelf_popup(
             "VIEW3D_AST_brush_sculpt_curves",
-            params.spacebar_action
-        )])
+            {"type": 'SPACE', "value": 'PRESS', "shift": True}
+        ),
+    ])
 
     return keymap
 
