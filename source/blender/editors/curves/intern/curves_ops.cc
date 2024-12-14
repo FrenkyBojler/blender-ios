2959f934a1e26939f1192dca6793275b27baed27
Laurynas Duburas <laduem@gmail.com>
2024-12-14 11:22:07 +0200

    auto handles

--- source/blender/editors/curves/intern/curves_ops.cc
+++ source/blender/editors/curves/intern/curves_ops.cc
@@ -1157,6 +1157,8 @@ static int split_exec(bContext *C, wmOperator * /*op*/)
     }
     split_points(points_to_split, curves, memory);
 
+    curves.calculate_bezier_auto_handles();
+
     DEG_id_tag_update(&curves_id->id, ID_RECALC_GEOMETRY);
     WM_event_add_notifier(C, NC_GEOM | ND_DATA, curves_id);
   }
