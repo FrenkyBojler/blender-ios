3d25d15e9a4e477321bb707953dada28e2b854bb
Alaska <alaskayou01@gmail.com>
2024-12-18 20:59:39 +1300

    Shift quality setting to it's own function

--- source/blender/nodes/composite/nodes/node_composite_denoise.cc
+++ source/blender/nodes/composite/nodes/node_composite_denoise.cc
@@ -134,11 +134,9 @@ class DenoiseOperation : public NodeOperation {
     filter.setImage("color", input_color, oidn::Format::Float3, width, height, 0, pixel_stride);
     filter.setImage("output", output_color, oidn::Format::Float3, width, height, 0, pixel_stride);
     filter.set("hdr", use_hdr());
-#  if OIDN_VERSION_MAJOR >= 2
-    OIDNQuality quality_setting = get_quality();
     filter.set("cleanAux", auxiliary_passes_are_clean());
-    filter.set("quality", quality_setting);
-#  endif
+    OIDNQuality quality_setting = get_quality();
+    set_filter_quality(filter, quality_setting);
     filter.setProgressMonitorFunction(oidn_progress_monitor_function, &context());
 
     /* If the albedo input is not a single value input, download the albedo texture, denoise it
@@ -155,9 +153,7 @@ class DenoiseOperation : public NodeOperation {
 
       if (should_denoise_auxiliary_passes()) {
         oidn::FilterRef albedoFilter = device.newFilter("RT");
-#  if OIDN_VERSION_MAJOR >= 2
-        albedoFilter.set("quality", quality_setting);
-#  endif
+        set_filter_quality(albedoFilter, quality_setting);
         albedoFilter.setImage(
             "albedo", albedo, oidn::Format::Float3, width, height, 0, pixel_stride);
         albedoFilter.setImage(
@@ -186,9 +182,7 @@ class DenoiseOperation : public NodeOperation {
 
       if (should_denoise_auxiliary_passes()) {
         oidn::FilterRef normalFilter = device.newFilter("RT");
-#  if OIDN_VERSION_MAJOR >= 2
-        normalFilter.set("quality", quality_setting);
-#  endif
+        set_filter_quality(normalFilter, quality_setting);
         normalFilter.setImage(
             "normal", normal, oidn::Format::Float3, width, height, 0, pixel_stride);
         normalFilter.setImage(
@@ -292,7 +286,17 @@ class DenoiseOperation : public NodeOperation {
         return OIDN_QUALITY_HIGH;
     }
   }
-#endif
+
+  void set_filter_quality(oidn::FilterRef &filter, OIDNQuality quality)
+  {
+#  if OIDN_VERSION_MAJOR >= 2
+    filter.set("quality", quality);
+#  else
+    (void)filter;
+    (void)quality;
+#  endif
+  }
+#endif /* WITH_OPENIMAGEDENOISE */
 
   /* OIDN can be disabled as a build option, so check WITH_OPENIMAGEDENOISE. Additionally, it is
    * only supported at runtime for CPUs that supports SSE4.1, except for MacOS where it is always
