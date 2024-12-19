4f414234e790c21363142d0c8b7318c4eff39cb3
Falk David <falk@blender.org>
2024-12-19 12:04:15 +0100

    Compute origin by curve

--- source/blender/editors/transform/transform_convert_curves.cc
+++ source/blender/editors/transform/transform_convert_curves.cc
@@ -496,26 +496,55 @@ void curve_populate_trans_data_structs(
   const float3x3 mtx_base = transform.view<3, 3>();
   const float3x3 smtx_base = math::pseudo_invert(mtx_base);
 
+  const OffsetIndices<int> points_by_curve = curves.points_by_curve();
+  Array<float3> center_point_per_curve(curves.curves_num());
+  if (is_individual_origin) {
+    affected_curves.foreach_index(GrainSize(512), [&](const int64_t curve_i) {
+      const IndexRange points = points_by_curve[curve_i];
+      IndexMaskMemory memory;
+      const IndexMask selection =
+          IndexMask::from_bools(point_selection, memory).slice_content(points);
+      if (selection.is_empty()) {
+        return;
+      }
+      float3 center(0.0f);
+      selection.foreach_index([&](const int64_t point_i) { center += point_positions[point_i]; });
+      center /= selection.size();
+      center_point_per_curve[curve_i] = center;
+    });
+  }
+
+  const Array<int> point_to_curve_map = curves.point_to_curve_map();
   for (const int selection_i : position_offsets_in_td.index_range()) {
     if (position_offsets_in_td[selection_i].is_empty()) {
       continue;
     }
     MutableSpan<TransData> tc_data = all_tc_data.slice(position_offsets_in_td[selection_i]);
     MutableSpan<float3> positions = positions_per_selection_attr[selection_i];
-    IndexMask points_to_transform = points_to_transform_per_attr[selection_i];
-    VArray<bool> selection = selection_attrs[selection_i];
+    const IndexMask points_to_transform = points_to_transform_per_attr[selection_i];
+    const VArray<bool> selection = selection_attrs[selection_i];
 
     points_to_transform.foreach_index(
         GrainSize(1024), [&](const int64_t domain_i, const int64_t transform_i) {
           TransData &td = tc_data[transform_i];
           float3 *elem = &positions[transform_i];
 
+          float3 center;
+          const bool use_local_center = hide_handles || (t.around == V3D_AROUND_LOCAL_ORIGINS) ||
+                                        point_selection[domain_i];
+          const int curve_i = point_to_curve_map[domain_i];
+          if (is_individual_origin) {
+            center = center_point_per_curve[curve_i];
+          }
+          else if (use_local_center) {
+            center = point_positions[domain_i];
+          }
+          else {
+            center = td.iloc;
+          }
+
           copy_v3_v3(td.iloc, *elem);
-          copy_v3_v3(td.center,
-                     hide_handles || (t.around == V3D_AROUND_LOCAL_ORIGINS) ||
-                             point_selection[domain_i] ?
-                         point_positions[domain_i] :
-                         td.iloc);
+          copy_v3_v3(td.center, center);
           td.loc = *elem;
 
           td.flag = 0;
@@ -546,7 +575,6 @@ void curve_populate_trans_data_structs(
   }
   if (use_connected_only) {
     const VArray<int8_t> curve_types = curves.curve_types();
-    const OffsetIndices<int> points_by_curve = curves.points_by_curve();
     Array<int> bezier_offsets_in_td(curves.curves_num() + 1, 0);
     offset_indices::copy_group_sizes(points_by_curve, bezier_curves, bezier_offsets_in_td);
     offset_indices::accumulate_counts_to_offsets(bezier_offsets_in_td);
