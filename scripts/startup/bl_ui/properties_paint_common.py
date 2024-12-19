7c20484bbb222d2718ed93f6c0f9e577d65050af
Nicola <nicdessi@gmail.com>
2024-12-19 15:28:22 +0100

    fix UI

--- scripts/startup/bl_ui/properties_paint_common.py
+++ scripts/startup/bl_ui/properties_paint_common.py
@@ -885,18 +885,8 @@ def brush_settings(layout, context, brush, popover=False):
             layout.separator()
             layout.prop(brush, "use_cloth_collision")
             layout.separator()
-            
-        elif sculpt_tool == 'FLATTEN':
-            layout.prop(brush, "plane_inversion_mode")
 
-        elif sculpt_tool == 'SCRAPE':
-            row = layout.row(align=True)
-            row.prop(brush, "area_radius_factor")
-            row.prop(brush, "use_pressure_area_radius", text="")
-            row = layout.row()
-            layout.prop(brush, "plane_inversion_mode")
-
-        elif sculpt_tool == 'FILL':
+        elif sculpt_tool == 'PLANE':
             row = layout.row(align=True)
             row.prop(brush, "area_radius_factor")
             row.prop(brush, "use_pressure_area_radius", text="")
