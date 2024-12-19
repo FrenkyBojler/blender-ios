cd8fb044b876c2d99fb157e5917b0c468a9714d9
Gerard Taulats <tabra@noreply.localhost>
2024-12-19 19:46:00 +0100

    Perform local loop

--- source/blender/editors/transform/transform_snap.cc
+++ source/blender/editors/transform/transform_snap.cc
@@ -1623,7 +1623,9 @@ bool peelObjectsTransform(TransInfo *t,
     r_no[1] = 0.0;
     r_no[2] = 1.0;
 
-    BLI_deletelist(&depths_peel);
+    LISTBASE_FOREACH_MUTABLE (SnapObjectHitDepth *, link, &depths_peel) {
+      MEM_delete(link);
+    }
     return true;
   }
   return false;
