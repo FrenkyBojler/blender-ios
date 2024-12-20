fa73fea81f1198cbbcaa5ce84a79cdbd1306f17d
Jeff Moguillansky <quic_jmoguill@quicinc.com>
2024-12-20 12:12:58 -0800

    Revert "vk_command_buffer_wrapper: set local_read to false by default"
    This reverts commit 7451ff659bf2560469d68d4a09a52eda8357be53.

--- source/blender/gpu/vulkan/render_graph/vk_command_buffer_wrapper.hh
+++ source/blender/gpu/vulkan/render_graph/vk_command_buffer_wrapper.hh
@@ -18,7 +18,7 @@ namespace blender::gpu::render_graph {
 class VKCommandBufferInterface {
  public:
   bool use_dynamic_rendering = true;
-  bool use_dynamic_rendering_local_read = false;
+  bool use_dynamic_rendering_local_read = true;
 
   VKCommandBufferInterface() {}
   virtual ~VKCommandBufferInterface() = default;
