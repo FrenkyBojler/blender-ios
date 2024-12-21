86d2fd3f733f46843446917c1a71449a6444aebb
Jacques Lucke <jacques@blender.org>
2024-12-22 00:33:26 +0100

    fix

--- source/blender/blenkernel/intern/node_tree_update.cc
+++ source/blender/blenkernel/intern/node_tree_update.cc
@@ -571,6 +571,8 @@ class NodeTreeMainUpdater {
     const Span<const bNode *> group_nodes = tree.group_nodes();
     for (const bNode *group_node : group_nodes) {
       const bNodeTree *group = reinterpret_cast<const bNodeTree *>(group_node->id);
+      group->ensure_interface_cache();
+      group->ensure_topology_cache();
       if (group == nullptr) {
         continue;
       }
