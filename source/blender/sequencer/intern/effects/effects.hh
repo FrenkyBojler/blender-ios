6ecdfa5c1fb0f503aee572c9718d4f7349115674
Aras Pranckevicius <aras@nesnausk.org>
2024-12-25 12:50:41 +0200

    VSE: split off SpeedEffect into vse_effect_speed.cc file

--- source/blender/sequencer/intern/effects/effects.hh
+++ source/blender/sequencer/intern/effects/effects.hh
@@ -82,6 +82,9 @@ void get_default_fac_fade(const Scene *scene,
                           float timeline_frame,
                           float *fac);
 
+SeqEffectHandle get_sequence_effect_impl(int seq_type);
+
 void gaussian_blur_effect_get_handle(SeqEffectHandle &rval);
+void speed_effect_get_handle(SeqEffectHandle &rval);
 void text_effect_get_handle(SeqEffectHandle &rval);
 void wipe_effect_get_handle(SeqEffectHandle &rval);
