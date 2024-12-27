3db58debbdec250ffc161cdcf97085c81daed6fc
Laurynas Duburas <laduem@gmail.com>
2024-12-27 15:03:12 +0200

    circles as GPU_PRIM_POINTS

--- source/blender/draw/engines/overlay/overlay_next_curve.hh
+++ source/blender/draw/engines/overlay/overlay_next_curve.hh
@@ -217,7 +217,8 @@ class Curves : Overlay {
       gpu::Batch *geom = DRW_curves_batch_cache_get_edit_bezier_segments(&curves);
       edit_bezier_segments_->bind_ubo("curves_data",
                                       DRW_curves_batch_cache_get_curves_data(&curves));
-      edit_segment_joints_->draw_expand(geom, GPU_PRIM_TRIS, 2, 1, manager.unique_handle(ob_ref));
+      edit_segment_joints_->draw_expand(
+          geom, GPU_PRIM_POINTS, 1, 1, manager.unique_handle(ob_ref));
       edit_bezier_segments_->draw_expand(geom, GPU_PRIM_TRIS, 2, 1, manager.unique_handle(ob_ref));
     }
   }
