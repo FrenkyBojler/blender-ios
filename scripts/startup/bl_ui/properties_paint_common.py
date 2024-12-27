005b4e0ae8c8c420d2edc0b0b1a795599b9161ea
Nicola <nicdessi@gmail.com>
2024-12-28 00:40:26 +0100

    add plane_offset to Plane brush

--- scripts/startup/bl_ui/properties_paint_common.py
+++ scripts/startup/bl_ui/properties_paint_common.py
@@ -789,12 +789,13 @@ def brush_settings(layout, context, brush, popover=False):
                 pressure_name="use_offset_pressure",
                 slider=True,
             )
-
-            row = layout.row(heading="Plane Trim")
-            row.prop(brush, "use_plane_trim", text="")
-            sub = row.row()
-            sub.active = brush.use_plane_trim
-            sub.prop(brush, "plane_trim", slider=True, text="")
+            
+            if sculpt_tool != 'PLANE':
+                row = layout.row(heading="Plane Trim")
+                row.prop(brush, "use_plane_trim", text="")
+                sub = row.row()
+                sub.active = brush.use_plane_trim
+                sub.prop(brush, "plane_trim", slider=True, text="")
 
             layout.separator()
 
--- source/blender/makesrna/intern/rna_brush.cc
+++ source/blender/makesrna/intern/rna_brush.cc
@@ -487,6 +487,7 @@ static bool rna_BrushCapabilitiesSculpt_has_plane_offset_get(PointerRNA *ptr)
               SCULPT_BRUSH_TYPE_CLAY,
               SCULPT_BRUSH_TYPE_CLAY_STRIPS,
               SCULPT_BRUSH_TYPE_CLAY_THUMB,
+              SCULPT_BRUSH_TYPE_PLANE,
               SCULPT_BRUSH_TYPE_FILL,
               SCULPT_BRUSH_TYPE_FLATTEN,
               SCULPT_BRUSH_TYPE_SCRAPE);
