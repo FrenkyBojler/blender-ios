31456a5e326b2962439f4edfe331fe9c94f65a24
Aras Pranckevicius <aras@nesnausk.org>
2024-12-28 22:36:35 +0200

    Import cameras and lights

--- source/blender/io/fbx/importer/fbx_import.cc
+++ source/blender/io/fbx/importer/fbx_import.cc
@@ -9,7 +9,9 @@
 #include <cstdio>
 
 #include "BKE_attribute.hh"
+#include "BKE_camera.h"
 #include "BKE_layer.hh"
+#include "BKE_light.h"
 #include "BKE_mesh.hh"
 #include "BKE_object.hh"
 #include "BKE_report.hh"
@@ -18,12 +20,15 @@
 #include "BLI_fileops.h"
 #include "BLI_map.hh"
 #include "BLI_math_matrix.h"
+#include "BLI_math_rotation.h"
 #include "BLI_string.h"
 
 #include "DEG_depsgraph.hh"
 #include "DEG_depsgraph_build.hh"
 
+#include "DNA_camera_types.h"
 #include "DNA_collection_types.h"
+#include "DNA_light_types.h"
 #include "DNA_scene_types.h"
 
 #include "IO_fbx.hh"
@@ -49,6 +54,8 @@ struct FbxImportContext {
   }
 
   void import_meshes();
+  void import_cameras();
+  void import_lights();
   void import_empties();
   void setup_hierarchy();
 };
@@ -84,7 +91,7 @@ void FbxImportContext::import_meshes()
 {
   for (ufbx_mesh *fmesh : this->fbx.meshes) {
     if (fmesh->instances.count == 0) {
-      continue; /* Ignore meshes that aren't used by any objects. */
+      continue; /* Ignore if not used by any objects. */
     }
 
     /* Create Mesh outside of main. */
@@ -201,9 +208,86 @@ void FbxImportContext::import_meshes()
     obj->data = BKE_object_obdata_add_from_type(this->bmain, OB_MESH, mesh_name);
     BKE_mesh_nomain_to_mesh(mesh, static_cast<Mesh *>(obj->data), obj);
 
-    /* Transform matrix. */
     node_matrix_to_obj(node, obj);
+    this->node_to_object.add(node, obj);
+  }
+}
 
+void FbxImportContext::import_cameras()
+{
+  for (ufbx_camera *fcam : this->fbx.cameras) {
+    if (fcam->instances.count == 0) {
+      continue; /* Ignore if not used by any objects. */
+    }
+    const ufbx_node *node = fcam->instances[0];
+    const char *ob_name = get_node_name(node);
+
+    Camera *bcam = BKE_camera_add(this->bmain, ob_name);
+
+    bcam->type = fcam->projection_mode == UFBX_PROJECTION_MODE_ORTHOGRAPHIC ? CAM_ORTHO :
+                                                                              CAM_PERSP;
+    bcam->dof.focus_distance = ufbx_find_real(&fcam->props, "FocusDistance", 10.0f) *
+                               this->fbx.metadata.geometry_scale * this->fbx.metadata.root_scale;
+    if (ufbx_find_bool(&fcam->props, "UseDepthOfField", false)) {
+      bcam->dof.flag |= CAM_DOF_ENABLED;
+    }
+    bcam->lens = fcam->focal_length_mm;
+    constexpr double m_to_in = 0.0393700787;
+    bcam->sensor_x = fcam->film_size_inch.x / m_to_in;
+    bcam->sensor_y = fcam->film_size_inch.y / m_to_in;
+    bcam->ortho_scale = fcam->orthographic_extent;
+    bcam->shiftx = ufbx_find_real(&fcam->props, "FilmOffsetX", 0.0) / (m_to_in * bcam->sensor_x);
+    bcam->shifty = ufbx_find_real(&fcam->props, "FilmOffsetY", 0.0) / (m_to_in * bcam->sensor_x);
+    bcam->clip_start = fcam->near_plane * this->fbx.metadata.root_scale;
+    bcam->clip_end = fcam->far_plane * this->fbx.metadata.root_scale;
+
+    Object *obj = BKE_object_add_only_object(this->bmain, OB_CAMERA, ob_name);
+    obj->data = bcam;
+
+    node_matrix_to_obj(node, obj);
+    this->node_to_object.add(node, obj);
+  }
+}
+
+void FbxImportContext::import_lights()
+{
+  for (ufbx_light *flight : this->fbx.lights) {
+    if (flight->instances.count == 0) {
+      continue; /* Ignore if not used by any objects. */
+    }
+    const ufbx_node *node = flight->instances[0];
+    const char *ob_name = get_node_name(node);
+
+    Light *lamp = BKE_light_add(this->bmain, ob_name);
+    switch (flight->type) {
+      case UFBX_LIGHT_POINT:
+        lamp->type = LA_LOCAL;
+        break;
+      case UFBX_LIGHT_DIRECTIONAL:
+        lamp->type = LA_SUN;
+        break;
+      case UFBX_LIGHT_SPOT:
+        lamp->type = LA_SPOT;
+        lamp->spotsize = DEG2RAD(flight->outer_angle);
+        lamp->spotblend = 1.0f - flight->inner_angle / flight->outer_angle;
+        break;
+      default:
+        break;
+    }
+
+    lamp->r = flight->color.x;
+    lamp->g = flight->color.y;
+    lamp->b = flight->color.z;
+    lamp->energy = flight->intensity;
+    if (flight->cast_shadows) {
+      lamp->mode |= LA_SHADOW;
+    }
+    //@TODO: if hasattr(lamp, "cycles"): lamp.cycles.cast_shadow = lamp.use_shadow
+
+    Object *obj = BKE_object_add_only_object(this->bmain, OB_LAMP, ob_name);
+    obj->data = lamp;
+
+    node_matrix_to_obj(node, obj);
     this->node_to_object.add(node, obj);
   }
 }
@@ -260,10 +344,18 @@ void importer_main(Main *bmain, Scene *scene, ViewLayer *view_layer, const FBXIm
   opts.load_external_files = false;
   opts.clean_skin_weights = true;
   opts.use_blender_pbr_material = true;
+  //@TODO: axes according to import settings
   opts.target_axes.right = UFBX_COORDINATE_AXIS_POSITIVE_X;
   opts.target_axes.up = UFBX_COORDINATE_AXIS_POSITIVE_Z;
   opts.target_axes.front = UFBX_COORDINATE_AXIS_NEGATIVE_Y;
   opts.target_unit_meters = 1.0f;
+
+  opts.target_camera_axes.right = UFBX_COORDINATE_AXIS_POSITIVE_X;
+  opts.target_camera_axes.up = UFBX_COORDINATE_AXIS_POSITIVE_Y;
+  opts.target_camera_axes.front = UFBX_COORDINATE_AXIS_POSITIVE_Z;
+  opts.target_light_axes.right = UFBX_COORDINATE_AXIS_POSITIVE_X;
+  opts.target_light_axes.up = UFBX_COORDINATE_AXIS_POSITIVE_Y;
+  opts.target_light_axes.front = UFBX_COORDINATE_AXIS_POSITIVE_Z;
 
   ufbx_error fbx_error;
   ufbx_scene *fbx = ufbx_load_stdio(file, &opts, &fbx_error);
@@ -287,6 +379,8 @@ void importer_main(Main *bmain, Scene *scene, ViewLayer *view_layer, const FBXIm
 
   FbxImportContext ctx(bmain, fbx, params);
   ctx.import_meshes();
+  ctx.import_cameras();
+  ctx.import_lights();
   ctx.import_empties();
   ctx.setup_hierarchy();
 
