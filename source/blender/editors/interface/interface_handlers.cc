blender/blender

commit 3d0988a7192abd7d7aeb9378ee7df8218b906005

Cleanup: Move some math C-API usage to C++ API
Mostly part of an attempt to remove some of the lesser-used
parts of the C math API.

source/blender/editors/interface/interface_handlers.cc
@@ -10679,12 +10679,11 @@ static int ui_handle_menu_event(bContext *C,
     else {
       if (event->type == MOUSEMOVE) {
         WM_cursor_set(win, PopupTitleDragCursor);
-        int mdiff[2];
+        blender::int2 mdiff = blender::int2(event->xy) - blender::int2(menu->grab_xy_prev);
 
-        sub_v2_v2v2_int(mdiff, event->xy, menu->grab_xy_prev);
         copy_v2_v2_int(menu->grab_xy_prev, event->xy);
 
-        add_v2_v2v2_int(menu->popup_create_vars.event_xy, menu->popup_create_vars.event_xy, mdiff);
+        menu->popup_create_vars.event_xy -= mdiff;
 
         ui_popup_translate(region, mdiff);
       }

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -354,8 +354,7 @@ void ViewOpsData::init_navigation(bContext *C,
     ED_view3d_win_to_vector(region, mval, this->init.mousevec);
 
     {
-      int event_xy_offset[2];
-      add_v2_v2v2_int(event_xy_offset, event->xy, this->init.event_xy_offset);
+      int2 event_xy_offset = int2(event->xy) - this->init.event_xy_offset;
 
       /* For rotation with trackball rotation. */
       calctrackballvec(&region->winrct, event_xy_offset, this->init.trackvec);

source/blender/blenkernel/intern/armature_test.cc
@@ -6,6 +6,7 @@
 
 #include "BLI_listbase.h"
 #include "BLI_math_matrix.h"
+#include "BLI_math_matrix.hh"
 #include "BLI_math_rotation.h"
 #include "BLI_math_vector.h"
 #include "BLI_string.h"
@@ -29,13 +30,13 @@ static double EXPECT_M3_ORTHOGONAL(const float mat[3][3],
                                    double epsilon_ortho)
 {
   /* Do the checks in double precision to avoid precision issues in the checks themselves. */
-  double dmat[3][3];
-  copy_m3d_m3(dmat, mat);
+  double3x3 dmat;
+  copy_m3d_m3(dmat.ptr(), mat);
 
   /* Check individual axis scaling. */
-  EXPECT_NEAR(len_v3_db(dmat[0]), 1.0, epsilon_scale);
-  EXPECT_NEAR(len_v3_db(dmat[1]), 1.0, epsilon_scale);
-  EXPECT_NEAR(len_v3_db(dmat[2]), 1.0, epsilon_scale);
+  EXPECT_NEAR(math::length(dmat[0]), 1.0, epsilon_scale);
+  EXPECT_NEAR(math::length(dmat[1]), 1.0, epsilon_scale);
+  EXPECT_NEAR(math::length(dmat[2]), 1.0, epsilon_scale);
 
   /* Check orthogonality. */
   EXPECT_NEAR(dot_v3v3_db(dmat[0], dmat[1]), 0.0, epsilon_ortho);
@@ -43,7 +44,7 @@ static double EXPECT_M3_ORTHOGONAL(const float mat[3][3],
   EXPECT_NEAR(dot_v3v3_db(dmat[1], dmat[2]), 0.0, epsilon_ortho);
 
   /* Check determinant to detect flipping and as a secondary volume change check. */
-  double determinant = determinant_m3_array_db(dmat);
+  double determinant = math::determinant(dmat);
 
   EXPECT_NEAR(determinant, 1.0, epsilon_ortho);
 

source/blender/blenkernel/intern/constraint.cc
@@ -4588,12 +4588,14 @@ static void damptrack_evaluate(bConstraint *con, bConstraintOb *cob, ListBase *t
 
 static void damptrack_do_transform(float matrix[4][4], const float tarvec_in[3], int track_axis)
 {
+  using namespace blender;
   /* find the (unit) direction vector going from the owner to the target */
-  float tarvec[3];
+  float3 tarvec;
 
   if (normalize_v3_v3(tarvec, tarvec_in) != 0.0f) {
-    float obvec[3], obloc[3];
-    float raxis[3], rangle;
+    float3 obvec, obloc;
+    float3 raxis;
+    float rangle;
     float rmat[3][3], tmat[4][4];
 
     /* find the (unit) direction that the axis we're interested in currently points
@@ -4619,7 +4621,7 @@ static void damptrack_do_transform(float matrix[4][4], const float tarvec_in[3],
      * - the min/max wrappers around (obvec . tarvec) result (stored temporarily in rangle)
      *   are used to ensure that the smallest angle is chosen
      */
-    cross_v3_v3v3_hi_prec(raxis, obvec, tarvec);
+    raxis = math::cross_high_precision(obvec, tarvec);
 
     rangle = dot_v3v3(obvec, tarvec);
     rangle = acosf(max_ff(-1.0f, min_ff(1.0f, rangle)));

source/blender/blenkernel/intern/fluid.cc
@@ -893,14 +893,10 @@ static void update_velocities(FluidEffectorSettings *fes,
       mul_v3_fl(hit_vel, fes->vel_multi);
 
       /* Absolute representation of new object velocity. */
-      float abs_hit_vel[3];
-      copy_v3_v3(abs_hit_vel, hit_vel);
-      abs_v3(abs_hit_vel);
+      blender::float3 abs_hit_vel = blender::math::abs(blender::float3(hit_vel));
 
       /* Absolute representation of current object velocity. */
-      float abs_vel[3];
-      copy_v3_v3(abs_vel, &velocity_map[index * 3]);
-      abs_v3(abs_vel);
+      blender::float3 abs_vel = blender::math::abs(blender::float3(&velocity_map[index * 3]));
 
       switch (fes->guide_mode) {
         case FLUID_EFFECTOR_GUIDE_AVERAGED:

source/blender/blenkernel/intern/pbvh_pixels.cc
@@ -321,7 +321,7 @@ static void apply_watertight_check(Tree &pbvh, Image &image, ImageUser &image_us
           }
           if (image_buffer->byte_buffer.data) {
             uint8_t *dest = &image_buffer->byte_buffer.data[pixel_offset * 4];
-            copy_v4_uchar(dest, 255);
+            dest[0] = dest[1] = dest[2] = dest[3] = 255;
           }
           pixel_offset += 1;
         }

source/blender/bmesh/intern/bmesh_polygon.cc
@@ -1179,7 +1179,7 @@ void BM_face_triangulate(BMesh *bm,
 
 void BM_face_splits_check_legal(BMesh *bm, BMFace *f, BMLoop *(*loops)[2], int len)
 {
-  float out[2] = {-FLT_MAX, -FLT_MAX};
+  blender::float2 out = {-FLT_MAX, -FLT_MAX};
   float center[2] = {0.0f, 0.0f};
   float axis_mat[3][3];
   float(*projverts)[2] = BLI_array_alloca(projverts, f->len);
@@ -1215,7 +1215,7 @@ void BM_face_splits_check_legal(BMesh *bm, BMFace *f, BMLoop *(*loops)[2], int l
   bm->elem_index_dirty |= BM_LOOP;
 
   /* ensure we are well outside the face bounds (value is arbitrary) */
-  add_v2_fl(out, 1.0f);
+  out += 1.0f;
 
   for (i = 0; i < len; i++) {
     edgeverts[i][0] = projverts[BM_elem_index_get(loops[i][0])];

source/blender/draw/intern/draw_fluid.cc
@@ -172,7 +172,7 @@ static GPUTexture *create_volume_texture(const int dim[3],
                                          const void *data)
 {
   GPUTexture *tex = nullptr;
-  int final_dim[3] = {UNPACK3(dim)};
+  blender::int3 final_dim = {UNPACK3(dim)};
 
   if (data == nullptr) {
     return nullptr;
@@ -199,7 +199,7 @@ static GPUTexture *create_volume_texture(const int dim[3],
     printf("Error: Could not create 3D texture.\n");
     tex = GPU_texture_create_error(3, false);
   }
-  else if (equals_v3v3_int(dim, final_dim)) {
+  else if (blender::int3(dim) == final_dim) {
     /* No need to resize, just upload the data. */
     GPU_texture_update_sub(tex, data_format, data, 0, 0, 0, UNPACK3(final_dim));
   }

FrenkyBojler/blender-ios

commit f0451aa3396cabd9eb9a72ed3f076624dfa9f77d

Fix #132396: Orbit Gizmo navigation used wrong math op
Regression from 3d0988a7192abd7d7aeb9378ee7df8218b906005 where two adds
were accidentally changed to subtracts.

source/blender/editors/interface/interface_handlers.cc
@@ -10683,7 +10683,7 @@ static int ui_handle_menu_event(bContext *C,
 
         copy_v2_v2_int(menu->grab_xy_prev, event->xy);
 
-        menu->popup_create_vars.event_xy -= mdiff;
+        menu->popup_create_vars.event_xy += mdiff;
 
         ui_popup_translate(region, mdiff);
       }

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -354,7 +354,7 @@ void ViewOpsData::init_navigation(bContext *C,
     ED_view3d_win_to_vector(region, mval, this->init.mousevec);
 
     {
-      int2 event_xy_offset = int2(event->xy) - this->init.event_xy_offset;
+      int2 event_xy_offset = int2(event->xy) + this->init.event_xy_offset;
 
       /* For rotation with trackball rotation. */
       calctrackballvec(&region->winrct, event_xy_offset, this->init.trackvec);