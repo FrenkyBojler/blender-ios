6af2851481e8d6e80e2cdc72aa9d35594a8f0b5e
Laurynas Duburas <laduem@gmail.com>
2025-01-06 11:53:38 +0200

    static_cast

--- source/blender/blenkernel/intern/curve.cc
+++ source/blender/blenkernel/intern/curve.cc
@@ -1182,7 +1182,7 @@ static void makeknots(Nurb *nu, short uv)
 
 void BKE_nurb_knot_alloc_u(Nurb *nu)
 {
-  nu->knotsu = (float *)MEM_calloc_arrayN(KNOTSU(nu) + 1, sizeof(float), "makeknots");
+  nu->knotsu = static_cast<float *>(MEM_calloc_arrayN(KNOTSU(nu) + 1, sizeof(float), __func__));
 }
 
 void BKE_nurb_knot_calc_u(Nurb *nu)
