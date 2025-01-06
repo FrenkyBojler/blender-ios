a2ab541201d261e1402eaff1f395ce425dcfcddc
Iliya Katueshenock <modormoder@gmail.com>
2025-01-06 19:23:30 +0300

    add new node

--- source/blender/nodes/function/nodes/node_fn_find_in_string.cc
+++ source/blender/nodes/function/nodes/node_fn_find_in_string.cc
@@ -58,7 +58,7 @@ static void node_register()
 {
   static blender::bke::bNodeType ntype;
 
-  fn_node_type_base(&ntype, FN_NODE_FIND_IN_STRING, "Find in String", NODE_CLASS_CONVERTER);
+  fn_node_type_base(&ntype, FN_NODE_FIND_IN_STRING, "Find in String", blender::bke::NodeClass::Converter);
   ntype.declare = node_declare;
   ntype.build_multi_function = node_build_multi_function;
   blender::bke::node_register_type(&ntype);
