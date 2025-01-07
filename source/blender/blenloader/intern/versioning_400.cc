d7277836ab2c81541e99ea029b1da602ccfc5b5d
Omar Emara <mail@OmarEmara.dev>
2025-01-07 15:18:13 +0200

    Compositor: Allow tinting Glare
    This patch adds two new inputs to the Glare node, Saturation and Tint.
    Their function is to tint the color of Glare to any color the user
    wants.

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -1503,6 +1503,10 @@ void do_versions_after_linking_400(FileData *fd, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 404, 19)) {
+    version_node_socket_index_animdata(bmain, NTREE_COMPOSIT, CMP_NODE_GLARE, 3, 2, 11);
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
