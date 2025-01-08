e04b508324e9014ce6bb343087ea347510b98bb8
Iliya Katueshenock <modormoder@gmail.com>
2025-01-08 18:36:39 +0300

    fix merge conflict

--- source/blender/blenkernel/BKE_curves.hh
+++ source/blender/blenkernel/BKE_curves.hh
@@ -875,8 +875,6 @@ CurvesGeometry curves_copy_curve_selection(const CurvesGeometry &curves,
 
 CurvesGeometry curves_new_no_attributes(int point_num, int curve_num);
 
-CurvesGeometry curves_new_no_attributes(int point_num, int curve_num);
-
 std::array<int, CURVE_TYPES_NUM> calculate_type_counts(const VArray<int8_t> &types);
 
 /* -------------------------------------------------------------------- */
