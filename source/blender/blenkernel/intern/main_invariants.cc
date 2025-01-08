cad828af981bad29bb7b420de79fddeeb5a9b838
Jacques Lucke <jacques@blender.org>
2025-01-08 18:18:33 +0100

    some progress with notifiers

--- source/blender/blenkernel/intern/main_invariants.cc
+++ source/blender/blenkernel/intern/main_invariants.cc
@@ -9,10 +9,40 @@
 
 #include "DNA_node_types.h"
 
-void BKE_main_ensure_invariants(Main &bmain)
+#include "WM_api.hh"
+#include "WM_types.hh"
+
+static void send_notifiers_after_node_tree_change(ID *id, bNodeTree *ntree)
+{
+  WM_main_add_notifier(NC_NODE | NA_EDITED, id);
+
+  if (ntree->type == NTREE_SHADER && id != nullptr) {
+    if (GS(id->name) == ID_MA) {
+      WM_main_add_notifier(NC_MATERIAL | ND_SHADING, id);
+    }
+    else if (GS(id->name) == ID_LA) {
+      WM_main_add_notifier(NC_LAMP | ND_LIGHTING, id);
+    }
+    else if (GS(id->name) == ID_WO) {
+      WM_main_add_notifier(NC_WORLD | ND_WORLD, id);
+    }
+  }
+  else if (ntree->type == NTREE_COMPOSIT) {
+    WM_main_add_notifier(NC_SCENE | ND_NODES, id);
+  }
+  else if (ntree->type == NTREE_TEXTURE) {
+    WM_main_add_notifier(NC_TEXTURE | ND_NODES, id);
+  }
+  else if (ntree->type == NTREE_GEOMETRY) {
+    WM_main_add_notifier(NC_OBJECT | ND_MODIFIER, id);
+  }
+}
+
+static void propagate_node_tree_changes(Main &bmain)
 {
   NodeTreeUpdateExtraParams params;
-  params.tree_changed_fn = [](bNodeTree &ntree, ID & /*owner_id*/) {
+  params.tree_changed_fn = [](bNodeTree &ntree, ID &owner_id) {
+    send_notifiers_after_node_tree_change(&owner_id, &ntree);
     DEG_id_tag_update(&ntree.id, ID_RECALC_SYNC_TO_EVAL);
   };
   params.tree_output_changed_fn = [](bNodeTree &ntree, ID & /*owner_id*/) {
@@ -20,4 +50,9 @@ void BKE_main_ensure_invariants(Main &bmain)
   };
 
   BKE_ntree_update_main(&bmain, &params);
+}
+
+void BKE_main_ensure_invariants(Main &bmain)
+{
+  propagate_node_tree_changes(bmain);
 }
