e6d757fa97182e70a681e53b0fef40b753ca5bd6
Habib Gahbiche <habib@blender.org>
2025-01-09 17:36:23 +0100

    Cast in switch for better type safety

--- source/blender/editors/space_node/node_edit.cc
+++ source/blender/editors/space_node/node_edit.cc
@@ -1013,17 +1013,17 @@ static int node_resize_modal(bContext *C, wmOperator *op, const wmEvent *event)
   NodeSizeWidget *nsw = (NodeSizeWidget *)op->customdata;
 
   if (event->type == EVT_MODAL_MAP) {
-    switch (event->val) {
-      case int(NodeResizeAction::Begin): {
+    switch (NodeResizeAction(event->val)) {
+      case NodeResizeAction::Begin: {
         return OPERATOR_RUNNING_MODAL;
       }
-      case int(NodeResizeAction::Cancel): {
+      case NodeResizeAction::Cancel: {
         node_resize_exit(C, op, true);
         ED_region_tag_redraw(region);
         return OPERATOR_CANCELLED;
       }
-      case int(NodeResizeAction::SnapInvertOn):
-      case int(NodeResizeAction::SnapInvertOff): {
+      case NodeResizeAction::SnapInvertOn:
+      case NodeResizeAction::SnapInvertOff: {
         nsw->snap_to_grid = !nsw->snap_to_grid;
         return OPERATOR_RUNNING_MODAL;
       }
