blender/blender

commit e6d941cdf428060c0d3ac1a60c1892e5f15aa303

Fix #129178: Opening the context menu on a numeric input shows the wrong cursor
Use non-modal setting of mouse cursor type so that the correct one is
shown for the numerical input context menu.

Pull Request: https://projects.blender.org/blender/blender/pulls/129455

source/blender/editors/interface/interface_handlers.cc
@@ -5485,13 +5485,13 @@ static void ui_numedit_set_active(uiBut *but)
   if ((but->flag & UI_SELECT) == 0) {
     if ((but->drawflag & UI_BUT_HOVER_LEFT) || (but->drawflag & UI_BUT_HOVER_RIGHT)) {
       if (data->changed_cursor) {
-        WM_cursor_modal_restore(data->window);
+        WM_cursor_set(data->window, WM_CURSOR_DEFAULT);
         data->changed_cursor = false;
       }
     }
     else {
       if (data->changed_cursor == false) {
-        WM_cursor_modal_set(data->window, WM_CURSOR_X_MOVE);
+        WM_cursor_set(data->window, WM_CURSOR_X_MOVE);
         data->changed_cursor = true;
       }
     }
@@ -8922,7 +8922,7 @@ static void button_activate_exit(
 #endif
 
   if (data->changed_cursor) {
-    WM_cursor_modal_restore(data->window);
+    WM_cursor_set(data->window, WM_CURSOR_DEFAULT);
   }
 
   /* redraw and refresh (for popups) */

FrenkyBojler/blender-ios

commit 50d16abf442b16f1b2ce93329419d841c61b7fde

Fix #132830: Regression: Cursor restores the update in GN input
For node region, `event_cursor` is true, it sends update to the cursor
every draw call, this restores the cursor `WM_CURSOR_X_MOVE`.
Issue is originally caused by e6d941cdf4 and it introduced couple other
issues. So best fix would be to revert the commit. And to fix #129178,
restore modal cursor after drawing context menu, i.e. execution of
`ui_popup_context_menu_for_button`.

source/blender/editors/interface/interface_handlers.cc
@@ -5485,13 +5485,13 @@ static void ui_numedit_set_active(uiBut *but)
   if ((but->flag & UI_SELECT) == 0) {
     if ((but->drawflag & UI_BUT_HOVER_LEFT) || (but->drawflag & UI_BUT_HOVER_RIGHT)) {
       if (data->changed_cursor) {
-        ED_region_cursor_set(data->window, data->area, data->region);
+        WM_cursor_modal_restore(data->window);
         data->changed_cursor = false;
       }
     }
     else {
       if (data->changed_cursor == false) {
-        WM_cursor_set(data->window, WM_CURSOR_X_MOVE);
+        WM_cursor_modal_set(data->window, WM_CURSOR_X_MOVE);
         data->changed_cursor = true;
       }
     }
@@ -8193,6 +8193,7 @@ static int ui_do_button(bContext *C, uiBlock *block, uiBut *but, const wmEvent *
 
       /* RMB has two options now */
       if (ui_popup_context_menu_for_button(C, but, event)) {
+        WM_cursor_modal_restore(data->window);
         return WM_UI_HANDLER_BREAK;
       }
     }
@@ -8925,7 +8926,7 @@ static void button_activate_exit(
 #endif
 
   if (data->changed_cursor) {
-    ED_region_cursor_set(data->window, data->area, data->region);
+    WM_cursor_modal_restore(win);
   }
 
   /* redraw and refresh (for popups) */