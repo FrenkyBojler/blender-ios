15640b1e6cdd0efc9af9935c45261acab1cc35e0
Aras Pranckevicius <aras@nesnausk.org>
2025-01-09 18:57:09 +0200

    Fix #132214: wrong Render Audio result when using Matroska container format
    Matroska container really really wants to internally keep on using
    milliseconds as a time base. Even if at initialization time both the
    m_stream and m_codecCtx are set to use {1, sample_rate} timebase,
    later during the avformat_write_header call the audio stream gets set
    to {1, 1000}.
    
    Doing av_packet_rescale_ts for each packet after receiving it but
    before writing it seems to do all the correct work. This is what is
    being done inside Blender movie_write_audio.cc, and in official
    ffmpeg mux.c example (of course it is not done in ffmpeg encode_audio.c
    example, but that whole example feels like it is from 20+ years ago
    and does not work with any modern-ish codec or container).

--- extern/audaspace/plugins/ffmpeg/FFMPEGWriter.cpp
+++ extern/audaspace/plugins/ffmpeg/FFMPEGWriter.cpp
@@ -120,6 +120,8 @@ void FFMPEGWriter::encode()
 	while(avcodec_receive_packet(m_codecCtx, m_packet) == 0)
 	{
 		m_packet->stream_index = m_stream->index;
+        
+        av_packet_rescale_ts(m_packet, m_codecCtx->time_base, m_stream->time_base);
 
 		if(av_write_frame(m_formatCtx, m_packet) < 0)
 			AUD_THROW(FileException, "Frame couldn't be writen to the file with ffmpeg.");
@@ -161,6 +163,8 @@ void FFMPEGWriter::close()
 	while(avcodec_receive_packet(m_codecCtx, m_packet) == 0)
 	{
 		m_packet->stream_index = m_stream->index;
+
+        av_packet_rescale_ts(m_packet, m_codecCtx->time_base, m_stream->time_base);
 
 		if(av_write_frame(m_formatCtx, m_packet) < 0)
 			AUD_THROW(FileException, "Frame couldn't be writen to the file with ffmpeg.");
