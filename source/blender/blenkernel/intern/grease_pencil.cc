dc5208e96d891d0bdd516f2599907d0e43d53ce0
Julian Eisel <julian@blender.org>
2025-01-10 16:44:02 +0100

    Avoid resetting active material, allow skipping as part of API instead

--- source/blender/blenkernel/intern/grease_pencil.cc
+++ source/blender/blenkernel/intern/grease_pencil.cc
@@ -2441,13 +2441,12 @@ Material *BKE_grease_pencil_object_material_ensure_from_brush(Main *bmain,
 
     /* check if the material is already on object material slots and add it if missing */
     if (ma && BKE_object_material_index_get(ob, ma) < 0) {
-      const int actcol_old = ob->actcol;
+      /* The object's active material is what's used for the unpinned material. Do not touch it
+       * while using a pinned material. */
+      const bool change_active_material = false;
 
-      BKE_object_material_slot_add(bmain, ob);
+      BKE_object_material_slot_add(bmain, ob, change_active_material);
       BKE_object_material_assign(bmain, ob, ma, ob->totcol, BKE_MAT_ASSIGN_USERPREF);
-      /* #BKE_object_material_slot_add() changes the active material to the new one, which is the
-       * unpinned material. We don't want to touch that, so set it back to what it was. */
-      ob->actcol = actcol_old;
     }
 
     return ma;
