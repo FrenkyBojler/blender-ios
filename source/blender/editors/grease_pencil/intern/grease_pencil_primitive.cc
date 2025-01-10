05d35b5840878703b026ffe9350dc6778fd16834
Lukas Tönne <lukas@blender.org>
2025-01-10 11:16:00 +0100

    Remove unused code.

--- source/blender/editors/grease_pencil/intern/grease_pencil_primitive.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_primitive.cc
@@ -701,9 +701,6 @@ static int grease_pencil_primitive_invoke(bContext *C, wmOperator *op, const wmE
   }
   else if (placement.use_project_to_stroke()) {
     placement.cache_viewport_depths(CTX_data_depsgraph_pointer(C), vc.region, view3d);
-    // placement.set_origin_to_nearest_stroke(start_coords);
-    if (std::optional<float3> loc = placement.project_depth(start_coords)) {
-    }
   }
 
   ptd.placement = placement;
