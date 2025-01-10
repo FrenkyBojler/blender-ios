f044e1a4b8ae1b4d3cdf0ac2106868b328415da8
Lukas Tönne <lukas@blender.org>
2025-01-10 10:12:09 +0100

    Support vertex group weights when changing spline types.

--- source/blender/geometry/intern/subdivide_curves.cc
+++ source/blender/geometry/intern/subdivide_curves.cc
@@ -380,10 +380,10 @@ bke::CurvesGeometry subdivide_curves(const bke::CurvesGeometry &src_curves,
     });
 
     /* Filter out positions and handles that are already interpolated. */
-    const Set<std::string> skip_attributes = {
+    const Set<std::string> attributes_to_skip = {
         "position", "handle_type_left", "handle_type_right", "handle_right", "handle_left"};
     for (auto &attribute : attributes_to_transfer) {
-      if (skip_attributes.contains(attribute.name)) {
+      if (attributes_to_skip.contains(attribute.name)) {
         continue;
       }
       subdivide_attribute_linear(src_points_by_curve,
