a734cea45253b26493cdd8e3234bda5416399ffb
Bastien Montagne <bastien@blender.org>
2025-01-10 16:22:28 +0100

    Refactor: RNA definition of Boolean bitset/bitflags properties.
    Existing code had several issues:
    * The 'DNA bitset as RNA array' behavior was totally undocumented,
      virtually impossible to discover, very confusing, and without any
      checks that given parameters (like the array length) were valid.
    * The DNA defaults handling was fully broken for 'negative' boolean
      properties.
    
    This commit:
    * Forbids calling `RNA_def_property_array` on a Boolean property with a
      non-null `booleanbit` value.
    * Introduces a new `RNA_def_property_boolean_bitset_array_sdna`,
      strictly to define 'DNA bitsets as RNA array' properties.
    * Adds several validation checks, in particular regarding the bitset
      case. In particular, it takes into account the fact that bitshift
      operations on negative numbers are
      [typically arithmetic ones](https://en.cppreference.com/w/cpp/language/operator_arithmetic),
      which means that they preserve the sign of the value, and that the
      left-most bit should never be used with signed integers for bitsets.
    
    -------------------
    
    The fix to DNA defaults handling on 'negative' properties revealed
    several RNA errors, listed below. Fixing them is not necessarily trivial
    (as some seem to contradict the DNA default values, e.g. the
    `View3DShading.use_studiolight_view_rotation` one), so it is kept
    outside of this refactor. Conflicting cases should likely be handled by
    the relevant modules (to decide whether the DNA, or the RNA default
    should be kept).
    
    ```
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "ObjectDisplay.show_shadows", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "View3DShading.use_studiolight_view_rotation", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PathCompareCollection.use_recent_searches", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesView.show_splash", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesView.use_text_antialiasing", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesEdit.use_auto_keying_warning", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesFilePaths.show_recent_locations", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesFilePaths.show_system_bookmarks", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesFilePaths.use_load_ui", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesFilePaths.use_tabs_as_spaces", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesSystem.use_edit_mode_smooth_wire", set from DNA.
    ERROR (rna.define): rna_define.cc:2043 RNA_def_property_boolean_default: "PreferencesSystem.use_select_pick_depth", set from DNA.
    ```
    
    -------------------
    
    Also realized that many DNA types are ignored when trying to find DNA
    default values. This will also be tackled separately, as fixing it is
    fairly straightforward.

--- source/blender/makesrna/RNA_define.hh
+++ source/blender/makesrna/RNA_define.hh
@@ -357,14 +357,38 @@ PropertyRNA *RNA_def_property(StructOrFunctionRNA *cont,
                               int type,
                               int subtype);
 
+/**
+ * Define a boolean property controlling one or more bitflags in the DNA member.
+ *
+ * \note This can be combined to a call to #RNA_def_property_array on the same property, in case
+ * the wrapped DNA member is an array of integers. Do not confuse it with defining a RNA boolean
+ * array property using a single DNA member as a bitset (use
+ * #RNA_def_property_boolean_bitset_array_sdna for this).
+ */
 void RNA_def_property_boolean_sdna(PropertyRNA *prop,
                                    const char *structname,
                                    const char *propname,
-                                   int64_t bit);
+                                   int64_t booleanbit);
 void RNA_def_property_boolean_negative_sdna(PropertyRNA *prop,
                                             const char *structname,
                                             const char *propname,
-                                            int64_t bit);
+                                            int64_t booleanbit);
+/**
+ * Used to define an array of boolean values using a single int/char/etc. member of a DNA struct
+ * (aka 'bitset array').
+ *
+ * The #booleanbit value should strictly have a single bit enabled (so typically come from a
+ * bit-shift expression like `1 << 0`), and be strictly positive (i.e. the left-most bit in the
+ * valid range should not be used). Multi-bit values are not valid. It will be used as first bit
+ * for the `0`-indexed item of the array.
+ *
+ * The maximum #len depends on the type of the DNA member, and the #booleanbit value. The left-most
+ * bit is not usable (because bitshift operations over signed negative values are typically
+ * 'arithmetic', and not 'bitwise', in C++). So e.g. `31` for an `int32_t` with a `booleanbit`
+ * value of `1 << 0`, and so on.
+ */
+void RNA_def_property_boolean_bitset_array_sdna(
+    PropertyRNA *prop, const char *structname, const char *propname, int64_t booleanbit, int len);
 void RNA_def_property_int_sdna(PropertyRNA *prop, const char *structname, const char *propname);
 void RNA_def_property_float_sdna(PropertyRNA *prop, const char *structname, const char *propname);
 void RNA_def_property_string_sdna(PropertyRNA *prop, const char *structname, const char *propname);
@@ -597,6 +621,15 @@ const char *RNA_property_typename(PropertyType type);
    strcmp(_str, "uchar") == 0 || strcmp(_str, "ushort") == 0 || strcmp(_str, "int8_t") == 0)
 #define IS_DNATYPE_BOOLEAN_COMPAT(_str) \
   (IS_DNATYPE_INT_COMPAT(_str) || strcmp(_str, "int64_t") == 0 || strcmp(_str, "uint64_t") == 0)
+
+/* Several types cannot use all their bytes to store a bitset (bitshift operations on negative
+ * numbers are 'arithmetic', i.e. preserve the sign, i.e. are not 'pure' binary shifting).
+ *
+ * Currently, all signed types and uint64_t cannot use their left-most bit (i.e. sign bit). */
+#define IS_DNATYPE_BOOLEAN_BITSHIFT_FULLRANGE_COMPAT(_str) \
+  (strcmp(_str, "char") == 0 || strcmp(_str, "uchar") == 0 || strcmp(_str, "ushort") == 0 || \
+   strcmp(_str, "uint") == 0 || strcmp(_str, "uint8_t") == 0 || strcmp(_str, "uint16_t") == 0 || \
+   strcmp(_str, "uint32_t") == 0)
 
 void RNA_identifier_sanitize(char *identifier, int property);
 
