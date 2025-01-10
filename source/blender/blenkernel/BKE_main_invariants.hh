435e9d13a340208d70d017038159bae5a0f4444a
Jacques Lucke <jacques@blender.org>
2025-01-10 13:11:47 +0100

    improve comment

--- source/blender/blenkernel/BKE_main_invariants.hh
+++ source/blender/blenkernel/BKE_main_invariants.hh
@@ -20,10 +20,14 @@ struct ID;
  * This has to be called after any kind of change to original DNA data that may be involved in some
  * of the maintained invariants. It's possible to do multiple changes in a row and then fixing all
  * invariants with a single call in the end. Obviously, the invariants are not maintained in the
- * meantime then.
+ * meantime then and functions relying on them might not work.
  *
  * If nothing is changed, this function does nothing and it should not be slower than checking a
  * flag on every data-block in the given bmain.
+ *
+ * Sometimes, it is known that only a single or very few data-blocks have been changed (e.g. when a
+ * node has been inserted in a node tree). Passing in #modified_ids can speed up the function
+ * because it may avoid the need to iterate over all data-blocks to find modified data-blocks.
  *
  * Examples of maintained invariants:
  * - Group nodes need to have the correct sockets based on the referenced node group.
