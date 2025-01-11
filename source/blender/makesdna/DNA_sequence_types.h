ea3b1ea30977b3176636eb015a94bf2992b99521
John Kiril Swenson <kirilswenson@gmail.com>
2025-01-10 19:43:03 -0600

    Cleanup: VSE: Rename `SeqConnection` to `StripConnection`

--- source/blender/makesdna/DNA_sequence_types.h
+++ source/blender/makesdna/DNA_sequence_types.h
@@ -241,7 +241,7 @@ typedef struct Strip {
   ListBase channels; /* SeqTimelineChannel */
 
   /* List of strip connections (one-way, not bidirectional). */
-  ListBase connections; /* SeqConnection */
+  ListBase connections; /* StripConnection */
 
   /** The linked "bSound" object. */
   struct bSound *sound;
@@ -317,10 +317,10 @@ typedef struct SeqTimelineChannel {
   int flag;
 } SeqTimelineChannel;
 
-typedef struct SeqConnection {
-  struct SeqConnection *next, *prev;
+typedef struct StripConnection {
+  struct StripConnection *next, *prev;
   Strip *seq_ref;
-} SeqConnection;
+} StripConnection;
 
 typedef struct EditingRuntime {
   struct StripLookup *strip_lookup;
