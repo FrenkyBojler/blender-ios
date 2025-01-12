2c1741fef1a87980f7e4784a8b6fd28e3aa4fcb7
Nicola <nicdessi@gmail.com>
2025-01-12 04:13:08 +0100

    fix paint brush

--- source/blender/editors/sculpt_paint/sculpt_paint_color.cc
+++ source/blender/editors/sculpt_paint/sculpt_paint_color.cc
@@ -460,8 +460,10 @@ static void do_paint_brush_task(const Scene &scene,
   Vector<float4> brush_colors(verts.size());
   brush_colors.fill(brush_color);
 
-  paint_sculpt_nodes_evaluate(
-      depsgraph, object, brush, *ss.cache, vert_positions, verts, brush_colors, new_colors);
+  if (brush.node_group) {
+    paint_sculpt_nodes_evaluate(
+        depsgraph, object, brush, *ss.cache, vert_positions, verts, brush_colors, new_colors);
+  }
 
   for (const int i : verts.index_range()) {
     /* Brush paint color, brush test falloff and flow. */
