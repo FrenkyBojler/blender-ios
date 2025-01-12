98d9358add4a417c434f2dea3af206875f07ed26
Dangry98 <danielgryning@gmail.com>
2025-01-12 16:12:26 +0100

    Revert commit

--- source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
@@ -229,16 +229,16 @@ class LinearGizmo : public NodeGizmos {
   void update_style(const NodeGeometryLinearGizmo &storage)
   {
     /* Make sure the enum values are in sync. */
-	    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_ARROW) == int(ED_GIZMO_ARROW_STYLE_NORMAL));
-	    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) == int(ED_GIZMO_ARROW_STYLE_BOX));
-	    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_CROSS) == int(ED_GIZMO_ARROW_STYLE_CROSS));
-	    RNA_enum_set(gizmo_->ptr, "draw_style", storage.draw_style);
+    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_ARROW) == int(ED_GIZMO_ARROW_STYLE_NORMAL));
+    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) == int(ED_GIZMO_ARROW_STYLE_BOX));
+    static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_CROSS) == int(ED_GIZMO_ARROW_STYLE_CROSS));
+    RNA_enum_set(gizmo_->ptr, "draw_style", storage.draw_style);
 
-      WM_gizmo_set_line_width(gizmo_, 1.0f);		  
+    WM_gizmo_set_line_width(gizmo_, 1.0f);		  
 
-      const float length = (storage.draw_style == GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) ? 0.8f :		
-                                                                                      1.0f;
-      RNA_float_set(gizmo_->ptr, "length", length);
+    const float length = (storage.draw_style == GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) ? 0.8f :		
+                                                                                    1.0f;
+    RNA_float_set(gizmo_->ptr, "length", length);
 
     const ThemeColorID color_theme_id = get_gizmo_theme_color_id(
         GeometryNodeGizmoColor(storage.color_id));
