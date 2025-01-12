8642367de103d9e11393c47669adf9043aa63b2b
Dangry98 <danielgryning@gmail.com>
2025-01-12 16:13:13 +0100

    Revert Commit

--- source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
+++ source/blender/editors/space_view3d/view3d_gizmo_geometry_nodes.cc
@@ -234,9 +234,9 @@ class LinearGizmo : public NodeGizmos {
     static_assert(int(GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_CROSS) == int(ED_GIZMO_ARROW_STYLE_CROSS));
     RNA_enum_set(gizmo_->ptr, "draw_style", storage.draw_style);
 
-    WM_gizmo_set_line_width(gizmo_, 1.0f);		  
+    WM_gizmo_set_line_width(gizmo_, 1.0f);
 
-    const float length = (storage.draw_style == GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) ? 0.8f :		
+    const float length = (storage.draw_style == GEO_NODE_LINEAR_GIZMO_DRAW_STYLE_BOX) ? 0.8f :
                                                                                     1.0f;
     RNA_float_set(gizmo_->ptr, "length", length);
 
