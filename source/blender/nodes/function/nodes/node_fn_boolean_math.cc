8eda34a5ec1e7ac9e58ee292be30af2ea42b2808
Jacques Lucke <jacques@blender.org>
2025-01-13 18:53:36 +0100

    function nodes

--- source/blender/nodes/function/nodes/node_fn_boolean_math.cc
+++ source/blender/nodes/function/nodes/node_fn_boolean_math.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BKE_node.hh"
 #include "BLI_listbase.h"
 #include "BLI_string.h"
 #include "BLI_string_utf8.h"
@@ -190,9 +191,10 @@ static void node_register()
 {
   static blender::bke::bNodeType ntype;
 
-  fn_node_type_base(&ntype, "FunctionNodeBooleanMath", FN_NODE_BOOLEAN_MATH, NODE_CLASS_CONVERTER);
+  fn_node_type_base(&ntype, "FunctionNodeBooleanMath", FN_NODE_BOOLEAN_MATH);
   ntype.ui_name = "Boolean Math";
   ntype.enum_name_legacy = "BOOLEAN_MATH";
+  ntype.nclass = NODE_CLASS_CONVERTER;
   ntype.declare = node_declare;
   ntype.labelfunc = node_label;
   ntype.updatefunc = node_update;
