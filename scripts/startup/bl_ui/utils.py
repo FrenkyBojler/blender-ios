c4c7ea56d796d66d311b0a20028ffdafe5a9ae7e
Nathan Vegdahl <cessen@cessen.com>
2025-01-16 12:17:56 +0100

    Anim: add Action+Slot selector for Masks
    This adds an Animation panel to the Mask tab of the n-panel of the
    Movie Clip and Image editors. Masks can be animated (for example, the
    alpha of a Mask Layer), but there was no way to manage the Action and
    Slot that held those F-Curves.
    
    To keep things DRY, this PR also moves the code for drawing Action+Slot
    selectors from the `PropertiesAnimationMixin` class to a utility
    function, which is now called from both that class and the Mask UI code.

--- scripts/startup/bl_ui/utils.py
+++ scripts/startup/bl_ui/utils.py
@@ -38,3 +38,30 @@ class PresetPanel:
         layout.operator_context = 'EXEC_DEFAULT'
 
         Menu.draw_preset(self, context)
+
+
+def draw_action_and_slot_selector_for_id(layout, animated_id):
+    """ Draw the action and slot selector for the provided ID, using the given
+        UI layout.
+
+        The ID must be an animatable ID.
+
+        Note that the slot selector is only drawn when the ID has an assigned
+        Action.
+    """
+
+    layout.template_action(animated_id, new="action.new", unlink="action.unlink")
+
+    adt = animated_id.animation_data
+    if not adt or not adt.action:
+        return
+
+    # Only show the slot selector when a layered Action is assigned.
+    if adt.action.is_action_layered:
+        layout.context_pointer_set("animated_id", animated_id)
+        layout.template_search(
+            adt, "action_slot",
+            adt, "action_suitable_slots",
+            new="anim.slot_new_for_id",
+            unlink="anim.slot_unassign_from_id",
+        )
