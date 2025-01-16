47e66bc993ec187bbec90ea2c9f65d54507fa375
Bastien Montagne <bastien@blender.org>
2025-01-16 14:50:10 +0100

    Cleanup.

--- tests/python/bl_blendfile_relationships.py
+++ tests/python/bl_blendfile_relationships.py
@@ -139,8 +139,6 @@ class TestBlendFilePathMap(TestBlendLibLinkHelper):
         }
         for k, v in expected_map.items():
             assert k in file_path_map
-            print(file_path_map[k])
-            print(v)
             assert file_path_map[k] == v
 
         file_path_map = abspaths(bpy.data.file_path_map(include_libraries=True))
