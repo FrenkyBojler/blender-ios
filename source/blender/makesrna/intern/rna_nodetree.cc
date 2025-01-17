67b98120f91c4526063bf6ac7a310d6511252e7f
Omar Emara <mail@OmarEmara.dev>
2025-01-17 11:01:19 +0200

    Use existing RNA enum list

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -7019,19 +7019,12 @@ static void def_cmp_set_alpha(BlenderRNA * /*brna*/, StructRNA *srna)
 
 static void def_cmp_set_realization_options(BlenderRNA * /*brna*/, StructRNA *srna)
 {
-  static const EnumPropertyItem interpolation_items[] = {
-      {CMP_NODE_INTERPOLATION_NEAREST, "Nearest", 0, "Nearest", "Use nearest interpolation"},
-      {CMP_NODE_INTERPOLATION_BILINEAR, "Bilinear", 0, "Bilinear", "Use bilinear interpolation"},
-      {CMP_NODE_INTERPOLATION_BICUBIC, "Bicubic", 0, "Bicubic", "Use bicubic interpolation"},
-      {0, nullptr, 0, nullptr, nullptr},
-  };
-
   RNA_def_struct_sdna_from(srna, "NodeSetRealizationOptionsData", "storage");
 
   PropertyRNA *prop;
   prop = RNA_def_property(srna, "interpolation", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_sdna(prop, nullptr, "interpolation");
-  RNA_def_property_enum_items(prop, interpolation_items);
+  RNA_def_property_enum_items(prop, cmp_interpolation_items);
   RNA_def_property_ui_text(prop, "Interpolation", "Interpolation to use when realizing the image");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_update");
 
