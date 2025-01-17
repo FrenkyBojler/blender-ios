99a6077779e6a36a28a23758c55f81fa363703c7
Pratik Borhade <pratikborhade302@gmail.com>
2025-01-17 14:24:00 +0530

    Bring back layout_type

--- scripts/startup/bl_ui/properties_material_gpencil.py
+++ scripts/startup/bl_ui/properties_material_gpencil.py
@@ -48,21 +48,22 @@ class GPENCIL_UL_matslots(UIList):
         if (ma is not None) and (ma.grease_pencil is not None):
             gpcolor = ma.grease_pencil
 
-        row = layout.row(align=True)
-        row.enabled = not gpcolor.lock
-        row.prop(ma, "name", text="", emboss=False, icon_value=icon)
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            row = layout.row(align=True)
+            row.enabled = not gpcolor.lock
+            row.prop(ma, "name", text="", emboss=False, icon_value=icon)
 
-        row = layout.row(align=True)
+            row = layout.row(align=True)
 
-        if gpcolor.ghost is True:
-            icon = 'ONIONSKIN_OFF'
-        else:
-            icon = 'ONIONSKIN_ON'
-        row.prop(gpcolor, "ghost", text="", icon=icon, emboss=False)
-        row.prop(gpcolor, "hide", text="", emboss=False)
-        row.prop(gpcolor, "lock", text="", emboss=False)
+            if gpcolor.ghost is True:
+                icon = 'ONIONSKIN_OFF'
+            else:
+                icon = 'ONIONSKIN_ON'
+            row.prop(gpcolor, "ghost", text="", icon=icon, emboss=False)
+            row.prop(gpcolor, "hide", text="", emboss=False)
+            row.prop(gpcolor, "lock", text="", emboss=False)
 
-        if self.layout_type == 'GRID':
+        elif self.layout_type == 'GRID':
             layout.alignment = 'CENTER'
             layout.label(text="", icon_value=icon)
 
