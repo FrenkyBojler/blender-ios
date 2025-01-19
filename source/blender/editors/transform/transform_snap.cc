c007d0bf6df05e10937e4b72e979fb02e75cb1b5
Richard Antalik <richardantalik@gmail.com>
2025-01-19 20:20:46 +0100

    Fix #133167: Scene snapping settings confilict with VSE
    Enabling `use_snap_rotate` in 3D viewport caused rotation in sequencer
    to apply snapping. Rotation snapping was using mixed snapping functions,
    which aren't implemented in sequencer.
    
    Ignore affect rotation/scale tool setting property and force increment
    snapping to be used with these transform modes.

--- source/blender/editors/transform/transform_snap.cc
+++ source/blender/editors/transform/transform_snap.cc
@@ -10,6 +10,7 @@
 #include "BLI_math_rotation.h"
 #include "BLI_time.h"
 
+#include "DNA_space_types.h"
 #include "GPU_immediate.hh"
 #include "GPU_matrix.hh"
 
@@ -148,16 +149,12 @@ bool transform_snap_is_active(const TransInfo *t)
 
 bool transformModeUseSnap(const TransInfo *t)
 {
-  /* The animation editors should not depend on the snapping options of the 3D viewport. */
-  if (ELEM(t->spacetype, SPACE_ACTION, SPACE_GRAPH, SPACE_NLA)) {
+  /* The VSE and animation editors should not depend on the snapping options of the 3D viewport. */
+  if (ELEM(t->spacetype, SPACE_ACTION, SPACE_GRAPH, SPACE_NLA, SPACE_SEQ)) {
     return true;
   }
   ToolSettings *ts = t->settings;
   if (t->mode == TFM_TRANSLATION) {
-    /* VSE preview snapping should also not depend on the 3D viewport. */
-    if (t->spacetype == SPACE_SEQ) {
-      return true;
-    }
     return (ts->snap_transform_mode_flag & SCE_SNAP_TRANSFORM_MODE_TRANSLATE) != 0;
   }
   if (t->mode == TFM_ROTATION) {
@@ -185,6 +182,11 @@ static bool doForceIncrementSnap(const TransInfo *t)
     /* These spaces don't support increment snapping. */
     return false;
   }
+
+  if (t->spacetype == SPACE_SEQ && ELEM(t->mode, TFM_ROTATION, TFM_RESIZE)) {
+    return true;
+  }
+
   if (t->modifiers & MOD_SNAP_FORCED) {
     return false;
   }
