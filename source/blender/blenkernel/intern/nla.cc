170a94a6a525a8a447d4c1a40ad61ffd38b53cb3
Nathan Vegdahl <cessen@cessen.com>
2025-01-20 12:57:39 +0100

    Fix #129540: allow stashing same Action with different slots

--- source/blender/blenkernel/intern/nla.cc
+++ source/blender/blenkernel/intern/nla.cc
@@ -2115,12 +2115,12 @@ void BKE_nla_validate_state(AnimData *adt)
 /* name of stashed tracks - the translation stuff is included here to save extra work */
 #define STASH_TRACK_NAME DATA_("[Action Stash]")
 
-bool BKE_nla_action_is_stashed(AnimData *adt, bAction *act)
+bool BKE_nla_action_is_stashed(AnimData *adt, bAction *act, int32_t slot_handle)
 {
   LISTBASE_FOREACH (NlaTrack *, nlt, &adt->nla_tracks) {
     if (strstr(nlt->name, STASH_TRACK_NAME)) {
       LISTBASE_FOREACH (NlaStrip *, strip, &nlt->strips) {
-        if (strip->act == act) {
+        if (strip->act == act && strip->action_slot_handle == slot_handle) {
           return true;
         }
       }
@@ -2144,7 +2144,7 @@ bool BKE_nla_action_stash(const OwnedAnimData owned_adt, const bool is_liboverri
   }
 
   /* do not add if it is already stashed */
-  if (BKE_nla_action_is_stashed(adt, adt->action)) {
+  if (BKE_nla_action_is_stashed(adt, adt->action, adt->slot_handle)) {
     return false;
   }
 
