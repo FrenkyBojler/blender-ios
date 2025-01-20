ace64be1db9e3a76d20b4c782c85ba7fedf4800c
Nicola <nicdessi@gmail.com>
2025-01-20 16:18:12 +0100

    add versioning for stabilizers

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -5625,7 +5625,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
       }
     }
   }
-  
+
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 404, 22)) {
     LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
       IDProperty *cscene = version_cycles_properties_from_ID(&scene->id);
@@ -5647,11 +5647,11 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
   }
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 404, 24)) {
-    LISTBASE_FOREACH(Brush*, brush, &bmain->brushes) {
+    LISTBASE_FOREACH (Brush *, brush, &bmain->brushes) {
       if (ELEM(brush->sculpt_brush_type,
-        SCULPT_BRUSH_TYPE_FLATTEN,
-        SCULPT_BRUSH_TYPE_FILL,
-        SCULPT_BRUSH_TYPE_SCRAPE))
+               SCULPT_BRUSH_TYPE_FLATTEN,
+               SCULPT_BRUSH_TYPE_FILL,
+               SCULPT_BRUSH_TYPE_SCRAPE))
       {
         if (brush->sculpt_brush_type == SCULPT_BRUSH_TYPE_FLATTEN) {
           brush->plane_height = 1.0f;
@@ -5664,22 +5664,27 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
           brush->plane_height = 0.0f;
           brush->plane_depth = 1.0f;
           brush->plane_inversion_mode = brush->flag & BRUSH_INVERT_TO_SCRAPE_FILL ?
-            BRUSH_PLANE_SWAP_HEIGHT_AND_DEPTH :
-            BRUSH_PLANE_INVERT_DISPLACEMENT;
+                                            BRUSH_PLANE_SWAP_HEIGHT_AND_DEPTH :
+                                            BRUSH_PLANE_INVERT_DISPLACEMENT;
         }
 
         if (brush->sculpt_brush_type == SCULPT_BRUSH_TYPE_SCRAPE) {
           brush->plane_height = 1.0f;
           brush->plane_depth = 0.0f;
           brush->plane_inversion_mode = brush->flag & BRUSH_INVERT_TO_SCRAPE_FILL ?
-            BRUSH_PLANE_SWAP_HEIGHT_AND_DEPTH :
-            BRUSH_PLANE_INVERT_DISPLACEMENT;
+                                            BRUSH_PLANE_SWAP_HEIGHT_AND_DEPTH :
+                                            BRUSH_PLANE_INVERT_DISPLACEMENT;
         }
 
         if (brush->flag & BRUSH_PLANE_TRIM) {
           brush->plane_height *= brush->plane_trim;
           brush->plane_depth *= brush->plane_trim;
         }
+
+        brush->stabilize_normal = (brush->flag & BRUSH_ORIGINAL_NORMAL) ? 1.0f : 0.0f;
+        brush->stabilize_plane = (brush->flag & BRUSH_ORIGINAL_PLANE) ? 1.0f : 0.0f;
+        brush->flag &= ~BRUSH_ORIGINAL_NORMAL;
+        brush->flag &= ~BRUSH_ORIGINAL_PLANE;
 
         brush->sculpt_brush_type = SCULPT_BRUSH_TYPE_PLANE;
       }
