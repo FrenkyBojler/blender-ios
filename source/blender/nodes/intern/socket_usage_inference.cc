2ce5f7d3460bdf58b8969c5aace0dc1d4bf10a2b
Jacques Lucke <jacques@blender.org>
2025-01-21 13:26:07 +0100

    initial mix node support

--- source/blender/nodes/intern/socket_usage_inference.cc
+++ source/blender/nodes/intern/socket_usage_inference.cc
@@ -179,6 +179,10 @@ struct SocketUsageInferencer {
         this->usage_task__input__generic_switch(socket, menu_switch__is_socket_selected);
         break;
       }
+      case SH_NODE_MIX: {
+        this->usage_task__input__generic_switch(socket, mix_node__is_socket_selected);
+        break;
+      }
       case GEO_NODE_SIMULATION_INPUT: {
         this->usage_task__input__simulation_input_node(socket);
         break;
@@ -217,13 +221,14 @@ struct SocketUsageInferencer {
   {
     const NodeInContext node = socket.owner_node();
     BLI_assert(node->input_sockets().size() >= 1);
-    BLI_assert(node->output_sockets().size() == 1);
+    BLI_assert(node->output_sockets().size() >= 1);
 
     if (socket->type == SOCK_CUSTOM && STREQ(socket->idname, "NodeSocketVirtual")) {
       all_socket_usages_.add_new(socket, false);
       return;
     }
-    const SocketInContext output_socket = node.output_socket(0);
+    const SocketInContext output_socket{socket.context,
+                                        this->get_first_available_bsocket(node->output_sockets())};
     const std::optional<bool> output_is_used = all_socket_usages_.lookup_try(output_socket);
     if (!output_is_used.has_value()) {
       this->push_usage_task(output_socket);
@@ -233,7 +238,8 @@ struct SocketUsageInferencer {
       all_socket_usages_.add_new(socket, false);
       return;
     }
-    const SocketInContext condition_socket = node.input_socket(0);
+    const SocketInContext condition_socket{
+        socket.context, this->get_first_available_bsocket(node->input_sockets())};
     if (socket == condition_socket) {
       all_socket_usages_.add_new(socket, true);
       return;
@@ -246,6 +252,16 @@ struct SocketUsageInferencer {
     }
     const bool is_used = is_selected_socket(socket, condition_value);
     all_socket_usages_.add_new(socket, is_used);
+  }
+
+  const bNodeSocket *get_first_available_bsocket(const Span<const bNodeSocket *> sockets) const
+  {
+    for (const bNodeSocket *socket : sockets) {
+      if (socket->is_available()) {
+        return socket;
+      }
+    }
+    return nullptr;
   }
 
   void usage_task__input__group_node(const SocketInContext &socket)
@@ -766,6 +782,35 @@ struct SocketUsageInferencer {
     const int menu_value = *static_cast<const int *>(condition);
     const NodeEnumItem &item = storage.enum_definition.items_array[socket->index() - 1];
     return menu_value == item.identifier;
+  }
+
+  static bool mix_node__is_socket_selected(const SocketInContext &socket, const void *condition)
+  {
+    const NodeShaderMix &storage = *static_cast<const NodeShaderMix *>(
+        socket.owner_node()->storage);
+    bool only_a = false;
+    bool only_b = false;
+    if (storage.data_type == SOCK_VECTOR && storage.factor_mode == NODE_MIX_MODE_NON_UNIFORM) {
+      const float3 mix_factor = *static_cast<const float3 *>(condition);
+      only_a = float3{0.0f, 0.0f, 0.0f} == mix_factor;
+      only_b = float3{1.0f, 1.0f, 1.0f} == mix_factor;
+    }
+    else {
+      const float mix_factor = *static_cast<const float *>(condition);
+      only_a = mix_factor == 0.0f;
+      only_b = mix_factor == 1.0f;
+    }
+    if (only_a) {
+      if (STREQ(socket->name, "B")) {
+        return false;
+      }
+    }
+    if (only_b) {
+      if (STREQ(socket->name, "A")) {
+        return false;
+      }
+    }
+    return true;
   }
 
   void push_usage_task(const SocketInContext &socket)
