4038ad6a7381c108d31598606cc35c6cf1c4b92a
YimingWu <xp8110@outlook.com>
2025-01-22 12:25:27 +0800

    Fix #133414: Sequencer: Ensure valid UTF-8 sequence when pasting text
    `TextVars::text` is only 512 bytes and does not include string ending
    byte, and `sequencer_text_edit_paste_exec` uses a very simple truncation
    that didn't guarantee valid UTF-8 sequence at the end. This is now fixed
    by using `BLI_str_utf8_invalid_strip` after truncating the string.

--- source/blender/editors/space_sequencer/sequencer_text_edit.cc
+++ source/blender/editors/space_sequencer/sequencer_text_edit.cc
@@ -820,14 +820,18 @@ static int sequencer_text_edit_paste_exec(bContext *C, wmOperator * /*op*/)
   }
 
   const int max_str_len = sizeof(data->text) - (BLI_strnlen(data->text, sizeof(data->text)) + 1);
-  clipboard_len = std::min(clipboard_len, max_str_len);
+
+  /* Truncate and validate UTF-8 sequence. */
+  int truncated_len = std::min(clipboard_len, max_str_len);
+  clipboard_buf[truncated_len] = '\0';
+  truncated_len -= BLI_str_utf8_invalid_strip(clipboard_buf, truncated_len);
 
   const seq::CharInfo cur_char = character_at_cursor_offset_get(text, data->cursor_offset);
   char *cursor_addr = const_cast<char *>(cur_char.str_ptr);
   const size_t move_str_len = BLI_strnlen(cursor_addr, sizeof(data->text)) + 1;
 
-  std::memmove(cursor_addr + clipboard_len, cursor_addr, move_str_len);
-  std::memcpy(cursor_addr, clipboard_buf, clipboard_len);
+  std::memmove(cursor_addr + truncated_len, cursor_addr, move_str_len);
+  std::memcpy(cursor_addr, clipboard_buf, truncated_len);
 
   data->cursor_offset += BLI_strlen_utf8(clipboard_buf);
 
