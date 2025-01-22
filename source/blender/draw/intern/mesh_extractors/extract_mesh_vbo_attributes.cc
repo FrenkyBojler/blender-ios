65efd135c70202da7d6c22f3d7877422e5966a97
Hans Goudey <hans@blender.org>
2025-01-22 18:23:22 -0500

    Cleanup

--- source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_attributes.cc
+++ source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_attributes.cc
@@ -263,18 +263,9 @@ void extract_attributes(const MeshRenderData &mr,
                         const Span<StringRef> requests,
                         const Span<gpu::VertBuf *> vbos)
 {
-  if (mr.extract_type == MeshExtractType::BMesh) {
-    for (const int i : vbos.index_range()) {
-      if (DRW_vbo_requested(vbos[i])) {
-        extract_attribute(mr, requests[i], *vbos[i]);
-      }
-    }
-  }
-  else {
-    for (const int i : vbos.index_range()) {
-      if (DRW_vbo_requested(vbos[i])) {
-        extract_attribute(mr, requests[i], *vbos[i]);
-      }
+  for (const int i : vbos.index_range()) {
+    if (DRW_vbo_requested(vbos[i])) {
+      extract_attribute(mr, requests[i], *vbos[i]);
     }
   }
 }
