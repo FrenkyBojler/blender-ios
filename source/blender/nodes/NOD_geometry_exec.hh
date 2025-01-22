6e77ba185856aca48f824becfeea944ff58a3140
Nicola <nicdessi@gmail.com>
2025-01-22 07:48:15 +0100

    fix Active Camera node

--- source/blender/nodes/NOD_geometry_exec.hh
+++ source/blender/nodes/NOD_geometry_exec.hh
@@ -244,6 +244,9 @@ class GeoNodeExecParams {
       if (data->call_data->operator_data) {
         return data->call_data->operator_data->depsgraphs->active;
       }
+      if (data->call_data->sculpt_data) {
+        return data->call_data->sculpt_data->depsgraph;
+      }
     }
     return nullptr;
   }
