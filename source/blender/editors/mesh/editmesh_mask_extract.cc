6e3c3fb67d55f6a27291ab7ad46c13409c848c07
Philipp Oeser <philipp@blender.org>
2025-01-22 13:57:25 +0100

    Sculpt: avoid adding ".sculpt_mask" attributes in certain cases
    Mask layers were added (unnecessarily) in these cases:
    - calling `Mask Slice` without having a mask even (now return
    `OPERATOR_CANCELLED` early)
    - calling `Mask Extract` without having a mask even (now return
    `OPERATOR_CANCELLED` early)
    - `Extract Face Set` was also adding a mask layer (only because both
    extracts used `geometry_extract_apply`), moved mask specific stuff into
    `paint_mask_extract_exec` (see above also)
    
    Came up in !123888

--- source/blender/editors/mesh/editmesh_mask_extract.cc
+++ source/blender/editors/mesh/editmesh_mask_extract.cc
@@ -87,8 +87,6 @@ static int geometry_extract_apply(bContext *C,
 
   blender::ed::sculpt_paint::object_sculpt_mode_exit(C, depsgraph);
 
-  BKE_sculpt_mask_layers_ensure(&depsgraph, bmain, ob, nullptr);
-
   /* Ensures that deformation from sculpt mode is taken into account before duplicating the mesh to
    * extract the geometry. */
   CTX_data_ensure_evaluated_depsgraph(C);
@@ -279,6 +277,14 @@ static void geometry_extract_tag_face_set(BMesh *bm, GeometryExtractParams *para
 
 static int paint_mask_extract_exec(bContext *C, wmOperator *op)
 {
+  Object *ob = CTX_data_active_object(C);
+  Mesh *mesh = static_cast<Mesh *>(ob->data);
+  if (!mesh->attributes().contains(".sculpt_mask")) {
+    return OPERATOR_CANCELLED;
+  }
+
+  BKE_sculpt_mask_layers_ensure(CTX_data_depsgraph_on_load(C), CTX_data_main(C), ob, nullptr);
+
   GeometryExtractParams params;
   params.mask_threshold = RNA_float_get(op->ptr, "mask_threshold");
   params.num_smooth_iterations = RNA_int_get(op->ptr, "smooth_iterations");
@@ -462,6 +468,11 @@ static int paint_mask_slice_exec(bContext *C, wmOperator *op)
   Main &bmain = *CTX_data_main(C);
   Object &ob = *CTX_data_active_object(C);
   View3D *v3d = CTX_wm_view3d(C);
+  Mesh *mesh = static_cast<Mesh *>(ob.data);
+
+  if (!mesh->attributes().contains(".sculpt_mask")) {
+    return OPERATOR_CANCELLED;
+  }
 
   BKE_sculpt_mask_layers_ensure(nullptr, nullptr, &ob, nullptr);
 
@@ -469,7 +480,6 @@ static int paint_mask_slice_exec(bContext *C, wmOperator *op)
   bool fill_holes = RNA_boolean_get(op->ptr, "fill_holes");
   float mask_threshold = RNA_float_get(op->ptr, "mask_threshold");
 
-  Mesh *mesh = static_cast<Mesh *>(ob.data);
   Mesh *new_mesh = (Mesh *)BKE_id_copy(&bmain, &mesh->id);
 
   /* Undo crashes when new object is created in the middle of a sculpt, see #87243. */
