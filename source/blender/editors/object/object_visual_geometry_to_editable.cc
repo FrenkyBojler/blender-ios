092523475f12b2b6274910f687abc474aef67800
Jacques Lucke <jacques@blender.org>
2025-01-23 21:00:35 +0100

    initial object generation

--- source/blender/editors/object/object_visual_geometry_to_editable.cc
+++ source/blender/editors/object/object_visual_geometry_to_editable.cc
@@ -3,17 +3,94 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "BKE_context.hh"
+#include "BKE_geometry_set.hh"
+#include "BKE_geometry_set_instances.hh"
+#include "BKE_instances.hh"
+#include "BKE_layer.hh"
+#include "BKE_lib_id.hh"
+#include "BKE_material.hh"
+
+#include "BKE_mesh.h"
+#include "BKE_object.hh"
+#include "DEG_depsgraph_query.hh"
+#include "DNA_mesh_types.h"
 
 #include "ED_screen.hh"
 
+#include "WM_api.hh"
 #include "WM_types.hh"
+
+#include "BLI_map.hh"
 
 namespace blender::ed::object {
 
+class GeometryToEditableOp {
+ private:
+  Main &bmain_;
+  Map<const ID *, Object *> new_object_by_generated_geometry_;
+  Map<bke::InstanceReference, Collection *> collection_by_instance_;
+
+ public:
+  GeometryToEditableOp(Main &bmain) : bmain_(bmain) {}
+
+  Object *build_object_for_geometry(const Object &src_ob_eval, bke::GeometrySet geometry)
+  {
+    Object *mesh_ob = nullptr;
+    if (const Mesh *mesh = geometry.get_mesh_for_write()) {
+      mesh_ob = this->get_or_create_object_for_mesh(src_ob_eval, *mesh, geometry.name);
+    }
+    return mesh_ob;
+  }
+
+ private:
+  Object *get_or_create_object_for_mesh(const Object & /*src_ob_eval*/,
+                                        const Mesh &src_mesh,
+                                        const StringRefNull name)
+  {
+    return new_object_by_generated_geometry_.lookup_or_add_cb(&src_mesh.id, [&]() {
+      Mesh *new_mesh = reinterpret_cast<Mesh *>(BKE_id_new(&bmain_, ID_ME, name.c_str()));
+      Object *new_ob = BKE_object_add_only_object(&bmain_, OB_MESH, name.c_str());
+      new_ob->data = new_mesh;
+
+      Mesh *mesh_to_move_from = BKE_mesh_copy_for_eval(src_mesh);
+      BKE_mesh_nomain_to_mesh(mesh_to_move_from, new_mesh, new_ob);
+      new_mesh->attributes_for_write().remove_anonymous();
+      /* TODO: #remove_invalid_attribute_strings */
+      /* TODO: #multires_customdata_delete */
+      return new_ob;
+    });
+  }
+};
+
 int visual_geometry_to_editable_exec(bContext *C, wmOperator * /*op*/)
 {
-  Object *ob = CTX_data_active_object(C);
+  Main &bmain = *CTX_data_main(C);
+  Scene &scene = *CTX_data_scene(C);
+  Depsgraph &depsgraph = *CTX_data_ensure_evaluated_depsgraph(C);
+  ViewLayer &view_layer = *CTX_data_view_layer(C);
+  LayerCollection &layer_collection = *BKE_layer_collection_get_active(&view_layer);
 
+  Object *src_ob_orig = CTX_data_active_object(C);
+  Object *src_ob_eval = DEG_get_evaluated_object(&depsgraph, src_ob_orig);
+  if (!src_ob_eval) {
+    return OPERATOR_CANCELLED;
+  }
+
+  GeometryToEditableOp op(bmain);
+
+  bke::GeometrySet geometry_eval = bke::object_get_evaluated_geometry_set(*src_ob_eval);
+  Object *new_ob = op.build_object_for_geometry(*src_ob_eval, geometry_eval);
+  BKE_collection_viewlayer_object_add(&bmain, &view_layer, layer_collection.collection, new_ob);
+
+  BKE_view_layer_base_deselect_all(&scene, &view_layer);
+  BKE_view_layer_synced_ensure(&scene, &view_layer);
+  if (Base *new_base = BKE_view_layer_base_find(&view_layer, new_ob)) {
+    BKE_view_layer_base_select_and_set_active(&view_layer, new_base);
+  }
+
+  DEG_id_tag_update(&new_ob->id, 0);
+  DEG_relations_tag_update(&bmain);
+  WM_event_add_notifier(C, NC_SCENE | ND_OB_SELECT, &scene);
   return OPERATOR_FINISHED;
 }
 
