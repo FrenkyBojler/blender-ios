fa76ce7af10fabf98b09551ea54d5c628cc90734
Jacques Lucke <jacques@blender.org>
2025-01-23 16:10:43 +0100

    initial boilerplate

--- source/blender/editors/object/object_ops.cc
+++ source/blender/editors/object/object_ops.cc
@@ -114,6 +114,7 @@ void operatortypes_object()
   WM_operatortype_append(OBJECT_OT_join);
   WM_operatortype_append(OBJECT_OT_join_shapes);
   WM_operatortype_append(OBJECT_OT_convert);
+  WM_operatortype_append(OBJECT_OT_visual_geometry_to_editable);
 
   WM_operatortype_append(OBJECT_OT_modifier_add);
   WM_operatortype_append(OBJECT_OT_modifier_remove);
---
+++ source/blender/editors/object/object_visual_geometry_to_editable.cc
@@ -0,0 +1,26 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "ED_screen.hh"
+
+#include "WM_types.hh"
+
+namespace blender::ed::object {
+
+int visual_geometry_to_editable_exec(bContext *C, wmOperator *op)
+{
+  return OPERATOR_FINISHED;
+}
+
+void OBJECT_OT_visual_geometry_to_editable(wmOperatorType *ot)
+{
+  ot->name = "Visual Geometry to Editable";
+  ot->description = "Convert geometry and instances into editable objects and collections";
+  ot->idname = "OBJECT_OT_visual_geometry_to_editable";
+
+  ot->exec = visual_geometry_to_editable_exec;
+  ot->poll = ED_operator_object_active;
+}
+
+}  // namespace blender::ed::object
