6a1f16d02a1724c4d55b64c35dfdcbe3cfa7d108
Pratik Borhade <pratikborhade302@gmail.com>
2025-01-24 14:44:16 +0530

    Fix #51739: Sliding values in add primitive redo panel not hiding cursor
    Cursor is actually wrapped at the beginning with the help of function
    `WM_cursor_grab_enable`. Though the `ND_MODE` event notifier added
    inside  `editmode_enter/exit_ex` further calls `region_cusor_set()`.
    This resets the cursor to default at the bottom of
    `ED_region_cursor_set()`. Such sitution could be avoided by creating a
    dummy cursor function for hud region.

--- source/blender/editors/interface/regions/interface_region_hud.cc
+++ source/blender/editors/interface/regions/interface_region_hud.cc
@@ -231,6 +231,11 @@ static void hud_region_listener(const wmRegionListenerParams *params)
   }
 }
 
+static void hud_region_cursor(wmWindow* /*win*/, ScrArea* /*area*/, ARegion* /*region*/)
+{
+  /* pass */
+}
+
 ARegionType *ED_area_type_hud(int space_type)
 {
   ARegionType *art = MEM_cnew<ARegionType>(__func__);
@@ -241,6 +246,7 @@ ARegionType *ED_area_type_hud(int space_type)
   art->draw = hud_region_draw;
   art->init = hud_region_init;
   art->free = hud_region_free;
+  art->cursor = hud_region_cursor;
 
   /* We need to indicate a preferred size to avoid false `RGN_FLAG_TOO_SMALL`
    * the first time the region is created. */
