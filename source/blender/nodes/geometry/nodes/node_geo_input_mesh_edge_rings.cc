5dbc71120f3f6f864101b6bee3f587ffd103e4cf
Iliya Katueshenock <modormoder@gmail.com>
2025-01-25 17:46:39 +0300

    Update

--- source/blender/nodes/geometry/nodes/node_geo_input_mesh_edge_rings.cc
+++ source/blender/nodes/geometry/nodes/node_geo_input_mesh_edge_rings.cc
@@ -1,4 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include "DNA_mesh_types.h"
 #include "DNA_meshdata_types.h"
