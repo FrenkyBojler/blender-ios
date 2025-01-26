924edd1e808976f0eb732aba5f9aa9d594188470
Alaska <alaskayou01@gmail.com>
2025-01-26 16:10:20 +1300

    Change name of script in readme

--- release/release_notes/bug_fixes_per_major_release.py
+++ release/release_notes/bug_fixes_per_major_release.py
@@ -31,7 +31,7 @@ are:
   - --backport-tasks (-bpt) (Optional but highly recommended)
 - Here is an example if you wish to collect the list for Blender 4.4 during
 the alpha stage of development for 4.4:
-  - `python main.py -cv 4.4 -pv 4.3 -ct main -pt v4.3.2 -bpt 109399 124452 130221`
+  - `python bug_fixes_per_major_release.py -cv 4.4 -pv 4.3 -ct main -pt v4.3.2 -bpt 109399 124452 130221`
 - Wait for the script to finish (This usually takes 20 minutes).
 - Follow the guide printed to terminal.
 
