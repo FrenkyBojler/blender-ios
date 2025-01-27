fed4b75ff25799bdb9ace61f40929c26bc4ea98f
Omar Emara <mail@OmarEmara.dev>
2025-01-27 13:47:26 +0200

    Skip realization for node

--- source/blender/nodes/composite/nodes/node_composite_set_realization_options.cc
+++ source/blender/nodes/composite/nodes/node_composite_set_realization_options.cc
@@ -17,7 +17,8 @@ static void cmp_node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Color>("Image")
       .default_value({1.0f, 1.0f, 1.0f, 1.0f})
-      .compositor_domain_priority(0);
+      .compositor_domain_priority(0)
+      .compositor_realization_options(CompositorInputRealizationOptions::None);
   b.add_output<decl::Color>("Image");
 }
 
