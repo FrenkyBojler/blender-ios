e562b95018ded21940ada722c1bc318204d21803
Hans Goudey <hans@blender.org>
2025-01-28 11:26:22 -0500

    Remove unused include

--- source/blender/editors/sculpt_paint/brushes/bmesh_topology_rake.cc
+++ source/blender/editors/sculpt_paint/brushes/bmesh_topology_rake.cc
@@ -18,7 +18,6 @@
 #include "editors/sculpt_paint/mesh_brush_common.hh"
 #include "editors/sculpt_paint/sculpt_automask.hh"
 #include "editors/sculpt_paint/sculpt_intern.hh"
-#include "editors/sculpt_paint/sculpt_nodes_evaluation.hh"
 #include "editors/sculpt_paint/sculpt_smooth.hh"
 
 #include "bmesh.hh"
