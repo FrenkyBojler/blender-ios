c48d9c5e737ae95bfbea57578f43f6faf393d013
Janne Nylander <j.nylander4444@gmail.com>
2025-01-28 11:51:22 +0200

    Review fixes

--- source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
@@ -495,16 +495,12 @@ static int grease_pencil_layer_reveal_exec(bContext *C, wmOperator * /*op*/)
   using namespace blender::bke::greasepencil;
   GreasePencil &grease_pencil = *blender::ed::greasepencil::from_context(*C);
 
-  if (!grease_pencil.has_active_layer() && !grease_pencil.has_active_group()) {
+  if (!grease_pencil.active_node) {
     return OPERATOR_CANCELLED;
   }
 
-  for (LayerGroup *group : grease_pencil.layer_groups_for_write()) {
-    group->set_visible(true);
-  }
-
-  for (Layer *layer : grease_pencil.layers_for_write()) {
-    layer->set_visible(true);
+  for (TreeNode *node : grease_pencil.nodes_for_write()) {
+    node->set_visible(true);
   }
 
   /* notifiers */
