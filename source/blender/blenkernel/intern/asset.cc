ebd634c961a53619dd885f0efc4a12a375a6bc54
Jacques Lucke <jacques@blender.org>
2025-01-28 11:47:34 +0100

    fix missing trim

--- source/blender/blenkernel/intern/asset.cc
+++ source/blender/blenkernel/intern/asset.cc
@@ -162,7 +162,7 @@ void BKE_asset_metadata_catalog_id_set(AssetMetaData *asset_data,
                                        const char *catalog_simple_name)
 {
   asset_data->catalog_id = catalog_id;
-  StringRef(catalog_simple_name).copy_utf8_truncated(asset_data->catalog_simple_name);
+  StringRef(catalog_simple_name).trim().copy_utf8_truncated(asset_data->catalog_simple_name);
 }
 
 void BKE_asset_metadata_idprop_ensure(AssetMetaData *asset_data, IDProperty *prop)
