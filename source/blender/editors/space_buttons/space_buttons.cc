01844de77d82bedf592856cc9d79815ad5d3c910
Richard Antalik <richardantalik@gmail.com>
2025-01-29 11:05:12 +0100

    Resolve build error

--- source/blender/editors/space_buttons/space_buttons.cc
+++ source/blender/editors/space_buttons/space_buttons.cc
@@ -167,7 +167,7 @@ static void buttons_main_region_init(wmWindowManager *wm, ARegion *region)
 
 void ED_buttons_visible_tabs_menu(bContext *C, uiLayout *layout, void * /*arg*/)
 {
-  PointerRNA ptr = RNA_pointer_create(
+  PointerRNA ptr = RNA_pointer_create_discrete(
       reinterpret_cast<ID *>(CTX_wm_screen(C)), &RNA_SpaceProperties, CTX_wm_space_properties(C));
 
   for (blender::StringRefNull item : blender::ed::properties::filter_items) {
