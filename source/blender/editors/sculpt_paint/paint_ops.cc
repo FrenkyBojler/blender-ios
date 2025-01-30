bf8dbc2eeb4727aace3a5b37b2ea345d6facd30e
Nicola <nicdessi@gmail.com>
2025-01-30 12:47:02 +0100

    initialize node group output with neutral values

--- source/blender/editors/sculpt_paint/paint_ops.cc
+++ source/blender/editors/sculpt_paint/paint_ops.cc
@@ -964,17 +964,18 @@ static bNodeTree *node_group_add_for_brush(Main *bmain,
 
   /* VDM brush expects a vector output */
   if (is_VDM_brush) {
-    bNodeTreeInterfaceSocket* socket = node_group->tree_interface.add_socket(
+    bNodeTreeInterfaceSocket *socket = node_group->tree_interface.add_socket(
         DATA_("Vector"), "", "NodeSocketVector", NODE_INTERFACE_SOCKET_OUTPUT, nullptr);
 
-    bNodeSocketValueVector* vector_data = static_cast<bNodeSocketValueVector*>(socket->socket_data);
+    bNodeSocketValueVector *vector_data = static_cast<bNodeSocketValueVector *>(
+        socket->socket_data);
     copy_v3_v3(vector_data->value, float3(1.0f));
   }
   else {
-    bNodeTreeInterfaceSocket* socket = node_group->tree_interface.add_socket(
+    bNodeTreeInterfaceSocket *socket = node_group->tree_interface.add_socket(
         DATA_("Value"), "", "NodeSocketFloat", NODE_INTERFACE_SOCKET_OUTPUT, nullptr);
 
-    bNodeSocketValueFloat* float_data = static_cast<bNodeSocketValueFloat*>(socket->socket_data);
+    bNodeSocketValueFloat *float_data = static_cast<bNodeSocketValueFloat *>(socket->socket_data);
     float_data->value = 1.0f;
   }
 
