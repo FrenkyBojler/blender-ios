37c0e683791de95cd57725341ab21a618d6eceb7
Philipp Oeser <philipp@blender.org>
2025-01-30 12:19:21 +0100

    make format

--- source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
+++ source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -2166,12 +2166,14 @@ static void legacy_object_modifier_opacity(ConversionData &conversion_data,
   /* Account for animation on renamed properties. */
   char modifier_name[MAX_NAME * 2];
   BLI_str_escape(modifier_name, md.name, sizeof(modifier_name));
-  AnimDataConvertor anim_convertor_factor(conversion_data, object.id, object.id, {{".factor", ".color_factor"}});
+  AnimDataConvertor anim_convertor_factor(
+      conversion_data, object.id, object.id, {{".factor", ".color_factor"}});
   anim_convertor_factor.root_path_src = fmt::format("modifiers[\"{}\"]", modifier_name);
   anim_convertor_factor.root_path_dst = fmt::format("modifiers[\"{}\"]", modifier_name);
   anim_convertor_factor.fcurves_convert();
   anim_convertor_factor.fcurves_convert_finalize();
-  AnimDataConvertor anim_convertor_hardness(conversion_data, object.id, object.id, {{".hardness", ".hardness_factor"}});
+  AnimDataConvertor anim_convertor_hardness(
+      conversion_data, object.id, object.id, {{".hardness", ".hardness_factor"}});
   anim_convertor_hardness.root_path_src = fmt::format("modifiers[\"{}\"]", modifier_name);
   anim_convertor_hardness.root_path_dst = fmt::format("modifiers[\"{}\"]", modifier_name);
   anim_convertor_hardness.fcurves_convert();
