5155879c89d1a8d7c83bc0e97bd5a898198c18a8
Hans Goudey <hans@blender.org>
2025-01-30 14:07:15 -0500

    Fix build error (use StringRef for BLT_is_default_context

--- source/blender/blentranslation/BLT_translation.hh
+++ source/blender/blentranslation/BLT_translation.hh
@@ -8,11 +8,12 @@
 
 #pragma once
 
+#include "BLI_string_ref.hh"
 #include "BLI_utildefines.h" /* for bool type */
 
 #define TEXT_DOMAIN_NAME "blender"
 
-bool BLT_is_default_context(const char *msgctxt);
+bool BLT_is_default_context(blender::StringRef msgctxt);
 const char *BLT_pgettext(const char *msgctxt, const char *msgid);
 
 /* Translation */
@@ -152,10 +153,7 @@ struct BLT_i18n_contexts_descriptor {
   const char *value;
 };
 
-#define BLT_I18NCONTEXTS_ITEM(ctxt_id, py_id) \
-  { \
-    #ctxt_id, py_id, ctxt_id \
-  }
+#define BLT_I18NCONTEXTS_ITEM(ctxt_id, py_id) {#ctxt_id, py_id, ctxt_id}
 
 #define BLT_I18NCONTEXTS_DESC \
   { \
