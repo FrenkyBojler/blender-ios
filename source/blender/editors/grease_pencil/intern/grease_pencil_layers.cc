5f75b73e721a3c14021190f81ecb41a6d92db0f5
Janne Nylander <j.nylander4444@gmail.com>
2025-01-31 16:18:43 +0200

    Grease Pencil: Make "Hide Others" layer operator take layer groups into account

--- source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
@@ -443,22 +443,38 @@ static int grease_pencil_layer_hide_exec(bContext *C, wmOperator *op)
   using namespace blender::bke::greasepencil;
   GreasePencil &grease_pencil = *blender::ed::greasepencil::from_context(*C);
   const bool unselected = RNA_boolean_get(op->ptr, "unselected");
+  
+  TreeNode *active_node = grease_pencil.get_active_node();
 
-  if (!grease_pencil.has_active_layer()) {
+  if (!active_node) {
     return OPERATOR_CANCELLED;
   }
 
   if (unselected) {
-    /* hide unselected */
-    for (Layer *layer : grease_pencil.layers_for_write()) {
-      const bool is_active = grease_pencil.is_layer_active(layer);
-      layer->set_visible(is_active);
+    /* if active node is a layergroup, only show parent layergroups and child nodes.
+     *  if active node is a layer, only show parent layergroups and active node. */
+
+    for (TreeNode *node : grease_pencil.nodes_for_write()) {
+      bool visibility = false;
+
+      if (active_node->is_group()) {
+        visibility = node->is_child_of(active_node->as_group());
+        if (node->is_group()) {
+          visibility = active_node->is_child_of(node->as_group()) ||
+                       node->is_child_of(active_node->as_group());
+        }
+      }
+      else if (node->is_group()) {
+        visibility = active_node->is_child_of(node->as_group());
+      }
+
+      node->set_visible(visibility);
     }
+    active_node->set_visible(true);
   }
   else {
     /* hide selected/active */
-    Layer &active_layer = *grease_pencil.get_active_layer();
-    active_layer.set_visible(false);
+    active_node->set_visible(false);
   }
 
   /* notifiers */
