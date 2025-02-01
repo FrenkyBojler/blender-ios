3c80067a29b9373fa77be2f58840288b9838ff6a
Dangry98 <danielgryning@gmail.com>
2025-02-01 22:45:48 +0100

    Added support for lattice

--- scripts/startup/bl_ui/properties_object.py
+++ scripts/startup/bl_ui/properties_object.py
@@ -197,7 +197,7 @@ class OBJECT_PT_display(ObjectButtonsPanel, Panel):
         obj_type = obj.type
         is_geometry = (obj_type in {'MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'VOLUME', 'CURVES', 'POINTCLOUD'})
         has_bounds = (is_geometry or obj_type in {'LATTICE', 'ARMATURE'})
-        is_wire = (obj_type in {'CAMERA', 'EMPTY', 'LIGHT', 'SPEAKER', 'LIGHT_PROBE'})
+        is_wire = (obj_type in {'CAMERA', 'EMPTY', 'LIGHT', 'SPEAKER', 'LIGHT_PROBE', 'LATTICE'})
         is_empty_image = (obj_type == 'EMPTY' and obj.empty_display_type == 'IMAGE')
         is_dupli = (obj.instance_type != 'NONE')
         is_gpencil = (obj_type == 'GREASEPENCIL')
