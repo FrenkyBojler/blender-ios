6c00f27d96df91627d83092a0b276c64d8cc7df7
Jonas Holzman <jonas@holzman.fr>
2025-02-02 13:46:42 +0100

    Remove faulty `WM_capabilities_flag()` check in `ED_area_newspace`

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -2720,10 +2720,8 @@ void ED_area_newspace(bContext *C, ScrArea *area, int type, const bool skip_regi
     WM_window_title(CTX_wm_manager(C), CTX_wm_window(C));
   }
 
-  /* If window decoration styles are supported, send a notification to re-apply them. */
-  if (WM_capabilities_flag() & WM_CAPABILITY_WINDOW_DECORATION_STYLES) {
-    WM_event_add_notifier(C, NC_WINDOW, nullptr);
-  }
+  /* Send a window notification to re-apply decoration styles. */
+  WM_event_add_notifier(C, NC_WINDOW, nullptr);
 
   /* also redraw when re-used */
   ED_area_tag_redraw(area);
