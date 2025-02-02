ba41f719be42de904a889b5a80f0009b1346d44c
Hans Goudey <hans@blender.org>
2025-02-02 00:04:57 -0500

    Remove pbvh build timing prints

--- source/blender/blenkernel/intern/pbvh.cc
+++ source/blender/blenkernel/intern/pbvh.cc
@@ -37,13 +37,9 @@
 
 #include "pbvh_intern.hh"
 
-#define DEBUG_BUILD_TIME
-
-#ifdef DEBUG_BUILD_TIME
-#  include "BLI_timeit.hh"
-#endif
-
 namespace blender::bke::pbvh {
+
+// #define DEBUG_BUILD_TIME
 
 #define STACK_FIXED_DEPTH 100
 
