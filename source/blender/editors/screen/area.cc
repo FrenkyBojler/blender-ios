d85f698fcc59bdfb91fc260df232eeee1cafa7af
Jonas Holzman <jonas@holzman.fr>
2025-02-03 22:12:43 +0100

    Revert "Remove faulty `WM_capabilities_flag()` check in `ED_area_newspace`"
    This reverts commit 6c00f27d96df91627d83092a0b276c64d8cc7df7.

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -2720,8 +2720,10 @@ void ED_area_newspace(bContext *C, ScrArea *area, int type, const bool skip_regi
     WM_window_title(CTX_wm_manager(C), CTX_wm_window(C));
   }
 
-  /* Send a window notification to re-apply decoration styles. */
-  WM_event_add_notifier(C, NC_WINDOW, nullptr);
+  /* If window decoration styles are supported, send a notification to re-apply them. */
+  if (WM_capabilities_flag() & WM_CAPABILITY_WINDOW_DECORATION_STYLES) {
+    WM_event_add_notifier(C, NC_WINDOW, nullptr);
+  }
 
   /* also redraw when re-used */
   ED_area_tag_redraw(area);
