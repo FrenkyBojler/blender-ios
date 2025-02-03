c3fabdd3ae6575c50357895417ee70c22b19992f
Aki <75532970+AkiSakurai@users.noreply.github.com>
2025-02-03 09:34:50 +0800

    Add comment about inverted tilt

--- intern/ghost/intern/GHOST_SystemCocoa.mm
+++ intern/ghost/intern/GHOST_SystemCocoa.mm
@@ -1478,6 +1478,8 @@ GHOST_TSuccess GHOST_SystemCocoa::handleTabletEvent(void *eventPtr, short eventT
 
       ct.Pressure = event.pressure;
       ct.Xtilt = event.tilt.x;
+      /* On macOS, the y tilt behavior is inverted; an increase in the tilt
+         value corresponds to tilting the device away from the user. */
       ct.Ytilt = -event.tilt.y;
       break;
 
