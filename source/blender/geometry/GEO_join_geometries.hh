664f34712e7dbc57f234c11be36f0831511cabbd
илья _ <modormoder@gmail.com>
2025-02-05 01:40:12 +0300

    cleant

--- source/blender/geometry/GEO_join_geometries.hh
+++ source/blender/geometry/GEO_join_geometries.hh
@@ -19,7 +19,11 @@ bke::GeometrySet join_geometries(Span<bke::GeometrySet> geometries,
                                  const std::optional<Span<bke::GeometryComponent::Type>>
                                      &component_types_to_join = std::nullopt);
 
-void join_attributes(const Span<std::optional<bke::AttributeAccessor>> attribute_accessors,
+/* In order to transfer ownership over geometries inside instances without copy this one will perform joining with modification of some instance.
+This will ensure #target-references will not be invalidated, but other #begin/end can be deleted if they will not unique. */
+void join_instances_into(const bke::AttributeFilter &attribute_filter, Span<const bke::Instances *> other_instances, bke::Instances &target);
+
+void join_attributes(const Span<bke::AttributeAccessor> attribute_accessors,
                      const Map<StringRef, eCustomDataType> &attribute_types,
                      const bke::AttrDomain src_domain,
                      const bke::AttrDomain dst_domain,
