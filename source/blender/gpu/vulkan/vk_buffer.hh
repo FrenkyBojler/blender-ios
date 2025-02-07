e3c2fecea4753f83c498618a54183440734f4dde
Jeroen Bakker <jeroen@blender.org>
2025-02-07 15:36:07 +0100

    Added documentation for vkbuffer

--- source/blender/gpu/vulkan/vk_buffer.hh
+++ source/blender/gpu/vulkan/vk_buffer.hh
@@ -54,8 +54,23 @@ class VKBuffer : public NonCopyable {
    */
   void update_render_graph(VKContext &context, void *data) const;
   void flush() const;
+
+  /**
+   * Read the buffer (synchronously).
+   */
   void read(VKContext &context, void *data) const;
+
+  /**
+   * Start a async read-back.
+   */
   void async_flush_to_host(VKContext &context);
+
+  /**
+   * Wait until the async read back is finished and fill the given data with the content of the
+   * buffer.
+   *
+   * Will start a new async read-back when there is no read back in progress.
+   */
   void read_async(VKContext &context, void *data);
 
   /**
