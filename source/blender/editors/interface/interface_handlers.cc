922edc002c0f7778d80daebca4f286947fd38619
Gilberto.R <gilbertorodrigues@outlook.com>
2025-02-07 07:47:37 -0300

    Fix: UI: close pie menu on windows focus loss

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -11680,6 +11680,13 @@ static int ui_pie_handler(bContext *C, const wmEvent *event, uiPopupBlockHandle 
             break;
           }
 #undef CASE_NUM_TO_DIR
+        case WINDEACTIVATE: {
+          /* If window focus is lost, exit the pie menu and release the pie event lock.
+          * Prevents waiting for the pie key release if it was released outside of focus. */
+          retval = ui_but_pie_menu_apply(C, menu, NULL, true);
+          CTX_wm_window(C)->pie_event_type_lock = EVENT_NONE;
+          break;
+        }
         default:
           retval = ui_handle_menu_button(C, event, menu);
           break;
