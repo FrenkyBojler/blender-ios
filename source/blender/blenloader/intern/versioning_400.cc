3d707d8a92ec8862666c831425f562bbb1ac217f
marcopavanello <marco.pavanello@protonmail.com>
2025-02-07 18:46:30 +0100

    add versioning for changed mie scattering

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -5817,6 +5817,18 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
         brush->mask_stencil_pos[1] = default_brush->mask_stencil_pos[1];
       }
     }
+    /* Update sky mie scattering to match old sky visually */
+    FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
+      if (ntree->type == NTREE_SHADER) {
+        LISTBASE_FOREACH (bNode *, node, &ntree->nodes) {
+          if (node->type_legacy == SH_NODE_TEX_SKY && node->storage) {
+            NodeTexSky *tex = (NodeTexSky *)node->storage;
+            tex->dust_density *= 5.0f;
+          }
+        }
+      }
+      FOREACH_NODETREE_END;
+    }
   }
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 404, 27)) {
