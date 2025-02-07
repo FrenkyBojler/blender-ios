0cd0c1081706fecd96607bcd0dd627377d18b0f7
Falk David <falk@blender.org>
2025-02-07 12:41:57 +0100

    Alow negative scales

--- source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
+++ source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
@@ -24,7 +24,7 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Geometry>("Profile Curve")
       .only_realized_data()
       .supported_type(GeometryComponent::Type::Curve);
-  b.add_input<decl::Float>("Scale").min(0.0f).default_value(1.0f).supports_field().description(
+  b.add_input<decl::Float>("Scale").default_value(1.0f).supports_field().description(
       "Scale of the profile at each point");
   b.add_input<decl::Bool>("Fill Caps")
       .description(
