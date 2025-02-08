627f17e446f6a81c185e7fa49c406484a6ad0423
Nig3l <nig3lpro@gmail.com>
2025-02-09 00:27:58 +0100

    USD: Add missing plane attr which value can varying accross time

--- source/blender/io/usd/intern/usd_reader_shape.cc
+++ source/blender/io/usd/intern/usd_reader_shape.cc
@@ -310,7 +310,11 @@ bool USDShapeReader::is_time_varying()
 
   if (prim_.IsA<pxr::UsdGeomPlane>()) {
     pxr::UsdGeomPlane geom(prim_);
-    return geom.GetDoubleSidedAttr().ValueMightBeTimeVarying();
+    return (geom.GetDoubleSidedAttr().ValueMightBeTimeVarying() ||
+            geom.GetWidthAttr().ValueMightBeTimeVarying() ||
+            geom.GetLengthAttr().ValueMightBeTimeVarying() ||
+            geom.GetAxisAttr().ValueMightBeTimeVarying() ||
+            geom.GetExtentAttr().ValueMightBeTimeVarying());
   }
 
   BKE_reportf(reports(),
