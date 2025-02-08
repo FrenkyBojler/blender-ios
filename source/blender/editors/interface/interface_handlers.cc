b66f253c6b14a0b815a27d90e104a1a4a9ca2ac6
Gilberto.R <gilbertorodrigues@outlook.com>
2025-02-08 10:08:39 -0300

    Fix left click not registered when the pie menu key is held

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -11597,13 +11597,11 @@ static int ui_pie_handler(bContext *C, const wmEvent *event, uiPopupBlockHandle 
           break;
 
         case LEFTMOUSE:
-          if (is_click_style) {
-            if (block->pie_data.flags & UI_PIE_INVALID_DIR) {
-              menu->menuretval = UI_RETURN_CANCEL;
-            }
-            else {
-              retval = ui_handle_menu_button(C, event, menu);
-            }
+          if (block->pie_data.flags & UI_PIE_INVALID_DIR) {
+            menu->menuretval = UI_RETURN_CANCEL;
+          }
+          else {
+            retval = ui_handle_menu_button(C, event, menu);
           }
           break;
         case WINDEACTIVATE:
