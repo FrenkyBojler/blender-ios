51f8197b8ecfe9f5b347fc78bcaad45a7f32867b
Brecht Van Lommel <brecht@blender.org>
2025-02-09 19:05:01 +0100

    Refactor: Move DEG2RAD and similar to BLI_math_constants
    So it can be used in DNA headers without pulling in full BLI_math_rotation.h.

--- source/blender/blenlib/BLI_math_rotation.h
+++ source/blender/blenlib/BLI_math_rotation.h
@@ -8,25 +8,14 @@
  * \ingroup bli
  */
 
-#include "BLI_math_base.h"  // IWYU pragma: keep
+#include "BLI_math_base.h"       // IWYU pragma: keep
+#include "BLI_math_constants.h"  // IWYU pragma: keep
 #include "BLI_utildefines.h"
 #include "DNA_vec_types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* -------------------------------------------------------------------- */
-/** \name Conversion Defines
- * \{ */
-
-#define RAD2DEG(_rad) ((_rad) * (180.0 / M_PI))
-#define DEG2RAD(_deg) ((_deg) * (M_PI / 180.0))
-
-#define RAD2DEGF(_rad) ((_rad) * (float)(180.0 / M_PI))
-#define DEG2RADF(_deg) ((_deg) * (float)(M_PI / 180.0))
-
-/** \} */
 
 /* -------------------------------------------------------------------- */
 /** \name Quaternions
