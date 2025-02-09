d1a0dc2cd80237919d3441d51efde1c8d364a075
илья _ <modormoder@gmail.com>
2025-02-09 21:33:30 +0300

    add test threshold

--- tests/python/geo_node_test.py
+++ tests/python/geo_node_test.py
@@ -9,7 +9,32 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from modules.mesh_test import BlendFileTest
 
-geo_node_test = BlendFileTest("test_object", "expected_object", threshold=0.1)
+geo_node_test = None
+
+if not geo_node_test:
+    try:
+        geo_node_test = BlendFileTest("test_object", "expected_object", threshold=1e-4)
+    except:
+        pass
+
+if not geo_node_test:
+    try:
+        geo_node_test = BlendFileTest("test_object (threshold = 0.1)", "expected_object", threshold=0.1)
+    except:
+        pass
+
+if not geo_node_test:
+    try:
+        geo_node_test = BlendFileTest("test_object (threshold = 0.2)", "expected_object", threshold=0.2)
+    except:
+        pass
+
+if not geo_node_test:
+    try:
+        geo_node_test = BlendFileTest("test_object (threshold = 0.3)", "expected_object", threshold=0.3)
+    except:
+        pass
+
 result = geo_node_test.run_test()
 
 # Telling `ctest` about the failed test by raising Exception.
