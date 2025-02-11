bf7a226e479cbb9d0f44e99932f77668755dea68
Sean Kim <SeanCTKim@protonmail.com>
2025-02-11 10:21:17 -0800

    Make test setup explicit

--- tests/python/bl_object.py
+++ tests/python/bl_object.py
@@ -6,7 +6,11 @@ from mathutils import Vector
 
 class TestObjectApi(unittest.TestCase):
     def test_closest_point_on_mesh_of_default_cube(self):
-        ret_val = bpy.context.active_object.closest_point_on_mesh(Vector((0.0, 0.0, 2.0)))
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete(use_global=False)
+        bpy.ops.outliner.orphans_purge()
+        bpy.ops.mesh.primitive_cube_add()
+        ret_val = bpy.context.scene.objects[0].closest_point_on_mesh(Vector((0.0, 0.0, 2.0)))
         self.assertTrue(ret_val[0])
         self.assertEqual(ret_val[1], Vector((0.0, 0.0, 1.0)))
 
