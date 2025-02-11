4c6e507d094fbab24d3b366c5d77bb2890a19fa9
Hans Goudey <hans@blender.org>
2025-02-10 19:07:31 -0500

    Fix shader

--- source/blender/gpu/shaders/gpu_shader_2D_node_socket_frag.glsl
+++ source/blender/gpu/shaders/gpu_shader_2D_node_socket_frag.glsl
@@ -97,7 +97,7 @@ void main()
     }
     case SOCK_DISPLAY_SHAPE_LINE: {
       float square_radius = square_radius - corner_rounding;
-      distance_squared = square_sdf(co, square_radius * 0.75, square_radius * 1.4);
+      distance_squared = square_sdf(co, vec2(square_radius * 0.75, square_radius * 1.4));
       alpha_threshold = corner_rounding;
       break;
     }
