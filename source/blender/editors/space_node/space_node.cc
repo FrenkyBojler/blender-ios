20c7e6328ff3b2208f169efe606a3bd5a3c4ee17
Pratik Borhade <pratikborhade302@gmail.com>
2025-02-12 12:35:27 +0530

    Fix #134428: Switching Animation Slots does not update node UI
    Node region is not tagged to redraw when animation slot is changed.
    `action_slot` RNA prop has `NC_ANIMATION`notifier set, use the same in
    `node_region_listener` to tag region redraw.

--- source/blender/editors/space_node/space_node.cc
+++ source/blender/editors/space_node/space_node.cc
@@ -961,6 +961,9 @@ static void node_region_listener(const wmRegionListenerParams *params)
           break;
       }
       break;
+    case NC_ANIMATION:
+        ED_region_tag_redraw(region);
+        break;
     case NC_SCREEN:
       if (wmn->data == ND_LAYOUTSET || wmn->action == NA_EDITED) {
         WM_gizmomap_tag_refresh(gzmap);
