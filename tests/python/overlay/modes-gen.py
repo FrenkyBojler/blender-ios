d89bac858ddf4ffe3861b13065734ccaa6572192
Miguel Pozo <pragma37@gmail.com>
2025-02-12 17:06:40 +0100

    Add missing licenses

--- tests/python/overlay/modes-gen.py
+++ tests/python/overlay/modes-gen.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Blender Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import os
 import sys
 import bpy
--- tests/python/overlay/overlay_common.py
+++ tests/python/overlay/overlay_common.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Blender Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import bpy
 import os
 from os import path
