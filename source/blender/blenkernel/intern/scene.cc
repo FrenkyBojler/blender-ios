8f10f16b613a0e34b01bb7109cc9a46cf6e81866
Sean Kim <SeanCTKim@protonmail.com>
2025-02-13 16:19:43 -0800

    Correctly handle saving and loading toolsetting data

--- source/blender/blenkernel/intern/scene.cc
+++ source/blender/blenkernel/intern/scene.cc
@@ -681,6 +681,13 @@ static void scene_foreach_toolsettings(LibraryForeachIDData *data,
                                                     reader,
                                                     &toolsett_old->imapaint.canvas,
                                                     IDWALK_CB_USER);
+  BKE_LIB_FOREACHID_UNDO_PRESERVE_PROCESS_IDSUPER_P(data,
+                                                    &toolsett->imapaint.clone_brush.image,
+                                                    do_undo_restore,
+                                                    SCENE_FOREACH_UNDO_RESTORE,
+                                                    reader,
+                                                    &toolsett_old->imapaint.clone_brush.image,
+                                                    IDWALK_CB_USER);
 
   Paint *paint, *paint_old;
 
