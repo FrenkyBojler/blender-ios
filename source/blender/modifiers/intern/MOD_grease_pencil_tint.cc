f530164ceebaa3c190a9d59345101d54f6eab845
YimingWu <xp8110@outlook.com>
2025-02-14 16:10:56 +0800

    Fix #134494: Grease Pencil: Tint modifier fix on gradient material
    When a stroke has a gradient fill material that starts with a color with
    zero alpha, the tint modifier would behave as if it's not effective at
    all, this is caused by referencing source material color only with the
    starting color in this case, and since the alpha is zero, the tint is
    applied wrongly. Now use average color of start and end color of the
    gradient to mix with the tint modifier color.
    
    Note that this isn't technically correct still, since material gradient
    is computed in the shader, and tint modifier isn't able to get the
    acutal fill color at a given vertex (especially for the radial gradient
    case) however the result of this patch looks visually good enough, and
    users can always set all alpha to 1 when they feel the color is off.

--- source/blender/modifiers/intern/MOD_grease_pencil_tint.cc
+++ source/blender/modifiers/intern/MOD_grease_pencil_tint.cc
@@ -263,8 +263,13 @@ static void modify_fill_color(Object &ob,
   auto get_material_color = [&](const int64_t curve_i) {
     const Material *ma = BKE_object_material_get(&ob, stroke_materials[curve_i] + 1);
     const MaterialGPencilStyle *gp_style = ma ? ma->gp_style : nullptr;
-    return (gp_style ? ColorGeometry4f(gp_style->fill_rgba) :
-                       ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
+    const bool is_gradient = gp_style ? gp_style->fill_style == GP_MATERIAL_FILL_STYLE_GRADIENT :
+                                        false;
+    const float4 average_color = is_gradient ?
+                                     (float4(gp_style->fill_rgba) + float4(gp_style->mix_rgba)) /
+                                         float4(2.0f) :
+                                     float4(gp_style->fill_rgba);
+    return (gp_style ? ColorGeometry4f(average_color) : ColorGeometry4f(0.0f, 0.0f, 0.0f, 0.0f));
   };
 
   auto get_curve_factor = [&](const int64_t curve_i) {
