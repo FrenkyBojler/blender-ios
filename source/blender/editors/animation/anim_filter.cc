a668d1c1af23fbefa49e91c2151b9b1ff5b97455
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-02-14 13:23:45 +0100

    don't show annotations with scene

--- source/blender/editors/animation/anim_filter.cc
+++ source/blender/editors/animation/anim_filter.cc
@@ -3392,7 +3392,6 @@ static size_t animdata_filter_dopesheet_scene(bAnimContext *ac,
   /* filter data contained under object first */
   BEGIN_ANIMFILTER_SUBCHANNELS (EXPANDED_SCEC(sce)) {
     bNodeTree *ntree = sce->nodetree;
-    bGPdata *gpd = sce->gpd;
     World *wo = sce->world;
 
     /* Action, Drivers, or NLA for Scene */
