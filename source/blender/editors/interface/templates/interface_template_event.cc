60d68fc0a0f2ff20b4676ffeed04aa1b7d4d2ea5
Harley Acheson <harley.acheson@gmail.com>
2025-02-13 16:36:50 -0800

    UI: Tightened Status Bar Spacing
    Slight adjustments to the spacing of items on the Status Bar. Mainly
    to reduce the distance between adjoining icons. Small decrease in space
    between icon and text. Slight decrease in text size inside the event
    icons. Proportionally wider gap between items.

--- source/blender/editors/interface/templates/interface_template_event.cc
+++ source/blender/editors/interface/templates/interface_template_event.cc
@@ -85,24 +85,27 @@ int uiTemplateStatusBarModalItem(uiLayout *layout,
 #endif
       for (int j = 0; j < ARRAY_SIZE(icon_mod) && icon_mod[j]; j++) {
         uiItemL(layout, "", icon_mod[j]);
-        const float offset = ui_event_icon_offset(icon_mod[j]);
+        const float offset = ui_event_icon_offset(icon_mod[j]) - 0.4f;
         if (offset != 0.0f) {
           uiItemS_ex(layout, offset);
         }
       }
       uiItemL(layout, "", icon);
+      uiItemS_ex(layout, -0.4f);
 
 #ifndef WITH_HEADLESS
       icon = UI_icon_from_keymap_item(kmi_y, icon_mod);
 #endif
       uiItemL(layout, "", icon);
+      uiItemS_ex(layout, -0.4f);
 
 #ifndef WITH_HEADLESS
       icon = UI_icon_from_keymap_item(kmi_z, icon_mod);
 #endif
       uiItemL(layout, "", icon);
+      uiItemS_ex(layout, -0.2f);
       uiItemL(layout, xyz_label, ICON_NONE);
-      uiItemS_ex(layout, 0.7f);
+      uiItemS_ex(layout, 0.6f);
       return 3;
     }
   }
@@ -127,7 +130,7 @@ bool uiTemplateEventFromKeymapItem(uiLayout *layout,
   if (icon != 0) {
     for (int j = 0; j < ARRAY_SIZE(icon_mod) && icon_mod[j]; j++) {
       uiItemL(layout, "", icon_mod[j]);
-      const float offset = ui_event_icon_offset(icon_mod[j]);
+      const float offset = ui_event_icon_offset(icon_mod[j]) - 0.4f;
       if (offset != 0.0f) {
         uiItemS_ex(layout, offset);
       }
@@ -138,23 +141,23 @@ bool uiTemplateEventFromKeymapItem(uiLayout *layout,
     uiItemL(layout, "", icon);
     if (icon >= ICON_MOUSE_LMB && icon <= ICON_MOUSE_MMB_SCROLL) {
       /* Negative space after narrow mice icons. */
-      uiItemS_ex(layout, -0.5f);
+      uiItemS_ex(layout, -0.7f);
     }
 
-    const float offset = ui_event_icon_offset(icon);
+    const float offset = ui_event_icon_offset(icon) - 0.2f;
     if (offset != 0.0f) {
       uiItemS_ex(layout, offset);
     }
 
     uiItemL(layout, CTX_IFACE_(BLT_I18NCONTEXT_ID_WINDOWMANAGER, text.c_str()), ICON_NONE);
-    uiItemS_ex(layout, 0.7f);
+    uiItemS_ex(layout, 0.6f);
     ok = true;
   }
   else if (text_fallback) {
     const char *event_text = WM_key_event_string(kmi->type, true);
     uiItemL(layout, event_text, ICON_NONE);
     uiItemL(layout, CTX_IFACE_(BLT_I18NCONTEXT_ID_WINDOWMANAGER, text.c_str()), ICON_NONE);
-    uiItemS_ex(layout, 0.5f);
+    uiItemS_ex(layout, 0.6f);
     ok = true;
   }
   return ok;
--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -880,12 +880,15 @@ WorkspaceStatus::WorkspaceStatus(bContext *C)
 /** \name Private helper functions to help ensure consistent spacing
  * \{ */
 
-static constexpr float STATUS_AFTER_TEXT = 0.7f;
-static constexpr float STATUS_MOUSE_ICON_PAD = -0.5f;
+static constexpr float STATUS_BEFORE_TEXT = 0.2f;
+static constexpr float STATUS_AFTER_TEXT = 0.6f;
+static constexpr float STATUS_MOUSE_ICON_PAD = -1.0f;
+static constexpr float STATUS_MOUSE_ICON_AFTER = -0.4f;
 
 static void ed_workspace_status_text_item(WorkSpace *workspace, std::string text)
 {
   if (!text.empty()) {
+    ed_workspace_status_space(workspace, STATUS_BEFORE_TEXT);
     ed_workspace_status_item(workspace, std::move(text), ICON_NONE);
     ed_workspace_status_space(workspace, STATUS_AFTER_TEXT);
   }
@@ -900,6 +903,9 @@ static void ed_workspace_status_icon_item(WorkSpace *workspace,
     if (icon >= ICON_MOUSE_LMB && icon <= ICON_MOUSE_MMB_SCROLL) {
       /* Negative space after narrow mice icons. */
       ed_workspace_status_space(workspace, STATUS_MOUSE_ICON_PAD);
+    }
+    else {
+      ed_workspace_status_space(workspace, STATUS_MOUSE_ICON_AFTER);
     }
   }
 }
