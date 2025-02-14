c7c9043544341fe82fc7a48d418c6b799534a31a
YimingWu <xp8110@outlook.com>
2025-02-14 19:30:28 +0800

    Cleanup stuff

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -243,20 +243,17 @@ static IndexMask simplify_fixed(const bke::CurvesGeometry &curves,
   const OffsetIndices points_by_curve = curves.points_by_curve();
   const Array<int> point_to_curve_map = curves.point_to_curve_map();
 
-  /* Only work on strokes that have more than 2 points. */
-  const IndexMask valid_strokes = IndexMask::from_predicate(
-      stroke_selection, GrainSize(2048), memory, [&](const int64_t i) {
-        return points_by_curve[i].size() > 2;
-      });
+  const IndexMask selected_points = IndexMask::from_ranges(
+      points_by_curve, stroke_selection, memory);
 
-  /* Construct a points mask from valid strokes. */
-  const IndexMask selected_points = IndexMask::from_ranges(points_by_curve, valid_strokes, memory);
-
-  /* Find out which points to keep among all selected points. */
+  /* Find points to keep among selected points. */
   const IndexMask selected_to_keep = IndexMask::from_predicate(
       selected_points, GrainSize(2048), memory, [&](const int64_t i) {
         const int curve_i = point_to_curve_map[i];
         const IndexRange points = points_by_curve[curve_i];
+        if (points.size() <= 2) {
+          return true;
+        }
         const int local_i = i - points.start();
         return (local_i % int(math::pow(2.0f, float(step))) == 0) || points.last() == i;
       });
@@ -333,28 +330,16 @@ static int grease_pencil_stroke_simplify_exec(bContext *C, wmOperator *op)
         const OffsetIndices<int> points_by_curve = curves.points_by_curve();
         const Array<int> point_to_curve_map = curves.point_to_curve_map();
         const float merge_distance = RNA_float_get(op->ptr, "distance");
-
-        /* Only work on strokes that have more than 2 points. */
-        const IndexMask valid_strokes = IndexMask::from_predicate(
-            strokes, GrainSize(2048), memory, [&](const int64_t i) {
-              return points_by_curve[i].size() > 2;
-            });
-
-        /* Construct a points mask from valid strokes. */
-        const IndexMask selected_points = IndexMask::from_ranges(
-            points_by_curve, valid_strokes, memory);
-
-        /* Only merge points in between the first and last points of a stroke. */
+        const IndexMask selected_points = IndexMask::from_ranges(points_by_curve, strokes, memory);
         const IndexMask filtered_points = IndexMask::from_predicate(
             selected_points, GrainSize(2048), memory, [&](const int64_t i) {
               const int curve_i = point_to_curve_map[i];
               const IndexRange points = points_by_curve[curve_i];
-              if (i == points.first() || i == points.last()) {
-                return false;
+              if (points.drop_front(1).drop_back(1).contains(i)) {
+                return true;
               }
-              return true;
+              return false;
             });
-
         info.drawing.strokes_for_write() = ed::greasepencil::curves_merge_by_distance(
             curves, merge_distance, filtered_points, {});
         info.drawing.tag_topology_changed();
