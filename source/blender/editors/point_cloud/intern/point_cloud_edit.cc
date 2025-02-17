76ad1420577c3f6b7c455815ede56f5df5862ba1
Dalai Felinto <dalai@blender.org>
2025-02-17 23:28:05 +0100

    From review: Cleanup

--- source/blender/editors/point_cloud/intern/point_cloud_edit.cc
+++ source/blender/editors/point_cloud/intern/point_cloud_edit.cc
@@ -17,24 +17,23 @@ namespace blender::ed::point_cloud {
 
 bool remove_selection(PointCloud &point_cloud)
 {
-  const bke::AttrDomain selection_domain = bke::AttrDomain::Point;
   const bke::AttributeAccessor attributes = point_cloud.attributes();
   const VArray<bool> selection = *attributes.lookup_or_default<bool>(
-      ".selection", selection_domain, true);
-  const int domain_size_orig = attributes.domain_size(selection_domain);
+      ".selection", bke::AttrDomain::Point, true);
+  const int domain_size_orig = point_cloud.totpoint;
   IndexMaskMemory memory;
   const IndexMask mask = IndexMask::from_bools_inverse(selection, memory);
 
   PointCloud *point_cloud_new = BKE_pointcloud_new_nomain(mask.size());
   bke::gather_attributes(attributes,
-                         selection_domain,
-                         selection_domain,
+                         bke::AttrDomain::Point,
+                         bke::AttrDomain::Point,
                          {},
                          mask,
                          point_cloud_new->attributes_for_write());
 
   BKE_pointcloud_nomain_to_pointcloud(point_cloud_new, &point_cloud);
-  return attributes.domain_size(selection_domain) != domain_size_orig;
+  return point_cloud.totpoint != domain_size_orig;
 }
 
 }  // namespace blender::ed::point_cloud
