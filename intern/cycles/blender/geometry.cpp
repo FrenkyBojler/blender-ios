bdb66bbca647c9d086273e3313bfa050306beacb
Brecht Van Lommel <brecht@blender.org>
2025-02-18 18:16:23 +0100

    Cycles: Refactor lights to be objects
    This is an intermediate steps towards making lights actual geometry.
    Light is now a subclass of Geometry, which simplifies some code.
    
    The geometry is not addeed to the BVH yet, which would be the next
    step and improve light intersection performance with many lights.

--- intern/cycles/blender/geometry.cpp
+++ intern/cycles/blender/geometry.cpp
@@ -4,6 +4,7 @@
 
 #include "scene/curves.h"
 #include "scene/hair.h"
+#include "scene/light.h"
 #include "scene/mesh.h"
 #include "scene/object.h"
 #include "scene/pointcloud.h"
@@ -18,6 +19,10 @@ CCL_NAMESPACE_BEGIN
 
 static Geometry::Type determine_geom_type(BObjectInfo &b_ob_info, bool use_particle_hair)
 {
+  if (b_ob_info.object_data.is_a(&RNA_Light)) {
+    return Geometry::LIGHT;
+  }
+
   if (b_ob_info.object_data.is_a(&RNA_Curves) || use_particle_hair) {
     return Geometry::HAIR;
   }
@@ -95,7 +100,10 @@ Geometry *BlenderSync::sync_geometry(BL::Depsgraph &b_depsgraph,
   bool sync = true;
   if (geom == nullptr) {
     /* Add new geometry if it did not exist yet. */
-    if (geom_type == Geometry::HAIR) {
+    if (geom_type == Geometry::LIGHT) {
+      geom = scene->create_node<Light>();
+    }
+    else if (geom_type == Geometry::HAIR) {
       geom = scene->create_node<Hair>();
     }
     else if (geom_type == Geometry::VOLUME) {
@@ -115,6 +123,11 @@ Geometry *BlenderSync::sync_geometry(BL::Depsgraph &b_depsgraph,
   }
 
   if (!sync) {
+    /* Need to determine this every sync. */
+    if (geom->is_light() && static_cast<const Light *>(geom)->get_is_portal()) {
+      world_use_portal = true;
+    }
+
     /* If transform was applied to geometry, need full update. */
     if (object_updated && geom->transform_applied) {
       ;
@@ -156,7 +169,11 @@ Geometry *BlenderSync::sync_geometry(BL::Depsgraph &b_depsgraph,
 
     progress.set_sync_status("Synchronizing object", b_ob_info.real_object.name());
 
-    if (geom_type == Geometry::HAIR) {
+    if (geom_type == Geometry::LIGHT) {
+      Light *light = static_cast<Light *>(geom);
+      sync_light(b_depsgraph, b_ob_info, light);
+    }
+    else if (geom_type == Geometry::HAIR) {
       Hair *hair = static_cast<Hair *>(geom);
       sync_hair(b_depsgraph, b_ob_info, hair);
     }
@@ -206,6 +223,11 @@ void BlenderSync::sync_geometry_motion(BL::Depsgraph &b_depsgraph,
   /* Ensure we only motion sync geometry that also had geometry synced, to avoid
    * unnecessary work and to ensure that its attributes were clear. */
   if (geometry_synced.find(geom) == geometry_synced.end()) {
+    return;
+  }
+
+  /* Nothing to do for lights. */
+  if (geom->is_light()) {
     return;
   }
 
--- intern/cycles/kernel/geom/object.h
+++ intern/cycles/kernel/geom/object.h
@@ -40,16 +40,6 @@ ccl_device_inline Transform object_fetch_transform(KernelGlobals kg,
   return kernel_data_fetch(objects, object).tfm;
 }
 
-/* Lamp to world space transformation */
-
-ccl_device_inline Transform lamp_fetch_transform(KernelGlobals kg, const int lamp, bool inverse)
-{
-  if (inverse) {
-    return kernel_data_fetch(lights, lamp).itfm;
-  }
-  return kernel_data_fetch(lights, lamp).tfm;
-}
-
 /* Object to world space transformation for motion vectors */
 
 ccl_device_inline Transform object_fetch_motion_pass_transform(KernelGlobals kg,
@@ -173,7 +163,7 @@ ccl_device_inline void object_inverse_normal_transform(KernelGlobals kg,
 {
 #ifdef __OBJECT_MOTION__
   if (sd->object_flag & SD_OBJECT_MOTION) {
-    if ((sd->object != OBJECT_NONE) || (sd->type == PRIMITIVE_LAMP)) {
+    if (sd->object != OBJECT_NONE) {
       *N = safe_normalize(transform_direction_transposed_auto(&sd->ob_tfm_motion, *N));
     }
     return;
@@ -182,10 +172,6 @@ ccl_device_inline void object_inverse_normal_transform(KernelGlobals kg,
 
   if (sd->object != OBJECT_NONE) {
     const Transform tfm = object_fetch_transform(kg, sd->object, OBJECT_TRANSFORM);
-    *N = safe_normalize(transform_direction_transposed(&tfm, *N));
-  }
-  else if (sd->type == PRIMITIVE_LAMP) {
-    const Transform tfm = lamp_fetch_transform(kg, sd->lamp, false);
     *N = safe_normalize(transform_direction_transposed(&tfm, *N));
   }
 }
@@ -205,10 +191,6 @@ ccl_device_inline void object_normal_transform(KernelGlobals kg,
 
   if (sd->object != OBJECT_NONE) {
     const Transform tfm = object_fetch_transform(kg, sd->object, OBJECT_INVERSE_TRANSFORM);
-    *N = normalize(transform_direction_transposed(&tfm, *N));
-  }
-  else if (sd->type == PRIMITIVE_LAMP) {
-    const Transform tfm = lamp_fetch_transform(kg, sd->lamp, true);
     *N = normalize(transform_direction_transposed(&tfm, *N));
   }
 }
@@ -304,17 +286,6 @@ ccl_device_inline float object_pass_id(KernelGlobals kg, const int object)
   return kernel_data_fetch(objects, object).pass_id;
 }
 
-/* Light-group of lamp. */
-
-ccl_device_inline int lamp_lightgroup(KernelGlobals kg, const int lamp)
-{
-  if (lamp == LAMP_NONE) {
-    return LIGHTGROUP_NONE;
-  }
-
-  return kernel_data_fetch(lights, lamp).lightgroup;
-}
-
 /* Light-group of object. */
 
 ccl_device_inline int object_lightgroup(KernelGlobals kg, const int object)
@@ -324,17 +295,6 @@ ccl_device_inline int object_lightgroup(KernelGlobals kg, const int object)
   }
 
   return kernel_data_fetch(objects, object).lightgroup;
-}
-
-/* Per lamp random number for shader variation */
-
-ccl_device_inline float lamp_random_number(KernelGlobals kg, const int lamp)
-{
-  if (lamp == LAMP_NONE) {
-    return 0.0f;
-  }
-
-  return kernel_data_fetch(lights, lamp).random;
 }
 
 /* Per object random number for shader variation */
