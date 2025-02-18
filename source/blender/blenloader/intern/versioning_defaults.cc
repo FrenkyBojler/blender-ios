92df05abbd30cc93e364c83339640a9753fb1a75
Sean Kim <SeanCTKim@protonmail.com>
2025-02-18 13:09:51 -0800

    Remove unnecessary default scene versioning

--- source/blender/blenloader/intern/versioning_defaults.cc
+++ source/blender/blenloader/intern/versioning_defaults.cc
@@ -444,10 +444,6 @@ static void blo_update_defaults_scene(Main *bmain, Scene *scene)
   ts->unified_paint_settings.flag = default_ups.flag;
   copy_v3_v3(ts->unified_paint_settings.rgb, default_ups.rgb);
   copy_v3_v3(ts->unified_paint_settings.secondary_rgb, default_ups.secondary_rgb);
-
-  if (ts->imapaint.clone_alpha == 0.0f) {
-    ts->imapaint.clone_alpha = 0.5f;
-  }
 }
 
 void BLO_update_defaults_startup_blend(Main *bmain, const char *app_template)
