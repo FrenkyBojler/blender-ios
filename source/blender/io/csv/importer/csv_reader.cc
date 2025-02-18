72d947e25da02ebff5b8c83cbe0257a68819007a
Jacques Lucke <jacques@blender.org>
2025-02-18 01:54:41 +0100

    cleanup

--- source/blender/io/csv/importer/csv_reader.cc
+++ source/blender/io/csv/importer/csv_reader.cc
@@ -32,15 +32,12 @@ struct ChunkResult {
   Vector<ColumnData> columns;
 };
 
-struct ColumnTypeInfo {
+struct ColumnInfo {
+  StringRef name;
+  bool has_invalid_name = false;
   std::atomic<bool> found_invalid = false;
   std::atomic<bool> found_int = false;
   std::atomic<bool> found_float = false;
-};
-
-struct ColumnsInfo {
-  Array<StringRef> names;
-  Array<ColumnTypeInfo> types;
 };
 
 struct ParseFloatColumnResult {
@@ -52,6 +49,11 @@ struct ParseIntColumnResult {
   Vector<int> data;
   bool found_invalid = false;
   bool found_float = false;
+};
+
+struct FlattenedAttribute {
+  eCustomDataType type;
+  void *data;
 };
 
 static ParseFloatColumnResult parse_column_as_floats(const csv_parse::CsvRecords &records,
@@ -128,24 +130,28 @@ static ParseIntColumnResult parse_column_as_ints(const csv_parse::CsvRecords &re
 }
 
 static ChunkResult parse_records_chunk(const csv_parse::CsvRecords &records,
-                                       ColumnsInfo &columns_info)
+                                       MutableSpan<ColumnInfo> columns_info)
 {
-  const int columns_num = columns_info.names.size();
+  const int columns_num = columns_info.size();
   ChunkResult chunk_result;
   chunk_result.rows_num = records.size();
   chunk_result.columns.resize(columns_num);
   for (const int column_i : IndexRange(columns_num)) {
-    ColumnTypeInfo &type_info = columns_info.types[column_i];
-    if (type_info.found_invalid.load(std::memory_order_relaxed)) {
+    ColumnInfo &column_info = columns_info[column_i];
+    if (column_info.has_invalid_name) {
+      /* Column can be ignored. */
+      continue;
+    }
+    if (column_info.found_invalid.load(std::memory_order_relaxed)) {
       /* Invalid values have been found in this column already, skip it. */
       continue;
     }
     /* A float was found in this column already, so parse everything as floats. */
-    const bool found_float = type_info.found_float.load(std::memory_order_relaxed);
+    const bool found_float = column_info.found_float.load(std::memory_order_relaxed);
     if (found_float) {
       ParseFloatColumnResult float_column_result = parse_column_as_floats(records, column_i);
       if (float_column_result.found_invalid) {
-        type_info.found_invalid.store(true, std::memory_order_relaxed);
+        column_info.found_invalid.store(true, std::memory_order_relaxed);
         continue;
       }
       chunk_result.columns[column_i] = std::move(float_column_result.data);
@@ -154,24 +160,96 @@ static ChunkResult parse_records_chunk(const csv_parse::CsvRecords &records,
     /* No float was found so far in this column, so attempt to parse it as integers. */
     ParseIntColumnResult int_column_result = parse_column_as_ints(records, column_i);
     if (int_column_result.found_invalid) {
-      type_info.found_invalid.store(true, std::memory_order_relaxed);
+      column_info.found_invalid.store(true, std::memory_order_relaxed);
       continue;
     }
     if (!int_column_result.found_float) {
       chunk_result.columns[column_i] = std::move(int_column_result.data);
-      type_info.found_int.store(true, std::memory_order_relaxed);
+      column_info.found_int.store(true, std::memory_order_relaxed);
       continue;
     }
     /* While parsing it as integers, floats were detected. So parse it as floats again. */
-    type_info.found_float.store(true, std::memory_order_relaxed);
+    column_info.found_float.store(true, std::memory_order_relaxed);
     ParseFloatColumnResult float_column_result = parse_column_as_floats(records, column_i);
     if (float_column_result.found_invalid) {
-      type_info.found_invalid.store(true, std::memory_order_relaxed);
+      column_info.found_invalid.store(true, std::memory_order_relaxed);
       continue;
     }
     chunk_result.columns[column_i] = std::move(float_column_result.data);
   }
   return chunk_result;
+}
+
+static Array<std::optional<FlattenedAttribute>> flatten_chunk_as_attributes(
+    const Span<ColumnInfo> columns_info,
+    OffsetIndices<int> chunk_offsets,
+    const Span<ChunkResult> chunks)
+{
+  const int points_num = chunk_offsets.total_size();
+  Array<std::optional<FlattenedAttribute>> flattened_attributes(columns_info.size());
+
+  threading::parallel_for(columns_info.index_range(), 1, [&](const IndexRange columns_range) {
+    for (const int column_i : columns_range) {
+      const ColumnInfo &column_info = columns_info[column_i];
+      if (column_info.has_invalid_name || column_info.found_invalid) {
+        /* Column can be ignored. */
+        continue;
+      }
+      if (column_info.found_float) {
+        /* Should read column as floats. */
+        float *attribute_buffer = static_cast<float *>(MEM_mallocN_aligned(
+            sizeof(float) * points_num, alignof(float), "csv float attribute"));
+        flattened_attributes[column_i] = FlattenedAttribute{CD_PROP_FLOAT, attribute_buffer};
+        threading::parallel_for(chunks.index_range(), 1, [&](const IndexRange chunks_range) {
+          for (const int chunk_i : chunks_range) {
+            const IndexRange dst_range = chunk_offsets[chunk_i];
+            const ChunkResult &chunk = chunks[chunk_i];
+            const ColumnData &column_data = chunk.columns[column_i];
+            if (const auto *float_vec = std::get_if<Vector<float>>(&column_data)) {
+              BLI_assert(float_vec->size() == dst_range.size());
+              uninitialized_copy_n(
+                  float_vec->data(), dst_range.size(), attribute_buffer + dst_range.first());
+            }
+            else if (const auto *int_vec = std::get_if<Vector<int>>(&column_data)) {
+              BLI_assert(int_vec->size() == dst_range.size());
+              uninitialized_convert_n(int_vec->data(), dst_range.size(), attribute_buffer);
+            }
+            else {
+              /* Expected data to be available, because the `found_invalid` flag was not
+               * set. */
+              BLI_assert_unreachable();
+            }
+          }
+        });
+        continue;
+      }
+      if (column_info.found_int) {
+        /* Should read column as ints. */
+        int *attribute_buffer = static_cast<int *>(
+            MEM_mallocN_aligned(sizeof(int) * points_num, alignof(int), "csv int attribute"));
+        flattened_attributes[column_i] = FlattenedAttribute{CD_PROP_INT32, attribute_buffer};
+        threading::parallel_for(chunks.index_range(), 1, [&](const IndexRange chunks_range) {
+          for (const int chunk_i : chunks_range) {
+            const IndexRange dst_range = chunk_offsets[chunk_i];
+            const ChunkResult &chunk = chunks[chunk_i];
+            const ColumnData &column_data = chunk.columns[column_i];
+            if (const auto *int_vec = std::get_if<Vector<int>>(&column_data)) {
+              BLI_assert(int_vec->size() == dst_range.size());
+              uninitialized_copy_n(
+                  int_vec->data(), dst_range.size(), attribute_buffer + dst_range.first());
+            }
+            else {
+              /* Expected data to be available, because the `found_invalid` and
+               * `found_float` flags were not set. */
+              BLI_assert_unreachable();
+            }
+          }
+        });
+        continue;
+      }
+    }
+  });
+  return flattened_attributes;
 }
 
 PointCloud *import_csv_as_point_cloud(const CSVImportParams &import_params)
@@ -185,22 +263,27 @@ PointCloud *import_csv_as_point_cloud(const CSVImportParams &import_params)
                 import_params.filepath);
     return nullptr;
   }
-
   BLI_SCOPED_DEFER([&]() { MEM_freeN(buffer); });
-
   if (buffer_len == 0) {
     BKE_reportf(
         import_params.reports, RPT_ERROR, "CSV Import: empty file '%s'", import_params.filepath);
     return nullptr;
   }
 
-  ColumnsInfo columns_info;
+  Array<ColumnInfo> columns_info;
 
   const auto parse_header = [&](const csv_parse::CsvRecord &record) {
-    columns_info.names.reinitialize(record.size());
-    columns_info.types.reinitialize(record.size());
+    columns_info.reinitialize(record.size());
     for (const int i : record.index_range()) {
-      columns_info.names[i] = record.field_str(i);
+      ColumnInfo &column_info = columns_info[i];
+      const StringRef name = record.field_str(i);
+      column_info.name = name;
+      if (!bke::allow_procedural_attribute_access(name) ||
+          bke::attribute_name_is_anonymous(name) || name.is_empty())
+      {
+        column_info.has_invalid_name = true;
+        continue;
+      }
     }
   };
   const auto parse_data_chunk = [&](const csv_parse::CsvRecords &records) {
@@ -212,9 +295,7 @@ PointCloud *import_csv_as_point_cloud(const CSVImportParams &import_params)
   const std::optional<Vector<ChunkResult>> parsed_chunks =
       csv_parse::parse_csv_in_chunks<ChunkResult>(
           buffer_span, parse_options, parse_header, parse_data_chunk);
-  for (StringRef name : columns_info.names) {
-    printf("name: %s\n", std::string(name).c_str());
-  }
+
   if (!parsed_chunks.has_value()) {
     BKE_reportf(import_params.reports,
                 RPT_ERROR,
@@ -223,103 +304,34 @@ PointCloud *import_csv_as_point_cloud(const CSVImportParams &import_params)
     return nullptr;
   }
 
-  Vector<int> flatten_offsets_vec;
-  flatten_offsets_vec.append(0);
+  Vector<int> chunk_offsets_vec;
+  chunk_offsets_vec.append(0);
   for (const ChunkResult &chunk : *parsed_chunks) {
-    flatten_offsets_vec.append(flatten_offsets_vec.last() + chunk.rows_num);
+    chunk_offsets_vec.append(chunk_offsets_vec.last() + chunk.rows_num);
   }
-  const OffsetIndices<int> chunk_offsets(flatten_offsets_vec);
-  const int points_num = flatten_offsets_vec.last();
-
-  struct FlattenedAttribute {
-    eCustomDataType type;
-    void *data;
-  };
-  Array<std::optional<FlattenedAttribute>> flattened_attributes(columns_info.names.size());
-  threading::parallel_for(
-      columns_info.names.index_range(), 1, [&](const IndexRange columns_range) {
-        for (const int column_i : columns_range) {
-          const ColumnTypeInfo &type_info = columns_info.types[column_i];
-          if (type_info.found_invalid) {
-            /* Can't read data from this column. */
-            continue;
-          }
-          if (type_info.found_float) {
-            /* Should read column as floats. */
-            float *attribute_buffer = static_cast<float *>(MEM_mallocN_aligned(
-                sizeof(float) * points_num, alignof(float), "csv float attribute"));
-            flattened_attributes[column_i] = FlattenedAttribute{CD_PROP_FLOAT, attribute_buffer};
-            threading::parallel_for(
-                parsed_chunks->index_range(), 1, [&](const IndexRange chunks_range) {
-                  for (const int chunk_i : chunks_range) {
-                    const IndexRange dst_range = chunk_offsets[chunk_i];
-                    const ChunkResult &chunk = (*parsed_chunks)[chunk_i];
-                    const ColumnData &column_data = chunk.columns[column_i];
-                    if (const auto *float_vec = std::get_if<Vector<float>>(&column_data)) {
-                      BLI_assert(float_vec->size() == dst_range.size());
-                      uninitialized_copy_n(float_vec->data(),
-                                           dst_range.size(),
-                                           attribute_buffer + dst_range.first());
-                    }
-                    else if (const auto *int_vec = std::get_if<Vector<int>>(&column_data)) {
-                      BLI_assert(int_vec->size() == dst_range.size());
-                      uninitialized_convert_n(int_vec->data(), dst_range.size(), attribute_buffer);
-                    }
-                    else {
-                      /* Expected data to be available, because the `found_invalid` flag was not
-                       * set. */
-                      BLI_assert_unreachable();
-                    }
-                  }
-                });
-            continue;
-          }
-          if (type_info.found_int) {
-            /* Should read column as ints. */
-            int *attribute_buffer = static_cast<int *>(
-                MEM_mallocN_aligned(sizeof(int) * points_num, alignof(int), "csv int attribute"));
-            flattened_attributes[column_i] = FlattenedAttribute{CD_PROP_INT32, attribute_buffer};
-            threading::parallel_for(
-                parsed_chunks->index_range(), 1, [&](const IndexRange chunks_range) {
-                  for (const int chunk_i : chunks_range) {
-                    const IndexRange dst_range = chunk_offsets[chunk_i];
-                    const ChunkResult &chunk = (*parsed_chunks)[chunk_i];
-                    const ColumnData &column_data = chunk.columns[column_i];
-                    if (const auto *int_vec = std::get_if<Vector<int>>(&column_data)) {
-                      BLI_assert(int_vec->size() == dst_range.size());
-                      uninitialized_copy_n(
-                          int_vec->data(), dst_range.size(), attribute_buffer + dst_range.first());
-                    }
-                    else {
-                      /* Expected data to be available, because the `found_invalid` and
-                       * `found_float` flags were not set. */
-                      BLI_assert_unreachable();
-                    }
-                  }
-                });
-            continue;
-          }
-        }
-      });
+  const OffsetIndices<int> chunk_offsets(chunk_offsets_vec);
+  const int points_num = chunk_offsets_vec.last();
 
   PointCloud *pointcloud = BKE_pointcloud_new_nomain(points_num);
-  pointcloud->positions_for_write().fill(float3(0));
 
+  Array<std::optional<FlattenedAttribute>> flattened_attributes;
+  threading::memory_bandwidth_bound_task(points_num * 16, [&]() {
+    threading::parallel_invoke([&]() { pointcloud->positions_for_write().fill(float3(0)); },
+                               [&]() {
+                                 flattened_attributes = flatten_chunk_as_attributes(
+                                     columns_info, chunk_offsets, *parsed_chunks);
+                               });
+  });
+
+  /* Add all valid attributes to the pointcloud. */
   bke::MutableAttributeAccessor attributes = pointcloud->attributes_for_write();
-
-  for (const int column_i : columns_info.names.index_range()) {
+  for (const int column_i : columns_info.index_range()) {
     const std::optional<FlattenedAttribute> &attribute = flattened_attributes[column_i];
     if (!attribute.has_value()) {
       continue;
     }
-    const StringRef name = columns_info.names[column_i];
-    if (!bke::allow_procedural_attribute_access(name)) {
-      continue;
-    }
-    if (bke::attribute_name_is_anonymous(name)) {
-      continue;
-    }
-    attributes.add(name,
+    const ColumnInfo &column_info = columns_info[column_i];
+    attributes.add(column_info.name,
                    bke::AttrDomain::Point,
                    attribute->type,
                    bke::AttributeInitMoveArray{attribute->data});
