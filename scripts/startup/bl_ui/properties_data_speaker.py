bbce167fb2cf24001571289a3da0650967090a6d
Aaron Carlisle <carlisle.b3d@gmail.com>
2025-02-17 23:35:22 -0500

    UI: Expose addition properties for speaker objects
    - Make "Distance", "Cone" sub panels to "Sound" this is similar to light objects
    - Move "Mute" to "Sound" header
    - Add settings for packing/caching the sound after adding
    - Add source information to display the Sample rate and channel count

--- scripts/startup/bl_ui/properties_data_speaker.py
+++ scripts/startup/bl_ui/properties_data_speaker.py
@@ -49,25 +49,71 @@ class DATA_PT_speaker(DataButtonsPanel, Panel):
         'BLENDER_WORKBENCH',
     }
 
+    def draw_header(self, context):
+        layout = self.layout
+        speaker = context.speaker
+
+        layout.prop(speaker, "muted", text="")
+
+
     def draw(self, context):
         layout = self.layout
 
         speaker = context.speaker
+        sound = speaker.sound
 
         layout.template_ID(speaker, "sound", open="sound.open_mono")
 
         layout.use_property_split = True
 
-        layout.prop(speaker, "muted")
+        if sound is not None:
+            col = layout.column()
+            col.prop(sound, "filepath", text="")
+
+            col.alignment = 'RIGHT'
+            sub = col.column(align=True)
+            split = sub.split(factor=0.5, align=True)
+            split.alignment = 'RIGHT'
+            if sound.packed_file:
+                split.label(text="Unpack")
+                split.operator("sound.unpack", icon='PACKAGE', text="")
+            else:
+                split.label(text="Pack")
+                split.operator("sound.pack", icon='UGLYPACKAGE', text="")
+
+            layout.prop(sound, "use_memory_cache")
 
         col = layout.column()
         col.active = not speaker.muted
         col.prop(speaker, "volume", slider=True)
         col.prop(speaker, "pitch")
 
+        if sound is not None:
+            col = layout.box()
+            col = col.column(align=True)
+            split = col.split(factor=0.5, align=False)
+            split.alignment = 'RIGHT'
+            split.label(text="Sample Rate")
+            split.alignment = 'LEFT'
+            if sound.samplerate <= 0:
+                split.label(text="Unknown")
+            else:
+                split.label(text="{:d} Hz".format(sound.samplerate), translate=False)
+
+            split = col.split(factor=0.5, align=False)
+            split.alignment = 'RIGHT'
+            split.label(text="Channels")
+            split.alignment = 'LEFT'
+
+            # FIXME(@campbellbarton): this is ugly, we may want to support a way of showing a label from an enum.
+            channel_enum_items = sound.bl_rna.properties["channels"].enum_items
+            split.label(text=channel_enum_items[channel_enum_items.find(sound.channels)].name)
+            del channel_enum_items
+
 
 class DATA_PT_distance(DataButtonsPanel, Panel):
     bl_label = "Distance"
+    bl_parent_id = "DATA_PT_speaker"
     bl_options = {'DEFAULT_CLOSED'}
     COMPAT_ENGINES = {
         'BLENDER_RENDER',
@@ -96,6 +142,7 @@ class DATA_PT_distance(DataButtonsPanel, Panel):
 
 class DATA_PT_cone(DataButtonsPanel, Panel):
     bl_label = "Cone"
+    bl_parent_id = "DATA_PT_speaker"
     bl_options = {'DEFAULT_CLOSED'}
     COMPAT_ENGINES = {
         'BLENDER_RENDER',
