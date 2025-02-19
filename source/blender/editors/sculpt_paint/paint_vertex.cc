104a6987dfacd7d2f74c83b30d61473a5abb64bc
Sean Kim <SeanCTKim@protonmail.com>
2025-02-18 21:41:35 -0800

    Use IndexMask instead of iterating over all nodes for accumulation

--- source/blender/editors/sculpt_paint/paint_vertex.cc
+++ source/blender/editors/sculpt_paint/paint_vertex.cc
@@ -1614,12 +1614,12 @@ static void calculate_average_color(VPaintData &vpd,
     Blend accum_value[3] = {0};
     Color blend(0, 0, 0, 0);
 
-    for (int i = 0; i < nodes.size(); i++) {
+    node_mask.foreach_index([&](const int i) {
       accum_len += accum[i].len;
       accum_value[0] += accum[i].value[0];
       accum_value[1] += accum[i].value[1];
       accum_value[2] += accum[i].value[2];
-    }
+    });
     if (accum_len != 0) {
       blend.r = Traits::round(sqrtf(Traits::divide_round(accum_value[0], accum_len)));
       blend.g = Traits::round(sqrtf(Traits::divide_round(accum_value[1], accum_len)));
