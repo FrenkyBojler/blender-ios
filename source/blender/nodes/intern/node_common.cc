769bc2ad741d884a3c222f68062fbdb869933eaf
Hans Goudey <hans@blender.org>
2025-02-19 15:10:14 -0500

    Cleanup

--- source/blender/nodes/intern/node_common.cc
+++ source/blender/nodes/intern/node_common.cc
@@ -785,8 +785,6 @@ static void group_input_declare(NodeDeclarationBuilder &b)
   }
   node_tree->ensure_interface_cache();
   const Span<const bNodeTreeInterfaceSocket *> inputs = node_tree->interface_inputs();
-  // const Span<StructureType> structure_types =
-  // node_tree->runtime->structure_type_interface->inputs;
   for (const int i : inputs.index_range()) {
     build_interface_socket_declaration(
         *node_tree, *inputs[i], StructureType::Dynamic, SOCK_OUT, b);
@@ -802,8 +800,6 @@ static void group_output_declare(NodeDeclarationBuilder &b)
   }
   node_tree->ensure_interface_cache();
   const Span<const bNodeTreeInterfaceSocket *> outputs = node_tree->interface_outputs();
-  // const Span<StructureType> structure_types =
-  //     node_tree->runtime->structure_type_interface->outputs;
   for (const int i : outputs.index_range()) {
     build_interface_socket_declaration(
         *node_tree, *outputs[i], StructureType::Dynamic, SOCK_IN, b);
