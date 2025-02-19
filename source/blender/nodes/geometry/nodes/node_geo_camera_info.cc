3712bd85345301b5b3f8b6679726de791dcdf995
Bbbn19 <85761261+Bbbn19@users.noreply.github.com>
2025-02-18 20:55:47 -0500

    add camera info node

---
+++ source/blender/nodes/geometry/nodes/node_geo_camera_info.cc
@@ -0,0 +1,154 @@
+#include "BKE_camera.h"
+#include "DEG_depsgraph_query.hh"
+#include "DNA_camera_types.h"
+#include "node_geometry_util.hh"
+
+#include "UI_interface.hh"
+#include "UI_resources.hh"
+
+namespace blender::nodes::node_geo_camera_info_cc {
+
+static void node_declare(NodeDeclarationBuilder &b)
+{
+  b.use_custom_socket_order();
+
+  b.add_output<decl::Bool>("Is Active Camera");
+  b.add_input<decl::Object>("Camera").hide_label();
+
+  PanelDeclarationBuilder &foc = b.add_panel("Focal").default_closed(true);
+  foc.add_output<decl::Float>("Focal Length");
+  foc.add_output<decl::Float>("Sensor Width");
+  foc.add_output<decl::Float>("Sensor Height");
+  foc.add_output<decl::Float>("Shift X");
+  foc.add_output<decl::Float>("Shift Y");
+  foc.add_output<decl::Float>("Clip Start");
+  foc.add_output<decl::Float>("Clip End");
+  foc.add_output<decl::Int>("Resolution X");
+  foc.add_output<decl::Int>("Resolution Y");
+  foc.add_output<decl::Float>("Aspect X");
+  foc.add_output<decl::Float>("Aspect Y");
+  foc.add_output<decl::Float>("Field of View X");
+  foc.add_output<decl::Float>("Field of View Y");
+
+  PanelDeclarationBuilder &dof = b.add_panel("Depth of Field").default_closed(true);
+  dof.add_output<decl::Bool>("Depth of Field Enabled");
+  dof.add_output<decl::Object>("Focus Object");
+  dof.add_output<decl::Float>("Focus Distance");
+  dof.add_output<decl::Float>("Aperture F-Stop");
+  dof.add_output<decl::Int>("Aperture Blades");
+  dof.add_output<decl::Float>("Aperture Rotation");
+  dof.add_output<decl::Float>("Aperture Ratio");
+
+  PanelDeclarationBuilder &ortho = b.add_panel("Orthographic").default_closed(true);
+  ortho.add_output<decl::Bool>("Is Orthographic");
+  ortho.add_output<decl::Float>("Orthographic Scale");
+
+  PanelDeclarationBuilder &pano = b.add_panel("Panoramic").default_closed(true);
+  pano.add_output<decl::Bool>("Is Panoramic");
+  pano.add_output<decl::Float>("Fisheye FOV");
+  pano.add_output<decl::Float>("Fisheye Lens");
+  pano.add_output<decl::Float>("Latitude Min");
+  pano.add_output<decl::Float>("Latitude Max");
+  pano.add_output<decl::Float>("Longitude Min");
+  pano.add_output<decl::Float>("Longitude Max");
+  pano.add_output<decl::Float>("Fisheye Polynomial K0");
+  pano.add_output<decl::Float>("Fisheye Polynomial K1");
+  pano.add_output<decl::Float>("Fisheye Polynomial K2");
+  pano.add_output<decl::Float>("Fisheye Polynomial K3");
+  pano.add_output<decl::Float>("Fisheye Polynomial K4");
+
+  PanelDeclarationBuilder &ster = b.add_panel("Stereo").default_closed(true);
+  ster.add_output<decl::Float>("Interocular Distance");
+  ster.add_output<decl::Float>("Convergence Distance");
+  ster.add_output<decl::Float>("Pole Merge Angle From");
+  ster.add_output<decl::Float>("Pole Merge Angle To");
+}
+
+static void node_geo_exec(GeoNodeExecParams params)
+{
+  Object *camera_obj = params.get_input<Object *>("Camera");
+
+  // Check if the object is valid and a camera
+  if (!camera_obj || camera_obj->type != OB_CAMERA) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
+  // Get data from the camera object
+  Camera *camera = (Camera *)camera_obj->data;
+  if (!camera) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
+  // Get the evaluated scene
+  const Scene *scene = DEG_get_evaluated_scene(params.depsgraph());
+  if (!scene) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
+  float fovx = scene->r.xsch * scene->r.xasp;
+  float fovy = scene->r.ysch * scene->r.yasp;
+
+  params.set_output("Is Active Camera", scene->camera == camera_obj);
+
+  params.set_output("Focal Length", camera->lens);
+  params.set_output("Sensor Width", camera->sensor_x);
+  params.set_output("Sensor Height", camera->sensor_y);
+  params.set_output("Shift X", camera->shiftx);
+  params.set_output("Shift Y", camera->shifty);
+  params.set_output("Clip Start", camera->clip_start);
+  params.set_output("Clip End", camera->clip_end);
+  params.set_output("Resolution X", scene->r.xsch);
+  params.set_output("Resolution Y", scene->r.ysch);
+  params.set_output("Aspect X", scene->r.xasp);
+  params.set_output("Aspect Y", scene->r.yasp);
+  params.set_output("Field of View X", (fovx >= fovy) ? 1.0f : fovx / fovy);
+  params.set_output("Field of View Y", (fovx >= fovy) ? fovy / fovx : 1.0f);
+
+  params.set_output("Depth of Field Enabled", camera->dof.flag == CAM_DOF_ENABLED);
+  params.set_output("Focus Object", camera->dof.focus_object);
+  params.set_output("Focus Distance", camera->dof.focus_distance);
+  params.set_output("Aperture F-Stop", camera->dof.aperture_fstop);
+  params.set_output("Aperture Blades", camera->dof.aperture_blades);
+  params.set_output("Aperture Rotation", camera->dof.aperture_rotation);
+  params.set_output("Aperture Ratio", camera->dof.aperture_ratio);
+
+  params.set_output("Is Orthographic", camera->type == CAM_ORTHO);
+  params.set_output("Orthographic Scale", camera->ortho_scale);
+
+  params.set_output("Is Panoramic", camera->type == CAM_PANO);
+  params.set_output("Fisheye FOV", camera->fisheye_fov);
+  params.set_output("Fisheye Lens", camera->fisheye_lens);
+  params.set_output("Latitude Min", camera->latitude_min);
+  params.set_output("Latitude Max", camera->latitude_max);
+  params.set_output("Longitude Min", camera->longitude_min);
+  params.set_output("Longitude Max", camera->longitude_max);
+  params.set_output("Fisheye Polynomial K0", camera->fisheye_polynomial_k0);
+  params.set_output("Fisheye Polynomial K1", camera->fisheye_polynomial_k1);
+  params.set_output("Fisheye Polynomial K2", camera->fisheye_polynomial_k2);
+  params.set_output("Fisheye Polynomial K3", camera->fisheye_polynomial_k3);
+  params.set_output("Fisheye Polynomial K4", camera->fisheye_polynomial_k4);
+
+  params.set_output("Interocular Distance", camera->stereo.interocular_distance);
+  params.set_output("Convergence Distance", camera->stereo.convergence_distance);
+  params.set_output("Pole Merge Angle From", camera->stereo.pole_merge_angle_from);
+  params.set_output("Pole Merge Angle To", camera->stereo.pole_merge_angle_to);
+}
+
+static void node_register()
+{
+  static blender::bke::bNodeType ntype;
+
+  geo_node_type_base(&ntype, "GeometryNodeCameraInfo");
+  ntype.ui_name = "Camera Info";
+  ntype.ui_description = "Retrieve information from a camera object";
+  ntype.nclass = NODE_CLASS_INPUT;
+  ntype.geometry_node_execute = node_geo_exec;
+  ntype.declare = node_declare;
+  blender::bke::node_register_type(&ntype);
+}
+NOD_REGISTER_NODE(node_register)
+
+}  // namespace blender::nodes::node_geo_camera_info_cc
