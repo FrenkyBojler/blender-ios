deb46247c18c671953b4278ec426f5577a8ce63e
Kamil Galik <kgalik@3dconnexion.com>
2025-02-20 16:01:58 +0100

    Fix #134730: adjust panning speed with distance to ndof_cor

--- source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
+++ source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
@@ -101,7 +101,8 @@ static float view3d_ndof_pan_speed_calc_from_dist(RegionView3D *rv3d, const floa
 static float view3d_ndof_pan_speed_calc(RegionView3D *rv3d)
 {
   float tvec[3];
-  negate_v3_v3(tvec, rv3d->ofs);
+  const float3& ofs = (U.ndof_flag & NDOF_ORBIT_CENTER_AUTO) ? rv3d->ndof_ofs : rv3d->ofs;
+  negate_v3_v3(tvec, ofs);
 
   return view3d_ndof_pan_speed_calc_ex(rv3d, tvec);
 }
