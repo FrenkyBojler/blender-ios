f33b385c1f977b61da66a6d2ee94a960a67ad098
Jeroen Bakker <jeroen@blender.org>
2025-02-20 14:02:25 +0100

    Fix for NVIDIA/OpenGL

--- source/blender/draw/intern/shaders/subdiv_normals_accumulate_comp.glsl
+++ source/blender/draw/intern/shaders/subdiv_normals_accumulate_comp.glsl
@@ -44,7 +44,8 @@ void main()
     /* Compute the face normal using Newell's method. */
     vec3 verts[4];
     for (uint j = 0; j < 4; j++) {
-      verts[j] = subdiv_get_vertex_pos(pos_nor[start_loop_index + j]);
+      PosNorLoop vertex_data = pos_nor[start_loop_index + j];
+      verts[j] = subdiv_get_vertex_pos(vertex_data);
     }
 
     vec3 face_normal = vec3(0.0);
