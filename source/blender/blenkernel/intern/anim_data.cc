013ddf24a533b2de1df521c5793a83e70d06324a
Sybren A. Stüvel <sybren@blender.org>
2025-02-20 12:50:17 +0100

    Anim: make `BKE_fcurves_id_cb()` only report F-Curves for that ID
    Update `BKE_fcurves_id_cb(id, callback)` so that it only reports
    F-Curves for `id`. With legacy Actions this used to be "all F-Curves in
    the Action", but with slotted Actions it should only report the F-Curves
    for the assigned slot.
    
    `BKE_fcurves_id_cb()` is used in two places:
    
    1. Updating F-Curve paths in versioning, for example when node sockets
       were added/removed. For this it is vital that the updates only happen
       for those nodes that were actually affected by this versioning code,
       and not blindly for all F-Curves in the Action.
    2. Marking properties as 'animated' in the depsgraph, and the 'value
       backup' for those properties. This should also be only done for the
       properties of that ID, and thus be limited to the F-Curves for the
       assigned slot.

--- source/blender/blenkernel/intern/anim_data.cc
+++ source/blender/blenkernel/intern/anim_data.cc
@@ -1245,7 +1245,10 @@ static void nlastrips_apply_all_curves_cb(ID *id,
   LISTBASE_FOREACH (NlaStrip *, strip, strips) {
     /* fix strip's action */
     if (strip->act) {
-      fcurves_apply_cb(id, blender::animrig::legacy::fcurves_all(strip->act), func);
+      fcurves_apply_cb(
+          id,
+          blender::animrig::fcurves_for_action_slot(strip->act->wrap(), strip->action_slot_handle),
+          func);
     }
 
     /* Check sub-strips (if meta-strips). */
@@ -1260,11 +1263,17 @@ static void adt_apply_all_fcurves_cb(ID *id,
                                      const FunctionRef<void(ID *, FCurve *)> func)
 {
   if (adt->action) {
-    fcurves_apply_cb(id, blender::animrig::legacy::fcurves_all(adt->action), func);
+    fcurves_apply_cb(
+        id,
+        blender::animrig::fcurves_for_action_slot(adt->action->wrap(), adt->slot_handle),
+        func);
   }
 
   if (adt->tmpact) {
-    fcurves_apply_cb(id, blender::animrig::legacy::fcurves_all(adt->tmpact), func);
+    fcurves_apply_cb(
+        id,
+        blender::animrig::fcurves_for_action_slot(adt->tmpact->wrap(), adt->tmp_slot_handle),
+        func);
   }
 
   /* free drivers - stored as a list of F-Curves */
