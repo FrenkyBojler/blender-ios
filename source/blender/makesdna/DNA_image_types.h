340e977982bc6c8a49a8597ed63fff8dcbf2f7e1
Omar Emara <mail@OmarEmara.dev>
2025-02-21 10:04:25 +0200

    Remove unrelated change

--- source/blender/makesdna/DNA_image_types.h
+++ source/blender/makesdna/DNA_image_types.h
@@ -1,4 +1,4 @@
-/*st SPDX-FileCopyrightText: 2001-2002 NaN Holding BV. All rights reserved.
+/* SPDX-FileCopyrightText: 2001-2002 NaN Holding BV. All rights reserved.
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
