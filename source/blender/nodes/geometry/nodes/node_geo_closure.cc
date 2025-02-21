120b2cf480688832f5b5b7c76df5351c2420d755
Jacques Lucke <jacques@blender.org>
2025-02-21 12:52:03 +0100

    fix after merge

--- source/blender/nodes/geometry/nodes/node_geo_closure.cc
+++ source/blender/nodes/geometry/nodes/node_geo_closure.cc
@@ -121,7 +121,7 @@ static void node_register()
   ntype.draw_buttons_ex = node_layout_ex;
   blender::bke::node_type_storage(
       &ntype, "NodeGeometryClosureInput", node_free_standard_storage, node_copy_standard_storage);
-  blender::bke::node_register_type(&ntype);
+  blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)
 
@@ -198,7 +198,7 @@ static void node_register()
   ntype.draw_buttons_ex = node_layout_ex;
   bke::node_type_storage(
       &ntype, "NodeGeometryClosureOutput", node_free_storage, node_copy_storage);
-  blender::bke::node_register_type(&ntype);
+  blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)
 
--- source/blender/nodes/geometry/nodes/node_geo_separate_bundle.cc
+++ source/blender/nodes/geometry/nodes/node_geo_separate_bundle.cc
@@ -142,7 +142,7 @@ static void node_register()
   ntype.register_operators = node_operators;
   bke::node_type_storage(
       &ntype, "NodeGeometrySeparateBundle", node_free_storage, node_copy_storage);
-  blender::bke::node_register_type(&ntype);
+  blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)
 
