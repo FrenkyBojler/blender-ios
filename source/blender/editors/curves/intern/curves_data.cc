58573f660686838982b7f7629794b5ab6f741556
Philipp Oeser <philipp@blender.org>
2025-02-21 18:09:39 +0100

    Fix: Cursor snapping does not work for Curves bezier handles
    For this to work, we have to take into account handles in TransVert as
    well.
    
    Behavior is the same as in legacy curves (meaning that if the control
    point itself is selected, the handles are ignores).
    
    Part of #133448 (same thing for grease pencil, which I plan to also make
    part of TransVert, but better solve for Curves first, so we can share
    code here).

--- source/blender/editors/curves/intern/curves_data.cc
+++ source/blender/editors/curves/intern/curves_data.cc
@@ -22,22 +22,69 @@ Vector<MutableSpan<float3>> get_curves_positions_for_write(bke::CurvesGeometry &
   return positions_per_attribute;
 }
 
-void transverts_from_curves_positions_create(bke::CurvesGeometry &curves, TransVertStore *tvs)
+void transverts_from_curves_positions_create(bke::CurvesGeometry &curves,
+                                             TransVertStore *tvs,
+                                             const bool skip_handles)
 {
+  Span<StringRef> selection_attribute_names = ed::curves::get_curves_selection_attribute_names(
+      curves);
+  std::array<IndexMask, 3> selection_per_attribute;
+
   IndexMaskMemory memory;
-  IndexMask selection = retrieve_selected_points(curves, memory);
-  MutableSpan<float3> positions = curves.positions_for_write();
+
+  for (const int attribute_i : selection_attribute_names.index_range()) {
+    const StringRef &selection_name = selection_attribute_names[attribute_i];
+    IndexMask selection = ed::curves::retrieve_selected_points(curves, selection_name, memory);
+    selection_per_attribute[attribute_i] = selection;
+  }
+
+  if (skip_handles) {
+    /* When the control point is selected, both handles are ignored. */
+    selection_per_attribute[1] = IndexMask::from_difference(
+        selection_per_attribute[1], selection_per_attribute[0], memory);
+    selection_per_attribute[2] = IndexMask::from_difference(
+        selection_per_attribute[2], selection_per_attribute[0], memory);
+  }
+
+  const int totselected = selection_per_attribute[0].size() + selection_per_attribute[1].size() +
+                          selection_per_attribute[2].size();
+
+  if (totselected == 0) {
+    return;
+  }
 
   tvs->transverts = static_cast<TransVert *>(
-      MEM_calloc_arrayN(selection.size(), sizeof(TransVert), __func__));
-  tvs->transverts_tot = selection.size();
+      MEM_calloc_arrayN(totselected, sizeof(TransVert), __func__));
+  tvs->transverts_tot = totselected;
 
-  selection.foreach_index(GrainSize(1024), [&](const int64_t i, const int64_t pos) {
-    TransVert &tv = tvs->transverts[pos];
-    tv.loc = positions[i];
-    tv.flag = SELECT;
-    copy_v3_v3(tv.oldloc, tv.loc);
-  });
+  int offset = 0;
+  MutableSpan<float3> positions;
+  for (const int attribute_i : selection_attribute_names.index_range()) {
+    if (selection_per_attribute[attribute_i].is_empty()) {
+      continue;
+    }
+
+    StringRef attribute_name = selection_attribute_names[attribute_i];
+    if (attribute_name == ".selection") {
+      positions = curves.positions_for_write();
+    }
+    else if (attribute_name == ".selection_handle_left") {
+      positions = curves.handle_positions_left_for_write();
+    }
+    else if (attribute_name == ".selection_handle_right") {
+      positions = curves.handle_positions_right_for_write();
+    }
+
+    selection_per_attribute[attribute_i].foreach_index(
+        GrainSize(1024), [&](const int64_t i, const int64_t pos) {
+          TransVert &tv = tvs->transverts[pos + offset];
+          tv.loc = positions[i];
+          tv.flag = SELECT;
+          copy_v3_v3(tv.oldloc, tv.loc);
+        });
+
+    offset += selection_per_attribute[attribute_i].size();
+  }
 }
 
 float (*point_normals_array_create(const Curves *curves_id))[3]
