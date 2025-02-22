6460e6a4532bc8f026942c469fb2ff2a583fc047
Alaska <Alaskayou01@gmail.com>
2025-02-22 16:23:57 +1300

    Adjust UI list for driver version

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -1775,8 +1775,10 @@ class CyclesPreferences(bpy.types.AddonPreferences):
                         text=rpt_("Requires AMD GPU with RDNA architecture"),
                         icon='BLANK1',
                         translate=False)
-                    col.label(text=rpt_("and AMD Adrenalin %s newer or AMD Radeon Pro %s driver or newer") %
-                              (adrenalin_driver_version, pro_driver_version), icon='BLANK1', translate=False)
+                    col.label(text=rpt_("and AMD Adrenalin driver %s or newer") %
+                              adrenalin_driver_version, icon='BLANK1', translate=False)
+                    col.label(text=rpt_("or AMD Radeon Pro %s driver or newer") %
+                              pro_driver_version, icon='BLANK1', translate=False)
                 elif sys.platform.startswith("linux"):
                     driver_version = "23.40"
                     col.label(
