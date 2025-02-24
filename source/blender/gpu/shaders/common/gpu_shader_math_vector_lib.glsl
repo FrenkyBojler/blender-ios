blender/blender

commit 3ca5f6f62e35b18df90c62a383de5a66d41611bd

Cleanup: GPU: Reduce compilation warnings
When compiling shaders using GCC there are warnings about functions
being declared twice. This PR will remove those warnings as they are
false positives. The warnings exists to identify typing errors.

Pull Request: https://projects.blender.org/blender/blender/pulls/134832

source/blender/gpu/shaders/common/gpu_shader_math_vector_lib.glsl
@@ -16,9 +16,12 @@
 /**
  * Return true if all components is equal to zero.
  */
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wredundant-decls"
 bool is_zero(vec2 vec);
 bool is_zero(vec3 vec);
 bool is_zero(vec4 vec);
+#    pragma GCC diagnostic pop * /
 
 /**
  * Return true if any component is equal to zero.

FrenkyBojler/blender-ios

commit 7697f3e5f76d2f7fd7f35566a53e71b02b9eb620

Fix #134941: GPU: Performance regression
A performance regression was detected after 3ca5f6f62e35b18df90c62a383de5a66d41611bd.
The cause is unclear as it shader compilation is done as part of the
OpenGL driver. This PR works around the issue by moving the suppression
of compilation warnings to `CMakeLists.txt`.

source/blender/draw/engines/eevee_next/shaders/CMakeLists.txt
@@ -216,6 +216,10 @@ if(WITH_GPU_SHADER_CPP_COMPILATION)
   # TODO Remove
   add_definitions(-DUSE_GPU_SHADER_CREATE_INFO)
 
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
+  
   compile_sources_as_cpp(eevee_cpp_shaders_vert "${SRC_GLSL_VERT}" "GPU_VERTEX_SHADER")
   compile_sources_as_cpp(eevee_cpp_shaders_frag "${SRC_GLSL_FRAG}" "GPU_FRAGMENT_SHADER")
   compile_sources_as_cpp(eevee_cpp_shaders_comp "${SRC_GLSL_COMP}" "GPU_COMPUTE_SHADER")

source/blender/draw/engines/gpencil/shaders/CMakeLists.txt
@@ -44,6 +44,10 @@ set(SRC_GLSL_LIB
 # Compile shaders with shader code.
 if(WITH_GPU_SHADER_CPP_COMPILATION)
   with_shader_cpp_compilation_config()
+  
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
 
   compile_sources_as_cpp(gpencil_cpp_shaders_vert "${SRC_GLSL_VERT}" "GPU_VERTEX_SHADER")
   compile_sources_as_cpp(gpencil_cpp_shaders_frag "${SRC_GLSL_FRAG}" "GPU_FRAGMENT_SHADER")

source/blender/draw/engines/workbench/shaders/CMakeLists.txt
@@ -73,6 +73,10 @@ if(WITH_GPU_SHADER_CPP_COMPILATION)
   with_shader_cpp_compilation_config()
   # TODO Remove
   add_definitions(-DUSE_GPU_SHADER_CREATE_INFO)
+  
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
 
   compile_sources_as_cpp(workbench_cpp_shaders_vert "${SRC_GLSL_VERT}" "GPU_VERTEX_SHADER")
   compile_sources_as_cpp(workbench_cpp_shaders_frag "${SRC_GLSL_FRAG}" "GPU_FRAGMENT_SHADER")

source/blender/draw/intern/shaders/CMakeLists.txt
@@ -45,6 +45,14 @@ if(WITH_GPU_SHADER_CPP_COMPILATION)
   # TODO Remove
   add_definitions(-DUSE_GPU_SHADER_CREATE_INFO)
 
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
+
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
+  
   compile_sources_as_cpp(draw_cpp_shaders_vert "${SRC_GLSL_VERT}" "GPU_VERTEX_SHADER")
   compile_sources_as_cpp(draw_cpp_shaders_frag "${SRC_GLSL_FRAG}" "GPU_FRAGMENT_SHADER")
   compile_sources_as_cpp(draw_cpp_shaders_comp "${SRC_GLSL_COMP}" "GPU_COMPUTE_SHADER")

source/blender/gpu/shaders/CMakeLists.txt
@@ -89,6 +89,10 @@ set(SRC_GLSL_LIB
 if(WITH_GPU_SHADER_CPP_COMPILATION)
   with_shader_cpp_compilation_config()
 
+  if(CMAKE_COMPILER_IS_GNUCC)
+    remove_cc_flag("-Wredundant-decls")
+  endif()
+  
   compile_sources_as_cpp(gpu_cpp_shaders_vert "${SRC_GLSL_VERT}" "GPU_VERTEX_SHADER")
   compile_sources_as_cpp(gpu_cpp_shaders_frag "${SRC_GLSL_FRAG}" "GPU_FRAGMENT_SHADER")
   compile_sources_as_cpp(gpu_cpp_shaders_lib "${SRC_GLSL_LIB}" "GPU_LIBRARY_SHADER")

source/blender/gpu/shaders/common/gpu_shader_math_vector_lib.glsl
@@ -16,12 +16,9 @@
 /**
  * Return true if all components is equal to zero.
  */
-#    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Wredundant-decls"
 bool is_zero(vec2 vec);
 bool is_zero(vec3 vec);
 bool is_zero(vec4 vec);
-#    pragma GCC diagnostic pop * /
 
 /**
  * Return true if any component is equal to zero.