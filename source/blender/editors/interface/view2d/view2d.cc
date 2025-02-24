a078cac58f63efee683fab98855faba693b332d1
John Kiril Swenson <kirilswenson@gmail.com>
2025-02-24 00:44:19 -0600

    Fix: UI: Improve scrollbar hotspot region
    Mouse and action zone interaction for scrollbars depend on `v2d->vert`
    and `v2d->hor`, which are updated through `view2d_masks`. However,
    scrollbar drawing through `UI_view2d_scrollers_draw` calls
    `view2d_scrollers_calc`, which pads these sizes further, meaning that
    drawn scrollbars are slightly out of sync with their hotspots. This is
    noticeable at track edges for shrinking scrollers or when tracks are
    opaque.
    
    Fix by moving the extra (noticeable) padding code from
    `view2d_scrollers_calc` to `view2d_masks`. Ignore the edge padding since
    it's unnoticeable, but rename variables to make it clearer.

--- source/blender/editors/interface/view2d/view2d.cc
+++ source/blender/editors/interface/view2d/view2d.cc
@@ -200,6 +200,12 @@ static void view2d_masks(View2D *v2d, const rcti *mask_scroll)
         v2d->vert.ymax = v2d->hor.ymin;
       }
     }
+
+    /* Slider rects need to be smaller than the region and not interfere with splitter areas. */
+    v2d->hor.xmin += UI_HEADER_OFFSET;
+    v2d->hor.xmax -= UI_HEADER_OFFSET;
+    v2d->vert.ymin += UI_HEADER_OFFSET;
+    v2d->vert.ymax -= UI_HEADER_OFFSET;
   }
 }
 
@@ -1376,7 +1382,6 @@ void view2d_scrollers_calc(View2D *v2d, const rcti *mask_custom, View2DScrollers
   rcti vert, hor;
   float fac1, fac2, totsize, scrollsize;
   const int scroll = view2d_scroll_mapped(v2d->scroll);
-  int smaller;
 
   /* Always update before drawing (for dynamically sized scrollers). */
   view2d_masks(v2d, mask_custom);
@@ -1384,26 +1389,20 @@ void view2d_scrollers_calc(View2D *v2d, const rcti *mask_custom, View2DScrollers
   vert = v2d->vert;
   hor = v2d->hor;
 
-  /* slider rects need to be smaller than region and not interfere with splitter areas */
-  hor.xmin += UI_HEADER_OFFSET;
-  hor.xmax -= UI_HEADER_OFFSET;
-  vert.ymin += UI_HEADER_OFFSET;
-  vert.ymax -= UI_HEADER_OFFSET;
-
-  /* width of sliders */
-  smaller = int(0.1f * U.widget_unit);
+  /* Pad scrollbar drawing away from region edges. */
+  const int edge_pad = int(0.1f * U.widget_unit);
   if (scroll & V2D_SCROLL_BOTTOM) {
-    hor.ymin += smaller;
+    hor.ymin += edge_pad;
   }
   else {
-    hor.ymax -= smaller;
+    hor.ymax -= edge_pad;
   }
 
   if (scroll & V2D_SCROLL_LEFT) {
-    vert.xmin += smaller;
+    vert.xmin += edge_pad;
   }
   else {
-    vert.xmax -= smaller;
+    vert.xmax -= edge_pad;
   }
 
   CLAMP_MAX(vert.ymin, vert.ymax - V2D_SCROLL_HANDLE_SIZE_HOTSPOT);
