2584cccd56610d896e0ad5c9616b83ac631b2bb8
Harley Acheson <harley.acheson@gmail.com>
2025-02-24 15:51:07 -0800

    Fix: Defaults for Lights
    When using "Reset to Default Value" there are a number of Light object
    data properties that change from the defaults in factory settings. This
    PR corrects energy, spotsize, radius, area sizes, area shape, and sun
    angle.

--- source/blender/makesdna/DNA_light_defaults.h
+++ source/blender/makesdna/DNA_light_defaults.h
@@ -19,15 +19,17 @@
     .r = 1.0f, \
     .g = 1.0f, \
     .b = 1.0f, \
-    .energy = 10.0f, \
+    .energy = 1000.0f, \
     .energy_deprecated = 10.0f, \
-    .spotsize = DEG2RADF(45.0f), \
+    .spotsize = DEG2RADF(75.0f), \
     .spotblend = 0.15f, \
+    .radius = 0.1f, \
     .mode = LA_SHADOW | LA_USE_SOFT_FALLOFF, \
     .clipsta = 0.05f, \
-    .area_size = 0.25f, \
-    .area_sizey = 0.25f, \
-    .area_sizez = 0.25f, \
+    .area_size = 0.1f, \
+    .area_sizey = 0.1f, \
+    .area_sizez = 0.1f, \
+    .area_shape = LA_AREA_RECT, \
     .preview = NULL, \
     .cascade_max_dist = 200.0f, \
     .cascade_count = 4, \
@@ -41,7 +43,7 @@
     .shadow_maximum_resolution = 0.001f, \
     .shadow_jitter_overblur = 10.0f, \
     .att_dist = 40.0f, \
-    .sun_angle = DEG2RADF(0.526f), \
+    .sun_angle = DEG2RADF(11.4f), \
     .area_spread = DEG2RADF(180.0f), \
   }
 
