4c391627e7eda45c718635dcff06e777a2362659
Omar Emara <mail@OmarEmara.dev>
2025-02-25 14:34:18 +0200

    Compositor: Add Image Info node
    This patch adds a new Image Info node which returns information about
    compositor images. The node has three sources:
    
    - Image: Where the source of the information is the input image.
    - Render: Where the source of the information is the render.
    - Size: Where the source of the information is a width and a height.
    
    The node returns the following information:
    
    - Pixel Coordinates: The coordinates of the centers of the pixels in the
      image. Those are essentially the integer coordinates with half pixels
      offsets added.
    - Texture Coordinates: Zero centered pixel coordinates normalized along
      the greater dimension. Somewhat analogous to Object coordinates in
      shader nodes.
    - Size: The size of the image.
    - Location: The location of the image in the virtual compositing space.
    - Rotation: The rotation of the image in the virtual compositing space.
    - Scale: The scale of the image in the virtual compositing space.
    
    This node is very useful to allow greater flexibility and procedural
    creations. For instance, coordinates can be used to create procedural
    effects like vignette using very simple math nodes. And size can be used
    to compute size-relative parameters for pixel-parameter nodes.

---
+++ source/blender/nodes/composite/nodes/node_composite_image_info.cc
@@ -0,0 +1,171 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BLI_math_angle_types.hh"
+#include "BLI_math_matrix.hh"
+#include "BLI_math_vector_types.hh"
+
+#include "UI_interface.hh"
+#include "UI_resources.hh"
+
+#include "COM_node_operation.hh"
+
+#include "node_composite_util.hh"
+
+namespace blender::nodes::node_composite_image_info_cc {
+
+NODE_STORAGE_FUNCS(NodeImageInfoData)
+
+static void node_declare(NodeDeclarationBuilder &b)
+{
+  b.add_input<decl::Color>("Image").compositor_domain_priority(0).compositor_realization_mode(
+      CompositorInputRealizationMode::None);
+  b.add_input<decl::Int>("Width")
+      .default_value(512)
+      .min(1)
+      .max(4096)
+      .compositor_expects_single_value();
+  b.add_input<decl::Int>("Height")
+      .default_value(512)
+      .min(1)
+      .max(4096)
+      .compositor_expects_single_value();
+
+  b.add_output<decl::Vector>("Texture Coordinates");
+  b.add_output<decl::Vector>("Pixel Coordinates");
+  b.add_output<decl::Vector>("Size");
+  b.add_output<decl::Vector>("Location");
+  b.add_output<decl::Float>("Rotation");
+  b.add_output<decl::Vector>("Scale");
+}
+
+static void node_init(bNodeTree * /*ntree*/, bNode *node)
+{
+  NodeImageInfoData *data = MEM_cnew<NodeImageInfoData>(__func__);
+  data->source = CMP_NODE_IMAGE_INFO_SOURCE_IMAGE;
+  node->storage = data;
+}
+
+static void node_draw_buttons(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+{
+  uiItemR(layout, ptr, "source", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
+}
+
+static void node_update(bNodeTree *node_tree, bNode *node)
+{
+  const NodeImageInfoData &storage = node_storage(*node);
+
+  bNodeSocket *input_image = bke::node_find_socket(*node, SOCK_IN, "Image");
+  bke::node_set_socket_availability(
+      *node_tree, *input_image, storage.source == CMP_NODE_IMAGE_INFO_SOURCE_IMAGE);
+
+  bNodeSocket *input_width = bke::node_find_socket(*node, SOCK_IN, "Width");
+  bNodeSocket *input_height = bke::node_find_socket(*node, SOCK_IN, "Height");
+  bke::node_set_socket_availability(
+      *node_tree, *input_width, storage.source == CMP_NODE_IMAGE_INFO_SOURCE_SIZE);
+  bke::node_set_socket_availability(
+      *node_tree, *input_height, storage.source == CMP_NODE_IMAGE_INFO_SOURCE_SIZE);
+}
+
+using namespace blender::compositor;
+
+class ImageInfoOperation : public NodeOperation {
+ public:
+  ImageInfoOperation(Context &context, DNode node) : NodeOperation(context, node)
+  {
+    InputDescriptor &image_descriptor = this->get_input_descriptor("Image");
+    image_descriptor.skip_type_conversion = true;
+  }
+
+  void execute() override
+  {
+    const Domain domain = compute_domain();
+
+    Result &texture_coordinates_result = this->get_result("Texture Coordinates");
+    if (texture_coordinates_result.should_compute()) {
+      const Result &texture_coordinates = this->context().cache_manager().texture_coordinates.get(
+          this->context(), domain.size);
+      texture_coordinates_result.wrap_external(texture_coordinates);
+    }
+
+    Result &pixel_coordinates_result = this->get_result("Pixel Coordinates");
+    if (pixel_coordinates_result.should_compute()) {
+      const Result &pixel_coordinates = this->context().cache_manager().pixel_coordinates.get(
+          this->context(), domain.size);
+      pixel_coordinates_result.wrap_external(pixel_coordinates);
+    }
+
+    Result &size_result = this->get_result("Size");
+    if (size_result.should_compute()) {
+      size_result.allocate_single_value();
+      size_result.set_single_value(float3(domain.size, 0.0f));
+    }
+
+    math::AngleRadian rotation;
+    float2 location, scale;
+    math::to_loc_rot_scale(domain.transformation, location, rotation, scale);
+
+    Result &location_result = this->get_result("Location");
+    if (location_result.should_compute()) {
+      location_result.allocate_single_value();
+      location_result.set_single_value(float3(location, 0.0f));
+    }
+
+    Result &rotation_result = this->get_result("Rotation");
+    if (rotation_result.should_compute()) {
+      rotation_result.allocate_single_value();
+      rotation_result.set_single_value(float(rotation));
+    }
+
+    Result &scale_result = this->get_result("Scale");
+    if (scale_result.should_compute()) {
+      scale_result.allocate_single_value();
+      scale_result.set_single_value(float3(scale, 0.0f));
+    }
+  }
+
+  Domain compute_domain() override
+  {
+    switch (CMPNodeImageInfoSource(node_storage(this->bnode()).source)) {
+      case CMP_NODE_IMAGE_INFO_SOURCE_IMAGE:
+        return this->get_input("Image").domain();
+      case CMP_NODE_IMAGE_INFO_SOURCE_RENDER:
+        return Domain(this->context().get_compositing_region_size());
+      case CMP_NODE_IMAGE_INFO_SOURCE_SIZE:
+        return Domain(int2(this->get_input("Width").get_single_value_default(512),
+                           this->get_input("Height").get_single_value_default(512)));
+    }
+
+    BLI_assert_unreachable();
+    return Domain(int2(512));
+  }
+};
+
+static NodeOperation *get_compositor_operation(Context &context, DNode node)
+{
+  return new ImageInfoOperation(context, node);
+}
+
+}  // namespace blender::nodes::node_composite_image_info_cc
+
+void register_node_type_cmp_image_info()
+{
+  namespace file_ns = blender::nodes::node_composite_image_info_cc;
+
+  static blender::bke::bNodeType ntype;
+
+  cmp_node_type_base(&ntype, "CompositorNodeImageInfo", CMP_NODE_IMAGE_INFO);
+  ntype.ui_name = "Image Info";
+  ntype.ui_description = "Returns information about an image";
+  ntype.nclass = NODE_CLASS_INPUT;
+  ntype.declare = file_ns::node_declare;
+  ntype.draw_buttons = file_ns::node_draw_buttons;
+  ntype.initfunc = file_ns::node_init;
+  ntype.updatefunc = file_ns::node_update;
+  blender::bke::node_type_storage(
+      ntype, "NodeImageInfoData", node_free_standard_storage, node_copy_standard_storage);
+  ntype.get_compositor_operation = file_ns::get_compositor_operation;
+
+  blender::bke::node_register_type(ntype);
+}
