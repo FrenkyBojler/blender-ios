cbd267b6aaf913823a67957286ca4c6a059fad4d
Julian Eisel <julian@blender.org>
2025-02-27 19:35:41 +0100

    Assets: Batch create/remove previews for all selected assets
    Part of #134755/#134766.
    
    Previously operators to create or remove previews would only work on the
    active asset/data-block. From feedback this can be quite an
    inconvenience, e.g. when curating asset libraries for sharing or
    production environments.
    
    This change makes it so the following operators (all available in the
    asset browser side-bar) create or remove the preview on the whole
    selection:
    - Generate Preview (refresh icon)
    - Generate Preview from Object
    - Remove Preview
    
    Loading preview images from disk doesn't support this yet.

--- source/blender/editors/include/ED_util.hh
+++ source/blender/editors/include/ED_util.hh
@@ -49,6 +49,19 @@ void ED_spacedata_id_remap(ScrArea *area,
                            SpaceLink *sl,
                            const blender::bke::id::IDRemapper &mappings);
 
+/**
+ * Helper for context sensitive operations: Returns the "id" context member wrapped in a
+ * #PointerRNA vector. Useful when the API uses vectors to also support acting on multiple IDs,
+ * e.g. as returned by #ED_operator_get_ids_from_context_as_vec().
+ */
+blender::Vector<PointerRNA> ED_operator_single_id_from_context_as_vec(const bContext *C);
+/**
+ * Helper for context sensitive operations: Returns the "selected_ids" context member or if none,
+ * the "id" context member as a #PointerRNA vector. Multiple selected IDs are prioritized because
+ * batch operations are generally more desired behavior.
+ */
+blender::Vector<PointerRNA> ED_operator_get_ids_from_context_as_vec(const bContext *C);
+
 void ED_operatortypes_edutils();
 
 /* Drawing */
