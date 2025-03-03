be80c06644bf2ad0aa647be34150ef57571fc526
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-03-02 21:37:15 -0800

    Fix: Segment problem.

--- source/blender/geometry/intern/boolean_curves.cc
+++ source/blender/geometry/intern/boolean_curves.cc
@@ -884,7 +884,7 @@ bke::CurvesGeometry curve_boolean(const Operation boolean_mode,
 
     /* Find the first segment that is not clipping. */
     for (const int segment_i : segment_range) {
-      if (!clipping_shapes.contains(segment_i)) {
+      if (!clipping_shapes.contains(result.segments[segment_i].curve)) {
         old_by_new_map[i] = result.segments[segment_i].curve;
         continue;
       }
