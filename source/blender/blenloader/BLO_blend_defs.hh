f112bc0fc3bdcc895de882ff84bf31bfe6f8989c
Jacques Lucke <jacques@blender.org>
2025-03-04 16:36:12 +0100

    improve description

--- source/blender/blenloader/BLO_blend_defs.hh
+++ source/blender/blenloader/BLO_blend_defs.hh
@@ -69,21 +69,21 @@ enum {
  */
 
 /**
- * The header is 12 bytes long:
- * 0-6:  "BLENDER"
+ * Low level version 0: the header is 12 bytes long.
+ * 0-6:  'BLENDER'
  * 7:    '-' for 8 byte pointers (#SmallBHead8) or '_' for 4 byte pointers (#BHead4)
  * 8:    'v' for little endian or 'V' for big endian
- * 9-11: 3 digits encoding #BLENDER_FILE_VERSION (e.g. 305 for Blender 3.5)
+ * 9-11: 3 ASCII digits encoding #BLENDER_FILE_VERSION (e.g. '305' for Blender 3.5)
  */
 #define BLEND_FILE_VERSION_FORMAT_0 0
 /**
- * The header is 17 bytes long:
- * 0-6:   "BLENDER"
- * 7-8:   size of the header in bytes (always 17 currently)
+ * Lower level version 1: the header is 17 bytes long.
+ * 0-6:   'BLENDER'
+ * 7-8:   size of the header in bytes encoded as ASCII digits (always '17' currently)
  * 9:     always '-'
- * 10-11: File version format (always '01' currently)
+ * 10-11: File version format as ASCII digits (always '01' currently)
  * 12:    always 'v'
- * 13-16: 4 digits encoding #BLENDER_FILE_VERSION (e.g. 405 for Blender 4.5)
+ * 13-16: 4 ASCII digits encoding #BLENDER_FILE_VERSION (e.g. '0405' for Blender 4.5)
  *
  * With this header, #LargeBHead8 is always used.
  */
