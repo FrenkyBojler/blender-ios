3f6ec7dc9e5b02ed20013028f20f9ee1d8b2d7ed
Sybren A. Stüvel <sybren@blender.org>
2025-03-04 17:58:39 +0100

    Fix: reorder versioning of legacy Grease Pencil and Actions
    The versioning of legacy-to-modern Grease Pencil is using slotted
    Actions (because that's the current animation API), and therefore
    assumes that the Action & Slot assignments have been versioned as well.
    The latter now actually happens before the former, so that assumption is
    now actually valid.

--- source/blender/animrig/intern/action.cc
+++ source/blender/animrig/intern/action.cc
@@ -2803,6 +2803,8 @@ void action_fcurve_move(Action &action_dst,
                         Action &action_src,
                         FCurve &fcurve)
 {
+  BLI_assert(action_slot_dst != Slot::unassigned);
+
   /* Store the group name locally, as the group will be removed if this was its
    * last F-Curve. */
   std::optional<std::string> group_name;
