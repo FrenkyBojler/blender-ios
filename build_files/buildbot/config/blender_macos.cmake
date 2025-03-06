f7317259accf1b55acbab66a2535d244e0c0ff5c
Johan Walles <johan.walles@gmail.com>
2025-03-06 16:57:05 +0100

    Descriptions over links

--- build_files/buildbot/config/blender_macos.cmake
+++ build_files/buildbot/config/blender_macos.cmake
@@ -7,7 +7,10 @@ include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/config/blender_release.cmake")
 set(WITH_CYCLES_TEST_OSL     ON CACHE BOOL "" FORCE)
 
 if(${XCODE_VERSION} VERSION_GREATER_EQUAL 15.0 AND "${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
-  # Ref: https://projects.blender.org/blender/blender/pulls/134639#issuecomment-1513274
+  # The legacy linker emits fewer warnings than the modern one.
+  #
+  # It is also a lot slower, but in CI the warnings amount is more important
+  # than the minutes it takes to link.
   set(WITH_LINKER_LEGACY ON CACHE BOOL "" FORCE)
   mark_as_advanced(WITH_LINKER_LEGACY)
 endif()
