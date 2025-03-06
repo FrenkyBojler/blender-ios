6d3e3bb77cf2fb03f0c3aea50288948bdadc9097
Johan Walles <johan.walles@gmail.com>
2025-03-06 17:17:32 +0100

    Try making buildbot happy
    Neither XCODE_VERSION nor CMAKE_OSX_ARCHITECTURES were set.
    
    I believe XCODE_VERSION still isn't, so let's not look for that at all.

--- build_files/buildbot/config/blender_macos.cmake
+++ build_files/buildbot/config/blender_macos.cmake
@@ -4,13 +4,16 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/config/blender_release.cmake")
 
+# Define CMAKE_OSX_ARCHITECTURES if not set.
+include("${CMAKE_CURRENT_LIST_DIR}/../../cmake/platform/platform_apple_xcode.cmake")
+
 set(WITH_CYCLES_TEST_OSL     ON CACHE BOOL "" FORCE)
 
-if(${XCODE_VERSION} VERSION_GREATER_EQUAL 15.0 AND "${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
+if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
   # The legacy linker emits fewer warnings than the modern one.
   #
   # It is also a lot slower, but in CI the warnings amount is more important
-  # than the minutes it takes to link.
+  # than the extra minutes it takes to link.
   set(WITH_LINKER_LEGACY ON CACHE BOOL "" FORCE)
   mark_as_advanced(WITH_LINKER_LEGACY)
 endif()
