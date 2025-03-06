93969625bbb6f5a1350021a2b044c99f4e3aa501
Sergey Sharybin <sergey@blender.org>
2025-03-06 11:50:21 +0100

    Fix: Cycles HIP-RT crash
    The crash has been introduced by the refactor of lights to be
    objects in #134846.
    
    We can make such cases easier to catch at compile time in the
    future, but for now applying the minimal patch which solves the
    problem without going deeper into refactor.

--- intern/cycles/device/hiprt/device_impl.cpp
+++ intern/cycles/device/hiprt/device_impl.cpp
@@ -761,6 +761,9 @@ void HIPRTDevice::build_blas(BVHHIPRT *bvh, Geometry *geom, hiprtBuildOptions op
       break;
     }
 
+    case Geometry::LIGHT:
+      return;
+
     default:
       assert(geom_input.geomType != hiprtInvalidValue);
   }
