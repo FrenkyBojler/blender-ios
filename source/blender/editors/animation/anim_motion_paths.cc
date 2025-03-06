7789ba18e2535cdf018b0d082514625b993d1aa3
Philipp Oeser <philipp@blender.org>
2025-03-06 10:40:44 +0100

    Fix #135523: Calculate Motion Paths ignores Selected Keys option
    Since the switch to slotted actions, we cannot solely rely on the
    Listbase of `curves` (this is for legacy actions only), but need to
    consider Channelbag for new slotted actions. There is
    `fcurves_for_assigned_action` that handles both of these, so use it.

--- source/blender/editors/animation/anim_motion_paths.cc
+++ source/blender/editors/animation/anim_motion_paths.cc
@@ -363,9 +363,13 @@ void animviz_motionpath_compute_range(Object *ob, Scene *scene)
   }
 
   const bool has_action = ob->adt && ob->adt->action;
-  if (avs->path_range == MOTIONPATH_RANGE_SCENE || !has_action ||
-      BLI_listbase_is_empty(&ob->adt->action->curves))
-  {
+
+  blender::Vector<FCurve *> fcurves = {};
+  if (has_action) {
+    fcurves = blender::animrig::legacy::fcurves_for_assigned_action(ob->adt);
+  }
+
+  if (avs->path_range == MOTIONPATH_RANGE_SCENE || !has_action || fcurves.is_empty()) {
     /* Default to the scene (preview) range if there is no animation data to
      * find selected keys in. */
     avs->path_sf = PSFRA;
@@ -374,8 +378,8 @@ void animviz_motionpath_compute_range(Object *ob, Scene *scene)
   }
 
   AnimKeylist *keylist = ED_keylist_create();
-  for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(ob->adt)) {
-    fcurve_to_keylist(ob->adt, fcu, keylist, 0, {-FLT_MAX, FLT_MAX}, true);
+  for (FCurve *fcurve : fcurves) {
+    fcurve_to_keylist(ob->adt, fcurve, keylist, 0, {-FLT_MAX, FLT_MAX}, true);
   }
 
   blender::Bounds<float> frame_range;
