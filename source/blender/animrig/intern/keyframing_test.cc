f5f4db9d3cc4421e5285cdc0ef33e449cd487f05
Bastien Montagne <bastien@blender.org>
2025-03-06 13:16:03 +0100

    Update against latest changes to MEM_guardedalloc.

--- source/blender/animrig/intern/keyframing_test.cc
+++ source/blender/animrig/intern/keyframing_test.cc
@@ -79,7 +79,7 @@ class KeyframingTest : public testing::Test {
     object = BKE_object_add_only_object(bmain, OB_EMPTY, "Empty");
     object_rna_pointer = RNA_id_pointer_create(&object->id);
 
-    Bone *bone = MEM_cnew<Bone>("BONE");
+    Bone *bone = MEM_mallocN<Bone>("BONE");
     STRNCPY(bone->name, "Bone");
 
     armature = BKE_armature_add(bmain, "Armature");
@@ -159,7 +159,7 @@ class KeyframingTest : public testing::Test {
    */
   void ensure_action_is_legacy(bAction &action)
   {
-    bActionGroup *new_group = MEM_cnew<bActionGroup>(__func__);
+    bActionGroup *new_group = MEM_callocN<bActionGroup>(__func__);
     STRNCPY(new_group->name, "Legacy Forcer");
     BLI_addtail(&action.groups, new_group);
   }
--- source/blender/animrig/intern/pose_test.cc
+++ source/blender/animrig/intern/pose_test.cc
@@ -67,11 +67,11 @@ class PoseTest : public testing::Test {
     bArmature *armature = BKE_armature_add(bmain, "ArmatureA");
     obj_armature_a->data = armature;
 
-    Bone *bone = MEM_cnew<Bone>("BONE");
+    Bone *bone = MEM_callocN<Bone>("BONE");
     STRNCPY(bone->name, "BoneA");
     BLI_addtail(&armature->bonebase, bone);
 
-    bone = MEM_cnew<Bone>("BONE");
+    bone = MEM_callocN<Bone>("BONE");
     STRNCPY(bone->name, "BoneB");
     BLI_addtail(&armature->bonebase, bone);
 
@@ -80,11 +80,11 @@ class PoseTest : public testing::Test {
     armature = BKE_armature_add(bmain, "ArmatureB");
     obj_armature_b->data = armature;
 
-    bone = MEM_cnew<Bone>("BONE");
+    bone = MEM_callocN<Bone>("BONE");
     STRNCPY(bone->name, "BoneA");
     BLI_addtail(&armature->bonebase, bone);
 
-    bone = MEM_cnew<Bone>("BONE");
+    bone = MEM_callocN<Bone>("BONE");
     STRNCPY(bone->name, "BoneB");
     BLI_addtail(&armature->bonebase, bone);
 
