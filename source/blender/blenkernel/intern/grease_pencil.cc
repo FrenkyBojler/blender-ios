3d5e1933d50f736780552b3f865858c4f94b67b8
YimingWu <xp8110@outlook.com>
2025-03-07 09:22:36 +0800

    Use early return

--- source/blender/blenkernel/intern/grease_pencil.cc
+++ source/blender/blenkernel/intern/grease_pencil.cc
@@ -111,53 +111,55 @@ static void grease_pencil_set_runtime_visibilities(ID &id_dst, GreasePencil &gre
 {
   using namespace blender::bke;
 
-  if (DEG_is_evaluated_id(&id_dst) && grease_pencil.adt) {
-    PropertyRNA *layer_hide_prop = RNA_struct_type_find_property(&RNA_GreasePencilLayer, "hide");
-    BLI_assert_msg(layer_hide_prop,
-                   "RNA struct GreasePencilLayer is expected to have a 'hide' property.");
-    PropertyRNA *group_hide_prop = RNA_struct_type_find_property(&RNA_GreasePencilLayerGroup,
-                                                                 "hide");
-    BLI_assert_msg(group_hide_prop,
-                   "RNA struct GreasePencilLayerGroup is expected to have a 'hide' property.");
+  if (!DEG_is_evaluated_id(&id_dst) || !grease_pencil.adt) {
+    return;
+  }
 
-    for (greasepencil::LayerGroup *layer_group : grease_pencil.layer_groups_for_write()) {
-      PointerRNA layer_ptr = RNA_pointer_create_discrete(
-          &id_dst, &RNA_GreasePencilLayerGroup, layer_group);
-      std::optional<std::string> rna_path = RNA_path_from_ID_to_property(&layer_ptr,
-                                                                         group_hide_prop);
-      BLI_assert_msg(
-          rna_path,
-          "It should be possible to construct the RNA path of a grease pencil layer group.");
+  PropertyRNA *layer_hide_prop = RNA_struct_type_find_property(&RNA_GreasePencilLayer, "hide");
+  BLI_assert_msg(layer_hide_prop,
+                 "RNA struct GreasePencilLayer is expected to have a 'hide' property.");
+  PropertyRNA *group_hide_prop = RNA_struct_type_find_property(&RNA_GreasePencilLayerGroup,
+                                                               "hide");
+  BLI_assert_msg(group_hide_prop,
+                 "RNA struct GreasePencilLayerGroup is expected to have a 'hide' property.");
 
-      layer_group->runtime->is_visibility_animated_ = animdata::prop_is_animated(
-          grease_pencil.adt, rna_path->c_str(), 0);
+  for (greasepencil::LayerGroup *layer_group : grease_pencil.layer_groups_for_write()) {
+    PointerRNA layer_ptr = RNA_pointer_create_discrete(
+        &id_dst, &RNA_GreasePencilLayerGroup, layer_group);
+    std::optional<std::string> rna_path = RNA_path_from_ID_to_property(&layer_ptr,
+                                                                       group_hide_prop);
+    BLI_assert_msg(
+        rna_path,
+        "It should be possible to construct the RNA path of a grease pencil layer group.");
+
+    layer_group->runtime->is_visibility_animated_ = animdata::prop_is_animated(
+        grease_pencil.adt, rna_path->c_str(), 0);
+  }
+
+  std::function<bool(greasepencil::LayerGroup &)> parent_group_visibility_animated =
+      [&](greasepencil::LayerGroup &parent) {
+        if (parent.runtime->is_visibility_animated_) {
+          return true;
+        }
+        greasepencil::LayerGroup *parent_group = parent.as_node().parent_group();
+        if (parent_group) {
+          return parent_group_visibility_animated(*parent_group);
+        }
+        return false;
+      };
+
+  for (greasepencil::Layer *layer : grease_pencil.layers_for_write()) {
+    if (parent_group_visibility_animated(layer->parent_group())) {
+      layer->runtime->is_visibility_animated_ = true;
+      continue;
     }
-
-    std::function<bool(greasepencil::LayerGroup &)> parent_group_visibility_animated =
-        [&](greasepencil::LayerGroup &parent) {
-          if (parent.runtime->is_visibility_animated_) {
-            return true;
-          }
-          greasepencil::LayerGroup *parent_group = parent.as_node().parent_group();
-          if (parent_group) {
-            return parent_group_visibility_animated(*parent_group);
-          }
-          return false;
-        };
-
-    for (greasepencil::Layer *layer : grease_pencil.layers_for_write()) {
-      if (parent_group_visibility_animated(layer->parent_group())) {
-        layer->runtime->is_visibility_animated_ = true;
-        continue;
-      }
-      PointerRNA layer_ptr = RNA_pointer_create_discrete(&id_dst, &RNA_GreasePencilLayer, layer);
-      std::optional<std::string> rna_path = RNA_path_from_ID_to_property(&layer_ptr,
-                                                                         layer_hide_prop);
-      BLI_assert_msg(rna_path,
-                     "It should be possible to construct the RNA path of a grease pencil layer.");
-      layer->runtime->is_visibility_animated_ = animdata::prop_is_animated(
-          grease_pencil.adt, rna_path.value(), 0);
-    }
+    PointerRNA layer_ptr = RNA_pointer_create_discrete(&id_dst, &RNA_GreasePencilLayer, layer);
+    std::optional<std::string> rna_path = RNA_path_from_ID_to_property(&layer_ptr,
+                                                                       layer_hide_prop);
+    BLI_assert_msg(rna_path,
+                   "It should be possible to construct the RNA path of a grease pencil layer.");
+    layer->runtime->is_visibility_animated_ = animdata::prop_is_animated(
+        grease_pencil.adt, rna_path.value(), 0);
   }
 }
 
