44cbbb2e723a15198d87094e384d1cb1ddbaebc9
Howard Trickey <howard.trickey@gmail.com>
2025-03-06 22:16:45 -0500

    Turn off timing debug.

--- source/blender/geometry/intern/mesh_boolean_manifold.cc
+++ source/blender/geometry/intern/mesh_boolean_manifold.cc
@@ -19,7 +19,7 @@
 #include "BLI_span.hh"
 #include "BLI_task.hh"
 
-#define DEBUG_TIME
+// #define DEBUG_TIME
 #ifdef DEBUG_TIME
 #  include "BLI_timeit.hh"
 #endif
@@ -1157,7 +1157,9 @@ static MeshAssembly assemble_mesh_from_meshgl(const MeshGL &mgl, const MeshOffse
         merge_out_faces(group_faces);
       }
     });
+#ifdef DEBUG_TIME
     timeit::ScopedTimer xtimer("copying groups at end");
+#endif
     for (const int i : new_groups.index_range()) {
       ma.new_faces.extend(new_groups[i].as_span());
     }
