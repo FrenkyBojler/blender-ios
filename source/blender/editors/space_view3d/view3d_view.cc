8c47930f79ff0fcf6e5e4254b6ea1e81e1109958
David Murmann <david@23x5.net>
2025-03-08 18:02:16 +0100

    Rounding down to zero is only necessary on the diagonal.

--- source/blender/editors/space_view3d/view3d_view.cc
+++ source/blender/editors/space_view3d/view3d_view.cc
@@ -385,13 +385,11 @@ static void obmat_to_viewmat(RegionView3D *rv3d, Object *ob)
   mat4_normalized_to_quat(rv3d->viewquat, rv3d->viewmat);
 }
 
-static void round_mat4_epsilon(float m[4][4], float epsilon)
+static void round_diag_mat4_epsilon(float m[4][4], float epsilon)
 {
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      if (fabsf(m[i][j]) < epsilon) {
-        m[i][j] = 0.0f;
-      }
+  for (int i = 0; i < 3; ++i) {
+    if (fabsf(m[i][i]) < epsilon) {
+      m[i][i] = 0.0f;
     }
   }
 }
@@ -422,7 +420,7 @@ void view3d_viewmatrix_set(const Depsgraph *depsgraph,
 
     quat_to_mat4(rv3d->viewmat, rv3d->viewquat);
     /* Snap values close to zero to exactly zero, so that views along an axis are precise. */
-    round_mat4_epsilon(rv3d->viewmat, 5e-8f);
+    round_diag_mat4_epsilon(rv3d->viewmat, 5e-8f);
     if (rv3d->persp == RV3D_PERSP) {
       rv3d->viewmat[3][2] -= rv3d->dist;
     }
