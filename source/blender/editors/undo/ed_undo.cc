60ac46d3226f3168c7136ce9b2fea4be0d5c7e3a
Sean Kim <SeanCTKim@protonmail.com>
2025-03-10 12:12:39 -0700

    Weight Paint: Allow editor actions to create undo steps
    Currently, no change invoked from the UI while inside Weight Paint mode
    creates an undo step. This includes both tool properties (e.g. brush
    size, tool settings) as well as mesh properties (symmetry, active vertex
    group, selection mode).
    
    This behavior was initially introduced to mitigate lag when changing
    brush settings while sculpting. It is not needed for Weight Paint, as
    the mode does not use the paint BVH, and is generally undesired from a
    user perspective, as it means too many changes are undone when undoing a
    paint stroke.
    
    To fix this behavior, this commit builds off of previous work (see
    !134548 and !134836), and simply excludes Weight Paint from the relevant
    modes which still need to prevent memfile / global undo from running.
    
    Ref: #134220, #69760, and many others

--- source/blender/editors/undo/ed_undo.cc
+++ source/blender/editors/undo/ed_undo.cc
@@ -415,9 +415,11 @@ bool ED_undo_is_legacy_compatible_for_property(bContext *C, ID *id, PointerRNA &
     BKE_view_layer_synced_ensure(scene, view_layer);
     Object *obact = BKE_view_layer_active_object_get(view_layer);
     if (obact != nullptr) {
-      if (obact->mode & OB_MODE_ALL_PAINT) {
+      if (obact->mode & (OB_MODE_ALL_PAINT & ~OB_MODE_WEIGHT_PAINT)) {
         /* Don't store property changes when painting
          * (only do undo pushes on brush strokes which each paint operator handles on its own). */
+        /* Weight Paint uses global undo and is not subject to the same constraints as Vertex Paint
+         * and Sculpt. */
         CLOG_INFO(&LOG, 1, "skipping undo for paint-mode");
         return false;
       }
