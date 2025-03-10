ede472c04defa7125d2783aed55427fb6e569deb
Harley Acheson <harley.acheson@gmail.com>
2025-03-10 16:42:08 -0700

    Comment changes

--- source/blender/editors/include/UI_interface_c.hh
+++ source/blender/editors/include/UI_interface_c.hh
@@ -2222,8 +2222,8 @@ ENUM_OPERATORS(eUI_Item_Flag, UI_ITEM_R_TEXT_BUT_FORCE_SEMI_MODAL_ACTIVE)
 
 #define UI_HEADER_OFFSET ((void)0, 0.4f * UI_UNIT_X)
 
-#define UI_AZONESPOTW UI_HEADER_OFFSET       /* width of corner #AZone - max */
-#define UI_AZONESPOTH (0.6f * U.widget_unit) /* height of corner #AZone */
+#define UI_AZONESPOTW UI_HEADER_OFFSET       /* Width of corner action zone #AZone. */
+#define UI_AZONESPOTH (0.6f * U.widget_unit) /* Height of corner action zone #AZone. */
 
 /* uiLayoutOperatorButs flags */
 enum {
--- source/blender/editors/interface/view2d/view2d.cc
+++ source/blender/editors/interface/view2d/view2d.cc
@@ -198,7 +198,7 @@ static void view2d_masks(View2D *v2d, const rcti *mask_scroll)
     /* Adjust vertical scroller to avoid horizontal scrollers and splitter areas. */
     if (scroll & V2D_SCROLL_VERTICAL) {
       /* Note that top splitter areas are in the header,
-      outside of `mask_scroll`, so we can ignore them. */
+       * outside of `mask_scroll`, so we can ignore them. */
       v2d->vert.ymin += UI_AZONESPOTH;
       if (scroll & V2D_SCROLL_BOTTOM) {
         /* on bottom edge of region */
