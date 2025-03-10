3aa48c8f9aa36cf8ea2ce29e97c33b0814a42963
Lukas Tönne <lukas@blender.org>
2025-03-10 14:42:36 +0100

    Ignore real part of the bending strain measure to avoid sudden flipping.

--- source/blender/nodes/NOD_xpbd_constraints.hh
+++ source/blender/nodes/NOD_xpbd_constraints.hh
@@ -201,7 +201,8 @@ inline float4 quaternion_difference(const math::Quaternion &a, const math::Quate
   //   return q.w >= 0.0f ? float4(q) : -float4(q);
   // };
   // return positive_quaternion(a) - positive_quaternion(b);
-  return float4(a) - float4(b);
+  // return float4(a) - float4(b);
+  return float4(0.0f, a.imaginary_part() - b.imaginary_part());
 }
 
 /** Add a positive or negative offset depending on the quaternion sign. */
