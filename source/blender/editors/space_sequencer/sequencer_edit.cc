0d0c7ed4a7d8de358f822d628568e6817114ed73
Richard Antalik <richardantalik@gmail.com>
2025-03-11 23:19:31 +0100

    Fix stuff

--- source/blender/editors/space_sequencer/sequencer_edit.cc
+++ source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -845,10 +845,8 @@ static int sequencer_mute_exec(bContext *C, wmOperator *op)
   Scene *scene = CTX_data_scene(C);
   Editing *ed = seq::editing_get(scene);
 
-  bool selected = !RNA_boolean_get(op->ptr, "unselected");
-
   LISTBASE_FOREACH (Strip *, strip, ed->seqbasep) {
-    if (selected) {
+    if (!RNA_boolean_get(op->ptr, "unselected")) {
       if (strip->flag & SELECT) {
         strip->flag |= SEQ_MUTE;
         seq::relations_invalidate_dependent(scene, strip);
@@ -896,10 +894,9 @@ static int sequencer_unmute_exec(bContext *C, wmOperator *op)
 {
   Scene *scene = CTX_data_scene(C);
   Editing *ed = seq::editing_get(scene);
-  bool selected = !RNA_boolean_get(op->ptr, "unselected");
 
   LISTBASE_FOREACH (Strip *, strip, ed->seqbasep) {
-    if (selected) {
+    if (RNA_boolean_get(op->ptr, "unselected")) {
       if (strip->flag & SELECT) {
         strip->flag &= ~SEQ_MUTE;
         seq::relations_invalidate_dependent(scene, strip);
