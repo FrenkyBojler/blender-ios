0fc1cbd5de3daa37080c7f8b276db6dff19b1bb6
John Kiril Swenson <kirilswenson@gmail.com>
2025-03-11 17:25:14 -0400

    Also include preview range snapping, rearrange UI
    Plus, update versioning code number.

--- source/blender/makesrna/intern/rna_scene.cc
+++ source/blender/makesrna/intern/rna_scene.cc
@@ -4229,7 +4229,7 @@ static void rna_def_sequencer_tool_settings(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "snap_to_frame_range", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "snap_mode", SEQ_SNAP_TO_FRAME_RANGE);
-  RNA_def_property_ui_text(prop, "Frame Range", "Snap to scene start and end frame");
+  RNA_def_property_ui_text(prop, "Frame Range", "Snap to preview or scene start and end frame");
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr); /* header redraw */
 
   prop = RNA_def_property(srna, "snap_to_borders", PROP_BOOLEAN, PROP_NONE);
