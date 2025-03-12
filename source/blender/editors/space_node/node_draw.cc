5fb2e6f12eb1495336b9afe114089cf310a5c12b
IREXTIA <developer@irextia.studio>
2025-03-13 01:09:33 +0545

    UI: node: additional check for availibilty of next socket before adding spacing

--- source/blender/editors/space_node/node_draw.cc
+++ source/blender/editors/space_node/node_draw.cc
@@ -1211,7 +1211,7 @@ static void node_update_basis_from_socket_lists(
     socket->flag &= ~SOCK_PANEL_COLLAPSED;
 
     if (node_update_basis_socket(C, ntree, node, nullptr, nullptr, socket, block, locx, locy)) {
-      if (socket->next) {
+      if (socket->next && socket->next->is_available()) {
         locy -= NODE_ITEM_SPACING_Y;
       }
       add_output_space = true;
