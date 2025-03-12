22417643af9662e50e8a65516d00f75e82411cdf
Andrej730 <azhilenkov@gmail.com>
2025-03-12 13:55:20 +0500

    bpy.types.Operator docs - add note on execution context
    To clarify the default behaviour when invoke and execute is called.

--- doc/python_api/examples/bpy.types.Operator.2.py
+++ doc/python_api/examples/bpy.types.Operator.2.py
@@ -9,6 +9,13 @@ execute().
 Some operators don't have an execute() function, removing the ability to be
 repeated from a script or macro.
 
+By default when operator is called directly from other scripts it's not going to use invoke()
+but going to use execute(). When it's called from UI it's going to
+use operator context from :class:`UILayout.operator_context` and in most cases it is invoke().
+In both cases different execution context can be provided:
+for UI - by changing :class:`UILayout.operator_context`
+and for scripts - by providing execution context argument to ``bpy.ops`` function.
+
 This example shows how to define an operator which gets mouse input to
 execute a function and that this operator can be invoked or executed from
 the python api.
