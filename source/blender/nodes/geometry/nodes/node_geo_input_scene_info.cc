d93810c124997f04010178b7a6055926318af493
Bbbn19 <85761261+Bbbn19@users.noreply.github.com>
2025-03-12 10:41:19 -0400

    sim frame outputs

--- source/blender/nodes/geometry/nodes/node_geo_input_scene_info.cc
+++ source/blender/nodes/geometry/nodes/node_geo_input_scene_info.cc
@@ -20,10 +20,12 @@ static void node_declare(NodeDeclarationBuilder &b)
 
   b.add_output<decl::Int>("Resolution X");
   b.add_output<decl::Int>("Resolution Y");
-  b.add_output<decl::Float>("Aspect X");
-  b.add_output<decl::Float>("Aspect Y");
+  b.add_output<decl::Vector>("Aspect");
 
   b.add_output<decl::Vector>("Gravity");
+
+  b.add_output<decl::Int>("Simulation Start Frame");
+  b.add_output<decl::Int>("Simulation End Frame");
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -35,16 +37,25 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
   const double frame_rate = double(scene->r.frs_sec) / double(scene->r.frs_sec_base);
 
+  int sim_start = scene->r.sfra;
+  int sim_end = scene->r.efra;
+  if ((scene->flag & SCE_CUSTOM_SIMULATION_RANGE)) {
+    sim_start = scene->simulation_frame_start;
+    sim_end = scene->simulation_frame_end;
+  }
+
   params.set_output("Start Frame", scene->r.sfra);
   params.set_output("End Frame", scene->r.efra);
   params.set_output("Frame Rate", float(frame_rate));
 
   params.set_output("Resolution X", scene->r.xsch);
   params.set_output("Resolution Y", scene->r.ysch);
-  params.set_output("Aspect X", scene->r.xasp);
-  params.set_output("Aspect Y", scene->r.yasp);
+  params.set_output("Aspect", float3{scene->r.xasp, scene->r.yasp, 0.0f});
 
   params.set_output("Gravity", float3(scene->physics_settings.gravity));
+
+  params.set_output("Simulation Start Frame", sim_start);
+  params.set_output("Simulation End Frame", sim_end);
 }
 
 static void node_register()
