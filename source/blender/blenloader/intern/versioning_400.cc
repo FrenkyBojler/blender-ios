58265d7226a4e73a67437aa9882e3565abb12026
Julian Eisel <julian@blender.org>
2025-03-13 17:02:58 +0100

    From review: move versioning to function

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -3864,6 +3864,36 @@ static void version_sequencer_update_overdrop(Main *bmain)
   }
 }
 
+static void asset_browser_add_list_view(Main *bmain)
+{
+  LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
+    LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+      LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
+        if (sl->spacetype != SPACE_FILE) {
+          continue;
+        }
+        SpaceFile *sfile = reinterpret_cast<SpaceFile *>(sl);
+        if (sfile->params) {
+          if (sfile->params->list_thumbnail_size == 0) {
+            sfile->params->list_thumbnail_size = 16;
+          }
+          if (sfile->params->list_column_size == 0) {
+            sfile->params->list_thumbnail_size = 500;
+          }
+        }
+        if (sfile->asset_params) {
+          if (sfile->asset_params->base_params.list_thumbnail_size == 0) {
+            sfile->asset_params->base_params.list_thumbnail_size = 32;
+          }
+          if (sfile->asset_params->base_params.list_column_size == 0) {
+            sfile->asset_params->base_params.list_column_size = 220;
+          }
+        }
+      }
+    }
+  }
+}
+
 void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 400, 1)) {
@@ -6004,34 +6034,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
     rename_mesh_uv_seam_attribute(*mesh);
   }
 
-  {
-    LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
-      LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
-        LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
-          if (sl->spacetype != SPACE_FILE) {
-            continue;
-          }
-          SpaceFile *sfile = reinterpret_cast<SpaceFile *>(sl);
-          if (sfile->params) {
-            if (sfile->params->list_thumbnail_size == 0) {
-              sfile->params->list_thumbnail_size = 16;
-            }
-            if (sfile->params->list_column_size == 0) {
-              sfile->params->list_thumbnail_size = 500;
-            }
-          }
-          if (sfile->asset_params) {
-            if (sfile->asset_params->base_params.list_thumbnail_size == 0) {
-              sfile->asset_params->base_params.list_thumbnail_size = 32;
-            }
-            if (sfile->asset_params->base_params.list_column_size == 0) {
-              sfile->asset_params->base_params.list_column_size = 220;
-            }
-          }
-        }
-      }
-    }
-  }
+  asset_browser_add_list_view(bmain);
 
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
