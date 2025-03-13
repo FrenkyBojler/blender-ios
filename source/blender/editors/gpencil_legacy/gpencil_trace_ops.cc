f6dbec3d7bc59cc26f70961eb36f5d8190737b1d
YimingWu <xp8110@outlook.com>
2025-03-13 12:00:53 +0800

    Formatting

--- source/blender/editors/gpencil_legacy/gpencil_trace_ops.cc
+++ source/blender/editors/gpencil_legacy/gpencil_trace_ops.cc
@@ -253,9 +253,9 @@ static void trace_start_job(void *customdata, wmJobWorkerStatus *worker_status)
 
         BKE_image_release_ibuf(trace_job->image, ibuf, lock);
       }
-    
+
       /* If source type is not sequence, override `trace_job.mode` to single because we need the
-      * correct mode for finalization. */
+       * correct mode for finalization. */
       trace_job->mode = GPENCIL_TRACE_MODE_SINGLE;
     }
   }
