2ddd97196c4efd8e2c960fd6ab50d7ea68bae4c5
Campbell Barton <campbell@blender.org>
2025-03-14 14:34:50 +1100

    Update based on feedback

--- source/blender/editors/curves/intern/curves_edit.cc
+++ source/blender/editors/curves/intern/curves_edit.cc
@@ -116,11 +116,10 @@ static void curve_offsets_from_selection(const Span<IndexRange> selected_points,
 
 void duplicate_points(bke::CurvesGeometry &curves, const IndexMask &mask)
 {
-  const OffsetIndices<int> points_by_curve = curves.points_by_curve();
-  if (points_by_curve.is_empty()) {
+  if (curves.is_empty()) {
     return;
   }
-
+  const OffsetIndices<int> points_by_curve = curves.points_by_curve();
   const VArray<bool> src_cyclic = curves.cyclic();
   Vector<int> dst_to_src_curve;
   Vector<int> new_curve_offsets({points_by_curve.data().last()});
