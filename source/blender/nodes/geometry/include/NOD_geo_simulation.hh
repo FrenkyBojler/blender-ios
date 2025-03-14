5b514ad7876fc462f172920c2485e7f6910f7c92
Jacques Lucke <jacques@blender.org>
2025-03-14 10:00:52 +0100

    add comment

--- source/blender/nodes/geometry/include/NOD_geo_simulation.hh
+++ source/blender/nodes/geometry/include/NOD_geo_simulation.hh
@@ -68,6 +68,7 @@ struct SimulationItemsAccessor {
 
   static bool supports_socket_type(const eNodeSocketDatatype socket_type)
   {
+    /* Data-block types and closures are not supported. */
     return ELEM(socket_type,
                 SOCK_FLOAT,
                 SOCK_VECTOR,
