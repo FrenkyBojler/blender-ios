7bba686a903c884c9f3de5a4b938b976a02cf918
Campbell Barton <campbell@blender.org>
2025-03-14 14:37:52 +1100

    remove whitespace edit

--- source/blender/editors/curves/intern/curves_edit.cc
+++ source/blender/editors/curves/intern/curves_edit.cc
@@ -121,6 +121,7 @@ void duplicate_points(bke::CurvesGeometry &curves, const IndexMask &mask)
   }
   const OffsetIndices<int> points_by_curve = curves.points_by_curve();
   const VArray<bool> src_cyclic = curves.cyclic();
+
   Vector<int> dst_to_src_curve;
   Vector<int> new_curve_offsets({points_by_curve.data().last()});
   Vector<IndexRange> src_ranges;
