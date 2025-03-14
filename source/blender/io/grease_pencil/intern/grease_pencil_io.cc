4b285ffc39fdaf0f003e02bc9a909d43c50a2c44
Philipp Oeser <philipp@blender.org>
2025-03-14 14:47:42 +0100

    from review:
    - just use the default grease pencil material when no material found

--- source/blender/io/grease_pencil/intern/grease_pencil_io.cc
+++ source/blender/io/grease_pencil/intern/grease_pencil_io.cc
@@ -464,20 +464,23 @@ void GreasePencilExporter::foreach_stroke_in_layer(const Object &object,
 
     const bool is_cyclic = cyclic[i_curve];
     const int material_index = material_indices[i_curve];
-    const Material *material = BKE_object_material_get(const_cast<Object *>(&object),
-                                                       material_index + 1);
 
-    if (material != nullptr) {
-      BLI_assert(material->gp_style != nullptr);
-      if (material->gp_style->flag & GP_MATERIAL_HIDE) {
-        continue;
+    const Material *material = [&]() {
+      const Material *material = BKE_object_material_get(const_cast<Object *>(&object),
+                                                         material_index + 1);
+      if (!material) {
+        const Material *material_default = BKE_material_default_gpencil();
+        return material_default;
       }
+      return material;
+    }();
+
+    BLI_assert(material->gp_style != nullptr);
+    if (material->gp_style->flag & GP_MATERIAL_HIDE) {
+      continue;
     }
-    const bool is_stroke_material = material ?
-                                        (material->gp_style->flag & GP_MATERIAL_STROKE_SHOW) :
-                                        true;
-    const bool is_fill_material = material ? (material->gp_style->flag & GP_MATERIAL_FILL_SHOW) :
-                                             false;
+    const bool is_stroke_material = material->gp_style->flag & GP_MATERIAL_STROKE_SHOW;
+    const bool is_fill_material = material->gp_style->flag & GP_MATERIAL_FILL_SHOW;
 
     /* Fill. */
     if (is_fill_material && params_.export_fill_materials) {
@@ -495,14 +498,10 @@ void GreasePencilExporter::foreach_stroke_in_layer(const Object &object,
 
     /* Stroke. */
     if (is_stroke_material && params_.export_stroke_materials) {
-      const ColorGeometry4f default_stroke_color = {0.0f, 0.0f, 0.0f, 1.0f};
-      const ColorGeometry4f stroke_color = material ? compute_average_stroke_color(
-                                                          *material, vertex_colors.slice(points)) :
-                                                      default_stroke_color;
-      const float stroke_opacity = material ?
-                                       compute_average_stroke_opacity(opacities.slice(points)) *
-                                           layer.opacity :
-                                       1.0f;
+      const ColorGeometry4f stroke_color = compute_average_stroke_color(
+          *material, vertex_colors.slice(points));
+      const float stroke_opacity = compute_average_stroke_opacity(opacities.slice(points)) *
+                                   layer.opacity;
       const std::optional<float> uniform_width = params_.use_uniform_width ?
                                                      try_get_uniform_point_width(
                                                          *context_.rv3d,
