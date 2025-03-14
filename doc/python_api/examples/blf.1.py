d9f69e737d4f9afb236eb9b1aa035f2abd80d390
Campbell Barton <campbell@blender.org>
2025-03-14 23:35:08 +1100

    Limit the logic that runs in the context manager for clarity

--- doc/python_api/examples/blf.1.py
+++ doc/python_api/examples/blf.1.py
@@ -16,14 +16,14 @@ ibuf = imbuf.new(image_size)
 
 font_id = blf.load("/path/to/font.ttf")
 
+blf.color(font_id, 1.0, 1.0, 1.0, 1.0)
+blf.size(font_id, font_size)
+blf.position(font_id, 0, image_size[0] - font_size, 0)
+
+blf.enable(font_id, blf.WORD_WRAP)
+blf.word_wrap(font_id, image_size[0])
+
 with blf.bind_imbuf(font_id, ibuf, display_name="sRGB"):
-    blf.color(font_id, 1.0, 1.0, 1.0, 1.0)
-    blf.size(font_id, font_size)
-    blf.position(font_id, 0, image_size[0] - font_size, 0)
-
-    blf.enable(font_id, blf.WORD_WRAP)
-    blf.word_wrap(font_id, image_size[0])
-
     blf.draw_buffer(font_id, "Lots of wrapped text. " * 50)
 
 ibuf.filepath = "/path/to/image.png"
