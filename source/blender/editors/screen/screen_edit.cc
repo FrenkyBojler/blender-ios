b8a9d82c86322699578850e6fd292c359ef6be53
Harley Acheson <harley.acheson@gmail.com>
2025-03-13 18:37:34 -0700

    UI: Increase Border Edge Drag Width
    When resizing areas by dragging at the edges, the hit area is wider
    than the visible border. This extra border padding is extended in this
    PR by another one pixel on either side. This means at 1X scale the hit
    width is two pixels wider, 4 pixels wider at 2X scale (or on Retina).
    This is possible because it adjusts for scrollbars at the edge of the
    area.

--- source/blender/editors/screen/screen_edit.cc
+++ source/blender/editors/screen/screen_edit.cc
@@ -1044,9 +1044,7 @@ static void screen_cursor_set(wmWindow *win, const int xy[2])
 
   LISTBASE_FOREACH (ScrArea *, area_iter, &screen->areabase) {
     az = ED_area_actionzone_find_xy(area_iter, xy);
-    /* Scrollers use default cursor and their zones extend outside of their
-     * areas. Ignore here so we can always detect screen edges - #110085. */
-    if (az && az->type != AZONE_REGION_SCROLL) {
+    if (az) {
       area = area_iter;
       break;
     }
