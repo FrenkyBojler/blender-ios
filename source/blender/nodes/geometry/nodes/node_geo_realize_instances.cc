5ad50a02b31a0d1c87fa6eafc9d01a5f41426c36
илья _ <modormoder@gmail.com>
2025-03-15 17:41:08 +0300

    fix build

--- source/blender/nodes/geometry/nodes/node_geo_realize_instances.cc
+++ source/blender/nodes/geometry/nodes/node_geo_realize_instances.cc
@@ -12,6 +12,9 @@
 
 #include "NOD_rna_define.hh"
 
+#include "UI_interface.hh"
+#include "UI_resources.hh"
+
 namespace blender::nodes::node_geo_realize_instances_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
