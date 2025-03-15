6f2d408d4cd7091340ed2331d8ca2c3c624fdf3e
Guillermo Venegas <guillermovcra@gmail.com>
2025-03-15 13:24:58 -0600

    remove duplicated code after merge

--- source/blender/makesrna/intern/rna_access.cc
+++ source/blender/makesrna/intern/rna_access.cc
@@ -6530,20 +6530,6 @@ void RNA_parameter_list_free(ParameterList *parms)
         MEM_freeN(data_alloc->array);
       }
     }
-    else if ((parm->flag_parameter & PARM_RNAPTR) && (parm->flag & PROP_THICK_WRAP)) {
-      BLI_assert(parm->type == PROP_POINTER);
-      PointerRNA *ptr = static_cast<PointerRNA *>(data);
-      /* #RNA_parameter_list_create ensures that 'thick wrap' PointerRNA parameters are
-       * constructed. */
-      ptr->~PointerRNA();
-    }
-    else if (parm->flag & PROP_DYNAMIC) {
-      /* for dynamic arrays and strings, data is a pointer to an array */
-      ParameterDynAlloc *data_alloc = static_cast<ParameterDynAlloc *>(data);
-      if (data_alloc->array) {
-        MEM_freeN(data_alloc->array);
-      }
-    }
   }
   if (!runtime->reusable_data_alloc) {
     runtime->reusable_data_alloc = parms->data;
