ad7c3238571566fc643f66bad2f3f0f3227cdf5d
Campbell Barton <campbell@blender.org>
2025-03-17 15:18:28 +1100

    Fix #136059: Crash clicking on the marker-region in the time line
    When a marker exists always return the nearest instead of using
    an arbitrary large threshold which caused
    region_position_is_over_marker to crash when a non-empty marker list
    returned a null nearest marker.

--- source/blender/editors/animation/anim_markers.cc
+++ source/blender/editors/animation/anim_markers.cc
@@ -143,13 +143,12 @@ int ED_markers_post_apply_transform(
 TimeMarker *ED_markers_find_nearest_marker(ListBase *markers, float x)
 {
   TimeMarker *nearest = nullptr;
-  float dist, min_dist = 1000000;
 
   if (markers) {
+    float min_dist;
     LISTBASE_FOREACH (TimeMarker *, marker, markers) {
-      dist = fabsf(float(marker->frame) - x);
-
-      if (dist < min_dist) {
+      const float dist = fabsf(float(marker->frame) - x);
+      if (!nearest || (dist < min_dist)) {
         min_dist = dist;
         nearest = marker;
       }
