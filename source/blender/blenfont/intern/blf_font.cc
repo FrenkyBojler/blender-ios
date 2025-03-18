e8670bf39b3b98c06b3001b26ea7d3cf6023ef10
Harley Acheson <harley.acheson@gmail.com>
2025-03-18 16:18:19 -0700

    formatting change.

--- source/blender/blenfont/intern/blf_font.cc
+++ source/blender/blenfont/intern/blf_font.cc
@@ -1308,8 +1308,8 @@ static void blf_font_wrap_apply(FontBLF *font,
     if (UNLIKELY((pen_x_next >= wrap.wrap_width) && (wrap.start != wrap.last[0]))) {
       do_draw = true;
     }
-    else if (UNLIKELY((int(mode) & int(BLFWrapMode::HardLimit)) && (pen_x_next >= wrap.wrap_width) &&
-                 (advance_x != 0)))
+    else if (UNLIKELY((int(mode) & int(BLFWrapMode::HardLimit)) &&
+                      (pen_x_next >= wrap.wrap_width) && (advance_x != 0)))
     {
       wrap.last[0] = i_curr;
       wrap.last[1] = i_curr;
