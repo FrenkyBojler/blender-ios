9fb5b8835d69b55b0bbe465c1cc86c4b3fcc3994
Jeroen Bakker <jeroen@blender.org>
2025-03-18 11:13:40 +0100

    Revert whitespaces

--- intern/ghost/GHOST_C-api.h
+++ intern/ghost/GHOST_C-api.h
@@ -1295,16 +1295,15 @@ void GHOST_GetVulkanHandles(GHOST_ContextHandle context, GHOST_VulkanHandles *r_
  *
  * \param context: GHOST context handle of a vulkan context to
  *     get the Vulkan handles from.
- * \param swap_buffers_pre_callback: Function pointer to be called at the beginning of
- *     swapBuffers. Inside this callback the next swap chain image needs to be acquired and
- *     filled.
- * \param swap_buffers_post_callback: Function to be called at th end of swapBuffers.
- *     swapBuffers can recreate the swap chain. When this is done the application should be
- *     informed by those changes.
- * \param openxr_acquire_image_callback: Function to be called when an
- *     image needs to be acquired to be drawn to an OpenXR swap chain.
- * \param openxr_release_image_callback: Function to be called after an image has been drawn to
- *     the OpenXR swap chain.
+ * \param swap_buffers_pre_callback: Function pointer to be called at the beginning of swapBuffers.
+ *     Inside this callback the next swap chain image needs to be acquired and filled.
+ * \param swap_buffers_post_callback: Function to be called at th end of swapBuffers. swapBuffers
+ *     can recreate the swap chain. When this is done the application should be informed by those
+ *     changes.
+ * \param openxr_acquire_image_callback: Function to be called when an image needs to be acquired
+ *     to be drawn to an OpenXR swap chain.
+ * \param openxr_release_image_callback: Function to be called after an image has been drawn to the
+ *     OpenXR swap chain.
  */
 void GHOST_SetVulkanSwapBuffersCallbacks(
     GHOST_ContextHandle context,
