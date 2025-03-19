c70d6ac94d88bf86eaff5aa82e7317e7fbcf8c25
Pratik Borhade <pratikborhade302@gmail.com>
2025-03-19 14:40:00 +0530

    Remove extra case

--- source/blender/editors/interface/eyedroppers/eyedropper_datablock.cc
+++ source/blender/editors/interface/eyedroppers/eyedropper_datablock.cc
@@ -242,17 +242,15 @@ static void datadropper_set_draw_callback_region(ScrArea *area, DataDropper *ddr
       ARegion *region = BKE_area_find_region_type(ddr->cursor_area, RGN_TYPE_WINDOW);
       ED_region_tag_redraw(region);
 
-      if (area->spacetype != ddr->cursor_area->spacetype) {
-        /* Remove old callback */
-        ED_region_draw_cb_exit(ddr->art, ddr->draw_handle_pixel);
-        /* Set draw callback in new region */
-        ARegionType *art = BKE_regiontype_from_id(area->type, RGN_TYPE_WINDOW);
+      /* Remove old callback */
+      ED_region_draw_cb_exit(ddr->art, ddr->draw_handle_pixel);
+      /* Set draw callback in new region */
+      ARegionType *art = BKE_regiontype_from_id(area->type, RGN_TYPE_WINDOW);
 
-        ddr->cursor_area = area;
-        ddr->art = art;
-        ddr->draw_handle_pixel = ED_region_draw_cb_activate(
-            art, datadropper_draw_cb, ddr, REGION_DRAW_POST_PIXEL);
-      }
+      ddr->cursor_area = area;
+      ddr->art = art;
+      ddr->draw_handle_pixel = ED_region_draw_cb_activate(
+          art, datadropper_draw_cb, ddr, REGION_DRAW_POST_PIXEL);
     }
   }
 }
