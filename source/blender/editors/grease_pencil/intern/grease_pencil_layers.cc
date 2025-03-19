ada4c470db2f13ad1408b9525af09d31c4577a26
Pratik Borhade <pratikborhade302@gmail.com>
2025-03-19 15:05:01 +0530

    Fix #136156: Grease Pencil: Crash calling active layer operator with out-of-bounds index
    Add extra check to exit early from the operator when `layer_index > total
    layers`.

--- source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
@@ -297,6 +297,10 @@ static int grease_pencil_layer_active_exec(bContext *C, wmOperator *op)
   GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object->data);
   int layer_index = RNA_int_get(op->ptr, "layer");
 
+  if (layer_index >= grease_pencil.layers().size()) {
+    return OPERATOR_CANCELLED;
+  }
+
   Layer &layer = grease_pencil.layer(layer_index);
   if (grease_pencil.is_layer_active(&layer)) {
     return OPERATOR_CANCELLED;
