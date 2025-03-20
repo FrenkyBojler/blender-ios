13c97cd5c66c6cd7673f034ae33b97fdd464fca9
tariqsulley <tariqsulley3c@gmail.com>
2025-03-20 20:30:10 +0000

    sets offset even in extrude individual faces and move to hidden

--- source/blender/editors/transform/transform_ops.cc
+++ source/blender/editors/transform/transform_ops.cc
@@ -1132,11 +1132,13 @@ static void TRANSFORM_OT_shrink_fatten(wmOperatorType *ot)
 
   RNA_def_float_distance(ot->srna, "value", 0, -FLT_MAX, FLT_MAX, "Offset", "", -FLT_MAX, FLT_MAX);
 
-  RNA_def_boolean(ot->srna,
-                  "use_even_offset",
-                  false,
-                  "Offset Even",
-                  "Scale the offset to give more even thickness");
+  PropertyRNA *prop;
+  prop = RNA_def_boolean(ot->srna,
+                         "use_even_offset",
+                         false,
+                         "Offset Even",
+                         "Scale the offset to give more even thickness");
+  RNA_def_property_flag(prop, PROP_HIDDEN);
 
   WM_operatortype_props_advanced_begin(ot);
 
