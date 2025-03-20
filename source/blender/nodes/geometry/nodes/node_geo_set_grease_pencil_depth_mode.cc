4774660749a5d0234315f103b19a7d09b9ef93ce
Falk David <falk@blender.org>
2025-03-20 12:51:23 +0100

    Fix default

--- source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_depth_mode.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_depth_mode.cc
@@ -28,7 +28,7 @@ static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
-  node->custom1 = NORMAL_MODE_MINIMUM_TWIST;
+  node->custom1 = 0;
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
