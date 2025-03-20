a648b46cd3bcd936a12f1d427af930f5ea0fd13c
Sean Kim <SeanCTKim@protonmail.com>
2025-03-20 10:37:58 -0700

    Revert "Fix: Use non-zero timer value for UI test event simulation"
    This reverts commit 9b98fea71b4b9afedd3871f108d83cca487e1957.

--- tests/python/ui_simulate/modules/easy_keys.py
+++ tests/python/ui_simulate/modules/easy_keys.py
@@ -288,11 +288,15 @@ def run(
 ):
     import bpy
 
-    # Each step in a script will wait at least this long before executing further
-    # Going lower, to 1/120, causes some tests to fail.
-    EXECUTION_WAIT = 1.0 / 60.0
+    TICKS = 4  # 3 works, 4  to be on the safe side.
 
     def event_step():
+        # Run once 'TICKS' is reached.
+        if event_step._ticks < TICKS:
+            event_step._ticks += 1
+            return 0.0
+        event_step._ticks = 0
+
         if on_step_command_pre:
             if event_step.run_events.gi_frame is not None:
                 import shlex
@@ -332,7 +336,7 @@ def run(
                 )
 
         if isinstance(val, EventGenerate) or val is None:
-            return EXECUTION_WAIT
+            return 0.0
         elif val is Ellipsis:
             if on_exit is not None:
                 on_exit()
