cef710339f340f5270db9530335c84cb1800c77a
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-03-20 14:45:49 +0100

    add menu to all editors

--- scripts/startup/bl_ui/space_graph.py
+++ scripts/startup/bl_ui/space_graph.py
@@ -8,6 +8,11 @@ from bl_ui.space_dopesheet import (
     DopesheetFilterPopoverBase,
     dopesheet_filter,
 )
+from bl_ui.space_toolsystem_common import PlayheadSnappingPanel
+
+
+class GRAPH_PT_playhead_snapping(PlayheadSnappingPanel, Panel):
+    bl_space_type = 'GRAPH_EDITOR'
 
 
 class GRAPH_HT_header(Header):
@@ -57,6 +62,7 @@ class GRAPH_HT_header(Header):
             panel="GRAPH_PT_snapping",
             text="",
         )
+        layout.popover(panel="GRAPH_PT_playhead_snapping")
 
         row = layout.row(align=True)
         row.prop(tool_settings, "use_proportional_fcurve", text="", icon_only=True)
@@ -540,6 +546,7 @@ classes = (
     GRAPH_MT_view_pie,
     GRAPH_PT_filters,
     GRAPH_PT_snapping,
+    GRAPH_PT_playhead_snapping,
 )
 
 if __name__ == "__main__":  # only for live edit.
