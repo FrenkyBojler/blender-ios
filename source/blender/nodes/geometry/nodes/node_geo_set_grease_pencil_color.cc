0eceb41faa33e3b9f2eae83b01b57bdc1e41e332
Falk David <falk@blender.org>
2025-03-20 16:02:09 +0100

    Add mode selection

--- source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_color.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_grease_pencil_color.cc
@@ -5,59 +5,52 @@
 #include "BKE_curves.hh"
 #include "BKE_grease_pencil.hh"
 
+#include "UI_interface.hh"
+
+#include "NOD_rna_define.hh"
+
+#include "RNA_enum_types.hh"
+
 #include "node_geometry_util.hh"
 
 namespace blender::nodes::node_geo_set_grease_pencil_color_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.use_custom_socket_order();
-  b.allow_any_socket_order();
-
   b.add_output<decl::Geometry>("Grease Pencil").propagate_all();
   b.add_input<decl::Geometry>("Grease Pencil")
       .supported_type(GeometryComponent::Type::GreasePencil)
       .align_with_previous();
 
-  PanelDeclarationBuilder &point_panel = b.add_panel("Point").default_closed(false).description(
-      "Set color and opacity attributes on the point domain");
-  point_panel.add_input<decl::Bool>("Point").default_value(true).field_on_all().panel_toggle();
-  point_panel.add_input<decl::Color>("Color", "Point Color")
+  b.add_input<decl::Bool>("Selection").default_value(true).hide_value().field_on_all();
+  b.add_input<decl::Color>("Color")
       .default_value(ColorGeometry4f(1.0f, 1.0f, 1.0f, 1.0f))
       .field_on_all()
       .hide_label();
-  point_panel.add_input<decl::Float>("Opacity", "Point Opacity")
-      .default_value(1.0f)
-      .min(0.0f)
-      .max(1.0f)
-      .field_on_all();
+  b.add_input<decl::Float>("Opacity").default_value(1.0f).min(0.0f).max(1.0f).field_on_all();
+}
 
-  PanelDeclarationBuilder &fill_panel = b.add_panel("Fill").default_closed(false).description(
-      "Set fill and opacity attributes on the spline domain");
-  fill_panel.add_input<decl::Bool>("Fill").default_value(true).field_on_all().panel_toggle();
-  fill_panel.add_input<decl::Color>("Color", "Fill Color")
-      .default_value(ColorGeometry4f(1.0f, 1.0f, 1.0f, 1.0f))
-      .field_on_all()
-      .hide_label();
-  fill_panel.add_input<decl::Float>("Opacity", "Fill Opacity")
-      .default_value(1.0f)
-      .min(0.0f)
-      .max(1.0f)
-      .field_on_all();
+static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+{
+  uiItemR(layout, ptr, "mode", UI_ITEM_NONE, "", ICON_NONE);
+}
+static void node_init(bNodeTree * /*tree*/, bNode *node)
+{
+  node->custom1 = int(AttrDomain::Point);
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  GeometrySet geometry_set = params.extract_input<GeometrySet>("Grease Pencil");
-  const Field<bool> point_selection = params.extract_input<Field<bool>>("Point");
-  const Field<ColorGeometry4f> point_color = params.extract_input<Field<ColorGeometry4f>>(
-      "Point Color");
-  const Field<float> point_opacity = params.extract_input<Field<float>>("Point Opacity");
+  const bNode &node = params.node();
+  const AttrDomain domain = AttrDomain(node.custom1);
 
-  const Field<bool> fill_selection = params.extract_input<Field<bool>>("Fill");
-  const Field<ColorGeometry4f> fill_color = params.extract_input<Field<ColorGeometry4f>>(
-      "Fill Color");
-  const Field<float> fill_opacity = params.extract_input<Field<float>>("Fill Opacity");
+  GeometrySet geometry_set = params.extract_input<GeometrySet>("Grease Pencil");
+  const Field<bool> selection = params.extract_input<Field<bool>>("Selection");
+  const Field<ColorGeometry4f> color_field = params.extract_input<Field<ColorGeometry4f>>("Color");
+  const Field<float> opacity_field = params.extract_input<Field<float>>("Opacity");
+
+  const StringRef color_attr_name = domain == AttrDomain::Point ? "vertex_color" : "fill_color";
+  const StringRef opacity_attr_name = domain == AttrDomain::Point ? "opacity" : "fill_opacity";
 
   if (GreasePencil *grease_pencil = geometry_set.get_grease_pencil_for_write()) {
     using namespace blender::bke::greasepencil;
@@ -67,58 +60,55 @@ static void node_geo_exec(GeoNodeExecParams params)
         continue;
       }
       bke::CurvesGeometry &curves = drawing->strokes_for_write();
+      const int64_t domain_size = curves.attributes().domain_size(domain);
 
-      bke::GreasePencilLayerFieldContext layer_field_context_on_point(
-          *grease_pencil, AttrDomain::Point, layer_index);
-
-      /* TODO: Avoid doing this if the selection is false. */
-      if (!curves.attributes().contains("opacity")) {
-        curves.attributes_for_write().add(
-            "opacity",
-            bke::AttrDomain::Point,
-            CD_PROP_FLOAT,
-            bke::AttributeInitVArray(VArray<float>::ForSingle(1.0f, curves.points_num())));
-      }
-      bke::try_capture_field_on_geometry(curves.attributes_for_write(),
-                                         layer_field_context_on_point,
-                                         "vertex_color",
-                                         bke::AttrDomain::Point,
-                                         point_selection,
-                                         point_color);
-      bke::try_capture_field_on_geometry(curves.attributes_for_write(),
-                                         layer_field_context_on_point,
-                                         "opacity",
-                                         bke::AttrDomain::Point,
-                                         point_selection,
-                                         point_opacity);
-
-      bke::GreasePencilLayerFieldContext layer_field_context_on_curves(
-          *grease_pencil, AttrDomain::Curve, layer_index);
+      const bke::GreasePencilLayerFieldContext layer_field_context(
+          *grease_pencil, domain, layer_index);
 
       /* TODO: Avoid doing this if the selection is false. */
-      if (!curves.attributes().contains("fill_opacity")) {
+      if (!curves.attributes().contains(opacity_attr_name)) {
         curves.attributes_for_write().add(
-            "fill_opacity",
-            bke::AttrDomain::Curve,
+            opacity_attr_name,
+            domain,
             CD_PROP_FLOAT,
-            bke::AttributeInitVArray(VArray<float>::ForSingle(1.0f, curves.curves_num())));
+            bke::AttributeInitVArray(VArray<float>::ForSingle(1.0f, domain_size)));
       }
       bke::try_capture_field_on_geometry(curves.attributes_for_write(),
-                                         layer_field_context_on_point,
-                                         "fill_color",
-                                         bke::AttrDomain::Curve,
-                                         fill_selection,
-                                         fill_color);
+                                         layer_field_context,
+                                         color_attr_name,
+                                         domain,
+                                         selection,
+                                         color_field);
       bke::try_capture_field_on_geometry(curves.attributes_for_write(),
-                                         layer_field_context_on_curves,
-                                         "fill_opacity",
-                                         bke::AttrDomain::Curve,
-                                         fill_selection,
-                                         fill_opacity);
+                                         layer_field_context,
+                                         opacity_attr_name,
+                                         domain,
+                                         selection,
+                                         opacity_field);
     }
   }
 
   params.set_output("Grease Pencil", std::move(geometry_set));
+}
+
+static void node_rna(StructRNA *srna)
+{
+  static const EnumPropertyItem color_mode_items[] = {
+      {int(AttrDomain::Point),
+       "POINT",
+       ICON_NONE,
+       "Point",
+       "Set the color and opacity for individual points"},
+      {int(AttrDomain::Curve),
+       "CURVE",
+       ICON_NONE,
+       "Fill",
+       "Set the color and opacity for fills (curves)"},
+      {0, nullptr, 0, nullptr, nullptr},
+  };
+
+  RNA_def_node_enum(
+      srna, "mode", "Mode", "", color_mode_items, NOD_inline_enum_accessors(custom1));
 }
 
 static void node_register()
@@ -131,8 +121,12 @@ static void node_register()
   ntype.nclass = NODE_CLASS_GEOMETRY;
   ntype.geometry_node_execute = node_geo_exec;
   ntype.declare = node_declare;
+  ntype.initfunc = node_init;
+  ntype.draw_buttons = node_layout;
   bke::node_type_size(ntype, 170, 120, NODE_DEFAULT_MAX_WIDTH);
   blender::bke::node_register_type(ntype);
+
+  node_rna(ntype.rna_ext.srna);
 }
 NOD_REGISTER_NODE(node_register)
 
