81e935894d0de58f272a0c523f83a1ccaae77a4a
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-03-20 11:43:38 +0100

    format

--- source/blender/editors/space_graph/ED_space_graph.hh
+++ source/blender/editors/space_graph/ED_space_graph.hh
@@ -2,13 +2,13 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
- #include "BLI_vector.hh"
+#include "BLI_vector.hh"
 
- struct FCurve;
- struct bContext;
+struct FCurve;
+struct bContext;
 
-namespace blender::ed::graph{
+namespace blender::ed::graph {
 
-    blender::Vector<FCurve *> get_visible_fcurves(bContext *C);
-    
-}
+blender::Vector<FCurve *> get_visible_fcurves(bContext *C);
+
+}  // namespace blender::ed::graph
