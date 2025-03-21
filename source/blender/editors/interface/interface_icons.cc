162e0a7b1f4c72fe3918b996ee80133a1d400c98
Leon Schittek <leon.schittek@gmx.net>
2025-03-21 07:17:29 +0100

    Remove unused variable.

--- source/blender/editors/interface/interface_icons.cc
+++ source/blender/editors/interface/interface_icons.cc
@@ -317,8 +317,6 @@ static void vicon_handletype_auto_clamp_draw(int x, int y, int w, int h, float a
 
 static void icon_node_socket_draw(int socket_type, int x, int y, int w, int h, float /*alpha*/)
 {
-  const float scale = 0.9f * float(w) / float(ICON_DEFAULT_WIDTH);
-
   /* Factor to account for the draw function of the node socket being based on the widget unit,
    * which is 10 px by default, which differs from icons. */
   constexpr float size_factor = 10.0f / float(ICON_DEFAULT_WIDTH);
