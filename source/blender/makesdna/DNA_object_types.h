c2bd5b88dbe769a5b0aec7617639eff378ad36d6
Johnny Matthews <johnny.matthews@gmail.com>
2025-03-21 09:23:48 -0500

    Change naming convention Geometry->Indices

--- source/blender/makesdna/DNA_object_types.h
+++ source/blender/makesdna/DNA_object_types.h
@@ -573,8 +573,8 @@ enum {
   /* runtime constraints disable */
   OB_NO_CONSTRAINTS = 1 << 13,
   /* when calculating vertex parent position, ignore CD_ORIGINDEX layer */
-  OB_PARENT_USE_FINAL_GEOMETRY = 1 << 14,
-
+  OB_PARENT_USE_FINAL_INDICES = 1 << 14,
+  
   OB_DUPLI = OB_DUPLIVERTS | OB_DUPLICOLLECTION | OB_DUPLIFACES | OB_DUPLIPARTS,
 };
 
