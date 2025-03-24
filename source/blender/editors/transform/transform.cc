c89588a7b3bde7fdec92ab6b722aed96351e3603
Richard Antalik <richardantalik@gmail.com>
2025-03-24 21:20:03 +0100

    Cleanup - forgot to remove condition

--- source/blender/editors/transform/transform.cc
+++ source/blender/editors/transform/transform.cc
@@ -1306,9 +1306,7 @@ int transformEvent(TransInfo *t, wmOperator *op, const wmEvent *event)
         }
         break;
       case TFM_MODAL_ORIGIN:
-        if (t->flag & T_ORIGIN) {
-          t->flag ^= T_ORIGIN;
-        }
+        t->flag ^= T_ORIGIN;
         freeSnapping(t);
         initSnapping(t, nullptr);
         t->redraw |= TREDRAW_HARD;
