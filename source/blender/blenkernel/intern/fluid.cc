blender/blender

commit 024d7d12e24a5468d3e9878adf29b0b0443b5dc8

Mesh: Move BVH storage to shared cache system
Avoid rebuilding BVH trees when meshes are copied.
Similar to the other uses of the shared cache system,
this can arbitrarily improve performance when meshes
are copied but not deformed and BVH building is the
main bottleneck. In a simple test file I got a 6x speedup.

The amount of code is also reduced and the system is
much simpler overall-- built out of common threading
patterns like `SharedCache` with its double-checked lock.
RAII is used in a few places to simplify memory management
too.

The downside is storing more `SharedCache` items in the
mesh runtime struct. That has a slight cost when copying
a small mesh many times, but we have ideas to improve that
in the future anyway (#104327).

Pull Request: https://projects.blender.org/blender/blender/pulls/130865

source/blender/blenkernel/intern/fluid.cc
@@ -1009,7 +1009,6 @@ static void obstacles_from_mesh(Object *coll_ob,
                                 float dt)
 {
   if (fes->mesh) {
-    BVHTreeFromMesh tree_data = {nullptr};
     int numverts, i;
 
     float *vert_vel = nullptr;
@@ -1079,8 +1078,8 @@ static void obstacles_from_mesh(Object *coll_ob,
 
     /* Skip effector sampling loop if object has disabled effector. */
     bool use_effector = fes->flags & FLUID_EFFECTOR_USE_EFFEC;
-    if (use_effector && BKE_bvhtree_from_mesh_get(&tree_data, mesh, BVHTREE_FROM_CORNER_TRIS, 4)) {
-
+    BVHTreeFromMesh tree_data = mesh->bvh_corner_tris();
+    if (use_effector && tree_data.tree != nullptr) {
       ObstaclesFromDMData data{};
       data.fes = fes;
       data.vert_positions = positions;
@@ -1099,8 +1098,6 @@ static void obstacles_from_mesh(Object *coll_ob,
       settings.min_iter_per_thread = 2;
       BLI_task_parallel_range(min[2], max[2], &data, obstacles_from_mesh_task_cb, &settings);
     }
-    /* Free bvh tree. */
-    free_bvhtree_from_mesh(&tree_data);
 
     if (vert_vel) {
       MEM_freeN(vert_vel);
@@ -2068,7 +2065,6 @@ static void emit_from_mesh(
     Object *flow_ob, FluidDomainSettings *fds, FluidFlowSettings *ffs, FluidObjectBB *bb, float dt)
 {
   if (ffs->mesh) {
-    BVHTreeFromMesh tree_data = {nullptr};
     int i;
 
     float *vert_vel = nullptr;
@@ -2147,7 +2143,8 @@ static void emit_from_mesh(
 
     /* Skip flow sampling loop if object has disabled flow. */
     bool use_flow = ffs->flags & FLUID_FLOW_USE_INFLOW;
-    if (use_flow && BKE_bvhtree_from_mesh_get(&tree_data, mesh, BVHTREE_FROM_CORNER_TRIS, 4)) {
+    BVHTreeFromMesh tree_data = mesh->bvh_corner_tris();
+    if (use_flow && tree_data.tree != nullptr) {
 
       EmitFromDMData data{};
       data.fds = fds;
@@ -2173,8 +2170,6 @@ static void emit_from_mesh(
       settings.min_iter_per_thread = 2;
       BLI_task_parallel_range(min[2], max[2], &data, emit_from_mesh_task_cb, &settings);
     }
-    /* Free bvh tree. */
-    free_bvhtree_from_mesh(&tree_data);
 
     if (vert_vel) {
       MEM_freeN(vert_vel);

source/blender/blenkernel/BKE_bvhutils.hh
@@ -23,11 +23,19 @@ struct MFace;
 struct Mesh;
 struct PointCloud;
 
+class BVHTreeDeleter {
+ public:
+  void operator()(BVHTree *tree)
+  {
+    BLI_bvhtree_free(tree);
+  }
+};
+
 /**
  * Struct that stores basic information about a #BVHTree built from a mesh.
  */
 struct BVHTreeFromMesh {
-  BVHTree *tree = nullptr;
+  const BVHTree *tree = nullptr;
 
   /** Default callbacks to BVH nearest and ray-cast. */
   BVHTree_NearestPointCallback nearest_callback;
@@ -41,8 +49,7 @@ struct BVHTreeFromMesh {
 
   const MFace *face = nullptr;
 
-  /* Private data */
-  bool cached = false;
+  std::unique_ptr<BVHTree, BVHTreeDeleter> owned_tree;
 };
 
 enum BVHCacheType {
@@ -111,17 +118,6 @@ BVHTree *bvhtree_from_mesh_corner_tris_ex(BVHTreeFromMesh *data,
                                           int axis);
 
 /**
- * Builds or queries a BVH-cache for the cache BVH-tree of the request type.
- *
- * \note This function only fills a cache, and therefore the mesh argument can
- * be considered logically const. Concurrent access is protected by a mutex.
- */
-BVHTree *BKE_bvhtree_from_mesh_get(BVHTreeFromMesh *data,
-                                   const Mesh *mesh,
-                                   BVHCacheType bvh_cache_type,
-                                   int tree_type);
-
-/**
  * Build a bvh tree from the triangles in the mesh that correspond to the faces in the given mask.
  */
 void BKE_bvhtree_from_mesh_tris_init(const Mesh &mesh,
@@ -141,11 +137,6 @@ void BKE_bvhtree_from_mesh_edges_init(const Mesh &mesh,
 void BKE_bvhtree_from_mesh_verts_init(const Mesh &mesh,
                                       const blender::IndexMask &verts_mask,
                                       BVHTreeFromMesh &r_data);
-
-/**
- * Frees data allocated by a call to `bvhtree_from_mesh_*`.
- */
-void free_bvhtree_from_mesh(BVHTreeFromMesh *data);
 
 /**
  * Math functions used by callbacks
@@ -172,16 +163,3 @@ void BKE_bvhtree_from_pointcloud_get(const PointCloud &pointcloud,
                                      BVHTreeFromPointCloud &r_data);
 
 void free_bvhtree_from_pointcloud(BVHTreeFromPointCloud *data);
-
-/**
- * BVHCache
- */
-
-/* Using local coordinates */
-
-bool bvhcache_has_tree(const BVHCache *bvh_cache, const BVHTree *tree);
-BVHCache *bvhcache_init();
-/**
- * Frees a BVH-cache.
- */
-void bvhcache_free(BVHCache *bvh_cache);

source/blender/blenkernel/BKE_mesh_types.hh
@@ -22,14 +22,14 @@
 #include "DNA_customdata_types.h"
 
 struct BMEditMesh;
-struct BVHCache;
+struct BVHTree;
 struct Mesh;
 class ShrinkwrapBoundaryData;
 struct SubdivCCG;
 struct SubsurfRuntimeData;
 namespace blender::bke {
 struct EditMeshData;
-}
+}  // namespace blender::bke
 namespace blender::bke::bake {
 struct BakeMaterialsList;
 }
@@ -107,6 +107,12 @@ struct TrianglesCache {
   void tag_dirty();
 };
 
+struct BVHCacheItem {
+  BVHTree *tree = nullptr;
+  BVHCacheItem();
+  ~BVHCacheItem();
+};
+
 struct MeshRuntime {
   /**
    * "Evaluated" mesh owned by this mesh. Used for objects which don't have effective modifiers, so
@@ -158,8 +164,15 @@ struct MeshRuntime {
   /** Cache for triangle to original face index map, accessed with #Mesh::corner_tri_faces(). */
   SharedCache<Array<int>> corner_tri_faces_cache;
 
-  /** Cache for BVH trees generated for the mesh. Defined in 'BKE_bvhutil.c' */
-  BVHCache *bvh_cache = nullptr;
+  SharedCache<BVHCacheItem> bvh_cache_verts;
+  SharedCache<BVHCacheItem> bvh_cache_edges;
+  SharedCache<BVHCacheItem> bvh_cache_faces;
+  SharedCache<BVHCacheItem> bvh_cache_corner_tris;
+  SharedCache<BVHCacheItem> bvh_cache_corner_tris_no_hidden;
+  SharedCache<BVHCacheItem> bvh_cache_loose_verts;
+  SharedCache<BVHCacheItem> bvh_cache_loose_verts_no_hidden;
+  SharedCache<BVHCacheItem> bvh_cache_loose_edges;
+  SharedCache<BVHCacheItem> bvh_cache_loose_edges_no_hidden;
 
   /** Needed in case we need to lazily initialize the mesh. */
   CustomData_MeshMasks cd_mask_extra = {};

source/blender/blenkernel/BKE_shrinkwrap.hh
@@ -79,10 +79,11 @@ const ShrinkwrapBoundaryData &boundary_cache_ensure(const Mesh &mesh);
 struct ShrinkwrapTreeData {
   Mesh *mesh;
 
-  BVHTree *bvh;
+  const BVHTree *bvh;
   BVHTreeFromMesh treeData;
 
   blender::OffsetIndices<int> faces;
+  blender::Span<blender::int2> edges;
   blender::Span<int> corner_edges;
 
   blender::Span<blender::float3> face_normals;

source/blender/blenkernel/intern/attribute_access.cc
@@ -481,8 +481,12 @@ GAttributeWriter CustomDataAttributeProvider::try_get_for_write(void *owner,
     if (type == nullptr) {
       continue;
     }
+    std::function<void()> tag_modified_fn;
+    if (custom_data_access_.get_tag_modified_function != nullptr) {
+      tag_modified_fn = custom_data_access_.get_tag_modified_function(owner, attribute_id);
+    }
     GMutableSpan data{*type, layer.data, element_num};
-    return {GVMutableArray::ForSpan(data), domain_};
+    return {GVMutableArray::ForSpan(data), domain_, tag_modified_fn};
   }
   return {};
 }
@@ -498,6 +502,13 @@ bool CustomDataAttributeProvider::try_delete(void *owner, const StringRef attrib
     const CustomDataLayer &layer = custom_data->layers[i];
     if (this->type_is_supported(eCustomDataType(layer.type)) && layer.name == attribute_id) {
       CustomData_free_layer(custom_data, eCustomDataType(layer.type), element_num, i);
+      if (custom_data_access_.get_tag_modified_function != nullptr) {
+        if (const std::function<void()> fn = custom_data_access_.get_tag_modified_function(
+                owner, attribute_id))
+        {
+          fn();
+        }
+      }
       return true;
     }
   }
@@ -528,6 +539,18 @@ bool CustomDataAttributeProvider::try_create(void *owner,
   const int element_num = custom_data_access_.get_element_num(owner);
   add_custom_data_layer_from_attribute_init(
       attribute_id, *custom_data, data_type, element_num, initializer, {});
+  if (initializer.type != AttributeInit::Type::Construct) {
+    /* Avoid calling update function when values are not default-initialized. Without default
+     * initialization or otherwise meaningful initial values, they should be set elsewhere
+     * anyway, which will cause a separate update tag. */
+    if (custom_data_access_.get_tag_modified_function != nullptr) {
+      if (const std::function<void()> fn = custom_data_access_.get_tag_modified_function(
+              owner, attribute_id))
+      {
+        fn();
+      }
+    }
+  }
   return true;
 }
 

source/blender/blenkernel/intern/attribute_access_intern.hh
@@ -2,6 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <functional>
+
 #include "BLI_generic_pointer.hh"
 #include "BLI_map.hh"
 #include "BLI_span.hh"
@@ -21,10 +23,12 @@ struct CustomDataAccessInfo {
   using CustomDataGetter = CustomData *(*)(void *owner);
   using ConstCustomDataGetter = const CustomData *(*)(const void *owner);
   using GetElementNum = int (*)(const void *owner);
+  using GetTagModifiedFunction = std::function<void()> (*)(void *owner, StringRef name);
 
   CustomDataGetter get_custom_data;
   ConstCustomDataGetter get_const_custom_data;
   GetElementNum get_element_num;
+  GetTagModifiedFunction get_tag_modified_function;
 };
 
 /**

source/blender/blenkernel/intern/cloth.cc
@@ -1500,7 +1500,6 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
   bool use_internal_springs = (clmd->sim_parms->flags & CLOTH_SIMSETTINGS_FLAG_INTERNAL_SPRINGS);
 
   if (use_internal_springs && numface > 0) {
-    BVHTreeFromMesh treedata = {nullptr};
     int tar_v_idx;
     Mesh *tmp_mesh = nullptr;
     RNG *rng;
@@ -1513,7 +1512,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
     }
 
     Set<OrderedEdge> existing_vert_pairs;
-    BKE_bvhtree_from_mesh_get(&treedata, tmp_mesh ? tmp_mesh : mesh, BVHTREE_FROM_CORNER_TRIS, 2);
+    BVHTreeFromMesh treedata = tmp_mesh ? tmp_mesh->bvh_corner_tris() : mesh->bvh_corner_tris();
     rng = BLI_rng_new_srandom(0);
 
     const blender::Span<blender::float3> vert_normals = tmp_mesh ? tmp_mesh->vert_normals() :
@@ -1561,7 +1560,6 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
         }
         else {
           cloth_free_errorsprings(cloth, edgelist, spring_ref);
-          free_bvhtree_from_mesh(&treedata);
           if (tmp_mesh) {
             BKE_id_free(nullptr, &tmp_mesh->id);
           }
@@ -1571,7 +1569,6 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
       }
     }
     existing_vert_pairs.clear_and_shrink();
-    free_bvhtree_from_mesh(&treedata);
     if (tmp_mesh) {
       BKE_id_free(nullptr, &tmp_mesh->id);
     }

FrenkyBojler/blender-ios

commit 0effecd17633703d47ba9ceab71e29baa1512b9c

Fix #136317: Mantaflow moving Collision not detected
Caused by 024d7d12e24a

To resolve ( similar to d0a89a0950e2 ), add missing dirty tagging for
the positions.

source/blender/blenkernel/intern/fluid.cc
@@ -1055,6 +1055,8 @@ static void obstacles_from_mesh(Object *coll_ob,
       bb_boundInsert(bb, positions[i]);
     }
 
+    mesh->tag_positions_changed();
+
     /* Set emission map.
      * Use 3 cell diagonals as margin (3 * 1.732 = 5.196). */
     int bounds_margin = int(ceil(5.196));