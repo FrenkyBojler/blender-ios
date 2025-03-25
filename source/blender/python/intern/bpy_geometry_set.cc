ff69b98c0cc247236ce3b3fd8c90dc4a2344520e
Jacques Lucke <jacques@blender.org>
2025-03-25 17:21:04 +0100

    improve docstring

--- source/blender/python/intern/bpy_geometry_set.cc
+++ source/blender/python/intern/bpy_geometry_set.cc
@@ -171,8 +171,8 @@ PyDoc_STRVAR(
     "   The returned pointcloud should not be modified.\n"
     "   There is a point per instance and per-instance data is stored in point attributes.\n"
     "   The local transforms are stored in the \"instance_transform\" attribute.\n"
-    "   The data instanced by each point is referenced by the \".reference_index\" attribute.\n"
-    "   This is an index into the list returned by :func:`GeometrySet.instance_references`.\n"
+    "   The data instanced by each point is referenced by the \".reference_index\" attribute,\n"
+    "   indexing into the list returned by :func:`GeometrySet.instance_references`.\n"
     "\n"
     "   :rtype: bpy.types.PointCloud\n");
 static PyObject *BPy_GeometrySet_get_instances_pointcloud(BPy_GeometrySet *self)
