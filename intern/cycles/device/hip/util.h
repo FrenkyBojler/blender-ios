167b393b721e53d06701db155f2f4ddd508a17c1
salipour <sahar.kashi@amd.com>
2025-03-25 09:03:53 -0700

    code review

--- intern/cycles/device/hip/util.h
+++ intern/cycles/device/hip/util.h
@@ -80,7 +80,7 @@ static inline bool hipIsRDNA2OrNewer(const int hipDevId)
 static inline bool hipNeedPreciseMath(const std::string &arch)
 {
 #  ifdef _WIN32
-  /* Enable fast math option only for non-RDNA2 GPUs (compiler bug on Windows). */
+  /* Enable stricter math options for RDNA2 GPUs (compiler bug on Windows). */
   return (arch == "gfx1030" || arch == "gfx1031" || arch == "gfx1032" || arch == "gfx1033" ||
           arch == "gfx1034" || arch == "gfx1035" || arch == "gfx1036");
 #  else
--- intern/cycles/device/hiprt/device_impl.cpp
+++ intern/cycles/device/hiprt/device_impl.cpp
@@ -224,7 +224,7 @@ string HIPRTDevice::compile_kernel(const uint kernel_features, const char *name,
   const char *const kernel_ext = "genco";
   string options;
   options.append(
-      "-Wno-parentheses-equality -Wno-unused-value -O3 -std=c++17 -D __HIPRT__ -ffast-math");
+      "-Wno-parentheses-equality -Wno-unused-value -ffast-math -O3 -std=c++17 -D __HIPRT__");
   options.append(" --offload-arch=").append(arch.c_str());
   if (hipNeedPreciseMath(arch)) {
     options.append(
