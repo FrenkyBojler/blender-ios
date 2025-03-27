f38e7b205048b54468b933ba8716d05d7da5af31
Aras Pranckevicius <aras@nesnausk.org>
2025-03-27 13:56:21 +0200

    Disable file drop handler of experimental importer for now

--- source/blender/editors/io/io_ops.cc
+++ source/blender/editors/io/io_ops.cc
@@ -90,7 +90,7 @@ void ED_operatortypes_io()
 
 #ifdef WITH_IO_FBX
   WM_operatortype_append(WM_OT_fbx_import);
-  ed::io::fbx_file_handler_add();
+  /* ed::io::fbx_file_handler_add(); TODO: add once not experimental */
 #endif
 
   WM_operatortype_append(WM_OT_drop_import_file);
