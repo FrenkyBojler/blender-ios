0103e524797f750c2679daf77857050322500ba5
Clément Foucault <foucault.clem@gmail.com>
2025-03-27 19:19:52 +0100

    Add perceptual space accumulation

--- source/blender/draw/engines/gpencil/shaders/gpencil_antialiasing_accumulation_frag.glsl
+++ source/blender/draw/engines/gpencil/shaders/gpencil_antialiasing_accumulation_frag.glsl
@@ -6,11 +6,21 @@
 
 FRAGMENT_SHADER_CREATE_INFO(gpencil_antialiasing_accumulation)
 
+vec4 colorspace_scene_to_perceptual(vec4 color)
+{
+  return vec4(log2(color.rgb + 0.5), color.a);
+}
+
+vec4 colorspace_perceptual_to_scene(vec4 color)
+{
+  return vec4(exp2(color.rgb) - 0.5, color.a);
+}
+
 void main()
 {
   ivec2 texel = ivec2(gl_FragCoord.xy);
-  vec4 data_src = imageLoadFast(src_img, texel);
-  vec4 data_dst = imageLoadFast(dst_img, texel);
+  vec4 data_src = colorspace_scene_to_perceptual(imageLoadFast(src_img, texel));
+  vec4 data_dst = colorspace_scene_to_perceptual(imageLoadFast(dst_img, texel));
   vec4 result = data_src * weight_src;
   if (weight_dst > 0.0) {
     /* Avoid uncleared data to mess with the result value. */
@@ -20,5 +30,5 @@ void main()
     /* Avoid float imprecision leading to non fully opaque renders. */
     result.a = 1.0;
   }
-  imageStoreFast(dst_img, texel, result);
+  imageStoreFast(dst_img, texel, colorspace_perceptual_to_scene(result));
 }
