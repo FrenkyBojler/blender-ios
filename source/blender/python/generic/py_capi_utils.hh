986ef9b7004d324884c6bceebf228800de5e813d
Jacques Lucke <jacques@blender.org>
2025-03-28 18:04:24 +0100

    clarify comment

--- source/blender/python/generic/py_capi_utils.hh
+++ source/blender/python/generic/py_capi_utils.hh
@@ -259,7 +259,9 @@ bool PyC_RunString_AsStringOrNone(const char **imports,
                                   const char *filename,
                                   char **r_value) ATTR_NONNULL(2, 3, 4) ATTR_WARN_UNUSED_RESULT;
 
-/** Flush stdout and stderr. */
+/**
+ * Flush stdout and stderr. Errors are ignored.
+ */
 void PyC_StdFilesFlush();
 
 /**
