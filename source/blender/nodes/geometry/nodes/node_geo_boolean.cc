5ec8516f1a625752d8e2d54354d9179012458dc6
Hans Goudey <hans@blender.org>
2025-03-27 23:09:16 -0400

    use ELEM macro

--- source/blender/nodes/geometry/nodes/node_geo_boolean.cc
+++ source/blender/nodes/geometry/nodes/node_geo_boolean.cc
@@ -181,9 +181,7 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
 
   AttributeOutputs attribute_outputs;
-  if (solver == geometry::boolean::Solver::MeshArr ||
-      solver == geometry::boolean::Solver::Manifold)
-  {
+  if (ELEM(solver, geometry::boolean::Solver::MeshArr, geometry::boolean::Solver::Manifold)) {
     attribute_outputs.intersecting_edges_id = params.get_output_anonymous_attribute_id_if_needed(
         "Intersecting Edges");
   }
