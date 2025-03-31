872713bb2eb59d2f99b77d1cec9b25e4f880c1a7
Clément Foucault <foucault.clem@gmail.com>
2025-04-01 01:04:49 +0200

    Add simpler density fade

--- source/blender/draw/engines/overlay/shaders/overlay_grid_mesh_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_grid_mesh_vert.glsl
@@ -14,7 +14,15 @@ VERTEX_SHADER_CREATE_INFO(overlay_grid_mesh)
 #include "gpu_shader_math_base_lib.glsl"
 #include "gpu_shader_utildefines_lib.glsl"
 
-vec4 get_homogenous_space_grid_point(int x, int y, out float dist_to_cam, out float view_angle)
+float approximate_grid_cell_screen_size(float dist_to_cam, float view_angle)
+{
+  vec3 vs_P = vec3(0.0, unit_scale, -dist_to_cam);
+  vec3 ss_P = drw_point_view_to_screen(vs_P);
+  return (ss_P.y - 0.5) * sizeViewport.y * sqrt(abs(view_angle));
+}
+
+vec4 get_homogenous_space_grid_point(
+    int x, int y, out float dist_to_cam, out float z_to_cam, out float view_angle)
 {
   vec3 ls_P = vec3(x, y, 0.0) * unit_scale;
 
@@ -34,8 +42,10 @@ vec4 get_homogenous_space_grid_point(int x, int y, out float dist_to_cam, out fl
   }
   ls_P.z -= camera_P.z;
 
+  dist_to_cam = length(ls_P);
+
   /* ls_P is already centered around the camera. */
-  vec3 V = drw_view_is_perspective() ? normalize(ls_P) : drw_view_forward();
+  vec3 V = drw_view_is_perspective() ? ls_P * safe_rcp(dist_to_cam) : drw_view_forward();
   view_angle = V.z;
   if (axis == 3) {
     /* Don't fade. */
@@ -45,7 +55,7 @@ vec4 get_homogenous_space_grid_point(int x, int y, out float dist_to_cam, out fl
   /* Do not use matrix translation as it degrades precision. */
   vec3 vs_P = drw_normal_world_to_view(ls_P);
 
-  dist_to_cam = abs(vs_P.z);
+  z_to_cam = abs(vs_P.z);
 
   return drw_point_view_to_homogenous(vs_P);
 }
@@ -55,16 +65,11 @@ void main()
   int x = int(uint(gl_VertexID) >> 16u) - 0x7FFF;
   int y = int(uint(gl_VertexID) & (~0x0u >> 16u)) - 0x7FFF;
 
-  float dist_to_cam, view_angle;
-  /* Adjacent points used to get screen space grid density. */
-  vec4 hs_P_dx = get_homogenous_space_grid_point(x + 1, y, dist_to_cam, view_angle);
-  vec4 hs_P_dy = get_homogenous_space_grid_point(x, y + 1, dist_to_cam, view_angle);
-  vec4 hs_P = get_homogenous_space_grid_point(x, y, dist_to_cam, view_angle);
+  float dist_to_cam, z_to_cam, view_angle;
+  vec4 hs_P = get_homogenous_space_grid_point(x, y, dist_to_cam, z_to_cam, view_angle);
 
   /* Convert to screen position [0..sizeVp]. */
   vec2 ss_P = drw_ndc_to_screen(drw_perspective_divide(hs_P)).xy * sizeViewport;
-  vec2 ss_P_dx = drw_ndc_to_screen(drw_perspective_divide(hs_P_dx)).xy * sizeViewport;
-  vec2 ss_P_dy = drw_ndc_to_screen(drw_perspective_divide(hs_P_dy)).xy * sizeViewport;
 
   if (axis == 1) {
     finalColor = colorGridAxisX;
@@ -76,22 +81,21 @@ void main()
     finalColor = colorGridAxisZ;
   }
   else {
-    /* TODO(fclem): Scale area depending on UI scale. */
     /* Area of the projected tile in pixels. */
-    float area = length(cross(vec3(ss_P_dx - ss_P, 0.0), vec3(ss_P_dy - ss_P, 0.0)));
-    float fac = sqrt(area);
-    float mix_fade = smoothstep(1.0, 30.0, fac);
+    float size = approximate_grid_cell_screen_size(dist_to_cam, view_angle) /
+                 globalsBlock.size_pixel;
+    float mix_fade = smoothstep(2.0, 25.0, size);
     /* TODO(fclem): Adjust with relative density with level N-2. */
-    float mix_highlight = smoothstep(500.0, 1000.0, fac);
+    float mix_highlight = smoothstep(20.0, 300.0, size);
     finalColor = mix(colorGrid, colorGridEmphasis, mix_highlight);
     finalColor.a *= mix_fade;
   }
 
-  /*  Angle fading. */
+  /* Angle fading. */
   finalColor.a *= 1.0 - square(square(1.0 - abs(view_angle)));
 
   /* Distance fading. */
-  finalColor.a *= smoothstep(far_clip, far_clip * 0.5f, dist_to_cam);
+  finalColor.a *= smoothstep(far_clip, far_clip * 0.5f, z_to_cam);
 
   edgePos = edgeStart = ss_P;
 
