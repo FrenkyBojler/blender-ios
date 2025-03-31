c4800286dcf33cc40ddf3f9c582751d7b583056a
Aras Pranckevicius <aras@nesnausk.org>
2025-03-31 10:31:29 +0300

    Fix embedded textures not loading properly

--- source/blender/io/fbx/importer/fbx_import_material.cc
+++ source/blender/io/fbx/importer/fbx_import_material.cc
@@ -229,6 +229,7 @@ static Image *load_texture_image(Main *bmain, const std::string &file_dir, const
 
   /* Use embedded data for this image, if we haven't done that yet. */
   if (tex.content.size > 0 && (image == nullptr || !BKE_image_has_packedfile(image))) {
+    BKE_image_free_buffers(image); /* Free cached placeholder images. */
     char *data_dup = static_cast<char *>(MEM_mallocN(tex.content.size, __func__));
     memcpy(data_dup, tex.content.data, tex.content.size);
     BKE_image_packfiles_from_mem(nullptr, image, data_dup, tex.content.size);
