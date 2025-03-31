f7c8068f35e582f3320c4df56ddadc820909a81e
Aras Pranckevicius <aras@nesnausk.org>
2025-03-31 11:43:36 +0300

    Fix #136651: H265 output not compatible with Apple (Quicktime/FCP)

--- source/blender/imbuf/movie/intern/movie_write.cc
+++ source/blender/imbuf/movie/intern/movie_write.cc
@@ -761,6 +761,12 @@ static AVStream *alloc_video_stream(MovieWriter *context,
     c->codec_tag = (('D' << 24) + ('I' << 16) + ('V' << 8) + 'X');
   }
 
+  if (codec_id == AV_CODEC_ID_H265) {
+    /* H.265 needs hvc1 tag for Apple compatibility, see
+     * https://trac.ffmpeg.org/wiki/Encode/H.265#FinalCutandApplestuffcompatibility */
+    c->codec_tag = MKTAG('h', 'v', 'c', '1');
+  }
+
   /* Keep lossless encodes in the RGB domain. */
   if (codec_id == AV_CODEC_ID_HUFFYUV) {
     if (rd->im_format.planes == R_IMF_PLANES_RGBA) {
