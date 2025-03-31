44b646fc3f6ed30aea86c60a94fb5a4719edd355
Aras Pranckevicius <aras@nesnausk.org>
2025-03-31 14:15:53 +0300

    Clarify comment

--- source/blender/imbuf/movie/intern/movie_write.cc
+++ source/blender/imbuf/movie/intern/movie_write.cc
@@ -763,7 +763,9 @@ static AVStream *alloc_video_stream(MovieWriter *context,
 
   if (codec_id == AV_CODEC_ID_H265) {
     /* H.265 needs hvc1 tag for Apple compatibility, see
-     * https://trac.ffmpeg.org/wiki/Encode/H.265#FinalCutandApplestuffcompatibility */
+     * https://trac.ffmpeg.org/wiki/Encode/H.265#FinalCutandApplestuffcompatibility
+     * Note that in case we are doing H.265 into an XviD container,
+     * this overwrites the tag set above. But that should not be what anyone does. */
     c->codec_tag = MKTAG('h', 'v', 'c', '1');
   }
 
