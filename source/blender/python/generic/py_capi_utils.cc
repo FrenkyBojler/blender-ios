820739d9dce470ad916a7bf819238f7204955e09
Jacques Lucke <jacques@blender.org>
2025-03-31 12:19:01 +0200

    use PyImport_ImportModule and handle return value

--- source/blender/python/generic/py_capi_utils.cc
+++ source/blender/python/generic/py_capi_utils.cc
@@ -1694,33 +1694,28 @@ bool PyC_RunString_AsStringOrNone(const char *imports[],
 
 void PyC_StdFilesFlush()
 {
-  PyObject *sys_module = PyImport_ImportModule("sys");
-  if (!sys_module) {
-    PyErr_Clear();
-    return;
-  }
   PyObject *py_flush = PyUnicode_FromString("flush");
   if (!py_flush) {
     PyErr_Clear();
     return;
   }
-
   auto flush = [&](const char *name) {
-    PyObject *py_file = PyObject_GetAttrString(sys_module, name);
+    PyObject *py_file = PySys_GetObject(name);
     if (!py_file) {
       PyErr_Clear();
       return;
     }
-    if (!PyObject_CallMethodNoArgs(py_file, py_flush)) {
+    PyObject *py_flush_retval = PyObject_CallMethodNoArgs(py_file, py_flush);
+    if (py_flush_retval) {
+      Py_DECREF(py_flush_retval);
+    }
+    else {
       PyErr_Clear();
     }
-    Py_DECREF(py_file);
   };
-
   flush("stdout");
   flush("stderr");
   Py_DECREF(py_flush);
-  Py_DECREF(sys_module);
 }
 
 /** \} */
