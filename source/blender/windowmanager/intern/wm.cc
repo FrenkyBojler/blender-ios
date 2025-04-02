4dd467a8f84cab0a1585028800d8f529b760ee3c
YimingWu <xp8110@outlook.com>
2025-04-02 13:21:44 +0800

    formatting

--- source/blender/windowmanager/intern/wm.cc
+++ source/blender/windowmanager/intern/wm.cc
@@ -173,10 +173,9 @@ static void window_manager_blend_read_data(BlendDataReader *reader, ID *id)
     win->cursor_keymap_status = nullptr;
 
     /* Some files could be saved with ime_data still present.
-    * See https://projects.blender.org/blender/blender/issues/136829 */
+     * See https://projects.blender.org/blender/blender/issues/136829 */
     win->ime_data = nullptr;
     win->ime_data_is_composing = false;
-    
 
     BLI_listbase_clear(&win->handlers);
     BLI_listbase_clear(&win->modalhandlers);
