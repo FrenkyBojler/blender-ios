aefeb9d7664413df212ab7d01e18991449c7ef28
mvji <33432858+mvji@users.noreply.github.com>
2025-04-02 15:28:51 +0200

    FFmpeg Output: Fix unused func args
    When compiling without FFmpeg supports

--- source/blender/imbuf/movie/intern/movie_util.cc
+++ source/blender/imbuf/movie/intern/movie_util.cc
@@ -513,7 +513,7 @@ bool MOV_codec_supports_alpha(int av_codec_id, int prores_profile)
               AV_CODEC_ID_VP9,
               AV_CODEC_ID_HUFFYUV);
 #else
-  UNUSED_VARS(av_codec_id, profile);
+  UNUSED_VARS(av_codec_id, prores_profile);
   return false;
 #endif
 }
