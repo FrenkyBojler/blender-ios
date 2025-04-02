465267d4526e4ef0ceb060e551c08aa890f279ab
Hans Goudey <hans@blender.org>
2025-04-02 16:46:52 -0400

    Move implementation to a new file
    So this won't have to be recompiled so much ideally

---
+++ source/blender/nodes/intern/volume_grid_function_eval.hh
@@ -0,0 +1,22 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup nodes
+ */
+
+#pragma once
+
+#include "FN_multi_function.hh"
+
+#include "NOD_geometry_exec.hh"
+
+namespace blender::nodes {
+
+void execute_multi_function_on_value_variant__volume_grid(
+    const mf::MultiFunction &fn,
+    const Span<SocketValueVariant *> input_values,
+    const Span<SocketValueVariant *> output_values);
+
+}
