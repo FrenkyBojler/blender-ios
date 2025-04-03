6e32ff7357f99e7ddb8dd0473b79bacfa255b2a9
HeCorr <hccaseclone@gmail.com>
2025-04-03 14:46:38 -0300

    KeyingSets: Always show 'Available' option in Insert Keyframe Menu
    see #127175.
    currently, if an object (i.e. Armature in Pose Mode) has no animation data yet, and the K
    shortcut is pressed to open the keyframe menu, the Available option will not be on the list,
    which is a problem because that will mess up the hotkey assignments, i.e.:
    
    - Location & Rotation (for bones) is usually T, but is assigned to A.
    - Rotation & Scale (for bones) is usually E, but is assigned to N.
    
    The proper fix for this would be to explicitly define the hotkeys for each option,
    and ideally make them customizable, but it would be way too difficult for me,
    so I'm submitting this hotfix instead, which simply forces the Available option
    to always be visible, so they hotkeys won't change.
    
    In my personal opinion this is a good tradeoff:
    - Super simple change: just enforce the eventual behavior.
    - We get consistent hotkeys which is very important.
    - Users just get an error if they pick Available and there's no anim data yet. no big deal.

--- scripts/startup/keyingsets_builtins.py
+++ scripts/startup/keyingsets_builtins.py
@@ -350,14 +350,9 @@ class BUILTIN_KSI_Available(KeyingSetInfo):
     bl_idname = ANIM_KS_AVAILABLE_ID
     bl_label = "Available"
 
-    # poll - selected objects or selected object with animation data
+    # poll - selected objects
     def poll(self, context):
-        ob = context.active_object
-        if ob:
-            # TODO: this fails if one animation-less object is active, but many others are selected
-            return ob.animation_data and ob.animation_data.action
-        else:
-            return bool(context.selected_objects)
+        return bool(context.selected_objects)
 
     # iterator - use callback for selected bones/objects
     iterator = keyingsets_utils.RKS_ITER_selected_item
