395412f74eb7cbd0bdd1167df5468090448be45d
Bbbn19 <85761261+Bbbn19@users.noreply.github.com>
2025-04-03 18:21:26 -0400

    remove sim frames

--- source/blender/nodes/geometry/nodes/node_geo_input_scene_info.cc
+++ source/blender/nodes/geometry/nodes/node_geo_input_scene_info.cc
@@ -23,9 +23,6 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Vector>("Aspect");
 
   b.add_output<decl::Vector>("Gravity");
-
-  b.add_output<decl::Int>("Simulation Start Frame");
-  b.add_output<decl::Int>("Simulation End Frame");
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -37,12 +34,6 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
   const double frame_rate = double(scene->r.frs_sec) / double(scene->r.frs_sec_base);
   float res_scale = scene->r.size * 0.01f;
-  int sim_start = scene->r.sfra;
-  int sim_end = scene->r.efra;
-  if ((scene->flag & SCE_CUSTOM_SIMULATION_RANGE)) {
-    sim_start = scene->simulation_frame_start;
-    sim_end = scene->simulation_frame_end;
-  }
 
   params.set_output("Start Frame", scene->r.sfra);
   params.set_output("End Frame", scene->r.efra);
@@ -53,9 +44,6 @@ static void node_geo_exec(GeoNodeExecParams params)
   params.set_output("Aspect", float3{scene->r.xasp, scene->r.yasp, 0.0f});
 
   params.set_output("Gravity", float3(scene->physics_settings.gravity));
-
-  params.set_output("Simulation Start Frame", sim_start);
-  params.set_output("Simulation End Frame", sim_end);
 }
 
 static void node_register()
