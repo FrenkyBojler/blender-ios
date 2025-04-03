zyxcambridge/blender_llm

Fix: VSE crash when moving retiming key
Caused by not checking against key array size. The issue was introduced
in ee9e4ead8d. It looks like mistake done during refactoring, because
the code was supposed to fix previous transition segment, not only the
next one.

The previously code attempted to reduce levels of indentation, but it
was less readable and error prone. Now it should be more obvious what
the intent is.

Pull Request: https://projects.blender.org/blender/blender/pulls/136743

source/blender/sequencer/intern/strip_retiming.cc
@@ -692,22 +692,21 @@ static void strip_retiming_fix_transition(const Scene *scene, Strip *strip, SeqR
 
 static void strip_retiming_fix_transitions(const Scene *scene, Strip *strip, SeqRetimingKey *key)
 {
+  /* Store value, since handles array will be reallocated. */
   const int key_index = retiming_key_index_get(strip, key);
 
-  if (!retiming_is_last_key(strip, key)) {
-    SeqRetimingKey *next_key = key + 1;
-    if (retiming_key_is_transition_start(next_key)) {
-      strip_retiming_fix_transition(scene, strip, next_key);
+  if (key_index > 1) {
+    SeqRetimingKey *prev_key = key - 2;
+    if (retiming_key_is_transition_start(prev_key)) {
+      strip_retiming_fix_transition(scene, strip, prev_key);
     }
   }
 
-  if (key_index <= 1) {
-    return;
-  }
-
-  SeqRetimingKey *next_key = &retiming_keys_get(strip)[key_index + 1];
-  if (retiming_key_is_transition_start(next_key)) {
-    strip_retiming_fix_transition(scene, strip, next_key);
+  if (!retiming_is_last_key(strip, key)) {
+    SeqRetimingKey *next_key = &retiming_keys_get(strip)[key_index + 1];
+    if (retiming_key_is_transition_start(next_key)) {
+      strip_retiming_fix_transition(scene, strip, next_key);
+    }
   }
 }
 

FrenkyBojler/blender-ios

source/blender/sequencer/intern/strip_retiming.cc
@@ -695,22 +695,21 @@ static void strip_retiming_fix_transition(const Scene *scene, Strip *strip, SeqR
 
 static void strip_retiming_fix_transitions(const Scene *scene, Strip *strip, SeqRetimingKey *key)
 {
+  /* Store value, since handles array will be reallocated. */
   const int key_index = SEQ_retiming_key_index_get(strip, key);
 
-  if (!SEQ_retiming_is_last_key(strip, key)) {
-    SeqRetimingKey *next_key = key + 1;
-    if (SEQ_retiming_key_is_transition_start(next_key)) {
-      strip_retiming_fix_transition(scene, strip, next_key);
+  if (key_index > 1) {
+    SeqRetimingKey *prev_key = key - 2;
+    if (SEQ_retiming_key_is_transition_start(prev_key)) {
+      strip_retiming_fix_transition(scene, strip, prev_key);
     }
   }
 
-  if (key_index <= 1) {
-    return;
-  }
-
-  SeqRetimingKey *next_key = &SEQ_retiming_keys_get(strip)[key_index + 1];
-  if (SEQ_retiming_key_is_transition_start(next_key)) {
-    strip_retiming_fix_transition(scene, strip, next_key);
+  if (!SEQ_retiming_is_last_key(strip, key)) {
+    SeqRetimingKey *next_key = &SEQ_retiming_keys_get(strip)[key_index + 1];
+    if (SEQ_retiming_key_is_transition_start(next_key)) {
+      strip_retiming_fix_transition(scene, strip, next_key);
+    }
   }
 }
 