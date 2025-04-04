a0f08fdc4c10ba7127ebb17db5e6c9983d6a19e2
Sybren A. Stüvel <sybren@blender.org>
2025-04-04 11:01:24 +0200

    Fix #136967: Crash on Merge Animation when Slot has no Channelbag
    The Merge Animation operator assumed that when a keyframe strip exists,
    it always has a channelbag for the to-be-moved slot. This isn't
    necessarily the case, so now it only moves the channelbag if it exists.

--- source/blender/animrig/intern/action.cc
+++ source/blender/animrig/intern/action.cc
@@ -3104,13 +3104,15 @@ void move_slot(Main &bmain, Slot &source_slot, Action &from_action, Action &to_a
     StripKeyframeData &to_strip_data = to_action.layer(0)->strip(0)->data<StripKeyframeData>(
         to_action);
     Channelbag *channelbag = from_strip_data.channelbag_for_slot(source_slot.handle);
-    BLI_assert(channelbag != nullptr);
-    channelbag->slot_handle = target_slot.handle;
-    grow_array_and_append<ActionChannelbag *>(
-        &to_strip_data.channelbag_array, &to_strip_data.channelbag_array_num, channelbag);
-    int index = from_strip_data.find_channelbag_index(*channelbag);
-    shrink_array_and_remove<ActionChannelbag *>(
-        &from_strip_data.channelbag_array, &from_strip_data.channelbag_array_num, index);
+    /* It's perfectly fine for a slot to not have a channelbag on each keyframe strip. */
+    if (channelbag) {
+      channelbag->slot_handle = target_slot.handle;
+      grow_array_and_append<ActionChannelbag *>(
+          &to_strip_data.channelbag_array, &to_strip_data.channelbag_array_num, channelbag);
+      const int index = from_strip_data.find_channelbag_index(*channelbag);
+      shrink_array_and_remove<ActionChannelbag *>(
+          &from_strip_data.channelbag_array, &from_strip_data.channelbag_array_num, index);
+    }
   }
 
   /* Reassign all users of `source_slot` to the action `to_action` and the slot `target_slot`. */
