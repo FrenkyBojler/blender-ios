873e03e1fb57bd19180760d05d0821f16d1868bc
Sean Kim <SeanCTKim@protonmail.com>
2025-04-04 16:22:33 -0700

    make format

--- source/blender/editors/sculpt_paint/brushes/clay_thumb.cc
+++ source/blender/editors/sculpt_paint/brushes/clay_thumb.cc
@@ -188,7 +188,8 @@ void do_clay_thumb_brush(const Depsgraph &depsgraph,
   float4x4 scale = math::from_scale<float4x4>(float3(ss.cache->radius));
   const float4x4 tmat = mat * scale;
 
-  const float3 normal_tilt = math::rotate_direction_around_axis(area_position, tmat.x_axis(), DEG2RADF(-ss.cache->clay_thumb_brush.front_angle));
+  const float3 normal_tilt = math::rotate_direction_around_axis(
+      area_position, tmat.x_axis(), DEG2RADF(-ss.cache->clay_thumb_brush.front_angle));
 
   /* Tilted plane (front part of the brush). */
   float4 plane_tilt;
