73cc03cdcb70b3d737e5b8db696e9359ab5f3b4b
Nicola <nicdessi@gmail.com>
2025-04-04 15:25:00 +0200

    remove bounds update in vertex painting

--- source/blender/editors/sculpt_paint/paint_vertex.cc
+++ source/blender/editors/sculpt_paint/paint_vertex.cc
@@ -538,10 +538,6 @@ void update_cache_variants(bContext *C, VPaint &vp, Object &ob, PointerRNA *ptr)
   }
 
   cache->radius_squared = cache->radius * cache->radius;
-
-  //if (bke::pbvh::Tree *pbvh = bke::object::pbvh_get(ob)) {
-  //  pbvh->update_bounds(depsgraph, ob);
-  //}
 }
 
 void get_brush_alpha_data(const Scene &scene,
