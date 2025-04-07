86601cd3ae9d5f538e7fd8d4043905014b358207
Sybren A. Stüvel <sybren@blender.org>
2025-04-07 12:59:30 +0200

    Fix comment order

--- scripts/startup/bl_operators/anim.py
+++ scripts/startup/bl_operators/anim.py
@@ -678,11 +678,11 @@ class ANIM_OT_slot_new_for_id(Operator):
 
     Note that _which_ ID should get this slot must be set in the 'animated_id' context pointer, using:
 
+    >>> layout.context_pointer_set("animated_id", animated_id)
+
     When the ID already has a slot assigned, the newly-created slot will be
     named after it (ensuring uniqueness with a numerical suffix) and any
     animation data of the assigned slot will be duplicated for the new slot.
-
-    >>> layout.context_pointer_set("animated_id", animated_id)
     """
     bl_idname = "anim.slot_new_for_id"
     bl_label = "New Slot"
