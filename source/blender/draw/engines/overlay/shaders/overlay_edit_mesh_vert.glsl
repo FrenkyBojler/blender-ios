8d6ae693de9b28d3da60771586ef0aa8a796d8b5
Miguel Pozo <pragma37@gmail.com>
2025-04-08 16:35:21 +0200

    Fix test_occlusion

--- source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_vert.glsl
@@ -25,7 +25,14 @@ VERTEX_SHADER_CREATE_INFO(overlay_edit_mesh_vert)
 bool test_occlusion()
 {
   vec3 ndc = (gl_Position.xyz / gl_Position.w) * 0.5 + 0.5;
-  return ndc.z > texture(depthTex, ndc.xy).r;
+  ivec2 coord = ivec2(ndc.xy * textureSize(depthTex, 0).xy);
+  bool occluded = true;
+  for (int x = -1; x <= 1; x += 2) {
+    for (int y = -1; y <= 1; y += 2) {
+      occluded = occluded && (ndc.z > texelFetch(depthTex, coord + ivec2(x, y), 0).r);
+    }
+  }
+  return occluded;
 }
 
 vec3 non_linear_blend_color(vec3 col1, vec3 col2, float fac)
