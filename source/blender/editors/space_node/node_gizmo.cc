4c4f42d4d467feefe8a0919f922acb78c4d1fa3f
Habib Gahbiche <habib@blender.org>
2025-04-08 11:09:59 +0100

    Fix headers

--- source/blender/editors/space_node/node_gizmo.cc
+++ source/blender/editors/space_node/node_gizmo.cc
@@ -16,7 +16,7 @@
 
 #include "BKE_context.hh"
 #include "BKE_image.hh"
-#include "BKE_node_legacy_types.hh"
+#include "BKE_node_runtime.hh"
 
 #include "ED_gizmo_library.hh"
 #include "ED_screen.hh"
