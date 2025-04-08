8d901e456e7372fedc6e69978a874f39f12b8b23
Jeroen Bakker <jeroen@blender.org>
2025-04-08 10:14:18 +0200

    APIENTRY

--- source/blender/gpu/vulkan/vk_common.hh
+++ source/blender/gpu/vulkan/vk_common.hh
@@ -13,7 +13,14 @@
 #ifdef __APPLE__
 #  include <MoltenVK/vk_mvk_moltenvk.h>
 #else
+#  ifdef _WIN32
+#    include "BLI_winstuff.h"
+#    define VK_USE_PLATFORM_WIN32_KHR
+#  endif
 #  include <vulkan/vulkan.h>
+#  ifdef _WIN32
+#    include <vulkan/vulkan_win32.h>
+#  endif
 #endif
 
 #include "vk_mem_alloc.h"
