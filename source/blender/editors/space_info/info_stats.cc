2939ee7fed863ca7b44623039da2de1ccf8811b2
Pratik Borhade <pratikborhade302@gmail.com>
2025-04-09 15:17:44 +0530

    Add totcurvepoints

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -73,6 +73,7 @@ struct SceneStats {
   uint64_t totlamp, totlampsel;
   uint64_t tottri, tottrisel;
   uint64_t totgplayer, totgpframe, totgpstroke, totgppoint;
+  uint64_t totcuvepoints;
 };
 
 struct SceneStatsFmt {
@@ -90,6 +91,7 @@ struct SceneStatsFmt {
       totgpframe[BLI_STR_FORMAT_UINT64_GROUPED_SIZE];
   char totgpstroke[BLI_STR_FORMAT_UINT64_GROUPED_SIZE],
       totgppoint[BLI_STR_FORMAT_UINT64_GROUPED_SIZE];
+  char totcuvepoints[BLI_STR_FORMAT_UINT64_GROUPED_SIZE];
 };
 
 static bool stats_mesheval(const Mesh *mesh_eval, bool is_selected, SceneStats *stats)
@@ -194,6 +196,13 @@ static void stats_object(Object *ob,
       break;
     }
     case OB_CURVES:
+    {
+      using namespace blender;
+      const Curves &curves_id = *static_cast<Curves *>(ob->data);
+      const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
+      stats->totcuvepoints += curves.points_num();
+      break;
+    }
     case OB_POINTCLOUD:
     case OB_VOLUME: {
       break;
@@ -324,7 +333,7 @@ static void stats_object_edit(Object *obedit, SceneStats *stats)
     const VArray<bool> selection = *curves.attributes().lookup_or_default<bool>(
         ".selection", bke::AttrDomain::Point, true);
     stats->totvertsel += array_utils::count_booleans(selection);
-    stats->totvert += curves.points_num();
+    stats->totcuvepoints += curves.points_num();
   }
 }
 
@@ -534,6 +543,8 @@ static bool format_stats(
   SCENE_STATS_FMT_INT(totgpstroke);
   SCENE_STATS_FMT_INT(totgppoint);
 
+  SCENE_STATS_FMT_INT(totcuvepoints);
+
 #undef SCENE_STATS_FMT_INT
   return true;
 }
@@ -589,6 +600,13 @@ static void get_stats_string(char *info,
                                 stats_fmt->totbonesel,
                                 stats_fmt->totbone);
     }
+    else if (ob->type == OB_CURVES) {
+      *ofs += BLI_snprintf_rlen(info + *ofs,
+                                len - *ofs,
+                                IFACE_("Points:%s/%s"),
+                                stats_fmt->totvertsel,
+                                stats_fmt->totcuvepoints);
+    }
     else {
       *ofs += BLI_snprintf_rlen(info + *ofs,
                                 len - *ofs,
@@ -612,11 +630,12 @@ static void get_stats_string(char *info,
                               stats_fmt->totgppoint);
   }
   else if (ob && (ob->type == OB_CURVES)) {
+    const char *count = (object_mode == OB_MODE_SCULPT_CURVES) ? stats_fmt->totvertsculpt :
+                                                                 stats_fmt->totcuvepoints;
     *ofs += BLI_snprintf_rlen(info + *ofs,
                               len - *ofs,
-
                               IFACE_("Points:%s"),
-                              stats_fmt->totvertsculpt);
+                              count);
   }
   else if (ob && (object_mode & OB_MODE_SCULPT)) {
     if (stats_is_object_dynamic_topology_sculpt(ob)) {
