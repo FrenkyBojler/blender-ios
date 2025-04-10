d3ee2de3d030812afc34e3a97f8a37a4083d94b2
Jacques Lucke <jacques@blender.org>
2025-04-10 20:10:50 +0200

    improve uniquename api

--- source/blender/blenlib/BLI_string_utils.hh
+++ source/blender/blenlib/BLI_string_utils.hh
@@ -18,8 +18,6 @@
 
 struct ListBase;
 
-using UniquenameCheckCallback = bool (*)(void *arg, const char *name);
-
 /* ------------------------------------------------------------------------- */
 /** \name String Replace
  * \{ */
@@ -153,12 +151,11 @@ size_t BLI_string_flip_side_name(char *name_dst,
  * \param name: Name to be ensured unique
  * \param name_maxncpy: Maximum length of name area
  */
-void BLI_uniquename_cb(UniquenameCheckCallback unique_check,
-                       void *arg,
+void BLI_uniquename_cb(blender::FunctionRef<bool(blender::StringRefNull)> unique_check,
                        const char *defname,
                        char delim,
                        char *name,
-                       size_t name_maxncpy) ATTR_NONNULL(1, 3, 5);
+                       size_t name_maxncpy) ATTR_NONNULL(2, 4);
 
 /**
  * Ensures name is unique (according to criteria specified by caller in unique_check callback),
