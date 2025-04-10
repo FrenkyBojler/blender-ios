fbcd7dff6411720bf8db6567e27907304662ca8f
Brecht Van Lommel <brecht@blender.org>
2025-04-10 20:20:39 +0200

    Fix camera inside volume check

--- intern/cycles/scene/camera.cpp
+++ intern/cycles/scene/camera.cpp
@@ -469,9 +469,6 @@ void Camera::update(Scene *scene)
   kcam->nearclip = nearclip;
   kcam->cliplength = (farclip == FLT_MAX) ? FLT_MAX : farclip - nearclip;
 
-  /* Camera in volume. */
-  kcam->is_inside_volume = 0;
-
   /* Rolling shutter effect */
   kcam->rolling_shutter_type = rolling_shutter_type;
   kcam->rolling_shutter_duration = rolling_shutter_duration;
@@ -530,9 +527,10 @@ void Camera::device_update_volume(Device * /*device*/, DeviceScene *dscene, Scen
     return;
   }
 
+  kernel_camera.is_inside_volume = 0;
+
   KernelIntegrator *kintegrator = &dscene->data.integrator;
   if (kintegrator->use_volumes) {
-    KernelCamera *kcam = &dscene->data.cam;
     BoundBox viewplane_boundbox = viewplane_bounds_get();
 
     /* Parallel object update, with grain size to avoid too much threading overhead
@@ -546,17 +544,19 @@ void Camera::device_update_volume(Device * /*device*/, DeviceScene *dscene, Scen
                          viewplane_boundbox.intersects(object->bounds)) {
                        /* TODO(sergey): Consider adding more grained check. */
                        VLOG_INFO << "Detected camera inside volume.";
-                       kcam->is_inside_volume = 1;
+                       kernel_camera.is_inside_volume = 1;
                        parallel_for_cancel();
                        break;
                      }
                    }
                  });
 
-    if (!kcam->is_inside_volume) {
+    if (!kernel_camera.is_inside_volume) {
       VLOG_INFO << "Camera is outside of the volume.";
     }
   }
+
+  dscene->data.cam.is_inside_volume = kernel_camera.is_inside_volume;
 
   need_device_update = false;
   need_flags_update = false;
