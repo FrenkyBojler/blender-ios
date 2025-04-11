89b58920907e596cb308ea056fd25a01d1685210
Nicola <nicdessi@gmail.com>
2025-04-11 08:52:33 +0200

    fix assert

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -807,12 +807,11 @@ static bool brush_uses_topology_rake(const SculptSession &ss, const Brush &brush
  */
 static int sculpt_brush_needs_normal(const SculptSession &ss, const Sculpt &sd, const Brush &brush)
 {
-  /* The normal for the Plane brush is expected to have already been calculated in
-   * #calc_brush_plane. */
-  BLI_assert_msg(!math::is_zero(ss.cache->sculpt_normal),
-                 "Normal should have been previously calculated.");
-
   if (brush.sculpt_brush_type == SCULPT_BRUSH_TYPE_PLANE) {
+    /* The normal for the Plane brush is expected to have already been calculated in
+     * #calc_brush_plane. */
+    BLI_assert_msg(!math::is_zero(ss.cache->sculpt_normal),
+                   "Normal should have been previously calculated.");
     return false;
   }
 
