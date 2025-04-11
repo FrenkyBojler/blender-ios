f345179e0bfb7570b8f92bf6bc3a55fadc7a392b
Harley Acheson <harley.acheson@gmail.com>
2025-04-10 18:29:01 -0700

    UI: Minimum Height for Timeline Marker Operations
    When Timeline (and similar editors) is reduced to a just the height of
    two headers you can still change the play head. Except if you have any
    markers and then marker selection operator interferes. This PR sets a
    minimum area height for marker operations. This minimum differs if you
    hide the header.

--- source/blender/editors/animation/anim_markers.cc
+++ source/blender/editors/animation/anim_markers.cc
@@ -211,6 +211,14 @@ static bool operator_markers_region_active(bContext *C)
     return false;
   }
 
+  /* Minimum vertical size to select markers, while still scrubbing frames. */
+  ARegion *region = BKE_area_find_region_type(area, RGN_TYPE_HEADER);
+  const bool header_hidden = region->flag & RGN_FLAG_HIDDEN;
+  const int min_height = HEADERY * UI_SCALE_FAC * (header_hidden ? 2.4f : 3.4f);
+  if (area->winy < min_height) {
+    return false;
+  }
+
   switch (area->spacetype) {
     case SPACE_ACTION: {
       SpaceAction *saction = static_cast<SpaceAction *>(area->spacedata.first);
