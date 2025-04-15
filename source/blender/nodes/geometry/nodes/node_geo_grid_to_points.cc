1e560a111559a9a6e252316820eb968404e3de1e
Lukas Tönne <lukas@blender.org>
2025-04-15 12:38:32 +0200

    Simplify by only outputting voxels.

--- source/blender/nodes/geometry/nodes/node_geo_grid_to_points.cc
+++ source/blender/nodes/geometry/nodes/node_geo_grid_to_points.cc
@@ -35,9 +35,6 @@ static void node_declare(NodeDeclarationBuilder &b)
   eNodeSocketDatatype data_type = eNodeSocketDatatype(node->custom1);
 
   b.add_input(data_type, "Grid");
-  b.add_input<decl::Bool>("Use Tiles")
-      .default_value(false)
-      .description("Output tiles as single points instead of generating a point for each voxel");
   b.add_input<decl::Bool>("Selection").default_value(true).supports_field().hide_value();
 
   b.add_output<decl::Geometry>("Points").description(
@@ -47,16 +44,6 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Vector>("Coordinate")
       .description("Index-space coordinate of the voxel")
       .field_on_all();
-  b.add_output<decl::Vector>("Bounds Min")
-      .description("Minimum voxel coordinate covered by a node")
-      .field_on_all();
-  b.add_output<decl::Vector>("Bounds Max")
-      .description("Maximum voxel coordinate covered by a node")
-      .field_on_all();
-  b.add_output<decl::Int>("Level")
-      .description("Node level, -1 = leaf voxel, 0 = leaf node, 1,2,.. = other node")
-      .field_on_all();
-
   b.add_output(data_type, "Background").description("Value of the grid in empty regions");
   b.add_output(data_type, "Value")
       .description("Value stored in grid voxels and tiles")
@@ -158,203 +145,175 @@ template<typename LeafNodeType> class GridLeafFieldContext : public FieldContext
   }
 };
 
-template<typename T> struct ThreadPointData { /* Coordinates of the node in the tree. */
-  using type_traits = bke::VolumeGridTraits<T>;
-  using LeafNodeType = typename type_traits::TreeType::LeafNodeType;
-
-  Vector<float3> coords;
-  /* Node coordinate min/max bounds. */
-  Vector<float3> bounds_min;
-  Vector<float3> bounds_max;
-  /* Depth of the node:
-   * 0 = leaf node
-   * 1..depth-1 = intermediate node
-   * depth = root node
-   * -1 = voxel (not an actual node in the tree)
-   */
-  Vector<int> levels;
-  Vector<T> values;
-
-  /* Buffer for evaluating the selection field. */
-  Array<bool> selection_buffer;
-
-  ThreadPointData() : selection_buffer(LeafNodeType::NUM_VOXELS) {}
-};
-
-/* Convenience functions for reserving needed data. */
-template<typename T> void reserve_data(Vector<T> &data, int count)
-{
-  data.reserve(data.size() + count);
-}
-
-template<typename T>
-void reserve_data_if_needed(std::optional<std::string> id, Vector<T> &data, int count)
-{
-  if (id) {
-    data.reserve(data.size() + count);
-  }
-}
-
-template<typename T> void grid_to_points(GeoNodeExecParams params)
-{
+template<typename T> struct GridToPointsConverter {
   using type_traits = bke::VolumeGridTraits<T>;
   using TreeType = typename type_traits::TreeType;
   using GridType = openvdb::Grid<TreeType>;
   using LeafNodeType = typename TreeType::LeafNodeType;
 
-  const auto grid = params.extract_input<bke::VolumeGrid<T>>("Grid");
-  if (!grid) {
-    params.set_default_remaining_outputs();
-    return;
-  }
-  bke::VolumeTreeAccessToken tree_token;
-  const GridType &vdb_grid = grid.grid(tree_token);
-  const float4x4 transform = transform_to_matrix(vdb_grid.transform());
-  // const bool use_tiles = params.extract_input<bool>("Use Tiles");
-  const Field<bool> selection_field = params.extract_input<Field<bool>>("Selection");
+  struct PointData {
+    using type_traits = bke::VolumeGridTraits<T>;
+    using LeafNodeType = typename type_traits::TreeType::LeafNodeType;
 
-  const std::optional<std::string> coord_id = params.get_output_anonymous_attribute_id_if_needed(
-      "Coordinate");
-  const std::optional<std::string> bounds_min_id =
-      params.get_output_anonymous_attribute_id_if_needed("Bounds Min");
-  const std::optional<std::string> bounds_max_id =
-      params.get_output_anonymous_attribute_id_if_needed("Bounds Max");
-  const std::optional<std::string> level_id = params.get_output_anonymous_attribute_id_if_needed(
-      "Level");
-  const std::optional<std::string> value_id = params.get_output_anonymous_attribute_id_if_needed(
-      "Value");
+    Vector<float3> coords;
+    Vector<T> values;
 
-  openvdb::tree::NodeManager<const TreeType> node_manager(vdb_grid.tree());
-  threading::EnumerableThreadSpecific<ThreadPointData<T>> thread_point_data;
+    /* Buffer for evaluating the selection field. */
+    Array<bool> selection_buffer;
 
-  node_manager.foreachTopDown([&](auto &node) {
-    /* Leaf node. */
-    if constexpr (node.LEVEL == 0) {
-      if (node.isEmpty()) {
-        return;
+    PointData() : selection_buffer(LeafNodeType::NUM_VOXELS) {}
+  };
+  using ThreadPointData = threading::EnumerableThreadSpecific<PointData>;
+
+  template<typename NodeType>
+  static void add_leaf_points(const float4x4 &transform,
+                              const openvdb::Coord &leaf_origin,
+                              const Field<bool> &selection_field,
+                              const bool store_value,
+                              const NodeType &node,
+                              PointData &point_data)
+  {
+    constexpr int num_voxels = LeafNodeType::NUM_VOXELS;
+
+    const GridLeafFieldContext<LeafNodeType> field_context(transform, leaf_origin);
+    FieldEvaluator evaluator(field_context, field_context.size());
+    evaluator.add_with_destination(selection_field, point_data.selection_buffer.as_mutable_span());
+    evaluator.evaluate();
+    const VArraySpan<bool> selection = evaluator.get_evaluated<bool>(0);
+
+    /* Always record coordinates even if output is unused, we use these for counting and
+     * positions as well. */
+    point_data.coords.reserve(point_data.coords.size() + num_voxels);
+    if (store_value) {
+      point_data.values.reserve(point_data.values.size() + num_voxels);
+    }
+
+    for (const int index : IndexRange(num_voxels)) {
+      if (!selection[index]) {
+        continue;
       }
 
-      ThreadPointData<T> &point_data = thread_point_data.local();
+      const openvdb::Coord coord = leaf_origin + LeafNodeType::offsetToLocalCoord(index);
+      if (!node.isValueOn(coord)) {
+        continue;
+      }
 
-      /* Always record coordinates even if output is unused, we use these for counting and
-       * positions as well. */
-      reserve_data(point_data.coords, node.NUM_VOXELS);
-      reserve_data_if_needed(bounds_min_id, point_data.bounds_min, node.NUM_VOXELS);
-      reserve_data_if_needed(bounds_max_id, point_data.bounds_max, node.NUM_VOXELS);
-      reserve_data_if_needed(level_id, point_data.levels, node.NUM_VOXELS);
-      reserve_data_if_needed(value_id, point_data.values, node.NUM_VOXELS);
+      const float3 fcoord = {float(coord.x()), float(coord.y()), float(coord.z())};
+      point_data.coords.append(fcoord);
+      if (store_value) {
+        point_data.values.append(type_traits::to_blender(node.getValue(coord)));
+      }
+    }
+  };
 
-      const GridLeafFieldContext<LeafNodeType> field_context(transform, node.origin());
-      FieldEvaluator evaluator(field_context, field_context.size());
-      evaluator.add_with_destination(selection_field,
-                                     point_data.selection_buffer.as_mutable_span());
-      evaluator.evaluate();
+  static void voxels_to_points(const TreeType &vdb_tree,
+                               const float4x4 &transform,
+                               const Field<bool> &selection_field,
+                               ThreadPointData &thread_point_data,
+                               const bool store_value)
+  {
+    openvdb::tree::NodeManager<const TreeType> node_manager(vdb_tree);
 
-      const VArraySpan<bool> selection = evaluator.get_evaluated<bool>(0);
-      auto add_value = [&](const int index,
-                           const openvdb::Coord &coord,
-                           const typename LeafNodeType::ValueType &value) {
-        if (!selection[index]) {
-          return;
-        }
+    node_manager.foreachTopDown([&](auto &node) {
+      PointData &point_data = thread_point_data.local();
 
-        const float3 fcoord = {float(coord.x()), float(coord.y()), float(coord.z())};
-        point_data.coords.append(fcoord);
-        if (bounds_min_id) {
-          point_data.bounds_min.append(fcoord);
-        }
-        if (bounds_max_id) {
-          point_data.bounds_max.append(fcoord);
-        }
-        if (level_id) {
-          /* Indicates a voxel. */
-          point_data.levels.append(-1);
-        }
-        if (value_id) {
-          point_data.values.append(type_traits::to_blender(value));
-        }
-      };
-
-      if (node.isDense()) {
-        for (const int index : IndexRange(LeafNodeType::NUM_VOXELS)) {
-          add_value(index, node.offsetToGlobalCoord(index), node.getValue(index));
-        }
+      if constexpr (node.LEVEL == 0) {
+        add_leaf_points(transform, node.origin(), selection_field, store_value, node, point_data);
       }
       else {
         for (auto iter = node.cbeginValueOn(); iter; ++iter) {
-          add_value(iter.pos(), iter.getCoord(), iter.getValue());
+          const openvdb::Coord &value_origin = iter.getCoord();
+
+          /* Number of leaves in this node. */
+          using NodeType = std::decay_t<decltype(node)>;
+          using ChildNodeType = typename NodeType::ChildNodeType;
+          constexpr int leaves_log2dim = ChildNodeType::TOTAL - LeafNodeType::LOG2DIM;
+          constexpr int leaves_dim = 1 << leaves_log2dim;
+          for (const int leaf_i : IndexRange(leaves_dim)) {
+            for (const int leaf_j : IndexRange(leaves_dim)) {
+              for (const int leaf_k : IndexRange(leaves_dim)) {
+                const openvdb::Coord leaf_offset = {leaf_i * LeafNodeType::DIM,
+                                                    leaf_j * LeafNodeType::DIM,
+                                                    leaf_k * LeafNodeType::DIM};
+                const openvdb::Coord leaf_origin = value_origin + leaf_offset;
+
+                add_leaf_points(
+                    transform, leaf_origin, selection_field, store_value, node, point_data);
+              }
+            }
+          }
         }
       }
-    }
-  });
-
-  int points_num = 0;
-  for (const ThreadPointData<T> &point_data : thread_point_data) {
-    points_num += point_data.coords.size();
-  }
-  PointCloud *points = BKE_pointcloud_new_nomain(points_num);
-  bke::MutableAttributeAccessor attributes = points->attributes_for_write();
-
-  params.set_output("Transform", transform);
-  params.set_output("Depth", int(vdb_grid.tree().treeDepth()));
-  params.set_output<T>("Background", type_traits::to_blender(vdb_grid.background()));
-
-  IndexRange points_range = {};
-  MutableSpan<float3> positions = points->positions_for_write();
-  SpanAttributeWriter<float3> coord_writer;
-  SpanAttributeWriter<float3> bounds_min_writer;
-  SpanAttributeWriter<float3> bounds_max_writer;
-  SpanAttributeWriter<int> level_writer;
-  SpanAttributeWriter<T> value_writer;
-  if (coord_id) {
-    coord_writer = attributes.lookup_or_add_for_write_only_span<float3>(*coord_id,
-                                                                        AttrDomain::Point);
-  }
-  if (bounds_min_id) {
-    bounds_min_writer = attributes.lookup_or_add_for_write_only_span<float3>(*bounds_min_id,
-                                                                             AttrDomain::Point);
-  }
-  if (bounds_max_id) {
-    bounds_max_writer = attributes.lookup_or_add_for_write_only_span<float3>(*bounds_max_id,
-                                                                             AttrDomain::Point);
-  }
-  if (level_id) {
-    level_writer = attributes.lookup_or_add_for_write_only_span<int>(*level_id, AttrDomain::Point);
-  }
-  if (value_id) {
-    value_writer = attributes.lookup_or_add_for_write_only_span<T>(*value_id, AttrDomain::Point);
+    });
   }
 
-  for (const ThreadPointData<T> &point_data : thread_point_data) {
-    points_range = points_range.after(point_data.coords.size());
+  static void grid_to_points(GeoNodeExecParams params)
+  {
+    const auto grid = params.extract_input<bke::VolumeGrid<T>>("Grid");
+    if (!grid) {
+      params.set_default_remaining_outputs();
+      return;
+    }
+    bke::VolumeTreeAccessToken tree_token;
+    const float4x4 transform = transform_to_matrix(grid.grid(tree_token).transform());
+    T background_value = type_traits::to_blender(grid.grid(tree_token).background());
+    typename TreeType::ConstPtr vdb_tree = grid.grid(tree_token).treePtr();
+    const int tree_depth = vdb_tree->treeDepth();
 
-    for (const int i : points_range.index_range()) {
-      positions[points_range[i]] = math::transform_point(transform, point_data.coords[i]);
+    const Field<bool> selection_field = params.extract_input<Field<bool>>("Selection");
+
+    std::optional<std::string> coord_id;
+    std::optional<std::string> value_id;
+
+    ThreadPointData thread_point_data;
+    coord_id = params.get_output_anonymous_attribute_id_if_needed("Coordinate");
+    value_id = params.get_output_anonymous_attribute_id_if_needed("Value");
+
+    voxels_to_points(
+        *vdb_tree, transform, selection_field, thread_point_data, value_id.has_value());
+
+    int points_num = 0;
+    for (const PointData &point_data : thread_point_data) {
+      points_num += point_data.coords.size();
     }
-    if (coord_writer) {
-      coord_writer.span.slice(points_range).copy_from(point_data.coords);
+    PointCloud *points = BKE_pointcloud_new_nomain(points_num);
+    bke::MutableAttributeAccessor attributes = points->attributes_for_write();
+
+    params.set_output("Transform", transform);
+    params.set_output("Depth", tree_depth);
+    params.set_output("Background", background_value);
+
+    IndexRange points_range = {};
+    MutableSpan<float3> positions = points->positions_for_write();
+    SpanAttributeWriter<float3> coord_writer;
+    SpanAttributeWriter<T> value_writer;
+    if (coord_id) {
+      coord_writer = attributes.lookup_or_add_for_write_only_span<float3>(*coord_id,
+                                                                          AttrDomain::Point);
     }
-    if (bounds_min_writer) {
-      bounds_min_writer.span.slice(points_range).copy_from(point_data.bounds_min);
+    if (value_id) {
+      value_writer = attributes.lookup_or_add_for_write_only_span<T>(*value_id, AttrDomain::Point);
     }
-    if (bounds_max_writer) {
-      bounds_max_writer.span.slice(points_range).copy_from(point_data.bounds_max);
+
+    for (const PointData &point_data : thread_point_data) {
+      points_range = points_range.after(point_data.coords.size());
+
+      for (const int i : points_range.index_range()) {
+        positions[points_range[i]] = math::transform_point(transform, point_data.coords[i]);
+      }
+      if (coord_writer) {
+        coord_writer.span.slice(points_range).copy_from(point_data.coords);
+      }
+      if (value_writer) {
+        value_writer.span.slice(points_range).copy_from(point_data.values);
+      }
     }
-    if (level_writer) {
-      level_writer.span.slice(points_range).copy_from(point_data.levels);
-    }
-    if (value_writer) {
-      value_writer.span.slice(points_range).copy_from(point_data.values);
-    }
+    coord_writer.finish();
+    value_writer.finish();
+
+    params.set_output("Points", bke::GeometrySet::from_pointcloud(points));
   }
-  coord_writer.finish();
-  bounds_min_writer.finish();
-  bounds_max_writer.finish();
-  level_writer.finish();
-  value_writer.finish();
-
-  params.set_output("Points", bke::GeometrySet::from_pointcloud(points));
-}
+};
 #endif
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -368,7 +327,7 @@ static void node_geo_exec(GeoNodeExecParams params)
         using type_traits = typename bke::VolumeGridTraits<ValueT>;
 
         if constexpr (!std::is_same_v<typename type_traits::BlenderType, void>) {
-          grid_to_points<ValueT>(params);
+          GridToPointsConverter<ValueT>::grid_to_points(params);
         }
         else {
           params.set_default_remaining_outputs();
