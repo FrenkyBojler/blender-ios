3538f6518e3892acdb5afee1a12aa881bd1cefc6
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-04-15 11:53:06 +0200

    use eAnimFilter_Flags instead of int

--- source/blender/editors/space_graph/graph_query.cc
+++ source/blender/editors/space_graph/graph_query.cc
@@ -10,14 +10,13 @@ namespace blender::ed::graph {
 
 void get_editable_fcurves(bAnimContext &ac, ListBase &r_anim_data)
 {
-  int filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_CURVE_VISIBLE | ANIMFILTER_FCURVESONLY |
-                ANIMFILTER_NODUPLIS);
+  eAnimFilter_Flags filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_CURVE_VISIBLE |
+                              ANIMFILTER_FCURVESONLY | ANIMFILTER_NODUPLIS);
   if (U.animation_flag & USER_ANIM_ONLY_SHOW_SELECTED_CURVE_KEYS) {
     filter |= ANIMFILTER_SEL;
   }
 
-  ANIM_animdata_filter(
-      &ac, &r_anim_data, eAnimFilter_Flags(filter), ac.data, eAnimCont_Types(ac.datatype));
+  ANIM_animdata_filter(&ac, &r_anim_data, filter, ac.data, eAnimCont_Types(ac.datatype));
 }
 
 }  // namespace blender::ed::graph
