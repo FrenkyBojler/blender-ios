9466255fcc737886729d5f8b5f8b62d53193ffe6
Pratik Borhade <pratikborhade302@gmail.com>
2025-04-16 15:54:52 +0530

    Use uiLayoutSetEnabled()

--- source/blender/editors/interface/templates/interface_template_node_inputs.cc
+++ source/blender/editors/interface/templates/interface_template_node_inputs.cc
@@ -174,8 +174,8 @@ void uiTemplateNodeInputs(uiLayout *layout, bContext *C, PointerRNA *ptr)
   }
 
   if (uiLayoutIsEmpty(layout)) {
-    uiBut *but = uiItemL_ex(layout, "Nothing to display.", ICON_NONE, false, false);
-    UI_but_flag_enable(but, UI_BUT_DISABLED);
+    uiLayoutSetEnabled(layout, false);
+    uiItemL(layout, "Nothing to display.", ICON_NONE);
   }
 }
 
