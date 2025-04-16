6e2d5081f119700e6a03ef109776cc6ca04fa6d5
Habib Gahbiche <habib@blender.org>
2025-04-16 16:25:20 +0200

    UI: Improve default position of shader nodes
    After creating a new shading material, the new node tree might become invisible and the user has to scroll down to see the nodes.
    
    The idea of this PR is to create nodes with x-positions centered around zero, such that they are always visible no matter the screen size or workspace setup. Ideally, the nodes' position should depend on the region's zoom and pan. However, the node creation code currently does not depend on `SpaceNode`, so such solution would complicate the code a bit. Also, this heuristic seem to work well enough for most cases.
    
    Note: this only affects newly created materials. The material of the default cube and the default world material still have invisible nodes sometimes. This is because they are saved in the startup file, which will be addressed in a different patch.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/136926

--- source/blender/blenkernel/intern/material.cc
+++ source/blender/blenkernel/intern/material.cc
@@ -2054,10 +2054,10 @@ static void material_default_surface_init(Material *ma)
                               *output,
                               *blender::bke::node_find_socket(*output, SOCK_IN, "Surface"));
 
-  principled->location[0] = 10.0f;
-  principled->location[1] = 300.0f;
-  output->location[0] = 300.0f;
-  output->location[1] = 300.0f;
+  principled->location[0] = -200.0f;
+  principled->location[1] = 100.0f;
+  output->location[0] = 200.0f;
+  output->location[1] = 100.0f;
 
   blender::bke::node_set_active(*ntree, *output);
 }
@@ -2080,10 +2080,10 @@ static void material_default_volume_init(Material *ma)
                               *output,
                               *blender::bke::node_find_socket(*output, SOCK_IN, "Volume"));
 
-  principled->location[0] = 10.0f;
-  principled->location[1] = 300.0f;
-  output->location[0] = 300.0f;
-  output->location[1] = 300.0f;
+  principled->location[0] = -200.0f;
+  principled->location[1] = 100.0f;
+  output->location[0] = 200.0f;
+  output->location[1] = 100.0f;
 
   blender::bke::node_set_active(*ntree, *output);
 }
