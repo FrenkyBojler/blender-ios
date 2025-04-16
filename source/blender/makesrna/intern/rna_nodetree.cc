7bf7327924840e84a5ec4a3d0232417b9abeb6c5
David-Sebald <SebaldDM13@gmail.com>
2025-04-16 15:55:29 -0500

    Geometry Nodes: String to Value node

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -12367,7 +12367,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define("FunctionNode", "FunctionNodeSeparateTransform");
   define("FunctionNode", "FunctionNodeSliceString");
   define("FunctionNode", "FunctionNodeStringLength");
-  define("FunctionNode", "FunctionNodeStringToInt");
+  define("FunctionNode", "FunctionNodeStringToValue");
   define("FunctionNode", "FunctionNodeTransformDirection");
   define("FunctionNode", "FunctionNodeTransformPoint");
   define("FunctionNode", "FunctionNodeTransposeMatrix");
