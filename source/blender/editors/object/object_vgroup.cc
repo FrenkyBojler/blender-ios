2540057a7796a8714a8f43b4b95720b18f6a368c
Zheng-Ze <zheng-ze@noreply.localhost>
2025-04-18 12:37:00 +0200

    更新 source/blender/editors/object/object_vgroup.cc
    Qsort callback inv_cmp_mdef_vert_weights may return false result when input structs have all compared fields equal. This in turn may causes inconsistent order or even crashes in some qsort implementations.
    The location of the error function is source/blender/editors/object/object_vgroup.cc.
    According to the code before modification, when the two input structures are exactly the same, the function returns -1 instead of 0.

--- source/blender/editors/object/object_vgroup.cc
+++ source/blender/editors/object/object_vgroup.cc
@@ -1856,7 +1856,10 @@ static int inv_cmp_mdef_vert_weights(const void *a1, const void *a2)
   if (&dw1 < &dw2) {
     return 1; /* compare address for stable sort algorithm */
   }
-  return -1;
+  if (&dw1 > &dw2) {
+    return -1;
+  }
+return 0;
 }
 
 /* Used for limiting the number of influencing bones per vertex when exporting
