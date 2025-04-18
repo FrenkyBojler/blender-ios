93064c851a82f105e89506fd94754d7160e25b6e
YimingWu <xp8110@outlook.com>
2025-04-18 14:34:23 +0800

    use named variable

--- source/blender/render/intern/engine.cc
+++ source/blender/render/intern/engine.cc
@@ -1108,7 +1108,8 @@ bool RE_engine_render(Render *re, bool do_all)
   /* Perform delayed grease pencil rendering. */
   if (delay_grease_pencil) {
     FOREACH_VIEW_LAYER_TO_RENDER_BEGIN (re, view_layer_iter) {
-      if ((view_layer_iter->layflag & SCE_LAY_GREASE_PENCIL) == 0) {
+      const bool use_grease_pencil = (view_layer_iter->layflag & SCE_LAY_GREASE_PENCIL) != 0;
+      if (!use_grease_pencil) {
         continue;
       }
       engine_render_view_layer(re, engine, view_layer_iter, false, true);
