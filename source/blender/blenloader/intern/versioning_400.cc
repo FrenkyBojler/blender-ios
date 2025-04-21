0912386bc1f493ace3b6ed6fd486f6585d06c3b6
Omar Emara <mail@OmarEmara.dev>
2025-04-21 17:58:49 +0200

    Fix wrong subversion

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -8485,7 +8485,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 42)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 41)) {
     FOREACH_NODETREE_BEGIN (bmain, node_tree, id) {
       if (node_tree->type == NTREE_COMPOSIT) {
         LISTBASE_FOREACH (bNode *, node, &node_tree->nodes) {
