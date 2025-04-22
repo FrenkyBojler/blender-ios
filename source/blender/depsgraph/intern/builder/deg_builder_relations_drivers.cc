55f491f07686cdf91d5a60df6466fcf8c18cc528
Sybren A. Stüvel <sybren@blender.org>
2025-04-22 11:31:12 +0200

    Remove comment

--- source/blender/depsgraph/intern/builder/deg_builder_relations_drivers.cc
+++ source/blender/depsgraph/intern/builder/deg_builder_relations_drivers.cc
@@ -240,15 +240,6 @@ void DepsgraphRelationBuilder::build_driver_relations(IDNode *id_node)
   }
 }
 
-/**
- * Returns whether the data at the given path may be implicitly shared (also see
- * #ImplicitSharingInfo). If it is shared, writing to it through RNA will make a local copy that
- * can be edited without affecting the other users.
- *
- * If multi-threaded writing to the path is required, one should trigger making the mutable copy
- * before multi-threaded writing starts. Otherwise there is a race condition where each thread
- * tries to make its own copy. The "unsharing" can be triggered by doing a dummy-write to it.
- */
 bool data_path_maybe_shared(const ID &id, const StringRef data_path)
 {
   /* As it is hard to generally detect implicit sharing, this is implemented as
