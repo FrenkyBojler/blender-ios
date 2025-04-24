dc6b02c1fd32d6183799e161648ce8377c778278
Nicolas Paris <nicolas.paris490@gmail.com>
2025-04-24 12:07:58 +0200

    Change pow(,2.0f) to blender::math::square()

--- source/blender/blenkernel/intern/ocean_spectrum.cc
+++ source/blender/blenkernel/intern/ocean_spectrum.cc
@@ -7,6 +7,7 @@
  */
 
 #include "BKE_ocean.h"
+#include "BLI_math_base.hh"
 #include "ocean_intern.h"
 
 #include <algorithm>
@@ -46,8 +47,7 @@ static float peak_sharpen(const float omega, const float m_peakomega, const floa
 {
   const float peak_sharpening_sigma = (omega < m_peakomega) ? 0.07 : 0.09;
   const float peak_sharpening = pow(
-      m_gamma, exp(-pow((omega - m_peakomega) / (peak_sharpening_sigma * m_peakomega), 2.0) / 2.0));
-
+      m_gamma, exp(-blender::math::square((omega - m_peakomega) / (peak_sharpening_sigma * m_peakomega)) / 2.0));
   return peak_sharpening;
 }
 
