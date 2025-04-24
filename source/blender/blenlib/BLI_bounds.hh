7f1abcff7e6768c20e715d8f5c2ce2425d2f929f
Nathan Vegdahl <cessen@cessen.com>
2025-04-24 15:25:53 +0200

    Fix #137932: use object space for computing armature bounds

--- source/blender/blenlib/BLI_bounds.hh
+++ source/blender/blenlib/BLI_bounds.hh
@@ -14,6 +14,7 @@
 
 #include "BLI_bounds_types.hh"
 #include "BLI_index_mask.hh"
+#include "BLI_math_matrix.hh"
 #include "BLI_math_vector.hh"
 #include "BLI_task.hh"
 #include "BLI_virtual_array.hh"
@@ -181,6 +182,51 @@ template<typename T> inline std::optional<T> max(const VArray<T> &values)
         return current_max;
       },
       [](const int a, const int b) { return std::max(a, b); });
+}
+
+/**
+ * Transform a 3D bounding box.
+ *
+ * Note: this necessarily grows the bounding box, to ensure that the transformed
+ * bounding box fully contains the original. Therefore, calling this iteratively
+ * to transform from space A to space B, and then from space B to space C, etc.,
+ * will also iteratively grow the bounding box on each call. Try to avoid doing
+ * that, and instead first compose the transform matrices and then use that to
+ * transform the bounding box.
+ */
+template<typename T, int D>
+inline Bounds<VecBase<T, 3>> transform_bounds(const MatBase<T, D, D> &matrix,
+                                              const Bounds<VecBase<T, 3>> &bounds)
+{
+  /* Corners of the bounding box.*/
+  const blender::float3 p0{bounds.min[0], bounds.min[1], bounds.min[2]};
+  const blender::float3 p1{bounds.min[0], bounds.min[1], bounds.max[2]};
+  const blender::float3 p2{bounds.min[0], bounds.max[1], bounds.min[2]};
+  const blender::float3 p3{bounds.min[0], bounds.max[1], bounds.max[2]};
+  const blender::float3 p4{bounds.max[0], bounds.min[1], bounds.min[2]};
+  const blender::float3 p5{bounds.max[0], bounds.min[1], bounds.max[2]};
+  const blender::float3 p6{bounds.max[0], bounds.max[1], bounds.min[2]};
+  const blender::float3 p7{bounds.max[0], bounds.max[1], bounds.max[2]};
+
+  const blender::float3 p0_transformed = math::transform_point(matrix, p0);
+  const blender::float3 p1_transformed = math::transform_point(matrix, p1);
+  const blender::float3 p2_transformed = math::transform_point(matrix, p2);
+  const blender::float3 p3_transformed = math::transform_point(matrix, p3);
+  const blender::float3 p4_transformed = math::transform_point(matrix, p4);
+  const blender::float3 p5_transformed = math::transform_point(matrix, p5);
+  const blender::float3 p6_transformed = math::transform_point(matrix, p6);
+  const blender::float3 p7_transformed = math::transform_point(matrix, p7);
+
+  const blender::float3 new_min = math::min(math::min(math::min(p0_transformed, p1_transformed),
+                                                      math::min(p2_transformed, p3_transformed)),
+                                            math::min(math::min(p4_transformed, p5_transformed),
+                                                      math::min(p6_transformed, p7_transformed)));
+  const blender::float3 new_max = math::max(math::max(math::max(p0_transformed, p1_transformed),
+                                                      math::max(p2_transformed, p3_transformed)),
+                                            math::max(math::max(p4_transformed, p5_transformed),
+                                                      math::max(p6_transformed, p7_transformed)));
+
+  return blender::Bounds<blender::float3>{new_min, new_max};
 }
 
 }  // namespace bounds
