076aae5a242804e91da25639d3596872f7e9e134
Sybren A. Stüvel <sybren@blender.org>
2025-04-25 17:36:46 +0200

    Move comment

--- source/blender/depsgraph/intern/node/deg_node_operation.hh
+++ source/blender/depsgraph/intern/node/deg_node_operation.hh
@@ -49,7 +49,8 @@ enum class OperationCode {
   ANIMATION_EXIT,
   /* Driver */
   DRIVER,
-  DRIVER_UNSHARE, /* Writes to RNA properties to ensure implicitly-shared data is un-shared. */
+  /* Writes to RNA properties to ensure implicitly-shared data is un-shared. */
+  DRIVER_UNSHARE,
 
   /* Scene related. ------------------------------------------------------- */
   SCENE_EVAL,
