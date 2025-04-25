e4d9cc730948eff11509960e5f3091bbeb9234ee
Nicola <nicdessi@gmail.com>
2025-04-25 16:55:23 +0200

    fix build error

--- source/blender/editors/sculpt_paint/brushes/scene_project.cc
+++ source/blender/editors/sculpt_paint/brushes/scene_project.cc
@@ -38,6 +38,7 @@
 #include "BKE_subdiv_ccg.hh"
 
 #include "BLI_enumerable_thread_specific.hh"
+#include "BLI_kdopbvh.hh"
 #include "BLI_math_matrix.hh"
 #include "BLI_math_vector.hh"
 #include "BLI_task.hh"
