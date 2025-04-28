bfbca53eef0da3298f189272d53b68db87b5e2a1
Brecht Van Lommel <brecht@blender.org>
2025-04-28 18:07:17 +0200

    Fix more potential issues

--- source/blender/makesrna/intern/rna_access.cc
+++ source/blender/makesrna/intern/rna_access.cc
@@ -1712,7 +1712,7 @@ static void property_enum_translate(PropertyRNA *prop,
                                     const int *totitem,
                                     bool *r_free)
 {
-  if (!(prop->flag & PROP_ENUM_NO_TRANSLATE)) {
+  if (!(RNA_property_flag(prop) & PROP_ENUM_NO_TRANSLATE)) {
     int i;
 
     /* NOTE: Only do those tests once, and then use BLT_pgettext. */
@@ -1747,9 +1747,11 @@ static void property_enum_translate(PropertyRNA *prop,
       *r_free = true;
     }
 
+    const char *translation_context = RNA_property_translation_context(prop);
+
     for (i = 0; nitem[i].identifier; i++) {
       if (nitem[i].name && do_iface) {
-        nitem[i].name = BLT_pgettext(RNA_property_translation_context(prop), nitem[i].name);
+        nitem[i].name = BLT_pgettext(translation_context, nitem[i].name);
       }
       if (nitem[i].description && do_tooltip) {
         nitem[i].description = BLT_pgettext(nullptr, nitem[i].description);
@@ -1797,7 +1799,7 @@ void RNA_property_enum_items_gettexted_all(bContext *C,
   }
 
   if (eprop->item_fn != nullptr) {
-    const bool no_context = (prop->flag & PROP_ENUM_NO_CONTEXT) ||
+    const bool no_context = (eprop->property.flag & PROP_ENUM_NO_CONTEXT) ||
                             ((ptr->type->flag & STRUCT_NO_CONTEXT_WITHOUT_OWNER_ID) &&
                              (ptr->owner_id == nullptr));
     if (C != nullptr || no_context) {
@@ -2059,7 +2061,7 @@ bool RNA_property_enum_item_from_value_gettexted(
 {
   const bool result = RNA_property_enum_item_from_value(C, ptr, prop, value, r_item);
 
-  if (result && !(prop->flag & PROP_ENUM_NO_TRANSLATE)) {
+  if (result && !(RNA_property_flag(prop) & PROP_ENUM_NO_TRANSLATE)) {
     r_item->name = BLT_translate_do_iface(RNA_property_translation_context(prop), r_item->name);
     r_item->description = BLT_translate_do_tooltip(nullptr, r_item->description);
   }
