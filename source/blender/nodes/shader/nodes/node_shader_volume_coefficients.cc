7bd6094f13e6af7f959e40c75205bb7d55064f3b
Alexandre Cardaillac <cardaillac.alexandre@gmail.com>
2025-04-28 21:52:07 +1000

    Cycles: Fix panel and order of volume coefficients shader

--- source/blender/nodes/shader/nodes/node_shader_volume_coefficients.cc
+++ source/blender/nodes/shader/nodes/node_shader_volume_coefficients.cc
@@ -13,15 +13,22 @@ namespace blender::nodes::node_shader_volume_coefficients_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
+  b.use_custom_socket_order();
+
+  b.add_output<decl::Shader>("Volume").translation_context(BLT_I18NCONTEXT_ID_ID);
+
+  b.add_input<decl::Float>("Weight").available(false);
+#define SOCK_WEIGHT_ID 0
+
   PanelDeclarationBuilder &abs = b.add_panel("Absorption").default_closed(false);
-  abs.add_input<decl::Vector>("Absorption Coefficientsssss")
+  abs.add_input<decl::Vector>("Absorption Coefficients")
       .default_value({1.0f, 1.0f, 1.0f})
       .min(0.0f)
       .max(1000.0f)
       .description(
           "Probability density per color channel that light is absorbed per unit distance "
           "traveled in the medium");
-#define SOCK_ABS_COEFFICIENTS_ID 0
+#define SOCK_ABSORPTION_COEFFICIENTS_ID 1
   PanelDeclarationBuilder &sca = b.add_panel("Scatter").default_closed(false);
   sca.add_layout([](uiLayout *layout, bContext * /*C*/, PointerRNA *ptr) {
     uiItemR(layout, ptr, "phase", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
@@ -33,7 +40,7 @@ static void node_declare(NodeDeclarationBuilder &b)
       .description(
           "Probability density per color channel of an out-scattering event occurring per unit "
           "distance");
-#define SOCK_SCT_COEFFICIENTS_ID 1
+#define SOCK_SCATTER_COEFFICIENTS_ID 2
   sca.add_input<decl::Float>("Anisotropy")
       .default_value(0.0f)
       .min(-1.0f)
@@ -42,32 +49,36 @@ static void node_declare(NodeDeclarationBuilder &b)
       .description(
           "Directionality of the scattering. Zero is isotropic, negative is backward, "
           "positive is forward");
+#define SOCK_SCATTER_ANISOTROPY_ID 3
   sca.add_input<decl::Float>("IOR")
       .default_value(1.33f)
       .min(1.0f)
       .max(2.0f)
       .subtype(PROP_FACTOR)
       .description("Index Of Refraction of the scattering particles");
+#define SOCK_SCATTER_IOR_ID 4
   sca.add_input<decl::Float>("Backscatter")
       .default_value(0.1f)
       .min(0.0f)
       .max(0.5f)
       .subtype(PROP_FACTOR)
       .description("Fraction of light that is scattered backwards");
+#define SOCK_SCATTER_BACKSCATTER_ID 5
   sca.add_input<decl::Float>("Alpha").default_value(0.5f).min(0.0f).max(500.0f);
+#define SOCK_SCATTER_ALPHA_ID 6
   sca.add_input<decl::Float>("Diameter")
       .default_value(20.0f)
       .min(0.0f)
       .max(50.0f)
       .description("Diameter of the water droplets, in micrometers");
+#define SOCK_SCATTER_DIAMETER_ID 7
   PanelDeclarationBuilder &emi = b.add_panel("Emission").default_closed(false);
   emi.add_input<decl::Vector>("Emission Coefficients")
       .default_value({0.0f, 0.0f, 0.0f})
       .min(0.0f)
       .max(1000.0f)
       .description("Emitted radiance per color channel that is added to a ray per unit distance");
-  b.add_input<decl::Float>("Weight").available(false);
-  b.add_output<decl::Shader>("Volume").translation_context(BLT_I18NCONTEXT_ID_ID);
+#define SOCK_EMISSION_COEFFICIENTS_ID 8
 }
 
 static void node_shader_init_coefficients(bNodeTree * /*ntree*/, bNode *node)
@@ -103,19 +114,24 @@ static int node_shader_gpu_volume_coefficients(GPUMaterial *mat,
                                                GPUNodeStack *in,
                                                GPUNodeStack *out)
 {
-  if (node_socket_not_black(in[SOCK_SCT_COEFFICIENTS_ID])) {
+  if (node_socket_not_black(in[SOCK_SCATTER_COEFFICIENTS_ID])) {
     GPU_material_flag_set(mat, GPU_MATFLAG_VOLUME_SCATTER | GPU_MATFLAG_VOLUME_ABSORPTION);
   }
-  if ((node_socket_not_black(in[SOCK_ABS_COEFFICIENTS_ID]) ||
-       node_socket_not_black(in[SOCK_ABS_COEFFICIENTS_ID])))
-  {
+  if (node_socket_not_black(in[SOCK_ABSORPTION_COEFFICIENTS_ID])) {
     GPU_material_flag_set(mat, GPU_MATFLAG_VOLUME_ABSORPTION);
   }
   return GPU_stack_link(mat, node, "node_volume_coefficients", in, out);
 }
 
-#undef SOCK_ABS_COEFFICIENTS_ID
-#undef SOCK_SCT_COEFFICIENTS_ID
+#undef SOCK_WEIGHT_ID
+#undef SOCK_ABSORPTION_COEFFICIENTS_ID
+#undef SOCK_SCATTER_COEFFICIENTS_ID
+#undef SOCK_SCATTER_ANISOTROPY_ID
+#undef SOCK_SCATTER_IOR_ID
+#undef SOCK_SCATTER_BACKSCATTER_ID
+#undef SOCK_SCATTER_ALPHA_ID
+#undef SOCK_SCATTER_DIAMETER_ID
+#undef SOCK_EMISSION_COEFFICIENTS_ID
 
 }  // namespace blender::nodes::node_shader_volume_coefficients_cc
 
