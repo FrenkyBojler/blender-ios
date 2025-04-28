029a827836438d848add905b8dfeea54922b5583
Alexandre Cardaillac <cardaillac.alexandre@gmail.com>
2025-04-29 09:39:15 +1000

    Cycles: Fix format

--- source/blender/gpu/shaders/material/gpu_shader_material_volume_coefficients.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_volume_coefficients.glsl
@@ -13,8 +13,7 @@ void node_volume_coefficients(float weight,
                               float Alpha,
                               float Diameter,
                               float3 EmissionCoefficients,
-                              out Closure result
-                              )
+                              out Closure result)
 {
   ClosureVolumeScatter volume_scatter_data;
   volume_scatter_data.weight = weight;
