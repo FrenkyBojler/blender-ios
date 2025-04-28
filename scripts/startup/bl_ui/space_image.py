1f166912d58f05e79e09d42022c6c97058818780
Pratik Borhade <pratikborhade302@gmail.com>
2025-04-28 13:16:14 +0530

    Fix in context menu and return back to EXEC_REGION_WIN

--- scripts/startup/bl_ui/space_image.py
+++ scripts/startup/bl_ui/space_image.py
@@ -471,6 +471,7 @@ class IMAGE_MT_uvs(Menu):
         layout.operator("uv.minimize_stretch")
         layout.operator_context = 'INVOKE_REGION_WIN'
         layout.operator("uv.stitch")
+        layout.operator_context = 'EXEC_REGION_WIN'
         layout.menu("IMAGE_MT_uvs_align")
         layout.operator("uv.align_rotation")
 
@@ -589,7 +590,9 @@ class IMAGE_MT_uvs_context_menu(Menu):
 
             # Remove
             layout.menu("IMAGE_MT_uvs_merge")
+            layout.operator_context = 'INVOKE_REGION_WIN'
             layout.operator("uv.stitch")
+            layout.operator_context = 'EXEC_REGION_WIN'
             layout.menu("IMAGE_MT_uvs_split")
 
 
