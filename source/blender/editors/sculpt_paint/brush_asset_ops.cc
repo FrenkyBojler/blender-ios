64cee284a8b344cc91e25b3f995b5c854f44b6a7
Sean Kim <SeanCTKim@protonmail.com>
2025-04-30 17:40:37 -0700

    Assets: Enable asset metadata operators for brush shelf context menu
    Currently, depending on whether or not a brush asset is part of a
    library, the context menu shown for it changes significantly.
    
    To better support local brush asset modifications, and make the UI more
    consistent, this commit exposes the following operators:
    
    * `brush.asset_edit_metadata`
    * `brush.asset_load_preview`
    * `brush.asset_save`
    * `brush.asset_revert`
    
    Of these four operators, the first two are allowed to execute, whereas
    the latter two are always disabled on local assets via the corresponding
    `poll` methods, as neither is applicable for this usecase.
    
    Related to #138105

--- source/blender/editors/sculpt_paint/brush_asset_ops.cc
+++ source/blender/editors/sculpt_paint/brush_asset_ops.cc
@@ -433,7 +433,7 @@ static bool brush_asset_edit_metadata_poll(bContext *C)
     CTX_wm_operator_poll_msg_set(C, "Asset library is not editable");
     return false;
   }
-  if (!bke::asset_edit_id_is_writable(brush->id)) {
+  if (!(library_ref->type & ASSET_LIBRARY_LOCAL) && !bke::asset_edit_id_is_writable(brush->id)) {
     CTX_wm_operator_poll_msg_set(C, "Asset file is not editable");
     return false;
   }
@@ -601,16 +601,40 @@ static bool brush_asset_save_poll(bContext *C)
     return false;
   }
 
-  if (!bke::asset_edit_id_is_editable(brush->id)) {
+  if (!ID_IS_ASSET(&brush->id)) {
+    BLI_assert_unreachable();
     return false;
   }
 
-  if (!(paint->brush_asset_reference && ID_IS_ASSET(brush))) {
+  const AssetWeakReference *brush_weak_ref = paint->brush_asset_reference;
+  if (!brush_weak_ref) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  const asset_system::AssetRepresentation *asset = asset::find_asset_from_weak_ref(
+      *C, *brush_weak_ref, CTX_wm_reports(C));
+  if (!asset) {
+    /* May happen if library loading hasn't finished. */
+    return false;
+  }
+  const std::optional<AssetLibraryReference> library_ref =
+      asset->owner_asset_library().library_reference();
+  if (!library_ref) {
+    BLI_assert_unreachable();
+    return false;
+  }
+
+  if ((library_ref->type == ASSET_LIBRARY_LOCAL)) {
+    CTX_wm_operator_poll_msg_set(C, "Local assets cannot be individually saved");
     return false;
   }
 
   if (!bke::asset_edit_id_is_writable(brush->id)) {
     CTX_wm_operator_poll_msg_set(C, "Asset blend file is not editable");
+    return false;
+  }
+
+  if (!bke::asset_edit_id_is_editable(brush->id)) {
     return false;
   }
 
@@ -660,7 +684,32 @@ static bool brush_asset_revert_poll(bContext *C)
     return false;
   }
 
-  return paint->brush_asset_reference && bke::asset_edit_id_is_editable(brush->id);
+  if (!ID_IS_ASSET(&brush->id)) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  const AssetWeakReference *brush_weak_ref = paint->brush_asset_reference;
+  if (!brush_weak_ref) {
+    return false;
+  }
+  const asset_system::AssetRepresentation *asset = asset::find_asset_from_weak_ref(
+      *C, *brush_weak_ref, CTX_wm_reports(C));
+  if (!asset) {
+    /* May happen if library loading hasn't finished. */
+    return false;
+  }
+  const std::optional<AssetLibraryReference> library_ref =
+      asset->owner_asset_library().library_reference();
+  if (!library_ref) {
+    BLI_assert_unreachable();
+    return false;
+  }
+  if ((library_ref->type == ASSET_LIBRARY_LOCAL)) {
+    CTX_wm_operator_poll_msg_set(C, "Local assets cannot be reverted");
+    return false;
+  }
+
+  return bke::asset_edit_id_is_editable(brush->id);
 }
 
 static wmOperatorStatus brush_asset_revert_exec(bContext *C, wmOperator *op)
