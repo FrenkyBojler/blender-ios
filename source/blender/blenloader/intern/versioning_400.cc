b0195df46ef3aba03e82ba5509b20e2eed4899f7
Sergey Sharybin <sergey@blender.org>
2025-05-02 09:49:09 +0200

    Sequencer: Support HDR in Sequencer Preview
    The title is pretty self-explanatory: this change brings support of
    displaying HDR content in the sequencer preview. Before this change
    it was clamped to the 0..1 range, now it is unclamped sRGB, similar
    to how image editor, viewport, and nodes backdrop works.
    
    The general idea is to draw the sequencer content on a non-overlay
    frame-buffer and tag viewport as having non-standard input color
    space as the sequencer operates in a different space.
    
    The way it is done mimics what happens from the draw manager side
    for the nodes backdrop, but bypassing the image engine. Partially
    because the image engine expects the Image data-block to be displayed,
    but also because of performance: there are a lot of things going on
    like float buffer creation, clamping etc. Overall the image engine is
    not fast enough for the sequencer needs.
    
    Code-side changes that worth mentioning to highlight the overall
    direction for the possible future refactors in the area:
    
    - Decouple arguments from the scene: editing, render data, color
      management etc are now passed as individual arguments.
    
      This is an anticipation of story tools project where this data
      might be coming from a different place.
    
    - Move the entire preview region drawing to sequencer_preview_draw.cc
    
      Previously logic was split across sequencer_preview_draw.cc and
      space_sequencer.cc which was quite tricky to know what should go
      where.
    
    - Split functions which had boolean argument to define their behavior
      into individual functions.
    
      Generally if a function has boolean argument used in a way
    
        if(foo) { do_something_(); } else { do_something_else() }
    
      it is a good indication that the function is to be split.

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -10550,6 +10550,20 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 64)) {
+    /* Clear unused darw flag (used to be SEQ_DRAW_BACKDROP). */
+    LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
+      LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+        LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
+          if (sl->spacetype == SPACE_SEQ) {
+            SpaceSeq *space_sequencer = (SpaceSeq *)sl;
+            space_sequencer->draw_flag &= ~SEQ_DRAW_UNUSED_0;
+          }
+        }
+      }
+    }
+  }
+
   /* Always run this versioning (keep at the bottom of the function). Meshes are written with the
    * legacy format which always needs to be converted to the new format on file load. To be moved
    * to a subversion check in 5.0. */
@@ -10557,19 +10571,6 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
     blender::bke::mesh_sculpt_mask_to_generic(*mesh);
     blender::bke::mesh_custom_normals_to_generic(*mesh);
     rename_mesh_uv_seam_attribute(*mesh);
-  }
-
-  /* Clear unused darw flag (used to be SEQ_DRAW_BACKDROP). */
-  // TODO: Move into a dedicated subversion check.
-  LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
-    LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
-      LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
-        if (sl->spacetype == SPACE_SEQ) {
-          SpaceSeq *space_sequencer = (SpaceSeq *)sl;
-          space_sequencer->draw_flag &= ~SEQ_DRAW_UNUSED_0;
-        }
-      }
-    }
   }
 
   /**
