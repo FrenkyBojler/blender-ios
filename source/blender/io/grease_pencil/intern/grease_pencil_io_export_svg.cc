6d7b470d034e3da929eb7b02403b2902fa5b86a4
Leon Schittek <leon.schittek@gmx.net>
2025-05-03 09:28:31 +0200

    Fix error after merge.

--- source/blender/io/grease_pencil/intern/grease_pencil_io_export_svg.cc
+++ source/blender/io/grease_pencil/intern/grease_pencil_io_export_svg.cc
@@ -164,7 +164,7 @@ bool SVGExporter::export_scene(Scene &scene, StringRefNull filepath)
 
       IndexMaskMemory memory;
       if (selection_only) {
-        const Object &ob_eval = *DEG_get_evaluated_object(context_.depsgraph, params_.object);
+        const Object &ob_eval = *DEG_get_evaluated(context_.depsgraph, params_.object);
         const GreasePencil &grease_pencil = *static_cast<GreasePencil *>(ob_eval.data);
         frames = IndexMask::from_predicate(
             frames, GrainSize(1024), memory, [&](const int frame_number) {
