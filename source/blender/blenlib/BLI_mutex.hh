b9d38e805210f09ea3a521a5a2d4bc3a9c5d39a2
Jacques Lucke <jacques@blender.org>
2025-05-04 10:45:54 +0200

    improve comment

--- source/blender/blenlib/BLI_mutex.hh
+++ source/blender/blenlib/BLI_mutex.hh
@@ -16,16 +16,32 @@
 namespace blender {
 
 #ifdef WITH_TBB
+
 /**
- * This mutex has a size of just 1 byte (compared to e.g. the 40 bytes of std::mutex when using
- * GCC). It also has good performance characteristics in the non-contended and contended case. It's
- * not a fair mutex though, so if the mutex is always contended, it is possible that some thread
- * will never lock the mutex.
+ * This mutex has pretty much the same interface as std::mutex which some limitations. Therefore,
+ * it can be used with std::lock_guard and the like. It should be used by default in Blender unless
+ * there are some special needs that really require a different mutex. For example, if the mutex
+ * needs to be used with std::condition_variable, then std::mutex has to be used.
+ *
+ * The mutex provided by TBB has these properties:
+ * - It's only 1 byte large, compared to e.g. 40 bytes when using the std::mutex of GCC.
+ * - It's as fast as a spin-lock in the non-contended case, i.e. when no other thread is trying to
+ * lock the mutex at the same time.
+ * - In the contended case, it spins a couple of times but then blocks to avoid draining system
+ *   resources by spinning for a long time.
+ * - It is *not* a fair mutex, i.e. it's not guaranteed that a thread will ever be able to lock the
+ *   mutex when there are always more than one threads that try to lock it. In the majority of
+ *   cases, using a fair mutex just causes extra overhead without any benefit. std::mutex is not
+ *   guaranteed to be fair either.
  */
 using Mutex = tbb::mutex;
 static_assert(sizeof(Mutex) == 1);
+
 #else
+
+/** Use std::mutex as fallback when compiling without TBB. */
 using Mutex = std::mutex;
+
 #endif
 
 }  // namespace blender
