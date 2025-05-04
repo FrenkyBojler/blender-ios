3cc8ca10ed72fecad799beba5f772dfbc77666ad
Harley Acheson <harley.acheson@gmail.com>
2025-05-04 11:09:21 -0700

    UI: Increase Gizmo 3D Part Hit Size For Pens
    Gizmos that contain 3D parts, like the rounded lines of the "Rotate"
    gizmo, have a fairly small hit size that make it hard to grab with a
    tablet pen. This PR just increases the radius around the pointer
    position while using a pen. This another attempt after #136847 was
    reverted.

--- source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
+++ source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
@@ -660,6 +660,7 @@ static int gizmo_find_intersected_3d_intern(wmGizmo **visible_gizmos,
  */
 static wmGizmo *gizmo_find_intersected_3d(bContext *C,
                                           const int co[2],
+                                          const wmEvent *event,
                                           wmGizmo **visible_gizmos,
                                           const int visible_gizmos_len,
                                           int *r_part)
@@ -717,19 +718,23 @@ static wmGizmo *gizmo_find_intersected_3d(bContext *C,
     GPU_framebuffer_bind(depth_read_fb);
 
     /* NOTE(@ideasman42): Regarding the hit-radius:
-     *
-     * - These must remain constant for all event types
-     *   since changing the radius per event types means non-motion events
-     *   can cause the gizmo not to be highlighted.
      * - A single large radius would result in gizmos that are further away from the cursor
      *   with a nearer Z-depth being highlighted.
      *   So only use the larger radius when the first (smaller) pass has no hits.
      * - As this runs on cursor-motion, avoid doing too many tests (currently 2x).
      */
+
+    /* Wider test area for tablet pens. Also include modifiers otherwise there
+     * is a difference in what is highlighted while using a pen and then pressing
+     * a modifier while between the first and second pass thresholds. */
+    const bool inc_precision =
+        event->tablet.active != EVT_TABLET_NONE || event->modifier != 0 ||
+        ELEM(event->type, EVT_LEFTSHIFTKEY, EVT_RIGHTSHIFTKEY, EVT_RIGHTCTRLKEY, EVT_LEFTCTRLKEY);
+
     const int hotspot_radii[] = {
         /* Use a small value so it's possible to accurately pick a gizmo
          * when multiple are overlapping. */
-        int(3.0f * UI_SCALE_FAC),
+        int((inc_precision ? 6.0f : 3.0f) * UI_SCALE_FAC),
         /* Use a larger value as a fallback so wire gizmos aren't difficult to click on.
          * This value has been selected for both mouse & tablet motion, see !136847. */
         int(12.0f * UI_SCALE_FAC),
@@ -824,6 +829,7 @@ wmGizmo *wm_gizmomap_highlight_find(wmGizmoMap *gzmap,
     if (gz == nullptr) {
       gz = gizmo_find_intersected_3d(C,
                                      mval,
+                                     event,
                                      static_cast<wmGizmo **>(visible_3d_gizmos.data),
                                      visible_3d_gizmos.count,
                                      r_part);
