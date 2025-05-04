188b272720a12d91bf5c94b92953fe0995d9635c
Jacques Lucke <jacques@blender.org>
2025-05-04 06:59:04 +0200

    initial support for expanded menu socket

--- source/blender/nodes/NOD_socket_declarations.hh
+++ source/blender/nodes/NOD_socket_declarations.hh
@@ -222,6 +222,7 @@ class Menu : public SocketDeclaration {
   static constexpr eNodeSocketDatatype static_socket_type = SOCK_MENU;
 
   int32_t default_value;
+  bool is_expanded = false;
 
   friend MenuBuilder;
 
@@ -236,6 +237,8 @@ class Menu : public SocketDeclaration {
 class MenuBuilder : public SocketDeclarationBuilder<Menu> {
  public:
   MenuBuilder &default_value(int32_t value);
+
+  MenuBuilder &expanded(bool value = true);
 };
 
 class BundleBuilder;
@@ -558,6 +561,12 @@ inline StringBuilder &StringBuilder::subtype(PropertySubType subtype)
 inline MenuBuilder &MenuBuilder::default_value(const int32_t value)
 {
   decl_->default_value = value;
+  return *this;
+}
+
+inline MenuBuilder &MenuBuilder::expanded(const bool value)
+{
+  decl_->is_expanded = value;
   return *this;
 }
 
--- source/blender/nodes/intern/node_common.cc
+++ source/blender/nodes/intern/node_common.cc
@@ -325,7 +325,9 @@ static BaseSocketDeclarationBuilder &build_interface_socket_declaration(
       }
       case SOCK_MENU: {
         const auto &value = node_interface::get_socket_data_as<bNodeSocketValueMenu>(io_socket);
-        decl = &b.add_socket<decl::Menu>(name, identifier, in_out).default_value(value.value);
+        decl = &b.add_socket<decl::Menu>(name, identifier, in_out)
+                    .default_value(value.value)
+                    .expanded();
         break;
       }
       case SOCK_OBJECT: {
