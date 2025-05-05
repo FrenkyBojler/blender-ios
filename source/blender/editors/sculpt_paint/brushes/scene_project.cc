07954dc0282d1a6d97471dbf3aed0c219891f813
Nicola <nicdessi@gmail.com>
2025-05-05 04:37:45 +0200

    use const in absolute_min_distance

--- source/blender/editors/sculpt_paint/brushes/scene_project.cc
+++ source/blender/editors/sculpt_paint/brushes/scene_project.cc
@@ -63,7 +63,7 @@ struct LocalData {
   Vector<float3> translations;
 };
 
-static inline float absolute_min_distance(float d1, float d2)
+static inline float absolute_min_distance(const float d1, const float d2)
 {
   return math::abs(d1) < math::abs(d2) ? d1 : d2;
 }
