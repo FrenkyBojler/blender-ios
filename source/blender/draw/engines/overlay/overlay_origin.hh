9cc408c3ab20e11e84f6bb2d774167c80cc1baf4
sentharn <sentharn@gmail.com>
2025-05-05 16:44:29 -0700

    Fix #138301: Rename GLSL "select" for Intel GPUs
    The GLSL processor appears to dislike a member named "select"
    when a function named "select" exists. The member has been
    renamed to object_select to avoid collisions.

--- source/blender/draw/engines/overlay/overlay_origin.hh
+++ source/blender/draw/engines/overlay/overlay_origin.hh
@@ -63,7 +63,7 @@ class Origins : Overlay {
     else if (ob->base_flag & BASE_SELECTED) {
       select_buf_.select_append(res.select_id(ob_ref));
       point_buf_.append(VertexData{
-          location, is_library ? res.theme.colors.library_select : res.theme.colors.select});
+          location, is_library ? res.theme.colors.library_select : res.theme.colors.object_select});
     }
     else if (state.v3d_flag & V3D_DRAW_CENTERS) {
       select_buf_.select_append(res.select_id(ob_ref));
