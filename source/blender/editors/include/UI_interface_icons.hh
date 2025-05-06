284ed37fd5b0b23ce339476704beb5271ae67611
Harley Acheson <harley.acheson@gmail.com>
2025-05-06 12:57:09 -0700

    Need to change ALL the references to this define.

--- source/blender/editors/include/UI_interface_icons.hh
+++ source/blender/editors/include/UI_interface_icons.hh
@@ -38,7 +38,7 @@ struct IconTextOverlay {
 
 #define PREVIEW_DEFAULT_HEIGHT 128
 
-#define PREVIEW_DRAG_SIZE 96.0f
+#define PREVIEW_DRAG_DRAW_SIZE 96.0f
 
 enum eAlertIcon {
   ALERT_ICON_NONE = -1,
