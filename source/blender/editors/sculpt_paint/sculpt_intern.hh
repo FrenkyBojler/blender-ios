2c41d03dfa182a82e1d76468d5dae3a78921ea32
Pratik Borhade <pratikborhade302@gmail.com>
2025-05-07 15:10:53 +0530

    Cleanup: Move eculpt extract operators to sculpt module
    Follow up on !133852

--- source/blender/editors/sculpt_paint/sculpt_intern.hh
+++ source/blender/editors/sculpt_paint/sculpt_intern.hh
@@ -939,6 +939,9 @@ void SCULPT_OT_face_set_polyline_gesture(wmOperatorType *ot);
 namespace blender::ed::sculpt_paint {
 
 void SCULPT_OT_set_pivot_position(wmOperatorType *ot);
+void SCULPT_OT_paint_mask_extract(wmOperatorType *ot);
+void SCULPT_OT_face_set_extract(wmOperatorType *ot);
+void SCULPT_OT_paint_mask_slice(wmOperatorType *ot);
 
 }
 
