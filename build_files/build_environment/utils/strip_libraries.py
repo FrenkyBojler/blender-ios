d73d5d465a0ec9682129171979d02f3156e50f88
Sebastian Parborg <sebastian@blender.org>
2025-05-07 15:19:30 +0200

    Don't strip symlinks and fixup argparse variable names

--- build_files/build_environment/utils/strip_libraries.py
+++ build_files/build_environment/utils/strip_libraries.py
@@ -30,13 +30,24 @@ def strip_libs(strip_dir):
     os.chdir(strip_dir)
     prev_print_len = 0;
     for shared_lib in glob.iglob("**/*.so*", recursive=True):
-        suffix = Path(shared_lib).suffix
-        if suffix == ".py":
+        shared_path = Path(shared_lib)
+        if shared_path.suffix == ".py":
             # Work around badly named sycl scripts
             continue
+
+        if shared_path.is_symlink():
+            # Don't strip symlinks
+            continue
+
         prev_print_len = print_strip_lib(shared_lib, prev_print_len)
         subprocess.check_call(["strip", "-s", "--enable-deterministic-archives", shared_lib])
     for static_lib in glob.iglob("**/*.a", recursive=True):
+        static_path = Path(static_lib)
+
+        if static_path.is_symlink():
+            # Don't strip symlinks
+            continue
+
         prev_print_len = print_strip_lib(static_lib, prev_print_len)
         subprocess.check_call(["objcopy", "--enable-deterministic-archives", static_lib])
 
@@ -48,7 +59,7 @@ def main():
         description=__doc__,
         formatter_class=argparse.RawTextHelpFormatter,
     )
-    parser.add_argument("bpy_dir", type=Path, help="Path to the bpy directory to cleanup")
+    parser.add_argument("directory", type=Path, help="Path to the library directory to strip")
     args = parser.parse_args()
 
     if sys.platform == "linux":
