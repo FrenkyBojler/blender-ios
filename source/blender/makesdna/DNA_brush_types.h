46bbdf2b60bfd653f55354fb13d7f9c20e220303
Sean Kim <SeanCTKim@protonmail.com>
2025-05-08 12:35:13 -0700

    Revert "Cleanup: Remove unused Brush `toggle_brush` field"
    This reverts commit c89baaf768cab4dbb52a20969062325a08574d90.

--- source/blender/makesdna/DNA_brush_types.h
+++ source/blender/makesdna/DNA_brush_types.h
@@ -174,6 +174,8 @@ typedef struct Brush {
   struct MTex mtex;
   struct MTex mask_mtex;
 
+  struct Brush *toggle_brush;
+
   struct ImBuf *icon_imbuf;
   PreviewImage *preview;
   /** Color gradient. */
