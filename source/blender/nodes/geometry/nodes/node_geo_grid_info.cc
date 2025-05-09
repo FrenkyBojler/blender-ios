eda4b28b1f8fe58f7cc0112d63dfde54afbdf241
Lukas Tönne <lukas@blender.org>
2025-05-09 08:42:20 +0200

    Hide grid input value.

--- source/blender/nodes/geometry/nodes/node_geo_grid_info.cc
+++ source/blender/nodes/geometry/nodes/node_geo_grid_info.cc
@@ -29,7 +29,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 
   eNodeSocketDatatype data_type = eNodeSocketDatatype(node->custom1);
 
-  b.add_input(data_type, "Grid");
+  b.add_input(data_type, "Grid").hide_value();
 
   b.add_output<decl::Matrix>("Transform")
       .description("Transform from grid index space to object space");
