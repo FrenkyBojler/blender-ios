f108345b05a308efa8622f444e539367850e9cda
Jacques Lucke <jacques@blender.org>
2025-05-11 07:01:33 +0200

    progress

--- source/blender/blenkernel/intern/node.cc
+++ source/blender/blenkernel/intern/node.cc
@@ -1190,9 +1190,6 @@ static void node_blend_write_storage(BlendWriter *writer, bNodeTree *ntree, bNod
     nodes::socket_items::blend_write<nodes::CaptureAttributeItemsAccessor>(writer, *node);
   }
 
-  if (node->type_legacy == GEO_NODE_SEPARATE_BUNDLE) {
-    nodes::socket_items::blend_write<nodes::SeparateBundleItemsAccessor>(writer, *node);
-  }
   if (node->type_legacy == GEO_NODE_MENU_SWITCH) {
     nodes::socket_items::blend_write<nodes::MenuSwitchItemsAccessor>(writer, *node);
   }
@@ -1470,10 +1467,6 @@ static void node_blend_read_data_storage(BlendDataReader *reader, bNodeTree *ntr
     case CMP_NODE_OUTPUT_FILE: {
       NodeImageMultiFile *nimf = static_cast<NodeImageMultiFile *>(node->storage);
       BKE_image_format_blend_read_data(reader, &nimf->format);
-      break;
-    }
-    case GEO_NODE_SEPARATE_BUNDLE: {
-      nodes::socket_items::blend_read_data<nodes::SeparateBundleItemsAccessor>(reader, *node);
       break;
     }
     case GEO_NODE_MENU_SWITCH: {
