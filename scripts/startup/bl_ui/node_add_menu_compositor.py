b9244d66ef106d2272e560f763365166c1cac0a2
Habib Gahbiche <habib@blender.org>
2025-05-13 19:19:02 +0200

    Add blur options to search menu

--- scripts/startup/bl_ui/node_add_menu_compositor.py
+++ scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -137,6 +137,7 @@ class NODE_MT_category_compositor_color_mix(Menu):
         layout.separator()
         node_add_menu.add_color_mix_node(context, layout)
         node_add_menu.add_node_type_with_searchable_enum(context, layout, "CompositorNodeGlare", "glare_type")
+        node_add_menu.add_node_type_with_searchable_enum(context, layout, "CompositorNodeBlur", "filter_type")
         node_add_menu.add_node_type(layout, "CompositorNodeZcombine")
         node_add_menu.draw_assets_for_catalog(layout, "Color/Mix")
 
