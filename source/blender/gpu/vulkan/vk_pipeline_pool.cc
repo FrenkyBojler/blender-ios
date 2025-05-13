6ee03ed2326f749d6b9888638d83755dedfaa39d
Jeroen Bakker <jeroen@blender.org>
2025-05-13 09:12:48 +0200

    Fix #138236: Vulkan: destroyed pipeline layouts could be reused
    Due to an oversight in the pipeline pool/shader, pipelines that were
    invalid could be reused. This happened more often when using the
    compositor as it recreates shaders over and over. It also depended on
    driver handle reuses on how often artifacts could have been seen.
    
    This is fixed by discarded any associated pipeline when the pipeline
    layout is discarded.

--- source/blender/gpu/vulkan/vk_pipeline_pool.cc
+++ source/blender/gpu/vulkan/vk_pipeline_pool.cc
@@ -646,31 +646,27 @@ VkPipeline VKPipelinePool::get_or_create_graphics_pipeline(VKGraphicsInfo &graph
   return pipeline;
 }
 
-void VKPipelinePool::remove(Span<VkShaderModule> vk_shader_modules)
+void VKPipelinePool::discard(VKDiscardPool &discard_pool, VkPipelineLayout vk_pipeline_layout)
 {
   std::scoped_lock lock(mutex_);
-  Vector<VkPipeline> pipelines_to_destroy;
+  Vector<VkPipeline> pipelines_to_discard;
   compute_pipelines_.remove_if([&](auto item) {
-    if (vk_shader_modules.contains(item.key.vk_shader_module)) {
-      pipelines_to_destroy.append(item.value);
+    if (item.key.vk_pipeline_layout == vk_pipeline_layout) {
+      pipelines_to_discard.append(item.value);
       return true;
     }
     return false;
   });
   graphic_pipelines_.remove_if([&](auto item) {
-    if (vk_shader_modules.contains(item.key.pre_rasterization.vk_vertex_module) ||
-        vk_shader_modules.contains(item.key.pre_rasterization.vk_geometry_module) ||
-        vk_shader_modules.contains(item.key.fragment_shader.vk_fragment_module))
-    {
-      pipelines_to_destroy.append(item.value);
+    if (item.key.vk_pipeline_layout == vk_pipeline_layout) {
+      pipelines_to_discard.append(item.value);
       return true;
     }
     return false;
   });
 
-  VKDevice &device = VKBackend::get().device;
-  for (VkPipeline vk_pipeline : pipelines_to_destroy) {
-    vkDestroyPipeline(device.vk_handle(), vk_pipeline, nullptr);
+  for (VkPipeline vk_pipeline : pipelines_to_discard) {
+    discard_pool.discard_pipeline(vk_pipeline);
   }
 }
 
