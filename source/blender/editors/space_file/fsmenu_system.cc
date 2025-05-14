9b88dccb9b05ce2b0ccdf95a42799444a3c4d932
RedMser <redmser.jj2@gmail.com>
2025-05-14 18:26:06 +0200

    Fix #138863: File Browser: quick access shows files
    Recently opened archive files showed up in quick access.
    Since there are many supported archive file extensions, we now
    check for if the path is actually a directory.

--- source/blender/editors/space_file/fsmenu_system.cc
+++ source/blender/editors/space_file/fsmenu_system.cc
@@ -212,8 +212,9 @@ static void fsmenu_add_windows_quick_access(FSMenu *fsmenu,
     char utf_path[FILE_MAXDIR];
     conv_utf_16_to_8(path, utf_path, FILE_MAXDIR);
 
-    /* Skip library folders since they are not currently supported. */
-    if (!BLI_strcasestr(utf_path, ".library-ms")) {
+    /* Despite the above IsFolder check, Windows considers libraries and archives to be folders.
+     * However, as Blender does not support opening them, they must be filtered out. */
+    if (BLI_is_dir(utf_path)) {
       /* Add folder to the fsmenu. */
       fsmenu_insert_entry(fsmenu, category, utf_path, NULL, ICON_FILE_FOLDER, flag);
     }
