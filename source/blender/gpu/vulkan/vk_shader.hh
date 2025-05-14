1aa39fe01e613aee92241c3babaf4d789aee728c
Clément Foucault <foucault.clem@gmail.com>
2025-05-14 13:16:34 +0200

    Vulkan: Move specialization constant state to Context

--- source/blender/gpu/vulkan/vk_shader.hh
+++ source/blender/gpu/vulkan/vk_shader.hh
@@ -89,11 +89,13 @@ class VKShader : public Shader {
   std::string geometry_layout_declare(const shader::ShaderCreateInfo &info) const override;
   std::string compute_layout_declare(const shader::ShaderCreateInfo &info) const override;
 
-  VkPipeline ensure_and_get_compute_pipeline();
+  VkPipeline ensure_and_get_compute_pipeline(
+      const shader::SpecializationConstants &constants_state);
   VkPipeline ensure_and_get_graphics_pipeline(GPUPrimType primitive,
                                               VKVertexAttributeObject &vao,
                                               VKStateManager &state_manager,
-                                              VKFrameBuffer &framebuffer);
+                                              VKFrameBuffer &framebuffer,
+                                              shader::SpecializationConstants &constants_state);
 
   const VKShaderInterface &interface_get() const;
 
