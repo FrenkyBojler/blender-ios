036353a5f75273409650816f6145aab857a555e7
Habib Gahbiche <habib@blender.org>
2025-05-14 10:29:24 +0200

    rm double entry

--- scripts/startup/bl_ui/node_add_menu_compositor.py
+++ scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -136,7 +136,6 @@ class NODE_MT_category_compositor_color_mix(Menu):
         node_add_menu.add_node_type(layout, "CompositorNodeSeparateColor")
         layout.separator()
         node_add_menu.add_color_mix_node(context, layout)
-        node_add_menu.add_node_type_with_searchable_enum(context, layout, "CompositorNodeGlare", "glare_type")
         node_add_menu.add_node_type(layout, "CompositorNodeZcombine")
         node_add_menu.draw_assets_for_catalog(layout, "Color/Mix")
 
@@ -145,7 +144,7 @@ class NODE_MT_category_compositor_filter(Menu):
     bl_idname = "NODE_MT_category_compositor_filter"
     bl_label = "Filter"
 
-    def draw(self, _context):
+    def draw(self, context):
         layout = self.layout
         layout.menu("NODE_MT_category_compositor_filter_blur")
         layout.separator()
@@ -157,7 +156,7 @@ class NODE_MT_category_compositor_filter(Menu):
         node_add_menu.add_node_type(layout, "CompositorNodeInpaint")
         layout.separator()
         node_add_menu.add_node_type(layout, "CompositorNodeFilter")
-        node_add_menu.add_node_type(layout, "CompositorNodeGlare")
+        node_add_menu.add_node_type_with_searchable_enum(context, layout, "CompositorNodeGlare", "glare_type")
         node_add_menu.add_node_type(layout, "CompositorNodeKuwahara")
         node_add_menu.add_node_type(layout, "CompositorNodePixelate")
         node_add_menu.add_node_type(layout, "CompositorNodePosterize")
