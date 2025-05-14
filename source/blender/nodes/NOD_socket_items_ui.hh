6ce00171c2081c5c4e98f17ede17e6cb750f4d50
Jacques Lucke <jacques@blender.org>
2025-05-14 07:04:44 +0200

    typo

--- source/blender/nodes/NOD_socket_items_ui.hh
+++ source/blender/nodes/NOD_socket_items_ui.hh
@@ -53,7 +53,7 @@ static void draw_items_list_with_operators(const bContext *C,
                                            const bNodeTree &tree,
                                            const bNode &node)
 {
-  BLI_assert(Accessor::nodenode_idname_type == node.idname);
+  BLI_assert(Accessor::node_idname == node.idname);
   PointerRNA node_ptr = RNA_pointer_create_discrete(
       const_cast<ID *>(&tree.id), &RNA_Node, const_cast<bNode *>(&node));
 
