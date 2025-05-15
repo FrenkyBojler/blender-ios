36bdbb5727258afc402c7df32ee4183253327730
Omar Emara <mail@OmarEmara.dev>
2025-05-15 11:17:39 +0300

    Address review

--- source/blender/nodes/NOD_socket_declarations.hh
+++ source/blender/nodes/NOD_socket_declarations.hh
@@ -511,6 +511,7 @@ inline VectorBuilder &VectorBuilder::subtype(PropertySubType subtype)
 
 inline VectorBuilder &VectorBuilder::dimensions(int dimensions)
 {
+  BLI_assert(dimensions >= 2 && dimensions <= 4);
   decl_->dimensions = dimensions;
   return *this;
 }
