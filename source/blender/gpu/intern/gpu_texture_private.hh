6a23fe5856f5663d05e55380233379a5e2999ee5
Hans Goudey <hans@blender.org>
2025-05-14 22:17:14 -0400

    Formatting

--- source/blender/gpu/intern/gpu_texture_private.hh
+++ source/blender/gpu/intern/gpu_texture_private.hh
@@ -339,7 +339,7 @@ class PixelBuffer {
   size_t size_ = 0;
 
  public:
-  PixelBuffer(size_t size) : size_(size) {};
+  PixelBuffer(size_t size) : size_(size){};
   virtual ~PixelBuffer() = default;
 
   virtual void *map() = 0;
