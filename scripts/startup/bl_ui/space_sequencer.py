511dbeea973b78dea9c253411abc2f43a88f970b
Sebastian Parborg <sebastian@blender.org>
2025-05-16 16:58:58 +0200

    VSE: Move the seq prefetch setting to the cache tab and turn on per default

--- scripts/startup/bl_ui/space_sequencer.py
+++ scripts/startup/bl_ui/space_sequencer.py
@@ -2570,6 +2570,10 @@ class SEQUENCER_PT_cache_settings(SequencerButtonsPanel, Panel):
 
         ed = context.scene.sequence_editor
 
+        col = layout.column()
+        if ed:
+            col.prop(ed, "use_prefetch")
+
         col = layout.column(heading="Cache", align=True)
 
         col.prop(ed, "use_cache_raw", text="Raw")
@@ -2759,10 +2763,6 @@ class SEQUENCER_PT_view(SequencerButtonsPanel_Output, Panel):
         col.prop(st, "use_proxies")
         if st.proxy_render_size in {'NONE', 'SCENE'}:
             col.enabled = False
-
-        col = layout.column()
-        if ed:
-            col.prop(ed, "use_prefetch")
 
         col.prop(st, "display_channel", text="Channel")
 
