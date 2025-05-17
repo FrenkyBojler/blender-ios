0a08c5a67c9ec3c10db79cc3e54525f32dfe589f
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2025-05-17 08:05:02 +0200

    OBJ IO: NURBS IO Tests with [internal] support for exporting bke::CurvesGeometry
    Adds test class OBJCurvesTest for validating NURBS curve OBJ IO. Reason
    for implementing an additional testing method is to find an approach
    that is stable with future changes and avoids self-validation using its
    own generated data. To do this, the additional test method takes the
    following approach:
    
    1. Avoid comparisons in text (.obj) form, as this can easily change (e.g. formatting).
    2. Validating using geometry expectations, not files (using bke::CurvesGeometry for long term stability).
    3. Uses high level internal OBJ API to avoid context requirements while not unit testing individual functions [prone to change].
    4. Test input/data should instead manage granularity to validate the intended function/expectation.
    
    This should provide testing capabilties that will scale while minimizing
    maintainance. It also put some pressure on existing implementation and
    required refactoring. Changes are [generally] not functional but either
    adjusts for readability, object responsibility, or modifies function
    signatures due to added abstractions. Example of this is added support
    for exporting bke::CurvesGeometry through IOBJCurve interface and
    addition of IO streams in IOBJInputStream.
    
    While one could ask for more then just two integrated tests, additional
    tests would likely involve functional changes and better kept separate.
    They already identified a memory leak and a double initialization of
    Curve objects introduced in #123967 and some issues with knot mode
    idenfitication from #138778.
    
    Remove typelength
    
    Remove typelength
    
    Fix is_vector_base template magic
    
    Correct comment
    
    Iterate in correct order
    
    Revert order of points in test
    
    Compare elementwise to single value
    
    Write proper relative, cyclic uniform test
    
    Verify cyclic
    
    Correct Uniform cyclic
    
    Format
    
    Format
    
    Make test duplication use utility function, don't validate multiplicity (it's a CurvesGeometry test)
    
    Additional uniform tests
    
    Don't treat clamped polyline as bezier
    
    Format
    
    250527_fix_bezier_nurbs
    
    Fix num points
    
    Adjust Nurbs identification
    
    Fix bezier endpoint
    
    Add bezier clamped cyclic case
    
    Format
    
    Progress
    
    Progress
    
    Functional
    
    Fix position data
    
    abc
    
    Progress...
    
    Reset obj_export_fw
    
    Reset obj_export_fw_hh
    
    Revert obj_exporter
    
    Revert obj_export_io
    
    Reset obj_importer
    
    Revert ObjExporter_tests
    
    Revert tests
    
    Revert Obj_utility
    
    Revert utility and make compilable
    
    Format
    
    Expose obj object export
    
    Fix tests
    
    Revert is_close templating
    
    Remove shared utility files
    
    Cleanup imports

--- source/blender/io/wavefront_obj/exporter/obj_export_nurbs.hh
+++ source/blender/io/wavefront_obj/exporter/obj_export_nurbs.hh
@@ -9,73 +9,202 @@
 #pragma once
 
 #include "BLI_math_vector_types.hh"
-#include "BLI_utility_mixins.hh"
 
-#include "DNA_curve_types.h"
-
-#include "IO_orientation.hh"
-
+struct Curve;
+struct Nurb;
 struct OBJExportParams;
 
+namespace blender::bke {
+class CurvesGeometry;
+}
+
 namespace blender::io::obj {
+
+/**
+ * Finds the range within the control points that represents the sequence of valid spans or
+ * 'segments'.
+ *
+ * For example, if a NURBS curve of order 2 has following 5 knots:
+ *      [0, 0, 0, 1, 1]
+ * associated to two control points, the valid control point range would be the
+ * interval [1, 2] as the first point is not included in any valid span.
+ */
+void valid_nurb_control_point_range(int8_t order,
+                                    bool cyclic,
+                                    Span<float> &knots,
+                                    IndexRange &point_range);
+
+/**
+ * Curve object wrapper for curve objects exported by the exporter. Curve objects can contain
+ * multiple individual splines.
+ */
+class IOBJCurve {
+ public:
+  virtual ~IOBJCurve() = default;
+
+  virtual const float4x4 &object_transform() const = 0;
+
+  virtual const char *get_curve_name() const = 0;
+
+  /**
+   *  Number of splines associated with the Curve object.assign_if_different
+   */
+  virtual int total_splines() const = 0;
+  /**
+   * \param spline_index: Zero-based index of spline of interest.
+   * \return Total vertices in a spline.
+   */
+  virtual int total_spline_vertices(int spline_index) const = 0;
+  /**
+   * Get the number of control points on the U-dimension.
+   */
+  virtual int num_control_points_u(int spline_index) const = 0;
+  /**
+   * Get the number of control points on the V-dimension.
+   */
+  virtual int num_control_points_v(int spline_index) const = 0;
+  /**
+   * Get the degree of the NURBS spline for the U-dimension.
+   */
+  virtual int get_nurbs_degree_u(int spline_index) const = 0;
+  /**
+   * Get the degree of the NURBS spline for the V-dimension.
+   */
+  virtual int get_nurbs_degree_v(int spline_index) const = 0;
+  /**
+   * True if the indexed spline is cyclic along U dimension.
+   */
+  virtual bool get_cyclic_u(int spline_index) const = 0;
+  /**
+   * Get the knot vector for the U-dimension. Computes knots using the buffer if necessary.
+   */
+  virtual Span<float> get_knots_u(int spline_index, Vector<float> &buffer) const = 0;
+  /**
+   * Get coordinates for the (non-looped) spline control points.
+   */
+  virtual Span<float3> vertex_coordinates(int spline_index,
+                                          Vector<float3> &dynamic_point_buffer) const = 0;
+};
+
+/**
+ * Provides access to the a Curve Object's properties.
+ * Only support NURBS, TODO: support other types.
+ *
+ * \note Used for legacy Curve export.
+ */
+class OBJCurves : public IOBJCurve, NonCopyable {
+ private:
+  const bke::CurvesGeometry &curve_;
+  const float4x4 transform_;
+  const std::string name_;
+
+ public:
+  OBJCurves(const bke::CurvesGeometry &curve, const float4x4 &transform, const std::string &name);
+  virtual ~OBJCurves() override = default;
+
+  const float4x4 &object_transform() const override;
+
+  const char *get_curve_name() const override;
+
+  /**
+   *  Number of splines associated with the Curve object.assign_if_different
+   */
+  int total_splines() const override;
+  /**
+   * \param spline_index: Zero-based index of spline of interest.
+   * \return Total vertices in a spline.
+   */
+  int total_spline_vertices(int spline_index) const override;
+  /**
+   * Get the number of control points on the U-dimension.
+   */
+  int num_control_points_u(int spline_index) const override;
+  /**
+   * Get the number of control points on the V-dimension.
+   */
+  int num_control_points_v(int spline_index) const override;
+  /**
+   * Get the degree of the NURBS spline for the U-dimension.
+   */
+  int get_nurbs_degree_u(int spline_index) const override;
+  /**
+   * Get the degree of the NURBS spline for the V-dimension.
+   */
+  int get_nurbs_degree_v(int spline_index) const override;
+  /**
+   * True if the indexed spline is cyclic along U dimension.
+   */
+  bool get_cyclic_u(int spline_index) const override;
+  /**
+   * Get the knot vector for the U-dimension. Computes knots using the buffer if necessary.
+   */
+  Span<float> get_knots_u(int spline_index, Vector<float> &buffer) const override;
+  /**
+   * Get coordinates for the (non-looped) spline control points.
+   */
+  Span<float3> vertex_coordinates(int spline_index,
+                                  Vector<float3> &dynamic_point_buffer) const override;
+};
 
 /**
  * Provides access to the a Curve Object's properties.
  * Only #CU_NURBS type is supported.
  *
- * \note Used for Curves to be exported in parameter form, and not converted to meshes.
+ * \note Used for legacy Curve export.
  */
-class OBJCurve : NonCopyable {
+class OBJLegacyCurve : public IOBJCurve, NonCopyable {
  private:
   const Object *export_object_eval_;
   const Curve *export_curve_;
-  float world_axes_transform_[4][4];
+
+  const Nurb *get_spline(int spline_index) const;
 
  public:
-  OBJCurve(const Depsgraph *depsgraph, const OBJExportParams &export_params, Object *curve_object);
+  OBJLegacyCurve(const Depsgraph *depsgraph, Object *curve_object);
+  virtual ~OBJLegacyCurve() override = default;
 
-  const char *get_curve_name() const;
-  int total_splines() const;
-  const Nurb *get_spline(int spline_index) const;
+  const float4x4 &object_transform() const override;
+
+  const char *get_curve_name() const override;
+
+  /**
+   *  Number of splines associated with the Curve object.assign_if_different
+   */
+  int total_splines() const override;
   /**
    * \param spline_index: Zero-based index of spline of interest.
    * \return Total vertices in a spline.
    */
-  int total_spline_vertices(int spline_index) const;
-  /**
-   * Get coordinates of the vertex at the given index on the given spline.
-   */
-  float3 vertex_coordinates(int spline_index, int vertex_index, float global_scale) const;
+  int total_spline_vertices(int spline_index) const override;
   /**
    * Get the number of control points on the U-dimension.
    */
-  int num_control_points_u(int spline_index) const;
+  int num_control_points_u(int spline_index) const override;
   /**
    * Get the number of control points on the V-dimension.
    */
-  int num_control_points_v(int spline_index) const;
+  int num_control_points_v(int spline_index) const override;
   /**
    * Get the degree of the NURBS spline for the U-dimension.
    */
-  int get_nurbs_degree_u(int spline_index) const;
+  int get_nurbs_degree_u(int spline_index) const override;
   /**
    * Get the degree of the NURBS spline for the V-dimension.
    */
-  int get_nurbs_degree_v(int spline_index) const;
+  int get_nurbs_degree_v(int spline_index) const override;
   /**
-   * Get the U flags (CU_NURB_*) of the NURBS spline at the given index.
+   * True if the indexed spline is cyclic along U dimension.
    */
-  short get_nurbs_flagu(int spline_index) const;
+  bool get_cyclic_u(int spline_index) const override;
   /**
    * Get the knot vector for the U-dimension. Computes knots using the buffer if necessary.
    */
-  Span<float> get_knots_u(int spline_index, Vector<float> &buffer) const;
-
- private:
+  Span<float> get_knots_u(int spline_index, Vector<float> &buffer) const override;
   /**
-   * Set the final transform after applying axes settings and an Object's world transform.
+   * Get coordinates for the (non-looped) spline control points.
    */
-  void set_world_axes_transform(eIOAxis forward, eIOAxis up);
+  Span<float3> vertex_coordinates(int spline_index,
+                                  Vector<float3> &dynamic_point_buffer) const override;
 };
 
 }  // namespace blender::io::obj
--- source/blender/io/wavefront_obj/importer/obj_import_nurbs.cc
+++ source/blender/io/wavefront_obj/importer/obj_import_nurbs.cc
@@ -17,6 +17,7 @@
 #include "DNA_curve_types.h"
 
 #include "IO_wavefront_obj.hh"
+
 #include "importer_mesh_utils.hh"
 #include "obj_import_nurbs.hh"
 #include "obj_import_objects.hh"
@@ -83,15 +84,18 @@ static int8_t get_valid_nurbs_degree(const NurbsElement &element)
 }
 
 /**
- * Get the number of control points repeated for a cyclic curve given the multiplicity at the end
- * of the knot vectors (multiplicity at both ends need to match).
+ * Get the number of control points repeated for a cyclic curve given the multiplicity found
+ * at the endpoints (assumes cyclic curve).
  */
-static int cyclic_repeated_points(const int8_t order, const int end_multiplicity)
+static int repeating_cyclic_point_num(const int8_t order, const Span<float> knots)
 {
-  /* Since multiplicity == order is considered 'cyclic' even if it is only C0 continuous in
-   * principle (it is technically discontinuous!), it needs to be clamped to 1.
+  /* Due to the additional start knot, drop first.
    */
-  return std::max<int>(order - end_multiplicity, 1);
+  Vector<int> multiplicity = bke::curves::nurbs::calculate_multiplicity_sequence(
+      knots.slice(1, order - 1));
+
+  BLI_assert(order > multiplicity.first());
+  return order - multiplicity.first();
 }
 
 void CurveFromGeometry::create_nurbs(Curve *curve, const OBJImportParams &import_params)
@@ -116,14 +120,8 @@ void CurveFromGeometry::create_nurbs(Curve *curve, const OBJImportParams &import
   nurb->flagu = this->detect_knot_mode(
       import_params, degree, nurbs_geometry.curv_indices, nurbs_geometry.parm, multiplicity);
 
-  if ((nurb->flagu & (CU_NURB_CUSTOM | CU_NURB_CYCLIC | CU_NURB_ENDPOINT)) == CU_NURB_CUSTOM) {
-    /* TODO: If mode is CU_NURB_CUSTOM, but not CU_NURB_CYCLIC and CU_NURB_ENDPOINT, then make
-     * curve clamped instead of removing CU_NURB_CUSTOM. */
-    nurb->flagu &= ~CU_NURB_CUSTOM;
-  }
-
   const int repeated_points = nurb->flagu & CU_NURB_CYCLIC ?
-                                  cyclic_repeated_points(nurb->orderu, multiplicity.first()) :
+                                  repeating_cyclic_point_num(nurb->orderu, nurbs_geometry.parm) :
                                   0;
   const Span<int> indices = nurbs_geometry.curv_indices.as_span().slice(
       nurbs_geometry.curv_indices.index_range().drop_back(repeated_points));
@@ -161,25 +159,7 @@ static bool detect_knot_mode_cyclic(const int8_t degree,
   constexpr float epsilon = 1e-7;
   const int8_t order = degree + 1;
 
-  /* This is a good distinction between the 'cyclic' property and a true periodic NURBS curve. A
-   * periodic curve should be smooth to the degree - 1 derivative (which is the maximum possible).
-   * Allowing matching `multiplicity > 1` is not a periodic NURBS but can be considered cyclic.
-   */
-  if (multiplicity.first() != multiplicity.last()) {
-    return false;
-  }
-
-  /* Multiplicity m is continuous to the `degree - m` derivative and as such
-   * `multiplicity == order` is discontinuous.
-   * By allowing it, clamped or Bezier curves can still be considered cyclic but
-   * ensure [here] that illogical `multiplicities > order` is not considered cyclic.
-   */
-  if (multiplicity.first() > order || multiplicity.last() > order) {
-
-    return false;
-  }
-
-  const int repeated_points = cyclic_repeated_points(order, multiplicity.first());
+  const int repeated_points = repeating_cyclic_point_num(order, knots);
   const Span<int> indices_tail = indices.take_back(repeated_points);
   for (const int64_t i : indices_tail.index_range()) {
     if (indices[i] != indices_tail[i]) {
@@ -187,32 +167,98 @@ static bool detect_knot_mode_cyclic(const int8_t degree,
     }
   }
 
-  if (multiplicity.first() >= degree) {
+  /* Multiplicity m is continuous to the `degree - m` derivative and as such
+   * `multiplicity == degree` is discontinuous. Due to the superfluous knots
+   * the first/last entry can be `order`.
+   */
+  if (multiplicity.first() > order || multiplicity.last() > order) {
+    return false;
+  }
+  for (const int m : multiplicity.drop_front(1).drop_back(1)) {
+    if (m > degree) {
+      return false;
+    }
+  }
+
+  if (multiplicity.first() >= degree && multiplicity.last() >= degree) {
     /* There is no overlap in the knot spans. */
     return true;
   }
 
   /* Ensure it matches on both of the knot spans adjacent to the start/end of the parameter range.
+   * Ignore first and last superfluous knots.
    */
-  const Span<float> knots_tail = knots.take_back(order + degree);
+  const Span<float> real_knots = knots.drop_front(1).drop_back(1);
+  const Span<float> knots_tail = real_knots.take_back(2 * degree);
   for (const int64_t i : knots_tail.index_range().drop_back(1)) {
-    const float head_span = knots[i + 1] - knots[i];
+    const float head_span = real_knots[i + 1] - real_knots[i];
     const float tail_span = knots_tail[i + 1] - knots_tail[i];
     if (abs(head_span - tail_span) > epsilon) {
       return false;
     }
   }
-  return true;
+  /* Check superfluous first/last knot to also be 'symmetric' */
+  return abs((knots[1] - knots[0]) - (knots.last() - knots.last(1))) <= epsilon;
 }
 
-static bool detect_knot_mode_bezier(const int8_t degree, const Span<int> multiplicity)
+static bool detect_knot_mode_bezier_clamped(const int8_t degree,
+                                            const int num_points,
+                                            const Span<int> multiplicity,
+                                            const bool cyclic)
 {
   const int8_t order = degree + 1;
-  if (multiplicity.first() != order || multiplicity.last() != order) {
+  /* Don't treat polylines as Beziers. */
+  if (order == 2) {
     return false;
   }
 
-  for (const int m : multiplicity.drop_front(1).drop_back(1)) {
+  /* Allow patterns:
+    O d ..
+    1 d d ..
+  */
+  if (multiplicity[0] < order && (multiplicity[0] != 1 || multiplicity[1] < degree)) {
+    return false;
+  }
+
+  Span<int> mdegree_span = multiplicity.drop_front(1);
+  if (cyclic) {
+    /* Cyclic, allow patterns:
+       .. d O
+       .. d 1
+    */
+    if (multiplicity.last() != order &&
+        (multiplicity.last() == 1 && multiplicity.last(1) != degree))
+    {
+      /* No match to the valid patterns. */
+      return false;
+    }
+  }
+  else if (multiplicity.size() == 2) {
+    /* Single non-cyclic segment, allow patterns:
+     * O a
+     * where a > 0
+     */
+    if (multiplicity.first() != order) {
+      return false;
+    }
+  }
+  else {
+    /* Non-cyclic, allow patterns:
+      .. d O+
+      .. d d 1
+      k
+    */
+    const int remainder = (num_points - 1) % degree;
+    if (multiplicity.last() != order + remainder &&
+        (multiplicity.last() != 1 || multiplicity.last(1) < degree))
+    {
+      return false;
+    }
+  }
+  mdegree_span = mdegree_span.drop_back(1);
+
+  /* Verify all other knots are of degree multiplicity */
+  for (const int m : mdegree_span) {
     if (m != degree) {
       return false;
     }
@@ -223,7 +269,7 @@ static bool detect_knot_mode_bezier(const int8_t degree, const Span<int> multipl
 static bool detect_knot_mode_uniform(const int8_t degree,
                                      const Span<float> knots,
                                      const Span<int> multiplicity,
-                                     bool clamped)
+                                     const bool clamped)
 {
   constexpr float epsilon = 1e-7;
 
@@ -231,20 +277,25 @@ static bool detect_knot_mode_uniform(const int8_t degree,
    * curve, all multiplicity entries equals 1 and the array size should match.
    */
   const int clamped_offset = clamped * degree;
-  if (knots.size() != multiplicity.size() - 2 * clamped_offset) {
+  if (knots.size() != multiplicity.size() + 2 * clamped_offset) {
     return false;
   }
 
   /* Ensure it's not a single segment with clamped ends (it would be a Bezier segment). */
   const Span<float> unclamped_knots = knots.drop_front(clamped_offset).drop_back(clamped_offset);
-  if (!unclamped_knots.size()) {
+  if (unclamped_knots.size() == 2) {
     return false;
+  }
+  if (unclamped_knots.size() < 2) {
+    /* Classify single point as uniform? */
+    return true;
   }
 
   /* Verify spacing is uniform (excluding clamped ends). */
   const float uniform_delta = unclamped_knots[1] - unclamped_knots[0];
-  for (const int64_t i : knots.index_range().drop_front(2)) {
-    if (abs((knots[i] - knots[i - 1]) - uniform_delta) < epsilon) {
+  for (const int64_t i : unclamped_knots.index_range().drop_front(2)) {
+    const float delta = unclamped_knots[i] - unclamped_knots[i - 1];
+    if (abs(delta - uniform_delta) > epsilon) {
       return false;
     }
   }
@@ -264,15 +315,9 @@ short CurveFromGeometry::detect_knot_mode(const OBJImportParams &import_params,
         knot_mode, detect_knot_mode_cyclic(degree, indices, knots, multiplicity), CU_NURB_CYCLIC);
   }
 
-  if (detect_knot_mode_bezier(degree, multiplicity)) {
-    /* Currently endpoint flag is not parsed for Bezier, mainly because a clamped Bezier curve in
-     * Blender is either:
-     * a) A valid Bezier curve for given degree/order with correct number of points to form one.
-     * b) Not a valid Bezier curve for given degree, last span/segment is of a lower degree Bezier.
-     *
-     * Set ENDPOINT to true since legacy Bezier NURBS only validates and compute knots if it
-     * contains order + 1 control points unless endpoint is set...?
-     */
+  if (detect_knot_mode_bezier_clamped(
+          degree, indices.size(), multiplicity, knot_mode & CU_NURB_CYCLIC))
+  {
     SET_FLAG_FROM_TEST(knot_mode, true, CU_NURB_ENDPOINT);
     SET_FLAG_FROM_TEST(knot_mode, true, CU_NURB_BEZIER);
   }
