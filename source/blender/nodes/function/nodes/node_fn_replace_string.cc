b19662abe1ab2681873bb37d221e90252200f261
Alberto12345678999 <a.pilotto@icloud.com>
2025-05-19 16:24:55 -0300

    Align sockets for Replace String and Slice String text nodes.

--- source/blender/nodes/function/nodes/node_fn_replace_string.cc
+++ source/blender/nodes/function/nodes/node_fn_replace_string.cc
@@ -12,6 +12,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
   b.allow_any_socket_order();
+  b.add_default_layout();
   b.add_input<decl::String>("String").hide_label();
   b.add_output<decl::String>("String").align_with_previous();
   b.add_input<decl::String>("Find").description("The string to find in the input string");
