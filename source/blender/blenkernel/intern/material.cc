547c27f5ad6f1dcdb2fe6c5a100af47650ace90a
Brecht Van Lommel <brecht@blender.org>
2025-05-19 20:52:12 +0200

    Fix build error

--- source/blender/blenkernel/intern/material.cc
+++ source/blender/blenkernel/intern/material.cc
@@ -85,7 +85,7 @@ static void material_init_data(ID *id)
 {
   Material *material = (Material *)id;
 
-  INIT_DEFAULT_STRUCT_AFTER(material, id);
+  MEMCPY_STRUCT_AFTER_CHECKED(material, DNA_struct_default_get(Material), id);
 }
 
 static void material_copy_data(Main *bmain,
