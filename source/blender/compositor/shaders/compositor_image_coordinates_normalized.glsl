d7a2389250109212e93c1255d7c67514efabff16
Omar Emara <mail@OmarEmara.dev>
2025-05-21 14:20:20 +0300

    Fix typo

--- source/blender/compositor/shaders/compositor_image_coordinates_normalized.glsl
+++ source/blender/compositor/shaders/compositor_image_coordinates_normalized.glsl
@@ -7,7 +7,7 @@ void main()
   const int2 texel = int2(gl_GlobalInvocationID.xy);
 
   const int2 size = imageSize(output_img);
-  const float2 normalized_coordinates = (float2(texel) + 0.5f) * float2(size);
+  const float2 normalized_coordinates = (float2(texel) + 0.5f) / float2(size);
 
   imageStore(output_img, texel, float4(normalized_coordinates, float2(0.0f)));
 }
