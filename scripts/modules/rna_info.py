d7c4923b5b8929275e3b32539217766a0868259b
Nathan Vegdahl <cessen@cessen.com>
2025-05-22 09:57:26 +0200

    Use more succicnt syntax

--- scripts/modules/rna_info.py
+++ scripts/modules/rna_info.py
@@ -488,7 +488,7 @@ class InfoPropertyRNA:
         if self.is_path_supports_templates:
             type_info.append(
                 "Supports `template expressions <https://docs.blender.org/manual/en/{:d}.{:d}/files/file_paths.html#path-templates>`_".format(
-                    bpy.app.version[0], bpy.app.version[1]))
+                    *bpy.app.version[:2]))
 
         if type_info:
             type_str += ", ({:s})".format(", ".join(type_info))
