a7efdba45d033f8b3f7358797ae46514efebb51a
Nathan Vegdahl <cessen@cessen.com>
2025-05-22 16:08:02 +0200

    Missed a word

--- source/blender/blenkernel/BKE_path_templates.hh
+++ source/blender/blenkernel/BKE_path_templates.hh
@@ -152,7 +152,7 @@ std::optional<blender::bke::path_templates::VariableMap> BKE_build_template_vari
  * Build a template variable map for render output paths.
  *
  * All parameters are allowed to be null, in which case the variables derived
- * those parameters will simply not be included.
+ * from those parameters will simply not be included.
  *
  * This is typically used to create the variables passed to
  * `BKE_path_apply_template()`.
