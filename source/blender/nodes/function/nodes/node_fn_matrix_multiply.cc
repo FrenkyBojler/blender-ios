d0204bcfcc912fa397c244b45dc6e5e66b3f59d6
Alberto12345678999 <a.pilotto@icloud.com>
2025-05-22 15:08:42 -0300

    Geometry Nodes: Align sockets of Join Strings and Matrix Multiply nodes.

--- source/blender/nodes/function/nodes/node_fn_matrix_multiply.cc
+++ source/blender/nodes/function/nodes/node_fn_matrix_multiply.cc
@@ -13,10 +13,12 @@ namespace blender::nodes::node_fn_matrix_multiply_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
+  b.use_custom_socket_order();
+  b.allow_any_socket_order();
   b.is_function_node();
   b.add_input<decl::Matrix>("Matrix");
+  b.add_output<decl::Matrix>("Matrix").align_with_previous();
   b.add_input<decl::Matrix>("Matrix", "Matrix_001");
-  b.add_output<decl::Matrix>("Matrix");
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
