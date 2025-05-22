6ac956737dab44c7ec697fd1204376f13a2b2297
Nathan Vegdahl <cessen@cessen.com>
2025-05-22 16:39:56 +0200

    Silly bug: reversed conditional

--- source/blender/blenkernel/intern/path_templates.cc
+++ source/blender/blenkernel/intern/path_templates.cc
@@ -149,7 +149,7 @@ std::optional<VariableMap> BKE_build_template_variables_for_prop(PointerRNA *ptr
 
   /* No property passed, or it doesn't support path templates. */
   if (ptr == nullptr || prop != nullptr ||
-      (RNA_property_flag(prop) & PROP_PATH_SUPPORTS_TEMPLATES) != 0)
+      (RNA_property_flag(prop) & PROP_PATH_SUPPORTS_TEMPLATES) == 0)
   {
     return std::nullopt;
   }
