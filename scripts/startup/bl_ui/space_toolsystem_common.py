eaef03b6daf63279ccd16faee4431dd48356287d
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-05-22 13:27:36 +0200

    wip icons

--- scripts/startup/bl_ui/space_toolsystem_common.py
+++ scripts/startup/bl_ui/space_toolsystem_common.py
@@ -1252,7 +1252,6 @@ class PlayheadSnappingPanel:
         layout = self.layout
         col = layout.column()
 
-        col.prop(tool_settings, "use_snap_playhead")
         col.prop(tool_settings, "playhead_snap_distance")
         col.separator()
         col.label(text="Snap Target")
