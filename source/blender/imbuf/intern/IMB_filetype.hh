c20605b7c2d15f72e77fab2c6ba1511a886bb93f
Peter Kovář <peter.kovar@reflexion.tv>
2025-05-24 18:27:34 +0200

    Updated the JPEG XL I/O
    Signed-off-by: Peter Kovář <peter.kovar@reflexion.tv>

--- source/blender/imbuf/intern/IMB_filetype.hh
+++ source/blender/imbuf/intern/IMB_filetype.hh
@@ -158,11 +158,11 @@ ImBuf *imb_thumbnail_jpeg(const char *filepath,
 /** \name Format: JPEG XL (#IMB_FTYPE_JXL)
  * \{ */
 
-bool imb_is_a_jxl(const unsigned char *buf, size_t size);
+bool imb_is_a_jxl(const unsigned char *mem, size_t size);
 ImBuf *imb_load_jxl(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 bool imb_save_jxl(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
