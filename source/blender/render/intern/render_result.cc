58f4d2e8a0859c5564ed93105b12105e584c8c8f
Bastien Montagne <bastien@blender.org>
2025-05-26 20:31:34 +0200

    Comment with `/* FIXME: MAX_ID_NAME & FILE_MAXFILE` some filename length isues.
    Seems like we already have such problems, even without considering
    increased ID name lenght?

--- source/blender/render/intern/render_result.cc
+++ source/blender/render/intern/render_result.cc
@@ -1034,6 +1034,11 @@ static void render_result_exr_file_cache_path(Scene *sce,
     root = root_buf;
   }
 
+  /* FIXME: MAX_ID_NAME & FILE_MAXFILE
+   *
+   * This can already easily overflow I think? `filename` itself is already `FILE_MAXFILE`, and
+   * initialized from the blendfile name itself.
+   */
   SNPRINTF(filename_full, "cached_RR_%s_%s_%s.exr", filename, sce->id.name + 2, path_hexdigest);
 
   BLI_path_join(r_path, FILE_CACHE_MAX, root, filename_full);
