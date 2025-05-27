5cfb8b11df3ef64897aaf5ea73cbf4ea2ca7992e
Campbell Barton <campbell@blender.org>
2025-05-27 13:53:15 +1000

    Fix #136737: Periodic freezes in edit-mode with high poly meshes
    Meshes with millions of vertices could freeze when storing
    edit-mode undo steps. This occurred when de-duplicating boolean arrays
    (typically selection & hidden data).
    
    This problem occurred after v3.3 because vert/edge/face flags were
    moved into their own boolean custom-data layers.
    
    Resolve using run-length encoding, used to encode all boolean
    custom-data layers before adding them to the BArrayStore.
    
    The overall speedup depends a lot on the geometry & data layout
    as well as the previous state of the mesh.
    
    Testing box selection with subdivided cubes (3.9 million vertices).
    
    - 8..16 seconds (before).
    - ~0.025 seconds (after).
    
    Even though this ran in a background thread, adding undo steps
    waits for the previous step to finish which could freeze.
    
    Details:
    
    - This adds overhead in the case where nothing changes because
      boolean layers need to be encoded *before* checking for changes.
      In practice the overhead is minimal even in the worst case where
      no run-length spans are found.
    
      Times for encoding 32million bytes on an AMD Threadripper 3990X.
    
      - ~0.11 seconds on random bytes.
      - ~0.0025 seconds for uniform arrays (common case for selection).
    
    - Tweaks to BLI_array_store such as increasing the block-size used by
      edit-mesh undo and increasing length of hashed data helped but didn't
      resolve the issue, see: !138975.

--- source/blender/editors/mesh/editmesh_undo.cc
+++ source/blender/editors/mesh/editmesh_undo.cc
@@ -72,6 +72,29 @@
 #  define ARRAY_CHUNK_NUM_MIN 256
 
 #  define USE_ARRAY_STORE_THREAD
+
+/**
+ * Use run length encoding for boolean custom-data.
+ *
+ * Avoid poor performance caused by boolean arrays not having enough *uniqueness* to
+ * efficiently de-duplicate, see: #136737.
+ *
+ * NOTE(@ideasman42): This has the down-side that creating undo steps needs to encode
+ * data *before* comparing it with the previous state (when creating each undo step).
+ * Adding additional work even when nothing change.
+ *
+ * Since there is overhead for RLE encoding this is only used on boolean array,
+ * typically used for storing selection/hidden state as well as edge flags.
+ * The encoding has also been optimized for performance.
+ *
+ * In practice, arrays of 32 million booleans (on an AMD TRX 3990X):
+ * - ~0.11 seconds for random data.
+ * - ~0.0025 seconds uniform arrays.
+ * ... so the tradeoff seems reasonable.
+ *
+ * There is also the benefit of reduced memory use, although that isn't the goal.
+ */
+#  define USE_ARRAY_STORE_RLE
 #endif
 
 #ifdef USE_ARRAY_STORE_THREAD
@@ -283,8 +306,34 @@ static void um_arraystore_cd_compact(CustomData *cdata,
               state_reference = std::get<BArrayState *>(bcd_reference_current->states[i]);
             }
 
-            bcd->states[i] = BLI_array_store_state_add(
-                bs, layer->data, size_t(data_len) * stride, state_reference);
+            void *data_final = layer->data;
+            size_t data_final_size = size_t(data_len) * stride;
+
+#  ifdef USE_ARRAY_STORE_RLE
+            uint8_t *data_enc = nullptr;
+            if ((stride == 1) && (bcd->type == CD_PROP_BOOL)) {
+              /* Store the size in the encoded data (for convenience). */
+              size_t data_enc_extra_size = sizeof(size_t);
+              size_t data_enc_size;
+              data_enc = BLI_array_store_rle_encode(reinterpret_cast<const uint8_t *>(data_final),
+                                                    data_final_size,
+                                                    data_enc_extra_size,
+                                                    &data_enc_size);
+              memcpy(data_enc, &data_final_size, data_enc_extra_size);
+              data_final = data_enc;
+              data_final_size = data_enc_extra_size + data_enc_size;
+            }
+#  endif
+
+            bcd->states[i] = {
+                BLI_array_store_state_add(bs, data_final, data_final_size, state_reference),
+            };
+
+#  ifdef USE_ARRAY_STORE_RLE
+            if (data_enc) {
+              MEM_freeN(data_enc);
+            }
+#  endif
           }
         }
         else {
@@ -334,7 +383,28 @@ static void um_arraystore_cd_expand(const BArrayCustomData *bcd,
         BArrayState *state = std::get<BArrayState *>(bcd->states[i]);
         if (state) {
           size_t state_len;
-          layer->data = BLI_array_store_state_data_get_alloc(state, &state_len);
+          void *data = BLI_array_store_state_data_get_alloc(state, &state_len);
+
+#  ifdef USE_ARRAY_STORE_RLE
+          if ((stride == 1) && (bcd->type == CD_PROP_BOOL)) {
+            /* Store the size in the encoded data (for convenience). */
+            size_t data_enc_extra_size = sizeof(size_t);
+            const uint8_t *data_enc = reinterpret_cast<uint8_t *>(data);
+            size_t data_dec_size;
+            memcpy(&data_dec_size, data_enc, sizeof(size_t));
+            uint8_t *data_dec = MEM_malloc_arrayN<uint8_t>(data_dec_size, __func__);
+            BLI_array_store_rle_decode(data_enc + data_enc_extra_size,
+                                       state_len - data_enc_extra_size,
+                                       data_dec,
+                                       data_dec_size);
+            MEM_freeN(data);
+            data = static_cast<void *>(data_dec);
+            /* Just for the assert to succeed. */
+            state_len = data_dec_size;
+          }
+#  endif
+
+          layer->data = data;
           layer->sharing_info = implicit_sharing::info_for_mem_free(layer->data);
           BLI_assert(stride * data_len == state_len);
           UNUSED_VARS_NDEBUG(stride, data_len);
