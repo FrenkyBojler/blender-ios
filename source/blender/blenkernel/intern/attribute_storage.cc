3ad12b4f48fed3de20aa49278858dc9c2aada342
Hans Goudey <hans@blender.org>
2025-05-27 18:02:49 -0400

    Comment wording

--- source/blender/blenkernel/intern/attribute_storage.cc
+++ source/blender/blenkernel/intern/attribute_storage.cc
@@ -64,7 +64,7 @@ Attribute::ArrayData Attribute::ArrayData::ForValue(const GPointer &value,
   const CPPType &type = *value.type();
   const void *value_ptr = type.default_value();
 
-  /* Prefer `calloc` to filling after allocation since it is faster. */
+  /* Prefer `calloc` to zeroing after allocation since it is faster. */
   if (BLI_memory_is_zero(value_ptr, type.size) && type.alignment <= MEM_MIN_CPP_ALIGNMENT) {
     data.data = MEM_calloc_arrayN(domain_size, type.size, __func__);
   }
