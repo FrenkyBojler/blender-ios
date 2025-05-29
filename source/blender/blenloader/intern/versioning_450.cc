b1ab601da03fd8f479faffc28c79b2e5d9608f43
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-05-29 11:01:08 +0200

    fix merge issues

--- source/blender/blenloader/intern/versioning_450.cc
+++ source/blender/blenloader/intern/versioning_450.cc
@@ -4880,7 +4880,7 @@ void do_versions_after_linking_450(FileData * /*fd*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 81)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 83)) {
     LISTBASE_FOREACH (Object *, object, &bmain->objects) {
       if (object->type != OB_ARMATURE) {
         continue;
