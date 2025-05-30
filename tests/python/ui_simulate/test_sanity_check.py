94623637c9f2064be834dba3edd84757b3689f60
Sean Kim <SeanCTKim@protonmail.com>
2025-05-30 12:53:43 -0700

    Feedback
    * Change test to add workspaces instead of assuming a default order
    * Split out test by "category" and add workspaces that weren't activated
      previously

--- tests/python/ui_simulate/test_sanity_check.py
+++ tests/python/ui_simulate/test_sanity_check.py
@@ -34,38 +34,142 @@ def _call_by_name(e, text: str):
     yield e.ret()
 
 
-def check_all_workspaces():
-    """Test that we can cycle through all default workspaces without crashing"""
+def _call_menu(e, text: str):
+    yield e.f3()
+    yield e.text_unicode(text.replace(" -> ", " \u25b8 "))
+    yield e.ret()
+
+
+def check_all_general_workspaces():
     e, t = _test_vars(window := _test_window())
 
-    t.assertEqual(window.workspace.name_full, "Layout")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.a()     # Animation
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Animation")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Modeling")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.c()     # Compositing
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Compositing")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Sculpting")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.g()     # Geometry Nodes
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Geometry Nodes")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "UV Editing")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.l()     # Layout
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Layout")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Texture Paint")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.m()     # Modeling
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Modeling")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Shading")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.r()     # Rendering
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Rendering")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Animation")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.s()     # Scripting
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Scripting")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Rendering")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.p()     # Sculpting
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Sculpting")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Compositing")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.h()     # Shading
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Shading")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Geometry Nodes")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.t()     # Texture Paint
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Texture Paint")
 
-    yield from _call_by_name(e, "Next Workspace")
-    t.assertEqual(window.workspace.name_full, "Scripting")
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.g()     # General
+    yield e.u()     # UV Editing
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "UV Editing")
+
+
+def check_all_2d_animation_workspaces():
+    e, t = _test_vars(window := _test_window())
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.d()     # 2D Animation
+    yield e.d()     # 2D Animation
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "2D Animation")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.d()     # 2D Animation
+    yield e.f()     # 2D Full Canvas
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "2D Full Canvas")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.d()     # 2D Animation
+    yield e.c()     # Compositing
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Compositing")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.d()     # 2D Animation
+    yield e.r()     # Rendering
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Rendering")
+
+
+def check_all_sculpting_workspaces():
+    e, t = _test_vars(window := _test_window())
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.s()     # Sculpting
+    yield e.s()     # Sculpting
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Sculpting")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.s()     # Sculpting
+    yield e.h()     # Shading
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Shading")
+
+
+def check_all_vfx_workspaces():
+    e, t = _test_vars(window := _test_window())
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.v()     # VFX
+    yield e.c()     # Compositing
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Compositing")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.v()     # VFX
+    yield e.m()     # Masking
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Masking")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.v()     # VFX
+    yield e.t()     # Motion Tracking
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Motion Tracking")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.v()     # VFX
+    yield e.r()     # Rendering
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Rendering")
+
+
+def check_all_video_editing_workspaces():
+    e, t = _test_vars(window := _test_window())
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.e()     # Video Editing
+    yield e.r()     # Rendering
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Rendering")
+
+    yield from _call_by_name(e, "Add Workspace")
+    yield e.e()     # Video Editing
+    yield e.v()     # Video Editing
+    t.assertEqual(window.workspace.name_full.split('.', 1)[0], "Video Editing")
