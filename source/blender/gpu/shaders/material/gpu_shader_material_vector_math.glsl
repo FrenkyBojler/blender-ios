50eb14797f9572131945863c9d5fe95d1ae26b41
quackarooni <alfonsomartzii@gmail.com>
2025-05-31 09:56:03 +0800

    Fix: Hitting assert in debug mode
    The input parameter types in `gpu_shader_material_vector_math` are now `float3` instead of `vec3`.

--- source/blender/gpu/shaders/material/gpu_shader_material_vector_math.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_vector_math.glsl
@@ -6,7 +6,7 @@
 
 float3 vector_math_safe_normalize(float3 a)
 {
-  /* Match the safe normalize function in Cycles by defaulting to vec3(0.0f) */
+  /* Match the safe normalize function in Cycles by defaulting to float3(0.0f) */
   float length_sqr = length_squared(a);
   return (length_sqr > 1e-35f) ? a * inversesqrt(length_sqr) : float3(0.0f);
 }
@@ -131,12 +131,12 @@ void vector_math_absolute(
   outVector = abs(a);
 }
 
-void vector_math_power(vec3 a, vec3 b, vec3 c, float scale, out vec3 outVector, out float outValue)
+void vector_math_power(float3 a, float3 b, float3 c, float scale, out float3 outVector, out float outValue)
 {
   outVector = compatible_pow(a, b);
 }
 
-void vector_math_sign(vec3 a, vec3 b, vec3 c, float scale, out vec3 outVector, out float outValue)
+void vector_math_sign(float3 a, float3 b, float3 c, float scale, out float3 outVector, out float outValue)
 {
   outVector = sign(a);
 }
