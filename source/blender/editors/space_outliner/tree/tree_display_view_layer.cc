76beb8380a14238f832f3e43327942fbea86334c
Pratik Borhade <pratikborhade302@gmail.com>
2025-06-03 15:16:34 +0530

    Code comments

--- source/blender/editors/space_outliner/tree/tree_display_view_layer.cc
+++ source/blender/editors/space_outliner/tree/tree_display_view_layer.cc
@@ -16,8 +16,8 @@
 #include "BLI_listbase.h"
 #include "BLI_listbase_wrapper.hh"
 #include "BLI_map.hh"
-#include "BLI_vector.hh"
 #include "BLI_set.hh"
+#include "BLI_vector.hh"
 
 #include "../outliner_intern.hh"
 #include "common.hh"
@@ -33,7 +33,9 @@ class ObjectsChildrenBuilder {
 
   SpaceOutliner &outliner_;
   ObjectTreeElementsMap object_tree_elements_map_;
+  /* Stores object such that parents are before children. */
   Vector<Object *> ordered_objects_;
+  /* Unique object list to later check and store objects in ordered way. */
   Set<Object *> objects_in_ordered_objects_;
 
  public:
@@ -238,6 +240,8 @@ void ObjectsChildrenBuilder::object_tree_elements_lookup_create_recursive(TreeEl
  */
 void ObjectsChildrenBuilder::make_object_parent_hierarchy_collections()
 {
+  /* Ordered list (parent before children) is important. Otherwise, it is easy to miss objects that
+   * are linked with another collection. For details, see: !136971. */
   for (Object *ob : ordered_objects_) {
     if (ob->parent == nullptr) {
       continue;
