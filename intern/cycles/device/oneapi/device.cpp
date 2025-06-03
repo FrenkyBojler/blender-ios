dcc318be5ff9cc826ffebae941ebf85cca517f6d
Nikita Sirgienko <nikita.sirgienko@intel.com>
2025-06-03 16:46:50 +0200

    Using US English in the variable name.
    After successful review this commit would be rebased into
    "Cycles: oneAPI: Mark devices by their optimization status"

--- intern/cycles/device/oneapi/device.cpp
+++ intern/cycles/device/oneapi/device.cpp
@@ -99,7 +99,7 @@ static void device_iterator_cb(const char *id,
                                const int num,
                                bool hwrt_support,
                                bool oidn_support,
-                               bool has_execution_optimisation,
+                               bool has_execution_optimization,
                                void *user_ptr)
 {
   vector<DeviceInfo> *devices = (vector<DeviceInfo> *)user_ptr;
@@ -141,7 +141,7 @@ static void device_iterator_cb(const char *id,
   (void)hwrt_support;
 #  endif
 
-  info.has_execution_optimization = has_execution_optimisation;
+  info.has_execution_optimization = has_execution_optimization;
 
   devices->push_back(info);
   VLOG_INFO << "Added device \"" << info.description << "\" with id \"" << info.id << "\".";
