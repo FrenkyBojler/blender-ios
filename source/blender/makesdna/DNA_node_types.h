63e9ead59e99fb7bcf01fe9a0269d51cb687f629
Benjamin Beilharz <b@ben.graphics>
2025-06-04 17:28:16 +0200

    Refactor corner pin interpolation to use shared enum.
    Replaced `CMPNodeCornerPinInterpolation` with the generic `CMPNodeInterpolation` enum for consistency and deduplication. Removed the redundant `CMPNodeCornerPinInterpolation` definition as it is no longer necessary.

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -3010,14 +3010,6 @@ typedef enum CMPNodeInterpolation {
   CMP_NODE_INTERPOLATION_ANISOTROPIC = 3,
 } CMPNodeInterpolation;
 
-/* CornerPin node interpolation option. */
-typedef enum CMPNodeCornerPinInterpolation {
-  CMP_NODE_CORNER_PIN_INTERPOLATION_NEAREST = 0,
-  CMP_NODE_CORNER_PIN_INTERPOLATION_BILINEAR = 1,
-  CMP_NODE_CORNER_PIN_INTERPOLATION_BICUBIC = 2,
-  CMP_NODE_CORNER_PIN_INTERPOLATION_ANISOTROPIC = 3,
-} CMPNodeCornerPinInterpolation;
-
 /* Set Alpha Node. */
 
 /** #NodeSetAlpha.mode */
