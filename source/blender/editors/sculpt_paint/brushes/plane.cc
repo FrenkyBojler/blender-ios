955b38b3b754c8de298f7d941424ea718f6d995e
Hans Goudey <hans@blender.org>
2025-06-04 15:34:48 -0400

    Support plane brush

--- source/blender/editors/sculpt_paint/brushes/plane.cc
+++ source/blender/editors/sculpt_paint/brushes/plane.cc
@@ -38,6 +38,7 @@
 #include "editors/sculpt_paint/mesh_brush_common.hh"
 #include "editors/sculpt_paint/sculpt_automask.hh"
 #include "editors/sculpt_paint/sculpt_intern.hh"
+#include "editors/sculpt_paint/sculpt_nodes_evaluation.hh"
 
 #include "bmesh.hh"
 
@@ -239,6 +240,7 @@ static void calc_faces(const Depsgraph &depsgraph,
   auto_mask::calc_vert_factors(depsgraph, object, cache.automasking.get(), node, verts, factors);
 
   calc_brush_texture_factors(ss, brush, position_data.eval, verts, factors);
+  nodes_evaluate_factors_mesh(depsgraph, object, cache, position_data.eval, verts, factors);
 
   tls.translations.resize(verts.size());
   const MutableSpan<float3> translations = tls.translations;
@@ -292,6 +294,7 @@ static void calc_grids(const Depsgraph &depsgraph,
   auto_mask::calc_grids_factors(depsgraph, object, cache.automasking.get(), node, grids, factors);
 
   calc_brush_texture_factors(ss, brush, positions, factors);
+  nodes_evaluate_factors_grids(cache, subdiv_ccg, grids, positions, factors);
 
   tls.translations.resize(positions.size());
   const MutableSpan<float3> translations = tls.translations;
@@ -343,6 +346,7 @@ static void calc_bmesh(const Depsgraph &depsgraph,
   auto_mask::calc_vert_factors(depsgraph, object, cache.automasking.get(), node, verts, factors);
 
   calc_brush_texture_factors(ss, brush, positions, factors);
+  nodes_evaluate_factors_bmesh(cache, verts, positions, factors);
 
   tls.translations.resize(positions.size());
   const MutableSpan<float3> translations = tls.translations;
