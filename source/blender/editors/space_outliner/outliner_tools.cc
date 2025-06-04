cfe29deea23fc8c371ce0c6a4dcda40ac663a64b
Jacques Lucke <jacques@blender.org>
2025-06-04 10:46:05 +0200

    remove outliner operator to embed ids

--- source/blender/editors/space_outliner/outliner_tools.cc
+++ source/blender/editors/space_outliner/outliner_tools.cc
@@ -1621,27 +1621,6 @@ static void id_select_linked_fn(bContext *C,
   object::select_linked_by_id(C, id);
 }
 
-static void id_embed_linked(bContext *C,
-                            ReportList *reports,
-                            Scene * /*scene*/,
-                            TreeElement * /*te*/,
-                            TreeStoreElem * /*tsep*/,
-                            TreeStoreElem *tselem)
-{
-  ID *root_id = tselem->id;
-  if (!ID_IS_LINKED(root_id)) {
-    BKE_reportf(reports, RPT_ERROR, "Only linked IDs can be linked-embedded");
-    return;
-  }
-  if (ID_IS_LINKED_EMBEDDED(root_id)) {
-    /* Nothing to do. */
-    return;
-  }
-  Main *bmain = CTX_data_main(C);
-  blender::bke::library::embed_linked_id_hierarchy(*bmain, *root_id);
-  BKE_main_id_newptr_and_tag_clear(bmain);
-}
-
 static void singleuser_action_fn(bContext *C,
                                  ReportList * /*reports*/,
                                  Scene * /*scene*/,
@@ -2790,8 +2769,6 @@ enum eOutlinerIdOpTypes {
   OUTLINER_IDOP_RENAME,
 
   OUTLINER_IDOP_SELECT_LINKED,
-
-  OUTLINER_IDOP_EMBED_LINKED,
 };
 
 /* TODO: implement support for changing the ID-block used. */
@@ -2818,11 +2795,6 @@ static const EnumPropertyItem prop_id_op_types[] = {
     {OUTLINER_IDOP_FAKE_CLEAR, "CLEAR_FAKE", 0, "Clear Fake User", ""},
     {OUTLINER_IDOP_RENAME, "RENAME", 0, "Rename", ""},
     {OUTLINER_IDOP_SELECT_LINKED, "SELECT_LINKED", 0, "Select Linked", ""},
-    {OUTLINER_IDOP_EMBED_LINKED,
-     "EMBED_LINKED",
-     0,
-     "Embed Linked",
-     "Embed data-block and its dependencies into the current .blend file"},
     {0, nullptr, 0, nullptr, nullptr},
 };
 
@@ -3037,11 +3009,6 @@ static wmOperatorStatus outliner_id_operation_exec(bContext *C, wmOperator *op)
       ED_outliner_select_sync_from_all_tag(C);
       ED_undo_push(C, "Select");
       break;
-    case OUTLINER_IDOP_EMBED_LINKED: {
-      outliner_do_libdata_operation(C, op->reports, scene, space_outliner, id_embed_linked);
-      ED_undo_push(C, "Embed Linked");
-      break;
-    }
 
     default:
       /* Invalid - unhandled. */
