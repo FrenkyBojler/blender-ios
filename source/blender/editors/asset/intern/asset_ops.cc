4e8f6812a0604ffeececb530a15c84c69c2ed6ae
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-06-05 14:28:38 +0200

    use zero because max size 0 is also useless

--- source/blender/editors/asset/intern/asset_ops.cc
+++ source/blender/editors/asset/intern/asset_ops.cc
@@ -1030,7 +1030,7 @@ static void generate_previewimg_from_buffer(ID *id, const ImBuf *image_buffer)
     BKE_previewimg_ensure(preview_image, size_type);
     int width = image_buffer->x;
     int height = image_buffer->y;
-    int max_size = -1;
+    int max_size = 0;
     switch (size_type) {
       case ICON_SIZE_ICON:
         max_size = ICON_RENDER_DEFAULT_HEIGHT;
@@ -1039,7 +1039,7 @@ static void generate_previewimg_from_buffer(ID *id, const ImBuf *image_buffer)
         max_size = PREVIEW_RENDER_LARGE_HEIGHT;
         break;
     }
-    if (max_size < 0) {
+    if (max_size == 0) {
       /* Can only be reached if a new icon size is added. */
       BLI_assert_unreachable();
       continue;
