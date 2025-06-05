9052048e7eca8658ad6e5c2a4c2fc0aba9a15b6e
Falk David <falk@blender.org>
2025-06-05 11:28:25 +0200

    Actually fix it

--- source/blender/editors/undo/undo_system_types.cc
+++ source/blender/editors/undo/undo_system_types.cc
@@ -37,15 +37,9 @@ void ED_undosys_type_init()
   BKE_undosys_type_append(ED_lattice_undosys_type);
   BKE_undosys_type_append(ED_mball_undosys_type);
   BKE_undosys_type_append(ED_mesh_undosys_type);
-  if (U.uiflag & USER_EDIT_UNDO) {
-    BKE_undosys_type_append(curves::undosys_type_register);
-  }
-  if (U.uiflag & USER_EDIT_UNDO) {
-    BKE_undosys_type_append(pointcloud::undosys_type_register);
-  }
-  if (U.uiflag & USER_EDIT_UNDO) {
-    BKE_undosys_type_append(ED_undosys_type_grease_pencil);
-  }
+  BKE_undosys_type_append(curves::undosys_type_register);
+  BKE_undosys_type_append(pointcloud::undosys_type_register);
+  BKE_undosys_type_append(ED_undosys_type_grease_pencil);
 
   /* Paint Modes */
   BKE_UNDOSYS_TYPE_IMAGE = BKE_undosys_type_append(ED_image_undosys_type);
