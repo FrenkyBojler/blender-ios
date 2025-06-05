87bdcdfe2a7e6284c6514c06a3f9a3e93d0755e6
Falk David <falk@blender.org>
2025-06-05 18:11:06 +0200

    Flush edit data when encoding memfile undo step

--- source/blender/editors/undo/memfile_undo.cc
+++ source/blender/editors/undo/memfile_undo.cc
@@ -73,8 +73,8 @@ static bool memfile_undosys_step_encode(bContext * /*C*/, Main *bmain, UndoStep 
   /* Important we only use 'main' from the context (see: BKE_undosys_stack_init_from_main). */
   UndoStack *ustack = ED_undo_stack_get();
 
-  if (bmain->is_memfile_undo_flush_needed) {
-    ED_editors_flush_edits_ex(bmain, false, true);
+  if (bmain->is_memfile_undo_flush_needed || (U.uiflag & USER_EDIT_UNDO) == 0) {
+    ED_editors_flush_edits_ex(bmain, false, (U.uiflag & USER_EDIT_UNDO));
   }
 
   /* can be null, use when set. */
