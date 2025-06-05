daa058007c412ec63d74a56e13e799269f7755d5
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-06-05 16:11:50 +0200

    remove code from masks

--- source/blender/editors/mask/mask_ops.cc
+++ source/blender/editors/mask/mask_ops.cc
@@ -29,7 +29,6 @@
 #include "WM_api.hh"
 #include "WM_types.hh"
 
-#include "ED_anim_api.hh"
 #include "ED_clip.hh"
 #include "ED_image.hh"
 #include "ED_mask.hh"
@@ -872,7 +871,6 @@ static wmOperatorStatus slide_point_modal(bContext *C, wmOperator *op, const wmE
         /* Don't key sliding feather UW's. */
         if ((data->action == SLIDE_ACTION_FEATHER && data->uw) == false) {
           if (blender::animrig::is_autokey_on(scene)) {
-            ANIM_deselect_keys_in_animation_editors(C);
             ED_mask_layer_shape_auto_key(data->mask_layer, scene->r.cfra);
           }
         }
