ef07dbd74c9ac625524f63fea83b78daf1256571
Harley Acheson <harley.acheson@gmail.com>
2025-06-07 18:09:40 -0700

    Incorrect calc of each area type

--- source/blender/makesrna/intern/rna_screen.cc
+++ source/blender/makesrna/intern/rna_screen.cc
@@ -141,16 +141,16 @@ static void rna_Area_type_set(PointerRNA *ptr, int value)
 static int rna_Screen_primary_area_get(struct bScreen *_self, bContext *C)
 {
   wmWindow *win = CTX_wm_window(C);
-  int cursor[2] = {win->posx + 100, win->posy + 100};
-  ScrArea *area = BKE_screen_find_area_xy(_self, SPACE_TYPE_ANY, cursor);
+  int pos[2] = {20, 100};
+  ScrArea *area = BKE_screen_find_area_xy(_self, SPACE_TYPE_ANY, pos);
   return area ? area->spacetype : 0;
 }
 
 static int rna_Screen_secondary_area_get(struct bScreen *_self, bContext *C)
 {
   wmWindow *win = CTX_wm_window(C);
-  int cursor[2] = {win->posx + win->sizex - 50, win->posy + win->sizey - 100};
-  ScrArea *area = BKE_screen_find_area_xy(_self, SPACE_TYPE_ANY, cursor);
+  int pos[2] = {win->sizex - 20, win->sizey - 100};
+  ScrArea *area = BKE_screen_find_area_xy(_self, SPACE_TYPE_ANY, pos);
   return area ? area->spacetype : 0;
 }
 
