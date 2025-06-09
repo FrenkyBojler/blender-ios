d0dc23c7d4ff6fa36761f371fa2b8cb1749d8f27
Sean Kim <SeanCTKim@protonmail.com>
2025-06-09 13:14:55 -0700

    Remove unnecessary comment

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -7226,7 +7226,6 @@ void reset_translations_to_original(const MutableSpan<float3> translations,
   }
 }
 
-/** Helper function to assert on translations that attempt to propagate NaN */
 static bool contains_nan(const Span<float> values)
 {
   return std::any_of(values.begin(), values.end(), [&](const float v) { return std::isnan(v); });
