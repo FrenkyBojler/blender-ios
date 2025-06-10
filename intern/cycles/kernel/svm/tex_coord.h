f7b78a8e0959890ae3203b16959bb346e764ad78
Weizhen Huang <weizhen@blender.org>
2025-06-10 16:02:36 +0200

    Fix: Cycles: Normals from Normal Map can be NaN
    a degenerate triangle could produce a tangent that is antiparallel to
    the normal, resulting the mapped normal to be zero, and becomes NaN when
    normalized in `object_normal_transform()`. Fixed by falling back to
    unperturbed normal in this case.
    
    Fixes an assertion in the attic benchmark scene.

--- intern/cycles/kernel/svm/tex_coord.h
+++ intern/cycles/kernel/svm/tex_coord.h
@@ -395,6 +395,12 @@ ccl_device_noinline void svm_node_normal_map(KernelGlobals kg,
     const float3 B = sign * cross(normal, tangent);
     N = safe_normalize(to_global(color, tangent, B, normal));
 
+    if (is_zero(N)) {
+      /* Fall back to unperturbed normal. */
+      stack_store_float3(stack, normal_offset, sd->N);
+      return;
+    }
+
     /* transform to world space */
     object_normal_transform(kg, sd, &N);
 
