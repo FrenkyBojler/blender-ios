e01196e7ae8501f147946ee66e4f12331f544d96
Sean Kim <SeanCTKim@protonmail.com>
2025-06-10 20:27:34 -0700

    PR feedback

--- source/blender/editors/sculpt_paint/sculpt_automasking.cc
+++ source/blender/editors/sculpt_paint/sculpt_automasking.cc
@@ -366,9 +366,7 @@ static void calc_blurred_cavity_mesh(const Depsgraph &depsgraph,
   }
 
   const float3 vec = all_verts.position - verts_in_range.position;
-  float factor_sum = all_verts.distance == 0.0f ?
-                         0.0f :
-                         math::dot(vec, verts_in_range.normal) / all_verts.distance;
+  float factor_sum = math::safe_divide(math::dot(vec, verts_in_range.normal), all_verts.distance);
   cavity_factors[vert] = calc_cavity_factor(automasking, factor_sum);
 }
 
