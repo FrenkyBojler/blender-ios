385d54808833bbd6129d03ee53d402aaee94fa5a
Sean Kim <SeanCTKim@protonmail.com>
2025-06-10 20:35:40 -0700

    Revert "Fix #140120: Multiplane Scrape behaves incorrectly"
    This reverts commit 42b75c2cc46e333150dc58a0d8bee77971c25765.

--- source/blender/editors/sculpt_paint/brushes/multiplane_scrape.cc
+++ source/blender/editors/sculpt_paint/brushes/multiplane_scrape.cc
@@ -392,9 +392,6 @@ static void calc_faces(const Depsgraph &depsgraph,
   }
 
   calc_distances(local_positions, distances);
-  /* TODO: Using the radius for the filter here is probably too high, but due to the Y-axis
-   * deformation, a simple value of 1.0 isn't correct. */
-  filter_distances_with_radius(cache.radius, distances, factors);
 
   apply_hardness_to_distances(cache, distances);
   calc_brush_strength_factors(cache, brush, distances, factors);
@@ -454,9 +451,6 @@ static void calc_grids(const Depsgraph &depsgraph,
   }
 
   calc_distances(local_positions, distances);
-  /* TODO: Using the radius for the filter here is probably too high, but due to the Y-axis
-   * deformation, a simple value of 1.0 isn't correct. */
-  filter_distances_with_radius(cache.radius, distances, factors);
 
   apply_hardness_to_distances(cache, distances);
   calc_brush_strength_factors(cache, brush, distances, factors);
@@ -515,9 +509,6 @@ static void calc_bmesh(const Depsgraph &depsgraph,
   }
 
   calc_distances(local_positions, distances);
-  /* TODO: Using the radius for the filter here is probably too high, but due to the Y-axis
-   * deformation, a simple value of 1.0 isn't correct. */
-  filter_distances_with_radius(cache.radius, distances, factors);
 
   apply_hardness_to_distances(cache, distances);
   calc_brush_strength_factors(cache, brush, distances, factors);
