0225a1f795fecc58fd6d4c919208af8b76256a49
Sean Kim <SeanCTKim@protonmail.com>
2025-06-12 11:51:25 -0700

    Revert "Cleanup: Silence unused function warning"
    This reverts commit bd83c98565721753120de1e300db2ceb5c366811.

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -7226,7 +7226,7 @@ void reset_translations_to_original(const MutableSpan<float3> translations,
   }
 }
 
-bool contains_nan(const Span<float> values)
+static bool contains_nan(const Span<float> values)
 {
   return std::any_of(values.begin(), values.end(), [&](const float v) { return std::isnan(v); });
 }
