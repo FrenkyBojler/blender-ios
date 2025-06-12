0ad0e5f9b30cbda3bc2a7b2da5723615a750024f
Clément Foucault <foucault.clem@gmail.com>
2025-06-12 17:44:41 +0200

    EEVEE: Rename and move old gtao properties
    - Move `gtao_distance` to view layer and rename to
      `ambient_occlusion_distance` (API change).
    - Remove `gtao_quality` from the RNA (API change).
    - Remove `use_gtao` (unused) from the RNA (API change).
    - Rename `gtao_focus` to `fast_gi_bias` in the DNA (no API
      change).
    - Rename `gtao_resolution` to `fast_gi_resolution` in the
      DNA (no API change).

--- source/blender/blenloader/intern/versioning_410.cc
+++ source/blender/blenloader/intern/versioning_410.cc
@@ -931,14 +931,6 @@ void blo_do_versions_410(FileData *fd, Library * /*lib*/, Main *bmain)
       }
     }
 
-    if (!DNA_struct_member_exists(fd->filesdna, "SceneEEVEE", "float", "gtao_thickness")) {
-      SceneEEVEE default_eevee = *DNA_struct_default_get(SceneEEVEE);
-      LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-        scene->eevee.gtao_thickness = default_eevee.gtao_thickness;
-        scene->eevee.gtao_focus = default_eevee.gtao_focus;
-      }
-    }
-
     if (!DNA_struct_member_exists(fd->filesdna, "LightProbe", "float", "data_display_size")) {
       LightProbe default_probe = *DNA_struct_default_get(LightProbe);
       LISTBASE_FOREACH (LightProbe *, probe, &bmain->lightprobes) {
