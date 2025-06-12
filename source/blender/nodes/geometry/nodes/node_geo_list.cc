38d31d71ee859df22c4a1d084a2c567a02a526a0
Hans Goudey <hans@blender.org>
2025-06-12 16:40:48 -0400

    Format

--- source/blender/nodes/geometry/nodes/node_geo_list.cc
+++ source/blender/nodes/geometry/nodes/node_geo_list.cc
@@ -82,7 +82,7 @@ static void node_geo_exec(GeoNodeExecParams params)
   evaluator.evaluate();
 
   params.set_output(
-"List", nodes::ListPtr(MEM_new<List>(__func__, cpp_type, std::move(array_data), count)));
+      "List", nodes::ListPtr(MEM_new<List>(__func__, cpp_type, std::move(array_data), count)));
 }
 
 static void node_rna(StructRNA *srna)
