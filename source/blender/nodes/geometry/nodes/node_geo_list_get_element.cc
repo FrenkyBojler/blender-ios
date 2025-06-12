90c3cf1e7663bee2abb8c111df62bae0fc795a7a
Hans Goudey <hans@blender.org>
2025-06-12 16:47:32 -0400

    OOpsie

--- source/blender/nodes/geometry/nodes/node_geo_list_get_element.cc
+++ source/blender/nodes/geometry/nodes/node_geo_list_get_element.cc
@@ -18,7 +18,10 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   const bNode *node = b.node_or_null();
 
-  b.add_input<decl::Int>("Index").min(0).supports_field();
+  b.add_input<decl::Int>("Index")
+      .min(0)
+      .implicit_field(NODE_DEFAULT_INPUT_INDEX_FIELD)
+      .supports_field();
 
   if (node != nullptr) {
     const eNodeSocketDatatype type = eNodeSocketDatatype(node->custom1);
