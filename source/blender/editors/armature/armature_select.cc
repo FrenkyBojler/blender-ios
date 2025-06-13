075a50709a6896ffd5b902d4c9d93ca8d1415a0d
Philipp Oeser <philipp@blender.org>
2025-06-13 13:26:19 +0200

    make format

--- source/blender/editors/armature/armature_select.cc
+++ source/blender/editors/armature/armature_select.cc
@@ -657,8 +657,8 @@ static EditBone *get_nearest_editbonepoint(
   result_bias.base = nullptr;
   result_bias.ebone = nullptr;
 
-  /* Find the bone after the current (selected) active bone, so as to bump up its chances in selection.
-   * this way overlapping bones will cycle selection state as with objects. */
+  /* Find the bone after the current (selected) active bone, so as to bump up its chances in
+   * selection. this way overlapping bones will cycle selection state as with objects. */
   Object *obedit_orig = vc->obedit;
   EditBone *ebone_active_orig = static_cast<bArmature *>(obedit_orig->data)->act_edbone;
   if ((ebone_active_orig->flag & (BONE_SELECTED | BONE_ROOTSEL | BONE_TIPSEL)) == 0) {
