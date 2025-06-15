87c3980bdf3760305bc2599e8b6ccadc0eee1475
Sean Kim <SeanCTKim@protonmail.com>
2025-06-15 15:39:08 -0700

    make format

--- source/blender/blenloader/intern/versioning_290.cc
+++ source/blender/blenloader/intern/versioning_290.cc
@@ -12,8 +12,8 @@
 
 #include "BLI_listbase.h"
 #include "BLI_math_matrix.h"
-#include "BLI_math_vector.h"
 #include "BLI_math_rotation.h"
+#include "BLI_math_vector.h"
 #include "BLI_string.h"
 #include "BLI_utildefines.h"
 
