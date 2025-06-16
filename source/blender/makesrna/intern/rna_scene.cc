56773b6b21b8c935b9a6c102fc49691748ede12f
Habib Gahbiche <habib@blender.org>
2025-06-16 20:49:05 +0200

    Update tooltip

--- source/blender/makesrna/intern/rna_scene.cc
+++ source/blender/makesrna/intern/rna_scene.cc
@@ -7346,7 +7346,7 @@ static void rna_def_scene_render_data(BlenderRNA *brna)
   RNA_def_property_ui_text(prop,
                            "Compositing",
                            "Process the render result through the compositing pipeline, "
-                           "if compositing nodes are enabled");
+                           "if a compositing node group is assigned to the scene");
   RNA_def_property_update(prop, NC_SCENE | ND_RENDER_OPTIONS, nullptr);
 
   prop = RNA_def_property(srna, "use_sequencer", PROP_BOOLEAN, PROP_NONE);
