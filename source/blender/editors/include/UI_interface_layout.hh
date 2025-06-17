b647fad62ae2fe78865f58e7ec801324bf57a468
Guillermo Venegas <guillermovcra@gmail.com>
2025-06-17 15:16:23 -0600

    sort method

--- source/blender/editors/include/UI_interface_layout.hh
+++ source/blender/editors/include/UI_interface_layout.hh
@@ -130,11 +130,11 @@ struct uiLayout : uiItem {
   uiBlock *block() const;
 
   void context_copy(const bContextStore *context);
-  bContextStore *context_store() const;
   void context_set(blender::StringRef name, PointerRNA *ptr);
   void context_set(blender::StringRef name, blender::StringRef value);
   /** Only for convenience. */
   void context_set_from_but(uiBut *but);
+  bContextStore *context_store() const;
 
   bool enabled() const;
   /**
