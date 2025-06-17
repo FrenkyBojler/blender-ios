cac43f647510a686354d935cfb411408921a0139
Ramon Klauck <ramonklauck3@gmail.com>
2025-06-17 21:08:02 +0200

    move check if in sequencer out of context_mode swich

--- source/blender/editors/animation/keyframing.cc
+++ source/blender/editors/animation/keyframing.cc
@@ -302,14 +302,15 @@ static blender::Vector<RNAPath> construct_rna_paths(PointerRNA *ptr)
 static bool get_selection(bContext *C, blender::Vector<PointerRNA> *r_selection)
 {
   const eContextObjectMode context_mode = CTX_data_mode_enum(C);
+  ScrArea *area = CTX_wm_area(C);
+
+  if (area && area->spacetype == SPACE_SEQ) {
+    CTX_data_selected_strips(C, r_selection);
+    return true;
+  }
 
   switch (context_mode) {
     case CTX_MODE_OBJECT: {
-      ScrArea *area = CTX_wm_area(C);
-      if (area && area->spacetype == SPACE_SEQ) {
-        CTX_data_selected_strips(C, r_selection);
-        break;
-      }
       CTX_data_selected_objects(C, r_selection);
       break;
     }
