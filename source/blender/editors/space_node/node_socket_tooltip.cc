90a0160148b48fac8eac31e65464355e20fe8b67
Jacques Lucke <jacques@blender.org>
2025-06-17 11:07:38 +0200

    progress

--- source/blender/editors/space_node/node_socket_tooltip.cc
+++ source/blender/editors/space_node/node_socket_tooltip.cc
@@ -12,7 +12,19 @@
 
 namespace blender::ed::space_node {
 
-static std::optional<std::string> create_description_inspection_string(const bNodeSocket &socket)
+static void build_tooltip_label(uiTooltipData &tip_data, const bNodeSocket &socket)
+{
+  const StringRefNull translated_socket_label = node_socket_get_label(&socket, nullptr);
+  UI_tooltip_text_field_add(
+      tip_data, translated_socket_label, {}, UI_TIP_STYLE_HEADER, UI_TIP_LC_MAIN);
+}
+
+static void add_space(uiTooltipData &tip_data)
+{
+  UI_tooltip_text_field_add(tip_data, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
+}
+
+static std::optional<std::string> get_socket_description(const bNodeSocket &socket)
 {
   if (socket.runtime->declaration == nullptr) {
     if (socket.description[0]) {
@@ -29,27 +41,31 @@ static std::optional<std::string> create_description_inspection_string(const bNo
   return TIP_(description);
 }
 
+static void build_tooltip_description(uiTooltipData &tip_data, const bNodeSocket &socket)
+{
+  std::optional<std::string> description_opt = get_socket_description(socket);
+  if (!description_opt) {
+    return;
+  }
+  std::string description = std::move(*description_opt);
+  if (description.empty()) {
+    return;
+  }
+  if (description[description.size() - 1] != '.') {
+    description += '.';
+  }
+  UI_tooltip_text_field_add(
+      tip_data, std::move(description), {}, UI_TIP_STYLE_NORMAL, UI_TIP_LC_NORMAL);
+}
+
 void build_socket_tooltip(uiTooltipData &tip_data,
                           bContext & /*C*/,
                           const bNodeTree & /*tree*/,
                           const bNodeSocket &socket)
 {
-  /* Add label. */
-  const StringRefNull translated_socket_label = node_socket_get_label(&socket, nullptr);
-  UI_tooltip_text_field_add(
-      tip_data, translated_socket_label, {}, UI_TIP_STYLE_HEADER, UI_TIP_LC_MAIN);
-  UI_tooltip_text_field_add(tip_data, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
-
-  /* Add description. */
-  if (std::optional<std::string> description_opt = create_description_inspection_string(socket)) {
-    std::string description = std::move(*description_opt);
-    if (!description.empty()) {
-      if (description[description.size() - 1] != '.') {
-        description += '.';
-      }
-      UI_tooltip_text_field_add(tip_data, description, {}, UI_TIP_STYLE_NORMAL, UI_TIP_LC_NORMAL);
-    }
-  }
+  build_tooltip_label(tip_data, socket);
+  add_space(tip_data);
+  build_tooltip_description(tip_data, socket);
 
   /* Add last value. */
 
