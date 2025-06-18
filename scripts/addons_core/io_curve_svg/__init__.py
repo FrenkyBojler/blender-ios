1fd60ab2cba9e3f8401f5cc7d59e7c714fc93bbe
Guillermo Venegas <guillermovcra@gmail.com>
2025-06-18 09:00:31 -0600

    make name plural

--- scripts/addons_core/io_curve_svg/__init__.py
+++ scripts/addons_core/io_curve_svg/__init__.py
@@ -70,9 +70,9 @@ class ImportSVG(bpy.types.Operator, ImportHelper):
             return import_svg.load(self, context, filepath=self.filepath)
 
 
-class IMPORT_FH_svg_as_curve(bpy.types.FileHandler):
-    bl_idname = "IMPORT_FH_svg_as_curve"
-    bl_label = "Import SVG as curve"
+class IMPORT_FH_svg_as_curves(bpy.types.FileHandler):
+    bl_idname = "IMPORT_FH_svg_as_curves"
+    bl_label = "Import SVG as Curves"
     bl_import_operator = "import_curve.svg"
     bl_file_extensions = ".svg"
 
@@ -88,7 +88,7 @@ def menu_func_import(self, context):
 
 classes = [
     ImportSVG,
-    IMPORT_FH_svg_as_curve,
+    IMPORT_FH_svg_as_curves,
 ]
 
 
