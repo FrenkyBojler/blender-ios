7bdeaf5db17fe885fb1b721b35b1255907efaaee
YimingWu <xp8110@outlook.com>
2025-06-19 19:35:31 +0800

    Panel issue

--- scripts/startup/bl_ui/properties_view_layer.py
+++ scripts/startup/bl_ui/properties_view_layer.py
@@ -275,7 +275,7 @@ class VIEWLAYER_PT_override(ViewLayerButtonsPanel, Panel):
     bl_label = "Override"
     bl_options = {'DEFAULT_CLOSED'}
     COMPAT_ENGINES = {
-        'BLENDER_EEVEE',
+        'BLENDER_EEVEE_NEXT',
     }
 
     def draw(self, context):
