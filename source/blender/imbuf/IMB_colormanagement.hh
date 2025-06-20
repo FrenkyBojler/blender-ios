blender/blender

commit 7ceb4495c5a8e577441a57afed47febcfb4566ad

Refactor: OpenColorIO integration
Briefly about this change:
- OpenColorIO C-API is removed.
- The information about color spaces in ImBuf module is removed.
  It was stored in global ListBase in colormanagement.cc.
- Both OpenColorIO and fallback implementation supports GPU drawing.
- Fallback implementation supports white point, RGB curves, etc.
- Removed check for support of GPU drawing in IMB.

Historically it was implemented in a separate library with C-API, this
is because way back C++ code needed to stay in intern. This causes all
sort of overheads, and even calls that are strictly considered bad
level.

This change moves OpenColorIO integration into a module within imbuf,
next to movie, and next to IMB_colormanagement which is the main user
of it. This allows to avoid copy of color spaces, displays, views etc
in the ImBuf: they were used to help quickly querying information to
be shown on the interface. With this change it can be stored in the
same data structures as what is used by the OpenColorIO integration.
While it might not be fully avoiding duplication it is now less, and
there is no need in the user code to maintain the copies.

In a lot of cases this change also avoids allocations done per access
to the OpenColorIO. For example, it is not needed anymore to allocate
image descriptor in a heap.

The bigger user-visible change is that the fallback implementation now
supports GLSL drawing, with the whole list of supported features, such
as curve mapping and white point. This should help simplifying code
which relies on color space conversion on GPU: there is no need to
figure out fallback solution in such cases. The only case when drawing
will not work is when there is some actual bug, or driver issue, and
shader has failed to compile.

The change avoids having an opaque type for color space, and instead
uses forward declaration. It is a bit verbose on declaration, but helps
avoiding unsafe type-casts. There are ways to solve this in the future,

source/blender/imbuf/IMB_colormanagement.hh
@@ -24,8 +24,12 @@ struct ImageFormatData;
 struct Main;
 struct bContext;
 
-struct ColorManagedDisplay;
-struct ColorSpace;
+namespace blender::ocio {
+class ColorSpace;
+class Display;
+}  // namespace blender::ocio
+using ColorSpace = blender::ocio::ColorSpace;
+using ColorManagedDisplay = blender::ocio::Display;
 
 /* -------------------------------------------------------------------- */
 /** \name Generic Functions
@@ -47,10 +51,10 @@ const char *IMB_colormanagement_get_float_colorspace(ImBuf *ibuf);
 const char *IMB_colormanagement_get_rect_colorspace(ImBuf *ibuf);
 const char *IMB_colormanagement_space_from_filepath_rules(const char *filepath);
 
-ColorSpace *IMB_colormanagement_space_get_named(const char *name);
-bool IMB_colormanagement_space_is_data(ColorSpace *colorspace);
-bool IMB_colormanagement_space_is_scene_linear(ColorSpace *colorspace);
-bool IMB_colormanagement_space_is_srgb(ColorSpace *colorspace);
+const ColorSpace *IMB_colormanagement_space_get_named(const char *name);
+bool IMB_colormanagement_space_is_data(const ColorSpace *colorspace);
+bool IMB_colormanagement_space_is_scene_linear(const ColorSpace *colorspace);
+bool IMB_colormanagement_space_is_srgb(const ColorSpace *colorspace);
 bool IMB_colormanagement_space_name_is_data(const char *name);
 bool IMB_colormanagement_space_name_is_scene_linear(const char *name);
 bool IMB_colormanagement_space_name_is_srgb(const char *name);
@@ -145,25 +149,31 @@ void IMB_colormanagement_transform_v4(float pixel[4],
  * For performance, use #IMB_colormanagement_colorspace_to_scene_linear
  * when converting an array of pixels.
  */
-void IMB_colormanagement_colorspace_to_scene_linear_v3(float pixel[3], ColorSpace *colorspace);
+void IMB_colormanagement_colorspace_to_scene_linear_v3(float pixel[3],
+                                                       const ColorSpace *colorspace);
 void IMB_colormanagement_colorspace_to_scene_linear_v4(float pixel[4],
                                                        bool predivide,
-                                                       ColorSpace *colorspace);
+                                                       const ColorSpace *colorspace);
 
 /**
  * Convert pixel from scene linear space to specified color space.
  * For performance, use #IMB_colormanagement_scene_linear_to_colorspace
  * when converting an array of pixels.
  */
-void IMB_colormanagement_scene_linear_to_colorspace_v3(float pixel[3], ColorSpace *colorspace);
+void IMB_colormanagement_scene_linear_to_colorspace_v3(float pixel[3],
+                                                       const ColorSpace *colorspace);
 
 /**
  * Converts a (width)x(height) block of float pixels from given color space to
  * scene linear space. This is much higher performance than converting pixels
  * one by one.
  */
-void IMB_colormanagement_colorspace_to_scene_linear(
-    float *buffer, int width, int height, int channels, ColorSpace *colorspace, bool predivide);
+void IMB_colormanagement_colorspace_to_scene_linear(float *buffer,
+                                                    int width,
+                                                    int height,
+                                                    int channels,
+                                                    const ColorSpace *colorspace,
+                                                    bool predivide);
 
 /**
  * Converts a (width)x(height) block of float pixels from scene linear space
@@ -171,7 +181,7 @@ void IMB_colormanagement_colorspace_to_scene_linear(
  * one by one.
  */
 void IMB_colormanagement_scene_linear_to_colorspace(
-    float *buffer, int width, int height, int channels, ColorSpace *colorspace);
+    float *buffer, int width, int height, int channels, const ColorSpace *colorspace);
 
 void IMB_colormanagement_imbuf_to_byte_texture(unsigned char *out_buffer,
                                                int offset_x,
@@ -216,12 +226,14 @@ BLI_INLINE void IMB_colormanagement_srgb_to_scene_linear_v3(float scene_linear[3
  * used by performance-critical areas such as color-related widgets where we want to reduce
  * amount of per-widget allocations.
  */
-void IMB_colormanagement_scene_linear_to_display_v3(float pixel[3], ColorManagedDisplay *display);
+void IMB_colormanagement_scene_linear_to_display_v3(float pixel[3],
+                                                    const ColorManagedDisplay *display);
 /**
  * Same as #IMB_colormanagement_scene_linear_to_display_v3,
  * but converts color in opposite direction.
  */
-void IMB_colormanagement_display_to_scene_linear_v3(float pixel[3], ColorManagedDisplay *display);
+void IMB_colormanagement_display_to_scene_linear_v3(float pixel[3],
+                                                    const ColorManagedDisplay *display);
 
 void IMB_colormanagement_pixel_to_display_space_v4(
     float result[4],
@@ -311,10 +323,10 @@ const char *IMB_colormanagement_display_get_default_name();
 /**
  * Used by performance-critical pixel processing areas, such as color widgets.
  */
-ColorManagedDisplay *IMB_colormanagement_display_get_named(const char *name);
+const ColorManagedDisplay *IMB_colormanagement_display_get_named(const char *name);
 const char *IMB_colormanagement_display_get_none_name();
 const char *IMB_colormanagement_display_get_default_view_transform_name(
-    ColorManagedDisplay *display);
+    const ColorManagedDisplay *display);
 
 /** \} */
 
@@ -322,8 +334,8 @@ const char *IMB_colormanagement_display_get_default_view_transform_name(
 /** \name View Functions
  * \{ */
 
-int IMB_colormanagement_view_get_named_index(const char *name);
-const char *IMB_colormanagement_view_get_indexed_name(int index);
+int IMB_colormanagement_view_get_named_index(const char *display, const char *name);
+const char *IMB_colormanagement_view_get_indexed_name(const char *display, int index);
 
 /** \} */
 
@@ -442,10 +454,6 @@ void IMB_colormanagement_processor_free(ColormanageProcessor *cm_processor);
  * \{ */
 
 /**
- * Test if GLSL drawing is supported for combination of graphics card and this configuration.
- */
-bool IMB_colormanagement_support_glsl_draw(const ColorManagedViewSettings *view_settings);
-/**
  * Configures GLSL shader for conversion from scene linear to display space.
  */
 bool IMB_colormanagement_setup_glsl_draw(const ColorManagedViewSettings *view_settings,
@@ -471,7 +479,7 @@ bool IMB_colormanagement_setup_glsl_draw(const ColorManagedViewSettings *view_se
 bool IMB_colormanagement_setup_glsl_draw_from_space(
     const ColorManagedViewSettings *view_settings,
     const ColorManagedDisplaySettings *display_settings,
-    ColorSpace *from_colorspace,
+    const ColorSpace *from_colorspace,
     float dither,
     bool predivide,
     bool do_overlay_merge);
@@ -484,7 +492,7 @@ bool IMB_colormanagement_setup_glsl_draw_ctx(const bContext *C, float dither, bo
  * but color management settings are guessing from a given context.
  */
 bool IMB_colormanagement_setup_glsl_draw_from_space_ctx(const bContext *C,
-                                                        ColorSpace *from_colorspace,
+                                                        const ColorSpace *from_colorspace,
                                                         float dither,
                                                         bool predivide);
 

source/blender/makesrna/intern/rna_color.cc
@@ -495,16 +495,28 @@ static void rna_ColorManagedDisplaySettings_display_device_update(Main *bmain,
 
 static int rna_ColorManagedViewSettings_view_transform_get(PointerRNA *ptr)
 {
+  const ID *id = ptr->owner_id;
+  BLI_assert(GS(id->name) == ID_SCE);
+
+  const Scene *scene = reinterpret_cast<const Scene *>(id);
+
   ColorManagedViewSettings *view = (ColorManagedViewSettings *)ptr->data;
 
-  return IMB_colormanagement_view_get_named_index(view->view_transform);
+  return IMB_colormanagement_view_get_named_index(scene->display_settings.display_device,
+                                                  view->view_transform);
 }
 
 static void rna_ColorManagedViewSettings_view_transform_set(PointerRNA *ptr, int value)
 {
+  const ID *id = ptr->owner_id;
+  BLI_assert(GS(id->name) == ID_SCE);
+
+  const Scene *scene = reinterpret_cast<const Scene *>(id);
+
   ColorManagedViewSettings *view = (ColorManagedViewSettings *)ptr->data;
 
-  const char *view_name = IMB_colormanagement_view_get_indexed_name(value);
+  const char *view_name = IMB_colormanagement_view_get_indexed_name(
+      scene->display_settings.display_device, value);
   if (!view_name) {
     return;
   }

FrenkyBojler/blender-ios

commit 5e7b40b610b49b58fe31d43f49f317197a3d8b3f

Fix #140693: Crash on enabling override in file output color settings
The code around view transform selection accidentally got too coupled
to the scene. However, the file output settings do not have scene, so
the enumerator getter was crashing.

Caused by 7ceb4495c5.

This patch brings the code closer to the Blender 4.4 behavior by
maintaining a global from view name to an ID.

There is still a bug in the code which displays views from the active
scene's display settings in the override panel. This is because the
itemf() callback is hardcoded to use the display settings from the
active scene (via bContext). However, this issue exists since the
initial commit of the color management override option.

source/blender/imbuf/IMB_colormanagement.hh
@@ -334,8 +334,8 @@ const char *IMB_colormanagement_display_get_default_view_transform_name(
 /** \name View Functions
  * \{ */
 
-int IMB_colormanagement_view_get_named_index(const char *display, const char *name);
-const char *IMB_colormanagement_view_get_indexed_name(const char *display, int index);
+int IMB_colormanagement_view_get_id_by_name(const char *name);
+const char *IMB_colormanagement_view_get_name_by_id(int index);
 
 /** \} */
 

source/blender/imbuf/intern/colormanagement.cc
@@ -39,6 +39,7 @@
 #include "BLI_string.h"
 #include "BLI_task.hh"
 #include "BLI_threads.h"
+#include "BLI_vector_set.hh"
 
 #include "BKE_appdir.hh"
 #include "BKE_colortools.hh"
@@ -67,6 +68,7 @@ namespace math = blender::math;
  * \{ */
 
 static std::unique_ptr<ocio::Config> g_config = nullptr;
+static blender::VectorSet<blender::StringRefNull> g_all_view_names;
 
 #define DISPLAY_BUFFER_CHANNELS 4
 
@@ -307,12 +309,10 @@ static void colormanage_cachedata_set(ImBuf *ibuf, ColormanageCacheData *data)
 
 static void colormanage_view_settings_to_cache(ImBuf *ibuf,
                                                ColormanageCacheViewSettings *cache_view_settings,
-                                               const ColorManagedViewSettings *view_settings,
-                                               const ColorManagedDisplaySettings *display_settings)
+                                               const ColorManagedViewSettings *view_settings)
 {
   int look = IMB_colormanagement_look_get_named_index(view_settings->look);
-  int view = IMB_colormanagement_view_get_named_index(display_settings->display_device,
-                                                      view_settings->view_transform);
+  int view = IMB_colormanagement_view_get_id_by_name(view_settings->view_transform);
 
   cache_view_settings->look = look;
   cache_view_settings->view = view;
@@ -542,13 +542,19 @@ static bool colormanage_load_config(ocio::Config &config)
 
   for (const int display_index : blender::IndexRange(g_config->get_num_displays())) {
     const ocio::Display *display = g_config->get_display_by_index(display_index);
-    if (display->get_num_views() <= 0) {
+    const int num_views = display->get_num_views();
+    if (num_views <= 0) {
       if (!G.quiet) {
         printf("Color management: Error, could not find any views for display %s\n",
                display->name().c_str());
       }
       ok = false;
       break;
+    }
+
+    for (const int view_index : blender::IndexRange(num_views)) {
+      const ocio::View *view = display->get_view_by_index(view_index);
+      g_all_view_names.add(view->name());
     }
   }
 
@@ -571,6 +577,7 @@ static bool colormanage_load_config(ocio::Config &config)
 static void colormanage_free_config()
 {
   g_config = nullptr;
+  g_all_view_names.clear();
 }
 
 void colormanagement_init()
@@ -935,7 +942,8 @@ static void colormanage_check_view_settings(ColorManagedDisplaySettings *display
     else if (!colormanage_compatible_look(look, view_settings->view_transform)) {
       if (!G.quiet) {
         printf(
-            "Color management: %s look \"%s\" is not compatible with view \"%s\", setting default "
+            "Color management: %s look \"%s\" is not compatible with view \"%s\", setting "
+            "default "
             "\"%s\".\n",
             what,
             view_settings->look,
@@ -2321,8 +2329,8 @@ ImBuf *IMB_colormanagement_imbuf_for_write(ImBuf *ibuf,
    * it leads to bad artifacts especially when saving byte images.
    *
    * What we do here is we're overlaying our image on top of background color (which is currently
-   * black). This is quite much the same as what Gimp does and it seems to be what artists expects
-   * from saving.
+   * black). This is quite much the same as what Gimp does and it seems to be what artists
+   * expects from saving.
    *
    * Do a conversion here, so image format writers could happily assume all the alpha tricks were
    * made already. helps keep things locally here, not spreading it to all possible image writers
@@ -2483,8 +2491,7 @@ uchar *IMB_display_buffer_acquire(ImBuf *ibuf,
     }
   }
 
-  colormanage_view_settings_to_cache(
-      ibuf, &cache_view_settings, applied_view_settings, display_settings);
+  colormanage_view_settings_to_cache(ibuf, &cache_view_settings, applied_view_settings);
   colormanage_display_settings_to_cache(&cache_display_settings, display_settings);
 
   if (ibuf->invalid_rect.xmin != ibuf->invalid_rect.xmax) {
@@ -2685,34 +2692,22 @@ const char *IMB_colormanagement_display_get_default_view_transform_name(
 /** \name View Functions
  * \{ */
 
-int IMB_colormanagement_view_get_named_index(const char *display, const char *name)
+int IMB_colormanagement_view_get_id_by_name(const char *name)
 {
-  const ocio::Display *ocio_display = g_config->get_display_by_name(display);
-  if (!ocio_display) {
-    return -1;
-  }
-
-  const ocio::View *view = ocio_display->get_view_by_name(name);
-  if (!view) {
-    return -1;
-  }
-
-  return view->index;
+  return g_all_view_names.index_of(name);
 }
 
-const char *IMB_colormanagement_view_get_indexed_name(const char *display, const int index)
+const char *IMB_colormanagement_view_get_name_by_id(const int index)
 {
-  const ocio::Display *ocio_display = g_config->get_display_by_name(display);
-  if (!ocio_display) {
+  /* The code is expected to be used for the purposes of the EnumPropertyItem and maintaining the
+   * DNA values from RNA's get() and set(). It is unexpected that an invalid index will be passed
+   * here, as it will indicate a coding error somewhere else. */
+  if (index < 0 || index >= g_all_view_names.size()) {
+    BLI_assert(0);
     return "";
   }
 
-  const ocio::View *view = ocio_display->get_view_by_index(index);
-  if (!view) {
-    return "";
-  }
-
-  return view->name().c_str();
+  return g_all_view_names[index].c_str();
 }
 
 const char *IMB_colormanagement_view_get_default_name(const char *display_name)
@@ -2913,7 +2908,7 @@ void IMB_colormanagement_view_items_add(EnumPropertyItem **items,
 
     EnumPropertyItem item;
 
-    item.value = view->index;
+    item.value = IMB_colormanagement_view_get_id_by_name(view->name().c_str());
     item.name = view->name().c_str();
     item.identifier = view->name().c_str();
     item.icon = 0;
@@ -3160,8 +3155,7 @@ static void imb_partial_display_buffer_update_ex(
   if (ibuf->display_buffer_flags) {
     int view_flag, display_index;
 
-    colormanage_view_settings_to_cache(
-        ibuf, &cache_view_settings, view_settings, display_settings);
+    colormanage_view_settings_to_cache(ibuf, &cache_view_settings, view_settings);
     colormanage_display_settings_to_cache(&cache_display_settings, display_settings);
 
     view_flag = 1 << cache_view_settings.view;

source/blender/makesrna/intern/rna_color.cc
@@ -495,28 +495,15 @@ static void rna_ColorManagedDisplaySettings_display_device_update(Main *bmain,
 
 static int rna_ColorManagedViewSettings_view_transform_get(PointerRNA *ptr)
 {
-  const ID *id = ptr->owner_id;
-  BLI_assert(GS(id->name) == ID_SCE);
-
-  const Scene *scene = reinterpret_cast<const Scene *>(id);
-
   ColorManagedViewSettings *view = (ColorManagedViewSettings *)ptr->data;
-
-  return IMB_colormanagement_view_get_named_index(scene->display_settings.display_device,
-                                                  view->view_transform);
+  return IMB_colormanagement_view_get_id_by_name(view->view_transform);
 }
 
 static void rna_ColorManagedViewSettings_view_transform_set(PointerRNA *ptr, int value)
 {
-  const ID *id = ptr->owner_id;
-  BLI_assert(GS(id->name) == ID_SCE);
-
-  const Scene *scene = reinterpret_cast<const Scene *>(id);
-
   ColorManagedViewSettings *view = (ColorManagedViewSettings *)ptr->data;
 
-  const char *view_name = IMB_colormanagement_view_get_indexed_name(
-      scene->display_settings.display_device, value);
+  const char *view_name = IMB_colormanagement_view_get_name_by_id(value);
   if (!view_name) {
     return;
   }