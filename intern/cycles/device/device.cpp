3b0c3c2a04ddbb94d854ee526e49b737875aa40e
Brecht Van Lommel <brecht@blender.org>
2025-06-20 16:26:33 +0200

    Fix: Wrong Cycles NanoVDB memory alignment on Windows
    This was not a problem in practice so far, but will be with upcoming changes.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/132908

--- intern/cycles/device/device.cpp
+++ intern/cycles/device/device.cpp
@@ -498,7 +498,7 @@ OSLGlobals *Device::get_cpu_osl_memory()
 
 void *Device::host_alloc(const MemoryType /*type*/, const size_t size)
 {
-  return util_aligned_malloc(size, MIN_ALIGNMENT_CPU_DATA_TYPES);
+  return util_aligned_malloc(size, MIN_ALIGNMENT_DEVICE_MEMORY);
 }
 
 void Device::host_free(const MemoryType /*type*/, void *host_pointer, const size_t size)
