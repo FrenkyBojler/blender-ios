2f5d59207e9afd37040a7d9e0787493c0eb4d490
Benjamin Beilharz <b@ben.graphics>
2025-06-23 22:56:35 +0200

    Added tooltips and naming to nodes

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -571,9 +571,21 @@ const EnumPropertyItem rna_enum_geometry_nodes_linear_gizmo_draw_style_items[] =
 
 #ifndef RNA_RUNTIME
 static const EnumPropertyItem cmp_border_condition_items[] = {
-    {CMP_NODE_BORDER_CONDITION_ZERO, "ZERO", 0, "Zero", "Use zero padding"},
-    {CMP_NODE_BORDER_CONDITION_REPEAT, "REPEAT", 0, "Repeat", "Repeat border pixel"},
-    {CMP_NODE_BORDER_CONDITION_EXTEND, "EXTEND", 0, "Extend", "Extend border pixel"},
+    {CMP_NODE_BORDER_CONDITION_ZERO,
+     "ZERO",
+     0,
+     "Zero",
+     "Pads the domain exterior with zero values."},
+    {CMP_NODE_BORDER_CONDITION_EXTEND,
+     "EXTEND",
+     0,
+     "Extend",
+     "Pads the domain exterior with the color value at the border."},
+    {CMP_NODE_BORDER_CONDITION_REPEAT,
+     "REPEAT",
+     0,
+     "Repeat",
+     "Pads the domain exterior with image copies."},
     {0, nullptr, 0, nullptr, nullptr},
 };
 
@@ -7472,15 +7484,13 @@ static void def_cmp_translate(BlenderRNA * /*brna*/, StructRNA *srna)
   prop = RNA_def_property(srna, "border_condition_x", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_sdna(prop, nullptr, "border_condition_x");
   RNA_def_property_enum_items(prop, cmp_border_condition_items);
-  // TODO: requires some naming of UI label
-  RNA_def_property_ui_text(prop, "", "");
+  RNA_def_property_ui_text(prop, "X", "The border condition applied to the X axis.");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_update");
 
   prop = RNA_def_property(srna, "border_condition_y", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_sdna(prop, nullptr, "border_condition_y");
   RNA_def_property_enum_items(prop, cmp_border_condition_items);
-  // TODO: requires some naming of UI label
-  RNA_def_property_ui_text(prop, "", "");
+  RNA_def_property_ui_text(prop, "Y", "The border condition applied to the Y axis.");
   RNA_def_property_update(prop, NC_NODE | NA_EDITED, "rna_Node_update");
 }
 
