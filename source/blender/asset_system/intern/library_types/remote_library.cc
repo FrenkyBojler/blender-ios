d7c3bf584d7e94fb9570840286220802b3bf1abc
Julian Eisel <julian@blender.org>
2025-06-23 21:36:26 +0200

    Cleanup: Improve comment

--- source/blender/asset_system/intern/library_types/remote_library.cc
+++ source/blender/asset_system/intern/library_types/remote_library.cc
@@ -56,9 +56,10 @@ void RemoteAssetLibrary::refresh_catalogs()
 /*
  * Note: Some of the status setters here only modify status if the current status is
  * #RemoteLibraryLoadingStatus::Loading. That is done to avoid status changes after loading ended,
- * mostly after a timeout. E.g. if the status is failed after a timeout, but Python is actually
- * still processing items, it might send a #RemoteLibraryLoadingStatus::set_finished(). The UI
- * would stop displaying an error on the next redraw, even though C++ side loading was aborted.
+ * mostly after a timeout. E.g. after a timeout of the C++ status because Python didn't send status
+ * updates, Python might eventually resume sending updates. These shouldn't affect the C++ status
+ * anymore, since the earlier failure aborted the C++ side asset library loading. Plus, the UI
+ * might show an error, and would suddenly switch to showing an incompletly loaded asset library.
  */
 
 using UrlToLibraryStatusMap = Map<std::string /*url*/, asset_system::RemoteLibraryLoadingStatus>;
