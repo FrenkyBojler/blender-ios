d0f1eb85488679723367532d0dd9a9e0373e21ec
Habib Gahbiche <habib@blender.org>
2025-06-24 16:44:41 +0200

    Bump version

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -1185,7 +1185,7 @@ void blo_do_versions_500(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 29)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 28)) {
     FOREACH_NODETREE_BEGIN (bmain, node_tree, id) {
       if (node_tree->type == NTREE_COMPOSIT) {
         LISTBASE_FOREACH (bNode *, node, &node_tree->nodes) {
