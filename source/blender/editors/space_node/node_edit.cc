83a08c71ef94a4e1d9a062f48f3cf292cd49ba76
Omar Emara <mail@OmarEmara.dev>
2025-06-24 17:28:08 +0300

    Fix: Compositor redundantly executes if previews are enabled
    The compositor executes redundantly if previews are enabled but no
    other output is computed. This shouldn't be the case because previews
    are secondary outputs that need a primary output to compute with.
    
    We fix this by only executing the compositor if a primary output is
    needed, regardless if previews are needed or not.

--- source/blender/editors/space_node/node_edit.cc
+++ source/blender/editors/space_node/node_edit.cc
@@ -38,6 +38,7 @@
 #include "BLI_math_vector.hh"
 #include "BLI_string.h"
 #include "BLI_string_utf8.h"
+#include "BLI_utildefines.h"
 
 #include "BLT_translation.hh"
 
@@ -436,8 +437,13 @@ static blender::compositor::OutputTypes get_compositor_needed_outputs(const bCon
 
 void ED_node_composite_job(const bContext *C, bNodeTree *nodetree, Scene *scene_owner)
 {
+  /* None of the outputs are needed, so no need to execute the compositor. Except maybe previews
+   * which is a secondary output that needs another output to compute. */
   blender::compositor::OutputTypes needed_outputs = get_compositor_needed_outputs(C);
-  if (needed_outputs == blender::compositor::OutputTypes::None) {
+  if (ELEM(needed_outputs,
+           blender::compositor::OutputTypes::None,
+           blender::compositor::OutputTypes::Previews))
+  {
     return;
   }
 
