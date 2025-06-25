0fab25716ba72559879c8a68a979533a807f122f
Harley Acheson <harley.acheson@gmail.com>
2025-06-25 12:26:57 -0700

    Use Pablo's suggestion for transition time. Separate out the editor highlight from edge drawing.

--- source/blender/editors/screen/screen_draw.cc
+++ source/blender/editors/screen/screen_draw.cc
@@ -108,20 +108,8 @@ static void drawscredge_area(const ScrArea &area, float edge_thickness)
   GPU_batch_draw(batch);
 }
 
-void ED_screen_draw_edges(wmWindow *win)
+static void screen_draw_editor_outlines(bScreen *screen, wmWindow *win)
 {
-  bScreen *screen = WM_window_get_active_screen(win);
-  screen->do_draw = false;
-
-  if (screen->state != SCREENNORMAL) {
-    return;
-  }
-
-  if (BLI_listbase_is_single(&screen->areabase) && win->global_areas.areabase.first == nullptr) {
-    /* Do not show edges on windows without global areas and with only one editor. */
-    return;
-  }
-
   ARegion *region = screen->active_region;
   ScrArea *active_area = nullptr;
 
@@ -149,6 +137,61 @@ void ED_screen_draw_edges(wmWindow *win)
     if (active_area && !BLI_listbase_is_empty(&win->drawcalls)) {
       active_area = nullptr;
     }
+  }
+
+  float col_inactive[4];
+  float col_active[4];
+  float col_active_last[4];
+  rctf bounds;
+  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE, col_inactive);
+  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE, col_active_last);
+  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE_ACTIVE, col_active);
+
+  static ScrArea *current_active_area = nullptr;
+  static ScrArea *last_active_area = nullptr;
+  double now = BLI_time_now_seconds();
+  static double start_time = now;
+  float factor = 1.0f;
+
+  if (active_area != current_active_area) {
+    if (now > start_time + AREA_ACTIVE_FADEIN) {
+      start_time = now;
+    }
+    last_active_area = current_active_area;
+    current_active_area = active_area;
+  }
+
+  const double end_time = start_time + AREA_ACTIVE_FADEIN;
+  if (now < end_time) {
+    factor = pow((now - start_time) / (end_time - start_time), 2);
+    UI_GetThemeColorBlend4f(TH_EDITOR_OUTLINE, TH_EDITOR_OUTLINE_ACTIVE, factor, col_active);
+    UI_GetThemeColorBlend4f(TH_EDITOR_OUTLINE_ACTIVE, TH_EDITOR_OUTLINE, factor, col_active_last);
+    screen->do_refresh = true;
+  }
+
+  UI_draw_roundbox_corner_set(UI_CNR_ALL);
+  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+    BLI_rctf_rcti_copy(&bounds, &area->totrct);
+    float *color = (area == active_area) ? col_active : col_inactive;
+    if (area == last_active_area) {
+      color = col_active_last;
+    }
+    UI_draw_roundbox_4fv_ex(&bounds, nullptr, nullptr, 1.0f, color, U.pixelsize, EDITORRADIUS);
+  }
+}
+
+void ED_screen_draw_edges(wmWindow *win)
+{
+  bScreen *screen = WM_window_get_active_screen(win);
+  screen->do_draw = false;
+
+  if (screen->state != SCREENNORMAL) {
+    return;
+  }
+
+  if (BLI_listbase_is_single(&screen->areabase) && win->global_areas.areabase.first == nullptr) {
+    /* Do not show edges on windows without global areas and with only one editor. */
+    return;
   }
 
   rcti scissor_rect;
@@ -196,48 +239,10 @@ void ED_screen_draw_edges(wmWindow *win)
     drawscredge_area(*area, edge_thickness);
   }
 
-  float col_inactive[4];
-  float col_active[4];
-  float col_active_last[4];
-  rctf bounds;
-  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE, col_inactive);
-  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE, col_active_last);
-  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE_ACTIVE, col_active);
-
-  static ScrArea *current_active_area = nullptr;
-  static ScrArea *last_active_area = nullptr;
-  double now = BLI_time_now_seconds();
-  static double start_time = now;
-  float factor = 1.0f;
-
-  if (active_area != current_active_area) {
-    if (now > start_time + AREA_ACTIVE_FADEIN) {
-      start_time = now;
-    }
-    last_active_area = current_active_area;
-    current_active_area = active_area;
-  }
-
-  const double end_time = start_time + AREA_ACTIVE_FADEIN;
-  if (now < end_time) {
-    factor = pow((now - start_time) / (end_time - start_time), 2);
-    UI_GetThemeColorBlend4f(TH_EDITOR_OUTLINE, TH_EDITOR_OUTLINE_ACTIVE, factor, col_active);
-    UI_GetThemeColorBlend4f(TH_EDITOR_OUTLINE_ACTIVE, TH_EDITOR_OUTLINE, factor, col_active_last);
-    screen->do_refresh = true;
-  }
-
-  UI_draw_roundbox_corner_set(UI_CNR_ALL);
-  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
-    BLI_rctf_rcti_copy(&bounds, &area->totrct);
-    float *color = (area == active_area) ? col_active : col_inactive;
-    if (area == last_active_area) {
-      color = col_active_last;
-    }
-    UI_draw_roundbox_4fv_ex(&bounds, nullptr, nullptr, 1.0f, color, U.pixelsize, EDITORRADIUS);
-  }
-
   GPU_blend(GPU_BLEND_NONE);
   GPU_scissor_test(false);
+
+  screen_draw_editor_outlines(screen, win);
 }
 
 void screen_draw_move_highlight(const wmWindow *win, bScreen *screen, eScreenAxis dir_axis)
