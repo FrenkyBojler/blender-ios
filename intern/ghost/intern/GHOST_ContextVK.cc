5cc262c683a941e0c1ebf93ecd058bdcd68275ed
Jeroen Bakker <jeroen@blender.org>
2025-06-26 15:58:19 +0200

    Test

--- intern/ghost/intern/GHOST_ContextVK.cc
+++ intern/ghost/intern/GHOST_ContextVK.cc
@@ -1073,13 +1073,10 @@ GHOST_TSuccess GHOST_ContextVK::recreateSwapchain()
   GHOST_FrameDiscard &discard_pile = m_frame_data[m_render_frame].discard_pile;
   for (GHOST_SwapchainImage &swapchain_image : m_swapchain_images) {
     swapchain_image.vk_image = VK_NULL_HANDLE;
-    if (!vulkan_device->use_vk_ext_swapchain_maintenance_1 &&
-        swapchain_image.present_semaphore != VK_NULL_HANDLE)
-    {
+    if (swapchain_image.present_semaphore != VK_NULL_HANDLE) {
       discard_pile.semaphores.push_back(swapchain_image.present_semaphore);
       swapchain_image.present_semaphore = VK_NULL_HANDLE;
     }
-    swapchain_image.vk_image = VK_NULL_HANDLE;
   }
   m_swapchain_images.resize(actual_image_count);
   std::vector<VkImage> swapchain_images(actual_image_count);
@@ -1208,17 +1205,8 @@ GHOST_TSuccess GHOST_ContextVK::initializeDrawingContext()
     optional_device_extensions.push_back(VK_EXT_SWAPCHAIN_COLOR_SPACE_EXTENSION_NAME);
 
     /* X11 doesn't use the correct swapchain offset, flipping can squash the first frames. */
-    const bool use_swapchain_maintenance1 = false;
-#ifdef WITH_GHOST_X11
-    m_platform !=
-            GHOST_kVulkanPlatformX11 &&
-#endif
-#ifdef WITH_GHOST_WAYLAND
-                m_platform !=
-            GHOST_kVulkanPlatformWayland &&
-#endif
-                contains_extension(extensions_available,
-                                   VK_EXT_SURFACE_MAINTENANCE_1_EXTENSION_NAME) &&
+    const bool use_swapchain_maintenance1 =
+        contains_extension(extensions_available, VK_EXT_SURFACE_MAINTENANCE_1_EXTENSION_NAME) &&
         contains_extension(extensions_available, VK_KHR_GET_SURFACE_CAPABILITIES_2_EXTENSION_NAME);
     if (use_swapchain_maintenance1) {
       requireExtension(
