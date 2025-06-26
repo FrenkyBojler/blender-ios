f0ab40f4432e81e42b3edeb9e4328efc1520f69c
Eitan Traurig <eitant13@gmail.com>
2025-06-26 09:47:38 -0400

    Make Format

--- source/blender/editors/render/render_shading.cc
+++ source/blender/editors/render/render_shading.cc
@@ -739,12 +739,18 @@ void OBJECT_OT_material_slot_remove_unused(wmOperatorType *ot)
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 }
+
 static bool material_slot_remove_all_poll(bContext *C)
 {
+  if (!material_slot_remove_poll(C)) {
+    return false;
+  }
+
   const Object *ob_active = CTX_data_active_object(C);
   if (ob_active->actcol <= 0) {
     return false;
   }
+
   return true;
 }
 
@@ -752,10 +758,6 @@ static wmOperatorStatus material_slot_remove_all_exec(bContext *C, wmOperator *o
 {
   /* Removing material slots in edit mode screws things up, see bug #21822. */
   Object *ob_active = CTX_data_active_object(C);
-  if (ob_active && BKE_object_is_in_editmode(ob_active)) {
-    BKE_report(op->reports, RPT_ERROR, "Unable to remove material slot in edit mode");
-    return OPERATOR_CANCELLED;
-  }
   Main *bmain = CTX_data_main(C);
   int removed = 0;
 
