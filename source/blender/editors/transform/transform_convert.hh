325bac6f0ab27fe5f151b06d3759e8aae3faf5c5
John Kiril Swenson <kirilswenson@gmail.com>
2025-06-26 21:46:45 -0500

    VSE: Clamp strip handles to video/audio bounds
    This initial commit properly clamps handles with versioning that clamps
    video and audio strips from old files when upgrading.
    
    Transform logic was altered, fixing a few bugs:
    
    - When initializing a transform, createTransSeqData would already create
    some clamping data for channels. Replacing this with rcti offset_clamp
    lets us also populate handle clamping info.
        - This is necessary for the double handle tweak case --
        flushTransSeq only works one strip at a time, so we can't clamp
        post-hoc.
        - In applySeqSlideValue, we apply the offset_clamp with
        transform_convert_sequencer_clamp before values are printed to the
        header, applied to the transform data, or flushed to the strips
        themselves. This means snap lines no longer draw if strips are
        clamped, and headerSeqSlide will print exact offsets rather than
        attempted ones (as visible in the gif below). Later on, in
        flushTransSeq, we "reconstruct" clamp information whether strips are
        at their limits to determine which to highlight in red.
    
    Note that currently, handle clamping is drawn identically to overlaps.
    
    More information in PR.

--- source/blender/editors/transform/transform_convert.hh
+++ source/blender/editors/transform/transform_convert.hh
@@ -148,7 +148,8 @@ void transform_convert_mesh_customdatacorrect_init(TransInfo *t);
 
 /* `transform_convert_sequencer.cc` */
 
-void transform_convert_sequencer_channel_clamp(TransInfo *t, float r_val[2]);
+const rcti *transform_convert_sequencer_offset_clamp_get(const TransInfo *t);
+bool transform_convert_sequencer_clamp(const TransInfo *t, float r_val[2]);
 
 /********************* intern **********************/
 
