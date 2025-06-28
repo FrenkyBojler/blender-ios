fa08c7853b1eb90354cf21ac9b176cdae8961ccc
Habib Gahbiche <habib@blender.org>
2025-06-28 14:28:25 +0200

    Keep the old behavior

--- scripts/startup/bl_operators/clip.py
+++ scripts/startup/bl_operators/clip.py
@@ -708,10 +708,15 @@ class CLIP_OT_setup_tracking_scene(Operator):
 
     @staticmethod
     def _wipeDefaultNodes(tree):
-        if not tree:
-            return
-        for node in tree.nodes:
-            tree.nodes.remove(node)
+        if len(tree.nodes) != 4:
+            return False
+        types = [node.type for node in tree.nodes]
+        types.sort()
+
+        if (types[0] == 'COMPOSITE' and types[1] == 'REROUTE'
+                and types[2] == 'R_LAYERS' and types[3] == 'VIEWER'):
+            while tree.nodes:
+                tree.nodes.remove(tree.nodes[0])
 
     @staticmethod
     def _findNode(tree, type):
