9079343f23fdb3f084fffb67d811626741994a6a
илья _ <modormoder@gmail.com>
2025-06-28 16:34:30 +0300

    Cleanup: MEM_malloc_arrayN

--- source/blender/gpu/intern/gpu_index_buffer.cc
+++ source/blender/gpu/intern/gpu_index_buffer.cc
@@ -66,7 +66,7 @@ void GPU_indexbuf_init_ex(GPUIndexBufBuilder *builder,
   builder->restart_index_value = RESTART_INDEX;
 #endif
   builder->uses_restart_indices = false;
-  builder->data = static_cast<uint *>(MEM_mallocN(int64_t(sizeof(uint)) * builder->max_index_len, "IndexBuf data"));
+  builder->data = MEM_malloc_arrayN<uint>(builder->max_index_len, "IndexBuf data");
 }
 
 void GPU_indexbuf_allocate_uninitialized(GPUIndexBufBuilder *builder,
