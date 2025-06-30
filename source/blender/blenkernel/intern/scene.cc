7290b9fdda8d40161960685e6f111b4f9b4037bd
Falk David <falk@blender.org>
2025-06-30 12:34:44 +0200

    Initial commit

--- source/blender/blenkernel/intern/scene.cc
+++ source/blender/blenkernel/intern/scene.cc
@@ -1617,7 +1617,7 @@ constexpr IDTypeInfo get_type_info()
   info.name = "Scene";
   info.name_plural = "scenes";
   info.translation_context = BLT_I18NCONTEXT_ID_SCENE;
-  info.flags = IDTYPE_FLAGS_NEVER_UNUSED;
+  info.flags = IDTYPE_FLAGS_NEVER_UNUSED /* | IDTYPE_FLAGS_APPEND_IS_REUSABLE */;
   info.asset_type_info = nullptr;
 
   info.init_data = scene_init_data;
