a10775d44a50f68151302a852e94c79f54a9391c
Jacques Lucke <jacques@blender.org>
2025-07-01 08:05:42 +0200

    fix

--- source/blender/blenkernel/intern/node_tree_update.cc
+++ source/blender/blenkernel/intern/node_tree_update.cc
@@ -833,7 +833,7 @@ class NodeTreeMainUpdater {
   void ensure_closure_socket_value(bNodeSocketValueClosure &socket_data)
   {
     if (socket_data.type == CLOSURE_SOCKET_VALUE_TYPE_CURVE && !socket_data.curve_mapping) {
-      socket_data.curve_mapping = BKE_curvemapping_add(1, 0.0f, 1.0f, 0.0f, 1.0f);
+      socket_data.curve_mapping = BKE_curvemapping_add(1, 0.0f, 0.0f, 1.0f, 1.0f);
     }
   }
 
