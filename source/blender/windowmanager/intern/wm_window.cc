6366a319687c6ab9c0f0ba0f081467b12b0b3f9a
Pratik Borhade <pratikborhade302@gmail.com>
2025-07-03 12:51:41 +0530

    Fix format

--- source/blender/windowmanager/intern/wm_window.cc
+++ source/blender/windowmanager/intern/wm_window.cc
@@ -473,12 +473,13 @@ void wm_window_close(bContext *C, wmWindowManager *wm, wmWindow *win)
     ED_screen_exit(C, win, screen);
   }
   const bool is_single_editor = !WM_window_is_main_top_level(win) &&
-                              (screen && BLI_listbase_is_single(&screen->areabase));
+                                (screen && BLI_listbase_is_single(&screen->areabase));
 
   wm_window_free(C, wm, win);
 
   /* If temp screen, delete it after window free (it stops jobs that can access it).
-   * Also delete windows with single editor. If required, they are easy to restore, see: !132978. */
+   * Also delete windows with single editor. If required, they are easy to restore, see: !132978.
+   */
   if ((screen && screen->temp) || is_single_editor) {
     Main *bmain = CTX_data_main(C);
 
