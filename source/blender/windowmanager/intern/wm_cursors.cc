3656027a92fc2e1efbe4d04951e351c1ec72264f
Harley Acheson <harley.acheson@gmail.com>
2025-07-03 15:53:17 -0700

    Use BLF for a generic wm_cursor_text

--- source/blender/windowmanager/intern/wm_cursors.cc
+++ source/blender/windowmanager/intern/wm_cursors.cc
@@ -21,6 +21,8 @@
 #include "BKE_global.hh"
 #include "BKE_main.hh"
 
+#include "BLF_api.hh"
+
 #include "nanosvgrast.h"
 #include "svg_cursors.h"
 
@@ -592,86 +594,71 @@ static void wm_cursor_time_small(wmWindow *win, int nr)
                              false);
 }
 
-static void wm_cursor_time_rgba(wmWindow *win, int number)
+static void wm_cursor_text(wmWindow *win, std::string text)
 {
-  const float size = std::min(cursor_size() * 0.75f, 128.0f);
-  const int digit_height = ceill(size);
-  const int digit_width = ceill(size * 0.82f);
+  int font_id = BLF_default();
+  float size = cursor_size() * 0.8f;
+  BLF_size(font_id, size);
+  float width;
+  float height;
+  BLF_width_and_height(font_id, text.c_str(), text.size(), &width, &height);
+  if ((width * height) > (128.0f * 128.0f)) {
+    size = 128.0f / width * size;
+    BLF_size(font_id, size);
+    BLF_width_and_height(font_id, text.c_str(), text.size(), &width, &height);
+  }
 
-  int number_test = number;
-  int digits = 0;
-  do {
-    number_test /= 10;
-    digits++;
-  } while (number_test != 0);
+  const float padding = size * 0.15f;
+  width += padding * 2.0f;
+  height += padding * 2.0f;
 
-  const int full_width = digit_width * digits;
-  blender::Array<uint8_t> bitmap_rgba(full_width * digit_height * sizeof(int), 0);
+  blender::Array<uint> bitmap(width * height, 0x90000000);
 
-  for (int idx = digits - 1; idx >= 0; idx--) {
-    char digit = number % 10;
+  float color[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+  BLF_buffer_col(font_id, color);
+  BLF_buffer(font_id, nullptr, (uchar *)bitmap.data(), width, height, nullptr);
+  BLF_position(font_id, padding, padding, 0.0f);
+  BLF_draw_buffer(font_id, text.c_str(), text.size());
 
-    const char *source;
-    if (digit == 0) {
-      source = datatoc_cursor_0_svg;
-    }
-    else if (digit == 1) {
-      source = datatoc_cursor_1_svg;
-    }
-    else if (digit == 2) {
-      source = datatoc_cursor_2_svg;
-    }
-    else if (digit == 3) {
-      source = datatoc_cursor_3_svg;
-    }
-    else if (digit == 4) {
-      source = datatoc_cursor_4_svg;
-    }
-    else if (digit == 5) {
-      source = datatoc_cursor_5_svg;
-    }
-    else if (digit == 6) {
-      source = datatoc_cursor_6_svg;
-    }
-    else if (digit == 7) {
-      source = datatoc_cursor_7_svg;
-    }
-    else if (digit == 8) {
-      source = datatoc_cursor_8_svg;
-    }
-    else {
-      source = datatoc_cursor_9_svg;
+  /* Flip Y. */
+  {
+    size_t x_size, y_size;
+    uint *top, *bottom, *line;
+    x_size = width;
+    y_size = height;
+    const size_t stride = x_size * sizeof(int);
+
+    top = (uint *)bitmap.data();
+    bottom = top + ((y_size - 1) * x_size);
+    line = MEM_malloc_arrayN<uint>(x_size, "linebuf");
+
+    y_size >>= 1;
+
+    for (; y_size > 0; y_size--) {
+      memcpy(line, top, stride);
+      memcpy(top, bottom, stride);
+      memcpy(bottom, line, stride);
+      bottom -= x_size;
+      top += x_size;
     }
 
-    int bitmap_size[2];
-    blender::Array<uint8_t> digit_bmp = cursor_bitmap_from_svg(source, size, bitmap_size);
-    if (UNLIKELY(digit_bmp.is_empty())) {
-      return;
-    }
-
-    uint8_t *srect = digit_bmp.data();
-    uint8_t *drect = bitmap_rgba.data() + (idx * digit_width * 4);
-    for (int i = 0; i < bitmap_size[1]; i++) {
-      memcpy(drect, srect, bitmap_size[0] * sizeof(int));
-      srect += bitmap_size[0] * sizeof(int);
-      drect += full_width * sizeof(int);
-    }
-
-    number /= 10;
+    MEM_freeN(line);
   }
 
   const int hot_spot[2] = {
-      int(0.5f * (full_width - 1)),
-      int(0.5f * (digit_height - 1)),
+      int(0.5f * (width - 1)),
+      int(0.5f * (height - 1)),
   };
-  const int icon_size[2] = {full_width, digit_height};
+  const int icon_size[2] = {width, height};
   GHOST_TSuccess success = GHOST_SetCustomCursorShape(
       static_cast<GHOST_WindowHandle>(win->ghostwin),
-      bitmap_rgba.data(),
+      (uchar *)bitmap.data(),
       nullptr,
       icon_size,
       hot_spot,
       true);
+
+  BLF_buffer(font_id, nullptr, nullptr, 0, 0, nullptr);
 }
 
 void WM_cursor_time(wmWindow *win, int nr)
@@ -682,7 +669,7 @@ void WM_cursor_time(wmWindow *win, int nr)
 
   /* Use `U.ui_scale` instead of `UI_SCALE_FAC` here to ignore HiDPI/Retina scaling. */
   if (WM_capabilities_flag() & WM_CAPABILITY_RGBA_CURSORS) {
-    wm_cursor_time_rgba(win, nr);
+    wm_cursor_text(win, std::to_string(nr));
   }
   else if (U.ui_scale < 1.45f || !wm_cursor_time_large(win, nr)) {
     wm_cursor_time_small(win, nr);
