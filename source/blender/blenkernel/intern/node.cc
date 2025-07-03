e608345fd2ebc6d8e9a6142506d24150e2249db5
Jacques Lucke <jacques@blender.org>
2025-07-03 08:48:17 +0200

    cleanup

--- source/blender/blenkernel/intern/node.cc
+++ source/blender/blenkernel/intern/node.cc
@@ -2950,7 +2950,6 @@ static void node_socket_free(bNodeSocket *sock, const bool do_id_user)
         default_value_menu.enum_items->remove_user_and_delete_if_last();
       }
     }
-    // TODO: Extract function to free default value.
     if (sock->type == SOCK_CLOSURE) {
       auto &default_value_closure = *sock->default_value_typed<bNodeSocketValueClosure>();
       BKE_curvemapping_free(default_value_closure.curve_mapping);
@@ -3240,7 +3239,6 @@ static void node_socket_copy(bNodeSocket *sock_dst, const bNodeSocket *sock_src,
         default_value_menu.enum_items->add_user();
       }
     }
-    // TODO: use #node_socket_copy_default_value_data
     if (sock_src->type == SOCK_CLOSURE) {
       auto &default_value_closure = *sock_dst->default_value_typed<bNodeSocketValueClosure>();
       default_value_closure.curve_mapping = BKE_curvemapping_copy(
