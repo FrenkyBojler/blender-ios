7a7946f29b7eb18ebb5fa7fe88cfa00732f4f10c
Sybren A. Stüvel <sybren@blender.org>
2025-07-03 12:31:53 +0200

    Fix #141341: Assert using proportional editing in object mode
    Remove `BLI_assert(tc->sorted_index_map);` calls. Add one special case
    in the function that investigates whether there is any selected item in
    `tc->data`.
    
    Earlier, I've been quite liberal in spreading these asserts around, to
    guard against code paths that should have originally sorted `tc->data`
    but by mistake didn't create `tc->sorted_index_map`.
    
    They've been removed now, as they seem to be causing more trouble than
    they're worth: the "sorting selected first" behaviour is only explicitly
    there for proportional editing. With proportional editing disabled,
    `tc->data` **only** contains selected items, and those are trivially
    sorted first.
    
    By now `tc->foreach_index_selected` can work without `sorted_index_map`;
    if it is `nullptr`, it will assume that we're in the trivial case, and
    that the array items can just be visited in index order.

--- source/blender/editors/transform/transform.cc
+++ source/blender/editors/transform/transform.cc
@@ -1952,7 +1952,16 @@ bool initTransform(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
         continue;
       }
 
-      BLI_assert(tc->sorted_index_map);
+      /* `tc->sorted_index_map` is only used in cases where `tc->data` has a mixture of selected
+       * and unselected items (AFAIK just for proportial editing). Without `tc->sorted_index_map`,
+       * all items in `tc->data` are expected to be selected. */
+      if (!tc->sorted_index_map) {
+        BLI_assert_msg(tc->data[0].flag & TD_SELECTED,
+                       "Without sorted_index_map, all items are expected to be selected");
+        has_selected_any = true;
+        break;
+      }
+
       const int first_selected_index = tc->sorted_index_map[0];
       TransData *td = &tc->data[first_selected_index];
       if (td->flag & TD_SELECTED) {
