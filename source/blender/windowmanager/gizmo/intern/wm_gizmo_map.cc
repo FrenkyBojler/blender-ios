dc1b05ba88c4c354e352973fc89ac69d1338aeb6
Germano Cavalcante <germano.costa@ig.com.br>
2025-07-04 10:43:40 -0300

    Cleanup: Improve readability on issues with Intel drivers

--- source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
+++ source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
@@ -519,11 +519,15 @@ static void gizmo_draw_select_3d_loop(const bContext *C,
                                       const int visible_gizmos_len,
                                       bool *r_use_select_bias)
 {
-  /**
-   * Set default depth state.
-   * NOTE: Use #GPU_DEPTH_ALWAYS instead of #GPU_DEPTH_NONE due to issues with Intel GPU Drivers.
-   */
-  GPU_depth_test(GPU_DEPTH_ALWAYS);
+  /* WORKAROUND(#132196): `GPU_DEPTH_NONE` leads to issues with Intel GPU drivers on Windows
+   * where camera gizmos cannot be shifted. `glGetQueryObjectuiv` for `GL_SAMPLES_PASSED`
+   * seems to return zero in all cases. This might be due to undefined behavior of OpenGL
+   * when the depth test is disabled and rendering to a depth render target-only framebuffer.
+   * Using `GPU_DEPTH_ALWAYS` fixes the issue. */
+  const bool use_intel_gpu_workaround = true;
+
+  /* Set default depth state. */
+  GPU_depth_test(use_intel_gpu_workaround ? GPU_DEPTH_ALWAYS : GPU_DEPTH_NONE);
   GPU_depth_mask(true);
   bool is_depth_prev = false;
   bool is_depth_skip_prev = false;
@@ -543,12 +547,7 @@ static void gizmo_draw_select_3d_loop(const bContext *C,
         GPU_depth_test(GPU_DEPTH_LESS_EQUAL);
       }
       else {
-        /* WORKAROUND(#132196): `GPU_DEPTH_NONE` leads to issues with Intel GPU drivers on Windows
-         * where camera gizmos cannot be shifted. `glGetQueryObjectuiv` for `GL_SAMPLES_PASSED`
-         * seems to return zero in all cases. This might be due to undefined behavior of OpenGL
-         * when the depth test is disabled and rendering to a depth render target-only framebuffer.
-         * Using `GPU_DEPTH_ALWAYS` fixes the issue. */
-        GPU_depth_test(GPU_DEPTH_ALWAYS);
+        GPU_depth_test(use_intel_gpu_workaround ? GPU_DEPTH_ALWAYS : GPU_DEPTH_NONE);
       }
       is_depth_prev = is_depth;
     }
@@ -572,7 +571,7 @@ static void gizmo_draw_select_3d_loop(const bContext *C,
 
   /* Reset depth state.*/
 
-  /* if (is_depth_prev) */ {
+  if (is_depth_prev || use_intel_gpu_workaround) {
     GPU_depth_test(GPU_DEPTH_NONE);
   }
   if (is_depth_skip_prev) {
