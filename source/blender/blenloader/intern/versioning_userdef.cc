4afbb5e3c617c941ffc9eb2872ad1af58057e375
Nika Kutsniashvili <nickberckley@gmail.com>
2025-07-04 17:15:05 +0400

    Remove icon; update subversion number

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -341,7 +341,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_node.console_output);
   }
 
-  if (!USER_VERSION_ATLEAST(500, 23)) {
+  if (!USER_VERSION_ATLEAST(500, 34)) {
     FROM_DEFAULT_V4_UCHAR(common.anim.preview_range);
   }
 
--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -4496,7 +4496,7 @@ static void rna_def_userdef_themes(BlenderRNA *brna)
   static const EnumPropertyItem active_theme_area[] = {
       {0, "USER_INTERFACE", ICON_WORKSPACE, "User Interface", ""},
       {19, "STYLE", ICON_FONTPREVIEW, "Text Style", ""},
-      {25, "COMMON", ICON_OVERLAY, "Common", ""},
+      {25, "COMMON", ICON_NONE, "Common", ""},
       {24, "ASSET_SHELF", ICON_ASSET_MANAGER, "Asset Shelf", ""},
       {1, "VIEW_3D", ICON_VIEW3D, "3D Viewport", ""},
       {4, "DOPESHEET_EDITOR", ICON_ACTION, "Dope Sheet/Timeline", ""},
