eba336894d3ec9bc8b138969e31bb7ac23f8d5b4
Damien Picard <dam.pic@free.fr>
2025-07-05 20:20:41 +0200

    I18n: Disambiguate "Edges" in Keying compositing node
    "Edges" usually refers to the edges of a mesh. In the context of the
    Keying compositing node, it should be the image processing sense of
    edges between color areas. This mirrors the singular "Edge", already
    similarly disambiguated.
    
    Reported by Satoshi Yamasaki in #43295.

--- source/blender/nodes/composite/nodes/node_composite_keying.cc
+++ source/blender/nodes/composite/nodes/node_composite_keying.cc
@@ -34,9 +34,9 @@ static void cmp_node_keying_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
 
-  b.add_output<decl::Color>("Image").structure_type(StructureType::Dynamic);
-  b.add_output<decl::Float>("Matte").structure_type(StructureType::Dynamic);
-  b.add_output<decl::Float>("Edges").structure_type(StructureType::Dynamic);
+  b.add_output<decl::Color>("Image");
+  b.add_output<decl::Float>("Matte");
+  b.add_output<decl::Float>("Edges").translation_context(BLT_I18NCONTEXT_ID_IMAGE);
 
   b.add_input<decl::Color>("Image")
       .default_value({0.8f, 0.8f, 0.8f, 1.0f})
@@ -83,7 +83,9 @@ static void cmp_node_keying_declare(NodeDeclarationBuilder &b)
           "The matte gets remapped such matte values higher than the white level become white. "
           "Pixels at the identified edges are excluded from the remapping to preserve details");
 
-  PanelDeclarationBuilder &edges_panel = tweak_panel.add_panel("Edges").default_closed(true);
+  PanelDeclarationBuilder &edges_panel =
+      tweak_panel.add_panel("Edges").default_closed(true).translation_context(
+          BLT_I18NCONTEXT_ID_IMAGE);
   edges_panel.add_input<decl::Int>("Size", "Edge Search Size")
       .default_value(3)
       .min(0)
