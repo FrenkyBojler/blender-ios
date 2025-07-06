0df9d89d965cb310a370aa0ba8509eecb0b62b55
илья _ <modormoder@gmail.com>
2025-07-06 19:32:06 +0300

    make format

--- source/blender/draw/engines/workbench/workbench_volume.cc
+++ source/blender/draw/engines/workbench/workbench_volume.cc
@@ -8,11 +8,11 @@
 #include "workbench_private.hh"
 
 #include "BKE_volume.hh"
-#include "GEO_volume_meshing.hh"
 #include "BLI_math_geom.h"
 #include "BLI_rand.h"
 #include "DNA_fluid_types.h"
 #include "DNA_modifier_types.h"
+#include "GEO_volume_meshing.hh"
 
 namespace blender::workbench {
 
