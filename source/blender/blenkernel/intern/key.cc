62b0a41c48fa635bc6947cdb65517f9cacfbcafa
Sybren A. Stüvel <sybren@blender.org>
2025-07-07 14:03:21 +0200

    Limit the default value to operators & Python code
    Instead of always creating keys with value=1, limit that to the keys
    created via the `object.shape_key_add` operator and the RNA function of the
    same name.
    
    This reduces the influence of this PR. Otherwise all kinds of internal
    C++ code (like the FBX and USD importers) would have to be revisited as
    well.

--- source/blender/blenkernel/intern/key.cc
+++ source/blender/blenkernel/intern/key.cc
@@ -1848,8 +1848,6 @@ KeyBlock *BKE_keyblock_add(Key *key, const char *name)
    * which could cause problems in some cases, see #BKE_keyblock_add_ctime. */
   kb->pos = curpos + 0.1f; /* only used for absolute shape keys */
 
-  kb->curval = 1.0f;
-
   return kb;
 }
 
