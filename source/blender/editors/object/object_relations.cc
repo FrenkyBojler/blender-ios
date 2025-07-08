1a3896cf1127dd7e4e4f8b7f84da30d9fedfe6d9
Guillermo Venegas <guillermovcra@gmail.com>
2025-07-08 14:51:20 -0600

    init

--- source/blender/editors/object/object_relations.cc
+++ source/blender/editors/object/object_relations.cc
@@ -496,6 +496,14 @@ const EnumPropertyItem prop_make_parent_types[] = {
     {0, nullptr, 0, nullptr, nullptr},
 };
 
+blender::StringRef make_parent_enum_name(int value)
+{
+  const char *name;
+  [[maybe_unused]] bool found = RNA_enum_name(prop_make_parent_types, value, &name);
+  BLI_assert(found);
+  return IFACE_(name);
+}
+
 static bool parent_set_with_depsgraph(ReportList *reports,
                                       const bContext *C,
                                       Scene *scene,
@@ -950,11 +958,7 @@ static wmOperatorStatus parent_set_invoke_menu(bContext *C, wmOperatorType *ot)
   uiPopupMenu *pup = UI_popup_menu_begin(C, IFACE_("Set Parent To"), ICON_NONE);
   uiLayout *layout = UI_popup_menu_layout(pup);
 
-  PointerRNA opptr;
-#if 0
-  uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_OBJECT);
-#else
-  opptr = layout->op(ot, IFACE_("Object"), ICON_NONE, WM_OP_EXEC_DEFAULT, UI_ITEM_NONE);
+  PointerRNA opptr = layout->op(ot, IFACE_("Object"), ICON_NONE, WM_OP_EXEC_DEFAULT, UI_ITEM_NONE);
   RNA_enum_set(&opptr, "type", PAR_OBJECT);
   RNA_boolean_set(&opptr, "keep_transform", false);
 
@@ -962,7 +966,6 @@ static wmOperatorStatus parent_set_invoke_menu(bContext *C, wmOperatorType *ot)
       ot, IFACE_("Object (Keep Transform)"), ICON_NONE, WM_OP_EXEC_DEFAULT, UI_ITEM_NONE);
   RNA_enum_set(&opptr, "type", PAR_OBJECT);
   RNA_boolean_set(&opptr, "keep_transform", true);
-#endif
 
   PointerRNA op_ptr = layout->op(
       "OBJECT_OT_parent_no_inverse_set", IFACE_("Object (Without Inverse)"), ICON_NONE);
@@ -1007,27 +1010,37 @@ static wmOperatorStatus parent_set_invoke_menu(bContext *C, wmOperatorType *ot)
   if (parent->type == OB_ARMATURE) {
 
     if (can_support.armature_deform) {
-      uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_ARMATURE);
+      op_ptr = layout->op(ot, make_parent_enum_name(PAR_ARMATURE), ICON_NONE);
+      RNA_enum_set(&op_ptr, "type", PAR_ARMATURE);
     }
     if (can_support.empty_groups) {
-      uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_ARMATURE_NAME);
+      op_ptr = layout->op(ot, make_parent_enum_name(PAR_ARMATURE_NAME), ICON_NONE);
+      RNA_enum_set(&op_ptr, "type", PAR_ARMATURE_NAME);
     }
     if (can_support.envelope_weights) {
-      uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_ARMATURE_ENVELOPE);
+      op_ptr = layout->op(ot, make_parent_enum_name(PAR_ARMATURE_ENVELOPE), ICON_NONE);
+      RNA_enum_set(&op_ptr, "type", PAR_ARMATURE_ENVELOPE);
     }
     if (can_support.automatic_weights) {
-      uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_ARMATURE_AUTO);
+      op_ptr = layout->op(ot, make_parent_enum_name(PAR_ARMATURE_AUTO), ICON_NONE);
+      RNA_enum_set(&op_ptr, "type", PAR_ARMATURE_AUTO);
     }
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_BONE);
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_BONE_RELATIVE);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_BONE), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_BONE);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_BONE_RELATIVE), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_BONE_RELATIVE);
   }
   else if (parent->type == OB_CURVES_LEGACY) {
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_CURVE);
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_FOLLOW);
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_PATH_CONST);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_CURVE), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_CURVE);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_FOLLOW), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_FOLLOW);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_PATH_CONST), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_PATH_CONST);
   }
   else if (parent->type == OB_LATTICE) {
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_LATTICE);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_LATTICE), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_LATTICE);
   }
   else if (parent->type == OB_MESH) {
     if (can_support.attach_surface) {
@@ -1037,8 +1050,10 @@ static wmOperatorStatus parent_set_invoke_menu(bContext *C, wmOperatorType *ot)
 
   /* vertex parenting */
   if (OB_TYPE_SUPPORT_PARVERT(parent->type)) {
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_VERTEX);
-    uiItemEnumO_ptr(layout, ot, std::nullopt, ICON_NONE, "type", PAR_VERTEX_TRI);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_VERTEX), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_VERTEX);
+    op_ptr = layout->op(ot, make_parent_enum_name(PAR_VERTEX_TRI), ICON_NONE);
+    RNA_enum_set(&op_ptr, "type", PAR_VERTEX_TRI);
   }
 
   UI_popup_menu_end(C, pup);
