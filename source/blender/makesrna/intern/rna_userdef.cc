caa5d643b31e092d6d9f8f7577e11335a05c8961
Falk David <falk@blender.org>
2025-07-08 15:32:30 +0200

    UI: Theme: Expose alpha channel of `wire_edit` color
    Internally, the `wire_edit` uses all 4 rgba channels. This makes
    it possible for users to fade out the edit wire colors. For Grease
    Pencil users for example, this makes the edit lines less distracting
    (from the selected one).

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -2659,7 +2659,7 @@ static void rna_def_userdef_theme_space_view3d(BlenderRNA *brna)
   RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
 
   prop = RNA_def_property(srna, "wire_edit", PROP_FLOAT, PROP_COLOR_GAMMA);
-  RNA_def_property_array(prop, 3);
+  RNA_def_property_array(prop, 4);
   RNA_def_property_ui_text(
       prop, "Wire Edit", "Color for wireframe when in edit mode, but edge selection is active");
   RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
