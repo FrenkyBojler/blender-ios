22bb4f3b4c7bfe2b42d83962201f72304fca354b
Jonas Ådahl <jadahl@gmail.com>
2025-07-09 21:07:05 +0200

    wayland: Always use libdecor if there is no xdg_decoration_manager
    This avoids limiting the usage of libdecor to GNOME and GNOME based
    desktop environments, by looking at the advertised Wayland globals,
    instead of env vars about desktop environments.
    
    This makes Blender work on weston and probably any other compositor that
    enforces client side decorations.

--- intern/ghost/intern/GHOST_SystemWayland.cc
+++ intern/ghost/intern/GHOST_SystemWayland.cc
@@ -1979,37 +1979,6 @@ static uint32_t rgba_straight_to_premul_inverted(uint32_t rgba_uint)
   return rgba_uint;
 }
 
-#ifdef WITH_GHOST_WAYLAND_LIBDECOR
-static const char *strchr_or_end(const char *str, const char ch)
-{
-  const char *p = str;
-  while (!ELEM(*p, ch, '\0')) {
-    p++;
-  }
-  return p;
-}
-
-static bool string_elem_split_by_delim(const char *haystack, const char delim, const char *needle)
-{
-  /* Local copy of #BLI_string_elem_split_by_delim (would be a bad level call). */
-
-  /* May be zero, returns true when an empty span exists. */
-  const size_t needle_len = strlen(needle);
-  const char *p = haystack, *p_next;
-  while (true) {
-    p_next = strchr_or_end(p, delim);
-    if ((size_t(p_next - p) == needle_len) && (memcmp(p, needle, needle_len) == 0)) {
-      return true;
-    }
-    if (*p_next == '\0') {
-      break;
-    }
-    p = p_next + 1;
-  }
-  return false;
-}
-#endif /* WITH_GHOST_WAYLAND_LIBDECOR */
-
 static uint64_t sub_abs_u64(const uint64_t a, const uint64_t b)
 {
   return a > b ? a - b : b - a;
@@ -7651,21 +7620,11 @@ GHOST_SystemWayland::GHOST_SystemWayland(const bool background)
 #ifdef WITH_GHOST_WAYLAND_LIBDECOR
   bool libdecor_required = false;
   if (use_window_frame) {
-    const char *xdg_current_desktop = [] {
-      /* Account for VSCode overriding this value (TSK!), see: #133921. */
-      const char *key = "ORIGINAL_XDG_CURRENT_DESKTOP";
-      const char *value = getenv(key);
-      return value ? value : getenv(key + 9);
-    }();
-
-    if (xdg_current_desktop) {
-      /* See the free-desktop specifications for details on `XDG_CURRENT_DESKTOP`.
-       * https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
-       */
-      if (string_elem_split_by_delim(xdg_current_desktop, ':', "GNOME")) {
-        libdecor_required = true;
-      }
-    }
+    /* xdg_decoration_manager can configure surfaces to use client side decorations, so this check
+     * is not correct; reconsider this to be able to handle switching between client side and
+     * server side decorations. */
+    if (!display_->xdg_decor->manager)
+      libdecor_required = true;
   }
 
   if (libdecor_required) {
