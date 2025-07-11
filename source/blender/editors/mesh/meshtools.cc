blender/blender

commit 3d6ea7c0756f107cc2455db43f765a33f99c1700

Shape Keys: Use actual evaluated meshes in Update/Join operators
Previously this attempted to use the evaluated deform mesh (otherwise
known as the cage mesh). Because this isn't the mesh displayed in the
viewport it can be unintuitive, and it also doesn't work when the other
meshes are procedurally generated or have other "generative" modifiers.
This commit switches to just checking the evaluated mesh.

A potential downside is that modifiers like subdivision are also taken into
account and can potentially make the objects incompatible because of
mismatched vertex counts.

Part of #135095

Pull Request: https://projects.blender.org/blender/blender/pulls/138330

source/blender/editors/mesh/meshtools.cc
@@ -727,9 +727,18 @@ wmOperatorStatus ED_mesh_shapes_join_objects_exec(bContext *C,
   Depsgraph &depsgraph = *CTX_data_ensure_evaluated_depsgraph(C);
   Mesh &active_mesh = *static_cast<Mesh *>(active_object.data);
 
+  struct ObjectInfo {
+    StringRefNull name;
+    const Mesh &mesh;
+  };
+
+  auto topology_count_matches = [](const Mesh &a, const Mesh &b) {
+    return a.verts_num == b.verts_num && a.edges_num == b.edges_num && a.faces_num == b.faces_num;
+  };
+
   bool found_object = false;
-  bool found_non_equal_verts_num = false;
-  Vector<Object *> compatible_objects;
+  bool found_non_equal_count = false;
+  Vector<ObjectInfo> compatible_objects;
   CTX_DATA_BEGIN (C, Object *, ob_iter, selected_editable_objects) {
     if (ob_iter == &active_object) {
       continue;
@@ -737,13 +746,24 @@ wmOperatorStatus ED_mesh_shapes_join_objects_exec(bContext *C,
     if (ob_iter->type != OB_MESH) {
       continue;
     }
-    found_object = true;
-    const Mesh &mesh = *static_cast<Mesh *>(ob_iter->data);
-    if (mesh.verts_num != active_mesh.verts_num) {
-      found_non_equal_verts_num = true;
+    const Object *object_eval = DEG_get_evaluated(&depsgraph, ob_iter);
+    if (!object_eval) {
       continue;
     }
-    compatible_objects.append(ob_iter);
+    found_object = true;
+    if (const Mesh *mesh = BKE_object_get_evaluated_mesh(object_eval)) {
+      if (topology_count_matches(*mesh, active_mesh)) {
+        compatible_objects.append({BKE_id_name(ob_iter->id), *mesh});
+        continue;
+      }
+    }
+    /* Fall back to the original mesh. */
+    const Mesh &mesh_orig = *static_cast<const Mesh *>(ob_iter->data);
+    if (topology_count_matches(mesh_orig, active_mesh)) {
+      compatible_objects.append({BKE_id_name(ob_iter->id), mesh_orig});
+      continue;
+    }
+    found_non_equal_count = true;
   }
   CTX_DATA_END;
 
@@ -752,8 +772,10 @@ wmOperatorStatus ED_mesh_shapes_join_objects_exec(bContext *C,
     return OPERATOR_CANCELLED;
   }
 
-  if (found_non_equal_verts_num) {
-    BKE_report(reports, RPT_WARNING, "Selected meshes must have equal numbers of vertices");
+  if (found_non_equal_count) {
+    BKE_report(reports,
+               RPT_WARNING,
+               "Selected meshes must have equal numbers of vertices, edges, and faces");
     return OPERATOR_CANCELLED;
   }
 
@@ -772,22 +794,14 @@ wmOperatorStatus ED_mesh_shapes_join_objects_exec(bContext *C,
   }
 
   int keys_changed = 0;
-  Scene *scene_eval = DEG_get_evaluated_scene(&depsgraph);
-  for (Object *object : compatible_objects) {
-    Object *object_eval = DEG_get_evaluated(&depsgraph, object);
-    Mesh *deformed_mesh = blender::bke::mesh_get_eval_deform(
-        &depsgraph, scene_eval, object_eval, &CD_MASK_BAREMESH);
-    if (!deformed_mesh) {
-      continue;
-    }
-    const char *name = BKE_id_name(object->id);
+  for (const ObjectInfo &info : compatible_objects) {
     if (ensure_keys_exist) {
-      KeyBlock *kb = BKE_keyblock_add(active_mesh.key, name);
-      BKE_keyblock_convert_from_mesh(deformed_mesh, active_mesh.key, kb);
+      KeyBlock *kb = BKE_keyblock_add(active_mesh.key, info.name.c_str());
+      BKE_keyblock_convert_from_mesh(&info.mesh, active_mesh.key, kb);
     }
-    else if (KeyBlock *kb = BKE_keyblock_find_name(active_mesh.key, name)) {
+    else if (KeyBlock *kb = BKE_keyblock_find_name(active_mesh.key, info.name.c_str())) {
       keys_changed++;
-      BKE_keyblock_update_from_mesh(deformed_mesh, kb);
+      BKE_keyblock_update_from_mesh(&info.mesh, kb);
     }
   }
 

FrenkyBojler/blender-ios

commit dd54467259e53451ee3b3997913db345bf2549a6

Fix #141735: "Join as Shapes" operator too restrictive
Caused by 3d6ea7c0756f107cc2455db43f765a33f99c1700
Before that commit, it was possible to retrieve shape key data from
other objects with a different number of edges or faces. One crucial
case where that is important is when the other mesh is a triangulated
with otherwise the same vertex count.

In general it's better to be flexible and allow other changes besides
the vertex count, especially since that's such a simple check that users
can easily understand.

source/blender/editors/mesh/meshtools.cc
@@ -733,7 +733,7 @@ wmOperatorStatus ED_mesh_shapes_join_objects_exec(bContext *C,
   };
 
   auto topology_count_matches = [](const Mesh &a, const Mesh &b) {
-    return a.verts_num == b.verts_num && a.edges_num == b.edges_num && a.faces_num == b.faces_num;
+    return a.verts_num == b.verts_num;
   };
 
   bool found_object = false;