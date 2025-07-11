9af78200e1f48a614ab616724b15bb38c6b2371e
marcopavanello <marco.pavanello@protonmail.com>
2025-07-11 14:32:02 +0200

    refactor glsl

--- source/blender/gpu/shaders/material/gpu_shader_material_tex_sky.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_tex_sky.glsl
@@ -33,38 +33,30 @@ void node_tex_sky(float3 co,
   float3 spherical = sky_spherical_coordinates(co);
   float3 xyz;
   float dir_elevation = M_PI_2 - spherical.x;
+  float x = (spherical.y + M_PI + sun_rotation) / (2.0 * M_PI);
+  float fade = 1.0;
+  float y;
 
+  if (sky_type == 0.0 && co.z < -0.4) {
+    /* Black ground if Single Scattering model */
+    color = float4(0.0, 0.0, 0.0, 1.0);
+    return;
+  }
   if (sky_type == 0.0 && co.z < 0.0) {
-    /* Black ground if Single Scattering model, otherwise get Sky LUT */
-    if (co.z < -0.4) {
-      color = float4(0.0, 0.0, 0.0, 1.0);
+    /* Ground fade */
+    if (x > 1.0) {
+      x -= 1.0;
     }
-    else {
-      /* Ground fade */
-      constexpr float tau = 6.28318530717958647692;
-      float x = (spherical.y + M_PI + sun_rotation) / tau;
-      if (x > 1.0) {
-        x -= 1.0;
-      }
-      float fade = 1.0 + co.z * 2.5;
-      fade = fade * fade * fade;
-      float y = 0.508;
-
-      /* look up color in the precomputed map and convert to RGB */
-      xyz = fade * texture(ima, float3(x, y, layer)).rgb;
-      color = float4(dot(xyz_to_r, xyz), dot(xyz_to_g, xyz), dot(xyz_to_b, xyz), 1);
-    }
+    fade = 1.0 + co.z * 2.5;
+    fade = fade * fade * fade;
+    y = 0.508;
   }
   else {
-    /* Sky */
-    constexpr float tau = 6.28318530717958647692;
-    float x = (spherical.y + M_PI + sun_rotation) / tau;
     /* Undo the non-linear transformation from the sky LUT. */
     float dir_elevation_abs = (dir_elevation < 0.0) ? -dir_elevation : dir_elevation;
-    float y = sqrt(dir_elevation_abs / M_PI_2) * signx(dir_elevation) * 0.5 + 0.5;
-
-    /* look up color in the precomputed map and convert to RGB */
-    xyz = texture(ima, float3(x, y, layer)).rgb;
-    color = float4(dot(xyz_to_r, xyz), dot(xyz_to_g, xyz), dot(xyz_to_b, xyz), 1);
+    y = sqrt(dir_elevation_abs / M_PI_2) * signx(dir_elevation) * 0.5 + 0.5;
   }
+  /* Look up color in the precomputed map and convert to RGB. */
+  xyz = fade * texture(ima, float3(x, y, layer)).rgb;
+  color = float4(dot(xyz_to_r, xyz), dot(xyz_to_g, xyz), dot(xyz_to_b, xyz), 1);
 }
