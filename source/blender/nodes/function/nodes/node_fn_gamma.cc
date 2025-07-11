fa1e5f9cef12be60fabb4b47737340da03ac35f4
Alberto12345678999 <a.pilotto@icloud.com>
2025-07-11 20:05:33 -0300

    Wrong commit on purpose just to rescuse earlier changes

--- source/blender/nodes/function/nodes/node_fn_gamma.cc
+++ source/blender/nodes/function/nodes/node_fn_gamma.cc
@@ -42,11 +42,11 @@ using namespace blender::math;
 
 static void node_build_multi_function(blender::nodes::NodeMultiFunctionBuilder &builder)
 {
-  static auto fn = mf::build::SI2_SO<ColorGeometry4f, float, ColorGeometry4f>(
+  static auto fn = mf::build::SI2_SO<float4, float, float4>(
       "Gamma",
-      [](const ColorGeometry4f &color, float gamma) -> ColorGeometry4f {
+      [](const float4 &color, float gamma) -> float4 {
         float4 gamma_applied = float4(math::safe_pow(float3(color.x, color.y, color.z), gamma), color.w);
-        return ColorGeometry4f(gamma_applied.x, gamma_applied.y, gamma_applied.z, gamma_applied.w);
+        return gamma_applied;
       },
       mf::build::exec_presets::AllSpanOrSingle());
   builder.set_matching_fn(fn);
