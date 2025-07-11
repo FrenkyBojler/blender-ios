f6867c9964a8b095e15509838fa18a59d3eb0f33
Pratik Borhade <pratikborhade302@gmail.com>
2025-07-11 12:05:57 +0530

    Fix: Grease Pencil: Problem removing last layer with attributes
    When deleting first layer (i.e. last in the tree view), layer attribute
    values of previous layers gets messed up due to wrong offset value when
    copying attribute data

--- source/blender/blenkernel/intern/grease_pencil.cc
+++ source/blender/blenkernel/intern/grease_pencil.cc
@@ -4147,7 +4147,7 @@ static void shrink_attribute_storage(blender::bke::AttributeStorage &storage,
         type.copy_construct_n(data.data, new_data.data, range_before.size());
         type.copy_construct_n(
             POINTER_OFFSET(data.data, type.size * range_after.start()),
-            POINTER_OFFSET(new_data.data, type.size * (range_before.start() - 1)),
+            POINTER_OFFSET(new_data.data, type.size * index_to_remove),
             range_after.size());
 
         attr.assign_data(std::move(new_data));
