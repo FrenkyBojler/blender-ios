8625441acb759421af90be28a808ad65a5035d68
tariqsulley <tariqsulley3c@gmail.com>
2025-07-12 13:16:08 +0000

    changes

--- source/blender/editors/object/object_edit.cc
+++ source/blender/editors/object/object_edit.cc
@@ -460,7 +460,7 @@ void collection_hide_menu_draw(const bContext *C, uiLayout *layout)
   ViewLayer *view_layer = CTX_data_view_layer(C);
   LayerCollection *lc_scene = static_cast<LayerCollection *>(view_layer->layer_collections.first);
 
-  layout->operator_context_set(WM_OP_EXEC_REGION_WIN);
+  layout->operator_context_set(WM_OP_INVOKE_REGION_WIN);
 
   LISTBASE_FOREACH (LayerCollection *, lc, &lc_scene->layer_collections) {
     int index = BKE_layer_collection_findindex(view_layer, lc);
