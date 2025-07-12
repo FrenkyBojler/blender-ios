98a89ce41db8a8b877071336ffbf44d0f7127b7f
Ramon Klauck <ramonklauck3@gmail.com>
2025-07-12 13:43:28 +0200

    use PREVIEW for the sequencer preview select circle

--- scripts/startup/bl_ui/space_toolsystem_common.py
+++ scripts/startup/bl_ui/space_toolsystem_common.py
@@ -1079,7 +1079,12 @@ def _activate_by_item(context, space_type, item, index, *, as_fallback=False):
     if item.draw_cursor is not None:
         def handle_fn(context, item, tool, xy):
             item.draw_cursor(context, tool, xy)
-        handle = WindowManager.draw_cursor_add(handle_fn, (context, item, tool), space_type, 'WINDOW')
+        # Check if tool is sequencer.select_circle because select circle is a vse preview tool and the region_type has
+        # to be PREVIEW not WINDOW.
+        if item.idname == "sequencer.select_circle":
+            handle = WindowManager.draw_cursor_add(handle_fn, (context, item, tool), space_type, 'PREVIEW')
+        else:
+            handle = WindowManager.draw_cursor_add(handle_fn, (context, item, tool), space_type, 'WINDOW')
         handle_map[space_type] = handle
 
 
