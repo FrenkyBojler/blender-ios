55fb996c069c5a469734f9be7b68e56637a42029
Miguel Pozo <pragma37@gmail.com>
2025-07-14 19:46:31 +0200

    Fix duplicated uniforms

--- source/blender/nodes/shader/nodes/node_shader_repeat.cc
+++ source/blender/nodes/shader/nodes/node_shader_repeat.cc
@@ -194,7 +194,8 @@ static void node_declare(NodeDeclarationBuilder &b)
       const std::string identifier = ShRepeatItemsAccessor::socket_identifier_for_item(item);
       auto &input_decl = b.add_input(socket_type, name, identifier)
                              .socket_name_ptr(
-                                 &tree->id, ShRepeatItemsAccessor::item_srna, &item, "name");
+                                 &tree->id, ShRepeatItemsAccessor::item_srna, &item, "name")
+                             .hide_value();
       auto &output_decl = b.add_output(socket_type, name, identifier).align_with_previous();
       if (socket_type_supports_fields(socket_type)) {
         input_decl.supports_field();
