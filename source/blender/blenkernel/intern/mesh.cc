c74436814e3521bc9093381e8f340f120eff91c5
Namit Bhutani <namit.bhut@gmail.com>
2025-07-15 19:00:27 +0530

    refactor: copy_from instead of a loop

--- source/blender/blenkernel/intern/mesh.cc
+++ source/blender/blenkernel/intern/mesh.cc
@@ -888,9 +888,7 @@ void mesh_apply_spatial_organization(Mesh &mesh)
   MutableSpan<int> face_offsets = mesh.face_offsets_for_write();
   Vector<int> face_sizes(new_face_order.size());
   gather_group_sizes(old_faces, new_face_order, face_sizes);
-  for (int i = 0; i < face_sizes.size(); i++) {
-    face_offsets[i] = face_sizes[i];
-  }
+  face_offsets.copy_from(face_sizes);
   offset_indices::accumulate_counts_to_offsets(face_offsets);
 
   MutableAttributeAccessor attributes_for_write = mesh.attributes_for_write();
