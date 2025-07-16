5d725d058c624479ac87a5165673da3f71be60ed
Jacques Lucke <jacques@blender.org>
2025-07-16 08:42:51 +0200

    improve message

--- source/blender/editors/space_node/node_socket_tooltip.cc
+++ source/blender/editors/space_node/node_socket_tooltip.cc
@@ -188,7 +188,7 @@ class SocketTooltipBuilder {
 
   void build_tooltip_value_unknown()
   {
-    this->build_tooltip_value_and_type_oneline(TIP_("Unknown"), TIP_("Unknown"));
+    this->build_tooltip_value_and_type_oneline(TIP_("Unknown (not evaluated)"), TIP_("Unknown"));
   }
 
   void build_tooltip_value_socket_default()
