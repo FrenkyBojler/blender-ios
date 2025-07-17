bd767c020040d117ac648ad940efeca4af1aec09
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-07-17 12:52:08 +0200

    the brainfuck

--- source/blender/blenloader/intern/versioning_450.cc
+++ source/blender/blenloader/intern/versioning_450.cc
@@ -4147,7 +4147,9 @@ void do_versions_after_linking_450(FileData * /*fd*/, Main *bmain)
     }
   }
 
-  if (MAIN_VERSION_FILE_ATLEAST(bmain, 404, 33) && !MAIN_VERSION_FILE_ATLEAST(bmain, 405, 14)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 404, 32) ||
+      (!MAIN_VERSION_FILE_ATLEAST(bmain, 405, 14) && bmain->versionfile >= 405))
+  {
     LISTBASE_FOREACH (bAction *, dna_action, &bmain->actions) {
       blender::animrig::Action &action = dna_action->wrap();
       blender::animrig::foreach_fcurve_in_action(
