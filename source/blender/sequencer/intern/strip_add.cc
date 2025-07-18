d63c10e55f93c77bab929586c0b62bfe5c4c2aa4
Richard Antalik <richardantalik@gmail.com>
2025-07-18 15:57:48 +0200

    Fix #142195: Crash when adding stereo 3D image
    Caused by assigning pointer to address which is eventually freed.
    
    Use `MEM_dupallocN()` instead.

--- source/blender/sequencer/intern/strip_add.cc
+++ source/blender/sequencer/intern/strip_add.cc
@@ -252,7 +252,8 @@ Strip *add_image_strip(Main *bmain, Scene *scene, ListBase *seqbase, LoadData *l
     strip->views_format = load_data->views_format;
   }
   if (load_data->stereo3d_format) {
-    strip->stereo3d_format = load_data->stereo3d_format;
+    strip->stereo3d_format = static_cast<Stereo3dFormat *>(
+        MEM_dupallocN(load_data->stereo3d_format));
   }
 
   /* Set initial scale based on load_data->fit_method. */
