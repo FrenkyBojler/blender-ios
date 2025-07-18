b2b056661094f9b41f8d2e8ad2eab48d6318e020
Alberto12345678999 <a.pilotto@icloud.com>
2025-07-18 17:38:54 -0300

    Fix: Improve lambda function in node_build_multi_function for gamma correction

--- source/blender/nodes/shader/nodes/node_shader_gamma.cc
+++ source/blender/nodes/shader/nodes/node_shader_gamma.cc
@@ -46,9 +46,9 @@ static void node_build_multi_function(blender::nodes::NodeMultiFunctionBuilder &
 {
   static auto fn = mf::build::SI2_SO<ColorGeometry4f, float, ColorGeometry4f>(
       "Gamma",
-      [](const ColorGeometry4f &color, float gamma) -> ColorGeometry4f {
-        float3 rgb = float3(color.r, color.g, color.b);
-        float3 rgb_gamma = math::safe_pow(rgb, gamma);
+      [](const ColorGeometry4f &color, const float gamma) -> ColorGeometry4f {
+        const float3 rgb = float3(color.r, color.g, color.b);
+        const float3 rgb_gamma = math::safe_pow(rgb, gamma);
         return ColorGeometry4f(rgb_gamma.x, rgb_gamma.y, rgb_gamma.z, color.a);
       },
       mf::build::exec_presets::AllSpanOrSingle());
