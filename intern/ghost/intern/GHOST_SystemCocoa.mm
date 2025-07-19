2fa8950e15a372f9df59e4478d22a2421b3838c8
Jonas Holzman <jonas@holzman.fr>
2025-07-19 22:24:09 +0200

    Remove TODO

--- intern/ghost/intern/GHOST_SystemCocoa.mm
+++ intern/ghost/intern/GHOST_SystemCocoa.mm
@@ -793,7 +793,6 @@ GHOST_IContext *GHOST_SystemCocoa::createOffscreenContext(GHOST_GPUSettings gpuS
 
 #ifdef WITH_METAL_BACKEND
     case GHOST_kDrawingContextTypeMetal: {
-      /* TODO(fclem): Remove OpenGL support and rename context to ContextMTL */
       GHOST_Context *context = new GHOST_ContextMTL(false, nullptr, nullptr, debug_context);
       if (context->initializeDrawingContext()) {
         return context;
