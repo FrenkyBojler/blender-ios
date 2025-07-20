8273cb72728cb7d29d2ab15f3d9419f7c698d6f4
Jonas Holzman <jonas@holzman.fr>
2025-07-21 00:00:30 +0200

    Texture formats / SRGB transforms progress

--- intern/ghost/intern/GHOST_XrGraphicsBindingMetal.mm
+++ intern/ghost/intern/GHOST_XrGraphicsBindingMetal.mm
@@ -6,16 +6,59 @@
  * \ingroup GHOST
  */
 
-#include "GHOST_ContextCGL.hh"
-#include "GHOST_XrGraphicsBindingMetal.h"
-
 #include "GPU_framebuffer.hh"
-
 #include "gpu_framebuffer_private.hh"
 
 #include "mtl_context.hh"
 #include "mtl_framebuffer.hh"
 #include "mtl_texture.hh"
+
+#include "GHOST_ContextCGL.hh"
+#include "GHOST_XrException.hh"
+#include "GHOST_XrGraphicsBindingMetal.h"
+
+static MTLPixelFormat ghost_format_to_mtl_format(GHOST_TXrSwapchainFormat ghost_format,
+                                                 bool expects_srgb_buffer)
+{
+  MTLPixelFormat format = MTLPixelFormatInvalid;
+
+  switch (ghost_format) {
+    case GHOST_kXrSwapchainFormatRGBA8:
+      format = expects_srgb_buffer ? MTLPixelFormatRGBA8Unorm_sRGB : MTLPixelFormatRGBA8Unorm;
+      break;
+    case GHOST_kXrSwapchainFormatRGBA16:
+      format = MTLPixelFormatRGBA16Unorm;
+      break;
+    case GHOST_kXrSwapchainFormatRGBA16F:
+      format = MTLPixelFormatRGBA16Float;
+      break;
+    case GHOST_kXrSwapchainFormatRGB10_A2:
+      format = MTLPixelFormatRGB10A2Unorm;
+      break;
+  }
+
+  if (format == MTLPixelFormatInvalid) {
+    throw GHOST_XrException("No supported DirectX swapchain format found.");
+  }
+
+  return format;
+}
+
+static eGPUTextureFormat ghost_format_to_gpu_format(GHOST_TXrSwapchainFormat ghost_format)
+{
+  switch (ghost_format) {
+    case GHOST_kXrSwapchainFormatRGBA8:
+      /* TODO: Assume SRGB for now, storing the proper SRGB info in draw_info as bad side-effects. Will be fixed
+       *       separately after dropping the use of the GPU backend. See below in chooseSwapchainFormat. */
+      return GPU_SRGB8_A8;
+    case GHOST_kXrSwapchainFormatRGBA16:
+      return GPU_RGBA16;
+    case GHOST_kXrSwapchainFormatRGBA16F:
+      return GPU_RGBA16F;
+    case GHOST_kXrSwapchainFormatRGB10_A2:
+      return GPU_RGB10_A2;
+  }
+}
 
 static std::optional<int64_t> choose_swapchain_format_from_candidates(
     const std::vector<int64_t> &gpu_binding_formats, const std::vector<int64_t> &runtime_formats)
@@ -91,16 +134,9 @@ std::optional<int64_t> GHOST_XrGraphicsBindingMetal::chooseSwapchainFormat(
     bool &r_is_srgb_format) const
 {
   std::vector<int64_t> gpu_binding_formats = {
-#if 0 /* RGB10A2, RGBA16 don't seem to work with Oculus head-sets, \
-       * so move them after RGBA16F for the time being. */
-      MTLPixelFormatRGB10A2Unorm,
-      MTLPixelFormatRGBA16Unorm,
-#endif
       MTLPixelFormatRGBA16Float,
-#if 1
       MTLPixelFormatRGB10A2Unorm,
       MTLPixelFormatRGBA16Unorm,
-#endif
       MTLPixelFormatRGBA8Unorm,
       MTLPixelFormatRGBA8Unorm_sRGB,
   };
@@ -108,30 +144,30 @@ std::optional<int64_t> GHOST_XrGraphicsBindingMetal::chooseSwapchainFormat(
   const std::optional result = choose_swapchain_format_from_candidates(gpu_binding_formats,
                                                                        runtime_formats);
 
-  if (result) {
-    switch (*result) {
-      case MTLPixelFormatRGB10A2Unorm:
-        r_format = GHOST_kXrSwapchainFormatRGB10_A2;
-        break;
-      case MTLPixelFormatRGBA16Unorm:
-        r_format = GHOST_kXrSwapchainFormatRGBA16;
-        break;
-      case MTLPixelFormatRGBA16Float:
-        r_format = GHOST_kXrSwapchainFormatRGBA16F;
-        break;
-      case MTLPixelFormatRGBA8Unorm:
-      case MTLPixelFormatRGBA8Unorm_sRGB:
-        r_format = GHOST_kXrSwapchainFormatRGBA8;
-        break;
-    }
-    r_is_srgb_format = (*result == MTLPixelFormatRGBA8Unorm_sRGB);
-  }
-  else {
+  if (!result) {
     r_format = GHOST_kXrSwapchainFormatRGBA8;
     r_is_srgb_format = false;
+    return result;
   }
 
-  // TODO: Figure out proper sRGB transforms
+  switch (*result) {
+    case MTLPixelFormatRGB10A2Unorm:
+      r_format = GHOST_kXrSwapchainFormatRGB10_A2;
+      break;
+    case MTLPixelFormatRGBA16Unorm:
+      r_format = GHOST_kXrSwapchainFormatRGBA16;
+      break;
+    case MTLPixelFormatRGBA16Float:
+      r_format = GHOST_kXrSwapchainFormatRGBA16F;
+      break;
+    case MTLPixelFormatRGBA8Unorm:
+    case MTLPixelFormatRGBA8Unorm_sRGB:
+      r_format = GHOST_kXrSwapchainFormatRGBA8;
+      break;
+  }
+
+  // TODO: Setting the proper srgb format value here causes side-effects for now, fix separately.
+  // r_is_srgb_format = (*result == MTLPixelFormatRGBA8Unorm_sRGB);
   r_is_srgb_format = false;
 
   return result;
@@ -165,11 +201,13 @@ void GHOST_XrGraphicsBindingMetal::submitToSwapchainImage(
   XrSwapchainImageMetalKHR &metal_swapchain_image = reinterpret_cast<XrSwapchainImageMetalKHR &>(
       swapchain_image);
 
+  id<MTLTexture> metal_xr_texture = static_cast<id<MTLTexture>>(metal_swapchain_image.texture);
+
   using namespace blender;
 
-  id<MTLTexture> metal_xr_texture = static_cast<id<MTLTexture>>(metal_swapchain_image.texture);
+  const eGPUTextureFormat tex_format = ghost_format_to_gpu_format(draw_info.swapchain_format);
   gpu::MTLTexture metal_gpu_texture = gpu::MTLTexture(
-      "xr_texture", GPU_SRGB8_A8, blender::gpu::GPU_TEXTURE_2D, metal_xr_texture);
+      "xr_swapchain_tex", tex_format , gpu::GPU_TEXTURE_2D, metal_xr_texture);
 
   gpu::MTLContext *ctx = gpu::MTLContext::get();
 
