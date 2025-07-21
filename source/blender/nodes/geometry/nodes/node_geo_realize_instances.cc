d6008311214cf40922de3ec2c27c4ab9bc250810
W_Cloud <yunkezengren@outlook.com>
2025-07-21 17:12:51 +0800

    revert change for Realize Instances node

--- source/blender/nodes/geometry/nodes/node_geo_realize_instances.cc
+++ source/blender/nodes/geometry/nodes/node_geo_realize_instances.cc
@@ -16,9 +16,9 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
   b.allow_any_socket_order();
-  b.add_input<decl::Geometry>("Instances", "Geometry")
+  b.add_input<decl::Geometry>("Geometry")
       .description("Geometry whose instances are (partially) realized");
-  b.add_output<decl::Geometry>("Instances", "Geometry").propagate_all().align_with_previous();
+  b.add_output<decl::Geometry>("Geometry").propagate_all().align_with_previous();
   b.add_input<decl::Bool>("Selection")
       .default_value(true)
       .hide_value()
