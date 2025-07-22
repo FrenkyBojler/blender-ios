6b2da111b65caefc252159693d65e8b81cadcee0
Clément Foucault <foucault.clem@gmail.com>
2025-07-22 21:33:39 +0200

    Refactor: GPU: Use macros for TextureFormat conversions
    This reduces the risk of errors and code duplicatino.
    
    Rel #130632

--- source/blender/gpu/GPU_format.hh
+++ source/blender/gpu/GPU_format.hh
@@ -91,15 +91,15 @@ namespace blender::gpu {
 
 /* clang-format off */
 /*                                           type       size comps blender_enum            vk_enum                   mtl_pixel_enum         mtl_vertex_enum        gl_pixel_enum                        shader_enum  */
-#define SNORM_10_10_10_2_(impl)         impl(/*TODO*/,  4,   4,    SNORM_10_10_10_2,       A2R10G10B10_SNORM_PACK32, /* n/a */,             Int1010102Normalized,  /* n/a */,                           /* n/a */       )
-#define UNORM_10_10_10_2_(impl)         impl(/*TODO*/,  4,   4,    UNORM_10_10_10_2,       A2R10G10B10_UNORM_PACK32, RGB10A2Unorm,          UInt1010102Normalized, RGB10_A2,                            rgb10_a2_unorm  )
-#define UINT_10_10_10_2_(impl)          impl(/*TODO*/,  4,   4,    UINT_10_10_10_2,        A2R10G10B10_UINT_PACK32,  RGB10A2Uint,           /* n/a */,             RGB10_A2UI,                          rgb10_a2_uint   )
+#define SNORM_10_10_10_2_(impl)         impl(/*TODO*/,  4,   4,    SNORM_10_10_10_2,       A2B10G10R10_SNORM_PACK32, /* n/a */,             Int1010102Normalized,  /* n/a */,                           /* n/a */       )
+#define UNORM_10_10_10_2_(impl)         impl(/*TODO*/,  4,   4,    UNORM_10_10_10_2,       A2B10G10R10_UNORM_PACK32, RGB10A2Unorm,          UInt1010102Normalized, RGB10_A2,                            rgb10_a2_unorm  )
+#define UINT_10_10_10_2_(impl)          impl(/*TODO*/,  4,   4,    UINT_10_10_10_2,        A2B10G10R10_UINT_PACK32,  RGB10A2Uint,           /* n/a */,             RGB10_A2UI,                          rgb10_a2_uint   )
 /*                                           type       size comps blender_enum            vk_enum                   mtl_pixel_enum         mtl_vertex_enum        gl_pixel_enum                        shader_enum  */
 #define UFLOAT_11_11_10_(impl)          impl(/*TODO*/,  4,   3,    UFLOAT_11_11_10,        B10G11R11_UFLOAT_PACK32,  RG11B10Float,          FloatRG11B10,          R11F_G11F_B10F,                      r11_g11_b10_ufloat)
 #define UFLOAT_9_9_9_EXP_5_(impl)       impl(/*TODO*/,  4,   3,    UFLOAT_9_9_9_EXP_5,     E5B9G9R9_UFLOAT_PACK32,   RGB9E5Float,           FloatRGB9E5,           RGB9_E5,                             /* n/a */)
 /*                                           type       size comps blender_enum            vk_enum                   mtl_pixel_enum         mtl_vertex_enum        gl_pixel_enum                        shader_enum  */
 #define SRGBA_8_8_8_8_(impl)            impl(/*TODO*/,  4,   4,    SRGBA_8_8_8_8,          R8G8B8A8_SRGB,            RGBA8Unorm_sRGB,       /* n/a */,             SRGB8_ALPHA8,                        /* n/a */   )
-#define SRGBA_8_8_8_(impl)              impl(/*TODO*/,  3,   3,    SRGBA_8_8_8,            R8G8B8_SRGB,              /* n/a */,             /* n/a */,             SRGB8,                               /* n/a */   )
+#define SRGBA_8_8_8_(impl)              impl(/*TODO*/,  3,   3,    SRGBA_8_8_8,            R8G8B8_SRGB,              RGBA8Unorm_sRGB,       /* n/a */,             SRGB8,                               /* n/a */   )
 /*                                           type       size comps blender_enum            vk_enum                   mtl_pixel_enum         mtl_vertex_enum        gl_pixel_enum                        shader_enum  */
 #define UNORM_16_DEPTH_(impl)           impl(/*TODO*/,  4,   1,    UNORM_16_DEPTH,         D16_UNORM,                Depth16Unorm,          /* n/a */,             DEPTH_COMPONENT16,                   /* n/a */   )
 #define UNORM_24_DEPTH_(impl)           impl(/*TODO*/,  4,   1,    UNORM_24_DEPTH,         X8_D24_UNORM_PACK32,      Depth24Unorm_Stencil8, /* n/a */,             DEPTH_COMPONENT24,                   /* n/a */   )
