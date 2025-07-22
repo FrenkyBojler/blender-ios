797878a2f2c79e1d92eee42055134269fe64fdb6
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-07-22 16:09:42 -0700

    Fix: Last edge point.

--- source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
@@ -216,12 +216,11 @@ static int pen_find_closest_edge_point(const PenToolOperation &ptd,
         const IndexRange eval_range = IndexRange::from_begin_end(offsets[src_i],
                                                                  offsets[src_i + 1])
                                           .shift(eval_points.first());
-
-        const int point_num = eval_range.size() - 1;
+        const int point_num = eval_range.size();
 
         for (const int eval_i : IndexRange(point_num)) {
-          const int eval_point_i_1 = eval_range[eval_i];
-          const int eval_point_i_2 = eval_range[(eval_i + 1) % eval_range.size()];
+          const int eval_point_i_1 = eval_range.first() + eval_i;
+          const int eval_point_i_2 = eval_range.first() + eval_i + 1;
           const float2 pos_1_proj = pen_global_to_screen(ptd, evaluated_positions[eval_point_i_1]);
           const float2 pos_2_proj = pen_global_to_screen(ptd, evaluated_positions[eval_point_i_2]);
           float local_t;
