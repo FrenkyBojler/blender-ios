91661a1208d3bcef4f10d8127d614dafc86e58f4
Lukas Tönne <lukas@blender.org>
2025-07-22 10:30:03 +0200

    Check explicitly for the `SOCK_CUSTOM` type, otherwise expect base type.

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -777,12 +777,17 @@ int rna_node_socket_idname_to_enum(const char *idname)
   using namespace blender;
 
   bke::bNodeSocketType *socket_type = bke::node_socket_type_find(idname);
+
+  /* Regular socket types use the base type as their enum value.
+   * Custom sockets don't have a base type and are used directly as the enum entry. */
+  if (socket_type->type == SOCK_CUSTOM) {
+    return bke::node_socket_types_get().first_index(socket_type);
+  }
+
   bke::bNodeSocketType *base_socket_type = bke::node_socket_type_find_static(socket_type->type,
                                                                              PROP_NONE);
-  /* Regular socket types use the base type as their enum value, custom sockets don't have a base
-   * type and are their own enum entry. */
-  return base_socket_type ? bke::node_socket_types_get().first_index(base_socket_type) :
-                            bke::node_socket_types_get().first_index(socket_type);
+  BLI_assert(base_socket_type != nullptr);
+  return bke::node_socket_types_get().first_index(base_socket_type);
 }
 
 blender::bke::bNodeSocketType *rna_node_socket_type_from_enum(int value)
