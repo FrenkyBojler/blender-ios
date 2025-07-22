a4171b1db4a8361ff52f42db7195f0fdf33a24c5
Jonas Holzman <jonas@holzman.fr>
2025-07-22 12:04:40 +0200

    Review feedback: Use i18n macro for window title argument

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -6107,8 +6107,12 @@ static wmOperatorStatus userpref_show_exec(bContext *C, wmOperator *op)
   };
 
   /* changes context! */
-  if (ScrArea *area = ED_screen_temp_space_open(
-          C, "Preferences", &window_rect, SPACE_USERPREF, U.preferences_display_type, false))
+  if (ScrArea *area = ED_screen_temp_space_open(C,
+                                                IFACE_("Preferences"),
+                                                &window_rect,
+                                                SPACE_USERPREF,
+                                                U.preferences_display_type,
+                                                false))
   {
     /* The header only contains the editor switcher and looks empty.
      * So hiding in the temp window makes sense. */
