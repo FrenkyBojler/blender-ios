fe7765a65740813f9ae6eeea303e2dd4cd0962bb
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2025-07-23 04:11:19 +0200

    exporter refactor

--- source/blender/io/wavefront_obj/exporter/obj_exporter.cc
+++ source/blender/io/wavefront_obj/exporter/obj_exporter.cc
@@ -10,6 +10,9 @@
 #include <memory>
 #include <system_error>
 
+#include "DNA_curve_enums.h"
+#include "DNA_curve_types.h"
+
 #include "BKE_context.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_report.hh"
@@ -130,14 +133,15 @@ filter_supported_objects(Depsgraph *depsgraph, const OBJExportParams &export_par
         if (!nurb) {
           /* An empty curve. Not yet supported to export these as meshes. */
           if (export_params.export_curves_as_nurbs) {
-            r_exportable_nurbs.append(
-                std::make_unique<OBJCurve>(depsgraph, export_params, object));
+            OBJCurve *obj_curve = new OBJCurve(depsgraph, export_params, object);
+            r_exportable_nurbs.append(std::unique_ptr<OBJCurve>(obj_curve));
           }
           break;
         }
         if (export_params.export_curves_as_nurbs && is_curve_nurbs_compatible(nurb)) {
           /* Export in parameter form: control points. */
-          r_exportable_nurbs.append(std::make_unique<OBJCurve>(depsgraph, export_params, object));
+          OBJCurve *obj_curve = new OBJCurve(depsgraph, export_params, object);
+          r_exportable_nurbs.append(std::unique_ptr<OBJCurve>(obj_curve));
         }
         else {
           /* Export in mesh form: edges and vertices. */
@@ -269,25 +273,45 @@ static void write_nurbs_curve_objects(const Span<std::unique_ptr<OBJCurve>> expo
   fh.write_to_file(obj_writer.get_outfile());
 }
 
-void export_frame(Depsgraph *depsgraph, const OBJExportParams &export_params, const char *filepath)
+static void write_materials(MTLWriter *mtl_writer, const OBJExportParams &export_params)
 {
-  std::unique_ptr<OBJWriter> frame_writer = nullptr;
+  BLI_assert(mtl_writer);
+  mtl_writer->write_header(export_params.blen_filepath);
+  char dest_dir[FILE_MAX];
+  if (export_params.file_base_for_tests[0] == '\0') {
+    BLI_path_split_dir_part(export_params.filepath, dest_dir, sizeof(dest_dir));
+  }
+  else {
+    STRNCPY(dest_dir, export_params.file_base_for_tests);
+  }
+  BLI_path_slash_native(dest_dir);
+  BLI_path_normalize(dest_dir);
+  mtl_writer->write_materials(export_params.blen_filepath,
+                              export_params.path_mode,
+                              dest_dir,
+                              export_params.export_pbr_extensions);
+}
+
+static bool open_stream_writers(const OBJExportParams &export_params,
+                                const char *filepath,
+                                std::unique_ptr<OBJWriter> &r_frame_writer,
+                                std::unique_ptr<MTLWriter> &r_mtl_writer)
+{
   try {
-    frame_writer = std::make_unique<OBJWriter>(filepath, export_params);
+    r_frame_writer = std::make_unique<OBJWriter>(filepath, export_params);
   }
   catch (const std::system_error &ex) {
     print_exception_error(ex);
     BKE_reportf(export_params.reports, RPT_ERROR, "OBJ Export: Cannot open file '%s'", filepath);
-    return;
+    return false;
   }
-  if (!frame_writer) {
+  if (!r_frame_writer) {
     BLI_assert_msg(false, "File should be writable by now.");
-    return;
+    return false;
   }
-  std::unique_ptr<MTLWriter> mtl_writer = nullptr;
   if (export_params.export_materials || export_params.export_material_groups) {
     try {
-      mtl_writer = std::make_unique<MTLWriter>(filepath, export_params.export_materials);
+      r_mtl_writer = std::make_unique<MTLWriter>(filepath, export_params.export_materials);
     }
     catch (const std::system_error &ex) {
       print_exception_error(ex);
@@ -297,30 +321,41 @@ void export_frame(Depsgraph *depsgraph, const OBJExportParams &export_params, co
                   filepath);
     }
   }
+  return true;
+}
 
-  frame_writer->write_header();
+void export_objects(const OBJExportParams &export_params,
+                    const Span<std::unique_ptr<OBJMesh>> meshes,
+                    const Span<std::unique_ptr<OBJCurve>> curves,
+                    const char *filepath)
+{
+  /* Open */
+  std::unique_ptr<OBJWriter> obj_writer;
+  std::unique_ptr<MTLWriter> mtl_writer;
+  if (!open_stream_writers(export_params, filepath, obj_writer, mtl_writer)) {
+    return;
+  }
 
+  /* Write */
+  obj_writer->write_header();
+  write_mesh_objects(meshes, *obj_writer, mtl_writer.get(), export_params);
+  write_nurbs_curve_objects(curves, *obj_writer);
+  if (mtl_writer && export_params.export_materials) {
+    write_materials(mtl_writer.get(), export_params);
+  }
+}
+
+void export_frame(Depsgraph *depsgraph, const OBJExportParams &export_params, const char *filepath)
+{
   auto [exportable_as_mesh, exportable_as_nurbs] = filter_supported_objects(depsgraph,
                                                                             export_params);
 
-  write_mesh_objects(exportable_as_mesh, *frame_writer, mtl_writer.get(), export_params);
-  if (mtl_writer && export_params.export_materials) {
-    mtl_writer->write_header(export_params.blen_filepath);
-    char dest_dir[FILE_MAX];
-    if (export_params.file_base_for_tests[0] == '\0') {
-      BLI_path_split_dir_part(export_params.filepath, dest_dir, sizeof(dest_dir));
-    }
-    else {
-      STRNCPY(dest_dir, export_params.file_base_for_tests);
-    }
-    BLI_path_slash_native(dest_dir);
-    BLI_path_normalize(dest_dir);
-    mtl_writer->write_materials(export_params.blen_filepath,
-                                export_params.path_mode,
-                                dest_dir,
-                                export_params.export_pbr_extensions);
+  if (exportable_as_mesh.size() == 0 && exportable_as_nurbs.size() == 0) {
+    BKE_reportf(export_params.reports, RPT_WARNING, "OBJ Export: No information to write");
+    return;
   }
-  write_nurbs_curve_objects(exportable_as_nurbs, *frame_writer);
+
+  export_objects(export_params, exportable_as_mesh, exportable_as_nurbs, filepath);
 }
 
 bool append_frame_to_filename(const char *filepath,
