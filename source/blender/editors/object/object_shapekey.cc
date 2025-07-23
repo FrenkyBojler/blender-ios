blender/blender

commit 29b90af6796ab2d3244ef299a7c75b7637ea924d

Shape Key: Delete selected keyblocks
Now that multi-select works with shape key, support deleting selected
keyblocks. New function `shape_key_is_selected` being introduced that
returns true for selected/active shape keys

Co-authored-by: Sybren A. Stüvel <sybren@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/141881

source/blender/editors/object/object_shapekey.cc
@@ -126,6 +126,12 @@ bool shape_key_report_if_any_locked(Object *ob, ReportList *reports)
   return false;
 }
 
+bool shape_key_is_selected(const Object &object, const KeyBlock &kb, const int keyblock_index)
+{
+  /* The active shape key is always considered selected. */
+  return (kb.flag & KEYBLOCK_SEL) || keyblock_index == object.shapenr - 1;
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
@@ -150,23 +156,6 @@ static void object_shape_key_add(bContext *C, Object *ob, const bool from_mix)
 /* -------------------------------------------------------------------- */
 /** \name Remove Shape Key Function
  * \{ */
-
-static bool object_shapekey_remove(Main *bmain, Object *ob)
-{
-  KeyBlock *kb;
-  Key *key = BKE_key_from_object(ob);
-
-  if (key == nullptr) {
-    return false;
-  }
-
-  kb = static_cast<KeyBlock *>(BLI_findlink(&key->block, ob->shapenr - 1));
-  if (kb) {
-    return BKE_object_shapekey_remove(bmain, ob, kb);
-  }
-
-  return false;
-}
 
 static bool object_shape_key_mirror(
     bContext *C, Object *ob, int *r_totmirr, int *r_totfail, bool use_topology)
@@ -429,11 +418,38 @@ static wmOperatorStatus shape_key_remove_exec(bContext *C, wmOperator *op)
     changed = BKE_object_shapekey_free(bmain, ob);
   }
   else {
-    if (shape_key_report_if_active_locked(ob, op->reports)) {
-      return OPERATOR_CANCELLED;
-    }
 
-    changed = object_shapekey_remove(bmain, ob);
+    /* This could be moved into a function of its own at some point. Right now it's only used here,
+     * though, since its inner structure is taylored for allowing shapekey deletion. */
+    const auto visit_selected_shapekeys = [&](FunctionRef<void(KeyBlock & kb)> callback) {
+      Key &key = *BKE_key_from_object(ob);
+      LISTBASE_FOREACH_MUTABLE (KeyBlock *, kb, &key.block) {
+        /* Always try to find the keyblock again, as the previous one may have been deleted. For
+         * the same reason, ob->shapenr has to be re-evaluated on every loop iteration. */
+        const int cur_index = BLI_findindex(&key.block, kb);
+        if (!shape_key_is_selected(*ob, *kb, cur_index)) {
+          continue;
+        }
+        callback(*kb);
+      }
+    };
+
+    int num_selected_but_locked = 0;
+    visit_selected_shapekeys([&](KeyBlock &kb) {
+      if (kb.flag & KEYBLOCK_LOCKED_SHAPE) {
+        num_selected_but_locked++;
+        return;
+      }
+
+      changed |= BKE_object_shapekey_remove(bmain, ob, &kb);
+    });
+
+    if (num_selected_but_locked) {
+      BKE_reportf(op->reports,
+                  changed ? RPT_WARNING : RPT_ERROR,
+                  "Could not delete %d locked shape key(s)",
+                  num_selected_but_locked);
+    }
   }
 
   if (changed) {

source/blender/editors/include/ED_object.hh
@@ -11,11 +11,13 @@
 #include <string>
 
 #include "BLI_compiler_attrs.h"
+#include "BLI_function_ref.hh"
 #include "BLI_map.hh"
 #include "BLI_math_matrix_types.hh"
 #include "BLI_string_ref.hh"
 #include "BLI_vector.hh"
 
+#include "DNA_key_types.h"
 #include "DNA_object_enums.h"
 #include "DNA_userdef_enums.h"
 #include "DNA_windowmanager_types.h"
@@ -98,6 +100,14 @@ bool shape_key_report_if_active_locked(Object *ob, ReportList *reports);
  * \return true if a shape key was locked.
  */
 bool shape_key_report_if_any_locked(Object *ob, ReportList *reports);
+
+/**
+ * Return whether this shapekey is considered 'selected'.
+ *
+ * The active shapekey is always considered 'selected', even though it may not
+ * have its selection flag set.
+ */
+bool shape_key_is_selected(const Object &object, const KeyBlock &kb, int keyblock_index);
 
 /* `object_utils.cc` */
 

FrenkyBojler/blender-ios

commit 031dc7dbc79e08ce40d44c7a33d2ed7ec5eb5a4f

Fix: Delete basis removes all shape key
Caused by 29b90af6796ab2d3244ef299a7c75b7637ea924d
When deleting the first/basis shape key, all successive keys are
deleted. `shapenr` remains 1 during entire iteration and `BLI_findindex`
returns 1 for every keyblock as first element is deleted. This
eventually clears the entire list. To fix this, we could use `cur_index`
as counter intead of finding kb index in the list. This counter will
prevent from running into this situation.

source/blender/editors/object/object_shapekey.cc
@@ -418,7 +418,7 @@ static wmOperatorStatus shape_key_remove_exec(bContext *C, wmOperator *op)
     changed = BKE_object_shapekey_free(bmain, ob);
   }
   else {
-
+    int cur_index = 0;
     /* This could be moved into a function of its own at some point. Right now it's only used here,
      * though, since its inner structure is taylored for allowing shapekey deletion. */
     const auto visit_selected_shapekeys = [&](FunctionRef<void(KeyBlock & kb)> callback) {
@@ -426,8 +426,7 @@ static wmOperatorStatus shape_key_remove_exec(bContext *C, wmOperator *op)
       LISTBASE_FOREACH_MUTABLE (KeyBlock *, kb, &key.block) {
         /* Always try to find the keyblock again, as the previous one may have been deleted. For
          * the same reason, ob->shapenr has to be re-evaluated on every loop iteration. */
-        const int cur_index = BLI_findindex(&key.block, kb);
-        if (!shape_key_is_selected(*ob, *kb, cur_index)) {
+        if (!shape_key_is_selected(*ob, *kb, cur_index++)) {
           continue;
         }
         callback(*kb);