bc54965af1ebfd1a070a2b14201dc9fcc5b4c268
Martijn Versteegh <blender@aaltjegron.nl>
2025-07-23 22:58:54 +0200

    Some variable renaming

--- source/blender/blenkernel/intern/image.cc
+++ source/blender/blenkernel/intern/image.cc
@@ -69,6 +69,7 @@
 #include "BKE_idtype.hh"
 #include "BKE_image.h"
 #include "BKE_image_format.h"
+#include "BKE_image_save.h"
 #include "BKE_lib_id.hh"
 #include "BKE_main.hh"
 #include "BKE_node.hh"
@@ -3865,17 +3866,16 @@ void BKE_image_backup_render(Scene *scene, Image *ima, bool free_current_slot, b
     //! save last slot to file, create BKE_renderslot_backup_to_file or something , probably using BKE_image_render_write_exr
 
     // placeholder. abstract this away and generate name based on project name + slot
-    char buffer[1024];
-    sprintf(buffer, "/tmp/renderslot%d.exr", ima->last_render_slot);
+    char name[1024];
+    sprintf(name, "/tmp/renderslot%d.exr", ima->last_render_slot);
 
-    //! need format from imagesaveopts BKE_image_save_options_init(&opts, bmain, scene, image, nullptr, false, true))  (so need bmain)
     BKE_image_render_write_exr(NULL,
                                last_slot->render,
-                               buffer,
-                                const ImageFormatData *imf,
+                               name,
+                                NULL,
                                 true,
                                 NULL,
-                                -1)
+                                -1);
 
 
     if (cur_slot->render) {
