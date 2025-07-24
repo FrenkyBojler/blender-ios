dbd3abac6873474ba5a9164764675402cb19ac64
Brecht Van Lommel <brecht@blender.org>
2025-07-24 18:15:54 +0200

    Fix typo

--- intern/cycles/scene/geometry_attributes.cpp
+++ intern/cycles/scene/geometry_attributes.cpp
@@ -548,7 +548,7 @@ void GeometryManager::device_update_attributes(Device *device,
     for (AttributeRequest &req : attributes.requests) {
       Attribute *attr = geom->attributes.find(req);
 
-      /* Keep "N" attribute undisplaced for backwareds compatibility in Blender 4.5. */
+      /* Keep "N" attribute undisplaced for backwards compatibility in Blender 4.5. */
       if (attr && attr->std == ATTR_STD_VERTEX_NORMAL) {
         if (Attribute *undisplaced_attr = geom->attributes.find(ATTR_STD_NORMAL_UNDISPLACED)) {
           attr = undisplaced_attr;
