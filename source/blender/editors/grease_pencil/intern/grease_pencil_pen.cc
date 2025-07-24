3ef1a0db017ae743140c8fd94656994c5bbf3e10
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-07-23 20:34:09 -0700

    Initialize `aspect_radio` and `u_scale`

--- source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
@@ -406,6 +406,20 @@ static void pen_add_single(const PenToolOperation &ptd)
   material_indexes.span.last() = material_index;
   material_indexes.finish();
 
+  bke::SpanAttributeWriter<float> aspect_ratios = attributes.lookup_or_add_for_write_span<float>(
+      "aspect_ratio",
+      bke::AttrDomain::Curve,
+      bke::AttributeInitVArray(VArray<float>::from_single(0.0f, curves.curves_num())));
+  aspect_ratios.span.last() = 1.0f;
+  aspect_ratios.finish();
+
+  bke::SpanAttributeWriter<float> u_scales = attributes.lookup_or_add_for_write_span<float>(
+      "u_scale",
+      bke::AttrDomain::Curve,
+      bke::AttributeInitVArray(VArray<float>::from_single(0.0f, curves.curves_num())));
+  u_scales.span.last() = 1.0f;
+  u_scales.finish();
+
   MutableSpan<float3> handles_left = curves.handle_positions_left_for_write();
   MutableSpan<float3> handles_right = curves.handle_positions_right_for_write();
   handles_left.last() = pen_screen_to_global(
@@ -445,7 +459,8 @@ static void pen_add_single(const PenToolOperation &ptd)
   bke::fill_attribute_range_default(
       attributes,
       bke::AttrDomain::Curve,
-      bke::attribute_filter_from_skip_ref({"curve_type", "material_index"}),
+      bke::attribute_filter_from_skip_ref(
+          {"curve_type", "material_index", "aspect_ratio", "u_scale"}),
       curves.curves_range().take_back(1));
 
   drawing->tag_topology_changed();
