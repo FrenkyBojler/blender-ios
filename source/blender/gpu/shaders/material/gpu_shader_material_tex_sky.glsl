99f73b3664ee6bbf9c8de453d5687a6b3c6b73be
marcopavanello <marco.pavanello@protonmail.com>
2025-07-24 10:30:46 +0200

    format glsl

--- source/blender/gpu/shaders/material/gpu_shader_material_tex_sky.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_tex_sky.glsl
@@ -4,20 +4,7 @@
 
 float3 sky_spherical_coordinates(float3 dir)
 {
-  return float3(M_PI_2 - atan(dir.z, length(dir.xy)), atan(dir.x, dir.y), 0.0);
-}
-
-float signx(float x)
-{
-  if (x < 0.0) {
-    return -1.0;
-  }
-  else if (x > 0.0) {
-    return 1.0;
-  }
-  else {
-    return 0.0;
-  }
+  return float3(M_PI_2 - atan(dir.z, length(dir.xy)), atan(dir.x, dir.y), 0.0f);
 }
 
 void node_tex_sky(float3 co,
@@ -33,26 +20,26 @@ void node_tex_sky(float3 co,
   float3 spherical = sky_spherical_coordinates(co);
   float3 xyz;
   float dir_elevation = M_PI_2 - spherical.x;
-  float x = (spherical.y + M_PI + sun_rotation) / (2.0 * M_PI);
-  float fade = 1.0;
+  float x = (spherical.y + M_PI + sun_rotation) / (2.0f * M_PI);
+  float fade = 1.0f;
   float y;
 
-  if (sky_type == 0.0 && co.z < -0.4) {
+  if (sky_type == 0.0f && co.z < -0.4f) {
     /* Black ground if Single Scattering model */
-    color = float4(0.0, 0.0, 0.0, 1.0);
+    color = float4(0.0f, 0.0f, 0.0f, 1.0f);
     return;
   }
-  if (sky_type == 0.0 && co.z < 0.0) {
+  if (sky_type == 0.0f && co.z < 0.0f) {
     /* Ground fade */
-    fade = pow(1.0 + co.z * 2.5, 3.0);
-    y = 0.508;
+    fade = pow(1.0f + co.z * 2.5f, 3.0f);
+    y = 0.508f;
   }
   else {
     /* Undo the non-linear transformation from the sky LUT. */
-    float dir_elevation_abs = (dir_elevation < 0.0) ? -dir_elevation : dir_elevation;
-    y = sqrt(dir_elevation_abs / M_PI_2) * signx(dir_elevation) * 0.5 + 0.5;
+    float dir_elevation_abs = (dir_elevation < 0.0f) ? -dir_elevation : dir_elevation;
+    y = sqrt(dir_elevation_abs / M_PI_2) * sign(dir_elevation) * 0.5f + 0.5f;
   }
   /* Look up color in the precomputed map and convert to RGB. */
   xyz = fade * texture(ima, float3(x, y, layer)).rgb;
-  color = float4(dot(xyz_to_r, xyz), dot(xyz_to_g, xyz), dot(xyz_to_b, xyz), 1);
+  color = float4(dot(xyz_to_r, xyz), dot(xyz_to_g, xyz), dot(xyz_to_b, xyz), 1.0f);
 }
