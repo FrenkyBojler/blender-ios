a6e5f41cb0f8d487746959220f188ecd27ef95e5
Sean Kim <SeanCTKim@protonmail.com>
2025-07-23 19:05:58 -0700

    Fix #142151: Scrape brush 'Plane Offset' not versioned correctly
    The general Plane brush is a replacement for the Flatten, Fill, and
    Scrape brushes that formerly existed. When the old brush types were
    converted to the new type, the 'Plane Offset' usecase was missed due to
    the field not being used for the bundled essential brushes.
    
    To preserve brush behavior across 4.4 and 4.5, Scrape brushes with a
    given 'Plane Offset' need to have this signed distance inverted.
    
    Unfortunately, as this was discovered after 4.5 was officially released,
    and the versioning having no clear indicators that can be used to fix
    this with a subsequent version bump, this commit cannot address users
    who have already started using 4.5. The best we can do in this case is
    fix the incorrect versioning and make a note of the issue in the release
    notes so that future users will not continue to experience this issue.

--- source/blender/blenloader/intern/versioning_450.cc
+++ source/blender/blenloader/intern/versioning_450.cc
@@ -4937,6 +4937,13 @@ static void version_convert_sculpt_planar_brushes(Main *bmain)
         brush->plane_inversion_mode = brush->flag & BRUSH_INVERT_TO_SCRAPE_FILL ?
                                           BRUSH_PLANE_SWAP_HEIGHT_AND_DEPTH :
                                           BRUSH_PLANE_INVERT_DISPLACEMENT;
+
+        /* Note, this fix was committed after some users had already run the versioning after
+         * 4.5 was released. Since 4.5 is an LTS and will be used for the foreseeable future to
+         * transition between 4.x and 5.x, and there being no easy way to version this field
+         * correctly with a new version bump, the fix has been added here, even though that does
+         * not fix the issue for some users currently experiencing this issue. */
+        brush->plane_offset *= -1.0f;
       }
 
       if (brush->flag & BRUSH_PLANE_TRIM) {
