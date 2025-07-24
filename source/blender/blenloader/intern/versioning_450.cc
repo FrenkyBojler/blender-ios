ea4738f764cb809ccce6ae716e081b7d6b675d43
Sean Kim <SeanCTKim@protonmail.com>
2025-07-24 12:33:37 -0700

    Update comment

--- source/blender/blenloader/intern/versioning_450.cc
+++ source/blender/blenloader/intern/versioning_450.cc
@@ -4940,9 +4940,16 @@ static void version_convert_sculpt_planar_brushes(Main *bmain)
 
         /* Note, this fix was committed after some users had already run the versioning after
          * 4.5 was released. Since 4.5 is an LTS and will be used for the foreseeable future to
-         * transition between 4.x and 5.x, and there being no easy way to version this field
-         * correctly with a new version bump, the fix has been added here, even though that does
-         * not fix the issue for some users currently experiencing this issue. */
+         * transition between 4.x and 5.x the fix has been added here, even though that does
+         * not fix the issue for some users with custom brush assets who have started using 4.5
+         * already.
+         *
+         * Since the `sculpt_brush_type` field changed from 'SCULPT_BRUSH_TYPE_SCRAPE' to
+         * 'SCULPT_BRUSH_TYPE_PLANE', we do not have a value that can be used to definitively apply
+         * a corrective versioning step along with a subversion bump without potentially affecting
+         * some false positives.
+         *
+         * See #142151 for more details. */
         brush->plane_offset *= -1.0f;
       }
 
