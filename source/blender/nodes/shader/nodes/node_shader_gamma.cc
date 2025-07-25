b1d747d9dc5a2371078cb682a8fa371e65622f80
Alberto12345678999 <a.pilotto@icloud.com>
2025-07-25 00:42:37 -0300

    Refactor: Reorder includes once more in node_shader_gamma.cc for consistency

--- source/blender/nodes/shader/nodes/node_shader_gamma.cc
+++ source/blender/nodes/shader/nodes/node_shader_gamma.cc
@@ -2,13 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "FN_multi_function_builder.hh"
-#include "NOD_multi_function.hh"
 #include "node_shader_util.hh"
-
-#include "BLI_math_vector.hh"
-#include "BLI_math_vector_types.hh"
-
 
 #include "BLI_math_vector.hh"
 #include "BLI_math_vector_types.hh"
