53540b7b9b045877754c0b8c6423269aaaa4f87e
Mattias Fredriksson <mattias-fredriksson91@hotmail.se>
2025-07-25 03:57:01 +0200

    Readd imports

--- source/blender/io/wavefront_obj/exporter/obj_exporter.cc
+++ source/blender/io/wavefront_obj/exporter/obj_exporter.cc
@@ -10,6 +10,9 @@
 #include <memory>
 #include <system_error>
 
+#include "DNA_curve_enums.h"
+#include "DNA_curve_types.h"
+
 #include "BKE_context.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_report.hh"
