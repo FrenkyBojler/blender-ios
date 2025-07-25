4d63c9845fc9dd2996c468600e2e29cc7802659b
Clément Foucault <foucault.clem@gmail.com>
2025-07-25 17:21:42 +0200

    Refactor: Isolate Catmull Rom code

--- source/blender/draw/intern/shaders/draw_curves_interpolation_comp.glsl
+++ source/blender/draw/intern/shaders/draw_curves_interpolation_comp.glsl
@@ -31,6 +31,11 @@ template float2 mix4<float2>(float2, float2, float2, float2, float4);
 template float3 mix4<float3>(float3, float3, float3, float3, float4);
 template float4 mix4<float4>(float4, float4, float4, float4, float4);
 
+struct EvaluatedPoint {
+  float3 position;
+  float radius;
+};
+
 namespace catmull_rom {
 
 float4 calculate_basis(const float parameter)
@@ -44,18 +49,41 @@ float4 calculate_basis(const float parameter)
                        -s * t * t);
 }
 
-}  // namespace catmull_rom
-
-int4 get_points(CurveType curve_type, uint segment_id, uint curve_start, uint curve_end)
+int4 get_points(uint segment_id, uint curve_start, uint curve_end)
 {
   int4 point_ids = int(segment_id) + int4(-1, +0, +1, +2);
   return clamp(int(curve_start) + point_ids, int4(curve_start), int4(curve_end - 1));
 }
 
-float4 get_weights(CurveType curve_type, float parameter)
+float4 get_weights(float parameter)
 {
   return catmull_rom::calculate_basis(parameter);
 }
+
+EvaluatedPoint get_evaluated_point(uint segment_id,
+                                   uint curve_start,
+                                   uint curve_end,
+                                   float parameter)
+{
+  const int4 point_ids = get_points(segment_id, curve_start, curve_end);
+  const float3 lP_0 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.x);
+  const float3 lP_1 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.y);
+  const float3 lP_2 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.z);
+  const float3 lP_3 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.w);
+
+  const float rad_0 = points_rad_buf[point_ids.x];
+  const float rad_1 = points_rad_buf[point_ids.y];
+  const float rad_2 = points_rad_buf[point_ids.z];
+  const float rad_3 = points_rad_buf[point_ids.w];
+
+  EvaluatedPoint pt;
+  const float4 weights = get_weights(parameter);
+  pt.position = mix4(lP_0, lP_1, lP_2, lP_3, weights);
+  pt.radius = mix4(rad_0, rad_1, rad_2, rad_3, weights);
+  return pt;
+}
+
+}  // namespace catmull_rom
 
 void main()
 {
@@ -72,33 +100,29 @@ void main()
   const uint evaluated_start = curves_evaluated_offsets_buf[curve_id];
   const uint evaluated_end = curves_evaluated_offsets_buf[curve_id + 1];
 
-  float3 last_lP = float3(0.0f);
+  float3 last_position = float3(0.0f);
   float distance_along_curve = 0.0f;
   for (uint i = 0; i < evaluated_end - evaluated_start; i++) {
     const uint out_id = evaluated_start + i;
     const uint segment_id = i / curve_resolution;
-    const int4 point_ids = get_points(curve_type, segment_id, curve_start, curve_end);
-
-    const float3 lP_0 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.x);
-    const float3 lP_1 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.y);
-    const float3 lP_2 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.z);
-    const float3 lP_3 = gpu_attr_load_float3(points_pos_buf, int2(3, 0), point_ids.w);
-
-    const float rad_0 = points_rad_buf[point_ids.x];
-    const float rad_1 = points_rad_buf[point_ids.y];
-    const float rad_2 = points_rad_buf[point_ids.z];
-    const float rad_3 = points_rad_buf[point_ids.w];
-
     const float parameter = float(i % curve_resolution) / float(curve_resolution);
-    const float4 weights = get_weights(curve_type, parameter);
-    const float3 lP = mix4(lP_0, lP_1, lP_2, lP_3, weights);
-    const float radius = mix4(rad_0, rad_1, rad_2, rad_3, weights);
 
-    points_pos_rad_buf[out_id] = float4(lP, radius);
+    EvaluatedPoint pt;
+    switch (curve_type) {
+      case CURVE_TYPE_CATMULL_ROM:
+        pt = catmull_rom::get_evaluated_point(segment_id, curve_start, curve_end, parameter);
+        break;
+      case CURVE_TYPE_POLY:
+      case CURVE_TYPE_BEZIER:
+      case CURVE_TYPE_NURBS:
+        break;
+    }
+
+    points_pos_rad_buf[out_id] = float4(pt.position, pt.radius);
 
     if (true /* TODO(fclem) Make it optional. */) {
-      distance_along_curve += (i == 0) ? 0.0f : distance(last_lP, lP);
-      last_lP = lP;
+      distance_along_curve += (i == 0) ? 0.0f : distance(last_position, pt.position);
+      last_position = pt.position;
       points_time_buf[out_id] = distance_along_curve;
     }
   }
