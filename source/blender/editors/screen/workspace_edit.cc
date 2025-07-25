6efaa123018f9b89774162ad9b98f33922a6530c
Sean Kim <SeanCTKim@protonmail.com>
2025-07-25 15:19:41 -0700

    Pr changes

--- source/blender/editors/screen/workspace_edit.cc
+++ source/blender/editors/screen/workspace_edit.cc
@@ -211,7 +211,10 @@ bool ED_workspace_change(WorkSpace *workspace_new, bContext *C, wmWindowManager 
   /* Automatic mode switching. */
   if (workspace_new->object_mode != workspace_old->object_mode) {
     const Object *object = CTX_data_active_object(C);
-    const View3D *v3d = CTX_wm_view3d(C);
+    /* Behavior that depends on the active area is not expected in the context of workspace
+     * switching, ignore the view-port even if it's available. */
+    const View3D *v3d = nullptr;
+
     const Base *base = CTX_data_active_base(C);
     /* When attempting to switch modes automatically, if the object is not visible and is in Object
      * Mode, prevent forcibly changing the object's mode. This is consistent with both the mode
