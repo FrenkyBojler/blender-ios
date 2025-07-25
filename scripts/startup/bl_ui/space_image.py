3086fd4964f5f241cc5ce4809f525978856d5843
Dalai Felinto <dalai@blender.org>
2025-07-26 01:22:30 +0200

    Move the toggle button to Select menu
    <img width="462" alt="Screenshot 2025-07-25 at 16.08.20.png" src="attachments/80408eec-87b8-49b8-8091-f4deadf5ef39">
    
    Pull Request #1

--- scripts/startup/bl_ui/space_image.py
+++ scripts/startup/bl_ui/space_image.py
@@ -144,7 +144,7 @@ class IMAGE_MT_view_zoom(Menu):
 class IMAGE_MT_select(Menu):
     bl_label = "Select"
 
-    def draw(self, _context):
+    def draw(self, context):
         layout = self.layout
 
         layout.operator("uv.select_all", text="All").action = 'SELECT'
@@ -173,6 +173,10 @@ class IMAGE_MT_select(Menu):
         layout.operator("uv.select_pinned", text="Select Pinned")
         layout.operator("uv.select_split")
         layout.operator("uv.select_overlap")
+
+        layout.separator()
+
+        layout.prop(context.tool_settings, "use_uv_select_sync", text="Sync with Viewport")
 
 
 class IMAGE_MT_select_linked(Menu):
@@ -868,8 +872,6 @@ class IMAGE_HT_header(Header):
 
         # UV editing.
         if show_uvedit:
-            layout.prop(tool_settings, "use_uv_select_sync", text="")
-
             if tool_settings.use_uv_select_sync:
                 layout.template_edit_mode_selection()
             else:
