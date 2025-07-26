a3c6d2994700480000ea47848b65581d8a69f6d7
Aaron Carlisle <carlisle.b3d@gmail.com>
2025-07-26 03:42:15 +0200

    Core: Add-ons: Display Maintainer as "Blender Foundation"
    The author field is displayed in the UI as "Maintainer",
    most of the original authors are no longer active or are part of the foundation.
    Since the core add-ons are maintained the same as the rest of Blender they should be shown accordingly.
    
    The exceptions here are the GLTF and Hydra add-ons; these Khronos Group and AMD are included as maintainers.
    
    The orginal author field is preserved in the chance the add-on looses it's "core" status.
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/140690

--- scripts/addons_core/io_scene_gltf2/__init__.py
+++ scripts/addons_core/io_scene_gltf2/__init__.py
@@ -4,8 +4,10 @@
 
 bl_info = {
     'name': 'glTF 2.0 format',
-    'author': 'Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein, and many external contributors',
-    "version": (4, 5, 47),
+    # This is now displayed as the maintainer, so show the foundation.
+    # "author": "Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein", # Original Authors
+    'author': "Blender Foundation, Khronos Group",
+    "version": (5, 0, 16),
     'blender': (4, 4, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
