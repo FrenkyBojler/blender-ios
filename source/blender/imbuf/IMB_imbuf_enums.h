44b30c3be870aea69593965278fe3af7a5afe716
Peter Kovář <peter.kovar@reflexion.tv>
2025-07-26 19:19:07 +0200

    Add JPEG XL I/O
    Signed-off-by: Peter Kovář <peter.kovar@reflexion.tv>

--- source/blender/imbuf/IMB_imbuf_enums.h
+++ source/blender/imbuf/IMB_imbuf_enums.h
@@ -36,6 +36,7 @@ enum eImbFileType {
 #ifdef WITH_IMAGE_WEBP
   IMB_FTYPE_WEBP = 14,
 #endif
+  IMB_FTYPE_JXL = 15,
 };
 
 /** NOTE: Keep in sync with #MovieClipProxy.build_size_flag */
---
+++ source/blender/imbuf/intern/format_jxl.cc
@@ -0,0 +1,43 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup imbuf
+ */
+
+#include "oiio/openimageio_support.hh"
+
+#include "IMB_filetype.hh"
+#include "IMB_imbuf_types.hh"
+#include <cstdio>
+
+OIIO_NAMESPACE_USING
+using namespace blender::imbuf;
+
+bool imb_is_a_jxl(const unsigned char *mem, size_t size)
+{
+  return imb_oiio_check(mem, size, "jxl");
+}
+
+ImBuf *imb_load_jxl(const unsigned char *mem, size_t size, int flags, ImFileColorSpace &r_colorspace)
+{
+  ImageSpec config, spec;
+
+  ReadContext ctx{mem, size, "jxl", IMB_FTYPE_JXL, flags};
+
+  ImBuf *ibuf = imb_oiio_read(ctx, config, r_colorspace, spec);
+
+  return ibuf;
+}
+
+bool imb_save_jxl(ImBuf *ibuf, const char *filepath, int flags)
+{
+  const int file_channels = ibuf->planes >> 3;
+  const TypeDesc data_format = TypeDesc::FLOAT;
+
+  WriteContext ctx = imb_create_write_context("jxl", ibuf, flags);
+  ImageSpec file_spec = imb_create_write_spec(ctx, file_channels, data_format);
+
+  return imb_oiio_write(ctx, filepath, file_spec);
+}
