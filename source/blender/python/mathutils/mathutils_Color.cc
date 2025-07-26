235d64eacf0d3569be96fafa169f9ac2ba19547a
Andrej730 <azhilenkov@gmail.com>
2025-07-26 11:59:36 +0500

    Color, Euler - improve python signatures

--- source/blender/python/mathutils/mathutils_Color.cc
+++ source/blender/python/mathutils/mathutils_Color.cc
@@ -1201,7 +1201,7 @@ static PyMethodDef Color_methods[] = {
 PyDoc_STRVAR(
     /* Wrap. */
     color_doc,
-    ".. class:: Color(rgb)\n"
+    ".. class:: Color(rgb=(0.0, 0.0, 0.0), /)\n"
     "\n"
     "   This object gives access to Colors in Blender.\n"
     "\n"
@@ -1210,7 +1210,7 @@ PyDoc_STRVAR(
     "   which are in sRGB color space.\n"
     "\n"
     "   :arg rgb: (red, green, blue) color values where (0, 0, 0) is black & (1, 1, 1) is white.\n"
-    "   :type rgb: Sequence[float]\n");
+    "   :type rgb: Sequence[float], (optional)\n");
 PyTypeObject color_Type = {
     /*ob_base*/ PyVarObject_HEAD_INIT(nullptr, 0)
     /*tp_name*/ "Color",
