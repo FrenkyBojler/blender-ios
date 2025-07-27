f95b29bab6af6af696ba25c509d3fd7eedde13b1
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-07-26 21:53:08 -0700

    Fix.

--- source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_pen.cc
@@ -764,10 +764,10 @@ static wmOperatorStatus grease_pencil_pen_invoke(bContext *C, wmOperator *op, co
   op->customdata = ptd_pointer;
   PenToolOperation &ptd = *ptd_pointer;
 
+  ptd.vc = vc;
   GreasePencil *grease_pencil = static_cast<GreasePencil *>(vc.obact->data);
   ptd.grease_pencil = grease_pencil;
   ptd.projection = ED_view3d_ob_project_mat_get(ptd.vc.rv3d, ptd.vc.obact);
-  ptd.vc = vc;
 
   /* Distance threshold for mouse clicks to affect the spline or its points */
   ptd.mouse_co = float2(event->mval);
