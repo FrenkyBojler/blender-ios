882f01691f138ba9a83c6174c48a1f991b2ead2f
Benjamin Beilharz <b@ben.graphics>
2025-07-27 15:49:24 +0200

    Compositor: Adding extension modes to displacement node
    The node uses anisotropic sampling (which is currently not supported for the
    extension mode) the extension modes are only rendered in the node if any
    other interpolation method but anisotropic is selected.

--- source/blender/makesdna/DNA_node_types.h
+++ source/blender/makesdna/DNA_node_types.h
@@ -1589,6 +1589,8 @@ typedef struct NodeScaleData {
 
 typedef struct NodeDisplaceData {
   short interpolation;
+  char extension_x;
+  char extension_y;
 } NodeDisplaceData;
 
 typedef struct NodePlaneTrackDeformData {
