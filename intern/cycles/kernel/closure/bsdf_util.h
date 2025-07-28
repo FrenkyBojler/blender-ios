43ce08c1f45b717f92a27be19ecd40a8ff03b733
Amogh Shivaram <ashivaram264@gmail.com>
2025-07-28 17:16:32 -0500

    Bring back IOR adjustment for very thin films

--- intern/cycles/kernel/closure/bsdf_util.h
+++ intern/cycles/kernel/closure/bsdf_util.h
@@ -465,6 +465,12 @@ ccl_device Spectrum fresnel_iridescence(KernelGlobals kg,
                                         const float thickness,
                                         ccl_private float *r_cos_theta_3)
 {
+  /* For films below 1nm, the wave-optic-based Airy summation approach no longer applies,
+   * so blend towards the case without coating. */
+  if (thickness < 1.0f) {
+    eta2 = mix(eta1, eta2, smoothstep(0.0f, 1.0f, thickness));
+  }
+
   float cos_theta_2;
   float2 phi12;
 
