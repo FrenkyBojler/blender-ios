blender/blender

commit 93be12fde050ed5a2102a0e01db659c97e318114

ImBuf: Linearize float buffers from FFmpeg
Blender expects float buffers to be in scene linear space, which was
violated bu the 1012bit movie reading code. While such image buffers
can be displayed correctly, performing operations in various areas
of Blender might lead to unexpected results.

The non-linear colorspace for ImBuf is expected to be "internal-only"
to a specific area, like VSE.

This is only done for Image and MovieClip data-blocks, sequencer still
reads movie files in their original colorspace as it helps performance and
sequencer can not be referenced from places where linear colorspace
for float buffer is really important.

Pull Request: https://projects.blender.org/blender/blender/pulls/141603

source/blender/blenkernel/intern/image.cc
@@ -2677,25 +2677,27 @@ bool BKE_imbuf_write_stamp(const Scene *scene,
 }
 
 MovieReader *openanim_noload(const char *filepath,
-                             int flags,
-                             int streamindex,
+                             const int flags,
+                             const int streamindex,
+                             const bool keep_original_colorspace,
                              char colorspace[IMA_MAX_SPACE])
 {
   MovieReader *anim;
 
-  anim = MOV_open_file(filepath, flags, streamindex, colorspace);
+  anim = MOV_open_file(filepath, flags, streamindex, keep_original_colorspace, colorspace);
   return anim;
 }
 
 MovieReader *openanim(const char *filepath,
-                      int flags,
-                      int streamindex,
+                      const int ibuf_flags,
+                      const int streamindex,
+                      const bool keep_original_colorspace,
                       char colorspace[IMA_MAX_SPACE])
 {
   MovieReader *anim;
   ImBuf *ibuf;
 
-  anim = MOV_open_file(filepath, flags, streamindex, colorspace);
+  anim = MOV_open_file(filepath, ibuf_flags, streamindex, keep_original_colorspace, colorspace);
   if (anim == nullptr) {
     return nullptr;
   }
@@ -4145,7 +4147,7 @@ static ImBuf *load_movie_single(Image *ima, ImageUser *iuser, int frame, const i
     BKE_image_user_file_path(&iuser_t, ima, filepath);
 
     /* FIXME: make several stream accessible in image editor, too. */
-    ia->anim = openanim(filepath, flags, 0, ima->colorspace_settings.name);
+    ia->anim = openanim(filepath, flags, 0, false, ima->colorspace_settings.name);
 
     /* let's initialize this user */
     if (ia->anim && iuser && iuser->frames == 0) {

source/blender/blenkernel/intern/movieclip.cc
@@ -591,7 +591,7 @@ static void movieclip_open_anim_file(MovieClip *clip)
     BLI_path_abs(filepath_abs, ID_BLEND_PATH_FROM_GLOBAL(&clip->id));
 
     /* FIXME: make several stream accessible in image editor, too */
-    clip->anim = openanim(filepath_abs, IB_byte_data, 0, clip->colorspace_settings.name);
+    clip->anim = openanim(filepath_abs, IB_byte_data, 0, false, clip->colorspace_settings.name);
 
     if (clip->anim) {
       if (clip->flag & MCLIP_USE_PROXY_CUSTOM_DIR) {

source/blender/blenkernel/BKE_image.hh
@@ -137,12 +137,14 @@ bool BKE_imbuf_write_as(ImBuf *ibuf,
  * Used by sequencer too.
  */
 MovieReader *openanim(const char *filepath,
-                      int flags,
+                      int ibuf_flags,
                       int streamindex,
+                      bool keep_original_colorspace,
                       char colorspace[IMA_MAX_SPACE]);
 MovieReader *openanim_noload(const char *filepath,
                              int flags,
                              int streamindex,
+                             bool keep_original_colorspace,
                              char colorspace[IMA_MAX_SPACE]);
 
 void BKE_image_tag_time(Image *ima);

source/blender/editors/space_sequencer/sequencer_drag_drop.cc
@@ -548,8 +548,10 @@ static void prefetch_data_fn(void *custom_data, wmJobWorkerStatus * /*worker_sta
 #endif
   }
 
+  /* The movie reader is not used to access pixel data here, so avoid internal colorspace
+   * conversions that ensures typical color pipeline in Blender as they might be expensive. */
   char colorspace[/*MAX_COLORSPACE_NAME*/ 64] = "\0";
-  MovieReader *anim = openanim(job_data->path, IB_byte_data, 0, colorspace);
+  MovieReader *anim = openanim(job_data->path, IB_byte_data, 0, true, colorspace);
 
   if (anim != nullptr) {
     g_drop_coords.strip_len = MOV_get_duration_frames(anim, IMB_TC_NONE);

source/blender/imbuf/intern/thumbs.cc
@@ -393,7 +393,10 @@ static ImBuf *thumb_create_ex(const char *file_path,
       }
       else if (THB_SOURCE_MOVIE == source) {
         MovieReader *anim = nullptr;
-        anim = MOV_open_file(file_path, IB_byte_data | IB_metadata, 0, nullptr);
+        /* Image buffer is converted from float to byte and only the latter one is used, and the
+         * conversion process is aware of the float colorspace. So it is possible to save some
+         * compute time by keeping the original colorspace for movies. */
+        anim = MOV_open_file(file_path, IB_byte_data | IB_metadata, 0, true, nullptr);
         if (anim != nullptr) {
           img = MOV_decode_frame(anim, 0, IMB_TC_NONE, IMB_PROXY_NONE);
           if (img == nullptr) {

source/blender/imbuf/movie/MOV_read.hh
@@ -38,6 +38,7 @@ struct MovieProxyBuilder;
 MovieReader *MOV_open_file(const char *filepath,
                            int ib_flags,
                            int streamindex,
+                           bool keep_original_colorspace,
                            char colorspace[IM_MAX_SPACE]);
 
 /**

source/blender/imbuf/movie/intern/movie_proxy_indexer.cc
@@ -1258,8 +1258,11 @@ MovieReader *movie_open_proxy(MovieReader *anim, IMB_Proxy_Size preview_size)
 
   get_proxy_filepath(anim, preview_size, filepath, false);
 
-  /* proxies are generated in the same color space as animation itself */
-  anim->proxy_anim[i] = MOV_open_file(filepath, 0, 0, anim->colorspace);
+  /* Proxies are generated in the same color space as animation itself.
+   *
+   * Also skip any colorspace conversion to the color pipeline design as it helps performance and
+   * the image buffers from the proxy builder are not used anywhere else in Blender. */
+  anim->proxy_anim[i] = MOV_open_file(filepath, 0, 0, true, anim->colorspace);
 
   anim->proxies_tried |= preview_size;
 

source/blender/imbuf/movie/intern/movie_read.cc
@@ -99,8 +99,9 @@ IDProperty *MOV_load_metadata(MovieReader *anim)
 }
 
 MovieReader *MOV_open_file(const char *filepath,
-                           int ib_flags,
-                           int streamindex,
+                           const int ib_flags,
+                           const int streamindex,
+                           const bool keep_original_colorspace,
                            char colorspace[IM_MAX_SPACE])
 {
   MovieReader *anim;
@@ -122,6 +123,7 @@ MovieReader *MOV_open_file(const char *filepath,
     STRNCPY(anim->filepath, filepath);
     anim->ib_flags = ib_flags;
     anim->streamindex = streamindex;
+    anim->keep_original_colorspace = keep_original_colorspace;
   }
   return anim;
 }
@@ -1248,11 +1250,9 @@ static ImBuf *ffmpeg_fetchibuf(MovieReader *anim, int position, IMB_Timecode_Typ
       MEM_mallocN_aligned(pixel_size * anim->x * anim->y, align, "ffmpeg ibuf"));
   if (anim->is_float) {
     IMB_assign_float_buffer(cur_frame_final, (float *)buffer_data, IB_TAKE_OWNERSHIP);
-    cur_frame_final->float_buffer.colorspace = colormanage_colorspace_get_named(anim->colorspace);
   }
   else {
     IMB_assign_byte_buffer(cur_frame_final, buffer_data, IB_TAKE_OWNERSHIP);
-    cur_frame_final->byte_buffer.colorspace = colormanage_colorspace_get_named(anim->colorspace);
   }
 
   AVFrame *final_frame = ffmpeg_frame_by_pts_get(anim, pts_to_search);
@@ -1266,6 +1266,30 @@ static ImBuf *ffmpeg_fetchibuf(MovieReader *anim, int position, IMB_Timecode_Typ
    * this case skip post-processing and return current image buffer. */
   if (final_frame != nullptr) {
     ffmpeg_postprocess(anim, final_frame, cur_frame_final);
+  }
+
+  if (anim->is_float) {
+    if (anim->keep_original_colorspace) {
+      /* Movie has been explicitly requested to keep original colorspace, regardless of the nature
+       * of the buffer. */
+      cur_frame_final->float_buffer.colorspace = colormanage_colorspace_get_named(
+          anim->colorspace);
+    }
+    else {
+      /* Float buffers are expected to be in the scene linear color space.
+       * Linearize the buffer if it is in a different space.
+       *
+       * It might not be the most optimal thing to do from the playback performance in the
+       * sequencer perspective, but it ensures that other areas in Blender do not run into obscure
+       * color space mismatches. */
+      colormanage_imbuf_make_linear(cur_frame_final, anim->colorspace);
+    }
+  }
+  else {
+    /* Colorspace conversion is lossy for byte buffers, so only assign the colorspace.
+     * It is up to artists to ensure operations on byte buffers do not involve mixing different
+     * colorspaces. */
+    cur_frame_final->byte_buffer.colorspace = colormanage_colorspace_get_named(anim->colorspace);
   }
 
   anim->cur_position = position;

FrenkyBojler/blender-ios

commit 16e117cc7cf637e439d70c5f1bd93234a24f5a26

Fix #143191: 10/12 bit videos import much too dark
Originally reported for ProRes codec, but it affects multiple
other codecs: any format which is read as >= 10bit had the issue.

A regression since 93be12fde0.

The cause of the issue is that movie clip and image data-blocks
did linearization on their side, which was quite hidden to find.
Now the color management is done on the movie reader level.

Additionally, added regression test to catch such issues early
on in the future.

source/blender/blenkernel/intern/image.cc
@@ -4166,7 +4166,6 @@ static ImBuf *load_movie_single(Image *ima, ImageUser *iuser, int frame, const i
     ibuf = IMB_makeSingleUser(MOV_decode_frame(ia->anim, fra, IMB_TC_RECORD_RUN, IMB_PROXY_NONE));
 
     if (ibuf) {
-      colormanage_imbuf_make_linear(ibuf, ima->colorspace_settings.name);
       image_init_after_load(ima, iuser, ibuf);
     }
   }

source/blender/blenkernel/intern/movieclip.cc
@@ -619,9 +619,6 @@ static ImBuf *movieclip_load_movie_file(MovieClip *clip,
     int fra = framenr - clip->start_frame + clip->frame_offset;
 
     ibuf = MOV_decode_frame(clip->anim, fra, IMB_Timecode_Type(tc), IMB_Proxy_Size(proxy));
-    if (ibuf) {
-      colormanage_imbuf_make_linear(ibuf, clip->colorspace_settings.name);
-    }
   }
 
   return ibuf;

tests/files/compositor/input/compositor_renders/movie_input_colorspace_float.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a3c2866dc547ce7037df587fc3c1bf6fffe41acf5962a238cdab4bb9d21edcb5
+size 122127

tests/files/compositor/input/media/movie_10bit.mov
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5b3ca7f847ddf94db0127a50391bb834f8d5affcf99ceb5c57043f7bf0f7c53
+size 34417

tests/files/compositor/input/movie_input_colorspace_float.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e281ea3d2449bd74921fd414d63f9e87d9280901d2298e50d575af70f282a3e9
+size 958927