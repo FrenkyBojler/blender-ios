fe8e43f03151a55c48faca01d4ddddfcf9274ddf
Habib Gahbiche <habib@blender.org>
2025-07-30 11:53:28 +0100

    make format

--- tests/python/compositing_node_group.py
+++ tests/python/compositing_node_group.py
@@ -5,10 +5,12 @@
 import bpy
 import unittest
 
+
 class CompositingNodeGroupTest(unittest.TestCase):
     """
     Tests specific to the root compositing node tree
     """
+
     def setUp(self):
         bpy.ops.wm.read_factory_settings(use_empty=True)
 
@@ -16,6 +18,7 @@ class CompositingNodeGroupTest(unittest.TestCase):
         scene = bpy.data.scenes["Scene"]
         with self.assertRaises(RuntimeError):
             scene.compositing_node_group = bpy.data.node_groups.new("invalid", "GeometryNodeTree")
+
 
 if __name__ == "__main__":
     import sys
