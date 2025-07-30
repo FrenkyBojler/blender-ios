d1b20a411e13d2d42972de0f316af51427a44728
Jacques Lucke <jacques@blender.org>
2025-07-30 09:23:50 +0200

    cleanup

--- source/blender/nodes/geometry/nodes/node_geo_combine_bundle.cc
+++ source/blender/nodes/geometry/nodes/node_geo_combine_bundle.cc
@@ -5,10 +5,13 @@
 #include "node_geometry_util.hh"
 
 #include "NOD_geo_bundle.hh"
+#include "NOD_node_extra_info.hh"
 #include "NOD_socket_items_blend.hh"
 #include "NOD_socket_items_ops.hh"
 #include "NOD_socket_items_ui.hh"
 #include "NOD_socket_search_link.hh"
+
+#include "BKE_idprop.hh"
 
 #include "BLO_read_write.hh"
 
@@ -66,32 +69,6 @@ static bool node_insert_link(bNodeTree *tree, bNode *node, bNodeLink *link)
 {
   return socket_items::try_add_item_via_any_extend_socket<CombineBundleItemsAccessor>(
       *tree, *node, *node, *link);
-}
-
-static void node_layout(uiLayout *layout, bContext *C, PointerRNA *node_ptr)
-{
-  const SpaceNode *snode = CTX_wm_space_node(C);
-  bNode &node = *node_ptr->data_as<bNode>();
-  NodeGeometryCombineBundle &storage = node_storage(node);
-  if (snode && storage.flag & NODE_GEO_COMBINE_BUNDLE_FLAG_MAY_NEED_SYNC) {
-    const ed::space_node::NodeSyncState state = ed::space_node::sync_sockets_state_combine_bundle(
-        *snode, node);
-    switch (state) {
-      case ed::space_node::NodeSyncState::NoSyncSource:
-      case ed::space_node::NodeSyncState::Synced: {
-        storage.flag &= ~NODE_GEO_COMBINE_BUNDLE_FLAG_MAY_NEED_SYNC;
-        break;
-      }
-      case ed::space_node::NodeSyncState::CanBeSynced: {
-        PointerRNA props = layout->op("node.sockets_sync", "Sync", ICON_FILE_REFRESH);
-        RNA_string_set(&props, "node_name", node.name);
-        break;
-      }
-      case ed::space_node::NodeSyncState::ConflictingSyncSources: {
-        break;
-      }
-    }
-  }
 }
 
 static void node_layout_ex(uiLayout *layout, bContext *C, PointerRNA *node_ptr)
@@ -177,6 +154,42 @@ static void node_blend_read(bNodeTree & /*tree*/, bNode &node, BlendDataReader &
   socket_items::blend_read_data<CombineBundleItemsAccessor>(&reader, node);
 }
 
+static void node_extra_info(NodeExtraInfoParams &params)
+{
+  const SpaceNode *snode = CTX_wm_space_node(&params.C);
+  const bNode &node = params.node;
+  const NodeGeometryCombineBundle &storage = node_storage(node);
+  if (snode && storage.flag & NODE_GEO_COMBINE_BUNDLE_FLAG_MAY_NEED_SYNC) {
+    const ed::space_node::NodeSyncState state = ed::space_node::sync_sockets_state_combine_bundle(
+        *snode, node);
+    switch (state) {
+      case ed::space_node::NodeSyncState::NoSyncSource:
+      case ed::space_node::NodeSyncState::Synced: {
+        const_cast<NodeGeometryCombineBundle &>(storage).flag &=
+            ~NODE_GEO_COMBINE_BUNDLE_FLAG_MAY_NEED_SYNC;
+        break;
+      }
+      case ed::space_node::NodeSyncState::CanBeSynced: {
+        NodeExtraInfoRow row;
+        row.text = TIP_("Sync");
+        row.icon = ICON_FILE_REFRESH;
+        row.set_execute_fn = [node = &params.node](uiBut &but) {
+          wmOperatorType *ot = WM_operatortype_find("NODE_OT_sockets_sync", false);
+          UI_but_operator_set(&but, ot, wm::OpCallContext::InvokeDefault);
+          PointerRNA *opptr = UI_but_operator_ptr_ensure(&but);
+          opptr->data = bke::idprop::create_group("wmOperatorProperties").release();
+          RNA_string_set(opptr, "node_name", node->name);
+        };
+        params.rows.append(std::move(row));
+        break;
+      }
+      case ed::space_node::NodeSyncState::ConflictingSyncSources: {
+        break;
+      }
+    }
+  }
+}
+
 static void node_register()
 {
   static blender::bke::bNodeType ntype;
@@ -189,10 +202,10 @@ static void node_register()
   ntype.initfunc = node_init;
   ntype.geometry_node_execute = node_geo_exec;
   ntype.insert_link = node_insert_link;
-  ntype.draw_buttons = node_layout;
   ntype.draw_buttons_ex = node_layout_ex;
   ntype.gather_link_search_ops = node_gather_link_searches;
   ntype.register_operators = node_operators;
+  ntype.get_extra_info = node_extra_info;
   ntype.blend_write_storage_content = node_blend_write;
   ntype.blend_data_read_storage_content = node_blend_read;
   bke::node_type_storage(ntype, "NodeGeometryCombineBundle", node_free_storage, node_copy_storage);
