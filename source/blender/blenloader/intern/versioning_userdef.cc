25f3e1f882cd17d32cc7569b58aa3a5c27c76dbe
Pablo Vazquez <pablo@blender.org>
2025-07-31 16:25:47 +0200

    Update versioning code to main

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -345,7 +345,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_node.node_zone_closure);
   }
 
-  if (!USER_VERSION_ATLEAST(500, 45)) {
+  if (!USER_VERSION_ATLEAST(500, 50)) {
     FROM_DEFAULT_V4_UCHAR(common.anim.preview_range);
   }
 
