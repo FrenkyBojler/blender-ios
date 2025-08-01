96b903dfd3309d9c229c05d648236a7e186434aa
Nika Kutsniashvili <nickberckley@gmail.com>
2025-08-01 13:46:40 +0400

    Remove Draw Action from Sequencer

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -3800,12 +3800,6 @@ static void rna_def_userdef_theme_space_seq(BlenderRNA *brna)
   RNA_def_property_ui_text(prop, "Keyframe Border Selected", "Color of selected keyframe border");
   RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
 
-  prop = RNA_def_property(srna, "draw_action", PROP_FLOAT, PROP_COLOR_GAMMA);
-  RNA_def_property_float_sdna(prop, nullptr, "bone_pose");
-  RNA_def_property_array(prop, 3);
-  RNA_def_property_ui_text(prop, "Draw Action", "");
-  RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
-
   prop = RNA_def_property(srna, "preview_back", PROP_FLOAT, PROP_COLOR_GAMMA);
   RNA_def_property_float_sdna(prop, nullptr, "preview_back");
   RNA_def_property_array(prop, 3);
