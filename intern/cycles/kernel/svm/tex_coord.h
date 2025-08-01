9a0b87db98931e51da9c3f7b2ca6582fb7dcc343
Brecht Van Lommel <brecht@blender.org>
2025-08-01 20:45:19 +0200

    Update for changes in main

--- intern/cycles/kernel/svm/tex_coord.h
+++ intern/cycles/kernel/svm/tex_coord.h
@@ -364,8 +364,8 @@ ccl_device_noinline void svm_node_normal_map(KernelGlobals kg,
       const AttributeDescriptor attr_undisplaced_normal = find_attribute(
           kg, sd->object, sd->prim, ATTR_STD_NORMAL_UNDISPLACED);
       if (attr_undisplaced_normal.offset != ATTR_STD_NOT_FOUND) {
-        normal = primitive_surface_attribute<float3>(
-            kg, sd, attr_undisplaced_normal, nullptr, nullptr);
+        normal =
+            primitive_surface_attribute<float3>(kg, sd, attr_undisplaced_normal, false, false).val;
       }
       else {
         normal = triangle_smooth_normal_unnormalized(kg, sd, sd->Ng, sd->prim, sd->u, sd->v);
