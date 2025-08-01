b60a6bcee9e5f9c624a0c0526a7d0274758a8799
Alberto12345678999 <a.pilotto@icloud.com>
2025-08-01 19:57:00 -0300

    Refactor: Update CMP_NODE_GAMMA to CMP_NODE_GAMMA_DEPRECATED for better clarity and maintainability;
    Change socket names to  `version_node_socket_name`; remove `printf` debug test.

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -1163,15 +1163,15 @@ static void do_version_convert_to_generic_nodes(bNodeTree *node_tree)
 
         /* The Compositor node uses "Image" and "Gamma" as socket names and identifiers while the
          * Shader node uses "Color" and "Gamma" as socket names and identifiers. */
-        bNodeSocket *gamma_first_input = blender::bke::node_find_socket(*node, SOCK_IN, "Image");
-        STRNCPY_UTF8(gamma_first_input->identifier, "Color");
-        STRNCPY_UTF8(gamma_first_input->name, "Color");
-        bNodeSocket *gamma_float_input = blender::bke::node_find_socket(*node, SOCK_IN, "Gamma");
-        STRNCPY_UTF8(gamma_float_input->identifier, "Gamma");
-        STRNCPY_UTF8(gamma_float_input->name, "Gamma");
-        bNodeSocket *gamma_image_output = blender::bke::node_find_socket(*node, SOCK_OUT, "Image");
-        STRNCPY_UTF8(gamma_image_output->identifier, "Color");
-        STRNCPY_UTF8(gamma_image_output->name, "Color");
+        bNodeSocket *version_node_socket_name = blender::bke::node_find_socket(*node, SOCK_IN, "Image");
+        STRNCPY_UTF8(version_node_socket_name->identifier, "Color");
+        STRNCPY_UTF8(version_node_socket_name->name, "Color");
+        bNodeSocket *version_node_socket_name = blender::bke::node_find_socket(*node, SOCK_IN, "Gamma");
+        STRNCPY_UTF8(version_node_socket_name->identifier, "Gamma");
+        STRNCPY_UTF8(version_node_socket_name->name, "Gamma");
+        bNodeSocket *version_node_socket_name = blender::bke::node_find_socket(*node, SOCK_OUT, "Image");
+        STRNCPY_UTF8(version_node_socket_name->identifier, "Color");
+        STRNCPY_UTF8(version_node_socket_name->name, "Color");
         break;
       }
       default:
@@ -1459,7 +1459,6 @@ static void do_version_composite_node_in_scene_tree(bNodeTree &node_tree, bNode 
   if (STREQ(image_input->name, "Imagem")) {
   STRNCPY_UTF8(image_input->name, "Image");
   }
-  printf("do_version: first Group-Output input is '%s'\n", image_input->name);
   BLI_assert(blender::StringRef(image_input->name) == "Image");
   copy_v4_v4(image_input->default_value_typed<bNodeSocketValueRGBA>()->value,
              old_image_input->default_value_typed<bNodeSocketValueRGBA>()->value);
