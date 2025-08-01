92956815627be9a758e535e287375495009983af
Sybren A. Stüvel <sybren@blender.org>
2025-08-01 16:06:05 +0200

    Move `make_generate_datamodels.py` from `build_files` to `tools`

--- build_files/cmake/generate_datamodels.cmake
+++ build_files/cmake/generate_datamodels.cmake
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-set(GENERATE_DATAMODELS_SCRIPT "${CMAKE_SOURCE_DIR}/build_files/utils/make_generate_datamodels.py")
+set(GENERATE_DATAMODELS_SCRIPT "${CMAKE_SOURCE_DIR}/tools/utils/make_generate_datamodels.py")
 
 add_custom_target(generate_datamodels
     COMMAND ${PYTHON_EXECUTABLE} ${GENERATE_DATAMODELS_SCRIPT} ${CMAKE_SOURCE_DIR}
