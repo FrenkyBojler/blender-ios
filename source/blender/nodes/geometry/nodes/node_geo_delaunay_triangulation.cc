46f25ba0bd2e0e265eefee4ae54d024e2816fd5f
Falk David <falk@blender.org>
2025-08-02 13:31:24 +0200

    Populate Group ID annonymous attribute

--- source/blender/nodes/geometry/nodes/node_geo_delaunay_triangulation.cc
+++ source/blender/nodes/geometry/nodes/node_geo_delaunay_triangulation.cc
@@ -12,7 +12,6 @@
 
 #include "FN_field.hh"
 
-#include "GEO_CDT_to_mesh.hh"
 #include "GEO_mesh_copy_selection.hh"
 
 #include "node_geometry_util.hh"
@@ -472,6 +471,96 @@ static Vector<CDTGeometrySetInput> cdt_inputs_from_groups(const GeometrySet &geo
   return inputs;
 }
 
+static Mesh *cdts_to_mesh(const Span<meshintersect::CDT_result<double>> results,
+                          const std::optional<std::string> dst_group_id_attribute_id)
+{
+  Array<int> vert_groups_data(results.size() + 1);
+  Array<int> edge_groups_data(results.size() + 1);
+  Array<int> face_groups_data(results.size() + 1);
+  Array<int> loop_groups_data(results.size() + 1);
+  threading::parallel_for(results.index_range(), 1024, [&](const IndexRange results_range) {
+    for (const int i_result : results_range) {
+      const meshintersect::CDT_result<double> &result = results[i_result];
+      vert_groups_data[i_result] = result.vert.size();
+      edge_groups_data[i_result] = result.edge.size();
+      face_groups_data[i_result] = result.face.size();
+      int loop_len = 0;
+      for (const Vector<int> &face : result.face) {
+        loop_len += face.size();
+      }
+      loop_groups_data[i_result] = loop_len;
+    }
+  });
+
+  const OffsetIndices vert_groups = offset_indices::accumulate_counts_to_offsets(vert_groups_data);
+  const OffsetIndices edge_groups = offset_indices::accumulate_counts_to_offsets(edge_groups_data);
+  const OffsetIndices face_groups = offset_indices::accumulate_counts_to_offsets(face_groups_data);
+  const OffsetIndices loop_groups = offset_indices::accumulate_counts_to_offsets(loop_groups_data);
+
+  Mesh *mesh = BKE_mesh_new_nomain(vert_groups.total_size(),
+                                   edge_groups.total_size(),
+                                   face_groups.total_size(),
+                                   loop_groups.total_size());
+
+  MutableSpan<float3> all_positions = mesh->vert_positions_for_write();
+  MutableSpan<int2> all_edges = mesh->edges_for_write();
+  MutableSpan<int> all_face_offsets = mesh->face_offsets_for_write();
+  MutableSpan<int> all_corner_verts = mesh->corner_verts_for_write();
+
+  threading::parallel_for(results.index_range(), 1024, [&](const IndexRange results_range) {
+    for (const int i_result : results_range) {
+      const meshintersect::CDT_result<double> &result = results[i_result];
+      const IndexRange verts_range = vert_groups[i_result];
+      const IndexRange edges_range = edge_groups[i_result];
+      const IndexRange faces_range = face_groups[i_result];
+      const IndexRange loops_range = loop_groups[i_result];
+
+      MutableSpan<float3> positions = all_positions.slice(verts_range);
+      for (const int i : result.vert.index_range()) {
+        positions[i] = float3(float(result.vert[i].x), float(result.vert[i].y), 0.0f);
+      }
+
+      MutableSpan<int2> edges = all_edges.slice(edges_range);
+      for (const int i : result.edge.index_range()) {
+        edges[i] = int2(result.edge[i].first + verts_range.start(),
+                        result.edge[i].second + verts_range.start());
+      }
+
+      MutableSpan<int> face_offsets = all_face_offsets.slice(faces_range);
+      MutableSpan<int> corner_verts = all_corner_verts.slice(loops_range);
+      int i_face_corner = 0;
+      for (const int i_face : result.face.index_range()) {
+        face_offsets[i_face] = i_face_corner + loops_range.start();
+        for (const int i_corner : result.face[i_face].index_range()) {
+          corner_verts[i_face_corner] = result.face[i_face][i_corner] + verts_range.start();
+          i_face_corner++;
+        }
+      }
+    }
+  });
+
+  if (dst_group_id_attribute_id) {
+    SpanAttributeWriter<int> dst_group_id =
+        mesh->attributes_for_write().lookup_or_add_for_write_span<int>(*dst_group_id_attribute_id,
+                                                                       AttrDomain::Point);
+    threading::parallel_for(results.index_range(), 1024, [&](const IndexRange results_range) {
+      for (const int i_result : results_range) {
+        const IndexRange verts_range = vert_groups[i_result];
+        dst_group_id.span.slice(verts_range).fill(i_result);
+      }
+    });
+    dst_group_id.finish();
+  }
+
+  /* The delaunay triangulation doesn't seem to return all of the necessary all_edges, even in
+   * triangulation mode. */
+  bke::mesh_calc_edges(*mesh, true, false);
+  bke::mesh_smooth_set(*mesh, false);
+
+  mesh->tag_overlapping_none();
+
+  return mesh;
+}
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
@@ -482,13 +571,15 @@ static void node_geo_exec(GeoNodeExecParams params)
   const CDT_output_type output_type = get_cdt_output_type(mode);
 
   const AttributeFilter &attribute_filter = params.get_attribute_filter("Mesh");
+  std::optional<std::string> dst_group_id_attribute_id =
+      params.get_output_anonymous_attribute_id_if_needed("Group ID");
 
   geometry_set.modify_geometry_sets([&](GeometrySet &geometry_set) {
     Vector<CDTGeometrySetInput> cdt_inputs_by_group = cdt_inputs_from_groups(
         geometry_set, group_index, attribute_filter);
     Array<meshintersect::CDT_result<double>> results = calculate_cdts(cdt_inputs_by_group,
                                                                       output_type);
-    Mesh *mesh = geometry::cdts_to_mesh(results.as_span());
+    Mesh *mesh = cdts_to_mesh(results.as_span(), dst_group_id_attribute_id);
     geometry_set.replace_mesh(mesh);
     geometry_set.keep_only_during_modify({GeometryComponent::Type::Mesh});
   });
