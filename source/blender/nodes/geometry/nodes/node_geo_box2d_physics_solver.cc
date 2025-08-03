eaa16221e8d80425fb608d35508fe149a11355b0
Jacques Lucke <jacques@blender.org>
2025-08-03 06:40:09 +0200

    initial state management

--- source/blender/nodes/geometry/nodes/node_geo_box2d_physics_solver.cc
+++ source/blender/nodes/geometry/nodes/node_geo_box2d_physics_solver.cc
@@ -2,6 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "box2d/box2d.h"
+
 #include "node_geometry_util.hh"
 
 #include "NOD_geometry_nodes_physics_bundles.hh"
@@ -48,9 +50,89 @@ static const bNodeSocket *node_internally_linked_input(const bNodeTree & /*tree*
   return node.input_by_identifier(output_socket.identifier);
 }
 
+class Box2DState {
+ public:
+  bool is_initialized = false;
+  int update_counter = 0;
+};
+
+class Box2DStateOwner : public BundleItemInternalValueMixin {
+ public:
+  mutable Mutex mutex;
+  mutable Box2DState state;
+
+  void delete_self() override
+  {
+    MEM_delete(this);
+  }
+
+  StringRefNull type_name() const override
+  {
+    return TIP_("Box2D Physics State");
+  }
+};
+using Box2DStateOwnerPtr = ImplicitSharingPtr<Box2DStateOwner>;
+
 static void node_geo_exec(GeoNodeExecParams params)
 {
-  params.set_default_remaining_outputs();
+  BundlePtr old_state_bundle_ptr = params.extract_input<BundlePtr>("State");
+  BundlePtr world_bundle_ptr = params.extract_input<BundlePtr>("World");
+  const float delta_time = params.extract_input<float>("Delta Time");
+  const int sub_steps = params.extract_input<int>("Substeps");
+
+  if (!world_bundle_ptr) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+
+  int update_counter = 0;
+  if (old_state_bundle_ptr) {
+    update_counter = old_state_bundle_ptr->lookup<int>("_counter").value_or(0);
+  }
+
+  Box2DStateOwnerPtr box2d_state_owner;
+  if (old_state_bundle_ptr) {
+    box2d_state_owner = old_state_bundle_ptr->lookup<Box2DStateOwnerPtr>("_state").value_or(
+        nullptr);
+  }
+  if (!box2d_state_owner) {
+    box2d_state_owner = Box2DStateOwnerPtr{MEM_new<Box2DStateOwner>(__func__)};
+  }
+
+  if (!box2d_state_owner->mutex.try_lock()) {
+    params.error_message_add(NodeWarningType::Error,
+                             TIP_("Box2D physics state cannot be used by multiple nodes"));
+    params.set_default_remaining_outputs();
+    return;
+  }
+  BLI_SCOPED_DEFER([&]() { box2d_state_owner->mutex.unlock(); });
+
+  Box2DState &state = const_cast<Box2DState &>(box2d_state_owner->state);
+  if (!state.is_initialized) {
+    state.is_initialized = true;
+  }
+
+  const bool is_resimulating = update_counter < state.update_counter;
+  update_counter++;
+  if (!is_resimulating) {
+    state.update_counter = update_counter;
+  }
+
+  BundlePtr new_state_bundle_ptr = Bundle::create();
+  Bundle &new_state_bundle = const_cast<Bundle &>(*new_state_bundle_ptr);
+  new_state_bundle.add("_state", box2d_state_owner);
+  new_state_bundle.add("_counter", update_counter);
+
+  if (!world_bundle_ptr->is_mutable()) {
+    world_bundle_ptr = world_bundle_ptr->copy();
+  }
+  else {
+    world_bundle_ptr->tag_ensured_mutable();
+  }
+  Bundle &world_bundle = const_cast<Bundle &>(*world_bundle_ptr);
+
+  params.set_output("State", std::move(new_state_bundle_ptr));
+  params.set_output("World", std::move(world_bundle_ptr));
 }
 
 static void node_register()
