blender/blender

commit 4434a30d4042594ee2fbb95f4b9c46da38ca950a

Paint: Make UnifiedPaintSettings a per-mode instead of scene-wide value
Historically, the `UnifiedPaintSettings` struct has been used to
provide users the ability to set brush values at a scene level. Examples
of such attributes are the brush size, strength, and color, to name a
few.

Instead of these values being shared across all of the grease pencil,
mesh painting, and curves sculpting modes. This commit migrates the
data to the `Paint` struct, meaning that each individual mode (e.g.
Sculpt, Vertex Paint, Grease Pencil Draw) now has the ability to change
these values without affecting other modes.

While this change is large, the majority of the work is simply
refactoring access to the `UnifiedPaintSetting` struct. to ensure the
correct property is being retrieved.

Resolves #134077

Pull Request: https://projects.blender.org/blender/blender/pulls/139766

scripts/startup/bl_ui/properties_paint_common.py
@@ -271,7 +271,7 @@ class UnifiedPaintPanel:
         """ Generalized way of adding brush options to the UI,
             along with their pen pressure setting and global toggle, if they exist. """
         row = layout.row(align=True)
-        ups = context.tool_settings.unified_paint_settings
+        ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
         prop_owner = brush
         if unified_name and getattr(ups, unified_name):
             prop_owner = ups
@@ -289,13 +289,13 @@ class UnifiedPaintPanel:
 
     @staticmethod
     def prop_unified_color(parent, context, brush, prop_name, *, text=None):
-        ups = context.tool_settings.unified_paint_settings
+        ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
         prop_owner = ups if ups.use_unified_color else brush
         parent.prop(prop_owner, prop_name, text=text)
 
     @staticmethod
     def prop_unified_color_picker(parent, context, brush, prop_name, value_slider=True):
-        ups = context.tool_settings.unified_paint_settings
+        ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
         prop_owner = ups if ups.use_unified_color else brush
         parent.template_color_picker(prop_owner, prop_name, value_slider=value_slider)
 
@@ -832,7 +832,7 @@ def brush_settings(layout, context, brush, popover=False):
             layout.separator()
 
         if capabilities.has_color:
-            ups = context.scene.tool_settings.unified_paint_settings
+            ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
             row = layout.row(align=True)
             UnifiedPaintPanel.prop_unified_color(row, context, brush, "color", text="")
             UnifiedPaintPanel.prop_unified_color(row, context, brush, "secondary_color", text="")
@@ -1124,7 +1124,7 @@ def brush_shared_settings(layout, context, brush, popover=False):
         strength = True
 
     ### Draw settings. ###
-    ups = context.scene.tool_settings.unified_paint_settings
+    ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
 
     if blend_mode:
         layout.prop(brush, "blend", text="Blend")
@@ -1179,7 +1179,7 @@ def brush_shared_settings(layout, context, brush, popover=False):
 
 def color_jitter_panel(layout, context, brush):
     mode = UnifiedPaintPanel.get_brush_mode(context)
-    ups = context.scene.tool_settings.unified_paint_settings
+    ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
 
     is_sculpt_paint_mode = mode == 'SCULPT' and brush.sculpt_capabilities.has_color
     if mode in {'PAINT_TEXTURE', 'PAINT_2D', 'PAINT_VERTEX'} or is_sculpt_paint_mode:
@@ -1414,7 +1414,7 @@ def brush_settings_advanced(layout, context, settings, brush, popover=False):
 
 def draw_color_settings(context, layout, brush, color_type=False):
     """Draw color wheel and gradient settings."""
-    ups = context.scene.tool_settings.unified_paint_settings
+    ups = UnifiedPaintPanel.paint_settings(context).unified_paint_settings
 
     if color_type:
         row = layout.row()

FrenkyBojler/blender-ios

commit a8c01102676ec4b8a5b5b1395d8fe8c3f34914d8

Fix: Grease Pencil Vertex Paint uses incorrect path for `unified_paint_settings`
Introduced in 4434a30d4042594ee2fbb95f4b9c46da38ca950a

scripts/startup/bl_ui/properties_paint_common.py
@@ -1889,7 +1889,7 @@ def brush_basic_gpencil_weight_settings(layout, _context, brush, *, compact=Fals
 def brush_basic_gpencil_vertex_settings(layout, context, brush, *, compact=False):
     del compact  # UNUSED.
     gp_settings = brush.gpencil_settings
-    ups = context.tool_settings.unified_paint_settings
+    ups = context.tool_settings.gpencil_vertex_paint.unified_paint_settings
     brush_prop_owner = ups if ups.use_unified_size else brush
 
     # Brush details