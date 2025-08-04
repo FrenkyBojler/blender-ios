97255c13e37b9a5903e30e675b3f69884bb59010
YimingWu <xp8110@outlook.com>
2025-08-04 16:40:17 +0800

    Re-bind point program

--- source/blender/editors/transform/transform_mode_vert_slide.cc
+++ source/blender/editors/transform/transform_mode_vert_slide.cc
@@ -307,7 +307,7 @@ static void drawVertSlide(TransInfo *t)
       const uint shdr_pos = GPU_vertformat_attr_add(
           immVertexFormat(), "pos", blender::gpu::VertAttrType::SFLOAT_32_32_32);
 
-      immBindBuiltinProgram(GPU_SHADER_3D_POINT_UNIFORM_COLOR);
+      immBindBuiltinProgram(GPU_SHADER_3D_UNIFORM_COLOR);
       immUniformThemeColorShadeAlpha(TH_EDGE_SELECT, 80, alpha_shade);
 
       immBegin(GPU_PRIM_LINES, sld->sv.size() * 2);
@@ -334,7 +334,12 @@ static void drawVertSlide(TransInfo *t)
       }
       immEnd();
 
+      immUnbindProgram();
+
+      immBindBuiltinProgram(GPU_SHADER_3D_POINT_UNIFORM_COLOR);
+
       GPU_point_size(ctrl_size);
+      immUniformThemeColorShadeAlpha(TH_VERTEX_ACTIVE, 80, alpha_shade);
 
       immBegin(GPU_PRIM_POINTS, 1);
       immVertex3fv(shdr_pos, (slp->flipped && slp->use_even) ? co_dest_3d_act : co_orig_3d_act);
