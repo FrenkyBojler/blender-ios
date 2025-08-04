9746e11167e226d413ad4d728b042fa075fce3fa
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-08-03 17:27:43 -0700

    Grease Pencil: `Set Corner Type` Operator.

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -4634,6 +4634,127 @@ static void GREASE_PENCIL_OT_convert_curve_type(wmOperatorType *ot)
 
 /** \} */
 
+/* -------------------------------------------------------------------- */
+/** \name Set Corner Type Operator
+ * \{ */
+
+static const EnumPropertyItem prop_corner_types[] = {
+    {int(GP_STROKE_LINE_JOIN_TYPE_ROUND), "ROUND", 0, "Round", ""},
+    {int(GP_STROKE_LINE_JOIN_TYPE_BEVEL), "BEVEL", 0, "Bevel", ""},
+    {int(GP_STROKE_LINE_JOIN_TYPE_MITER), "MITER", 0, "Miter", ""},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
+static wmOperatorStatus grease_pencil_set_corner_type_exec(bContext *C, wmOperator *op)
+{
+  using bke::greasepencil::Layer;
+
+  const Scene *scene = CTX_data_scene(C);
+  Object *object = CTX_data_active_object(C);
+  GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object->data);
+
+  const int corner_type = RNA_enum_get(op->ptr, "corner_type");
+  const float miter_angle = RNA_float_get(op->ptr, "miter_angle");
+
+  bool changed = false;
+  const Vector<MutableDrawingInfo> drawings = retrieve_editable_drawings(*scene, grease_pencil);
+  threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
+    IndexMaskMemory memory;
+    const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
+        *object, info.drawing, info.layer_index, memory);
+    if (strokes.is_empty()) {
+      return;
+    }
+
+    bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
+    bke::MutableAttributeAccessor attributes = curves.attributes_for_write();
+
+    bke::SpanAttributeWriter<int> corner_types = attributes.lookup_or_add_for_write_span<int>(
+        "corner_type",
+        bke::AttrDomain::Curve,
+        bke::AttributeInitVArray(
+            VArray<int>::from_single(GP_STROKE_CAP_ROUND, curves.curves_num())));
+
+    index_mask::masked_fill(corner_types.span, corner_type, strokes);
+
+    if (corner_type == GP_STROKE_LINE_JOIN_TYPE_MITER) {
+      bke::SpanAttributeWriter<float> miter_angles =
+          attributes.lookup_or_add_for_write_span<float>(
+              "miter_angle",
+              bke::AttrDomain::Curve,
+              bke::AttributeInitVArray(
+                  VArray<float>::from_single(DEG2RADF(90.0f), curves.curves_num())));
+
+      index_mask::masked_fill(miter_angles.span, miter_angle, strokes);
+    }
+
+    changed = true;
+  });
+
+  if (changed) {
+    DEG_id_tag_update(&grease_pencil.id, ID_RECALC_GEOMETRY);
+    WM_event_add_notifier(C, NC_GEOM | ND_DATA, &grease_pencil);
+  }
+
+  return OPERATOR_FINISHED;
+}
+
+static void grease_pencil_set_corner_type_ui(bContext *C, wmOperator *op)
+{
+  uiLayout *layout = op->layout;
+  wmWindowManager *wm = CTX_wm_manager(C);
+
+  PointerRNA ptr = RNA_pointer_create_discrete(&wm->id, op->type->srna, op->properties);
+
+  layout->use_property_split_set(true);
+  layout->use_property_decorate_set(false);
+
+  layout->prop(&ptr, "corner_type", UI_ITEM_NONE, std::nullopt, ICON_NONE);
+
+  const int corner_type = RNA_enum_get(op->ptr, "corner_type");
+
+  if (corner_type != GP_STROKE_LINE_JOIN_TYPE_MITER) {
+    return;
+  }
+
+  layout->prop(&ptr, "miter_angle", UI_ITEM_NONE, std::nullopt, ICON_NONE);
+}
+
+static void GREASE_PENCIL_OT_set_corner_type(wmOperatorType *ot)
+{
+  /* Identifiers. */
+  ot->name = "Set Corner Type";
+  ot->idname = "GREASE_PENCIL_OT_set_corner_type";
+  ot->description = "Set the selected strokes corners";
+
+  /* Callbacks. */
+  ot->exec = grease_pencil_set_corner_type_exec;
+  ot->poll = editable_grease_pencil_poll;
+  ot->ui = grease_pencil_set_corner_type_ui;
+
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+
+  /* Properties */
+  ot->prop = RNA_def_enum(ot->srna,
+                          "corner_type",
+                          prop_corner_types,
+                          GP_STROKE_LINE_JOIN_TYPE_BEVEL,
+                          "Corner Type",
+                          "");
+  ot->prop = RNA_def_float_distance(ot->srna,
+                                    "miter_angle",
+                                    DEG2RADF(45.0f),
+                                    0.0f,
+                                    M_PI,
+                                    "Miter Cut Angle",
+                                    "All corners sharper than the Miter angle will be cut",
+                                    0.0f,
+                                    M_PI);
+  RNA_def_property_subtype(ot->prop, PROP_ANGLE);
+}
+
+/** \} */
+
 }  // namespace blender::ed::greasepencil
 
 void ED_operatortypes_grease_pencil_edit()
@@ -4677,6 +4798,7 @@ void ED_operatortypes_grease_pencil_edit()
   WM_operatortype_append(GREASE_PENCIL_OT_remove_fill_guides);
   WM_operatortype_append(GREASE_PENCIL_OT_outline);
   WM_operatortype_append(GREASE_PENCIL_OT_convert_curve_type);
+  WM_operatortype_append(GREASE_PENCIL_OT_set_corner_type);
 }
 
 /* -------------------------------------------------------------------- */
