2f65cf59caa603de08a76dce442253e609b01e7b
Clément Foucault <foucault.clem@gmail.com>
2025-08-04 11:14:23 +0200

    Fix #143336: Frustum culling is incorrect in Blender 4.5
    Attempt to fix the issue.
    
    Likely caused by a compiler issue with the global variable
    access.

--- source/blender/draw/intern/shaders/draw_intersect_lib.glsl
+++ source/blender/draw/intern/shaders/draw_intersect_lib.glsl
@@ -137,9 +137,9 @@ IsectFrustum isect_frustum_setup(Frustum shape)
 #ifdef DRW_VIEW_CULLING_INFO
 SHADER_LIBRARY_CREATE_INFO(draw_view_culling)
 
-ViewCullingData drw_view_culling()
+ViewCullingData drw_view_culling(uint view_id = drw_view_id)
 {
-  return drw_view_culling_buf[drw_view_id];
+  return drw_view_culling_buf[view_id];
 }
 
 bool intersect_view(Pyramid pyramid)
@@ -236,7 +236,7 @@ bool intersect_view(Box box)
   return intersects;
 }
 
-bool intersect_view(IsectBox i_box)
+bool intersect_view(IsectBox i_box, uint view_id = drw_view_id)
 {
   bool intersects = true;
 
@@ -244,7 +244,7 @@ bool intersect_view(IsectBox i_box)
   for (int p = 0; p < 6; ++p) {
     bool is_any_vertex_on_positive_side = false;
     for (int v = 0; v < 8; ++v) {
-      float test = dot(drw_view_culling().frustum_planes.planes[p],
+      float test = dot(drw_view_culling(view_id).frustum_planes.planes[p],
                        float4(i_box.corners[v], 1.0f));
       if (test > 0.0f) {
         is_any_vertex_on_positive_side = true;
@@ -266,7 +266,7 @@ bool intersect_view(IsectBox i_box)
     bool is_any_vertex_on_positive_side = false;
     for (int v = 0; v < 8; ++v) {
       float test = dot(i_box.planes[p],
-                       float4(drw_view_culling().frustum_corners.corners[v].xyz, 1.0f));
+                       float4(drw_view_culling(view_id).frustum_corners.corners[v].xyz, 1.0f));
       if (test > 0.0f) {
         is_any_vertex_on_positive_side = true;
         break;
@@ -282,12 +282,12 @@ bool intersect_view(IsectBox i_box)
   return intersects;
 }
 
-bool intersect_view(Sphere sphere)
+bool intersect_view(Sphere sphere, uint view_id = drw_view_id)
 {
   bool intersects = true;
 
   for (int p = 0; p < 6 && intersects; ++p) {
-    float dist_to_plane = dot(drw_view_culling().frustum_planes.planes[p],
+    float dist_to_plane = dot(drw_view_culling(view_id).frustum_planes.planes[p],
                               float4(sphere.center, 1.0f));
     if (dist_to_plane < -sphere.radius) {
       intersects = false;
