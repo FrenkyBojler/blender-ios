3eda3e6dad4c46043418a731ec2976b36817e9a1
Sybren A. Stüvel <sybren@blender.org>
2025-08-05 17:23:19 +0200

    Correct the use of file versions in the versioning code
    The code should use `BLENDER_FILE_SUBVERSION`, not
    `BLENDER_FILE_MIN_SUBVERSION`.

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -27,7 +27,7 @@
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 51
+#define BLENDER_FILE_SUBVERSION 52
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to
@@ -37,7 +37,7 @@
  * https://developer.blender.org/docs/handbook/guidelines/compatibility_handling_for_blend_files/
  * for details. */
 #define BLENDER_FILE_MIN_VERSION 405
-#define BLENDER_FILE_MIN_SUBVERSION 86
+#define BLENDER_FILE_MIN_SUBVERSION 85
 
 /** User readable version string. */
 const char *BKE_blender_version_string(void);
