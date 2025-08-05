8236a7540d39d5d9872e3a0e1669f51b6a76cabf
Sybren A. Stüvel <sybren@blender.org>
2025-08-05 17:26:59 +0200

    Fix formatting

--- source/blender/makesrna/intern/rna_fcurve.cc
+++ source/blender/makesrna/intern/rna_fcurve.cc
@@ -2555,7 +2555,8 @@ static void rna_def_fcurve(BlenderRNA *brna)
        "AUTO_YRGB",
        0,
        "Auto WXYZ to YRGB",
-       "Use WXYZ axis colors for quaternion/axis-angle rotations, XYZ axis colors for other transform and color properties, and auto-rainbow for the rest"},
+       "Use WXYZ axis colors for quaternion/axis-angle rotations, XYZ axis colors for other "
+       "transform and color properties, and auto-rainbow for the rest"},
       {FCURVE_COLOR_CUSTOM,
        "CUSTOM",
        0,
