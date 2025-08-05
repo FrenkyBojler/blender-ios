19c24100ae0622280c67dbeb4369bdfc732d5a20
Jacques Lucke <jacques@blender.org>
2025-08-05 05:48:36 +0200

    use normal node freeing function

--- source/blender/nodes/shader/node_shader_tree.cc
+++ source/blender/nodes/shader/node_shader_tree.cc
@@ -935,7 +935,7 @@ static void ntree_shader_pruned_unused(bNodeTree *ntree, bNode *output_node)
 
   LISTBASE_FOREACH_MUTABLE (bNode *, node, &ntree->nodes) {
     if (node->runtime->tmp_flag == 0) {
-      blender::bke::node_tree_free_local_node(*ntree, *node);
+      blender::bke::node_free_node(ntree, *node);
       changed = true;
     }
   }
