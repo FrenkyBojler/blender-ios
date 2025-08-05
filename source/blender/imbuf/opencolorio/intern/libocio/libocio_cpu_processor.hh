blender/blender

commit 7ceb4495c5a8e577441a57afed47febcfb4566ad

Refactor: OpenColorIO integration
Briefly about this change:
- OpenColorIO C-API is removed.
- The information about color spaces in ImBuf module is removed.
  It was stored in global ListBase in colormanagement.cc.
- Both OpenColorIO and fallback implementation supports GPU drawing.
- Fallback implementation supports white point, RGB curves, etc.
- Removed check for support of GPU drawing in IMB.

Historically it was implemented in a separate library with C-API, this
is because way back C++ code needed to stay in intern. This causes all
sort of overheads, and even calls that are strictly considered bad
level.

This change moves OpenColorIO integration into a module within imbuf,
next to movie, and next to IMB_colormanagement which is the main user
of it. This allows to avoid copy of color spaces, displays, views etc
in the ImBuf: they were used to help quickly querying information to
be shown on the interface. With this change it can be stored in the
same data structures as what is used by the OpenColorIO integration.
While it might not be fully avoiding duplication it is now less, and
there is no need in the user code to maintain the copies.

In a lot of cases this change also avoids allocations done per access
to the OpenColorIO. For example, it is not needed anymore to allocate
image descriptor in a heap.

The bigger user-visible change is that the fallback implementation now
supports GLSL drawing, with the whole list of supported features, such
as curve mapping and white point. This should help simplifying code
which relies on color space conversion on GPU: there is no need to
figure out fallback solution in such cases. The only case when drawing
will not work is when there is some actual bug, or driver issue, and
shader has failed to compile.

The change avoids having an opaque type for color space, and instead
uses forward declaration. It is a bit verbose on declaration, but helps
avoiding unsafe type-casts. There are ways to solve this in the future,

source/blender/imbuf/opencolorio/intern/libocio/libocio_cpu_processor.hh
@@ -1,0 +1,41 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#if defined(WITH_OPENCOLORIO)
+
+#  include "MEM_guardedalloc.h"
+
+#  include "OCIO_cpu_processor.hh"
+
+#  include "../opencolorio.hh"
+
+namespace blender::ocio {
+
+class LibOCIOCPUProcessor : public CPUProcessor {
+  OCIO_NAMESPACE::ConstCPUProcessorRcPtr ocio_cpu_processor_;
+
+ public:
+  explicit LibOCIOCPUProcessor(const OCIO_NAMESPACE::ConstCPUProcessorRcPtr &ocio_cpu_processor);
+
+  bool is_noop() const override
+  {
+    return false;
+  }
+
+  void apply_rgb(float rgb[3]) const override;
+  void apply_rgba(float rgba[4]) const override;
+
+  void apply_rgba_predivide(float rgba[4]) const override;
+
+  void apply(const PackedImage &image) const override;
+  void apply_predivide(const PackedImage &image) const override;
+
+  MEM_CXX_CLASS_ALLOC_FUNCS("LibOCIOCPUProcessor");
+};
+
+}  // namespace blender::ocio
+
+#endif

FrenkyBojler/blender-ios

commit 7ea97b9744c2308d012a261ebed12bb343593b23

Fix #143775: Cryptomatte: Precision issues with matte output
The issue has been introduced in 7ceb4495c5 by missing implementation
of the is_noop() callback for the CPU processor.

While it was not noticeable perceptually, an exact floating point value
might have changed due to un-premultiplication+premultiplication.

The fix needs to be ported to the Blender 4.5 LTS.

source/blender/imbuf/opencolorio/intern/libocio/libocio_cpu_processor.hh
@@ -22,7 +22,7 @@ class LibOCIOCPUProcessor : public CPUProcessor {
 
   bool is_noop() const override
   {
-    return false;
+    return ocio_cpu_processor_->isNoOp();
   }
 
   void apply_rgb(float rgb[3]) const override;