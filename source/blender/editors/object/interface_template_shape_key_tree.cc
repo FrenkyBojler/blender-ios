fdc0737bb47d0ba921bf167b74bfa5396b38f8b9
Pratik Borhade <pratikborhade302@gmail.com>
2025-08-07 16:44:07 +0530

    Fix format

--- source/blender/editors/object/interface_template_shape_key_tree.cc
+++ source/blender/editors/object/interface_template_shape_key_tree.cc
@@ -239,7 +239,7 @@ class ShapeKeyItem : public ui::AbstractTreeViewItem {
     WM_event_add_notifier(C, NC_OBJECT | ND_DRAW, nullptr);
     ED_undo_grouped_push(C, "Delete Shape Key");
   }
-  
+
   void build_context_menu(bContext &C, uiLayout &layout) const override
   {
     MenuType *mt = WM_menutype_find("MESH_MT_shape_key_tree_context_menu", true);
