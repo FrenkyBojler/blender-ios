4c8b02f67a53dcd65404a8bd4f229ad1dcbd3fb9
Miguel Pozo <pragma37@gmail.com>
2025-08-08 19:54:38 +0200

    Don't declare WindowClass twice

--- intern/ghost/intern/GHOST_SystemWin32.cc
+++ intern/ghost/intern/GHOST_SystemWin32.cc
@@ -633,7 +633,8 @@ GHOST_TSuccess GHOST_SystemWin32::init()
   /* Determine whether this system has a high frequency performance counter. */
   m_hasPerformanceCounter = ::QueryPerformanceFrequency((LARGE_INTEGER *)&m_freq) == TRUE;
 
-  if (success) {
+  static bool GHOST_WindowClass_registered = false;
+  if (success && !GHOST_WindowClass_registered) {
     WNDCLASSW wc = {0};
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = s_wndProc;
@@ -653,6 +654,9 @@ GHOST_TSuccess GHOST_SystemWin32::init()
     /* Use #RegisterClassEx for setting small icon. */
     if (::RegisterClassW(&wc) == 0) {
       success = GHOST_kFailure;
+    }
+    else {
+      GHOST_WindowClass_registered = true;
     }
   }
 
