b1c909b742e1a81b8fb871711fe1fa61820958fd
Miguel Pozo <pragma37@gmail.com>
2025-08-12 20:57:23 +0200

    Add int and boolean support to gpu materials

--- source/blender/gpu/intern/gpu_node_graph.cc
+++ source/blender/gpu/intern/gpu_node_graph.cc
@@ -217,7 +217,7 @@ static GPUNodeLink *gpu_uniformbuffer_link(GPUMaterial *mat,
     return nullptr;
   }
 
-  if (!ELEM(socket->type, SOCK_FLOAT, SOCK_VECTOR, SOCK_RGBA)) {
+  if (!ELEM(socket->type, SOCK_INT, SOCK_FLOAT, SOCK_VECTOR, SOCK_RGBA)) {
     return nullptr;
   }
 
