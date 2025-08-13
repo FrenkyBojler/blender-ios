e3dd9d71fa406028cca2439b14cca485115f4d46
Sean Kim <SeanCTKim@protonmail.com>
2025-08-12 20:52:31 -0700

    Use different max

--- source/blender/makesrna/intern/rna_sculpt_paint.cc
+++ source/blender/makesrna/intern/rna_sculpt_paint.cc
@@ -816,8 +816,8 @@ static void rna_def_unified_paint_settings(BlenderRNA *brna)
   prop = RNA_def_property(srna, "size", PROP_INT, PROP_PIXEL);
   RNA_def_property_int_funcs(prop, nullptr, "rna_UnifiedPaintSettings_size_set", nullptr);
   RNA_def_property_flag(prop, PROP_CONTEXT_UPDATE);
-  RNA_def_property_range(prop, 1, MAX_BRUSH_PIXEL_RADIUS * 10);
-  RNA_def_property_ui_range(prop, 1, MAX_BRUSH_PIXEL_RADIUS, 1, -1);
+  RNA_def_property_range(prop, 1, MAX_BRUSH_PIXEL_DIAMETER * 10);
+  RNA_def_property_ui_range(prop, 1, MAX_BRUSH_PIXEL_DIAMETER, 1, -1);
   RNA_def_property_ui_text(prop, "Size", "Diameter of the brush");
   RNA_def_property_update(prop, 0, "rna_UnifiedPaintSettings_radius_update");
 
