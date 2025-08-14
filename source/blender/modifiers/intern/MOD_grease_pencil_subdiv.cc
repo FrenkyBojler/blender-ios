e5920ee3ee4c2c48f1b77cd6536dd03588458933
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-08-14 10:07:18 -0700

    Fix: Reading and writing to the same span.

--- source/blender/modifiers/intern/MOD_grease_pencil_subdiv.cc
+++ source/blender/modifiers/intern/MOD_grease_pencil_subdiv.cc
@@ -95,25 +95,26 @@ static void subdivide_drawing(ModifierData &md, Object &ob, bke::greasepencil::D
           subdivided_curves, strokes, std::move(one_cut), {});
 
       offset_indices::OffsetIndices<int> points_by_curve = subdivided_curves.points_by_curve();
-      MutableSpan<float3> positions = subdivided_curves.positions_for_write();
+      const Array<float3> src_positions = subdivided_curves.positions();
+      MutableSpan<float3> dst_positions = subdivided_curves.positions_for_write();
       threading::parallel_for(subdivided_curves.curves_range(), 1024, [&](const IndexRange range) {
         for (const int curve_i : range) {
           const IndexRange points = points_by_curve[curve_i];
           for (const int point_i : points.drop_front(1).drop_back(1)) {
-            positions[point_i] = math::interpolate(
-                positions[point_i],
-                math::interpolate(positions[point_i - 1], positions[point_i + 1], 0.5f),
+            dst_positions[point_i] = math::interpolate(
+                src_positions[point_i],
+                math::interpolate(src_positions[point_i - 1], src_positions[point_i + 1], 0.5f),
                 0.5f);
           }
 
           if (cyclic[curve_i] && points.size() > 1) {
-            float3 &first_pos = positions[points.first()];
-            float3 &last_pos = positions[points.last()];
-            const float3 &after_first_pos = positions[points.first() + 1];
-            const float3 &before_last_pos = positions[points.last() - 1];
-            first_pos = math::interpolate(
+            const float3 &first_pos = src_positions[points.first()];
+            const float3 &last_pos = src_positions[points.last()];
+            const float3 &after_first_pos = src_positions[points.first() + 1];
+            const float3 &before_last_pos = src_positions[points.last() - 1];
+            dst_positions[points.first()] = math::interpolate(
                 first_pos, math::interpolate(last_pos, after_first_pos, 0.5f), 0.5f);
-            last_pos = math::interpolate(
+            dst_positions[points.last()] = math::interpolate(
                 last_pos, math::interpolate(before_last_pos, first_pos, 0.5f), 0.5f);
           }
         }
