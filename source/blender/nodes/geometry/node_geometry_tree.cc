b6ec8fcfab46d198bc3d681d742f6863d23aa88e
Omar Emara <mail@OmarEmara.dev>
2025-08-14 11:34:56 +0300

    Refactor: Nodes: Generalize node tree subtypes
    This patch generalizes node tree subtypes to be usable for node trees
    other than Geometry Nodes. In particular, this:
    
    - Renames SpaceNode.geometry_nodes_type to node_tree_sub_type, which now
      store a tree type-specific enum.
    - Renames SpaceNode.geometry_nodes_tool_tree to node_group_tree, which
      now stores any context-less tree of any type.

--- source/blender/nodes/geometry/node_geometry_tree.cc
+++ source/blender/nodes/geometry/node_geometry_tree.cc
@@ -34,8 +34,8 @@ static void geometry_node_tree_get_from_context(const bContext *C,
                                                 ID **r_from)
 {
   const SpaceNode *snode = CTX_wm_space_node(C);
-  if (snode->geometry_nodes_type == SNODE_GEOMETRY_TOOL) {
-    *r_ntree = snode->geometry_nodes_tool_tree;
+  if (snode->node_tree_sub_type == SNODE_GEOMETRY_TOOL) {
+    *r_ntree = snode->node_group_tree;
     return;
   }
 
