36df816cb2bd4e9f36255f44f88768d48adeb057
Richard Antalik <richardantalik@gmail.com>
2025-08-14 04:05:18 +0200

    Address review

--- source/blender/sequencer/intern/render.cc
+++ source/blender/sequencer/intern/render.cc
@@ -2047,12 +2047,8 @@ ImBuf *render_give_ibuf_direct(const RenderData *context, float timeline_frame, 
 {
   SeqRenderState state;
 
-  ImBuf *cached_buf = source_image_cache_get(context, strip, timeline_frame);
-  if (cached_buf != nullptr) {
-    return cached_buf;
-  }
-
-  intra_frame_cache_invalidate(context->scene);
+  intra_frame_cache_set_cur_frame(
+      context->scene, timeline_frame, context->view_id, context->rectx, context->recty);
   ImBuf *ibuf = seq_render_strip(context, &state, strip, timeline_frame);
   return ibuf;
 }
