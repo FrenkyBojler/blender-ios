e1ae5ee61d41415877162d17e1a08f18243bd375
Thomas Hope <active.desk1410@fastmail.com>
2025-08-15 09:56:36 +0100

    apply make format

--- source/blender/editors/io/io_obj.cc
+++ source/blender/editors/io/io_obj.cc
@@ -309,8 +309,11 @@ void WM_OT_obj_export(wmOperatorType *ot)
   /* File Writer options. */
   RNA_def_boolean(
       ot->srna, "apply_modifiers", true, "Apply Modifiers", "Apply modifiers to exported meshes");
-  RNA_def_boolean(
-      ot->srna, "apply_transform", true, "Apply Transform", "Apply object transforms to exported vertices");
+  RNA_def_boolean(ot->srna,
+                  "apply_transform",
+                  true,
+                  "Apply Transform",
+                  "Apply object transforms to exported vertices");
   RNA_def_enum(ot->srna,
                "export_eval_mode",
                io_obj_export_evaluation_mode,
