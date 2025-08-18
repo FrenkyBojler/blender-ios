5c82cd1b7856fb90362783673541475ba563ef3b
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-08-17 17:42:42 -0700

    Cleanup: Move `ClosestElement` finding to `curves_pen.cc`

--- source/blender/editors/include/ED_curves.hh
+++ source/blender/editors/include/ED_curves.hh
@@ -190,6 +190,23 @@ class PenToolOperation {
                               const IndexRange points,
                               bool clear_selection);
 };
+
+void pen_find_closest_point_or_handle(const PenToolOperation &ptd,
+                                      const bke::CurvesGeometry &curves,
+                                      const IndexMask &editable_points,
+                                      const IndexMask &bezier_points,
+                                      const float4x4 &layer_to_object,
+                                      int drawing_index,
+                                      const float2 &mouse_co,
+                                      ClosestElement &r_closest_element);
+void pen_find_closest_edge_point(const PenToolOperation &ptd,
+                                 const bke::CurvesGeometry &curves,
+                                 const IndexMask &editable_curves,
+                                 const float4x4 &layer_to_object,
+                                 int drawing_index,
+                                 const float2 &mouse_co,
+                                 ClosestElement &r_closest_element);
+
 void pen_tool_common_props(wmOperatorType *ot);
 
 }  // namespace pen_tool
