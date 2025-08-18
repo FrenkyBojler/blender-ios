31454c02c246cceb0deeb490198591564f8292bd
Christoph Neuhauser <christoph.neuhauser@intel.com>
2025-08-18 18:29:34 +0200

    WM: Fix OS_WINDOWS include guard in wm_jobs.cc

--- source/blender/windowmanager/intern/wm_jobs.cc
+++ source/blender/windowmanager/intern/wm_jobs.cc
@@ -21,7 +21,7 @@
 #include "BLI_time.h"
 #include "BLI_utildefines.h"
 
-#ifdef OS_WINDOWS
+#if OS_WINDOWS
 #  include "BLI_winstuff.h"
 #endif
 
@@ -163,7 +163,7 @@ static void wm_job_main_thread_yield(wmJob *wm_job)
 static void wm_jobs_update_qos(const wmWindowManager *wm)
 {
   /* A QoS API is currently only available for Windows. */
-#ifdef OS_WINDOWS
+#if OS_WINDOWS
   LISTBASE_FOREACH (wmJob *, wm_job, &wm->jobs) {
     if (wm_job->flag & WM_JOB_PRIORITY) {
       BLI_windows_process_set_qos(QoSMode::HIGH, QoSPrecedence::JOB);
