9d5da7155123bd18553f571d32fca85aaf394628
Benjamin Beilharz <b@ben.graphics>
2025-08-19 16:56:58 +0200

    Vectorized forms

--- source/blender/gpu/shaders/common/gpu_shader_ewa_sampler_lib.glsl
+++ source/blender/gpu/shaders/common/gpu_shader_ewa_sampler_lib.glsl
@@ -44,14 +44,15 @@ void clamp_anisotropy(inout float2 x_gradient_texel_space,
 }
 
 /* Normalized ellipse describing the footprint in texel space:
- * r^2(Δu,Δv) = A * Δu^2 + B * ΔuΔv + C * Δv^2   (inside if r^2 < 1)
+ * r^2(dx,dy) = A * dx^2 + B * dxdy + C * dy^2   (inside if r^2 < 1)
  * s_lower_bound..s_upper_bound, t_lower_bound..t_upper_bound: integer
  * bounding box to scan (inclusive)
  * texel_center_x, texel_center_y: center of the footprint in texel coordinates */
 struct Ellipse {
   float a, b, c;
-  float u_coord_center, v_coord_center;
-  int s_lower_bound, s_upper_bound, t_lower_bound, t_upper_bound;
+  float2 uv_coord_center;
+  int2 st_lower_bound;
+  int2 st_upper_bound;
   bool valid;
 };
 
@@ -83,9 +84,9 @@ Ellipse build_ellipse(float2 uv_texel_space,
    * center lies just outside the true ellipse, as long as its 1x1 area
    * overlaps the ellipse. If we only test the true ellipse we will miss those border
    * texels, causing artefacts. */
-  float a = dv_dx * dv_dx + dv_dy * dv_dy + 1.0;
+  float a = square(dv_dx) + square(dv_dy) + 1.0;
   float b = -2.0 * (du_dx * dv_dx + du_dy * dv_dy);
-  float c = du_dx * du_dx + du_dy * du_dy + 1.0;
+  float c = square(du_dx) + square(du_dy) + 1.0;
 
   /* Computes det(M) = AC - B^2 * 0.25. We require that the determinant is
    * positive define so r^2 =1 is a bounded ellipse.
@@ -93,7 +94,7 @@ Ellipse build_ellipse(float2 uv_texel_space,
    * (singluar Jacobian/extreme anisotropy).
    * https://en.wikipedia.org/wiki/Matrix_representation_of_conic_sections#Classification */
   float determinant = a * c - 0.25 * b * b;
-  if (determinant < 0.0) {
+  if (determinant <= 0.0) {
     Ellipse e;
     e.valid = false;
     return e;
@@ -104,59 +105,41 @@ Ellipse build_ellipse(float2 uv_texel_space,
   b *= inv;
   c *= inv;
 
-  float u_coord_center = uv_texel_space.x - 0.5;
-  float v_coord_center = uv_texel_space.y - 0.5;
+  float2 uv_coord_center = uv_texel_space - 0.5f;
 
-  /* This is a check for numerical stability computing 4 * det(M).
-   * If the sqrt has slightly negative values due to floating point errors,
-   * sqrt would produce NaN.
-   * https://en.wikipedia.org/wiki/Matrix_representation_of_conic_sections#Classification */
+
   float conic_discriminant = -b * b + 4.0 * a * c;
-  if (conic_discriminant < 0.0) {
-    Ellipse e;
-    e.valid = false;
-    return e;
-  }
-
   float inv_conic_discriminant = 1.0 / conic_discriminant;
-  float u_extent = 2.0 * inv_conic_discriminant * sqrt(conic_discriminant * c);
-  float v_extent = 2.0 * inv_conic_discriminant * sqrt(a * conic_discriminant);
-
-  int s_lower_bound = int(ceil(u_coord_center - u_extent));
-  int s_upper_bound = int(floor(u_coord_center + u_extent));
-  int t_lower_bound = int(ceil(v_coord_center - v_extent));
-  int t_upper_bound = int(floor(v_coord_center + v_extent));
+  float2 uv_extent = 2.0f * inv_conic_discriminant * sqrt(float2(conic_discriminant * c,
+                                                                 conic_discriminant * a));
+  int2 st_lower_bound = int2(ceil(uv_coord_center - uv_extent));
+  int2 st_upper_bound = int2(floor(uv_coord_center + uv_extent));
 
   Ellipse e;
   e.a = a;
   e.b = b;
   e.c = c;
-  e.u_coord_center = u_coord_center;
-  e.v_coord_center = v_coord_center;
-  e.s_lower_bound = s_lower_bound;
-  e.s_upper_bound = s_upper_bound;
-  e.t_lower_bound = t_lower_bound;
-  e.t_upper_bound = t_upper_bound;
-  e.valid = (s_lower_bound <= s_upper_bound && t_lower_bound <= t_upper_bound);
+  e.uv_coord_center = uv_coord_center;
+  e.st_lower_bound = st_lower_bound;
+  e.st_upper_bound = st_upper_bound;
+  e.valid = all(lessThanEqual(st_lower_bound, st_upper_bound));
   return e;
 }
 
 /* Computes the elliptical weighted average for a given input texture with their
- * uv coordiantes and the Jacobian (x_gradient, y_gradient). The function builds
- * an ellipse and its bounding box which is used to computed weighted sums for each
- * tapped texel within the ellipsoid. */
+ * normalized uv coordinates and the normalized Jacobian (x_gradient, y_gradient). */
 float4 texture_ewa(sampler2D input_tx, float2 coordinates, float2 x_gradient, float2 y_gradient)
 {
   /* TODO(Ben): After feedback, check in with users if they need these as accessible
    * paramemters. */
-  constexpr float alpha = 2.0f;
+  constexpr float smoothness = 2.0f;
   constexpr float max_ratio_between_axes = 8.0f;
 
   /* Bring derivatives to texel space. */
   float2 size = float2(textureSize(input_tx, 0));
 
-  float2 uv_texel_space = coordinates * size;
   /* Scale the gradients back into texel space. */
+  float2 uv_texel_space = coordinates * size;
   float2 x_gradient_texel_space = x_gradient * size;
   float2 y_gradient_texel_space = y_gradient * size;
 
@@ -172,22 +155,22 @@ float4 texture_ewa(sampler2D input_tx, float2 coordinates, float2 x_gradient, fl
   float4 accum_rgba = float4(0.0f);
   float accum_weight = 0.0f;
 
-  for (int v = ellipse.t_lower_bound; v <= ellipse.t_upper_bound; ++v) {
-    float dv = float(v) - ellipse.v_coord_center;
-    float c_dv = ellipse.c * dv * dv;
-    float b_dv = ellipse.b * dv;
+  for (int y = ellipse.st_lower_bound.y; y <= ellipse.st_upper_bound.y; ++y) {
+    float dy = float(y) - ellipse.uv_coord_center.y;
+    float c_dy = ellipse.c * square(dy);
+    float b_dy = ellipse.b * dy;
 
-    for (int u = ellipse.s_lower_bound; u <= ellipse.s_upper_bound; ++u) {
-      float du = float(u) - ellipse.u_coord_center;
+    for (int x = ellipse.st_lower_bound.x; x <= ellipse.st_upper_bound.x; ++x) {
+      float dx = float(x) - ellipse.uv_coord_center.x;
       /* Based on the matrix representation of conic sections:
        * https://en.wikipedia.org/wiki/Matrix_representation_of_conic_sections
-       * Evaluates r^2 = A*Δu^2 + B*Δu*Δv + C*Δv^2 */
-      float r2 = ellipse.a * (du * du) + b_dv * du + c_dv;
+       * Evaluates r^2 = A*dx^2 + B*dx*dy + C*dy^2 */
+      float r2 = ellipse.a * square(dx) + b_dy * dx + c_dy;
 
       if (r2 < 1.0f) {
-        float weight = exp(-alpha * r2);
+        float weight = exp(-smoothness * r2);
         if (weight > 0.0f) {
-          float4 rgba = texelFetch(input_tx, int2(u, v), 0);
+          float4 rgba = texelFetch(input_tx, int2(x, y), 0);
           accum_rgba += weight * rgba;
           accum_weight += weight;
         }
