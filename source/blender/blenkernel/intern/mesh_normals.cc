3c747bbbe4ad3af5b73a86fe4e19ecc18894105a
Hans Goudey <hans@blender.org>
2025-08-20 10:48:22 -0400

    Fix: Grain size calculation for setting custom normals
    Recent change 3a6a663a159fb54a913554a9fc6c8e has some logic issues here.
    First, the grain size was clamped to be never lower than 256, which
    doesn't make sense since the size of the array is never greater than
    the number of threads. The numbers used in the division were also large
    enough that multithreading wouldn't be used in many realistic cases
    anyway. There's no need to cap the grain size at 1024, and also
    `safe_divide` doesn't make sense in this scenario.
    
    Now the grain size can be 1, and will be smaller in general. And we just
    use `std::max` to ensure it's not zero, rather than doing clamping.

--- source/blender/blenkernel/intern/mesh_normals.cc
+++ source/blender/blenkernel/intern/mesh_normals.cc
@@ -1352,9 +1352,9 @@ void normals_calc_corners(const Span<float3> vert_positions,
     r_fan_spaces->corners_by_space.reinitialize(space_offsets.total_size());
   }
 
-  const int64_t mean_size = space_offsets.total_size() / space_offsets.size();
-  const int64_t grain_size = math::clamp<int64_t>(
-      math::safe_divide<int64_t>(1024 * 512, mean_size), 256, 1024 * 16);
+  const int64_t mean_size = std::max<int64_t>(1,
+                                              space_offsets.total_size() / space_offsets.size());
+  const int64_t grain_size = std::max<int64_t>(1, 1024 * 16 / mean_size);
   threading::parallel_for(all_space_groups.index_range(), grain_size, [&](const IndexRange range) {
     for (const int thread_i : range) {
       Vector<CornerSpaceGroup, 0> &local_space_groups = all_space_groups[thread_i];
