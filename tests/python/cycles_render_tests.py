3efb998f39a85a1bbe55c7c13de4a076bfacfd88
Clément Foucault <foucault.clem@gmail.com>
2025-08-20 17:52:20 +0200

    Disable cycles test for faster iteration

--- tests/python/cycles_render_tests.py
+++ tests/python/cycles_render_tests.py
@@ -22,6 +22,7 @@ BLOCKLIST_ALL = [
     "visibility_particles.blend",
     # Temporarily blocked for 4.4 lib upgrade, due to PNG alpha minor difference.
     "image_log_osl.blend",
+    "*",
 ]
 
 # Blocklist for device + build configuration that does not support OSL at all.
