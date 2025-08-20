e6fd4b65c1fcd7e2995445d950a0e5f3cfe97d78
Clément Foucault <foucault.clem@gmail.com>
2025-08-20 16:12:30 +0200

    Fix draw tests on metal

--- source/blender/draw/intern/shaders/draw_curves_test.glsl
+++ source/blender/draw/intern/shaders/draw_curves_test.glsl
@@ -18,5 +18,7 @@ void main()
   result_indices_buf[gl_VertexID].y = pt.curve_id;
   result_indices_buf[gl_VertexID].z = pt.curve_segment;
   result_indices_buf[gl_VertexID].w = int(pt.azimuthal_offset);
+
+  gl_Position = float4(0);
 #endif
 }
