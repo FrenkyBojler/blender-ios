aebeacd9a43b9814f9c5594052204ca5691934cc
Clément Foucault <foucault.clem@gmail.com>
2025-08-20 12:19:36 +0200

    Fix compilation issue with mesh volume

--- source/blender/draw/engines/eevee/eevee_shader.cc
+++ source/blender/draw/engines/eevee/eevee_shader.cc
@@ -870,30 +870,34 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
     info.vertex_out_interfaces_.clear();
   }
 
-  const char *domain_type = "";
+  const char *domain_type_frag = "";
+  const char *domain_type_vert = "";
   switch (geometry_type) {
     case MAT_GEOM_MESH:
-      domain_type = (pipeline_type == MAT_PIPE_VOLUME_MATERIAL) ? "VolumePoint" : "MeshVertex";
+      domain_type_frag = (pipeline_type == MAT_PIPE_VOLUME_MATERIAL) ? "VolumePoint" :
+                                                                       "MeshVertex";
+      domain_type_vert = "MeshVertex";
       break;
     case MAT_GEOM_POINTCLOUD:
-      domain_type = "PointCloudPoint";
+      domain_type_frag = domain_type_vert = "PointCloudPoint";
       break;
     case MAT_GEOM_CURVES:
-      domain_type = "CurvesPoint";
+      domain_type_frag = domain_type_vert = "CurvesPoint";
       break;
     case MAT_GEOM_WORLD:
-      domain_type = (pipeline_type == MAT_PIPE_VOLUME_MATERIAL) ? "VolumePoint" : "WorldPoint";
+      domain_type_frag = (pipeline_type == MAT_PIPE_VOLUME_MATERIAL) ? "VolumePoint" :
+                                                                       "WorldPoint";
+      domain_type_vert = "WorldPoint";
       break;
     case MAT_GEOM_GPENCIL:
-      domain_type = "GPencilPoint";
+      domain_type_frag = domain_type_vert = "GPencilPoint";
       break;
     case MAT_GEOM_VOLUME:
-      domain_type = "VolumePoint";
+      domain_type_frag = domain_type_vert = "VolumePoint";
       break;
   }
 
   std::stringstream attr_load;
-  attr_load << "void attrib_load(" << domain_type << " domain)\n";
   attr_load << "{\n";
   attr_load << (!codegen.attr_load.empty() ? codegen.attr_load : "");
   attr_load << "}\n\n";
@@ -901,12 +905,14 @@ void ShaderModule::material_create_info_amend(GPUMaterial *gpumat, GPUCodegenOut
   std::stringstream vert_gen, frag_gen;
 
   if (do_vertex_attrib_load) {
-    vert_gen << global_vars.str() << attr_load.str();
-    frag_gen << "void attrib_load(" << domain_type << " domain) {}\n"; /* Placeholder. */
+    vert_gen << global_vars.str() << "void attrib_load(" << domain_type_vert << " domain)"
+             << attr_load.str();
+    frag_gen << "void attrib_load(" << domain_type_frag << " domain) {}\n"; /* Placeholder. */
   }
   else {
-    vert_gen << "void attrib_load(" << domain_type << " domain) {}\n"; /* Placeholder. */
-    frag_gen << global_vars.str() << attr_load.str();
+    vert_gen << "void attrib_load(" << domain_type_vert << " domain) {}\n"; /* Placeholder. */
+    frag_gen << global_vars.str() << "void attrib_load(" << domain_type_frag << " domain)"
+             << attr_load.str();
   }
 
   /* TODO(fclem): This should become part of the dependency system. */
