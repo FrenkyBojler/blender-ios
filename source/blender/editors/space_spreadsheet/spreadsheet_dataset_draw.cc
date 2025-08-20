4e01e5d4c08d70b0e6a0c8b827e2a2dd58604d48
Hans Goudey <hans@blender.org>
2025-08-20 15:48:50 -0400

    Don't show fields in the tree

--- source/blender/editors/space_spreadsheet/spreadsheet_dataset_draw.cc
+++ source/blender/editors/space_spreadsheet/spreadsheet_dataset_draw.cc
@@ -1130,9 +1130,12 @@ class ViewerDataTreeView : public ui::AbstractTreeView {
   {
     const nodes::geo_eval_log::ViewerNodeLog *log = viewer_node_log_lookup(sspreadsheet_);
     for (const nodes::geo_eval_log::ViewerNodeLog::Item &item : log->items) {
+      const bke::SocketValueVariant &value = item.value;
+      if (!value.is_single()) {
+        continue;
+      }
       auto &child_item = this->add_tree_item<ViewerNodeItem>(item);
       child_item.uncollapse_by_default();
-      const bke::SocketValueVariant &value = item.value;
       this->build_value(child_item, value);
     }
   }
