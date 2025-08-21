a97570b1e68dcb935e598707c03e6e8168bb9714
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-08-21 12:04:54 +0200

    clenaup remove docstring reference to old enum

--- source/blender/blenkernel/BKE_action.hh
+++ source/blender/blenkernel/BKE_action.hh
@@ -310,8 +310,7 @@ void BKE_pose_itasc_init(bItasc *itasc);
 bool BKE_pose_channel_in_IK_chain(Object *ob, bPoseChannel *pchan);
 
 /**
- * Get the effective gizmo location, accounting for PCHAN_GIZMO_MODE_CUSTOM_LOCATION
- * and PCHAN_GIZMO_MODE_LOCAL_SPACE
+ * Get the effective gizmo location, accounting for POSE_GIZMO_AT_CUSTOM_TX.
  */
 void BKE_pose_channel_gizmo_location(const bArmature *arm,
                                      const bPoseChannel *pose_bone,
@@ -319,7 +318,7 @@ void BKE_pose_channel_gizmo_location(const bArmature *arm,
 
 /**
  * Get the effective gizmo pose orientation, accounting for
- * PCHAN_GIZMO_MODE_LOCAL_SPACE.
+ * POSE_GIZMO_AT_CUSTOM_TX.
  */
 void BKE_pose_channel_gizmo_orientation(const bArmature *arm,
                                         const bPoseChannel *pose_bone,
@@ -327,7 +326,7 @@ void BKE_pose_channel_gizmo_orientation(const bArmature *arm,
 
 /**
  * Get the effective BoneParentTransform to use for gizmos, accounting for
- * PCHAN_GIZMO_MODE_LOCAL_SPACE.
+ * POSE_GIZMO_AT_CUSTOM_TX.
  */
 void BKE_pose_channel_gizmo_parent_transform(const bArmature *arm,
                                              const bPoseChannel *pose_bone,
