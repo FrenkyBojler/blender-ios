5729e2e9a3b67358ab1617fbd6e87583e5fb4122
Miguel Pozo <pragma37@gmail.com>
2025-08-21 19:51:41 +0200

    Fix compilation

--- source/blender/gpu/intern/gpu_shader.cc
+++ source/blender/gpu/intern/gpu_shader.cc
@@ -1034,7 +1034,7 @@ bool ShaderCompiler::is_compiling_impl()
   /* The mutex should be locked befor calling this function. */
   BLI_assert(!mutex_.try_lock());
 
-  if (!compilation_queue_.is_empty()) {
+  if (!compilation_worker_->is_empty()) {
     return true;
   }
 
