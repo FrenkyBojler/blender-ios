f9c0d8aefb64f8acc4c04b619c0756540bf5384b
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-08-21 14:48:30 +0200

    remove one usage of the panel I missed

--- scripts/startup/bl_ui/space_sequencer.py
+++ scripts/startup/bl_ui/space_sequencer.py
@@ -189,11 +189,6 @@ class SEQUENCER_HT_header(Header):
         row.prop(tool_settings, "use_snap_sequencer", text="")
         sub = row.row(align=True)
         sub.popover(panel="SEQUENCER_PT_snapping")
-        if st.view_type in {'SEQUENCER', 'SEQUENCER_PREVIEW'}:
-            row = layout.row(align=True)
-            row.prop(tool_settings, "use_snap_playhead", text="")
-            sub = row.row(align=True)
-            sub.popover(panel="SEQUENCER_PT_playhead_snapping", text="")
         layout.separator_spacer()
 
         if st.view_type in {'PREVIEW', 'SEQUENCER_PREVIEW'}:
