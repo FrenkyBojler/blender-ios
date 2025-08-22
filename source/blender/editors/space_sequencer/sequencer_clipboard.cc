8043332263090fa21404ef81cae6a0a53f1f6b9c
Ramon Klauck <ramonklauck3@gmail.com>
2025-08-22 17:43:11 +0200

    only place at mouse position when keep_offset is false

--- source/blender/editors/space_sequencer/sequencer_clipboard.cc
+++ source/blender/editors/space_sequencer/sequencer_clipboard.cc
@@ -536,7 +536,7 @@ wmOperatorStatus sequencer_clipboard_paste_exec(bContext *C, wmOperator *op)
     if (seq::transform_test_overlap(scene_dst, ed_dst->seqbasep, istrip)) {
       seq::transform_seqbase_shuffle(ed_dst->seqbasep, istrip, scene_dst);
     }
-    if (region->regiontype == RGN_TYPE_PREVIEW) {
+    if (region->regiontype == RGN_TYPE_PREVIEW && !(RNA_boolean_get(op->ptr, "keep_offset"))) {
       StripTransform *transform = istrip->data->transform;
       const float2 mirror = seq::image_transform_mirror_factor_get(istrip);
       const float2 origin = seq::image_transform_origin_offset_pixelspace_get(scene, istrip);
