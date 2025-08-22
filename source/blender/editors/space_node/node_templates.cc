blender/blender

commit 87c011f8bb0ac47280d866a644f12341f428133d

Nodes: minify value input nodes
This removes redundant labels from various input nodes like the Value, Integer
and Object node.

Design wise, this is mostly straight forward except for two aspects:
* Some input nodes some have a gizmo icon. In this case I just added the gizmo
  icon on the same row.
* The checkbox in the Boolean input node should probably still have a label, so
  I kept that.

Implementation wise this adds a new function to socket declarations that allows
us to override the draw behavior of individual sockets per node.

Pull Request: https://projects.blender.org/blender/blender/pulls/139432

source/blender/nodes/composite/nodes/node_composite_rgb.cc
@@ -12,6 +12,9 @@
 
 #include "COM_node_operation.hh"
 
+#include "UI_interface.hh"
+#include "UI_resources.hh"
+
 #include "node_composite_util.hh"
 
 /* **************** RGB ******************** */
@@ -20,7 +23,15 @@ namespace blender::nodes::node_composite_rgb_cc {
 
 static void cmp_node_rgb_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Color>("RGBA").default_value({0.5f, 0.5f, 0.5f, 1.0f});
+  b.add_output<decl::Color>("RGBA")
+      .default_value({0.5f, 0.5f, 0.5f, 1.0f})
+      .custom_draw([](CustomSocketDrawParams &params) {
+        uiLayoutSetAlignment(&params.layout, UI_LAYOUT_ALIGN_EXPAND);
+        uiLayout &col = params.layout.column(true);
+        uiTemplateColorPicker(
+            &col, &params.socket_ptr, "default_value", true, false, false, false);
+        col.prop(&params.socket_ptr, "default_value", UI_ITEM_R_SLIDER, "", ICON_NONE);
+      });
 }
 
 using namespace blender::compositor;

source/blender/nodes/function/nodes/node_fn_input_bool.cc
@@ -4,6 +4,8 @@
 
 #include "node_function_util.hh"
 
+#include "NOD_geometry_nodes_gizmos.hh"
+
 #include "UI_interface.hh"
 #include "UI_resources.hh"
 
@@ -11,13 +13,13 @@ namespace blender::nodes::node_fn_input_bool_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Bool>("Boolean");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  uiLayout *col = &layout->column(true);
-  col->prop(ptr, "boolean", UI_ITEM_R_EXPAND, IFACE_("Value"), ICON_NONE);
+  b.add_output<decl::Bool>("Boolean").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayout &row = params.layout.row(true);
+    row.prop(&params.node_ptr, "boolean", UI_ITEM_NONE, "Boolean", ICON_NONE);
+    if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
+      row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
+    }
+  });
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -46,7 +48,6 @@ static void node_register()
   blender::bke::node_type_storage(
       ntype, "NodeInputBool", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/function/nodes/node_fn_input_int.cc
@@ -7,17 +7,20 @@
 #include "UI_interface.hh"
 #include "UI_resources.hh"
 
+#include "NOD_geometry_nodes_gizmos.hh"
+
 namespace blender::nodes::node_fn_input_int_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Int>("Integer");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  uiLayout *col = &layout->column(true);
-  col->prop(ptr, "integer", UI_ITEM_R_EXPAND, "", ICON_NONE);
+  b.add_output<decl::Int>("Integer").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayout &row = params.layout.row(true);
+    row.prop(&params.node_ptr, "integer", UI_ITEM_NONE, "", ICON_NONE);
+    if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
+      row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
+    }
+  });
+  ;
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -46,7 +49,6 @@ static void node_register()
   blender::bke::node_type_storage(
       ntype, "NodeInputInt", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/function/nodes/node_fn_input_rotation.cc
@@ -4,7 +4,7 @@
 
 #include "BLI_math_euler.hh"
 
-#include "NOD_socket_search_link.hh"
+#include "NOD_geometry_nodes_gizmos.hh"
 
 #include "UI_interface.hh"
 #include "UI_resources.hh"
@@ -15,13 +15,13 @@ namespace blender::nodes::node_fn_input_rotation_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Rotation>("Rotation");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  uiLayout *col = &layout->column(true);
-  col->prop(ptr, "rotation_euler", UI_ITEM_R_EXPAND, "", ICON_NONE);
+  b.add_output<decl::Rotation>("Rotation").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayout &row = params.layout.row(true);
+    row.column(true).prop(&params.node_ptr, "rotation_euler", UI_ITEM_NONE, "", ICON_NONE);
+    if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
+      row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
+    }
+  });
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -54,7 +54,6 @@ static void node_register()
   blender::bke::node_type_storage(
       ntype, "NodeInputRotation", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/function/nodes/node_fn_input_string.cc
@@ -16,13 +16,12 @@ namespace blender::nodes::node_fn_input_string_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.is_function_node();
-  b.add_output<decl::String>("String");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  PropertyRNA *prop = RNA_struct_find_property(ptr, "string");
-  layout->prop(ptr, prop, -1, 0, UI_ITEM_NONE, "", ICON_NONE, IFACE_("String"));
+  b.add_output<decl::String>("String").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayoutSetAlignment(&params.layout, UI_LAYOUT_ALIGN_EXPAND);
+    PropertyRNA *prop = RNA_struct_find_property(&params.node_ptr, "string");
+    params.layout.prop(
+        &params.node_ptr, prop, -1, 0, UI_ITEM_NONE, "", ICON_NONE, IFACE_("String"));
+  });
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -86,7 +85,6 @@ static void node_register()
   ntype.initfunc = node_init;
   blender::bke::node_type_storage(ntype, "NodeInputString", node_storage_free, node_storage_copy);
   ntype.build_multi_function = node_build_multi_function;
-  ntype.draw_buttons = node_layout;
   ntype.blend_write_storage_content = node_blend_write;
   ntype.blend_data_read_storage_content = node_blend_read;
   blender::bke::node_register_type(ntype);

source/blender/nodes/function/nodes/node_fn_input_vector.cc
@@ -4,6 +4,8 @@
 
 #include "node_function_util.hh"
 
+#include "NOD_geometry_nodes_gizmos.hh"
+
 #include "UI_interface.hh"
 #include "UI_resources.hh"
 
@@ -11,13 +13,13 @@ namespace blender::nodes::node_fn_input_vector_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Vector>("Vector");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  uiLayout *col = &layout->column(true);
-  col->prop(ptr, "vector", UI_ITEM_R_EXPAND, "", ICON_NONE);
+  b.add_output<decl::Vector>("Vector").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayout &row = params.layout.row(true);
+    row.column(true).prop(&params.node_ptr, "vector", UI_ITEM_NONE, "", ICON_NONE);
+    if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
+      row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
+    }
+  });
 }
 
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
@@ -47,7 +49,6 @@ static void node_register()
   blender::bke::node_type_storage(
       ntype, "NodeInputVector", node_free_standard_storage, node_copy_standard_storage);
   ntype.build_multi_function = node_build_multi_function;
-  ntype.draw_buttons = node_layout;
   blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/nodes/geometry/nodes/node_geo_input_collection.cc
@@ -11,12 +11,10 @@ namespace blender::nodes::node_geo_input_collection_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Collection>("Collection");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  layout->prop(ptr, "collection", UI_ITEM_NONE, "", ICON_NONE);
+  b.add_output<decl::Collection>("Collection").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayoutSetAlignment(&params.layout, UI_LAYOUT_ALIGN_EXPAND);
+    params.layout.prop(&params.node_ptr, "collection", UI_ITEM_NONE, "", ICON_NONE);
+  });
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -34,7 +32,6 @@ static void node_register()
   ntype.ui_description = "Output a single collection";
   ntype.enum_name_legacy = "INPUT_COLLECTION";
   ntype.nclass = NODE_CLASS_INPUT;
-  ntype.draw_buttons = node_layout;
   ntype.declare = node_declare;
   ntype.geometry_node_execute = node_geo_exec;
   blender::bke::node_register_type(ntype);

source/blender/nodes/geometry/nodes/node_geo_input_material.cc
@@ -11,12 +11,10 @@ namespace blender::nodes::node_geo_input_material_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_output<decl::Material>("Material");
-}
-
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
-{
-  layout->prop(ptr, "material", UI_ITEM_NONE, "", ICON_NONE);
+  b.add_output<decl::Material>("Material").custom_draw([](CustomSocketDrawParams &params) {
+    uiLayoutSetAlignment(&params.layout, UI_LAYOUT_ALIGN_EXPAND);
+    params.layout.prop(&params.node_ptr, "material", UI_ITEM_NONE, "", ICON_NONE);
+  });
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -34,7 +32,6 @@ static void node_register()
   ntype.ui_description = "Output a single material";
   ntype.enum_name_legacy = "INPUT_MATERIAL";
   ntype.nclass = NODE_CLASS_INPUT;
-  ntype.draw_buttons = node_layout;
   ntype.declare = node_declare;
   ntype.geometry_node_execute = node_geo_exec;
   blender::bke::node_register_type(ntype);

FrenkyBojler/blender-ios

commit 939d33a2f71402367d682418819b38d4ed34da96

Fix #144760: Some (Input) Nodes not changeable in Properties Editor
A node would not show the "Collapse/Expand" arrows (caused by
87c011f8bb0ac which moved the drawing code for the input nodes a new
`custom_draw_fn` callback on the socket declaration).

Related fix: 7f07124d30f1

So in order to resolve, make sure we have correct conditions to show the
expander and also make sure we draw the custo buttons in appropriate
places.

NOTE: not sure if there are better ways to achieve the correct UI split
alignment, since we draw with empty labels, I had to flag with
`UI_ITEM_R_SPLIT_EMPTY_NAME` in the new `custom_draw_fn` callbacks.

source/blender/editors/space_node/node_templates.cc
@@ -769,14 +769,21 @@ static void ui_node_draw_recursive(uiLayout &layout,
       continue;
     }
     if (const auto *socket_decl = dynamic_cast<const nodes::SocketDeclaration *>(item_decl)) {
-      if (socket_decl->in_out == SOCK_IN) {
-        ui_node_draw_input(*panel_layout.body,
-                           C,
-                           ntree,
-                           node,
-                           node.socket_by_decl(*socket_decl),
-                           depth,
-                           panel_decl.name.c_str());
+      bNodeSocket &socket = node.socket_by_decl(*socket_decl);
+      if (socket_decl->custom_draw_fn) {
+        nodes::CustomSocketDrawParams params{
+            C,
+            *panel_layout.body,
+            ntree,
+            node,
+            socket,
+            RNA_pointer_create_discrete(&ntree.id, &RNA_Node, &node),
+            RNA_pointer_create_discrete(&ntree.id, &RNA_NodeSocket, &socket)};
+        (*socket_decl->custom_draw_fn)(params);
+      }
+      else if (socket_decl->in_out == SOCK_IN) {
+        ui_node_draw_input(
+            *panel_layout.body, C, ntree, node, socket, depth, panel_decl.name.c_str());
       }
     }
     else if (const auto *sub_panel_decl = dynamic_cast<const nodes::PanelDeclaration *>(item_decl))
@@ -795,7 +802,15 @@ static void ui_node_draw_node(
 {
   PointerRNA nodeptr = RNA_pointer_create_discrete(&ntree.id, &RNA_Node, &node);
 
-  if (node.declaration() && node.declaration()->use_custom_socket_order) {
+  /* Draw top-level node buttons. */
+  if (node.typeinfo->draw_buttons) {
+    if (node.type_legacy != NODE_GROUP) {
+      layout.use_property_split_set(true);
+      node.typeinfo->draw_buttons(&layout, &C, &nodeptr);
+    }
+  }
+
+  if (node.declaration()) {
     const nodes::NodeDeclaration &node_decl = *node.declaration();
     for (const nodes::ItemDeclaration *item_decl : node_decl.root_items) {
       if (const auto *panel_decl = dynamic_cast<const nodes::PanelDeclaration *>(item_decl)) {
@@ -803,9 +818,20 @@ static void ui_node_draw_node(
       }
       else if (const auto *socket_decl = dynamic_cast<const nodes::SocketDeclaration *>(item_decl))
       {
-        if (socket_decl->in_out == SOCK_IN) {
-          ui_node_draw_input(
-              layout, C, ntree, node, node.socket_by_decl(*socket_decl), depth + 1, nullptr);
+        bNodeSocket &socket = node.socket_by_decl(*socket_decl);
+        if (socket_decl->custom_draw_fn) {
+          nodes::CustomSocketDrawParams params{
+              C,
+              layout,
+              ntree,
+              node,
+              socket,
+              RNA_pointer_create_discrete(&ntree.id, &RNA_Node, &node),
+              RNA_pointer_create_discrete(&ntree.id, &RNA_NodeSocket, &socket)};
+          (*socket_decl->custom_draw_fn)(params);
+        }
+        else if (socket_decl->in_out == SOCK_IN) {
+          ui_node_draw_input(layout, C, ntree, node, socket, depth + 1, nullptr);
         }
       }
       else if (const auto *layout_decl = dynamic_cast<const nodes::LayoutDeclaration *>(item_decl))
@@ -816,13 +842,7 @@ static void ui_node_draw_node(
     }
   }
   else {
-    if (node.typeinfo->draw_buttons) {
-      if (node.type_legacy != NODE_GROUP) {
-        layout.use_property_split_set(true);
-        node.typeinfo->draw_buttons(&layout, &C, &nodeptr);
-      }
-    }
-
+    /* Draw socket values using the flat inputs list. */
     LISTBASE_FOREACH (bNodeSocket *, input, &node.inputs) {
       ui_node_draw_input(layout, C, ntree, node, *input, depth + 1, nullptr);
     }
@@ -872,11 +892,34 @@ static void ui_node_draw_input(uiLayout &layout,
     if (depth > 0) {
       UI_block_emboss_set(block, ui::EmbossType::None);
 
-      if (lnode && (lnode->inputs.first ||
-                    (lnode->typeinfo->draw_buttons && lnode->type_legacy != NODE_GROUP)))
-      {
-        int icon = (input.flag & SOCK_COLLAPSED) ? ICON_RIGHTARROW : ICON_DOWNARROW_HLT;
-        sub->prop(&inputptr, "show_expanded", UI_ITEM_R_ICON_ONLY, "", icon);
+      if (lnode) {
+        /* Input linked to a node, we can expand/collapse if
+         - linked node has inputs
+         - linked node has dedicated button drawing
+         - linked node has dedicated socket drawing
+        */
+        bool can_expand = lnode->inputs.first;
+        if (lnode->type_legacy != NODE_GROUP) {
+          if (lnode->typeinfo->draw_buttons) {
+            can_expand = true;
+          }
+          else if (lnode->declaration()) {
+            const nodes::NodeDeclaration &lnode_decl = *lnode->declaration();
+            for (const nodes::ItemDeclaration *item_decl : lnode_decl.root_items) {
+              if (const auto *socket_decl = dynamic_cast<const nodes::SocketDeclaration *>(
+                      item_decl))
+              {
+                if (socket_decl->custom_draw_fn) {
+                  can_expand = true;
+                }
+              }
+            }
+          }
+        }
+        if (can_expand) {
+          int icon = (input.flag & SOCK_COLLAPSED) ? ICON_RIGHTARROW : ICON_DOWNARROW_HLT;
+          sub->prop(&inputptr, "show_expanded", UI_ITEM_R_ICON_ONLY, "", icon);
+        }
       }
 
       UI_block_emboss_set(block, ui::EmbossType::Emboss);

source/blender/nodes/composite/nodes/node_composite_rgb.cc
@@ -31,7 +31,11 @@ static void cmp_node_rgb_declare(NodeDeclarationBuilder &b)
         uiLayout &col = params.layout.column(false);
         uiTemplateColorPicker(
             &col, &params.socket_ptr, "default_value", true, false, false, false);
-        col.prop(&params.socket_ptr, "default_value", UI_ITEM_R_SLIDER, "", ICON_NONE);
+        col.prop(&params.socket_ptr,
+                 "default_value",
+                 UI_ITEM_R_SLIDER | UI_ITEM_R_SPLIT_EMPTY_NAME,
+                 "",
+                 ICON_NONE);
       });
 }
 

source/blender/nodes/function/nodes/node_fn_input_bool.cc
@@ -15,7 +15,8 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Bool>("Boolean").custom_draw([](CustomSocketDrawParams &params) {
     uiLayout &row = params.layout.row(true);
-    row.prop(&params.node_ptr, "boolean", UI_ITEM_NONE, IFACE_("Boolean"), ICON_NONE);
+    row.prop(
+        &params.node_ptr, "boolean", UI_ITEM_R_SPLIT_EMPTY_NAME, IFACE_("Boolean"), ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
       row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
     }

source/blender/nodes/function/nodes/node_fn_input_int.cc
@@ -15,7 +15,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Int>("Integer").custom_draw([](CustomSocketDrawParams &params) {
     uiLayout &row = params.layout.row(true);
-    row.prop(&params.node_ptr, "integer", UI_ITEM_NONE, "", ICON_NONE);
+    row.prop(&params.node_ptr, "integer", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
       row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
     }

source/blender/nodes/function/nodes/node_fn_input_rotation.cc
@@ -17,7 +17,8 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Rotation>("Rotation").custom_draw([](CustomSocketDrawParams &params) {
     uiLayout &row = params.layout.row(true);
-    row.column(true).prop(&params.node_ptr, "rotation_euler", UI_ITEM_NONE, "", ICON_NONE);
+    row.column(true).prop(
+        &params.node_ptr, "rotation_euler", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
       row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
     }

source/blender/nodes/function/nodes/node_fn_input_string.cc
@@ -24,8 +24,14 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::String>("String").custom_draw([](CustomSocketDrawParams &params) {
     params.layout.alignment_set(ui::LayoutAlign::Expand);
     PropertyRNA *prop = RNA_struct_find_property(&params.node_ptr, "string");
-    params.layout.prop(
-        &params.node_ptr, prop, -1, 0, UI_ITEM_NONE, "", ICON_NONE, IFACE_("String"));
+    params.layout.prop(&params.node_ptr,
+                       prop,
+                       -1,
+                       0,
+                       UI_ITEM_R_SPLIT_EMPTY_NAME,
+                       "",
+                       ICON_NONE,
+                       IFACE_("String"));
   });
 }
 

source/blender/nodes/function/nodes/node_fn_input_vector.cc
@@ -15,7 +15,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Vector>("Vector").custom_draw([](CustomSocketDrawParams &params) {
     uiLayout &row = params.layout.row(true);
-    row.column(true).prop(&params.node_ptr, "vector", UI_ITEM_NONE, "", ICON_NONE);
+    row.column(true).prop(&params.node_ptr, "vector", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
       row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
     }

source/blender/nodes/geometry/nodes/node_geo_input_collection.cc
@@ -13,7 +13,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Collection>("Collection").custom_draw([](CustomSocketDrawParams &params) {
     params.layout.alignment_set(ui::LayoutAlign::Expand);
-    params.layout.prop(&params.node_ptr, "collection", UI_ITEM_NONE, "", ICON_NONE);
+    params.layout.prop(&params.node_ptr, "collection", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
   });
 }
 

source/blender/nodes/geometry/nodes/node_geo_input_material.cc
@@ -13,7 +13,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Material>("Material").custom_draw([](CustomSocketDrawParams &params) {
     params.layout.alignment_set(ui::LayoutAlign::Expand);
-    params.layout.prop(&params.node_ptr, "material", UI_ITEM_NONE, "", ICON_NONE);
+    params.layout.prop(&params.node_ptr, "material", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
   });
 }
 

source/blender/nodes/geometry/nodes/node_geo_input_object.cc
@@ -13,7 +13,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Object>("Object").custom_draw([](CustomSocketDrawParams &params) {
     params.layout.alignment_set(ui::LayoutAlign::Expand);
-    params.layout.prop(&params.node_ptr, "object", UI_ITEM_NONE, "", ICON_NONE);
+    params.layout.prop(&params.node_ptr, "object", UI_ITEM_R_SPLIT_EMPTY_NAME, "", ICON_NONE);
   });
 }
 

source/blender/nodes/shader/nodes/node_shader_value.cc
@@ -22,7 +22,7 @@ static void sh_node_value_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Float>("Value").custom_draw([](CustomSocketDrawParams &params) {
     uiLayout &row = params.layout.row(true);
-    row.prop(&params.socket_ptr, "default_value", UI_ITEM_NONE, "", ICON_NONE);
+    row.prop(&params.socket_ptr, "default_value", UI_ITEM_R_SPLIT_EMPTY_NAME, " ", ICON_NONE);
     if (gizmos::value_node_has_gizmo(params.tree, params.node)) {
       row.prop(&params.socket_ptr, "pin_gizmo", UI_ITEM_NONE, "", ICON_GIZMO);
     }