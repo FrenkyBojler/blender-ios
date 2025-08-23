be01896e933dc88d71315f9f08ff809be9d1ee6a
Ramon Klauck <ramonklauck3@gmail.com>
2025-08-23 05:32:00 +0200

    create view_type variable

--- scripts/startup/bl_ui/space_toolsystem_common.py
+++ scripts/startup/bl_ui/space_toolsystem_common.py
@@ -1074,18 +1074,18 @@ def _activate_by_item(context, space_type, item, index, *, as_fallback=False):
 
     handle_map = _activate_by_item._cursor_draw_handle
     # view_type used when in VSE, check if view_type exists because not every space_data has it.
-    handle_type = (space_type, getattr(context.space_data, "view_type", None))
-    handle = handle_map.pop(handle_type, None)
+    view_type = getattr(context.space_data, "view_type", None)
+    handle = handle_map.pop((space_type, view_type), None)
     if handle is not None:
         WindowManager.draw_cursor_remove(handle)
     if item.draw_cursor is not None:
         def handle_fn(context, item, tool, xy):
             item.draw_cursor(context, tool, xy)
-        if handle_type[1] == 'PREVIEW':
+        if view_type == 'PREVIEW':
             handle = WindowManager.draw_cursor_add(handle_fn, (context, item, tool), space_type, 'PREVIEW')
         else:
             handle = WindowManager.draw_cursor_add(handle_fn, (context, item, tool), space_type, 'WINDOW')
-        handle_map[handle_type] = handle
+        handle_map[(space_type, view_type)] = handle
 
 
 _activate_by_item._cursor_draw_handle = {}
