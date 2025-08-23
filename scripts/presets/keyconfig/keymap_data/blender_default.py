727241b6952c03922b066fc6281cfbc77537081d
Ramon Klauck <ramonklauck3@gmail.com>
2025-08-23 05:22:56 +0200

    seperate lasso timeline and preview keymap

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -8452,6 +8452,18 @@ def km_sequencer_tool_generic_select_box(params, *, fallback):
         ]},
     )
 
+def km_sequencer_tool_generic_select_lasso(params, *, fallback):
+    return (
+        _fallback_id("Preview Tool: Select Lasso", fallback),
+        {"space_type": 'SEQUENCE_EDITOR', "region_type": 'WINDOW'},
+        {"items": [
+            *([] if (fallback and not params.use_fallback_tool) else _template_items_tool_select_actions_simple(
+                "sequencer.select_lasso",
+                **(params.select_tweak_event if (fallback and params.use_fallback_tool_select_mouse) else
+                   params.tool_tweak_event))),
+        ]},
+    )
+
 
 def km_sequencer_preview_tool_generic_select(params, *, fallback):
     return (
@@ -8862,6 +8874,8 @@ def generate_keymaps(params=None):
         *(km_sequencer_preview_tool_generic_select(params, fallback=fallback)
           for fallback in (False, True)),
         *(km_sequencer_preview_tool_generic_select_box(params, fallback=fallback)
+          for fallback in (False, True)),
+        *(km_sequencer_tool_generic_select_lasso(params, fallback=fallback)
           for fallback in (False, True)),
         *(km_sequencer_preview_tool_generic_select_lasso(params, fallback=fallback)
           for fallback in (False, True)),
