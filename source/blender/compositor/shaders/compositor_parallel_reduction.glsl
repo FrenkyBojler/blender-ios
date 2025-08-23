514f99045a7040e09a7e577e51fb1a9632b6fc42
Clément Foucault <foucault.clem@gmail.com>
2025-08-23 12:20:54 +0200

    Rework compositor parallel reduction to use classes

--- source/blender/compositor/shaders/compositor_parallel_reduction.glsl
+++ source/blender/compositor/shaders/compositor_parallel_reduction.glsl
@@ -59,93 +59,143 @@ COMPUTE_SHADER_CREATE_INFO(compositor_parallel_reduction_output_float4)
 
 #define reduction_size (gl_WorkGroupSize.x * gl_WorkGroupSize.y)
 
-/* Operation */
-struct Min {};
-struct Max {};
-struct Sum {};
-struct MaxVelocity {};
-struct MaxInRange {};
-struct MinInRange {};
-struct SumSquareDifference {};
+/* Operations */
 
-/* Initialize. */
+template<typename T> struct Min {
+  static T identity()
+  {
+    return T(0);
+  }
 
-template<typename T, typename Operation> T initialize(T value)
-{
-  return value;
-}
-template float initialize<float, Min>(float);
-template float initialize<float, Max>(float);
-template float2 initialize<float2, Max>(float2);
-template float initialize<float, Sum>(float);
-template float4 initialize<float4, Sum>(float4);
-template float4 initialize<float4, MaxVelocity>(float4);
+  static T initialize(T value)
+  {
+    return value;
+  }
 
-template<> float initialize<float, MaxInRange>(float value)
-{
-  float max = push_constant_get(compositor_maximum_float_in_range, upper_bound);
-  float min = push_constant_get(compositor_maximum_float_in_range, lower_bound);
-  return clamp(value, min, max);
-}
-template<> float initialize<float, MinInRange>(float value)
-{
-  float max = push_constant_get(compositor_minimum_float_in_range, upper_bound);
-  float min = push_constant_get(compositor_minimum_float_in_range, lower_bound);
-  return clamp(value, min, max);
-}
+  static T reduce(T lhs, T rhs)
+  {
+    return min(lhs, rhs);
+  }
+};
+template struct Min<float>;
 
-template<> float initialize<float, SumSquareDifference>(float value)
-{
-  float sub = push_constant_get(compositor_sum_squared_difference_float_shared, subtrahend);
-  return square(value - sub);
-}
+template<typename T> struct Max {
+  static T identity()
+  {
+    return T(0);
+  }
 
-/* Identity. */
+  static T initialize(T value)
+  {
+    return value;
+  }
 
-template<typename T, typename Operation> T identity()
-{
-  return T(0);
-}
-template float identity<float, Min>(); /* TODO */
-template float identity<float, Max>();
-template float2 identity<float2, Max>();
-template float identity<float, Sum>();
-template float4 identity<float4, Sum>();
-template float4 identity<float4, MaxVelocity>(); /* TODO */
-template float identity<float, MaxInRange>();    /* TODO */
-template float identity<float, MinInRange>();    /* TODO */
-template float identity<float, SumSquareDifference>();
+  static T reduce(T lhs, T rhs)
+  {
+    return max(lhs, rhs);
+  }
+};
+template struct Max<float>;
+template struct Max<float4>;
 
-/* Reduce. */
+template<typename T> struct Sum {
+  static T identity()
+  {
+    return T(0);
+  }
 
-template<typename T, typename Operation> T reduce(T lhs, T rhs)
-{
-  return lhs + rhs;
-}
-template float reduce<float, Sum>(float, float);
-template float4 reduce<float4, Sum>(float4, float4);
-template float reduce<float, SumSquareDifference>(float, float);
-/* clang-format off */
-template<> float reduce<float, Min>(float lhs, float rhs) { return min(lhs, rhs); }
-template<> float2 reduce<float2, Min>(float2 lhs, float2 rhs) { return min(lhs, rhs); }
-template<> float reduce<float, Max>(float lhs, float rhs) { return max(lhs, rhs); }
-template<> float2 reduce<float2, Max>(float2 lhs, float2 rhs) { return max(lhs, rhs); }
-/* clang-format on */
-template<> float reduce<float, MaxInRange>(float lhs, float rhs)
-{
-  float max = push_constant_get(compositor_maximum_float_in_range, upper_bound);
-  return ((rhs > lhs) && (rhs <= max)) ? rhs : lhs;
-}
-template<> float reduce<float, MinInRange>(float lhs, float rhs)
-{
-  float min = push_constant_get(compositor_minimum_float_in_range, lower_bound);
-  return ((rhs < lhs) && (rhs >= min)) ? rhs : lhs;
-}
-template<> float4 reduce<float4, MaxVelocity>(float4 lhs, float4 rhs)
-{
-  return float4(dot(lhs.xy, lhs.xy) > dot(rhs.xy, rhs.xy) ? lhs.xy : rhs.xy,
-                dot(lhs.zw, lhs.zw) > dot(rhs.zw, rhs.zw) ? lhs.zw : rhs.zw);
-}
+  static T initialize(T value)
+  {
+    return value;
+  }
+
+  static T reduce(T lhs, T rhs)
+  {
+    return lhs + rhs;
+  }
+};
+template struct Sum<float>;
+template struct Sum<float4>;
+
+template<typename T> struct MaxVelocity {
+  static T identity()
+  {
+    return T(0);
+  }
+
+  static T initialize(T value)
+  {
+    return value;
+  }
+
+  static T reduce(T lhs, T rhs)
+  {
+    return float4(dot(lhs.xy, lhs.xy) > dot(rhs.xy, rhs.xy) ? lhs.xy : rhs.xy,
+                  dot(lhs.zw, lhs.zw) > dot(rhs.zw, rhs.zw) ? lhs.zw : rhs.zw);
+  }
+};
+template struct MaxVelocity<float4>;
+
+template<typename T> struct MaxInRange {
+  static T identity()
+  {
+    return T(0);
+  }
+
+  static T initialize(T value)
+  {
+    float max = push_constant_get(compositor_maximum_float_in_range, upper_bound);
+    float min = push_constant_get(compositor_maximum_float_in_range, lower_bound);
+    return clamp(value, min, max);
+  }
+
+  static T reduce(T lhs, T rhs)
+  {
+    float max = push_constant_get(compositor_maximum_float_in_range, upper_bound);
+    return ((rhs > lhs) && (rhs <= max)) ? rhs : lhs;
+  }
+};
+template struct MaxInRange<float>;
+
+template<typename T> struct MinInRange {
+  static T identity()
+  {
+    return T(0);
+  }
+
+  static T initialize(T value)
+  {
+    float max = push_constant_get(compositor_minimum_float_in_range, upper_bound);
+    float min = push_constant_get(compositor_minimum_float_in_range, lower_bound);
+    return clamp(value, min, max);
+  }
+
+  static T reduce(T lhs, T rhs)
+  {
+    float min = push_constant_get(compositor_minimum_float_in_range, lower_bound);
+    return ((rhs < lhs) && (rhs >= min)) ? rhs : lhs;
+  }
+};
+template struct MinInRange<float>;
+
+template<typename T> struct SumSquareDifference {
+  static T identity()
+  {
+    return T(0);
+  }
+
+  static T initialize(T value)
+  {
+    float sub = push_constant_get(compositor_sum_squared_difference_float_shared, subtrahend);
+    return square(value - sub);
+  }
+
+  static T reduce(T lhs, T rhs)
+  {
+    return lhs + rhs;
+  }
+};
+template struct SumSquareDifference<float>;
 
 /* ChannelMix */
 struct ChannelR {};
@@ -226,7 +276,7 @@ template<typename T, typename Operation, typename ChannelMix> void reduction()
    * not affect the output of the reduction. For instance, sum reductions have an identity of 0.0,
    * while max value reductions have an identity of FLT_MIN */
   if (any(lessThan(texel, int2(0))) || any(greaterThanEqual(texel, texture_size(input_tx)))) {
-    store_shared_data(gl_LocalInvocationIndex, identity<T, Operation>());
+    store_shared_data(gl_LocalInvocationIndex, Operation::identity());
   }
   else {
     float4 value = texture_load_unbound(input_tx, texel);
@@ -243,7 +293,7 @@ template<typename T, typename Operation, typename ChannelMix> void reduction()
      * will be loaded directly and reduced without extra processing. So the developer is expected
      * to define the INITIALIZE and LOAD macros to be expressions that derive the needed value from
      * the loaded value for the initial reduction pass and latter ones respectively. */
-    T data = is_initial_reduction ? initialize<T, Operation>(channel_mix<T, ChannelMix>(value)) :
+    T data = is_initial_reduction ? Operation::initialize(channel_mix<T, ChannelMix>(value)) :
                                     load<T>(value);
     store_shared_data(gl_LocalInvocationIndex, data);
   }
@@ -266,7 +316,7 @@ template<typename T, typename Operation, typename ChannelMix> void reduction()
     T lhs, rhs;
     load_shared_data(gl_LocalInvocationIndex, lhs);
     load_shared_data(gl_LocalInvocationIndex + stride, rhs);
-    T result = reduce<T, Operation>(lhs, rhs);
+    T result = Operation::reduce(lhs, rhs);
     store_shared_data(gl_LocalInvocationIndex, result);
   }
 
@@ -281,111 +331,111 @@ template<typename T, typename Operation, typename ChannelMix> void reduction()
   }
 }
 
-template void reduction<float, Sum, ChannelR>();
-template void reduction<float, Sum, ChannelG>();
-template void reduction<float, Sum, ChannelB>();
-template void reduction<float, Sum, ChannelLuma>();
-template void reduction<float, Sum, ChannelLogLuma>();
-template void reduction<float4, Sum, ChannelRGBA>();
+template void reduction<float, Sum<float>, ChannelR>();
+template void reduction<float, Sum<float>, ChannelG>();
+template void reduction<float, Sum<float>, ChannelB>();
+template void reduction<float, Sum<float>, ChannelLuma>();
+template void reduction<float, Sum<float>, ChannelLogLuma>();
+template void reduction<float4, Sum<float4>, ChannelRGBA>();
 
 void reduce_sum_red()
 {
-  reduction<float, Sum, ChannelR>();
+  reduction<float, Sum<float>, ChannelR>();
 }
 void reduce_sum_green()
 {
-  reduction<float, Sum, ChannelG>();
+  reduction<float, Sum<float>, ChannelG>();
 }
 void reduce_sum_blue()
 {
-  reduction<float, Sum, ChannelB>();
+  reduction<float, Sum<float>, ChannelB>();
 }
 void reduce_sum_luminance()
 {
-  reduction<float, Sum, ChannelLuma>();
+  reduction<float, Sum<float>, ChannelLuma>();
 }
 void reduce_sum_log_luminance()
 {
-  reduction<float, Sum, ChannelLogLuma>();
+  reduction<float, Sum<float>, ChannelLogLuma>();
 }
 void reduce_sum_color()
 {
-  reduction<float4, Sum, ChannelRGBA>();
+  reduction<float4, Sum<float4>, ChannelRGBA>();
 }
 
-template void reduction<float, SumSquareDifference, ChannelR>();
-template void reduction<float, SumSquareDifference, ChannelG>();
-template void reduction<float, SumSquareDifference, ChannelB>();
-template void reduction<float, SumSquareDifference, ChannelLuma>();
+template void reduction<float, SumSquareDifference<float>, ChannelR>();
+template void reduction<float, SumSquareDifference<float>, ChannelG>();
+template void reduction<float, SumSquareDifference<float>, ChannelB>();
+template void reduction<float, SumSquareDifference<float>, ChannelLuma>();
 
 void reduce_sum_red_squared_difference()
 {
-  reduction<float, SumSquareDifference, ChannelR>();
+  reduction<float, SumSquareDifference<float>, ChannelR>();
 }
 void reduce_sum_green_squared_difference()
 {
-  reduction<float, SumSquareDifference, ChannelG>();
+  reduction<float, SumSquareDifference<float>, ChannelG>();
 }
 void reduce_sum_blue_squared_difference()
 {
-  reduction<float, SumSquareDifference, ChannelB>();
+  reduction<float, SumSquareDifference<float>, ChannelB>();
 }
 void reduce_sum_luminance_squared_difference()
 {
-  reduction<float, SumSquareDifference, ChannelLuma>();
+  reduction<float, SumSquareDifference<float>, ChannelLuma>();
 }
 
-template void reduction<float, Max, ChannelLuma>();
-template void reduction<float, Max, ChannelMax>();
-template void reduction<float, Max, ChannelR>();
-template void reduction<float2, Max, ChannelRG>();
+template void reduction<float, Max<float>, ChannelLuma>();
+template void reduction<float, Max<float>, ChannelMax>();
+template void reduction<float, Max<float>, ChannelR>();
+template void reduction<float2, Max<float2>, ChannelRG>();
 
 void reduce_maximum_luminance()
 {
-  reduction<float, Max, ChannelLuma>();
+  reduction<float, Max<float>, ChannelLuma>();
 }
 void reduce_maximum_brightness()
 {
-  reduction<float, Max, ChannelMax>();
+  reduction<float, Max<float>, ChannelMax>();
 }
 void reduce_maximum_float()
 {
-  reduction<float, Max, ChannelR>();
+  reduction<float, Max<float>, ChannelR>();
 }
 void reduce_maximum_float2()
 {
-  reduction<float2, Max, ChannelRG>();
+  reduction<float2, Max<float2>, ChannelRG>();
 }
 
-template void reduction<float, MaxInRange, ChannelR>();
+template void reduction<float, MaxInRange<float>, ChannelR>();
 
 void reduce_maximum_float_in_range()
 {
-  reduction<float, MaxInRange, ChannelR>();
+  reduction<float, MaxInRange<float>, ChannelR>();
 }
 
-template void reduction<float, Min, ChannelLuma>();
-template void reduction<float, Min, ChannelR>();
+template void reduction<float, Min<float>, ChannelLuma>();
+template void reduction<float, Min<float>, ChannelR>();
 
 void reduce_minimum_luminance()
 {
-  reduction<float, Min, ChannelLuma>();
+  reduction<float, Min<float>, ChannelLuma>();
 }
 void reduce_minimum_float()
 {
-  reduction<float, Min, ChannelR>();
+  reduction<float, Min<float>, ChannelR>();
 }
 
-template void reduction<float, MinInRange, ChannelR>();
+template void reduction<float, MinInRange<float>, ChannelR>();
 
 void reduce_minimum_float_in_range()
 {
-  reduction<float, MinInRange, ChannelR>();
+  reduction<float, MinInRange<float>, ChannelR>();
 }
 
-template void reduction<float4, MaxVelocity, ChannelRGBA>();
+template void reduction<float4, MaxVelocity<float4>, ChannelRGBA>();
 
 void reduce_max_velocity()
 {
-  reduction<float4, MaxVelocity, ChannelRGBA>();
+  reduction<float4, MaxVelocity<float4>, ChannelRGBA>();
 }
