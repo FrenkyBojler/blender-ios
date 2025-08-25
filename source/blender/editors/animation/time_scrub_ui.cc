a5c7fdffd0267343130c6a3c1835eaebe41cec60
Harley Acheson <harley.acheson@gmail.com>
2025-08-25 09:52:59 -0700

    Improved shadow

--- source/blender/editors/animation/time_scrub_ui.cc
+++ source/blender/editors/animation/time_scrub_ui.cc
@@ -94,9 +94,29 @@ static void draw_current_frame(const Scene *scene,
   const float text_width = UI_fontstyle_string_width(fstyle, frame_str);
   const float box_width = std::max(text_width + (2.0f * text_padding), box_min_width);
   const float box_margin = 4 * UI_SCALE_FAC;
+  const float shadow_width = U.pixelsize;
 
-  float color[4];
-  UI_GetThemeColor4fv(TH_CFRAME, color);
+  float fg_color[4];
+  UI_GetThemeColor4fv(TH_CFRAME, fg_color);
+  float bg_color[4];
+  UI_GetThemeColorShade4fv(TH_BACK, -20, bg_color);
+
+  /* Box. */
+  UI_draw_roundbox_corner_set(UI_CNR_ALL);
+  const float box_corner_radius = 3 * UI_SCALE_FAC;
+  rctf rect{};
+  rect.xmin = frame_x - (box_width / 2.0f);
+  rect.xmax = frame_x + (box_width / 2.0f) + 1.0f;
+  rect.ymin = floor(scrub_region_rect->ymin + box_margin - shadow_width);
+  rect.ymax = ceil(scrub_region_rect->ymax - box_margin + shadow_width);
+  UI_draw_roundbox_4fv_ex(
+      &rect, fg_color, nullptr, 1.0f, bg_color, shadow_width, box_corner_radius);
+
+  /* Frame number text. */
+  uchar text_color[4];
+  UI_GetThemeColor4ubv(TH_HEADER_TEXT_HI, text_color);
+  const int y = BLI_rcti_cent_y(scrub_region_rect) - int(fstyle->points * UI_SCALE_FAC * 0.38f);
+  UI_fontstyle_draw_simple(fstyle, frame_x - (text_width / 2.0f), y, frame_str, text_color);
 
   if (display_stalk) {
     /* Draw vertical line from the bottom of the current frame box to the bottom of the screen. */
@@ -110,26 +130,30 @@ static void draw_current_frame(const Scene *scene,
      * used to force an odd width (but still pixel-aligned) to better
      * line up with the odd widths of the keyframe icons. #98089. */
 
-    const float shadow_width = U.pixelsize;
     const float tri_top = scrub_region_rect->ymin + box_margin;
     const float tri_half_width = 7 * UI_SCALE_FAC;
     const float tri_height = 10 * UI_SCALE_FAC;
 
     /* Shadow. */
     GPU_polygon_smooth(true);
-    immUniformThemeColorAlpha(TH_BLACK, 0.3f);
+    immUniformColor4fv(bg_color);
     immBegin(GPU_PRIM_TRI_STRIP, 6);
     immVertex2f(pos, floor(subframe_x + U.pixelsize + 1.0f + shadow_width), 0.0f);
     immVertex2f(pos, floor(subframe_x - U.pixelsize - shadow_width), 0.0f);
-    immVertex2f(pos, floor(subframe_x + U.pixelsize + 1.0f + shadow_width), tri_top - tri_height);
-    immVertex2f(pos, floor(subframe_x - U.pixelsize - shadow_width), tri_top - tri_height);
-    immVertex2f(pos, floor(frame_x + tri_half_width + 1 + shadow_width), tri_top);
+
+    immVertex2f(pos,
+                floor(subframe_x + U.pixelsize + 1.0f + shadow_width),
+                tri_top - tri_height + shadow_width);
+    immVertex2f(
+        pos, floor(subframe_x - U.pixelsize - shadow_width), tri_top - tri_height + shadow_width);
+
+    immVertex2f(pos, floor(frame_x + tri_half_width + shadow_width + 1.0f), tri_top);
     immVertex2f(pos, floor(frame_x - tri_half_width - shadow_width), tri_top);
     immEnd();
     GPU_polygon_smooth(false);
 
     /* Line. */
-    immUniformColor4fv(color);
+    immUniformColor4fv(fg_color);
     immRectf(pos,
              floor(subframe_x - U.pixelsize),
              scrub_region_rect->ymin,
@@ -138,7 +162,7 @@ static void draw_current_frame(const Scene *scene,
 
     /* Triangular base. */
     GPU_polygon_smooth(true);
-    immUniformColor4fv(color);
+    immUniformColor4fv(fg_color);
     immBegin(GPU_PRIM_TRIS, 3);
     immVertex2f(pos, frame_x - tri_half_width, tri_top);
     immVertex2f(pos, frame_x + tri_half_width + 1, tri_top);
@@ -148,22 +172,6 @@ static void draw_current_frame(const Scene *scene,
     GPU_polygon_smooth(false);
     GPU_blend(GPU_BLEND_NONE);
   }
-
-  /* Box. */
-  UI_draw_roundbox_corner_set(UI_CNR_ALL);
-  const float box_corner_radius = 2 * UI_SCALE_FAC;
-  rctf rect{};
-  rect.xmin = frame_x - (box_width / 2.0f);
-  rect.xmax = frame_x + (box_width / 2.0f) + 1.0f;
-  rect.ymin = floor(scrub_region_rect->ymin + box_margin);
-  rect.ymax = ceil(scrub_region_rect->ymax - box_margin);
-  UI_draw_roundbox_4fv_ex(&rect, color, nullptr, 1.0f, nullptr, U.pixelsize, box_corner_radius);
-
-  /* Frame number text. */
-  uchar text_color[4];
-  UI_GetThemeColor4ubv(TH_HEADER_TEXT_HI, text_color);
-  const int y = BLI_rcti_cent_y(scrub_region_rect) - int(fstyle->points * UI_SCALE_FAC * 0.38f);
-  UI_fontstyle_draw_simple(fstyle, frame_x - (text_width / 2.0f), y, frame_str, text_color);
 }
 
 void ED_time_scrub_draw_current_frame(const ARegion *region,
