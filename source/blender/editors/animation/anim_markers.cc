1be5899a5c71925476d16903c72b48a0e0a18a70
Wayne Dixon <thewaylow@gmail.com>
2025-08-25 22:28:02 +1000

    shift ctrl clicking now works correctly

--- source/blender/editors/animation/anim_markers.cc
+++ source/blender/editors/animation/anim_markers.cc
@@ -1310,14 +1310,15 @@ static void select_marker_camera_switch(
     }
 
     BKE_view_layer_synced_ensure(scene, view_layer);
+
     LISTBASE_FOREACH (TimeMarker *, marker, markers) {
-      if (marker->camera) {
-        if (marker->frame == cfra) {
-          base = BKE_view_layer_base_find(view_layer, marker->camera);
-          if (base) {
-            object::base_select(base, object::eObjectSelect_Mode(SEL_OP_ADD));
+      if (marker->camera && marker->frame == cfra) {
+        Base *base = BKE_view_layer_base_find(view_layer, marker->camera);
+        if (base) {
+          object::base_select(base, object::eObjectSelect_Mode(SEL_OP_ADD));
+
+          if (!extend) {
             object::base_activate(C, base);
-            
           }
         }
       }
@@ -1403,6 +1404,11 @@ static wmOperatorStatus ed_marker_select(bContext *C,
         marker_found->flag |= SELECT;
       }
     }
+  }
+  /* if extend is set (but holding Shift), then add the camera to the selection too*/
+  if (found && camera) {
+    const int marker_frame = nearest_marker->frame;
+    select_marker_camera_switch(C, true, extend, markers, marker_frame);
   }
 
   WM_event_add_notifier(C, NC_SCENE | ND_MARKERS, nullptr);
