c79616ce489dd4fab9446a5fa0570378a5301017
salipour <sahar.kashi@amd.com>
2025-08-25 11:01:12 -0700

    Dropping ROCm 5

--- extern/hipew/src/hipew.c
+++ extern/hipew/src/hipew.c
@@ -38,11 +38,7 @@ thipDriverGetVersion *hipDriverGetVersion;
 thipRuntimeGetVersion *hipRuntimeGetVersion;
 thipGetDevice *hipGetDevice;
 thipGetDeviceCount *hipGetDeviceCount;
-#ifdef WITH_HIP_SDK_5
-  thipGetDeviceProperties *hipGetDeviceProperties;
-#else
-  thipGetDevicePropertiesR0600 *hipGetDevicePropertiesR0600;
-#endif
+thipGetDevicePropertiesR0600 *hipGetDevicePropertiesR0600;
 thipDeviceGet* hipDeviceGet;
 thipDeviceGetName *hipDeviceGetName;
 thipDeviceGetAttribute *hipDeviceGetAttribute;
@@ -237,19 +233,14 @@ static int hipewHipInit(void) {
   /* Library paths. */
 #ifdef _WIN32
   /* Expected in C:/Windows/System32 or similar, no path needed. */
-  const char *hip_paths[] = { "amdhip64_7", "amdhip64_6.dll", NULL};
+  const char *hip_paths[] = { "amdhip64_7.dll", "amdhip64_6.dll", NULL};
 #elif defined(__APPLE__)
   /* Default installation path. */
   const char *hip_paths[] = {"", NULL};
 #else
   /* ROCm 6 changes paths from /opt/rocm/hip/lib to /opt/rocm/lib, so
-   * search for libraries there. It still includes .so.5. */
-  #ifdef WITH_HIP_SDK_5
-      const char *hip_paths[] = {"libamdhip64.so.5",
-                               "/opt/rocm/lib/libamdhip64.so.5",
-                               "/opt/rocm/hip/lib/libamdhip64.so.5",
-                                NULL};
-  #else
+   * search for libraries there. */
+
   const char* hip_versions[] = { "", ".7", ".6", NULL };
   const char* hip_path[] = {
       "",
@@ -272,10 +263,6 @@ static int hipewHipInit(void) {
     }
     hip_paths[idx] = NULL;
   }
-
-
-
-  #endif
 #endif
   static int initialized = 0;
   static int result = 0;
@@ -310,11 +297,7 @@ static int hipewHipInit(void) {
   }
 
   /* Fetch all function pointers. */
-#ifdef WITH_HIP_SDK_5
-  HIP_LIBRARY_FIND_CHECKED(hipGetDeviceProperties);
-#else
   HIP_LIBRARY_FIND_CHECKED(hipGetDevicePropertiesR0600);
-#endif
   HIP_LIBRARY_FIND_CHECKED(hipGetErrorName);
   HIP_LIBRARY_FIND_CHECKED(hipGetErrorString);
   HIP_LIBRARY_FIND_CHECKED(hipGetLastError);
