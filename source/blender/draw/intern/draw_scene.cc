076383b9c342b1bdb02143006134a38753dd7f5c
Miguel Pozo <pragma37@gmail.com>
2025-08-25 16:19:58 +0200

    Make classes internal

--- source/blender/draw/intern/draw_scene.cc
+++ source/blender/draw/intern/draw_scene.cc
@@ -4,17 +4,25 @@
 
 #include "draw_scene.hh"
 
+#include "BLI_listbase.h"
+#include "BLI_map.hh"
+#include "BLI_math_matrix.h"
+#include "BLI_math_matrix_types.hh"
+#include "BLI_math_vector.h"
+#include "BLI_sys_types.h"
+#include "BLI_utildefines.h"
+
 #include "BKE_idprop.hh"
 #include "BKE_layer.hh"
 #include "BKE_modifier.hh"
 #include "BKE_object.hh"
 #include "BKE_particle.h"
 #include "BKE_scene.hh"
-#include "BLI_listbase.h"
-#include "BLI_math_matrix.h"
-#include "BLI_math_vector.h"
+
 #include "DEG_depsgraph_query.hh"
+
 #include "DNA_view3d_types.h"
+
 #include "DRW_render.hh"
 #include "draw_handle.hh"
 #include "draw_view_data.hh"
@@ -37,6 +45,90 @@ static bool supports_handle_ranges(Object *ob)
   }
   return ELEM(ob->type, OB_CURVES_LEGACY, OB_SURF, OB_FONT, OB_POINTCLOUD, OB_GREASE_PENCIL);
 }
+
+enum class InstancesFlags : uint8_t {
+  IsNegativeScale = 1 << 0,
+};
+ENUM_OPERATORS(InstancesFlags, InstancesFlags::IsNegativeScale);
+
+struct InstancesKey {
+  uint64_t hash_value;
+
+  Object *object;
+  ID *ob_data;
+  const blender::bke::GeometrySet *preview_base_geometry;
+  int preview_instance_index;
+  InstancesFlags flags;
+
+  InstancesKey(Object *object,
+               ID *ob_data,
+               InstancesFlags flags,
+               const blender::bke::GeometrySet *preview_base_geometry,
+               int preview_instance_index)
+      : object(object),
+        ob_data(ob_data),
+        preview_base_geometry(preview_base_geometry),
+        preview_instance_index(preview_instance_index),
+        flags(flags)
+  {
+    hash_value = get_default_hash(object);
+    hash_value = get_default_hash(hash_value, ob_data);
+    hash_value = get_default_hash(hash_value, preview_base_geometry);
+    hash_value = get_default_hash(hash_value, preview_instance_index);
+    hash_value = get_default_hash(hash_value, uint8_t(flags));
+  }
+
+  uint64_t hash() const
+  {
+    return hash_value;
+  }
+
+  bool operator<(const InstancesKey &k) const
+  {
+    if (hash_value != k.hash_value) {
+      return hash_value < k.hash_value;
+    }
+    if (object != k.object) {
+      return object < k.object;
+    }
+    if (ob_data != k.ob_data) {
+      return ob_data < k.ob_data;
+    }
+    if (flags != k.flags) {
+      return flags < k.flags;
+    }
+    if (preview_base_geometry != k.preview_base_geometry) {
+      return preview_base_geometry < k.preview_base_geometry;
+    }
+    if (preview_instance_index != k.preview_instance_index) {
+      return preview_instance_index < k.preview_instance_index;
+    }
+    return false;
+  }
+
+  bool operator==(const InstancesKey &k) const
+  {
+    if (hash_value != k.hash_value) {
+      return false;
+    }
+    if (object != k.object) {
+      return false;
+    }
+    if (ob_data != k.ob_data) {
+      return false;
+    }
+    if (flags != k.flags) {
+      return false;
+    }
+    if (preview_base_geometry != k.preview_base_geometry) {
+      return false;
+    }
+    if (preview_instance_index != k.preview_instance_index) {
+      return false;
+    }
+    return true;
+  }
+};
 
 void foreach_obref_in_scene(DRWContext &draw_ctx,
                             FunctionRef<bool(Object &)> should_draw_object_cb,
--- source/blender/draw/intern/draw_scene.hh
+++ source/blender/draw/intern/draw_scene.hh
@@ -4,110 +4,15 @@
 
 #pragma once
 
-#include "BLI_ghash.h"
-#include "BLI_map.hh"
-#include "BLI_math_matrix_types.hh"
-#include "BLI_sys_types.h"
-#include "BLI_utildefines.h"
-
 #include "BLI_function_ref.hh"
 
 struct Object;
-struct ID;
-struct LightLinking;
-namespace blender::bke {
-struct GeometrySet;
-}
 struct DRWContext;
 namespace blender::draw {
 class ObjectRef;
 }
 
 namespace blender::draw {
-
-enum class InstancesFlags : uint8_t {
-  IsNegativeScale = 1 << 0,
-};
-ENUM_OPERATORS(InstancesFlags, InstancesFlags::IsNegativeScale);
-
-struct InstancesKey {
-  uint64_t hash_value;
-
-  Object *object;
-  ID *ob_data;
-  const blender::bke::GeometrySet *preview_base_geometry;
-  int preview_instance_index;
-  InstancesFlags flags;
-
-  InstancesKey(Object *object,
-               ID *ob_data,
-               InstancesFlags flags,
-               const blender::bke::GeometrySet *preview_base_geometry,
-               int preview_instance_index)
-      : object(object),
-        ob_data(ob_data),
-        preview_base_geometry(preview_base_geometry),
-        preview_instance_index(preview_instance_index),
-        flags(flags)
-  {
-    hash_value = get_default_hash(object);
-    hash_value = get_default_hash(hash_value, ob_data);
-    hash_value = get_default_hash(hash_value, preview_base_geometry);
-    hash_value = get_default_hash(hash_value, preview_instance_index);
-    hash_value = get_default_hash(hash_value, uint8_t(flags));
-  }
-
-  uint64_t hash() const
-  {
-    return hash_value;
-  }
-
-  bool operator<(const InstancesKey &k) const
-  {
-    if (hash_value != k.hash_value) {
-      return hash_value < k.hash_value;
-    }
-    if (object != k.object) {
-      return object < k.object;
-    }
-    if (ob_data != k.ob_data) {
-      return ob_data < k.ob_data;
-    }
-    if (flags != k.flags) {
-      return flags < k.flags;
-    }
-    if (preview_base_geometry != k.preview_base_geometry) {
-      return preview_base_geometry < k.preview_base_geometry;
-    }
-    if (preview_instance_index != k.preview_instance_index) {
-      return preview_instance_index < k.preview_instance_index;
-    }
-    return false;
-  }
-
-  bool operator==(const InstancesKey &k) const
-  {
-    if (hash_value != k.hash_value) {
-      return false;
-    }
-    if (object != k.object) {
-      return false;
-    }
-    if (ob_data != k.ob_data) {
-      return false;
-    }
-    if (flags != k.flags) {
-      return false;
-    }
-    if (preview_base_geometry != k.preview_base_geometry) {
-      return false;
-    }
-    if (preview_instance_index != k.preview_instance_index) {
-      return false;
-    }
-    return true;
-  }
-};
 
 void foreach_obref_in_scene(DRWContext &draw_ctx,
                             FunctionRef<bool(Object &)> should_draw_object_cb,
