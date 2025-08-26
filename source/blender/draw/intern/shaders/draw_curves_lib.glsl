f3fd9aa0ba3a2aa112f9ff1f6e452df528cb1d1e
Clément Foucault <foucault.clem@gmail.com>
2025-08-26 15:51:09 +0200

    Fix typo

--- source/blender/draw/intern/shaders/draw_curves_lib.glsl
+++ source/blender/draw/intern/shaders/draw_curves_lib.glsl
@@ -47,7 +47,8 @@ Segment segment_get(uint vertex_id)
   segment.is_end_of_segment = is_cylinder && (segment.v_idx == drw_curves.vertex_per_segment - 1);
   if (is_cylinder && !segment.is_end_of_segment && (segment.id & 1u) == 0u) {
     /* The topology is not actually restarted and the winding order changes (because we skip an odd
-     * number of triangle). So we have to manually reverse the winding so that is stays consistent.
+     * number of triangles). So we have to manually reverse the winding so that is stays
+     * consistent.
      */
     segment.v_idx ^= 1u;
   }
