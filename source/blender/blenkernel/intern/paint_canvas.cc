40883581e5a2f2578272b2d04c04835a1c709bf8
Hans Goudey <hans@blender.org>
2025-08-26 22:16:28 -0400

    BKE_paint_canvas_key_get

--- source/blender/blenkernel/intern/paint_canvas.cc
+++ source/blender/blenkernel/intern/paint_canvas.cc
@@ -9,7 +9,7 @@
 #include "DNA_mesh_types.h"
 #include "DNA_scene_types.h"
 
-#include "BKE_customdata.hh"
+#include "BKE_attribute.hh"
 #include "BKE_image.hh"
 #include "BKE_material.hh"
 #include "BKE_paint.hh"
@@ -71,20 +71,39 @@ bool BKE_paint_canvas_image_get(PaintModeSettings *settings,
   return *r_image != nullptr;
 }
 
-int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Object *ob)
+static bool has_uv_map_attribute(const Mesh &mesh, const blender::StringRef name)
+{
+  using namespace blender;
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  const std::optional<bke::AttributeMetaData> meta_data = attributes.lookup_meta_data(name);
+  if (!meta_data) {
+    return false;
+  }
+  if (meta_data->data_type != bke::AttrType::Float2 ||
+      meta_data->domain != bke::AttrDomain::Corner)
+  {
+    return false;
+  }
+  return true;
+}
+
+std::optional<blender::StringRef> BKE_paint_canvas_uvmap_name_get(
+    const PaintModeSettings *settings, Object *ob)
 {
   switch (settings->canvas_source) {
     case PAINT_CANVAS_SOURCE_COLOR_ATTRIBUTE:
-      return -1;
+      return std::nullopt;
     case PAINT_CANVAS_SOURCE_IMAGE: {
       /* Use active uv map of the object. */
       if (ob->type != OB_MESH) {
-        return -1;
+        return std::nullopt;
       }
 
-      // TODO_MESH_ATTR
       const Mesh *mesh = static_cast<Mesh *>(ob->data);
-      return CustomData_get_active_layer_index(&mesh->corner_data, CD_PROP_FLOAT2);
+      if (!has_uv_map_attribute(*mesh, mesh->active_uv_map_attribute)) {
+        return std::nullopt;
+      }
+      return mesh->active_uv_map_attribute;
     }
     case PAINT_CANVAS_SOURCE_MATERIAL: {
       /* Use uv map of the canvas. */
@@ -94,25 +113,27 @@ int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Ob
       }
 
       if (ob->type != OB_MESH) {
-        return -1;
+        return std::nullopt;
       }
 
       if (slot->uvname == nullptr) {
-        return -1;
+        return std::nullopt;
       }
 
       const Mesh *mesh = static_cast<Mesh *>(ob->data);
-      return CustomData_get_named_layer_index(&mesh->corner_data, CD_PROP_FLOAT2, slot->uvname);
+      if (!has_uv_map_attribute(*mesh, slot->uvname)) {
+        return std::nullopt;
+      }
+      return slot->uvname;
     }
   }
-  return -1;
+  return std::nullopt;
 }
 
 char *BKE_paint_canvas_key_get(PaintModeSettings *settings, Object *ob)
 {
   std::stringstream ss;
-  int active_uv_map_layer_index = BKE_paint_canvas_uvmap_layer_index_get(settings, ob);
-  ss << "UV_MAP:" << active_uv_map_layer_index;
+  ss << "UV_MAP:" << BKE_paint_canvas_uvmap_name_get(settings, ob).value_or("");
 
   Image *image;
   ImageUser *image_user;
