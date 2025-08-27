05cd8441a8093af22838676c4fd6a7b8e5965ccd
Sybren A. Stüvel <sybren@blender.org>
2025-08-27 12:27:28 +0200

    Update comment

--- source/blender/animrig/intern/versioning.cc
+++ source/blender/animrig/intern/versioning.cc
@@ -241,10 +241,10 @@ void convert_legacy_action_assignments(Main &bmain, ReportList *reports)
   };
 
   /* Note that the code below does not remove the `action_assignment_needs_slot` tag. One ID can
-   * use multiple Actions (via NLA, Action constraints, etc.); if one of those Action is an ancient
-   * one from before 2.50 (just to name one example case) this ID may needs to be re-visited
-   * after those were versioned. Rather than trying to figure out if re-visiting is necessary, this
-   * function is safe to call multiple times, and all that's lost is a little bit of CPU time. */
+   * use multiple Actions (via NLA, Action constraints, etc.); if one of those Action is a legacy
+   * one from a linked datablock, this ID may needs to be re-visited after the library file was
+   * versioned. Rather than trying to figure out if re-visiting is necessary, this function is safe
+   * to call multiple times, and all that's lost is a little bit of CPU time. */
 
   ID *id;
   FOREACH_MAIN_ID_BEGIN (&bmain, id) {
