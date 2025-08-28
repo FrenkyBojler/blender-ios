d4621b8dddc7bc23d82b87c9a83f26791867f91d
Guillermo Venegas <guillermovcra@gmail.com>
2025-08-28 12:27:29 -0600

    init

--- source/blender/editors/interface/interface_layout.cc
+++ source/blender/editors/interface/interface_layout.cc
@@ -5289,9 +5289,7 @@ static void ui_item_align(uiLayout *litem, short nr)
     uiItem *item = *riter;
     if (item->type() == uiItemType::Button) {
       uiButtonItem *bitem = static_cast<uiButtonItem *>(item);
-#ifndef USE_UIBUT_SPATIAL_ALIGN
       if (ui_but_can_align(bitem->but))
-#endif
       {
         if (!bitem->but->alignnr) {
           bitem->but->alignnr = nr;
