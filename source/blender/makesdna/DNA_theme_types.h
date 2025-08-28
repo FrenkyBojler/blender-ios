4ad867809e69f14c1dd263bb21a75fbda6832385
Nika Kutsniashvili <nickberckley@gmail.com>
2025-08-28 22:25:34 +0400

    Remove `active_spline` theme property

--- source/blender/makesdna/DNA_theme_types.h
+++ source/blender/makesdna/DNA_theme_types.h
@@ -321,7 +321,8 @@ typedef struct ThemeSpace {
   unsigned char time_scrub_background[4];
 
   unsigned char nurb_uline[4], nurb_vline[4];
-  unsigned char act_spline[4], nurb_sel_uline[4], nurb_sel_vline[4];
+  unsigned char nurb_sel_uline[4], nurb_sel_vline[4];
+  char _pad10[4];
 
   /** Dope-sheet. */
   unsigned char ds_ipoline[4];
