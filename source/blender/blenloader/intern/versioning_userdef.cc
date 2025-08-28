a159cc93554059b34b8058091aab9c5d91ebcfa5
Nika Kutsniashvili <nickberckley@gmail.com>
2025-08-28 21:11:21 +0400

    Bump versioning

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -394,7 +394,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     btheme->common.curves.handle_vertex_size = U_theme_default.common.curves.handle_vertex_size;
   }
 
-  if (!USER_VERSION_ATLEAST(500, 100)) {
+  if (!USER_VERSION_ATLEAST(500, 68)) {
     FROM_DEFAULT_V4_UCHAR(space_clip.track_selected);
   }
 
