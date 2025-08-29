e92d71ac989e73f3f3bece6806083a600300728f
Nika Kutsniashvili <nickberckley@gmail.com>
2025-08-30 01:37:41 +0400

    Padding

--- source/blender/makesdna/DNA_theme_types.h
+++ source/blender/makesdna/DNA_theme_types.h
@@ -330,7 +330,7 @@ typedef struct ThemeSpace {
 
   unsigned char nurb_uline[4], nurb_vline[4];
   unsigned char nurb_sel_uline[4], nurb_sel_vline[4];
-  char _pad10[4];
+  char _pad7[4];
 
   /** Dope-sheet. */
   unsigned char ds_ipoline[4];
