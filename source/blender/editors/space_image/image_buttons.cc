blender/blender

commit 7d7562e849735a1d5837f31b2ff059aba2d3ecbc

Color Management: Support for saving wide gamut images
* Bundled ICC profiles for display spaces supported by Blender, and
  embed them in the image file when saving.
* Verified to work for PNG, TIFF, JPEG and WebP, but not all file
  formats support this.
* No ICC profile is written for sRGB currently. It would be a matter
  of adding an icc file, however this may be a breaking change for
some use cases.
* Fix save as render of EXR files not properly changing the image
  colorspace to match.

Uses CC0 licensed ICC files from the Compact ICC Profiles project.

This does not include support for saving HDR images. While there exist
ICC profiles for PQ, they are not well supported and the preferred method
for HDR is to write CICP tags. However OpenImageIO support for this is
still under development.

Ref #144911

Pull Request: https://projects.blender.org/blender/blender/pulls/144565

source/blender/editors/space_image/image_buttons.cc
@@ -1066,6 +1066,7 @@ void uiTemplateImageSettings(uiLayout *layout,
 
   /* Override color management */
   if (color_management) {
+    col->separator_spacer();
 
     if (uiLayout *panel = col->panel(C,
                                      panel_idname ? panel_idname : "settings_color_management",

release/datafiles/colormanagement/icc/g24_rec2020_display.icc
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b03c9218e82ab12fc4fcac7f5a0fb10ff53ebc7ad8387ae68a6d8cf5d4dfbaa3
+size 464

release/datafiles/colormanagement/icc/g24_rec709_display.icc
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d10744483b3448daceacc880566b004cd90aa0ca535f651c8c88ba61646c965
+size 596

release/datafiles/colormanagement/icc/g26_xyzd65_display.icc
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58ac463ea778e9b70692971dd4ba9071363a03aba77043894162b1ad6c051da7
+size 464

release/datafiles/colormanagement/icc/srgb_p3d65_display.icc
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb51de38e482ee974c0c76b9689e16aad04bad16e226fed2f30c842d15ff3a3d
+size 480

source/blender/blenkernel/intern/image_save.cc
@@ -13,6 +13,7 @@
 #include "BLI_index_range.hh"
 #include "BLI_listbase.h"
 #include "BLI_path_utils.hh"
+#include "BLI_string_ref.hh"
 #include "BLI_string_utf8.h"
 #include "BLI_task.hh"
 #include "BLI_vector.hh"
@@ -279,6 +280,24 @@ static void image_save_post(ReportList *reports,
     {
       BKE_color_managed_colorspace_settings_copy(&ima->colorspace_settings,
                                                  &opts->im_format.linear_colorspace_settings);
+      *r_colorspace_changed = true;
+    }
+  }
+  else if (opts->save_as_render) {
+    /* Set the display colorspace that we converted to. */
+    const ColorSpace *colorspace = IMB_colormangement_display_get_color_space(
+        &opts->im_format.display_settings);
+    if (colorspace) {
+      blender::StringRefNull colorspace_name = IMB_colormanagement_colorspace_get_name(colorspace);
+      if (colorspace_name != ima->colorspace_settings.name) {
+        STRNCPY(ima->colorspace_settings.name, colorspace_name.c_str());
+        *r_colorspace_changed = true;
+      }
+    }
+
+    /* View transform is now baked in, so don't apply it a second time for viewing. */
+    if (ima->flag & IMA_VIEW_AS_RENDER) {
+      ima->flag &= ~IMA_VIEW_AS_RENDER;
       *r_colorspace_changed = true;
     }
   }

source/blender/imbuf/IMB_colormanagement.hh
@@ -9,6 +9,7 @@
  */
 
 #include "BLI_compiler_compat.h"
+#include "BLI_vector.hh"
 
 #include "BLI_math_matrix_types.hh"
 
@@ -80,6 +81,8 @@ bool IMB_colormanagement_space_is_srgb(const ColorSpace *colorspace);
 bool IMB_colormanagement_space_name_is_data(const char *name);
 bool IMB_colormanagement_space_name_is_scene_linear(const char *name);
 bool IMB_colormanagement_space_name_is_srgb(const char *name);
+
+blender::Vector<char> IMB_colormanagement_space_icc_profile(const ColorSpace *colorspace);
 
 BLI_INLINE void IMB_colormanagement_get_luminance_coefficients(float r_rgb[3]);
 

source/blender/imbuf/intern/colormanagement.cc
@@ -29,6 +29,7 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "BLI_fileops.hh"
 #include "BLI_listbase.h"
 #include "BLI_math_color.h"
 #include "BLI_math_color.hh"
@@ -1288,6 +1289,58 @@ const char *IMB_colormanagement_srgb_colorspace_name_get()
 
   /* Fallback if nothing can be found. */
   return global_role_default_byte;
+}
+
+blender::Vector<char> IMB_colormanagement_space_icc_profile(const ColorSpace *colorspace)
+{
+  /* ICC profiles shipped with Blender are named after the OpenColorIO interop ID. */
+  blender::Vector<char> icc_profile;
+
+  const StringRefNull interop_id = colorspace->interop_id();
+  if (interop_id.is_empty()) {
+    return icc_profile;
+  }
+
+  const std::optional<std::string> dir = BKE_appdir_folder_id(BLENDER_DATAFILES,
+                                                              "colormanagement");
+  if (!dir.has_value()) {
+    return icc_profile;
+  }
+
+  char icc_filename[FILE_MAX];
+  STRNCPY(icc_filename, (interop_id + ".icc").c_str());
+  BLI_path_make_safe_filename(icc_filename);
+
+  char icc_filepath[FILE_MAX];
+  BLI_path_join(icc_filepath, sizeof(icc_filepath), dir->c_str(), "icc", icc_filename);
+
+  blender::fstream f(icc_filepath, std::ios::binary | std::ios::in | std::ios::ate);
+  if (!f.is_open()) {
+    /* If we can't find a scene referred filename, try display referred. */
+    blender::StringRef icc_filepath_ref = icc_filepath;
+    if (icc_filepath_ref.endswith("_scene.icc")) {
+      std::string icc_filepath_display = icc_filepath_ref.drop_suffix(strlen("_scene.icc")) +
+                                         "_display.icc";
+      f.open(icc_filepath_display, std::ios::binary | std::ios::in | std::ios::ate);
+    }
+
+    if (!f.is_open()) {
+      return icc_profile;
+    }
+  }
+
+  std::streamsize size = f.tellg();
+  if (size <= 0) {
+    return icc_profile;
+  }
+  icc_profile.resize(size);
+
+  f.seekg(0, std::ios::beg);
+  if (!f.read(icc_profile.data(), icc_profile.size())) {
+    icc_profile.clear();
+  }
+
+  return icc_profile;
 }
 
 blender::float3x3 IMB_colormanagement_get_xyz_to_scene_linear()

FrenkyBojler/blender-ios

commit 83b8c9423910d7c53a7694a33c1d36eee0d531f9

Fix #145391: File Output node: "Layers" uilist button is misaligned
Caused by 7d7562e84973

Above commit added a `separator_spacer` (for no obvious reason?)
This PR removes it again.

source/blender/editors/space_image/image_buttons.cc
@@ -1066,8 +1066,6 @@ void uiTemplateImageSettings(uiLayout *layout,
 
   /* Override color management */
   if (color_management) {
-    col->separator_spacer();
-
     if (uiLayout *panel = col->panel(C,
                                      panel_idname ? panel_idname : "settings_color_management",
                                      true,