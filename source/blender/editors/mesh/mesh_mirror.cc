df07860233abcaa85ca20a9f77609f5b4309aa7f
tariqsulley <tariqsulley3c@gmail.com>
2025-08-30 23:39:34 +0000

    initial mirroring implementation for box select

--- source/blender/editors/mesh/mesh_mirror.cc
+++ source/blender/editors/mesh/mesh_mirror.cc
@@ -377,85 +377,123 @@ std::optional<EditMeshSymmetryHelper> EditMeshSymmetryHelper::create_if_needed(O
 EditMeshSymmetryHelper::EditMeshSymmetryHelper(Object *ob, uchar htype)
     : em_(BKE_editmesh_from_object(ob)), mesh_(static_cast<Mesh *>(ob->data)), htype_(htype)
 {
-  BMesh *bmesh = em_->bm;
   use_topology_mirror_ = (mesh_->editflag & ME_EDIT_MIRROR_TOPO) != 0;
-
-  blender::Set<BMVert *> processed_verts;
-  blender::Set<BMEdge *> processed_edges;
-  blender::Set<BMFace *> processed_faces;
-
-  BMIter iter;
 
   for (int axis = 0; axis < 3; axis++) {
     if (mesh_->symmetry & (ME_SYMMETRY_X << axis)) {
-      EDBM_verts_mirror_cache_begin(em_,
-                                    axis,
-                                    (htype_ & BM_VERT) != 0,
-                                    (htype_ & BM_EDGE) != 0,
-                                    (htype_ & BM_FACE) != 0,
-                                    use_topology_mirror_);
-
-      if (htype_ & BM_VERT) {
-        BMVert *v_curr;
-        BM_ITER_MESH (v_curr, &iter, bmesh, BM_VERTS_OF_MESH) {
-          if (processed_verts.contains(v_curr)) {
-            continue;
-          }
-          BMVert *v_mirr = EDBM_verts_mirror_get(em_, v_curr);
-          if (v_mirr && v_mirr != v_curr) {
-            BMVert *v_mirr_check = EDBM_verts_mirror_get(em_, v_mirr);
-            if (v_mirr_check == v_curr) {
-              vert_to_mirror_map_.lookup_or_add(v_curr, {}).append(v_mirr);
-              vert_to_mirror_map_.lookup_or_add(v_mirr, {}).append(v_curr);
-              processed_verts.add(v_curr);
-              processed_verts.add(v_mirr);
-            }
-          }
-        }
-      }
-
-      if (htype_ & BM_EDGE) {
-        BMEdge *e_curr;
-        BM_ITER_MESH (e_curr, &iter, bmesh, BM_EDGES_OF_MESH) {
-          if (processed_edges.contains(e_curr)) {
-            continue;
-          }
-          BMEdge *e_mirr = EDBM_verts_mirror_get_edge(em_, e_curr);
-          if (e_mirr && e_mirr != e_curr) {
-            BMEdge *e_mirr_check = EDBM_verts_mirror_get_edge(em_, e_mirr);
-            if (e_mirr_check == e_curr) {
-              edge_to_mirror_map_.lookup_or_add(e_curr, {}).append(e_mirr);
-              edge_to_mirror_map_.lookup_or_add(e_mirr, {}).append(e_curr);
-              processed_edges.add(e_curr);
-              processed_edges.add(e_mirr);
-            }
-          }
-        }
-      }
-
-      if (htype_ & BM_FACE) {
-        BMFace *f_curr;
-        BM_ITER_MESH (f_curr, &iter, bmesh, BM_FACES_OF_MESH) {
-          if (processed_faces.contains(f_curr)) {
-            continue;
-          }
-          BMFace *f_mirr = EDBM_verts_mirror_get_face(em_, f_curr);
-          if (f_mirr && f_mirr != f_curr) {
-            BMFace *f_mirr_check = EDBM_verts_mirror_get_face(em_, f_mirr);
-            if (f_mirr_check == f_curr) {
-              face_to_mirror_map_.lookup_or_add(f_curr, {}).append(f_mirr);
-              face_to_mirror_map_.lookup_or_add(f_mirr, {}).append(f_curr);
-              processed_faces.add(f_curr);
-              processed_faces.add(f_mirr);
-            }
-          }
-        }
-      }
-
-      EDBM_verts_mirror_cache_end(em_);
+      build_mirror_maps_for_axis(axis);
     }
   }
 }
+
+void EditMeshSymmetryHelper::build_mirror_maps_for_axis(int axis)
+{
+  BMesh *bmesh = em_->bm;
+  BMIter iter;
+
+  /*This function initializes mirror cache for the given axis.
+   * Despite the name, the vertex cache is also used by
+   * EDBM_verts_mirror_get_edge() and EDBM_verts_mirror_get_face(),
+   * so it must always be built even if only edges/faces are needed. */
+  EDBM_verts_mirror_cache_begin(em_, axis, true, true, true, use_topology_mirror_);
+
+  if (htype_ & BM_VERT) {
+    BMVert *v_curr;
+    BM_ITER_MESH (v_curr, &iter, bmesh, BM_VERTS_OF_MESH) {
+      BMVert *v_mirr = EDBM_verts_mirror_get(em_, v_curr);
+      if (v_mirr && v_mirr != v_curr) {
+        BMVert *v_mirr_check = EDBM_verts_mirror_get(em_, v_mirr);
+        if (v_mirr_check == v_curr) {
+          add_mirror_relationship((BMElem *)v_curr, (BMElem *)v_mirr);
+        }
+      }
+    }
+  }
+  if (htype_ & BM_EDGE) {
+    BMEdge *e_curr;
+    BM_ITER_MESH (e_curr, &iter, bmesh, BM_EDGES_OF_MESH) {
+      BMEdge *e_mirr = EDBM_verts_mirror_get_edge(em_, e_curr);
+      if (e_mirr && e_mirr != e_curr) {
+        BMEdge *e_mirr_check = EDBM_verts_mirror_get_edge(em_, e_mirr);
+        if (e_mirr_check == e_curr) {
+          add_mirror_relationship((BMElem *)e_curr, (BMElem *)e_mirr);
+        }
+      }
+    }
+  }
+  if (htype_ & BM_FACE) {
+    BMFace *f_curr;
+    BM_ITER_MESH (f_curr, &iter, bmesh, BM_FACES_OF_MESH) {
+      BMFace *f_mirr = EDBM_verts_mirror_get_face(em_, f_curr);
+      if (f_mirr && f_mirr != f_curr) {
+        BMFace *f_mirr_check = EDBM_verts_mirror_get_face(em_, f_mirr);
+        if (f_mirr_check == f_curr) {
+          add_mirror_relationship((BMElem *)f_curr, (BMElem *)f_mirr);
+        }
+      }
+    }
+  }
+  EDBM_verts_mirror_cache_end(em_);
+}
+
+void EditMeshSymmetryHelper::add_mirror_relationship(BMElem *elem1, BMElem *elem2)
+{
+  BLI_assert(elem1->head.htype == elem2->head.htype);
+
+  switch (elem1->head.htype) {
+    case BM_VERT: {
+      BMVert *v1 = (BMVert *)elem1;
+      BMVert *v2 = (BMVert *)elem2;
+
+      blender::Vector<BMVert *> &mirrors1 = vert_to_mirror_map_.lookup_or_add(v1, {});
+      blender::Vector<BMVert *> &mirrors2 = vert_to_mirror_map_.lookup_or_add(v2, {});
+
+      if (!mirrors1.contains(v2)) {
+        mirrors1.append(v2);
+      }
+      if (!mirrors2.contains(v1)) {
+        mirrors2.append(v1);
+      }
+      break;
+    }
+
+    case BM_EDGE: {
+      BMEdge *e1 = (BMEdge *)elem1;
+      BMEdge *e2 = (BMEdge *)elem2;
+
+      blender::Vector<BMEdge *> &mirrors1 = edge_to_mirror_map_.lookup_or_add(e1, {});
+      blender::Vector<BMEdge *> &mirrors2 = edge_to_mirror_map_.lookup_or_add(e2, {});
+
+      if (!mirrors1.contains(e2)) {
+        mirrors1.append(e2);
+      }
+      if (!mirrors2.contains(e1)) {
+        mirrors2.append(e1);
+      }
+      break;
+    }
+
+    case BM_FACE: {
+      BMFace *f1 = (BMFace *)elem1;
+      BMFace *f2 = (BMFace *)elem2;
+
+      blender::Vector<BMFace *> &mirrors1 = face_to_mirror_map_.lookup_or_add(f1, {});
+      blender::Vector<BMFace *> &mirrors2 = face_to_mirror_map_.lookup_or_add(f2, {});
+      if (!mirrors1.contains(f2)) {
+        mirrors1.append(f2);
+      }
+      if (!mirrors2.contains(f1)) {
+        mirrors2.append(f1);
+      }
+      break;
+    }
+
+    default:
+      BLI_assert_unreachable();
+      break;
+  }
+}
+
 void EditMeshSymmetryHelper::apply_on_mirror_verts(BMVert *v,
                                                    blender::FunctionRef<void(BMVert *)> op) const
 {
--- source/blender/editors/space_view3d/view3d_select.cc
+++ source/blender/editors/space_view3d/view3d_select.cc
@@ -4491,11 +4491,90 @@ static wmOperatorStatus view3d_box_select_exec(bContext *C, wmOperator *op)
         case OB_MESH:
           vc.em = BKE_editmesh_from_object(vc.obedit);
           changed = do_mesh_box_select(&vc, wm_userdata, &rect, sel_op);
+
+          if (changed) {
+            char symmetry_htype = 0;
+            if (vc.em->selectmode & SCE_SELECT_FACE) {
+              symmetry_htype |= BM_FACE;
+            }
+            if (vc.em->selectmode & SCE_SELECT_EDGE) {
+              symmetry_htype |= BM_EDGE;
+            }
+            if (vc.em->selectmode & SCE_SELECT_VERTEX) {
+              symmetry_htype |= BM_VERT;
+            }
+
+            if (std::optional<EditMeshSymmetryHelper> symmetry_helper =
+                    EditMeshSymmetryHelper::create_if_needed(vc.obedit, symmetry_htype))
+            {
+              BMesh *bm = vc.em->bm;
+              const char hflag = BM_ELEM_SELECT;
+              BMIter iter;
+              bool mirror_changed = false;
+
+              if (vc.em->selectmode & SCE_SELECT_FACE) {
+                blender::Vector<BMFace *> source_faces;
+                source_faces.reserve(bm->totfacesel);
+                BMFace *f;
+                BM_ITER_MESH (f, &iter, bm, BM_FACES_OF_MESH) {
+                  if (BM_elem_flag_test(f, hflag)) {
+                    source_faces.append(f);
+                  }
+                }
+                const int totfacesel_prev = bm->totfacesel;
+                for (BMFace *f_orig : source_faces) {
+                  symmetry_helper->set_hflag_on_mirror_faces(f_orig, hflag, true);
+                }
+                if (bm->totfacesel != totfacesel_prev) {
+                  mirror_changed = true;
+                }
+              }
+              if (vc.em->selectmode & SCE_SELECT_EDGE) {
+                blender::Vector<BMEdge *> source_edges;
+                source_edges.reserve(bm->totedgesel);
+                BMEdge *e;
+                BM_ITER_MESH (e, &iter, bm, BM_EDGES_OF_MESH) {
+                  if (BM_elem_flag_test(e, hflag)) {
+                    source_edges.append(e);
+                  }
+                }
+                const int totedgesel_prev = bm->totedgesel;
+                for (BMEdge *e_orig : source_edges) {
+                  symmetry_helper->set_hflag_on_mirror_edges(e_orig, hflag, true);
+                }
+                if (bm->totedgesel != totedgesel_prev) {
+                  mirror_changed = true;
+                }
+              }
+              if (vc.em->selectmode & SCE_SELECT_VERTEX) {
+                blender::Vector<BMVert *> source_verts;
+                source_verts.reserve(bm->totvertsel);
+                BMVert *v;
+                BM_ITER_MESH (v, &iter, bm, BM_VERTS_OF_MESH) {
+                  if (BM_elem_flag_test(v, hflag)) {
+                    source_verts.append(v);
+                  }
+                }
+                const int totvertsel_prev = bm->totvertsel;
+                for (BMVert *v_orig : source_verts) {
+                  symmetry_helper->set_hflag_on_mirror_verts(v_orig, hflag, true);
+                }
+                if (bm->totvertsel != totvertsel_prev) {
+                  mirror_changed = true;
+                }
+              }
+              if (mirror_changed) {
+                EDBM_selectmode_flush(vc.em);
+              }
+            }
+          }
+
           if (changed) {
             DEG_id_tag_update(static_cast<ID *>(vc.obedit->data), ID_RECALC_SELECT);
             WM_event_add_notifier(C, NC_GEOM | ND_SELECT, vc.obedit->data);
           }
           break;
+
         case OB_CURVES_LEGACY:
         case OB_SURF:
           changed = do_nurbs_box_select(&vc, &rect, sel_op);
