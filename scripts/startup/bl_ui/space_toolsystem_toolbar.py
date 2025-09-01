70cad220d3c3348c9c2835d62401207cc7d7bda8
Ramon Klauck <ramonklauck3@gmail.com>
2025-09-01 16:31:58 +0200

    rearrange blade tool menu

--- scripts/startup/bl_ui/space_toolsystem_toolbar.py
+++ scripts/startup/bl_ui/space_toolsystem_toolbar.py
@@ -3144,10 +3144,14 @@ class _defs_sequencer_generic:
     @ToolDef.from_fn
     def blade():
         def draw_settings(_context, layout, tool):
-            # props = tool.operator_properties("sequencer.split")
+            props = tool.operator_properties("sequencer.split")
+            row = layout.row()
+            row.prop(props, "type", expand=True)
+
+            layout.separator()
+
             props = tool.operator_properties("sequencer.box_cut")
             row = layout.row()
-            # row.prop(props, "type", expand=True)
             row.prop(props, "remove_gaps", expand=True)
         return dict(
             idname="builtin.blade",
