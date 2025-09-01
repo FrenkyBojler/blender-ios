bf98adda32aed6855a6f7b5c09745646413eb073
Jeroen Bakker <jeroen@blender.org>
2025-09-01 14:38:12 +0200

    Fix #144636: EEVEE could raytrace invalid samples
    Raytracing pipeline in EEVEE interpolates in time for better results.
    But depending on the interpolation could try to load a sample that isn't
    available. This PR fixies this for planar probes. Screen tracing doesn't
    seem to be effective (or the artifacts are not visible as the input
    radiance is always known).

--- source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_lib.glsl
@@ -150,7 +150,6 @@ METAL_ATTR ScreenTraceHitData raytrace_screen(RayTraceData rt_data,
   time = mix(prev_time, time, saturate(prev_delta / (prev_delta - delta)));
 
   ScreenTraceHitData result;
-  result.valid = hit;
   result.ss_hit_P = ssray.origin.xyz + ssray.direction.xyz * time;
   result.v_hit_P = drw_point_screen_to_view(result.ss_hit_P);
   /* Convert to world space ray time. */
@@ -217,13 +216,15 @@ ScreenTraceHitData raytrace_planar(RayTraceData rt_data,
   time = mix(prev_time, time, saturate(prev_delta / (prev_delta - delta)));
 
   ScreenTraceHitData result;
-  result.valid = hit;
   result.ss_hit_P = ssray.origin.xyz + ssray.direction.xyz * time;
+  /* Update the validity as v_hit_P can point to a not loaded sample. */
+  result.valid = hit && textureLod(planar_depth_tx, vec3(result.ss_hit_P.xy, planar.layer_id), 0.0f).r != 0.0;
 
   /* NOTE: v_hit_P is in planar reflected view space. */
   result.v_hit_P = project_point(planar.wininv, drw_screen_to_ndc(result.ss_hit_P));
   /* Convert to world space ray time. */
   result.time = length(result.v_hit_P - ray.origin) / length(ray.direction);
+
   return result;
 }
 
