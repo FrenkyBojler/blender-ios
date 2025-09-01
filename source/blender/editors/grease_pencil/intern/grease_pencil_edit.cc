a1c7f5f0624f0dabc04957a94abfa69012ff838a
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-09-01 07:58:16 -0700

    Fix: Grease Pencil: Debug assert when converting to `Catmull Rom`

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -4505,7 +4505,11 @@ static void convert_to_catmull_rom(bke::CurvesGeometry &curves,
   const IndexMask non_catmull_rom_curves_selection =
       curves.indices_for_curve_type(CURVE_TYPE_CATMULL_ROM, selection, memory)
           .complement(selection, memory);
-  BLI_assert(!non_catmull_rom_curves_selection.is_empty());
+
+  if (non_catmull_rom_curves_selection.is_empty()) {
+    return;
+  }
+
   curves = geometry::resample_to_evaluated(curves, non_catmull_rom_curves_selection);
 
   /* To avoid having too many control points, simplify the position attribute based on the
