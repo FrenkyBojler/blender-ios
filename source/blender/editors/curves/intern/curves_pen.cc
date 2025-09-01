9b39ae736b72a6ded3d11539d47e3cc437842388
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-09-01 08:42:18 -0700

    Fix: Merge error.

--- source/blender/editors/curves/intern/curves_pen.cc
+++ source/blender/editors/curves/intern/curves_pen.cc
@@ -455,16 +455,23 @@ static bool move_handles_in_curve(const PenToolOperation &ptd,
       return;
     }
 
+    const bool is_left = !right_selected[point_i];
     if (ptd.move_handle) {
-      const float2 pos_right = ptd.layer_to_screen(layer_to_object, handles_right[point_i]);
-      handles_right[point_i] = ptd.screen_to_layer(
-          layer_to_world, pos_right + offset, depth_point);
+      if (is_left) {
+        const float2 pos_left = ptd.layer_to_screen(layer_to_object, handles_left[point_i]);
+        handles_left[point_i] = ptd.screen_to_layer(
+            layer_to_world, pos_left + offset, depth_point);
+      }
+      else {
+        const float2 pos_right = ptd.layer_to_screen(layer_to_object, handles_right[point_i]);
+        handles_right[point_i] = ptd.screen_to_layer(
+            layer_to_world, pos_right + offset, depth_point);
+      }
       handle_types_left[point_i] = BEZIER_HANDLE_FREE;
       handle_types_right[point_i] = BEZIER_HANDLE_FREE;
       return;
     }
 
-    const bool is_left = !right_selected[point_i];
     const float2 center_point = ptd.layer_to_screen(layer_to_object, depth_point);
     offset = ptd.mouse_co - ptd.center_of_mass_co;
 
