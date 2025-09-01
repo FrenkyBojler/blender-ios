35ffec2d03646dfee845c5f5c535132d7ec51c4b
Jeroen Bakker <jeroen@blender.org>
2025-09-01 15:36:28 +0200

    Use float3 + formatting

--- source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_lib.glsl
@@ -155,7 +155,8 @@ METAL_ATTR ScreenTraceHitData raytrace_screen(RayTraceData rt_data,
   /* Convert to world space ray time. */
   result.time = length(result.v_hit_P - ray.origin) / length(ray.direction);
   /* Update the validity as ss_hit_P can point to a background sample. */
-  result.valid = hit && (textureLod(hiz_tx, result.ss_hit_P.xy * hiz_data.uv_scale, 0.0f).r != 1.0f);
+  result.valid = hit &&
+                 (textureLod(hiz_tx, result.ss_hit_P.xy * hiz_data.uv_scale, 0.0f).r != 1.0f);
 
 #ifdef METAL_AMD_RAYTRACE_WORKAROUND
   /* Check failed ray flag to discard bad hits. */
@@ -220,7 +221,9 @@ ScreenTraceHitData raytrace_planar(RayTraceData rt_data,
   ScreenTraceHitData result;
   result.ss_hit_P = ssray.origin.xyz + ssray.direction.xyz * time;
   /* Update the validity as ss_hit_P can point to a not loaded sample. */
-  result.valid = hit && textureLod(planar_depth_tx, vec3(result.ss_hit_P.xy, planar.layer_id), 0.0f).r != 0.0;
+  result.valid =
+      hit &&
+      textureLod(planar_depth_tx, float3(result.ss_hit_P.xy, planar.layer_id), 0.0f).r != 0.0;
 
   /* NOTE: v_hit_P is in planar reflected view space. */
   result.v_hit_P = project_point(planar.wininv, drw_screen_to_ndc(result.ss_hit_P));
