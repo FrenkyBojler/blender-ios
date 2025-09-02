dc95d852def05e6c6f53e11d44940f21dbd2f6b8
Kace <lakacey03@gmail.com>
2025-09-02 16:39:34 -0400

    adjust comment

--- source/blender/sequencer/intern/strip_retiming.cc
+++ source/blender/sequencer/intern/strip_retiming.cc
@@ -1051,7 +1051,7 @@ void retiming_sound_animation_data_set(const Scene *scene, const Strip *strip)
 
   RetimingRangeData retiming_data = strip_retiming_range_data_get(scene, strip);
 
-  /* No need to apply the time-stretch modifier if all the retiming range speed are 1, as the
+  /* No need to apply the time-stretch effect if all the retiming range speed are 1, as the
    * effect itself is still expensive while the audio is playing and want to avoid having to use it
    * if we can. */
   bool correct_pitch = (strip->flag & SEQ_AUDIO_PITCH_CORRECTION) &&
