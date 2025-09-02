84df15415259f9fa40d5d1a6009e5d2337f34734
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-09-02 09:57:02 +0200

    reformat

--- scripts/startup/bl_ui/space_sequencer.py
+++ scripts/startup/bl_ui/space_sequencer.py
@@ -193,7 +193,7 @@ class SEQUENCER_HT_header(Header):
             row.prop(tool_settings, "use_snap_sequencer", text="")
             sub = row.row(align=True)
             sub.popover(panel="SEQUENCER_PT_snapping")
-            
+
         layout.separator_spacer()
 
         if st.view_type in {'PREVIEW', 'SEQUENCER_PREVIEW'}:
