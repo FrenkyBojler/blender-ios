487eb4544c67d140ae864949ebd09c895ce94f0f
salipour <sahar.kashi@amd.com>
2025-09-02 07:51:45 -0700

    Reverting HIP version check back to exact match

--- build_files/build_environment/cmake/hiprt.cmake
+++ build_files/build_environment/cmake/hiprt.cmake
@@ -7,7 +7,7 @@ if(NOT HIP_FOUND)
   return()
 endif()
 
-if(HIP_VERSION LESS "${RELEASE_HIP_VERSION}.*")
+if(NOT HIP_VERSION MATCHES "${RELEASE_HIP_VERSION}.*")
   message(STATUS "Wrong HIP compiler version (expected ${RELEASE_HIP_VERSION}), skipping HIPRT build")
   return()
 endif()
