4968f244d981d0e53847983c29b9e85038d2b00b
Harley Acheson <harley.acheson@gmail.com>
2025-09-03 10:06:53 -0700

    UI: Correction & Improvements to File Browser Font Tooltips
    The tooltips for fonts in the file browser, while in list view, now
    shows either no preview or a blank white block (depending if you viewed
    them with thumbnails in the current session. This PR corrects that to
    always show a preview. It also shows this preview while in thumbnail
    view.

--- source/blender/editors/space_file/file_draw.cc
+++ source/blender/editors/space_file/file_draw.cc
@@ -279,6 +279,11 @@ static void file_draw_tooltip_custom_func(bContext & /*C*/,
         }
       }
     }
+    else if (file->typeflag & FILE_TYPE_FTFONT) {
+      float color[4];
+      UI_GetThemeColor4fv(TH_TEXT, color);
+      thumb = IMB_font_preview(full_path, 192 * UI_SCALE_FAC, color);
+    }
 
     char date_str[FILELIST_DIRENTRY_DATE_LEN], time_str[FILELIST_DIRENTRY_TIME_LEN];
     bool is_today, is_yesterday;
@@ -321,7 +326,20 @@ static void file_draw_tooltip_custom_func(bContext & /*C*/,
     }
   }
 
-  if (thumb && params->display != FILE_IMGDISPLAY) {
+  if (thumb && file->typeflag & FILE_TYPE_FTFONT) {
+    UI_tooltip_text_field_add(tip, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
+    UI_tooltip_text_field_add(tip, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
+    uiTooltipImage image_data;
+    float scale = (192.0f * UI_SCALE_FAC) / float(std::max(thumb->x, thumb->y));
+    image_data.ibuf = thumb;
+    image_data.width = short(float(thumb->x) * scale);
+    image_data.height = short(float(thumb->y) * scale);
+    image_data.border = true;
+    image_data.background = uiTooltipImageBackground::None;
+    image_data.premultiplied = false;
+    UI_tooltip_image_field_add(tip, image_data);
+  }
+  else if (thumb && params->display != FILE_IMGDISPLAY) {
     UI_tooltip_text_field_add(tip, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
     UI_tooltip_text_field_add(tip, {}, {}, UI_TIP_STYLE_SPACER, UI_TIP_LC_NORMAL);
 
