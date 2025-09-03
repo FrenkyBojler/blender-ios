66fc6067fc46398487daeb5f644f2b58b1d83a9a
quackarooni <alfonsomartzii@gmail.com>
2025-09-03 15:39:19 +0800

    Node Wrangler: Use socket icons for Lazy Connect menu
    Replace the icons for the "From Socket" and "To Socket" pop-up menus from `RADIOBUT_OFF` and `FORWARD`, to each socket type's corresponding icon.
    
    This makes it easier to pick out sockets in longer lists, as they could now be distinguished by their color.

--- scripts/addons_core/node_wrangler/interface.py
+++ scripts/addons_core/node_wrangler/interface.py
@@ -13,6 +13,18 @@ from .utils.constants import blend_types, geo_combine_operations, operations
 from .utils.nodes import get_nodes_links, NWBaseMenu
 
 
+def socket_to_icon(socket):
+    socket_type = socket.type
+
+    if socket_type == "CUSTOM":
+        return "RADIOBUT_OFF"
+
+    if socket_type == "VALUE":
+        socket_type = "FLOAT"
+
+    return "NODE_SOCKET_" + socket_type
+
+
 def drawlayout(context, layout, mode='non-panel'):
     tree_type = context.space_data.tree_type
 
@@ -177,7 +189,7 @@ class NWConnectionListOutputs(Menu, NWBaseMenu):
                     operators.NWCallInputsMenu.bl_idname,
                     text=output.name,
                     text_ctxt=i18n_contexts.default,
-                    icon="RADIOBUT_OFF",
+                    icon=socket_to_icon(output),
                 ).from_socket = index
 
 
@@ -200,7 +212,7 @@ class NWConnectionListInputs(Menu, NWBaseMenu):
                 op = layout.operator(
                     operators.NWMakeLink.bl_idname, text=input.name,
                     text_ctxt=i18n_contexts.default,
-                    icon="FORWARD",
+                    icon=socket_to_icon(input),
                 )
                 op.from_socket = context.scene.NWSourceSocket
                 op.to_socket = index
