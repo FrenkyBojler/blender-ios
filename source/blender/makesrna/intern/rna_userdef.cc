0b322bfbeec8bef96b91f8d4c6b76887d3f12499
Falk David <falk@blender.org>
2025-09-04 12:28:47 +0200

    Formatting

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -2006,7 +2006,8 @@ static void rna_def_userdef_theme_ui(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "panel_active", PROP_FLOAT, PROP_COLOR_GAMMA);
   RNA_def_property_array(prop, 4);
-  RNA_def_property_ui_text(prop, "Active Panel Outline", "Color of the outline of top-level panels that are active");
+  RNA_def_property_ui_text(
+      prop, "Active Panel Outline", "Color of the outline of top-level panels that are active");
   RNA_def_property_update(prop, 0, "rna_userdef_theme_update");
 
   /* Transparent Grid */
