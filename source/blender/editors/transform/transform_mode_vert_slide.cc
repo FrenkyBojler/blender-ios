blender/blender

commit 0bcab8913fc8826575d6647008ae85b57114ecd2

UI: Vertex Slide Status Display
While using Vertex Slide show only changing values in the area header and
only keymap entries on the Status Bar

Pull Request: https://projects.blender.org/blender/blender/pulls/140505

source/blender/editors/transform/transform_mode_vert_slide.cc
@@ -143,7 +143,7 @@ struct VertSlideData {
 
 struct VertSlideParams {
   float perc;
-
+  wmOperator *op;
   bool use_even;
   bool flipped;
 };
@@ -489,6 +489,9 @@ static void applyVertSlide(TransInfo *t)
   const bool use_even = slp->use_even;
   const bool is_clamp = !(t->flag & T_ALT_TRANSFORM);
   const bool is_constrained = !(is_clamp == false || hasNumInput(&t->num));
+  const bool is_precision = t->modifiers & MOD_PRECISION;
+  const bool is_snap = t->modifiers & MOD_SNAP;
+  const bool is_snap_invert = t->modifiers & MOD_SNAP_INVERT;
 
   final = t->values[0] + t->values_modal_offset[0];
 
@@ -516,14 +519,6 @@ static void applyVertSlide(TransInfo *t)
   else {
     ofs += BLI_snprintf_rlen(str + ofs, sizeof(str) - ofs, "%.4f ", final);
   }
-  ofs += BLI_snprintf_rlen(
-      str + ofs, sizeof(str) - ofs, IFACE_("(E)ven: %s, "), WM_bool_as_string(use_even));
-  if (use_even) {
-    ofs += BLI_snprintf_rlen(
-        str + ofs, sizeof(str) - ofs, IFACE_("(F)lipped: %s, "), WM_bool_as_string(flipped));
-  }
-  ofs += BLI_snprintf_rlen(
-      str + ofs, sizeof(str) - ofs, IFACE_("Alt or (C)lamp: %s"), WM_bool_as_string(is_clamp));
   /* Done with header string. */
 
   /* Do stuff here. */
@@ -532,6 +527,27 @@ static void applyVertSlide(TransInfo *t)
   recalc_data(t);
 
   ED_area_status_text(t->area, str);
+
+  wmOperator *op = slp->op;
+  if (!op) {
+    return;
+  }
+
+  WorkspaceStatus status(t->context);
+  status.opmodal(IFACE_("Confirm"), op->type, TFM_MODAL_CONFIRM);
+  status.opmodal(IFACE_("Cancel"), op->type, TFM_MODAL_CONFIRM);
+  status.opmodal(IFACE_("Snap"), op->type, TFM_MODAL_SNAP_TOGGLE, is_snap);
+  status.opmodal(IFACE_("Snap Invert"), op->type, TFM_MODAL_SNAP_INV_ON, is_snap_invert);
+  status.opmodal(IFACE_("Set Snap Base"), op->type, TFM_MODAL_EDIT_SNAP_SOURCE_ON);
+  status.opmodal(IFACE_("Move"), op->type, TFM_MODAL_TRANSLATE);
+  status.opmodal(IFACE_("Rotate"), op->type, TFM_MODAL_ROTATE);
+  status.opmodal(IFACE_("Resize"), op->type, TFM_MODAL_RESIZE);
+  status.opmodal(IFACE_("Precision Mode"), op->type, TFM_MODAL_PRECISION, is_precision);
+  status.item_bool(IFACE_("Clamp"), is_clamp, ICON_EVENT_C, ICON_EVENT_ALT);
+  status.item_bool(IFACE_("Even"), use_even, ICON_EVENT_E);
+  if (use_even) {
+    status.item_bool(IFACE_("Flipped"), flipped, ICON_EVENT_F);
+  }
 }
 
 static void vert_slide_transform_matrix_fn(TransInfo *t, float mat_xform[4][4])
@@ -563,7 +579,8 @@ static void vert_slide_transform_matrix_fn(TransInfo *t, float mat_xform[4][4])
   add_v3_v3(mat_xform[3], delta);
 }
 
-static void initVertSlide_ex(TransInfo *t, bool use_even, bool flipped, bool use_clamp)
+static void initVertSlide_ex(
+    TransInfo *t, wmOperator *op, bool use_even, bool flipped, bool use_clamp)
 {
 
   t->mode = TFM_VERT_SLIDE;
@@ -573,6 +590,7 @@ static void initVertSlide_ex(TransInfo *t, bool use_even, bool flipped, bool use
     slp->use_even = use_even;
     slp->flipped = flipped;
     slp->perc = 0.0f;
+    slp->op = op;
 
     if (!use_clamp) {
       t->flag |= T_ALT_TRANSFORM;
@@ -620,11 +638,15 @@ static void initVertSlide(TransInfo *t, wmOperator *op)
   bool flipped = false;
   bool use_clamp = true;
   if (op) {
-    use_even = RNA_boolean_get(op->ptr, "use_even");
-    flipped = RNA_boolean_get(op->ptr, "flipped");
-    use_clamp = RNA_boolean_get(op->ptr, "use_clamp");
+    PropertyRNA *prop;
+    prop = RNA_struct_find_property(op->ptr, "use_even");
+    use_even = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : false;
+    prop = RNA_struct_find_property(op->ptr, "flipped");
+    flipped = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : false;
+    prop = RNA_struct_find_property(op->ptr, "use_clamp");
+    use_clamp = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : true;
   }
-  initVertSlide_ex(t, use_even, flipped, use_clamp);
+  initVertSlide_ex(t, op, use_even, flipped, use_clamp);
 }
 
 /** \} */

source/blender/editors/transform/transform.cc
@@ -1090,7 +1090,7 @@ wmOperatorStatus transformEvent(TransInfo *t, wmOperator *op, const wmEvent *eve
           if (t->state == TRANS_CANCEL) {
             resetTransModal(t);
             t->state = TRANS_STARTING;
-            transform_mode_init(t, nullptr, TFM_VERT_SLIDE);
+            transform_mode_init(t, op, TFM_VERT_SLIDE);
           }
           /* Vert Slide can fail on unconnected vertices (rare but possible). */
           if (t->state == TRANS_CANCEL) {

FrenkyBojler/blender-ios

commit eac3d8c0231fb6986c7240d5844a9b62ca51c3c1

Fix #145703: Set clamp mode to on by default during vertex slide
Caused by 0bcab8913fc8826575d6647008ae85b57114ecd2
Default value for `use_clamp` is true. But negation in above commit
affected the clamp value. Remove `!`, that we actual property value is
retrieved. Did the same for other two properties as well.

source/blender/editors/transform/transform_mode_vert_slide.cc
@@ -645,11 +645,11 @@ static void initVertSlide(TransInfo *t, wmOperator *op)
   if (op) {
     PropertyRNA *prop;
     prop = RNA_struct_find_property(op->ptr, "use_even");
-    use_even = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : false;
+    use_even = (prop) ? RNA_property_boolean_get(op->ptr, prop) : false;
     prop = RNA_struct_find_property(op->ptr, "flipped");
-    flipped = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : false;
+    flipped = (prop) ? RNA_property_boolean_get(op->ptr, prop) : false;
     prop = RNA_struct_find_property(op->ptr, "use_clamp");
-    use_clamp = (prop) ? !RNA_property_boolean_get(op->ptr, prop) : true;
+    use_clamp = (prop) ? RNA_property_boolean_get(op->ptr, prop) : true;
   }
   initVertSlide_ex(t, op, use_even, flipped, use_clamp);
 }