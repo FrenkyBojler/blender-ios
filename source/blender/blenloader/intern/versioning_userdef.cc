e904b0270f236227d4443dae7baf1e3692c55b39
Nika Kutsniashvili <nickberckley@gmail.com>
2025-09-04 16:38:08 +0400

    Bump versioning

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -399,7 +399,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(common.anim.long_key_selected);
   }
 
-  if (!USER_VERSION_ATLEAST(500, 71)) {
+  if (!USER_VERSION_ATLEAST(500, 74)) {
     FROM_DEFAULT_V4_UCHAR(space_action.interpolation_bezier);
     FROM_DEFAULT_V4_UCHAR(space_action.interpolation_constant);
     FROM_DEFAULT_V4_UCHAR(space_action.interpolation_linear);
