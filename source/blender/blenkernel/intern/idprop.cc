4a6c45c0476885f99da9aa6834580515980e1abb
Jacques Lucke <jacques@blender.org>
2025-09-05 09:28:49 +0200

    typo

--- source/blender/blenkernel/intern/idprop.cc
+++ source/blender/blenkernel/intern/idprop.cc
@@ -830,7 +830,7 @@ static void IDP_FreeGroup(IDProperty *prop, const bool do_id_user)
   BLI_assert(prop->type == IDP_GROUP);
 
   MEM_delete(prop->data.children_map);
-  /* Setting to null helps with error detection in case the #IDProperty is reused and free later
+  /* Setting to null helps with error detection in case the #IDProperty is reused and freed later
    * again. */
   prop->data.children_map = nullptr;
 
