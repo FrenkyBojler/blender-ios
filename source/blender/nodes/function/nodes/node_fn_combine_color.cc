44cb0a59ff9a50539cc334a90c4b03080a506660
quackarooni <alfonsomartzii@gmail.com>
2025-09-05 16:19:44 +0800

    Fix: Nodes: Combine Color tooltip mentions creating an image instead of simply a color

--- source/blender/nodes/function/nodes/node_fn_combine_color.cc
+++ source/blender/nodes/function/nodes/node_fn_combine_color.cc
@@ -100,7 +100,7 @@ static void node_register()
   fn_node_type_base(&ntype, "FunctionNodeCombineColor", FN_NODE_COMBINE_COLOR);
   ntype.ui_name = "Combine Color";
   ntype.ui_description =
-      "Combine four grayscale channels into one color image, based on a particular color model";
+      "Combine four grayscale channels into a single color, based on a particular color model";
   ntype.enum_name_legacy = "COMBINE_COLOR";
   ntype.nclass = NODE_CLASS_CONVERTER;
   ntype.declare = node_declare;
