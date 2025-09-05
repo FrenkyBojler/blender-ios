d5c99cd6fd2a7b236e3c93659a3a802d710b59ff
Sean Kim <SeanCTKim@protonmail.com>
2025-09-05 16:28:23 -0700

    UI: Experiment: Disable curve mapping when parent layout is inactive

--- source/blender/editors/interface/templates/interface_template_curve_mapping.cc
+++ source/blender/editors/interface/templates/interface_template_curve_mapping.cc
@@ -580,6 +580,9 @@ static void curvemap_buttons_layout(uiLayout *layout,
                                                                1.0f,
                                                                "");
   curve_but->gradient_type = bg;
+  if (!layout->active()) {
+    UI_but_disable(curve_but, "");
+  }
 
   /* Sliders for selected curve point. */
   int i;
