143bfe8ff97ed6101cff9d5924c87b8f0cb13a12
Clément Foucault <foucault.clem@gmail.com>
2025-09-05 10:27:54 +0200

    Fix wrong texel used for fetching thickness

--- source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_comp.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_ray_trace_screen_comp.glsl
@@ -72,7 +72,7 @@ void main()
 
   /* Only closure 0 can be a transmission closure. */
   if (closure_index == 0) {
-    const float thickness = gbuffer::read_thickness(gbuf_header, texel);
+    const float thickness = gbuffer::read_thickness(gbuf_header, texel_fullres);
     if (thickness != 0.0f) {
       ClosureUndetermined cl = gbuffer::read_bin(texel_fullres, closure_index);
       ray = raytrace_thickness_ray_amend(ray, cl, V, thickness);
