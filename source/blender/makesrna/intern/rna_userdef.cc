41fc9c6b4ff99d9b017b118ae50781620165323b
Harley Acheson <harley.acheson@gmail.com>
2025-09-05 14:37:22 -0700

    "Show Number Input Arrows"

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -4947,7 +4947,7 @@ static void rna_def_userdef_view(BlenderRNA *brna)
   prop = RNA_def_property(srna, "show_number_arrows", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "uiflag2", USER_ALWAYS_SHOW_NUMBER_ARROWS);
   RNA_def_property_ui_text(
-      prop, "Always show number arrows", "Display the arrows on each side of numerical inputs");
+      prop, "Show Number Input Arrows", "Display arrows in numeric input fields for increasing or decreasing values");
   RNA_def_property_update(prop, 0, "rna_userdef_update");
 
   prop = RNA_def_property(srna, "show_object_info", PROP_BOOLEAN, PROP_NONE);
