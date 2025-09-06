63fe40ddfb6e6f6ed1b2c5f31429243719062155
илья _ <modormoder@gmail.com>
2025-09-06 17:46:07 +0300

    fix typo 2

--- source/blender/blenkernel/intern/attribute_access.cc
+++ source/blender/blenkernel/intern/attribute_access.cc
@@ -1065,7 +1065,7 @@ void scatter_attributes(const AttributeAccessor src_attributes,
 
     attribute_math::convert_to_static_type(dst.span.type(), [&](auto dummy) {
       using T = decltype(dummy);
-      фrray_utils::scatter<T>(src.varray.typed<T>(), selection, dst.span.typed<T>());
+      array_utils::scatter<T>(src.varray.typed<T>(), selection, dst.span.typed<T>());
     });
 
     dst.finish();
