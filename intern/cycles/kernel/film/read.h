c4c8132b8974578f2b1615f5564c96a1e9e90794
Lukas Stockner <lukas@lukasstockner.de>
2025-09-06 20:54:13 +0200

    Fix scale being ignored with adaptive sampling

--- intern/cycles/kernel/film/read.h
+++ intern/cycles/kernel/film/read.h
@@ -37,10 +37,10 @@ ccl_device_inline float film_get_scale(const ccl_global KernelFilmConvert *ccl_r
   if (kfilm_convert->pass_use_filter) {
     const uint sample_count = *(
         (const ccl_global uint *)(buffer + kfilm_convert->pass_sample_count));
-    return 1.0f / sample_count;
+    return kfilm_convert->scale / sample_count;
   }
 
-  return 1.0f;
+  return kfilm_convert->scale;
 }
 
 ccl_device_inline float film_get_scale_exposure(const ccl_global KernelFilmConvert *ccl_restrict
