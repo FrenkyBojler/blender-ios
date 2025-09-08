b9e94c109872297a0d06f5a1a357666093462e9a
Alberto12345678999 <a.pilotto@icloud.com>
2025-09-08 14:25:39 -0300

    Update Blender file subversion to 77 and adjust version check in versioning logic

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -3047,7 +3047,7 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 65)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 77)) {
     FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
       if (ntree->type != NTREE_COMPOSIT) {
         continue;
