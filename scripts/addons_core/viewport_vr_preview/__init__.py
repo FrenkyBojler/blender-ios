194f36b83c47e99209168951af7a7e145b0e92ae
Jonas Holzman <jonas@holzman.fr>
2025-09-09 16:00:09 +0200

    Revert "VR Addon: Remove now obsolete "unsupported" macOS warning"
    This reverts commit 6c4dc132eb77f6d92bed11375def635156d05207.

--- scripts/addons_core/viewport_vr_preview/__init__.py
+++ scripts/addons_core/viewport_vr_preview/__init__.py
@@ -27,8 +27,14 @@ if "bpy" in locals():
 else:
     from . import action_map, gui, operators, properties
 
+import bpy
+
 
 def register():
+    if not bpy.app.build_options.xr_openxr:
+        bpy.utils.register_class(gui.VIEW3D_PT_vr_info)
+        return
+
     action_map.register()
     gui.register()
     operators.register()
@@ -36,6 +42,10 @@ def register():
 
 
 def unregister():
+    if not bpy.app.build_options.xr_openxr:
+        bpy.utils.unregister_class(gui.VIEW3D_PT_vr_info)
+        return
+
     action_map.unregister()
     gui.unregister()
     operators.unregister()
