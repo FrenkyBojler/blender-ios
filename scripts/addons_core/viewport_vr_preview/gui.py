712d94b55e8d9d801315323ab19396dd0ffad25f
Jonas Holzman <jonas@holzman.fr>
2025-09-09 16:04:30 +0200

    Restore missing OpenXR feature warning, only remove the macOS part

--- scripts/addons_core/viewport_vr_preview/gui.py
+++ scripts/addons_core/viewport_vr_preview/gui.py
@@ -247,8 +247,6 @@ class VIEW3D_PT_vr_info(bpy.types.Panel):
         import platform
         layout = self.layout
         missing_support_string = "Built without VR/OpenXR features"
-        if platform.system() == "Darwin":
-            missing_support_string = "VR is not supported on macOS at the moment"
         layout.label(icon='ERROR', text=missing_support_string)
 
 
