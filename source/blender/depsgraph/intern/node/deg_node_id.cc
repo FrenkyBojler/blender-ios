82441d77cfe213ab89c224b4b6fde705cbddcb8d
Bastien Montagne <bastien@blender.org>
2025-09-10 16:50:04 +0200

    Updates from review.

--- source/blender/depsgraph/intern/node/deg_node_id.cc
+++ source/blender/depsgraph/intern/node/deg_node_id.cc
@@ -58,7 +58,7 @@ void IDNode::init(const ID *id, const char * /*subdata*/)
   previously_visible_components_mask = 0;
 }
 
-void IDNode::init_copy_on_write(Depsgraph &depsgraph, ID *id, ID *id_cow_hint)
+void IDNode::init_copy_on_write(Depsgraph &depsgraph, ID *id_cow_hint)
 {
   /* Create pointer as early as possible, so we can use it for function
    * bindings. Rest of data we'll be copying to the new datablock when
@@ -68,12 +68,12 @@ void IDNode::init_copy_on_write(Depsgraph &depsgraph, ID *id, ID *id_cow_hint)
     if (deg_eval_copy_is_needed(id_orig)) {
       id_cow = id_cow_hint;
 
-      /* While `id_cow->orig_id == id` should be `true` most of the time (a same 'orig' ID shoudl
+      /* While `id_cow->orig_id == id` should be `true` most of the time (a same 'orig' ID should
        * keep a same pointer in most cases), in can happen that the same 'orig' ID got a new
        * address, e.g. after being deleted and re-loaded from memfile undo, without any update of
        * the depsgraph in-between (e.g. when the depsgraph belongs to an inactive viewlayer).
        * Ref. #145848. */
-      id_cow->orig_id = id;
+      id_cow->orig_id = this->id_orig;
     }
     else {
       id_cow = id_orig;
