fb9e18ed1ecbd44a1709d0776b5679083f7b7311
Sean Kim <SeanCTKim@protonmail.com>
2025-09-10 15:59:44 -0700

    Use inactive state from parent, draw points

--- source/blender/editors/interface/templates/interface_template_curve_mapping.cc
+++ source/blender/editors/interface/templates/interface_template_curve_mapping.cc
@@ -581,7 +581,7 @@ static void curvemap_buttons_layout(uiLayout *layout,
                                                                "");
   curve_but->gradient_type = bg;
   if (!layout->active()) {
-    UI_but_disable(curve_but, "");
+    UI_but_flag_enable(curve_but, UI_BUT_INACTIVE);
   }
 
   /* Sliders for selected curve point. */
