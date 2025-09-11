058a1bbef51604c1f8048b614d14eabdeab61007
Omar Emara <mail@OmarEmara.dev>
2025-09-11 11:26:01 +0300

    Support pinning and fix if no active modifier

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -153,21 +153,20 @@ class NODE_HT_header(Header):
                 row.enabled = not snode.pin
                 row.template_ID(scene, "compositing_node_group", new="node.new_compositing_node_group")
             elif snode.node_tree_sub_type == 'SEQUENCER_STRIP_MODIFIER':
+                row = layout.row()
                 sequencer_scene = context.workspace.sequencer_scene
-                if sequencer_scene and sequencer_scene.sequence_editor:
-                    ed = sequencer_scene.sequence_editor
-                    if ed.active_strip and ed.active_strip.modifiers.active and ed.active_strip.modifiers.active.type == 'COMPOSITOR':
-                        modifier = ed.active_strip.modifiers.active
-                        layout.template_ID(
-                            modifier,
-                            "node_group",
-                            new="node.new_compositor_sequencer_strip_modifier_node_group")
-                    else:
-                        layout.template_ID(
-                            snode,
-                            "node_tree",
-                            new="node.new_compositing_node_group")
-                        display_pin = False
+                sequencer_editor = sequencer_scene.sequence_editor if sequencer_scene else None
+                active_strip = sequencer_editor.active_strip if sequencer_editor else None
+                active_modifier = active_strip.modifiers.active if active_strip else None
+                is_compositor_modifier_active = active_modifier and active_modifier.type == 'COMPOSITOR'
+                if is_compositor_modifier_active and not snode.pin:
+                    row.template_ID(
+                        active_modifier,
+                        "node_group",
+                        new="node.new_compositor_sequencer_strip_modifier_node_group")
+                else:
+                    row.enabled = False
+                    row.template_ID(snode, "node_tree", new="node.new_compositor_sequencer_strip_modifier_node_group")
 
         elif snode.tree_type == 'GeometryNodeTree':
             layout.prop(snode, "node_tree_sub_type", text="")
