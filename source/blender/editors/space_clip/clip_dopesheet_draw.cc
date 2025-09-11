9c81481438d578514e1699b661075ee1d1f6bb75
Nika Kutsniashvili <nickberckley@gmail.com>
2025-09-12 02:05:53 +0400

    Missed color for unselected Movie Clip channel

--- source/blender/editors/space_clip/clip_dopesheet_draw.cc
+++ source/blender/editors/space_clip/clip_dopesheet_draw.cc
@@ -50,7 +50,7 @@ static void track_channel_color(MovieTrackingTrack *track, bool default_color, f
       UI_GetThemeColor4fv(TH_CHANNEL_SELECT, color);
     }
     else {
-      UI_GetThemeColor3fv(TH_HEADER, color);
+      UI_GetThemeColor3fv(TH_CHANNEL, color);
     }
   }
 }
