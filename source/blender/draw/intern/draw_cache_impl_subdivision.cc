blender/blender

commit 1e4764a9a598684250d0117e5fef62deab6427d3

Refactor: Simplify subdiv utilities
- Return evaluated points as function return value.
  Used in cases when only limit or final point is requested, without
  partial derivatives.

- Re-order function argument list and use default argument values to
  simplify usage in the simple/typical cases.

No functional changes expected.

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -1628,12 +1628,11 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
   };
 
   if (!bke::subdiv::eval_begin_from_mesh(
-          subdiv, mesh_eval, {}, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU, g_subdiv_evaluator_cache))
+          subdiv, mesh_eval, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU))
   {
     /* This could happen in two situations:
      * - OpenSubdiv is disabled.
-     * - Something totally bad happened, and OpenSubdiv rejected our
-     *   topology.
+     * - Something totally bad happened, and OpenSubdiv rejected our topology.
      * In either way, we can't safely continue. However, we still have to handle potential loose
      * geometry, which is done separately. */
     if (mesh_eval->faces_num) {

source/blender/blenkernel/BKE_subdiv_ccg.hh
@@ -278,9 +278,8 @@ inline int BKE_subdiv_ccg_grid_to_face_index(const SubdivCCG &subdiv_ccg, const 
   return subdiv_ccg.grid_to_face_map[grid_index];
 }
 
-void BKE_subdiv_ccg_eval_limit_point(const SubdivCCG &subdiv_ccg,
-                                     const SubdivCCGCoord &coord,
-                                     blender::float3 &r_point);
+blender::float3 BKE_subdiv_ccg_eval_limit_point(const SubdivCCG &subdiv_ccg,
+                                                const SubdivCCGCoord &coord);
 void BKE_subdiv_ccg_eval_limit_positions(const SubdivCCG &subdiv_ccg,
                                          const CCGKey &key,
                                          int grid_index,

source/blender/blenkernel/BKE_subdiv_eval.hh
@@ -35,9 +35,9 @@ bool eval_begin(Subdiv *subdiv,
  */
 bool eval_begin_from_mesh(Subdiv *subdiv,
                           const Mesh *mesh,
-                          Span<float3> coarse_vert_positions,
                           eSubdivEvaluatorType evaluator_type,
-                          OpenSubdiv_EvaluatorCache *evaluator_cache);
+                          Span<float3> coarse_vert_positions = {},
+                          OpenSubdiv_EvaluatorCache *evaluator_cache = nullptr);
 bool eval_refine_from_mesh(Subdiv *subdiv, const Mesh *mesh, Span<float3> coarse_vert_positions);
 
 /* Makes sure displacement evaluator is initialized.
@@ -50,7 +50,7 @@ void eval_init_displacement(Subdiv *subdiv);
 
 /* Evaluate point at a limit surface, with optional derivatives and normal. */
 
-void eval_limit_point(Subdiv *subdiv, int ptex_face_index, float u, float v, float3 &r_P);
+float3 eval_limit_point(Subdiv *subdiv, int ptex_face_index, float u, float v);
 void eval_limit_point_and_derivatives(Subdiv *subdiv,
                                       int ptex_face_index,
                                       float u,
@@ -88,6 +88,6 @@ void eval_displacement(Subdiv *subdiv,
                        float3 &r_D);
 
 /* Evaluate point on a limit surface with displacement applied to it. */
-void eval_final_point(Subdiv *subdiv, int ptex_face_index, float u, float v, float3 &r_P);
+float3 eval_final_point(Subdiv *subdiv, int ptex_face_index, float u, float v);
 
 }  // namespace blender::bke::subdiv

source/blender/blenkernel/BKE_subdiv_foreach.hh
@@ -98,48 +98,48 @@ struct ForeachContext {
    *
    * NOTE: If this callback returns false, the foreach loop is aborted.
    */
-  ForeachTopologyInformationCb topology_info;
+  ForeachTopologyInformationCb topology_info = nullptr;
   /* These callbacks are called from every ptex which shares "emitting"
    * vertex or edge.
    */
-  ForeachVertexFromCornerCb vertex_every_corner;
-  ForeachVertexFromEdgeCb vertex_every_edge;
+  ForeachVertexFromCornerCb vertex_every_corner = nullptr;
+  ForeachVertexFromEdgeCb vertex_every_edge = nullptr;
   /* Those callbacks are run once per subdivision vertex, ptex is undefined
    * as in it will be whatever first ptex face happened to be traversed in
    * the multi-threaded environment and which shares "emitting" vertex or
    * edge.
    */
-  ForeachVertexFromCornerCb vertex_corner;
-  ForeachVertexFromEdgeCb vertex_edge;
+  ForeachVertexFromCornerCb vertex_corner = nullptr;
+  ForeachVertexFromEdgeCb vertex_edge = nullptr;
   /* Called exactly once, always corresponds to a single ptex face. */
-  ForeachVertexInnerCb vertex_inner;
+  ForeachVertexInnerCb vertex_inner = nullptr;
   /* Called once for each loose vertex. One loose coarse vertex corresponds
    * to a single subdivision vertex.
    */
-  ForeachLooseCb vertex_loose;
+  ForeachLooseCb vertex_loose = nullptr;
   /* Called once per vertex created for loose edge. */
-  ForeachVertexOfLooseEdgeCb vertex_of_loose_edge;
+  ForeachVertexOfLooseEdgeCb vertex_of_loose_edge = nullptr;
   /* NOTE: If subdivided edge does not come from coarse edge, ORIGINDEX_NONE
    * will be passed as coarse_edge_index.
    */
-  ForeachEdgeCb edge;
+  ForeachEdgeCb edge = nullptr;
   /* NOTE: If subdivided loop does not come from coarse loop, ORIGINDEX_NONE
    * will be passed as coarse_loop_index.
    */
-  ForeachLoopCb loop;
-  ForeachPolygonCb poly;
+  ForeachLoopCb loop = nullptr;
+  ForeachPolygonCb poly = nullptr;
 
   /* User-defined pointer, to allow callbacks know something about context the
    * traversal is happening for.
    */
-  void *user_data;
+  void *user_data = nullptr;
 
   /* Initial value of TLS data. */
-  void *user_data_tls;
+  void *user_data_tls = nullptr;
   /* Size of TLS data. */
-  size_t user_data_tls_size;
+  size_t user_data_tls_size = 0;
   /* Function to free TLS storage. */
-  void (*user_data_tls_free)(void *tls);
+  void (*user_data_tls_free)(void *tls) = nullptr;
 };
 
 /* Invokes callbacks in the order and with values which corresponds to creation

source/blender/blenkernel/BKE_subdiv_mesh.hh
@@ -25,9 +25,9 @@ struct ToMeshSettings {
    * created for a corner of non-quad face) will have resolution of
    * `resolution - 1`.
    */
-  int resolution;
+  int resolution = -1;
   /** When true, only edges emitted from coarse ones will be displayed. */
-  bool use_optimal_display;
+  bool use_optimal_display = false;
 };
 
 /** Create real hi-res mesh from subdivision, all geometry is "real". */

source/blender/blenkernel/intern/multires_reshape_smooth.cc
@@ -1319,9 +1319,8 @@ static void evaluate_higher_grid_positions(MultiresReshapeSmoothContext *reshape
             reshape_context, grid_coord);
 
         /* Surface. */
-        blender::float3 P;
-        blender::bke::subdiv::eval_limit_point(
-            reshape_subdiv, ptex_coord->ptex_face_index, ptex_coord->u, ptex_coord->v, P);
+        const blender::float3 P = blender::bke::subdiv::eval_limit_point(
+            reshape_subdiv, ptex_coord->ptex_face_index, ptex_coord->u, ptex_coord->v);
 
         *grid_element.displacement = P;
 

source/blender/blenkernel/intern/multires_reshape_util.cc
@@ -56,9 +56,7 @@ blender::bke::subdiv::Subdiv *multires_reshape_create_subdiv(Depsgraph *depsgrap
   if (!subdiv) {
     return nullptr;
   }
-  if (!subdiv::eval_begin_from_mesh(
-          subdiv, base_mesh, {}, subdiv::SUBDIV_EVALUATOR_TYPE_CPU, nullptr))
-  {
+  if (!subdiv::eval_begin_from_mesh(subdiv, base_mesh, subdiv::SUBDIV_EVALUATOR_TYPE_CPU)) {
     subdiv::free(subdiv);
     return nullptr;
   }

source/blender/blenkernel/intern/multires_versioning.cc
@@ -48,9 +48,7 @@ static blender::bke::subdiv::Subdiv *subdiv_for_simple_to_catmull_clark(Object *
   subdiv::Subdiv *subdiv = subdiv::new_from_converter(&subdiv_settings, &converter);
   subdiv::converter_free(&converter);
 
-  if (!subdiv::eval_begin_from_mesh(
-          subdiv, base_mesh, {}, subdiv::SUBDIV_EVALUATOR_TYPE_CPU, nullptr))
-  {
+  if (!subdiv::eval_begin_from_mesh(subdiv, base_mesh, subdiv::SUBDIV_EVALUATOR_TYPE_CPU)) {
     subdiv::free(subdiv);
     return nullptr;
   }

FrenkyBojler/blender-ios

commit 4de88b6ea2819491a937399110d455c6e4c8d463

Fix #146086: Regression: OpenSubdiv: GPU Shaders gets recompiled for each evaluation
Regression in 1e4764a9a598 which accidentally removed use of
the g_subdiv_evaluator_cache.

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -1642,7 +1642,7 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
   };
 
   if (!bke::subdiv::eval_begin_from_mesh(
-          subdiv, mesh_eval, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU))
+          subdiv, mesh_eval, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU, {}, g_subdiv_evaluator_cache))
   {
     /* This could happen in two situations:
      * - OpenSubdiv is disabled.