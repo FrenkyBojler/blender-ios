760d0cf0b662e87778dea6c2bf6c91a2913b732a
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-09-11 10:26:57 +0200

    apply diff

--- source/blender/editors/transform/transform_convert_armature.cc
+++ source/blender/editors/transform/transform_convert_armature.cc
@@ -398,6 +398,7 @@ static void add_pose_transdata(
 
   const bArmature *arm = static_cast<bArmature *>(ob->data);
   BKE_pose_channel_gizmo_location(arm, pchan, td->center);
+  copy_v3_v3(td_ext->center_no_override, pchan->pose_mat[3]);
 
   td->flag = TD_SELECTED;
   if (bone->flag & BONE_HINGE_CHILD_TRANSFORM) {
