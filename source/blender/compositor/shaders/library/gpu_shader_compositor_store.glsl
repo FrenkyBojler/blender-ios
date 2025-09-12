bfb2cd585e90b33bbde4a20df3f96b6ce8f80819
Clément Foucault <foucault.clem@gmail.com>
2025-09-12 14:07:36 +0200

    Fix codestyle

--- source/blender/compositor/shaders/library/gpu_shader_compositor_store.glsl
+++ source/blender/compositor/shaders/library/gpu_shader_compositor_store.glsl
@@ -6,14 +6,16 @@
 /* This file must replaced at runtime. The following content is only a possible implementation. */
 #pragma runtime_generated
 
-/* Function generated from #ShaderOperation::generate_code_for_outputs. */
+/* Functions generated from #ShaderOperation::generate_code_for_outputs. */
 /* Commented out because they mess with the library parser. */
-// void store_float(const uint id, float value) {}
-// void store_int(const uint id, float value) {}
-// void store_bool(const uint id, float value) {}
-// void store_float3(const uint id, vec3 value) {}
-// void store_color(const uint id, vec4 value) {}
-// void store_float4(const uint id, vec4 value) {}
-// void store_float2(const uint id, vec3 value) {}
-// void store_int2(const uint id, vec3 value) {}
-// void store_menu(const uint id, float value) {}
+/**
+ * void store_float(const uint id, float value) {}
+ * void store_int(const uint id, float value) {}
+ * void store_bool(const uint id, float value) {}
+ * void store_float3(const uint id, vec3 value) {}
+ * void store_color(const uint id, vec4 value) {}
+ * void store_float4(const uint id, vec4 value) {}
+ * void store_float2(const uint id, vec3 value) {}
+ * void store_int2(const uint id, vec3 value) {}
+ * void store_menu(const uint id, float value) {}
+ */
