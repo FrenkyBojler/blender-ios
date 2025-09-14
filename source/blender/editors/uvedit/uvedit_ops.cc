d32201b5c6b06e418d176e7d72f761e6b0f1938c
Eitan Traurig <eitant13@gmail.com>
2025-09-14 05:23:47 +0000

    UV: add pack to custom region option to "Pack Islands" operator
    This commit implements the design task #78398
    
    - Box region
      - Set Custom UV Region (Ctrl-B).
      - Disabled with Custom Region checkbox in the menu or (Ctrl-Alt-B).
    - Box Select (Pinned) changed to (Alt-B).
    - When the Custom Region enum is chosen the islands are packed
      into the bounding box of the drawn region.
    
    Ref !140020

--- source/blender/editors/uvedit/uvedit_ops.cc
+++ source/blender/editors/uvedit/uvedit_ops.cc
@@ -2741,6 +2741,7 @@ void ED_operatortypes_uvedit()
   WM_operatortype_append(UV_OT_select_less);
   WM_operatortype_append(UV_OT_select_overlap);
   WM_operatortype_append(UV_OT_select_mode);
+  WM_operatortype_append(UV_OT_custom_region_set);
 
   WM_operatortype_append(UV_OT_snap_cursor);
   WM_operatortype_append(UV_OT_snap_selected);
