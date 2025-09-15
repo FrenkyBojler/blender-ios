53a5bd062cd73da3f905233c57e7a7454915064d
W_Cloud <yunkezengren@outlook.com>
2025-09-15 08:10:25 +0800

    cleanup

--- source/blender/editors/space_spreadsheet/spreadsheet_layout.cc
+++ source/blender/editors/space_spreadsheet/spreadsheet_layout.cc
@@ -10,7 +10,6 @@
 #include "BLF_api.hh"
 
 #include "BLI_color.hh"
-#include "BLI_math_base.h"
 #include "BLI_math_matrix.hh"
 #include "BLI_math_quaternion_types.hh"
 #include "BLI_math_vector_types.hh"
