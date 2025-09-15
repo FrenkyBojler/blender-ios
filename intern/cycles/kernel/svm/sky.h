354cbc03762fc8da267bb69639c28d26b1bc2345
Lukas Stockner <lukas@lukasstockner.de>
2025-09-15 23:50:03 +0200

    Cleanups

--- intern/cycles/kernel/svm/sky.h
+++ intern/cycles/kernel/svm/sky.h
@@ -10,48 +10,31 @@
 
 #include "kernel/util/colorspace.h"
 
-#include "util/color.h"
-
 CCL_NAMESPACE_BEGIN
 
 /* Sky texture */
 
-ccl_device float sky_angle_between(const float thetav,
-                                   const float phiv,
-                                   const float theta,
-                                   const float phi)
-{
-  const float cospsi = sinf(thetav) * sinf(theta) * cosf(phi - phiv) + cosf(thetav) * cosf(theta);
-  return safe_acosf(cospsi);
-}
-
-/* Nishita improved sky model */
-ccl_device float3 geographical_to_direction(const float lat, const float lon)
-{
-  return spherical_to_direction(lat - M_PI_2_F, lon - M_PI_2_F);
-}
-
-ccl_device float3 sky_radiance_nishita(KernelGlobals kg,
-                                       const NodeSkyType type,
-                                       const float3 dir,
-                                       const uint32_t path_flag,
-                                       const float3 pixel_bottom,
-                                       const float3 pixel_top,
-                                       const ccl_private float *nishita_data,
-                                       const uint texture_id)
+ccl_device float3 sky_radiance(KernelGlobals kg,
+                               const NodeSkyType type,
+                               const float3 dir,
+                               const uint32_t path_flag,
+                               const float3 pixel_bottom,
+                               const float3 pixel_top,
+                               const ccl_private float *sky_data,
+                               const uint texture_id)
 {
   /* definitions */
-  const float sun_elevation = nishita_data[0];
-  const float sun_rotation = nishita_data[1];
-  const float angular_diameter = nishita_data[2];
-  const float sun_intensity = nishita_data[3];
-  const float earth_intersection_angle = nishita_data[4];
+  const float sun_elevation = sky_data[0];
+  const float sun_rotation = sky_data[1];
+  const float angular_diameter = sky_data[2];
+  const float sun_intensity = sky_data[3];
+  const float earth_intersection_angle = sky_data[4];
   const bool sun_disc = (angular_diameter >= 0.0f);
   float3 xyz = zero_float3();
   /* convert dir to spherical coordinates */
   const float2 direction = direction_to_spherical(dir);
   /* definitions */
-  const float3 sun_dir = geographical_to_direction(sun_elevation, sun_rotation);
+  const float3 sun_dir = spherical_to_direction(sun_elevation - M_PI_2_F, sun_rotation - M_PI_2_F);
   const float sun_dir_angle = precise_angle(dir, sun_dir);
   const float half_angular = angular_diameter * 0.5f;
   const float dir_elevation = M_PI_2_F - direction.x;
@@ -106,32 +89,28 @@ ccl_device_noinline int svm_node_tex_sky(KernelGlobals kg,
   const NodeSkyType type = (NodeSkyType)node.w;
 
   const float3 dir = stack_load_float3(stack, dir_offset);
-  float3 f;
-
-  /* Nishita */
-  /* Define variables */
-  float nishita_data[5];
 
   float4 data = read_node_float(kg, &offset);
   const float3 pixel_bottom = make_float3(data.x, data.y, data.z);
   float3 pixel_top;
   pixel_top.x = data.w;
 
+  float sky_data[5];
   data = read_node_float(kg, &offset);
   pixel_top.y = data.x;
   pixel_top.z = data.y;
-  nishita_data[0] = data.z;
-  nishita_data[1] = data.w;
+  sky_data[0] = data.z;
+  sky_data[1] = data.w;
 
   data = read_node_float(kg, &offset);
-  nishita_data[2] = data.x;
-  nishita_data[3] = data.y;
-  nishita_data[4] = data.z;
+  sky_data[2] = data.x;
+  sky_data[3] = data.y;
+  sky_data[4] = data.z;
   const uint texture_id = __float_as_uint(data.w);
 
   /* Compute Sky */
-  f = sky_radiance_nishita(
-      kg, type, dir, path_flag, pixel_bottom, pixel_top, nishita_data, texture_id);
+  const float3 f = sky_radiance(
+      kg, type, dir, path_flag, pixel_bottom, pixel_top, sky_data, texture_id);
 
   stack_store_float3(stack, out_offset, f);
   return offset;
