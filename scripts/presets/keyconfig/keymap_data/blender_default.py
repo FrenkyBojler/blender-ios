91b4a9d6a2c6420e95235184cd538126ef3d03c2
Ramon Klauck <ramonklauck3@gmail.com>
2025-09-18 23:24:40 +0200

    use alt instead of ctrl

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -8623,7 +8623,7 @@ def km_sequencer_tool_blade(_params):
                  ("use_cursor_position", True),
                  ("ignore_selection", True),
              ]}),
-            ("sequencer.split", {"type": 'LEFTMOUSE', "value": 'PRESS', "ctrl": True},
+            ("sequencer.split", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True},
              {"properties": [
                  ("type", 'SOFT'),
                  ("side", 'NO_CHANGE'),
