bf7537ad381ff8ce0a017175190fa4f7dd70e987
Guillermo Venegas <guillermovcra@gmail.com>
2025-09-18 08:34:58 -0600

    remove outdated example

--- scripts/startup/bl_ui/properties_object.py
+++ scripts/startup/bl_ui/properties_object.py
@@ -81,7 +81,6 @@ class OBJECT_PT_transform(ObjectButtonsPanel, Panel):
         row.prop(ob, "scale")
         row.use_property_decorate = False
         row.prop(ob, "lock_scale", text="", emboss=False, icon='DECORATE_UNLOCKED')
-        col.prop_textbox(bpy.data.screens["Layout"].areas[1].spaces[0], "filter_text", "search_filter")
 
 
 class OBJECT_PT_delta_transform(ObjectButtonsPanel, Panel):
