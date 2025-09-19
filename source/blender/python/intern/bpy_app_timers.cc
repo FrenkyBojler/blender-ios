9eb112e873089e98bc390eefd17d7a90ac533789
Oxicid <Oxicid2019@gmail.com>
2025-09-19 17:47:00 +0300

    Cleanup: correct misleading PyError message formatting

--- source/blender/python/intern/bpy_app_timers.cc
+++ source/blender/python/intern/bpy_app_timers.cc
@@ -19,7 +19,7 @@
 static double handle_returned_value(PyObject *function, PyObject *ret)
 {
   if (ret == nullptr) {
-    PyErr_PrintEx(0);
+    PyErr_Print();
     return -1;
   }
 
@@ -128,7 +128,7 @@ PyDoc_STRVAR(
 static PyObject *bpy_app_timers_unregister(PyObject * /*self*/, PyObject *function)
 {
   if (!BLI_timer_unregister(intptr_t(function))) {
-    PyErr_SetString(PyExc_ValueError, "Error: function is not registered");
+    PyErr_SetString(PyExc_ValueError, "function is not registered");
     return nullptr;
   }
   Py_RETURN_NONE;
