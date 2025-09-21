f936efda344233dc1d73f7019429f3dc2c056bb0
Iliya Katushenock <modormoder@gmail.com>
2025-09-21 21:45:54 +0200

    cleanup

--- source/blender/blenkernel/BKE_node_runtime.hh
+++ source/blender/blenkernel/BKE_node_runtime.hh
@@ -439,7 +439,7 @@ class AllowUsingOutdatedInfo : NonCopyable, NonMovable {
 };
 
 /* If result is not true then this means that the last node tree editing operation was not covered
- * by the topology cache update ensure call. All all derivative information about topology is not
+ * by the topology cache update ensure call. All derivative information about topology is not
  * available. You should call "tree.ensure_topology_cache();" first.
  */
 inline bool topology_cache_is_available(const bNodeTree &tree)
