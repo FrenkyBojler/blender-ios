66a9aebedf36fcb0326dd67e94c4256e778ae53e
Dalai Felinto <dalai@blender.org>
2025-09-22 23:57:39 +0200

    Cleanup from original code

--- source/blender/editors/screen/screen_edit.cc
+++ source/blender/editors/screen/screen_edit.cc
@@ -1452,17 +1452,16 @@ static void screen_set_3dview_camera(Scene *scene,
                                      ScrArea *area,
                                      View3D *v3d)
 {
-  /* fix any cameras that are used in the 3d view but not in the scene */
+  /* Fix any cameras that are used in the 3d view but not in the scene. */
   BKE_screen_view3d_sync(v3d, scene);
 
   BKE_view_layer_synced_ensure(scene, view_layer);
   if (!v3d->camera || !BKE_view_layer_base_find(view_layer, v3d->camera)) {
     v3d->camera = BKE_view_layer_camera_find(scene, view_layer);
   }
-  // XXX if (screen == curscreen) handle_view3d_lock();
   ListBase *regionbase;
 
-  /* regionbase is in different place depending if space is active */
+  /* regionbase is in different place depending if space is active. */
   if (v3d == area->spacedata.first) {
     regionbase = &area->regionbase;
   }
