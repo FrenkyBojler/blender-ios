da37a1379983126132164b2c5722d120be747cbe
Dalai Felinto <dalai@blender.org>
2025-09-22 23:55:52 +0200

    From review: Add temporary flag
    I tested with Quad view, which would lead to a funny issue where
    all 4 viewports would be camera mode when switching back to a scene with
    a camera.

--- source/blender/editors/screen/screen_edit.cc
+++ source/blender/editors/screen/screen_edit.cc
@@ -1447,7 +1447,10 @@ bool ED_screen_change(bContext *C, bScreen *screen)
   return false;
 }
 
-static void screen_set_3dview_camera(Scene *scene, ViewLayer *view_layer, View3D *v3d)
+static void screen_set_3dview_camera(Scene *scene,
+                                     ViewLayer *view_layer,
+                                     ScrArea *area,
+                                     View3D *v3d)
 {
   /* fix any cameras that are used in the 3d view but not in the scene */
   BKE_screen_view3d_sync(v3d, scene);
@@ -1455,10 +1458,36 @@ static void screen_set_3dview_camera(Scene *scene, ViewLayer *view_layer, View3D
   BKE_view_layer_synced_ensure(scene, view_layer);
   if (!v3d->camera || !BKE_view_layer_base_find(view_layer, v3d->camera)) {
     v3d->camera = BKE_view_layer_camera_find(scene, view_layer);
+  }
+  // XXX if (screen == curscreen) handle_view3d_lock();
+  ListBase *regionbase;
 
-    /* Keep the RegionView3D persp as RV3D_CAMOB even if no camera can be found,
+  /* regionbase is in different place depending if space is active */
+  if (v3d == area->spacedata.first) {
+    regionbase = &area->regionbase;
+  }
+  else {
+    regionbase = &v3d->regionbase;
+  }
+
+  LISTBASE_FOREACH (ARegion *, region, regionbase) {
+    if (region->regiontype != RGN_TYPE_WINDOW) {
+      continue;
+    }
+    RegionView3D *rv3d = static_cast<RegionView3D *>(region->regiondata);
+    /* Keep the information about RV3D_CAMOB even when no camera can be found.
      * This prevents jumping off the camera view while scrubbing through the
      * Sequencer with some Scene strips without scene. */
+    if (!v3d->camera) {
+      if (rv3d->persp == RV3D_CAMOB) {
+        rv3d->persp = RV3D_PERSP;
+        rv3d->rflag |= RV3D_WAS_CAMOB;
+      }
+    }
+    else if ((rv3d->rflag & RV3D_WAS_CAMOB) != 0) {
+      rv3d->persp = RV3D_CAMOB;
+      rv3d->rflag &= ~RV3D_WAS_CAMOB;
+    }
   }
 }
 
@@ -1499,7 +1528,7 @@ void ED_screen_scene_change(bContext *C,
     LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
       if (sl->spacetype == SPACE_VIEW3D) {
         View3D *v3d = (View3D *)sl;
-        screen_set_3dview_camera(scene, view_layer, v3d);
+        screen_set_3dview_camera(scene, view_layer, area, v3d);
       }
     }
   }
