27b1ff970e3662dd34b72f14de1d0778c5f71e0b
Philipp Oeser <philipp@blender.org>
2025-09-22 13:47:35 +0200

    Fix #146529: Crash getting RNA path to scene rigid body pointcache
    Spawning the tooltip or changing values would cause this.
    
    Error in 28827b62f7
    
    Above commit only considered PointCaches associated with **objects**,
    not taking into account scene rigid body cache.
    This is now added with this PR.
    Previously it would cast into modifiers when we actually had a scene as
    owner... resulting in a crash.
    
    Havent dug deep enough to tell why this was only exposed? by
    6bf868562850 (it crashes since, was just not working properly before...)

--- source/blender/makesrna/intern/rna_object_force.cc
+++ source/blender/makesrna/intern/rna_object_force.cc
@@ -118,7 +118,7 @@ static const EnumPropertyItem empty_vortex_shape_items[] = {
 
 #  include "ED_object.hh"
 
-static bool rna_Cache_get_valid_owner_ID(PointerRNA *ptr, Object **ob, Scene **scene)
+static bool rna_Cache_get_valid_owner_ID(const PointerRNA *ptr, Object **ob, Scene **scene)
 {
   switch (GS(ptr->owner_id->name)) {
     case ID_OB:
@@ -139,65 +139,81 @@ static bool rna_Cache_get_valid_owner_ID(PointerRNA *ptr, Object **ob, Scene **s
 
 static std::optional<std::string> rna_PointCache_path(const PointerRNA *ptr)
 {
-  ModifierData *md;
-  Object *ob = (Object *)ptr->owner_id;
   PointCache *cache = static_cast<PointCache *>(ptr->data);
 
-  for (md = static_cast<ModifierData *>(ob->modifiers.first); md; md = md->next) {
-    const ModifierTypeInfo *mti = BKE_modifier_get_info(ModifierType(md->type));
+  Object *ob = nullptr;
+  Scene *scene = nullptr;
 
-    if (!(mti->flags & eModifierTypeFlag_UsesPointCache)) {
-      continue;
-    }
+  if (!rna_Cache_get_valid_owner_ID(ptr, &ob, &scene)) {
+    return std::nullopt;
+  }
 
-    char name_esc[sizeof(md->name) * 2];
-    BLI_str_escape(name_esc, md->name, sizeof(name_esc));
+  if (ob != nullptr) {
+    ModifierData *md;
+    for (md = static_cast<ModifierData *>(ob->modifiers.first); md; md = md->next) {
+      const ModifierTypeInfo *mti = BKE_modifier_get_info(ModifierType(md->type));
 
-    switch (md->type) {
-      case eModifierType_ParticleSystem: {
-        ParticleSystemModifierData *psmd = (ParticleSystemModifierData *)md;
-        if (psmd->psys->pointcache == cache) {
-          return fmt::format("modifiers[\"{}\"].particle_system.point_cache", name_esc);
-        }
-        break;
+      if (!(mti->flags & eModifierTypeFlag_UsesPointCache)) {
+        continue;
       }
-      case eModifierType_DynamicPaint: {
-        DynamicPaintModifierData *pmd = (DynamicPaintModifierData *)md;
-        if (pmd->canvas) {
-          DynamicPaintSurface *surface = static_cast<DynamicPaintSurface *>(
-              pmd->canvas->surfaces.first);
-          for (; surface; surface = surface->next) {
-            if (surface->pointcache == cache) {
-              char name_surface_esc[sizeof(surface->name) * 2];
-              BLI_str_escape(name_surface_esc, surface->name, sizeof(name_surface_esc));
-              return fmt::format(
-                  "modifiers[\"{}\"].canvas_settings.canvas_surfaces[\"{}\"].point_cache",
-                  name_esc,
-                  name_surface_esc);
+
+      char name_esc[sizeof(md->name) * 2];
+      BLI_str_escape(name_esc, md->name, sizeof(name_esc));
+
+      switch (md->type) {
+        case eModifierType_ParticleSystem: {
+          ParticleSystemModifierData *psmd = (ParticleSystemModifierData *)md;
+          if (psmd->psys->pointcache == cache) {
+            return fmt::format("modifiers[\"{}\"].particle_system.point_cache", name_esc);
+          }
+          break;
+        }
+        case eModifierType_DynamicPaint: {
+          DynamicPaintModifierData *pmd = (DynamicPaintModifierData *)md;
+          if (pmd->canvas) {
+            DynamicPaintSurface *surface = static_cast<DynamicPaintSurface *>(
+                pmd->canvas->surfaces.first);
+            for (; surface; surface = surface->next) {
+              if (surface->pointcache == cache) {
+                char name_surface_esc[sizeof(surface->name) * 2];
+                BLI_str_escape(name_surface_esc, surface->name, sizeof(name_surface_esc));
+                return fmt::format(
+                    "modifiers[\"{}\"].canvas_settings.canvas_surfaces[\"{}\"].point_cache",
+                    name_esc,
+                    name_surface_esc);
+              }
             }
           }
+          break;
         }
-        break;
-      }
-      case eModifierType_Cloth: {
-        ClothModifierData *clmd = (ClothModifierData *)md;
-        if (clmd->point_cache == cache) {
+        case eModifierType_Cloth: {
+          ClothModifierData *clmd = (ClothModifierData *)md;
+          if (clmd->point_cache == cache) {
+            return fmt::format("modifiers[\"{}\"].point_cache", name_esc);
+          }
+          break;
+        }
+        case eModifierType_Softbody: {
+          SoftBody *sb = ob->soft;
+          if (sb && sb->shared->pointcache == cache) {
+            return fmt::format("modifiers[\"{}\"].point_cache", name_esc);
+          }
+          break;
+        }
+        default: {
           return fmt::format("modifiers[\"{}\"].point_cache", name_esc);
         }
-        break;
-      }
-      case eModifierType_Softbody: {
-        SoftBody *sb = ob->soft;
-        if (sb && sb->shared->pointcache == cache) {
-          return fmt::format("modifiers[\"{}\"].point_cache", name_esc);
-        }
-        break;
-      }
-      default: {
-        return fmt::format("modifiers[\"{}\"].point_cache", name_esc);
       }
     }
   }
+
+  /* Scene rigid body. */
+  if (scene != nullptr && scene->rigidbody_world->shared != nullptr) {
+    if (scene->rigidbody_world->shared->pointcache == cache) {
+      return "rigidbody_world.point_cache";
+    }
+  }
+
   return std::nullopt;
 }
 
