54fa167dd35d44ab9cb344094bdd7ee61671610d
W_Cloud <yunkezengren@outlook.com>
2025-09-22 20:49:45 +0800

    Cleanup: Remove unnecessary `.c_str()` call for TIP_ macro

--- source/blender/editors/space_node/node_draw.cc
+++ source/blender/editors/space_node/node_draw.cc
@@ -2730,8 +2730,8 @@ static void node_header_custom_tooltip(const bNode &node, uiBut &but)
       [](bContext & /*C*/, uiTooltipData &data, uiBut * /*but*/, void *argN) {
         const bNode &node = *static_cast<const bNode *>(argN);
         const std::string description = node.typeinfo->ui_description_fn ?
-                                            TIP_(node.typeinfo->ui_description_fn(node).c_str()) :
-                                            TIP_(node.typeinfo->ui_description.c_str());
+                                            TIP_(node.typeinfo->ui_description_fn(node)) :
+                                            TIP_(node.typeinfo->ui_description);
         if (!description.empty()) {
           UI_tooltip_text_field_add(
               data, std::move(description), "", UI_TIP_STYLE_NORMAL, UI_TIP_LC_NORMAL);
