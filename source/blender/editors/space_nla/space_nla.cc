626342fd5ccfc7941f8f16574eb521be78880054
Falk David <falk@blender.org>
2025-09-22 10:42:07 +0200

    Don't change graph editor or nla

--- source/blender/editors/space_nla/space_nla.cc
+++ source/blender/editors/space_nla/space_nla.cc
@@ -297,8 +297,6 @@ static void nla_main_region_draw(const bContext *C, ARegion *region)
   UI_view2d_view_ortho(v2d);
   ANIM_draw_previewrange(scene, v2d, 0);
 
-  ANIM_draw_scene_strip_range(C, v2d, 0);
-
   /* callback */
   UI_view2d_view_ortho(v2d);
   ED_region_draw_cb_draw(C, region, REGION_DRAW_POST_VIEW);
