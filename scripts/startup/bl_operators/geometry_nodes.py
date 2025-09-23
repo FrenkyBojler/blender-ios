blender/blender

commit 40ecce9843e58888b953c87c8951b25085d79539

Fix: Geometry Nodes: Move to Nodes operator not disabled on linked data
This operator can only be used if the data-block the modifier belongs to
is editable.

scripts/startup/bl_operators/geometry_nodes.py
@@ -92,7 +92,10 @@ def get_context_modifier(context):
 
 
 def edit_geometry_nodes_modifier_poll(context):
-    return get_context_modifier(context) is not None
+    modifier = get_context_modifier(context)
+    if modifier is None:
+        return False
+    return modifier.id_data.is_editable
 
 
 def socket_idname_to_attribute_type(idname):

FrenkyBojler/blender-ios

commit 6f5db0bc75c167b2fa4a6c2adb479e6d2a8b58dd

Fix: Operator search emits python error
Caused by 40ecce9843e58888b953c87c8951b25085d79539

scripts/startup/bl_operators/geometry_nodes.py
@@ -93,7 +93,7 @@ def get_context_modifier(context):
 
 def edit_geometry_nodes_modifier_poll(context):
     modifier = get_context_modifier(context)
-    if modifier is None:
+    if not modifier:
         return False
     return modifier.id_data.is_editable
 