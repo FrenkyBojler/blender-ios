5e064742abc78aba3ee5cbf6ac10c7ca1ea0406e
Pablo Vazquez <pablo@blender.org>
2025-09-23 17:29:15 +0200

    Fix check for edit scene

--- scripts/startup/bl_app_templates_system/Storyboarding/__init__.py
+++ scripts/startup/bl_app_templates_system/Storyboarding/__init__.py
@@ -35,8 +35,8 @@ def update_factory_startup_scenes():
         scene.tool_settings.use_keyframe_insert_auto = True
         scene.tool_settings.gpencil_sculpt.use_scale_thickness = True
 
-    if scene.name == 'Edit':
-        scene.tool_settings.use_keyframe_insert_auto = False
+        if scene.name == 'Edit':
+            scene.tool_settings.use_keyframe_insert_auto = False
 
 
 def update_factory_startup_grease_pencils():
