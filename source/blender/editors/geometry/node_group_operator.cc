6e1ae57119e8196c2f79eec500d1210e5d198d39
Hans Goudey <hans@blender.org>
2025-09-24 00:02:26 +0200

    Fix name fallback

--- source/blender/editors/geometry/node_group_operator.cc
+++ source/blender/editors/geometry/node_group_operator.cc
@@ -822,7 +822,7 @@ static wmOperatorStatus run_node_group_exec(bContext *C, wmOperator *op)
          * losing all the data. */
         bke::GeometrySet geometry;
         references[handles[i]].to_geometry_set(geometry);
-        const StringRef name = geometry.name.empty() ? geometry.name : DATA_("Object");
+        const StringRef name = geometry.name.empty() ? DATA_("Object") : geometry.name;
         std::string unique_name = BLI_uniquename_cb(
             [&](const StringRef name) { return instance_by_name.contains(name); }, '.', name);
         instance_by_name.add(std::move(unique_name), {transforms[i], std::move(geometry)});
