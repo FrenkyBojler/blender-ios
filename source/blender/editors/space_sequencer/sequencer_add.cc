169c216418013cab77716385f07b602454f0f9c1
Dalai Felinto <dalai@blender.org>
2025-09-23 16:53:19 +0200

    VSE: Story Tools: Scene Asset behaviour: always add a new scene
    If the asset was local to the file, creates a copy of it.
    If the asset was from an external asset library makes sure it is
    appended.
    
    Design ref: #145522

--- source/blender/editors/space_sequencer/sequencer_add.cc
+++ source/blender/editors/space_sequencer/sequencer_add.cc
@@ -29,6 +29,7 @@
 
 #include "BKE_context.hh"
 #include "BKE_global.hh"
+#include "BKE_lib_id.hh"
 #include "BKE_main.hh"
 #include "BKE_report.hh"
 #include "BKE_scene.hh"
@@ -791,8 +792,13 @@ void SEQUENCER_OT_scene_strip_add_new(wmOperatorType *ot)
 
 /* -------------------------------------------------------------------- */
 /** \name Add Scene Strip From Scene Asset
+ *
  * \{ */
 
+/**
+ * Make sure the scene is always unique and ready to edit.
+ * If it was local it should be duplicated. If linked it should be appended.
+ */
 static Scene *sequencer_add_scene_asset(const bContext &C,
                                         const asset_system::AssetRepresentation &asset,
                                         ReportList & /*reports*/)
@@ -800,6 +806,30 @@ static Scene *sequencer_add_scene_asset(const bContext &C,
   Main &bmain = *CTX_data_main(&C);
   Scene *scene_asset = reinterpret_cast<Scene *>(
       asset::asset_local_id_ensure_imported(bmain, asset));
+
+  if (asset.is_local_id()) {
+    /* Local scene that needs to be duplicated. */
+    Scene *scene_copy = BKE_scene_duplicate(&bmain, scene_asset, SCE_COPY_FULL);
+    return scene_copy;
+  }
+
+  switch (asset.get_import_method().value_or(ASSET_IMPORT_APPEND_REUSE)) {
+    case ASSET_IMPORT_LINK:
+      /* Force it to be append it. */
+      BKE_lib_id_make_local(&bmain,
+                            &scene_asset->id,
+                            LIB_ID_MAKELOCAL_FORCE_COPY | LIB_ID_MAKELOCAL_INDIRECT |
+                                LIB_ID_MAKELOCAL_ASSET_DATA_CLEAR);
+      if (scene_asset->id.newid) {
+        ID *newid = scene_asset->id.newid;
+        scene_asset->id.newid = nullptr;
+        scene_asset = reinterpret_cast<Scene *>(newid);
+      }
+      break;
+    case ASSET_IMPORT_APPEND:
+    case ASSET_IMPORT_APPEND_REUSE:
+      break;
+  }
   return scene_asset;
 }
 
