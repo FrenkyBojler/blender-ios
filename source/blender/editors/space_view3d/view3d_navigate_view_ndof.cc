de26f906c1dba850a897d5502a515586a8a87da2
Patryk-Skowronski <patryk_skowronski@3dconnexion.com>
2025-09-23 16:25:33 +0200

    Sampling the orbit center using discrete grid

--- source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
+++ source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
@@ -491,16 +491,14 @@ static std::optional<float3> ndof_orbit_center_calc_from_bounds(Depsgraph *depsg
   return std::nullopt;
 }
 
-static float ndof_read_zbuf(ARegion *region)
+static float ndof_read_zbuf(ARegion *region, const int x, const int y)
 {
-  view3d_region_operator_needs_gpu(region);
-
   /* Avoid allocating the whole depth buffer. */
   ViewDepths depth_temp = {0};
   {
     /* Some small rectangle in the middle of the view3d region. */
     rcti rect;
-    const int region_center[2] = {region->winx / 2, region->winy / 2};
+    const int region_center[2] = {x, y};
     BLI_rcti_init_pt_radius(&rect, region_center, 2);
     view3d_depths_rect_create(region, &rect, &depth_temp);
   }
@@ -513,20 +511,56 @@ static float ndof_read_zbuf(ARegion *region)
   return depth_near;
 }
 
+/*
+Sample view3d region using a view-centered, square, discrete grid and get the closest (depth-wise) point in screen space.
+Return value:
+x component = screen space x coordinate of the sample
+y component = screen space y coordinate of the sample
+z component = depth of the sample (the lowest value)
+*/
+static float3 ndof_get_min_depth_coords(ARegion *region,
+                                             const int sample_grid_width,
+                                             const int sample_grid_resolution)
+{
+  rcti sample_grid_area;
+  const int sample_grid_center[2] = {region->winx / 2, region->winy / 2};
+  BLI_rcti_init_pt_radius(&sample_grid_area, sample_grid_center, sample_grid_width / 2);
+
+  const int step = sample_grid_width / sample_grid_resolution;
+  float3 result(FLT_MAX);
+
+  for (int x = sample_grid_area.xmin; x <= sample_grid_area.xmax; x += step) {
+    for (int y = sample_grid_area.ymin; y <= sample_grid_area.ymax; y += step) {
+      float depth_near = ndof_read_zbuf(region, x, y);
+      if (depth_near < result.z) {
+        result.x = x;
+        result.y = y;
+        result.z = depth_near;
+      }
+    }
+  }
+
+  return result;
+}
+
 static std::optional<float3> ndof_orbit_center_calc_from_zbuf(Depsgraph *depsgraph,
                                                               ScrArea *area,
                                                               ARegion *region)
 {
+  float3 min_depth_pt = ndof_get_min_depth_coords(region, region->winx / 3, 8);
 
-  const float depth_near = ndof_read_zbuf(region);
-  if (depth_near == FLT_MAX) {
+  if (min_depth_pt.z == FLT_MAX) {
     return std::nullopt;
   }
-  float region_center_x = region->winx / 2.0f;
-  float region_center_y = region->winy / 2.0f;
+
   blender::float3 zbuf_center{};
 
-  if (!ED_view3d_unproject_v3(region, region_center_x, region_center_y, depth_near, zbuf_center)) {
+  if (!ED_view3d_unproject_v3(region,
+                              min_depth_pt.x,
+                              min_depth_pt.y,
+                              min_depth_pt.z,
+                              zbuf_center))
+  {
     return std::nullopt;
   }
 
