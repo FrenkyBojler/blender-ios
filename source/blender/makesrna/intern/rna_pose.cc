60c4eaf0db28b8715cbd9b40a6b9b53cc8e90393
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-09-25 11:51:29 +0200

    rename back to "Affect Gizmo"

--- source/blender/makesrna/intern/rna_pose.cc
+++ source/blender/makesrna/intern/rna_pose.cc
@@ -1173,7 +1173,7 @@ static void rna_def_pose_channel(BlenderRNA *brna)
   prop = RNA_def_property(srna, "use_transform_at_custom_shape", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", POSE_TRANSFORM_AT_CUSTOM_TX);
   RNA_def_property_ui_text(prop,
-                           "Affect Transform",
+                           "Affect Gizmo",
                            "When enabled, the transform gizmo will be placed at the location and "
                            "orientation of the Custom Shape Transform bone");
   RNA_def_property_update(prop, NC_OBJECT | ND_POSE, "rna_Pose_update");
