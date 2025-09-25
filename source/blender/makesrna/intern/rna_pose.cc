e63f14fe45573e4034f23248c4c9b4b0dd342326
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-09-25 12:06:27 +0200

    use longer description

--- source/blender/makesrna/intern/rna_pose.cc
+++ source/blender/makesrna/intern/rna_pose.cc
@@ -1172,10 +1172,13 @@ static void rna_def_pose_channel(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "use_transform_at_custom_shape", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", POSE_TRANSFORM_AT_CUSTOM_TX);
-  RNA_def_property_ui_text(prop,
-                           "Affect Gizmo",
-                           "When enabled, the transform gizmo will be placed at the location and "
-                           "orientation of the Custom Shape Transform bone");
+  RNA_def_property_ui_text(
+      prop,
+      "Affect Gizmo",
+      "The location and orientation of the Custom Shape Transform bone will be used for transform "
+      "gizmos and for other transform operators in the 3D Viewport. When disabled, the 3D "
+      "Viewport will still use the actual bone transform for these, even when the custom bone "
+      "shape transform is overridden.");
   RNA_def_property_update(prop, NC_OBJECT | ND_POSE, "rna_Pose_update");
 
   prop = RNA_def_property(srna, "use_transform_around_custom_shape", PROP_BOOLEAN, PROP_NONE);
