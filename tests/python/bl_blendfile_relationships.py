88f61a3cb0ed46544de682435d03e8d5599bb3a2
Sybren A. Stüvel <sybren@blender.org>
2025-09-25 17:48:16 +0200

    Add unit test
    This did need some work on the `TestHelper` class to support the standard
    `setUp()` and `tearDown()` methods of unit test classes.
    
    It would be good to get rid of this class entirely at some point, and rely
    on `unittest.main()` to run the unit tests. That way the life cycle of the
    tests is as the Python documentation describes, and it's also possible to
    run specific tests from the CLI.

--- tests/python/bl_blendfile_relationships.py
+++ tests/python/bl_blendfile_relationships.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-./blender.bin --background --python tests/python/bl_blendfile_liblink.py
+blender -b -P tests/python/bl_blendfile_relationships.py --src-test-dir tests/files --output-dir /tmp/blendfile_io/
 """
 __all__ = (
     "main",
@@ -12,10 +12,14 @@ __all__ = (
 import bpy
 import os
 import sys
+import unittest
+from pathlib import Path
 
 from bpy.path import native_pathsep
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+_my_dir = Path(__file__).resolve().parent
+sys.path.append(str(_my_dir))
+
 from bl_blendfile_utils import TestBlendLibLinkHelper
 
 
@@ -166,9 +170,81 @@ class TestBlendFilePathMap(TestBlendLibLinkHelper):
         self.assertRaises(TypeError, bpy.data.file_path_map, subset=[bpy.data.objects[0], bpy.data.images[0], "FooBar"])
 
 
+class TestBlendFilePathForeach(TestBlendLibLinkHelper):
+    def setUp(self) -> None:
+        super().setUp()
+
+        # File paths can get long, and thus also so can diffs when things go wrong.
+        self.maxDiff = 10240
+        self.testdir = _my_dir.parent / "files/libraries_and_linking"
+
+        bpy.ops.wm.open_mainfile(filepath=str(self.testdir / "library_test_scene.blend"))
+
+        # Name the library data-blocks after their filename, so that they can be reliably identified later.
+        # Otherwise we're stuck with 'Lib', 'Lib.001', etc.
+        for lib_id in bpy.data.libraries[:]:
+            abspath = Path(str(bpy.path.abspath(lib_id.filepath)))
+            lib_id.name = abspath.stem
+
+    def test_without_args(self) -> None:
+        """Test file_path_foreach() without any arguments, it should report everything but packed files."""
+
+        visited_paths = self._file_path_foreach()
+        self.assertEqual({
+            (bpy.data.libraries['direct_linked_A'], self.testdir / "libraries/direct_linked_A.blend"),
+            (bpy.data.libraries['direct_linked_B'], self.testdir / "libraries/direct_linked_B.blend"),
+            (bpy.data.libraries['indirect_datablocks'], self.testdir / "libraries/indirect_datablocks.blend"),
+        }, visited_paths)
+
+    def test_without_skip_packed(self) -> None:
+        """Test file_path_foreach() without the SKIP_PACKED flag."""
+
+        # If this works, it's enough as a test to assume the flags are parsed and
+        # passed to the C++ code correctly. There is no need to exhaustively test
+        # all the flags here.
+        visited_paths = self._file_path_foreach(flags={'SKIP_WEAK_REFERENCES'})
+        self.assertEqual({
+            (bpy.data.libraries['direct_linked_A'], self.testdir / "libraries/direct_linked_A.blend"),
+            (bpy.data.libraries['direct_linked_B'], self.testdir / "libraries/direct_linked_B.blend"),
+            (bpy.data.libraries['indirect_datablocks'], self.testdir / "libraries/indirect_datablocks.blend"),
+            (bpy.data.images['pack.png'], self.testdir / "libraries/pack.png"),
+        }, visited_paths, "testing without SKIP_PACKED")
+
+    @staticmethod
+    def _file_path_foreach(
+        subtypes: set[str] | None = None,
+        visit_keys: set[str] | None = None,
+        flags: set[str] | None = None,
+    ) -> set[tuple[bpy.types.ID, Path]]:
+        """Call bpy.data.file_path_foreach(), returning the visited paths as set of (datablock, path) tuples.
+
+        A set is used because the order of visiting is not relevant.
+        """
+        visisted_paths: set[tuple[bpy.types.ID, Path]] = set()
+
+        def visit_path_fn(owner_id: bpy.types.ID, path: str) -> str | None:
+            abspath = Path(str(bpy.path.abspath(path, library=owner_id.library)))
+            visisted_paths.add((owner_id, abspath))
+
+        # Dynamically build the keyword arguments, because the None values are not
+        # valid, and should be encoded as not having the kwarg.
+        kwargs = {}
+        if subtypes is not None:
+            kwargs['subtypes'] = subtypes
+        if visit_keys is not None:
+            kwargs['visit_keys'] = visit_keys
+        if flags is not None:
+            kwargs['flags'] = flags
+
+        bpy.data.file_path_foreach(visit_path_fn, **kwargs)
+
+        return visisted_paths
+
+
 TESTS = (
     TestBlendUserMap,
     TestBlendFilePathMap,
+    TestBlendFilePathForeach,
 )
 
 
