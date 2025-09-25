7fe081c48ee6a2d18ce9bdf8e4e8f7df01790b9b
Omar Emara <mail@OmarEmara.dev>
2025-09-25 14:29:44 +0300

    Add versioning

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -2445,6 +2445,82 @@ static void do_version_bokeh_blur_pixel_size(bNodeTree &node_tree, bNode &node)
   }
 }
 
+/* The Mix mode of the Mix node previously assumed the alpha of the first input as opposed to
+ * mixing the alpha as well. */
+static void do_version_mix_node_mix_mode(bNodeTree &node_tree, bNode &node)
+{
+  const NodeShaderMix *data = reinterpret_cast<NodeShaderMix *>(node.storage);
+  if (data->data_type != SOCK_RGBA) {
+    return;
+  }
+
+  if (data->blend_type != MA_RAMP_BLEND) {
+    return;
+  }
+
+  blender::bke::node_tree_set_type(node_tree);
+
+  bNodeSocket *first_input = blender::bke::node_find_socket(node, SOCK_IN, "A_Color");
+  bNodeSocket *output = blender::bke::node_find_socket(node, SOCK_OUT, "Result_Color");
+
+  /* Find the link going into the inputs of the node. */
+  bNodeLink *first_link = nullptr;
+  LISTBASE_FOREACH (bNodeLink *, link, &node_tree.links) {
+    if (link->tosock == first_input) {
+      first_link = link;
+    }
+  }
+
+  bNode &separate_node = version_node_add_empty(node_tree, "CompositorNodeSeparateColor");
+  separate_node.parent = node.parent;
+  separate_node.location[0] = node.location[0] - node.width - 20.0f;
+  separate_node.location[1] = node.location[1];
+  NodeCMPCombSepColor *storage = MEM_callocN<NodeCMPCombSepColor>(__func__);
+  storage->mode = CMP_NODE_COMBSEP_COLOR_RGB;
+  separate_node.storage = storage;
+
+  bNodeSocket &separate_input = version_node_add_socket(
+      node_tree, separate_node, SOCK_IN, "NodeSocketColor", "Image");
+  bNodeSocket &separate_alpha_output = version_node_add_socket(
+      node_tree, separate_node, SOCK_OUT, "NodeSocketFloat", "Alpha");
+
+  copy_v4_v4(separate_input.default_value_typed<bNodeSocketValueRGBA>()->value,
+             first_input->default_value_typed<bNodeSocketValueRGBA>()->value);
+  if (first_link) {
+    version_node_add_link(
+        node_tree, *first_link->fromnode, *first_link->fromsock, separate_node, separate_input);
+  }
+
+  bNode *set_alpha_node = blender::bke::node_add_node(
+      nullptr, node_tree, "CompositorNodeSetAlpha");
+  set_alpha_node->parent = node.parent;
+  set_alpha_node->location[0] = node.location[0] + node.width - 20.0f;
+  set_alpha_node->location[1] = node.location[1];
+
+  bNodeSocket *set_alpha_image_input = blender::bke::node_find_socket(
+      *set_alpha_node, SOCK_IN, "Image");
+  bNodeSocket *set_alpha_alpha_input = blender::bke::node_find_socket(
+      *set_alpha_node, SOCK_IN, "Alpha");
+  bNodeSocket *set_alpha_type_input = blender::bke::node_find_socket(
+      *set_alpha_node, SOCK_IN, "Type");
+  bNodeSocket *set_alpha_output = blender::bke::node_find_socket(
+      *set_alpha_node, SOCK_OUT, "Image");
+
+  set_alpha_type_input->default_value_typed<bNodeSocketValueMenu>()->value =
+      CMP_NODE_SETALPHA_MODE_REPLACE_ALPHA;
+  version_node_add_link(node_tree, node, *output, *set_alpha_node, *set_alpha_image_input);
+  version_node_add_link(
+      node_tree, separate_node, separate_alpha_output, *set_alpha_node, *set_alpha_alpha_input);
+
+  LISTBASE_FOREACH_BACKWARD_MUTABLE (bNodeLink *, link, &node_tree.links) {
+    if (link->fromsock == output && link->tonode != set_alpha_node) {
+      version_node_add_link(
+          node_tree, *set_alpha_node, *set_alpha_output, *link->tonode, *link->tosock);
+      blender::bke::node_remove_link(&node_tree, *link);
+    }
+  }
+}
+
 void do_versions_after_linking_500(FileData *fd, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 9)) {
@@ -3623,6 +3699,20 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
         }
       }
     }
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 90)) {
+    FOREACH_NODETREE_BEGIN (bmain, node_tree, id) {
+      if (node_tree->type != NTREE_COMPOSIT) {
+        continue;
+      }
+      LISTBASE_FOREACH (bNode *, node, &node_tree->nodes) {
+        if (node->type_legacy == SH_NODE_MIX) {
+          do_version_mix_node_mix_mode(*node_tree, *node);
+        }
+      }
+    }
+    FOREACH_NODETREE_END;
   }
 
   /**
