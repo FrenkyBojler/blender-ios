blender/blender

commit 327a1925cfa11338d3b400c6d7725a3c63a65bdb

Paint: Rename `curve` and `curve_preset`
This commit renames the following DNA fields and corresponding RNA
properties to better represnt usage:

* `Brush.curve` to `Brush.curve_distance_falloff`
* `Brush.curve_preset` to `Brush.curve_distance_falloff_preset`

Pull Request: https://projects.blender.org/blender/blender/pulls/146254

scripts/startup/bl_ui/properties_grease_pencil_common.py
@@ -184,8 +184,8 @@ class GreasePencilBrushFalloff:
                 row = col.row(align=True)
                 col.prop(brush, "curve_preset", text="")
 
-            if brush.curve_preset == 'CUSTOM':
-                layout.template_curve_mapping(brush, "curve", brush=True, use_negative_slope=True)
+            if brush.curve_distance_falloff_preset == 'CUSTOM':
+                layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True, use_negative_slope=True)
 
                 col = layout.column(align=True)
                 row = col.row(align=True)

scripts/startup/bl_ui/properties_paint_common.py
@@ -630,7 +630,7 @@ class FalloffPanel(BrushPanel):
         if not super().poll(context):
             return False
         settings = cls.paint_settings(context)
-        if not (settings and settings.brush and settings.brush.curve):
+        if not (settings and settings.brush and settings.brush.curve_distance_falloff):
             return False
         if cls.get_brush_mode(context) == 'SCULPT_CURVES':
             brush = settings.brush
@@ -654,8 +654,8 @@ class FalloffPanel(BrushPanel):
             row = col.row(align=True)
             col.prop(brush, "curve_preset", text="")
 
-        if brush.curve_preset == 'CUSTOM':
-            layout.template_curve_mapping(brush, "curve", brush=True, use_negative_slope=True)
+        if brush.curve_distance_falloff_preset == 'CUSTOM':
+            layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True, use_negative_slope=True)
 
             col = layout.column(align=True)
             row = col.row(align=True)

scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -871,8 +871,8 @@ class VIEW3D_PT_tools_weight_gradient(Panel, View3DPaintPanel):
         col = layout.column(align=True)
         col.prop(brush, "curve_preset", expand=True)
 
-        if brush.curve_preset == 'CUSTOM':
-            layout.template_curve_mapping(brush, "curve", brush=True)
+        if brush.curve_distance_falloff_preset == 'CUSTOM':
+            layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True)
 
             col = layout.column(align=True)
             row = col.row(align=True)
@@ -1616,7 +1616,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_weight_falloff(GreasePencilBrushFallof
         tool_settings = context.tool_settings
         settings = tool_settings.gpencil_weight_paint
         brush = settings.brush
-        return (brush and brush.curve)
+        return (brush and brush.curve_distance_falloff)
 
 
 class VIEW3D_PT_tools_grease_pencil_weight_options(Panel, View3DPanel, GreasePencilWeightPanel):
@@ -1719,7 +1719,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_vertex_falloff(GreasePencilBrushFallof
     def poll(cls, context):
         tool_settings = context.tool_settings
         settings = tool_settings.gpencil_vertex_paint
-        return (settings and settings.brush and settings.brush.curve)
+        return (settings and settings.brush and settings.brush.curve_distance_falloff)
 
 
 class VIEW3D_PT_tools_grease_pencil_brush_vertex_palette(View3DPanel, Panel):

source/blender/blenkernel/intern/brush.cc
@@ -82,7 +82,7 @@ static void brush_copy_data(Main * /*bmain*/,
     brush_dst->preview = nullptr;
   }
 
-  brush_dst->curve = BKE_curvemapping_copy(brush_src->curve);
+  brush_dst->curve_distance_falloff = BKE_curvemapping_copy(brush_src->curve_distance_falloff);
   brush_dst->automasking_cavity_curve = BKE_curvemapping_copy(brush_src->automasking_cavity_curve);
 
   brush_dst->curve_rand_hue = BKE_curvemapping_copy(brush_src->curve_rand_hue);
@@ -130,7 +130,7 @@ static void brush_copy_data(Main * /*bmain*/,
 static void brush_free_data(ID *id)
 {
   Brush *brush = reinterpret_cast<Brush *>(id);
-  BKE_curvemapping_free(brush->curve);
+  BKE_curvemapping_free(brush->curve_distance_falloff);
   BKE_curvemapping_free(brush->automasking_cavity_curve);
 
   BKE_curvemapping_free(brush->curve_rand_hue);
@@ -236,8 +236,8 @@ static void brush_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   BLO_write_id_struct(writer, Brush, id_address, &brush->id);
   BKE_id_blend_write(writer, &brush->id);
 
-  if (brush->curve) {
-    BKE_curvemapping_blend_write(writer, brush->curve);
+  if (brush->curve_distance_falloff) {
+    BKE_curvemapping_blend_write(writer, brush->curve_distance_falloff);
   }
 
   if (brush->automasking_cavity_curve) {
@@ -311,12 +311,12 @@ static void brush_blend_read_data(BlendDataReader *reader, ID *id)
   Brush *brush = reinterpret_cast<Brush *>(id);
 
   /* Falloff curve. */
-  BLO_read_struct(reader, CurveMapping, &brush->curve);
+  BLO_read_struct(reader, CurveMapping, &brush->curve_distance_falloff);
 
   BLO_read_struct(reader, ColorBand, &brush->gradient);
 
-  if (brush->curve) {
-    BKE_curvemapping_blend_read(reader, brush->curve);
+  if (brush->curve_distance_falloff) {
+    BKE_curvemapping_blend_read(reader, brush->curve_distance_falloff);
   }
   else {
     BKE_brush_curve_preset(brush, CURVE_PRESET_SHARP);
@@ -878,14 +878,14 @@ void BKE_brush_curve_preset(Brush *b, eCurveMappingPreset preset)
   CurveMapping *cumap = nullptr;
   CurveMap *cuma = nullptr;
 
-  if (!b->curve) {
-    b->curve = BKE_curvemapping_add(1, 0, 0, 1, 1);
+  if (!b->curve_distance_falloff) {
+    b->curve_distance_falloff = BKE_curvemapping_add(1, 0, 0, 1, 1);
   }
-  cumap = b->curve;
+  cumap = b->curve_distance_falloff;
   cumap->flag &= ~CUMA_EXTEND_EXTRAPOLATE;
   cumap->preset = preset;
 
-  cuma = b->curve->cm;
+  cuma = b->curve_distance_falloff->cm;
   BKE_curvemap_reset(cuma, &cumap->clipr, cumap->preset, CURVEMAP_SLOPE_NEGATIVE);
   BKE_curvemapping_changed(cumap, false);
   BKE_brush_tag_unsaved_changes(b);
@@ -1617,7 +1617,8 @@ float BKE_brush_curve_strength(const eBrushCurvePreset preset,
 
 float BKE_brush_curve_strength(const Brush *br, float p, const float len)
 {
-  return BKE_brush_curve_strength(eBrushCurvePreset(br->curve_preset), br->curve, p, len);
+  return BKE_brush_curve_strength(
+      eBrushCurvePreset(br->curve_distance_falloff_preset), br->curve_distance_falloff, p, len);
 }
 
 float BKE_brush_curve_strength_clamped(const Brush *br, float p, const float len)
@@ -1666,7 +1667,7 @@ ImBuf *BKE_brush_gen_radial_control_imbuf(Brush *br, bool secondary, bool displa
   int side = 512;
   int half = side / 2;
 
-  BKE_curvemapping_init(br->curve);
+  BKE_curvemapping_init(br->curve_distance_falloff);
 
   float *rect_float = MEM_calloc_arrayN<float>(size_t(side) * size_t(side), "radial control rect");
   IMB_assign_float_buffer(im, rect_float, IB_DO_NOT_TAKE_OWNERSHIP);

source/blender/blenkernel/intern/paint.cc
@@ -284,7 +284,7 @@ void BKE_paint_invalidate_cursor_overlay(Scene *scene, ViewLayer *view_layer, Cu
   }
 
   Brush *br = BKE_paint_brush(paint);
-  if (br && br->curve == curve) {
+  if (br && br->curve_distance_falloff == curve) {
     overlay_flags |= PAINT_OVERLAY_INVALID_CURVE;
   }
 }

source/blender/blenloader/intern/versioning_250.cc
@@ -1391,8 +1391,8 @@ void blo_do_versions_250(FileData *fd, Library * /*lib*/, Main *bmain)
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 252, 1)) {
     LISTBASE_FOREACH (Brush *, brush, &bmain->brushes) {
-      if (brush->curve) {
-        brush->curve->preset = CURVE_PRESET_SMOOTH;
+      if (brush->curve_distance_falloff) {
+        brush->curve_distance_falloff->preset = CURVE_PRESET_SMOOTH;
       }
     }
 

source/blender/blenloader/intern/versioning_280.cc
@@ -838,8 +838,8 @@ static void do_version_curvemapping_walker(Main *bmain, void (*callback)(CurveMa
   FOREACH_NODETREE_END;
 
   LISTBASE_FOREACH (Brush *, brush, &bmain->brushes) {
-    if (brush->curve) {
-      callback(brush->curve);
+    if (brush->curve_distance_falloff) {
+      callback(brush->curve_distance_falloff);
     }
     if (brush->gpencil_settings) {
       if (brush->gpencil_settings->curve_sensitivity) {

source/blender/blenloader/intern/versioning_300.cc
@@ -3302,7 +3302,7 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
         br->spacing = 5;
         br->flag &= ~BRUSH_ALPHA_PRESSURE;
         br->flag &= ~BRUSH_SPACE_ATTEN;
-        br->curve_preset = BRUSH_CURVE_SPHERE;
+        br->curve_distance_falloff_preset = BRUSH_CURVE_SPHERE;
       }
     }
 
@@ -3456,7 +3456,7 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
         br->spacing = 5;
         br->flag &= ~BRUSH_ALPHA_PRESSURE;
         br->flag &= ~BRUSH_SPACE_ATTEN;
-        br->curve_preset = BRUSH_CURVE_SPHERE;
+        br->curve_distance_falloff_preset = BRUSH_CURVE_SPHERE;
       }
     }
 

FrenkyBojler/blender-ios

commit bbbdb29caaeb3744f3cf0d4c525c79dbfbcc4fdf

Fix: Python errors in brush falloff panel
Caused by 327a1925cfa11338d3b400c6d7725a3c63a65bdb

scripts/startup/bl_ui/properties_grease_pencil_common.py
@@ -179,10 +179,10 @@ class GreasePencilBrushFalloff:
 
             col = layout.column(align=True)
             if context.region.type == 'TOOL_HEADER':
-                col.prop(brush, "curve_preset", expand=True)
+                col.prop(brush, "curve_distance_falloff_preset", expand=True)
             else:
                 row = col.row(align=True)
-                col.prop(brush, "curve_preset", text="")
+                col.prop(brush, "curve_distance_falloff_preset", text="")
 
             if brush.curve_distance_falloff_preset == 'CUSTOM':
                 layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True, use_negative_slope=True)

scripts/startup/bl_ui/properties_paint_common.py
@@ -649,10 +649,10 @@ class FalloffPanel(BrushPanel):
 
         col = layout.column(align=True)
         if context.region.type == 'TOOL_HEADER':
-            col.prop(brush, "curve_preset", expand=True)
+            col.prop(brush, "curve_distance_falloff_preset", expand=True)
         else:
             row = col.row(align=True)
-            col.prop(brush, "curve_preset", text="")
+            col.prop(brush, "curve_distance_falloff_preset", text="")
 
         if brush.curve_distance_falloff_preset == 'CUSTOM':
             layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True, use_negative_slope=True)

scripts/startup/bl_ui/space_image.py
@@ -1411,9 +1411,9 @@ class IMAGE_PT_uv_sculpt_curve(Panel):
         props = context.scene.tool_settings.uv_sculpt
 
         col = layout.column()
-        col.prop(props, "curve_preset", expand=True)
+        col.prop(props, "curve_distance_falloff_preset", expand=True)
 
-        if props.curve_preset == 'CUSTOM':
+        if props.curve_distance_falloff_preset == 'CUSTOM':
             col = layout.column()
             col.template_curve_mapping(props, "strength_curve")
 

scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -869,7 +869,7 @@ class VIEW3D_PT_tools_weight_gradient(Panel, View3DPaintPanel):
         brush = settings.brush
 
         col = layout.column(align=True)
-        col.prop(brush, "curve_preset", expand=True)
+        col.prop(brush, "curve_distance_falloff_preset", expand=True)
 
         if brush.curve_distance_falloff_preset == 'CUSTOM':
             layout.template_curve_mapping(brush, "curve_distance_falloff", brush=True)