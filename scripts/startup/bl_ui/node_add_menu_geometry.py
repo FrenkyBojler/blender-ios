15efc034708ce288d7687ec47cbb825d2a15410a
Brady Johnston <brady.johnston@me.com>
2025-09-27 18:40:58 +0100

    move nodes to "Volumes/Operations"

--- scripts/startup/bl_ui/node_add_menu_geometry.py
+++ scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -919,12 +919,6 @@ class NODE_MT_gn_volume_sample_base(node_add_menu.NodeMenu):
     def draw(self, context):
         del context
         layout = self.layout
-        self.node_operator(layout, "GeometryNodeGridClosestPoint")
-        self.node_operator(layout, "GeometryNodeGridCurl")
-        self.node_operator(layout, "GeometryNodeGridDivergence")
-        self.node_operator(layout, "GeometryNodeGridGradient")
-        self.node_operator(layout, "GeometryNodeGridLaplacian")
-        self.node_operator(layout, "GeometryNodeGridMeanCurvature")
         self.node_operator(layout, "GeometryNodeSampleGrid")
         self.node_operator(layout, "GeometryNodeSampleGridIndex")
 
@@ -941,6 +935,12 @@ class NODE_MT_gn_volume_operations_base(node_add_menu.NodeMenu):
         if context.preferences.experimental.use_new_volume_nodes:
             self.node_operator(layout, "GeometryNodeGridToMesh")
             self.node_operator(layout, "GeometryNodeSDFGridBoolean")
+            self.node_operator(layout, "GeometryNodeGridClosestPoint")
+            self.node_operator(layout, "GeometryNodeGridCurl")
+            self.node_operator(layout, "GeometryNodeGridDivergence")
+            self.node_operator(layout, "GeometryNodeGridGradient")
+            self.node_operator(layout, "GeometryNodeGridLaplacian")
+            self.node_operator(layout, "GeometryNodeGridMeanCurvature")
 
         self.draw_assets_for_catalog(layout, self.menu_path)
 
