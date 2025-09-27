0b86f47cde817ea13b4f3bc492eaff62e13c2d89
Jacques Lucke <jacques@blender.org>
2025-09-27 19:19:19 +0200

    remove unnecessary includes

--- source/blender/nodes/geometry/nodes/node_geo_join_bundle.cc
+++ source/blender/nodes/geometry/nodes/node_geo_join_bundle.cc
@@ -2,10 +2,6 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BKE_mesh.hh"
-
-#include "BLI_math_vector_types.hh"
-
 #include "NOD_geometry_nodes_bundle.hh"
 
 #include "node_geometry_util.hh"
