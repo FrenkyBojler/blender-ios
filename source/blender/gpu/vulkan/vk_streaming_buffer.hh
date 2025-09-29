2b4b54962fbf154a7fb989731e3724631b04567e
Jeroen Bakker <jeroen@blender.org>
2025-09-29 14:23:15 +0200

    Cleanup: Make format

--- source/blender/gpu/vulkan/vk_streaming_buffer.hh
+++ source/blender/gpu/vulkan/vk_streaming_buffer.hh
@@ -2,28 +2,29 @@
 
 #pragma once
 
+#include "render_graph/vk_render_graph.hh"
 #include "vk_common.hh"
 #include "vk_staging_buffer.hh"
-#include "render_graph/vk_render_graph.hh"
 
 namespace blender::gpu {
-    class VKBuffer;
-    class VKContext;
- 
-    class VKStreamingBuffer {
-        std::optional<std::unique_ptr<VKBuffer>> host_buffer_;
-        VkBuffer vk_buffer_dst_;
-        VkDeviceSize vk_buffer_size_;
-        VkDeviceSize offset_ = 0;
-        render_graph::NodeHandle copy_buffer_handle_ = 0;
+class VKBuffer;
+class VKContext;
 
-        public:
-        VKStreamingBuffer(VKBuffer&buffer);
-        ~VKStreamingBuffer();
-        VkDeviceSize update(VKContext&context, const void*data, size_t data_size);
+class VKStreamingBuffer {
+  std::optional<std::unique_ptr<VKBuffer>> host_buffer_;
+  VkBuffer vk_buffer_dst_;
+  VkDeviceSize vk_buffer_size_;
+  VkDeviceSize offset_ = 0;
+  render_graph::NodeHandle copy_buffer_handle_ = 0;
 
-        VkBuffer vk_buffer_dst() {
-            return vk_buffer_dst_;
-        }
-    };
-}
+ public:
+  VKStreamingBuffer(VKBuffer &buffer);
+  ~VKStreamingBuffer();
+  VkDeviceSize update(VKContext &context, const void *data, size_t data_size);
+
+  VkBuffer vk_buffer_dst()
+  {
+    return vk_buffer_dst_;
+  }
+};
+}  // namespace blender::gpu
