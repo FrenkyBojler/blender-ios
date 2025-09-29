430247d3119e44e7f719777d9efbd8a4a3ca9839
Jeroen Bakker <jeroen@blender.org>
2025-09-29 14:54:56 +0200

    Added documentation

--- source/blender/gpu/vulkan/vk_streaming_buffer.cc
+++ source/blender/gpu/vulkan/vk_streaming_buffer.cc
@@ -1,3 +1,11 @@
+/* SPDX-FileCopyrightText: 2022 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup gpu
+ */
+
 #include "vk_streaming_buffer.hh"
 #include "vk_buffer.hh"
 #include "vk_context.hh"
