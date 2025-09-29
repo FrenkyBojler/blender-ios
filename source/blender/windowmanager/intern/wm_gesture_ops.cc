ac3f62caf02b2e75990c472d6dc9a1dc6e69e459
Ramon Klauck <ramonklauck3@gmail.com>
2025-09-29 17:58:22 +0200

    delete `case EVT_FKEY`

--- source/blender/windowmanager/intern/wm_gesture_ops.cc
+++ source/blender/windowmanager/intern/wm_gesture_ops.cc
@@ -272,25 +272,6 @@ wmOperatorStatus WM_gesture_box_modal(bContext *C, wmOperator *op, const wmEvent
 
         break;
       }
-      case EVT_FKEY: {
-        if (event->val == KM_PRESS) {
-          int window_cursor_x, window_cursor_y;
-          if (wm_cursor_position_get(win, &window_cursor_x, &window_cursor_y)) {
-            const int diff_cursor_x = window_cursor_x - event->mval[0];
-            const int diff_cursor_y = window_cursor_y - event->mval[1];
-            WM_cursor_warp(win, rect->xmin + diff_cursor_x, rect->ymin + diff_cursor_y);
-            /* Rotate rect values. */
-            int tmp = rect->xmin;
-            rect->xmin = rect->xmax;
-            rect->xmax = tmp;
-
-            tmp = rect->ymin;
-            rect->ymin = rect->ymax;
-            rect->ymax = tmp;
-          }
-        }
-        return OPERATOR_RUNNING_MODAL;
-      }
 #ifdef WITH_INPUT_NDOF
       case NDOF_MOTION: {
         return OPERATOR_PASS_THROUGH;
