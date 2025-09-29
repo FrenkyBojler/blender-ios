aed333b158c4e2b829c3dee44e85de4df0c72222
Hans Goudey <hans@blender.org>
2025-09-29 10:37:03 -0400

    Formatting

--- scripts/startup/bl_ui/properties_data_modifier.py
+++ scripts/startup/bl_ui/properties_data_modifier.py
@@ -49,7 +49,8 @@ class ModifierAddMenu:
         )
         props.asset_library_type = 'ESSENTIALS'
         props.asset_library_identifier = ""
-        props.relative_asset_identifier="geometry_nodes/geometry_nodes_essentials.blend/NodeTree/" + name
+        props.relative_asset_identifier = "geometry_nodes/geometry_nodes_essentials.blend/NodeTree/" + name
+
 
 class DATA_PT_modifiers(ModifierButtonsPanel, Panel):
     bl_label = "Modifiers"
