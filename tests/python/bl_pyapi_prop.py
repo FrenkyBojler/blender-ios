blender/blender

commit 469f54f4845d6fe5dfdff990ded89a4e97b7fd98

BPY: Implement get_transform and set_transform for runtime-defined RNA properties.
Improve handling of runtime defined python RNA properties. Mainly:
* Add `get_transform` and `set_transform` new callbacks.
  These allow to edit the value, while still using the default
  (IDProperty-based) storage system.
* Read-only properties should now be defined using a new `options` flag,
  `READ_ONLY`.
* `get`/`set` should only be used when storing data outside of the
  default system now.
  * Having a `get` without a `set` defined forces property to be
    read-only (same behavior as before).
  * Having a `set` without a `get` is now an error.
* Just like with existing `get/set` callbacks, `get_/set_transform`
  callbacks must always generate values matching the constraints defined
  by their `bpy.props` property definition (same type, within required
  range, same dimensions/sizes for the `Vector` properties, etc.).
* To simplify handling of non-statically sized strings, the relevant
  RNA API has been modified, to use `std::string` instead of
  (allocated) char arrays.

Relevant unittests and benchmarking have been added or updated as part
of this project.

Note: From initial benchmarking, 'transform' versions of get/set are
several times faster than 'real' get/set.

Implements #141042.

Pull Request: https://projects.blender.org/blender/blender/pulls/141303

tests/python/bl_pyapi_prop.py
@@ -45,33 +45,70 @@ class TestPropNumerical(unittest.TestCase):
         id_type.test_float = FloatProperty(default=float(self.default_value))
 
         self.test_bool_storage = bool(self.custom_value)
+        self.test_int_storage = int(self.custom_value)
+        self.test_float_storage = float(self.custom_value)
 
-        def set_(s, v):
+        def bool_set_(s, v):
             self.test_bool_storage = v
+
+        def int_set_(s, v):
+            self.test_int_storage = v
+
+        def float_set_(s, v):
+            self.test_float_storage = v
+
         id_type.test_bool_getset = BoolProperty(
             default=bool(self.default_value),
             get=lambda s: self.test_bool_storage,
-            set=set_,
+            set=bool_set_,
         )
-
-        self.test_int_storage = int(self.custom_value)
-
-        def set_(s, v):
-            self.test_int_storage = v
         id_type.test_int_getset = IntProperty(
             default=int(self.default_value),
             get=lambda s: self.test_int_storage,
-            set=set_,
+            set=int_set_,
         )
-
-        self.test_float_storage = float(self.custom_value)
-
-        def set_(s, v):
-            self.test_float_storage = v
         id_type.test_float_getset = FloatProperty(
             default=float(self.default_value),
             get=lambda s: self.test_float_storage,
-            set=set_,
+            set=float_set_,
+        )
+
+        id_type.test_bool_transform = BoolProperty(
+            default=bool(self.default_value),
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+        id_type.test_int_transform = IntProperty(
+            default=int(self.default_value),
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+        id_type.test_float_transform = FloatProperty(
+            default=float(self.default_value),
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+
+        id_type.test_bool_getset_transform = BoolProperty(
+            default=bool(self.default_value),
+            get=lambda s: self.test_bool_storage,
+            set=bool_set_,
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+        id_type.test_int_getset_transform = IntProperty(
+            default=int(self.default_value),
+            get=lambda s: self.test_int_storage,
+            set=int_set_,
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+        id_type.test_float_getset_transform = FloatProperty(
+            default=float(self.default_value),
+            get=lambda s: self.test_float_storage,
+            set=float_set_,
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
         )
 
     def tearDown(self):
@@ -82,6 +119,14 @@ class TestPropNumerical(unittest.TestCase):
         del id_type.test_float_getset
         del id_type.test_int_getset
         del id_type.test_bool_getset
+
+        del id_type.test_float_transform
+        del id_type.test_int_transform
+        del id_type.test_bool_transform
+
+        del id_type.test_float_getset_transform
+        del id_type.test_int_getset_transform
+        del id_type.test_bool_getset_transform
 
     def do_test_access(self, prop_name, py_type, expected_value):
         v = getattr(id_inst, prop_name)
@@ -109,6 +154,24 @@ class TestPropNumerical(unittest.TestCase):
 
     def test_access_float_getset(self):
         self.do_test_access("test_float_getset", float, float(self.custom_value))
+
+    def test_access_bool_transform(self):
+        self.do_test_access("test_bool_transform", bool, bool(self.default_value))
+
+    def test_access_int_transform(self):
+        self.do_test_access("test_int_transform", int, int(self.default_value))
+
+    def test_access_float_transform(self):
+        self.do_test_access("test_float_transform", float, float(self.default_value))
+
+    def test_access_bool_getset_transform(self):
+        self.do_test_access("test_bool_getset_transform", bool, bool(self.custom_value))
+
+    def test_access_int_getset_transform(self):
+        self.do_test_access("test_int_getset_transform", int, int(self.custom_value))
+
+    def test_access_float_getset_transform(self):
+        self.do_test_access("test_float_getset_transform", float, float(self.custom_value))
 
     # TODO: Add expected failure cases (e.g. handling of out-of range values).
 
@@ -171,28 +234,63 @@ class TestPropEnum(unittest.TestCase):
         )
 
         self.test_enum_storage = self.enum_expected_values[self.custom_value]
-
-        def set_(s, v):
-            self.test_enum_storage = v
-        id_type.test_enum_getset = EnumProperty(
-            items=self.enum_items,
-            default=self.default_value,
-            get=lambda s: self.test_enum_storage,
-            set=set_,
-        )
-
         self.test_enum_bitflag_storage = functools.reduce(
             lambda a, b: a | b,
             (self.enum_expected_bitflag_values[bf] for bf in self.custom_bitflag_value))
 
-        def set_(s, v):
+        def enum_set_(s, v):
+            self.test_enum_storage = v
+
+        def bitflag_set_(s, v):
             self.test_enum_bitflag_storage = v
+
+        id_type.test_enum_getset = EnumProperty(
+            items=self.enum_items,
+            default=self.default_value,
+            get=lambda s: self.test_enum_storage,
+            set=enum_set_,
+        )
+
         id_type.test_enum_bitflag_getset = EnumProperty(
             items=self.enum_items,
             default=self.default_bitflag_value,
             options={"ENUM_FLAG"},
             get=lambda s: self.test_enum_bitflag_storage,
-            set=set_,
+            set=bitflag_set_,
+        )
+
+        id_type.test_enum_transform = EnumProperty(
+            items=self.enum_items,
+            default=self.default_value,
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+
+        id_type.test_enum_bitflag_transform = EnumProperty(
+            items=self.enum_items,
+            default=self.default_bitflag_value,
+            options={"ENUM_FLAG"},
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+
+        id_type.test_enum_getset_transform = EnumProperty(
+            items=self.enum_items,
+            default=self.default_value,
+            get=lambda s: self.test_enum_storage,
+            set=enum_set_,
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+        )
+
+        id_type.test_enum_bitflag_getset_transform = EnumProperty(
+            items=self.enum_items,
+            default=self.default_bitflag_value,
+            options={"ENUM_FLAG"},
+            get_transform=lambda s, c_v, isset: c_v,
+            set_transform=lambda s, n_v, c_v, isset: n_v,
+            get=lambda s: self.test_enum_bitflag_storage,
+            set=bitflag_set_,
         )
 
     def tearDown(self):
@@ -200,6 +298,10 @@ class TestPropEnum(unittest.TestCase):
         del id_type.test_enum_bitflag
         del id_type.test_enum_getset
         del id_type.test_enum_bitflag_getset
+        del id_type.test_enum_transform
+        del id_type.test_enum_bitflag_transform
+        del id_type.test_enum_getset_transform
+        del id_type.test_enum_bitflag_getset_transform
 
     # Test expected generated values for enum items.
     def do_test_enum_values(self, prop_name, expected_item_values):
@@ -214,6 +316,24 @@ class TestPropEnum(unittest.TestCase):
 
     def test_enum_bitflag_item_values(self):
         self.do_test_enum_values("test_enum_bitflag", self.enum_expected_bitflag_values)
+
+    def test_enum_getset_item_values(self):
+        self.do_test_enum_values("test_enum_getset", self.enum_expected_values)
+
+    def test_enum_bitflag_getset_item_values(self):
+        self.do_test_enum_values("test_enum_bitflag_getset", self.enum_expected_bitflag_values)
+
+    def test_enum_transform_item_values(self):
+        self.do_test_enum_values("test_enum_transform", self.enum_expected_values)
+
+    def test_enum_bitflag_transform_item_values(self):
+        self.do_test_enum_values("test_enum_bitflag_transform", self.enum_expected_bitflag_values)
+
+    def test_enum_getset_transform_item_values(self):
+        self.do_test_enum_values("test_enum_getset_transform", self.enum_expected_values)
+
+    def test_enum_bitflag_getset_transform_item_values(self):
+        self.do_test_enum_values("test_enum_bitflag_getset_transform", self.enum_expected_bitflag_values)
 
     # Test basic access to enum values.
     def do_test_access(self, prop_name, py_type, expected_value):
@@ -233,6 +353,18 @@ class TestPropEnum(unittest.TestCase):
 
     def test_access_enum_bitflag_getset(self):
         self.do_test_access("test_enum_bitflag_getset", set, self.custom_bitflag_value)
+
+    def test_access_enum_transform(self):
+        self.do_test_access("test_enum_transform", str, self.default_value)
+
+    def test_access_enum_bitflag_transform(self):
+        self.do_test_access("test_enum_bitflag_transform", set, self.default_bitflag_value)
+
+    def test_access_enum_getset_transform(self):
+        self.do_test_access("test_enum_getset_transform", str, self.custom_value)
+
+    def test_access_enum_bitflag_getset_transform(self):
+        self.do_test_access("test_enum_bitflag_getset_transform", set, self.custom_bitflag_value)
 
     # TODO: Add expected failure cases (e.g. handling of invalid items identifiers).
 

source/blender/editors/gizmo_library/gizmo_types/primitive3d_gizmo.cc
@@ -280,6 +280,8 @@ static void GIZMO_GT_primitive_3d(wmGizmoType *gzt)
   RNA_def_property_enum_funcs_runtime(prop,
                                       gizmo_primitive_rna__draw_style_get_fn,
                                       gizmo_primitive_rna__draw_style_set_fn,
+                                      nullptr,
+                                      nullptr,
                                       nullptr);
 
   prop = RNA_def_float_factor(
@@ -287,11 +289,16 @@ static void GIZMO_GT_primitive_3d(wmGizmoType *gzt)
   RNA_def_property_float_funcs_runtime(prop,
                                        gizmo_primitive_rna__arc_inner_factor_get_fn,
                                        gizmo_primitive_rna__arc_inner_factor_set_fn,
+                                       nullptr,
+                                       nullptr,
                                        nullptr);
 
   prop = RNA_def_boolean(gzt->srna, "draw_inner", true, "Draw Inner", "");
-  RNA_def_property_boolean_funcs_runtime(
-      prop, gizmo_primitive_rna__draw_inner_get_fn, gizmo_primitive_rna__draw_inner_set_fn);
+  RNA_def_property_boolean_funcs_runtime(prop,
+                                         gizmo_primitive_rna__draw_inner_get_fn,
+                                         gizmo_primitive_rna__draw_inner_set_fn,
+                                         nullptr,
+                                         nullptr);
 }
 
 void ED_gizmotypes_primitive_3d()

source/blender/editors/gizmo_library/gizmo_types/snap3d_gizmo.cc
@@ -339,8 +339,12 @@ static void GIZMO_GT_snap_3d(wmGizmoType *gzt)
                              "Point that defines the location of the perpendicular snap",
                              FLT_MIN,
                              FLT_MAX);
-  RNA_def_property_float_array_funcs_runtime(
-      prop, gizmo_snap_rna_prevpoint_get_fn, gizmo_snap_rna_prevpoint_set_fn, nullptr);
+  RNA_def_property_float_array_funcs_runtime(prop,
+                                             gizmo_snap_rna_prevpoint_get_fn,
+                                             gizmo_snap_rna_prevpoint_set_fn,
+                                             nullptr,
+                                             nullptr,
+                                             nullptr);
 
   /* Returns. */
   prop = RNA_def_float_translation(gzt->srna,
@@ -353,8 +357,12 @@ static void GIZMO_GT_snap_3d(wmGizmoType *gzt)
                                    "Snap Point Location",
                                    FLT_MIN,
                                    FLT_MAX);
-  RNA_def_property_float_array_funcs_runtime(
-      prop, gizmo_snap_rna_location_get_fn, gizmo_snap_rna_location_set_fn, nullptr);
+  RNA_def_property_float_array_funcs_runtime(prop,
+                                             gizmo_snap_rna_location_get_fn,
+                                             gizmo_snap_rna_location_set_fn,
+                                             nullptr,
+                                             nullptr,
+                                             nullptr);
 
   prop = RNA_def_float_vector_xyz(gzt->srna,
                                   "normal",
@@ -366,7 +374,8 @@ static void GIZMO_GT_snap_3d(wmGizmoType *gzt)
                                   "Snap Point Normal",
                                   FLT_MIN,
                                   FLT_MAX);
-  RNA_def_property_float_array_funcs_runtime(prop, gizmo_snap_rna_normal_get_fn, nullptr, nullptr);
+  RNA_def_property_float_array_funcs_runtime(
+      prop, gizmo_snap_rna_normal_get_fn, nullptr, nullptr, nullptr, nullptr);
 
   prop = RNA_def_int_vector(gzt->srna,
                             "snap_elem_index",
@@ -379,7 +388,7 @@ static void GIZMO_GT_snap_3d(wmGizmoType *gzt)
                             INT_MIN,
                             INT_MAX);
   RNA_def_property_int_array_funcs_runtime(
-      prop, gizmo_snap_rna_snap_elem_index_get_fn, nullptr, nullptr);
+      prop, gizmo_snap_rna_snap_elem_index_get_fn, nullptr, nullptr, nullptr, nullptr);
 
   prop = RNA_def_enum(gzt->srna,
                       "snap_source_type",
@@ -390,6 +399,8 @@ static void GIZMO_GT_snap_3d(wmGizmoType *gzt)
   RNA_def_property_enum_funcs_runtime(prop,
                                       gizmo_snap_rna_snap_srouce_type_get_fn,
                                       gizmo_snap_rna_snap_srouce_type_set_fn,
+                                      nullptr,
+                                      nullptr,
                                       nullptr);
 }
 

source/blender/editors/object/object_data_transfer.cc
@@ -783,7 +783,8 @@ void OBJECT_OT_data_transfer(wmOperatorType *ot)
                       DT_LAYERS_ACTIVE_SRC,
                       "Source Layers Selection",
                       "Which layers to transfer, in case of multi-layers types");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_layers_select_itemf);
+  RNA_def_property_enum_funcs_runtime(
+      prop, nullptr, nullptr, dt_layers_select_itemf, nullptr, nullptr);
 
   prop = RNA_def_enum(ot->srna,
                       "layers_select_dst",
@@ -791,7 +792,8 @@ void OBJECT_OT_data_transfer(wmOperatorType *ot)
                       DT_LAYERS_ACTIVE_DST,
                       "Destination Layers Matching",
                       "How to match source and destination layers");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_layers_select_itemf);
+  RNA_def_property_enum_funcs_runtime(
+      prop, nullptr, nullptr, dt_layers_select_itemf, nullptr, nullptr);
 
   prop = RNA_def_enum(ot->srna,
                       "mix_mode",
@@ -799,7 +801,7 @@ void OBJECT_OT_data_transfer(wmOperatorType *ot)
                       CDT_MIX_TRANSFER,
                       "Mix Mode",
                       "How to affect destination elements with source values");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_mix_mode_itemf);
+  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_mix_mode_itemf, nullptr, nullptr);
   RNA_def_float(
       ot->srna,
       "mix_factor",
@@ -948,7 +950,8 @@ void OBJECT_OT_datalayout_transfer(wmOperatorType *ot)
                       DT_LAYERS_ACTIVE_SRC,
                       "Source Layers Selection",
                       "Which layers to transfer, in case of multi-layers types");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_layers_select_src_itemf);
+  RNA_def_property_enum_funcs_runtime(
+      prop, nullptr, nullptr, dt_layers_select_src_itemf, nullptr, nullptr);
 
   prop = RNA_def_enum(ot->srna,
                       "layers_select_dst",
@@ -956,7 +959,8 @@ void OBJECT_OT_datalayout_transfer(wmOperatorType *ot)
                       DT_LAYERS_ACTIVE_DST,
                       "Destination Layers Matching",
                       "How to match source and destination layers");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, dt_layers_select_dst_itemf);
+  RNA_def_property_enum_funcs_runtime(
+      prop, nullptr, nullptr, dt_layers_select_dst_itemf, nullptr, nullptr);
 }
 
 }  // namespace blender::ed::object

source/blender/editors/space_outliner/outliner_edit.cc
@@ -762,7 +762,7 @@ void OUTLINER_OT_id_remap(wmOperatorType *ot)
 
   prop = RNA_def_enum(
       ot->srna, "old_id", rna_enum_dummy_NULL_items, 0, "Old ID", "Old ID to replace");
-  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, outliner_id_itemf);
+  RNA_def_property_enum_funcs_runtime(prop, nullptr, nullptr, outliner_id_itemf, nullptr, nullptr);
   RNA_def_property_flag(prop, PROP_ENUM_NO_TRANSLATE | PROP_HIDDEN);
 
   ot->prop = RNA_def_enum(ot->srna,
@@ -771,7 +771,8 @@ void OUTLINER_OT_id_remap(wmOperatorType *ot)
                           0,
                           "New ID",
                           "New ID to remap all selected IDs' users to");
-  RNA_def_property_enum_funcs_runtime(ot->prop, nullptr, nullptr, outliner_id_itemf);
+  RNA_def_property_enum_funcs_runtime(
+      ot->prop, nullptr, nullptr, outliner_id_itemf, nullptr, nullptr);
   RNA_def_property_flag(ot->prop, PROP_ENUM_NO_TRANSLATE);
 }
 

source/blender/makesrna/RNA_access.hh
@@ -541,7 +541,8 @@ std::optional<std::string> RNA_property_string_path_filter(const bContext *C,
                                                            PropertyRNA *prop);
 
 /**
- * \return the length without `\0` terminator.
+ * \return The final length without `\0` terminator (might differ from the length of the stored
+ * string, when a `get_transform` callback is defined).
  */
 int RNA_property_string_length(PointerRNA *ptr, PropertyRNA *prop);
 void RNA_property_string_get_default(PropertyRNA *prop, char *value, int value_maxncpy);

FrenkyBojler/blender-ios

commit 62629a2181b926e48c34a8bd0dd48d1d00195c34

Fix #146802: Py-defined property min/max handling is broken.
Regression from 469f54f484, somehow the clamping for default set of Int
properties was removed in this big refactor, merely add it back, and add
some unittests covering min/max handling.

source/blender/makesrna/intern/rna_access.cc
@@ -3108,6 +3108,7 @@ void RNA_property_int_set(PointerRNA *ptr, PropertyRNA *prop, int value)
   }
 
   if (idprop) {
+    RNA_property_int_clamp(ptr, &iprop->property, &value);
     IDP_int_set(idprop, value);
     rna_idproperty_touch(idprop);
   }
@@ -3119,6 +3120,7 @@ void RNA_property_int_set(PointerRNA *ptr, PropertyRNA *prop, int value)
   }
   else if (iprop->property.flag & PROP_EDITABLE) {
     if (IDProperty *group = RNA_struct_system_idprops(ptr, true)) {
+      RNA_property_int_clamp(ptr, &iprop->property, &value);
       IDP_AddToGroup(
           group,
           blender::bke::idprop::create(prop_rna_or_id.identifier, value, IDP_FLAG_STATIC_TYPE)
@@ -3555,6 +3557,7 @@ void RNA_property_float_set(PointerRNA *ptr, PropertyRNA *prop, float value)
   }
 
   if (idprop) {
+    RNA_property_float_clamp(ptr, &fprop->property, &value);
     if (idprop->type == IDP_FLOAT) {
       IDP_float_set(idprop, value);
     }
@@ -3570,8 +3573,6 @@ void RNA_property_float_set(PointerRNA *ptr, PropertyRNA *prop, float value)
     fprop->set_ex(ptr, &fprop->property, value);
   }
   else if (fprop->property.flag & PROP_EDITABLE) {
-    /* FIXME: This is only called here? What about already existing IDProps (see above)? And
-     * similar code for Int properties? */
     RNA_property_float_clamp(ptr, &fprop->property, &value);
     if (IDProperty *group = RNA_struct_system_idprops(ptr, true)) {
       IDP_AddToGroup(

tests/python/bl_pyapi_prop.py
@@ -38,11 +38,21 @@ class TestPropertyGroup(bpy.types.PropertyGroup):
 class TestPropNumerical(unittest.TestCase):
     default_value = 0
     custom_value = 1
+    min_value = -1
+    max_value = 5
 
     def setUp(self):
         id_type.test_bool = BoolProperty(default=bool(self.default_value))
-        id_type.test_int = IntProperty(default=int(self.default_value))
-        id_type.test_float = FloatProperty(default=float(self.default_value))
+        id_type.test_int = IntProperty(
+            default=int(self.default_value),
+            min=int(self.min_value),
+            max=int(self.max_value),
+        )
+        id_type.test_float = FloatProperty(
+            default=float(self.default_value),
+            min=float(self.min_value),
+            max=float(self.max_value),
+        )
 
         self.test_bool_storage = bool(self.custom_value)
         self.test_int_storage = int(self.custom_value)
@@ -64,11 +74,15 @@ class TestPropNumerical(unittest.TestCase):
         )
         id_type.test_int_getset = IntProperty(
             default=int(self.default_value),
+            min=int(self.min_value),
+            max=int(self.max_value),
             get=lambda s: self.test_int_storage,
             set=int_set_,
         )
         id_type.test_float_getset = FloatProperty(
             default=float(self.default_value),
+            min=float(self.min_value),
+            max=float(self.max_value),
             get=lambda s: self.test_float_storage,
             set=float_set_,
         )
@@ -80,11 +94,15 @@ class TestPropNumerical(unittest.TestCase):
         )
         id_type.test_int_transform = IntProperty(
             default=int(self.default_value),
+            min=int(self.min_value),
+            max=int(self.max_value),
             get_transform=lambda s, c_v, isset: c_v,
             set_transform=lambda s, n_v, c_v, isset: n_v,
         )
         id_type.test_float_transform = FloatProperty(
             default=float(self.default_value),
+            min=float(self.min_value),
+            max=float(self.max_value),
             get_transform=lambda s, c_v, isset: c_v,
             set_transform=lambda s, n_v, c_v, isset: n_v,
         )
@@ -98,6 +116,8 @@ class TestPropNumerical(unittest.TestCase):
         )
         id_type.test_int_getset_transform = IntProperty(
             default=int(self.default_value),
+            min=int(self.min_value),
+            max=int(self.max_value),
             get=lambda s: self.test_int_storage,
             set=int_set_,
             get_transform=lambda s, c_v, isset: c_v,
@@ -105,6 +125,8 @@ class TestPropNumerical(unittest.TestCase):
         )
         id_type.test_float_getset_transform = FloatProperty(
             default=float(self.default_value),
+            min=float(self.min_value),
+            max=float(self.max_value),
             get=lambda s: self.test_float_storage,
             set=float_set_,
             get_transform=lambda s, c_v, isset: c_v,
@@ -128,7 +150,22 @@ class TestPropNumerical(unittest.TestCase):
         del id_type.test_int_getset_transform
         del id_type.test_bool_getset_transform
 
-    def do_test_access(self, prop_name, py_type, expected_value):
+    def do_min_max_expect_success(self, prop_name, py_type):
+        # This property is expected to properly clamp set values within required range.
+        setattr(id_inst, prop_name, py_type(self.min_value - 1))
+        self.assertEqual(getattr(id_inst, prop_name), py_type(self.min_value))
+        setattr(id_inst, prop_name, py_type(self.max_value + 1))
+        self.assertEqual(getattr(id_inst, prop_name), py_type(self.max_value))
+
+    def do_min_max_expect_failure(self, prop_name, py_type):
+        # This property is not expected to properly clamp set values within required range.
+        # This happens when using custom setters.
+        setattr(id_inst, prop_name, py_type(self.min_value - 1))
+        self.assertNotEqual(getattr(id_inst, prop_name), py_type(self.min_value))
+        setattr(id_inst, prop_name, py_type(self.max_value + 1))
+        self.assertNotEqual(getattr(id_inst, prop_name), py_type(self.max_value))
+
+    def do_test_access(self, prop_name, py_type, expected_value, do_min_max=None):
         v = getattr(id_inst, prop_name)
         self.assertIsInstance(v, py_type)
         self.assertEqual(v, expected_value)
@@ -136,42 +173,84 @@ class TestPropNumerical(unittest.TestCase):
         self.assertEqual(getattr(id_inst, prop_name), expected_value)
         setattr(id_inst, prop_name, py_type(self.custom_value))
         self.assertEqual(getattr(id_inst, prop_name), py_type(self.custom_value))
+        if do_min_max:
+            do_min_max(prop_name, py_type)
 
     def test_access_bool(self):
         self.do_test_access("test_bool", bool, bool(self.default_value))
 
     def test_access_int(self):
-        self.do_test_access("test_int", int, int(self.default_value))
+        self.do_test_access(
+            "test_int",
+            int,
+            int(self.default_value),
+            do_min_max=self.do_min_max_expect_success,
+        )
 
     def test_access_float(self):
-        self.do_test_access("test_float", float, float(self.default_value))
+        self.do_test_access(
+            "test_float",
+            float,
+            float(self.default_value),
+            do_min_max=self.do_min_max_expect_success,
+        )
 
     def test_access_bool_getset(self):
         self.do_test_access("test_bool_getset", bool, bool(self.custom_value))
 
     def test_access_int_getset(self):
-        self.do_test_access("test_int_getset", int, int(self.custom_value))
+        self.do_test_access(
+            "test_int_getset",
+            int,
+            int(self.custom_value),
+            do_min_max=self.do_min_max_expect_failure,
+        )
 
     def test_access_float_getset(self):
-        self.do_test_access("test_float_getset", float, float(self.custom_value))
+        self.do_test_access(
+            "test_float_getset",
+            float,
+            float(self.custom_value),
+            do_min_max=self.do_min_max_expect_failure,
+        )
 
     def test_access_bool_transform(self):
         self.do_test_access("test_bool_transform", bool, bool(self.default_value))
 
     def test_access_int_transform(self):
-        self.do_test_access("test_int_transform", int, int(self.default_value))
+        self.do_test_access(
+            "test_int_transform",
+            int,
+            int(self.default_value),
+            do_min_max=self.do_min_max_expect_success,
+        )
 
     def test_access_float_transform(self):
-        self.do_test_access("test_float_transform", float, float(self.default_value))
+        self.do_test_access(
+            "test_float_transform",
+            float,
+            float(self.default_value),
+            do_min_max=self.do_min_max_expect_success,
+        )
 
     def test_access_bool_getset_transform(self):
         self.do_test_access("test_bool_getset_transform", bool, bool(self.custom_value))
 
     def test_access_int_getset_transform(self):
-        self.do_test_access("test_int_getset_transform", int, int(self.custom_value))
+        self.do_test_access(
+            "test_int_getset_transform",
+            int,
+            int(self.custom_value),
+            do_min_max=self.do_min_max_expect_failure,
+        )
 
     def test_access_float_getset_transform(self):
-        self.do_test_access("test_float_getset_transform", float, float(self.custom_value))
+        self.do_test_access(
+            "test_float_getset_transform",
+            float,
+            float(self.custom_value),
+            do_min_max=self.do_min_max_expect_failure,
+        )
 
     # TODO: Add expected failure cases (e.g. handling of out-of range values).
 