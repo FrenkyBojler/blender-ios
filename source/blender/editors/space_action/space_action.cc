11dc75adc769450b22e967bd7a3ad6ecf1e74e3a
Falk David <falk@blender.org>
2025-09-29 13:03:33 +0200

    Enable overlays and scene strip range by default

--- source/blender/editors/space_action/space_action.cc
+++ source/blender/editors/space_action/space_action.cc
@@ -72,6 +72,8 @@ static SpaceLink *action_create(const ScrArea *area, const Scene *scene)
                            TIME_CACHE_CLOTH | TIME_CACHE_SMOKE | TIME_CACHE_DYNAMICPAINT |
                            TIME_CACHE_RIGIDBODY | TIME_CACHE_SIMULATION_NODES;
 
+  saction->overlays.flag |= (ADS_OVERLAY_SHOW_OVERLAYS | ADS_SHOW_SCENE_STRIP_FRAME_RANGE);
+
   /* header */
   region = BKE_area_region_new();
 
