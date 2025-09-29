694a246c1263582c7cf8309199eb292f3af82b2b
MattiasF <mattias-fredriksson91@hotmail.se>
2025-09-29 14:28:39 +0200

    Improve language in comment

--- source/blender/blenkernel/intern/curve_poly.cc
+++ source/blender/blenkernel/intern/curve_poly.cc
@@ -61,8 +61,8 @@ static float3 direction_bisect(const float3 &pos,
     if (norm < 2e-7) {     /* Approximately < sin(1e-5) */
       return other_dir;
     }
-    /* Compute using the cross product as catastrophic cancellation occur in `tangent`
-     * when the sum approach 0 generating significant errors, see #146332.
+    /* Compute using the cross product, as catastrophic cancellation occurs in `tangent` 
+     * when the sum approaches 0, leading to significant numerical errors (see #146332).
      */
     const float3 binormal = math::cross(other_dir, prev_dir);
     const float3 normal = other_dir - prev_dir;
