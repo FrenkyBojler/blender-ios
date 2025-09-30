b938e33f6ad6681dee0cfcfb9ece43a61b83709b
Sean Kim <SeanCTKim@protonmail.com>
2025-09-30 12:00:23 -0700

    Fix #132016: UV Sculpt crash with bad tool data

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -3677,6 +3677,24 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
     }
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 96)) {
+    /* For a brief period of time, these values were not properly versioned, so it is possible for
+     * files to be in an odd state. Detect this by looking for either the brush size being 0 or
+     * the curve being invalid.
+     *
+     * This versioning effectively redoes the step from 4.2 subversion 23.
+     */
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      UvSculpt& uvsculpt = scene->toolsettings->uvsculpt;
+      if (uvsculpt.size == 0 || uvsculpt.strength_curve == nullptr) {
+        uvsculpt.size = 100;
+        uvsculpt.strength = 1.0f;
+        uvsculpt.curve_preset = BRUSH_CURVE_SMOOTH;
+        uvsculpt.strength_curve = BKE_curvemapping_add(1, 0.0f, 0.0f, 1.0f, 1.0f);
+      }
+    }
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
