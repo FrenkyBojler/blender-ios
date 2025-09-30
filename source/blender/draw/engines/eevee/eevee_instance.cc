d2a429e3c0deae92a724a32c34af7beffee05fc7
Clément Foucault <foucault.clem@gmail.com>
2025-09-30 15:21:26 +0200

    Add more safety to sample batching and fix display of remaining samples

--- source/blender/draw/engines/eevee/eevee_instance.cc
+++ source/blender/draw/engines/eevee/eevee_instance.cc
@@ -942,10 +942,14 @@ void Instance::light_bake_irradiance(
     context_wrapper([&]() {
       DebugScope debug_scope(debug_scope_irradiance_sample, "EEVEE.irradiance_sample");
 
+      int remaining_samples = sampling.sample_count() - sampling.sample_index();
       /* In background mode, assume we don't need as much interactivity. */
       int time_budget_ms = G.background ? 32 : 16;
       /* Batch ray cast. Avoids too much overhead of the context switch. */
-      int sample_count_in_batch = clamp_i(time_budget_ms / time_per_sample_ms_smooth, 1, 100);
+      int sample_count_in_batch = ceilf(time_budget_ms / max(0.1f, time_per_sample_ms_smooth));
+      /* Avoid batching too many rays, keep system responsive in case of bad values. */
+      sample_count_in_batch = min_iii(32, sample_count_in_batch, remaining_samples);
+
       CLOG_INFO(&Instance::log, "IrradianceBake: Casting %d rays.", sample_count_in_batch);
 
       double time_it_begin_ms = BLI_time_now_seconds() * 1000.0;
