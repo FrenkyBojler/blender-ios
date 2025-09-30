blender/blender

commit 8de2e6f9fb0628d63c2a4a4cffae5a8d391fecbd

UI: Gray out labels for non-editable linked items
Similar to !145387, disable layout when rna property is non editable.
Let the widget code handle the theme of the label in
front of the non-editable property. Also do it separately inside
`v3d_object_dimension_buts` for transform properties of N-panel

Resolves #146290

Pull Request: https://projects.blender.org/blender/blender/pulls/146565

source/blender/editors/space_view3d/view3d_buttons.cc
@@ -1594,7 +1594,8 @@ static void v3d_editvertex_buts(
 
 static void v3d_object_dimension_buts(bContext *C, uiLayout *layout, View3D *v3d, Object *ob)
 {
-  uiBlock *block = (layout) ? layout->absolute_block() : nullptr;
+  uiBlock *block = (layout) ? layout->block() : nullptr;
+  uiLayout &sub_layout = layout->absolute(false);
   TransformProperties *tfp = v3d_transform_props_ensure(v3d);
   const bool is_editable = ID_IS_EDITABLE(&ob->id);
 
@@ -1608,6 +1609,10 @@ static void v3d_object_dimension_buts(bContext *C, uiLayout *layout, View3D *v3d
     copy_v3_v3(tfp->ob_dims_orig, tfp->ob_dims);
     copy_v3_v3(tfp->ob_scale_orig, ob->scale);
     copy_m4_m4(tfp->ob_obmat_orig, ob->object_to_world().ptr());
+
+    if (!is_editable) {
+      sub_layout.enabled_set(false);
+    }
 
     uiDefBut(block,
              ButType::Label,

source/blender/editors/interface/interface_layout.cc
@@ -2100,6 +2100,10 @@ void uiLayout::prop(PointerRNA *ptr,
       uiLayout *layout_sub = &layout_split->column(true);
       layout_sub->space_ = 0;
 
+      if (!RNA_property_editable(ptr, prop)) {
+        layout_split->enabled_set(false);
+      }
+
       if (!use_prop_sep_split_label) {
         /* Pass */
       }

FrenkyBojler/blender-ios

commit 733b864fc2f4ff3a055586379d47453bc592973d

Fix #147014: Crash when manually typing object dimension
Caused by 8de2e6f9fb
Craah due to missing nullptr check for `layout` pointer.

source/blender/editors/space_view3d/view3d_buttons.cc
@@ -1595,7 +1595,7 @@ static void v3d_editvertex_buts(
 static void v3d_object_dimension_buts(bContext *C, uiLayout *layout, View3D *v3d, Object *ob)
 {
   uiBlock *block = (layout) ? layout->block() : nullptr;
-  uiLayout &sub_layout = layout->absolute(false);
+  uiLayout *sub_layout = layout ? &layout->absolute(false) : nullptr;
   TransformProperties *tfp = v3d_transform_props_ensure(v3d);
   const bool is_editable = ID_IS_EDITABLE(&ob->id);
 
@@ -1610,8 +1610,8 @@ static void v3d_object_dimension_buts(bContext *C, uiLayout *layout, View3D *v3d
     copy_v3_v3(tfp->ob_scale_orig, ob->scale);
     copy_m4_m4(tfp->ob_obmat_orig, ob->object_to_world().ptr());
 
-    if (!is_editable) {
-      sub_layout.enabled_set(false);
+    if (!is_editable && sub_layout) {
+      sub_layout->enabled_set(false);
     }
 
     uiDefBut(block,