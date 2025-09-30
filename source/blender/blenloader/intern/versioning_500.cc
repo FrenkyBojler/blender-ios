4546cef847749941d3d40d9b83c137736cb98510
Sean Kim <SeanCTKim@protonmail.com>
2025-09-30 12:55:04 -0700

    Fix versioning

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -3694,7 +3694,7 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
     }
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 96)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 97)) {
     /* For a brief period of time, these values were not properly versioned, so it is possible for
      * files to be in an odd state. Detect this by looking for either the brush size being 0 or
      * the curve being invalid.
