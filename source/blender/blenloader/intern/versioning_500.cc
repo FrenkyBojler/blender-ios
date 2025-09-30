9975194f24305441a5ca729f8cb155abb7aafde7
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-09-30 15:14:37 +0200

    increment blender version

--- source/blender/blenloader/intern/versioning_500.cc
+++ source/blender/blenloader/intern/versioning_500.cc
@@ -3677,7 +3677,7 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
     }
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 96)) {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 97)) {
     LISTBASE_FOREACH (bScreen *, screen, &bmain->screens) {
       LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
         LISTBASE_FOREACH (SpaceLink *, sl, &area->spacedata) {
