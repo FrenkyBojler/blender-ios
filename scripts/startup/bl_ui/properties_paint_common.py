003e0a6a5a3b4b134087d716aad8ed08d60918f6
Sean Kim <SeanCTKim@protonmail.com>
2025-09-30 21:06:44 -0700

    WIP

--- scripts/startup/bl_ui/properties_paint_common.py
+++ scripts/startup/bl_ui/properties_paint_common.py
@@ -258,6 +258,7 @@ class UnifiedPaintPanel:
             context,
             brush,
             prop_name,
+            pressure_name=None,
             unified_paint_settings_override=None,
             unified_name=None,
             icon='NONE',
@@ -286,30 +287,35 @@ class UnifiedPaintPanel:
             # NOTE: We don't draw UnifiedPaintSettings in the header to reduce clutter. D5928#136281
             row.prop(ups, unified_name, text="", icon='BRUSHES_ALL')
 
+        if pressure_name:
+            row.prop(brush, pressure_name, text="")
+
         return row
 
     @staticmethod
-    def prop_pressure(
+    def prop_custom_pressure(
             layout,
             context,
             parent_row,
             brush,
             pressure_name,
-            curve_visibility_name=None,
-            custom_curve_name=None,
-            header=False
+            curve_visibility_name,
+            custom_curve_name,
     ):
         paint = UnifiedPaintPanel.paint_settings(context)
-        parent_row.prop(brush, pressure_name, text="")
 
-        if curve_visibility_name and not header:
-            is_active = getattr(paint, curve_visibility_name)
-            parent_row.prop(
-                paint,
-                curve_visibility_name,
-                text="",
-                icon='DOWNARROW_HLT' if is_active else 'RIGHTARROW',
-                emboss=False)
+        is_active = getattr(paint, curve_visibility_name)
+        parent_row.prop(
+            paint,
+            curve_visibility_name,
+            text="",
+            icon='DOWNARROW_HLT' if is_active else 'RIGHTARROW',
+            emboss=False)
+
+        if is_active:
+            subcol = layout.column()
+            subcol.active = getattr(brush, pressure_name)
+            subcol.template_curve_mapping(brush, custom_curve_name, brush=True)
 
 
     @staticmethod
@@ -1180,45 +1186,51 @@ def brush_shared_settings(layout, context, brush, popover=False):
     if size or size_mode:
         if size:
             pressure_name = "use_pressure_size" if size_pressure else None
-            curve_visibility_name = "show_size_curve" if size_pressure else None
-            UnifiedPaintPanel.prop_unified(
+            unified_row = UnifiedPaintPanel.prop_unified(
                 layout,
                 context,
                 brush,
                 size_prop,
                 unified_name="use_unified_size",
                 pressure_name=pressure_name,
-                curve_visibility_name=curve_visibility_name,
                 text="Size",
                 slider=True,
             )
-        if mode in {'PAINT_TEXTURE', 'PAINT_2D', 'SCULPT', 'PAINT_VERTEX', 'PAINT_WEIGHT', 'SCULPT_CURVES'}:
-            if paint.show_size_curve and not popover:
-                subcol = layout.column()
-                subcol.active = brush.use_pressure_size
-                subcol.template_curve_mapping(brush, "curve_size", brush=True)
+            if size_pressure and mode in {'PAINT_TEXTURE', 'PAINT_2D', 'SCULPT', 'PAINT_VERTEX', 'PAINT_WEIGHT', 'SCULPT_CURVES'}:
+                UnifiedPaintPanel.prop_custom_pressure(
+                    layout,
+                    context,
+                    unified_row,
+                    brush,
+                    pressure_name,
+                    "show_size_curve",
+                    "curve_size",
+                )
         if size_mode:
             layout.row().prop(size_owner, "use_locked_size", expand=True)
             layout.separator()
 
     if strength:
         pressure_name = "use_pressure_strength" if strength_pressure else None
-        curve_visibility_name = "show_strength_curve" if strength_pressure else None
-        UnifiedPaintPanel.prop_unified(
+        unified_row = UnifiedPaintPanel.prop_unified(
             layout,
             context,
             brush,
             "strength",
             unified_name="use_unified_strength",
             pressure_name=pressure_name,
-            curve_visibility_name=curve_visibility_name,
             slider=True,
         )
-        if mode in {'PAINT_TEXTURE', 'PAINT_2D', 'SCULPT', 'PAINT_VERTEX', 'PAINT_WEIGHT', 'SCULPT_CURVES'}:
-            if paint.show_strength_curve and not popover:
-                subcol = layout.column()
-                subcol.active = brush.use_pressure_strength
-                subcol.template_curve_mapping(brush, "curve_strength", brush=True)
+        if strength_pressure and mode in {'PAINT_TEXTURE', 'PAINT_2D', 'SCULPT', 'PAINT_VERTEX', 'PAINT_WEIGHT', 'SCULPT_CURVES'}:
+            UnifiedPaintPanel.prop_custom_pressure(
+                layout,
+                context,
+                unified_row,
+                brush,
+                pressure_name,
+                "show_strength_curve",
+                "curve_strength",
+            )
         layout.separator()
 
     if direction:
