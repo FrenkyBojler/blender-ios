348c618fbb1f37c09fa1a3f3ac3c2e98e37d535e
Miguel Pozo <pragma37@gmail.com>
2025-10-01 16:48:49 +0200

    Revert "Node: Rewrite iter_shader_to_rgba_depth_count to not be recursive"
    This reverts commit 6cca396e95169ed549eacbf62a41493000216263.

--- source/blender/nodes/shader/node_shader_tree.cc
+++ source/blender/nodes/shader/node_shader_tree.cc
@@ -7,7 +7,6 @@
  */
 
 #include <cstring>
-#include <stack>
 
 #include "DNA_light_types.h"
 #include "DNA_linestyle_types.h"
@@ -832,45 +831,31 @@ static void ntree_shader_shader_to_rgba_branches(bNodeTree *ntree)
   }
 }
 
-static void iter_shader_to_rgba_depth_count(bNode *start_node, int16_t &max_depth)
+static void iter_shader_to_rgba_depth_count(bNode *node,
+                                            int16_t &max_depth,
+                                            int16_t depth_level = 0)
 {
-  struct StackNode {
-    bNode *node;
-    int16_t depth;
-  };
+  if (node->type_legacy == SH_NODE_SHADERTORGB) {
+    depth_level++;
+    max_depth = std::max(max_depth, depth_level);
+  }
 
-  std::stack<StackNode> stack;
-  stack.push({start_node, 0});
+  if (node->runtime->tmp_flag >= depth_level) {
+    /* We already iterated this branch at this or a greater depth. */
+    return;
+  }
+  node->runtime->tmp_flag = std::max(node->runtime->tmp_flag, depth_level);
 
-  while (!stack.empty()) {
-    StackNode s_node = stack.top();
-    stack.pop();
-
-    bNode *node = s_node.node;
-    int16_t depth_level = s_node.depth;
-
-    if (node->type_legacy == SH_NODE_SHADERTORGB) {
-      depth_level++;
-      max_depth = std::max(max_depth, depth_level);
-    }
-
-    if (node->runtime->tmp_flag >= depth_level) {
-      /* We already iterated this branch at this or a greater depth. */
+  LISTBASE_FOREACH (bNodeSocket *, sock, &node->inputs) {
+    bNodeLink *link = sock->link;
+    if (link == nullptr) {
       continue;
     }
-    node->runtime->tmp_flag = std::max(node->runtime->tmp_flag, depth_level);
-
-    LISTBASE_FOREACH (bNodeSocket *, sock, &node->inputs) {
-      bNodeLink *link = sock->link;
-      if (link == nullptr) {
-        continue;
-      }
-      if ((link->flag & NODE_LINK_VALID) == 0) {
-        /* Skip links marked as cyclic. */
-        continue;
-      }
-      stack.push({link->fromnode, depth_level});
+    if ((link->flag & NODE_LINK_VALID) == 0) {
+      /* Skip links marked as cyclic. */
+      continue;
     }
+    iter_shader_to_rgba_depth_count(link->fromnode, max_depth, depth_level);
   }
 }
 
