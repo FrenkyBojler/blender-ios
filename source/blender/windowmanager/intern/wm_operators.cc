ea2847b00c49c23f948987f14083cf47e39d0a7c
Ramon Klauck <ramonklauck3@gmail.com>
2025-10-01 14:45:16 +0200

    fix typo

--- source/blender/windowmanager/intern/wm_operators.cc
+++ source/blender/windowmanager/intern/wm_operators.cc
@@ -4346,7 +4346,7 @@ static void gesture_box_modal_keymap(wmKeyConfig *keyconf)
       {GESTURE_MODAL_DESELECT, "DESELECT", 0, "Deselect", ""},
       {GESTURE_MODAL_BEGIN, "BEGIN", 0, "Begin", ""},
       {GESTURE_MODAL_MOVE, "MOVE", 0, "Move", ""},
-      {GESTURE_MODAL_SWITCH_CORNER, "SWITCH_CORNER", 0, "Switch Cornder", ""},
+      {GESTURE_MODAL_SWITCH_CORNER, "SWITCH_CORNER", 0, "Switch Corner", ""},
       {0, nullptr, 0, nullptr, nullptr},
   };
 
