d26b6b1669875df7ad2686e19ec7f26746e0d051
Sahar Kashi <sahar.kashi@amd.com>
2025-10-02 17:10:50 -0400

    [Cycles][HIPRT] Move BVH device emory management to Device class

--- intern/cycles/device/hiprt/device_impl.cpp
+++ intern/cycles/device/hiprt/device_impl.cpp
@@ -112,6 +112,7 @@ HIPRTDevice::HIPRTDevice(const DeviceInfo &info,
 HIPRTDevice::~HIPRTDevice()
 {
   HIPContextScope scope(this);
+  free_bvh();
   user_instance_id.free();
   prim_visibility.free();
   hiprt_blas_ptr.free();
@@ -125,8 +126,7 @@ HIPRTDevice::~HIPRTDevice()
 
   hiprtDestroyGlobalStackBuffer(hiprt_context, global_stack_buffer);
   hiprtDestroyFuncTable(hiprt_context, functions_table);
-  hiprtDestroyScene(hiprt_context, scene);
-  hiprtDestroyContext(hiprt_context);
+    hiprtDestroyContext(hiprt_context);
 }
 
 unique_ptr<DeviceQueue> HIPRTDevice::gpu_queue_create()
@@ -1155,6 +1155,25 @@ hiprtScene HIPRTDevice::build_tlas(BVHHIPRT *bvh,
   return scene;
 }
 
+void HIPRTDevice::free_bvh(){
+  for (int bvh_index = 0; bvh_index <stale_bvh.size(); bvh_index++)
+  {
+    hiprtGeometry hiprt_geom = stale_bvh[bvh_index];
+    hiprtDestroyGeometry(hiprt_context, hiprt_geom);
+    hiprt_geom = nullptr;
+  }
+  stale_bvh.clear();
+  hiprtDestroyScene(hiprt_context, scene);
+  scene = nullptr;
+}
+
+void HIPRTDevice::track_bvh(BVHHIPRT *bvh){
+  thread_scoped_lock lock(hiprt_mutex);
+  // Tracks BLAS pointers whose BVH destructors have been called
+  stale_bvh.push_back(bvh->hiprt_geom);
+
+}
+
 void HIPRTDevice::build_bvh(BVH *bvh, Progress &progress, bool refit)
 {
   if (have_error()) {
@@ -1175,10 +1194,7 @@ void HIPRTDevice::build_bvh(BVH *bvh, Progress &progress, bool refit)
     build_blas(bvh_rt, geometry[0], options);
   }
   else {
-
-    if (scene) {
-      hiprtDestroyScene(hiprt_context, scene);
-    }
+      free_bvh();
     scene = build_tlas(bvh_rt, bvh_rt->objects, options, refit);
   }
 }
