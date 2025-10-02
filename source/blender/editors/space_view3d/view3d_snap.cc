b2fa15f14759e46e3bf6341ad98ab84e951b1017
tariqsulley <tariqsulley3c@gmail.com>
2025-10-02 12:58:14 +0000

    fixes selection to active bug

--- source/blender/editors/space_view3d/view3d_snap.cc
+++ source/blender/editors/space_view3d/view3d_snap.cc
@@ -558,8 +558,10 @@ static bool snap_selected_to_location_rotation(bContext *C,
     }
 
     for (Object *ob : objects) {
-      if (ob->parent && BKE_object_flag_test_recursive(ob->parent, OB_DONE)) {
-        continue;
+      if (use_offset) {
+        if (ob->parent && BKE_object_flag_test_recursive(ob->parent, OB_DONE)) {
+          continue;
+        }
       }
 
       blender::float3 target_loc_local; /* parent-relative */
