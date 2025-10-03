96819e7de82657f5eac2d014f3b896b590126d75
Brady Johnston <brady.johnston@me.com>
2025-10-03 08:53:36 +0100

    data_type menu

--- source/blender/nodes/geometry/nodes/node_geo_grid_advect.cc
+++ source/blender/nodes/geometry/nodes/node_geo_grid_advect.cc
@@ -91,6 +91,7 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
   b.allow_any_socket_order();
+  b.add_default_layout();
 
   const bNode *node = b.node_or_null();
   if (!node) {
