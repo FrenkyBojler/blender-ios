8fba38accd6b8054a532e691e546277fc98c3676
W_Cloud <yunkezengren@outlook.com>
2025-10-04 07:25:02 +0800

    remove change for socket order

--- source/blender/nodes/shader/nodes/node_shader_invert.cc
+++ source/blender/nodes/shader/nodes/node_shader_invert.cc
@@ -14,16 +14,16 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.use_custom_socket_order();
   b.allow_any_socket_order();
-  b.add_input<decl::Color>("Color")
-      .default_value({0.0f, 0.0f, 0.0f, 1.0f})
-      .description("Color input on which inversion will be applied");
-  b.add_output<decl::Color>("Color").align_with_previous();
   b.add_input<decl::Float>("Fac")
       .default_value(1.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR)
       .description("Amount of influence the node exerts on the image");
+  b.add_input<decl::Color>("Color")
+      .default_value({0.0f, 0.0f, 0.0f, 1.0f})
+      .description("Color input on which inversion will be applied");
+  b.add_output<decl::Color>("Color").align_with_previous();
 }
 
 static int gpu_shader_invert(GPUMaterial *mat,
