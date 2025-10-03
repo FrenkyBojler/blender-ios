9f2cd77b9c008e48cbc217e42f36c3e79ab85eef
Pablo Vazquez <pablo@blender.org>
2025-10-03 12:56:17 +0200

    Draw unselected keyframes a little smaller and dimmer

--- source/blender/editors/animation/keyframes_draw.cc
+++ source/blender/editors/animation/keyframes_draw.cc
@@ -372,10 +372,12 @@ static void draw_keylist_keys(const DrawKeylistUIData *ctx,
         extreme_type = ak->extreme_type;
       }
 
-      /* Draw smaller keyframes if is_expanded is set.
-       * Selected keys draw at regular size. */
-      const float key_size = (is_expanded && !(ak->sel & SELECT)) ? U.widget_unit * 0.33f :
+      /* Draw keyframes smaller and dimmer if is_expanded is set.
+       * Selected keys draw at regular size and opacity. */
+      const float key_size_smallest = min_ff(ctx->icon_size, U.widget_unit * 0.25f);
+      const float key_size = (is_expanded && !(ak->sel & SELECT)) ? key_size_smallest :
                                                                     ctx->icon_size;
+      const float key_alpha = (is_expanded ? ctx->alpha * 0.8f : ctx->alpha);
 
       draw_keyframe_shape(ak->cfra,
                           ypos,
@@ -383,7 +385,7 @@ static void draw_keylist_keys(const DrawKeylistUIData *ctx,
                           (ak->sel & SELECT),
                           eBezTriple_KeyframeType(ak->key_type),
                           KEYFRAME_SHAPE_BOTH,
-                          ctx->alpha,
+                          key_alpha,
                           sh_bindings,
                           handle_type,
                           extreme_type);
