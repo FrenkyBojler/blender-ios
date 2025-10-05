e68feb913093aa8e16e942aac481b6b03e682a0a
Nick Alberelli <nick@tinymedia.ca>
2025-10-05 11:35:32 -0400

    Use f-string in image text loop

--- doc/python_api/examples/bpy.data.py
+++ doc/python_api/examples/bpy.data.py
@@ -34,4 +34,4 @@ if "Cube" in bpy.data.meshes:
 import os
 with open(os.path.splitext(bpy.data.filepath)[0] + ".txt", 'w') as fs:
     for image in bpy.data.images:
-        fs.write("{:s} {:d} x {:d}\n".format(image.filepath, image.size[0], image.size[1]))
+        fs.write(f"{image.filepath} {image.size[0]} x {image.size[1]}\n")
