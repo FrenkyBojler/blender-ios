8cef7712f1d5b41e9d2046ba412fb979b6ad1fd4
Nick Alberelli <nick@tinymedia.ca>
2025-10-05 13:20:10 -0400

    Add additional note

--- doc/python_api/examples/bpy.types.Context.temp_override.4.py
+++ doc/python_api/examples/bpy.types.Context.temp_override.4.py
@@ -2,10 +2,13 @@
 **Logging Context Member Access**
 
 Context members can be logged by calling ``logging_set(True)`` on the with-target of a temporary
-override. This will create a log of the members that are being accessed during the operation and may 
+override. This will create a log of the members that are being accessed during the operation and may
 assist in debugging when it is unclear which members need to be overridden.
 
-This example shows how to log which context members are being accessed. Log statements are 
+In the event an operator fails to execute because of a missing context member, logging may help
+identify which member is required.
+
+This example shows how to log which context members are being accessed. Log statements are
 printed to your system's console.
 """
 
