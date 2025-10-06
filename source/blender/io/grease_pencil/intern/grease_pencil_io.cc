cc4274a11be52fdb69ed6ea9470e8a25e0476983
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-10-06 09:27:28 -0700

    Cleanup: Check.

--- source/blender/io/grease_pencil/intern/grease_pencil_io.cc
+++ source/blender/io/grease_pencil/intern/grease_pencil_io.cc
@@ -351,7 +351,7 @@ Vector<GreasePencilExporter::ObjectInfo> GreasePencilExporter::retrieve_objects(
       break;
     case SelectMode::Visible:
       LISTBASE_FOREACH (Base *, base, BKE_view_layer_object_bases_get(view_layer)) {
-        if (base->flag & BASE_ENABLED_RENDER) {
+        if ((base->flag & BASE_ENABLED_RENDER) != 0) {
           add_object(base->object);
         }
       }
