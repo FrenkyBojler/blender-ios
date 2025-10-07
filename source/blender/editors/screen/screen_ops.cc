c9260d9a11f1bfdde81162690e090c4e771248fc
Nika Kutsniashvili <nickberckley@gmail.com>
2025-10-07 12:43:39 +0400

    Make subframe rounding post-process

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -3384,22 +3384,18 @@ static wmOperatorStatus frame_jump_delta_exec(bContext *C, wmOperator *op)
   if (backward) {
     scene->r.cfra -= delta;
     scene->r.subframe -= delta - int(delta);
-    /* Check if subframe has a non-fractional component, and roll that into cfra. */
-    if (scene->r.subframe < 0.0f) {
-      int borrow = (int)ceilf(-scene->r.subframe);
-      scene->r.cfra -= borrow;
-      scene->r.subframe += borrow;
-    }
   }
   else {
     scene->r.cfra += delta;
     scene->r.subframe += delta - int(delta);
-    /* Check if subframe has a non-fractional component, and roll that into cfra. */
-    if (scene->r.subframe >= 1.0f) {
-      int carry = (int)scene->r.subframe;
-      scene->r.cfra += carry;
-      scene->r.subframe -= carry;
-    }
+  }
+
+  /* Check if subframe has a non-fractional component, and roll that into cfra. */
+  if (scene->r.subframe < 0.0f || scene->r.subframe >= 1.0f) {
+    const float subframe_offset = floorf(scene->r.subframe);
+    const int frame_offset = (int)subframe_offset;
+    scene->r.cfra += frame_offset;
+    scene->r.subframe -= subframe_offset;
   }
 
   ED_areas_do_frame_follow(C, true);
