8190dafa6d850ae37c142e131805fbb1a07bf78e
Pratik Borhade <pratikborhade302@gmail.com>
2025-10-08 12:00:25 +0530

    Fix format

--- source/blender/editors/include/UI_interface_c.hh
+++ source/blender/editors/include/UI_interface_c.hh
@@ -3030,7 +3030,10 @@ bool UI_view_item_drag_start(bContext &C, blender::ui::AbstractViewItem &item);
  * \param xy: Coordinate to find a view item at, in window space.
  * \param pad: Extra padding added to the bounding box of the view.
  */
-blender::ui::AbstractView *UI_region_view_find_at(const ARegion *region, const int xy[2], int pad, uiBlock **r_block = nullptr);
+blender::ui::AbstractView *UI_region_view_find_at(const ARegion *region,
+                                                  const int xy[2],
+                                                  int pad,
+                                                  uiBlock **r_block = nullptr);
 void UI_region_view_scroll_at_borders(ARegion *region, const int xy[2]);
 /**
  * \param xy: Coordinate to find a view item at, in window space.
