18b2435d57ad428a7780f97d6a522d6e177f389a
Hans Goudey <hans@blender.org>
2025-10-08 11:08:59 -0400

    mesh_cd_pdata_get_from_mesh

--- source/blender/draw/intern/draw_cache_extract_mesh_render_data.cc
+++ source/blender/draw/intern/draw_cache_extract_mesh_render_data.cc
@@ -375,23 +375,6 @@ const CustomData &mesh_cd_ldata_get_from_mesh(const Mesh &mesh)
   return mesh.corner_data;
 }
 
-const CustomData &mesh_cd_pdata_get_from_mesh(const Mesh &mesh)
-{
-  switch (mesh.runtime->wrapper_type) {
-    case ME_WRAPPER_TYPE_SUBD:
-    case ME_WRAPPER_TYPE_MDATA:
-      // TODO_MESH_ATTR
-      return mesh.face_data;
-      break;
-    case ME_WRAPPER_TYPE_BMESH:
-      return mesh.runtime->edit_mesh->bm->pdata;
-      break;
-  }
-
-  BLI_assert(0);
-  return mesh.face_data;
-}
-
 const CustomData &mesh_cd_edata_get_from_mesh(const Mesh &mesh)
 {
   switch (mesh.runtime->wrapper_type) {
