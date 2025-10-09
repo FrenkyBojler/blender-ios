eebf69fed61fe688cc061d4c70147bb4b284106e
Clément Foucault <foucault.clem@gmail.com>
2025-10-09 17:08:47 +0200

    Limit this fix to opengl

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -3904,7 +3904,8 @@ void ED_region_header_draw(const bContext *C, ARegion *region)
   /* clear */
   ED_region_clear(C, region, region_background_color_id(C, region));
 
-  if (GPU_type_matches(GPU_DEVICE_ANY, GPU_OS_UNIX, GPU_DRIVER_OPENSOURCE)) {
+  if (GPU_type_matches_ex(GPU_DEVICE_ANY, GPU_OS_UNIX, GPU_DRIVER_OPENSOURCE, GPU_BACKEND_OPENGL))
+  {
     /* WORKAROUND: Driver bug. Fixes invalid glyph being rendered (see #147168). */
     BLF_batch_discard();
   }
