ba7bbc777a1ba71435a762ba8e42aa40c204fb8a
Eqkoss / T1NT1N <eqkoss@gmail.com>
2025-10-11 19:21:38 +0200

    Add Index Switch to compositor and shader editors

--- scripts/startup/bl_ui/node_add_menu_compositor.py
+++ scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -276,6 +276,7 @@ class NODE_MT_compositor_node_utilities_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "CompositorNodeSplit")
         self.node_operator(layout, "CompositorNodeSwitch")
         self.node_operator(layout, "GeometryNodeMenuSwitch")
+        self.node_operator(layout, "GeometryNodeIndexSwitch")
         self.node_operator(
             layout, "CompositorNodeSwitchView",
             label="Switch Stereo View")
--- scripts/startup/bl_ui/node_add_menu_shader.py
+++ scripts/startup/bl_ui/node_add_menu_shader.py
@@ -424,6 +424,7 @@ class NODE_MT_shader_node_utilities_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "NodeSeparateBundle")
         layout.separator()
         self.node_operator(layout, "GeometryNodeMenuSwitch")
+        self.node_operator(layout, "GeometryNodeIndexSwitch")
         if cycles_shader_nodes_poll(context):
             layout.separator()
             self.node_operator(layout, "ShaderNodeScript")
