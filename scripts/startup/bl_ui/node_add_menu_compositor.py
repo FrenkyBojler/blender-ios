d779b5c000e036f52f7058a072f352ed1539710d
Eqkoss / T1NT1N <eqkoss@gmail.com>
2025-10-12 10:48:39 +0200

    Reorder in compositor and shader add menu

--- scripts/startup/bl_ui/node_add_menu_compositor.py
+++ scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -275,8 +275,8 @@ class NODE_MT_compositor_node_utilities_base(node_add_menu.NodeMenu):
         layout.separator()
         self.node_operator(layout, "CompositorNodeSplit")
         self.node_operator(layout, "CompositorNodeSwitch")
-        self.node_operator(layout, "GeometryNodeMenuSwitch")
         self.node_operator(layout, "GeometryNodeIndexSwitch")
+        self.node_operator(layout, "GeometryNodeMenuSwitch")
         self.node_operator(
             layout, "CompositorNodeSwitchView",
             label="Switch Stereo View")
