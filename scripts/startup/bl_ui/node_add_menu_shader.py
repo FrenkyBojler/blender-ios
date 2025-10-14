900b8ab5c5b9920699f58aa2e56d101b88bc6312
Eqkoss / T1NT1N <eqkoss@gmail.com>
2025-10-14 21:42:01 +0200

    Remove Index Switch from Shader Editor by commenting out

--- scripts/startup/bl_ui/node_add_menu_shader.py
+++ scripts/startup/bl_ui/node_add_menu_shader.py
@@ -423,7 +423,8 @@ class NODE_MT_shader_node_utilities_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "NodeCombineBundle")
         self.node_operator(layout, "NodeSeparateBundle")
         layout.separator()
-        self.node_operator(layout, "GeometryNodeIndexSwitch")
+        # NOTE: Dynamic Value are not ready yet, so Index Switch is disable in shader editor
+        # self.node_operator(layout, "GeometryNodeIndexSwitch")
         self.node_operator(layout, "GeometryNodeMenuSwitch")
         if cycles_shader_nodes_poll(context):
             layout.separator()
