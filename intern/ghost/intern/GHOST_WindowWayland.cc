10c96b79c29931b3e47761ded312effb8d55fb1c
Jonas Ådahl <jadahl@gmail.com>
2025-10-14 11:21:09 +0200

    wayland: nullptr check vulkan_window_info
    The equivalent check is done for EGL, so do it for Vulkan too.

--- intern/ghost/intern/GHOST_WindowWayland.cc
+++ intern/ghost/intern/GHOST_WindowWayland.cc
@@ -513,8 +513,10 @@ static void gwl_window_resize_for_backend(GWL_Window *win, const int32_t size[2]
      * which is needed in situations where the buffer scale changes
      * (moving the window to an output with a different scale for e.g.).
      * Failing to do so can exit with a protocol error, see: #148243. */
-    win->backend.vulkan_window_info->size[0] = size[0];
-    win->backend.vulkan_window_info->size[1] = size[1];
+    if (win->backend.vulkan_window_info) {
+      win->backend.vulkan_window_info->size[0] = size[0];
+      win->backend.vulkan_window_info->size[1] = size[1];
+    }
   }
 #endif
 }
