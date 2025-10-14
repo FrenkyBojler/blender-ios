cd5f7c5cc86aef703bc11b93f763c5e0bc588b2a
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-10-13 20:48:41 -0700

    Fix: Index problem.

--- source/blender/editors/grease_pencil/intern/grease_pencil_join_selection.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_join_selection.cc
@@ -481,10 +481,8 @@ void append_strokes_from(bke::CurvesGeometry &&other, bke::CurvesGeometry &dst)
     curve_attributes_to_skip.add("resolution");
   }
   else if (!dst_attributes.contains("resolution") && src_attributes.contains("resolution")) {
-    dst.resolution_for_write().slice(other_curve_offsets[0]).fill(12);
-
+    dst.resolution_for_write().slice(IndexRange::from_begin_end(0, initial_curves_num)).fill(12);
     array_utils::copy(other.resolution(), dst.resolution_for_write().slice(dst_curve_offsets[0]));
-
     curve_attributes_to_skip.add("resolution");
   }
 
