e8e290df1c06c06ec56395aab5d2794fa18a349c
Brecht Van Lommel <brecht@blender.org>
2025-10-15 16:18:22 +0200

    Tests: Blocklist failing Storm tests on AMD and Intel

--- tests/python/storm_render_tests.py
+++ tests/python/storm_render_tests.py
@@ -59,6 +59,15 @@ BLOCKLIST_METAL = [
     "autosmooth_custom_normals.blend",
 ]
 
+# AMD seems to have similar limitations as Metal for transparency.
+BLOCKLIST_AMD = BLOCKLIST_METAL + [
+    "noise_lacunarity.blend",
+]
+
+# Minor difference in texture coordinate for white noise hash.
+BLOCKLIST_INTEL = [
+    "white_noise.*.blend",
+]
 
 def setup():
     import bpy
@@ -120,7 +129,16 @@ def main():
 
     from modules import render_report
 
-    blocklist = BLOCKLIST_METAL if sys.platform == "darwin" else []
+    if sys.platform == "darwin":
+        blocklist = BLOCKLIST_METAL
+    else:
+        gpu_vendor = render_report.get_gpu_device_vendor(args.blender)
+        if gpu_vendor == "AMD":
+            blocklist = BLOCKLIST_AMD
+        elif gpu_vendor == "INTEL":
+            blocklist = BLOCKLIST_INTEL
+        else:
+            blocklist = []
 
     if args.export_method == 'HYDRA':
         report = render_report.Report("Storm Hydra", args.outdir, args.oiiotool, blocklist=blocklist + BLOCKLIST_HYDRA)
