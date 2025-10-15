2a19a4eb217e2952ebfe49e9b1c3a17e6f3e1ad2
Habib Gahbiche <habib@blender.org>
2025-10-15 18:17:05 +0200

    Backport Fix #145675: Crash when opening file with undefined node

--- source/blender/blenloader/intern/versioning_400.cc
+++ source/blender/blenloader/intern/versioning_400.cc
@@ -26,6 +26,7 @@
 #include "DNA_mesh_types.h"
 #include "DNA_modifier_types.h"
 #include "DNA_movieclip_types.h"
+#include "DNA_node_types.h"
 #include "DNA_scene_types.h"
 #include "DNA_sequence_types.h"
 #include "DNA_world_types.h"
@@ -4344,6 +4345,31 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
         }
       }
     }
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 67)) {
+    FOREACH_NODETREE_BEGIN (bmain, node_tree, id) {
+      if (node_tree->type != NTREE_COMPOSIT) {
+        continue;
+      }
+      LISTBASE_FOREACH (bNode *, node, &node_tree->nodes) {
+        if (node->type == CMP_NODE_DENOISE) {
+          if (node->storage == nullptr) {
+            /* Some known files were saved without a valid storage. These are likely corrupt files
+             * that have been produced by a non official blender release. The node type will be set
+             * to Undefined during linking, see #ntree_set_typeinfo. However, a valid storage might
+             * be needed for future versioning (before linking), see
+             * #do_version_denoise_menus_to_inputs so we set a valid storage at this stage such
+             * that the node becomes well defined. */
+            NodeDenoise *ndg = MEM_callocN<NodeDenoise>(__func__);
+            ndg->hdr = true;
+            ndg->prefilter = CMP_NODE_DENOISE_PREFILTER_ACCURATE;
+            node->storage = ndg;
+          }
+        }
+      }
+    }
+    FOREACH_NODETREE_END;
   }
 
   /**
