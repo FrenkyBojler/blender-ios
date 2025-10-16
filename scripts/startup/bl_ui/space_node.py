cd669f7cd5075d0b0bf55c4be4c4c480ef728839
Habib Gahbiche <habib@blender.org>
2025-10-16 12:29:48 +0200

    Compositor: Change asset shelf preview size to 48
    This will make the asset shelf size consistent with all other editors.
    
    Note: effects on thumbnails will also get stronger to compensate for the
    smaller thumbnail. See https://projects.blender.org/blender/blender/pulls/148192

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -1145,6 +1145,7 @@ class NODE_AST_compositor(bpy.types.AssetShelf):
     bl_space_type = 'NODE_EDITOR'
     bl_region_type = 'UI'
     bl_options = {'DEFAULT_VISIBLE'}
+    bl_default_preview_size = 48
 
     @classmethod
     def poll(cls, context):
