blender/blender

commit 09f9edb38c0f359428733c78db9275e4658e88bf

I18n: Translate "Hide Hotkey List" in Node Wrangler preferences
The button used a custom text that could be the same as the default,
properly extracted text ("Show Hotkey Text"), or a different
one ("Hide Hotkey Text"), and was thus not always shown.

Reported by Ye Gui in #43295.

scripts/addons_core/node_wrangler/preferences.py
@@ -139,10 +139,8 @@ class NWNodeWrangler(bpy.types.AddonPreferences):
 
         box = layout.box()
         col = box.column(align=True)
-        hotkey_button_name = "Show Hotkey List"
-        if self.show_hotkey_list:
-            hotkey_button_name = "Hide Hotkey List"
-        col.prop(self, "show_hotkey_list", text=hotkey_button_name, toggle=True)
+        hotkey_button_name = iface_("Show Hotkey List") if self.show_hotkey_list else iface_("Hide Hotkey List")
+        col.prop(self, "show_hotkey_list", text=hotkey_button_name, translate=False, toggle=True)
         if self.show_hotkey_list:
             col.prop(self, "hotkey_list_filter", icon="VIEWZOOM")
             col.separator()

FrenkyBojler/blender-ios

commit 608b06121f2746f75c341762e528d5dd2d115cbc

Fix: Swap messages for hotkey List button in Node Wrangler prefs
The button to toggle the list of hotkeys in the Node Wrangler
preferences can show two messages depending on whether the list is
currently shown: "Hide Hotkey List" or "Show Hotkey List".

My commit 09f9edb38c introduced translation for them but swapped them
by mistake.

scripts/addons_core/node_wrangler/preferences.py
@@ -139,7 +139,7 @@ class NWNodeWrangler(bpy.types.AddonPreferences):
 
         box = layout.box()
         col = box.column(align=True)
-        hotkey_button_name = iface_("Show Hotkey List") if self.show_hotkey_list else iface_("Hide Hotkey List")
+        hotkey_button_name = iface_("Hide Hotkey List") if self.show_hotkey_list else iface_("Show Hotkey List")
         col.prop(self, "show_hotkey_list", text=hotkey_button_name, translate=False, toggle=True)
         if self.show_hotkey_list:
             col.prop(self, "hotkey_list_filter", icon="VIEWZOOM")