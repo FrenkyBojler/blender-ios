1fe5e13296bbe4d7755cf60f765ef526e37106a4
Alaska <Alaskayou01@gmail.com>
2025-10-18 16:00:13 +1300

    Update driver version in another user facing area

--- intern/cycles/device/hip/device.cpp
+++ intern/cycles/device/hip/device.cpp
@@ -64,7 +64,7 @@ bool device_hip_init()
     else if (hipew_result == HIPEW_ERROR_OLD_DRIVER) {
       LOG_WARNING
           << "HIPEW initialization failed: Driver version too old, requires AMD Radeon Pro "
-             "24.Q2 driver or newer";
+             "24.Q4 driver or newer";
     }
     else {
       LOG_WARNING << "HIPEW initialization failed: Error opening HIP dynamic library";
