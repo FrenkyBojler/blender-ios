blender/blender

commit 60de2103c09775b11b05a9d7e261b4e5338d719c

Fix: Crash when adding strips above channel 128
Crashes were caused by setting `Strip::machine` above `MAX_CHANNELS`
value.

Instead of clamping this value at multiple places, setter function
`seq::strip_channel_set` was added that implements clamping.

This also allows for better handling of clamping if it was ever needed.

Function `strip_transform_handle_expand_to_fit` still sets machine value
directly and above `MAX_CHANNELS` as a hack. This could be avoided, but
it would require some refactoring.

Pull Request: https://projects.blender.org/blender/blender/pulls/136163

source/blender/sequencer/intern/strip_transform.cc
@@ -9,6 +9,7 @@
  */
 
 #include "BLI_bounds.hh"
+#include "BLI_math_base.hh"
 #include "DNA_scene_types.h"
 #include "DNA_sequence_types.h"
 
@@ -102,13 +103,13 @@ bool transform_seqbase_shuffle_ex(ListBase *seqbasep,
   const int orig_machine = test->machine;
   BLI_assert(ELEM(channel_delta, -1, 1));
 
-  test->machine += channel_delta;
+  strip_channel_set(test, test->machine + channel_delta);
   while (transform_test_overlap(evil_scene, seqbasep, test)) {
     if ((channel_delta > 0) ? (test->machine >= MAX_CHANNELS) : (test->machine < 1)) {
       break;
     }
 
-    test->machine += channel_delta;
+    strip_channel_set(test, test->machine + channel_delta);
   }
 
   if (!is_valid_strip_channel(test)) {
@@ -123,7 +124,8 @@ bool transform_seqbase_shuffle_ex(ListBase *seqbasep,
       }
     }
 
-    test->machine = orig_machine;
+    strip_channel_set(test, orig_machine);
+
     new_frame = new_frame + (test->start - time_left_handle_frame_get(
                                                evil_scene, test)); /* adjust by the startdisp */
     transform_translate_sequence(evil_scene, test, new_frame - test->start);
@@ -549,6 +551,11 @@ void transform_offset_after_frame(Scene *scene,
       }
     }
   }
+}
+
+void strip_channel_set(Strip *strip, int channel)
+{
+  strip->machine = math::clamp(channel, 1, MAX_CHANNELS);
 }
 
 bool transform_is_locked(ListBase *channels, const Strip *strip)

source/blender/editors/space_sequencer/sequencer_add.cc
@@ -905,7 +905,7 @@ static void sequencer_add_movie_multiple_strips(bContext *C,
           /* The video has sound, shift the video strip up a channel to make room for the sound
            * strip. */
           added_strips.append(strip_sound);
-          strip_movie->machine++;
+          seq::strip_channel_set(strip_movie, strip_movie->machine + 1);
         }
       }
 
@@ -967,7 +967,7 @@ static bool sequencer_add_movie_single_strip(bContext *C,
       added_strips.append(strip_sound);
       /* The video has sound, shift the video strip up a channel to make room for the sound
        * strip. */
-      strip_movie->machine++;
+      seq::strip_channel_set(strip_movie, strip_movie->machine + 1);
     }
   }
 

source/blender/editors/space_sequencer/sequencer_drag_drop.cc
@@ -222,7 +222,7 @@ static float update_overlay_strip_position_data(bContext *C, const int mval[2])
   /* Check if there is a strip that would intersect with the new strip(s). */
   coords->is_intersecting = false;
   Strip dummy_seq{};
-  dummy_seq.machine = coords->channel;
+  seq::strip_channel_set(&dummy_seq, coords->channel);
   dummy_seq.start = coords->start_frame;
   dummy_seq.len = coords->strip_len;
   dummy_seq.speed_factor = 1.0f;
@@ -232,7 +232,7 @@ static float update_overlay_strip_position_data(bContext *C, const int mval[2])
 
   for (int i = 0; i < coords->channel_len && !coords->is_intersecting; i++) {
     coords->is_intersecting = seq::transform_test_overlap(scene, ed->seqbasep, &dummy_seq);
-    dummy_seq.machine++;
+    seq::strip_channel_set(&dummy_seq, dummy_seq.machine + 1);
   }
 
   return strip_len;

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -2091,7 +2091,8 @@ static int sequencer_meta_make_exec(bContext *C, wmOperator * /*op*/)
     channel_meta->flag = channel_cur->flag;
   }
 
-  seqm->machine = active_strip ? active_strip->machine : channel_max;
+  const int channel = active_strip ? active_strip->machine : channel_max;
+  seq::strip_channel_set(seqm, channel);
   BLI_strncpy(seqm->name + 2, DATA_("MetaStrip"), sizeof(seqm->name) - 2);
   seq::sequence_base_unique_name_recursive(scene, &ed->seqbase, seqm);
   seqm->start = meta_start_frame;

source/blender/editors/transform/transform_convert_sequencer.cc
@@ -596,8 +596,7 @@ static void flushTransSeq(TransInfo *t)
             max_offset = offset;
           }
         }
-        strip->machine = round_fl_to_int(td->loc[1] + edge_pan_offset[1]);
-        CLAMP(strip->machine, 1, seq::MAX_CHANNELS);
+        seq::strip_channel_set(strip, round_fl_to_int(td->loc[1] + edge_pan_offset[1]));
         break;
       }
       case SEQ_LEFTSEL: { /* No vertical transform. */

source/blender/makesrna/intern/rna_sequencer.cc
@@ -600,7 +600,7 @@ static void rna_Strip_channel_set(PointerRNA *ptr, int value)
 
   /* check channel increment or decrement */
   const int channel_delta = (value >= strip->machine) ? 1 : -1;
-  strip->machine = value;
+  blender::seq::strip_channel_set(strip, value);
 
   if (blender::seq::transform_test_overlap(scene, seqbase, strip)) {
     blender::seq::transform_seqbase_shuffle_ex(seqbase, strip, scene, channel_delta);

source/blender/sequencer/SEQ_transform.hh
@@ -58,6 +58,10 @@ void transform_handle_overlap(Scene *scene,
                               blender::Span<Strip *> transformed_strips,
                               bool use_sync_markers);
 /**
+ * Set strip channel. This value is clamped to valid values.
+ */
+void strip_channel_set(Strip *strip, int channel);
+/**
  * Move strips and markers (if not locked) that start after timeline_frame by delta frames
  *
  * \param scene: Scene in which strips are located

source/blender/sequencer/intern/sequencer.cc
@@ -12,6 +12,7 @@
 #include "BLI_assert.h"
 #include "BLI_map.hh"
 #include "DNA_listBase.h"
+#include "SEQ_transform.hh"
 #include <cstddef>
 #define DNA_DEPRECATED_ALLOW
 
@@ -135,7 +136,7 @@ Strip *sequence_alloc(ListBase *lb, int timeline_frame, int machine, int type)
 
   strip->flag = SELECT;
   strip->start = timeline_frame;
-  strip->machine = machine;
+  strip_channel_set(strip, machine);
   strip->sat = 1.0;
   strip->mul = 1.0;
   strip->blend_opacity = 100.0;

FrenkyBojler/blender-ios

commit a819011f65fe9325f147cb6ed0aa3ab5aa9bda0b

Fix: VSE: Infinite loop when setting strip channel
Commit 60de2103c0 introduced set function for strip channel, which
validated its value. However function `transform_seqbase_shuffle_ex()`
relied on setting it to invalid value temporarily and check its validity
later.

Use boolean instead to convey the logic.

source/blender/sequencer/intern/strip_transform.cc
@@ -113,10 +113,15 @@ bool transform_seqbase_shuffle_ex(ListBase *seqbasep,
   const ListBase *channels = channels_displayed_get(editing_get(evil_scene));
   SeqTimelineChannel *channel = channel_get_by_index(channels, test->channel);
 
+  bool use_fallback_translation = false;
+
   while (transform_test_overlap(evil_scene, seqbasep, test) || channel_is_muted(channel) ||
          channel_is_locked(channel))
   {
-    if ((channel_delta > 0) ? (test->channel >= MAX_CHANNELS) : (test->channel < 1)) {
+    if ((channel_delta > 0) ? (test->channel + channel_delta >= MAX_CHANNELS) :
+                              (test->channel + channel_delta < 1))
+    {
+      use_fallback_translation = true;
       break;
     }
 
@@ -124,10 +129,8 @@ bool transform_seqbase_shuffle_ex(ListBase *seqbasep,
     channel = channel_get_by_index(channels, test->channel);
   }
 
-  if (!is_valid_strip_channel(test)) {
-    /* Blender 2.4x would remove the strip.
-     * nicer to move it to the end */
-
+  /* Strip can not be moved to next free channel, translate it instead. */
+  if (use_fallback_translation) {
     int new_frame = time_right_handle_frame_get(evil_scene, test);
 
     LISTBASE_FOREACH (Strip *, strip, seqbasep) {