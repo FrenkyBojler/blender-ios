82a6b1eeab613cb8975d22ebfa1592a12fc8d1b1
Richard Antalik <richardantalik@gmail.com>
2025-10-20 06:05:17 +0200

    Fix #146682: Swap data operator breaks effect strips
    Caused by not invalidating lookup cache, which is used for tracking
    relations between effects and inputs.

--- source/blender/sequencer/intern/strip_edit.cc
+++ source/blender/sequencer/intern/strip_edit.cc
@@ -87,7 +87,7 @@ bool SEQ_edit_sequence_swap(Scene *scene, Sequence *seq_a, Sequence *seq_b, cons
   seq_time_effect_range_set(scene, seq_a);
   seq_time_effect_range_set(scene, seq_b);
 
-  strip_lookup_invalidate(editing_get(scene));
+  SEQ_sequence_lookup_tag(scene, SEQ_LOOKUP_TAG_INVALID);
 
   return true;
 }
