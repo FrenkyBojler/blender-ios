bd521d5d71f004df8b3c2282a5cbd367d4d4785b
Jonas Holzman <jonas@blender.org>
2025-10-22 14:58:24 +0200

    Compensate for loss of precision in |num| < 1 by bumping the precision by 1 for translate, bevel, inset and shrink/flatten

--- source/blender/editors/mesh/editmesh_inset.cc
+++ source/blender/editors/mesh/editmesh_inset.cc
@@ -87,14 +87,14 @@ static void edbm_inset_update_header(wmOperator *op, bContext *C)
       BKE_unit_value_as_string(flts_str,
                                NUM_STR_REP_LEN,
                                RNA_float_get(op->ptr, "thickness"),
-                               -4,
+                               -5,
                                B_UNIT_LENGTH,
                                sce->unit,
                                true);
       BKE_unit_value_as_string(flts_str + NUM_STR_REP_LEN,
                                NUM_STR_REP_LEN,
                                RNA_float_get(op->ptr, "depth"),
-                               -4,
+                               -5,
                                B_UNIT_LENGTH,
                                sce->unit,
                                true);
