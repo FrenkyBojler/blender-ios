51064a07c5774ab09db499ebe4715f747adfef8e
Nick Alberelli <nick@tinymedia.ca>
2025-10-24 14:08:58 -0400

    Only Show Tooltip when template variables are present

--- source/blender/editors/interface/templates/interface_templates.cc
+++ source/blender/editors/interface/templates/interface_templates.cc
@@ -348,7 +348,9 @@ static void file_select_path_tooltip_custom(bContext &C,
 
   /* Evaluated/resolved path (in Python style). */
   FileSelectParams *params = static_cast<FileSelectParams *>(argN);
-  if (params != nullptr && params->dir_resolved[0] != '\0') {
+  if (params != nullptr && params->dir_resolved[0] != '\0' &&
+      strcmp(params->dir_resolved, params->dir_template) != 0)
+  {
     char filepath[FILE_MAX];
     BLI_strncpy(filepath, params->dir_resolved, sizeof(filepath));
 
