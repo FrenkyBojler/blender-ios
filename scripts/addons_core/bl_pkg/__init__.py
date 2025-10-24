e65c9acfbdac6a98a072d7c20c7d5cb9dcb48994
Sybren A. Stüvel <sybren@blender.org>
2025-10-24 16:29:30 +0200

    Assets: periodically download remote asset listings
    Re-download remote asset library listings every week. This is checked,
    just like done with the Extensions platform, on Blender start-up.
    
    When a remote listing is downloaded completely, i.e. the last page has
    been downloaded, validated, and parsed, the top metadata file is
    touched. Its timestamp serves as an indicator of the last succesful
    download of the listing.

--- scripts/addons_core/bl_pkg/__init__.py
+++ scripts/addons_core/bl_pkg/__init__.py
@@ -422,7 +422,11 @@ _downloaders: list[_RemoteAssetListingDownloader] = []
 
 
 @bpy.app.handlers.persistent
-def remote_asset_libraries_sync(library: bpy.types.UserAssetLibrary, *args) -> None:
+def remote_asset_libraries_sync(
+    library: bpy.types.UserAssetLibrary,
+    *args,
+    only_if_older_than_sec=0,
+) -> None:
     """Download the remote asset library listing."""
 
     # Ignore in background mode, as that should trigger these updates explicitly.
@@ -430,7 +434,7 @@ def remote_asset_libraries_sync(library: bpy.types.UserAssetLibrary, *args) -> N
         return
 
     # Only download remote libraries.
-    if not library.use_remote_url:
+    if not library.use_remote_url or not library.remote_url:
         return
 
     # Only download over HTTP.
@@ -444,14 +448,18 @@ def remote_asset_libraries_sync(library: bpy.types.UserAssetLibrary, *args) -> N
         print("  skipping {!r}, online access is not allowed,".format(library.remote_url))
         return
 
-    # Create the downloader and start downloading.
     from _bpy_internal.assets.remote_library_listing import listing_downloader
+
+    # Check if the download should happen at all.
+    if only_if_older_than_sec and listing_downloader.is_more_recent_than(library, only_if_older_than_sec):
+        return
 
     # Communicate to the asset system that we started loading a library. It will let asset browsers
     # and other UIs displaying this library indicate that loading is ongoing then, until finished.
     wm = bpy.context.window_manager
     wm.asset_library_status_begin_loading(library.remote_url)
 
+    # Create the downloader and start downloading.
     downloader = listing_downloader.RemoteAssetListingDownloader(
         library.remote_url,
         library.path,
@@ -504,6 +512,19 @@ def _remote_asset_libraries_sync_metafiles_done(downloader: _RemoteAssetListingD
 def _remote_asset_libraries_sync_new_page_done(downloader: _RemoteAssetListingDownloader) -> None:
     wm = bpy.context.window_manager
     wm.asset_library_status_ping_loaded_new_pages(downloader.remote_url)
+
+
+def _remote_asset_libraries_sync_all_periodic():
+    """Periodically download remote asset library listings."""
+    if not bpy.app.online_access:
+        return
+
+    # Refresh once every 7 days.
+    max_age = 3600 * 24 * 7
+
+    for asset_lib in bpy.context.preferences.filepaths.asset_libraries:
+        # TODO: check when the listing was last downloaded.
+        remote_asset_libraries_sync(asset_lib, only_if_older_than_sec=max_age)
 
 
 @bpy.app.handlers.persistent
@@ -876,6 +897,7 @@ def register():
         if prefs.view.show_extensions_updates:
             from . import bl_extension_notify
             bl_extension_notify.update_non_blocking(repos_fn=repos_to_notify)
+        _remote_asset_libraries_sync_all_periodic()
 
 
 def unregister():
