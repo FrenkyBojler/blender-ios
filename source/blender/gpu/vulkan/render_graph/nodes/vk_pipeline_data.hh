fdd83c82909541618475b2f289e28ef3009da111
Jeroen Bakker <jeroen@blender.org>
2025-10-24 10:58:00 +0200

    Vulkan: Remove descriptor buffers

--- source/blender/gpu/vulkan/render_graph/nodes/vk_pipeline_data.hh
+++ source/blender/gpu/vulkan/render_graph/nodes/vk_pipeline_data.hh
@@ -24,9 +24,6 @@ struct VKPipelineData {
   VkPipeline vk_pipeline;
   VkPipelineLayout vk_pipeline_layout;
   VkDescriptorSet vk_descriptor_set;
-  /* VK_EXT_descriptor_buffer */
-  VkDeviceAddress descriptor_buffer_device_address;
-  VkDeviceSize descriptor_buffer_offset;
 
   uint32_t push_constants_size;
   const void *push_constants_data;
--- source/blender/gpu/vulkan/vk_descriptor_set_layouts.hh
+++ source/blender/gpu/vulkan/vk_descriptor_set_layouts.hh
@@ -120,23 +120,6 @@ class VKDescriptorSetLayouts : NonCopyable {
                                       bool &r_needed);
 
   /**
-   * Return the descriptor buffer layout offsets and alignment for the given
-   * vk_descriptor_set_layout handle.
-   *
-   * A copy of the buffer layout is returned due to other threads can alter the location of the
-   * items.
-   *
-   * This function has undefined behavior when descriptor buffers extension isn't enabled on the
-   * VKDevice.
-   */
-  VKDescriptorBufferLayout descriptor_buffer_layout_get(
-      VkDescriptorSetLayout vk_descriptor_set_layout)
-  {
-    std::scoped_lock lock(mutex_);
-    return descriptor_buffer_layouts_.lookup(vk_descriptor_set_layout);
-  }
-
-  /**
    * Free all descriptor set layouts.
    *
    * This method is called when the VKDevice is destroyed.
@@ -153,10 +136,6 @@ class VKDescriptorSetLayouts : NonCopyable {
 
  private:
   void update_layout_bindings(const VKDescriptorSetLayoutInfo &info);
-  VKDescriptorBufferLayout create_descriptor_buffer_layout(
-      const VKDevice &device,
-      const VKDescriptorSetLayoutInfo &info,
-      VkDescriptorSetLayout vk_descriptor_set_layout) const;
 };
 
 }  // namespace blender::gpu
