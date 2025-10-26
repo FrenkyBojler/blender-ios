b0dffc5556906ce493d6ef134235fedf011ac38d
Ahmed-Nassar <anassar3@u.rochester.edu>
2025-10-26 00:05:18 -0400

    Fix: fixed transform geometry node not accepting rotation on small values.

--- source/blender/nodes/geometry/nodes/node_geo_transform_geometry.cc
+++ source/blender/nodes/geometry/nodes/node_geo_transform_geometry.cc
@@ -47,8 +47,12 @@ static void node_declare(NodeDeclarationBuilder &b)
 }
 
 static bool use_translate(const math::Quaternion &rotation, const float3 scale)
-{
-  if (math::angle_of(rotation).radian() > 1e-7f) {
+{ 
+  float x = rotation.x;
+  float y = rotation.y;
+  float z = rotation.z;
+  float vector_magnitude_sq = x*x + y*y + z*z;
+  if (vector_magnitude_sq> 1e-10f) {
     return false;
   }
   if (compare_ff(scale.x, 1.0f, 1e-9f) != 1 || compare_ff(scale.y, 1.0f, 1e-9f) != 1 ||
