f818194fd58e54ab179d3403e62e65876a156861
Patryk-Skowronski <patryk_skowronski@3dconnexion.com>
2025-10-27 11:57:13 +0100

    Removed speed value buffering

--- source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
+++ source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
@@ -128,8 +128,7 @@ static void view3d_ndof_pan_zoom(const wmNDOFMotionData &ndof,
                                  ScrArea *area,
                                  ARegion *region,
                                  const bool has_translate,
-                                 const bool has_zoom,
-                                 const float speed)
+                                 const bool has_zoom)
 {
   RegionView3D *rv3d = static_cast<RegionView3D *>(region->regiondata);
 
@@ -165,6 +164,7 @@ static void view3d_ndof_pan_zoom(const wmNDOFMotionData &ndof,
 
   if (has_translate) {
 
+    const float speed = view3d_ndof_pan_speed_calc(rv3d);
     pan_vec *= speed * ndof.time_delta;
 
     /* transform motion from view to world coordinates */
@@ -174,6 +174,11 @@ static void view3d_ndof_pan_zoom(const wmNDOFMotionData &ndof,
 
     /* move center of view opposite of hand motion (this is camera mode, not object mode) */
     sub_v3_v3(rv3d->ofs, pan_vec);
+
+    /* When in Fly mode with Auto speed, move ndof_ofs as well (to keep the speed constant) */
+    if (!NDOF_IS_ORBIT_AROUND_CENTER_MODE(&U) && (U.ndof_flag & NDOF_DYNAMIC_FLY_SPEED)) {
+      sub_v3_v3(rv3d->ndof_ofs, pan_vec);
+    }
 
     if (RV3D_LOCK_FLAGS(rv3d) & RV3D_BOXVIEW) {
       view3d_boxview_sync(area, region);
@@ -529,7 +534,9 @@ static std::optional<float3> ndof_orbit_center_calc_from_zbuf(Depsgraph *depsgra
                                                               ARegion *region)
 {
   rcti sample_grid_area;
+  int sampling_resolution = 0; /* sample count = (sampling_resolution) ^ 2 */
   if (U.ndof_navigation_mode == NDOF_NAVIGATION_MODE_FLY) {
+    /* Move the grid to the bottom to enhance navigation in Arch Viz */
     sample_grid_area.xmin = 0.3 * region->winx;
     sample_grid_area.xmax = 0.7 * region->winx;
     sample_grid_area.ymin = 0.2 * region->winy;
@@ -710,8 +717,7 @@ static wmOperatorStatus ndof_orbit_invoke_impl(bContext *C,
     const bool has_zoom = (rv3d->is_persp == false) && WM_event_ndof_translation_has_zoom(ndof);
 
     if (has_translate || has_zoom) {
-      const float pan_speed = view3d_ndof_pan_speed_calc(rv3d);
-      view3d_ndof_pan_zoom(ndof, vod->area, vod->region, has_translate, has_zoom, pan_speed);
+      view3d_ndof_pan_zoom(ndof, vod->area, vod->region, has_translate, has_zoom);
       xform_flag |= HAS_TRANSLATE;
     }
 
@@ -785,9 +791,6 @@ static wmOperatorStatus ndof_orbit_zoom_invoke_impl(bContext *C,
   char xform_flag = 0;
   /* off by default, until changed later this function */
   rv3d->ndof_rot_angle = 0.0f;
-  const bool fly_with_auto_speed = !NDOF_IS_ORBIT_AROUND_CENTER_MODE(&U) &&
-                                   (U.ndof_flag & NDOF_DYNAMIC_FLY_SPEED);
-  static float pan_speed_buffer = 0.;
 
   if (ndof.progress == P_FINISHING) {
     /* pass */
@@ -808,11 +811,6 @@ static wmOperatorStatus ndof_orbit_zoom_invoke_impl(bContext *C,
           }
         }
         rv3d->ndof_flag |= RV3D_NDOF_OFS_IS_VALID;
-        // Update and buffer pan speed value only when in Fly mode
-        // with automatic speed enabled, and the motion is starting.
-        if (fly_with_auto_speed) {
-          pan_speed_buffer = view3d_ndof_pan_speed_calc(rv3d);
-        }
       }
     }
   }
@@ -823,10 +821,7 @@ static wmOperatorStatus ndof_orbit_zoom_invoke_impl(bContext *C,
                           ED_view3d_offset_lock_check(v3d, rv3d);
 
     if (has_translate || has_zoom) {
-      if (!fly_with_auto_speed) {
-        pan_speed_buffer = view3d_ndof_pan_speed_calc(rv3d);
-      }
-      view3d_ndof_pan_zoom(ndof, vod->area, vod->region, has_translate, true, pan_speed_buffer);
+      view3d_ndof_pan_zoom(ndof, vod->area, vod->region, has_translate, true);
       xform_flag |= HAS_TRANSLATE;
     }
   }
@@ -864,11 +859,7 @@ static wmOperatorStatus ndof_orbit_zoom_invoke_impl(bContext *C,
     }
 
     if (has_translate || has_zoom) {
-      if (!fly_with_auto_speed) {
-        pan_speed_buffer = view3d_ndof_pan_speed_calc(rv3d);
-      }
-      view3d_ndof_pan_zoom(
-          ndof, vod->area, vod->region, has_translate, has_zoom, pan_speed_buffer);
+      view3d_ndof_pan_zoom(ndof, vod->area, vod->region, has_translate, has_zoom);
       xform_flag |= HAS_TRANSLATE;
     }
   }
@@ -954,8 +945,7 @@ static wmOperatorStatus ndof_pan_invoke_impl(bContext *C,
     ScrArea *area = vod->area;
 
     if (has_translate || has_zoom) {
-      const float pan_speed = view3d_ndof_pan_speed_calc(rv3d);
-      view3d_ndof_pan_zoom(ndof, area, region, has_translate, has_zoom, pan_speed);
+      view3d_ndof_pan_zoom(ndof, area, region, has_translate, has_zoom);
       xform_flag |= HAS_TRANSLATE;
     }
   }
