ad47d93293d147bc874e830dacb3668129de987a
Miguel Pozo <pragma37@gmail.com>
2025-10-28 18:28:51 +0100

    Revert "Optimize Batch size"
    This reverts commit b7b5706b08222b97afaf6fefff045756972b7f3f.

--- source/blender/gpu/intern/gpu_shader_private.hh
+++ source/blender/gpu/intern/gpu_shader_private.hh
@@ -137,7 +137,7 @@ class ShaderCompiler {
 
   struct Batch {
     Vector<Shader *> shaders;
-    Vector<shader::ShaderCreateInfo, 1> infos;
+    Vector<shader::ShaderCreateInfo> infos;
 
     Vector<ShaderSpecialization> specializations;
 
