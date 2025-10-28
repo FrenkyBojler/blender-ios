3fbd6eb6c539515b08d7809ecd14208f2b177c53
Sean Kim <SeanCTKim@protonmail.com>
2025-10-28 15:51:41 -0700

    Add more brush support

--- source/blender/editors/sculpt_paint/brushes/draw.cc
+++ source/blender/editors/sculpt_paint/brushes/draw.cc
@@ -89,7 +89,7 @@ static void calc_grids(const Depsgraph &depsgraph,
   translations_from_offset_and_factors(offset, tls.factors, translations);
 
   clip_and_lock_translations(sd, ss, positions, translations);
-  apply_translations(translations, grids, subdiv_ccg);
+  apply_translations(translations, grids, subdiv_ccg, ss.multires.runtime.modified_at_level[subdiv_ccg.level - 1]);
 }
 
 static void calc_bmesh(const Depsgraph &depsgraph,
