3962402a0afc5c7543689a7aaf5af311fda384a8
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2025-10-28 12:05:34 -0700

    Extending SCOPED_TIMER_AVERAGED

--- source/blender/blenlib/intern/timeit.cc
+++ source/blender/blenlib/intern/timeit.cc
@@ -66,32 +66,50 @@ ScopedTimerAveraged::~ScopedTimerAveraged()
   const TimePoint end = Clock::now();
   const Nanoseconds duration = end - start_;
 
-  total_count_++;
-  total_time_ += duration;
+  TimerMapEntry state{-1, blender::timeit::Nanoseconds::zero(), blender::timeit::Nanoseconds::max(), window_size_, start_ - std::chrono::seconds(1)};
+  state = map_.lookup_or_add(name_, state);
 
-  if (!window_size_ || total_count_ < window_size_) {
-    rolling_average_ = total_time_ / total_count_;
+  if (state.total_count < 0) {
+    state.total_count = 0;
+    map_.add_overwrite(name_, state);
+    // First hit; discard the measurement and return.
+    return;
+  }
+
+  state.total_count++;
+  state.total_time += duration;
+  state.min_time = std::min(duration, state.min_time);
+
+  if (!state.window_size_ || state.total_count < state.window_size_) {
+    state.rolling_average_ = state.total_time / state.total_count;
   }
   else {
-    rolling_average_ = (rolling_average_ * (window_size_.value() - 1) / window_size_.value()) +
-                       (duration / window_size_.value());
+    state.rolling_average_ = (state.rolling_average_ * (state.window_size_.value() - 1) / state.window_size_.value()) +
+                       (duration / state.window_size_.value());
   }
 
-  min_time_ = std::min(duration, min_time_);
+  // Each timer instance only prints at most once per second.
+  if ((end - state.last_print) < std::chrono::seconds(1)) {
+    map_.add_overwrite(name_, state);
+    return;
+  }
 
   fmt::memory_buffer buf;
   fmt::format_to(fmt::appender(buf), FMT_STRING("Timer '{}': (Average: "), name_);
-  format_duration(rolling_average_, buf);
+  format_duration(state.rolling_average_, buf);
   if (window_size_) {
     fmt::format_to(
-        fmt::appender(buf), " of last {} events", std::min(window_size_.value(), total_count_));
+        fmt::appender(buf), " of last {} events", std::min(state.window_size_.value(), state.total_count));
   }
   buf.append(StringRef(", Min: "));
-  format_duration(min_time_, buf);
+  format_duration(state.min_time, buf);
   buf.append(StringRef(", Last: "));
   format_duration(duration, buf);
-  fmt::format_to(fmt::appender(buf), ", Samples: {})\n", total_count_);
+  fmt::format_to(fmt::appender(buf), ", Samples: {})\n", state.total_count);
   std::cout << StringRef(buf.data(), buf.size());
+
+  state.last_print = end;
+  map_.add_overwrite(name_, state);
 }
 
 }  // namespace blender::timeit
