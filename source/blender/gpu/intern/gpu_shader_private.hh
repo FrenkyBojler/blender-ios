43eddbd487081871f84cdc72dea63d7f83862f96
Miguel Pozo <pragma37@gmail.com>
2025-10-28 19:03:40 +0100

    Missing default initialization

--- source/blender/gpu/intern/gpu_shader_private.hh
+++ source/blender/gpu/intern/gpu_shader_private.hh
@@ -257,7 +257,7 @@ class ShaderCompiler {
 
   bool is_compiling_impl();
 
-  std::atomic<bool> is_paused_;
+  std::atomic<bool> is_paused_ = false;
   std::condition_variable pause_finished_notification_;
   /** WARNING: This mutex is only for the condition_variable, is_paused_ should be modified under
    * the main mutex_. */
