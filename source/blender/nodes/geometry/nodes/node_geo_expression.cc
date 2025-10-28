2f9eddd29bf3b3d6b0f30f168e8b07dd3b5e7bd3
Jacques Lucke <jacques@blender.org>
2025-10-28 17:34:03 +0100

    add multiply

--- source/blender/nodes/geometry/nodes/node_geo_expression.cc
+++ source/blender/nodes/geometry/nodes/node_geo_expression.cc
@@ -88,18 +88,25 @@ constexpr nterm<ast::Expr *> expr("expr");
 constexpr char number_pattern[] = "[1-9][0-9]*";
 constexpr regex_term<number_pattern> number("number");
 
+constexpr char_term op_plus('+', 1);
+constexpr char_term op_multiply('*', 2);
+
 constexpr parser p(
     expr,
-    terms('+', number),
+    terms(op_plus, op_multiply, number),
     nterms(expr),
     rules(
         expr(number) >>=
         [](ParseContext &ctx, const term_value<std::string_view> &v_number) {
           return &ctx.scope.construct<ast::Expr>(ast::Number{v_number.get_value()});
         },
-        expr(expr, '+', expr) >>=
+        expr(expr, op_plus, expr) >>=
         [](ParseContext &ctx, ast::Expr *v_expr_a, char /*skip*/, ast::Expr *v_expr_b) {
           return &ctx.scope.construct<ast::Expr>(ast::BinaryOp{"+", v_expr_a, v_expr_b});
+        },
+        expr(expr, op_multiply, expr) >>=
+        [](ParseContext &ctx, ast::Expr *v_expr_a, char /*skip*/, ast::Expr *v_expr_b) {
+          return &ctx.scope.construct<ast::Expr>(ast::BinaryOp{"*", v_expr_a, v_expr_b});
         }));
 }  // namespace grammar
 
