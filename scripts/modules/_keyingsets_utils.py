ba7f3bd240e4a263e7f32eb8d2218f5e950209ca
Wayne Dixon <thewaylow@gmail.com>
2025-10-29 11:06:15 +1100

    fix b-bone keying set property identifiers

--- scripts/modules/_keyingsets_utils.py
+++ scripts/modules/_keyingsets_utils.py
@@ -319,9 +319,9 @@ def RKS_GEN_custom_props(_ksi, _context, ks, data):
 # Property identifiers for Bendy Bones
 bbone_property_ids = (
     "bbone_curveinx",
-    "bbone_curveiny",
+    "bbone_curveinz",
     "bbone_curveoutx",
-    "bbone_curveouty",
+    "bbone_curveoutz",
 
     "bbone_rollin",
     "bbone_rollout",
@@ -332,8 +332,8 @@ bbone_property_ids = (
     # NOTE: These are in the nested bone struct
     # Do it this way to force them to be included
     # in whatever actions are being keyed here
-    "bone.bbone_in",
-    "bone.bbone_out",
+    "bbone_easein",
+    "bbone_easeout",
 )
 
 
