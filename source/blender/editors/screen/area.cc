3e8a5d6da48625aa9b562c4c5ec864941023b6c5
Richard Antalik <richardantalik@gmail.com>
2025-10-29 01:14:33 +0100

    VSE: Use region overlap for side bar

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -1516,6 +1516,8 @@ bool ED_region_is_overlap(const int spacetype, const int regiontype)
                   RGN_TYPE_TOOL_HEADER,
                   RGN_TYPE_ASSET_SHELF,
                   RGN_TYPE_ASSET_SHELF_HEADER);
+    case SPACE_SEQ:
+      return regiontype == RGN_TYPE_UI;
 
     default:
       /* Most editors do not support any region overlap. It is fine if newly-added space types also
