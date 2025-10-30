bcc6f4237148b948292868bc4755ec2890a07cf2
Harley Acheson <harley.acheson@gmail.com>
2025-10-29 21:51:35 -0700

    formatting

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -1927,7 +1927,7 @@ static int area_snap_calc_location(sAreaMoveData *md, const int delta)
 
       /* Slight snap to vertical minimum and maximum. */
       const int snap_threshold = int(float(ED_area_headersize()) * 0.6f);
-      
+
       /* Extra snaps for Timeline and Graph Editor. */
       const bool has_scrub = md->area2 &&
                              ELEM(md->area2->spacetype, SPACE_ACTION, SPACE_GRAPH, SPACE_NLA);
