ec6e9d3e2a14156b09150477c6a8c46f440fde83
Philipp Oeser <philipp@blender.org>
2025-10-30 11:05:45 +0100

    Fix #148938: VFX: Mask keyframes are not automatically selected
    This was split off of #138877.
    Related Animation and Rigging module meeting: https://
    devtalk.blender.org/t/2025-06-26-animation-rigging-module-meeting/
    41272#p-153605-patches-review-decision-time-5
    
    So new (consistent) behavior now is to deselect other keys prior and
    only select the new key.

--- source/blender/editors/mask/mask_shapekey.cc
+++ source/blender/editors/mask/mask_shapekey.cc
@@ -45,6 +45,12 @@ static wmOperatorStatus mask_shape_key_insert_exec(bContext *C, wmOperator * /*o
     mask_layer_shape = BKE_mask_layer_shape_verify_frame(mask_layer, frame);
     BKE_mask_layer_shape_from_mask(mask_layer, mask_layer_shape);
     changed = true;
+
+    /* Only select the new key (deselect others). */
+    LISTBASE_FOREACH (MaskLayerShape *, mask_layer_shape_iter, &mask_layer->splines_shapes) {
+      mask_layer_shape_iter->flag &= ~MASK_SHAPE_SELECT;
+    }
+    mask_layer_shape->flag |= MASK_SHAPE_SELECT;
   }
 
   if (changed) {
@@ -379,6 +385,12 @@ void ED_mask_layer_shape_auto_key(MaskLayer *mask_layer, const int frame)
 
   mask_layer_shape = BKE_mask_layer_shape_verify_frame(mask_layer, frame);
   BKE_mask_layer_shape_from_mask(mask_layer, mask_layer_shape);
+
+  /* Only select the autokey (deselect others). */
+  LISTBASE_FOREACH (MaskLayerShape *, mask_layer_shape_iter, &mask_layer->splines_shapes) {
+    mask_layer_shape_iter->flag &= ~MASK_SHAPE_SELECT;
+  }
+  mask_layer_shape->flag |= MASK_SHAPE_SELECT;
 }
 
 bool ED_mask_layer_shape_auto_key_all(Mask *mask, const int frame)
