c0b10085ec8963dbf13362aa501824b51885b0b0
Patryk-Skowronski <patryk_skowronski@3dconnexion.com>
2025-10-31 12:02:47 +0100

    Cleanup

--- source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
+++ source/blender/editors/space_view3d/view3d_navigate_view_ndof.cc
@@ -227,12 +227,10 @@ static float compute_leveling_angle(const float view_x_axis[3],
 {
   /* View leveling algorithm */
 
-  /* Check if viewing direction is aligned with a horizon plane normal or if it's already leveled */
+  /* Check if view is already leveled */
   bool viewNotLeveled = (view_x_axis[2] > 0.001f) || (view_x_axis[2] < -0.001f);
-  bool viewNotTopOrBttm = (view_z_axis[2] > -0.99f) && (view_z_axis[2] < 0.99f);
 
-  /* Level the view only if there is no alignment */
-  if (viewNotLeveled && viewNotTopOrBttm) {
+  if (viewNotLeveled) {
 
     float isect_vec[3] = {0, 0, 0};
     float isect_pt[3] = {0, 0, 0};
@@ -311,12 +309,12 @@ static void view3d_ndof_orbit(const wmNDOFMotionData &ndof,
     mul_qt_v3(view_inv, zvec);
 
     /* Level the view to a "horizon plane" */
-    const float leveling_angle = compute_leveling_angle(xvec, yvec, zvec);
-    if (leveling_angle != 0.) {
-      float leveling_quat[4];
-      axis_angle_to_quat(leveling_quat, zvec, leveling_angle);
-      mul_qt_qtqt(rv3d->viewquat, rv3d->viewquat, leveling_quat);
-    }
+      const float leveling_angle = compute_leveling_angle(xvec, yvec, zvec);
+      if (leveling_angle != 0.) {
+        float leveling_quat[4];
+        axis_angle_to_quat(leveling_quat, zvec, leveling_angle);
+        mul_qt_qtqt(rv3d->viewquat, rv3d->viewquat, leveling_quat);
+      }
 
     /* Perform the up/down rotation */
     angle = ndof.time_delta * rot[0];
