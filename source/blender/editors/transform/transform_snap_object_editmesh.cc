36ae9ecf9727ea2d572e9609dd22054b190fb228
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-01 11:01:15 +0000

    Changes

--- source/blender/editors/transform/transform_snap_object_editmesh.cc
+++ source/blender/editors/transform/transform_snap_object_editmesh.cc
@@ -13,6 +13,7 @@
 #include "BKE_mesh.hh"
 #include "BKE_object.hh"
 #include "BKE_object_types.hh"
+
 #include "ED_transform_snap_object_context.hh"
 
 #include "transform_snap_object.hh"
@@ -28,6 +29,7 @@ static const Mesh *get_mesh_ref(const Object *ob_eval)
   if (const Mesh *me = BKE_object_get_editmesh_eval_final(ob_eval)) {
     return me;
   }
+
   if (const Mesh *me = BKE_object_get_editmesh_eval_cage(ob_eval)) {
     return me;
   }
