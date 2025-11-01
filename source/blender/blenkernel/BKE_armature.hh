1d7fb1b0576435d3754208e3c9ae5f1cea019297
Lenotir <Lenotir>
2025-11-01 12:48:03 -0400

    Increase Spline IK Limit Further
    Increase the max spline IK chain length from 4096 to 8192, where 8192 was chosen as the maximum amount of bones selectable in the viewport due to bug https://projects.blender.org/blender/blender/issues/106542

--- source/blender/blenkernel/BKE_armature.hh
+++ source/blender/blenkernel/BKE_armature.hh
@@ -680,7 +680,7 @@ void BKE_armature_deform_coords_with_editmesh(
 
 namespace blender::bke {
 
-constexpr int SPLINE_IK_MAX_CHAIN_LEN = 4096;
+constexpr int SPLINE_IK_MAX_CHAIN_LEN = 8192;
 
 struct SelectedBonesResult {
   bool all_bones_selected = true;
