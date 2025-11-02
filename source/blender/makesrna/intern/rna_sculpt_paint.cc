285b1609f260fcf733b44ef6b4bf1914c3c0c0ba
Sean Kim <SeanCTKim@protonmail.com>
2025-11-01 19:55:57 -0700

    make format

--- source/blender/makesrna/intern/rna_sculpt_paint.cc
+++ source/blender/makesrna/intern/rna_sculpt_paint.cc
@@ -778,7 +778,8 @@ static void rna_def_paint(BlenderRNA *brna)
       srna, "show_jitter_curve", "Show Jitter Curve", PAINT_CURVE_SHOW_JITTER);
 
   prop = RNA_def_property(srna, "show_auto_smooth_curve", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_AUTO_SMOOTH);
+  RNA_def_property_boolean_sdna(
+      prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_AUTO_SMOOTH);
   RNA_def_property_ui_text(prop, "Show Auto Smooth Curve", "Show auto smooth curve");
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr);
 
@@ -788,17 +789,20 @@ static void rna_def_paint(BlenderRNA *brna)
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr);
 
   prop = RNA_def_property(srna, "show_hardness_curve", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_HARDNESS);
+  RNA_def_property_boolean_sdna(
+      prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_HARDNESS);
   RNA_def_property_ui_text(prop, "Show Hardness Curve", "Show hardness curve");
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr);
 
   prop = RNA_def_property(srna, "show_plane_offset_curve", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_PLANE_OFFSET);
+  RNA_def_property_boolean_sdna(
+      prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_PLANE_OFFSET);
   RNA_def_property_ui_text(prop, "Show Plane Offset Curve", "Show plane offset curve");
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr);
 
   prop = RNA_def_property(srna, "show_area_radius_curve", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_AREA_RADIUS);
+  RNA_def_property_boolean_sdna(
+      prop, nullptr, "curve_visibility_flags", PAINT_CURVE_SHOW_AREA_RADIUS);
   RNA_def_property_ui_text(prop, "Show Area Radius Curve", "Show area radius curve");
   RNA_def_property_update(prop, NC_SCENE | ND_TOOLSETTINGS, nullptr);
 
