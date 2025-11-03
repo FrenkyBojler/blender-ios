blender/blender

commit db570bfe096e51e61ee30438a5b0bcaa4480dbd6

UI: Tree View: Search filtering
Similar to UIList, add search box inside collapsible panel at the bottom.
When search string matches with some element, their parent hierarchy
is displayed as well (`is_filtered_visible_` is set to true inside `on_filter_change()`
for parent tree elements to be visible) .

`search_string` is stored in DNA (`uiViewState`) so we don't loose it after
collapsing panel, reopening blender, etc.

Resolves #147813
Part of #118201

See PR description for the videos

Co-authored-by: Julian Eisel <julian@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/136815

source/blender/editors/interface/views/abstract_view.cc
@@ -171,6 +171,10 @@ void AbstractView::filter(std::optional<StringRef> filter_str)
     item.is_filtered_visible_ = is_empty ||
                                 item.should_be_filtered_visible(StringRefNull(*filter_str));
 
+    if (item.is_filtered_visible_) {
+      item.on_filter_change();
+    }
+
     if (filter_changed) {
       item.is_highlighted_search_ = false;
       /* On new filtering input, force the first visible item to be highlighted and in view, so

source/blender/editors/include/UI_abstract_view.hh
@@ -364,6 +364,7 @@ class AbstractViewItem {
   void rename_apply(const bContext &C);
 
   virtual void delete_item(bContext *C);
+  virtual void on_filter_change();
 
  protected:
   AbstractViewItem() = default;

source/blender/editors/include/UI_tree_view.hh
@@ -132,6 +132,10 @@ class AbstractTreeView : public AbstractView, public TreeViewItemContainer {
   int last_tot_items_ = 0;
 
   bool scroll_active_into_view_on_draw_ = false;
+  bool show_display_options_ = false;
+  /* `char[UI_MAX_NAME_STR]` wrapped in shared pointer, to keep a stable pointer over
+   * reconstruction that can be passed to buttons. */
+  std::shared_ptr<char[]> search_string_{new char[256 /*UI_MAX_NAME_STR*/]{}};
 
   friend class AbstractTreeViewItem;
   friend class TreeViewBuilder;
@@ -163,6 +167,7 @@ class AbstractTreeView : public AbstractView, public TreeViewItemContainer {
    * \note Value should be greater than #MIN_ROWS. This is to prevent resizing below certain
    * height. */
   void set_default_rows(int default_rows);
+  void toggle_show_display_options();
 
  protected:
   virtual void build_tree() = 0;
@@ -280,6 +285,8 @@ class AbstractTreeViewItem : public AbstractViewItem, public TreeViewItemContain
   bool is_collapsible() const;
 
   int count_parents() const;
+
+  void on_filter_change() override;
 
  protected:
   /** See AbstractViewItem::get_rename_string(). */
@@ -429,7 +436,6 @@ class TreeViewBuilder {
   static void build_tree_view(const bContext &C,
                               AbstractTreeView &tree_view,
                               uiLayout &layout,
-                              std::optional<StringRef> search_string = {},
                               bool add_box = true);
 
  private:

source/blender/editors/interface/views/abstract_view_item.cc
@@ -8,6 +8,7 @@
 
 #include "BKE_context.hh"
 
+#include "BLI_fnmatch.h"
 #include "BLI_listbase.h"
 
 #include "WM_api.hh"
@@ -257,6 +258,11 @@ void AbstractViewItem::delete_item(bContext * /*C*/)
   /* No deletion by default. Needs type specific implementation. */
 }
 
+void AbstractViewItem::on_filter_change()
+{
+  /* No action by default. Needs type specific implementation. */
+}
+
 /** \} */
 
 /* ---------------------------------------------------------------------- */
@@ -274,9 +280,10 @@ void AbstractViewItem::build_context_menu(bContext & /*C*/, uiLayout & /*column*
 /** \name Filtering
  * \{ */
 
-bool AbstractViewItem::should_be_filtered_visible(const StringRefNull /*filter_string*/) const
+bool AbstractViewItem::should_be_filtered_visible(const StringRefNull filter_string) const
 {
-  return true;
+  StringRef name = this->get_rename_string();
+  return fnmatch(filter_string.c_str(), name.data(), FNM_CASEFOLD) == 0;
 }
 
 bool AbstractViewItem::is_filtered_visible() const

source/blender/editors/interface/views/tree_view.cc
@@ -27,6 +27,7 @@
 #include "BLI_listbase.h"
 #include "BLI_math_base.h"
 #include "BLI_multi_value_map.hh"
+#include "BLI_string.h"
 
 #include "UI_tree_view.hh"
 
@@ -145,13 +146,21 @@ void AbstractTreeView::set_default_rows(int default_rows)
   custom_height_ = std::make_unique<int>(default_rows * padded_item_height());
 }
 
+void AbstractTreeView::toggle_show_display_options()
+{
+  show_display_options_ = !show_display_options_;
+}
+
 std::optional<uiViewState> AbstractTreeView::persistent_state() const
 {
+  uiViewState state{};
+
+  SET_FLAG_FROM_TEST(state.flag, !show_display_options_, UI_VIEW_COLLAPSE_FILTER_OPTIONS);
+  BLI_strncpy(state.search_string, search_string_.get(), sizeof(state.search_string));
+
   if (!custom_height_ && !scroll_value_) {
     return {};
   }
-
-  uiViewState state{0};
 
   if (custom_height_) {
     state.custom_height = *custom_height_ * UI_INV_SCALE_FAC;
@@ -172,6 +181,9 @@ void AbstractTreeView::persistent_state_apply(const uiViewState &state)
   if (state.scroll_offset) {
     scroll_value_ = std::make_shared<int>(state.scroll_offset);
   }
+
+  show_display_options_ = (state.flag & UI_VIEW_COLLAPSE_FILTER_OPTIONS) == 0;
+  BLI_strncpy(search_string_.get(), state.search_string, sizeof(search_string_));
 }
 
 int AbstractTreeView::count_visible_descendants(const AbstractTreeViewItem &parent) const
@@ -322,6 +334,8 @@ void AbstractTreeView::update_children_from_old(const AbstractView &old_view)
 
   custom_height_ = old_tree_view.custom_height_;
   scroll_value_ = old_tree_view.scroll_value_;
+  search_string_ = old_tree_view.search_string_;
+  show_display_options_ = old_tree_view.show_display_options_;
   update_children_from_old_recursive(*this, old_tree_view);
 }
 
@@ -795,6 +809,16 @@ bool AbstractTreeViewItem::matches(const AbstractViewItem &other) const
   return true;
 }
 
+void AbstractTreeViewItem::on_filter_change()
+{
+  if (is_filtered_visible_) {
+    foreach_parent([&](AbstractTreeViewItem &item) {
+      item.is_filtered_visible_ = true;
+      item.set_collapsed(false);
+    });
+  }
+}
+
 /* ---------------------------------------------------------------------- */
 
 class TreeViewLayoutBuilder {
@@ -824,6 +848,24 @@ static int count_visible_items(AbstractTreeView &tree_view)
                          AbstractTreeView::IterOptions::SkipCollapsed |
                              AbstractTreeView::IterOptions::SkipFiltered);
   return item_count;
+}
+
+static void set_filtering_collapsed_fn(bContext *C, void * /*but_arg1*/, void * /*arg2*/)
+{
+  const wmWindow *win = CTX_wm_window(C);
+  if (!(win && win->eventstate)) {
+    return;
+  }
+  const ARegion *region = CTX_wm_region(C);
+  if (!region) {
+    return;
+  }
+
+  if (AbstractView *view = UI_region_view_find_at(region, win->eventstate->xy, 2 * UI_UNIT_Y)) {
+    if (AbstractTreeView *tree_view = dynamic_cast<AbstractTreeView *>(view)) {
+      tree_view->toggle_show_display_options();
+    }
+  }
 }
 
 void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
@@ -866,7 +908,9 @@ void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
   tree_view.foreach_item(
       [&, this](AbstractTreeViewItem &item) {
         if ((index >= first_visible_index) && (index <= max_visible_index)) {
-          this->build_row(item);
+          if (item.is_filtered_visible()) {
+            this->build_row(item);
+          }
         }
         index++;
       },
@@ -898,6 +942,18 @@ void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
     }
 
     block_layout_set_current(block, col);
+
+    /* Bottom */
+    uiLayout *bottom = &col->row(false);
+    UI_block_emboss_set(block, ui::EmbossType::None);
+    int icon = tree_view.show_display_options_ ? ICON_DISCLOSURE_TRI_DOWN :
+                                                 ICON_DISCLOSURE_TRI_RIGHT;
+    uiBut *but = uiDefIconBut(
+        block, ButType::IconToggle, 0, icon, 0, 0, UI_UNIT_X, UI_UNIT_Y * 0.3, nullptr, 0, 0, "");
+    UI_but_func_set(but, set_filtering_collapsed_fn, nullptr, nullptr);
+    UI_block_emboss_set(block, ui::EmbossType::Emboss);
+    bottom->column(false);
+
     uiDefIconButI(block,
                   ButType::Grip,
                   0,
@@ -910,6 +966,25 @@ void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
                   0,
                   0,
                   "");
+
+    if (tree_view.show_display_options_) {
+      block_layout_set_current(block, col);
+      uiBut *but = uiDefBut(block,
+                            ButType::Text,
+                            1,
+                            "",
+                            0,
+                            0,
+                            UI_TREEVIEW_INDENT,
+                            UI_UNIT_Y,
+                            tree_view.search_string_.get(),
+                            0,
+                            UI_MAX_NAME_STR,
+                            "");
+      UI_but_flag_enable(but, UI_BUT_TEXTEDIT_UPDATE | UI_BUT_VALUE_CLEAR);
+      UI_but_flag_enable(but, UI_BUT_UNDO);
+      ui_def_but_icon(but, ICON_VIEWZOOM, UI_HAS_ICON);
+    }
   }
 
   block_layout_set_current(block, &parent_layout);
@@ -1008,7 +1083,6 @@ void TreeViewBuilder::ensure_min_rows_items(AbstractTreeView &tree_view)
 void TreeViewBuilder::build_tree_view(const bContext &C,
                                       AbstractTreeView &tree_view,
                                       uiLayout &layout,
-                                      std::optional<StringRef> search_string,
                                       const bool add_box)
 {
   uiBlock &block = *layout.block();
@@ -1021,8 +1095,12 @@ void TreeViewBuilder::build_tree_view(const bContext &C,
   tree_view.build_tree();
   tree_view.update_from_old(block);
   tree_view.change_state_delayed();
-  tree_view.filter(search_string);
-
+  {
+    /* Setup search string to filter out elements with matching characters. */
+    char string[UI_MAX_NAME_STR];
+    BLI_strncpy_ensure_pad(string, tree_view.search_string_.get(), '*', sizeof(string));
+    tree_view.filter(tree_view.search_string_ ? std::optional{string} : std::nullopt);
+  }
   ensure_min_rows_items(tree_view);
 
   /* Ensure the given layout is actually active. */

source/blender/editors/space_spreadsheet/spreadsheet_dataset_draw.cc
@@ -1225,7 +1225,7 @@ static void draw_viewer_path_panel(const bContext &C, uiLayout &layout)
   ui::AbstractTreeView *tree_view = UI_block_add_view(
       *block, "Viewer Path", std::make_unique<ViewerPathTreeView>(C));
   tree_view->set_context_menu_title("Viewer Path");
-  ui::TreeViewBuilder::build_tree_view(C, *tree_view, layout, {}, true);
+  ui::TreeViewBuilder::build_tree_view(C, *tree_view, layout, true);
 }
 
 static void draw_viewer_data_panel(const bContext &C, uiLayout &layout)
@@ -1234,7 +1234,7 @@ static void draw_viewer_data_panel(const bContext &C, uiLayout &layout)
   ui::AbstractTreeView *tree_view = UI_block_add_view(
       *block, "Viewer Data", std::make_unique<ViewerDataTreeView>(C));
   tree_view->set_context_menu_title("Viewer Data");
-  ui::TreeViewBuilder::build_tree_view(C, *tree_view, layout, {}, false);
+  ui::TreeViewBuilder::build_tree_view(C, *tree_view, layout, false);
 }
 
 static void draw_context_panel_content(const bContext &C, uiLayout &layout)
@@ -1329,7 +1329,7 @@ void spreadsheet_data_set_panel_draw(const bContext *C, Panel *panel)
           "Instances Tree View",
           std::make_unique<GeometryInstancesTreeView>(*root_geometry, *C));
       tree_view->set_context_menu_title("Instance");
-      ui::TreeViewBuilder::build_tree_view(*C, *tree_view, *panel, {}, false);
+      ui::TreeViewBuilder::build_tree_view(*C, *tree_view, *panel, false);
     }
     if (uiLayout *panel = layout->panel(C, "geometry_domain_tree_view", false, IFACE_("Domain"))) {
       bke::GeometrySet instance_geometry = get_geometry_set_for_instance_ids(
@@ -1340,7 +1340,7 @@ void spreadsheet_data_set_panel_draw(const bContext *C, Panel *panel)
           "Data Set Tree View",
           std::make_unique<GeometryDataSetTreeView>(std::move(instance_geometry), *C));
       tree_view->set_context_menu_title("Domain");
-      ui::TreeViewBuilder::build_tree_view(*C, *tree_view, *panel, {}, false);
+      ui::TreeViewBuilder::build_tree_view(*C, *tree_view, *panel, false);
     }
   }
 }

source/blender/makesdna/DNA_screen_types.h
@@ -339,6 +339,10 @@ typedef struct uiList { /* some list UI data need to be saved in file */
   uiListDyn *dyn_data;
 } uiList;
 
+typedef enum uiViewStateFlag {
+  UI_VIEW_COLLAPSE_FILTER_OPTIONS = (1 << 0),
+} uiViewStateFlag;
+
 /** See #uiViewStateLink. */
 typedef struct uiViewState {
   /**
@@ -352,6 +356,10 @@ typedef struct uiViewState {
    *   scrolled out of view).
    */
   int scroll_offset;
+  uint16_t flag; /* #uiViewStateFlag */
+  char _pad[6];
+
+  char search_string[/*UI_MAX_NAME_STR*/ 256];
 } uiViewState;
 
 /**

FrenkyBojler/blender-ios

commit 77c5448f4a90b28b8e82917c21c88eac4e87c66e

Fix #149178: Regression: Cannot collpase nested tree view elements
Caused by db570bfe096e51e61ee30438a5b0bcaa4480dbd6
When search string is empty, `is_filtered_visible_` is true for every
element, this executes `on_filter_change()` where we force expand tree
elements. Purpose of expanding and `on_filter_change()` only make sense
when we're searching, i.e. to show searched element that is deep inside
the hierarchy. To fix this, add `is_empty` condition.

source/blender/editors/interface/views/abstract_view.cc
@@ -171,7 +171,8 @@ void AbstractView::filter(std::optional<StringRef> filter_str)
     item.is_filtered_visible_ = is_empty ||
                                 item.should_be_filtered_visible(StringRefNull(*filter_str));
 
-    if (item.is_filtered_visible_) {
+    if (!is_empty && item.is_filtered_visible_) {
+      /* Don't force parent elements to be visible when search string is not empty. */
       item.on_filter_change();
     }
 