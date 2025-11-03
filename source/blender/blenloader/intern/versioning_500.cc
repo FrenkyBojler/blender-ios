blender/blender

commit 153f14be2b67fba0d3d92eb08eff7659f2dffae7

Compositor: Make split viewer a regular split node
Changes:
- Renamed Split Viewer Node to Split Node
- Split Node is now under `Utilities` (similar to Switch node)
- Versioning: split viewer from 4.0 and before is replaced with the new split node connected to a new viewer node.

Pull Request: https://projects.blender.org/blender/blender/pulls/114245

source/blender/nodes/composite/nodes/node_composite_split.cc
@@ -1,0 +1,123 @@
+/* SPDX-FileCopyrightText: 2006 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup cmpnodes
+ */
+
+#include "BKE_global.h"
+#include "BKE_image.h"
+
+#include "UI_interface.hh"
+#include "UI_resources.hh"
+
+#include "GPU_shader.h"
+#include "GPU_texture.h"
+
+#include "COM_node_operation.hh"
+#include "COM_utilities.hh"
+
+#include "node_composite_util.hh"
+
+/* **************** SPLIT NODE ******************** */
+
+namespace blender::nodes::node_composite_split_cc {
+
+static void cmp_node_split_declare(NodeDeclarationBuilder &b)
+{
+  b.add_input<decl::Color>("Image");
+  b.add_input<decl::Color>("Image", "Image_001");
+  b.add_output<decl::Color>("Image");
+}
+
+static void node_composit_init_split(bNodeTree * /*ntree*/, bNode *node)
+{
+  node->custom1 = 50; /* default 50% split */
+}
+
+static void node_composit_buts_split(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+{
+  uiLayout *row, *col;
+
+  col = uiLayoutColumn(layout, false);
+  row = uiLayoutRow(col, false);
+  uiItemR(row, ptr, "axis", UI_ITEM_R_SPLIT_EMPTY_NAME | UI_ITEM_R_EXPAND, nullptr, ICON_NONE);
+  uiItemR(col, ptr, "factor", UI_ITEM_R_SPLIT_EMPTY_NAME, nullptr, ICON_NONE);
+}
+
+using namespace blender::realtime_compositor;
+
+class SplitOperation : public NodeOperation {
+ public:
+  using NodeOperation::NodeOperation;
+
+  void execute() override
+  {
+    GPUShader *shader = get_split_shader();
+    GPU_shader_bind(shader);
+
+    GPU_shader_uniform_1f(shader, "split_ratio", get_split_ratio());
+
+    const Result &first_image = get_input("Image");
+    first_image.bind_as_texture(shader, "first_image_tx");
+    const Result &second_image = get_input("Image_001");
+    second_image.bind_as_texture(shader, "second_image_tx");
+
+    const Domain domain = compute_domain();
+    Result &output_image = get_result("Image");
+    output_image.allocate_texture(domain);
+    output_image.bind_as_image(shader, "output_img");
+
+    compute_dispatch_threads_at_least(shader, domain.size);
+
+    first_image.unbind_as_texture();
+    second_image.unbind_as_texture();
+    output_image.unbind_as_image();
+    GPU_shader_unbind();
+  }
+
+  GPUShader *get_split_shader()
+  {
+    if (get_split_axis() == CMP_NODE_SPLIT_HORIZONTAL) {
+      return context().get_shader("compositor_split_horizontal");
+    }
+
+    return context().get_shader("compositor_split_vertical");
+  }
+
+  CMPNodeSplitAxis get_split_axis()
+  {
+    return (CMPNodeSplitAxis)bnode().custom2;
+  }
+
+  float get_split_ratio()
+  {
+    return bnode().custom1 / 100.0f;
+  }
+};
+
+static NodeOperation *get_compositor_operation(Context &context, DNode node)
+{
+  return new SplitOperation(context, node);
+}
+
+}  // namespace blender::nodes::node_composite_split_cc
+
+void register_node_type_cmp_split()
+{
+  namespace file_ns = blender::nodes::node_composite_split_cc;
+
+  static bNodeType ntype;
+
+  cmp_node_type_base(&ntype, CMP_NODE_SPLIT, "Split", NODE_CLASS_CONVERTER);
+  ntype.declare = file_ns::cmp_node_split_declare;
+  ntype.draw_buttons = file_ns::node_composit_buts_split;
+  ntype.flag |= NODE_PREVIEW;
+  ntype.initfunc = file_ns::node_composit_init_split;
+  ntype.get_compositor_operation = file_ns::get_compositor_operation;
+
+  ntype.no_muting = true;
+
+  nodeRegisterType(&ntype);
+}

scripts/startup/bl_ui/node_add_menu_compositor.py
@@ -71,7 +71,6 @@ class NODE_MT_category_compositor_output(Menu):
 
         layout = self.layout
         node_add_menu.add_node_type(layout, "CompositorNodeComposite")
-        node_add_menu.add_node_type(layout, "CompositorNodeSplitViewer")
         node_add_menu.add_node_type(layout, "CompositorNodeViewer")
         layout.separator()
         node_add_menu.add_node_type(layout, "CompositorNodeOutputFile")
@@ -279,6 +278,7 @@ class NODE_MT_category_compositor_utilities(Menu):
         node_add_menu.add_node_type(layout, "CompositorNodeLevels")
         node_add_menu.add_node_type(layout, "CompositorNodeNormalize")
         layout.separator()
+        node_add_menu.add_node_type(layout, "CompositorNodeSplit")
         node_add_menu.add_node_type(layout, "CompositorNodeSwitch")
         node_add_menu.add_node_type(
             layout, "CompositorNodeSwitchView",

source/blender/blenkernel/BKE_node.h
@@ -1017,7 +1017,8 @@ void BKE_nodetree_remove_layer_n(struct bNodeTree *ntree, struct Scene *scene, i
 #define CMP_NODE_CHROMA_MATTE 237
 #define CMP_NODE_CHANNEL_MATTE 238
 #define CMP_NODE_FLIP 239
-#define CMP_NODE_SPLITVIEWER 240
+/* Split viewer node is now a regular split node: CMP_NODE_SPLIT. */
+#define CMP_NODE_SPLITVIEWER__DEPRECATED 240
 // #define CMP_NODE_INDEX_MASK  241
 #define CMP_NODE_MAP_UV 242
 #define CMP_NODE_ID_MASK 243
@@ -1054,6 +1055,7 @@ void BKE_nodetree_remove_layer_n(struct bNodeTree *ntree, struct Scene *scene, i
 #define CMP_NODE_DESPECKLE 273
 #define CMP_NODE_ANTIALIASING 274
 #define CMP_NODE_KUWAHARA 275
+#define CMP_NODE_SPLIT 276
 
 #define CMP_NODE_GLARE 301
 #define CMP_NODE_TONEMAP 302

source/blender/blenkernel/intern/node.cc
@@ -993,8 +993,7 @@ void ntreeBlendReadData(BlendDataReader *reader, ID *owner_id, bNodeTree *ntree)
         }
         case CMP_NODE_IMAGE:
         case CMP_NODE_R_LAYERS:
-        case CMP_NODE_VIEWER:
-        case CMP_NODE_SPLITVIEWER: {
+        case CMP_NODE_VIEWER: {
           ImageUser *iuser = static_cast<ImageUser *>(node->storage);
           iuser->scene = nullptr;
           break;
@@ -3458,7 +3457,7 @@ void ntreeSetOutput(bNodeTree *ntree)
       if (ELEM(node->type, CMP_NODE_OUTPUT_FILE, GEO_NODE_VIEWER)) {
         continue;
       }
-      const bool node_is_output = ELEM(node->type, CMP_NODE_VIEWER, CMP_NODE_SPLITVIEWER);
+      const bool node_is_output = node->type == CMP_NODE_VIEWER;
 
       int output = 0;
       /* there is more types having output class, each one is checked */
@@ -3469,7 +3468,7 @@ void ntreeSetOutput(bNodeTree *ntree)
         }
 
         /* same type, exception for viewer */
-        const bool tnode_is_output = ELEM(tnode->type, CMP_NODE_VIEWER, CMP_NODE_SPLITVIEWER);
+        const bool tnode_is_output = tnode->type == CMP_NODE_VIEWER;
         const bool compositor_case = is_compositor && tnode_is_output && node_is_output;
         if (tnode->type == node->type || compositor_case) {
           if (tnode->flag & NODE_DO_OUTPUT) {

source/blender/blenloader/intern/versioning_260.cc
@@ -708,7 +708,7 @@ static const char *node_get_static_idname(int type, int treetype)
         return "CompositorNodeChannelMatte";
       case CMP_NODE_FLIP:
         return "CompositorNodeFlip";
-      case CMP_NODE_SPLITVIEWER:
+      case CMP_NODE_SPLITVIEWER__DEPRECATED:
         return "CompositorNodeSplitViewer";
       case CMP_NODE_MAP_UV:
         return "CompositorNodeMapUV";

source/blender/blenloader/intern/versioning_400.cc
@@ -325,6 +325,33 @@ static void versioning_eevee_shadow_settings(Object *object)
   SET_FLAG_FROM_TEST(object->visibility_flag, hide_shadows, OB_HIDE_SHADOW);
 }
 
+static void versioning_replace_splitviewer(bNodeTree *ntree)
+{
+  /* Split viewer was replaced with a regular split node, so add a viewer node,
+   * and link it to the new split node to achive the same behavior of the split viewer node. */
+
+  LISTBASE_FOREACH_MUTABLE (bNode *, node, &ntree->nodes) {
+    if (node->type != CMP_NODE_SPLITVIEWER__DEPRECATED) {
+      continue;
+    }
+
+    STRNCPY(node->idname, "CompositorNodeSplit");
+    node->type = CMP_NODE_SPLIT;
+    MEM_freeN(node->storage);
+
+    bNode *viewer_node = nodeAddStaticNode(nullptr, ntree, CMP_NODE_VIEWER);
+    /* Nodes are created stacked on top of each other, so separate them a bit. */
+    viewer_node->locx = node->locx + node->width + viewer_node->width / 4.0f;
+    viewer_node->locy = node->locy;
+
+    bNodeSocket *split_out_socket = nodeAddStaticSocket(
+        ntree, node, SOCK_OUT, SOCK_IMAGE, PROP_NONE, "Image", "Image");
+    bNodeSocket *viewer_in_socket = nodeFindSocket(viewer_node, SOCK_IN, "Image");
+
+    nodeAddLink(ntree, node, split_out_socket, viewer_node, viewer_in_socket);
+  }
+}
+
 void do_versions_after_linking_400(FileData *fd, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 400, 9)) {
@@ -2431,6 +2458,13 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
         SET_FLAG_FROM_TEST(material->blend_flag, transparent_shadow, MA_BL_TRANSPARENT_SHADOW);
       }
     }
+
+    FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
+      if (ntree->type == NTREE_COMPOSIT) {
+        versioning_replace_splitviewer(ntree);
+      }
+    }
+    FOREACH_NODETREE_END;
   }
 
   /* 401 6 did not require any do_version here. */

source/blender/blenloader/intern/versioning_legacy.cc
@@ -363,7 +363,7 @@ static void do_version_ntree_242_2(bNodeTree *ntree)
 {
   if (ntree->type == NTREE_COMPOSIT) {
     LISTBASE_FOREACH (bNode *, node, &ntree->nodes) {
-      if (ELEM(node->type, CMP_NODE_IMAGE, CMP_NODE_VIEWER, CMP_NODE_SPLITVIEWER)) {
+      if (ELEM(node->type, CMP_NODE_IMAGE, CMP_NODE_VIEWER)) {
         /* only image had storage */
         if (node->storage) {
           NodeImageAnim *nia = static_cast<NodeImageAnim *>(node->storage);

source/blender/compositor/CMakeLists.txt
@@ -122,6 +122,8 @@ if(WITH_COMPOSITOR_CPU)
     nodes/COM_RenderLayersNode.h
     nodes/COM_SceneTimeNode.cc
     nodes/COM_SceneTimeNode.h
+    nodes/COM_SplitNode.cc
+    nodes/COM_SplitNode.h
     nodes/COM_SwitchNode.cc
     nodes/COM_SwitchNode.h
     nodes/COM_SwitchViewNode.cc
@@ -136,8 +138,6 @@ if(WITH_COMPOSITOR_CPU)
     # output nodes
     nodes/COM_CompositorNode.cc
     nodes/COM_CompositorNode.h
-    nodes/COM_SplitViewerNode.cc
-    nodes/COM_SplitViewerNode.h
     nodes/COM_ViewLevelsNode.cc
     nodes/COM_ViewLevelsNode.h
     nodes/COM_ViewerNode.cc

FrenkyBojler/blender-ios

commit 9bccdb569ba599bb104bfe1e0037dc0ad466f71f

Fix: Split node cannot be muted
There is no reason why the split node should not be muted.
The bug was introduced with `153f14be2b` when the split view node was
converted into a regular node, but `ntype.no_muting = true;` was not
removed.

source/blender/blenloader/intern/versioning_500.cc
@@ -4084,8 +4084,7 @@ void blo_do_versions_500(FileData *fd, Library * /*lib*/, Main *bmain)
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 500, 113)) {
     /* Clear mute flag on node types that set ntype->no_muting = true. */
-    static const Set<std::string> no_muting_nodes = {"CompositorNodeSplit",
-                                                     "CompositorNodeViewer",
+    static const Set<std::string> no_muting_nodes = {"CompositorNodeViewer",
                                                      "NodeClosureInput",
                                                      "NodeClosureOutput",
                                                      "GeometryNodeForeachGeometryElementInput",

source/blender/nodes/composite/nodes/node_composite_split.cc
@@ -148,8 +148,6 @@ static void register_node_type_cmp_split()
   ntype.flag |= NODE_PREVIEW;
   ntype.get_compositor_operation = file_ns::get_compositor_operation;
 
-  ntype.no_muting = true;
-
   blender::bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(register_node_type_cmp_split)