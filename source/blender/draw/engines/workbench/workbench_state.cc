blender/blender

commit bdb5154c5fcb51b91e3f77f7746aad5809a36c61

Refactor: Mesh: Access active/default UV maps via functions
To make the transition for #122398 simpler, access the names of active UV
maps with a function. This way fewer places will need to change in one diff.
Also add a function to create a VectorSet of the UV maps on a mesh, and
access UV maps via name instead of index in a couple places (apply the
same change to color attribute in 2 places for consistency).

Pull Request: https://projects.blender.org/blender/blender/pulls/148466

source/blender/draw/engines/workbench/workbench_state.cc
@@ -9,6 +9,7 @@
 #include "BKE_camera.h"
 #include "BKE_customdata.hh"
 #include "BKE_editmesh.hh"
+#include "BKE_mesh.hh"
 #include "BKE_mesh_types.hh"
 #include "BKE_paint.hh"
 #include "BKE_paint_bvh.hh"
@@ -248,24 +249,26 @@ void SceneState::init(const DRWContext *context,
   draw_object_id = (draw_outline || draw_curvature);
 };
 
-static const CustomData *get_loop_custom_data(const Mesh *mesh)
+static bool mesh_has_color_attribute(const Mesh &mesh)
 {
-  if (mesh->runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH) {
-    BLI_assert(mesh->runtime->edit_mesh != nullptr);
-    BLI_assert(mesh->runtime->edit_mesh->bm != nullptr);
-    return &mesh->runtime->edit_mesh->bm->ldata;
+  if (mesh.runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH) {
+    const BMesh &bm = *mesh.runtime->edit_mesh->bm;
+    const BMDataLayerLookup attr = BM_data_layer_lookup(bm, mesh.active_color_attribute);
+    return attr && bke::mesh::is_color_attribute(bke::AttributeMetaData{attr.domain, attr.type});
   }
-  return &mesh->corner_data;
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  return bke::mesh::is_color_attribute(*attributes.lookup_meta_data(mesh.active_color_attribute));
 }
 
-static const CustomData *get_vert_custom_data(const Mesh *mesh)
+static bool mesh_has_uv_map_attribute(const Mesh &mesh)
 {
-  if (mesh->runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH) {
-    BLI_assert(mesh->runtime->edit_mesh != nullptr);
-    BLI_assert(mesh->runtime->edit_mesh->bm != nullptr);
-    return &mesh->runtime->edit_mesh->bm->vdata;
+  if (mesh.runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH) {
+    const BMesh &bm = *mesh.runtime->edit_mesh->bm;
+    const BMDataLayerLookup attr = BM_data_layer_lookup(bm, mesh.active_uv_map_name());
+    return attr && bke::mesh::is_uv_map(bke::AttributeMetaData{attr.domain, attr.type});
   }
-  return &mesh->vert_data;
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  return bke::mesh::is_uv_map(attributes.lookup_meta_data(mesh.active_uv_map_name()));
 }
 
 ObjectState::ObjectState(const DRWContext *draw_ctx,
@@ -288,12 +291,7 @@ ObjectState::ObjectState(const DRWContext *draw_ctx,
       return false;
     }
     const Mesh &mesh = DRW_object_get_data_for_drawing<Mesh>(*ob);
-    const CustomData *cd_vdata = get_vert_custom_data(&mesh);
-    const CustomData *cd_ldata = get_loop_custom_data(&mesh);
-    return CustomData_has_layer(cd_vdata, CD_PROP_COLOR) ||
-           CustomData_has_layer(cd_vdata, CD_PROP_BYTE_COLOR) ||
-           CustomData_has_layer(cd_ldata, CD_PROP_COLOR) ||
-           CustomData_has_layer(cd_ldata, CD_PROP_BYTE_COLOR);
+    return mesh_has_color_attribute(mesh);
   };
 
   const auto has_uv = [&]() {
@@ -301,8 +299,7 @@ ObjectState::ObjectState(const DRWContext *draw_ctx,
       return false;
     }
     const Mesh &mesh = DRW_object_get_data_for_drawing<Mesh>(*ob);
-    const CustomData *cd_ldata = get_loop_custom_data(&mesh);
-    return CustomData_has_layer(cd_ldata, CD_PROP_FLOAT2);
+    return mesh_has_uv_map_attribute(mesh);
   };
 
   if (color_type == V3D_SHADING_TEXTURE_COLOR && (!has_uv() || ob->dt < OB_TEXTURE)) {

source/blender/blenkernel/BKE_customdata.hh
@@ -618,11 +618,6 @@ int CustomData_name_maxncpy_calc(blender::StringRef name);
  */
 void CustomData_set_layer_unique_name(CustomData *data, int index);
 
-void CustomData_validate_layer_name(const CustomData *data,
-                                    eCustomDataType type,
-                                    blender::StringRef name,
-                                    char *outname);
-
 /**
  * For file reading compatibility, returns false if the layer was freed,
  * only after this test passes, `layer->data` should be assigned.

source/blender/blenkernel/BKE_paint.hh
@@ -17,6 +17,7 @@
 #include "BLI_math_vector_types.hh"
 #include "BLI_offset_indices.hh"
 #include "BLI_shared_cache.hh"
+#include "BLI_string_ref.hh"
 #include "BLI_utility_mixins.hh"
 #include "BLI_vector.hh"
 
@@ -649,7 +650,8 @@ bool BKE_paint_canvas_image_get(PaintModeSettings *settings,
                                 Object *ob,
                                 Image **r_image,
                                 ImageUser **r_image_user);
-int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Object *ob);
+std::optional<blender::StringRef> BKE_paint_canvas_uvmap_name_get(
+    const PaintModeSettings *settings, Object *ob);
 void BKE_sculpt_cavity_curves_ensure(Sculpt *sd);
 CurveMapping *BKE_sculpt_default_cavity_curve();
 CurveMapping *BKE_paint_default_curve();

source/blender/blenkernel/intern/customdata.cc
@@ -4308,30 +4308,6 @@ void CustomData_set_layer_unique_name(CustomData *data, const int index)
       name_maxncpy);
 }
 
-void CustomData_validate_layer_name(const CustomData *data,
-                                    const eCustomDataType type,
-                                    const StringRef name,
-                                    char *outname)
-{
-  int index = -1;
-
-  /* if a layer name was given, try to find that layer */
-  if (!name.is_empty()) {
-    index = CustomData_get_named_layer_index(data, type, name);
-  }
-
-  if (index == -1) {
-    /* either no layer was specified, or the layer we want has been
-     * deleted, so assign the active layer to name
-     */
-    index = CustomData_get_active_layer_index(data, type);
-    BLI_strncpy_utf8(outname, data->layers[index].name, MAX_CUSTOMDATA_LAYER_NAME);
-  }
-  else {
-    name.copy_utf8_truncated(outname, MAX_CUSTOMDATA_LAYER_NAME);
-  }
-}
-
 bool CustomData_verify_versions(CustomData *data, const int index)
 {
   const LayerTypeInfo *typeInfo;

source/blender/blenkernel/intern/dynamicpaint.cc
@@ -1639,15 +1639,14 @@ static void dynamicPaint_setInitialColor(const Scene * /*scene*/, DynamicPaintSu
     const blender::Span<int> corner_verts = mesh->corner_verts();
     const blender::Span<int3> corner_tris = mesh->corner_tris();
 
-    char uvname[MAX_CUSTOMDATA_LAYER_NAME];
-
     if (!tex) {
       return;
     }
 
     /* get uv map */
-    CustomData_validate_layer_name(
-        &mesh->corner_data, CD_PROP_FLOAT2, surface->init_layername, uvname);
+    const StringRef uvname = mesh->uv_map_names().contains(surface->init_layername) ?
+                                 surface->init_layername :
+                                 mesh->active_uv_map_name();
     const VArraySpan uv_map = *attributes.lookup<float2>(uvname, bke::AttrDomain::Corner);
 
     if (uv_map.is_empty()) {
@@ -2833,7 +2832,6 @@ int dynamicPaint_createUVSurface(Scene *scene,
   using namespace blender;
   /* Anti-alias jitter point relative coords. */
   const int aa_samples = (surface->flags & MOD_DPAINT_ANTIALIAS) ? 5 : 1;
-  char uvname[MAX_CUSTOMDATA_LAYER_NAME];
   uint32_t active_points = 0;
   bool error = false;
 
@@ -2862,9 +2860,11 @@ int dynamicPaint_createUVSurface(Scene *scene,
   const blender::Span<int3> corner_tris = mesh->corner_tris();
 
   /* get uv map */
-  if (CustomData_has_layer(&mesh->corner_data, CD_PROP_FLOAT2)) {
-    CustomData_validate_layer_name(
-        &mesh->corner_data, CD_PROP_FLOAT2, surface->uvlayer_name, uvname);
+  const VectorSet<StringRefNull> uv_map_names = mesh->uv_map_names();
+  if (!uv_map_names.is_empty()) {
+    const StringRef uvname = uv_map_names.contains(surface->uvlayer_name) ?
+                                 surface->uvlayer_name :
+                                 mesh->active_uv_map_name();
     const bke::AttributeAccessor attributes = mesh->attributes();
     uv_map = *attributes.lookup<float2>(uvname, bke::AttrDomain::Corner);
   }

source/blender/blenkernel/intern/mesh.cc
@@ -1191,6 +1191,45 @@ blender::bke::MutableAttributeAccessor Mesh::attributes_for_write()
                                                 blender::bke::mesh_attribute_accessor_functions());
 }
 
+blender::VectorSet<blender::StringRefNull> Mesh::uv_map_names() const
+{
+  blender::VectorSet<blender::StringRefNull> names;
+  this->attributes().foreach_attribute([&](const blender::bke::AttributeIter &iter) {
+    if (blender::bke::mesh::is_uv_map({iter.domain, iter.data_type})) {
+      names.add_new(iter.name);
+    }
+  });
+  return names;
+}
+
+blender::StringRefNull Mesh::active_uv_map_name() const
+{
+  /* Currently this information is stored in CustomData. Once it switches to using
+   * #Mesh::active_uv_map_attribute, this logic can be removed. This function's only purpose is to
+   * ease that transition. */
+  if (this->runtime->edit_mesh) {
+    const char *name = CustomData_get_active_layer_name(&this->runtime->edit_mesh->bm->ldata,
+                                                        CD_PROP_FLOAT2);
+    return name ? name : "";
+  }
+  const char *name = CustomData_get_active_layer_name(&this->corner_data, CD_PROP_FLOAT2);
+  return name ? name : "";
+}
+
+blender::StringRefNull Mesh::default_uv_map_name() const
+{
+  /* Currently this information is stored in CustomData. Once it switches to using
+   * #Mesh::default_uv_map_attribute, this logic can be removed. This function's only purpose is to
+   * ease that transition. */
+  if (this->runtime->edit_mesh) {
+    const char *name = CustomData_get_render_layer_name(&this->runtime->edit_mesh->bm->ldata,
+                                                        CD_PROP_FLOAT2);
+    return name ? name : "";
+  }
+  const char *name = CustomData_get_render_layer_name(&this->corner_data, CD_PROP_FLOAT2);
+  return name ? name : "";
+}
+
 Mesh *BKE_mesh_new_nomain(const int verts_num,
                           const int edges_num,
                           const int faces_num,

source/blender/blenkernel/intern/object_dupli.cc
@@ -1334,6 +1334,7 @@ static void make_child_duplis_faces_from_editmesh(const DupliContext *ctx,
 
 static void make_duplis_faces(const DupliContext *ctx)
 {
+  using namespace blender;
   Object *parent = ctx->object;
 
   /* Gather mesh info. */
@@ -1348,29 +1349,28 @@ static void make_duplis_faces(const DupliContext *ctx)
   FaceDupliData_Params fdd_params = {ctx, (parent->transflag & OB_DUPLIFACES_SCALE) != 0};
 
   if (em != nullptr) {
-    const int uv_idx = CustomData_get_render_layer(&em->bm->ldata, CD_PROP_FLOAT2);
+    const int uv_offset = CustomData_get_offset_named(
+        &em->bm->ldata, CD_PROP_FLOAT2, mesh_eval->default_uv_map_name());
     FaceDupliData_EditMesh fdd{};
     fdd.params = fdd_params;
     fdd.em = em;
     fdd.vert_positions_deform = vert_positions_deform;
     fdd.has_orco = !vert_positions_deform.is_empty();
-    fdd.has_uvs = (uv_idx != -1);
-    fdd.cd_loop_uv_offset = (uv_idx != -1) ?
-                                CustomData_get_n_offset(&em->bm->ldata, CD_PROP_FLOAT2, uv_idx) :
-                                -1;
+    fdd.has_uvs = (uv_offset != -1);
+    fdd.cd_loop_uv_offset = uv_offset;
     make_child_duplis(ctx, &fdd, make_child_duplis_faces_from_editmesh);
   }
   else {
-    const int uv_idx = CustomData_get_render_layer(&mesh_eval->corner_data, CD_PROP_FLOAT2);
+    const bke::AttributeAccessor attributes = mesh_eval->attributes();
+    const VArraySpan uv_map = *attributes.lookup<float2>(mesh_eval->default_uv_map_name(),
+                                                         bke::AttrDomain::Corner);
     FaceDupliData_Mesh fdd{};
     fdd.params = fdd_params;
     fdd.totface = mesh_eval->faces_num;
     fdd.faces = mesh_eval->faces();
     fdd.corner_verts = mesh_eval->corner_verts();
     fdd.vert_positions = mesh_eval->vert_positions();
-    fdd.uv_map = (uv_idx != -1) ? (const float2 *)CustomData_get_layer_n(
-                                      &mesh_eval->corner_data, CD_PROP_FLOAT2, uv_idx) :
-                                  nullptr;
+    fdd.uv_map = uv_map.is_empty() ? nullptr : uv_map.data();
     fdd.orco = (const float (*)[3])CustomData_get_layer(&mesh_eval->vert_data, CD_ORCO);
 
     make_child_duplis(ctx, &fdd, make_child_duplis_faces_from_mesh);

source/blender/blenkernel/intern/paint_canvas.cc
@@ -9,9 +9,10 @@
 #include "DNA_mesh_types.h"
 #include "DNA_scene_types.h"
 
-#include "BKE_customdata.hh"
+#include "BKE_attribute.hh"
 #include "BKE_image.hh"
 #include "BKE_material.hh"
+#include "BKE_mesh.hh"
 #include "BKE_paint.hh"
 
 #include "IMB_imbuf_types.hh"
@@ -71,19 +72,29 @@ bool BKE_paint_canvas_image_get(PaintModeSettings *settings,
   return *r_image != nullptr;
 }
 
-int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Object *ob)
+static bool has_uv_map_attribute(const Mesh &mesh, const blender::StringRef name)
+{
+  using namespace blender;
+  return bke::mesh::is_uv_map(mesh.attributes().lookup_meta_data(name));
+}
+
+std::optional<blender::StringRef> BKE_paint_canvas_uvmap_name_get(
+    const PaintModeSettings *settings, Object *ob)
 {
   switch (settings->canvas_source) {
     case PAINT_CANVAS_SOURCE_COLOR_ATTRIBUTE:
-      return -1;
+      return std::nullopt;
     case PAINT_CANVAS_SOURCE_IMAGE: {
       /* Use active uv map of the object. */
       if (ob->type != OB_MESH) {
-        return -1;
+        return std::nullopt;
       }
 
       const Mesh *mesh = static_cast<Mesh *>(ob->data);
-      return CustomData_get_active_layer_index(&mesh->corner_data, CD_PROP_FLOAT2);
+      if (!has_uv_map_attribute(*mesh, mesh->active_uv_map_name())) {
+        return std::nullopt;
+      }
+      return mesh->active_uv_map_name();
     }
     case PAINT_CANVAS_SOURCE_MATERIAL: {
       /* Use uv map of the canvas. */
@@ -93,25 +104,27 @@ int BKE_paint_canvas_uvmap_layer_index_get(const PaintModeSettings *settings, Ob
       }
 
       if (ob->type != OB_MESH) {
-        return -1;
+        return std::nullopt;
       }
 
       if (slot->uvname == nullptr) {
-        return -1;
+        return std::nullopt;
       }
 
       const Mesh *mesh = static_cast<Mesh *>(ob->data);
-      return CustomData_get_named_layer_index(&mesh->corner_data, CD_PROP_FLOAT2, slot->uvname);
+      if (!has_uv_map_attribute(*mesh, slot->uvname)) {
+        return std::nullopt;
+      }
+      return slot->uvname;
     }
   }
-  return -1;
+  return std::nullopt;
 }
 
 char *BKE_paint_canvas_key_get(PaintModeSettings *settings, Object *ob)
 {
   std::stringstream ss;
-  int active_uv_map_layer_index = BKE_paint_canvas_uvmap_layer_index_get(settings, ob);
-  ss << "UV_MAP:" << active_uv_map_layer_index;
+  ss << "UV_MAP:" << BKE_paint_canvas_uvmap_name_get(settings, ob).value_or("");
 
   Image *image;
   ImageUser *image_user;

FrenkyBojler/blender-ios

commit ff1629f184437d8d23339a5e7e2516bd3f4ad1aa

Fix: Workbench color attribute check null optional dereference
Caused by bdb5154c5fcb51b91e3f77f7746aad5809a36c61

source/blender/draw/engines/workbench/workbench_state.cc
@@ -257,7 +257,7 @@ static bool mesh_has_color_attribute(const Mesh &mesh)
     return attr && bke::mesh::is_color_attribute(bke::AttributeMetaData{attr.domain, attr.type});
   }
   const bke::AttributeAccessor attributes = mesh.attributes();
-  return bke::mesh::is_color_attribute(*attributes.lookup_meta_data(mesh.active_color_attribute));
+  return bke::mesh::is_color_attribute(attributes.lookup_meta_data(mesh.active_color_attribute));
 }
 
 static bool mesh_has_uv_map_attribute(const Mesh &mesh)