f0d72ac27e6811c833559532be5d9100bf10c1b3
Brecht Van Lommel <brecht@blender.org>
2025-11-03 11:59:43 +0100

    Cleanup: make format

--- tests/blender_as_python_module/cleanup_bpy.py
+++ tests/blender_as_python_module/cleanup_bpy.py
@@ -8,12 +8,15 @@ sys.path.append(sys.argv[1])
 
 import bpy
 
+
 def _update_handler(self, context):
     pass
+
 
 def main():
     # This used to cause a memory leak and crash due to missing _bpy cleanup.
     bpy.types.Object.custom_property = bpy.props.FloatProperty(update=_update_handler)
+
 
 if __name__ == "__main__":
     main()
