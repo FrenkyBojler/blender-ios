blender/blender

commit 583ae8990cc60896ce86014227b28a37cc771691

Sculpt: Optimize sculpt vertex neighbor storage
Currently we calculate neighbors for vertices in batches. The idea is
to access topology data all at once, storing the result in a local node-
sized vector that can fit in CPU caches. Currently we use
`Vector<Vector<int>>` for storing the neighbors, and that works well
enough when the number of neighbors fits into the inline buffer, but
whenever it doesn't it causes many vector reallocations and scatters
the neighbor storage in arbitrary memory locations.

This commit changes to using a `GroupedSpan` to store the neighbors, in
other words contiguous storage split into groups by an array of offset
integers. This reduces local per-vertex memory usage by 20 bytes and
makes memory access more predictable.

I observed a 1.39x performance improvement for the mesh smooth filter
on a large triangulated mesh, and a 1.14x improvement for a mesh of
mostly quads. In the brush benchmark on a quad mesh the difference I
observed is a small slowdown. I'm not sure why that happens but I trust
the results from the filter a bit more.

Resolves #130797.

Pull Request: https://projects.blender.org/blender/blender/pulls/130941

source/blender/editors/sculpt_paint/sculpt.cc
@@ -449,6 +449,37 @@ Span<int> vert_neighbors_get_mesh(const OffsetIndices<int> faces,
   return r_neighbors.as_span();
 }
 
+inline void append_neighbors_to_vector(const OffsetIndices<int> faces,
+                                       const Span<int> corner_verts,
+                                       const GroupedSpan<int> vert_to_face,
+                                       const Span<bool> hide_poly,
+                                       const int vert,
+                                       Vector<int> &r_data)
+{
+  const int vert_start = r_data.size();
+  for (const int face : vert_to_face[vert]) {
+    if (!hide_poly.is_empty() && hide_poly[face]) {
+      continue;
+    }
+    /* In order to support non-manifold topology, both neighboring vertices are added for each
+     * face corner. That results in half being duplicates for any "normal" topology. */
+    const int2 neighbors = bke::mesh::face_find_adjacent_verts(faces[face], corner_verts, vert);
+    for (const int neighbor : {neighbors[0], neighbors[1]}) {
+      bool found = false;
+      for (int i = r_data.size() - 1; i >= vert_start; i--) {
+        if (r_data[i] == neighbor) {
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        continue;
+      }
+      r_data.append(neighbor);
+    }
+  }
+}
+
 namespace boundary {
 
 bool vert_is_boundary(const GroupedSpan<int> vert_to_face_map,
@@ -7334,88 +7365,112 @@ OffsetIndices<int> create_node_vert_offsets_bmesh(const Span<bke::pbvh::BMeshNod
   return offset_indices::accumulate_counts_to_offsets(node_data);
 }
 
-void calc_vert_neighbors(const OffsetIndices<int> faces,
-                         const Span<int> corner_verts,
-                         const GroupedSpan<int> vert_to_face,
-                         const Span<bool> hide_poly,
-                         const Span<int> verts,
-                         const MutableSpan<Vector<int>> result)
+GroupedSpan<int> calc_vert_neighbors(const OffsetIndices<int> faces,
+                                     const Span<int> corner_verts,
+                                     const GroupedSpan<int> vert_to_face,
+                                     const Span<bool> hide_poly,
+                                     const Span<int> verts,
+                                     Vector<int> &r_offset_data,
+                                     Vector<int> &r_data)
 {
-  BLI_assert(result.size() == verts.size());
   BLI_assert(corner_verts.size() == faces.total_size());
+  r_offset_data.resize(verts.size() + 1);
+  r_data.clear();
   for (const int i : verts.index_range()) {
-    vert_neighbors_get_mesh(faces, corner_verts, vert_to_face, hide_poly, verts[i], result[i]);
+    r_offset_data[i] = r_data.size();
+    append_neighbors_to_vector(faces, corner_verts, vert_to_face, hide_poly, verts[i], r_data);
   }
+  r_offset_data.last() = r_data.size();
+  return GroupedSpan<int>(r_offset_data.as_span(), r_data.as_span());
 }
 
-void calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
-                         const Span<int> grids,
-                         const MutableSpan<Vector<SubdivCCGCoord>> result)
+GroupedSpan<int> calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
+                                     const Span<int> grids,
+                                     Vector<int> &r_offset_data,
+                                     Vector<int> &r_data)
 {
   const CCGKey key = BKE_subdiv_ccg_key_top_level(subdiv_ccg);
   SubdivCCGNeighbors neighbors;
-  BLI_assert(result.size() == grids.size() * key.grid_area);
+
+  r_offset_data.resize(key.grid_area * grids.size() + 1);
+  r_data.clear();
+
   for (const int i : grids.index_range()) {
     const int grid = grids[i];
     const int node_verts_start = i * key.grid_area;
+    r_offset_data[node_verts_start] = r_data.size();
 
-    for (const int y : IndexRange(key.grid_size)) {
-      for (const int x : IndexRange(key.grid_size)) {
-        const int offset = CCG_grid_xy_to_index(key.grid_size, x, y);
-        const int node_vert_index = node_verts_start + offset;
-
+    for (const short y : IndexRange(key.grid_size)) {
+      for (const short x : IndexRange(key.grid_size)) {
         SubdivCCGCoord coord{};
         coord.grid_index = grid;
         coord.x = x;
         coord.y = y;
-
         BKE_subdiv_ccg_neighbor_coords_get(subdiv_ccg, coord, false, neighbors);
-
-        result[node_vert_index] = Vector<SubdivCCGCoord>(neighbors.coords.as_span());
+        for (const SubdivCCGCoord neighbor : neighbors.coords) {
+          r_data.append(neighbor.to_index(key));
+        }
       }
     }
   }
+  return GroupedSpan<int>(r_offset_data.as_span(), r_data.as_span());
 }
-void calc_vert_neighbors(Set<BMVert *, 0> verts, const MutableSpan<Vector<BMVert *>> result)
-{
-  BLI_assert(verts.size() == result.size());
 
-  int i = 0;
+GroupedSpan<BMVert *> calc_vert_neighbors(Set<BMVert *, 0> verts,
+                                          Vector<int> &r_offset_data,
+                                          Vector<BMVert *> &r_data)
+{
+  r_offset_data.resize(verts.size() + 1);
+  r_data.clear();
+
   Vector<BMVert *, 64> neighbor_data;
+  int i = 0;
   for (BMVert *vert : verts) {
-    Span<BMVert *> neighbors = vert_neighbors_get_bmesh(*vert, neighbor_data);
-    result[i] = Vector<BMVert *>(neighbors);
+    r_offset_data[i] = r_data.size();
+    r_data.extend(vert_neighbors_get_bmesh(*vert, neighbor_data));
     i++;
   }
+  r_offset_data.last() = r_data.size();
+  return GroupedSpan<BMVert *>(r_offset_data.as_span(), r_data.as_span());
 }
 
-void calc_vert_neighbors_interior(const OffsetIndices<int> faces,
-                                  const Span<int> corner_verts,
-                                  const GroupedSpan<int> vert_to_face,
-                                  const BitSpan boundary_verts,
-                                  const Span<bool> hide_poly,
-                                  const Span<int> verts,
-                                  const MutableSpan<Vector<int>> result)
+GroupedSpan<int> calc_vert_neighbors_interior(const OffsetIndices<int> faces,
+                                              const Span<int> corner_verts,
+                                              const GroupedSpan<int> vert_to_face,
+                                              const BitSpan boundary_verts,
+                                              const Span<bool> hide_poly,
+                                              const Span<int> verts,
+                                              Vector<int> &r_offset_data,
+                                              Vector<int> &r_data)
 {
-  BLI_assert(result.size() == verts.size());
   BLI_assert(corner_verts.size() == faces.total_size());
+
+  r_offset_data.resize(verts.size() + 1);
+  r_data.clear();
 
   for (const int i : verts.index_range()) {
     const int vert = verts[i];
-    Vector<int> &neighbors = result[i];
-    vert_neighbors_get_mesh(faces, corner_verts, vert_to_face, hide_poly, verts[i], neighbors);
+    const int vert_start = r_data.size();
+    r_offset_data[i] = vert_start;
+    append_neighbors_to_vector(faces, corner_verts, vert_to_face, hide_poly, vert, r_data);
 
     if (boundary_verts[vert]) {
-      if (neighbors.size() == 2) {
-        /* Do not include neighbors of corner vertices. */
-        neighbors.clear();
+      /* Do not include neighbors of corner vertices. */
+      if (r_data.size() == vert_start + 2) {
+        r_data.resize(vert_start);
       }
       else {
         /* Only include other boundary vertices as neighbors of boundary vertices. */
-        neighbors.remove_if([&](const int vert) { return !boundary_verts[vert]; });
+        for (int neighbor_i = r_data.size() - 1; neighbor_i >= vert_start; neighbor_i--) {
+          if (!boundary_verts[r_data[neighbor_i]]) {
+            r_data.remove_and_reorder(neighbor_i);
+          }
+        }
       }
     }
   }
+  r_offset_data.last() = r_data.size();
+  return GroupedSpan<int>(r_offset_data.as_span(), r_data.as_span());
 }
 
 void calc_vert_neighbors_interior(const OffsetIndices<int> faces,

source/blender/editors/sculpt_paint/brushes/enhance_details.cc
@@ -34,7 +34,8 @@ struct LocalData {
   Vector<float3> new_positions;
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
+  Vector<int> neighbor_offsets;
+  Vector<int> neighbor_data;
   Vector<float3> translations;
 };
 
@@ -138,9 +139,13 @@ static void calc_translations_faces(const Span<float3> vert_positions,
 {
   const Span<int> verts = node.verts();
 
-  tls.vert_neighbors.resize(verts.size());
-  const MutableSpan<Vector<int>> neighbors = tls.vert_neighbors;
-  calc_vert_neighbors(faces, corner_verts, vert_to_face_map, hide_poly, verts, neighbors);
+  const GroupedSpan<int> neighbors = calc_vert_neighbors(faces,
+                                                         corner_verts,
+                                                         vert_to_face_map,
+                                                         hide_poly,
+                                                         verts,
+                                                         tls.neighbor_offsets,
+                                                         tls.neighbor_data);
 
   tls.new_positions.resize(verts.size());
   const MutableSpan<float3> new_positions = tls.new_positions;

source/blender/editors/sculpt_paint/brushes/relax.cc
@@ -33,19 +33,16 @@ inline namespace relax_cc {
 struct MeshLocalData {
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
 };
 
 struct GridLocalData {
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<SubdivCCGCoord>> vert_neighbors;
 };
 
 struct BMeshLocalData {
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<BMVert *>> vert_neighbors;
 };
 
 static void apply_positions_faces(const Sculpt &sd,
@@ -189,7 +186,6 @@ static void do_relax_face_sets_brush_mesh(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    MeshLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_faces(
         position_data.eval,
         vert_normals,
@@ -202,7 +198,6 @@ static void do_relax_face_sets_brush_mesh(const Depsgraph &depsgraph,
         relax_face_sets,
         nodes[i].verts(),
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 
@@ -321,7 +316,6 @@ static void do_relax_face_sets_brush_grids(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    GridLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_grids(
         subdiv_ccg,
         faces,
@@ -332,7 +326,6 @@ static void do_relax_face_sets_brush_grids(const Depsgraph &depsgraph,
         nodes[i].grids(),
         relax_face_sets,
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 
@@ -425,14 +418,12 @@ static void do_relax_face_sets_brush_bmesh(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    BMeshLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_bmesh(
         BKE_pbvh_bmesh_node_unique_verts(&nodes[i]),
         current_positions.as_mutable_span().slice(node_vert_offsets[pos]),
         face_set_offset,
         relax_face_sets,
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 
@@ -528,7 +519,6 @@ static void do_topology_relax_brush_mesh(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    MeshLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_faces(
         position_data.eval,
         vert_normals,
@@ -541,7 +531,6 @@ static void do_topology_relax_brush_mesh(const Depsgraph &depsgraph,
         false,
         nodes[i].verts(),
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 
@@ -643,7 +632,6 @@ static void do_topology_relax_brush_grids(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    GridLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_grids(
         subdiv_ccg,
         faces,
@@ -654,7 +642,6 @@ static void do_topology_relax_brush_grids(const Depsgraph &depsgraph,
         nodes[i].grids(),
         false,
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 
@@ -745,14 +732,12 @@ static void do_topology_relax_brush_bmesh(const Depsgraph &depsgraph,
   });
 
   node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
-    BMeshLocalData &tls = all_tls.local();
     smooth::calc_relaxed_translations_bmesh(
         BKE_pbvh_bmesh_node_unique_verts(&nodes[i]),
         current_positions.as_mutable_span().slice(node_vert_offsets[pos]),
         face_set_offset,
         false,
         factors.as_span().slice(node_vert_offsets[pos]),
-        tls.vert_neighbors,
         translations.as_mutable_span().slice(node_vert_offsets[pos]));
   });
 

source/blender/editors/sculpt_paint/brushes/smooth.cc
@@ -51,7 +51,8 @@ struct LocalData {
   Vector<float3> positions;
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
+  Vector<int> neighbor_offsets;
+  Vector<int> neighbor_data;
   Vector<float3> new_positions;
   Vector<float3> translations;
 };
@@ -126,18 +127,18 @@ BLI_NOINLINE static void do_smooth_brush_mesh(const Depsgraph &depsgraph,
     node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
       LocalData &tls = all_tls.local();
       const Span<int> verts = nodes[i].verts();
-      tls.vert_neighbors.resize(verts.size());
-      calc_vert_neighbors_interior(faces,
-                                   corner_verts,
-                                   vert_to_face_map,
-                                   ss.vertex_info.boundary,
-                                   attribute_data.hide_poly,
-                                   verts,
-                                   tls.vert_neighbors);
+      const GroupedSpan<int> neighbors = calc_vert_neighbors_interior(faces,
+                                                                      corner_verts,
+                                                                      vert_to_face_map,
+                                                                      ss.vertex_info.boundary,
+                                                                      attribute_data.hide_poly,
+                                                                      verts,
+                                                                      tls.neighbor_offsets,
+                                                                      tls.neighbor_data);
       smooth::neighbor_data_average_mesh_check_loose(
           position_data.eval,
           verts,
-          tls.vert_neighbors,
+          neighbors,
           new_positions.as_mutable_span().slice(node_vert_offsets[pos]));
     });
 

source/blender/editors/sculpt_paint/brushes/smooth_mask.cc
@@ -31,7 +31,8 @@ struct LocalData {
   Vector<float3> positions;
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
+  Vector<int> neighbor_offsets;
+  Vector<int> neighbor_data;
   Vector<float> masks;
   Vector<float> new_masks;
 };
@@ -52,21 +53,6 @@ static Vector<float> iteration_strengths(const float strength)
   result.append_n_times(1.0f, count);
   result.append(last);
   return result;
-}
-
-static void calc_smooth_masks_faces(const OffsetIndices<int> faces,
-                                    const Span<int> corner_verts,
-                                    const GroupedSpan<int> vert_to_face_map,
-                                    const Span<bool> hide_poly,
-                                    const Span<int> verts,
-                                    const Span<float> masks,
-                                    LocalData &tls,
-                                    const MutableSpan<float> new_masks)
-{
-  tls.vert_neighbors.resize(verts.size());
-  calc_vert_neighbors(faces, corner_verts, vert_to_face_map, hide_poly, verts, tls.vert_neighbors);
-  const Span<Vector<int>> vert_neighbors = tls.vert_neighbors;
-  smooth::neighbor_data_average_mesh(masks, vert_neighbors, new_masks);
 }
 
 static void apply_masks_faces(const Depsgraph &depsgraph,
@@ -154,14 +140,17 @@ static void do_smooth_brush_mesh(const Depsgraph &depsgraph,
      * neighboring nodes. */
     node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {
       LocalData &tls = all_tls.local();
-      calc_smooth_masks_faces(faces,
-                              corner_verts,
-                              vert_to_face_map,
-                              hide_poly,
-                              nodes[i].verts(),
-                              mask.span.as_span(),
-                              tls,
-                              new_masks.as_mutable_span().slice(node_vert_offsets[pos]));
+      const GroupedSpan<int> neighbors = calc_vert_neighbors(faces,
+                                                             corner_verts,
+                                                             vert_to_face_map,
+                                                             hide_poly,
+                                                             nodes[i].verts(),
+                                                             tls.neighbor_offsets,
+                                                             tls.neighbor_data);
+      smooth::neighbor_data_average_mesh(
+          mask.span.as_span(),
+          neighbors,
+          new_masks.as_mutable_span().slice(node_vert_offsets[pos]));
     });
 
     node_mask.foreach_index(GrainSize(1), [&](const int i, const int pos) {

source/blender/editors/sculpt_paint/brushes/surface_smooth.cc
@@ -35,7 +35,8 @@ struct LocalData {
   Vector<float3> positions;
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
+  Vector<int> neighbor_offsets;
+  Vector<int> neighbor_data;
   Vector<float3> laplacian_disp;
   Vector<float3> average_positions;
   Vector<float3> translations;
@@ -112,18 +113,17 @@ BLI_NOINLINE static void do_surface_smooth_brush_mesh(const Depsgraph &depsgraph
       const OrigPositionData orig_data = orig_position_data_get_mesh(object, nodes[i]);
       const Span<float> factors = all_factors.as_span().slice(node_offsets[pos]);
 
-      tls.vert_neighbors.resize(verts.size());
-      calc_vert_neighbors(faces,
-                          corner_verts,
-                          vert_to_face_map,
-                          attribute_data.hide_poly,
-                          verts,
-                          tls.vert_neighbors);
+      const GroupedSpan<int> neighbors = calc_vert_neighbors(faces,
+                                                             corner_verts,
+                                                             vert_to_face_map,
+                                                             attribute_data.hide_poly,
+                                                             verts,
+                                                             tls.neighbor_offsets,
+                                                             tls.neighbor_data);
 
       tls.average_positions.resize(verts.size());
       const MutableSpan<float3> average_positions = tls.average_positions;
-      smooth::neighbor_data_average_mesh(
-          position_data.eval, tls.vert_neighbors, average_positions);
+      smooth::neighbor_data_average_mesh(position_data.eval, neighbors, average_positions);
 
       tls.laplacian_disp.resize(verts.size());
       const MutableSpan<float3> laplacian_disp = tls.laplacian_disp;
@@ -147,18 +147,18 @@ BLI_NOINLINE static void do_surface_smooth_brush_mesh(const Depsgraph &depsgraph
       const MutableSpan<float3> laplacian_disp = gather_data_mesh(
           all_laplacian_disp.as_span(), verts, tls.laplacian_disp);
 
-      tls.vert_neighbors.resize(verts.size());
-      calc_vert_neighbors(faces,
-                          corner_verts,
-                          vert_to_face_map,
-                          attribute_data.hide_poly,
-                          verts,
-                          tls.vert_neighbors);
+      const GroupedSpan<int> neighbors = calc_vert_neighbors(faces,
+                                                             corner_verts,
+                                                             vert_to_face_map,
+                                                             attribute_data.hide_poly,
+                                                             verts,
+                                                             tls.neighbor_offsets,
+                                                             tls.neighbor_data);
 
       tls.average_positions.resize(verts.size());
       const MutableSpan<float3> average_laplacian_disps = tls.average_positions;
       smooth::neighbor_data_average_mesh(
-          all_laplacian_disp.as_span(), tls.vert_neighbors, average_laplacian_disps);
+          all_laplacian_disp.as_span(), neighbors, average_laplacian_disps);
 
       tls.translations.resize(verts.size());
       const MutableSpan<float3> translations = tls.translations;

source/blender/editors/sculpt_paint/brushes/topology_slide.cc
@@ -34,7 +34,8 @@ struct LocalData {
   Vector<float3> positions;
   Vector<float> factors;
   Vector<float> distances;
-  Vector<Vector<int>> vert_neighbors;
+  Vector<int> neighbor_offsets;
+  Vector<int> neighbor_data;
   Vector<float3> translations;
 };
 
@@ -75,7 +76,7 @@ static inline void add_neighbor_influence(const float3 &position,
 
 BLI_NOINLINE static void calc_neighbor_influence(const Span<float3> vert_positions,
                                                  const Span<float3> positions,
-                                                 const Span<Vector<int>> vert_neighbors,
+                                                 const GroupedSpan<int> vert_neighbors,
                                                  const MutableSpan<float3> translations)
 {
   for (const int i : positions.index_range()) {
@@ -181,15 +182,18 @@ static void calc_faces(const Depsgraph &depsgraph,
 
   scale_factors(tls.factors, cache.bstrength);
 
-  tls.vert_neighbors.resize(verts.size());
-  calc_vert_neighbors(
-      faces, corner_verts, vert_to_face_map, attribute_data.hide_poly, verts, tls.vert_neighbors);
-  const Span<Vector<int>> vert_neighbors = tls.vert_neighbors;
+  const GroupedSpan<int> neighbors = calc_vert_neighbors(faces,
+                                                         corner_verts,
+                                                         vert_to_face_map,
+                                                         attribute_data.hide_poly,
+                                                         verts,
+                                                         tls.neighbor_offsets,
+                                                         tls.neighbor_data);
 
   tls.translations.resize(verts.size());
   const MutableSpan<float3> translations = tls.translations;
   calc_translation_directions(brush, cache, positions, translations);
-  calc_neighbor_influence(position_data.eval, positions, vert_neighbors, translations);
+  calc_neighbor_influence(position_data.eval, positions, neighbors, translations);
   scale_translations(translations, tls.factors);
 
   clip_and_lock_translations(sd, ss, position_data.eval, verts, translations);

FrenkyBojler/blender-ios

commit b050faf6d90cbd0c754473973f3c799dc7513854

Fix #149229: Multires Boundary Smooth brush crashes
Introduced with 583ae8990cc60896ce86014227b28a37cc771691

The multires variant of `calc_vert_neighbors` incorrectly populated the
offset data for its `GroupedSpan`:
* Entries were only populated once per grid, not once per grid
  coordinate
* The final offset was not populated

This lead to slices of data accessing invalid memory due to the
neighboring offset values being uninitialized data and resulting in
negative or arbitrarily sized slices

source/blender/editors/sculpt_paint/sculpt.cc
@@ -7627,10 +7627,12 @@ GroupedSpan<int> calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
   for (const int i : grids.index_range()) {
     const int grid = grids[i];
     const int node_verts_start = i * key.grid_area;
-    r_offset_data[node_verts_start] = r_data.size();
 
     for (const short y : IndexRange(key.grid_size)) {
       for (const short x : IndexRange(key.grid_size)) {
+        const int offset = CCG_grid_xy_to_index(key.grid_size, x, y);
+        r_offset_data[node_verts_start + offset] = r_data.size();
+
         SubdivCCGCoord coord{};
         coord.grid_index = grid;
         coord.x = x;
@@ -7642,6 +7644,7 @@ GroupedSpan<int> calc_vert_neighbors(const SubdivCCG &subdiv_ccg,
       }
     }
   }
+  r_offset_data.last() = r_data.size();
   return GroupedSpan<int>(r_offset_data.as_span(), r_data.as_span());
 }
 