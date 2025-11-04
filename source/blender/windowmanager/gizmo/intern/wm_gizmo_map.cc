6c12793c776ebc550c0574f8b14011afe209a2a1
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-04 08:34:28 +0000

    Fixes small scale gizmo issue

--- source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
+++ source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
@@ -644,7 +644,7 @@ static int gizmo_find_intersected_3d_intern(wmGizmo **visible_gizmos,
       GPU_matrix_unproject_3fv(co_screen, rv3d->viewinv, rv3d->winmat, viewport, co_3d);
       float select_bias = gz->select_bias;
       if ((gz->flag & WM_GIZMO_DRAW_NO_SCALE) == 0) {
-        select_bias *= gz->scale_final;
+        select_bias *= gz->scale_basis;
       }
       sub_v3_v3(co_3d, co_3d_origin);
       const float dot_test = dot_v3v3(co_3d, co_direction) - select_bias;
