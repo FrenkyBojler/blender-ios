098e7a7e3967d8ee1b7b2681ae1331879409cd80
Julian Eisel <julian@blender.org>
2025-11-04 16:07:32 +0100

    Compositor: Hide essentials from asset shelf meant for menu only
    These essentials assets are meant to be exposed in the "Add" menu but
    don't really fit into the asset shelf. They are just implemented as
    assets because they can be created from other nodes, but still serve a
    low level purpose. They cannot be represented visually well, so they
    don't have an asset preview. For the same reason we don't have an asset
    shelf for geometry nodes at all, and only display them in the menu.
    Displaying them alongside other essentials assets that have a preview in
    the asset shelf looked odd.

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -1165,8 +1165,26 @@ class NODE_AST_compositor(bpy.types.AssetShelf):
 
     @classmethod
     def asset_poll(cls, asset):
+        import os
+        from pathlib import Path
+
         compositing_type = bpy.types.NodeTree.bl_rna.properties["type"].enum_items["COMPOSITING"]
-        return asset.id_type == 'NODETREE' and asset.metadata.get("type") == compositing_type.value
+        if asset.id_type != 'NODETREE' or asset.metadata.get("type") != compositing_type.value:
+            return False
+
+        ignored_essentials = {
+            "Combine Cylindrical",
+            "Combine Spherical",
+            "Separate Cylindrical",
+            "Separate Spherical",
+        }
+
+        compositor_essentials_path = Path(os.path.join(bpy.utils.system_resource('DATAFILES'), "assets", "nodes", "compositing_nodes_essentials.blend"))
+        if Path(asset.full_library_path) == compositor_essentials_path:
+            if asset.name in ignored_essentials:
+                return False
+
+        return True
 
 
 classes = (
