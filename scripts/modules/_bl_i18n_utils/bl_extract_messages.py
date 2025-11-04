81480c2e37d5a66dab225776084ac6ab01896d70
Gabor Vasarhelyi <vasarhelyi@collmot.com>
2025-11-04 14:34:27 +0100

    fix: skip unregistered classes in addon translation string extractor if their base class is Panel or Menu also, not just Operator

--- scripts/modules/_bl_i18n_utils/bl_extract_messages.py
+++ scripts/modules/_bl_i18n_utils/bl_extract_messages.py
@@ -222,7 +222,19 @@ def dump_rna_messages(msgs, reports, settings, verbose=False):
         blacklist_rna_class |= {cls for cls in bpy.types.Property.__subclasses__()}
 
         return blacklist_rna_class
+    
+    def class_skip_unregistered():
+        skip_unregistered_rna_class = {
+            getattr(bpy.types, cls_id)
+            for cls_id in (
+                "Menu",
+                "Operator",
+                "Panel",
+            )
+        }
 
+        return skip_unregistered_rna_class
+    
     check_ctxt_rna = check_ctxt_rna_tip = None
     check_ctxt = reports["check_ctxt"]
     if check_ctxt:
@@ -345,6 +357,7 @@ def dump_rna_messages(msgs, reports, settings, verbose=False):
                     process_tooldef(tool_context, tools_group)
 
     blacklist_rna_class = class_blacklist()
+    skip_unregistered_rna_class = class_skip_unregistered()
 
     def walk_class(cls):
         bl_rna = cls.bl_rna
@@ -424,11 +437,12 @@ def dump_rna_messages(msgs, reports, settings, verbose=False):
             # Do not process blacklisted classes, but do handle their children.
             if cls in blacklist_rna_class:
                 reports["rna_structs_skipped"].append(cls)
-            elif bpy.types.Operator in cls.__bases__ and not getattr(cls, "is_registered", True):
-                # unregistering a python-defined operator does not remove it from the list of subclasses of
-                # `bpy.types.Operator`, this works around this issue.
-                # While not a huge problem for main UI messages extraction, it does break fairly badly
-                # extraction of specific add-ons UI messages, see #116579.
+            elif any(
+                t in cls.__bases__ for t in skip_unregistered_rna_class
+            ) and not getattr(cls, "is_registered", True):
+                # unregistering a python-defined class does not remove it from the list of subclasses of some
+                # base types, like `bpy.types.Operator`, `bpy.types.Panel` etc. This works around this issue.                # While not a huge problem for main UI messages extraction, it does break fairly badly
+                # extraction of specific add-ons UI messages, see #116579. Also see related #137910.
                 print("SKIPPING because unregistered:", cls)
                 continue
             elif cls in ret_cls_set:
