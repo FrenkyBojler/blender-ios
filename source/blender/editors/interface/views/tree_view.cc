94512f192ad5e394e0031702b83e40f8b27a9c25
Pratik Borhade <pratikborhade302@gmail.com>
2025-11-05 18:33:54 +0530

    Fix: TreeView: Skip undo push for search_string and filter panel
    Disable undo push for these buttons

--- source/blender/editors/interface/views/tree_view.cc
+++ source/blender/editors/interface/views/tree_view.cc
@@ -950,6 +950,7 @@ void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
                                                  ICON_DISCLOSURE_TRI_RIGHT;
     uiBut *but = uiDefIconBut(
         block, ButType::IconToggle, 0, icon, 0, 0, UI_UNIT_X, UI_UNIT_Y * 0.3, nullptr, 0, 0, "");
+    UI_but_flag_disable(but, UI_BUT_UNDO);
     UI_but_func_set(but, set_filtering_collapsed_fn, nullptr, nullptr);
     UI_block_emboss_set(block, ui::EmbossType::Emboss);
     bottom->column(false);
@@ -982,7 +983,7 @@ void TreeViewLayoutBuilder::build_from_tree(AbstractTreeView &tree_view)
                             UI_MAX_NAME_STR,
                             "");
       UI_but_flag_enable(but, UI_BUT_TEXTEDIT_UPDATE | UI_BUT_VALUE_CLEAR);
-      UI_but_flag_enable(but, UI_BUT_UNDO);
+      UI_but_flag_disable(but, UI_BUT_UNDO);
       ui_def_but_icon(but, ICON_VIEWZOOM, UI_HAS_ICON);
     }
   }
