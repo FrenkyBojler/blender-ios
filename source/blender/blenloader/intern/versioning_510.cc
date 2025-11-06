7af3061a418700513c4de64bbc43a165801c0519
Hans Goudey <hans@blender.org>
2025-11-06 14:33:00 -0500

    Versioning to set tool idnames

--- source/blender/blenloader/intern/versioning_510.cc
+++ source/blender/blenloader/intern/versioning_510.cc
@@ -15,8 +15,10 @@
 
 #include "BLI_listbase.h"
 #include "BLI_math_vector.h"
+#include "BLI_string.h"
 #include "BLI_sys_types.h"
 
+#include "BKE_lib_id.hh"
 #include "BKE_main.hh"
 #include "BKE_node.hh"
 #include "BKE_node_legacy_types.hh"
@@ -220,6 +222,37 @@ static void do_version_mix_node_mix_mode_geometry(bNodeTree &node_tree, bNode &n
   }
 }
 
+static void init_node_tool_operator_idnames(Main &bmain)
+{
+  using namespace blender;
+  LISTBASE_FOREACH (bNodeTree *, group, &bmain.nodetrees) {
+    if (group->type != NTREE_GEOMETRY) {
+      continue;
+    }
+    if (!group->geometry_node_asset_traits) {
+      continue;
+    }
+    if (group->geometry_node_asset_traits->node_tool_idname) {
+      continue;
+    }
+    std::string name_str = "geometry.";
+    for (char c : StringRef(BKE_id_name(group->id))) {
+      c = tolower(c);
+      if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_') {
+        name_str.push_back(c);
+      }
+      else {
+        const bool last_is_underscore = name_str[name_str.size() - 1] == '_';
+        if (!last_is_underscore) {
+          name_str.push_back('_');
+        }
+      }
+    }
+    group->geometry_node_asset_traits->node_tool_idname = BLI_strdupn(name_str.c_str(),
+                                                                      name_str.size());
+  }
+}
+
 void do_versions_after_linking_510(FileData * /*fd*/, Main *bmain)
 {
   /* Some blend files were saved with an invalid active viewer key, possibly due to a bug that was
@@ -271,6 +304,10 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       }
     }
     FOREACH_NODETREE_END;
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 5)) {
+    init_node_tool_operator_idnames(*bmain);
   }
 
   /**
