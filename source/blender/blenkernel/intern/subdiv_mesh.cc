35b7877588b1ec2e78c174651f1c1ba49c59e2aa
Hans Goudey <hans@blender.org>
2025-11-05 21:19:27 -0500

    subdiv_mesh_ctx_cache_uv_layers

--- source/blender/blenkernel/intern/subdiv_mesh.cc
+++ source/blender/blenkernel/intern/subdiv_mesh.cc
@@ -126,24 +126,6 @@ static void subdiv_mesh_ctx_cache_uv_layers(SubdivMeshContext *ctx)
     ctx->uv_maps.append(
         attributes.lookup_or_add_for_write_only_span<float2>(name, AttrDomain::Corner));
   }
-
-  /* Copy active & default UV map names. */
-  if (const char *name = CustomData_get_active_layer_name(&coarse_mesh.corner_data,
-                                                          CD_PROP_FLOAT2))
-  {
-    const int i = CustomData_get_named_layer(&subdiv_mesh->corner_data, CD_PROP_FLOAT2, name);
-    if (i >= 0) {
-      CustomData_set_layer_active(&subdiv_mesh->corner_data, CD_PROP_FLOAT2, i);
-    }
-  }
-  if (const char *name = CustomData_get_render_layer_name(&coarse_mesh.corner_data,
-                                                          CD_PROP_FLOAT2))
-  {
-    const int i = CustomData_get_named_layer(&subdiv_mesh->corner_data, CD_PROP_FLOAT2, name);
-    if (i >= 0) {
-      CustomData_set_layer_render(&subdiv_mesh->corner_data, CD_PROP_FLOAT2, i);
-    }
-  }
 }
 
 static void subdiv_mesh_ctx_cache_custom_data_layers(SubdivMeshContext *ctx)
