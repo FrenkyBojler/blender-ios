e4692e0f879d7f91da5d7b349d6dd973f1b06cd2
Sean Kim <SeanCTKim@protonmail.com>
2025-11-05 19:44:38 -0800

    Fix tangent space calc

--- source/blender/blenkernel/intern/multires_inline.hh
+++ source/blender/blenkernel/intern/multires_inline.hh
@@ -37,7 +37,7 @@ BLI_INLINE void BKE_multires_construct_tangent_matrix(blender::float3x3 &tangent
   else {
     BLI_assert_msg(0, "Unhandled corner index");
   }
-  tangent_matrix.z_axis() = blender::math::cross(dPdu, dPdv);
+  tangent_matrix.z_axis() = blender::math::cross(tangent_matrix.x_axis(), tangent_matrix.y_axis());
   float geometric_mean = blender::math::sqrt(blender::math::length(dPdu) * blender::math::length(dPdv));
 
   tangent_matrix.x_axis() = tangent_matrix.x_axis();
