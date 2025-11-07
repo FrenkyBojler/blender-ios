aa849085b1e0f34efac64026c7b9267d46e65e13
Sean Kim <SeanCTKim@protonmail.com>
2025-11-07 15:48:54 -0800

    Fix displacement clearing check

--- source/blender/blenkernel/intern/subdiv_ccg.cc
+++ source/blender/blenkernel/intern/subdiv_ccg.cc
@@ -466,7 +466,7 @@ Mesh *BKE_subdiv_to_ccg_mesh(Object &object,
   SubdivCCGMaskEvaluator mask_evaluator;
   bool has_mask = BKE_subdiv_ccg_mask_init_from_paint(&mask_evaluator, &coarse_mesh);
 
-  bool mdisp_valid = delta != 0;
+  bool mdisp_valid = delta == 0;
   if (!mdisp_valid) {
     CLOG_DEBUG(&LOG, "Removing displacement evaluator");
     displacement_detach(&subdiv);
