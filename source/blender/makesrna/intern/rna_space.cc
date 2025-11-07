ab36ef8ad63979063bd1890bfad845e34981ff3f
Falk David <falk@blender.org>
2025-11-07 18:04:39 +0100

    Update description

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -4863,7 +4863,7 @@ static void rna_def_space_view3d_overlay(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "show_performance", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "overlay.flag", V3D_OVERLAY_PERFORMANCE);
-  RNA_def_property_ui_text(prop, "Show Performance", "Display viewport performance statistics");
+  RNA_def_property_ui_text(prop, "Show Performance", "Display viewport performance timings");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_VIEW3D, nullptr);
 
   /* show camera composition guides */
