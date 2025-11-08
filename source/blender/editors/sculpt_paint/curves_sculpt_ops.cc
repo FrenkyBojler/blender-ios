bc516a1ce1305c14bd691f7b7779cd9a3f8d2825
Viraj Bhartiya <vlbhartiya@gmail.com>
2025-11-08 16:20:05 +0530

    Refactor curves sculpt brush operation: Change brush type declaration to use explicit enum type for improved clarity and type safety.

--- source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
+++ source/blender/editors/sculpt_paint/curves_sculpt_ops.cc
@@ -128,9 +128,9 @@ static std::unique_ptr<CurvesSculptStrokeOperation> start_brush_operation(
   const CurvesSculpt &curves_sculpt = *scene.toolsettings->curves_sculpt;
   const Brush &brush = *BKE_paint_brush_for_read(&curves_sculpt.paint);
   
-  const auto brush_type = (mode == BRUSH_STROKE_SMOOTH) ?
-                             CURVES_SCULPT_BRUSH_TYPE_SMOOTH :
-                             brush.curves_sculpt_brush_type;
+  const eBrushCurvesSculptType brush_type = (mode == BRUSH_STROKE_SMOOTH) ?
+                                                CURVES_SCULPT_BRUSH_TYPE_SMOOTH :
+                                                eBrushCurvesSculptType(brush.curves_sculpt_brush_type);
   
   switch (brush_type) {
     case CURVES_SCULPT_BRUSH_TYPE_COMB:
