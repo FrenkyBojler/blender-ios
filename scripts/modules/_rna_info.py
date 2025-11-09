f677f8f9eb4c7640e6a25dc58d0b994e94bea5a6
Andrej730 <azhilenkov@gmail.com>
2025-11-09 19:09:31 +0500

    Fix: `_rna_info` - separate default values formatting for enum props type description

--- scripts/modules/_rna_info.py
+++ scripts/modules/_rna_info.py
@@ -463,10 +463,17 @@ class InfoPropertyRNA:
                     case "pointer":
                         pass
                     case "enum":
-                        # Empty enums typically only occur for enums which are dynamically generated.
-                        # In that case showing a default isn't helpful.
-                        if self.default_str:
-                            type_str += ", default {:s}".format(literal_fmt.format(self.default_str))
+                        if self.is_enum_flag:
+                            # Can't use `self.default_str`, because need to reformat each item with `literal_fmt`.
+                            default_str = "{{{:s}}}".format(
+                                ", ".join(literal_fmt.format(s) for s in sorted(self.default))
+                            )
+                            type_str += ", default {:s}".format(default_str)
+                        else:
+                            # Empty enums typically only occur for enums which are dynamically generated.
+                            # In that case showing a default isn't helpful.
+                            if self.default:
+                                type_str += ", default {:s}".format(literal_fmt.format(self.default))
                     case _:
                         type_str += ", default {:s}".format(self.default_str)
 
