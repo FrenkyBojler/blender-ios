9b793bc6dc6980045f29f40eb5b45d993b810f88
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-09 21:02:30 +0000

    Changes

--- source/blender/gpu/shaders/infos/gpu_shader_3D_uniform_color_infos.hh
+++ source/blender/gpu/shaders/infos/gpu_shader_3D_uniform_color_infos.hh
@@ -48,5 +48,4 @@ VERTEX_SOURCE("gpu_shader_3D_clipped_uniform_color_vert.glsl")
 FRAGMENT_SOURCE("gpu_shader_uniform_color_frag.glsl")
 ADDITIONAL_INFO(gpu_srgb_to_framebuffer_space)
 DO_STATIC_COMPILATION()
-
 GPU_SHADER_CREATE_END()
