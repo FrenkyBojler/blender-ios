7d4c20428a4694ab555d89a8091e26ac6106322c
Hans Goudey <hans@blender.org>
2025-11-10 15:47:49 -0500

    Remove unnecessary change

--- source/blender/windowmanager/intern/wm_operator_type.cc
+++ source/blender/windowmanager/intern/wm_operator_type.cc
@@ -42,8 +42,6 @@
 
 #define UNDOCUMENTED_OPERATOR_TIP N_("(undocumented operator)")
 
-using namespace blender;
-
 static void wm_operatortype_free_macro(wmOperatorType *ot);
 
 /* -------------------------------------------------------------------- */
