14fab387d70e7d8097da940dcf2c4ef22594fb62
Alaska <Alaskayou01@gmail.com>
2025-11-11 02:56:50 +1300

    Implement basis for a LLM to judge whether or not a version number is correct

--- release/release_notes/bug_fixes_per_major_release.py
+++ release/release_notes/bug_fixes_per_major_release.py
@@ -635,9 +635,30 @@ def llm_says_version_is_incorrect(
     if client is None:
         return False
 
-    # TODO: Actually run LLM here
+    user_prompt = f"""Using the information below, is Blender version {version_number} a {'Broken' if should_be_broken else 'Working'} version of Blender? Your output should be in a single word 'yes' or 'no'.
 
-    return False
+    Information:
+    '''
+    Blender versions:
+    {combined_lines}
+    '''
+    """
+
+    # TODO: Fix mypy complaining about a list of dicts being incorrect
+    messages = [{"role": "user", "content": user_prompt}]
+    for i in range(3):
+        # Run the LLM over the reqeust 3 times in case the LLM makes a mistake in one of it's runs.
+        # Use a fixed seed for reproducability.
+        response = client.chat.completions.create(model=llm_name, seed=i, messages=messages)
+        llm_choice = response.choices[0].message.content
+        if llm_choice is not None:
+            # Response can be None according to mypy.
+            llm_choice = llm_choice.strip().lower()
+            if llm_choice == "no":
+                # The LLM disagreed with the previous statement.
+                return False
+
+    return True
 
 
 # ---
