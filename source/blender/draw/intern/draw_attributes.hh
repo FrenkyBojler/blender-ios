blender/blender

commit 9a672cbe52638240e30a0f78d3618b00cc093502

Mesh: Reference UV map and tangents by name in draw requests
Currently UV maps and tangents are referenced by custom data layer index
by the mesh extraction system that decides what attributes are needed on
the GPU for rendering. This system is closely tied to CustomData, and
adds a limit on 8 total UV maps on the mesh (crucially different than
a limit on the supported UV map count for rendering).

The process of detecting which attributes to upload is also directly
tied to CustomData. In particular, `mesh_cd_calc_used_gpu_layers` is
quite complicated.

This commit reimplements that function, and references used UV maps
and UV tangents by name rather than by index. The process is now mostly
separated from `CustomData`, which is important as Mesh moves to
`AttributeStorage` instead.

Overall I noticed this introduces some overhead (a few percent cost to
playback in some extreme cases). `DRW_MeshCDMask` is bigger than before
(it was just an integer), and the attribute API currently has some
overhead that can be removed when it's backed by `AttributeStorage`.
The change still feels worth it to me, given other opportunities for
optimization in this area.

Ref #122398

Pull Request: https://projects.blender.org/blender/blender/pulls/141467

source/blender/draw/intern/draw_attributes.hh
@@ -12,9 +12,8 @@
 
 #include <string>
 
-#include "DNA_customdata_types.h"
-
 #include "BLI_string_ref.hh"
+#include "BLI_struct_equality_utils.hh"
 #include "BLI_sys_types.h"
 
 #include "BLI_vector_set.hh"
@@ -26,21 +25,24 @@ enum class AttrDomain : int8_t;
 namespace blender::draw {
 
 struct DRW_MeshCDMask {
-  uint32_t uv : 8;
-  uint32_t tan : 8;
-  uint32_t orco : 1;
-  uint32_t tan_orco : 1;
-  uint32_t sculpt_overlays : 1;
-  /**
-   * Edit uv layer is from the base edit mesh as modifiers could remove it. (see #68857)
-   */
-  uint32_t edit_uv : 1;
+  VectorSet<std::string> uv;
+  VectorSet<std::string> tan;
+  bool orco = false;
+  bool tan_orco = false;
+  bool sculpt_overlays = false;
+  bool edit_uv = false;
+  BLI_STRUCT_EQUALITY_OPERATORS_6(
+      DRW_MeshCDMask, uv.as_span(), uv.as_span(), orco, tan_orco, sculpt_overlays, edit_uv);
+  void clear()
+  {
+    uv.clear_and_keep_capacity();
+    tan.clear_and_keep_capacity();
+    orco = false;
+    tan_orco = false;
+    sculpt_overlays = false;
+    edit_uv = false;
+  }
 };
-
-/* Keep `DRW_MeshCDMask` struct within a `uint32_t`.
- * bit-wise and atomic operations are used to compare and update the struct.
- * See `mesh_cd_layers_type_*` functions. */
-static_assert(sizeof(DRW_MeshCDMask) <= sizeof(uint32_t), "DRW_MeshCDMask exceeds 32 bits");
 
 void drw_attributes_merge(VectorSet<std::string> *dst, const VectorSet<std::string> *src);
 
@@ -48,10 +50,5 @@ void drw_attributes_merge(VectorSet<std::string> *dst, const VectorSet<std::stri
 bool drw_attributes_overlap(const VectorSet<std::string> *a, const VectorSet<std::string> *b);
 
 void drw_attributes_add_request(VectorSet<std::string> *attrs, StringRef name);
-
-bool drw_custom_data_match_attribute(const CustomData &custom_data,
-                                     StringRef name,
-                                     int *r_layer_index,
-                                     eCustomDataType *r_type);
 
 }  // namespace blender::draw

source/blender/draw/intern/draw_attributes.cc
@@ -2,8 +2,6 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BKE_customdata.hh"
-
 #include "GPU_shader.hh"
 
 #include "draw_attributes.hh"
@@ -32,40 +30,6 @@ void drw_attributes_add_request(VectorSet<std::string> *attrs, const StringRef n
     return;
   }
   attrs->add_as(name);
-}
-
-bool drw_custom_data_match_attribute(const CustomData &custom_data,
-                                     const StringRef name,
-                                     int *r_layer_index,
-                                     eCustomDataType *r_type)
-{
-  const eCustomDataType possible_attribute_types[11] = {
-      CD_PROP_BOOL,
-      CD_PROP_INT8,
-      CD_PROP_INT16_2D,
-      CD_PROP_INT32_2D,
-      CD_PROP_INT32,
-      CD_PROP_FLOAT,
-      CD_PROP_FLOAT2,
-      CD_PROP_FLOAT3,
-      CD_PROP_COLOR,
-      CD_PROP_QUATERNION,
-      CD_PROP_BYTE_COLOR,
-  };
-
-  for (int i = 0; i < ARRAY_SIZE(possible_attribute_types); i++) {
-    const eCustomDataType attr_type = possible_attribute_types[i];
-    int layer_index = CustomData_get_named_layer(&custom_data, attr_type, name);
-    if (layer_index == -1) {
-      continue;
-    }
-
-    *r_layer_index = layer_index;
-    *r_type = attr_type;
-    return true;
-  }
-
-  return false;
 }
 
 }  // namespace blender::draw

source/blender/draw/intern/draw_sculpt.cc
@@ -218,15 +218,8 @@ Vector<SculptBatch> sculpt_batches_per_material_get(const Object *ob,
     attrs.append(pbvh::GenericRequest(name));
   }
 
-  /* UV maps are not in attribute requests. */
-  for (uint i = 0; i < 32; i++) {
-    if (cd_needed.uv & (1 << i)) {
-      int layer_i = CustomData_get_layer_index_n(&mesh.corner_data, CD_PROP_FLOAT2, i);
-      CustomDataLayer *layer = layer_i != -1 ? mesh.corner_data.layers + layer_i : nullptr;
-      if (layer) {
-        attrs.append(pbvh::GenericRequest(layer->name));
-      }
-    }
+  for (const StringRef name : cd_needed.uv) {
+    attrs.append(pbvh::GenericRequest(name));
   }
 
   return sculpt_batches_get_ex(ob, false, attrs);

source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_edituv_stretch_angle.cc
@@ -248,18 +248,18 @@ gpu::VertBufPtr extract_edituv_stretch_angle_subdiv(const MeshRenderData &mr,
   const CustomData *cd_ldata = (mr.extract_type == MeshExtractType::Mesh) ? &mr.mesh->corner_data :
                                                                             &mr.bm->ldata;
 
-  uint32_t uv_layers = cache.cd_used.uv;
+  VectorSet<std::string> uv_layers = cache.cd_used.uv;
   /* HACK to fix #68857 */
   if (mr.extract_type == MeshExtractType::BMesh && cache.cd_used.edit_uv == 1) {
-    int layer = CustomData_get_active_layer(cd_ldata, CD_PROP_FLOAT2);
-    if (layer != -1 && !CustomData_layer_is_anonymous(cd_ldata, CD_PROP_FLOAT2, layer)) {
-      uv_layers |= (1 << layer);
+    if (const char *active_name = CustomData_get_active_layer_name(cd_ldata, CD_PROP_FLOAT2)) {
+      uv_layers.add_as(active_name);
     }
   }
 
   int uvs_offset = 0;
   for (int i = 0; i < MAX_MTFACE; i++) {
-    if (uv_layers & (1 << i)) {
+    const char *name = CustomData_get_layer_name(cd_ldata, CD_PROP_FLOAT2, i);
+    if (uv_layers.contains_as(name)) {
       if (i == CustomData_get_active_layer(cd_ldata, CD_PROP_FLOAT2)) {
         break;
       }

source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_tan.cc
@@ -32,15 +32,16 @@ static Array<Array<float4>> extract_tan_init_common(const MeshRenderData &mr,
 
   const CustomData *cd_ldata = (mr.extract_type == MeshExtractType::BMesh) ? &mr.bm->ldata :
                                                                              &mr.mesh->corner_data;
-  uint32_t tan_layers = cache.cd_used.tan;
-  bool use_orco_tan = cache.cd_used.tan_orco != 0;
+  VectorSet<std::string> tan_layers = cache.cd_used.tan;
+  bool use_orco_tan = cache.cd_used.tan_orco;
+
+  const StringRef active_name = CustomData_get_active_layer_name(cd_ldata, CD_PROP_FLOAT2);
+  const StringRef default_name = CustomData_get_render_layer_name(cd_ldata, CD_PROP_FLOAT2);
 
   /* FIXME(#91838): This is to avoid a crash when orco tangent was requested but there are valid
    * uv layers. It would be better to fix the root cause. */
-  if (tan_layers == 0 && use_orco_tan &&
-      CustomData_get_layer_index(cd_ldata, CD_PROP_FLOAT2) != -1)
-  {
-    tan_layers = 1;
+  if (tan_layers.is_empty() && use_orco_tan && !default_name.is_empty()) {
+    tan_layers.add(default_name);
     use_orco_tan = false;
   }
 
@@ -88,24 +89,23 @@ static Array<Array<float4>> extract_tan_init_common(const MeshRenderData &mr,
   }
 
   Vector<StringRef> uv_names;
-  for (int i = 0; i < MAX_MTFACE; i++) {
-    if (tan_layers & (1 << i)) {
+  for (const StringRef name : tan_layers.as_span().take_front(MAX_MTFACE)) {
+    if (tan_layers.contains(name)) {
       char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
-      const char *layer_name = CustomData_get_layer_name(cd_ldata, CD_PROP_FLOAT2, i);
-      GPU_vertformat_safe_attr_name(layer_name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
+      GPU_vertformat_safe_attr_name(name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
       /* Tangent layer name. */
       SNPRINTF(attr_name, "t%s", attr_safe_name);
       GPU_vertformat_attr_add(format, attr_name, gpu_attr_type);
       /* Active render layer name. */
-      if (i == CustomData_get_render_layer(cd_ldata, CD_PROP_FLOAT2)) {
+      if (name == default_name) {
         GPU_vertformat_alias_add(format, "t");
       }
       /* Active display layer name. */
-      if (i == CustomData_get_active_layer(cd_ldata, CD_PROP_FLOAT2)) {
+      if (name == active_name) {
         GPU_vertformat_alias_add(format, "at");
       }
 
-      uv_names.append(layer_name);
+      uv_names.append(name);
     }
   }
 

source/blender/draw/intern/mesh_extractors/extract_mesh_vbo_uv.cc
@@ -10,6 +10,7 @@
 #include "BLI_math_vector_types.hh"
 #include "BLI_string_utf8.h"
 
+#include "BKE_anonymous_attribute_id.hh"
 #include "BKE_attribute.hh"
 
 #include "draw_subdivision.hh"
@@ -19,64 +20,59 @@ namespace blender::draw {
 
 /* Initialize the vertex format to be used for UVs. Return true if any UV layer is
  * found, false otherwise. */
-static bool mesh_extract_uv_format_init(GPUVertFormat *format,
-                                        const MeshBatchCache &cache,
-                                        const CustomData *cd_ldata,
-                                        const MeshExtractType extract_type,
-                                        uint32_t &r_uv_layers)
+static VectorSet<StringRef> mesh_extract_uv_format_init(GPUVertFormat *format,
+                                                        const MeshBatchCache &cache,
+                                                        const CustomData *cd_ldata,
+                                                        const MeshExtractType extract_type)
 {
   GPU_vertformat_deinterleave(format);
 
-  uint32_t uv_layers = cache.cd_used.uv;
+  VectorSet<StringRef> uv_layers;
+  for (const StringRef name : cache.cd_used.uv.as_span().take_front(MAX_MTFACE)) {
+    uv_layers.add_new(name);
+  }
+
+  const StringRef active_name = CustomData_get_active_layer_name(cd_ldata, CD_PROP_FLOAT2);
+  const StringRef default_name = CustomData_get_render_layer_name(cd_ldata, CD_PROP_FLOAT2);
+
   /* HACK to fix #68857 */
   if (extract_type == MeshExtractType::BMesh && cache.cd_used.edit_uv == 1) {
-    int layer = CustomData_get_active_layer(cd_ldata, CD_PROP_FLOAT2);
-    if (layer != -1 && !CustomData_layer_is_anonymous(cd_ldata, CD_PROP_FLOAT2, layer)) {
-      uv_layers |= (1 << layer);
+    if (!bke::attribute_name_is_anonymous(default_name)) {
+      uv_layers.add(default_name);
     }
   }
 
-  r_uv_layers = 0;
+  const StringRef stencil_name = [&]() -> StringRef {
+    const int stencil_index = CustomData_get_stencil_layer_index(cd_ldata, CD_PROP_FLOAT2);
+    if (stencil_index == -1) {
+      return "";
+    }
+    return cd_ldata->layers[stencil_index].name;
+  }();
 
-  for (int i = 0; i < MAX_MTFACE; i++) {
-    if (uv_layers & (1 << i)) {
-      char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
-      const char *layer_name = CustomData_get_layer_name(cd_ldata, CD_PROP_FLOAT2, i);
-
-      /* not all UV layers are guaranteed to exist, since the list of available UV
-       * layers is generated for the evaluated mesh, which is needed to show modifier
-       * results in editmode, but the actual mesh might be the base mesh.
-       */
-      if (layer_name) {
-        r_uv_layers |= (1 << i);
-        GPU_vertformat_safe_attr_name(layer_name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
-        /* UV layer name. */
-        SNPRINTF_UTF8(attr_name, "a%s", attr_safe_name);
-        GPU_vertformat_attr_add(format, attr_name, blender::gpu::VertAttrType::SFLOAT_32_32);
-        /* Active render layer name. */
-        if (i == CustomData_get_render_layer(cd_ldata, CD_PROP_FLOAT2)) {
-          GPU_vertformat_alias_add(format, "a");
-        }
-        /* Active display layer name. */
-        if (i == CustomData_get_active_layer(cd_ldata, CD_PROP_FLOAT2)) {
-          GPU_vertformat_alias_add(format, "au");
-          /* Alias to `pos` for edit uvs. */
-          GPU_vertformat_alias_add(format, "pos");
-        }
-        /* Stencil mask uv layer name. */
-        if (i == CustomData_get_stencil_layer(cd_ldata, CD_PROP_FLOAT2)) {
-          GPU_vertformat_alias_add(format, "mu");
-        }
-      }
+  for (const StringRef name : uv_layers) {
+    char attr_name[32], attr_safe_name[GPU_MAX_SAFE_ATTR_NAME];
+    GPU_vertformat_safe_attr_name(name, attr_safe_name, GPU_MAX_SAFE_ATTR_NAME);
+    SNPRINTF_UTF8(attr_name, "a%s", attr_safe_name);
+    GPU_vertformat_attr_add(format, attr_name, blender::gpu::VertAttrType::SFLOAT_32_32);
+    if (name == default_name) {
+      GPU_vertformat_alias_add(format, "a");
+    }
+    if (name == active_name) {
+      GPU_vertformat_alias_add(format, "au");
+      /* Alias to `pos` for edit uvs. */
+      GPU_vertformat_alias_add(format, "pos");
+    }
+    if (name == stencil_name) {
+      GPU_vertformat_alias_add(format, "mu");
     }
   }
 
   if (format->attr_len == 0) {
-    GPU_vertformat_attr_add(format, "dummy", blender::gpu::VertAttrType::SFLOAT_32);
-    return false;
+    GPU_vertformat_attr_add(format, "dummy", blender::gpu::VertAttrType::SFLOAT_32_32);
   }
 
-  return true;
+  return uv_layers;
 }
 
 gpu::VertBufPtr extract_uv_maps(const MeshRenderData &mr, const MeshBatchCache &cache)
@@ -86,8 +82,9 @@ gpu::VertBufPtr extract_uv_maps(const MeshRenderData &mr, const MeshBatchCache &
   const CustomData *cd_ldata = (mr.extract_type == MeshExtractType::BMesh) ? &mr.bm->ldata :
                                                                              &mr.mesh->corner_data;
   int v_len = mr.corners_num;
-  uint32_t uv_layers = cache.cd_used.uv;
-  if (!mesh_extract_uv_format_init(&format, cache, cd_ldata, mr.extract_type, uv_layers)) {
+  const VectorSet<StringRef> uv_layers = mesh_extract_uv_format_init(
+      &format, cache, cd_ldata, mr.extract_type);
+  if (uv_layers.is_empty()) {
     /* VBO will not be used, only allocate minimum of memory. */
     v_len = 1;
   }
@@ -95,20 +92,13 @@ gpu::VertBufPtr extract_uv_maps(const MeshRenderData &mr, const MeshBatchCache &
   gpu::VertBufPtr vbo = gpu::VertBufPtr(GPU_vertbuf_create_with_format(format));
   GPU_vertbuf_data_alloc(*vbo, v_len);
 
-  Vector<int> uv_indices;
-  for (const int i : IndexRange(MAX_MTFACE)) {
-    if (uv_layers & (1 << i)) {
-      uv_indices.append(i);
-    }
-  }
-
   MutableSpan<float2> uv_data = vbo->data<float2>();
   threading::memory_bandwidth_bound_task(uv_data.size_in_bytes() * 2, [&]() {
     if (mr.extract_type == MeshExtractType::BMesh) {
       const BMesh &bm = *mr.bm;
-      for (const int i : uv_indices.index_range()) {
+      for (const int i : uv_layers.index_range()) {
         MutableSpan<float2> data = uv_data.slice(i * bm.totloop, bm.totloop);
-        const int offset = CustomData_get_n_offset(cd_ldata, CD_PROP_FLOAT2, uv_indices[i]);
+        const int offset = CustomData_get_offset_named(cd_ldata, CD_PROP_FLOAT2, uv_layers[i]);
         threading::parallel_for(IndexRange(bm.totface), 2048, [&](const IndexRange range) {
           for (const int face_index : range) {
             const BMFace &face = *BM_face_at_index(&const_cast<BMesh &>(bm), face_index);
@@ -124,15 +114,26 @@ gpu::VertBufPtr extract_uv_maps(const MeshRenderData &mr, const MeshBatchCache &
     }
     else {
       const bke::AttributeAccessor attributes = mr.mesh->attributes();
-      for (const int i : uv_indices.index_range()) {
-        const StringRef name = CustomData_get_layer_name(cd_ldata, CD_PROP_FLOAT2, uv_indices[i]);
+      for (const int i : uv_layers.index_range()) {
         const VArray uv_map = *attributes.lookup_or_default<float2>(
-            name, bke::AttrDomain::Corner, float2(0));
+            uv_layers[i], bke::AttrDomain::Corner, float2(0));
         array_utils::copy(uv_map, uv_data.slice(i * mr.corners_num, mr.corners_num));
       }
     }
   });
   return vbo;
+}
+
+static VectorSet<StringRef> all_uv_map_attributes(const Mesh &mesh)
+{
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  VectorSet<StringRef> result;
+  attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
+    if (iter.domain == bke::AttrDomain::Corner && iter.data_type == bke::AttrType::Float2) {
+      result.add(iter.name);
+    }
+  });
+  return result;
 }
 
 gpu::VertBufPtr extract_uv_maps_subdiv(const DRWSubdivCache &subdiv_cache,
@@ -141,28 +142,29 @@ gpu::VertBufPtr extract_uv_maps_subdiv(const DRWSubdivCache &subdiv_cache,
   const Mesh *coarse_mesh = subdiv_cache.mesh;
   GPUVertFormat format = {0};
 
+  const VectorSet<StringRef> uv_layers = mesh_extract_uv_format_init(
+      &format, cache, &coarse_mesh->corner_data, MeshExtractType::Mesh);
+
   uint v_len = subdiv_cache.num_subdiv_loops;
-  uint uv_layers;
-  if (!mesh_extract_uv_format_init(
-          &format, cache, &coarse_mesh->corner_data, MeshExtractType::Mesh, uv_layers))
-  {
+  if (uv_layers.is_empty()) {
     /* TODO(kevindietrich): handle this more gracefully. */
     v_len = 1;
   }
 
   gpu::VertBufPtr vbo = gpu::VertBufPtr(GPU_vertbuf_create_on_device(format, v_len));
 
-  if (uv_layers == 0) {
+  if (uv_layers.is_empty()) {
     return vbo;
   }
 
+  const VectorSet<StringRef> all_uv_maps = all_uv_map_attributes(*coarse_mesh);
+
   /* Index of the UV layer in the compact buffer. Used UV layers are stored in a single buffer. */
-  int pack_layer_index = 0;
-  for (int i = 0; i < MAX_MTFACE; i++) {
-    if (uv_layers & (1 << i)) {
-      const int offset = int(subdiv_cache.num_subdiv_loops) * pack_layer_index++;
-      draw_subdiv_extract_uvs(subdiv_cache, vbo.get(), i, offset);
-    }
+  for (const int pack_layer_index : uv_layers.index_range()) {
+    const StringRef name = uv_layers[pack_layer_index];
+    const int i = all_uv_maps.index_of(name);
+    const int offset = int(subdiv_cache.num_subdiv_loops) * pack_layer_index;
+    draw_subdiv_extract_uvs(subdiv_cache, vbo.get(), i, offset);
   }
   return vbo;
 }

FrenkyBojler/blender-ios

commit 1567303bd6e551fbb9dd05ffaf5c121a3db254bd

Fix #149494: Performance regression with "orco tangents" and EEVEE
Caused by 9a672cbe52638240e30a0f78d3618b00cc093502

There is specific code to invalidate tangents and UV map vertex buffers
because we pack multiple attributes into the same vertex buffer, and we
don't necessarily want to rebuild the VBOs whenever the set of requested
UV maps/tangents changes at all. "Orco tangents" or non-UV-map tangents
are also packed into the same buffer. When you switch to EEVEE and then
back to workbench, orco tangents are requested but then not needed
anymore, causing `cd_used` and `cd_needed` not to match anymore.

Currently this causes the tangents to be rebuilt and re-uploaded on
on every redraw, but previously the code would skip this invalidation
because it skipped invalidating the VBOs when `cd_used` contained
something that `cd_needed` didn't.

This sounds very complicated (and it is, the system shouldn't be so
weird), but ultimately it's just a small logic error in the line that
used to read `if (cd_overlap == false || attr_overlap == false) {`.

Also fix an issue in the `DRW_MeshCDMask` equality operator,
though I don't know of that causing visible issues so far.

source/blender/draw/intern/draw_attributes.hh
@@ -32,7 +32,7 @@ struct DRW_MeshCDMask {
   bool sculpt_overlays = false;
   bool edit_uv = false;
   BLI_STRUCT_EQUALITY_OPERATORS_6(
-      DRW_MeshCDMask, uv.as_span(), uv.as_span(), orco, tan_orco, sculpt_overlays, edit_uv);
+      DRW_MeshCDMask, uv.as_span(), tan.as_span(), orco, tan_orco, sculpt_overlays, edit_uv);
   void clear()
   {
     uv.clear_and_keep_capacity();

source/blender/draw/intern/draw_cache_impl_mesh.cc
@@ -1112,31 +1112,30 @@ void DRW_mesh_batch_cache_create_requested(TaskGraph &task_graph,
       }
     }
 
-    /* Verify that all surface batches have needed attribute layers.
-     */
+    /* Verify that all surface batches have needed attribute layers, i.e. that none of the shaders
+     * need vertex buffers that aren't currently cached. */
     /* TODO(fclem): We could be a bit smarter here and only do it per
      * material. */
     const bool uvs_overlap = drw_attributes_overlap(&cache.cd_used.uv, &cache.cd_needed.uv);
     const bool tan_overlap = drw_attributes_overlap(&cache.cd_used.tan, &cache.cd_needed.tan);
     const bool attr_overlap = drw_attributes_overlap(&cache.attr_used, &cache.attr_needed);
-    const bool orco_overlap = cache.cd_used.orco == cache.cd_needed.orco;
-    const bool tan_orco_overlap = cache.cd_used.tan_orco == cache.cd_needed.tan_orco;
-    const bool sculpt_overlap = cache.cd_used.sculpt_overlays == cache.cd_needed.sculpt_overlays;
-    if (!uvs_overlap || !tan_overlap || !attr_overlap || !orco_overlap || !tan_orco_overlap ||
-        !sculpt_overlap)
+    if (!uvs_overlap || !tan_overlap || !attr_overlap ||
+        (cache.cd_needed.orco && !cache.cd_used.orco) ||
+        (cache.cd_needed.tan_orco && !cache.cd_used.tan_orco) ||
+        (cache.cd_needed.sculpt_overlays && !cache.cd_used.sculpt_overlays))
     {
       FOREACH_MESH_BUFFER_CACHE (cache, mbc) {
         if (!uvs_overlap) {
           mbc->buff.vbos.remove(VBOType::UVs);
           cd_uv_update = true;
         }
-        if (!tan_overlap || !tan_orco_overlap) {
+        if (!tan_overlap || cache.cd_used.tan_orco != cache.cd_needed.tan_orco) {
           mbc->buff.vbos.remove(VBOType::Tangents);
         }
-        if (!orco_overlap) {
+        if (cache.cd_used.orco != cache.cd_needed.orco) {
           mbc->buff.vbos.remove(VBOType::Orco);
         }
-        if (!sculpt_overlap) {
+        if (cache.cd_used.sculpt_overlays != cache.cd_needed.sculpt_overlays) {
           mbc->buff.vbos.remove(VBOType::SculptData);
         }
         if (!attr_overlap) {