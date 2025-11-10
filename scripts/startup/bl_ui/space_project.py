2096ccf64980b7bb981336da1db82abdb224ba5e
Nathan Vegdahl <cessen@cessen.com>
2025-11-10 08:42:21 +0000

    Remove addon

--- scripts/startup/bl_ui/space_project.py
+++ scripts/startup/bl_ui/space_project.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from pathlib import Path
+import tomllib
 
 import bpy
 from bpy.types import Header, Menu, Panel
@@ -160,3 +161,79 @@ classes = (
     PROJECT_OP_NewProject,
     PROJECT_OP_WriteProject
 )
+
+# --------------------------------------------------------------
+# EVERYTHING BELOW SHOULD PROBABLY GO SOMEWHERE ELSE...?
+
+PROJECT_DIR = ".blender_project"
+PROJECT_CONFIG = "project.toml"
+
+
+def find_project_root_from_blend_file_path(blend_path: Path) -> Path | None:
+    for parent in blend_path.parents:
+        if parent.joinpath(PROJECT_DIR).is_dir():
+            return parent
+    return None
+
+
+def read_project_config(root_path: Path) -> dict | None:
+    name = "My Project"
+
+    config_path = root_path.joinpath(PROJECT_DIR, PROJECT_CONFIG)
+    try:
+        with open(config_path, "rb") as f:
+            return tomllib.load(f)
+    except FileNotFoundError:
+        return None
+
+
+@bpy.app.handlers.persistent
+def on_blend_load(blend_path: str) -> None:
+    bpy.context.project.clear()
+
+    if blend_path == "":
+        # Not an on-disk blend file.
+        return
+
+    root_path = find_project_root_from_blend_file_path(Path(blend_path))
+    if root_path is None:
+        return
+
+    config = read_project_config(root_path)
+    if config is None:
+        print("Invalid project: no 'project.toml' found.")
+        return
+
+    if "name" not in config:
+        print("Invalid project: no project name defined in 'project.toml'.")
+        return
+
+    if type(config["name"]) != str:
+        print("Invalid project: project name is not a string.")
+        return
+
+    if config["name"] == "":
+        print("Invalid project: project name is empty.")
+        return
+
+    bpy.context.project.init(config["name"], str(root_path))
+
+
+@bpy.app.handlers.persistent
+def on_blend_save(blend_path: str) -> None:
+    # This is needed due to cases like a fresh new blend file being saved for
+    # the first time in a project.
+    on_blend_load(blend_path)
+
+
+####################
+# REGISTER
+
+def register():
+    bpy.app.handlers.load_pre.append(on_blend_load)
+    bpy.app.handlers.save_post.append(on_blend_save)
+
+
+def unregister():
+    bpy.app.handlers.load_pre.remove(on_blend_load)
+    bpy.app.handlers.save_post.append(on_blend_save)
