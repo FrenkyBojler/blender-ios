22585d0c23886b2f501e282a4daaf9d03092cc2c
Alaska <Alaskayou01@gmail.com>
2025-11-11 04:10:40 +1300

    Remove unncessary detail from user prompt when it's not needed

--- release/release_notes/bug_fixes_per_major_release.py
+++ release/release_notes/bug_fixes_per_major_release.py
@@ -640,16 +640,20 @@ def llm_says_version_is_incorrect(
 
     user_prompt = [
         f"Using the information below, is Blender version {version_number} a {'Broken' if should_be_broken else 'Working'} version of Blender?",
-        "**Important rules:**"
-        "- You should ignore patch version numbers. For example 3.2 and 3.2.7 are considered the same in this situation because we're ignoring the patch number (.7).",
-        "- If a working field says something similar to 'Worked: N/A as it's a new feature in 3.2', then you should consider 3.2 a working version of Blender in this situation.",
-        "- Your output should be in a single word 'yes' or 'no'.",
-        "",
-        "Information:",
-        "```",
-        "Blender versions:",
-        f"{version_info_from_report}",
-        "```"]
+        "**Important rules:**",
+        "- You should ignore patch version numbers. For example 3.2 and 3.2.7 are considered the same in this situation because we're ignoring the patch number (.7)."]
+
+    if not should_be_broken:
+        user_prompt.append(
+            "- If a working field says something similar to 'Worked: N/A as it's a new feature in 3.2', then you should consider 3.2 a working version of Blender in this situation.")
+
+    user_prompt.extend(["- Your output should be in a single word 'yes' or 'no'.",
+                        "",
+                        "Information:",
+                        "```",
+                        "Blender versions:",
+                        f"{version_info_from_report}",
+                        "```"])
 
     # TODO: Fix mypy complaining about a list of dicts being incorrect
     # TODO: Implement support for models that don't support reasoning
