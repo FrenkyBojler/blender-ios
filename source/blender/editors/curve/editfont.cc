c15faf04dfff379ffb7f71854c4cf5d212f2fda5
Jesse Yurkovich <jesse.y@gmail.com>
2025-11-13 14:45:29 -0800

    Cleanup: Remove depsgraph tags of 0 within editfont.cc

--- source/blender/editors/curve/editfont.cc
+++ source/blender/editors/curve/editfont.cc
@@ -413,7 +413,7 @@ static void text_update_edited(bContext *C, Object *obedit, const eEditFontMode 
   /* Run update first since it can move the cursor. */
   if (mode == FO_EDIT) {
     /* Re-tessellate. */
-    DEG_id_tag_update(static_cast<ID *>(obedit->data), 0);
+    DEG_id_tag_update(static_cast<ID *>(obedit->data), ID_RECALC_GEOMETRY);
   }
   else {
     /* Depsgraph runs above, but since we're not tagging for update, call directly. */
@@ -970,7 +970,7 @@ static wmOperatorStatus set_style(bContext *C, const int style, const bool clear
     }
   }
 
-  DEG_id_tag_update(static_cast<ID *>(obedit->data), 0);
+  DEG_id_tag_update(static_cast<ID *>(obedit->data), ID_RECALC_GEOMETRY);
   WM_event_add_notifier(C, NC_GEOM | ND_DATA, obedit->data);
 
   return OPERATOR_FINISHED;
@@ -2160,7 +2160,7 @@ static wmOperatorStatus textbox_add_exec(bContext *C, wmOperator * /*op*/)
     cu->totbox++;
   }
 
-  DEG_id_tag_update(static_cast<ID *>(obedit->data), 0);
+  DEG_id_tag_update(static_cast<ID *>(obedit->data), ID_RECALC_GEOMETRY_ALL_MODES);
   WM_event_add_notifier(C, NC_GEOM | ND_DATA, obedit->data);
   return OPERATOR_FINISHED;
 }
@@ -2203,7 +2203,7 @@ static wmOperatorStatus textbox_remove_exec(bContext *C, wmOperator *op)
     }
   }
 
-  DEG_id_tag_update(static_cast<ID *>(obedit->data), 0);
+  DEG_id_tag_update(static_cast<ID *>(obedit->data), ID_RECALC_GEOMETRY_ALL_MODES);
   WM_event_add_notifier(C, NC_GEOM | ND_DATA, obedit->data);
 
   return OPERATOR_FINISHED;
