ffe3c82320775b008c30168dbe1343861bf42f20
Clément Foucault <foucault.clem@gmail.com>
2025-11-14 19:05:31 +0100

    Fix wrong flag used for checking colored transparency

--- source/blender/draw/engines/eevee/eevee_pipeline.cc
+++ source/blender/draw/engines/eevee/eevee_pipeline.cc
@@ -442,7 +442,7 @@ PassMain::Sub *ForwardPipeline::material_transparent_add(const Object *ob,
     state |= DRW_STATE_CULL_BACK;
   }
   has_colored_transparency_ |= GPU_material_flag_get(gpumat,
-                                                     GPU_MATFLAG_REFLECTION_MAYBE_COLORED) != 0;
+                                                     GPU_MATFLAG_TRANSPARENT_MAYBE_COLORED) != 0;
   has_holdout_ |= GPU_material_flag_get(gpumat, GPU_MATFLAG_HOLDOUT) != 0;
   has_transparent_ = true;
   float sorting_value = math::dot(float3(ob->object_to_world().location()), camera_forward_);
