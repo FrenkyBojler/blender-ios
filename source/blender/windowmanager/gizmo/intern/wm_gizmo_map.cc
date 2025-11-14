63c90f91d6fbbf29668c2fbff9ca361a29732010
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-14 20:50:38 +0000

    Use std::max

--- source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
+++ source/blender/windowmanager/gizmo/intern/wm_gizmo_map.cc
@@ -646,7 +646,7 @@ static int gizmo_find_intersected_3d_intern(wmGizmo **visible_gizmos,
       if ((gz->flag & WM_GIZMO_DRAW_NO_SCALE) == 0) {
         /* Use the larger of the gizmo's final scale and a small minimum threshold to prevent the
          * selection bias from collapsing when the gizmo becomes extremely small. See #100321. */
-        const float scale = max_ff(gz->scale_final, 1e-2f);
+        const float scale = std::max(gz->scale_final, 1e-2f);
         select_bias *= scale;
       }
       sub_v3_v3(co_3d, co_3d_origin);
