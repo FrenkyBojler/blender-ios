349e0db3410242d79c047de5f9523a6f20b50d5b
Guillermo Venegas <guillermovcra@gmail.com>
2025-11-15 10:17:25 -0600

    Set explicit default value, fix comment

--- source/blender/windowmanager/WM_api.hh
+++ source/blender/windowmanager/WM_api.hh
@@ -898,13 +898,14 @@ wmOperatorStatus WM_operator_confirm_or_exec(bContext *C, wmOperator *op, const 
 /**
  * Like WM_operator_confirm, but with more options and can't be used as an invoke directly.
  */
-wmOperatorStatus WM_operator_confirm_ex(bContext *C,
-                                        wmOperator *op,
-                                        const char *title = nullptr,
-                                        const char *message = nullptr,
-                                        const char *confirm_text = nullptr,
-                                        blender::ui::AlertIcon icon = {}, /* ALERT_ICON_WARNING. */
-                                        bool cancel_default = false);
+wmOperatorStatus WM_operator_confirm_ex(
+    bContext *C,
+    wmOperator *op,
+    const char *title = nullptr,
+    const char *message = nullptr,
+    const char *confirm_text = nullptr,
+    blender::ui::AlertIcon icon = blender::ui::AlertIcon{0}, /* blender::ui::AlertIcon::Warning. */
+    bool cancel_default = false);
 
 /**
  * Invoke callback, file selector "filepath" unset + exec.
