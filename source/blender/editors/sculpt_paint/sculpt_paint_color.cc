c91c5c949a20ef97dec57b8c1fca7a0de09dadbf
Brecht Van Lommel <brecht@blender.org>
2025-11-15 01:04:23 +0100

    Fix: Wrong sculpt paint of byte attributes with wide gamut working space
    Missed in 9856615813ab137d0abd4baca847c7a2362bb794.
    
    Ref #145763.

--- source/blender/editors/sculpt_paint/sculpt_paint_color.cc
+++ source/blender/editors/sculpt_paint/sculpt_paint_color.cc
@@ -67,6 +67,7 @@ template<> float4 to_float(const MLoopCol &src)
   float4 dst;
   rgba_uchar_to_float(dst, reinterpret_cast<const uchar *>(&src));
   srgb_to_linearrgb_v3_v3(dst, dst);
+  IMB_colormanagement_rec709_to_scene_linear(dst, dst);
   return dst;
 }
 template<> float4 to_float(const MPropCol &src)
@@ -79,7 +80,8 @@ template<typename T> void from_float(const float4 &src, T &dst);
 template<> void from_float(const float4 &src, MLoopCol &dst)
 {
   float4 temp;
-  linearrgb_to_srgb_v3_v3(temp, src);
+  IMB_colormanagement_scene_linear_to_rec709(temp, src);
+  linearrgb_to_srgb_v3_v3(temp, temp);
   temp[3] = src[3];
   rgba_float_to_uchar(reinterpret_cast<uchar *>(&dst), temp);
 }
