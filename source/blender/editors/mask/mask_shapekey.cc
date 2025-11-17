5427ead16446172bbfbdba2b8ee33932d181c30f
Philipp Oeser <philipp@blender.org>
2025-11-17 14:13:39 +0100

    avoid code duplication by introducing utility function

--- source/blender/editors/mask/mask_shapekey.cc
+++ source/blender/editors/mask/mask_shapekey.cc
@@ -28,6 +28,13 @@
 
 #include "mask_intern.hh" /* own include */
 
+static void select_only_layer_shape(MaskLayer* mask_layer, MaskLayerShape* mask_layer_shape) {
+  LISTBASE_FOREACH (MaskLayerShape *, mask_layer_shape_iter, &mask_layer->splines_shapes) {
+    mask_layer_shape_iter->flag &= ~MASK_SHAPE_SELECT;
+  }
+  mask_layer_shape->flag |= MASK_SHAPE_SELECT;
+}
+
 static wmOperatorStatus mask_shape_key_insert_exec(bContext *C, wmOperator * /*op*/)
 {
   Scene *scene = CTX_data_scene(C);
@@ -46,11 +53,7 @@ static wmOperatorStatus mask_shape_key_insert_exec(bContext *C, wmOperator * /*o
     BKE_mask_layer_shape_from_mask(mask_layer, mask_layer_shape);
     changed = true;
 
-    /* Only select the new key (deselect others). */
-    LISTBASE_FOREACH (MaskLayerShape *, mask_layer_shape_iter, &mask_layer->splines_shapes) {
-      mask_layer_shape_iter->flag &= ~MASK_SHAPE_SELECT;
-    }
-    mask_layer_shape->flag |= MASK_SHAPE_SELECT;
+    select_only_layer_shape(mask_layer, mask_layer_shape);
   }
 
   if (changed) {
@@ -386,11 +389,7 @@ void ED_mask_layer_shape_auto_key(MaskLayer *mask_layer, const int frame)
   mask_layer_shape = BKE_mask_layer_shape_verify_frame(mask_layer, frame);
   BKE_mask_layer_shape_from_mask(mask_layer, mask_layer_shape);
 
-  /* Only select the autokey (deselect others). */
-  LISTBASE_FOREACH (MaskLayerShape *, mask_layer_shape_iter, &mask_layer->splines_shapes) {
-    mask_layer_shape_iter->flag &= ~MASK_SHAPE_SELECT;
-  }
-  mask_layer_shape->flag |= MASK_SHAPE_SELECT;
+  select_only_layer_shape(mask_layer, mask_layer_shape);
 }
 
 bool ED_mask_layer_shape_auto_key_all(Mask *mask, const int frame)
