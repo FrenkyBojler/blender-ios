8b3d472e721e80c045a8b1d3c11de2e5b15aa8a9
Philipp Oeser <philipp@blender.org>
2025-11-17 16:48:05 +0100

    add BKE_modifiers_add_at_end_if_possible utility
    This adds a modifier at the end of the stack, but respects if there are
    modifiers which are "pinned to last" (put the new modifier before then).
    Also take into account that some modifiers can only be added after
    "only-deforming" modifiers (but need to stay before e.g. "generating"
    modifiers).
    Shares logic with #object_modifier_check_move_after().
    
    Also used in a couple of ore places where pinning was not respected.

--- source/blender/blenkernel/intern/particle.cc
+++ source/blender/blenkernel/intern/particle.cc
@@ -3949,21 +3949,8 @@ static ModifierData *object_add_or_copy_particle_system(
 
   psmd = (ParticleSystemModifierData *)md;
   psmd->psys = psys;
-  ModifierData *pinned_md = nullptr;
-  LISTBASE_FOREACH_BACKWARD (ModifierData *, md, &ob->modifiers) {
-    if (md->flag & eModifierFlag_PinLast) {
-      pinned_md = md;
-    }
-    else {
-      break;
-    }
-  }
-  if (pinned_md) {
-    BLI_insertlinkbefore(&ob->modifiers, pinned_md, md);
-  }
-  else {
-    BLI_addtail(&ob->modifiers, md);
-  }
+
+  BKE_modifiers_add_at_end_if_possible(ob, md);
   BKE_object_modifier_set_active(ob, md);
   BKE_modifiers_persistent_uid_init(*ob, *md);
 
