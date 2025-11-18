be77f1b5cd2a3b69c7f5832c524086135b291faa
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-18 21:46:13 +0000

    Adds function to mesh_intern

--- source/blender/editors/mesh/mesh_intern.hh
+++ source/blender/editors/mesh/mesh_intern.hh
@@ -128,6 +128,11 @@ wmKeyMap *bevel_modal_keymap(wmKeyConfig *keyconf);
 
 void MESH_OT_bisect(wmOperatorType *ot);
 
+/* *** `editmesh_circularize.cc` *** */
+
+void MESH_OT_circularize(wmOperatorType *ot);
+
+
 /* *** `editmesh_extrude.cc` *** */
 
 void MESH_OT_extrude_repeat(wmOperatorType *ot);
