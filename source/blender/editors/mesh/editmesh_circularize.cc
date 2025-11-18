2d77e2b77a961bf21ea32d8f9153020db7dd8f14
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-18 22:50:36 +0000

    Sets up operator skeleton

--- source/blender/editors/mesh/editmesh_circularize.cc
+++ source/blender/editors/mesh/editmesh_circularize.cc
@@ -5,3 +5,21 @@
 /** \file
  * \ingroup edmesh
  */
+#include "WM_api.hh"
+
+static wmOperatorStatus edbm_circularize_exec(bContext *C, wmOperator *op)
+{
+  printf("Circularize exec");
+  return OPERATOR_FINISHED;
+}
+
+void MESH_OT_circularize(wmOperatorType *ot)
+{
+  ot->name = "Circularize";
+  ot->description = "Shpe boundary vertices into a circular form";
+  ot->idname = "MESH_OT_circularize";
+  
+  ot->exec = edbm_circularize_exec;
+
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+}
