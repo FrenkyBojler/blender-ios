300060320c042b94f17c4a2fb44873ee1209ae7d
Jonas Holzman <jonas@blender.org>
2025-11-18 15:29:01 +0100

    Slight additional color adjustements

--- source/blender/windowmanager/xr/intern/wm_xr_operators.cc
+++ source/blender/windowmanager/xr/intern/wm_xr_operators.cc
@@ -1712,9 +1712,9 @@ static void WM_OT_xr_navigation_teleport(wmOperatorType *ot)
                 5.0f);
 
   /* Ray colors. */
-  static const float default_teleport_ray_hit_color[4] = {0.3f, 0.6f, 0.9f, 1.0f};
+  static const float default_teleport_ray_hit_color[4] = {0.4f, 0.6f, 0.8f, 1.0f};
   static const float default_teleport_ray_miss_color[4] = {1.0f, 0.35f, 0.35f, 1.0f};
-  static const float default_teleport_ray_fallback_color[4] = {0.4f, 0.35f, 0.8f, 1.0f};
+  static const float default_teleport_ray_fallback_color[4] = {0.5f, 0.45f, 0.8f, 1.0f};
   RNA_def_float_color(ot->srna,
                       "hit_color",
                       4,
