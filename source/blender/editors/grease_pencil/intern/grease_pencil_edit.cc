blender/blender

commit 31c6dca04e5da5404317feaa73aa747e5b5ac99b

Fix #146446: Move to layer doesn't append strokes to existing drawing
When autokey is off, operation is expected to append selected strokes to
visible geometry, rather than inserting new frame. Now fixed with
`ensure_active_keyframe`, keyframe is inserted in that function when
autokey is on. When off, else block will append strokes to the visible
drawing.

Pull Request: https://projects.blender.org/blender/blender/pulls/149436

source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -1934,9 +1934,12 @@ static wmOperatorStatus grease_pencil_move_to_layer_exec(bContext *C, wmOperator
       continue;
     }
 
-    if (!layer_dst.frames().lookup_ptr(info.frame_number)) {
+    bool is_key_inserted = false;
+    const bool has_active_key = ensure_active_keyframe(
+        *scene, grease_pencil, layer_dst, false, is_key_inserted);
+    if (has_active_key && is_key_inserted) {
       /* Move geometry to a new drawing in target layer. */
-      Drawing &drawing_dst = *grease_pencil.insert_frame(layer_dst, info.frame_number);
+      Drawing &drawing_dst = *grease_pencil.get_drawing_at(layer_dst, info.frame_number);
       drawing_dst.strokes_for_write() = bke::curves_copy_curve_selection(
           curves_src, selected_strokes, {});
 

FrenkyBojler/blender-ios

commit 9d8d33016d3e30b7f1b78015c5f8190c943de705

Fix #150139: Grease Pencil: Move to layer fails with empty layer
After 31c6dca04e, new keyframe is added to layer_dst when `auto key` is
on, instead appends the drawing to active keyframe. If layer_dst doesn't
have a keyframe, operator silently fails. To fix this, add keyframe to
dst layer when frames() is empty.

source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -1934,6 +1934,10 @@ static wmOperatorStatus grease_pencil_move_to_layer_exec(bContext *C, wmOperator
       continue;
     }
 
+    if (layer_dst.frames().is_empty()) {
+      grease_pencil.insert_frame(layer_dst, scene->r.cfra);
+    }
+
     bool is_key_inserted = false;
     const bool has_active_key = ensure_active_keyframe(
         *scene, grease_pencil, layer_dst, false, is_key_inserted);

source/blender/editors/space_action/space_action.cc
@@ -425,7 +425,7 @@ static void action_channel_region_listener(const wmRegionListenerParams *params)
       }
       break;
     case NC_GPENCIL:
-      if (ELEM(wmn->action, NA_RENAME, NA_SELECTED)) {
+      if (ELEM(wmn->action, NA_RENAME, NA_SELECTED, NA_EDITED)) {
         ED_region_tag_redraw(region);
       }
       break;