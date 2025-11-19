5a4c39fc7bae337487c48024354ca0f1bb6dcc38
SensArice <sensmd@qq.com>
2025-11-19 21:27:09 +0800

    fix py merge error

--- scripts/startup/bl_ui/node_add_menu_geometry.py
+++ scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -569,7 +569,8 @@ class NODE_MT_gn_input_import_base(node_add_menu.NodeMenu):
         self.node_operator(layout, "GeometryNodeImportSTL", label="STL (.stl)")
         self.node_operator(layout, "GeometryNodeImportText", label="Text (.txt)")
         self.node_operator(layout, "GeometryNodeImportVDB", label="OpenVDB (.vdb)")
-        self.node_operator(layout, "GeometryNodeSearchInFolder"，label="Folder")
+        self.node_operator(layout, "GeometryNodeSearchInFolder", label="Folder")
+                           
         self.draw_assets_for_catalog(layout, self.menu_path)
 
 
