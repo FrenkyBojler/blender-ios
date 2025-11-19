5fc539e597d42922be51a16b0047445567dea785
skaplunzibra <s.kaplun@zibra.ai>
2025-11-19 13:59:19 +0200

    added extra parameter check

--- source/blender/blenkernel/intern/volume.cc
+++ source/blender/blenkernel/intern/volume.cc
@@ -1170,6 +1170,10 @@ openvdb::GridBase::Ptr BKE_volume_grid_create_with_changed_resolution(
 
 bool BKE_volume_add_new_empty_grid(Volume *volume, const char *grid_name, VolumeGridType grid_type)
 {
+  if (!volume) {
+    return false;
+  }
+
   openvdb::GridBase::Ptr new_grid = BKE_volume_grid_type_operation(grid_type, CreateGridOp{});
 
   if (!new_grid) {
