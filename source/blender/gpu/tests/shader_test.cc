8f741bd3b139e686ef5c87aad2f02e2f5ab06ca0
Clément Foucault <foucault.clem@gmail.com>
2025-11-20 15:10:32 +0100

    GPU: Add test for creating python shader
    This test should contain pure GLSL.
    The resources should be available in global space.
    The builtin should be automatically parsed.
    Minimal source processing should be performed.

--- source/blender/gpu/tests/shader_test.cc
+++ source/blender/gpu/tests/shader_test.cc
@@ -31,6 +31,37 @@ namespace blender::gpu::tests {
 
 using namespace blender::gpu::shader;
 
+/* This test should contain pure GLSL source as this is what we are expecting from the Python API.
+ * Make sure to keep it in sync with the Python API. */
+static void test_shader_python()
+{
+  using namespace shader;
+
+  ShaderCreateInfo create_info("python_shader");
+  create_info.image(0,
+                    TextureFormat::SFLOAT_16_16_16_16,
+                    Qualifier::write,
+                    ImageReadWriteType::image2D,
+                    "test_img");
+  create_info.local_group_size(16, 16, 1);
+  create_info.builtins(BuiltinBits::GLOBAL_INVOCATION_ID);
+  create_info.compute_source("draw_colormanagement_lib.glsl");
+  create_info.compute_source_generated = R"(
+  void main()
+  {
+    /* This is a test shader. */
+    imageStore(test_img, ivec2(gl_GlobalInvocationID.xy), vec4(0));
+  }
+  )";
+
+  gpu::Shader *shader = GPU_shader_create_from_info_python(
+      reinterpret_cast<GPUShaderCreateInfo *>(&create_info));
+  EXPECT_NE(shader, nullptr);
+
+  GPU_shader_free(shader);
+}
+GPU_TEST(shader_python)
+
 static void test_shader_compute_2d()
 {
 
