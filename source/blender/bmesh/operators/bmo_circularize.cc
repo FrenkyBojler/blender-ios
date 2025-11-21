303c5939148008a3da6f557b397eaa612d6d4e57
Tariq-Sulley <tariqsulley3c@gmail.com>
2025-11-21 14:48:20 +0000

    bmo progress

--- source/blender/bmesh/operators/bmo_circularize.cc
+++ source/blender/bmesh/operators/bmo_circularize.cc
@@ -9,7 +9,19 @@
 #include "bmesh.hh"
 #include "intern/bmesh_operators_private.hh" /* own include */
 
-void bmo_circularize_exec(BMesh *bm, BMOperator *op)
-{
-  printf("calling exec");
-}
+/* Holds data for a vertex projected onto the local plane. */
+struct CircleVert {
+  BMVert *v;
+  /* Current postion on the plane. */
+  float co_2d[2];
+  /* Where it should move to on the circle. */
+  float target_2d[2];
+};
+
+/* Stores the geometry loop and whether it forms a closed circle or open chain. */
+struct LoopData {
+  blender::Vector<BMVert *> verts;
+  bool is_closed;
+};
+
+void bmo_circularize_exec(BMesh * /*bm*/, BMOperator * /*op*/) {}
