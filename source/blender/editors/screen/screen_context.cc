d354d2c00a24e34dd1737c9457c47903e3abab8a
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-11-24 15:49:13 +0100

    remove parenthesis

--- source/blender/editors/screen/screen_context.cc
+++ source/blender/editors/screen/screen_context.cc
@@ -436,7 +436,7 @@ static eContextResult screen_ctx_selected_pose_bones(const bContext *C, bContext
   Object *obact = BKE_view_layer_active_object_get(view_layer);
   Object *obpose = BKE_object_pose_armature_get(obact);
   if (obpose && obpose->pose && obpose->data) {
-    if ((obpose->pose->flag & POSE_RECALC)) {
+    if (obpose->pose->flag & POSE_RECALC) {
       /* Can happen with undo-redo, see #150451. */
       BKE_pose_rebuild(CTX_data_main(C), obpose, (bArmature *)obpose->data, false);
     }
