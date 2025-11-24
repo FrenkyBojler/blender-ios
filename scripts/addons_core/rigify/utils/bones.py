c7cc64378ce312a038489c9d5f720ded29bfb9dd
Sophia Martinez <mail@zophiekat.art>
2025-11-24 15:40:01 -0600

    clean commit

--- scripts/addons_core/rigify/utils/bones.py
+++ scripts/addons_core/rigify/utils/bones.py
@@ -207,10 +207,38 @@ def copy_bone_properties(obj: ArmatureObject, bone_name_1: str, bone_name_2: str
 
         if widget:
             pose_bone_2.custom_shape = pose_bone_1.custom_shape
+            
+        # Copy BBone properties from Bone data
+        bone_1 = obj.data.bones[bone_name_1]
+        bone_2 = obj.data.bones[bone_name_2]
+        
+        # Only copy if source bone has bbone segments
+        if bone_1.bbone_segments > 1:
+            # Copy all BBone properties from obj.data.bones (Bone properties)
+            for name in ['bbone_curveinx', 'bbone_curveinz', 'bbone_curveoutx', 'bbone_curveoutz',
+                         'bbone_easein', 'bbone_easeout',
+                         'bbone_mapping_mode', 'bbone_rollin', 'bbone_rollout',
+                         'bbone_scalein', 'bbone_scaleout', 'bbone_segments',
+                         'bbone_x', 'bbone_z', 'use_endroll_as_inroll', 'use_scale_easing',
+                         'bbone_custom_handle_start', 'bbone_custom_handle_end']:
+                setattr(bone_2, name, getattr(bone_1, name))
+        
+        # Only copy if source bone has bbone segments
+        if bone_1.bbone_segments > 1:
+            # Copy all BBone properties from obj.data.pose.bones (PoseBone properties)
+            for name in ['bbone_curveinx', 'bbone_curveinz', 'bbone_curveoutx', 'bbone_curveoutz',
+                         'bbone_easein', 'bbone_easeout',
+                         'bbone_rollin', 'bbone_rollout',
+                         'bbone_scalein', 'bbone_scaleout',
+                        ]:
+                setattr(pose_bone_2, name, getattr(pose_bone_1, name))
+        
+            
     else:
         raise MetarigError("Cannot copy bone properties in edit mode")
 
-
+# Do we still want to keep the legacy functions?.
+# Unused Legacy Function
 def _legacy_copy_bone(obj, bone_name, assign_name=''):
     """LEGACY ONLY, DON'T USE"""
     new_name = copy_bone(obj, bone_name, assign_name, parent=True, bbone=True)
@@ -308,6 +336,7 @@ def disable_bbones(obj: ArmatureObject, bone_names: Iterable[str]):
 
 
 # noinspection SpellCheckingInspection
+# Unused Legacy Function
 def _legacy_make_nonscaling_child(obj, bone_name, location, child_name_postfix=""):
     """ Takes the named bone and creates a non-scaling child of it at
         the given location.  The returned bone (returned by name) is not
