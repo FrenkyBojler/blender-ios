096e151b13f88418145b50f43d9c6aaa7b442f02
Hans Goudey <hans@blender.org>
2025-11-24 15:26:21 -0500

    Shape Keys: Apply to Basis operator
    As desribed in #150299, this operator applies the deformations
    of the selected shape keys to the basis key and then removes them.

--- source/blender/editors/object/object_shapekey.cc
+++ source/blender/editors/object/object_shapekey.cc
@@ -921,4 +921,114 @@ void OBJECT_OT_shape_key_make_basis(wmOperatorType *ot)
 
 /** \} */
 
+/* -------------------------------------------------------------------- */
+/** \name Shape Key Make Basis Operator
+ * \{ */
+
+static bool shape_key_apply_to_basis_poll(bContext *C)
+{
+  if (!shape_key_exists_poll(C)) {
+    return false;
+  }
+
+  Object *ob = context_object(C);
+  if (ob->type != OB_MESH) {
+    return false;
+  }
+  /* 0 = nothing active, 1 = basis key active. */
+  return ob->shapenr > 1;
+}
+
+static void add_arrays(const MutableSpan<float3> a, const Span<float3> b)
+{
+  BLI_assert(a.size() == b.size());
+  threading::parallel_for(a.index_range(), 2048, [&](const IndexRange range) {
+    for (const int i : range) {
+      a[i] += b[i];
+    }
+  });
+}
+
+static wmOperatorStatus shape_key_apply_to_basis_exec(bContext *C, wmOperator *op)
+{
+  Main *bmain = CTX_data_main(C);
+  Object *ob = CTX_data_active_object(C);
+  Key *key = BKE_key_from_object(ob);
+  KeyBlock *basis_key = static_cast<KeyBlock *>(key->block.first);
+  MutableSpan<float3> basis_data(static_cast<float3 *>(basis_key->data), basis_key->totelem);
+
+  Array<float3> translations(basis_data.size(), float3(0));
+
+  Set<KeyBlock *> processed_keys;
+  int index;
+  int locked_count = 0;
+  LISTBASE_FOREACH_INDEX (KeyBlock *, kb, &key->block, index) {
+    if (!shape_key_is_selected(*ob, *kb, index)) {
+      continue;
+    }
+    if (kb == basis_key) {
+      continue;
+    }
+    if (kb->flag & KEYBLOCK_LOCKED_SHAPE) {
+      locked_count++;
+      continue;
+    }
+    const Span kb_data(static_cast<const float3 *>(kb->data), kb->totelem);
+    for (const int i : kb_data.index_range()) {
+      translations[i] += (kb_data[i] - basis_data[i]);
+    }
+    processed_keys.add_new(kb);
+  }
+
+  if (locked_count != 0) {
+    BKE_reportf(op->reports, RPT_ERROR, "Skipped %d locked shape keys", locked_count);
+  }
+
+  if (processed_keys.is_empty()) {
+    return OPERATOR_CANCELLED;
+  }
+
+  for (KeyBlock *kb : processed_keys) {
+    BKE_object_shapekey_remove(bmain, ob, kb);
+  }
+
+  add_arrays(basis_data, translations);
+
+  if (const std::optional<Array<bool>> dependent = BKE_keyblock_get_dependent_keys(key, 0)) {
+    int i;
+    LISTBASE_FOREACH_INDEX (KeyBlock *, kb, &key->block, i) {
+      if (kb == basis_key) {
+        continue;
+      }
+      if (!(*dependent)[i]) {
+        continue;
+      }
+      MutableSpan<float3> kb_data(static_cast<float3 *>(kb->data), kb->totelem);
+      add_arrays(kb_data, translations);
+    }
+  }
+
+  /* Make the basis key active. */
+  ob->shapenr = 1;
+
+  DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
+  WM_event_add_notifier(C, NC_OBJECT | ND_DRAW, ob);
+
+  return OPERATOR_FINISHED;
+}
+
+void OBJECT_OT_shape_key_apply_to_basis(wmOperatorType *ot)
+{
+  ot->name = "Apply to Basis Key";
+  ot->idname = "OBJECT_OT_shape_key_apply_to_basis";
+  ot->description = "Appply deformations of selected shape keys to the basis key, removing them";
+
+  ot->poll = shape_key_apply_to_basis_poll;
+  ot->exec = shape_key_apply_to_basis_exec;
+
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+}
+
+/** \} */
+
 }  // namespace blender::ed::object
