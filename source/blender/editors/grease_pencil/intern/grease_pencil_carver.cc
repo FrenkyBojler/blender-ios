3b4c4527ee55bec22d69237f7c199e0da110de2c
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-11-24 07:04:02 -0800

    Fix: Merge error.

--- source/blender/editors/grease_pencil/intern/grease_pencil_carver.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_carver.cc
@@ -1641,7 +1641,7 @@ static bke::CurvesGeometry create_curves_from_segments(const bke::CurvesGeometry
 
   /* Copy/Interpolate point attributes. */
   for (auto &attribute : bke::retrieve_attributes_for_transfer(
-           src_attributes, dst_attributes, ATTR_DOMAIN_MASK_POINT, {}))
+           src_attributes, dst_attributes, {bke::AttrDomain::Point}, {}))
   {
     bke::attribute_math::convert_to_static_type(attribute.dst.span.type(), [&](auto dummy) {
       using T = decltype(dummy);
