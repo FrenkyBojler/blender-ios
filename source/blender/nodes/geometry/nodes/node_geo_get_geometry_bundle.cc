1a4b7c87cb4f6ff8541a34b4ca83ef2364efe87a
Jacques Lucke <jacques@blender.org>
2025-11-25 13:40:49 +0100

    disable nodes when experimental option is not disabled

--- source/blender/nodes/geometry/nodes/node_geo_get_geometry_bundle.cc
+++ source/blender/nodes/geometry/nodes/node_geo_get_geometry_bundle.cc
@@ -5,6 +5,7 @@
 #include "node_geometry_util.hh"
 
 #include "NOD_geometry_nodes_bundle.hh"
+#include "NOD_socket_search_link.hh"
 
 namespace blender::nodes::node_geo_get_geometry_bundle {
 
@@ -16,9 +17,23 @@ static void node_declare(NodeDeclarationBuilder &b)
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
+  if (!U.experimental.use_geometry_bundle) {
+    params.error_message_add(NodeWarningType::Error,
+                             TIP_("The experimental option for this node is disabled"));
+    params.set_default_remaining_outputs();
+    return;
+  }
   GeometrySet geometry_set = params.extract_input<GeometrySet>("Geometry");
   BundlePtr bundle = geometry_set.bundle_ptr();
   params.set_output("Bundle", std::move(bundle));
+}
+
+static void node_gather_link_searches(GatherLinkSearchOpParams &params)
+{
+  if (!U.experimental.use_geometry_bundle) {
+    return;
+  }
+  search_link_ops_for_basic_node(params);
 }
 
 static void node_register()
@@ -31,6 +46,7 @@ static void node_register()
   ntype.nclass = NODE_CLASS_GEOMETRY;
   ntype.geometry_node_execute = node_geo_exec;
   ntype.declare = node_declare;
+  ntype.gather_link_search_ops = node_gather_link_searches;
   bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)
