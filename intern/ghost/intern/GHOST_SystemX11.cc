blender/blender

commit 720fc44ca38330bbc1b8cfec5f37b56a2d6b46c3

GHOST: Fix X11 clipboard selection requests
This commit fixes a pretty long-standing bug in the X11 clipboard code
which caused selection target requests (and thus pasting) to sometimes
fail on certain softwares like Firefox.

The problem lied in the fact that the property format used when setting
the supported target list was dependent on the original request, when
it should always be XA_ATOM (a list of Atom), this can be seen in other
implementations, like GLFW.

This commit also cleans up the surrounding code, by using m_atom to
access atoms instead of redeclaring them in the function, which also
clears up a few instance where the difference between `xse->target`
(the request target) and `target` (the TARGETS Atom) was really unclear.

Pull Request: https://projects.blender.org/blender/blender/pulls/131507

intern/ghost/intern/GHOST_SystemX11.cc
@@ -1465,14 +1465,7 @@ void GHOST_SystemX11::processEvent(XEvent *xe)
       break;
     case SelectionRequest: {
       XEvent nxe;
-      Atom target, utf8_string, string, compound_text, c_string;
       XSelectionRequestEvent *xse = &xe->xselectionrequest;
-
-      target = XInternAtom(m_display, "TARGETS", False);
-      utf8_string = XInternAtom(m_display, "UTF8_STRING", False);
-      string = XInternAtom(m_display, "STRING", False);
-      compound_text = XInternAtom(m_display, "COMPOUND_TEXT", False);
-      c_string = XInternAtom(m_display, "C_STRING", False);
 
       /* support obsolete clients */
       if (xse->property == None) {
@@ -1488,7 +1481,12 @@ void GHOST_SystemX11::processEvent(XEvent *xe)
       nxe.xselection.time = xse->time;
 
       /* Check to see if the requester is asking for String */
-      if (ELEM(xse->target, utf8_string, string, compound_text, c_string)) {
+      if (ELEM(xse->target,
+               m_atom.UTF8_STRING,
+               m_atom.STRING,
+               m_atom.COMPOUND_TEXT,
+               m_atom.C_STRING))
+      {
         if (xse->selection == XInternAtom(m_display, "PRIMARY", False)) {
           XChangeProperty(m_display,
                           xse->requestor,
@@ -1510,25 +1508,24 @@ void GHOST_SystemX11::processEvent(XEvent *xe)
                           strlen(txt_cut_buffer));
         }
       }
-      else if (xse->target == target) {
-        Atom alist[5];
-        alist[0] = target;
-        alist[1] = utf8_string;
-        alist[2] = string;
-        alist[3] = compound_text;
-        alist[4] = c_string;
+      else if (xse->target == m_atom.TARGETS) {
+        const Atom atom_list[] = {m_atom.TARGETS,
+                                  m_atom.UTF8_STRING,
+                                  m_atom.STRING,
+                                  m_atom.COMPOUND_TEXT,
+                                  m_atom.C_STRING};
         XChangeProperty(m_display,
                         xse->requestor,
                         xse->property,
-                        xse->target,
+                        XA_ATOM,
                         32,
                         PropModeReplace,
-                        (uchar *)alist,
-                        5);
+                        reinterpret_cast<const uchar *>(atom_list),
+                        ARRAY_SIZE(atom_list));
         XFlush(m_display);
       }
       else {
-        /* Change property to None because we do not support anything but STRING */
+        /* Change property to None because we do not support the selection request target. */
         nxe.xselection.property = None;
       }
 

intern/ghost/intern/GHOST_SystemX11.hh
@@ -314,6 +314,7 @@ class GHOST_SystemX11 : public GHOST_System {
     /* Atoms for Selection, copy & paste. */
     Atom TARGETS;
     Atom STRING;
+    Atom C_STRING;
     Atom COMPOUND_TEXT;
     Atom TEXT;
     Atom CLIPBOARD;

FrenkyBojler/blender-ios

commit 52a089b29c0e086fb8309a27bb44cd75e581c8e5

Fix #150414: Copying value to clipboard causes Steam to crash on Linux X11
The issue was caused by an oversight in 720fc44ca3 where atoms
previously defined directly in the functions where replaced by accesses
to the `atom_` struct to avoid unecessarily redeclaring them.

Since it wasn't already present, the C_STRING atom was added to the
struct.  However, its initialization was ommitted, leaving it default
initialized / undefined. This could then cause external application
(such as Steam) to fail when obtaining this atom as part of a TARGETS
request.

Fixed by properly initializing the atom in the SystemX11 constructor.

intern/ghost/intern/GHOST_SystemX11.cc
@@ -154,6 +154,7 @@ GHOST_SystemX11::GHOST_SystemX11()
   GHOST_INTERN_ATOM(_MOTIF_WM_HINTS);
   GHOST_INTERN_ATOM(TARGETS);
   GHOST_INTERN_ATOM(STRING);
+  GHOST_INTERN_ATOM(C_STRING);
   GHOST_INTERN_ATOM(COMPOUND_TEXT);
   GHOST_INTERN_ATOM(TEXT);
   GHOST_INTERN_ATOM(CLIPBOARD);