d57f5f3a56c01c67e46cc57a36c83dac77f03c12
Habib Gahbiche <habib@blender.org>
2025-11-27 18:15:58 +0100

    Fix #150582: Compositing applied to render when is disabled in render pipeline
    Also fixes the compositor overriding the render result when sequencer is
    enabled.

--- source/blender/editors/space_node/node_edit.cc
+++ source/blender/editors/space_node/node_edit.cc
@@ -319,8 +319,9 @@ static bool is_compositing_possible(const bContext *C)
 }
 
 /* Returns the compositor outputs that need to be computed because their result is visible to the
- * user. */
-static blender::compositor::OutputTypes get_compositor_needed_outputs(const bContext *C)
+ * user or required by the render pipeline. */
+static blender::compositor::OutputTypes get_compositor_needed_outputs(const bContext *C,
+                                                                      const Scene *scene)
 {
   blender::compositor::OutputTypes needed_outputs = blender::compositor::OutputTypes::None;
 
@@ -347,10 +348,20 @@ static blender::compositor::OutputTypes get_compositor_needed_outputs(const bCon
         if (!image || image->source != IMA_SRC_VIEWER) {
           continue;
         }
-        if (image->type == IMA_TYPE_R_RESULT) {
+        /* Do not override the Render Result if compositing is disabled in the render pipeline or
+         * if the sequencer is enabled. */
+        if (image->type == IMA_TYPE_R_RESULT && scene->r.scemode & R_DOCOMP &&
+            ((scene->r.scemode & R_DOSEQ) == 0))
+        {
           needed_outputs |= blender::compositor::OutputTypes::Composite;
         }
         else if (image->type == IMA_TYPE_COMPOSITE) {
+          needed_outputs |= blender::compositor::OutputTypes::Viewer;
+        }
+      }
+      else if (space_link->spacetype == SPACE_SEQ) {
+        const SpaceSeq *space_sequencer = reinterpret_cast<const SpaceSeq *>(space_link);
+        if (ELEM(space_sequencer->view, SEQ_VIEW_PREVIEW, SEQ_VIEW_SEQUENCE_PREVIEW)) {
           needed_outputs |= blender::compositor::OutputTypes::Viewer;
         }
       }
@@ -373,7 +384,7 @@ void ED_node_composite_job(const bContext *C, bNodeTree *nodetree, Scene *scene_
   /* None of the outputs are needed except maybe previews, so no need to execute the compositor.
    * Previews are not considered because they are a secondary output that needs another output to
    * be computed with. */
-  blender::compositor::OutputTypes needed_outputs = get_compositor_needed_outputs(C);
+  blender::compositor::OutputTypes needed_outputs = get_compositor_needed_outputs(C, scene_owner);
   if (ELEM(needed_outputs,
            blender::compositor::OutputTypes::None,
            blender::compositor::OutputTypes::Previews))
