a49bbeaf5154d6fa2817ddeb397cb272d14026e4
Jeroen Bakker <jeroen@blender.org>
2025-11-28 09:58:46 +0100

    Fix move record order to first switch pipelines then do dynamic state

--- source/blender/gpu/vulkan/vk_backend.cc
+++ source/blender/gpu/vulkan/vk_backend.cc
@@ -468,8 +468,10 @@ void VKBackend::detect_workarounds(VKDevice &device)
       VK_EXT_GRAPHICS_PIPELINE_LIBRARY_EXTENSION_NAME);
   extensions.line_rasterization = device.supports_extension(
       VK_EXT_LINE_RASTERIZATION_EXTENSION_NAME);
+#if 1
   extensions.extended_dynamic_state = device.supports_extension(
       VK_EXT_EXTENDED_DYNAMIC_STATE_EXTENSION_NAME);
+#endif
 #ifdef _WIN32
   extensions.external_memory = device.supports_extension(
       VK_KHR_EXTERNAL_MEMORY_WIN32_EXTENSION_NAME);
