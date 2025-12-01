2d41fdd3ded900ec4b4ca6dcf1d086b92cb89efa
IREXTIA <developer@irextia.com>
2025-12-01 15:46:13 +0545

    Fail the poll if the final destination is inside the dragged collection

--- source/blender/editors/space_outliner/outliner_dragdrop.cc
+++ source/blender/editors/space_outliner/outliner_dragdrop.cc
@@ -198,6 +198,24 @@ static bool is_collection_element(TreeElement *te)
   return outliner_is_collection_tree_element(te);
 }
 
+static bool outliner_is_tree_element_in_collection(TreeElement *te, Collection *collection)
+{
+  if (!(te && collection)) {
+    return false;
+  }
+
+  TreeElement *collection_te = outliner_data_from_tree_element_and_parents(is_collection_element,
+                                                                           te);
+  while (collection_te->parent != nullptr) {
+    Collection *parent_collection = outliner_collection_from_tree_element(collection_te->parent);
+    if (&parent_collection->id == &collection->id) {
+      return true;
+    }
+    collection_te = collection_te->parent;
+  }
+  return false;
+}
+
 static bool is_object_element(TreeElement *te)
 {
   TreeStoreElem *tselem = TREESTORE(te);
@@ -1157,6 +1175,11 @@ static bool collection_drop_init(bContext *C, wmDrag *drag, const int xy[2], Col
 
   ID *id = drag_id->id;
   if (!(id && ELEM(GS(id->name), ID_GR, ID_OB))) {
+    return false;
+  }
+
+  /* Check if destination is inside the dragged collection.*/
+  if (outliner_is_tree_element_in_collection(te, (Collection *)id)) {
     return false;
   }
 
