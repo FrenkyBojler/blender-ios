blender/blender

commit d4b32b8218db6631c41b63e54ec9571d9d51067c

Fix #150705: Relative multires navigation doesn't work with unselected object
When using the `object.subdivision_set` operator, it was possible for
there to be an active but unselected object (e.g. in Sculpt mode),
leading to the operator failing to execute. This is rather confusing
behavior, so it has been changed to fallback to the active object if
no selected objects exist.

Pull Request: https://projects.blender.org/blender/blender/pulls/150758

scripts/startup/bl_operators/object.py
@@ -249,12 +249,21 @@ class SubdivisionSet(Operator):
         relative = self.relative
         ensure_modifier = self.ensure_modifier
 
+        objs = context.selected_editable_objects
+        if not objs:
+            active = context.active_object
+            if active:
+                objs = [active]
+            else:
+                self.report({'WARNING'}, "No active object")
+                return {'CANCELLED'}
+
         if relative and level == 0:
             return {'CANCELLED'}  # nothing to do
 
         if not ensure_modifier:
             any_object_has_relevant_modifier = False
-            for obj in context.selected_editable_objects:
+            for obj in objs:
                 if obj.mode == 'SCULPT':
                     any_object_has_relevant_modifier |= any(mod.type == 'MULTIRES' for mod in obj.modifiers)
                 elif obj.mode == 'OBJECT':
@@ -322,7 +331,7 @@ class SubdivisionSet(Operator):
                 except Exception:
                     self.report({'WARNING'}, rpt_("Modifiers cannot be added to object: {:s}").format(obj.name))
 
-        for obj in context.selected_editable_objects:
+        for obj in objs:
             set_object_subd(obj)
 
         return {'FINISHED'}

FrenkyBojler/blender-ios

commit a4a04413e073d7e237f09170e65db7adf55398d2

Fix: `object.subdivion_set` behaves inconsistently in Object mode
Introduced with d4b32b8218db6631c41b63e54ec9571d9d51067c

In Object Mode, the operator continues to only work on selected,
editable objects. In Sculpt mode, the object now only works on the
current active object.

scripts/startup/bl_operators/object.py
@@ -250,13 +250,15 @@ class SubdivisionSet(Operator):
         ensure_modifier = self.ensure_modifier
 
         objs = context.selected_editable_objects
+        active_object = context.active_object
+
+        # For Sculpt, defer to the active object, not the selected objects
+        if active_object and active_object.mode == 'SCULPT':
+            objs = [active_object]
+
         if not objs:
-            active = context.active_object
-            if active:
-                objs = [active]
-            else:
-                self.report({'WARNING'}, "No active object")
-                return {'CANCELLED'}
+            self.report({'WARNING'}, "No applicable objects found")
+            return {'CANCELLED'}
 
         if relative and level == 0:
             return {'CANCELLED'}  # nothing to do