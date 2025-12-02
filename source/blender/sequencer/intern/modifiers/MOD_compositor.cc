c25c29c5ac6e19e19607006b6506fa34f61e0e0e
Guillermo Venegas <guillermovcra@gmail.com>
2025-12-02 11:17:57 -0600

    init

--- source/blender/sequencer/intern/modifiers/MOD_compositor.cc
+++ source/blender/sequencer/intern/modifiers/MOD_compositor.cc
@@ -251,12 +251,12 @@ static void compositor_modifier_apply(ModifierApplyContext &context,
 
 static void compositor_modifier_panel_draw(const bContext *C, Panel *panel)
 {
-  uiLayout *layout = panel->layout;
+  ui::Layout &layout = *panel->layout;
   PointerRNA *ptr = UI_panel_custom_data_get(panel);
 
-  layout->use_property_split_set(true);
+  layout.use_property_split_set(true);
 
-  uiTemplateID(layout,
+  uiTemplateID(&layout,
                C,
                ptr,
                "node_group",
@@ -264,10 +264,10 @@ static void compositor_modifier_panel_draw(const bContext *C, Panel *panel)
                nullptr,
                nullptr);
 
-  if (uiLayout *mask_input_layout = layout->panel_prop(
+  if (ui::Layout *mask_input_layout = layout.panel_prop(
           C, ptr, "open_mask_input_panel", IFACE_("Mask Input")))
   {
-    draw_mask_input_type_settings(C, mask_input_layout, ptr);
+    draw_mask_input_type_settings(C, *mask_input_layout, ptr);
   }
 }
 
