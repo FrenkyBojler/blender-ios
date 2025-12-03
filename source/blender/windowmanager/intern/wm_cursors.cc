8c4384029f6ab7a1be8fa6bb44167258d4ce330f
Harley Acheson <harley.acheson@gmail.com>
2025-12-03 15:31:12 -0800

    VSE cursors more like the Mac versions.

--- source/blender/windowmanager/intern/wm_cursors.cc
+++ source/blender/windowmanager/intern/wm_cursors.cc
@@ -173,7 +173,7 @@ static int wm_cursor_size(const wmWindow *win)
 
   if (OS_MAC) {
     /* MacOS always scales up this type of cursor for high-dpi displays. */
-    return 22;
+    return 21;
   }
 
   /* The DPI as a scale without the UI scale preference. */
