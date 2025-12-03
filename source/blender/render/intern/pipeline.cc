c8cfc9cd8617703b9c517fb88f5b7870b7382511
Richard Antalik <richardantalik@gmail.com>
2025-12-03 07:03:26 +0100

    Cleanup: VSE: Rename STRIP_TYPE_SOUND_RAM to STRIP_TYPE_SOUND
    There still is STRIP_TYPE_SOUND_HD, which is kept in code for
    compatibility. But I think, that it is reaonable to rename currently
    used type to more reasonable name. It is not stored in RAM after all.

--- source/blender/render/intern/pipeline.cc
+++ source/blender/render/intern/pipeline.cc
@@ -1364,7 +1364,7 @@ bool RE_seq_render_active(Scene *scene, RenderData *rd)
   }
 
   LISTBASE_FOREACH (Strip *, strip, &ed->seqbase) {
-    if (strip->type != STRIP_TYPE_SOUND_RAM &&
+    if (strip->type != STRIP_TYPE_SOUND &&
         !blender::seq::render_is_muted(&ed->channels, strip))
     {
       return true;
