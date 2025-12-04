76c746addfdd53c0fe52355b87f2560c27fe234a
Jacques Lucke <jacques@blender.org>
2025-12-04 23:23:54 +0100

    progress

--- source/blender/blenloader_diff/intern/blend_diff.cc
+++ source/blender/blenloader_diff/intern/blend_diff.cc
@@ -1,48 +1,38 @@
 #include <fmt/format.h>
+#include <fstream>
 
 #include "BLI_index_range.hh"
 
+namespace blender::blend_diff {
+
+}  // namespace blender::blend_diff
+
 int main(int argc, char *argv[])
 {
-  std::string data =
-      R"(diff --git a/source/blender/blenloader_diff/CMakeLists.txt b/source/blender/blenloader_diff/CMakeLists.txt
-index 949e6da4d5b..c654b3bb650 100644
---- a/source/blender/blenloader_diff/CMakeLists.txt
-+++ b/source/blender/blenloader_diff/CMakeLists.txt
-@@ -2,13 +2,10 @@
-#
-#SPDX - License - Identifier : GPL - 2.0 - or -later
+  if (argc <= 7) {
+    return 1;
+  }
+  const char *file_a = argv[1];
+  const char *file_b = argv[5];
 
--set(INC
-+set(TEXTCONV_INC
- )
+  std::fstream myfile("/home/jacques/Downloads/test.txt", std::ios::out);
+  for (const int i : blender::IndexRange(argc)) {
+    myfile << argv[i] << '\n';
+    // fmt::println("Arg {}: {}", i, argv[i]);
+  }
 
--set(INC_SYS
--)
--
--set(SRC
-+set(TEXTCONV_SRC
-   intern/blend_textconv.cc
- )
-
-)";
-  //   fmt::println(data);
-  //   for (const int i : blender::IndexRange(argc)) {
-  //     fmt::println("Arg {}: {}", i, argv[i]);
-  //   }
-  //   fmt::println(
-  //       R"(diff --git a/geometry_nodes_essentials.blend b/geometry_nodes_essentials.blend
-  // index 2343243..3452343 100644
-  // + sd sd fsdChanged
-  // )");
   fmt::println(
-      R"(diff --git a/geometry_nodes_essentials.blend b/geometry_nodes_essentials.blend
---- a/geometry_nodes_essentials.blend
-+++ b/geometry_nodes_essentials.blend
-@@ -1 +10000 @@
+      R"(diff --git a/{} b/{}
+--- a/{}
++++ b/{}
+@@ -1 +100000 @@
 -Hello
 -sdfsa
 +Hella
-+sdfsd)");
++sdfsd)",
+      file_a,
+      file_a,
+      file_a,
+      file_a);
   return 0;
 }
