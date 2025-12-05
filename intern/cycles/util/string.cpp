9cf392ce2ac100ee6e5c41a5eda7cd2b694d93df
Sergey Sharybin <sergey@blender.org>
2025-12-05 18:58:55 +0100

    Code style and unit test

--- intern/cycles/util/string.cpp
+++ intern/cycles/util/string.cpp
@@ -216,8 +216,9 @@ string string_to_lower(const string &s)
 
 string string_remove_gpu_from_cpu_name(const string &s)
 {
-  if (s.find("AMD") == std::string::npos)
+  if (s.find("AMD") == std::string::npos) {
     return s;
+  }
 
   size_t pos = s.find("w/");
   if (pos == std::string::npos) {
