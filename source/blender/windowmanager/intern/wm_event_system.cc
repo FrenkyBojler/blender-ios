1c5d76471d46b5b60d4c43f92e23ee9e8b858356
maroulokefalos <maroulokefalos@noreply.localhost>
2025-12-07 08:27:11 +0100

    Fix: Renamed misspelled function in wm_event_system.cc

--- source/blender/windowmanager/intern/wm_event_system.cc
+++ source/blender/windowmanager/intern/wm_event_system.cc
@@ -4685,7 +4685,7 @@ wmEventHandler_Op *WM_event_add_modal_handler(bContext *C, wmOperator *op)
   return WM_event_add_modal_handler_ex(win, area, region, op);
 }
 
-void WM_event_remove_model_handler(ListBase *handlers, const wmOperator *op, const bool postpone)
+void WM_event_remove_modal_handler(ListBase *handlers, const wmOperator *op, const bool postpone)
 {
   LISTBASE_FOREACH (wmEventHandler *, handler_base, handlers) {
     if (handler_base->type == WM_HANDLER_TYPE_OP) {
@@ -4710,7 +4710,7 @@ void WM_event_remove_modal_handler_all(const wmOperator *op, const bool postpone
   Main *bmain = G_MAIN;
   wmWindowManager *wm = static_cast<wmWindowManager *>(bmain->wm.first);
   LISTBASE_FOREACH (wmWindow *, win, &wm->windows) {
-    WM_event_remove_model_handler(&win->modalhandlers, op, postpone);
+    WM_event_remove_modal_handler(&win->modalhandlers, op, postpone);
   }
 }
 
