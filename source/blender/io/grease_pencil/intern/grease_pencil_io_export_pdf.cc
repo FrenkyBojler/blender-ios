4c77e841248e1299fca08e953208941fa9e77100
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-12-07 17:54:45 -0800

    Fix #150381: Grease Pencil: PDF Exporter does not follow the DPI setting

--- source/blender/io/grease_pencil/intern/grease_pencil_io_export_pdf.cc
+++ source/blender/io/grease_pencil/intern/grease_pencil_io_export_pdf.cc
@@ -41,7 +41,7 @@ class PDFExporter : public GreasePencilExporter {
                                   const bke::greasepencil::Drawing &drawing);
 
   bool create_document();
-  bool add_page();
+  bool add_page(Scene &scene);
 
   void write_stroke_to_polyline(const float4x4 &transform,
                                 const Span<float3> positions,
@@ -66,7 +66,7 @@ bool PDFExporter::export_scene(Scene &scene, StringRefNull filepath)
       const int frame_number = scene.r.cfra;
 
       this->prepare_render_params(scene, frame_number);
-      this->add_page();
+      this->add_page(scene);
       this->export_grease_pencil_objects(frame_number);
       result = this->write_to_file(filepath);
       break;
@@ -90,7 +90,7 @@ bool PDFExporter::export_scene(Scene &scene, StringRefNull filepath)
           BKE_scene_graph_update_for_newframe(context_.depsgraph);
 
           this->prepare_render_params(scene, frame_number);
-          this->add_page();
+          this->add_page(scene);
           this->export_grease_pencil_objects(frame_number);
         }
 
@@ -175,7 +175,7 @@ bool PDFExporter::create_document()
   return true;
 }
 
-bool PDFExporter::add_page()
+bool PDFExporter::add_page(Scene &scene)
 {
   page_ = HPDF_AddPage(pdf_);
   if (!pdf_) {
@@ -183,13 +183,26 @@ bool PDFExporter::add_page()
     return false;
   }
 
+  /* Pixels per meter. */
+  double2 ppm;
+  BKE_scene_ppm_get(&scene.r, ppm);
+
+  /* Pixels per millimeter. */
+  double2 ppmm = ppm / 1000.0;
+  /* Pixels per inch. */
+  double2 ppi = ppmm * 25.4;
+
+  /* PDFs have a default ppi of 72 */
+  double2 scale_factor = 72.0 / ppi;
+  HPDF_Page_Concat(page_, scale_factor.x, 0.0f, 0.0f, scale_factor.y, 0.0f, 0.0f);
+
   if (camera_persmat_) {
-    HPDF_Page_SetWidth(page_, camera_rect_.size().x);
-    HPDF_Page_SetHeight(page_, camera_rect_.size().y);
+    HPDF_Page_SetWidth(page_, camera_rect_.size().x * scale_factor.x);
+    HPDF_Page_SetHeight(page_, camera_rect_.size().y * scale_factor.y);
   }
   else {
-    HPDF_Page_SetWidth(page_, screen_rect_.size().x);
-    HPDF_Page_SetHeight(page_, screen_rect_.size().y);
+    HPDF_Page_SetWidth(page_, screen_rect_.size().x * scale_factor.x);
+    HPDF_Page_SetHeight(page_, screen_rect_.size().y * scale_factor.y);
   }
 
   return true;
