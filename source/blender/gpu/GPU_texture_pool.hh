162b883f8361668f292b0203d285ece3e32fbda7
Mark van de Ruit <mark@blender.org>
2025-12-08 16:44:23 +0100

    Added lifetime flag to TexturePool.acquire_texture()

--- source/blender/gpu/GPU_texture_pool.hh
+++ source/blender/gpu/GPU_texture_pool.hh
@@ -16,6 +16,13 @@
 #include "BLI_vector.hh"
 #include "GPU_texture.hh"
 
+/* Explicit lifetime hints for texture pool resources. Transient textures must be handed back in 
+ * the same cycle, while persistent textures must be handed back before a full reset. */
+enum eTextureLifetime {
+  TEXTURE_LIFETIME_TRANSIENT,
+  TEXTURE_LIFETIME_PERSISTENT
+};
+
 namespace blender::gpu {
 
 class TexturePool {
