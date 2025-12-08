0c1afcc53abd60113ef301e1036f1c9678906cda
Sergey Sharybin <sergey@blender.org>
2025-12-08 11:04:11 +0100

    Fix: Crash rendering subdivision in EEVEE
    The issue has been introduced by !149681: the subdiv_face_offset is
    no longer guaranteed to be allocated using guarded allocator, and the
    drawing was using MEM_dupallocN() on it.
    
    Pass the subdiv_face_offset as a Span, to simplify making copies in
    a more C++-friendly way.

--- source/blender/blenkernel/intern/subdiv_foreach.cc
+++ source/blender/blenkernel/intern/subdiv_foreach.cc
@@ -1760,12 +1760,15 @@ bool foreach_subdiv_geometry(Subdiv *subdiv,
   ctx.foreach_context = context;
   subdiv_foreach_ctx_init(subdiv, &ctx);
   if (context->topology_info != nullptr) {
+    /* Skip the last sentinel element so that the callback "sees" offsets of actual faces. */
+    const Span<int> subdiv_face_offset_no_sentinel(ctx.subdiv_face_offset.data(),
+                                                   ctx.subdiv_face_offset.size() - 1);
     if (!context->topology_info(context,
                                 ctx.num_subdiv_vertices,
                                 ctx.num_subdiv_edges,
                                 ctx.num_subdiv_loops,
                                 ctx.num_subdiv_faces,
-                                ctx.subdiv_face_offset.data()))
+                                subdiv_face_offset_no_sentinel))
     {
       subdiv_foreach_ctx_free(&ctx);
       return false;
