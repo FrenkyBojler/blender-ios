e6d12962ee6c87ba1e3b3a963db7f6b3513595e8
Falk David <falk@blender.org>
2025-12-09 16:02:35 +0100

    Select linked

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -3928,8 +3928,9 @@ def km_grease_pencil_selection(params):
     items.extend([
         # Select All
         *_template_items_select_actions(params, "grease_pencil.select_all"),
+        # Select shape
+        ("grease_pencil.select_shape", {"type": 'L', "value": 'PRESS'}, None),
         # Select linked
-        ("grease_pencil.select_linked", {"type": 'L', "value": 'PRESS'}, None),
         ("grease_pencil.select_linked", {"type": 'L', "value": 'PRESS', "ctrl": True}, None),
         # Select more/less
         ("grease_pencil.select_more", {"type": 'NUMPAD_PLUS', "value": 'PRESS', "ctrl": True, "repeat": True}, None),
