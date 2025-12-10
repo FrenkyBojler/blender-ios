a3aa0730712899670350c75eef5c2a37c1473717
Miguel Pozo <pragma37@gmail.com>
2025-12-10 16:23:37 +0100

    fix hiprt

--- intern/cycles/kernel/device/hiprt/bvh.h
+++ intern/cycles/kernel/device/hiprt/bvh.h
@@ -69,6 +69,14 @@ ccl_device_intersect bool scene_intersect(KernelGlobals kg,
   return false;
 }
 
+ccl_device_intersect bool scene_intersect_shadow(KernelGlobals kg,
+                                                 const ccl_private Ray *ray,
+                                                 const uint visibility,
+                                                 ccl_private Intersection *isect)
+{
+  return scene_intersect(kg, ray, visibility, isect);
+}
+
 ccl_device_intersect bool scene_intersect_material_raycast(KernelGlobals kg,
                                                            const ccl_private Ray *ray,
                                                            const uint visibility,
