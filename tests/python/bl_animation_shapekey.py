914755771d04ad180a43b61cedf3c5daee2f82a0
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-12-11 15:13:49 +0100

    the fix

--- tests/python/bl_animation_shapekey.py
+++ tests/python/bl_animation_shapekey.py
@@ -90,9 +90,9 @@ class CurveShapekeyTest(unittest.TestCase):
         key_1.value = 0.0
         key_2.value = 0.0
 
+        bpy.ops.object.mode_set(mode='EDIT')
         depsgraph = bpy.context.evaluated_depsgraph_get()
         eval_curve = self.curve_ob.evaluated_get(depsgraph)
-        bpy.ops.object.mode_set(mode='EDIT')
         self.assertEqual(eval_curve.data.splines[0].bezier_points[0].co[0], 1)
         self.assertEqual(eval_curve.data.splines[0].bezier_points[0].co[1], 1)
         self.assertEqual(eval_curve.data.splines[0].bezier_points[0].co[2], 1)
