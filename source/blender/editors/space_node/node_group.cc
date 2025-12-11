4dadfd9e520b4c05258caf86f34f182dd506e931
Lukas Tönne <lukas@blender.org>
2025-12-11 12:02:47 +0100

    Make sure depsgraph is rebuilt after moving drivers.

--- source/blender/editors/space_node/node_group.cc
+++ source/blender/editors/space_node/node_group.cc
@@ -660,6 +660,9 @@ static wmOperatorStatus node_group_separate_exec(bContext *C, wmOperator *op)
 
   BKE_main_ensure_invariants(*CTX_data_main(C));
 
+  /* Moving drivers may require a depsgraph rebuild. */
+  DEG_relations_tag_update(bmain);
+
   return OPERATOR_FINISHED;
 }
 
