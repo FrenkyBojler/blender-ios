5e051504e395e94c4b2b5e7e6c28add76b8af69c
Jacques Lucke <jacques@blender.org>
2025-12-11 22:25:35 +0100

    make format

--- source/blender/nodes/geometry/nodes/node_geo_switch.cc
+++ source/blender/nodes/geometry/nodes/node_geo_switch.cc
@@ -236,22 +236,23 @@ static const bNodeSocket *node_internally_linked_input(const bNodeTree & /*tree*
 
 static void node_rna(StructRNA *srna)
 {
-  RNA_def_node_enum(
-      srna,
-      "input_type",
-      "Input Type",
-      "",
-      rna_enum_node_socket_data_type_items,
-      NOD_storage_enum_accessors(input_type),
-      SOCK_GEOMETRY,
-      [](bContext * /*C*/, PointerRNA *ptr, PropertyRNA * /*prop*/, bool *r_free) {
-        *r_free = true;
-        const bNodeTree &ntree = *id_cast<const bNodeTree *>(ptr->owner_id);
-        return enum_items_filter(
-            rna_enum_node_socket_data_type_items, [&](const EnumPropertyItem &item) -> bool {
-              return bke::node_tree_type_supports_socket_type_static(ntree.type, eNodeSocketDatatype( item.value));
-            });
-      });
+  RNA_def_node_enum(srna,
+                    "input_type",
+                    "Input Type",
+                    "",
+                    rna_enum_node_socket_data_type_items,
+                    NOD_storage_enum_accessors(input_type),
+                    SOCK_GEOMETRY,
+                    [](bContext * /*C*/, PointerRNA *ptr, PropertyRNA * /*prop*/, bool *r_free) {
+                      *r_free = true;
+                      const bNodeTree &ntree = *id_cast<const bNodeTree *>(ptr->owner_id);
+                      return enum_items_filter(
+                          rna_enum_node_socket_data_type_items,
+                          [&](const EnumPropertyItem &item) -> bool {
+                            return bke::node_tree_type_supports_socket_type_static(
+                                ntree.type, eNodeSocketDatatype(item.value));
+                          });
+                    });
 }
 
 static void register_node()
