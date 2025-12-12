fcf18c2eeba297df16d5c8de20cdcd0ee9fc246c
Clément Foucault <foucault.clem@gmail.com>
2025-12-12 19:09:01 +0100

    Remove METAL_CONSTRUCTOR_ macros

--- source/blender/gpu/shaders/common/gpu_shader_math_rotation_lib.glsl
+++ source/blender/gpu/shaders/common/gpu_shader_math_rotation_lib.glsl
@@ -45,7 +45,7 @@ Quaternion interpolate(Quaternion a, Quaternion b, float t)
   }
   float2 w = interpolate_dot_slerp(t, cosom);
   quat = w.x * quat + w.y * b.as_float4();
-  return Quaternion(UNPACK4(quat));
+  return Quaternion{UNPACK4(quat)};
 }
 
 /** \} */
