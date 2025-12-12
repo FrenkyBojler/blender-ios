c5df79bfa5b258e366f162dc6f1095ff6010caab
Pratik Borhade <pratikborhade302@gmail.com>
2025-12-12 12:40:08 +0530

    Count within single loop

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -351,20 +351,22 @@ static void stats_object_edit(Object *obedit, SceneStats *stats)
     using namespace blender;
     const Curves &curves_id = *static_cast<Curves *>(obedit->data);
     const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
-    const VArray<bool> selected_points = *curves.attributes().lookup_or_default<bool>(
+    const VArray<bool> selection_attribute = *curves.attributes().lookup_or_default<bool>(
         ".selection", bke::AttrDomain::Point, true);
 
     const OffsetIndices points_by_curve = curves.points_by_curve();
-    for (const int curve_i : curves.curves_range()) {
-      const IndexRange points = points_by_curve[curve_i];
-      stats->totcurvesel += std::all_of(points.begin(), points.end(), [&](const int64_t i) {
-        return selected_points[i] == true;
-      });
-    }
+    threading::parallel_for(curves.curves_range(), 256, [&](const IndexRange curves_range) {
+      for (const int curve_i : curves_range) {
+        const IndexRange points = points_by_curve[curve_i];
+        Span<bool> range = selection_attribute.get_internal_span().slice(points);
+        const int selected_points = std::count(range.begin(), range.end(), true);
+        stats->totpointsel += selected_points;
+        stats->totcurvesel += (selected_points == range.size());
+      }
+    });
 
     stats->totpoints += curves.points_num();
     stats->totcurves += curves.curves_num();
-    stats->totpointsel += array_utils::count_booleans(selected_points);
   }
   else if (obedit->type == OB_POINTCLOUD) {
     using namespace blender;
