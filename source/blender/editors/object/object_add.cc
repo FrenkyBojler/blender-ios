e16f2654241bd4503e67d2337059e223e17ef017
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-12-12 12:16:51 -0800

    Use: VArray

--- source/blender/editors/object/object_add.cc
+++ source/blender/editors/object/object_add.cc
@@ -3665,15 +3665,15 @@ static Object *convert_curves_to_mesh(Base &base, ObjectConversionInfo &info, Ba
 static void grease_penci_separate_shapes_from_materials(bke::greasepencil::Drawing *drawing)
 {
   bke::CurvesGeometry &curves = drawing->strokes_for_write();
-  bke::SpanAttributeWriter<int> materials =
-      curves.attributes_for_write().lookup_or_add_for_write_span<int>("material_index",
-                                                                      bke::AttrDomain::Curve);
+  VArray<int> materials = *curves.attributes().lookup_or_default(
+      "material_index", bke::AttrDomain::Curve, 0);
+
   bke::SpanAttributeWriter<int> shape_id =
       curves.attributes_for_write().lookup_or_add_for_write_span<int>("shape_id",
                                                                       bke::AttrDomain::Curve);
 
   for (const int curve_i : curves.curves_range()) {
-    shape_id.span[curve_i] = materials.span[curve_i] + 1;
+    shape_id.span[curve_i] = materials[curve_i] + 1;
   }
 
   drawing->tag_topology_changed();
