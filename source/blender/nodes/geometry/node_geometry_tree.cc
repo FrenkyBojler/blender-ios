b2fffbb2893d6c1970d8db7fcfff3d6065de0886
Jacques Lucke <jacques@blender.org>
2025-12-12 15:07:58 +0100

    fix

--- source/blender/nodes/geometry/node_geometry_tree.cc
+++ source/blender/nodes/geometry/node_geometry_tree.cc
@@ -136,7 +136,7 @@ static bool geometry_node_tree_socket_type_valid(blender::bke::bNodeTreeType * /
                SOCK_IMAGE,
                SOCK_MATERIAL,
                SOCK_MENU) ||
-          ELEM(socket_type->type, SOCK_BUNDLE, SOCK_CLOSURE));
+          ELEM(socket_type->type, SOCK_BUNDLE, SOCK_CLOSURE, SOCK_FONT));
 }
 
 void register_node_tree_type_geo()
