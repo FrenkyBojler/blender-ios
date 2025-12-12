476ffc5f93c6159a693f0a9fd883dd45c0c1ea40
Brecht Van Lommel <brecht@blender.org>
2025-12-12 14:36:52 +0100

    Also ignore transparency surface shaders with no users

--- intern/cycles/scene/integrator.cpp
+++ intern/cycles/scene/integrator.cpp
@@ -219,9 +219,12 @@ void Integrator::device_update(Device *device, DeviceScene *dscene, Scene *scene
    * to improve performance a bit. */
   kintegrator->transparent_shadows = false;
   for (Shader *shader : scene->shaders) {
+    if (shader->reference_count() == 0) {
+      continue;
+    }
     /* keep this in sync with SD_HAS_TRANSPARENT_SHADOW in shader.cpp */
     if ((shader->has_surface_transparent && shader->get_use_transparent_shadow()) ||
-        (shader->has_volume && shader->reference_count()))
+        shader->has_volume)
     {
       kintegrator->transparent_shadows = true;
       break;
