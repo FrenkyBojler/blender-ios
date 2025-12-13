faaad36b91f9e2cd92bacbb325ef86d5f07cd08b
Casey Bianco-Davis <caseycasey739@gmail.com>
2025-12-13 13:45:37 -0800

    Cleanup: Remove used include.

--- source/blender/editors/sculpt_paint/paint_sample_color.cc
+++ source/blender/editors/sculpt_paint/paint_sample_color.cc
@@ -20,7 +20,6 @@
 #include "BLI_math_color.h"
 #include "BLI_math_matrix.hh"
 #include "BLI_math_vector.hh"
-#include "BLI_math_vector_types.hh"
 #include "BLI_rect.h"
 #include "BLI_string_utf8.h"
 #include "BLI_utildefines.h"
