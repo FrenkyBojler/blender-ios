4047276fe4f08da1e38c57eda4b093238a5a5df4
Mark van de Ruit <mark@blender.org>
2025-12-14 17:16:35 +0100

    Fix: early clipping on xr

--- source/blender/draw/engines/overlay/shaders/overlay_grid_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_grid_vert.glsl
@@ -155,7 +155,7 @@ void main()
   }
 
   /* Clip/clamp; lines entirely outside the rectangle get discarded; others get brought
-   * inside the rectangle to avoid precision problems with large lines. 
+   * inside the rectangle to avoid precision problems with large lines.
    * Z-axis line ignores this step. */
   if (line.axis != 2) {
     bool line_outside_rect = all(lessThan(line.P, clip_min)) || all(greaterThan(line.P, clip_max));
