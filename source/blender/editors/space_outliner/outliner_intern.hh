631afc904f8dd10ed3d0d2ce870ee675aa1ed275
IREXTIA <developer@irextia.com>
2025-12-14 17:56:06 +0545

    Implement skip_flags option in outliner_find_id()

--- source/blender/editors/space_outliner/outliner_intern.hh
+++ source/blender/editors/space_outliner/outliner_intern.hh
@@ -625,7 +625,10 @@ TreeElement *outliner_find_parent_element(ListBase *lb,
 /**
  * Find tree-store that refers to given ID.
  */
-TreeElement *outliner_find_id(SpaceOutliner *space_outliner, ListBase *lb, const ID *id);
+TreeElement *outliner_find_id(SpaceOutliner *space_outliner,
+                              ListBase *lb,
+                              const ID *id,
+                              const short skip_flags);
 TreeElement *outliner_find_posechannel(ListBase *lb, const bPoseChannel *pchan);
 TreeElement *outliner_find_editbone(ListBase *lb, const EditBone *ebone);
 TreeElement *outliner_search_back_te(TreeElement *te, short idcode);
