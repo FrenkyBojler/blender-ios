d1d55e2b90d43df30938a9cd57f12502ad3bcda9
Hans Goudey <hans@blender.org>
2025-12-15 15:45:46 -0500

    Oops, forgot to remove code

--- source/blender/io/fbx/importer/fbx_import_mesh.cc
+++ source/blender/io/fbx/importer/fbx_import_mesh.cc
@@ -555,12 +555,6 @@ void import_meshes(Main &bmain,
 
       /* Skinned mesh. */
       if (fmesh->skin_deformers.count > 0) {
-        /* Add vertex groups to the object. */
-        VectorSet<std::string> bone_set = get_skin_bone_name_set(mapping, fmesh);
-        for (const std::string &name : bone_set) {
-          BKE_object_defgroup_add_name(obj, name.c_str());
-        }
-
         /* Add armature modifiers for each skin deformer. */
         for (const ufbx_skin_deformer *skin : fmesh->skin_deformers) {
           if (!is_skin_deformer_usable(fmesh, skin)) {
