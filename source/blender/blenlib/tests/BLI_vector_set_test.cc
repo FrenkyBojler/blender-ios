74865ac59a02bf2c9497aa92d1160b6c157e4c73
Bastien Montagne <bastien@blender.org>
2025-12-15 15:36:19 +0100

    BLI: VectorSet: Document specific order handling on element removal.
    Add to documentation that order of items is maintained on removal for
    all items added _before_ the removed one.
    
    Also adds a unittest ensuring this.
    
    NOTE: This behavior was already used by readfile code (the
    `Main::split_mains` VectorSet is always assumed to have the local Main
    as first item, even once some library Mains have been moved around in
    undo readfile code e.g.).

--- source/blender/blenlib/tests/BLI_vector_set_test.cc
+++ source/blender/blenlib/tests/BLI_vector_set_test.cc
@@ -201,6 +201,37 @@ TEST(vector_set, Remove)
   EXPECT_FALSE(set.contains(5));
 }
 
+TEST(vector_set, OrderPreservation)
+{
+  Vector<int> ref_vec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  VectorSet<int> set = {ref_vec.as_span().slice(0, 5)};
+  EXPECT_EQ(set.as_span(), ref_vec.as_span().slice(0, 5));
+
+  /* Only adding items always preserve to full order. */
+  set.add(ref_vec[5]);
+  set.add_multiple(ref_vec.as_span().slice(6, 4));
+  EXPECT_EQ(set.as_span(), ref_vec.as_span());
+
+  /* Removing items and adding others should not affect the order of items added before the oldest
+   * removed one. */
+  set.remove(ref_vec[8]);
+  EXPECT_NE(set.as_span(), ref_vec.as_span());
+  EXPECT_EQ(set.as_span().slice(0, 8), ref_vec.as_span().slice(0, 8));
+
+  set.remove(ref_vec[5]);
+  EXPECT_EQ(set.as_span().slice(0, 5), ref_vec.as_span().slice(0, 5));
+
+  set.add(ref_vec[5]);
+  set.add(ref_vec[8]);
+  EXPECT_NE(set.as_span(), ref_vec.as_span());
+  EXPECT_EQ(set.as_span().slice(0, 5), ref_vec.as_span().slice(0, 5));
+
+  for (int i = 10; i < 1000; i++) {
+    set.add(i);
+  }
+  EXPECT_EQ(set.as_span().slice(0, 5), ref_vec.as_span().slice(0, 5));
+}
+
 TEST(vector_set, SpanConstructorExceptions)
 {
   std::array<ExceptionThrower, 5> array = {1, 2, 3, 4, 5};
