7df78cf2f78f8576baace7a357bb099b3e4e7149
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-12-16 16:02:27 +0100

    formatting

--- source/blender/blenkernel/intern/key_test.cc
+++ source/blender/blenkernel/intern/key_test.cc
@@ -2,8 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BKE_key.hh"
 #include "BKE_idtype.hh"
+#include "BKE_key.hh"
 #include "BKE_main.hh"
 
 #include "testing/testing.h"
@@ -28,13 +28,11 @@ class ShapekeyTest : public testing::Test {
   }
 };
 
- namespace blender::bke::tests {
-    TEST(ShapekeyTest, evaluation){
+namespace blender::bke::tests {
+TEST(ShapekeyTest, evaluation) {}
 
-    }
-
-    TEST(ShapekeyTest, evaluate_different_element_count){
-        // BKE_key_evaluate_object_ex
-    }
- }
- 
+TEST(ShapekeyTest, evaluate_different_element_count)
+{
+  // BKE_key_evaluate_object_ex
+}
+}  // namespace blender::bke::tests
