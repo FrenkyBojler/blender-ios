aa3e80c9509ff03c9158d8bab51528ec141924d3
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-12-16 17:14:46 +0100

    wip

--- source/blender/blenkernel/intern/key_test.cc
+++ source/blender/blenkernel/intern/key_test.cc
@@ -16,6 +16,8 @@
 class ShapekeyTest : public testing::Test {
  public:
   Main *bmain = nullptr;
+  Object *ob = nullptr;
+  Mesh *mesh = nullptr;
 
   static void SetUpTestSuite()
   {
@@ -25,6 +27,20 @@ class ShapekeyTest : public testing::Test {
   void SetUp() override
   {
     bmain = BKE_main_new();
+    ob = BKE_object_add_only_object(bmain, OB_MESH, "Object");
+    mesh = BKE_mesh_add(bmain, "Test Mesh");
+    ob->data = mesh;
+
+    mesh->verts_num = 4;
+    blender::bke::mesh_ensure_required_data_layers(*mesh);
+    /* Shapekeys don't affect edges and faces so they can be ignored here. */
+    blender::Vector<blender::float3> verts = {
+        {0, 0, 0},
+        {1, 0, 0},
+        {1, 1, 0},
+        {0, 1, 0},
+    };
+    mesh->vert_positions_for_write().copy_from(verts);
   }
 
   void TearDown() override
@@ -34,25 +50,63 @@ class ShapekeyTest : public testing::Test {
 };
 
 namespace blender::bke::tests {
-TEST_F(ShapekeyTest, evaluation)
-{
-  Object *ob = BKE_object_add_only_object(bmain, OB_MESH, "Object");
-  Mesh *mesh = BKE_mesh_add(bmain, "Test Mesh");
-  mesh->verts_num = 4;
-  blender::bke::mesh_ensure_required_data_layers(*mesh);
-  /* Shapekeys don't affect edges and faces so they can be ignored here. */
-  Vector<float3> verts = {
-      {0, 0, 0},
-      {1, 0, 0},
-      {1, 1, 0},
-      {0, 1, 0},
-  };
-  mesh->vert_positions_for_write().copy_from(verts);
-  ob->data = mesh;
 
-  Key *base = BKE_key_add(bmain, &ob->id);
-  Key *key1 = BKE_key_add(bmain, &ob->id);
-  Key *key2 = BKE_key_add(bmain, &ob->id);
+static void compare_float3(const float3 &a, const float3 &b)
+{
+  ASSERT_EQ(a[0], b[0]);
+  ASSERT_EQ(a[1], b[1]);
+  ASSERT_EQ(a[2], b[2]);
+}
+
+TEST_F(ShapekeyTest, mesh_key_creation)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  ASSERT_EQ(key->from, &mesh->id);
+  /* Assignment to the mesh does not happen automatically by adding it. */
+  mesh->key = key;
+  ASSERT_EQ(BKE_key_from_object(ob), key);
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  /* This should be set automatically after adding the first key. */
+  ASSERT_EQ(key->refkey, base);
+  /* The elemsize stores how many bytes one element has (vertex in this case). */
+  ASSERT_EQ(key->elemsize, 12);
+  /* Adding the keyblock does not actually allocate any data for it. */
+  ASSERT_EQ(base->data, nullptr);
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+  ASSERT_NE(base->data, nullptr);
+  ASSERT_EQ(base->totelem, 4);
+  float3 *data = reinterpret_cast<float3 *>(base->data);
+  compare_float3(data[0], {0, 0, 0});
+  compare_float3(data[1], {1, 0, 0});
+  compare_float3(data[2], {1, 1, 0});
+  compare_float3(data[3], {0, 1, 0});
+}
+
+TEST_F(ShapekeyTest, mesh_key_evaluation_relative)
+{
+  Key *key = BKE_key_add(bmain, &mesh->id);
+  mesh->key = key;
+  key->type = KEY_RELATIVE;
+  KeyBlock *base = BKE_keyblock_add(key, "base");
+  BKE_keyblock_convert_from_mesh(mesh, key, base);
+
+  KeyBlock *key1 = BKE_keyblock_add(key, "one");
+  BKE_keyblock_convert_from_mesh(mesh, key, key1);
+  float3 *key1_data = reinterpret_cast<float3 *>(key1->data);
+  key1_data[0] = {1, 1, 1};
+
+  key1->curval = 1.0;
+  int totelem = 0;
+  float3 *ob_eval = reinterpret_cast<float3 *>(BKE_key_evaluate_object(ob, &totelem));
+  ASSERT_NE(ob_eval, nullptr);
+  ASSERT_EQ(totelem, mesh->verts_num);
+
+  compare_float3(ob_eval[0], {1, 1, 1});
+  compare_float3(ob_eval[1], {1, 0, 0});
+  compare_float3(ob_eval[2], {1, 1, 0});
+  compare_float3(ob_eval[3], {0, 1, 0});
+
+  MEM_freeN(ob_eval);
 }
 
 TEST_F(ShapekeyTest, evaluate_different_element_count)
