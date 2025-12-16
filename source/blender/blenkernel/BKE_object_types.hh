235e0cea495e359f20abaeb778842de7c3c495d8
Falk David <falk@blender.org>
2025-12-16 16:41:11 +0100

    Update comment to clarify case for instances

--- source/blender/blenkernel/BKE_object_types.hh
+++ source/blender/blenkernel/BKE_object_types.hh
@@ -88,7 +88,8 @@ struct ObjectRuntime {
 
   /**
    * Bitflag where each bit at an index corresponds to a `GeometryComponent::Type`. When a bit is
-   * set, the geometry type is contained within #geometry_set_eval.
+   * set, the geometry type is contained within #geometry_set_eval. This includes referenced
+   * geometry in instances.
    */
   uint16_t contained_geometry_types = 0;
 
