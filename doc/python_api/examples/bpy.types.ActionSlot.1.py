30fce63ab16fd35d763739aa7311ceb9fd1349e5
Nick Alberelli <nick@tinymedia.ca>
2025-12-16 13:09:00 -0500

    Update Example Code

--- doc/python_api/examples/bpy.types.ActionSlot.1.py
+++ doc/python_api/examples/bpy.types.ActionSlot.1.py
@@ -14,20 +14,22 @@ suzanne.keyframe_insert("location", index=0)
 # Action slots can be accessed like this:
 action = suzanne.animation_data.action
 for slot in action.slots:
-    print(f"Slot Identifier '{slot.identifier}'", end=" ")
-    print(f"with name '{slot.name_display}'", end=" ")
-    print(f"targets ID type '{slot.target_id_type}'")
-
+    print(f"Slot Identifier {slot.identifier!r} "
+          f"with name {slot.name_display!r} "
+          f"targets ID type {slot.target_id_type!r}")
+        
 
 """
-Maually Creating Action Slots
+Manually Creating Action Slots
 """
 # Actions creation.
 action = bpy.data.actions.new("SuzanneAction")
 
 # Creation of slots requires an ID type and a name.
 slot = action.slots.new(id_type='OBJECT', name="Suzanne")
-print(f"slot type={slot.target_id_type} name={slot.name_display} identifier={slot.identifier}")
+print(f"slot type={slot.target_id_type!r} "
+      f"name={slot.name_display!r} "
+      f"identifier={slot.identifier!r}")
 # Output:
 #   slot type=OBJECT name=Suzanne identifier=OBSuzanne
 
@@ -38,6 +40,7 @@ Explicitly Assigning Slots
 # If there are multiple slots on the Action, pick the first one that's compatible
 anim_data = suzanne.animation_data_create()
 anim_data.action = action
+assert anim_data.action_suitable_slots, "expecting at least one suitable slot"
 anim_data.action_slot = anim_data.action_suitable_slots[0]
 
 
@@ -45,7 +48,9 @@ anim_data.action_slot = anim_data.action_suitable_slots[0]
 Finding Slot Users
 """
 # Iterate through all actions in the Blender data.
+print("Action & slot users:")
 for action in bpy.data.actions:
     for slot in action.slots:
         # Return the data-blocks that are animated by this slot of this action
-        users = slot.users
+        users = slot.users()
+        print(f"{action.name:20} slot={slot.identifier:12s} users: {users}")
