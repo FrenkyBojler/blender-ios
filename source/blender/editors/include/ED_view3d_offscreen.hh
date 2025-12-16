8b33f6107d3ac8cc67489fca40546b454f7f2067
Christoph Lendenfeld <chris.lenden@gmail.com>
2025-12-16 10:01:16 +0100

    fix docstring

--- source/blender/editors/include/ED_view3d_offscreen.hh
+++ source/blender/editors/include/ED_view3d_offscreen.hh
@@ -73,7 +73,7 @@ void ED_view3d_draw_offscreen_simple(Depsgraph *depsgraph,
  * (avoids re-creating when doing multiple GL renders,
  * allows keeping track of state across frames).
  * \param use_camera_view_bounds if true and the viewport is looking through a camera, only render
- * the what the camera can see. In case the aspect ratio of the given `sizex` and `sizey` does not
+ * what the camera can see. In case the aspect ratio of the given `sizex` and `sizey` does not
  * match the render aspect ratio of the scene, the returned image will be expanded so the camera
  * view bounds are contained within it.
  */
