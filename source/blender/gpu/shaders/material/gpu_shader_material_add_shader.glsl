f9dad2f766ce0050c0d1dfcdea467560aab3e733
Clément Foucault <foucault.clem@gmail.com>
2025-12-17 19:21:15 +0100

    Fix closure inout converted to output

--- source/blender/gpu/shaders/material/gpu_shader_material_add_shader.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_add_shader.glsl
@@ -2,7 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-void node_add_shader(Closure &shader1, Closure &shader2, Closure &shader)
+void node_add_shader(Closure shader1, Closure shader2, Closure &shader)
 {
   shader = closure_add(shader1, shader2);
 }
