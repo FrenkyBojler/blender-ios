4a3c7b57a5e81d1b5a17633797e2fcdddeed5f83
Harley Acheson <harley.acheson@gmail.com>
2025-12-18 10:53:44 -0800

    Fix #151759: Sort Append Filter List Internationally
    This report notes that some Append "Advanced Filter" items are not
    ordered correctly among a list that seems to be almost completely
    alphabetical. Notably "Collections" and "Freestyle linestyles". These
    are wrong because their identifiers are quite different from their
    names ("collection" versus "group").  But if these are sorted by name
    then it needs to be the translated name. If we just do that then items
    with accents will be at the end of the list, like French "Éclairages".
    This PR sorts these using translated strings with an international
    sort based on the current output language.

--- scripts/startup/bl_ui/space_filebrowser.py
+++ scripts/startup/bl_ui/space_filebrowser.py
@@ -387,11 +387,19 @@ class FILEBROWSER_PT_advanced_filter(Panel):
             col.prop(params, "use_filter_asset_only")
 
             filter_id = params.filter_id
-            for identifier in dir(filter_id):
-                if identifier.startswith("filter_"):
-                    row = col.row()
-                    row.label(icon=filter_id.bl_rna.properties[identifier].icon)
-                    row.prop(filter_id, identifier, toggle=False)
+            filters = [x for x in filter_id.bl_rna.properties if x.identifier.startswith("filter_")]
+
+            import locale
+            current_locale = locale.getlocale(locale.LC_ALL)
+            language = context.preferences.view.language
+            locale.setlocale(locale.LC_ALL, language if language != 'DEFAULT' else '')
+
+            for i in sorted(filters, key=lambda x: locale.strxfrm(bpy.app.translations.pgettext_iface(x.name))):
+                row = col.row()
+                row.label(icon=i.icon)
+                row.prop(filter_id, i.identifier, toggle=False)
+
+            locale.setlocale(locale.LC_ALL, current_locale)
 
 
 def is_option_region_visible(context, space):
