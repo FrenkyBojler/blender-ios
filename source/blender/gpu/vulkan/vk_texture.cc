111089a5e3b4ee52f426fba21344e421c471c6e1
Jeroen Bakker <jeroen@blender.org>
2025-12-18 11:14:07 +0100

    Vulkan: Sync GPU_TEXTURE_USAGE_SHADER_READ meaning
    GPU_TEXTURE_USAGE_SHADER_READ documentation mentions that the option
    should add loading and sampling capabilities to the texture. The Vulkan
    backend implemented it by only adding loading capability. The sampling
    capability was added globally. This PR sync the implementation with the
    documentation.

--- source/blender/gpu/vulkan/vk_texture.cc
+++ source/blender/gpu/vulkan/vk_texture.cc
@@ -50,10 +50,9 @@ static VkImageUsageFlags to_vk_image_usage(const eGPUTextureUsage usage,
   const VKDevice &device = VKBackend::get().device;
   const VKExtensions &extensions = device.extensions_get();
 
-  VkImageUsageFlags result = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-                             VK_IMAGE_USAGE_SAMPLED_BIT;
+  VkImageUsageFlags result = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
   if (usage & GPU_TEXTURE_USAGE_SHADER_READ) {
-    result |= VK_IMAGE_USAGE_STORAGE_BIT;
+    result |= VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
   }
   if (usage & GPU_TEXTURE_USAGE_SHADER_WRITE) {
     result |= VK_IMAGE_USAGE_STORAGE_BIT;
