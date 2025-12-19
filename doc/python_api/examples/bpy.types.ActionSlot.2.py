53557ea8d682339d54b1543e4a457f447342d76c
Nick Alberelli <nick@tinymedia.ca>
2025-12-19 11:11:15 -0500

    Fix formatting

--- doc/python_api/examples/bpy.types.ActionSlot.2.py
+++ doc/python_api/examples/bpy.types.ActionSlot.2.py
@@ -3,8 +3,8 @@ Manually Create an Action Slot
 ++++++++++++++++++++++++++++++
 If required you can also manually create Action Slots on an Action. Note the ``target_id_type``
 that matches the data-block type. Identifiers start with a prefix based on the ID type, 
-e.g. "OB" for objects, followed by the name. There can be identifiers like `OBSuzanne`
-and `MESuzanne` and the name (`Suzanne`) can be shared between them. This is intentional, 
+e.g. "OB" for objects, followed by the name. There can be identifiers like ``OBSuzanne``
+and ``MESuzanne`` and the name (``Suzanne``) can be shared between them. This is intentional, 
 so that the slots and the datablocks can have the same name.
 
 """
