97aab7c7a3ff9a899ba841ca7c3296e9ec693d95
Nick Alberelli <nick@tinymedia.ca>
2025-12-19 12:28:56 -0500

    Add action slot compatibility explainer

--- doc/python_api/examples/bpy.types.ActionSlot.3.py
+++ doc/python_api/examples/bpy.types.ActionSlot.3.py
@@ -1,6 +1,7 @@
 """
 Explicitly Assigning Action Slots
 +++++++++++++++++++++++++++++++++
+An action slot is compatible with a data-block if the slot's ``target_id_type`` matches the data-block's type.
 If there are multiple slots on the Action, and you want to just pick the first one that's
 compatible, use the following code. ``anim_data.action_suitable_slots`` can be used `after` the
 Action has been assigned; it is a list of action slots of that Action, but only the ones that
