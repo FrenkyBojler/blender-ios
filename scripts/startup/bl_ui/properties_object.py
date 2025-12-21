b23ad16eb913a02b78eb0b8ed88994c02d5e8eab
Treata11 <treata11@yahoo.com>
2025-12-21 16:33:17 +0330

    UI: Minor visual improvements to LODs `UIList`

--- scripts/startup/bl_ui/properties_object.py
+++ scripts/startup/bl_ui/properties_object.py
@@ -626,16 +626,55 @@ class OBJECT_PT_custom_props(ObjectButtonsPanel, PropertyPanel, Panel):
     _property_type = bpy.types.Object
 
 
+# class OBJECT_UL_lod_items(UIList):
+#     def draw_item(self, context, layout, data, item, icon, 
+#                   active_data, active_propname, index):
+#         # layout.use_property_split = False # !: If False, it would terminate the leadingSpacer for LOD, but converges the Dist text into the UI panel for Float assignation
+#         layout.use_property_decorate = False # This one simply shifts the whole panels & Texts to the Right.. Making the leadingSpacer bigger and the trailing smaller than before... Not idea
+#         # The goal is to minimize the leadingSpacer ...
+    
+#         lod = item
+#         col = layout.column(align=True)
+#         col.prop(lod, "target", text=f"LOD {index + 1}")
+#         # row.label(text=f"LOD {index + 1}", icon='MESH_CUBE')
+#         col.prop(lod, "distance", text="Dist")
+        
+#         # if index < len(data.lod_items) - 1:
+#         #     layout.separator() # FIXME: This is not providing vertical spacing... but rather horizontal
+#         #     # layout.divider()
+
+
 class OBJECT_UL_lod_items(UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+    def draw_item(self, context, layout, data, item, icon,
+                  active_data, active_propname, index):
+        
+
+        layout.use_property_decorate = False
+
+
         lod = item
-        col = layout.column(align=True)
-        col.prop(lod, "target", text=f"LOD {index}")
-        col.prop(lod, "distance", text="Dist")
+
+        row = layout.row(align=True)
+
+        # Left: compact label
+        label_col = row.column(align=True)
+        label_col.scale_x = 2/3
+        label_col.label(text=f"LOD {index + 1}")
+
+        label_col.label(text="Distance")
+        label_col.scale_x = 2/3
+
+
+        # Right: properties packed tightly
+        prop_col = row.column(align=True)
+        prop_col.prop(lod, "target", text="")
+        # prop_col.prop(lod, "distance", text="Dist")
+        prop_col.prop(lod, "distance", text="")
+                
 
 
 class OBJECT_PT_distance_lod(ObjectButtonsPanel, Panel):
-    bl_label = "Distance LOD"
+    bl_label = "Distance LOD" # TODO(Tri): Change the name to LOD?
     bl_idname = "OBJECT_PT_distance_lod"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
