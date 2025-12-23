blender/blender

commit 51d63b2e69c312798c4799a0279201afd769434d

Fix: VSE: invisible timeline gizmos
When adding gizmos to the Sequencer Timeline they got added but did not
get drawn properly so that they where invisible. This happened because
in the code above `UI_view2d_view_ortho` is used and
`UI_view2d_view_restore` was not called before executing the
`draw_timeline_gizmos` function. The Problem is simply fixed by adding
`UI_view2d_view_restore`.

Pull Request: https://projects.blender.org/blender/blender/pulls/148906

source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -1889,6 +1889,7 @@ void draw_timeline_seq(const bContext *C, ARegion *region)
   if (ctx.scene) {
     ANIM_draw_previewrange(ctx.scene, ctx.v2d, 1);
   }
+  UI_view2d_view_restore(C);
   draw_timeline_gizmos(&ctx);
   draw_timeline_post_view_callbacks(&ctx);
   if (ctx.scene) {

FrenkyBojler/blender-ios

commit 818b5c85cee7853257f2acce57251d2a51ead858

Fix #151799: VSE: Snap line not drawn
This is an alternative fix to !152008.

Drawing was broken in 51d63b2e69, which prevented snap code in
`draw_timeline_post_view_callbacks` from using `view2d_view_ortho`.

This patch groups together ortho drawing code in the timeline. Draw
order is kept the same, except now gizmos draw on top of snap lines
(which makes sense, they can have precedence). Also:

- Move the `view2d_view_ortho` call out of `draw_seq_timeline_channels`
  to make it clearer that it was indirectly affecting all of the
  following calls in `draw_timeline_seq`
- Add a line to restore this ortho in `draw_timeline_markers` since it
  uses `orthoSpecial`

source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -1186,7 +1186,6 @@ static void seq_prefetch_wm_notify(const bContext *C, Scene *scene)
 static void draw_seq_timeline_channels(const TimelineDrawContext &ctx)
 {
   View2D *v2d = ctx.v2d;
-  ui::view2d_view_ortho(v2d);
 
   GPU_blend(GPU_BLEND_ALPHA);
   uchar4 color;
@@ -1790,6 +1789,7 @@ static void draw_timeline_markers(const TimelineDrawContext &ctx)
 
   ui::view2d_view_orthoSpecial(ctx.region, ctx.v2d, true);
   ED_markers_draw(ctx.C, DRAW_MARKERS_MARGIN);
+  ui::view2d_view_ortho(ctx.v2d);
 }
 
 static void draw_timeline_gizmos(const TimelineDrawContext &ctx)
@@ -1825,18 +1825,22 @@ void draw_timeline_seq(const bContext *C, const ARegion *region)
 
   draw_timeline_pre_view_callbacks(ctx);
   ui::theme::frame_buffer_clear(TH_BACK);
+
+  ui::view2d_view_ortho(ctx.v2d);
   draw_seq_timeline_channels(ctx);
   draw_timeline_grid(ctx);
   draw_timeline_sfra_efra(ctx);
   draw_seq_strips(ctx, strips_batch);
   draw_timeline_markers(ctx);
-  ui::view2d_view_ortho(ctx.v2d);
   if (ctx.scene) {
     ANIM_draw_previewrange(ctx.scene, ctx.v2d, 1);
   }
-  ui::view2d_view_restore(C);
-  draw_timeline_gizmos(ctx);
   draw_timeline_post_view_callbacks(ctx);
+  ui::view2d_view_restore(C);
+
+  draw_timeline_gizmos(ctx);
+
+  ui::view2d_view_ortho(ctx.v2d);
   if (ctx.scene) {
     const int fps = round_db_to_int(ctx.scene->frames_per_second());
     ED_time_scrub_draw(region, ctx.scene, !(ctx.sseq->flag & SEQ_DRAWFRAMES), true, fps);