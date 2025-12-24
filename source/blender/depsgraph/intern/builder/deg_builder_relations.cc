a366f8560716bb4bb1c611359655019d4c031373
Bastien Montagne <bastien@blender.org>
2025-12-24 16:18:36 +0100

    Core: Depsgraph: Add sanity checks about building process.
    In debug builds, add checks for:
    - All built IDs have both nodes and relations steps propcessed.
    - All IDs in depsgraph are built.
    - No built ID is unknown to depsgraph.
    
    Also ensure that the unused 'RECALC_PROPVISION` ID reclac flags are not
    set when using the legacy `0` tagging.
    
    Co-authored-by: Sergey <sergey@blender.org>

--- source/blender/depsgraph/intern/builder/deg_builder_relations.cc
+++ source/blender/depsgraph/intern/builder/deg_builder_relations.cc
@@ -3760,4 +3760,9 @@ void DepsgraphRelationBuilder::constraint_walk(bConstraint * /*con*/,
   data->builder->build_id(id);
 }
 
+Set<const ID *> DepsgraphRelationBuilder::get_built_ids() const
+{
+  return built_map_.get_ids();
+}
+
 }  // namespace blender::deg
