689290a191f232e51c6871402d25592cd6d1a215
ReaalSATYAM <satyamnaithani14@gmail.com>
2025-12-28 00:41:11 +0530

    WORKING: fixing alt + shift

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -5332,21 +5332,15 @@ def km_sculpt(params):
              ("relative_asset_identifier", "brushes/essentials_brushes-mesh_sculpt.blend/Brush/Mask"),
              ("use_toggle", True)
          ]}),
-         # Mask brush strokes
-        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True},
-         {"properties": [
-            ("mode", 'MASK')
-         ]}),
+        # Mask toggle (Alt+LMB)
+        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True}, None),
 
-        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True, "shift": True},
-         {"properties": [
-            ("mode", 'MASK')
-         ]}),
+        # Smooth mask (Alt+Shift+LMB)
+        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True, "shift": True}, None),
 
-        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True, "ctrl": True},
-         {"properties": [
-            ("mode", 'MASK')
-         ]}),
+        # Invert mask (Alt+Ctrl+LMB)
+        ("sculpt.brush_stroke", {"type": 'LEFTMOUSE', "value": 'PRESS', "alt": True, "ctrl": True}, None),
+
         *_template_asset_shelf_popup("VIEW3D_AST_brush_sculpt", params.spacebar_action),
     ])
 
