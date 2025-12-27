eb2bdbe18f5c6af3dd02ca012526ad3f46268bbd
ReaalSATYAM <satyamnaithani14@gmail.com>
2025-12-27 14:17:51 +0530

    Directly changing cache->alt_mask flag inside sculpt_brush_stroke_invoke in sculpt.cc

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -5910,34 +5910,44 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
 
   PaintMode mode = BKE_paintmode_get_active_from_context(C);
 
-  /*For ALT-> Mask toggle*/
+  /* For ALT / SHIFT / CTRL toggles in Sculpt mode */
   if (mode == PaintMode::Sculpt) {
-      int stroke_mode = BRUSH_STROKE_NORMAL;
+    SculptSession &ss = *ob.sculpt;
+    StrokeCache *cache = ss.cache;
 
-      if (event->modifier & KM_ALT) {
-          stroke_mode = BRUSH_STROKE_MASK;
-          printf("[DEBUG] ALT detected: stroke_mode = MASK\n");
-          CLOG_INFO(&LOG, 0, "ALT pressed, stroke_mode set to MASK");
-      }
-      else if (event->modifier & KM_SHIFT) {
-          stroke_mode = BRUSH_STROKE_SMOOTH;
-          printf("[DEBUG] SHIFT detected: stroke_mode = SMOOTH\n");
-          CLOG_INFO(&LOG, 0, "SHIFT pressed, stroke_mode set to SMOOTH");
-      }
-      else if (event->modifier & KM_CTRL) {
-          stroke_mode = BRUSH_STROKE_INVERT;
-          printf("[DEBUG] CTRL detected: stroke_mode = INVERT\n");
-          CLOG_INFO(&LOG, 0, "CTRL pressed, stroke_mode set to INVERT");
-      }
-      else {
-          printf("[DEBUG] No modifier: stroke_mode = NORMAL\n");
-          CLOG_INFO(&LOG, 0, "No modifier pressed, stroke_mode = NORMAL");
-      }
-
-      RNA_enum_set(op->ptr, "stroke_mode", stroke_mode);
-      printf("[DEBUG] RNA_enum_set called, stroke_mode=%d\n", stroke_mode);
+    // ALT -> Mask
+    if (event->modifier & KM_ALT) {
+        printf("[DEBUG] ALT detected: stroke_mode = MASK\n");
+        CLOG_INFO(&LOG, 0, "ALT pressed, stroke_mode set to MASK");
+        cache->alt_mask = true;
+        cache->alt_smooth = false;
+        cache->invert = false;
+    } 
+    // SHIFT -> Smooth
+    else if (event->modifier & KM_SHIFT) {
+        printf("[DEBUG] SHIFT detected: stroke_mode = SMOOTH\n");
+        CLOG_INFO(&LOG, 0, "SHIFT pressed, stroke_mode set to SMOOTH");
+        cache->alt_mask = false;
+        cache->alt_smooth = true;
+        cache->invert = false;
+    } 
+    // CTRL -> Invert
+    else if (event->modifier & KM_CTRL) {
+        printf("[DEBUG] CTRL detected: stroke_mode = INVERT\n");
+        CLOG_INFO(&LOG, 0, "CTRL pressed, stroke_mode set to INVERT");
+        cache->alt_mask = false;
+        cache->alt_smooth = false;
+        cache->invert = true;
+    } 
+    // No modifier -> Normal
+    else {
+        printf("[DEBUG] No modifier detected: stroke_mode = NORMAL\n");
+        CLOG_INFO(&LOG, 0, "No modifier pressed, stroke_mode set to NORMAL");
+        cache->alt_mask = false;
+        cache->alt_smooth = false;
+        cache->invert = false;
+    }
   }
-
 
   /* For tablet rotation. */
   ignore_background_click = RNA_boolean_get(op->ptr, "ignore_background_click");
