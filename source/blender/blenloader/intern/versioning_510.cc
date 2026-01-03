baa772ed1f723853adf44e967794b56eb4a5bca0
Treata11 <treata11@yahoo.com>
2026-01-03 22:01:18 +0330

    versioning: add scene-lod props

--- source/blender/blenloader/intern/versioning_510.cc
+++ source/blender/blenloader/intern/versioning_510.cc
@@ -281,14 +281,19 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
    */
 
   /**
-  TODO(Treata): - Describe / move to _520
+  TODO(Tri): - Describe / move to _520
   */
-  // if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 1)) {
-  //   LISTBASE_FOREACH (Object *, ob, &bmain->objects) {
-  //     ob->lod_items = NULL;
-  //     ob->lod_items_num = 0;
-  //     // ob->cull_distance = 0.0f;
-  //     ob->lod_items_index = 0;
-  //   }
-  // }
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 0)) { // Change to 502
+    LISTBASE_FOREACH (Object *, ob, &bmain->objects) {
+    /* Ensure active index is valid */
+    if (ob->act_lod < 0) {
+      ob->act_lod = 0;
+    }
+    }
+
+    LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+      scene->lod.use_viewport = true;
+      scene->lod.use_render = false;
+    }
+  }
 }
