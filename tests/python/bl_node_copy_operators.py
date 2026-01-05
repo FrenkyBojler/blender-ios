7b1a317373075e36c2efc9a908b639f4edd73263
Lukas Tönne <lukas@blender.org>
2026-01-05 14:49:51 +0100

    Reenable test.

--- tests/python/bl_node_copy_operators.py
+++ tests/python/bl_node_copy_operators.py
@@ -435,17 +435,17 @@ def generate_test_data():
     for test_case in test_cases(tree_ungroup):
         execute_ungroup(test_case, tree_ungroup)
 
-    # mod_group_separate_copy = ob.modifiers["ExpectedSeparateCopy"]
-    # # Use result of grouping as starting point for separating.
-    # tree_group_separate_copy = copy_tree(tree_make_group, mod_group_separate_copy)
-    # for test_case in test_cases(tree_group_separate_copy):
-    #     execute_group_separate('COPY', test_case, tree_group_separate_copy)
+    mod_group_separate_copy = ob.modifiers["ExpectedSeparateCopy"]
+    # Use result of grouping as starting point for separating.
+    tree_group_separate_copy = copy_tree(tree_make_group, mod_group_separate_copy)
+    for test_case in test_cases(tree_group_separate_copy):
+        execute_group_separate('COPY', test_case, tree_group_separate_copy)
 
-    # mod_group_separate_move = ob.modifiers["ExpectedSeparateMove"]
-    # # Use result of grouping as starting point for separating.
-    # tree_group_separate_move = copy_tree(tree_make_group, mod_group_separate_move)
-    # for test_case in test_cases(tree_group_separate_move):
-    #     execute_group_separate('MOVE', test_case, tree_group_separate_move)
+    mod_group_separate_move = ob.modifiers["ExpectedSeparateMove"]
+    # Use result of grouping as starting point for separating.
+    tree_group_separate_move = copy_tree(tree_make_group, mod_group_separate_move)
+    for test_case in test_cases(tree_group_separate_move):
+        execute_group_separate('MOVE', test_case, tree_group_separate_move)
 
     save_test_file()
 
