2b1a514dbe25859ce35b44f71fca7f8e333df3ad
илья _ <modormoder@gmail.com>
2026-01-06 01:29:29 +0300

    progress

--- source/blender/blenlib/BLI_assume.hh
+++ source/blender/blenlib/BLI_assume.hh
@@ -17,10 +17,10 @@
 /* _BLI_ASSUME */
 #  if defined(__GNUC__)
 #    define _BLI_ASSUME(a) assume(a)
+#  elif defined(__clang__)
+#    define _BLI_ASSUME(a) __builtin_assume( [&]() __attribute__((pure)) { return bool(a); }())
 #  elif defined(_MSC_VER)
 #    define _BLI_ASSUME(a) __assume(a)
-#  elif defined(__clang__)
-#    define _BLI_ASSUME(a) __builtin_assume(a)
 #  else
 #    define _BLI_ASSUME(a) ((void)0)
 #  endif
