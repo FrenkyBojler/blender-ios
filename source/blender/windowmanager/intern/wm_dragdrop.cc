blender/blender

commit e69c647a4365c87dd02e72216207ddc3ac7bae98

Fix #150227: Crash when dropping asset after deleting it from disk
Disable dropping when the file cannot be found on disk. Also show a
"Asset not found" hint while dragging.

There are still other cases that can crash, see blender/blender!151000.

Pull Request: https://projects.blender.org/blender/blender/pulls/150999

source/blender/windowmanager/intern/wm_dragdrop.cc
@@ -21,6 +21,7 @@
 
 #include "BLT_translation.hh"
 
+#include "BLI_fileops.h"
 #include "BLI_listbase.h"
 #include "BLI_math_color.h"
 #include "BLI_path_utils.hh"
@@ -60,6 +61,7 @@
 
 #include "WM_api.hh"
 #include "WM_types.hh"
+#include "wm.hh"
 #include "wm_event_system.hh"
 #include "wm_window.hh"
 
@@ -505,6 +507,12 @@ static wmDropBox *dropbox_active(bContext *C,
 /* Return active operator tooltip/name when mouse is in box. */
 static wmDropBox *wm_dropbox_active(bContext *C, wmDrag *drag, const wmEvent *event)
 {
+  /* Always do this check for asset dragging (as if it was in every poll). */
+  if (!wm_drag_asset_path_exists(drag).value_or(true)) {
+    drag->drop_state.disabled_info = RPT_("Asset not found");
+    return nullptr;
+  }
+
   wmWindow *win = CTX_wm_window(C);
   bScreen *screen = WM_window_get_active_screen(win);
   ScrArea *area = BKE_screen_find_area_xy(screen, SPACE_TYPE_ANY, event->xy);
@@ -849,6 +857,28 @@ const ListBase *WM_drag_asset_list_get(const wmDrag *drag)
   }
 
   return &drag->asset_items;
+}
+
+std::optional<bool> wm_drag_asset_path_exists(const wmDrag *drag)
+{
+  if (!ELEM(drag->type, WM_DRAG_ASSET, WM_DRAG_ASSET_LIST)) {
+    return {};
+  }
+
+  if (const wmDragAsset *asset_drag = WM_drag_get_asset_data(drag, 0)) {
+    return BLI_is_file(asset_drag->asset->full_library_path().c_str());
+  }
+
+  if (const ListBase *asset_drags = WM_drag_asset_list_get(drag)) {
+    LISTBASE_FOREACH (wmDragAssetListItem *, asset_item, asset_drags) {
+      if (!asset_item->is_external ||
+          BLI_is_file(asset_item->asset_data.external_info->asset->full_library_path().c_str()))
+      {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 wmDragPath *WM_drag_create_path_data(blender::Span<const char *> paths)

source/blender/windowmanager/intern/wm_event_system.cc
@@ -3512,6 +3512,10 @@ static eHandlerActionFlag wm_handlers_do_intern(bContext *C,
             if (event->custom == EVT_DATA_DRAGDROP) {
               ListBase *lb = (ListBase *)event->customdata;
               LISTBASE_FOREACH_MUTABLE (wmDrag *, drag, lb) {
+                if (!wm_drag_asset_path_exists(drag).value_or(true)) {
+                  continue;
+                }
+
                 if (drop->poll(C, drag, event)) {
                   wm_drop_prepare(C, drag, drop);
 

source/blender/windowmanager/wm.hh
@@ -12,6 +12,7 @@
 
 #include "BLI_math_vector_types.hh"
 
+struct wmDrag;
 struct wmOperator;
 struct wmTimer;
 struct wmWindow;
@@ -69,6 +70,13 @@ void wm_operatortype_free();
  */
 void wm_window_keymap(wmKeyConfig *keyconf);
 void wm_operatortypes_register();
+
+/**
+ * Check if any of the dragged assets points to an existing file on disk.
+ *
+ * Checks the file system, so don't call too often.
+ */
+std::optional<bool> wm_drag_asset_path_exists(const wmDrag *drag);
 
 /* `wm_gesture.cc` */
 

FrenkyBojler/blender-ios

commit fd4f235cb403522c50b45bf63b21c79a12684d14

Fix #152458: Dragging data-block icons will show "Asset not found"
Caused by e69c647a4365

Above commit introduced a check (`wm_drag_asset_path_exists`) that
ignored one special case:
- dragging a `WM_DRAG_ID` button would start another, second drag in
`button_drag_start` of type `WM_DRAG_ASSET_LIST` (check the code comment
at the bottom)
- not totally sure why it is necessary, but decided to better not touch
that (could be the culprit already though)...
- ... because we always end up with an empty `wmDrag.asset_items` list
(`WM_drag_add_asset_list_item` is never called for `WM_DRAG_ID` it
seems)
- with an empty `wmDrag.asset_items` list, `wm_drag_asset_path_exists`
returns false atm, should return a nullopt though

So (at least) two ways to fix (this PR goes with the second option):
- do not start another, second drag in `button_drag_start` for
`WM_DRAG_ID`
- improve the behavior of `wm_drag_asset_path_exists` like described
above

The code comment mentioned above:
```
  /* Special feature for assets: We add another drag item that supports
multiple assets. It
   * gets the assets from context. */
```

source/blender/windowmanager/intern/wm_dragdrop.cc
@@ -873,6 +873,15 @@ std::optional<bool> wm_drag_asset_path_exists(const wmDrag *drag)
   }
 
   if (const ListBaseT<wmDragAssetListItem> *asset_drags = WM_drag_asset_list_get(drag)) {
+
+    if (BLI_listbase_is_empty(asset_drags)) {
+      /* #button_drag_start() will start a drag of type WM_DRAG_ASSET_LIST for dragging a
+       * WM_DRAG_ID button (so we do not early out above in this case). Its #asset_items list will
+       * always be empty though, so avoid returning false at the end of this function, treat this
+       * special case more like not being a "real" WM_DRAG_ASSET_LIST. */
+      return {};
+    }
+
     for (wmDragAssetListItem &asset_item : *asset_drags) {
       if (!asset_item.is_external ||
           BLI_is_file(asset_item.asset_data.external_info->asset->full_library_path().c_str()))