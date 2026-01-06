4d911e5789b5379fda5a9d7d1781632899196b55
Pratik Borhade <pratikborhade302@gmail.com>
2026-01-06 15:48:29 +0530

    Remove active/seleected function call

--- source/blender/editors/interface/views/abstract_view_item.cc
+++ source/blender/editors/interface/views/abstract_view_item.cc
@@ -472,10 +472,6 @@ bool view_item_drag_start(bContext &C, AbstractViewItem &item)
   }
   drag_controller->on_drag_start(C);
 
-  /* Make sure the view item is highlighted as active when dragging from it. This is useful user
-   * feedback. */
-  item.set_selected(true);
-
   return true;
 }
 
