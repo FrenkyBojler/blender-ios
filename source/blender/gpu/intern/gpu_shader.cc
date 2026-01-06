6dbffd57e36c250ce1a1a515ea3017cdc2461a3e
Clément Foucault <foucault.clem@gmail.com>
2026-01-06 16:51:38 +0100

    Avoid more comment

--- source/blender/gpu/intern/gpu_shader.cc
+++ source/blender/gpu/intern/gpu_shader.cc
@@ -136,7 +136,6 @@ static void standard_defines(Vector<StringRefNull> &sources)
   /* Version and specialization constants needs to be first.
    * Exact values will be added by implementation. */
   sources.append("version");
-  sources.append("/* specialization_constants */");
   /* Define to identify code usage in shading language. */
   sources.append("#define GPU_SHADER\n");
   /* some useful defines to detect GPU type */
