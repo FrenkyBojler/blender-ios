5c7726c01cb3691bb3e03d057010381385f03255
Sybren A. Stüvel <sybren@blender.org>
2026-01-06 13:02:17 +0100

    Feedback Campbell: Colon after `\param`

--- source/blender/python/BPY_extern_run.hh
+++ source/blender/python/BPY_extern_run.hh
@@ -104,9 +104,9 @@ bool BPY_run_string_eval(bContext *C, const char *imports[], const char *expr);
  * \param C: Optional context (may be null),
  *  used for `bpy.context` and reporting errors to `CTX_wm_reports(C)`.
  *
- * \param script The Python script to run, can be multiple lines.
+ * \param script: The Python script to run, can be multiple lines.
  *
- * \param locals group property with string keys, defining the script's local variables.
+ * \param locals: group property with string keys, defining the script's local variables.
  */
 bool BPY_run_string_exec_with_locals(bContext *C,
                                      const blender::StringRefNull script,
@@ -122,9 +122,9 @@ bool BPY_run_string_exec_with_locals(bContext *C,
  * \param C: Optional context (may be null),
  *  used for `bpy.context` and reporting errors to `CTX_wm_reports(C)`.
  *
- * \param script The Python script to run, can be multiple lines.
+ * \param script: The Python script to run, can be multiple lines.
  *
- * \param locals group property with string keys, defining the script's local variables.
+ * \param locals: group property with string keys, defining the script's local variables.
  *
  * \returns the value of `_result` in the script. If `_result` was not set, std::noopt is returned.
  * `_result=None` will return in a `nullptr` value of the `std::optional`.
