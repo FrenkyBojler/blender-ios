e9ae2814b4530f53ef1ff9f7f5c24912f25885c2
Clément Foucault <foucault.clem@gmail.com>
2026-01-07 14:56:10 +0100

    Fix missing GPU_SHADER macro in OpenGL!!?

--- source/blender/gpu/intern/gpu_shader.cc
+++ source/blender/gpu/intern/gpu_shader.cc
@@ -136,6 +136,8 @@ static void standard_defines(Vector<StringRefNull> &sources)
   /* Version and specialization constants needs to be first.
    * Exact values will be added by implementation. */
   sources.append("version");
+  /* Specialization constants will be inserted here. */
+  sources.append("\n");
   /* Define to identify code usage in shading language. */
   sources.append("#define GPU_SHADER\n");
   /* some useful defines to detect GPU type */
