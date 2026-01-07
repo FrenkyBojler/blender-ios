4492b4c36d6f8b992caeb76957935f12a6fe1ae1
Mark van de Ruit <mark@blender.org>
2026-01-07 11:56:46 +0100

    Fix: integer texture views don't use filtering (@fclem)

--- source/blender/gpu/intern/gpu_texture.cc
+++ source/blender/gpu/intern/gpu_texture.cc
@@ -481,6 +481,11 @@ gpu::Texture *GPU_texture_create_view(const char *name,
                   layer_len,
                   cube_as_array,
                   use_stencil);
+  if (GPU_texture_has_integer_format(view)) {
+    view->sampler_state.set_filtering_flag_from_test(GPU_SAMPLER_FILTERING_LINEAR, false);
+    view->sampler_state.set_filtering_flag_from_test(GPU_SAMPLER_FILTERING_MIPMAP, false);
+    view->sampler_state.set_filtering_flag_from_test(GPU_SAMPLER_FILTERING_ANISOTROPIC, false);
+  }
   return view;
 }
 
