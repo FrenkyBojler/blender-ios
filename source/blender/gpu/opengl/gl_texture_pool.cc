7caf57df4a16f2aa55445b67d1f09b149f516a47
Mark van de Ruit <mark@blender.org>
2026-01-07 12:00:17 +0100

    Fix: sRGB/non-sRGB incompatibility on Intel Iris

--- source/blender/gpu/opengl/gl_texture_pool.cc
+++ source/blender/gpu/opengl/gl_texture_pool.cc
@@ -17,10 +17,18 @@ static bool are_formats_compatible(const TextureFormat format_a, const TextureFo
 {
   GPUTextureFormatFlag flag_a = to_format_flag(format_a);
   GPUTextureFormatFlag flag_b = to_format_flag(format_b);
-  bool is_depth_or_stencil = (flag_a & GPU_FORMAT_DEPTH_STENCIL) != 0 |
+  bool is_depth_or_stencil = (flag_a & GPU_FORMAT_DEPTH_STENCIL) != 0 ||
                              (flag_b & GPU_FORMAT_DEPTH_STENCIL) != 0;
-  bool is_compressed = (flag_a & GPU_FORMAT_COMPRESSED) != 0 |
+  bool is_compressed = (flag_a & GPU_FORMAT_COMPRESSED) != 0 ||
                        (flag_b & GPU_FORMAT_COMPRESSED) != 0;
+
+  /* glTextureView on sRGB/non-sRGB formats breaks on Intel Iris Xe. */
+  if (GPU_type_matches(GPU_DEVICE_INTEL, GPU_OS_ANY, GPU_DRIVER_ANY)) {
+    bool is_srgb_match = (flag_a & GPU_FORMAT_SRGB) == (flag_b & GPU_FORMAT_SRGB);
+    if (!is_srgb_match) {
+      return false;
+    }
+  }
 
   /* glTextureView does not support depth/stencil formats, so we can only re-use
    * these textures if they share format. Compressed formats also have restricted
