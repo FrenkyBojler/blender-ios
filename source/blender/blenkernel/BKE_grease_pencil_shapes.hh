cd697d9616414114359c64bd4726c19a3a9ebd91
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-01-06 21:02:19 -0800

    Grease Pencil: Select shapes in `stroke` select mode

--- source/blender/blenkernel/BKE_grease_pencil_shapes.hh
+++ source/blender/blenkernel/BKE_grease_pencil_shapes.hh
@@ -17,4 +17,9 @@ namespace blender::bke::greasepencil {
 std::optional<ShapeCache> shape_cache_from_shape_ids(const int num_curves,
                                                      const VArray<int> &shape_ids);
 
+IndexMask selected_mask_to_shapes(const IndexMask selected_mask,
+                                  const bke::CurvesGeometry &curves,
+                                  const bke::AttrDomain selection_domain,
+                                  IndexMaskMemory &memory);
+
 }  // namespace blender::bke::greasepencil
