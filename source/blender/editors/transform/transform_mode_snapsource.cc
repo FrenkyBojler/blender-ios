5600472fc68765e9222811e45aa0af5ad45db6c5
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-01-08 17:33:50 +0000

    Fix #152554: Snap base does not work when rotating from the menu

--- source/blender/editors/transform/transform_mode_snapsource.cc
+++ source/blender/editors/transform/transform_mode_snapsource.cc
@@ -112,6 +112,7 @@ static void snapsource_confirm(TransInfo *t)
   snapsource_end(t);
   transform_input_reset(t, mval);
 
+  t->flag &= ~T_RELEASE_CONFIRM;
   /* Remote individual snap projection since this mode does not use the new `snap_source`. */
   t->tsnap.mode &= ~(SCE_SNAP_INDIVIDUAL_PROJECT | SCE_SNAP_INDIVIDUAL_NEAREST);
 }
