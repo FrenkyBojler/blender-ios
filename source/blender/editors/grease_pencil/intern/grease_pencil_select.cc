604ef94f0ecf02081e6c5b58529bf3e996ba9c4e
YimingWu <xp8110@outlook.com>
2026-01-12 21:30:41 +0800

    Revert "Fix #152690: Grease Pencil: Select similar on curves domain"
    This reverts commit 90d399f2b7a06ef23d2a0e3d1b13f1f84d1a0366.

--- source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -655,44 +655,17 @@ static void select_similar_by_value(Scene *scene,
 
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
-    if (selection_domain == bke::AttrDomain::Point) {
-      const IndexMask mask = ed::greasepencil::retrieve_editable_points(
-          *object, info.drawing, info.layer_index, memory);
-      bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
-      const VArraySpan<T> values = *curves.attributes().lookup_or_default<T>(
-          attribute_id, selection_domain, default_value);
+    const IndexMask mask = ed::greasepencil::retrieve_editable_points(
+        *object, info.drawing, info.layer_index, memory);
+    bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
+    const VArraySpan<T> values = *curves.attributes().lookup_or_default<T>(
+        attribute_id, selection_domain, default_value);
 
-      Span<StringRef> selection_attribute_names = ed::curves::get_curves_selection_attribute_names(
-          curves);
-      for (const int i : selection_attribute_names.index_range()) {
-        bke::GSpanAttributeWriter selection_writer = ed::curves::ensure_selection_attribute(
-            curves, selection_domain, bke::AttrType::Bool, selection_attribute_names[i]);
-        MutableSpan<bool> selection = selection_writer.span.typed<bool>();
-
-        mask.foreach_index(GrainSize(1024), [&](const int index) {
-          if (selection[index]) {
-            return;
-          }
-          for (const T &test_value : selected_values) {
-            if (distance_fn(values[index], test_value) <= threshold) {
-              selection[index] = true;
-            }
-          }
-        });
-
-        selection_writer.finish();
-      }
-    }
-    else {
-      ed::greasepencil::ensure_selection_domain(scene->toolsettings, object);
-      const IndexMask mask = ed::greasepencil::retrieve_editable_elements(
-          *object, info, selection_domain, memory);
-      bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
-      const VArraySpan<T> values = *curves.attributes().lookup_or_default<T>(
-          attribute_id, selection_domain, default_value);
-
+    Span<StringRef> selection_attribute_names = ed::curves::get_curves_selection_attribute_names(
+        curves);
+    for (const int i : selection_attribute_names.index_range()) {
       bke::GSpanAttributeWriter selection_writer = ed::curves::ensure_selection_attribute(
-          curves, selection_domain, bke::AttrType::Bool, ".selection");
+          curves, selection_domain, bke::AttrType::Bool, selection_attribute_names[i]);
       MutableSpan<bool> selection = selection_writer.span.typed<bool>();
 
       mask.foreach_index(GrainSize(1024), [&](const int index) {
@@ -705,6 +678,7 @@ static void select_similar_by_value(Scene *scene,
           }
         }
       });
+
       selection_writer.finish();
     }
   });
