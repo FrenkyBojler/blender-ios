ff33c4d3495765a23658ab8e0abde8e5d25329c9
Omar Emara <mail@OmarEmara.dev>
2026-01-13 14:42:41 +0200

    Refactor: Nodes: Remove node_add_init node tree callback
    This patch removes the node_add_init node tree type callback. It was
    only used by the compositor to setup the preview flag of nodes. Only
    four nodes have their preview flag enabled by default while all others
    are disabled, so it makes sense to move this code to the init function
    of the four nodes, with the default being no preview.
    
    Unfortunately, flags are shared between the node types and nodes
    themselves, so the default setup seems a bit weird. But this is for
    another refactor.

--- source/blender/blenkernel/intern/node.cc
+++ source/blender/blenkernel/intern/node.cc
@@ -2212,7 +2212,7 @@ static void node_init(const bContext *C, bNodeTree *ntree, bNode *node)
     return;
   }
 
-  node->flag = NODE_SELECT | NODE_OPTIONS | ntype->flag;
+  node->flag = NODE_SELECT | NODE_OPTIONS | (ntype->flag & ~NODE_PREVIEW);
   node->width = ntype->width;
   node->height = ntype->height;
   node->color[0] = node->color[1] = node->color[2] = 0.608; /* default theme color */
@@ -2245,10 +2245,6 @@ static void node_init(const bContext *C, bNodeTree *ntree, bNode *node)
      * Delayed init is not supported for nodes with context-based `initfunc_api` at the moment. */
     BLI_assert(C != nullptr);
     ntype->initfunc_api(C, &ptr);
-  }
-
-  if (ntree->typeinfo && ntree->typeinfo->node_add_init) {
-    ntree->typeinfo->node_add_init(ntree, node);
   }
 
   if (!add_sockets_before_init) {
