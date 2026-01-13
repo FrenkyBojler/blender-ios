8df75e8bd282e1c9a58bbe3d1fe91e5f580aa69f
Miguel Pozo <pragma37@gmail.com>
2026-01-13 18:28:11 +0100

    Fix EEVEE hit distance after clipping

--- source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.glsl
@@ -341,8 +341,8 @@ void raycast_eval(float3 position,
                                    hit_uv);
   if (result >= 0.0f) {
     is_hit = true;
-    hit_distance = result;
-    hit_position = ws_start + direction * hit_distance;
+    hit_position = ws_start + direction * result;
+    hit_distance = distance(position, hit_position);
     hit_normal = normalize(texture(prepass_normal_tx, hit_uv).xyz * 2.0f - 1.0f);
     int2 hit_texel = int2(hit_uv * float2(uniform_buf.film.render_extent));
     uint hit_id = texelFetch(object_id_tx, hit_texel, 0).x;
