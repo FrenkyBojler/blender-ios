7edd965b5708b525e873493038f1e6047f923f89
Julien Duroure <julien.duroure@gmail.com>
2026-01-14 09:43:30 +0100

    Cleanup: glTF: More pep8 format

--- scripts/addons_core/io_scene_gltf2/__init__.py
+++ scripts/addons_core/io_scene_gltf2/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     # This is now displayed as the maintainer, so show the foundation.
     # "author": "Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein", # Original Authors
     'author': "Blender Foundation, Khronos Group",
-    "version": (5, 1, 16),
+    "version": (5, 1, 15),
     'blender': (4, 4, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
@@ -1536,10 +1536,12 @@ def export_panel_data_material(layout, operator):
         if operator.export_image_format in ["AUTO", "JPEG", "WEBP"]:
             col.prop(operator, 'export_image_quality')
         col = body.column()
-        col.active = operator.export_image_format != "WEBP" and operator.export_materials not in ['PLACEHOLDER', 'NONE', 'VIEWPORT']
+        col.active = operator.export_image_format != "WEBP" and operator.export_materials not in [
+            'PLACEHOLDER', 'NONE', 'VIEWPORT']
         col.prop(operator, "export_image_add_webp")
         col = body.column()
-        col.active = operator.export_image_format != "WEBP" and operator.export_materials not in ['PLACEHOLDER', 'NONE', 'VIEWPORT']
+        col.active = operator.export_image_format != "WEBP" and operator.export_materials not in [
+            'PLACEHOLDER', 'NONE', 'VIEWPORT']
         col.prop(operator, "export_image_webp_fallback")
 
         header, sub_body = body.panel("GLTF_export_data_material_unused", default_closed=True)
