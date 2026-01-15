Pagecran/blender

Fix #144035: Image Editor ignores face selection
Introduced in 24d08e0bae

The above commit introduced a new batch for drawing UVs in the Image
editor that does not consider face selection when drawing the
corresponding UV map. This was done to reuse the IBO used in object
mode.

Unfortunately, this change didn't account for the case of being able to
select faces in the 3D Viewport (i.e. in Edit or Texture Paint mode)
while also viewing the Image Editor in Paint Mode.

To fix this, the following changes have been made:

* Introduce a new case when drawing the `MeshUV` overlay for objects
  being edited.
* Add two new IBO types and a new batch type to clearly differentiate
  between the above cases when extracting mesh data.
* Fixes some incorrect usage of the `sync_selection` concept

Pull Request: https://projects.blender.org/blender/blender/pulls/144105

source/blender/draw/intern/mesh_extractors/extract_mesh_ibo_edituv.cc
@@ -27,7 +27,7 @@ inline bool skip_bm_face(const BMFace &face, const bool sync_selection)
   if (BM_elem_flag_test(&face, BM_ELEM_HIDDEN)) {
     return true;
   }
-  if (!sync_selection) {
+  if (sync_selection) {
     if (!BM_elem_flag_test_bool(&face, BM_ELEM_SELECT)) {
       return true;
     }
@@ -104,17 +104,32 @@ static gpu::IndexBufPtr extract_edituv_tris_mesh(const MeshRenderData &mr,
   const Span<int3> corner_tris = mr.mesh->corner_tris();
 
   IndexMaskMemory memory;
-  const IndexMask selection = IndexMask::from_predicate(
-      faces.index_range(), GrainSize(4096), memory, [&](const int face) {
-        const BMFace *face_orig = bm_original_face_get(mr, face);
-        if (!face_orig) {
-          return false;
-        }
-        if (skip_bm_face(*face_orig, sync_selection)) {
-          return false;
-        }
-        return true;
-      });
+  IndexMask selection;
+  if (mr.bm) {
+    selection = IndexMask::from_predicate(
+        faces.index_range(), GrainSize(4096), memory, [&](const int face) {
+          const BMFace *face_orig = bm_original_face_get(mr, face);
+          if (!face_orig) {
+            return false;
+          }
+          if (skip_bm_face(*face_orig, sync_selection)) {
+            return false;
+          }
+          return true;
+        });
+  }
+  else {
+    if (mr.hide_poly.is_empty()) {
+      selection = faces.index_range();
+    }
+    else {
+      selection = IndexMask::from_bools_inverse(faces.index_range(), mr.hide_poly, memory);
+    }
+
+    if (sync_selection) {
+      selection = IndexMask::from_bools(selection, mr.select_poly, memory);
+    }
+  }
 
   if (selection.size() == faces.size()) {
     return gpu::IndexBufPtr(GPU_indexbuf_build_from_memory(GPU_PRIM_TRIS,
@@ -147,9 +162,9 @@ static gpu::IndexBufPtr extract_edituv_tris_mesh(const MeshRenderData &mr,
   return gpu::IndexBufPtr(GPU_indexbuf_build_ex(&builder, 0, mr.corners_num, false));
 }
 
-gpu::IndexBufPtr extract_edituv_tris(const MeshRenderData &mr)
+gpu::IndexBufPtr extract_edituv_tris(const MeshRenderData &mr, const bool edit_uvs)
 {
-  const bool sync_selection = (mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0;
+  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0) || !edit_uvs;
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_tris_bm(mr, sync_selection);
   }
@@ -314,7 +329,7 @@ static gpu::IndexBufPtr extract_edituv_lines_mesh(const MeshRenderData &mr,
     if (!mr.hide_poly.is_empty()) {
       visible = IndexMask::from_bools_inverse(visible, mr.hide_poly, memory);
     }
-    if (!sync_selection) {
+    if (sync_selection) {
       if (mr.select_poly.is_empty()) {
         visible = {};
       }
@@ -346,9 +361,20 @@ static gpu::IndexBufPtr extract_edituv_lines_mesh(const MeshRenderData &mr,
   return result;
 }
 
-gpu::IndexBufPtr extract_edituv_lines(const MeshRenderData &mr, bool edit_uvs)
+gpu::IndexBufPtr extract_edituv_lines(const MeshRenderData &mr, const UvExtractionMode mode)
 {
-  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0) || !edit_uvs;
+  bool sync_selection = true;
+  switch (mode) {
+    case UvExtractionMode::All:
+      sync_selection = false;
+      break;
+    case UvExtractionMode::Edit:
+      sync_selection = ((mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0);
+      break;
+    case UvExtractionMode::Selection:
+      sync_selection = true;
+      break;
+  }
 
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_lines_bm(mr, sync_selection);
@@ -450,9 +476,20 @@ static gpu::IndexBufPtr extract_edituv_lines_subdiv_mesh(const MeshRenderData &m
 
 gpu::IndexBufPtr extract_edituv_lines_subdiv(const MeshRenderData &mr,
                                              const DRWSubdivCache &subdiv_cache,
-                                             bool edit_uvs)
+                                             const UvExtractionMode mode)
 {
-  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0) || !edit_uvs;
+  bool sync_selection = true;
+  switch (mode) {
+    case UvExtractionMode::All:
+      sync_selection = false;
+      break;
+    case UvExtractionMode::Edit:
+      sync_selection = ((mr.toolsettings->uv_flag & UV_FLAG_SYNC_SELECT) != 0);
+      break;
+    case UvExtractionMode::Selection:
+      sync_selection = true;
+      break;
+  }
 
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_lines_subdiv_bm(mr, subdiv_cache, sync_selection);

FrenkyBojler/blender-ios

source/blender/draw/intern/mesh_extractors/extract_mesh_ibo_edituv.cc
@@ -27,7 +27,7 @@ inline bool skip_bm_face(const BMFace &face, const bool sync_selection)
   if (BM_elem_flag_test(&face, BM_ELEM_HIDDEN)) {
     return true;
   }
-  if (!sync_selection) {
+  if (sync_selection) {
     if (!BM_elem_flag_test_bool(&face, BM_ELEM_SELECT)) {
       return true;
     }
@@ -101,17 +101,32 @@ static gpu::IndexBufPtr extract_edituv_tris_mesh(const MeshRenderData &mr,
   const Span<int3> corner_tris = mr.mesh->corner_tris();
 
   IndexMaskMemory memory;
-  const IndexMask selection = IndexMask::from_predicate(
-      faces.index_range(), GrainSize(4096), memory, [&](const int face) {
-        const BMFace *face_orig = bm_original_face_get(mr, face);
-        if (!face_orig) {
-          return false;
-        }
-        if (skip_bm_face(*face_orig, sync_selection)) {
-          return false;
-        }
-        return true;
-      });
+  IndexMask selection;
+  if (mr.bm) {
+    selection = IndexMask::from_predicate(
+        faces.index_range(), GrainSize(4096), memory, [&](const int face) {
+          const BMFace *face_orig = bm_original_face_get(mr, face);
+          if (!face_orig) {
+            return false;
+          }
+          if (skip_bm_face(*face_orig, sync_selection)) {
+            return false;
+          }
+          return true;
+        });
+  }
+  else {
+    if (mr.hide_poly.is_empty()) {
+      selection = faces.index_range();
+    }
+    else {
+      selection = IndexMask::from_bools_inverse(faces.index_range(), mr.hide_poly, memory);
+    }
+
+    if (sync_selection) {
+      selection = IndexMask::from_bools(selection, mr.select_poly, memory);
+    }
+  }
 
   Array<int> selected_face_offset_data;
   const OffsetIndices selected_faces = gather_or_reference_offsets(
@@ -135,9 +150,9 @@ static gpu::IndexBufPtr extract_edituv_tris_mesh(const MeshRenderData &mr,
   return gpu::IndexBufPtr(GPU_indexbuf_build_ex(&builder, 0, mr.corners_num, false));
 }
 
-gpu::IndexBufPtr extract_edituv_tris(const MeshRenderData &mr)
+gpu::IndexBufPtr extract_edituv_tris(const MeshRenderData &mr, const bool edit_uvs)
 {
-  const bool sync_selection = (mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0;
+  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0) || !edit_uvs;
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_tris_bm(mr, sync_selection);
   }
@@ -295,7 +310,7 @@ static gpu::IndexBufPtr extract_edituv_lines_mesh(const MeshRenderData &mr,
     if (!mr.hide_poly.is_empty()) {
       visible = IndexMask::from_bools_inverse(visible, mr.hide_poly, memory);
     }
-    if (!sync_selection) {
+    if (sync_selection) {
       if (mr.select_poly.is_empty()) {
         visible = {};
       }
@@ -318,9 +333,20 @@ static gpu::IndexBufPtr extract_edituv_lines_mesh(const MeshRenderData &mr,
   return gpu::IndexBufPtr(GPU_indexbuf_build_ex(&builder, 0, mr.corners_num, false));
 }
 
-gpu::IndexBufPtr extract_edituv_lines(const MeshRenderData &mr, bool edit_uvs)
+gpu::IndexBufPtr extract_edituv_lines(const MeshRenderData &mr, const UvExtractionMode mode)
 {
-  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0) || !edit_uvs;
+  bool sync_selection = true;
+  switch (mode) {
+    case UvExtractionMode::All:
+      sync_selection = false;
+      break;
+    case UvExtractionMode::Edit:
+      sync_selection = ((mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0);
+      break;
+    case UvExtractionMode::Selection:
+      sync_selection = true;
+      break;
+  }
 
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_lines_bm(mr, sync_selection);
@@ -414,9 +440,20 @@ static gpu::IndexBufPtr extract_edituv_lines_subdiv_mesh(const MeshRenderData &m
 
 gpu::IndexBufPtr extract_edituv_lines_subdiv(const MeshRenderData &mr,
                                              const DRWSubdivCache &subdiv_cache,
-                                             bool edit_uvs)
+                                             const UvExtractionMode mode)
 {
-  const bool sync_selection = ((mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0) || !edit_uvs;
+  bool sync_selection = true;
+  switch (mode) {
+    case UvExtractionMode::All:
+      sync_selection = false;
+      break;
+    case UvExtractionMode::Edit:
+      sync_selection = ((mr.toolsettings->uv_flag & UV_SYNC_SELECTION) != 0);
+      break;
+    case UvExtractionMode::Selection:
+      sync_selection = true;
+      break;
+  }
 
   if (mr.extract_type == MeshExtractType::BMesh) {
     return extract_edituv_lines_subdiv_bm(mr, subdiv_cache, sync_selection);