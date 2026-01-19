b19009327606b100058dc3e96bf92f54105cf9fa
shokuyansh <anshmalgotra@gmail.com>
2026-01-19 13:10:06 +0530

    removed unnecessary printf statements

--- source/blender/editors/mesh/editface.cc
+++ source/blender/editors/mesh/editface.cc
@@ -413,7 +413,6 @@ static bool follow_face_loop(const int face_start_index,
 
 void paintface_select_loop(bContext *C, Object *ob, const int mval[2], const bool select)
 {
-  printf("DEBUG: paintface_select_loop started!\n"); // CHECK 1
   fflush(stdout);
   Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
   ViewContext vc = ED_view3d_viewcontext_init(C, depsgraph);
@@ -428,7 +427,6 @@ void paintface_select_loop(bContext *C, Object *ob, const int mval[2], const boo
   if (!ED_mesh_pick_edge(C, ob, mval, ED_MESH_PICK_DEFAULT_VERT_DIST, &closest_edge_index)) {
     return;
   }
-  printf("DEBUG: Edge Found! Index: %u\n", closest_edge_index); // CHECK 3
   fflush(stdout);
   if (closest_edge_index == -1) {
     return;
@@ -467,7 +465,6 @@ void paintface_select_loop(bContext *C, Object *ob, const int mval[2], const boo
                                                  corner_edges,
                                                  edge_to_face_map,
                                                  faces_to_select);
-  printf("DEBUG: Loop Finished. Loop Size: %ld\n", faces_to_select.size()); // CHECK 4
   fflush(stdout);
   if (!traced_full_loop && faces_to_closest_edge.size() > 1) {
     /* Trace the other way. */
