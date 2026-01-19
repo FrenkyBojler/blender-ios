ad7c22346c88b6981a70a6d241a0cec76c94101b
Sybren A. Stüvel <sybren@blender.org>
2026-01-19 18:02:11 +0100

    Move property above the warning

--- scripts/startup/bl_ui/space_userpref.py
+++ scripts/startup/bl_ui/space_userpref.py
@@ -666,9 +666,9 @@ class USERPREF_PT_animation_advanced(AnimationPanel, CenterAlignMixIn, Panel):
 
         box = layout.box()
         col = box.column(align=True)
+        col.prop(edit, "use_negative_frames")
         col.label(text="Using negative frames can cause issues, for example")
         col.label(text="with audio playback or with some exporters.")
-        col.prop(edit, "use_negative_frames")
 
 
 # -----------------------------------------------------------------------------
