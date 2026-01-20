c8b9e85a581ba881647aef8baedd205c6dbdd1c7
Treata11 <treata11@yahoo.com>
2026-01-20 23:21:58 +0330

    DNA: document `rt_scale`

--- source/blender/makesdna/DNA_scene_types.h
+++ source/blender/makesdna/DNA_scene_types.h
@@ -2527,6 +2527,9 @@ struct RaytraceEEVEE {
   /** #RaytraceEEVEE_DenoiseStages. */
   int denoise_stages = RAYTRACE_EEVEE_DENOISE_SPATIAL | RAYTRACE_EEVEE_DENOISE_TEMPORAL |
                        RAYTRACE_EEVEE_DENOISE_BILATERAL;
+  /** Global ray-traced lighting contribution scalar. */
+  float rt_scale = 1.0f;
+  char _pad0[4] = {};
 };
 
 /** #SceneEEVEE::flag */
