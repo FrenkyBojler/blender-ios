891246b839b5005709d3a6ed58c1a337a8b83f7f
Hans Goudey <hans@blender.org>
2026-01-20 09:22:38 -0500

    Remove assignment redundant with dna defaults

--- source/blender/nodes/geometry/nodes/node_geo_list_get_item.cc
+++ source/blender/nodes/geometry/nodes/node_geo_list_get_item.cc
@@ -52,7 +52,6 @@ static void node_layout_ex(ui::Layout &layout, bContext * /*C*/, PointerRNA *ptr
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
   auto *storage = MEM_new_for_free<NodeGeometryListGetItem>(__func__);
-  storage->socket_type = SOCK_FLOAT;
   node->storage = storage;
 }
 
