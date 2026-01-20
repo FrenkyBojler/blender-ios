dbd2f019787e66acc1c51800b836db028427f215
Xury Greer <Xury.Greer@Gmail.com>
2026-01-19 19:43:35 -0800

    Create curvature.blend test render file

--- tests/python/cycles_render_tests.py
+++ tests/python/cycles_render_tests.py
@@ -74,11 +74,12 @@ BLOCKLIST_OPTIX_OSL_LIMITED = [
 
 # Blocklist for SVM tests that fail when forced to run with OptiX OSL
 BLOCKLIST_OPTIX_OSL_ALL = BLOCKLIST_OPTIX_OSL_LIMITED + [
-    # OptiX OSL does support AO, Bevel or Raycast
+    # OptiX OSL doesn't support AO, Bevel, Raycast, or Curvature.
     'ambient_occlusion.*.blend',
     'bake_bevel.blend',
     'bevel.blend',
     'raycast.*.blend',
+    'curvature.blend',
     'principled_bsdf_bevel_emission_137420.blend',
     # Dicing tests use wireframe node which doesn't appear to be supported with OptiX OSL
     'dicing_camera.blend',
