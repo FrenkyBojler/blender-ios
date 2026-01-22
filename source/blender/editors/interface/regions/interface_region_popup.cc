fca51376f2fe42e8852f3662e55a8ecddd979b1c
Guillermo Venegas <guillermovcra@gmail.com>
2026-01-22 17:47:19 -0600

    fix comment

--- source/blender/editors/interface/regions/interface_region_popup.cc
+++ source/blender/editors/interface/regions/interface_region_popup.cc
@@ -845,7 +845,7 @@ Block *popup_block_refresh(bContext *C, PopupBlockHandle *handle, ARegion *butre
     ui_popup_block_clip(window, block);
 
     /* Avoid menu moving down and losing cursor focus by keeping it at the same height when the
-     * popup is displaced down by at least one pixel. */
+     * popup is displaced down by at least one window unit. */
     if (handle->refresh && (handle->prev_block_rect.ymax - block->rect.ymax) > 1.0f) {
       if (block->bounds_type != BLOCK_BOUNDS_POPUP_CENTER) {
         const float offset = handle->prev_block_rect.ymax - block->rect.ymax;
