blender/blender

commit 8adba33029f766eba157c283a9e01dd35384f97b

Mesh: Switch from CustomData to AttributeStorage
This commit changes Mesh to use AttributeStorage to store generic
attributes, similar to previous changes for curves and point clouds.
The main benefit is that this matches the generic attribute design
better than CustomData. This is discussed more in #122398.

Most of the necessary changes landed separately over the past year or so
by moving code to use the higher-level attribute API instead of using
CustomData directly. This commits changes the remaining cases where
lower level data access was necessary, and replaces the attribute
provider system.

Resolves #107500

Pull Request: https://projects.blender.org/blender/blender/pulls/152851

source/blender/editors/mesh/mesh_data.cc
@@ -687,17 +687,8 @@ static void mesh_add_verts(Mesh *mesh, int len)
   }
 
   int totvert = mesh->verts_num + len;
-  CustomData vert_data;
-  CustomData_init_layout_from(
-      &mesh->vert_data, &vert_data, CD_MASK_MESH.vmask, CD_SET_DEFAULT, totvert);
-  CustomData_copy_data(&mesh->vert_data, &vert_data, 0, 0, mesh->verts_num);
-
-  if (!CustomData_has_layer_named(&vert_data, CD_PROP_FLOAT3, "position")) {
-    CustomData_add_layer_named(&vert_data, CD_PROP_FLOAT3, CD_SET_DEFAULT, totvert, "position");
-  }
-
-  CustomData_free(&mesh->vert_data);
-  mesh->vert_data = vert_data;
+  mesh->attribute_storage.wrap().resize(bke::AttrDomain::Point, totvert);
+  CustomData_realloc(&mesh->vert_data, mesh->verts_num, totvert, CD_SET_DEFAULT);
 
   BKE_mesh_runtime_clear_cache(mesh);
 
@@ -708,11 +699,11 @@ static void mesh_add_verts(Mesh *mesh, int len)
       ".select_vert", bke::AttrDomain::Point);
   select_vert.span.take_back(len).fill(true);
   select_vert.finish();
+  attributes.add<float3>("position", bke::AttrDomain::Point, bke::AttributeInitDefaultValue());
 }
 
 static void mesh_add_edges(Mesh *mesh, int len)
 {
-  CustomData edge_data;
   int totedge;
 
   if (len == 0) {
@@ -721,18 +712,8 @@ static void mesh_add_edges(Mesh *mesh, int len)
 
   totedge = mesh->edges_num + len;
 
-  /* Update custom-data. */
-  CustomData_init_layout_from(
-      &mesh->edge_data, &edge_data, CD_MASK_MESH.emask, CD_SET_DEFAULT, totedge);
-  CustomData_copy_data(&mesh->edge_data, &edge_data, 0, 0, mesh->edges_num);
-
-  if (!CustomData_has_layer_named(&edge_data, CD_PROP_INT32_2D, ".edge_verts")) {
-    CustomData_add_layer_named(
-        &edge_data, CD_PROP_INT32_2D, CD_SET_DEFAULT, totedge, ".edge_verts");
-  }
-
-  CustomData_free(&mesh->edge_data);
-  mesh->edge_data = edge_data;
+  mesh->attribute_storage.wrap().resize(bke::AttrDomain::Edge, totedge);
+  CustomData_realloc(&mesh->edge_data, mesh->edges_num, totedge, CD_SET_DEFAULT);
 
   BKE_mesh_runtime_clear_cache(mesh);
 
@@ -743,11 +724,11 @@ static void mesh_add_edges(Mesh *mesh, int len)
       ".select_edge", bke::AttrDomain::Edge);
   select_edge.span.take_back(len).fill(true);
   select_edge.finish();
+  attributes.add<float3>(".edge_verts", bke::AttrDomain::Edge, bke::AttributeInitDefaultValue());
 }
 
 static void mesh_add_loops(Mesh *mesh, int len)
 {
-  CustomData ldata;
   int totloop;
 
   if (len == 0) {
@@ -756,24 +737,16 @@ static void mesh_add_loops(Mesh *mesh, int len)
 
   totloop = mesh->corners_num + len; /* new face count */
 
-  /* update customdata */
-  CustomData_init_layout_from(
-      &mesh->corner_data, &ldata, CD_MASK_MESH.lmask, CD_SET_DEFAULT, totloop);
-  CustomData_copy_data(&mesh->corner_data, &ldata, 0, 0, mesh->corners_num);
-
-  if (!CustomData_has_layer_named(&ldata, CD_PROP_INT32, ".corner_vert")) {
-    CustomData_add_layer_named(&ldata, CD_PROP_INT32, CD_SET_DEFAULT, totloop, ".corner_vert");
-  }
-  if (!CustomData_has_layer_named(&ldata, CD_PROP_INT32, ".corner_edge")) {
-    CustomData_add_layer_named(&ldata, CD_PROP_INT32, CD_SET_DEFAULT, totloop, ".corner_edge");
-  }
+  mesh->attribute_storage.wrap().resize(bke::AttrDomain::Corner, totloop);
+  CustomData_realloc(&mesh->corner_data, mesh->corners_num, totloop, CD_SET_DEFAULT);
 
   BKE_mesh_runtime_clear_cache(mesh);
 
-  CustomData_free(&mesh->corner_data);
-  mesh->corner_data = ldata;
-
   mesh->corners_num = totloop;
+
+  bke::MutableAttributeAccessor attributes = mesh->attributes_for_write();
+  attributes.add<int>(".corner_vert", bke::AttrDomain::Corner, bke::AttributeInitDefaultValue());
+  attributes.add<int>(".corner_edge", bke::AttrDomain::Corner, bke::AttributeInitDefaultValue());
 
   /* Keep the last face offset up to date with the corner total (they must be the same). We have
    * to be careful here though, since the mesh may not be in a valid state at this point. */
@@ -784,7 +757,6 @@ static void mesh_add_loops(Mesh *mesh, int len)
 
 static void mesh_add_faces(Mesh *mesh, int len)
 {
-  CustomData face_data;
   int faces_num;
 
   if (len == 0) {
@@ -793,10 +765,8 @@ static void mesh_add_faces(Mesh *mesh, int len)
 
   faces_num = mesh->faces_num + len; /* new face count */
 
-  /* update customdata */
-  CustomData_init_layout_from(
-      &mesh->face_data, &face_data, CD_MASK_MESH.pmask, CD_SET_DEFAULT, faces_num);
-  CustomData_copy_data(&mesh->face_data, &face_data, 0, 0, mesh->faces_num);
+  mesh->attribute_storage.wrap().resize(bke::AttrDomain::Face, faces_num);
+  CustomData_realloc(&mesh->face_data, mesh->faces_num, faces_num, CD_SET_DEFAULT);
 
   implicit_sharing::resize_trivial_array(&mesh->face_offset_indices,
                                          &mesh->runtime->face_offsets_sharing_info,
@@ -805,9 +775,6 @@ static void mesh_add_faces(Mesh *mesh, int len)
   /* Set common values for convenience. */
   mesh->face_offset_indices[0] = 0;
   mesh->face_offset_indices[faces_num] = mesh->corners_num;
-
-  CustomData_free(&mesh->face_data);
-  mesh->face_data = face_data;
 
   BKE_mesh_runtime_clear_cache(mesh);
 

source/blender/blenkernel/BKE_attribute.h
@@ -26,6 +26,7 @@ class AttributeAccessor;
 class AttributeStorage;
 class MutableAttributeAccessor;
 }  // namespace bke
+struct BMesh;
 struct CustomData;
 struct CustomDataLayer;
 struct ID;
@@ -89,27 +90,18 @@ class AttributeOwner {
 /**
  * Create a new attribute layer.
  */
-struct CustomDataLayer *BKE_attribute_new(AttributeOwner &owner,
+struct CustomDataLayer *BKE_attribute_new(Mesh &mesh,
+                                          BMesh &bm,
                                           StringRef name,
                                           eCustomDataType type,
                                           bke::AttrDomain domain,
                                           struct ReportList *reports);
 bool BKE_attribute_remove(AttributeOwner &owner, StringRef name, struct ReportList *reports);
 
-const struct CustomDataLayer *BKE_attribute_search(const AttributeOwner &owner,
-                                                   StringRef name,
-                                                   eCustomDataMask type,
-                                                   AttrDomainMask domain_mask);
-
-struct CustomDataLayer *BKE_attribute_search_for_write(AttributeOwner &owner,
-                                                       StringRef name,
-                                                       eCustomDataMask type,
-                                                       AttrDomainMask domain_mask);
-
-bke::AttrDomain BKE_attribute_domain(const AttributeOwner &owner,
+bke::AttrDomain BKE_attribute_domain(const Mesh &mesh,
+                                     const BMesh &bm,
                                      const struct CustomDataLayer *layer);
 int BKE_attribute_domain_size(const AttributeOwner &owner, int domain);
-int BKE_attribute_data_length(AttributeOwner &owner, struct CustomDataLayer *layer);
 bool BKE_attribute_required(const AttributeOwner &owner, StringRef name);
 bool BKE_attribute_rename(AttributeOwner &owner,
                           StringRef old_name,

source/blender/blenkernel/BKE_attribute_legacy_convert.hh
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "BLI_vector.hh"
+
 #include "DNA_attribute_types.h"
 
 #include "BKE_attribute.h"
@@ -38,13 +40,6 @@ std::optional<AttrType> custom_data_type_to_attr_type(eCustomDataType data_type)
 std::optional<eCustomDataType> attr_type_to_custom_data_type(AttrType attr_type);
 
 /**
- * Move attributes from the #AttributeStorage to the mesh's #CustomData structs. Used for forward
- * compatibility: converting newer files written with #AttributeStorage while #CustomData is still
- * used at runtime.
- */
-void mesh_convert_storage_to_customdata(Mesh &mesh);
-
-/**
  * Move generic attributes from #CustomData to #AttributeStorage (not including non-generic layer
  * types). Use for versioning old files when the newer #AttributeStorage format is used at runtime.
  */
@@ -61,6 +56,8 @@ void grease_pencil_convert_customdata_to_storage(GreasePencil &grease_pencil);
 
 /** Abstraction for copying #CustomData layers and #AttributeStorage attributes. */
 class LegacyMeshInterpolator {
+  Vector<GVArray> attrs_src_;
+  Vector<GMutableSpan> attrs_dst_;
 
   const CustomData &cd_src_;
   CustomData &cd_dst_;

source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ namespace blender {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 18
+#define BLENDER_FILE_SUBVERSION 19
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_customdata.hh
@@ -229,14 +229,6 @@ bool CustomData_bmesh_merge_layout(const CustomData *source,
                                    char htype);
 
 /**
- * Remove layers that aren't stored in BMesh or are stored as flags on BMesh.
- * The `layers` array of the returned #CustomData must be freed, but may be null.
- * Used during conversion of #Mesh data to #BMesh storage format.
- */
-CustomData CustomData_shallow_copy_remove_non_bmesh_attributes(const CustomData *src,
-                                                               eCustomDataMask mask);
-
-/**
  * NULL's all members and resets the #CustomData.typemap.
  *
  * \warning Does not free or release any internal resources.

source/blender/blenkernel/CMakeLists.txt
@@ -540,7 +540,6 @@ set(SRC
   nla_private.h
   particle_private.h
 
-  intern/attribute_access_intern.hh
   intern/attribute_storage_access.hh
   intern/data_transfer_intern.hh
   intern/lib_intern.hh

source/blender/blenkernel/intern/attribute_access.cc
@@ -27,8 +27,6 @@
 
 #include "FN_field.hh"
 
-#include "attribute_access_intern.hh"
-
 #ifndef NDEBUG
 #  include <iostream>
 #endif
@@ -321,341 +319,6 @@ AttrDomain attribute_domain_highest_priority(Span<AttrDomain> domains)
   }
 
   return highest_priority_domain;
-}
-
-static void *add_generic_custom_data_layer(CustomData &custom_data,
-                                           const eCustomDataType data_type,
-                                           const eCDAllocType alloctype,
-                                           const int domain_size,
-                                           const StringRef attribute_id)
-{
-  return CustomData_add_layer_named(&custom_data, data_type, alloctype, domain_size, attribute_id);
-}
-
-static const void *add_generic_custom_data_layer_with_existing_data(
-    CustomData &custom_data,
-    const eCustomDataType data_type,
-    const StringRef attribute_id,
-    const int domain_size,
-    void *layer_data,
-    const ImplicitSharingInfo *sharing_info)
-{
-  return CustomData_add_layer_named_with_data(
-      &custom_data, data_type, layer_data, domain_size, attribute_id, sharing_info);
-}
-
-static bool add_custom_data_layer_from_attribute_init(const StringRef attribute_id,
-                                                      CustomData &custom_data,
-                                                      const eCustomDataType data_type,
-                                                      const int domain_num,
-                                                      const AttributeInit &initializer,
-                                                      const GPointer custom_default_value_ptr)
-{
-  const int old_layer_num = custom_data.totlayer;
-  switch (initializer.type) {
-    case AttributeInit::Type::Construct: {
-      add_generic_custom_data_layer(
-          custom_data, data_type, CD_CONSTRUCT, domain_num, attribute_id);
-      break;
-    }
-    case AttributeInit::Type::DefaultValue: {
-      if (const void *default_value = custom_default_value_ptr.get()) {
-        const CPPType &type = *custom_default_value_ptr.type();
-        void *data = add_generic_custom_data_layer(
-            custom_data, data_type, CD_CONSTRUCT, domain_num, attribute_id);
-        type.fill_assign_n(default_value, data, domain_num);
-      }
-      else {
-        add_generic_custom_data_layer(
-            custom_data, data_type, CD_SET_DEFAULT, domain_num, attribute_id);
-      }
-      break;
-    }
-    case AttributeInit::Type::VArray: {
-      void *data = add_generic_custom_data_layer(
-          custom_data, data_type, CD_CONSTRUCT, domain_num, attribute_id);
-      if (data != nullptr) {
-        const GVArray &varray = static_cast<const AttributeInitVArray &>(initializer).varray;
-        varray.materialize_to_uninitialized(varray.index_range(), data);
-      }
-      break;
-    }
-    case AttributeInit::Type::MoveArray: {
-      void *data = static_cast<const AttributeInitMoveArray &>(initializer).data;
-      add_generic_custom_data_layer_with_existing_data(
-          custom_data, data_type, attribute_id, domain_num, data, nullptr);
-      break;
-    }
-    case AttributeInit::Type::Shared: {
-      const AttributeInitShared &init = static_cast<const AttributeInitShared &>(initializer);
-      add_generic_custom_data_layer_with_existing_data(custom_data,
-                                                       data_type,
-                                                       attribute_id,
-                                                       domain_num,
-                                                       const_cast<void *>(init.data),
-                                                       init.sharing_info);
-      break;
-    }
-  }
-  return old_layer_num < custom_data.totlayer;
-}
-
-bool BuiltinCustomDataLayerProvider::layer_exists(const CustomData &custom_data) const
-{
-  return CustomData_get_named_layer_index(&custom_data, data_type_, name_) != -1;
-}
-
-GAttributeReader BuiltinCustomDataLayerProvider::try_get_for_read(const void *owner) const
-{
-  const CustomData *custom_data = custom_data_access_.get_const_custom_data(owner);
-  if (custom_data == nullptr) {
-    return {};
-  }
-
-  /* When the number of elements is zero, layers might have null data but still exist. */
-  const CPPType &type = *custom_data_type_to_cpp_type(data_type_);
-  const int element_num = custom_data_access_.get_element_num(owner);
-  if (element_num == 0) {
-    if (this->layer_exists(*custom_data)) {
-      return {GVArray::from_span({type, nullptr, 0}), domain_, nullptr};
-    }
-    return {};
-  }
-
-  const int index = CustomData_get_named_layer_index(custom_data, data_type_, name_);
-  if (index == -1) {
-    return {};
-  }
-  const CustomDataLayer &layer = custom_data->layers[index];
-  return {GVArray::from_span({type, layer.data, element_num}), domain_, layer.sharing_info};
-}
-
-GAttributeWriter BuiltinCustomDataLayerProvider::try_get_for_write(void *owner) const
-{
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return {};
-  }
-
-  std::function<void()> tag_modified_fn;
-  if (update_on_change_ != nullptr) {
-    tag_modified_fn = [owner, update = update_on_change_]() { update(owner); };
-  }
-
-  /* When the number of elements is zero, layers might have null data but still exist. */
-  const CPPType &type = *custom_data_type_to_cpp_type(data_type_);
-  const int element_num = custom_data_access_.get_element_num(owner);
-  if (element_num == 0) {
-    if (this->layer_exists(*custom_data)) {
-      return {GVMutableArray::from_span({type, nullptr, 0}), domain_, std::move(tag_modified_fn)};
-    }
-    return {};
-  }
-
-  void *data = CustomData_get_layer_named_for_write(custom_data, data_type_, name_, element_num);
-  if (data == nullptr) {
-    return {};
-  }
-  return {
-      GVMutableArray::from_span({type, data, element_num}), domain_, std::move(tag_modified_fn)};
-}
-
-bool BuiltinCustomDataLayerProvider::try_delete(void *owner) const
-{
-  if (deletable_ != Deletable) {
-    return false;
-  }
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return {};
-  }
-
-  if (CustomData_free_layer_named(custom_data, name_)) {
-    if (update_on_change_ != nullptr) {
-      update_on_change_(owner);
-    }
-    return true;
-  }
-  return false;
-}
-
-bool BuiltinCustomDataLayerProvider::try_create(void *owner,
-                                                const AttributeInit &initializer) const
-{
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return false;
-  }
-
-  const int element_num = custom_data_access_.get_element_num(owner);
-  if (CustomData_has_layer_named(custom_data, data_type_, name_)) {
-    /* Exists already. */
-    return false;
-  }
-  if (add_custom_data_layer_from_attribute_init(
-          name_, *custom_data, data_type_, element_num, initializer, default_value_))
-  {
-    if (initializer.type != AttributeInit::Type::Construct) {
-      /* Avoid calling update function when values are not default-initialized. Without default
-       * initialization or otherwise meaningful initial values, they should be set elsewhere
-       * anyway, which will cause a separate update tag. */
-      if (update_on_change_ != nullptr) {
-        update_on_change_(owner);
-      }
-    }
-    return true;
-  }
-  return false;
-}
-
-bool BuiltinCustomDataLayerProvider::exists(const void *owner) const
-{
-  const CustomData *custom_data = custom_data_access_.get_const_custom_data(owner);
-  if (custom_data == nullptr) {
-    return false;
-  }
-  return CustomData_has_layer_named(custom_data, data_type_, name_);
-}
-
-GAttributeReader CustomDataAttributeProvider::try_get_for_read(const void *owner,
-                                                               const StringRef attribute_id) const
-{
-  const CustomData *custom_data = custom_data_access_.get_const_custom_data(owner);
-  if (custom_data == nullptr) {
-    return {};
-  }
-  const int element_num = custom_data_access_.get_element_num(owner);
-  for (const CustomDataLayer &layer : Span(custom_data->layers, custom_data->totlayer)) {
-    if (layer.name != attribute_id) {
-      continue;
-    }
-    const CPPType *type = custom_data_type_to_cpp_type(eCustomDataType(layer.type));
-    if (type == nullptr) {
-      continue;
-    }
-    GSpan data{*type, layer.data, element_num};
-    return {GVArray::from_span(data), domain_, layer.sharing_info};
-  }
-  return {};
-}
-
-GAttributeWriter CustomDataAttributeProvider::try_get_for_write(void *owner,
-                                                                const StringRef attribute_id) const
-{
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return {};
-  }
-  const int element_num = custom_data_access_.get_element_num(owner);
-  for (CustomDataLayer &layer : MutableSpan(custom_data->layers, custom_data->totlayer)) {
-    if (layer.name != attribute_id) {
-      continue;
-    }
-    CustomData_get_layer_named_for_write(
-        custom_data, eCustomDataType(layer.type), layer.name, element_num);
-
-    const CPPType *type = custom_data_type_to_cpp_type(eCustomDataType(layer.type));
-    if (type == nullptr) {
-      continue;
-    }
-    std::function<void()> tag_modified_fn;
-    if (custom_data_access_.get_tag_modified_function != nullptr) {
-      tag_modified_fn = custom_data_access_.get_tag_modified_function(owner, attribute_id);
-    }
-    GMutableSpan data{*type, layer.data, element_num};
-    return {GVMutableArray::from_span(data), domain_, tag_modified_fn};
-  }
-  return {};
-}
-
-bool CustomDataAttributeProvider::try_delete(void *owner, const StringRef attribute_id) const
-{
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return false;
-  }
-  for (const int i : IndexRange(custom_data->totlayer)) {
-    const CustomDataLayer &layer = custom_data->layers[i];
-    if (this->type_is_supported(eCustomDataType(layer.type)) && layer.name == attribute_id) {
-      CustomData_free_layer(custom_data, eCustomDataType(layer.type), i);
-      if (custom_data_access_.get_tag_modified_function != nullptr) {
-        if (const std::function<void()> fn = custom_data_access_.get_tag_modified_function(
-                owner, attribute_id))
-        {
-          fn();
-        }
-      }
-      return true;
-    }
-  }
-  return false;
-}
-
-bool CustomDataAttributeProvider::try_create(void *owner,
-                                             const StringRef attribute_id,
-                                             const AttrDomain domain,
-                                             const eCustomDataType data_type,
-                                             const AttributeInit &initializer) const
-{
-  if (domain_ != domain) {
-    return false;
-  }
-  if (!this->type_is_supported(data_type)) {
-    return false;
-  }
-  CustomData *custom_data = custom_data_access_.get_custom_data(owner);
-  if (custom_data == nullptr) {
-    return false;
-  }
-  for (const CustomDataLayer &layer : Span(custom_data->layers, custom_data->totlayer)) {
-    if (layer.name == attribute_id) {
-      return false;
-    }
-  }
-  const int element_num = custom_data_access_.get_element_num(owner);
-  add_custom_data_layer_from_attribute_init(
-      attribute_id, *custom_data, data_type, element_num, initializer, {});
-  if (initializer.type != AttributeInit::Type::Construct) {
-    /* Avoid calling update function when values are not default-initialized. Without default
-     * initialization or otherwise meaningful initial values, they should be set elsewhere
-     * anyway, which will cause a separate update tag. */
-    if (custom_data_access_.get_tag_modified_function != nullptr) {
-      if (const std::function<void()> fn = custom_data_access_.get_tag_modified_function(
-              owner, attribute_id))
-      {
-        fn();
-      }
-    }
-  }
-  return true;
-}
-
-bool CustomDataAttributeProvider::foreach_attribute(
-    const void *owner, const FunctionRef<void(const AttributeIter &)> fn) const
-{
-  const CustomData *custom_data = custom_data_access_.get_const_custom_data(owner);
-  if (custom_data == nullptr) {
-    return true;
-  }
-  for (const CustomDataLayer &layer : Span(custom_data->layers, custom_data->totlayer)) {
-    const eCustomDataType cd_type = eCustomDataType(layer.type);
-    if (this->type_is_supported(cd_type)) {
-      const auto get_fn = [&]() {
-        const CPPType *type = custom_data_type_to_cpp_type(cd_type);
-        BLI_assert(type);
-        GSpan data{*type, layer.data, custom_data_access_.get_element_num(owner)};
-        return GAttributeReader{GVArray::from_span(data), domain_, layer.sharing_info};
-      };
-
-      const AttrType data_type = *custom_data_type_to_attr_type(cd_type);
-      AttributeIter iter{layer.name, domain_, data_type, get_fn};
-      fn(iter);
-      if (iter.is_stopped()) {
-        return false;
-      }
-    }
-  }
-  return true;
 }
 
 /* -------------------------------------------------------------------- */

FrenkyBojler/blender-ios

commit 506c492cb1f3d6f6d1dfd15e834e97ef14a1d97f

Fix #153175: mesh.edges.add() creates attribute of type FLOAT_VECTOR
Should be of type INT32_2D.

Caused by 8adba33029f7

Just a copy-paste error regarding the data type (should be `int2`
instead of `float3`)

source/blender/editors/mesh/mesh_data.cc
@@ -724,7 +724,7 @@ static void mesh_add_edges(Mesh *mesh, int len)
       ".select_edge", bke::AttrDomain::Edge);
   select_edge.span.take_back(len).fill(true);
   select_edge.finish();
-  attributes.add<float3>(".edge_verts", bke::AttrDomain::Edge, bke::AttributeInitDefaultValue());
+  attributes.add<int2>(".edge_verts", bke::AttrDomain::Edge, bke::AttributeInitDefaultValue());
 }
 
 static void mesh_add_loops(Mesh *mesh, int len)