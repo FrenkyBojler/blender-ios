ad0d373ac3c6ef88b7f163114a9d84fd775a259f
Wayne Dixon <thewaylow@gmail.com>
2026-01-22 22:17:15 +1100

    Add comment for clarity

--- source/blender/editors/armature/pose_select.cc
+++ source/blender/editors/armature/pose_select.cc
@@ -400,26 +400,30 @@ static void selectconnected_posebonechildren(Object &ob,
                                              bPoseChannel &pose_bone,
                                              const bool extend)
 {
-  animrig::pose_bone_descendent_depth_iterator(*ob.pose, pose_bone, [extend, &pose_bone](bPoseChannel &child) {
-    if (!child.bone) {
-      BLI_assert_unreachable();
-      return false;
-    }
-    if (&child != &pose_bone) {
-      /* Stop when unconnected child is encountered, or when unselectable bone is encountered. */
-      if (!(child.bone->flag & BONE_CONNECTED) || (child.bone->flag & BONE_UNSELECTABLE)) {
-        return false;
-      }
-  }
+  animrig::pose_bone_descendent_depth_iterator(
+      *ob.pose, pose_bone, [extend, &pose_bone](bPoseChannel &child) {
+        if (!child.bone) {
+          BLI_assert_unreachable();
+          return false;
+        }
+        /* Ignore the check if it starts at the root of a chain, this allows the children to be
+         * selected. See Fix: #153068 */
+        if (&child != &pose_bone) {
+          /* Stop when unconnected child is encountered, or when unselectable bone is encountered.
+           */
+          if (!(child.bone->flag & BONE_CONNECTED) || (child.bone->flag & BONE_UNSELECTABLE)) {
+            return false;
+          }
+        }
 
-    if (extend) {
-      animrig::bone_deselect(&child);
-    }
-    else {
-      animrig::bone_select(&child);
-    }
-    return true;
-  });
+        if (extend) {
+          animrig::bone_deselect(&child);
+        }
+        else {
+          animrig::bone_select(&child);
+        }
+        return true;
+      });
 }
 
 /* within active object context */
@@ -485,7 +489,8 @@ void POSE_OT_select_linked_pick(wmOperatorType *ot)
   /* identifiers */
   ot->name = "Select Connected";
   ot->idname = "POSE_OT_select_linked_pick";
-  ot->description = "Select bones linked by connected parent/child relationships under the mouse cursor";
+  ot->description =
+      "Select bones linked by connected parent/child relationships under the mouse cursor";
 
   /* callbacks */
   /* leave 'exec' unset */
@@ -548,7 +553,8 @@ void POSE_OT_select_linked(wmOperatorType *ot)
   /* identifiers */
   ot->name = "Select Connected";
   ot->idname = "POSE_OT_select_linked";
-  ot->description = "Select all bones linked by connected parent/child relationships from the current selection";
+  ot->description =
+      "Select all bones linked by connected parent/child relationships from the current selection";
 
   /* callbacks */
   ot->exec = pose_select_linked_exec;
