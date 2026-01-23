blender/blender

commit 029beb67c464306f0020f10feabf69b28939cf41

Fix #151083: Separate by Material breaks custom normals
This change builds upon 93c8955a72 (uses the
use`BM_custom_loop_normals_to_vector_layer` /
`BM_custom_loop_normals_from_vector_layer` pair of calls).

Very similar to 67c7fe6ad62e

Pull Request: https://projects.blender.org/blender/blender/pulls/151125

source/blender/editors/mesh/editmesh_tools.cc
@@ -4066,7 +4066,14 @@ static Base *mesh_separate_tagged(
   BMesh *bm_new = BM_mesh_create(&bm_mesh_allocsize_default, &create_params);
   BM_mesh_elem_toolflags_ensure(bm_new); /* Needed for 'duplicate' BMO. */
 
-  BM_mesh_copy_init_customdata(bm_new, bm_old, &bm_mesh_allocsize_default);
+  const bool use_custom_normals = (bm_old->lnor_spacearr != nullptr);
+  if (use_custom_normals) {
+    /* Needed so the temporary normal layer is copied too. */
+    BM_mesh_copy_init_customdata_all_layers(bm_new, bm_old, BM_ALL, &bm_mesh_allocsize_default);
+  }
+  else {
+    BM_mesh_copy_init_customdata(bm_new, bm_old, &bm_mesh_allocsize_default);
+  }
 
   /* Take into account user preferences for duplicating actions. */
   const eDupli_ID_Flags dupflag = eDupli_ID_Flags(USER_DUP_MESH | (U.dupflag & USER_DUP_ACT));
@@ -4100,7 +4107,12 @@ static Base *mesh_separate_tagged(
    * since de-selecting all skips selection flushing logic */
   BM_mesh_elem_hflag_disable_all(bm_old, BM_VERT | BM_EDGE | BM_FACE, BM_ELEM_SELECT, false);
 
-  BM_mesh_normals_update(bm_new);
+  if (use_custom_normals) {
+    BM_custom_loop_normals_from_vector_layer(bm_new, false);
+  }
+  else {
+    BM_mesh_normals_update(bm_new);
+  }
 
   BMeshToMeshParams to_mesh_params{};
   BM_mesh_bm_to_me(bmain, bm_new, static_cast<Mesh *>(base_new->object->data), &to_mesh_params);
@@ -4188,7 +4200,13 @@ static bool mesh_separate_selected(
   BM_mesh_elem_hflag_enable_test(
       bm_old, BM_FACE | BM_EDGE | BM_VERT, BM_ELEM_TAG, true, false, BM_ELEM_SELECT);
 
-  return (mesh_separate_tagged(bmain, scene, view_layer, base_old, bm_old) != nullptr);
+  BM_custom_loop_normals_to_vector_layer(bm_old);
+
+  Base *base_new = mesh_separate_tagged(bmain, scene, view_layer, base_old, bm_old);
+
+  BM_custom_loop_normals_from_vector_layer(bm_old, false);
+
+  return (base_new != nullptr);
 }
 
 /**
@@ -4252,6 +4270,8 @@ static bool mesh_separate_material(
   BMIter iter;
   bool result = false;
 
+  BM_custom_loop_normals_to_vector_layer(bm_old);
+
   while ((f_cmp = static_cast<BMFace *>(BM_iter_at_index(bm_old, BM_FACES_OF_MESH, nullptr, 0)))) {
     Base *base_new;
     const short mat_nr = f_cmp->mat_nr;
@@ -4294,6 +4314,8 @@ static bool mesh_separate_material(
 
     result |= (base_new != nullptr);
   }
+
+  BM_custom_loop_normals_from_vector_layer(bm_old, false);
 
   return result;
 }

FrenkyBojler/blender-ios

commit c45991ceb2ad6347c2a42626acb354106634010b

Fix #153227: Separate By Selection issue when custom normals are used
Caused by 029beb67c4

Resulting mesh would be the selection (plus an unwanted copy of all
vertices and edges).

Reason for the bug the placement of the
`BM_custom_loop_normals_to_vector_layer` call in
`mesh_separate_selected` (compared to `mesh_separate_material` so for
#151083, everything worked as expected). In the former, it was placed
**after** clearing all bmesh tags with `BM_mesh_elem_hflag_disable_all`
in the later it was placed **before**.

`BM_custom_loop_normals_to_vector_layer` eventually calls
`bm_mesh_loops_calc_normals` which uses `BM_ELEM_TAG` internally, so
doing that after we cleared all tags messes up the tagging that is
actually used in the logic of `mesh_separate_selected`.

To resolve, just place `BM_custom_loop_normals_to_vector_layer` at the
very beginning (then clearing all flags happens afterwards and the logic
using flags is now unaffected -- as it was prior to 029beb67c4).

NOTE: 029beb67c4 unfortunately ended up in LTS, so needs to be
backported (quickly)

source/blender/editors/mesh/editmesh_tools.cc
@@ -4264,14 +4264,14 @@ static Base *mesh_separate_arrays(Main *bmain,
 static bool mesh_separate_selected(
     Main *bmain, Scene *scene, ViewLayer *view_layer, Base *base_old, BMesh *bm_old)
 {
+  BM_custom_loop_normals_to_vector_layer(bm_old);
+
   /* we may have tags from previous operators */
   BM_mesh_elem_hflag_disable_all(bm_old, BM_FACE | BM_EDGE | BM_VERT, BM_ELEM_TAG, false);
 
   /* sel -> tag */
   BM_mesh_elem_hflag_enable_test(
       bm_old, BM_FACE | BM_EDGE | BM_VERT, BM_ELEM_TAG, true, false, BM_ELEM_SELECT);
-
-  BM_custom_loop_normals_to_vector_layer(bm_old);
 
   Base *base_new = mesh_separate_tagged(bmain, scene, view_layer, base_old, bm_old);
 