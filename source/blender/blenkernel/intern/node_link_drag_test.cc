07e08cc7b53cc6977e12eab30b53ba14402ddf69
Lukas Tönne <lukas@blender.org>
2026-01-24 06:35:54 +0100

    progress

--- source/blender/blenkernel/intern/node_link_drag_test.cc
+++ source/blender/blenkernel/intern/node_link_drag_test.cc
@@ -2,48 +2,84 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BLI_listbase_iterator.hh"
+#include "BLI_path_utils.hh"
+
 #include "BKE_idtype.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_main.hh"
+
+#include "BLO_readfile.hh"
+
+#include "DNA_screen_types.h"
+#include "DNA_space_types.h"
+#include "DNA_windowmanager_types.h"
 
 #include "NOD_socket_search_link.hh"
 
 #include "CLG_log.h"
 
+#include "tests/blendfile_loading_base_test.h"
+
 #include "testing/testing.h"
 
 namespace blender::bke::tests {
 
-class NodeLinkDragTest : public testing::Test {
+class NodeLinkDragTest : public BlendfileLoadingBaseTest {
  public:
-  Main *bmain;
+  // Main *bmain;
 
-  static void SetUpTestSuite()
-  {
-    CLG_init();
-    BKE_idtype_init();
-  }
+  // static void SetUpTestSuite()
+  // {
+  //   CLG_init();
+  //   BKE_idtype_init();
+  // }
 
-  static void TearDownTestSuite()
-  {
-    CLG_exit();
-  }
+  // static void TearDownTestSuite()
+  // {
+  //   CLG_exit();
+  // }
 
   void SetUp() override
   {
-    bmain = BKE_main_new();
+    BlendfileLoadingBaseTest::SetUp();
   }
 
   void TearDown() override
   {
-    BKE_main_free(bmain);
+    BlendfileLoadingBaseTest::TearDown();
   }
 };
 
+static SpaceNode *find_space_node(const Main &bmain, const StringRef tree_idname)
+{
+  for (bScreen &screen : bmain.screens) {
+    for (ScrArea &area : screen.areabase) {
+      for (SpaceLink &sl : area.spacedata) {
+        if (sl.spacetype == SPACE_NODE) {
+          SpaceNode &snode = reinterpret_cast<SpaceNode &>(sl);
+          if (tree_idname == snode.tree_idname) {
+            return &snode;
+          }
+        }
+      }
+    }
+  }
+  return nullptr;
+}
+
 TEST_F(NodeLinkDragTest, NodeLinkDrag)
 {
-  nodes::GatherLinkSearchOpParams params{*node_type, snode, node_tree, socket, search_link_ops};
-  node_type->gather_link_search_ops(params);
+  /* Load the test blend file. */
+  if (!blendfile_load("node_group" SEP_STR "link_drag_search.blend")) {
+    return;
+  }
+
+  SpaceNode *snode = find_space_node(*bfile->main, )
+
+  // Main *bmain;
+  // nodes::GatherLinkSearchOpParams params{*node_type, snode, node_tree, socket,
+  // search_link_ops}; node_type->gather_link_search_ops(params);
 }
 
 }  // namespace blender::bke::tests
