blender/blender

commit 601462b45a79f9cbe24fecf995579cbf325cb67d

Shape Keys: Py API function to get selected keyblocks
Selecting multiple keyblocks are now possible. It has been mentioned
before to expose an API function that returns all selected shape keys.
Now created `shape_keys_selected()`, it can be accessed from object.

Also added `select` property on KeyBlock in RNA.

Example:
```
selected_keys = C.active_object.shape_keys_selected()
selected_keys[0].select = False

```

Resolves #151712

Pull Request: https://projects.blender.org/blender/blender/pulls/152285

source/blender/makesrna/intern/rna_object_api.cc
@@ -526,6 +526,22 @@ static void rna_Object_shape_key_clear(Object *ob, Main *bmain)
   WM_main_add_notifier(NC_OBJECT | ND_DRAW, ob);
 }
 
+static CollectionVector rna_Object_shape_keys_selected(Object *ob)
+{
+  Key *key = BKE_key_from_object(ob);
+  if (key == nullptr) {
+    return CollectionVector();
+  }
+
+  CollectionVector selected_keys;
+  for (KeyBlock &kb : key->block) {
+    if (kb.flag & KEYBLOCK_SEL) {
+      selected_keys.items.append(RNA_pointer_create_discrete(&key->id, &RNA_ShapeKey, &kb));
+    }
+  }
+  return selected_keys;
+}
+
 #  if 0
 static void rna_Mesh_assign_verts_to_group(
     Object *ob, bDeformGroup *group, int *indices, int totindex, float weight, int assignmode)
@@ -1134,6 +1150,13 @@ void RNA_api_object(StructRNA *srna)
   func = RNA_def_function(srna, "shape_key_clear", "rna_Object_shape_key_clear");
   RNA_def_function_ui_description(func, "Remove all Shape Keys from this object");
   RNA_def_function_flag(func, FUNC_USE_MAIN);
+
+  func = RNA_def_function(srna, "shape_keys_selected", "rna_Object_shape_keys_selected");
+  RNA_def_function_ui_description(func, "Return selected shape keys");
+
+  parm = RNA_def_property(func, "keyblocks", PROP_COLLECTION, PROP_NONE);
+  RNA_def_property_struct_type(parm, "ShapeKey");
+  RNA_def_function_return(func, parm);
 
   /* Ray Cast */
   func = RNA_def_function(srna, "ray_cast", "rna_Object_ray_cast");

source/blender/makesrna/intern/rna_key.cc
@@ -982,6 +982,10 @@ static void rna_def_keyblock(BlenderRNA *brna)
   RNA_def_property_update(prop, 0, "rna_Key_update_data");
   RNA_def_struct_name_property(srna, prop);
 
+  prop = RNA_def_property(srna, "select", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_sdna(prop, nullptr, "flag", KEYBLOCK_SEL);
+  RNA_def_property_ui_text(prop, "Select", "Shape key selection state");
+
   /* keys need to be sorted to edit this */
   prop = RNA_def_property(srna, "frame", PROP_FLOAT, PROP_TIME);
   RNA_def_property_clear_flag(prop, PROP_EDITABLE);

tests/python/bl_animation_shapekey.py
@@ -133,6 +133,35 @@ class LatticeShapekeyTest(unittest.TestCase):
         # Incomplete test, same issue as CurveShapekeyTest.
 
 
+class ShapekeySelectionTest(unittest.TestCase):
+    def setUp(self):
+        bpy.ops.wm.read_homefile(use_factory_startup=True)
+        self.cube = bpy.data.objects["Cube"]
+        self.cube.shape_key_add(name="Base")
+        self.key_1 = self.cube.shape_key_add(name="one")
+        self.key_2 = self.cube.shape_key_add(name="two")
+        self.key_3 = self.cube.shape_key_add(name="three")
+
+    def test_shape_keys_selected(self):
+        selected_keys = self.cube.shape_keys_selected()
+        # None selected
+        self.assertEqual(len(selected_keys), 0)
+
+        # One shape key selected
+        self.key_2.select = True
+        selected_keys = self.cube.shape_keys_selected()
+        self.assertEqual(selected_keys[0].name, "two")
+
+        # All shape keys selected
+        self.key_1.select = True
+        self.key_3.select = True
+        selected_keys = self.cube.shape_keys_selected()
+        self.assertEqual(len(selected_keys), 3)
+        self.assertEqual(selected_keys[0].name, "one")
+        self.assertEqual(selected_keys[1].name, "two")
+        self.assertEqual(selected_keys[2].name, "three")
+
+
 def main():
     global args
     import argparse

FrenkyBojler/blender-ios

commit bdc88449adb0a37606e2c86a692816249c095dd2

Fix: Build error
Caused by 601462b45a79f9cbe24fecf995579cbf325cb67d

source/blender/makesrna/intern/rna_object_api.cc
@@ -536,7 +536,7 @@ static CollectionVector rna_Object_shape_keys_selected(Object *ob)
   CollectionVector selected_keys;
   for (KeyBlock &kb : key->block) {
     if (kb.flag & KEYBLOCK_SEL) {
-      selected_keys.items.append(RNA_pointer_create_discrete(&key->id, &RNA_ShapeKey, &kb));
+      selected_keys.items.append(RNA_pointer_create_discrete(&key->id, RNA_ShapeKey, &kb));
     }
   }
   return selected_keys;