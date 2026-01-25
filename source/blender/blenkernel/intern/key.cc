blender/blender

commit 6e544314a6833d7f666af2724800018e1c1e6818

Fix: Warning in shapekey code
This seems to be a peculiar thing about C++.
Following this discussion [1] on stackoverflow
it seems that you cannot use `auto` and a lambda capture together.
However the same thing works if you explicitly make it a `pair`.

[1] https://stackoverflow.com/questions/46114214/lambda-implicit-capture-fails-with-variable-declared-from-structured-binding

Pull Request: https://projects.blender.org/blender/blender/pulls/152816

source/blender/blenkernel/intern/key.cc
@@ -681,7 +681,10 @@ static void key_evaluate_relative_float3(Key *key,
   /* Creates the basis values of the reference key in target_data. */
   copy_key_float3(vertex_count, key, active_keyblock, key->refkey, target_data);
 
-  for (const auto [keyblock_index, kb] : key->block.enumerate()) {
+  /* Cannot use auto [keyblock_index, kb] here because that would throw a warning at the
+   * parallel_for. */
+  for (std::pair<int, KeyBlock> enumerator : key->block.enumerate()) {
+    KeyBlock &kb = enumerator.second;
     if (&kb == key->refkey) {
       continue;
     }
@@ -699,7 +702,7 @@ static void key_evaluate_relative_float3(Key *key,
       continue;
     }
 
-    const float *weights = per_keyblock_weights ? per_keyblock_weights[keyblock_index] : nullptr;
+    const float *weights = per_keyblock_weights ? per_keyblock_weights[enumerator.first] : nullptr;
 
     char *freefrom = nullptr;
     const float *from = reinterpret_cast<float *>(

FrenkyBojler/blender-ios

commit 79bea28d150aa734eb418718440da85698006399

Fix: Shape Keys: broken edit mode hack due to KeyBlock copy
Iterating with a value pair caused a copy of the KeyBlock struct.
This broke pointer comparisons that identified the active keyblock.

This issue was introduced in 6e544314a68.

source/blender/blenkernel/intern/key.cc
@@ -683,7 +683,7 @@ static void key_evaluate_relative_float3(Key *key,
 
   /* Cannot use auto [keyblock_index, kb] here because that would throw a warning at the
    * parallel_for. */
-  for (std::pair<int, KeyBlock> enumerator : key->block.enumerate()) {
+  for (std::pair<int, KeyBlock &> enumerator : key->block.enumerate()) {
     KeyBlock &kb = enumerator.second;
     if (&kb == key->refkey) {
       continue;