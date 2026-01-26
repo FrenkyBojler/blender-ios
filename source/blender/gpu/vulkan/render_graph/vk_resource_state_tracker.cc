d54c8966244c6510f06c82e6ce4b181c6210e73c
Mark van de Ruit <mark@blender.org>
2026-01-26 10:29:58 +0100

    Removed incomplete `texture_view_test.cc`

--- source/blender/gpu/vulkan/render_graph/vk_resource_state_tracker.cc
+++ source/blender/gpu/vulkan/render_graph/vk_resource_state_tracker.cc
@@ -49,8 +49,9 @@ void VKResourceStateTracker::add_image(VkImage vk_image,
   resource.image.vk_image = vk_image;
   resource.image.use_subresource_tracking = use_subresource_tracking;
   resource.barrier_state = barrier_state;
+
 #ifndef NDEBUG
-  if ((G.debug & G_DEBUG_GPU) && name) {
+  if (name) {
     resource.name = name;
   }
 #endif
@@ -91,8 +92,9 @@ void VKResourceStateTracker::add_buffer(VkBuffer vk_buffer, const char *name)
   resource.type = VKResourceType::BUFFER;
   resource.buffer.vk_buffer = vk_buffer;
   resource.stamp = 0;
+
 #ifndef NDEBUG
-  if ((G.debug & G_DEBUG_GPU) && name) {
+  if (name) {
     resource.name = name;
   }
 #endif
