139efae415ab3e99473b0961276b5816c5196a70
Miguel Pozo <pragma37@gmail.com>
2026-01-26 13:30:24 +0100

    Disable empty attachments

--- source/blender/gpu/intern/gpu_framebuffer.cc
+++ source/blender/gpu/intern/gpu_framebuffer.cc
@@ -151,6 +151,7 @@ void FrameBuffer::subpass_transition(const GPUAttachmentState depth_attachment_s
       set_color_attachment_bit(type, color_attachment_states[i] == GPU_ATTACHMENT_WRITE);
     }
     else {
+      set_color_attachment_bit(type, false);
       BLI_assert(i >= color_attachment_states.size() ||
                  color_attachment_states[i] != GPU_ATTACHMENT_READ);
     }
