23dbd19bd1275f303b8f21df3af6a096efe57ed8
Jacques Lucke <jacques@blender.org>
2026-01-26 16:56:44 +0100

    use min x/y/z the way I expected

--- source/blender/nodes/geometry/nodes/node_geo_cube_grid_topology.cc
+++ source/blender/nodes/geometry/nodes/node_geo_cube_grid_topology.cc
@@ -25,24 +25,24 @@ static void node_declare(NodeDeclarationBuilder &b)
       .default_value(float3(1.0f))
       .description("Maximum boundary of the grid (world space)");
 
-  b.add_input<decl::Int>("Min X").default_value(0).min(0).description(
+  b.add_input<decl::Int>("Min X").default_value(0).description(
       "Minimum coordinate in X axis (grid index space)");
-  b.add_input<decl::Int>("Min Y").default_value(0).min(0).description(
+  b.add_input<decl::Int>("Min Y").default_value(0).description(
       "Minimum coordinate in Y axis (grid index space)");
-  b.add_input<decl::Int>("Min Z").default_value(0).min(0).description(
+  b.add_input<decl::Int>("Min Z").default_value(0).description(
       "Minimum coordinate in Z axis (grid index space)");
 
   b.add_input<decl::Int>("Resolution X")
       .default_value(32)
-      .min(2)
+      .min(1)
       .description("Number of voxels in the X axis");
   b.add_input<decl::Int>("Resolution Y")
       .default_value(32)
-      .min(2)
+      .min(1)
       .description("Number of voxels in the Y axis");
   b.add_input<decl::Int>("Resolution Z")
       .default_value(32)
-      .min(2)
+      .min(1)
       .description("Number of voxels in the Z axis");
 
   b.add_output<decl::Bool>("Topology")
@@ -95,9 +95,9 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   auto openvdb_grid = GridType::create(false /* background */);
 
-  const int3 grid_min = int3(params.extract_input<int>("Min X"),
-                             params.extract_input<int>("Min Y"),
-                             params.extract_input<int>("Min Z"));
+  const int3 grid_min_inclusive = int3(params.extract_input<int>("Min X"),
+                                       params.extract_input<int>("Min Y"),
+                                       params.extract_input<int>("Min Z"));
   const int3 resolution = int3(params.extract_input<int>("Resolution X"),
                                params.extract_input<int>("Resolution Y"),
                                params.extract_input<int>("Resolution Z"));
@@ -105,18 +105,13 @@ static void node_geo_exec(GeoNodeExecParams params)
   const float3 bounds_min = params.extract_input<float3>("Bounds Min");
   const float3 bounds_max = params.extract_input<float3>("Bounds Max");
 
-  if (resolution.x < 2 || resolution.y < 2 || resolution.z < 2) {
-    params.error_message_add(NodeWarningType::Error, TIP_("Resolution must be greater than 1"));
-    params.set_default_remaining_outputs();
-    return;
-  }
-  if (grid_min.x < 0 || grid_min.y < 0 || grid_min.z < 0) {
-    params.error_message_add(NodeWarningType::Error, TIP_("Grid minimum must be positive"));
+  if (resolution.x <= 0 || resolution.y <= 0 || resolution.z <= 0) {
+    params.error_message_add(NodeWarningType::Warning, TIP_("Resolution must be positive"));
     params.set_default_remaining_outputs();
     return;
   }
 
-  if (bounds_min.x == bounds_max.x || bounds_min.y == bounds_max.y || bounds_min.z == bounds_max.z)
+  if (bounds_min.x >= bounds_max.x || bounds_min.y >= bounds_max.y || bounds_min.z >= bounds_max.z)
   {
     params.error_message_add(NodeWarningType::Error,
                              TIP_("Bounding box volume must be greater than 0"));
@@ -124,12 +119,13 @@ static void node_geo_exec(GeoNodeExecParams params)
     return;
   }
 
-  const int3 grid_max = grid_min + resolution - 1;
-  openvdb::math::CoordBBox bbox({grid_min.x, grid_min.y, grid_min.z},
-                                {grid_max.x, grid_max.y, grid_max.z});
+  const int3 grid_max_inclusive = grid_min_inclusive + resolution - 1;
+  const openvdb::math::CoordBBox bbox(
+      {grid_min_inclusive.x, grid_min_inclusive.y, grid_min_inclusive.z},
+      {grid_max_inclusive.x, grid_max_inclusive.y, grid_max_inclusive.z});
   openvdb_grid->tree().denseFill(bbox, true, /*active=*/true);
 
-  const double3 scale_fac = double3(bounds_max - bounds_min) / double3(resolution + grid_min - 1);
+  const double3 scale_fac = double3(bounds_max - bounds_min) / double3(resolution);
   if (!BKE_volume_voxel_size_valid(float3(scale_fac))) {
     params.error_message_add(NodeWarningType::Warning,
                              TIP_("Volume scale is lower than permitted by OpenVDB"));
@@ -138,8 +134,9 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
 
   openvdb_grid->transform().postScale(openvdb::math::Vec3d(scale_fac.x, scale_fac.y, scale_fac.z));
+  const double3 translation = double3(bounds_min) - scale_fac * double3(grid_min_inclusive);
   openvdb_grid->transform().postTranslate(
-      openvdb::math::Vec3d(bounds_min.x, bounds_min.y, bounds_min.z));
+      openvdb::math::Vec3d(translation.x, translation.y, translation.z));
 
   bke::VolumeGrid<bool> topology_grid(std::move(openvdb_grid));
   params.set_output("Topology", bke::GVolumeGrid(std::move(topology_grid)));
