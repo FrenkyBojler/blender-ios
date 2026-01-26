622356d4daa5cb95739e726c77406066c8cae84a
Nikita Sirgienko <nikita.sirgienko@intel.com>
2026-01-26 08:44:00 +0100

    Cycles: Do not install DPC++ debug libraries for Release builds
    Initially, DPC++ runtime were providing only release and debug
    SYCL runtime library, with other libraries being release-only.
    Which is why Blender cmake code for DPC++ runtime installation
    were written with such assumption. But now, DPC++ runtime is
    providing all version of its libraries as either release or debug,
    with corresponding dependencies - e.g. release libraries are
    depending on other release libraries, and debug libraries are
    depending on other debug libraries (at least in theory, but
    there is an existing bug which we needed to workaround in
    regards to this, at least for now). So before these changes,
    Blender would install debug version of DPC++ libraries in both
    Debug and Release modes, which is not needed. And with these
    changes, this unintended behavior is addressed.

--- build_files/cmake/platform/platform_win32.cmake
+++ build_files/cmake/platform/platform_win32.cmake
@@ -1320,8 +1320,45 @@ if(WITH_CYCLES AND (WITH_CYCLES_DEVICE_ONEAPI OR (WITH_CYCLES_EMBREE AND EMBREE_
     ${SYCL_ROOT_DIR}/bin/pi_*.dll
     ${SYCL_ROOT_DIR}/bin/ur_*.dll
   )
+  # In order to avoid duplicated support of the same devices throw OpenCL and Level0,
+  # as well as some problems with OpenCL initialisation, which may cause problems for
+  # Blender on some platforms, we are avoiding OpenCL adapter all together.
   list(REMOVE_ITEM _sycl_pi_runtime_libraries_glob "${SYCL_ROOT_DIR}/bin/pi_opencl.dll")
-  list(APPEND _sycl_runtime_libraries ${_sycl_pi_runtime_libraries_glob})
+  list(REMOVE_ITEM _sycl_pi_runtime_libraries_glob "${SYCL_ROOT_DIR}/bin/pi_opencld.dll")
+  list(REMOVE_ITEM _sycl_pi_runtime_libraries_glob "${SYCL_ROOT_DIR}/bin/ur_adapter_opencl.dll")
+  list(REMOVE_ITEM _sycl_pi_runtime_libraries_glob "${SYCL_ROOT_DIR}/bin/ur_adapter_opencld.dll")
+
+  foreach(sycl_pi_runtime_library IN LISTS _sycl_pi_runtime_libraries_glob)
+    # We do not know, which library we would discover first, debug or release, so we check for both.
+    string(REPLACE ".dll" "d.dll" sycl_pi_runtime_library_debug ${sycl_pi_runtime_library})
+    # In case we are processing release version (no "d.dll" to replace), then
+    # sycl_pi_runtime_library_release will be identical to sycl_pi_runtime_library,
+    # there is a safe guard against it below.
+    string(REPLACE "d.dll" ".dll" sycl_pi_runtime_library_release ${sycl_pi_runtime_library})
+
+    list(FIND _sycl_pi_runtime_libraries_glob ${sycl_pi_runtime_library_debug} debug_index)
+    list(FIND _sycl_pi_runtime_libraries_glob ${sycl_pi_runtime_library_release} release_index)
+    if(NOT debug_index EQUAL -1)
+      set (sycl_pi_runtime_library_release ${sycl_pi_runtime_library})
+    elseif(NOT release_index EQUAL -1 AND NOT sycl_pi_runtime_library_release STREQUAL sycl_pi_runtime_library)
+      set (sycl_pi_runtime_library_debug ${sycl_pi_runtime_library})
+    else()
+      # If there is no debug pair version of the library, then we are assuming
+      # that this dll dependency is unique, and should be just added as both
+      # release and debug dependency.
+      set (sycl_pi_runtime_library_release ${sycl_pi_runtime_library})
+      set (sycl_pi_runtime_library_debug ${sycl_pi_runtime_library})
+    endif()
+    list(FIND _sycl_runtime_libraries ${sycl_pi_runtime_library_release} found_index)
+    if (found_index EQUAL -1)
+      list(APPEND _sycl_runtime_libraries RELEASE ${sycl_pi_runtime_library_release})
+      list(APPEND _sycl_runtime_libraries DEBUG ${sycl_pi_runtime_library_debug})
+      # NOTE(Sirgienko) Due to the issue in DPC++ compilation process, debug builds
+      # of DPC++ runtime link the wrong version of the library. Because of this,
+      # we need temporary put release files as debug dependencies as well.
+      list(APPEND _sycl_runtime_libraries DEBUG ${sycl_pi_runtime_library_release})
+    endif()
+  endforeach()
   unset(_sycl_pi_runtime_libraries_glob)
 
   list(APPEND PLATFORM_BUNDLED_LIBRARIES ${_sycl_runtime_libraries})
