e25c8248f05a4e9b5930ca5fad5899fdfce44e81
Lukas Tönne <lukas@blender.org>
2026-01-27 12:52:27 +0100

    Fix socket availability in the Metallic BSDF node.

--- source/blender/nodes/shader/nodes/node_shader_bsdf_metallic.cc
+++ source/blender/nodes/shader/nodes/node_shader_bsdf_metallic.cc
@@ -20,21 +20,25 @@ static void node_declare(NodeDeclarationBuilder &b)
 
   b.add_input<decl::Color>("Base Color")
       .default_value({0.617f, 0.577f, 0.540f, 1.0f})
-      .description("Color of the material");
+      .description("Color of the material")
+      .make_available([](bNode &node) { node.custom2 = SHD_CONDUCTOR_F82; });
   b.add_input<decl::Color>("Edge Tint")
       .default_value({0.695f, 0.726f, 0.770f, 1.0f})
       .description(
-          "Tint reflection at near-grazing incidence to simulate complex index of refraction");
+          "Tint reflection at near-grazing incidence to simulate complex index of refraction")
+      .make_available([](bNode &node) { node.custom2 = SHD_CONDUCTOR_F82; });
   b.add_input<decl::Vector>("IOR")
       .default_value({2.757f, 2.513f, 2.231f})
       .min(0.0f)
       .max(100.0f)
-      .description("Real part of the conductor's refractive index, often called n");
+      .description("Real part of the conductor's refractive index, often called n")
+      .make_available([](bNode &node) { node.custom2 = SHD_PHYSICAL_CONDUCTOR; });
   b.add_input<decl::Vector>("Extinction")
       .default_value({3.867f, 3.404f, 3.009f})
       .min(0.0f)
       .max(100.0f)
-      .description("Imaginary part of the conductor's refractive index, often called k");
+      .description("Imaginary part of the conductor's refractive index, often called k")
+      .make_available([](bNode &node) { node.custom2 = SHD_PHYSICAL_CONDUCTOR; });
   b.add_input<decl::Float>("Roughness")
       .default_value(0.5f)
       .min(0.0f)
