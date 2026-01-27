6abd8dd21b5446801af63d8e3586b8f9508a5ce1
Hans Goudey <hans@blender.org>
2026-01-27 09:59:18 -0500

    Fix

--- source/blender/nodes/geometry/include/NOD_geo_closure_to_list.hh
+++ source/blender/nodes/geometry/include/NOD_geo_closure_to_list.hh
@@ -51,7 +51,7 @@ struct ClosureToListItemsAccessor : public socket_items::SocketItemsAccessorDefa
 
   static void destruct_item(GeometryNodeClosureToListItem *item)
   {
-    MEM_SAFE_FREE(item->name);
+    MEM_SAFE_DELETE(item->name);
   }
 
   static void blend_write_item(BlendWriter *writer, const ItemT &item);
