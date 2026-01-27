blender/blender

commit 5cdc3e0ccf24bc784228cec9dcf959fa4e43eec7

Cleanup: Remove `VertProjHandle` and related functions
The data was only ever initialized and freed, never updated or accessed
while in vertex paint mode.

Pull Request: https://projects.blender.org/blender/blender/pulls/152872

source/blender/editors/sculpt_paint/paint_vertex.cc
@@ -951,7 +951,6 @@ struct VPaintData : public PaintModeData {
 
   ColorPaint4f paintcol;
 
-  VertProjHandle *vp_handle;
   /**
    * Owned by #vp_handle.
    * \todo Look into replacing this with just using the evaluated/deform positions.
@@ -970,13 +969,6 @@ struct VPaintData : public PaintModeData {
   /* For brushes that don't use accumulation, a temporary holding array */
   GArray<> prev_colors;
   GArray<> stroke_buffer;
-
-  ~VPaintData() override
-  {
-    if (vp_handle) {
-      ED_vpaint_proj_handle_free(vp_handle);
-    }
-  }
 };
 
 static std::unique_ptr<VPaintData> vpaint_init_vpaint(wmOperator *op,
@@ -1012,14 +1004,6 @@ static std::unique_ptr<VPaintData> vpaint_init_vpaint(wmOperator *op,
     attribute.materialize(vpd->smear.color_prev.data());
 
     vpd->smear.color_curr = vpd->smear.color_prev;
-  }
-
-  /* Create projection handle */
-  if (vpd->is_texbrush) {
-    ob.runtime->sculpt_session->building_vp_handle = true;
-    vpd->vp_handle = ED_vpaint_proj_handle_create(
-        depsgraph, scene, ob, vpd->vert_positions, vpd->vert_normals);
-    ob.runtime->sculpt_session->building_vp_handle = false;
   }
 
   if (!vwpaint::brush_use_accumulate(vp)) {

source/blender/blenkernel/BKE_paint.hh
@@ -501,10 +501,6 @@ struct SculptSession : NonCopyable, NonMovable {
   } mode = {};
   eObjectMode mode_type;
 
-  /* This flag prevents bke::pbvh::Tree from being freed when creating the vp_handle for
-   * texture paint. */
-  bool building_vp_handle = false;
-
   /**
    * ID data is older than sculpt-mode data.
    * Set #Main.is_memfile_undo_flush_needed when enabling.

source/blender/blenkernel/intern/paint.cc
@@ -2590,8 +2590,6 @@ static void sculpt_update_object(Depsgraph *depsgraph,
 
   ss.deform_modifiers_active = sculpt_modifiers_active(scene, sd, ob);
 
-  ss.building_vp_handle = false;
-
   ss.shapekey_active = (mmd == nullptr) ? BKE_keyblock_from_object(ob) : nullptr;
 
   /* NOTE: Weight pPaint require mesh info for loop lookup, but it never uses multires code path,
@@ -2712,9 +2710,6 @@ void BKE_sculpt_update_object_before_eval(Object *ob_eval)
   Object *ob_orig = DEG_get_original(ob_eval);
   SculptSession *ss = ob_orig->runtime->sculpt_session;
   if (!ss) {
-    return;
-  }
-  if (ss->building_vp_handle) {
     return;
   }
 

source/blender/editors/sculpt_paint/CMakeLists.txt
@@ -79,7 +79,6 @@ set(SRC
   paint_utils.cc
   paint_vertex.cc
   paint_vertex_color_ops.cc
-  paint_vertex_proj.cc
   paint_vertex_weight_ops.cc
   paint_vertex_weight_utils.cc
   paint_weight.cc

source/blender/editors/sculpt_paint/paint_intern.hh
@@ -429,20 +429,6 @@ void PAINT_OT_weight_from_bones(wmOperatorType *ot);
 void PAINT_OT_weight_sample(wmOperatorType *ot);
 void PAINT_OT_weight_sample_group(wmOperatorType *ot);
 
-/* `paint_vertex_proj.cc` */
-
-VertProjHandle *ED_vpaint_proj_handle_create(Depsgraph &depsgraph,
-                                             Scene &scene,
-                                             Object &ob,
-                                             Span<float3> &r_vert_positions,
-                                             Span<float3> &r_vert_normals);
-void ED_vpaint_proj_handle_update(Depsgraph *depsgraph,
-                                  VertProjHandle *vp_handle,
-                                  /* runtime vars */
-                                  ARegion *region,
-                                  const float mval_fl[2]);
-void ED_vpaint_proj_handle_free(VertProjHandle *vp_handle);
-
 /* `paint_image.cc` */
 
 struct ImagePaintPartialRedraw {

source/blender/editors/sculpt_paint/paint_vertex_proj.cc
@@ -1,201 +1,0 @@
-/* SPDX-FileCopyrightText: 2013 Blender Authors
- *
- * SPDX-License-Identifier: GPL-2.0-or-later */
-
-/** \file
- * \ingroup edsculpt
- *
- * Utility functions for getting vertex locations while painting
- * (since they may be instanced multiple times in an evaluated mesh)
- */
-
-#include "MEM_guardedalloc.h"
-
-#include "DNA_mesh_types.h"
-#include "DNA_object_types.h"
-
-#include "BKE_mesh_iterators.hh"
-#include "BKE_object.hh"
-
-#include "BLI_listbase.h"
-#include "BLI_math_vector.h"
-
-#include "DEG_depsgraph_query.hh"
-
-#include "ED_view3d.hh"
-
-#include "paint_intern.hh" /* own include */
-
-namespace blender {
-
-/* Opaque Structs for internal use */
-
-/* stored while painting */
-struct VertProjHandle {
-  Array<float3> vert_positions;
-  Array<float3> vert_normals;
-
-  bool use_update;
-
-  /* use for update */
-  Array<float> dists_sq;
-
-  Object *ob;
-  Scene *scene;
-};
-
-/* only for passing to the callbacks */
-struct VertProjUpdate {
-  VertProjHandle *vp_handle;
-
-  /* runtime */
-  ARegion *region;
-  const float *mval_fl;
-};
-
-/* -------------------------------------------------------------------- */
-/* Internal Init */
-
-static void vpaint_proj_dm_map_cosnos_init__map_cb(void *user_data,
-                                                   int index,
-                                                   const float co[3],
-                                                   const float no[3])
-{
-  VertProjHandle *vp_handle = static_cast<VertProjHandle *>(user_data);
-
-  /* check if we've been here before (normal should not be 0) */
-  if (!math::is_zero(vp_handle->vert_normals[index])) {
-    /* remember that multiple dm verts share the same source vert */
-    vp_handle->use_update = true;
-    return;
-  }
-
-  vp_handle->vert_positions[index] = co;
-  vp_handle->vert_normals[index] = no;
-}
-
-static void vpaint_proj_dm_map_cosnos_init(Depsgraph &depsgraph,
-                                           Scene & /*scene*/,
-                                           Object &ob,
-                                           VertProjHandle &vp_handle)
-{
-  const Object *ob_eval = DEG_get_evaluated(&depsgraph, &ob);
-  const Mesh *mesh_eval = BKE_object_get_evaluated_mesh(ob_eval);
-
-  vp_handle.vert_normals.fill(float3(0));
-  BKE_mesh_foreach_mapped_vert(
-      mesh_eval, vpaint_proj_dm_map_cosnos_init__map_cb, &vp_handle, MESH_FOREACH_USE_NORMAL);
-}
-
-/* -------------------------------------------------------------------- */
-/* Internal Update */
-
-/* Same as init but take mouse location into account */
-
-static void vpaint_proj_dm_map_cosnos_update__map_cb(void *user_data,
-                                                     int index,
-                                                     const float co[3],
-                                                     const float no[3])
-{
-  VertProjUpdate *vp_update = static_cast<VertProjUpdate *>(user_data);
-  VertProjHandle *vp_handle = vp_update->vp_handle;
-
-  /* find closest vertex */
-  {
-    /* first find distance to this vertex */
-    float co_ss[2]; /* screenspace */
-
-    if (ED_view3d_project_float_object(
-            vp_update->region, co, co_ss, V3D_PROJ_TEST_CLIP_BB | V3D_PROJ_TEST_CLIP_NEAR) ==
-        V3D_PROJ_RET_OK)
-    {
-      const float dist_sq = len_squared_v2v2(vp_update->mval_fl, co_ss);
-      if (dist_sq > vp_handle->dists_sq[index]) {
-        /* bail out! */
-        return;
-      }
-
-      vp_handle->dists_sq[index] = dist_sq;
-    }
-    else if (vp_handle->dists_sq[index] != FLT_MAX) {
-      /* already initialized & couldn't project this 'co' */
-      return;
-    }
-  }
-  /* continue with regular functionality */
-
-  vp_handle->vert_positions[index] = co;
-  vp_handle->vert_normals[index] = no;
-}
-
-static void vpaint_proj_dm_map_cosnos_update(Depsgraph *depsgraph,
-                                             VertProjHandle *vp_handle,
-                                             ARegion *region,
-                                             const float mval_fl[2])
-{
-  VertProjUpdate vp_update = {vp_handle, region, mval_fl};
-
-  Object &ob = *vp_handle->ob;
-
-  const Object *ob_eval = DEG_get_evaluated(depsgraph, &ob);
-  const Mesh *mesh_eval = BKE_object_get_evaluated_mesh(ob_eval);
-
-  /* quick sanity check - we shouldn't have to run this if there are no modifiers */
-  BLI_assert(BLI_listbase_is_empty(&ob.modifiers) == false);
-
-  vp_handle->dists_sq.fill(FLT_MAX);
-  BKE_mesh_foreach_mapped_vert(
-      mesh_eval, vpaint_proj_dm_map_cosnos_update__map_cb, &vp_update, MESH_FOREACH_USE_NORMAL);
-}
-
-/* -------------------------------------------------------------------- */
-/* Public Functions */
-
-VertProjHandle *ED_vpaint_proj_handle_create(Depsgraph &depsgraph,
-                                             Scene &scene,
-                                             Object &ob,
-                                             Span<float3> &r_vert_positions,
-                                             Span<float3> &r_vert_normals)
-{
-  VertProjHandle *vp_handle = MEM_new<VertProjHandle>(__func__);
-  Mesh *mesh = id_cast<Mesh *>(ob.data);
-
-  /* setup the handle */
-  vp_handle->vert_positions.reinitialize(mesh->verts_num);
-  vp_handle->vert_normals.reinitialize(mesh->verts_num);
-  vp_handle->use_update = false;
-
-  /* sets 'use_update' if needed */
-  vpaint_proj_dm_map_cosnos_init(depsgraph, scene, ob, *vp_handle);
-
-  if (vp_handle->use_update) {
-    vp_handle->dists_sq.reinitialize(mesh->verts_num);
-    vp_handle->ob = &ob;
-    vp_handle->scene = &scene;
-  }
-  else {
-    vp_handle->ob = nullptr;
-    vp_handle->scene = nullptr;
-  }
-
-  r_vert_positions = vp_handle->vert_positions;
-  r_vert_normals = vp_handle->vert_normals;
-  return vp_handle;
-}
-
-void ED_vpaint_proj_handle_update(Depsgraph *depsgraph,
-                                  VertProjHandle *vp_handle,
-                                  ARegion *region,
-                                  const float mval_fl[2])
-{
-  if (vp_handle->use_update) {
-    vpaint_proj_dm_map_cosnos_update(depsgraph, vp_handle, region, mval_fl);
-  }
-}
-
-void ED_vpaint_proj_handle_free(VertProjHandle *vp_handle)
-{
-  MEM_delete(vp_handle);
-}
-
-}  // namespace blender

FrenkyBojler/blender-ios

commit aa62ee68f9db98deaa12fa53ed88e18a3e204f77

Fix: Crash / UB when using texture brush in vertex paint
Introduced with 5cdc3e0ccf24bc784228cec9dcf959fa4e43eec7

The prior PR missed that some variables were assigned via return
arguments. This removes the still unused properties and replaces them
with direct access via the BVH evaluated position span.

source/blender/editors/sculpt_paint/paint_vertex.cc
@@ -911,13 +911,6 @@ struct VPaintData : public PaintModeData {
 
   ColorPaint4f paintcol;
 
-  /**
-   * Owned by #vp_handle.
-   * \todo Look into replacing this with just using the evaluated/deform positions.
-   */
-  Span<float3> vert_positions;
-  Span<float3> vert_normals;
-
   bool is_texbrush;
 
   /* Special storage for smear brush, avoid feedback loop - update each step. */
@@ -1825,7 +1818,7 @@ static void vpaint_do_draw(const Depsgraph &depsgraph,
            * position in order to project it. This ensures that the
            * brush texture will be oriented correctly.
            * This is the method also used in #sculpt_apply_texture(). */
-          float3 position = vpd.vert_positions[vert];
+          float3 position = vert_positions[vert];
           if (cache.radial_symmetry_pass) {
             position = math::transform_point(cache.symm_rot_mat_inv, position);
           }