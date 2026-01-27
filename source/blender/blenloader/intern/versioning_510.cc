f61f56739ff496eceb73ca7e9de16c9d35c98123
Treata11 <treata11@yahoo.com>
2026-01-27 22:05:32 +0330

    EEVEE: Version indirect lighting intensity default value

--- source/blender/blenloader/intern/versioning_510.cc
+++ source/blender/blenloader/intern/versioning_510.cc
@@ -700,7 +700,7 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 21)) {
     for (Scene &scene : bmain->scenes) {
-      scene.eevee.ray_tracing_options.rt_scale = 1.0f;
+      scene.eevee.indirect_intensity = 1.0f;
     }
   }
 
