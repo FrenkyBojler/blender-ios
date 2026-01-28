74e26cfbe9e64e644cd7e2ad1b2e496d2c7b738c
Philipp Oeser <philipp@blender.org>
2026-01-28 16:12:03 +0100

    Fix #153517: Paint/Sculpt crashes with Stroke Method "Curve"
    Caused by 6eae38e08dd3
    
    - `paint_stroke_free` used to delete the memory (and was called from
    invoke, stroke_done), was checking the passed in stroke pointer
    - now `PaintStroke::free` doesnt do it, deleting the memory happens in
    invoke, modal and exec
    
    That is problematic, we might end up calling it multiple times.
    
    To resolve, the situation is very similar to 3f91540cef7e:
    - set the operator customdata to null when we do `MEM_delete` in modal
    - check that (to not do it again) in invoke

--- source/blender/editors/sculpt_paint/sculpt.cc
+++ source/blender/editors/sculpt_paint/sculpt.cc
@@ -5989,8 +5989,11 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
   OPERATOR_RETVAL_CHECK(retval);
 
   if (ELEM(retval, OPERATOR_FINISHED, OPERATOR_CANCELLED)) {
-    MEM_delete(stroke);
-    stroke->free(C, op);
+    SculptPaintStroke *stroke = static_cast<SculptPaintStroke *>(op->customdata);
+    if (stroke) {
+      MEM_delete(stroke);
+      stroke->free(C, op);
+    }
     return retval;
   }
   /* Add modal handler. */
@@ -6039,6 +6042,7 @@ static wmOperatorStatus brush_stroke_modal(bContext *C, wmOperator *op, const wm
 
   if (ELEM(retval, OPERATOR_FINISHED, OPERATOR_CANCELLED)) {
     MEM_delete(stroke);
+    op->customdata = nullptr;
   }
 
   return retval;
