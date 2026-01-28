a9c39be089d25830d791c7cf9c9135c196590b92
Campbell Barton <campbell@blender.org>
2026-01-28 14:26:11 +1100

    CMake: use standard FindTIFF variable names
    TIFF_LIBRARY and TIFF_INCLUDE_DIR are cache variables set by CMake's
    FindTIFF module. Use TIFF_LIBRARIES and TIFF_INCLUDE_DIRS which are
    the result variables intended for external use.
    
    Resolves an uninitialized variable warning on platforms besides WIN32.

--- intern/cycles/cmake/macros.cmake
+++ intern/cycles/cmake/macros.cmake
@@ -131,7 +131,7 @@ macro(cycles_external_libraries_append libraries)
     ${OPENIMAGEIO_LIBRARIES}
     ${PNG_LIBRARIES}
     ${JPEG_LIBRARIES}
-    ${TIFF_LIBRARY}
+    ${TIFF_LIBRARIES}
     ${OPENJPEG_LIBRARIES}
     ${OPENEXR_LIBRARIES}
     ${OPENEXR_LIBRARIES} # For circular dependencies between libs.
