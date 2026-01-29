78b95e56009dbb25d02202c6504e92b4bfc4ee22
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-01-29 08:50:36 +0100

    test for not renaming other armatures

--- tests/python/bl_animation_rename.py
+++ tests/python/bl_animation_rename.py
@@ -52,50 +52,70 @@ class PoseBoneRenameTest(unittest.TestCase):
         edit_bone.tail = (0, 1, 0)
         bpy.ops.object.mode_set(mode='OBJECT')
 
+        # This is useful to check if the rename does not affect animation on identically named
+        # bones in a different object.
+        self.armature_obj_dupli = self.armature_obj.copy()
+        self.armature_obj_dupli.data = armature.copy()
+        bpy.context.scene.collection.objects.link(self.armature_obj_dupli)
+
     def test_rename_bone_driver(self):
-        fcu = self.armature_obj.data.bones[_BONE_NAME_A].driver_add(f"bbone_segments", -1)
+        fcu = self.armature_obj.data.bones[_BONE_NAME_A].driver_add("bbone_segments", -1)
         self.assertEqual(fcu.data_path, f"bones[\"{_BONE_NAME_A}\"].bbone_segments")
-        driver = fcu.driver
+        _set_up_driver(fcu.driver, self.armature_obj, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
 
-        _set_up_driver(driver, self.armature_obj, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
+        # Setting up the same driver on the duplicate armature.
+        fcu_dupli = self.armature_obj_dupli.data.bones[_BONE_NAME_A].driver_add("bbone_segments", -1)
+        self.assertEqual(fcu_dupli.data_path, fcu.data_path)
+        _set_up_driver(fcu_dupli.driver, self.armature_obj_dupli, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
 
-        bone_a_rename = "bone_a_2"
-        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_rename
-        # bpy.context.view_layer.update()
+        bone_a_new_name = "bone_a_2"
+        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_new_name
 
         # `bbone_segments` is a property of the bone thus armature.
         self.assertEqual(len(self.armature_obj.data.animation_data.drivers), 1, "Shouldn't remove the driver")
-        self.assertEqual(fcu.data_path, f"bones[\"{bone_a_rename}\"].bbone_segments")
+        self.assertEqual(fcu.data_path, f"bones[\"{bone_a_new_name}\"].bbone_segments")
 
-        bone_b_rename = "bone_b_2"
-        self.armature_obj.pose.bones[_BONE_NAME_B].name = bone_b_rename
+        self.assertEqual(
+            fcu_dupli.data_path,
+            f"bones[\"{_BONE_NAME_A}\"].bbone_segments",
+            "Should not modify duplicate armature")
+
+        bone_b_new_name = "bone_b_2"
+        self.armature_obj.pose.bones[_BONE_NAME_B].name = bone_b_new_name
 
         # Testing if driver targets are still correct after a rename.
         # TODO: this currently does not work!
-        # driver_target = driver.variables[0].targets[0]
+        # driver_target = fcu.driver.variables[0].targets[0]
         # self.assertEqual(driver_target.data_path, f"pose.bones[\"{bone_b_rename}\"].location[0]")
 
     def test_rename_pose_bone_driver(self):
         fcu = self.armature_obj.pose.bones[_BONE_NAME_A].driver_add("hide", -1)
         self.assertEqual(fcu.data_path, f"pose.bones[\"{_BONE_NAME_A}\"].hide")
-        driver = fcu.driver
+        _set_up_driver(fcu.driver, self.armature_obj, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
 
-        _set_up_driver(driver, self.armature_obj, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
+        # Setting up the same driver on the duplicate armature.
+        fcu_dupli = self.armature_obj_dupli.pose.bones[_BONE_NAME_A].driver_add("hide", -1)
+        self.assertEqual(fcu_dupli.data_path, fcu.data_path)
+        _set_up_driver(fcu_dupli.driver, self.armature_obj_dupli, f"pose.bones[\"{_BONE_NAME_B}\"].location[0]")
 
-        bone_a_rename = "bone_a_2"
-        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_rename
-        # bpy.context.view_layer.update()
+        bone_a_new_name = "bone_a_2"
+        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_new_name
 
         # `hide` is a property on the pose bone thus object.
         self.assertEqual(len(self.armature_obj.animation_data.drivers), 1, "Shouldn't remove the driver")
-        self.assertEqual(fcu.data_path, f"pose.bones[\"{bone_a_rename}\"].hide")
+        self.assertEqual(fcu.data_path, f"pose.bones[\"{bone_a_new_name}\"].hide")
 
-        bone_b_rename = "bone_b_2"
-        self.armature_obj.pose.bones[_BONE_NAME_B].name = bone_b_rename
+        self.assertEqual(
+            fcu_dupli.data_path,
+            f"pose.bones[\"{_BONE_NAME_A}\"].hide",
+            "Should not modify the duplicate armature")
+
+        bone_b_new_name = "bone_b_2"
+        self.armature_obj.pose.bones[_BONE_NAME_B].name = bone_b_new_name
 
         # Testing if driver targets are still correct after a rename.
         # TODO: this currently does not work!
-        # driver_target = driver.variables[0].targets[0]
+        # driver_target = fcu.driver.variables[0].targets[0]
         # self.assertEqual(driver_target.data_path, f"pose.bones[\"{bone_b_rename}\"].location[0]")
 
     def test_rename_bone_animation(self):
@@ -107,12 +127,19 @@ class PoseBoneRenameTest(unittest.TestCase):
         for fcurve in fcurves:
             self.assertEqual(fcurve.data_path, f"bones[\"{_BONE_NAME_A}\"].bbone_segments")
 
-        bone_a_rename = "bone_a_2"
+        self.armature_obj_dupli.data.bones[_BONE_NAME_A].keyframe_insert("bbone_segments")
+        self.assertNotEqual(self.armature_obj_dupli.data.animation_data.action, None)
+
+        bone_a_new_name = "bone_a_2"
         # Renaming the pose bone, also renames the bone itself which should rename the fcurve data path too.
-        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_rename
+        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_new_name
 
         for fcurve in fcurves:
-            self.assertEqual(fcurve.data_path, f"bones[\"{bone_a_rename}\"].bbone_segments")
+            self.assertEqual(fcurve.data_path, f"bones[\"{bone_a_new_name}\"].bbone_segments")
+
+        dupli_action = self.armature_obj_dupli.data.animation_data.action
+        for fcurve in dupli_action.layers[0].strips[0].channelbags[0].fcurves:
+            self.assertEqual(fcurve.data_path, f"bones[\"{_BONE_NAME_A}\"].bbone_segments")
 
     def test_rename_pose_bone_animation(self):
         self.armature_obj.pose.bones[_BONE_NAME_A].keyframe_insert("location")
@@ -122,11 +149,18 @@ class PoseBoneRenameTest(unittest.TestCase):
         for fcurve in fcurves:
             self.assertEqual(fcurve.data_path, f"pose.bones[\"{_BONE_NAME_A}\"].location")
 
-        bone_a_rename = "bone_a_2"
-        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_rename
+        self.armature_obj_dupli.pose.bones[_BONE_NAME_A].keyframe_insert("location")
+        self.assertNotEqual(self.armature_obj_dupli.animation_data.action, None)
+
+        bone_a_new_name = "bone_a_2"
+        self.armature_obj.pose.bones[_BONE_NAME_A].name = bone_a_new_name
 
         for fcurve in fcurves:
-            self.assertEqual(fcurve.data_path, f"pose.bones[\"{bone_a_rename}\"].location")
+            self.assertEqual(fcurve.data_path, f"pose.bones[\"{bone_a_new_name}\"].location")
+
+        dupli_action = self.armature_obj_dupli.animation_data.action
+        for fcurve in dupli_action.layers[0].strips[0].channelbags[0].fcurves:
+            self.assertEqual(fcurve.data_path, f"pose.bones[\"{_BONE_NAME_A}\"].location")
 
 
 def main():
