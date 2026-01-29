c46372884692cb3a3ee25295d0291369cc3d6b3a
Philipp Oeser <philipp@blender.org>
2026-01-29 15:40:32 +0100

    from review:
    avoid BKE_bpath_foreach_path_fixed_process being called multiple times
    using any_of

--- source/blender/blenkernel/intern/mesh.cc
+++ source/blender/blenkernel/intern/mesh.cc
@@ -289,10 +289,11 @@ static void mesh_foreach_path(ID *id, BPathForeachPathData *bpath_data)
   if (data.external) {
     /* CustomDataExternal should only be the case for CD_MDISPS, but check all layers regardless.
      */
-    for (const CustomDataLayer &layer : Span(data.layers, data.totlayer)) {
-      if (!CustomData_external_test(&data, eCustomDataType(layer.type))) {
-        continue;
-      }
+    const Span<CustomDataLayer> layers = Span(data.layers, data.totlayer);
+    if (std::any_of(layers.begin(), layers.end(), [&](const CustomDataLayer &layer) {
+          return CustomData_external_test(&data, eCustomDataType(layer.type));
+        }))
+    {
       BKE_bpath_foreach_path_fixed_process(
           bpath_data, data.external->filepath, sizeof(data.external->filepath));
     }
