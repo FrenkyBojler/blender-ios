aa2253417b61bda0d0e96ee9c5586cf59c0ed861
Jacques Lucke <jacques@blender.org>
2026-01-29 11:16:26 +0100

    remove gather bundle geometries node

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -10081,7 +10081,6 @@ static void rna_def_nodes(BlenderRNA *brna)
   define("GeometryNode", "GeometryNodeFlipFaces");
   define("GeometryNode", "GeometryNodeForeachGeometryElementInput", def_geo_foreach_geometry_element_input);
   define("GeometryNode", "GeometryNodeForeachGeometryElementOutput", def_geo_foreach_geometry_element_output);
-  define("GeometryNode", "GeometryNodeGatherBundleGeometries");
   define("GeometryNode", "GeometryNodeGeometryToInstance");
   define("GeometryNode", "GeometryNodeGetGeometryBundle");
   define("GeometryNode", "GeometryNodeGetNamedGrid");
--- source/blender/nodes/geometry/nodes/node_geo_get_typed_bundle_paths.cc
+++ source/blender/nodes/geometry/nodes/node_geo_get_typed_bundle_paths.cc
@@ -10,17 +10,25 @@ namespace blender::nodes::node_geo_get_typed_bundle_paths_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Bundle>("Bundle");
+  b.add_input<decl::String>("Type");
   b.add_output<decl::String>("Paths").structure_type(StructureType::List);
 }
 
 static void gather_bundle_paths_recursive(const BundlePtr &bundle_ptr,
                                           const StringRef self_path,
+                                          const StringRef type_filter,
                                           Vector<std::string> &r_paths)
 {
   if (!bundle_ptr) {
     return;
   }
-  if (bundle_ptr->type().has_value()) {
+  const std::optional<StringRef> bundle_type = bundle_ptr->type();
+  if (bundle_type.has_value()) {
+    if (!type_filter.is_empty()) {
+      if (*bundle_type != type_filter) {
+        return;
+      }
+    }
     r_paths.append(self_path);
     return;
   }
@@ -32,15 +40,16 @@ static void gather_bundle_paths_recursive(const BundlePtr &bundle_ptr,
     const std::string child_path = self_path.is_empty() ?
                                        item.key :
                                        fmt::format("{}/{}", self_path, item.key);
-    gather_bundle_paths_recursive(*child_bundle_ptr, child_path, r_paths);
+    gather_bundle_paths_recursive(*child_bundle_ptr, child_path, type_filter, r_paths);
   }
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
   BundlePtr bundle = params.extract_input<BundlePtr>("Bundle");
+  const std::string type_filter = params.extract_input<std::string>("Type");
   Vector<std::string> paths;
-  gather_bundle_paths_recursive(bundle, "", paths);
+  gather_bundle_paths_recursive(bundle, "", type_filter, paths);
 
   params.set_output("Paths", List::from_container(std::move(paths)));
 }
