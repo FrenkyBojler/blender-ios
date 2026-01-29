35a92effeb0d049a43ec83e6d09baee098aeae0f
Philipp Oeser <philipp@blender.org>
2026-01-29 14:04:28 +0100

    Fix #153548: Multires .btx filepath reported as missing after repacking
    So even if the external file is not used, a "missing" .btx would would
    show up in reports.
    
    The filepath is not cleared in `multires_external_pack_exec` (and maybe
    it shouldnt), also `CustomDataExternal` isnt freed (maybe it should?),
    but it does remove the `CD_FLAG_EXTERNAL` flag from the layer.
    
    When checking filepaths in `mesh_foreach_path`, we only considered if
    `CustomDataExternal` exists, but not if a `CustomDataLayer` is actually
    flagged `CD_FLAG_EXTERNAL` (only then we should really report this as
    missing, otherwise it is unused, we also dont report e.g. a "missing"
    Image filepath when it is packed). Use the same check that is used for
    determining "is_external".
    
    NOTE: Question remains why `multires_external_pack_exec` does not
    actually free via `CustomData_external_free` (it only does
    `CustomData_external_remove`), but that is out of the scope of this
    bugfix.

--- source/blender/blenkernel/intern/mesh.cc
+++ source/blender/blenkernel/intern/mesh.cc
@@ -285,10 +285,17 @@ static void mesh_foreach_id(ID *id, LibraryForeachIDData *data)
 static void mesh_foreach_path(ID *id, BPathForeachPathData *bpath_data)
 {
   Mesh *mesh = reinterpret_cast<Mesh *>(id);
-  if (mesh->corner_data.external) {
-    BKE_bpath_foreach_path_fixed_process(bpath_data,
-                                         mesh->corner_data.external->filepath,
-                                         sizeof(mesh->corner_data.external->filepath));
+  CustomData &data = mesh->corner_data;
+  if (data.external) {
+    /* CustomDataExternal should only be the case for CD_MDISPS, but check all layers regardless.
+     */
+    for (const CustomDataLayer &layer : Span(data.layers, data.totlayer)) {
+      if (!CustomData_external_test(&data, eCustomDataType(layer.type))) {
+        continue;
+      }
+      BKE_bpath_foreach_path_fixed_process(
+          bpath_data, data.external->filepath, sizeof(data.external->filepath));
+    }
   }
 }
 
