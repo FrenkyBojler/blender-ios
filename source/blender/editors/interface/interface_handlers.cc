af69b9cfd6070a76aa9292779a571649fcbd9df9
Pratik Borhade <pratikborhade302@gmail.com>
2026-01-29 15:00:19 +0530

    Revert earlier change

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -8792,15 +8792,13 @@ static void button_activate_state(bContext *C, Button *but, HandleButtonState st
         if (but->block->auto_open == true) { /* test for toolbox */
           time = 1;
         }
+        else if ((but->block->flag & BLOCK_LOOP && but->type != ButtonType::Block) ||
+                 (but->block->auto_open == true))
+        {
+          time = 5 * U.menuthreshold2;
+        }
         else if (U.uiflag & USER_MENUOPENAUTO) {
-          if ((but->block->flag & BLOCK_LOOP && but->type != ButtonType::Block) ||
-              (but->block->auto_open == true))
-          {
-            time = 5 * U.menuthreshold2;
-          }
-          else {
-            time = 5 * U.menuthreshold1;
-          }
+          time = 5 * U.menuthreshold1;
         }
         else {
           time = -1; /* do nothing */
