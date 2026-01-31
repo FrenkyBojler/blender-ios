18dae01ed92b95d8cecc33ec19bd8dc5505f5401
илья _ <modormoder@gmail.com>
2026-01-31 23:54:41 +0300

    added gspan test

--- source/blender/blenlib/tests/BLI_generic_span_test.cc
+++ source/blender/blenlib/tests/BLI_generic_span_test.cc
@@ -52,4 +52,16 @@ TEST(generic_mutable_span, BufferAndSizeConstructor)
   EXPECT_EQ(values[2], 20);
 }
 
+TEST(generic_mutable_span, Overlaps)
+{
+  int values[4] = {4, 7, 3, 5};
+  void *buffer = static_cast<void *>(values);
+  GSpan span(CPPType::get<int>(), buffer, 4);
+  EXPECT_TRUE(span.overlaps(span));
+  EXPECT_TRUE(span.drop_front(1).overlaps(span));
+  EXPECT_TRUE(span.drop_front(2).overlaps(span));
+  EXPECT_TRUE(span.drop_front(3).overlaps(span));
+  EXPECT_FALSE(span.drop_front(4).overlaps(span));
+}
+
 }  // namespace blender::tests
