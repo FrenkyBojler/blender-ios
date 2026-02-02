08d90080a2661436aac2a76467f412dba4f8b4bc
Guillermo Venegas <guillermovcra@gmail.com>
2026-02-02 11:12:35 -0600

    Comment

--- source/blender/editors/interface/interface_intern.hh
+++ source/blender/editors/interface/interface_intern.hh
@@ -744,6 +744,7 @@ struct Block {
     return *button;
   }
 
+  /** A view of #Block::buttons which allows range-based for loops as #Buttons references. */
   std::ranges::transform_view<std::ranges::ref_view<const Vector<std::unique_ptr<Button>>>,
                               Button &(*)(const std::unique_ptr<Button> &)>
   buttons_as_refs() const
