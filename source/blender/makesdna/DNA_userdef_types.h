45e5943038c08c4a88345fe220af1dba8a9eea13
Nathan Vegdahl <cessen@cessen.com>
2026-02-02 03:46:38 +0000

    Apply @dfelinto's patch to make the default project window size larger

--- source/blender/makesdna/DNA_userdef_types.h
+++ source/blender/makesdna/DNA_userdef_types.h
@@ -780,7 +780,7 @@ struct UserDef_TempWinBounds {
   rctf image = {50.0f, 1360.0f, 50.0f, 830.0f};
   rctf graph = {50.0f, 950.0f, 200.0f, 780.0f};
   rctf info = {100.0f, 1000.0f, 300.0f, 880.0f};
-  rctf project = {100.0f, 680.0f, 150.0f, 310.0f};
+  rctf project = {100.0f, 800.0f, 500.0f, 850.0f};
   rctf outliner = {100.0f, 550.0f, 350.0f, 800.0f};
 };
 
