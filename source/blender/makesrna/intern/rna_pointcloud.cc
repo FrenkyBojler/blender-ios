7b5ed5a002a81db6c143231b8190c919b633cc78
Julien Duroure <julien.duroure@gmail.com>
2026-02-03 18:29:30 +0100

    malke format

--- source/blender/makesrna/intern/rna_pointcloud.cc
+++ source/blender/makesrna/intern/rna_pointcloud.cc
@@ -188,8 +188,7 @@ static void rna_def_pointcloud(BlenderRNA *brna)
                                     nullptr);
   RNA_def_property_ui_text(prop, "Points", "");
   func = RNA_def_function(srna, "resize", "rna_PointCloud_resize");
-  parm = RNA_def_int(
-      func, "size", 0, 0, INT_MAX, "Size", "New number of points", 0, INT_MAX);
+  parm = RNA_def_int(func, "size", 0, 0, INT_MAX, "Size", "New number of points", 0, INT_MAX);
   RNA_def_parameter_flags(parm, PropertyFlag(0), PARM_REQUIRED);
 
   /* materials */
