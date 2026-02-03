bcd0a0a082ae5a154b5c15065cd6a6e98a26d5b2
Bill Spitzak <spitzak@gmail.com>
2026-02-02 20:02:16 -0800

    Remove use of function pointers.
    Instead the CPU sample_rect implementation is chosen per pixel. The
    implementations were made BLI_INLINE.
    
    This resulted in insignificant changes in speed (probably due to branch
    prediction) and makes it easier for user code to call sample_rect, and
    the code is somewhat easier to follow.

--- source/blender/blenlib/BLI_math_interp.hh
+++ source/blender/blenlib/BLI_math_interp.hh
@@ -524,20 +524,17 @@ void interpolate_cubic_mitchell_fl(
  * the sample area must be approximated by this rectangle, hypot(dPdx,dPdy) is recommended.
  * Sampler::Anisotropic does Sampler::Box.
  */
-using SampleRect = float4 (*)(const sampler2D &source, const float2 &uv, const float2 &wh);
-/** Lookup optimized function to call for \a source and sampler. */
-SampleRect sample_rect(Sampler sampler, const sampler2D &source);
+float4 sample_rect(Sampler sampler, const sampler2D &source, const float2 &uv, const float2 &wh);
 
 /**
  * Filtered sampling based on derivatives of the sample location. Only Sampler::Anisotropic
  * does something different here, all others call sample_rect.
  */
-using SampleArea = float4 (*)(const sampler2D &source,
-                              const float2 &uv,
-                              const float2 &dPdx,
-                              const float2 &dPdy);
-/** Lookup optimized function to call for \a source. */
-SampleArea sample_area(Sampler sampler, const sampler2D &source);
+float4 sample_area(Sampler sampler,
+                   const sampler2D &source,
+                   const float2 &uv,
+                   const float2 &dPdx,
+                   const float2 &dPdy);
 
 }  // namespace math
 
