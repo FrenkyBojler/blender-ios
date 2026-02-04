blender/blender

commit 6dfe6f34c82635b1d58763cab793dbec759cf03a

UI: Draw popups to the side of button
This PR adds a new "direction" property to "popover" that gives more
control over the direction of block drawing relative to button
position. This then uses this to show the Properties "Visibility"
popover to the side of the button so it no longer hides the categories.
Suggested in #150190.

Pull Request: https://projects.blender.org/blender/blender/pulls/152421

source/blender/editors/interface/regions/interface_region_popover.cc
@@ -134,7 +134,10 @@ static Block *block_func_POPOVER(bContext *C, PopupBlockHandle *handle, void *ar
   }
 
   block_layout_resolve(block);
-  block_direction_set(block, UI_DIR_DOWN | UI_DIR_CENTER_X);
+  const int direction = pup->panel_type->popup_draw_direction == PopupAttachDirection::Horizontal ?
+                            UI_DIR_LEFT :
+                            UI_DIR_DOWN | UI_DIR_CENTER_X;
+  block_direction_set(block, direction);
 
   const int block_margin = U.widget_unit / 2;
 
@@ -166,7 +169,7 @@ static Block *block_func_POPOVER(bContext *C, PopupBlockHandle *handle, void *ar
     if (!slideout) {
       ARegion *region = CTX_wm_region(C);
 
-      if (region && region->panels.first) {
+      if (region && region->panels.first && (direction & UI_DIR_DOWN)) {
         /* For regions with panels, prefer to open to top so we can
          * see the values of the buttons below changing. */
         block_direction_set(block, UI_DIR_UP | UI_DIR_CENTER_X);

scripts/startup/bl_ui/space_properties.py
@@ -101,7 +101,7 @@ class PROPERTIES_PT_navigation_bar(Panel):
         sub.scale_x = 0.8
         sub.scale_y = 0.8
         sub.separator(factor=0.7)
-        sub.popover(panel="PROPERTIES_PT_visibility", text="")
+        sub.popover(panel="PROPERTIES_PT_visibility", text="", direction='HORIZONTAL')
         sub.active = has_hidden_tabs(view)
 
 

source/blender/blenkernel/BKE_screen.hh
@@ -38,6 +38,7 @@ class AssetRepresentation;
 namespace ui {
 struct Layout;
 struct Block;
+enum class PopupAttachDirection : int8_t;
 }  // namespace ui
 
 struct ARegion;
@@ -414,7 +415,7 @@ struct PanelType {
   /** Sub panels. */
   PanelType *parent;
   ListBaseT<LinkData> children;
-
+  ui::PopupAttachDirection popup_draw_direction;
   /** RNA integration. */
   ExtensionRNA rna_ext;
 };

source/blender/editors/include/UI_interface_layout.hh
@@ -101,6 +101,14 @@ enum class NodeAssetMenuOperatorType : int8_t {
   Swap,
 };
 
+/**
+ * Panel popup draw direction.
+ */
+enum class PopupAttachDirection : int8_t {
+  Vertical = 0,
+  Horizontal = 1,
+};
+
 enum class EnumTabExpand {
   Default = 0,
   Row,
@@ -570,7 +578,8 @@ struct Layout : public Item, NonCopyable, NonMovable {
   void popover(const bContext *C,
                StringRef panel_type,
                std::optional<StringRef> name_opt,
-               int icon);
+               int icon,
+               PopupAttachDirection direction = PopupAttachDirection::Vertical);
   void popover_group(
       bContext *C, int space_id, int region_id, const char *context, const char *category);
 

source/blender/editors/interface/interface_layout.cc
@@ -3094,7 +3094,8 @@ void Layout::popover(const bContext *C,
 void Layout::popover(const bContext *C,
                      const StringRef panel_type,
                      std::optional<StringRef> name_opt,
-                     int icon)
+                     int icon,
+                     PopupAttachDirection direction)
 {
   PanelType *pt = WM_paneltype_find(panel_type, true);
   if (pt == nullptr) {
@@ -3102,6 +3103,7 @@ void Layout::popover(const bContext *C,
                      std::string(panel_type).c_str());
     return;
   }
+  pt->popup_draw_direction = direction;
   this->popover(C, pt, name_opt, icon);
 }
 

source/blender/makesrna/intern/rna_ui_api.cc
@@ -32,6 +32,19 @@ const EnumPropertyItem rna_enum_icon_items[] = {
     {0, nullptr, 0, nullptr, nullptr},
 };
 
+static const EnumPropertyItem popup_draw_direction_items[] = {
+    {int(ui::PopupAttachDirection::Vertical),
+     "VERTICAL",
+     0,
+     "Vertical",
+     "Draw popup panel above or below the button"},
+    {int(ui::PopupAttachDirection::Horizontal),
+     "HORIZONTAL",
+     0,
+     "Horizontal",
+     "Draw popup panel to the side of the button"},
+    {0, nullptr, 0, nullptr, nullptr},
+};
 }  // namespace blender
 
 #ifdef RNA_RUNTIME
@@ -554,7 +567,8 @@ static void rna_uiItemPopoverPanel(Layout *layout,
                                    const char *text_ctxt,
                                    bool translate,
                                    int icon,
-                                   int icon_value)
+                                   int icon_value,
+                                   int direction = int(ui::PopupAttachDirection::Vertical))
 {
   /* Get translated name (label). */
   std::optional<StringRefNull> text = rna_translate_ui_text(
@@ -564,7 +578,7 @@ static void rna_uiItemPopoverPanel(Layout *layout,
     icon = icon_value;
   }
 
-  layout->popover(C, panel_type, text, icon);
+  layout->popover(C, panel_type, text, icon, ui::PopupAttachDirection(direction));
 }
 
 static void rna_uiItemPopoverPanelFromGroup(Layout *layout,
@@ -1668,6 +1682,13 @@ void RNA_api_ui_layout(StructRNA *srna)
   RNA_def_parameter_flags(parm, PropertyFlag(0), PARM_REQUIRED);
   parm = RNA_def_property(func, "icon_value", PROP_INT, PROP_UNSIGNED);
   RNA_def_property_ui_text(parm, "Icon Value", "Override automatic icon of the item");
+  parm = RNA_def_enum(
+      func,
+      "direction",
+      popup_draw_direction_items,
+      0,
+      "Popup Direction",
+      "The direction in which the popup panel is drawn relative to button position");
 
   func = RNA_def_function(srna, "popover_group", "rna_uiItemPopoverPanelFromGroup");
   RNA_def_function_flag(func, FUNC_USE_CONTEXT);

FrenkyBojler/blender-ios

commit 23cc1d6441f861ac337d4ad2aeb12c0914973c50

Fix #153878: Crash when renaming nodes
Caused by 6dfe6f34c8
Add nullptr check for panel_type. They are not referenced in Popup member
when `keep_open` is false inside `popover_panel_invoke`

source/blender/editors/interface/regions/interface_region_popover.cc
@@ -134,7 +134,8 @@ static Block *block_func_POPOVER(bContext *C, PopupBlockHandle *handle, void *ar
   }
 
   block_layout_resolve(block);
-  const int direction = pup->panel_type->popup_draw_direction == PopupAttachDirection::Horizontal ?
+  const int direction = (pup->panel_type && (pup->panel_type->popup_draw_direction ==
+                                             PopupAttachDirection::Horizontal)) ?
                             UI_DIR_LEFT :
                             UI_DIR_DOWN | UI_DIR_CENTER_X;
   block_direction_set(block, direction);