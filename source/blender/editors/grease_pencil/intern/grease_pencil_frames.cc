192fa55f6db34e76548d776bb713b85d465e308f
Philipp Oeser <philipp@blender.org>
2026-02-04 16:01:00 +0100

    from review
    Fix case where copying from a single layer and pasting with no layers
    selected would paste into all layers

--- source/blender/editors/grease_pencil/intern/grease_pencil_frames.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_frames.cc
@@ -703,6 +703,10 @@ bool grease_pencil_paste_keyframes(bAnimContext *ac,
 
   const int offset = calculate_offset(offset_mode, ac->scene->r.cfra, clipboard);
 
+  /* Check if single channel in buffer (disregard names if so). */
+  const bool from_single_channel = clipboard.copy_buffer.size() == 1;
+  bool match_names = !from_single_channel;
+
   ListBaseT<bAnimListElem> anim_data = {nullptr, nullptr};
   /* Only paste into selected layers. */
   int filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_LIST_VISIBLE | ANIMFILTER_NODUPLIS |
@@ -715,10 +719,10 @@ bool grease_pencil_paste_keyframes(bAnimContext *ac,
               ANIMFILTER_FOREDIT);
     ANIM_animdata_filter(
         ac, &anim_data, eAnimFilter_Flags(filter), ac->data, eAnimCont_Types(ac->datatype));
+    /* In this case, always match names though (otherwise we would paste from a single channel into
+     * all others). */
+    match_names = true;
   }
-
-  /* Check if single channel in buffer (disregard names if so). */
-  const bool from_single_channel = clipboard.copy_buffer.size() == 1;
 
   for (bAnimListElem &ale : anim_data) {
     /* Only deal with GPlayers (case of calls from general dope-sheet). */
@@ -728,7 +732,7 @@ bool grease_pencil_paste_keyframes(bAnimContext *ac,
     GreasePencil *grease_pencil = reinterpret_cast<GreasePencil *>(ale.id);
     Layer &layer = *reinterpret_cast<Layer *>(ale.data);
     const std::string layer_name = layer.name();
-    if (!from_single_channel && !clipboard.copy_buffer.contains(layer_name)) {
+    if (match_names && !clipboard.copy_buffer.contains(layer_name)) {
       continue;
     }
     const KeyframeClipboard::LayerBufferItem layer_buffer =
