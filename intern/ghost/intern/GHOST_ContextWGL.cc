blender/blender

commit b330bef4196231f227886337134c1aebf0841362

Fix #152672: Cycles repeated render crash due to Windows handle leak
The dummy window and device context for the offscreen OpenGL context
were leaking. This was particularly problematic when calling the render
operator in a loop as a new context would be created every time, while
a single long animation render would reuse the same context.

All credit for figuring this out goes to Ash Tyler, many thanks.

Pull Request: https://projects.blender.org/blender/blender/pulls/153667

intern/ghost/intern/GHOST_ContextWGL.cc
@@ -29,6 +29,7 @@ GHOST_ContextWGL::GHOST_ContextWGL(const GHOST_ContextParams &context_params,
                                    bool alphaBackground,
                                    HWND hWnd,
                                    HDC hDC,
+                                   bool own_window_handle,
                                    int contextProfileMask,
                                    int contextMajorVersion,
                                    int contextMinorVersion,
@@ -37,6 +38,7 @@ GHOST_ContextWGL::GHOST_ContextWGL(const GHOST_ContextParams &context_params,
     : GHOST_Context(context_params),
       h_wnd_(hWnd),
       h_DC_(hDC),
+      own_window_handle_(own_window_handle),
       context_profile_mask_(contextProfileMask),
       context_major_version_(contextMajorVersion),
       context_minor_version_(contextMinorVersion),
@@ -73,6 +75,8 @@ GHOST_ContextWGL::~GHOST_ContextWGL()
       WIN32_CHK(::wglDeleteContext(h_GLRC_));
     }
   }
+
+  releaseNativeHandles();
 
 #ifndef NDEBUG
   if (dummy_renderer_) {
@@ -670,6 +674,15 @@ GHOST_TSuccess GHOST_ContextWGL::releaseNativeHandles()
 {
   GHOST_TSuccess success = h_GLRC_ != s_sharedHGLRC || s_sharedCount == 1 ? GHOST_kSuccess :
                                                                             GHOST_kFailure;
+
+  if (own_window_handle_) {
+    if (h_DC_ != nullptr) {
+      WIN32_CHK(::ReleaseDC(h_wnd_, h_DC_));
+    }
+    if (h_wnd_ != nullptr) {
+      WIN32_CHK(::DestroyWindow(h_wnd_));
+    }
+  }
 
   h_wnd_ = nullptr;
   h_DC_ = nullptr;

intern/ghost/intern/GHOST_ContextWGL.hh
@@ -28,6 +28,7 @@ class GHOST_ContextWGL : public GHOST_Context {
                    bool alphaBackground,
                    HWND hWnd,
                    HDC hDC,
+                   bool own_window_handle,
                    int contextProfileMask,
                    int contextMajorVersion,
                    int contextMinorVersion,
@@ -96,6 +97,7 @@ class GHOST_ContextWGL : public GHOST_Context {
 
   HWND h_wnd_;
   HDC h_DC_;
+  bool own_window_handle_;
 
   const int context_profile_mask_;
   const int context_major_version_;

intern/ghost/intern/GHOST_SystemWin32.cc
@@ -325,29 +325,30 @@ GHOST_IContext *GHOST_SystemWin32::createOffscreenContext(GHOST_GPUSettings gpu_
 #ifdef WITH_OPENGL_BACKEND
     case GHOST_kDrawingContextTypeOpenGL: {
 
-      /* OpenGL needs a dummy window to create a context on windows. */
-      HWND wnd = CreateWindowA("STATIC",
-                               "BlenderGLEW",
-                               WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-                               0,
-                               0,
-                               64,
-                               64,
-                               nullptr,
-                               nullptr,
-                               GetModuleHandle(nullptr),
-                               nullptr);
-
-      HDC mHDC = GetDC(wnd);
-      HDC prev_hdc = wglGetCurrentDC();
       HGLRC prev_context = wglGetCurrentContext();
+      HDC prev_hdc = wglGetCurrentDC();
 
       for (int minor = 6; minor >= 3; --minor) {
-        GHOST_Context *context = new GHOST_ContextWGL(
+        /* OpenGL needs a dummy window to create a context on windows. */
+        HWND wnd = CreateWindowA("STATIC",
+                                 "BlenderGLEW",
+                                 WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+                                 0,
+                                 0,
+                                 64,
+                                 64,
+                                 nullptr,
+                                 nullptr,
+                                 GetModuleHandle(nullptr),
+                                 nullptr);
+        HDC mHDC = GetDC(wnd);
+
+        GHOST_ContextWGL *context = new GHOST_ContextWGL(
             context_params_offscreen,
             true,
             wnd,
             mHDC,
+            true, /* owns_window_handle */
             WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
             4,
             minor,

intern/ghost/intern/GHOST_WindowWin32.cc
@@ -639,6 +639,7 @@ GHOST_Context *GHOST_WindowWin32::newDrawingContext(GHOST_TDrawingContextType ty
             false,
             h_wnd_,
             h_DC_,
+            false, /* owns_window_handle */
             WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
             4,
             minor,

FrenkyBojler/blender-ios

commit 144163943f2b50d00c1d35151a34075988b18abf

Fix #153862: Windows GHOST_ContextWGL error on shutdown
Caused by b330bef419. The window handle needs to be created and destroyed in
the same thread. This is difficult to ensure from the Blender side, so instead
cache and reuse the window handles and destroy them on exit. This is also
slightly more efficient.

intern/ghost/intern/GHOST_ContextWGL.cc
@@ -18,6 +18,8 @@
 
 HGLRC GHOST_ContextWGL::s_sharedHGLRC = nullptr;
 int GHOST_ContextWGL::s_sharedCount = 0;
+std::list<GHOST_ContextWGL::OffscreenWindowHandle>
+    GHOST_ContextWGL::s_sharedOffscreenWindowHandles;
 
 /* Some third-generation Intel video-cards are constantly bring problems */
 static bool is_crappy_intel_card()
@@ -29,7 +31,6 @@ GHOST_ContextWGL::GHOST_ContextWGL(const GHOST_ContextParams &context_params,
                                    bool alphaBackground,
                                    HWND hWnd,
                                    HDC hDC,
-                                   bool own_window_handle,
                                    int contextProfileMask,
                                    int contextMajorVersion,
                                    int contextMinorVersion,
@@ -38,21 +39,37 @@ GHOST_ContextWGL::GHOST_ContextWGL(const GHOST_ContextParams &context_params,
     : GHOST_Context(context_params),
       h_wnd_(hWnd),
       h_DC_(hDC),
-      own_window_handle_(own_window_handle),
       context_profile_mask_(contextProfileMask),
       context_major_version_(contextMajorVersion),
       context_minor_version_(contextMinorVersion),
       context_flags_(contextFlags),
       alpha_background_(alphaBackground),
       context_reset_notification_strategy_(contextResetNotificationStrategy),
-      h_GLRC_(nullptr)
+      h_GLRC_(nullptr),
 #ifndef NDEBUG
-      ,
       dummy_vendor_(nullptr),
       dummy_renderer_(nullptr),
-      dummy_version_(nullptr)
+      dummy_version_(nullptr),
 #endif
+      offscreen_window_handle_(nullptr)
 {
+  if (h_wnd_ == nullptr) {
+    /* For offscreen context, OpenGL needs a dummy window to create a context on windows.
+     * Create or reuse an existing one. */
+    for (OffscreenWindowHandle &handle : s_sharedOffscreenWindowHandles) {
+      if (!handle.used) {
+        offscreen_window_handle_ = &handle;
+        break;
+      }
+    }
+    if (offscreen_window_handle_ == nullptr) {
+      offscreen_window_handle_ = &s_sharedOffscreenWindowHandles.emplace_back();
+      offscreen_window_handle_->used = true;
+    }
+    h_wnd_ = offscreen_window_handle_->h_wnd;
+    h_DC_ = offscreen_window_handle_->h_DC;
+  }
+
   assert(h_DC_ != nullptr);
 }
 
@@ -675,17 +692,39 @@ GHOST_TSuccess GHOST_ContextWGL::releaseNativeHandles()
   GHOST_TSuccess success = h_GLRC_ != s_sharedHGLRC || s_sharedCount == 1 ? GHOST_kSuccess :
                                                                             GHOST_kFailure;
 
-  if (own_window_handle_) {
-    if (h_DC_ != nullptr) {
-      WIN32_CHK(::ReleaseDC(h_wnd_, h_DC_));
-    }
-    if (h_wnd_ != nullptr) {
-      WIN32_CHK(::DestroyWindow(h_wnd_));
-    }
+  if (offscreen_window_handle_) {
+    offscreen_window_handle_->used = false;
+    offscreen_window_handle_ = nullptr;
   }
 
   h_wnd_ = nullptr;
   h_DC_ = nullptr;
 
   return success;
+}
+
+GHOST_ContextWGL::OffscreenWindowHandle::OffscreenWindowHandle()
+{
+  h_wnd = CreateWindowA("STATIC",
+                        "BlenderGLEW",
+                        WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+                        0,
+                        0,
+                        64,
+                        64,
+                        nullptr,
+                        nullptr,
+                        GetModuleHandle(nullptr),
+                        nullptr);
+  h_DC = GetDC(h_wnd);
+}
+
+GHOST_ContextWGL::OffscreenWindowHandle::~OffscreenWindowHandle()
+{
+  if (h_DC != nullptr) {
+    WIN32_CHK(::ReleaseDC(h_wnd, h_DC));
+  }
+  if (h_wnd != nullptr) {
+    WIN32_CHK(::DestroyWindow(h_wnd));
+  }
 }

intern/ghost/intern/GHOST_ContextWGL.hh
@@ -12,6 +12,8 @@
 
 #include <epoxy/wgl.h>
 
+#include <list>
+
 #ifndef GHOST_OPENGL_WGL_RESET_NOTIFICATION_STRATEGY
 #  define GHOST_OPENGL_WGL_RESET_NOTIFICATION_STRATEGY 0
 #endif
@@ -28,7 +30,6 @@ class GHOST_ContextWGL : public GHOST_Context {
                    bool alphaBackground,
                    HWND hWnd,
                    HDC hDC,
-                   bool own_window_handle,
                    int contextProfileMask,
                    int contextMajorVersion,
                    int contextMinorVersion,
@@ -97,7 +98,6 @@ class GHOST_ContextWGL : public GHOST_Context {
 
   HWND h_wnd_;
   HDC h_DC_;
-  bool own_window_handle_;
 
   const int context_profile_mask_;
   const int context_major_version_;
@@ -116,4 +116,18 @@ class GHOST_ContextWGL : public GHOST_Context {
 
   static HGLRC s_sharedHGLRC;
   static int s_sharedCount;
+
+  /* Offscreen window handles are cached to ensure they are created and freed on the
+   * main thread, as required by the Windows API. */
+  struct OffscreenWindowHandle {
+    OffscreenWindowHandle();
+    ~OffscreenWindowHandle();
+
+    HWND h_wnd = nullptr;
+    HDC h_DC = nullptr;
+    bool used = false;
+  };
+
+  static std::list<OffscreenWindowHandle> s_sharedOffscreenWindowHandles;
+  OffscreenWindowHandle *offscreen_window_handle_;
 };

intern/ghost/intern/GHOST_SystemWin32.cc
@@ -329,26 +329,11 @@ GHOST_IContext *GHOST_SystemWin32::createOffscreenContext(GHOST_GPUSettings gpu_
       HDC prev_hdc = wglGetCurrentDC();
 
       for (int minor = 6; minor >= 3; --minor) {
-        /* OpenGL needs a dummy window to create a context on windows. */
-        HWND wnd = CreateWindowA("STATIC",
-                                 "BlenderGLEW",
-                                 WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-                                 0,
-                                 0,
-                                 64,
-                                 64,
-                                 nullptr,
-                                 nullptr,
-                                 GetModuleHandle(nullptr),
-                                 nullptr);
-        HDC mHDC = GetDC(wnd);
-
         GHOST_ContextWGL *context = new GHOST_ContextWGL(
             context_params_offscreen,
             true,
-            wnd,
-            mHDC,
-            true, /* owns_window_handle */
+            nullptr,
+            nullptr,
             WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
             4,
             minor,

intern/ghost/intern/GHOST_WindowWin32.cc
@@ -639,7 +639,6 @@ GHOST_Context *GHOST_WindowWin32::newDrawingContext(GHOST_TDrawingContextType ty
             false,
             h_wnd_,
             h_DC_,
-            false, /* owns_window_handle */
             WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
             4,
             minor,