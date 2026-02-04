0a383efb3ef01d18ad82ad8a2468075e7ed2aeff
Pablo Vazquez <pablo@blender.org>
2026-02-04 19:37:59 +0100

    Add operator to the context menu of X and Y resolution

--- source/blender/editors/interface/interface_context_menu.cc
+++ source/blender/editors/interface/interface_context_menu.cc
@@ -864,6 +864,17 @@ bool popup_context_menu_for_button(bContext *C, Button *but, const wmEvent *even
     layout.separator();
 
     /* Property Operators */
+    /* Swap render X and Y dimensions. */
+    {
+      const std::string prop_id = RNA_property_identifier(but->rnaprop);
+
+      if (prop_id == "resolution_x" || prop_id == "resolution_y") {
+        layout.op("RENDER_OT_swap_dimensions",
+                  CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Swap Dimensions"),
+                  ICON_RENDER_SWAP_DIMENSIONS);
+        layout.separator();
+      }
+    }
 
     /* Copy Property Value
      * Paste Property Value */
