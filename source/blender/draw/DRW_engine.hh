a0edd7f76cefd485633c73f4fad5f0ff17414111
Jonas Holzman <jonas@blender.org>
2026-02-05 20:56:51 +0100

    make format

--- source/blender/draw/DRW_engine.hh
+++ source/blender/draw/DRW_engine.hh
@@ -66,7 +66,7 @@ void DRW_draw_region_engine_info(int xoffset, int *yoffset, int line_height);
 
 /**
  * \param context: can be nullptr, optionally passed to the DRWContext for callback execution.
-*  \param viewport: can be nullptr, in this case we create one.
+ * \param viewport: can be nullptr, in this case we create one.
  */
 void DRW_draw_render_loop_offscreen(Depsgraph *depsgraph,
                                     RenderEngineType *engine_type,
