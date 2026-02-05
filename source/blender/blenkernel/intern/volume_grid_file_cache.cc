8ae36a6c3bfd0b41afcc04eb8c4efbee623a5d45
Jacques Lucke <jacques@blender.org>
2026-02-05 18:02:35 +0100

    fix

--- source/blender/blenkernel/intern/volume_grid_file_cache.cc
+++ source/blender/blenkernel/intern/volume_grid_file_cache.cc
@@ -141,12 +141,13 @@ class GridReadKey : public GenericKey {
     return get_default_hash(this->file_path, this->grid_name, this->simplify_level);
   }
 
-  friend bool operator==(const GridReadKey &a, const GridReadKey &b) = default;
-
   bool equal_to(const GenericKey &other) const override
   {
     if (const auto *other_typed = dynamic_cast<const GridReadKey *>(&other)) {
-      return *this == *other_typed;
+      const GridReadKey &a = *this;
+      const GridReadKey &b = *other_typed;
+      return a.file_path == b.file_path && a.grid_name == b.grid_name &&
+             a.simplify_level == b.simplify_level;
     }
     return false;
   }
