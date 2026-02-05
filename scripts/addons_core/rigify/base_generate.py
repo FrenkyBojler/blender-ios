eee50cd1df2a06392a55148c1d2ce4db3e1bf0a5
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-02-05 14:33:39 +0100

    actually correct fix

--- scripts/addons_core/rigify/base_generate.py
+++ scripts/addons_core/rigify/base_generate.py
@@ -215,7 +215,10 @@ class BaseGenerator:
         self.scene = context.scene
         self.view_layer = context.view_layer
         self.layer_collection = context.layer_collection
-        self.collection = context.collection
+        self.collection = self.layer_collection.collection
+        if self.collection.override_library:
+            raise RuntimeError(
+                "Cannot generate rig into a library override collection. Select a different collection in the outliner")
         self.metarig = metarig
 
         # List of all rig instances
