6d6d8bc6a1bab11697a1f504582b9339397c0f1f
Habib Gahbiche <habib@blender.org>
2026-02-06 11:18:47 +0100

    Refactor: Compositor: Move gizmos to common header/file
    Ref: https://projects.blender.org/blender/blender/pulls/153930
    
    Node gizmos will be ported to the Image Editor. For that we need to move
    common functions to a common header. Implementation is also moved to a
    separate file because many utility functions will be common with  Image
    Editor gizmos.

---
+++ source/blender/nodes/NOD_compositor_gizmos.hh
@@ -0,0 +1,95 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup nodes
+ */
+
+struct bContext;
+struct wmGizmoGroup;
+struct ARegion;
+struct wmGizmoGroupType;
+
+namespace blender::nodes::gizmos {
+
+/* -------------------------------------------------------------------- */
+/** \name Common BBox Gizmos
+ * \{ */
+
+void WIDGETGROUP_bbox_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_bbox_mask_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Box Mask
+ * \{ */
+
+void WIDGETGROUP_node_box_mask_setup(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_box_mask_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Crop
+ * \{ */
+
+void WIDGETGROUP_node_crop_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_node_crop_setup(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_crop_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+void WIDGETGROUP_node_crop_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Glare
+ * \{ */
+
+void WIDGETGROUP_node_glare_setup(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_node_glare_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_glare_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+void WIDGETGROUP_node_glare_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Corner Pin
+ * \{ */
+
+void WIDGETGROUP_node_corner_pin_setup(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_node_corner_pin_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_corner_pin_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+void WIDGETGROUP_node_corner_pin_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Ellipse Mask
+ * \{ */
+
+void WIDGETGROUP_node_ellipse_mask_setup(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_ellipse_mask_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Split
+ * \{ */
+
+void WIDGETGROUP_node_split_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_node_split_setup(const bContext *C, wmGizmoGroup *gzgroup);
+bool WIDGETGROUP_node_split_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Backdrop Gizmo
+ * \{ */
+
+bool WIDGETGROUP_node_transform_poll(const bContext *C, wmGizmoGroupType *gzgt);
+void WIDGETGROUP_node_transform_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+void WIDGETGROUP_node_transform_setup(const bContext *C, wmGizmoGroup *gzgroup);
+
+/** \} */
+
+}  // namespace blender::nodes::gizmos
