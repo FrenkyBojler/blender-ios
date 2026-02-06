blender/blender

commit f0bf966fd78028835a8e6c44b06d7022d031d5e5

Fix #149845: Some linked objects may not be instantiated.
With collections being used as mere sub-data of e.g. nodegroups, i.e.
not being instantiated on linking, the previous check simply testing if
an object belongs to a collection was not enough enymore.

Replace it by gathering all instantiated objects (i.e. objects in
collections that are either instantiated in the viewlayer, or by a
collection instantiated by an Empty object in that viewlayer).

Pull Request: https://projects.blender.org/blender/blender/pulls/150072

source/blender/blenkernel/intern/blendfile_link_append.cc
@@ -406,25 +406,6 @@ static bool object_in_any_scene(Main *bmain, Object *ob)
   return false;
 }
 
-static bool object_in_any_collection(Main *bmain, Object *ob)
-{
-  LISTBASE_FOREACH (Collection *, collection, &bmain->collections) {
-    if (BKE_collection_has_object(collection, ob)) {
-      return true;
-    }
-  }
-
-  LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-    if (scene->master_collection != nullptr &&
-        BKE_collection_has_object(scene->master_collection, ob))
-    {
-      return true;
-    }
-  }
-
-  return false;
-}
-
 static bool collection_instantiated_by_any_object(Main *bmain, Collection *collection)
 {
   LISTBASE_FOREACH (Object *, ob, &bmain->objects) {
@@ -726,6 +707,35 @@ static void loose_data_instantiate_collection_process(
   }
 }
 
+static blender::Set<Object *> loose_data_gather_instanciated_objects(
+    LooseDataInstantiateContext &instantiate_context)
+{
+  BlendfileLinkAppendContext *lapp_context = instantiate_context.lapp_context;
+  const Scene *scene = lapp_context->params->context.scene;
+  ViewLayer *view_layer = lapp_context->params->context.view_layer;
+
+  blender::Set<Object *> instanciated_objects;
+  BKE_view_layer_synced_ensure(scene, view_layer);
+
+  /* Linked/appended objects only need to be instantiated if they are not already in the current
+   * view layer, either:
+   * - Directly instantiated there (i.e. in one of the view layer instantiated collections).
+   * - Indirectly instanciated (i.e. being in a collection that is object-instanciated).
+   */
+  FOREACH_OBJECT_BEGIN (scene, view_layer, ob_iter) {
+    instanciated_objects.add(ob_iter);
+    if (ob_iter->instance_collection) {
+      FOREACH_COLLECTION_OBJECT_RECURSIVE_BEGIN (ob_iter->instance_collection, ob_coll_iter) {
+        instanciated_objects.add(ob_iter);
+      }
+      FOREACH_COLLECTION_OBJECT_RECURSIVE_END;
+    }
+  }
+  FOREACH_OBJECT_END;
+
+  return instanciated_objects;
+}
+
 static void loose_data_instantiate_object_process(LooseDataInstantiateContext *instantiate_context)
 {
   BlendfileLinkAppendContext *lapp_context = instantiate_context->lapp_context;
@@ -742,6 +752,9 @@ static void loose_data_instantiate_object_process(LooseDataInstantiateContext *i
   bool object_set_active = false;
 
   const bool is_linking = (lapp_context->params->flag & FILE_LINK) != 0;
+
+  const blender::Set<Object *> instanciated_objects = loose_data_gather_instanciated_objects(
+      *instantiate_context);
 
   /* NOTE: For objects we only view_layer-instantiate duplicated objects that are not yet used
    * anywhere. */
@@ -764,7 +777,7 @@ static void loose_data_instantiate_object_process(LooseDataInstantiateContext *i
 
     Object *ob = (Object *)id;
 
-    if (object_in_any_collection(bmain, ob)) {
+    if (instanciated_objects.contains(ob)) {
       continue;
     }
 

FrenkyBojler/blender-ios

commit 25f885171bcdc0a246552ae67c830e9fc6a2ffde

Fix #153986: Objects from appended scene are added to current scene.
Regression from f0bf966fd7, when a Scene is appended or linked,
instanciation of objects also needs to be tested against this scene and
its ViewLayers.

source/blender/blenkernel/intern/blendfile_link_append.cc
@@ -709,6 +709,23 @@ static void loose_data_instantiate_collection_process(
   }
 }
 
+static void loose_data_gather_instanciated_objects_for_viewlayer(
+    const Scene &scene, ViewLayer &view_layer, Set<Object *> &r_instanciated_objects)
+{
+  BKE_view_layer_synced_ensure(&scene, &view_layer);
+
+  FOREACH_OBJECT_BEGIN (&scene, &view_layer, ob_iter) {
+    r_instanciated_objects.add(ob_iter);
+    if (ob_iter->instance_collection) {
+      FOREACH_COLLECTION_OBJECT_RECURSIVE_BEGIN (ob_iter->instance_collection, ob_coll_iter) {
+        r_instanciated_objects.add(ob_coll_iter);
+      }
+      FOREACH_COLLECTION_OBJECT_RECURSIVE_END;
+    }
+  }
+  FOREACH_OBJECT_END;
+}
+
 static Set<Object *> loose_data_gather_instanciated_objects(
     LooseDataInstantiateContext &instantiate_context)
 {
@@ -717,23 +734,31 @@ static Set<Object *> loose_data_gather_instanciated_objects(
   ViewLayer *view_layer = lapp_context->params->context.view_layer;
 
   Set<Object *> instanciated_objects;
-  BKE_view_layer_synced_ensure(scene, view_layer);
 
   /* Linked/appended objects only need to be instantiated if they are not already in the current
    * view layer, either:
    * - Directly instantiated there (i.e. in one of the view layer instantiated collections).
    * - Indirectly instanciated (i.e. being in a collection that is object-instanciated).
    */
-  FOREACH_OBJECT_BEGIN (scene, view_layer, ob_iter) {
-    instanciated_objects.add(ob_iter);
-    if (ob_iter->instance_collection) {
-      FOREACH_COLLECTION_OBJECT_RECURSIVE_BEGIN (ob_iter->instance_collection, ob_coll_iter) {
-        instanciated_objects.add(ob_coll_iter);
-      }
-      FOREACH_COLLECTION_OBJECT_RECURSIVE_END;
+  loose_data_gather_instanciated_objects_for_viewlayer(*scene, *view_layer, instanciated_objects);
+
+  /* When linking or appending a whole Scene, typically its objects are already instantiated there,
+   * so no need to instantiate them in the active Scene.
+   *
+   * See also #153986. */
+  for (BlendfileLinkAppendContextItem &item : lapp_context->items) {
+    if (!item.new_id) {
+      continue;
+    }
+    if (GS(item.new_id->name) != ID_SCE) {
+      continue;
+    }
+    Scene &scene_iter = *id_cast<Scene *>(item.new_id);
+    for (ViewLayer &view_layer_iter : scene_iter.view_layers) {
+      loose_data_gather_instanciated_objects_for_viewlayer(
+          scene_iter, view_layer_iter, instanciated_objects);
     }
   }
-  FOREACH_OBJECT_END;
 
   return instanciated_objects;
 }