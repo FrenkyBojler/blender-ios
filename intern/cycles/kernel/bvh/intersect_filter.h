blender/blender

commit 5b5d9f2f0cc10eead9e7bd788c2d43d19c7e7c8d

Refactor: De-duplicate Cycles shadow_all HW-RT function
The goal is to re-use as much of non-trivial logic across devices as
possible, ideally including data-structures and their initialization.

The payload is a bit bigger than it was and it does not utilize OptiX
registers. It seems to be a bit hard to get reliable numbers as they
fluctuate quite a bit and depend on the order of renders, but the
worst performance impact is about 1.5% on the Bistro scene.

It feels there is a room for improvements, like relying on OptiX
itself to keep track on the maximum intersection distance (the code
does accept intersection, but it still tracks the maximum distance in
the filter function), and, maybe, putting payload back on the register.
However, it'll be a bit harder to replicate such behavior on other
platforms, so perhaps it is not a bad trade-off to have.

intern/cycles/kernel/bvh/intersect_filter.h
@@ -1,0 +1,255 @@
+/* SPDX-FileCopyrightText: 2026 Blender Foundation
+ *
+ * SPDX-License-Identifier: Apache-2.0 */
+
+/* Intersection and filtering functions for hardware ray-trace style of API.
+ *
+ * Filter functions are invoked for an intersection to give BVH traversal hints whether
+ * traversal is to continue. Returning true from filter functions means the intersection is
+ * filtered (ignored) and the traversal is to continue.
+ *
+ * Note on the template parameters
+ * ===============================
+ *
+ * perform_intersection_tests controls whether checks that are typically are performed during
+ * intersection are to be done in the filter function. Intersection checks that are done by the
+ * hardware do not perform self-intersection and shadow-linking checks: they are done in the
+ * filter function instead. However, if the intersection check uses custom function it performs
+ * these checks early on, so skipping them in the filter function will lead to a better
+ * performance. */
+
+#pragma once
+
+#include "kernel/bvh/util.h"
+#include "kernel/globals.h"
+#include "kernel/integrator/state.h"
+#include "kernel/types.h"
+
+CCL_NAMESPACE_BEGIN
+
+/* Special tricks to subclass payload.
+ * The issue here is Metal does not support subclassing, but HIP-RT had performance issues with
+ * composition in the past (see !136823). */
+#if defined(__KERNEL_HIPRT__)
+#  define BVH_PAYLOAD_SUBCLASS(cls, base_cls) struct cls : base_cls
+#  define BVH_PAYLOAD_SUBCLASS_DEFINE(base_cls)
+#  define BVH_PAYLOAD_BASE(obj) (obj)
+#else
+#  define BVH_PAYLOAD_SUBCLASS(cls, base_cls) struct cls
+#  define BVH_PAYLOAD_SUBCLASS_DEFINE(base_cls) base_cls base;
+#  define BVH_PAYLOAD_BASE(obj) ((obj).base)
+#endif
+
+struct BVHPayload {
+  /* In OptiX, self-intersection information and ray visibility are passed via Ray's pointer as
+   * extra payload data. */
+#if !defined(__KERNEL_OPTIX__)
+  /* Primitives for the self-intersections. */
+  RaySelfPrimitives ray_self;
+
+  /* Ray visibility flags and time. */
+  uint ray_visibility;
+#endif
+
+#if defined(__KERNEL_HIPRT__)
+  float ray_time;
+#endif
+};
+
+/* OptiX passes various parameters via registers to the tracing calls. No need to store duplicate
+ * data for OptiX. This will essentially make it so BVHPayload contains data which is strictly
+ * needed for intersection recording and for tracking curve transparency. */
+#if defined(__KERNEL_OPTIX__)
+#  define BVH_SHADOW_ALL_PAYLOAD_SUBCLASS(cls, base_cls) struct cls
+#  define BVH_SHADOW_ALL_PAYLOAD_SUBCLASS_DEFINE(base_cls)
+#else
+#  define BVH_SHADOW_ALL_PAYLOAD_SUBCLASS(cls, base_cls) BVH_PAYLOAD_SUBCLASS(cls, base_cls)
+#  define BVH_SHADOW_ALL_PAYLOAD_SUBCLASS_DEFINE(base_cls) BVH_PAYLOAD_SUBCLASS_DEFINE(base_cls)
+#endif
+
+BVH_SHADOW_ALL_PAYLOAD_SUBCLASS(BVHShadowAllPayload, BVHPayload)
+{
+  BVH_SHADOW_ALL_PAYLOAD_SUBCLASS_DEFINE(BVHPayload);
+
+  /* Using uint16_t is slower on HIP, while it is similar performance but potentially lower memory
+   * footprint on other backends. */
+#if defined(__KERNEL_HIPRT__)
+  using UIntType = uint;
+#else
+  using UIntType = uint16_t;
+#endif
+
+  IntegratorShadowState state;
+
+  /* The maximum number of transparent intersections to consider: if there are more intersections
+   * than this value, all light is considered blocked. */
+  UIntType max_transparent_hits;
+  /* The number of transparent intersections tested during BVH traversal. It might be higher than
+   * the number of recorded intersections. */
+  UIntType num_transparent_hits = 0;
+
+  /* Maximum intersection distance t for intersections that are to be recorded.
+   * If intersection's distance exceeds this value, it is not recoded. */
+  float max_record_isect_t;
+
+  /* An index within the shadow_isect array at which the next intersection will be recorded. */
+  UIntType record_isect_index = 0;
+
+  /* The number of intersections that has been attempted to be recorded.
+   * It might be higher than the shadow_isect size, indicating that more invocations of the
+   * intersection kernel are needed. It is different from the num_transparent_hits as it does not
+   * include transparent curve intersections that are handled by accumulating throughput in the
+   * filter function. */
+  UIntType num_recorded_hits = 0;
+
+  /* Accumulated throughput of transparent curve intersections.
+   * Curves are using special optimization by baking their transparency and handling it in the
+   * filter function. */
+  float throughput = 1.0f;
+};
+
+/* Filter intersection with possibly transparent surface.
+ *
+ * Designed to be used from the any-hit type of traversal:
+ * - If an opaque surface is hit, returns false, stopping traversal. The scene intersection
+ *   function will consider the shadow ray to be blocked.
+ * - If a transparent surface is hit, the intersection is recorded into the shadow_isect array in
+ *   the state. The closest N intersections are recorded. */
+template<bool perform_intersection_tests, uint enabled_primitive_types = PRIMITIVE_ALL>
+ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
+    KernelGlobals kg,
+    IntegratorShadowState state,
+    ccl_ray_data BVHShadowAllPayload &ccl_restrict payload,
+    const ccl_ray_data RaySelfPrimitives &ccl_restrict ray_self,
+    const uint ray_visibility,
+    const Intersection isect)
+
+{
+  if constexpr (perform_intersection_tests) {
+#if defined(__VISIBILITY_FLAG__)
+    if ((kernel_data_fetch(objects, isect.object).visibility & ray_visibility) == 0) {
+      return true;
+    }
+#endif
+
+#if defined(__SHADOW_LINKING__)
+    if (intersection_skip_shadow_link(kg, ray_self, isect.object)) {
+      return true;
+    }
+#endif
+
+    if (intersection_skip_self_shadow(ray_self, isect.object, isect.prim)) {
+      return true;
+    }
+  }
+
+#if !defined(__TRANSPARENT_SHADOWS__)
+  /* No transparent shadows in the scene, all light is blocked and we can stop immediately. */
+  payload.throughput = 0.0f;
+  return false;
+#else
+  /* Detect if this surface has a shader with transparent shadows. */
+  /* TODO: optimize so primitive visibility flag indicates if the primitive has a transparent
+   * shadow shader? */
+  const int shader_flags = intersection_get_shader_flags(kg, isect.prim, isect.type);
+  if ((shader_flags & SD_HAS_TRANSPARENT_SHADOW) == 0) {
+    /* No transparent shadows for the shader, all light is blocked, and we can stop immediately. */
+    payload.throughput = 0.0f;
+    return false;
+  }
+
+  /* Fetch commonly accessed payload data, ensuring that it is used from either register to a
+   * stack, without going to the global memory. */
+  uint num_recorded_hits = payload.num_recorded_hits;
+
+  /* If the intersection is already recorded, ignore it completely: don't update throughput as it
+   * has already been updated. But also don't count it for num_hits as that could result in a
+   * situation when the same ray will be considered transparent when spatial split is off and be
+   * opaque when spatial split is on. Since curves do not record intersections, there is
+   * a possibility for optimization:
+   * - Don't compile this code if the filter is only used for curve primitives.
+   * - Don't run the check if the current intersection comes from the curve, as it will not match
+   *   any recorded intersection anyway.
+   *
+   * NOTE: Currently, spatial splits are not used with OptiX, so there is no need to check whether
+   * the intersection has been already recorded. */
+#  if !defined(__KERNEL_OPTIX__)
+  if constexpr (enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) {
+    if ((isect.type & PRIMITIVE_CURVE) == 0) {
+      if (intersection_skip_shadow_already_recoded(
+              state, isect.object, isect.prim, num_recorded_hits))
+      {
+        return true;
+      }
+    }
+  }
+#  endif
+
+  /* Only count transparent bounces, volume bounds bounces are counted when shading. */
+  payload.num_transparent_hits += !(shader_flags & SD_HAS_ONLY_VOLUME);
+  if (payload.num_transparent_hits > payload.max_transparent_hits) {
+    /* The maximum number of intersections has been reached, consider that all light has been
+     * blocked. */
+    payload.throughput = 0.0f;
+    return false;
+  }
+
+#  if defined(__HAIR__)
+  if constexpr (enabled_primitive_types & PRIMITIVE_CURVE) {
+    /* Always use baked shadow transparency for curves. */
+    if (isect.type & PRIMITIVE_CURVE) {
+      payload.throughput *= intersection_curve_shadow_transparency(
+          kg, isect.object, isect.prim, isect.type, isect.u);
+
+      if (payload.throughput < CURVE_SHADOW_TRANSPARENCY_CUTOFF) {
+        /* Light attenuated too much through the curve intersections, assume all light is blocked
+         * and do early output. */
+        payload.throughput = 0.0f;
+        return false;
+      }
+
+      /* Don't record the intersection as the throughput has been already modified here.
+       * Simply continue BVH traversal for other intersections. */
+      return true;
+    }
+  }
+#  endif
+
+  /* If the filter function only handles curves, it is known for the fact that nothing is to be
+   * recorded: curves accumulated baked transparency. Skip this code for a curve-only case. */
+  if constexpr (enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) {
+    /* Always increase the number of recorded hits, even beyond the maximum, so that we can detect
+     * this and trace another ray if needed. */
+    num_recorded_hits += 1;
+    payload.num_recorded_hits = num_recorded_hits;
+
+    constexpr uint max_record_hits = INTEGRATOR_SHADOW_ISECT_SIZE;
+    if (num_recorded_hits <= max_record_hits || isect.t < payload.max_record_isect_t) {
+      integrator_state_write_shadow_isect(state, &isect, payload.record_isect_index);
+
+      if (num_recorded_hits >= max_record_hits) {
+        /* If the maximum number of hits is reached, find the furthest intersection to replace it
+         * with the next closer one. We want the N closest intersections. */
+        uint record_isect_index = 0;
+        float tmax_hits = INTEGRATOR_STATE_ARRAY(state, shadow_isect, 0, t);
+        for (uint i = 1; i < max_record_hits; ++i) {
+          const float isect_t = INTEGRATOR_STATE_ARRAY(state, shadow_isect, i, t);
+          if (isect_t > tmax_hits) {
+            record_isect_index = i;
+            tmax_hits = isect_t;
+          }
+        }
+        payload.max_record_isect_t = tmax_hits;
+        payload.record_isect_index = record_isect_index;
+      }
+      else {
+        payload.record_isect_index = num_recorded_hits;
+      }
+    }
+  }
+
+  return true;
+#endif
+}
+
+CCL_NAMESPACE_END

intern/cycles/kernel/CMakeLists.txt
@@ -146,6 +146,7 @@ set(SRC_KERNEL_BAKE_HEADERS
 
 set(SRC_KERNEL_BVH_HEADERS
   bvh/bvh.h
+  bvh/intersect_filter.h
   bvh/nodes.h
   bvh/shadow_all.h
   bvh/local.h

intern/cycles/kernel/bvh/bvh.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "kernel/bvh/intersect_filter.h"
 #include "kernel/bvh/nodes.h"
 #include "kernel/bvh/types.h"
 #include "kernel/bvh/util.h"
@@ -52,6 +53,133 @@ static constexpr sycl::specialization_id<RTCFeatureFlags> oneapi_embree_features
 #endif
 
 CCL_NAMESPACE_BEGIN
+
+/* --------------------------------------------------------------------
+ * Transparent shadow BVH traversal, recording multiple intersections.
+ */
+
+#ifdef __TRANSPARENT_SHADOWS__
+
+#  if defined(__BVH2__)
+#    define BVH_FUNCTION_NAME bvh_intersect_shadow_all
+#    define BVH_FUNCTION_FEATURES BVH_POINTCLOUD
+#    include "kernel/bvh/shadow_all.h"
+
+#    if defined(__HAIR__)
+#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_hair
+#      define BVH_FUNCTION_FEATURES BVH_HAIR | BVH_POINTCLOUD
+#      include "kernel/bvh/shadow_all.h"
+#    endif
+
+#    if defined(__OBJECT_MOTION__)
+#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_motion
+#      define BVH_FUNCTION_FEATURES BVH_MOTION | BVH_POINTCLOUD
+#      include "kernel/bvh/shadow_all.h"
+#    endif
+
+#    if defined(__HAIR__) && defined(__OBJECT_MOTION__)
+#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_hair_motion
+#      define BVH_FUNCTION_FEATURES BVH_HAIR | BVH_MOTION | BVH_POINTCLOUD
+#      include "kernel/bvh/shadow_all.h"
+#    endif
+
+ccl_device_inline void scene_intersect_shadow_all_bvh2(
+    KernelGlobals kg,
+    const ccl_private Ray *ccl_restrict ray,
+    ccl_private BVHShadowAllPayload &ccl_restrict payload)
+{
+#    ifdef __OBJECT_MOTION__
+  if (kernel_data.bvh.have_motion) {
+#      ifdef __HAIR__
+    if (kernel_data.bvh.have_curves) {
+      bvh_intersect_shadow_all_hair_motion(kg, ray, payload);
+      return;
+    }
+#      endif /* __HAIR__ */
+    bvh_intersect_shadow_all_motion(kg, ray, payload);
+    return;
+  }
+#    endif /* __OBJECT_MOTION__ */
+
+#    ifdef __HAIR__
+  if (kernel_data.bvh.have_curves) {
+    bvh_intersect_shadow_all_hair(kg, ray, payload);
+    return;
+  }
+#    endif /* __HAIR__ */
+  bvh_intersect_shadow_all(kg, ray, payload);
+}
+#  endif /* __BVH2__ */
+
+ccl_device_intersect void scene_intersect_shadow_all(KernelGlobals kg,
+                                                     IntegratorShadowState state,
+                                                     const ccl_private Ray *ray,
+                                                     const uint visibility,
+                                                     const uint max_transparent_hits,
+                                                     ccl_private uint *num_recorded_hits,
+                                                     ccl_private float *throughput)
+{
+#  if !defined(__KERNEL_OPTIX__)
+  /* OptiX does not perform well with conditional trace calls, so it handles the validity of the
+   * ray in the scene_intersect_shadow_all_optix(). */
+  if (!intersection_ray_valid(ray)) {
+    *num_recorded_hits = 0;
+    *throughput = 1.0f;
+    return;
+  }
+#  endif
+
+#  ifdef __EMBREE__
+  IF_USING_EMBREE
+  {
+    if (kernel_data.device_bvh) {
+      kernel_embree_intersect_shadow_all(
+          kg, state, ray, visibility, max_transparent_hits, num_recorded_hits, throughput);
+      return;
+    }
+  }
+#  endif
+
+  IF_NOT_USING_EMBREE
+  {
+    BVHShadowAllPayload payload;
+
+    /* A bit of a tricky initialization:
+     * - Some backends require extra ray information for custom motion blur intersection.
+     * - Some backends utilize registers to pass commonly accessed data to the trace calls. */
+#  if !defined(__KERNEL_OPTIX__)
+    BVH_PAYLOAD_BASE(payload).ray_self = ray->self;
+    BVH_PAYLOAD_BASE(payload).ray_visibility = visibility;
+#    if defined(__KERNEL_HIPRT__)
+    BVH_PAYLOAD_BASE(payload).ray_time = ray->time;
+#    endif
+#  endif
+
+    payload.state = state;
+    payload.max_transparent_hits = max_transparent_hits;
+    payload.max_record_isect_t = ray->tmax;
+
+#  if defined(__BVH2__)
+    scene_intersect_shadow_all_bvh2(kg, ray, payload);
+#  elif defined(__KERNEL_HIPRT__)
+    scene_intersect_shadow_all_hiprt(kg, ray, payload);
+#  elif defined(__KERNEL_METALRT__)
+    scene_intersect_shadow_all_metalrt(ray, payload);
+#  elif defined(__KERNEL_OPTIX__)
+    scene_intersect_shadow_all_optix(ray, visibility, payload);
+#  endif
+
+    *num_recorded_hits = payload.num_recorded_hits;
+    *throughput = payload.throughput;
+
+    return;
+  }
+
+  kernel_assert(false);
+}
+#endif /* __TRANSPARENT_SHADOWS__ */
+
+// ------------------------------------------------------------------------------------------------
 
 #ifdef __BVH2__
 
@@ -198,92 +326,6 @@ ccl_device_intersect bool scene_intersect_local(KernelGlobals kg,
   return false;
 }
 #  endif
-
-/* Transparent shadow BVH traversal, recording multiple intersections. */
-
-#  ifdef __TRANSPARENT_SHADOWS__
-
-#    define BVH_FUNCTION_NAME bvh_intersect_shadow_all
-#    define BVH_FUNCTION_FEATURES BVH_POINTCLOUD
-#    include "kernel/bvh/shadow_all.h"
-
-#    if defined(__HAIR__)
-#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_hair
-#      define BVH_FUNCTION_FEATURES BVH_HAIR | BVH_POINTCLOUD
-#      include "kernel/bvh/shadow_all.h"
-#    endif
-
-#    if defined(__OBJECT_MOTION__)
-#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_motion
-#      define BVH_FUNCTION_FEATURES BVH_MOTION | BVH_POINTCLOUD
-#      include "kernel/bvh/shadow_all.h"
-#    endif
-
-#    if defined(__HAIR__) && defined(__OBJECT_MOTION__)
-#      define BVH_FUNCTION_NAME bvh_intersect_shadow_all_hair_motion
-#      define BVH_FUNCTION_FEATURES BVH_HAIR | BVH_MOTION | BVH_POINTCLOUD
-#      include "kernel/bvh/shadow_all.h"
-#    endif
-
-ccl_device_intersect void scene_intersect_shadow_all(KernelGlobals kg,
-                                                     IntegratorShadowState state,
-                                                     const ccl_private Ray *ray,
-                                                     const uint visibility,
-                                                     const uint max_transparent_hits,
-                                                     ccl_private uint *num_recorded_hits,
-                                                     ccl_private float *throughput)
-{
-  if (!intersection_ray_valid(ray)) {
-    *num_recorded_hits = 0;
-    *throughput = 1.0f;
-    return;
-  }
-
-#    ifdef __EMBREE__
-  IF_USING_EMBREE
-  {
-    if (kernel_data.device_bvh) {
-      kernel_embree_intersect_shadow_all(
-          kg, state, ray, visibility, max_transparent_hits, num_recorded_hits, throughput);
-      return;
-    }
-  }
-#    endif
-
-  IF_NOT_USING_EMBREE
-  {
-#    ifdef __OBJECT_MOTION__
-    if (kernel_data.bvh.have_motion) {
-#      ifdef __HAIR__
-      if (kernel_data.bvh.have_curves) {
-        bvh_intersect_shadow_all_hair_motion(
-            kg, ray, state, visibility, max_transparent_hits, num_recorded_hits, throughput);
-        return;
-      }
-#      endif /* __HAIR__ */
-
-      bvh_intersect_shadow_all_motion(
-          kg, ray, state, visibility, max_transparent_hits, num_recorded_hits, throughput);
-      return;
-    }
-#    endif /* __OBJECT_MOTION__ */
-
-#    ifdef __HAIR__
-    if (kernel_data.bvh.have_curves) {
-      bvh_intersect_shadow_all_hair(
-          kg, ray, state, visibility, max_transparent_hits, num_recorded_hits, throughput);
-      return;
-    }
-#    endif /* __HAIR__ */
-
-    bvh_intersect_shadow_all(
-        kg, ray, state, visibility, max_transparent_hits, num_recorded_hits, throughput);
-    return;
-  }
-
-  kernel_assert(false);
-}
-#  endif /* __TRANSPARENT_SHADOWS__ */
 
 /* Volume BVH traversal, for initializing or updating the volume stack. */
 

intern/cycles/kernel/bvh/shadow_all.h
@@ -28,12 +28,8 @@ ccl_device_inline
 #endif
     void
     BVH_FUNCTION_FULL_NAME(BVH)(KernelGlobals kg,
-                                const ccl_private Ray *ray,
-                                IntegratorShadowState state,
-                                const uint visibility,
-                                const uint max_transparent_hits,
-                                ccl_private uint *r_num_recorded_hits,
-                                ccl_private float *r_throughput)
+                                const ccl_private Ray *ccl_restrict ray,
+                                ccl_private BVHShadowAllPayload &ccl_restrict payload)
 {
   /* todo:
    * - likely and unlikely for if() statements
@@ -54,18 +50,12 @@ ccl_device_inline
   float3 idir = bvh_inverse_direction(dir);
   float tmin = ray->tmin;
   int object = OBJECT_NONE;
-  uint num_transparent_hits = 0;
 
-  /* Max distance in world space. May be dynamically reduced when max number of
-   * recorded hits is exceeded and we no longer need to find hits beyond the max
-   * distance found. */
+  /* Max distance in world space. May be dynamically reduced when max number of recorded hits is
+   * exceeded and we no longer need to find hits beyond the max distance found. */
   const float tmax = ray->tmax;
-  float tmax_hits = tmax;
 
-  uint isect_index = 0;
-
-  *r_num_recorded_hits = 0;
-  *r_throughput = 1.0f;
+  const uint visibility = payload.base.ray_visibility;
 
   /* traversal loop */
   do {
@@ -222,77 +212,15 @@ ccl_device_inline
               }
             }
 
-            /* shadow ray early termination */
             if (hit) {
-              /* Detect if this surface has a shader with transparent shadows. */
-              /* TODO: optimize so primitive visibility flag indicates if the primitive has a
-               * transparent shadow shader? */
-              const int flags = intersection_get_shader_flags(kg, isect.prim, isect.type);
-              if ((flags & SD_HAS_TRANSPARENT_SHADOW) == 0) {
-                /* If no transparent shadows, all light is blocked and we can stop immediately. */
-                *r_throughput = 0.0f;
-                return;
-              }
-
-              /* If the intersection is already recoded ignore it completely: don't update
-               * throughput as it has been already updated. But also don't count it for num_hits
-               * as that could result in situation when the same ray will be considered transparent
-               * when spatial split is off, and be opaque when spatial split is on. */
-              if (intersection_skip_shadow_already_recoded(
-                      state, isect.object, isect.prim, *r_num_recorded_hits))
+              if (!bvh_shadow_all_anyhit_filter<false>(kg,
+                                                       payload.state,
+                                                       payload,
+                                                       payload.base.ray_self,
+                                                       payload.base.ray_visibility,
+                                                       isect))
               {
-                continue;
-              }
-
-              /* Only count transparent bounces, volume bounds bounces are counted when shading. */
-              num_transparent_hits += !(flags & SD_HAS_ONLY_VOLUME);
-              if (num_transparent_hits > max_transparent_hits) {
-                *r_throughput = 0.0f;
                 return;
-              }
-
-              bool record_intersection = true;
-
-              /* Always use baked shadow transparency for curves. */
-              if (isect.type & PRIMITIVE_CURVE) {
-                *r_throughput *= intersection_curve_shadow_transparency(
-                    kg, isect.object, isect.prim, isect.type, isect.u);
-
-                if (*r_throughput < CURVE_SHADOW_TRANSPARENCY_CUTOFF) {
-                  *r_throughput = 0.0f;
-                  return;
-                }
-                record_intersection = false;
-              }
-
-              if (record_intersection) {
-                /* Test if we need to record this transparent intersection. */
-
-                /* Always increase the number of recorded hits, even beyond the maximum,
-                 * so that we can detect this and trace another ray if needed. */
-                ++(*r_num_recorded_hits);
-
-                const uint max_record_hits = (uint)INTEGRATOR_SHADOW_ISECT_SIZE;
-                if (*r_num_recorded_hits <= max_record_hits || isect.t < tmax_hits) {
-                  integrator_state_write_shadow_isect(state, &isect, isect_index);
-
-                  if (*r_num_recorded_hits >= max_record_hits) {
-                    /* If the maximum number of hits is reached, find the furthest intersection to
-                     * replace it with the next closer one. We want N closest intersections. */
-                    isect_index = 0;
-                    tmax_hits = INTEGRATOR_STATE_ARRAY(state, shadow_isect, 0, t);
-                    for (uint i = 1; i < max_record_hits; ++i) {
-                      const float isect_t = INTEGRATOR_STATE_ARRAY(state, shadow_isect, i, t);
-                      if (isect_t > tmax_hits) {
-                        isect_index = i;
-                        tmax_hits = isect_t;
-                      }
-                    }
-                  }
-                  else {
-                    isect_index = *r_num_recorded_hits;
-                  }
-                }
               }
             }
           }
@@ -330,15 +258,10 @@ ccl_device_inline
 }
 
 ccl_device_inline void BVH_FUNCTION_NAME(KernelGlobals kg,
-                                         const ccl_private Ray *ray,
-                                         IntegratorShadowState state,
-                                         const uint visibility,
-                                         const uint max_transparent_hits,
-                                         ccl_private uint *num_recorded_hits,
-                                         ccl_private float *throughput)
+                                         const ccl_private Ray *ccl_restrict ray,
+                                         ccl_private BVHShadowAllPayload &ccl_restrict payload)
 {
-  BVH_FUNCTION_FULL_NAME(BVH)(
-      kg, ray, state, visibility, max_transparent_hits, num_recorded_hits, throughput);
+  BVH_FUNCTION_FULL_NAME(BVH)(kg, ray, payload);
 }
 
 #undef BVH_FUNCTION_NAME

FrenkyBojler/blender-ios

commit f8223769d2d9e7be437d6be5c5e32403113349d2

Fix #154069: Cycles Metal error on macOS 13
If constexpr must evaluate to a boolean in this Metal version.

Regression from 5b5d9f2f0cc10eead9e7bd788c2d43d19c7e7c8d.

intern/cycles/kernel/bvh/intersect_filter.h
@@ -174,7 +174,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
    * NOTE: Currently, spatial splits are not used with OptiX, so there is no need to check whether
    * the intersection has been already recorded. */
 #  if !defined(__KERNEL_OPTIX__)
-  if constexpr (enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) {
+  if constexpr ((enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) != 0) {
     if ((isect.type & PRIMITIVE_CURVE) == 0) {
       if (intersection_skip_shadow_already_recoded(
               state, isect.object, isect.prim, num_recorded_hits))
@@ -195,7 +195,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
   }
 
 #  if defined(__HAIR__)
-  if constexpr (enabled_primitive_types & PRIMITIVE_CURVE) {
+  if constexpr ((enabled_primitive_types & PRIMITIVE_CURVE) != 0) {
     /* Always use baked shadow transparency for curves. */
     if (isect.type & PRIMITIVE_CURVE) {
       payload.throughput *= intersection_curve_shadow_transparency(
@@ -217,7 +217,7 @@ ccl_device_forceinline bool bvh_shadow_all_anyhit_filter(
 
   /* If the filter function only handles curves, it is known for the fact that nothing is to be
    * recorded: curves accumulated baked transparency. Skip this code for a curve-only case. */
-  if constexpr (enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) {
+  if constexpr ((enabled_primitive_types & (PRIMITIVE_ALL & ~PRIMITIVE_CURVE)) != 0) {
     /* Always increase the number of recorded hits, even beyond the maximum, so that we can detect
      * this and trace another ray if needed. */
     num_recorded_hits += 1;