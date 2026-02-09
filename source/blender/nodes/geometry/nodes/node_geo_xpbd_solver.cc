c39d27874adecb52a486318a3c914621f4b9c661
Jacques Lucke <jacques@blender.org>
2026-02-09 08:47:19 +0100

    progress

--- source/blender/nodes/geometry/nodes/node_geo_xpbd_solver.cc
+++ source/blender/nodes/geometry/nodes/node_geo_xpbd_solver.cc
@@ -146,58 +146,131 @@ struct PinPositionConstraintInfo {
   std::string lambda_attribute_name;
 };
 
-static Geometries gather_geometries(Bundle &world)
-{
-  Geometries geometries;
+struct ConstraintsInfo {
+  Vector<PinPositionConstraintInfo> pin_positions;
+};
 
-  /* Gather geometry bundle paths. */
-  geometries.paths = gather_bundle_paths_by_type(world, XPBDGeometryBundle::name);
+class XpbdSolverStep {
+ private:
+  ResourceScope &scope_;
+  Bundle &world_;
+  const float delta_time_;
+  FieldEvaluators field_evaluators_;
 
-  /* Move the geometry sets out of the bundles. They are put back in after the simulation. */
-  geometries.geometry_sets.reinitialize(geometries.paths.size());
-  for (const int i : geometries.paths.index_range()) {
-    const StringRef geometry_path = geometries.paths[i];
-    if (GeometrySet *geometry = world.lookup_path_for_write_ptr<GeometrySet>(geometry_path +
-                                                                             "/geometry"))
-    {
-      geometries.geometry_sets[i] = std::move(*geometry);
-    }
+  Geometries geometries_;
+  ConstraintsInfo constraints_info_;
+
+ public:
+  XpbdSolverStep(ResourceScope &scope, Bundle &world, const float delta_time)
+      : scope_(scope), world_(world), delta_time_(delta_time), field_evaluators_(scope_)
+  {
   }
 
-  /* Gather individual components that should be simulated. There may be more than geometry sets
-   * because each geometry set could contain e.g. a mesh and curves. */
-  for (const int geo_bundle_i : geometries.paths.index_range()) {
-    GeometrySet &geometry = geometries.geometry_sets[geo_bundle_i];
-    for (bke::GeometryComponent::Type type : {bke::GeometryComponent::Type::Mesh,
-                                              bke::GeometryComponent::Type::PointCloud,
-                                              bke::GeometryComponent::Type::Curve,
-                                              bke::GeometryComponent::Type::Instance})
-    {
-      if (!geometry.has(type)) {
-        continue;
+  void do_step()
+  {
+    this->gather_geometries_from_world();
+    this->gather_constraints_from_world();
+    this->do_simulation();
+    this->write_back_geometries_to_world();
+  }
+
+ private:
+  void gather_geometries_from_world()
+  {
+    /* Gather geometry bundle paths. */
+    geometries_.paths = gather_bundle_paths_by_type(world_, XPBDGeometryBundle::name);
+
+    /* Move the geometry sets out of the bundles. They are put back in after the simulation. */
+    geometries_.geometry_sets.reinitialize(geometries_.paths.size());
+    for (const int i : geometries_.paths.index_range()) {
+      const StringRef geometry_path = geometries_.paths[i];
+      if (GeometrySet *geometry = world_.lookup_path_for_write_ptr<GeometrySet>(geometry_path +
+                                                                                "/geometry"))
+      {
+        geometries_.geometry_sets[i] = std::move(*geometry);
       }
-      bke::GeometryComponent &component = geometry.get_component_for_write(type);
-      geometries.data_keys.add_new({geo_bundle_i, type, std::nullopt});
-      geometries.attribute_accessors.append(*component.attributes_for_write());
     }
-    if (geometry.has_grease_pencil()) {
-      using namespace blender::bke::greasepencil;
-      GreasePencil &grease_pencil = *geometry.get_grease_pencil_for_write();
-      for (const int layer_i : grease_pencil.layers().index_range()) {
-        Layer &layer = grease_pencil.layer(layer_i);
-        Drawing *drawing = grease_pencil.get_eval_drawing(layer);
-        if (!drawing) {
+
+    /* Gather individual components that should be simulated. There may be more than geometry sets
+     * because each geometry set could contain e.g. a mesh and curves. */
+    for (const int geo_bundle_i : geometries_.paths.index_range()) {
+      GeometrySet &geometry = geometries_.geometry_sets[geo_bundle_i];
+      for (bke::GeometryComponent::Type type : {bke::GeometryComponent::Type::Mesh,
+                                                bke::GeometryComponent::Type::PointCloud,
+                                                bke::GeometryComponent::Type::Curve,
+                                                bke::GeometryComponent::Type::Instance})
+      {
+        if (!geometry.has(type)) {
           continue;
         }
-        bke::CurvesGeometry &curves = drawing->strokes_for_write();
-        geometries.attribute_accessors.append(curves.attributes_for_write());
-        geometries.data_keys.add_new({geo_bundle_i, bke::GeometryComponent::Type::Curve, layer_i});
+        bke::GeometryComponent &component = geometry.get_component_for_write(type);
+        geometries_.data_keys.add_new({geo_bundle_i, type, std::nullopt});
+        geometries_.attribute_accessors.append(*component.attributes_for_write());
+      }
+      if (geometry.has_grease_pencil()) {
+        using namespace blender::bke::greasepencil;
+        GreasePencil &grease_pencil = *geometry.get_grease_pencil_for_write();
+        for (const int layer_i : grease_pencil.layers().index_range()) {
+          Layer &layer = grease_pencil.layer(layer_i);
+          Drawing *drawing = grease_pencil.get_eval_drawing(layer);
+          if (!drawing) {
+            continue;
+          }
+          bke::CurvesGeometry &curves = drawing->strokes_for_write();
+          geometries_.attribute_accessors.append(curves.attributes_for_write());
+          geometries_.data_keys.add_new(
+              {geo_bundle_i, bke::GeometryComponent::Type::Curve, layer_i});
+        }
       }
     }
   }
 
-  return geometries;
-}
+  void gather_constraints_from_world()
+  {
+    const Vector<std::string> pin_positions_bundle_paths = gather_bundle_paths_by_type(
+        world_, PinnedPositionXPBDConstraintBundle::name);
+    for (const int pin_position_bundle_i : pin_positions_bundle_paths.index_range()) {
+      const StringRef pin_positions_path = pin_positions_bundle_paths[pin_position_bundle_i];
+    }
+  }
+
+  void do_simulation()
+  {
+    for (const int data_key_i : geometries_.data_keys.index_range()) {
+      bke::MutableAttributeAccessor &attributes = geometries_.attribute_accessors[data_key_i];
+      bke::SpanAttributeWriter<float3> positions_attr =
+          attributes.lookup_or_add_for_write_span<float3>("position", AttrDomain::Point);
+      bke::SpanAttributeWriter<float3> velocities_attr =
+          attributes.lookup_or_add_for_write_span<float3>("velocity", AttrDomain::Point);
+      MutableSpan<float3> positions = positions_attr.span;
+      MutableSpan<float3> velocities = velocities_attr.span;
+      const VArraySpan<float3> external_forces = *attributes.lookup_or_default<float3>(
+          "external_force", AttrDomain::Point, float3(0, 0, 0));
+      const VArraySpan<float> masses = *attributes.lookup_or_default<float>(
+          "mass", AttrDomain::Point, 1);
+      threading::parallel_for(positions.index_range(), 256, [&](const IndexRange range) {
+        for (const int i : range) {
+          const float3 external_force = external_forces[i];
+          const float mass = masses[i];
+          const float3 acceleration = math::safe_divide(external_force, mass);
+          velocities[i] += acceleration * delta_time_;
+          positions[i] += velocities[i] * delta_time_;
+        }
+      });
+      velocities_attr.finish();
+      positions_attr.finish();
+    }
+  }
+
+  void write_back_geometries_to_world()
+  {
+    for (const int geo_bundle_i : geometries_.paths.index_range()) {
+      const StringRef geometry_path = geometries_.paths[geo_bundle_i];
+      GeometrySet &geometry = geometries_.geometry_sets[geo_bundle_i];
+      world_.add_path_override(geometry_path + "/geometry", std::move(geometry));
+    }
+  }
+};
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
@@ -213,45 +286,10 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
   const float delta_time = std::max(0.0f, params.get_input<float>("Delta Time"));
   Bundle &world = world_ptr.ensure_mutable_inplace();
-
   ResourceScope scope;
-  FieldEvaluators field_evaluators(scope);
 
-  Geometries geometries = gather_geometries(world);
-
-  const Vector<std::string> pin_positions_bundle_paths = gather_bundle_paths_by_type(
-      world, PinnedPositionXPBDConstraintBundle::name);
-
-  for (const int data_key_i : geometries.data_keys.index_range()) {
-    bke::MutableAttributeAccessor &attributes = geometries.attribute_accessors[data_key_i];
-    bke::SpanAttributeWriter<float3> positions_attr =
-        attributes.lookup_or_add_for_write_span<float3>("position", AttrDomain::Point);
-    bke::SpanAttributeWriter<float3> velocities_attr =
-        attributes.lookup_or_add_for_write_span<float3>("velocity", AttrDomain::Point);
-    MutableSpan<float3> positions = positions_attr.span;
-    MutableSpan<float3> velocities = velocities_attr.span;
-    const VArraySpan<float3> external_forces = *attributes.lookup_or_default<float3>(
-        "external_force", AttrDomain::Point, float3(0, 0, 0));
-    const VArraySpan<float> masses = *attributes.lookup_or_default<float>(
-        "mass", AttrDomain::Point, 1);
-    threading::parallel_for(positions.index_range(), 256, [&](const IndexRange range) {
-      for (const int i : range) {
-        const float3 external_force = external_forces[i];
-        const float mass = masses[i];
-        const float3 acceleration = math::safe_divide(external_force, mass);
-        velocities[i] += acceleration * delta_time;
-        positions[i] += velocities[i] * delta_time;
-      }
-    });
-    velocities_attr.finish();
-    positions_attr.finish();
-  }
-
-  for (const int geo_bundle_i : geometries.paths.index_range()) {
-    const StringRef geometry_path = geometries.paths[geo_bundle_i];
-    GeometrySet &geometry = geometries.geometry_sets[geo_bundle_i];
-    world.add_path_override(geometry_path + "/geometry", std::move(geometry));
-  }
+  XpbdSolverStep step(scope, world, delta_time);
+  step.do_step();
 
   params.set_output("World", std::move(world_ptr));
 }
