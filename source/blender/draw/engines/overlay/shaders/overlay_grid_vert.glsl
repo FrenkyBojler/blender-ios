673d45df3186d6623e65a45e488625ad7b54ff4c
Mark van de Ruit <mark@blender.org>
2026-02-11 16:50:50 +0100

    Fix #154216: later grid fade near geometry

--- source/blender/draw/engines/overlay/shaders/overlay_grid_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_grid_vert.glsl
@@ -200,11 +200,14 @@ void main()
 
   /* Adjust z-component. */
   if (drw_view_is_perspective()) {
-    /* To minimize z-fighting, the grid is drawn N times with progressive alpha and z-bias,
-     * making it fade through geometry. The smaller the range below, the more it pops in. */
+    /* To minimize z-fighting, the grid is drawn N times with progressive z-bias, making it fade
+     * through geometry. The slight negative z-offset makes the grid "ghost" over geometry on the
+     * same plane, while the offset range determines how much fade-in there is. */
+    constexpr float z_min_offset = -0.00008f;
+    constexpr float z_max_offset = 0.00032f;
     float z_factor = float(grid_iter * OVERLAY_GRID_STEPS_DRAW + line.level) /
                      float(OVERLAY_GRID_ITER_LEN * OVERLAY_GRID_STEPS_DRAW);
-    gl_Position.z += mix(5e-4f, 1e-4f, z_factor);
+    gl_Position.z += mix(z_max_offset, z_min_offset, z_factor);
   }
   else if (flag_test(grid_flag, GRID_BEHIND_GEOMETRY)) {
     /* Set z to far plane, placing the grid behind all geometry. */
