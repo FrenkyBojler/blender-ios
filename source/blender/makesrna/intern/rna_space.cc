blender/blender

commit 3abd294f507cdf99d7f99d535792325117d1f72b

Fix: VSE thumbnails and source media caches are invalidated too often
The bug report was about thumbnails getting refreshed whenever adding
any keyframe on any strip, but there were more call sites that
were invalidating all the VSE caches, when what they needed to
invalidate was more like "rendered results" (final frame, intra frame):

- Around animation rendering,
- Scene changes (due to keyframes etc.) -- this one for
  #148121 was fixed in a smaller way for 5.0
- After proxy building,
- Changing camera resolution / aspect
- Changing proxy storage location

Pull Request: https://projects.blender.org/blender/blender/pulls/148165

source/blender/makesrna/intern/rna_space.cc
@@ -2423,7 +2423,7 @@ static void rna_SpaceConsole_rect_update(Main * /*bmain*/, Scene * /*scene*/, Po
 
 static void rna_SequenceEditor_update_cache(Main * /*bmain*/, Scene *scene, PointerRNA * /*ptr*/)
 {
-  blender::seq::cache_cleanup(scene);
+  blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::FinalAndIntra);
 }
 
 static void seq_build_proxy(bContext *C, PointerRNA *ptr)

source/blender/editors/render/render_internal.cc
@@ -386,8 +386,8 @@ static wmOperatorStatus screen_render_exec(bContext *C, wmOperator *op)
   /* cleanup sequencer caches before starting user triggered render.
    * otherwise, invalidated cache entries can make their way into
    * the output rendering. We can't put that into RE_RenderFrame,
-   * since sequence rendering can call that recursively... (peter) */
-  blender::seq::cache_cleanup(scene);
+   * since sequence rendering can call that recursively... */
+  blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::FinalAndIntra);
 
   RE_SetReports(re, op->reports);
 
@@ -1091,7 +1091,7 @@ static wmOperatorStatus screen_render_invoke(bContext *C, wmOperator *op, const 
   ED_editors_flush_edits_ex(bmain, true, false);
 
   /* Cleanup VSE cache, since it is not guaranteed that stored images are invalid. */
-  blender::seq::cache_cleanup(scene);
+  blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::FinalAndIntra);
 
   /* store spare
    * get view3d layer, local layer, make this nice API call to render

source/blender/editors/render/render_update.cc
@@ -318,15 +318,15 @@ static void update_sequencer(const DEGEditorUpdateContext *update_ctx, Main *bma
     blender::seq::relations_invalidate_scene_strips(bmain, changed_scene);
   }
 
-  /* Invalidate VSE cache in `changed_scene`, because strip animation may have been updated. */
+  /* Invalidate rendered VSE caches in `changed_scene`, because strip animation may have been
+   * updated. */
   if (GS(id->name) == ID_AC) {
     Editing *ed = blender::seq::editing_get(changed_scene);
     if (ed != nullptr && blender::seq::animation_keyframes_exist(changed_scene) &&
         &changed_scene->adt->action->id == id)
     {
       blender::seq::prefetch_stop(changed_scene);
-      blender::seq::cache_cleanup_intra(changed_scene);
-      blender::seq::cache_cleanup_final(changed_scene);
+      blender::seq::cache_cleanup(changed_scene, blender::seq::CacheCleanup::FinalAndIntra);
     }
   }
 

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1506,7 +1506,7 @@ static wmOperatorStatus sequencer_refresh_all_exec(bContext *C, wmOperator * /*o
 
   seq::relations_free_imbuf(scene, &ed->seqbase, false);
   blender::seq::media_presence_free(scene);
-  seq::cache_cleanup(scene);
+  seq::cache_cleanup(scene, seq::CacheCleanup::All);
 
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
 

source/blender/editors/space_sequencer/sequencer_proxy.cc
@@ -127,6 +127,7 @@ static wmOperatorStatus sequencer_rebuild_proxy_exec(bContext *C, wmOperator * /
       seq::relations_free_imbuf(scene, &ed->seqbase, false);
     }
   }
+  seq::cache_cleanup(scene, seq::CacheCleanup::FinalAndIntra);
 
   return OPERATOR_FINISHED;
 }

source/blender/makesrna/intern/rna_color.cc
@@ -811,7 +811,7 @@ static void rna_ColorManagedColorspaceSettings_reload_update(Main *bmain,
 
       if (&scene->sequencer_colorspace_settings == colorspace_settings) {
         /* Scene colorspace was changed. */
-        blender::seq::cache_cleanup(scene);
+        blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::All);
       }
       else {
         /* Strip colorspace was likely changed. */

source/blender/makesrna/intern/rna_scene.cc
@@ -2544,7 +2544,7 @@ static void rna_SceneCamera_update(Main * /*bmain*/, Scene * /*scene*/, PointerR
   Scene *scene = (Scene *)ptr->owner_id;
   Object *camera = scene->camera;
 
-  blender::seq::cache_cleanup(scene);
+  blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::FinalAndIntra);
 
   if (camera && (camera->type == OB_CAMERA)) {
     DEG_id_tag_update(&camera->id, ID_RECALC_GEOMETRY);
@@ -2553,7 +2553,7 @@ static void rna_SceneCamera_update(Main * /*bmain*/, Scene * /*scene*/, PointerR
 
 static void rna_SceneSequencer_update(Main * /*bmain*/, Scene * /*scene*/, PointerRNA *ptr)
 {
-  blender::seq::cache_cleanup((Scene *)ptr->owner_id);
+  blender::seq::cache_cleanup((Scene *)ptr->owner_id, blender::seq::CacheCleanup::FinalAndIntra);
 }
 
 static std::optional<std::string> rna_ToolSettings_path(const PointerRNA * /*ptr*/)

source/blender/makesrna/intern/rna_sequencer.cc
@@ -339,7 +339,7 @@ static void rna_SequenceEditor_update_cache(Main * /*bmain*/, Scene *scene, Poin
   Editing *ed = scene->ed;
 
   blender::seq::relations_free_imbuf(scene, &ed->seqbase, false);
-  blender::seq::cache_cleanup(scene);
+  blender::seq::cache_cleanup(scene, blender::seq::CacheCleanup::FinalAndIntra);
 }
 
 static void rna_SequenceEditor_cache_settings_changed(Main * /*bmain*/,

FrenkyBojler/blender-ios

commit e966f190bd43909f8c8a19e26a30331507035f67

Fix #152342: Incorrect image size in preview when changing render size
Caused by 3abd294f50 which tried to optimize cache cleanup. Also the
size update function used bContext and always used active scene, so the
cleanup may happen in incorrect scene.

source/blender/makesrna/intern/rna_space.cc
@@ -2599,8 +2599,14 @@ static void seq_build_proxy(bContext *C, PointerRNA *ptr)
 
 static void rna_SequenceEditor_render_size_update(bContext *C, PointerRNA *ptr)
 {
+  const bool is_sequencer = CTX_wm_space_seq(C) != nullptr;
+  Scene *scene = is_sequencer ? CTX_data_sequencer_scene(C) : CTX_data_scene(C);
+  if (scene == nullptr) {
+    return;
+  }
+
   seq_build_proxy(C, ptr);
-  rna_SequenceEditor_update_cache(CTX_data_main(C), CTX_data_sequencer_scene(C), ptr);
+  seq::cache_cleanup(scene, seq::CacheCleanup::All);
 }
 
 static bool rna_SequenceEditor_clamp_view_get(PointerRNA *ptr)