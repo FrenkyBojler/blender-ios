bde49072609c045734ed1140f60bc10625e520a9
Habib Gahbiche <habib@blender.org>
2026-02-13 10:43:12 +0100

    Cleanup

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -74,7 +74,6 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       scene.r.mode |= R_SAVE_OUTPUT;
     }
   }
-
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
