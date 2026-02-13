blender/blender

commit ebd9e6d2720e40b0a1e0bd02a55bdab2560a179d

Fix #71397: Cannot "batch" edit a Lattice resolution u, v, w at once
In order to set the resolution on a Lattice (`points_u`, `points_v`,
`points_w`) the process goes through
- setting RNA (`rna_Lattice_points_u_set`, `rna_Lattice_points_v_set`,
`rna_Lattice_points_w_set`) which will only set "temporary" values for
`opntsu`, `opntsv` and `opntsw`
- going through the RNA update `rna_Lattice_update_size` which will take
those `opntsX` if available call `BKE_lattice_resize` with it
- only at the end of `BKE_lattice_resize` the actual `points_u`,
`points_v`, `points_w` are set for the resized lattice

Now when we use the multi-but API (check `BUT_DRAG_MULTI`) to enter
values for multiple buttons simultaniously, we can run into the
situation where code actually updates RNA individual values (so
`rna_Lattice_points_u_set`, `rna_Lattice_points_v_set`,
`rna_Lattice_points_w_set` via `button_string_set` in
`ui_apply_but_NUM`) correctly, but then goes on assuming
`button_value_get` would get back that value. That is not the case
though, `button_value_get` picks up `points_u`, `points_v`, `points_w`
(not `opntsu`, `opntsv` and `opntsw` as set by
`rna_Lattice_points_X_set`) with the old values because
`BKE_lattice_resize` hasnt run yet.

To resolve, also provide "getter" RNA int functions -- which now can
also pick up the "temporary" values `opntsu`, `opntsv` and `opntsw`.

Pull Request: https://projects.blender.org/blender/blender/pulls/152641

source/blender/makesrna/intern/rna_lattice.cc
@@ -184,6 +184,12 @@ static int rna_Lattice_size_editable(const PointerRNA *ptr, const char ** /*r_in
   return (lt->key == nullptr) ? int(PROP_EDITABLE) : 0;
 }
 
+static int rna_Lattice_points_u_get(PointerRNA *ptr)
+{
+  Lattice *lt = static_cast<Lattice *>(ptr->data);
+  return (lt->opntsu > 0) ? lt->opntsu : lt->pntsu;
+}
+
 static void rna_Lattice_points_u_set(PointerRNA *ptr, int value)
 {
   Lattice *lt = static_cast<Lattice *>(ptr->data);
@@ -191,11 +197,23 @@ static void rna_Lattice_points_u_set(PointerRNA *ptr, int value)
   lt->opntsu = std::clamp(value, 1, 64);
 }
 
+static int rna_Lattice_points_v_get(PointerRNA *ptr)
+{
+  Lattice *lt = static_cast<Lattice *>(ptr->data);
+  return (lt->opntsv > 0) ? lt->opntsv : lt->pntsv;
+}
+
 static void rna_Lattice_points_v_set(PointerRNA *ptr, int value)
 {
   Lattice *lt = static_cast<Lattice *>(ptr->data);
 
   lt->opntsv = std::clamp(value, 1, 64);
+}
+
+static int rna_Lattice_points_w_get(PointerRNA *ptr)
+{
+  Lattice *lt = static_cast<Lattice *>(ptr->data);
+  return (lt->opntsw > 0) ? lt->opntsw : lt->pntsw;
 }
 
 static void rna_Lattice_points_w_set(PointerRNA *ptr, int value)
@@ -318,7 +336,8 @@ static void rna_def_lattice(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "points_u", PROP_INT, PROP_NONE);
   RNA_def_property_int_sdna(prop, nullptr, "pntsu");
-  RNA_def_property_int_funcs(prop, nullptr, "rna_Lattice_points_u_set", nullptr);
+  RNA_def_property_int_funcs(
+      prop, "rna_Lattice_points_u_get", "rna_Lattice_points_u_set", nullptr);
   RNA_def_property_range(prop, 1, 64);
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
   RNA_def_property_ui_text(
@@ -328,7 +347,8 @@ static void rna_def_lattice(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "points_v", PROP_INT, PROP_NONE);
   RNA_def_property_int_sdna(prop, nullptr, "pntsv");
-  RNA_def_property_int_funcs(prop, nullptr, "rna_Lattice_points_v_set", nullptr);
+  RNA_def_property_int_funcs(
+      prop, "rna_Lattice_points_v_get", "rna_Lattice_points_v_set", nullptr);
   RNA_def_property_range(prop, 1, 64);
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
   RNA_def_property_ui_text(
@@ -338,7 +358,8 @@ static void rna_def_lattice(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "points_w", PROP_INT, PROP_NONE);
   RNA_def_property_int_sdna(prop, nullptr, "pntsw");
-  RNA_def_property_int_funcs(prop, nullptr, "rna_Lattice_points_w_set", nullptr);
+  RNA_def_property_int_funcs(
+      prop, "rna_Lattice_points_w_get", "rna_Lattice_points_w_set", nullptr);
   RNA_def_property_range(prop, 1, 64);
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
   RNA_def_property_ui_text(

FrenkyBojler/blender-ios

commit 39086a785935519c74f2f8221cd66286e6b1f857

Fix #154381: Lattice resolution does not update RNA on undo in edit-mode
Caused by ebd9e6d272

The temporary resolution values `opntsu`, `opntsv` and `opntsw` were
never reset to 0 after being used. This caused the RNA getters to
continue returning these cached values even if the lattice was
subsequently resized by other means (which can happen in editmode undo).

---

Initially spotted by LLM review, just had to verify whare this could
actually go wrong and create an actual bug report for this :)

source/blender/makesrna/intern/rna_lattice.cc
@@ -149,6 +149,8 @@ static void rna_Lattice_update_size(Main *bmain, Scene *scene, PointerRNA *ptr)
     }
   }
 
+  lt->opntsu = lt->opntsv = lt->opntsw = 0;
+
   rna_Lattice_update_data(bmain, scene, ptr);
 }
 