c41bf90c761335ddecbf6e4df104ed50744e7353
Dalai Felinto <dalai@blender.org>
2026-02-16 11:51:52 +0100

    Small refactor

--- source/blender/editors/space_sequencer/sequencer_add_menu_scene.cc
+++ source/blender/editors/space_sequencer/sequencer_add_menu_scene.cc
@@ -188,13 +188,13 @@ static void sequencer_add_scene_draw(const bContext *C, Menu *menu)
               wm::OpCallContext::InvokeDefault,
               UI_ITEM_NONE);
   }
+  else if (scenes_len == 1) {
+    layout.label(IFACE_("Scene Strip"), ICON_SCENE_DATA);
+    layout.label(IFACE_("No other scenes."), ICON_NONE);
+  }
   else {
     layout.label(IFACE_("Scene Strip"), ICON_SCENE_DATA);
     const Scene *active_scene = CTX_data_sequencer_scene(C);
-
-    if (scenes_len == 1) {
-      layout.label(IFACE_("No other scenes."), ICON_NONE);
-    }
 
     for (const auto [i, scene] : bmain->scenes.enumerate()) {
       if (&scene == active_scene) {
