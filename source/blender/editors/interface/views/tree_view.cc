90e4a79f9bfc73c75892a036c1f40bae94e28854
Pratik Borhade <pratikborhade302@gmail.com>
2026-02-16 17:02:20 +0530

    Scroll active to the center

--- source/blender/editors/interface/views/tree_view.cc
+++ source/blender/editors/interface/views/tree_view.cc
@@ -438,7 +438,7 @@ void AbstractTreeView::scroll_active_into_view(bool scroll)
     foreach_item(
         [&, this](AbstractTreeViewItem &item) {
           if (item.is_active_) {
-            *scroll_value_ = std::max(0, index - *visible_row_count + 1);
+            *scroll_value_ = std::max(0, index - (*visible_row_count)/2 + 1);
             return;
           }
           index++;
