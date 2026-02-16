c6d500c154ad51f98a4b414072f424482a64fb78
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-02-16 12:56:56 +0000

    Adds s

--- source/blender/bmesh/intern/bmesh_opdefines.cc
+++ source/blender/bmesh/intern/bmesh_opdefines.cc
@@ -519,7 +519,7 @@ static BMOpDefine bmo_circularize_def = {
         {"angle", BMO_OP_SLOT_FLT},
         /* Method to fit the circle. */
         {"fit_method", BMO_OP_SLOT_INT},
-        /* Flatten the circle, when disabled the circle will be projected on the mesh. */
+        /* Flattens the circle, when disabled the circle will be projected on the mesh. */
         {"flatten", BMO_OP_SLOT_BOOL},
         /* Distributes vertices at constant distances, otherwise preserves original spacing . */
         {"regular", BMO_OP_SLOT_BOOL},
