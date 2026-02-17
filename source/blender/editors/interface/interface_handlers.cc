26ba85acb8ab8fdbb379936f9dd5fdae5fe1c272
Harley Acheson <harley.acheson@gmail.com>
2026-02-17 08:19:09 -0800

    Allow for pulldown submenus.

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -8868,6 +8868,9 @@ static void button_activate_state(bContext *C, Button *but, HandleButtonState st
         if (but->block->auto_open == true) { /* test for toolbox */
           time = 1;
         }
+        else if (but->block->flag & BLOCK_LOOP && but->type == ButtonType::Pulldown) {
+          time = 5 * U.menuthreshold2;
+        }
         else if (U.uiflag & USER_MENUOPENAUTO) {
           time = 5 * U.menuthreshold1;
         }
