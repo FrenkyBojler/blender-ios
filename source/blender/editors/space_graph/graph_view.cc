e317ee4a94ad22958f80910d8ce79b8dd34ead13
Pratik Borhade <pratikborhade302@gmail.com>
2026-02-17 14:21:36 +0530

    Code comment

--- source/blender/editors/space_graph/graph_view.cc
+++ source/blender/editors/space_graph/graph_view.cc
@@ -252,6 +252,8 @@ static wmOperatorStatus graphkeys_viewall(bContext *C,
   }
 
   if (ac.regiontype != RGN_TYPE_WINDOW) {
+    /* It is possible that operator is invoked from other regions like channel.
+    Main region is required here forgi smooth view function. */
     for (ARegion &region : ac.area->regionbase) {
       if (region.regiontype == RGN_TYPE_WINDOW) {
         ac.region = &region;
@@ -591,6 +593,8 @@ static wmOperatorStatus graphview_curves_isolate_exec(bContext *C, wmOperator * 
   }
 
   if (ac.regiontype != RGN_TYPE_WINDOW) {
+    /* It is possible that operator is invoked from other regions like channel.
+    Main region is required here for current bounds and smooth view function. */
     for (ARegion &region : ac.area->regionbase) {
       if (region.regiontype == RGN_TYPE_WINDOW) {
         ac.region = &region;
