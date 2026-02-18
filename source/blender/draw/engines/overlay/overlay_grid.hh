56c77df0f3401db6a680a26eda47b858a9413f31
Mark van de Ruit <mark@blender.org>
2026-02-18 14:18:23 +0100

    Only check `SI_OVERLAY_SHOW_GRID_BACKGROUND`

--- source/blender/draw/engines/overlay/overlay_grid.hh
+++ source/blender/draw/engines/overlay/overlay_grid.hh
@@ -146,9 +146,7 @@ class Grid : Overlay {
     SpaceImage *sima = (SpaceImage *)state.space_data;
 
     /* Grid is currently visible in UV and Image editors, if enabled. */
-    const bool show_grid =
-        ELEM(sima->mode, SI_MODE_UV, SI_MODE_VIEW, SI_MODE_PAINT, SI_MODE_MASK) &&
-        (sima->overlay.flag & SI_OVERLAY_SHOW_GRID_BACKGROUND);
+    const bool show_grid = bool(sima->overlay.flag & SI_OVERLAY_SHOW_GRID_BACKGROUND);
     if (!show_grid) {
       return false;
     }
