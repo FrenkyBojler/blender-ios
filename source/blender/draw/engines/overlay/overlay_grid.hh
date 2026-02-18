e733b50e5083344bf25f775cb606e1b01b8fbc08
Mark van de Ruit <mark@blender.org>
2026-02-18 10:13:25 +0100

    Add `SI_MODE_PAINT`, `SI_MODE_MASK` flag checks

--- source/blender/draw/engines/overlay/overlay_grid.hh
+++ source/blender/draw/engines/overlay/overlay_grid.hh
@@ -146,8 +146,9 @@ class Grid : Overlay {
     SpaceImage *sima = (SpaceImage *)state.space_data;
 
     /* Grid is currently visible in UV and Image editors, if enabled. */
-    const bool show_grid = ELEM(sima->mode, SI_MODE_UV, SI_MODE_VIEW) &&
-                           (sima->overlay.flag & SI_OVERLAY_SHOW_GRID_BACKGROUND);
+    const bool show_grid =
+        ELEM(sima->mode, SI_MODE_UV, SI_MODE_VIEW, SI_MODE_PAINT, SI_MODE_MASK) &&
+        (sima->overlay.flag & SI_OVERLAY_SHOW_GRID_BACKGROUND);
     if (!show_grid) {
       return false;
     }
