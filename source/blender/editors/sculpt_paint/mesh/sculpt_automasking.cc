1dfcc5dec7041d603d81006ebddbb997de517631
Sean Kim <SeanCTKim@protonmail.com>
2026-02-17 17:15:38 -0800

    Fix error

--- source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
+++ source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -691,7 +691,7 @@ void calc_vert_factors(const Depsgraph &depsgraph,
     }
 
     if (automasking.settings.flags & BRUSH_AUTOMASKING_FACE_SETS) {
-      if (automasking.settings.initial_face_set == face_set_none_id &&
+      if (automasking.settings.initial_face_set != face_set_none_id &&
           !face_set::vert_has_face_set(
               vert_to_face_map, face_sets, vert, automasking.settings.initial_face_set))
       {
