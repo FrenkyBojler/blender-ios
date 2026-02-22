a22eb6e3183eff0cb71bcc9142230bb28e9b9ea3
Jacques Lucke <jacques@blender.org>
2026-02-22 12:59:56 +0100

    generalize attribute transfer

--- source/blender/nodes/geometry/nodes/node_geo_transfer_attributes.cc
+++ source/blender/nodes/geometry/nodes/node_geo_transfer_attributes.cc
@@ -23,7 +23,13 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Geometry>("Target");
   b.add_output<decl::Geometry>("Target").align_with_previous();
   b.add_output<decl::Bool>("Success");
+  b.add_input<decl::Int>("Target ID")
+      .implicit_field(NODE_DEFAULT_INPUT_ID_INDEX_FIELD)
+      .structure_type(StructureType::Field);
   b.add_input<decl::Geometry>("Source");
+  b.add_input<decl::Int>("Source ID")
+      .implicit_field(NODE_DEFAULT_INPUT_ID_INDEX_FIELD)
+      .structure_type(StructureType::Field);
 
   b.add_input<decl::String>("Names")
       .optional_label()
@@ -33,137 +39,239 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.get_anonymous_attribute_relations().propagate_relations.append({0, 0});
 }
 
-static bool name_matches_pattern(const StringRef name, const VectorSet<std::string> &patterns)
-{
-  if (patterns.contains_as(name)) {
-    return true;
+class AttributeTransferer {
+ private:
+  ResourceScope scope_;
+  IndexMaskMemory mask_memory_;
+  GeometrySet &dst_geo_;
+  GeometrySet &src_geo_;
+  const VectorSet<std::string> &attribute_patterns_;
+  const Field<int> &dst_id_field_;
+  const Field<int> &src_id_field_;
+  bool any_transferred_ = false;
+
+ public:
+  AttributeTransferer(GeometrySet &dst_geo,
+                      GeometrySet &src_geo,
+                      const VectorSet<std::string> &attribute_patterns,
+                      const Field<int> &dst_id_field,
+                      const Field<int> &src_id_field)
+      : dst_geo_(dst_geo),
+        src_geo_(src_geo),
+        attribute_patterns_(attribute_patterns),
+        dst_id_field_(dst_id_field),
+        src_id_field_(src_id_field)
+  {
   }
-  for (const StringRef pattern : patterns) {
-    // TODO: Support wildcards similar to Remove Attribute node.
-    if (pattern.endswith("*")) {
-      if (name.startswith(pattern.drop_known_suffix("*"))) {
-        return true;
+
+  bool do_transfer()
+  {
+    for (const GeometryComponent::Type type : {bke::GeometryComponent::Type::Mesh,
+                                               bke::GeometryComponent::Type::PointCloud,
+                                               bke::GeometryComponent::Type::Curve,
+                                               bke::GeometryComponent::Type::Instance,
+                                               bke::GeometryComponent::Type::GreasePencil})
+    {
+      if (!dst_geo_.has(type)) {
+        continue;
+      }
+      if (!src_geo_.has(type)) {
+        continue;
+      }
+      const GeometryComponent &src_component = *src_geo_.get_component(type);
+      GeometryComponent &dst_component = dst_geo_.get_component_for_write(type);
+      const bke::AttributeAccessor src_attributes = *src_component.attributes();
+      bke::MutableAttributeAccessor dst_attributes = *dst_component.attributes_for_write();
+      this->transfer_attributes(
+          src_attributes,
+          dst_attributes,
+          [&](const AttrDomain domain) -> fn::FieldContext & {
+            return scope_.construct<bke::GeometryFieldContext>(src_component, domain);
+          },
+          [&](const AttrDomain domain) -> fn::FieldContext & {
+            return scope_.construct<bke::GeometryFieldContext>(dst_component, domain);
+          });
+    }
+
+    if (src_geo_.has_grease_pencil() && dst_geo_.has_grease_pencil()) {
+      const GreasePencil &src_grease_pencil = *src_geo_.get_grease_pencil();
+      GreasePencil &dst_grease_pencil = *dst_geo_.get_grease_pencil_for_write();
+      this->transfer_attributes_between_grease_pencil_layers(src_grease_pencil, dst_grease_pencil);
+    }
+
+    return any_transferred_;
+  }
+
+  void transfer_attributes_between_grease_pencil_layers(const GreasePencil &src_grease_pencil,
+                                                        GreasePencil &dst_grease_pencil)
+  {
+    using namespace blender::bke::greasepencil;
+    const int src_layer_num = src_grease_pencil.layers().size();
+    const int dst_layer_num = dst_grease_pencil.layers().size();
+    /* Could also support custom mapping of src to dst layers. */
+    const int common_layer_num = std::min(src_layer_num, dst_layer_num);
+    for (const int layer_i : IndexRange(common_layer_num)) {
+      const Layer &src_layer = src_grease_pencil.layer(layer_i);
+      const Drawing *src_drawing = src_grease_pencil.get_eval_drawing(src_layer);
+      if (!src_drawing) {
+        continue;
+      }
+      Layer &dst_layer = dst_grease_pencil.layer(layer_i);
+      Drawing *dst_drawing = dst_grease_pencil.get_eval_drawing(dst_layer);
+      if (!dst_drawing) {
+        continue;
+      }
+      const bke::CurvesGeometry &src_curves = src_drawing->strokes();
+      bke::CurvesGeometry &dst_curves = dst_drawing->strokes_for_write();
+      const bke::AttributeAccessor src_attributes = src_curves.attributes();
+      bke::MutableAttributeAccessor dst_attributes = dst_curves.attributes_for_write();
+      this->transfer_attributes(
+          src_attributes,
+          dst_attributes,
+          [&](const AttrDomain domain) -> fn::FieldContext & {
+            return scope_.construct<bke::GreasePencilLayerFieldContext>(
+                src_grease_pencil, domain, layer_i);
+          },
+          [&](const AttrDomain domain) -> fn::FieldContext & {
+            return scope_.construct<bke::GreasePencilLayerFieldContext>(
+                dst_grease_pencil, domain, layer_i);
+          });
+    }
+  }
+
+ private:
+  void transfer_attributes(
+      const bke::AttributeAccessor &src_attributes,
+      bke::MutableAttributeAccessor &dst_attributes,
+      FunctionRef<fn::FieldContext &(const AttrDomain domain)> create_src_field_context,
+      FunctionRef<fn::FieldContext &(const AttrDomain domain)> create_dst_field_context)
+  {
+    struct AttrItem {
+      std::string name;
+      AttrDomain domain;
+      bke::AttrType type;
+    };
+    Vector<AttrItem> items;
+    src_attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
+      if (this->name_matches_any_pattern(iter.name)) {
+        items.append({iter.name, iter.domain, iter.data_type});
+      }
+    });
+
+    struct IDs {
+      bool transfer_by_index = false;
+      Array<int> src_by_dst_index;
+      IndexMask dst_mask;
+    };
+    Array<std::optional<IDs>> ids_by_domain(ATTR_DOMAIN_NUM);
+    for (const AttrItem &item : items) {
+      std::optional<IDs> &ids = ids_by_domain[int(item.domain)];
+      if (ids.has_value()) {
+        continue;
+      }
+      ids.emplace();
+
+      if (dynamic_cast<const fn::IndexFieldInput *>(&src_id_field_.node()) &&
+          dynamic_cast<const fn::IndexFieldInput *>(&dst_id_field_.node()))
+      {
+        ids->transfer_by_index = true;
+        continue;
+      }
+
+      const int src_size = src_attributes.domain_size(item.domain);
+      const int dst_size = dst_attributes.domain_size(item.domain);
+
+      fn::FieldContext &src_field_context = create_src_field_context(item.domain);
+      fn::FieldEvaluator &src_evaluator = scope_.construct<fn::FieldEvaluator>(src_field_context,
+                                                                               src_size);
+      src_evaluator.add(src_id_field_);
+      src_evaluator.evaluate();
+      const VArraySpan<int> src_ids = src_evaluator.get_evaluated<int>(0);
+
+      fn::FieldContext &dst_field_context = create_dst_field_context(item.domain);
+      fn::FieldEvaluator &dst_evaluator = scope_.construct<fn::FieldEvaluator>(dst_field_context,
+                                                                               dst_size);
+      dst_evaluator.add(dst_id_field_);
+      dst_evaluator.evaluate();
+      const VArraySpan<int> dst_ids = dst_evaluator.get_evaluated<int>(0);
+
+      Map<int, int> src_index_by_id;
+      for (const int i : IndexRange(src_size)) {
+        const int id = src_ids[i];
+        src_index_by_id.add(id, i);
+      }
+      ids->src_by_dst_index.reinitialize(dst_size);
+      Array<bool> dst_mask_bools(dst_size);
+      threading::parallel_for(IndexRange(dst_size), 2048, [&](const IndexRange range) {
+        for (const int dst_i : range) {
+          const int dst_id = dst_ids[dst_i];
+          const int src_i = src_index_by_id.lookup_default(dst_id, -1);
+          ids->src_by_dst_index[dst_i] = src_i;
+          dst_mask_bools[dst_i] = src_i != -1;
+        }
+      });
+      ids->dst_mask = IndexMask::from_bools(dst_mask_bools, mask_memory_);
+    }
+
+    for (const AttrItem &item : items) {
+      const GVArraySpan src_attr = *src_attributes.lookup(item.name);
+      bke::GSpanAttributeWriter dst_attr = dst_attributes.lookup_or_add_for_write_span(
+          item.name, item.domain, item.type);
+      if (!dst_attr) {
+        continue;
+      }
+      const int src_size = src_attr.size();
+      const int dst_size = dst_attr.span.size();
+
+      const IDs &ids = *ids_by_domain[int(item.domain)];
+      const CPPType &cpp_type = src_attr.type();
+      if (ids.transfer_by_index) {
+        const int copy_num = std::min(src_size, dst_size);
+        const IndexRange slice(copy_num);
+        cpp_type.copy_assign_n(src_attr.data(), dst_attr.span.data(), copy_num);
+      }
+      else {
+        bke::attribute_math::to_static_type(cpp_type, [&]<typename T>() {
+          const Span<T> src_span = src_attr.typed<T>();
+          const MutableSpan<T> dst_span = dst_attr.span.typed<T>();
+          /* Should probably use some array_utils::gather overload, but the right one doesn't seem
+           * to exist yet. */
+          ids.dst_mask.foreach_index([&](const int dst_i) {
+            const int src_i = ids.src_by_dst_index[dst_i];
+            dst_span[dst_i] = src_span[src_i];
+          });
+        });
+      }
+      dst_attr.finish();
+      any_transferred_ = true;
+    }
+  }
+
+  bool name_matches_any_pattern(const StringRef name) const
+  {
+    if (attribute_patterns_.contains_as(name)) {
+      return true;
+    }
+    for (const StringRef pattern : attribute_patterns_) {
+      // TODO: Support wildcards similar to Remove Attribute node.
+      if (pattern.endswith("*")) {
+        if (name.startswith(pattern.drop_known_suffix("*"))) {
+          return true;
+        }
       }
     }
-  }
-  return false;
-}
-
-static bool transfer_attributes(const bke::AttributeAccessor &src_attributes,
-                                const VectorSet<std::string> &attribute_patterns,
-                                bke::MutableAttributeAccessor &dst_attributes)
-{
-  Vector<std::string> attribute_names;
-  src_attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
-    if (name_matches_pattern(iter.name, attribute_patterns)) {
-      attribute_names.append(iter.name);
-    }
-  });
-
-  bool propagated_attribute = false;
-  for (const StringRef attribute_name : attribute_names) {
-    const bke::AttributeMetaData meta_data = *src_attributes.lookup_meta_data(attribute_name);
-    const AttrDomain domain = meta_data.domain;
-    const int src_domain_size = src_attributes.domain_size(domain);
-    const int dst_domain_size = dst_attributes.domain_size(domain);
-    if (src_domain_size != dst_domain_size) {
-      continue;
-    }
-    const bke::GAttributeReader src_attribute = src_attributes.lookup(attribute_name);
-    if (!src_attribute) {
-      continue;
-    }
-    bke::GSpanAttributeWriter dst_attribute = dst_attributes.lookup_or_add_for_write_only_span(
-        attribute_name, domain, meta_data.data_type);
-    if (!dst_attribute) {
-      continue;
-    }
-    array_utils::copy(src_attribute.varray, dst_attribute.span);
-    dst_attribute.finish();
-    propagated_attribute = true;
-  }
-  return propagated_attribute;
-}
-
-static bool transfer_attributes_between_grease_pencil_layers(
-    const GreasePencil &src_grease_pencil,
-    GreasePencil &dst_grease_pencil,
-    const VectorSet<std::string> &attribute_patterns)
-{
-  using namespace blender::bke::greasepencil;
-  const int src_layer_num = src_grease_pencil.layers().size();
-  const int dst_layer_num = dst_grease_pencil.layers().size();
-  if (src_layer_num != dst_layer_num) {
     return false;
   }
-  const int layer_num = src_layer_num;
-  for (const int layer_i : IndexRange(layer_num)) {
-    const Layer &src_layer = src_grease_pencil.layer(layer_i);
-    const Drawing *src_drawing = src_grease_pencil.get_eval_drawing(src_layer);
-    if (!src_drawing) {
-      continue;
-    }
-    Layer &dst_layer = dst_grease_pencil.layer(layer_i);
-    Drawing *dst_drawing = dst_grease_pencil.get_eval_drawing(dst_layer);
-    if (!dst_drawing) {
-      continue;
-    }
-    const bke::CurvesGeometry &src_curves = src_drawing->strokes();
-    bke::CurvesGeometry &dst_curves = dst_drawing->strokes_for_write();
-    const bke::AttributeAccessor src_attributes = src_curves.attributes();
-    bke::MutableAttributeAccessor dst_attributes = dst_curves.attributes_for_write();
-    transfer_attributes(src_attributes, attribute_patterns, dst_attributes);
-  }
-  return true;
-}
-
-static bool transfer_attributes_between_geometry_sets(
-    GeometrySet &dst_geo,
-    const GeometrySet &src_geo,
-    const VectorSet<std::string> &attribute_patterns)
-{
-  bool propagated_attribute = false;
-
-  for (const bke::GeometryComponent::Type type : {bke::GeometryComponent::Type::Mesh,
-                                                  bke::GeometryComponent::Type::PointCloud,
-                                                  bke::GeometryComponent::Type::Curve,
-                                                  bke::GeometryComponent::Type::Instance,
-                                                  bke::GeometryComponent::Type::GreasePencil})
-  {
-    if (!dst_geo.has(type)) {
-      continue;
-    }
-    if (!src_geo.has(type)) {
-      continue;
-    }
-    const bke::GeometryComponent &src_component = *src_geo.get_component(type);
-    GeometryComponent &dst_component = dst_geo.get_component_for_write(type);
-    const bke::AttributeAccessor src_attributes = *src_component.attributes();
-    bke::MutableAttributeAccessor dst_attributes = *dst_component.attributes_for_write();
-
-    if (transfer_attributes(src_attributes, attribute_patterns, dst_attributes)) {
-      propagated_attribute = true;
-    }
-  }
-  /* Transfer attributes between grease pencil layers. */
-  if (src_geo.has_grease_pencil() && dst_geo.has_grease_pencil()) {
-    const GreasePencil &src_grease_pencil = *src_geo.get_grease_pencil();
-    GreasePencil &dst_grease_pencil = *dst_geo.get_grease_pencil_for_write();
-    if (transfer_attributes_between_grease_pencil_layers(
-            src_grease_pencil, dst_grease_pencil, attribute_patterns))
-    {
-      propagated_attribute = true;
-    }
-  }
-
-  return propagated_attribute;
-}
+};
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
   GeometrySet target_geo = params.extract_input<GeometrySet>("Target");
   GeometrySet source_geo = params.extract_input<GeometrySet>("Source");
   const ListPtr attribute_patterns_list = params.extract_input<ListPtr>("Names");
+  const Field<int> target_id = params.extract_input<Field<int>>("Target ID");
+  const Field<int> source_id = params.extract_input<Field<int>>("Source ID");
+
   VectorSet<std::string> attribute_patterns;
   if (attribute_patterns_list) {
     const CPPType &cpp_type = attribute_patterns_list->cpp_type();
@@ -175,8 +283,9 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   bool success = false;
   if (!attribute_patterns.is_empty()) {
-    success = transfer_attributes_between_geometry_sets(
-        target_geo, source_geo, attribute_patterns);
+    AttributeTransferer transferer(
+        target_geo, source_geo, attribute_patterns, target_id, source_id);
+    success = transferer.do_transfer();
   }
   params.set_output("Target", std::move(target_geo));
   params.set_output("Success", success);
