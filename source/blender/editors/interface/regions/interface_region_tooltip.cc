7c71a7ffc86d57181f3c967e718e7abf36debb9d
Guillermo Venegas <guillermovcra@gmail.com>
2026-02-22 09:40:43 -0600

    Unwanted change

--- source/blender/editors/interface/regions/interface_region_tooltip.cc
+++ source/blender/editors/interface/regions/interface_region_tooltip.cc
@@ -224,6 +224,7 @@ static void ui_tooltip_region_draw_cb(const bContext * /*C*/, ARegion *region)
   BLF_wordwrap(blf_mono_font,
                data->wrap_width,
                BLFWrapMode(int(BLFWrapMode::Path) | int(BLFWrapMode::HardLimit)));
+
   bbox.xmin += 0.5f * pad_x; /* add padding to the text */
   bbox.ymax -= 0.5f * pad_y;
   bbox.ymax -= BLF_descender(data->fstyle.uifont_id);
