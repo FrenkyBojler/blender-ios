blender/blender

commit 9095b2042f64c859bbfd18ccc8e5586203f7dcfe

Fix: glTF: Add missing initialization for animation

scripts/addons_core/io_scene_gltf2/blender/exp/animation/anim_utils.py
@@ -260,6 +260,7 @@ def bake_animation(obj_uuid: str, animation_key: str, export_settings, mode=None
         # We need to bake all bones. Because some bone can have some constraints linking to
         # some other armature bones, for example
 
+        animation = None
         channels, _ = gather_action_armature_sampled(obj_uuid, None, None, animation_key, export_settings)
         if channels:
             animation = gltf2_io.Animation(

scripts/addons_core/io_scene_gltf2/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     # This is now displayed as the maintainer, so show the foundation.
     # "author": "Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein", # Original Authors
     'author': "Blender Foundation, Khronos Group",
-    "version": (5, 1, 8),
+    "version": (5, 1, 9),
     'blender': (4, 4, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',

FrenkyBojler/blender-ios

commit 80ec6fa5e6fdc7b0eda80397ce4692b016bc4801

Fix: glTF operator precedence bug in animation baking
The condition was missing parentheses around 'mode is None or mode == "OBJECT"',
causing it to always evaluate to true when mode was "OBJECT".

This issue was introduced in 9095b2042f6.

scripts/addons_core/io_scene_gltf2/blender/exp/animation/anim_utils.py
@@ -256,7 +256,7 @@ def bake_animation(obj_uuid: str, animation_key: str, export_settings, mode=None
             or export_settings['gltf_animation_mode'] == "NLA_TRACKS") \
             and blender_object \
             and blender_object.type == "ARMATURE" \
-            and mode is None or mode == "OBJECT":
+            and (mode is None or mode == "OBJECT"):
         # We need to bake all bones. Because some bone can have some constraints linking to
         # some other armature bones, for example
 