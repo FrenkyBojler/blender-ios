blender/blender

commit 8adba33029f766eba157c283a9e01dd35384f97b

Mesh: Switch from CustomData to AttributeStorage
This commit changes Mesh to use AttributeStorage to store generic
attributes, similar to previous changes for curves and point clouds.
The main benefit is that this matches the generic attribute design
better than CustomData. This is discussed more in #122398.

Most of the necessary changes landed separately over the past year or so
by moving code to use the higher-level attribute API instead of using
CustomData directly. This commits changes the remaining cases where
lower level data access was necessary, and replaces the attribute
provider system.

Resolves #107500

Pull Request: https://projects.blender.org/blender/blender/pulls/152851

source/blender/makesrna/intern/rna_attribute.cc
@@ -338,8 +338,11 @@ static StructRNA *srna_by_custom_data_layer_type(const eCustomDataType type)
 static StructRNA *rna_Attribute_refine(PointerRNA *ptr)
 {
   if (GS(ptr->owner_id->name) == ID_ME) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
-    return srna_by_custom_data_layer_type(eCustomDataType(layer->type));
+    const Mesh *mesh = blender::id_cast<const Mesh *>(ptr->owner_id);
+    if (mesh->runtime->edit_mesh) {
+      CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
+      return srna_by_custom_data_layer_type(eCustomDataType(layer->type));
+    }
   }
   bke::Attribute *attr = ptr->data_as<bke::Attribute>();
   const eCustomDataType data_type = *bke::attr_type_to_custom_data_type(attr->data_type());
@@ -354,8 +357,11 @@ StringRefNull rna_Attribute_name_get(const PointerRNA &ptr)
   ID *owner_id = ptr.owner_id;
   const AttributeOwner owner = AttributeOwner::from_id(owner_id);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    const CustomDataLayer *layer = ptr.data_as<CustomDataLayer>();
-    return layer->name;
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      const CustomDataLayer *layer = ptr.data_as<CustomDataLayer>();
+      return layer->name;
+    }
   }
   const bke::Attribute *attr = ptr.data_as<bke::Attribute>();
   return attr->name();
@@ -375,9 +381,12 @@ void rna_Attribute_name_set(PointerRNA *ptr, const char *value)
 {
   AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    const CustomDataLayer *layer = static_cast<const CustomDataLayer *>(ptr->data);
-    BKE_attribute_rename(owner, layer->name, value, nullptr);
-    return;
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      const CustomDataLayer *layer = static_cast<const CustomDataLayer *>(ptr->data);
+      BKE_attribute_rename(owner, layer->name, value, nullptr);
+      return;
+    }
   }
 
   const bke::Attribute *attr = ptr->data_as<bke::Attribute>();
@@ -388,12 +397,15 @@ static int rna_Attribute_name_editable(const PointerRNA *ptr, const char **r_inf
 {
   AttributeOwner owner = owner_from_attribute_pointer_rna(const_cast<PointerRNA *>(ptr));
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
-    if (BKE_attribute_required(owner, layer->name)) {
-      *r_info = N_("Cannot modify name of required geometry attribute");
-      return false;
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
+      if (BKE_attribute_required(owner, layer->name)) {
+        *r_info = N_("Cannot modify name of required geometry attribute");
+        return false;
+      }
+      return true;
     }
-    return true;
   }
 
   bke::Attribute *attr = ptr->data_as<bke::Attribute>();
@@ -406,9 +418,13 @@ static int rna_Attribute_name_editable(const PointerRNA *ptr, const char **r_inf
 
 static int rna_Attribute_type_get(PointerRNA *ptr)
 {
-  if (GS(ptr->owner_id->name) == ID_ME) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
-    return layer->type;
+  AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
+  if (owner.type() == AttributeOwnerType::Mesh) {
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
+      return layer->type;
+    }
   }
   const bke::Attribute *attr = static_cast<const bke::Attribute *>(ptr->data);
   return *bke::attr_type_to_custom_data_type(attr->data_type());
@@ -416,8 +432,12 @@ static int rna_Attribute_type_get(PointerRNA *ptr)
 
 static int rna_Attribute_storage_type_get(PointerRNA *ptr)
 {
-  if (GS(ptr->owner_id->name) == ID_ME) {
-    return int(bke::AttrStorageType::Array);
+  AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
+  if (owner.type() == AttributeOwnerType::Mesh) {
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      return int(bke::AttrStorageType::Array);
+    }
   }
   const bke::Attribute *attr = static_cast<const bke::Attribute *>(ptr->data);
   return int(attr->storage_type());
@@ -490,7 +510,11 @@ static int rna_Attribute_domain_get(PointerRNA *ptr)
 {
   AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    return int(BKE_attribute_domain(owner, static_cast<const CustomDataLayer *>(ptr->data)));
+    const Mesh *mesh = owner.get_mesh();
+    if (BMEditMesh *em = mesh->runtime->edit_mesh.get()) {
+      return int(
+          BKE_attribute_domain(*mesh, *em->bm, static_cast<const CustomDataLayer *>(ptr->data)));
+    }
   }
   const bke::Attribute *attr = static_cast<const bke::Attribute *>(ptr->data);
   return int(attr->domain());
@@ -511,17 +535,11 @@ void rna_Attribute_data_begin(CollectionPropertyIterator *iter, PointerRNA *ptr)
 {
   AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
-    if (!(CD_TYPE_AS_MASK(eCustomDataType(layer->type)) & CD_MASK_PROP_ALL)) {
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
       iter->valid = false;
+      return;
     }
-
-    const int length = BKE_attribute_data_length(owner, layer);
-    const size_t struct_size = CustomData_get_elem_size(layer);
-    CustomData_ensure_data_is_mutable(layer, length);
-
-    rna_iterator_array_begin(iter, ptr, layer->data, struct_size, length, 0, nullptr);
-    return;
   }
 
   bke::MutableAttributeAccessor accessor = *owner.get_accessor();
@@ -550,8 +568,10 @@ int rna_Attribute_data_length(PointerRNA *ptr)
   }
   AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(ptr->data);
-    return BKE_attribute_data_length(owner, layer);
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      return 0;
+    }
   }
 
   const bke::Attribute *attr = ptr->data_as<bke::Attribute>();
@@ -696,37 +716,40 @@ static PointerRNA rna_AttributeGroupID_new(
 {
   AttributeOwner owner = AttributeOwner::from_id(id);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = BKE_attribute_new(
-        owner, name, eCustomDataType(type), AttrDomain(domain), reports);
-    if (!layer) {
-      return PointerRNA_NULL;
-    }
+    Mesh *mesh = owner.get_mesh();
+    if (BMEditMesh *em = mesh->runtime->edit_mesh.get()) {
+      CustomDataLayer *layer = BKE_attribute_new(
+          *mesh, *em->bm, name, eCustomDataType(type), AttrDomain(domain), reports);
+      if (!layer) {
+        return PointerRNA_NULL;
+      }
 
-    if ((GS(id->name) == ID_ME)) {
-      Mesh *mesh = id_cast<Mesh *>(id);
-      if (ELEM(layer->type, CD_PROP_COLOR, CD_PROP_BYTE_COLOR)) {
-        if (!mesh->active_color_attribute) {
-          mesh->active_color_attribute = BLI_strdup(layer->name);
+      if ((GS(id->name) == ID_ME)) {
+        Mesh *mesh = id_cast<Mesh *>(id);
+        if (ELEM(layer->type, CD_PROP_COLOR, CD_PROP_BYTE_COLOR)) {
+          if (!mesh->active_color_attribute) {
+            mesh->active_color_attribute = BLI_strdup(layer->name);
+          }
+          if (!mesh->default_color_attribute) {
+            mesh->default_color_attribute = BLI_strdup(layer->name);
+          }
         }
-        if (!mesh->default_color_attribute) {
-          mesh->default_color_attribute = BLI_strdup(layer->name);
+        if (ELEM(layer->type, CD_PROP_FLOAT2)) {
+          if (mesh->active_uv_map_name().is_empty()) {
+            mesh->uv_maps_active_set(layer->name);
+          }
+          if (mesh->default_uv_map_name().is_empty()) {
+            mesh->uv_maps_default_set(layer->name);
+          }
         }
       }
-      if (ELEM(layer->type, CD_PROP_FLOAT2)) {
-        if (mesh->active_uv_map_name().is_empty()) {
-          mesh->uv_maps_active_set(layer->name);
-        }
-        if (mesh->default_uv_map_name().is_empty()) {
-          mesh->uv_maps_default_set(layer->name);
-        }
-      }
+
+      DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
+      WM_main_add_notifier(NC_GEOM | ND_DATA, id);
+
+      PointerRNA ptr = RNA_pointer_create_discrete(id, RNA_Attribute, layer);
+      return ptr;
     }
-
-    DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
-    WM_main_add_notifier(NC_GEOM | ND_DATA, id);
-
-    PointerRNA ptr = RNA_pointer_create_discrete(id, RNA_Attribute, layer);
-    return ptr;
   }
 
   const bke::AttributeAccessor accessor = *owner.get_accessor();
@@ -754,13 +777,16 @@ static void rna_AttributeGroupID_remove(ID *id, ReportList *reports, PointerRNA 
 {
   AttributeOwner owner = AttributeOwner::from_id(id);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    const CustomDataLayer *layer = static_cast<const CustomDataLayer *>(attribute_ptr->data);
-    BKE_attribute_remove(owner, layer->name, reports);
-    attribute_ptr->invalidate();
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      const CustomDataLayer *layer = static_cast<const CustomDataLayer *>(attribute_ptr->data);
+      BKE_attribute_remove(owner, layer->name, reports);
+      attribute_ptr->invalidate();
 
-    DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
-    WM_main_add_notifier(NC_GEOM | ND_DATA, id);
-    return;
+      DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
+      WM_main_add_notifier(NC_GEOM | ND_DATA, id);
+      return;
+    }
   }
 
   const bke::Attribute *attr = static_cast<const bke::Attribute *>(attribute_ptr->data);
@@ -787,19 +813,19 @@ void rna_AttributeGroup_iterator_begin(CollectionPropertyIterator *iter,
   AttributeOwner owner = owner_from_pointer_rna(ptr);
   if (owner.type() == AttributeOwnerType::Mesh) {
     Mesh *mesh = owner.get_mesh();
-    Vector<CustomDataLayer *, 16> layers;
-    const auto add_layers = [&](CustomData &data) {
-      for (CustomDataLayer &layer : MutableSpan(data.layers, data.totlayer)) {
-        if (!(CD_TYPE_AS_MASK(eCustomDataType(layer.type)) & cd_type_mask)) {
-          continue;
-        }
-        if (!include_anonymous && bke::attribute_name_is_anonymous(layer.name)) {
-          continue;
-        }
-        layers.append(&layer);
-      }
-    };
     if (BMEditMesh *em = mesh->runtime->edit_mesh.get()) {
+      Vector<CustomDataLayer *, 16> layers;
+      const auto add_layers = [&](CustomData &data) {
+        for (CustomDataLayer &layer : MutableSpan(data.layers, data.totlayer)) {
+          if (!(CD_TYPE_AS_MASK(eCustomDataType(layer.type)) & cd_type_mask)) {
+            continue;
+          }
+          if (!include_anonymous && bke::attribute_name_is_anonymous(layer.name)) {
+            continue;
+          }
+          layers.append(&layer);
+        }
+      };
       if (domain_mask & ATTR_DOMAIN_MASK_POINT) {
         add_layers(em->bm->vdata);
       }
@@ -812,25 +838,11 @@ void rna_AttributeGroup_iterator_begin(CollectionPropertyIterator *iter,
       if (domain_mask & ATTR_DOMAIN_MASK_CORNER) {
         add_layers(em->bm->ldata);
       }
+      VectorData data = layers.release();
+      rna_iterator_array_begin(
+          iter, ptr, data.data, sizeof(CustomDataLayer *), data.size, true, nullptr);
+      return;
     }
-    else {
-      if (domain_mask & ATTR_DOMAIN_MASK_POINT) {
-        add_layers(mesh->vert_data);
-      }
-      if (domain_mask & ATTR_DOMAIN_MASK_EDGE) {
-        add_layers(mesh->edge_data);
-      }
-      if (domain_mask & ATTR_DOMAIN_MASK_FACE) {
-        add_layers(mesh->face_data);
-      }
-      if (domain_mask & ATTR_DOMAIN_MASK_CORNER) {
-        add_layers(mesh->corner_data);
-      }
-    }
-    VectorData data = layers.release();
-    rna_iterator_array_begin(
-        iter, ptr, data.data, sizeof(CustomDataLayer *), data.size, true, nullptr);
-    return;
   }
 
   bke::AttributeStorage &storage = *owner.get_storage();
@@ -861,12 +873,15 @@ PointerRNA rna_AttributeGroup_iterator_get(CollectionPropertyIterator *iter)
 {
   AttributeOwner owner = owner_from_pointer_rna(&iter->parent);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = *static_cast<CustomDataLayer **>(rna_iterator_array_get(iter));
-    StructRNA *type = srna_by_custom_data_layer_type(eCustomDataType(layer->type));
-    if (type == nullptr) {
-      return PointerRNA_NULL;
+    Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      CustomDataLayer *layer = *static_cast<CustomDataLayer **>(rna_iterator_array_get(iter));
+      StructRNA *type = srna_by_custom_data_layer_type(eCustomDataType(layer->type));
+      if (type == nullptr) {
+        return PointerRNA_NULL;
+      }
+      return RNA_pointer_create_with_parent(iter->parent, type, layer);
     }
-    return RNA_pointer_create_with_parent(iter->parent, type, layer);
   }
 
   bke::Attribute *attr = *static_cast<bke::Attribute **>(rna_iterator_array_get(iter));
@@ -916,14 +931,6 @@ PointerRNA rna_AttributeGroup_lookup_string(const PointerRNA &ptr,
           ptr, RNA_Attribute, const_cast<CustomDataLayer *>(attr.layer), result);
       return result;
     }
-    else if (CustomDataLayer *layer = BKE_attribute_search_for_write(
-                 owner, key, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL))
-    {
-      PointerRNA result;
-      rna_pointer_create_with_ancestors(ptr, RNA_Attribute, layer, result);
-      return result;
-    }
-    return PointerRNA_NULL;
   }
 
   bke::AttributeStorage &storage = *owner.get_storage();
@@ -970,14 +977,17 @@ static void rna_AttributeGroupID_active_set(PointerRNA *ptr,
 {
   AttributeOwner owner = AttributeOwner::from_id(ptr->owner_id);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    CustomDataLayer *layer = static_cast<CustomDataLayer *>(attribute_ptr.data);
-    if (layer) {
-      BKE_attributes_active_set(owner, layer->name);
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      CustomDataLayer *layer = static_cast<CustomDataLayer *>(attribute_ptr.data);
+      if (layer) {
+        BKE_attributes_active_set(owner, layer->name);
+      }
+      else {
+        BKE_attributes_active_clear(owner);
+      }
+      return;
     }
-    else {
-      BKE_attributes_active_clear(owner);
-    }
-    return;
   }
 
   bke::Attribute *attr = attribute_ptr.data_as<bke::Attribute>();
@@ -1022,7 +1032,10 @@ static int rna_AttributeGroupID_domain_size(ID *id, const int domain)
 {
   AttributeOwner owner = AttributeOwner::from_id(id);
   if (owner.type() == AttributeOwnerType::Mesh) {
-    return BKE_attribute_domain_size(owner, domain);
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh) {
+      return BKE_attribute_domain_size(owner, domain);
+    }
   }
 
   bke::AttributeAccessor attributes = *owner.get_accessor();

source/blender/blenkernel/BKE_attribute.h
@@ -26,6 +26,7 @@ class AttributeAccessor;
 class AttributeStorage;
 class MutableAttributeAccessor;
 }  // namespace bke
+struct BMesh;
 struct CustomData;
 struct CustomDataLayer;
 struct ID;
@@ -89,27 +90,18 @@ class AttributeOwner {
 /**
  * Create a new attribute layer.
  */
-struct CustomDataLayer *BKE_attribute_new(AttributeOwner &owner,
+struct CustomDataLayer *BKE_attribute_new(Mesh &mesh,
+                                          BMesh &bm,
                                           StringRef name,
                                           eCustomDataType type,
                                           bke::AttrDomain domain,
                                           struct ReportList *reports);
 bool BKE_attribute_remove(AttributeOwner &owner, StringRef name, struct ReportList *reports);
 
-const struct CustomDataLayer *BKE_attribute_search(const AttributeOwner &owner,
-                                                   StringRef name,
-                                                   eCustomDataMask type,
-                                                   AttrDomainMask domain_mask);
-
-struct CustomDataLayer *BKE_attribute_search_for_write(AttributeOwner &owner,
-                                                       StringRef name,
-                                                       eCustomDataMask type,
-                                                       AttrDomainMask domain_mask);
-
-bke::AttrDomain BKE_attribute_domain(const AttributeOwner &owner,
+bke::AttrDomain BKE_attribute_domain(const Mesh &mesh,
+                                     const BMesh &bm,
                                      const struct CustomDataLayer *layer);
 int BKE_attribute_domain_size(const AttributeOwner &owner, int domain);
-int BKE_attribute_data_length(AttributeOwner &owner, struct CustomDataLayer *layer);
 bool BKE_attribute_required(const AttributeOwner &owner, StringRef name);
 bool BKE_attribute_rename(AttributeOwner &owner,
                           StringRef old_name,

source/blender/blenkernel/BKE_attribute_legacy_convert.hh
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "BLI_vector.hh"
+
 #include "DNA_attribute_types.h"
 
 #include "BKE_attribute.h"
@@ -38,13 +40,6 @@ std::optional<AttrType> custom_data_type_to_attr_type(eCustomDataType data_type)
 std::optional<eCustomDataType> attr_type_to_custom_data_type(AttrType attr_type);
 
 /**
- * Move attributes from the #AttributeStorage to the mesh's #CustomData structs. Used for forward
- * compatibility: converting newer files written with #AttributeStorage while #CustomData is still
- * used at runtime.
- */
-void mesh_convert_storage_to_customdata(Mesh &mesh);
-
-/**
  * Move generic attributes from #CustomData to #AttributeStorage (not including non-generic layer
  * types). Use for versioning old files when the newer #AttributeStorage format is used at runtime.
  */
@@ -61,6 +56,8 @@ void grease_pencil_convert_customdata_to_storage(GreasePencil &grease_pencil);
 
 /** Abstraction for copying #CustomData layers and #AttributeStorage attributes. */
 class LegacyMeshInterpolator {
+  Vector<GVArray> attrs_src_;
+  Vector<GMutableSpan> attrs_dst_;
 
   const CustomData &cd_src_;
   CustomData &cd_dst_;

source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ namespace blender {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 18
+#define BLENDER_FILE_SUBVERSION 19
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_customdata.hh
@@ -229,14 +229,6 @@ bool CustomData_bmesh_merge_layout(const CustomData *source,
                                    char htype);
 
 /**
- * Remove layers that aren't stored in BMesh or are stored as flags on BMesh.
- * The `layers` array of the returned #CustomData must be freed, but may be null.
- * Used during conversion of #Mesh data to #BMesh storage format.
- */
-CustomData CustomData_shallow_copy_remove_non_bmesh_attributes(const CustomData *src,
-                                                               eCustomDataMask mask);
-
-/**
  * NULL's all members and resets the #CustomData.typemap.
  *
  * \warning Does not free or release any internal resources.

source/blender/blenkernel/CMakeLists.txt
@@ -540,7 +540,6 @@ set(SRC
   nla_private.h
   particle_private.h
 
-  intern/attribute_access_intern.hh
   intern/attribute_storage_access.hh
   intern/data_transfer_intern.hh
   intern/lib_intern.hh

FrenkyBojler/blender-ios

commit 91094bbc861395b0755e84b40a40a1d59bb5f84a

Fix: Mesh: Active/default color attribute and UV map not set when created via Python
Caused by 8adba33029f766eba157c283a9e01dd35384f97b.

Since this commit the active/default color and UV were only set in
edit mode. The code path using AttributeStorage did not set them.

source/blender/makesrna/intern/rna_attribute.cc
@@ -768,6 +768,26 @@ static PointerRNA rna_AttributeGroupID_new(
       *bke::custom_data_type_to_attr_type(eCustomDataType(type)),
       bke::Attribute::ArrayData::from_default_value(cpp_type, domain_size));
 
+  if (owner.type() == AttributeOwnerType::Mesh) {
+    Mesh *mesh = owner.get_mesh();
+    if (ELEM(attr.data_type(), bke::AttrType::ColorFloat, bke::AttrType::ColorByte)) {
+      if (!mesh->active_color_attribute) {
+        BKE_id_attributes_active_color_set(id, attr.name());
+      }
+      if (!mesh->default_color_attribute) {
+        BKE_id_attributes_default_color_set(id, attr.name());
+      }
+    }
+    if (ELEM(attr.data_type(), bke::AttrType::Float2)) {
+      if (mesh->active_uv_map_name().is_empty()) {
+        mesh->uv_maps_active_set(attr.name());
+      }
+      if (mesh->default_uv_map_name().is_empty()) {
+        mesh->uv_maps_default_set(attr.name());
+      }
+    }
+  }
+
   DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
   WM_main_add_notifier(NC_GEOM | ND_DATA, id);
 

tests/python/bl_geometry_attributes.py
@@ -68,6 +68,39 @@ class TestCurves(unittest.TestCase):
         self.assertTrue(self.curves.attributes["a" * 100].name == "a" * 100)
 
 
+class TestMesh(unittest.TestCase):
+    def setUp(self):
+        self.mesh = bpy.data.meshes.new("test")
+        self.mesh.vertices.add(10)
+
+    def tearDown(self):
+        bpy.data.meshes.remove(self.mesh)
+        del self.mesh
+
+    def test_set_active_color_attribute_if_empty(self):
+        self.assertTrue(self.mesh.attributes.active_color_name == "")
+        self.assertTrue(self.mesh.attributes.default_color_name == "")
+
+        self.mesh.attributes.new("a", 'FLOAT_COLOR', 'POINT')
+        self.assertTrue(self.mesh.attributes.active_color_name == "a")
+        self.assertTrue(self.mesh.attributes.default_color_name == "a")
+
+        self.mesh.attributes.new("b", 'FLOAT_COLOR', 'POINT')
+        self.assertTrue(self.mesh.attributes.active_color_name == "a")
+        self.assertTrue(self.mesh.attributes.default_color_name == "a")
+
+    def test_set_active_uv_map_if_empty(self):
+        self.assertTrue(self.mesh.uv_layers.active is None)
+
+        self.mesh.attributes.new("a", 'FLOAT2', 'CORNER')
+        self.assertTrue(self.mesh.uv_layers.active is not None)
+        self.assertTrue(self.mesh.uv_layers.active.name == "a")
+
+        self.mesh.attributes.new("b", 'FLOAT2', 'CORNER')
+        self.assertTrue(self.mesh.uv_layers.active is not None)
+        self.assertTrue(self.mesh.uv_layers.active.name == "a")
+
+
 if __name__ == '__main__':
     import sys
     sys.argv = [__file__] + (sys.argv[sys.argv.index("--") + 1:] if "--" in sys.argv else [])