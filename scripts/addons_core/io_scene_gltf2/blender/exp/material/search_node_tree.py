blender/blender

commit fd416bc1960f6d52359b13e909a31c1812bc6f7b

Fix: glTF: Check undefined group nodes
When linked lib is missing

scripts/addons_core/io_scene_gltf2/blender/exp/material/search_node_tree.py
@@ -831,7 +831,11 @@ def previous_socket(socket: NodeSocket):
         # If we are entering a node group (from outputs)
         if from_socket.node.type == "GROUP":
             socket_name = from_socket.name
-            sockets = [n for n in from_socket.node.node_tree.nodes if n.type == "GROUP_OUTPUT"][0].inputs
+            # Some groups can be undefined (because of linked librairies)
+            next_socket = next(iter([n for n in from_socket.node.node_tree.nodes if n.type == "GROUP_OUTPUT"]), None)
+            if next_socket is None:
+                return NodeSocket(None, None)
+            sockets = next_socket.inputs
             socket = [s for s in sockets if s.name == socket_name][0]
             group_path.append(from_socket.node)
             soc = socket

scripts/addons_core/io_scene_gltf2/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     # This is now displayed as the maintainer, so show the foundation.
     # "author": "Julien Duroure, Scurest, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein", # Original Authors
     'author': "Blender Foundation, Khronos Group",
-    "version": (5, 1, 13),
+    "version": (5, 1, 14),
     'blender': (4, 4, 0),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',

FrenkyBojler/blender-ios

commit c31db7d5700baef6103ca23b0645c2751ef6ec1b

Fix: glTF: crash when node group is undefined (missing linked library)
This issue was introduced in fd416bc1960.

scripts/addons_core/io_scene_gltf2/blender/exp/material/search_node_tree.py
@@ -833,6 +833,8 @@ def previous_socket(socket: NodeSocket):
         if from_socket.node.type == "GROUP":
             socket_name = from_socket.name
             # Some groups can be undefined (because of linked librairies)
+            if from_socket.node.node_tree is None:
+                return NodeSocket(None, None)
             next_socket = next(iter([n for n in from_socket.node.node_tree.nodes if n.type == "GROUP_OUTPUT"]), None)
             if next_socket is None:
                 return NodeSocket(None, None)