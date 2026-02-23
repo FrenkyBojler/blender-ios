1c29643f0da024b77b87893322883150ce8b1c8f
Martijn Versteegh <blender@aaltjegron.nl>
2026-02-23 13:24:01 +0100

    Nullptrcheck added

--- source/blender/windowmanager/xr/intern/wm_xr_operators.cc
+++ source/blender/windowmanager/xr/intern/wm_xr_operators.cc
@@ -535,6 +535,11 @@ static wmOperatorStatus wm_xr_navigation_grab_modal(bContext *C,
   wmWindowManager *wm = CTX_wm_manager(C);
   wmXrData *xr = &wm->xr;
 
+  //! very dubious 'fix' for entering this function with event->customdata null
+  if (data == nullptr) {
+    return       return OPERATOR_RUNNING_MODAL;
+  }
+
   WM_xr_session_state_vignette_activate(xr);
 
   const bool do_bimanual = wm_xr_navigation_grab_can_do_bimanual(actiondata, data);
