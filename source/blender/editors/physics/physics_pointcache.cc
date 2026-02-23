ea72adc148989977ad88b3490140acb93dc6fa22
Honda Guy <caleb.saxton2@gmail.com>
2026-02-24 01:17:36 +1100

    Fix #144104: Bake all physics tooltip is non-descriptive.

--- source/blender/editors/physics/physics_pointcache.cc
+++ source/blender/editors/physics/physics_pointcache.cc
@@ -292,7 +292,7 @@ void PTCACHE_OT_bake_all(wmOperatorType *ot)
 {
   /* identifiers */
   ot->name = "Bake All Dynamics";
-  ot->description = "Bake all dynamics";
+  ot->description = "Bake all physics simulations in the current scene";
   ot->idname = "PTCACHE_OT_bake_all";
 
   /* API callbacks. */
