blender/blender

commit 73f95d678524c89a65968a0915df3d5cae3414d8

Attributes: Add 4D float vector type
We already have 2D and 3D float vectors. This 4D vector will be used by
rotation constraints in upcoming hair physics solvers. This commit adds
support for propagation across all geometry operations, the spreadsheet,
and the "set attribute" operators. Unfortunately Geometry Nodes still
can't work well with non 3D vectors, that will have to be improved
separately.

Pull Request: https://projects.blender.org/blender/blender/pulls/154350

source/blender/blenkernel/intern/customdata.cc
@@ -1281,6 +1281,42 @@ static void layerInterp_propcol(const void **sources, const float *weights, int 
 /** \} */
 
 /* -------------------------------------------------------------------- */
+/** \name Callbacks for (#float4, #CD_PROP_FLOAT4)
+ * \{ */
+
+static void layerInterp_propfloat4(const void **sources,
+                                   const float *weights,
+                                   int count,
+                                   void *dest)
+{
+  vec4f result = {0.0f, 0.0f, 0.0f, 0.0f};
+  for (int i = 0; i < count; i++) {
+    const float interp_weight = weights[i];
+    const vec4f *src = static_cast<const vec4f *>(sources[i]);
+    madd_v4_v4fl(&result.x, &src->x, interp_weight);
+  }
+  copy_v4_v4(static_cast<float *>(dest), &result.x);
+}
+
+static void layerMultiply_propfloat4(void *data, const float fac)
+{
+  vec4f *vec = static_cast<vec4f *>(data);
+  vec->x *= fac;
+  vec->y *= fac;
+  vec->z *= fac;
+  vec->w *= fac;
+}
+
+static void layerAdd_propfloat4(void *data1, const void *data2)
+{
+  vec4f *vec1 = static_cast<vec4f *>(data1);
+  const vec4f *vec2 = static_cast<const vec4f *>(data2);
+  vec1->x += vec2->x;
+  vec1->y += vec2->y;
+  vec1->z += vec2->z;
+}
+
+/* -------------------------------------------------------------------- */
 /** \name Callbacks for (#vec3f, #CD_PROP_FLOAT3)
  * \{ */
 
@@ -1704,13 +1740,16 @@ static const LayerTypeInfo LAYERTYPEINFO[CD_NUMTYPES] = {
         .structname = "",
         .structnum = 0,
     },
-    /* 24: CD_RECAST */
+    /* 24: CD_PROP_FLOAT4 */
     {
-        .size = sizeof(MRecast),
-        .alignment = alignof(MRecast),
-        .structname = "MRecast",
+        .size = sizeof(float4),
+        .alignment = alignof(float4),
+        .structname = "vec4f",
         .structnum = 1,
-        .defaultname = N_("Recast"),
+        .defaultname = N_("Float4"),
+        .interp = layerInterp_propfloat4,
+        .multiply = layerMultiply_propfloat4,
+        .add = layerAdd_propfloat4,
     },
     /* 25: CD_MPOLY */ /* DEPRECATED */
     {

source/blender/blenkernel/BKE_attribute.hh
@@ -60,6 +60,7 @@ enum class AttrType : int16_t {
   ColorFloat = 10,
   Quaternion = 11,
   String = 12,
+  Float4 = 13,
 };
 
 const CPPType &attribute_type_to_cpp_type(AttrType type);

source/blender/blenkernel/BKE_attribute_math.hh
@@ -35,6 +35,7 @@ template<typename Fn> inline void to_static_type(const CPPType &cpp_type, Fn &&f
   cpp_type.to_static_type<float,
                           float2,
                           float3,
+                          float4,
                           int,
                           int2,
                           bool,
@@ -96,6 +97,11 @@ template<> inline float2 mix2(const float factor, const float2 &a, const float2 
 }
 
 template<> inline float3 mix2(const float factor, const float3 &a, const float3 &b)
+{
+  return math::interpolate(a, b, factor);
+}
+
+template<> inline float4 mix2(const float factor, const float4 &a, const float4 &b)
 {
   return math::interpolate(a, b, factor);
 }
@@ -163,6 +169,12 @@ inline float2 mix3(const float3 &weights, const float2 &v0, const float2 &v1, co
 
 template<>
 inline float3 mix3(const float3 &weights, const float3 &v0, const float3 &v1, const float3 &v2)
+{
+  return weights.x * v0 + weights.y * v1 + weights.z * v2;
+}
+
+template<>
+inline float4 mix3(const float3 &weights, const float4 &v0, const float4 &v1, const float4 &v2)
 {
   return weights.x * v0 + weights.y * v1 + weights.z * v2;
 }
@@ -255,6 +267,13 @@ inline float2 mix4(
 template<>
 inline float3 mix4(
     const float4 &weights, const float3 &v0, const float3 &v1, const float3 &v2, const float3 &v3)
+{
+  return weights.x * v0 + weights.y * v1 + weights.z * v2 + weights.w * v3;
+}
+
+template<>
+inline float4 mix4(
+    const float4 &weights, const float4 &v0, const float4 &v1, const float4 &v2, const float4 &v3)
 {
   return weights.x * v0 + weights.y * v1 + weights.z * v2 + weights.w * v3;
 }
@@ -566,6 +585,9 @@ template<> struct DefaultMixerStruct<float2> {
 };
 template<> struct DefaultMixerStruct<float3> {
   using type = SimpleMixer<float3>;
+};
+template<> struct DefaultMixerStruct<float4> {
+  using type = SimpleMixer<float4>;
 };
 template<> struct DefaultMixerStruct<ColorGeometry4f> {
   /* Use a special mixer for colors. ColorGeometry4f can't be added/multiplied, because this is not

source/blender/blenkernel/intern/attribute_access.cc
@@ -62,6 +62,8 @@ const CPPType &attribute_type_to_cpp_type(const AttrType type)
       return CPPType::get<math::Quaternion>();
     case AttrType::String:
       return CPPType::get<MStringProperty>();
+    case AttrType::Float4:
+      return CPPType::get<float4>();
   }
   BLI_assert_unreachable();
   return CPPType::get<bool>();
@@ -77,6 +79,9 @@ AttrType cpp_type_to_attribute_type(const CPPType &type)
   }
   if (type.is<float3>()) {
     return AttrType::Float3;
+  }
+  if (type.is<float4>()) {
+    return AttrType::Float4;
   }
   if (type.is<int>()) {
     return AttrType::Int32;
@@ -121,6 +126,8 @@ const CPPType *custom_data_type_to_cpp_type(const eCustomDataType type)
       return &CPPType::get<float2>();
     case CD_PROP_FLOAT3:
       return &CPPType::get<float3>();
+    case CD_PROP_FLOAT4:
+      return &CPPType::get<float4>();
     case CD_PROP_INT32:
       return &CPPType::get<int>();
     case CD_PROP_INT32_2D:
@@ -156,6 +163,9 @@ eCustomDataType cpp_type_to_custom_data_type(const CPPType &type)
   }
   if (type.is<float3>()) {
     return CD_PROP_FLOAT3;
+  }
+  if (type.is<float4>()) {
+    return CD_PROP_FLOAT4;
   }
   if (type.is<int>()) {
     return CD_PROP_INT32;
@@ -242,17 +252,19 @@ static int attribute_data_type_complexity(const AttrType data_type)
       return 6;
     case AttrType::Float3:
       return 7;
-    case AttrType::ColorByte:
+    case AttrType::Float4:
       return 8;
-    case AttrType::Quaternion:
+    case AttrType::ColorByte:
       return 9;
-    case AttrType::ColorFloat:
+    case AttrType::Quaternion:
       return 10;
-    case AttrType::Float4x4:
+    case AttrType::ColorFloat:
       return 11;
+    case AttrType::Float4x4:
+      return 12;
 #if 0 /* These attribute types are not supported yet. */
     case AttrType::String:
-      return 12;
+      return 13;
 #endif
     default:
       /* Only accept "generic" custom data types used by the attribute system. */

source/blender/blenkernel/intern/attribute_legacy_convert.cc
@@ -101,6 +101,8 @@ std::optional<AttrType> custom_data_type_to_attr_type(const eCustomDataType data
       return AttrType::ColorFloat;
     case CD_PROP_FLOAT3:
       return AttrType::Float3;
+    case CD_PROP_FLOAT4:
+      return AttrType::Float4;
     case CD_PROP_FLOAT2:
       return AttrType::Float2;
     case CD_PROP_BOOL:
@@ -205,6 +207,8 @@ std::optional<eCustomDataType> attr_type_to_custom_data_type(const AttrType attr
       return CD_PROP_FLOAT2;
     case AttrType::Float3:
       return CD_PROP_FLOAT3;
+    case AttrType::Float4:
+      return CD_PROP_FLOAT4;
     case AttrType::Float4x4:
       return CD_PROP_FLOAT4X4;
     case AttrType::ColorByte:

source/blender/blenkernel/intern/attribute_storage.cc
@@ -387,6 +387,9 @@ static void read_array_data(BlendDataReader &reader,
       BLO_read_struct_array(
           &reader, MStringProperty, size, reinterpret_cast<MStringProperty **>(data));
       return;
+    case int8_t(AttrType::Float4):
+      BLO_read_float_array(&reader, size * 4, reinterpret_cast<float **>(data));
+      return;
     default:
       *data = nullptr;
       return;
@@ -562,6 +565,9 @@ static void write_array_data(BlendWriter &writer,
       break;
     case AttrType::String:
       writer.write_struct_array_cast<MStringProperty>(size, data);
+      break;
+    case AttrType::Float4:
+      BLO_write_float_array(&writer, size * 4, static_cast<const float *>(data));
       break;
   }
 }

source/blender/blenkernel/intern/bake_items_serialize.cc
@@ -1246,6 +1246,10 @@ static std::shared_ptr<io::serialize::Value> serialize_primitive_value(
       const float3 value = *static_cast<const float3 *>(value_ptr);
       return serialize_float_array({&value.x, 3});
     }
+    case CD_PROP_FLOAT4: {
+      const float4 value = *static_cast<const float4 *>(value_ptr);
+      return serialize_float_array({&value.x, 4});
+    }
     case CD_PROP_BOOL: {
       const bool value = *static_cast<const bool *>(value_ptr);
       return std::make_shared<io::serialize::BooleanValue>(value);
@@ -1369,6 +1373,9 @@ template<typename T>
     }
     case CD_PROP_FLOAT3: {
       return deserialize_float_array(io_value, {static_cast<float *>(r_value), 3});
+    }
+    case CD_PROP_FLOAT4: {
+      return deserialize_float_array(io_value, {static_cast<float *>(r_value), 4});
     }
     case CD_PROP_BOOL: {
       if (const io::serialize::BooleanValue *io_value_boolean = io_value.as_boolean_value()) {

source/blender/blenkernel/intern/geometry_compare.cc
@@ -277,6 +277,10 @@ static bool values_different(const T value1,
     ASSERT_AND_ASSUME(component_i >= 0 && component_i < 3);
     return compare_threshold_relative(value1[component_i], value2[component_i], threshold);
   }
+  if constexpr (is_same_any_v<T, float4>) {
+    ASSERT_AND_ASSUME(component_i >= 0 && component_i < 4);
+    return compare_threshold_relative(value1[component_i], value2[component_i], threshold);
+  }
   if constexpr (is_same_any_v<T, ColorGeometry4f>) {
     ASSERT_AND_ASSUME(component_i >= 0 && component_i < 4);
     return compare_threshold_relative(value1[component_i], value2[component_i], threshold);

FrenkyBojler/blender-ios

commit 3106393259e562f84f544c67a574402e4d81122a

Fix: Attributes: missing w component addition for Float4 type
In source/blender/blenkernel/intern/customdata.cc, the layerAdd_propfloat4
function was only adding the x, y, and z components.

This issue was introduced in 73f95d67852.

source/blender/blenkernel/intern/customdata.cc
@@ -1314,6 +1314,7 @@ static void layerAdd_propfloat4(void *data1, const void *data2)
   vec1->x += vec2->x;
   vec1->y += vec2->y;
   vec1->z += vec2->z;
+  vec1->w += vec2->w;
 }
 
 /* -------------------------------------------------------------------- */