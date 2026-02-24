MaiKuraki/blender

commit c80f7a29a708e07e5be90e931bae88bb16111cea

Compositor: Node Gizmos in the Image Editor
When the node editor has an active compositor node, its gizmo is shown
in the image editor. The following nodes are supported:
- Crop
- Glare (Sun Beams)
- Corner pin
- Box Mask
- Ellipse Mask
- Split
- ~~Viewer~~:  (not ported because there are already dedicated
navigation gizmos in the image editor.)

When multiple node editors are present, the image editor shows gizmos
from the most recently used node editor (similar to geometry nodes
gizmos in viewport).

Implementation:
- The most recently used node editor is determined by
`bNodeTree::active_viewer_key`, see `find_node_editor()`
- Gizmos in both editors share `refresh()` and `setup()` callbacks but
each space implements its own `poll()` and `draw_prepare()` callback.
The reason is image drawing uses different conventions for offsets. So
`draw_prepare()` callbacks can later be unified once this is fixed.

Pull Request: https://projects.blender.org/blender/blender/pulls/153930

source/blender/editors/space_image/space_image.cc
@@ -45,6 +45,8 @@
 #include "ED_util.hh"
 #include "ED_uvedit.hh"
 
+#include "NOD_compositor_gizmos.hh"
+
 #include "WM_api.hh"
 #include "WM_types.hh"
 
@@ -342,6 +344,8 @@ static void image_listener(const wmSpaceTypeListenerParams *params)
             BKE_image_partial_update_mark_full_update(sima->image);
           }
           ED_area_tag_redraw(area);
+          const ARegion *region = BKE_area_find_region_type(area, RGN_TYPE_WINDOW);
+          WM_gizmomap_tag_refresh(region->runtime->gizmo_map);
           break;
       }
       break;
@@ -534,6 +538,90 @@ static void IMAGE_GGT_navigate(wmGizmoGroupType *gzgt)
   ui::VIEW2D_GGT_navigate_impl(gzgt, "IMAGE_GGT_navigate");
 }
 
+static void IMAGE_GGT_compositor_box_mask(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Box Mask Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_box_mask";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::box_mask_poll_space_image;
+  gzgt->setup = nodes::gizmos::box_mask_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::bbox_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::box_mask_refresh;
+}
+
+static void IMAGE_GGT_compositor_crop(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Crop Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_crop";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::crop_poll_space_image;
+  gzgt->setup = nodes::gizmos::crop_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::bbox_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::crop_refresh;
+}
+
+static void IMAGE_GGT_compositor_glare(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Glare Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_glare";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::glare_poll_space_image;
+  gzgt->setup = nodes::gizmos::glare_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::glare_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::glare_refresh;
+}
+
+static void IMAGE_GGT_compositor_corner_pin(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Glare Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_corner_pin";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::corner_pin_poll_space_image;
+  gzgt->setup = nodes::gizmos::corner_pin_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::corner_pin_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::corner_pin_refresh;
+}
+
+static void IMAGE_GGT_compositor_ellipse_mask(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Ellipse Mask Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_ellipse_mask";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::ellipse_mask_poll_space_image;
+  gzgt->setup = nodes::gizmos::ellipse_mask_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::bbox_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::box_mask_refresh;
+}
+
+static void IMAGE_GGT_compositor_split(wmGizmoGroupType *gzgt)
+{
+  gzgt->name = "Ellipse Mask Node Widget";
+  gzgt->idname = "IMAGE_GGT_compositor_split";
+
+  gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
+
+  gzgt->poll = nodes::gizmos::split_poll_space_image;
+  gzgt->setup = nodes::gizmos::split_setup;
+  gzgt->setup_keymap = WM_gizmogroup_setup_keymap_generic_maybe_drag;
+  gzgt->draw_prepare = nodes::gizmos::bbox_draw_prepare_space_image;
+  gzgt->refresh = nodes::gizmos::split_refresh;
+}
+
 static void image_widgets()
 {
   const wmGizmoMapType_Params params{SPACE_IMAGE, RGN_TYPE_WINDOW};
@@ -545,6 +633,13 @@ static void image_widgets()
   WM_gizmogrouptype_append(IMAGE_GGT_gizmo2d_rotate);
 
   WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_navigate);
+
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_box_mask);
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_crop);
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_glare);
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_corner_pin);
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_ellipse_mask);
+  WM_gizmogrouptype_append_and_link(gzmap_type, IMAGE_GGT_compositor_split);
 }
 
 /************************** main region ***************************/
@@ -833,6 +928,12 @@ static void image_main_region_listener(const wmRegionListenerParams *params)
         {
           ED_region_tag_redraw(region);
         }
+      }
+      break;
+    case NC_NODE:
+      if (ELEM(wmn->action, NA_EDITED, NA_SELECTED)) {
+        WM_gizmomap_tag_refresh(region->runtime->gizmo_map);
+        ED_region_tag_redraw(region);
       }
       break;
     case NC_SCREEN:

scripts/startup/bl_ui/space_image.py
@@ -1600,6 +1600,13 @@ class IMAGE_PT_gizmo_display(Panel):
         colsub = col.column()
         colsub.prop(view, "show_gizmo_navigate", text="Navigate")
 
+        image = view.image
+        show_compositor_gizmos = (image is not None and image.type == 'COMPOSITING' and
+                                  view.ui_mode in ('VIEW', 'MASK'))
+        if show_compositor_gizmos:
+            colsub = col.column()
+            colsub.prop(view, "show_gizmo_active_node")
+
 
 class IMAGE_PT_overlay(Panel):
     bl_space_type = 'IMAGE_EDITOR'

source/blender/makesdna/DNA_space_enums.h
@@ -799,6 +799,7 @@ enum {
   /** All gizmos. */
   SI_GIZMO_HIDE = (1 << 0),
   SI_GIZMO_HIDE_NAVIGATE = (1 << 1),
+  SI_GIZMO_HIDE_ACTIVE_NODE = (1 << 2),
 };
 
 /** Keep in sync with `STEPS_LEN` in `grid_frag.glsl`. */

source/blender/makesrna/intern/rna_space.cc
@@ -6380,6 +6380,11 @@ static void rna_def_space_image(BlenderRNA *brna)
   RNA_def_property_ui_text(prop, "Navigate Gizmo", "Viewport navigation gizmo");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_IMAGE, nullptr);
 
+  prop = RNA_def_property(srna, "show_gizmo_active_node", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_negative_sdna(prop, nullptr, "gizmo_flag", SI_GIZMO_HIDE_ACTIVE_NODE);
+  RNA_def_property_ui_text(prop, "Active Node", "Context sensitive gizmo for the active node");
+  RNA_def_property_update(prop, NC_SPACE | ND_SPACE_IMAGE, nullptr);
+
   /* Overlays */
   prop = RNA_def_property(srna, "overlay", PROP_POINTER, PROP_NONE);
   RNA_def_property_flag(prop, PROP_NEVER_NULL);

source/blender/nodes/NOD_compositor_gizmos.hh
@@ -20,6 +20,7 @@ namespace nodes::gizmos {
 
 void bbox_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
 void box_mask_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+void bbox_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup);
 
 /** \} */
 
@@ -29,6 +30,7 @@ void box_mask_refresh(const bContext *C, wmGizmoGroup *gzgroup);
 
 void box_mask_setup(const bContext *C, wmGizmoGroup *gzgroup);
 bool box_mask_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+bool box_mask_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
 
 /** \} */
 
@@ -40,6 +42,7 @@ void crop_refresh(const bContext *C, wmGizmoGroup *gzgroup);
 void crop_setup(const bContext *C, wmGizmoGroup *gzgroup);
 bool crop_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
 void crop_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
+bool crop_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
 
 /** \} */
 
@@ -49,6 +52,8 @@ void crop_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
 
 void glare_setup(const bContext *C, wmGizmoGroup *gzgroup);
 void glare_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+bool glare_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
+void glare_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup);
 bool glare_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
 void glare_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
 
@@ -60,7 +65,9 @@ void glare_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
 
 void corner_pin_setup(const bContext *C, wmGizmoGroup *gzgroup);
 void corner_pin_refresh(const bContext *C, wmGizmoGroup *gzgroup);
+bool corner_pin_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
 bool corner_pin_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+void corner_pin_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup);
 void corner_pin_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup);
 
 /** \} */
@@ -71,6 +78,8 @@ void corner_pin_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup
 
 void ellipse_mask_setup(const bContext *C, wmGizmoGroup *gzgroup);
 bool ellipse_mask_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+bool ellipse_mask_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
@@ -80,6 +89,7 @@ bool ellipse_mask_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
 void split_refresh(const bContext *C, wmGizmoGroup *gzgroup);
 void split_setup(const bContext *C, wmGizmoGroup *gzgroup);
 bool split_poll_space_node(const bContext *C, wmGizmoGroupType *gzgt);
+bool split_poll_space_image(const bContext *C, wmGizmoGroupType *gzgt);
 
 /** \} */
 

source/blender/nodes/intern/compositor_gizmos.cc
@@ -21,6 +21,7 @@
 #include "DNA_windowmanager_types.h"
 
 #include "ED_gizmo_library.hh"
+#include "ED_image.hh"
 
 #include "IMB_imbuf_types.hh"
 
@@ -96,6 +97,48 @@ static bool node_gizmo_is_set_visible(const SpaceNode &snode)
   return false;
 }
 
+static bool image_gizmo_is_set_visible(const SpaceImage &sima)
+{
+  if (!ELEM(sima.mode, SI_MODE_VIEW, SI_MODE_MASK)) {
+    return false;
+  }
+
+  if (sima.gizmo_flag & SI_GIZMO_HIDE_ACTIVE_NODE) {
+    return false;
+  }
+
+  Image *image = ED_space_image(&sima);
+  if (!(image && image->source == IMA_SRC_VIEWER && image->type == IMA_TYPE_COMPOSITE)) {
+    return false;
+  }
+
+  return true;
+}
+
+static SpaceNode *find_active_node_editor(const bContext *C)
+{
+  wmWindowManager *window_manager = CTX_wm_manager(C);
+
+  for (wmWindow &window : window_manager->windows) {
+    bScreen *screen = WM_window_get_active_screen(&window);
+    for (ScrArea &area : screen->areabase) {
+      SpaceLink *space_link = static_cast<SpaceLink *>(area.spacedata.first);
+      if (!space_link || space_link->spacetype != SPACE_NODE) {
+        continue;
+      }
+      SpaceNode *snode = reinterpret_cast<SpaceNode *>(space_link);
+      if (snode->edittree && snode->edittree->type == NTREE_COMPOSIT) {
+        bNodeTreePath *path = static_cast<bNodeTreePath *>(snode->treepath.last);
+        if (snode->nodetree->active_viewer_key == path->parent_key) {
+          return snode;
+        }
+      }
+    }
+  }
+
+  return nullptr;
+}
+
 /** \} */
 
 struct NodeBBoxWidgetGroup {
@@ -149,6 +192,25 @@ bool box_mask_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/)
   return show_box_mask_gizmo(*snode);
 }
 
+bool box_mask_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
+    return false;
+  }
+
+  return show_box_mask_gizmo(*snode);
+}
+
 void box_mask_setup(const bContext * /*C*/, wmGizmoGroup *gzgroup)
 {
   NodeBBoxWidgetGroup *mask_group = MEM_new<NodeBBoxWidgetGroup>(__func__);
@@ -179,6 +241,17 @@ void bbox_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup)
 
   node_gizmo_calc_matrix_space(
       region, snode->zoom, float2{-snode->xof, -snode->yof}, gz->matrix_space);
+}
+
+void bbox_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup)
+{
+  ARegion *region = CTX_wm_region(C);
+  wmGizmo *gz = static_cast<wmGizmo *>(gzgroup->gizmos.first);
+
+  SpaceImage *sima = CTX_wm_space_image(C);
+  const float2 offset = float2{sima->xof, sima->yof} * sima->zoom;
+
+  node_gizmo_calc_matrix_space(region, sima->zoom, offset, gz->matrix_space);
 }
 
 static void gizmo_node_box_mask_prop_matrix_get(const wmGizmo *gz,
@@ -323,7 +396,7 @@ void box_mask_refresh(const bContext *C, wmGizmoGroup *gzgroup)
   RNA_float_set_array(gz->ptr, "dimensions", mask_group->state.dims);
   WM_gizmo_set_flag(gz, WM_GIZMO_HIDDEN, false);
 
-  const SpaceNode *snode = CTX_wm_space_node(C);
+  const SpaceNode *snode = find_active_node_editor(C);
   BLI_assert(snode != nullptr);
 
   bNode *node = bke::node_get_active(*snode->edittree);
@@ -501,6 +574,25 @@ bool crop_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/)
   return show_crop_gizmo(*snode);
 }
 
+bool crop_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
+    return false;
+  }
+
+  return show_crop_gizmo(*snode);
+}
+
 void crop_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup)
 {
   ARegion *region = CTX_wm_region(C);
@@ -544,7 +636,7 @@ static void gizmo_node_crop_foreach_rna_prop(
 void crop_refresh(const bContext *C, wmGizmoGroup *gzgroup)
 {
   Main *bmain = CTX_data_main(C);
-  const SpaceNode *snode = CTX_wm_space_node(C);
+  const SpaceNode *snode = find_active_node_editor(C);
   BLI_assert(snode != nullptr);
 
   NodeBBoxWidgetGroup *crop_group = static_cast<NodeBBoxWidgetGroup *>(gzgroup->customdata);
@@ -657,6 +749,43 @@ bool glare_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/)
   return show_glare_gizmo(*snode);
 }
 
+void glare_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup)
+{
+
+  NodeGlareWidgetGroup *glare_group = static_cast<NodeGlareWidgetGroup *>(gzgroup->customdata);
+  ARegion *region = CTX_wm_region(C);
+  wmGizmo *gz = static_cast<wmGizmo *>(gzgroup->gizmos.first);
+
+  SpaceImage *sima = CTX_wm_space_image(C);
+  const float2 offset = float2{-sima->xof, -sima->yof} * sima->zoom;
+
+  node_gizmo_calc_matrix_space_with_image_dims(region,
+                                               sima->zoom,
+                                               offset,
+                                               glare_group->state.dims,
+                                               glare_group->state.offset,
+                                               gz->matrix_space);
+}
+
+bool glare_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
+    return false;
+  }
+
+  return show_glare_gizmo(*snode);
+}
+
 void glare_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup)
 {
 
@@ -708,7 +837,7 @@ void glare_refresh(const bContext *C, wmGizmoGroup *gzgroup)
   glare_group->state.offset = ibuf->flags & IB_has_display_window ? float2(ibuf->display_offset) :
                                                                     float2(0.0f);
 
-  SpaceNode *snode = CTX_wm_space_node(C);
+  SpaceNode *snode = find_active_node_editor(C);
   BLI_assert(snode != nullptr);
 
   bNode *node = bke::node_get_active(*snode->edittree);
@@ -757,6 +886,25 @@ bool corner_pin_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/)
   return show_corner_pin(*snode);
 }
 
+bool corner_pin_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
+    return false;
+  }
+
+  return show_corner_pin(*snode);
+}
+
 void corner_pin_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup)
 {
   NodeCornerPinWidgetGroup *cpin_group = static_cast<NodeCornerPinWidgetGroup *>(
@@ -776,6 +924,27 @@ void corner_pin_draw_prepare_space_node(const bContext *C, wmGizmoGroup *gzgroup
   for (int i = 0; i < 4; i++) {
     wmGizmo *gz = cpin_group->gizmos[i];
     copy_m4_m4(gz->matrix_space, matrix_space);
+  }
+}
+
+void corner_pin_draw_prepare_space_image(const bContext *C, wmGizmoGroup *gzgroup)
+{
+
+  ARegion *region = CTX_wm_region(C);
+  SpaceImage *sima = CTX_wm_space_image(C);
+
+  NodeCornerPinWidgetGroup *cpin_group = static_cast<NodeCornerPinWidgetGroup *>(
+      gzgroup->customdata);
+
+  const float2 offset = float2{-sima->xof, -sima->yof} * sima->zoom;
+
+  for (wmGizmo &gz : gzgroup->gizmos) {
+    node_gizmo_calc_matrix_space_with_image_dims(region,
+                                                 sima->zoom,
+                                                 offset,
+                                                 cpin_group->state.dims,
+                                                 cpin_group->state.offset,
+                                                 gz.matrix_space);
   }
 }
 
@@ -819,7 +988,7 @@ void corner_pin_refresh(const bContext *C, wmGizmoGroup *gzgroup)
   cpin_group->state.offset = ibuf->flags & IB_has_display_window ? float2(ibuf->display_offset) :
                                                                    float2(0.0f);
 
-  SpaceNode *snode = CTX_wm_space_node(C);
+  SpaceNode *snode = find_active_node_editor(C);
   BLI_assert(snode != nullptr);
 
   bNode *node = bke::node_get_active(*snode->edittree);
@@ -867,6 +1036,25 @@ bool ellipse_mask_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/
     return false;
   }
   if (!node_gizmo_is_set_visible(*snode)) {
+    return false;
+  }
+
+  return show_ellipse_mask_gizmo(*snode);
+}
+
+bool ellipse_mask_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
     return false;
   }
 
@@ -1019,7 +1207,7 @@ void split_refresh(const bContext *C, wmGizmoGroup *gzgroup)
   RNA_float_set_array(gz->ptr, "dimensions", split_group->state.dims);
   WM_gizmo_set_flag(gz, WM_GIZMO_HIDDEN, false);
 
-  SpaceNode *snode = CTX_wm_space_node(C);
+  SpaceNode *snode = find_active_node_editor(C);
   BLI_assert(snode != nullptr);
 
   bNode *node = bke::node_get_active(*snode->edittree);
@@ -1049,6 +1237,25 @@ bool split_poll_space_node(const bContext *C, wmGizmoGroupType * /*gzgt*/)
     return false;
   }
   if (!node_gizmo_is_set_visible(*snode)) {
+    return false;
+  }
+
+  return show_split(*snode);
+}
+
+bool split_poll_space_image(const bContext *C, wmGizmoGroupType * /*gzgt*/)
+{
+  const SpaceImage *sima = CTX_wm_space_image(C);
+  if (sima == nullptr) {
+    return false;
+  }
+
+  if (!image_gizmo_is_set_visible(*sima)) {
+    return false;
+  }
+
+  const SpaceNode *snode = find_active_node_editor(C);
+  if (snode == nullptr || snode->edittree == nullptr) {
     return false;
   }
 

FrenkyBojler/blender-ios

commit a32b5c7e6274f441dc187bf86c8c79b6cb044542

Fix: Incorrect gizmo names for compositor nodes in image editor
The corner pin and split node gizmos in the image editor had incorrect
names copied from other nodes.

This issue was introduced in c80f7a29a70.

source/blender/editors/space_image/space_image.cc
@@ -582,7 +582,7 @@ static void IMAGE_GGT_compositor_glare(wmGizmoGroupType *gzgt)
 
 static void IMAGE_GGT_compositor_corner_pin(wmGizmoGroupType *gzgt)
 {
-  gzgt->name = "Glare Node Widget";
+  gzgt->name = "Corner Pin Node Widget";
   gzgt->idname = "IMAGE_GGT_compositor_corner_pin";
 
   gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;
@@ -610,7 +610,7 @@ static void IMAGE_GGT_compositor_ellipse_mask(wmGizmoGroupType *gzgt)
 
 static void IMAGE_GGT_compositor_split(wmGizmoGroupType *gzgt)
 {
-  gzgt->name = "Ellipse Mask Node Widget";
+  gzgt->name = "Split Node Widget";
   gzgt->idname = "IMAGE_GGT_compositor_split";
 
   gzgt->flag |= WM_GIZMOGROUPTYPE_PERSISTENT | WM_GIZMOGROUPTYPE_DRAW_MODAL_ALL;