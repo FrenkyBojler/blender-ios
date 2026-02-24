blender/blender

commit a958e3d6b7c5b1a7ad20c1b276c9e404f79c0650

Refactor: Mesh: Use strings to store active UV map
For #122398, we need to move the storage of whether a UV map is active
or default out of CustomData. For colors attributes, and in the 5.0+
file format, we do that by storing strings referencing the names of
those attributes sepoarately on the mesh. This commit switches the way
the status is stored at runtime. Most of the work has happened already
as part of #122398, this just changes the last few places and simplifies
propagation of the active status where now possible.

For BMesh, the active/default status still uses the existing CustomData
system; the change would just be far too large if I started changing
that too: all the places that use `BM_uv_map_offsets_get` would have to
change.

Like for color attributes, this change makes setting the active/default
UV map a more explicit process.

Pull Request: https://projects.blender.org/blender/blender/pulls/150112

source/blender/bmesh/intern/bmesh_mesh_convert.cc
@@ -85,6 +85,7 @@
 #include "BLI_listbase.h"
 #include "BLI_math_vector.h"
 #include "BLI_span.hh"
+#include "BLI_string.h"
 #include "BLI_string_ref.hh"
 #include "BLI_task.hh"
 #include "BLI_vector.hh"
@@ -301,6 +302,17 @@ void BM_mesh_bm_from_me(BMesh *bm, const Mesh *mesh, const BMeshFromMeshParams *
         &mesh_pdata, &bm->pdata, mask.pmask, CD_SET_DEFAULT, bm, BM_FACE);
     CustomData_bmesh_merge_layout(
         &mesh_ldata, &bm->ldata, mask.lmask, CD_SET_DEFAULT, bm, BM_LOOP);
+  }
+
+  {
+    const StringRef name = mesh->active_uv_map_name();
+    const int index = CustomData_get_named_layer_index(&bm->ldata, CD_PROP_FLOAT2, name);
+    CustomData_set_layer_active_index(&bm->ldata, CD_PROP_FLOAT2, std::max(index, 0));
+  }
+  {
+    const StringRef name = mesh->default_uv_map_name();
+    const int index = CustomData_get_named_layer_index(&bm->ldata, CD_PROP_FLOAT2, name);
+    CustomData_set_layer_render_index(&bm->ldata, CD_PROP_FLOAT2, std::max(index, 0));
   }
 
   /* -------------------------------------------------------------------- */
@@ -1487,6 +1499,15 @@ void BM_mesh_bm_to_me(Main *bmain, BMesh *bm, Mesh *mesh, const BMeshToMeshParam
         &bm->pdata, &mesh->face_data, mask.pmask, CD_CONSTRUCT, mesh->faces_num);
   }
 
+  if (const char *name = CustomData_get_active_layer_name(&bm->ldata, CD_PROP_FLOAT2)) {
+    MEM_SAFE_FREE(mesh->active_uv_map_attribute);
+    mesh->active_uv_map_attribute = BLI_strdup(name);
+  }
+  if (const char *name = CustomData_get_render_layer_name(&bm->ldata, CD_PROP_FLOAT2)) {
+    MEM_SAFE_FREE(mesh->default_uv_map_attribute);
+    mesh->default_uv_map_attribute = BLI_strdup(name);
+  }
+
   /* Add optional mesh attributes before parallel iteration. */
   assert_bmesh_has_no_mesh_only_attributes(*bm);
   bke::MutableAttributeAccessor attrs = mesh->attributes_for_write();
@@ -1728,6 +1749,23 @@ void BM_mesh_bm_to_me_compact(BMesh &bm,
     CustomData_merge_layout(
         &bm.ldata, &mesh.corner_data, mask->lmask, CD_CONSTRUCT, mesh.corners_num);
     CustomData_merge_layout(&bm.pdata, &mesh.face_data, mask->pmask, CD_CONSTRUCT, mesh.faces_num);
+  }
+
+  {
+    const StringRef name = mesh.active_uv_map_name();
+    int index = CustomData_get_named_layer_index(&bm.ldata, CD_PROP_FLOAT2, name);
+    if (index == -1) {
+      index = CustomData_get_layer_index(&bm.ldata, CD_PROP_FLOAT2);
+    }
+    CustomData_set_layer_active_index(&bm.ldata, CD_PROP_FLOAT2, index);
+  }
+  {
+    const StringRef name = mesh.default_uv_map_name();
+    int index = CustomData_get_named_layer_index(&bm.ldata, CD_PROP_FLOAT2, name);
+    if (index == -1) {
+      index = CustomData_get_layer_index(&bm.ldata, CD_PROP_FLOAT2);
+    }
+    CustomData_set_layer_render_index(&bm.ldata, CD_PROP_FLOAT2, index);
   }
 
   /* Add optional mesh attributes before parallel iteration. */

source/blender/blenkernel/BKE_blender_version.h
@@ -28,7 +28,7 @@
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 6
+#define BLENDER_FILE_SUBVERSION 7
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_mesh.hh
@@ -444,6 +444,10 @@ void mesh_ensure_default_color_attribute_on_add(Mesh &mesh,
                                                 StringRef id,
                                                 AttrDomain domain,
                                                 bke::AttrType data_type);
+void mesh_ensure_default_uv_attribute_on_add(Mesh &mesh,
+                                             StringRef id,
+                                             AttrDomain domain,
+                                             bke::AttrType data_type);
 
 void mesh_data_update(Depsgraph &depsgraph,
                       const Scene &scene,

source/blender/blenkernel/intern/attribute.cc
@@ -441,6 +441,23 @@ static StringRef color_name_from_index(AttributeOwner &owner, int index)
       .value_or("");
 }
 
+static int uv_name_to_index(AttributeOwner &owner, const StringRef name)
+{
+  return BKE_attribute_to_index(owner, name, ATTR_DOMAIN_MASK_CORNER, CD_MASK_PROP_FLOAT2);
+}
+
+static int uv_clamp_index(AttributeOwner &owner, int index)
+{
+  const int length = BKE_attributes_length(owner, ATTR_DOMAIN_MASK_CORNER, CD_MASK_PROP_FLOAT2);
+  return min_ii(index, length - 1);
+}
+
+static StringRef uv_name_from_index(AttributeOwner &owner, int index)
+{
+  return BKE_attribute_from_index(owner, index, ATTR_DOMAIN_MASK_CORNER, CD_MASK_PROP_FLOAT2)
+      .value_or("");
+}
+
 bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportList *reports)
 {
   using namespace blender;
@@ -467,6 +484,8 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
           }
 
           const eCustomDataType type = eCustomDataType(data->layers[layer_index].type);
+          const bool is_active_uv_attribute = name_copy == mesh->active_uv_map_name();
+          const bool is_default_uv_attribute = name_copy == mesh->default_uv_map_name();
           const bool is_active_color_attribute = name_copy.c_str() ==
                                                  StringRef(mesh->active_color_attribute);
           const bool is_default_color_attribute = name_copy.c_str() ==
@@ -474,6 +493,8 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
           const int active_color_index = color_name_to_index(owner, mesh->active_color_attribute);
           const int default_color_index = color_name_to_index(owner,
                                                               mesh->default_color_attribute);
+          const int active_uv_index = uv_name_to_index(owner, mesh->active_uv_map_name());
+          const int default_uv_index = uv_name_to_index(owner, mesh->default_uv_map_name());
 
           if (!BM_data_layer_free_named(em->bm, data, name_copy.c_str())) {
             BLI_assert_unreachable();
@@ -488,6 +509,14 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
             BKE_id_attributes_default_color_set(
                 &mesh->id,
                 color_name_from_index(owner, color_clamp_index(owner, default_color_index)));
+          }
+          if (is_active_uv_attribute) {
+            mesh->uv_maps_active_set(
+                uv_name_from_index(owner, uv_clamp_index(owner, active_uv_index)));
+          }
+          if (is_default_uv_attribute) {
+            mesh->uv_maps_default_set(
+                uv_name_from_index(owner, uv_clamp_index(owner, default_uv_index)));
           }
 
           if (type == CD_PROP_FLOAT2 && domain == int(AttrDomain::Corner)) {
@@ -517,8 +546,12 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
     Mesh *mesh = owner.get_mesh();
     const bool is_active_color_attribute = name_copy == StringRef(mesh->active_color_attribute);
     const bool is_default_color_attribute = name_copy == StringRef(mesh->default_color_attribute);
+    const bool is_active_uv_attribute = name_copy == mesh->active_uv_map_name();
+    const bool is_default_uv_attribute = name_copy == mesh->default_uv_map_name();
     const int active_color_index = color_name_to_index(owner, mesh->active_color_attribute);
     const int default_color_index = color_name_to_index(owner, mesh->default_color_attribute);
+    const int active_uv_index = uv_name_to_index(owner, mesh->active_uv_map_name());
+    const int default_uv_index = uv_name_to_index(owner, mesh->default_uv_map_name());
 
     if (!attributes->remove(name_copy)) {
       BLI_assert_unreachable();
@@ -531,6 +564,13 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
     if (is_default_color_attribute) {
       BKE_id_attributes_default_color_set(
           &mesh->id, color_name_from_index(owner, color_clamp_index(owner, default_color_index)));
+    }
+    if (is_active_uv_attribute) {
+      mesh->uv_maps_active_set(uv_name_from_index(owner, uv_clamp_index(owner, active_uv_index)));
+    }
+    if (is_default_uv_attribute) {
+      mesh->uv_maps_default_set(
+          uv_name_from_index(owner, uv_clamp_index(owner, default_uv_index)));
     }
 
     if (bke::mesh::is_uv_map(metadata)) {

source/blender/blenkernel/intern/attribute_legacy_convert.cc
@@ -259,22 +259,6 @@ void mesh_convert_storage_to_customdata(Mesh &mesh)
                                  {AttrDomain::Edge, {mesh.edge_data, mesh.edges_num}},
                                  {AttrDomain::Face, {mesh.face_data, mesh.faces_num}},
                                  {AttrDomain::Corner, {mesh.corner_data, mesh.corners_num}}});
-  if (const char *name = mesh.active_uv_map_attribute) {
-    const int layer_n = CustomData_get_named_layer(&mesh.corner_data, CD_PROP_FLOAT2, name);
-    if (layer_n != -1) {
-      CustomData_set_layer_active(&mesh.corner_data, CD_PROP_FLOAT2, layer_n);
-    }
-    MEM_freeN(mesh.active_uv_map_attribute);
-    mesh.active_uv_map_attribute = nullptr;
-  }
-  if (const char *name = mesh.default_uv_map_attribute) {
-    const int layer_n = CustomData_get_named_layer(&mesh.corner_data, CD_PROP_FLOAT2, name);
-    if (layer_n != -1) {
-      CustomData_set_layer_render(&mesh.corner_data, CD_PROP_FLOAT2, layer_n);
-    }
-    MEM_freeN(mesh.default_uv_map_attribute);
-    mesh.default_uv_map_attribute = nullptr;
-  }
 }
 void mesh_convert_customdata_to_storage(Mesh &mesh)
 {

source/blender/blenkernel/intern/data_transfer.cc
@@ -317,6 +317,66 @@ static void transfer_default_color_string(Mesh *mesh_dst, const Mesh *mesh_src)
   }
 }
 
+static void transfer_active_uv_map_string(Mesh *mesh_dst, const Mesh *mesh_src)
+{
+  using namespace blender;
+  const StringRef name = mesh_src->active_uv_map_attribute;
+  if (!name.is_empty()) {
+    return;
+  }
+  const bke::AttributeAccessor attributes_src = mesh_src->attributes();
+  const bke::AttributeAccessor attributes_dst = mesh_dst->attributes();
+
+  if (!bke::mesh::is_uv_map(attributes_src.lookup_meta_data(name))) {
+    return;
+  }
+
+  if (bke::mesh::is_uv_map(attributes_dst.lookup_meta_data(name))) {
+    mesh_dst->uv_maps_active_set(name);
+  }
+  else {
+    mesh_dst->attributes().foreach_attribute([&](const bke::AttributeIter &iter) {
+      if (!mesh_dst->active_uv_map_name().is_empty()) {
+        return;
+      }
+      if (!bke::mesh::is_uv_map({iter.domain, iter.data_type})) {
+        return;
+      }
+      mesh_dst->uv_maps_active_set(iter.name);
+    });
+  }
+}
+
+static void transfer_default_uv_map_string(Mesh *mesh_dst, const Mesh *mesh_src)
+{
+  using namespace blender;
+  const StringRef name = mesh_src->default_uv_map_attribute;
+  if (!name.is_empty()) {
+    return;
+  }
+  const bke::AttributeAccessor attributes_src = mesh_src->attributes();
+  const bke::AttributeAccessor attributes_dst = mesh_dst->attributes();
+
+  if (!bke::mesh::is_uv_map(attributes_src.lookup_meta_data(name))) {
+    return;
+  }
+
+  if (bke::mesh::is_uv_map(attributes_dst.lookup_meta_data(name))) {
+    mesh_dst->uv_maps_default_set(name);
+  }
+  else {
+    mesh_dst->attributes().foreach_attribute([&](const bke::AttributeIter &iter) {
+      if (!mesh_dst->default_uv_map_name().is_empty()) {
+        return;
+      }
+      if (!bke::mesh::is_uv_map({iter.domain, iter.data_type})) {
+        return;
+      }
+      mesh_dst->uv_maps_default_set(iter.name);
+    });
+  }
+}
+
 /* ********** */
 
 static void data_transfer_dtdata_type_postprocess(Mesh *me_dst,
@@ -1302,6 +1362,10 @@ void BKE_object_data_transfer_layout(Depsgraph *depsgraph,
       if (ELEM(cddata_type, CD_PROP_COLOR, CD_PROP_BYTE_COLOR)) {
         transfer_active_color_string(me_dst, me_src);
         transfer_default_color_string(me_dst, me_src);
+      }
+      else if (ELEM(cddata_type, CD_PROP_FLOAT2)) {
+        transfer_active_uv_map_string(me_dst, me_src);
+        transfer_default_uv_map_string(me_dst, me_src);
       }
     }
     if (DT_DATATYPE_IS_FACE(dtdata_type)) {

source/blender/blenkernel/intern/mesh.cc
@@ -336,23 +336,6 @@ static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address
   mesh->totface_legacy = 0;
   mesh->fdata_legacy = CustomData{};
 
-  /* Convert from the format still used at runtime (flags on #CustomDataLayer) to the format
-   * reserved for future runtime use (names stored on #Mesh). */
-  if (const char *name = CustomData_get_active_layer_name(&mesh->corner_data, CD_PROP_FLOAT2)) {
-    mesh->active_uv_map_attribute = const_cast<char *>(
-        scope.allocator().copy_string(name).c_str());
-  }
-  else {
-    mesh->active_uv_map_attribute = nullptr;
-  }
-  if (const char *name = CustomData_get_render_layer_name(&mesh->corner_data, CD_PROP_FLOAT2)) {
-    mesh->default_uv_map_attribute = const_cast<char *>(
-        scope.allocator().copy_string(name).c_str());
-  }
-  else {
-    mesh->default_uv_map_attribute = nullptr;
-  }
-
   /* Do not store actual geometry data in case this is a library override ID. */
   if (ID_IS_OVERRIDE_LIBRARY(mesh) && !is_undo) {
     mesh->verts_num = 0;
@@ -594,6 +577,23 @@ void mesh_ensure_default_color_attribute_on_add(Mesh &mesh,
   mesh.default_color_attribute = BLI_strdupn(id.data(), id.size());
 }
 
+void mesh_ensure_default_uv_attribute_on_add(Mesh &mesh,
+                                             const StringRef id,
+                                             AttrDomain domain,
+                                             bke::AttrType data_type)
+{
+  if (bke::attribute_name_is_anonymous(id)) {
+    return;
+  }
+  if (!mesh::is_uv_map({domain, data_type})) {
+    return;
+  }
+  if (!mesh.default_uv_map_name().is_empty()) {
+    return;
+  }
+  mesh.uv_maps_default_set(id);
+}
+
 void mesh_ensure_required_data_layers(Mesh &mesh)
 {
   MutableAttributeAccessor attributes = mesh.attributes_for_write();
@@ -614,6 +614,12 @@ void mesh_remove_invalid_attribute_strings(Mesh &mesh)
   }
   if (!mesh::is_color_attribute(attributes.lookup_meta_data(mesh.default_color_attribute))) {
     MEM_SAFE_FREE(mesh.default_color_attribute);
+  }
+  if (!mesh::is_uv_map(attributes.lookup_meta_data(mesh.active_uv_map_name()))) {
+    MEM_SAFE_FREE(mesh.active_uv_map_attribute);
+  }
+  if (!mesh::is_uv_map(attributes.lookup_meta_data(mesh.default_uv_map_name()))) {
+    MEM_SAFE_FREE(mesh.default_uv_map_attribute);
   }
 }
 
@@ -1024,6 +1030,8 @@ static void clear_attribute_names(Mesh &mesh)
   BLI_freelistN(&mesh.vertex_group_names);
   MEM_SAFE_FREE(mesh.active_color_attribute);
   MEM_SAFE_FREE(mesh.default_color_attribute);
+  MEM_SAFE_FREE(mesh.active_uv_map_attribute);
+  MEM_SAFE_FREE(mesh.default_uv_map_attribute);
 }
 
 void BKE_mesh_clear_geometry(Mesh *mesh)
@@ -1204,30 +1212,50 @@ blender::VectorSet<blender::StringRefNull> Mesh::uv_map_names() const
 
 blender::StringRefNull Mesh::active_uv_map_name() const
 {
-  /* Currently this information is stored in CustomData. Once it switches to using
-   * #Mesh::active_uv_map_attribute, this logic can be removed. This function's only purpose is to
-   * ease that transition. */
-  if (this->runtime->edit_mesh) {
-    const char *name = CustomData_get_active_layer_name(&this->runtime->edit_mesh->bm->ldata,
-                                                        CD_PROP_FLOAT2);
+  if (BMEditMesh *em = this->runtime->edit_mesh.get()) {
+    const char *name = CustomData_get_active_layer_name(&em->bm->ldata, CD_PROP_FLOAT2);
     return name ? name : "";
   }
-  const char *name = CustomData_get_active_layer_name(&this->corner_data, CD_PROP_FLOAT2);
-  return name ? name : "";
+  return this->active_uv_map_attribute ? this->active_uv_map_attribute : "";
 }
 
 blender::StringRefNull Mesh::default_uv_map_name() const
 {
-  /* Currently this information is stored in CustomData. Once it switches to using
-   * #Mesh::default_uv_map_attribute, this logic can be removed. This function's only purpose is to
-   * ease that transition. */
-  if (this->runtime->edit_mesh) {
-    const char *name = CustomData_get_render_layer_name(&this->runtime->edit_mesh->bm->ldata,
-                                                        CD_PROP_FLOAT2);
+  if (BMEditMesh *em = this->runtime->edit_mesh.get()) {
+    const char *name = CustomData_get_render_layer_name(&em->bm->ldata, CD_PROP_FLOAT2);
     return name ? name : "";
   }
-  const char *name = CustomData_get_render_layer_name(&this->corner_data, CD_PROP_FLOAT2);
-  return name ? name : "";
+  return this->default_uv_map_attribute ? this->default_uv_map_attribute : "";
+}
+
+void Mesh::uv_maps_active_set(const StringRef name)
+{
+  MEM_SAFE_FREE(this->active_uv_map_attribute);
+  if (!name.is_empty()) {
+    this->active_uv_map_attribute = BLI_strdupn(name.data(), name.size());
+  }
+  if (BMEditMesh *em = this->runtime->edit_mesh.get()) {
+    int index = CustomData_get_named_layer_index(&em->bm->ldata, CD_PROP_FLOAT2, name);
+    if (index == -1) {
+      index = CustomData_get_layer_index(&em->bm->ldata, CD_PROP_FLOAT2);
+    }
+    CustomData_set_layer_active_index(&em->bm->ldata, CD_PROP_FLOAT2, index);
+  }
+}
+
+void Mesh::uv_maps_default_set(const StringRef name)
+{
+  MEM_SAFE_FREE(this->default_uv_map_attribute);
+  if (!name.is_empty()) {
+    this->default_uv_map_attribute = BLI_strdupn(name.data(), name.size());
+  }
+  if (BMEditMesh *em = this->runtime->edit_mesh.get()) {
+    int index = CustomData_get_named_layer_index(&em->bm->ldata, CD_PROP_FLOAT2, name);
+    if (index == -1) {
+      index = CustomData_get_layer_index(&em->bm->ldata, CD_PROP_FLOAT2);
+    }
+    CustomData_set_layer_render_index(&em->bm->ldata, CD_PROP_FLOAT2, index);
+  }
 }
 
 Mesh *BKE_mesh_new_nomain(const int verts_num,
@@ -1308,6 +1336,14 @@ static void copy_attribute_names(const Mesh &mesh_src, Mesh &mesh_dst)
   if (mesh_src.default_color_attribute) {
     MEM_SAFE_FREE(mesh_dst.default_color_attribute);
     mesh_dst.default_color_attribute = BLI_strdup(mesh_src.default_color_attribute);
+  }
+  if (mesh_src.active_uv_map_attribute) {
+    MEM_SAFE_FREE(mesh_dst.active_uv_map_attribute);
+    mesh_dst.active_uv_map_attribute = BLI_strdup(mesh_src.active_uv_map_attribute);
+  }
+  if (mesh_src.default_uv_map_attribute) {
+    MEM_SAFE_FREE(mesh_dst.default_uv_map_attribute);
+    mesh_dst.default_uv_map_attribute = BLI_strdup(mesh_src.default_uv_map_attribute);
   }
 }
 

source/blender/blenkernel/intern/mesh_convert.cc
@@ -123,8 +123,11 @@ static Mesh *mesh_nurbs_displist_to_mesh(const Curve *cu, const ListBase *dispba
       "material_index", AttrDomain::Face);
   SpanAttributeWriter<bool> sharp_faces = attributes.lookup_or_add_for_write_span<bool>(
       "sharp_face", AttrDomain::Face);
+  const StringRef uv_name = DATA_("UVMap");
   SpanAttributeWriter<float2> uv_attribute = attributes.lookup_or_add_for_write_span<float2>(
-      DATA_("UVMap"), AttrDomain::Corner);
+      uv_name, AttrDomain::Corner);
+  mesh->uv_maps_active_set(uv_name);
+  mesh->uv_maps_default_set(uv_name);
   MutableSpan<float2> uv_map = uv_attribute.span;
 
   int dst_vert = 0;
@@ -1146,6 +1149,8 @@ void BKE_mesh_nomain_to_mesh(Mesh *mesh_src, Mesh *mesh_dst, Object *ob, bool pr
   /* Make sure attribute names are moved. */
   std::swap(mesh_dst->active_color_attribute, mesh_src->active_color_attribute);
   std::swap(mesh_dst->default_color_attribute, mesh_src->default_color_attribute);
+  std::swap(mesh_dst->active_uv_map_attribute, mesh_src->active_uv_map_attribute);
+  std::swap(mesh_dst->default_uv_map_attribute, mesh_src->default_uv_map_attribute);
   std::swap(mesh_dst->vertex_group_names, mesh_src->vertex_group_names);
 
   BKE_mesh_copy_parameters(mesh_dst, mesh_src);

FrenkyBojler/blender-ios

commit 637f267b5e7890bb32e8dfec87a96933056dc2ab

Fix #154767: Active UV layer resets / non-changable in edit mode
Caused by a958e3d6b7

With certain modifier setups in editmode, we enter a codepath that uses
`BM_mesh_bm_to_me_compact`.
This function uses a bmesh as reference to create a Mesh from an empty
Mesh.
However, it set the active/default uv attributes on BMesh (this already
has it since it acts as the reference) instead of the Mesh. This is now
corrected (so it now actually does it similar to `BM_mesh_bm_to_me`).

source/blender/bmesh/intern/bmesh_mesh_convert.cc
@@ -2008,21 +2008,14 @@ void BM_mesh_bm_to_me_compact(BMesh &bm,
     add_bm_cd_to_mesh(bm, bke::AttrDomain::Corner, mask_final.lmask, mesh);
   }
 
-  {
-    const StringRef name = mesh.active_uv_map_name();
-    int index = CustomData_get_named_layer_index(&bm.ldata, CD_PROP_FLOAT2, name);
-    if (index == -1) {
-      index = CustomData_get_layer_index(&bm.ldata, CD_PROP_FLOAT2);
-    }
-    CustomData_set_layer_active_index(&bm.ldata, CD_PROP_FLOAT2, index);
+  if (const char *name = CustomData_get_active_layer_name(&bm.ldata, CD_PROP_FLOAT2)) {
+    MEM_SAFE_DELETE(mesh.active_uv_map_attribute);
+    mesh.active_uv_map_attribute = BLI_strdup(name);
   }
-  {
-    const StringRef name = mesh.default_uv_map_name();
-    int index = CustomData_get_named_layer_index(&bm.ldata, CD_PROP_FLOAT2, name);
-    if (index == -1) {
-      index = CustomData_get_layer_index(&bm.ldata, CD_PROP_FLOAT2);
-    }
-    CustomData_set_layer_render_index(&bm.ldata, CD_PROP_FLOAT2, index);
+
+  if (const char *name = CustomData_get_render_layer_name(&bm.ldata, CD_PROP_FLOAT2)) {
+    MEM_SAFE_DELETE(mesh.default_uv_map_attribute);
+    mesh.default_uv_map_attribute = BLI_strdup(name);
   }
 
   /* Add optional mesh attributes before parallel iteration. */