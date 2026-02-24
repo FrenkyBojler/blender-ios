500a395963965bd9c2b8f65ded5bf9a9a5e2fd79
Matej Kotas <matej.kotas@live.com>
2026-02-23 19:30:24 -0800

    Ply export: Add extra flushing
    This fixes exporting with dynamic topology and with the multiresolution modifier.

--- source/blender/io/ply/exporter/ply_export.cc
+++ source/blender/io/ply/exporter/ply_export.cc
@@ -7,7 +7,10 @@
  */
 
 #include "BKE_context.hh"
+#include "BKE_layer.hh"
 #include "BKE_lib_id.hh"
+#include "BKE_multires.hh"
+#include "BKE_paint.hh"
 #include "BKE_report.hh"
 #include "BKE_scene.hh"
 
@@ -38,6 +41,11 @@ void exporter_main(bContext *C, const PLYExportParams &export_params)
   Main *bmain = CTX_data_main(C);
   Scene *scene = CTX_data_scene(C);
   ViewLayer *view_layer = CTX_data_view_layer(C);
+
+  Object *ob = BKE_view_layer_active_object_get(view_layer);
+  multires_flush_sculpt_updates(ob);
+  BKE_sculptsession_bm_to_me_for_render(ob);
+
   Depsgraph *depsgraph = DEG_graph_new(bmain, scene, view_layer, DAG_EVAL_RENDER);
 
   if (export_params.collection[0]) {
