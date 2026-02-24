8530f7c443c1b957ab1636c866a86dba7394683d
Harley Acheson <harley.acheson@gmail.com>
2026-02-23 17:21:58 -0800

    Accidental testing change.

--- source/blender/windowmanager/intern/wm_window.cc
+++ source/blender/windowmanager/intern/wm_window.cc
@@ -476,9 +476,6 @@ static void save_window_bounds_settings(wmWindow *win)
 
   Settings settings("window.dimensions");
   settings[win->runtime->settings_key] = bounds;
-
-  settings.section = "";
-  settings["test"] = "Harley";
 }
 
 void wm_window_close(bContext *C, wmWindowManager *wm, wmWindow *win)
