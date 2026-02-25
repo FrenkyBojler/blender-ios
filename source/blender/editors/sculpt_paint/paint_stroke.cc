blender/blender

commit 29c9353429f4b60c6e4ad234a4d6392a1f513079

Sculpt: Extract stroke type enums from generic `Brush.flag` field
These properties are logically mutually exclusive. We do not support
choosing multiple stroke methods, a user is only able to select one.

To better reflect this in the code, this commit pulls converts usages of
the existing flags that use the generic `Brush.flag` field to a specific
equality check with the new `stroke_method` field.

In addition, a number of python properties related to this field have
been removed. Reading and modifying the field can now be done as usual
via assigning a string corresponding to the RNA enum value instead of
needing to access specific RNA properties.

Pull Request: https://projects.blender.org/blender/blender/pulls/153566

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -194,13 +194,13 @@ static bool paint_stroke_use_scene_spacing(const Brush &brush, const PaintMode m
 
 static bool paint_brush_type_raycast_original(const Brush &brush, PaintMode /*mode*/)
 {
-  return brush.flag & (BRUSH_ANCHORED | BRUSH_DRAG_DOT);
+  return ELEM(brush.stroke_method, BRUSH_STROKE_ANCHORED, BRUSH_STROKE_DRAG_DOT);
 }
 
 static bool paint_brush_type_require_inbetween_mouse_events(const Brush &brush,
                                                             const PaintMode mode)
 {
-  if (brush.flag & BRUSH_ANCHORED) {
+  if (brush.stroke_method == BRUSH_STROKE_ANCHORED) {
     return false;
   }
 
@@ -320,7 +320,7 @@ bool PaintStroke::update(bContext *C,
     }
   }
 
-  if (brush.flag & BRUSH_ANCHORED) {
+  if (brush.stroke_method == BRUSH_STROKE_ANCHORED) {
     bool hit = false;
     float2 halfway;
 
@@ -370,7 +370,7 @@ bool PaintStroke::update(bContext *C,
   }
   else {
     /* curve strokes do their own rake calculation */
-    if (!(brush.flag & BRUSH_CURVE)) {
+    if (brush.stroke_method != BRUSH_STROKE_CURVE) {
       if (!paint_calculate_rake_rotation(*paint, brush, mouse_init, mode, rake_started_)) {
         /* Not enough motion to define an angle. */
         if (!rake_started_) {
@@ -425,7 +425,7 @@ bool PaintStroke::update(bContext *C,
 static bool paint_stroke_use_dash(const Brush &brush)
 {
   /* Only these stroke modes support dash lines */
-  return brush.flag & BRUSH_SPACE || brush.flag & BRUSH_LINE || brush.flag & BRUSH_CURVE;
+  return ELEM(brush.stroke_method, BRUSH_STROKE_SPACE, BRUSH_STROKE_LINE, BRUSH_STROKE_CURVE);
 }
 
 static bool paint_stroke_use_jitter(const PaintMode mode, const Brush &brush, const bool invert)
@@ -435,7 +435,7 @@ static bool paint_stroke_use_jitter(const PaintMode mode, const Brush &brush, co
 
   /* jitter-ed brush gives weird and unpredictable result for this
    * kinds of stroke, so manually disable jitter usage (sergey) */
-  use_jitter &= (brush.flag & (BRUSH_DRAG_DOT | BRUSH_ANCHORED)) == 0;
+  use_jitter &= (ELEM(brush.stroke_method, BRUSH_STROKE_DRAG_DOT, BRUSH_STROKE_ANCHORED)) == 0;
   use_jitter &= !ELEM(mode, PaintMode::Texture2D, PaintMode::Texture3D) ||
                 !(invert && brush.image_brush_type == IMAGE_PAINT_BRUSH_TYPE_CLONE);
 
@@ -635,7 +635,7 @@ static float paint_space_stroke_spacing(const ViewContext &vc,
   float spacing = brush->spacing;
 
   /* apply spacing pressure */
-  if (brush->flag & BRUSH_SPACE && brush->flag & BRUSH_SPACING_PRESSURE) {
+  if (brush->stroke_method & BRUSH_STROKE_SPACE && brush->flag & BRUSH_SPACING_PRESSURE) {
     spacing = spacing * (1.5f - pressure);
   }
 
@@ -898,7 +898,7 @@ PaintStroke::PaintStroke(bContext *C, wmOperator *op, int event_type) : event_ty
   }
 
   if (stroke_mode_ == BrushStrokeMode::Invert) {
-    if (this->brush->flag & BRUSH_CURVE) {
+    if (this->brush->stroke_method == BRUSH_STROKE_CURVE) {
       RNA_enum_set(op->ptr, "mode", int(BrushStrokeMode::Normal));
     }
   }
@@ -985,7 +985,7 @@ static bool curves_sculpt_brush_uses_spacing(const eBrushCurvesSculptType tool)
 
 bool paint_space_stroke_enabled(const Brush &br, const PaintMode mode)
 {
-  if ((br.flag & BRUSH_SPACE) == 0) {
+  if (br.stroke_method != BRUSH_STROKE_SPACE) {
     return false;
   }
 
@@ -1030,7 +1030,7 @@ static bool sculpt_is_grab_tool(const Brush &br)
 
 bool paint_supports_dynamic_size(const Brush &br, const PaintMode mode)
 {
-  if (br.flag & BRUSH_ANCHORED) {
+  if (br.stroke_method == BRUSH_STROKE_ANCHORED) {
     return false;
   }
 
@@ -1065,7 +1065,7 @@ bool paint_supports_smooth_stroke(const Brush &brush,
     return true;
   }
   if (!(brush.flag & BRUSH_SMOOTH_STROKE) ||
-      (brush.flag & (BRUSH_ANCHORED | BRUSH_DRAG_DOT | BRUSH_LINE)))
+      ELEM(brush.stroke_method, BRUSH_STROKE_ANCHORED | BRUSH_STROKE_DRAG_DOT | BRUSH_STROKE_LINE))
   {
     return false;
   }
@@ -1091,7 +1091,7 @@ bool paint_supports_texture(const PaintMode mode)
 
 bool paint_supports_dynamic_tex_coords(const Brush &br, const PaintMode mode)
 {
-  if (br.flag & BRUSH_ANCHORED) {
+  if (br.stroke_method == BRUSH_STROKE_ANCHORED) {
     return false;
   }
 
@@ -1262,7 +1262,7 @@ void PaintStroke::line_end(bContext *C, wmOperator *op, const float2 mouse)
 {
   Brush *br = this->brush;
   bke::PaintRuntime *paint_runtime = this->paint->runtime;
-  if (stroke_started_ && (br->flag & BRUSH_LINE)) {
+  if (stroke_started_ && br->stroke_method == BRUSH_STROKE_LINE) {
     paint_runtime->overlap_factor = paint_stroke_integrate_overlap(*br, 1.0);
 
     this->add_step(C, op, this->last_mouse_position, 1.0);
@@ -1273,7 +1273,7 @@ void PaintStroke::line_end(bContext *C, wmOperator *op, const float2 mouse)
 bool PaintStroke::curve_end(bContext *C, wmOperator *op)
 {
   const Brush &br = *this->brush;
-  if (!(br.flag & BRUSH_CURVE)) {
+  if (br.stroke_method != BRUSH_STROKE_CURVE) {
     return false;
   }
 
@@ -1421,8 +1421,10 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
 
   /* see if tablet affects event. Line, anchored and drag dot strokes do not support pressure */
   const float tablet_pressure = WM_event_tablet_data(event, &pen_flip_, nullptr);
-  float pressure = ((br->flag & (BRUSH_LINE | BRUSH_ANCHORED | BRUSH_DRAG_DOT)) ? 1.0f :
-                                                                                  tablet_pressure);
+  float pressure =
+      ELEM(br->stroke_method, BRUSH_STROKE_LINE, BRUSH_STROKE_ANCHORED, BRUSH_STROKE_DRAG_DOT) ?
+          1.0f :
+          tablet_pressure;
 
   if (print_pressure_status_enabled() && WM_event_is_tablet(event)) {
     std::string msg = fmt::format("Tablet Pressure: {:.4f}", pressure);
@@ -1495,11 +1497,11 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
             SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_cursor_poll, paint_draw_smooth_cursor, this);
       }
 
-      if (br->flag & BRUSH_AIRBRUSH) {
+      if (br->stroke_method == BRUSH_STROKE_AIRBRUSH) {
         timer_ = WM_event_timer_add(CTX_wm_manager(C), CTX_wm_window(C), TIMER, this->brush->rate);
       }
 
-      if (br->flag & BRUSH_LINE) {
+      if (br->stroke_method == BRUSH_STROKE_LINE) {
         stroke_cursor_ = WM_paint_cursor_activate(
             SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_cursor_poll, paint_draw_line_cursor, this);
       }
@@ -1554,7 +1556,7 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
     this->stroke_done(C, op, false);
     return OPERATOR_FINISHED;
   }
-  else if (br->flag & BRUSH_LINE) {
+  else if (br->stroke_method == BRUSH_STROKE_LINE) {
     if (event->modifier & KM_ALT) {
       this->constrain_line = true;
     }
@@ -1576,9 +1578,10 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
   }
   else if (first_modal ||
            /* regular dabs */
-           (!(br->flag & BRUSH_AIRBRUSH) && ISMOUSE_MOTION(event->type)) ||
+           (!(br->stroke_method == BRUSH_STROKE_AIRBRUSH) && ISMOUSE_MOTION(event->type)) ||
            /* airbrush */
-           ((br->flag & BRUSH_AIRBRUSH) && event->type == TIMER && event->customdata == timer_))
+           ((br->stroke_method == BRUSH_STROKE_AIRBRUSH) && event->type == TIMER &&
+            event->customdata == timer_))
   {
     if (paint_smooth_stroke(*this->brush,
                             &sample_average,

scripts/startup/bl_ui/properties_paint_common.py
@@ -536,18 +536,18 @@ class StrokePanel(BrushPanel):
         col.prop(brush, "stroke_method")
         col.separator()
 
-        if brush.use_anchor:
+        if brush.stroke_method == 'ANCHORED':
             col.prop(brush, "use_edge_to_edge", text="Edge to Edge")
 
-        if brush.use_airbrush:
+        if brush.stroke_method == 'AIRBRUSH':
             col.prop(brush, "rate", text="Rate", slider=True)
 
-        if brush.use_space:
+        if brush.stroke_method == 'SPACE':
             row = col.row(align=True)
             row.prop(brush, "spacing", text="Spacing")
             row.prop(brush, "use_pressure_spacing", toggle=True, text="")
 
-        if brush.use_line or brush.use_curve:
+        if brush.stroke_method in {'LINE', 'CURVE'}:
             row = col.row(align=True)
             row.prop(brush, "spacing", text="Spacing")
 
@@ -558,13 +558,13 @@ class StrokePanel(BrushPanel):
             if brush.image_paint_capabilities.has_space_attenuation or brush.sculpt_capabilities.has_space_attenuation:
                 col.prop(brush, "use_space_attenuation")
 
-        if brush.use_curve:
+        if brush.stroke_method == 'CURVE':
             col.separator()
             col.template_ID(brush, "paint_curve", new="paintcurve.new")
             col.operator("paintcurve.draw")
             col.separator()
 
-        if brush.use_space or brush.use_line or brush.use_curve:
+        if brush.stroke_method in {'SPACE', 'LINE', 'CURVE'}:
             col.separator()
             row = col.row(align=True)
             col.prop(brush, "dash_ratio", text="Dash Ratio")

source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ namespace blender {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 22
+#define BLENDER_FILE_SUBVERSION 23
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/intern/brush.cc
@@ -812,14 +812,11 @@ void BKE_brush_debug_print_state(Brush *br)
   BR_TEST(size, d);
 
   /* br->flag */
-  BR_TEST_FLAG(BRUSH_AIRBRUSH);
   BR_TEST_FLAG(BRUSH_ALPHA_PRESSURE);
   BR_TEST_FLAG(BRUSH_SIZE_PRESSURE);
   BR_TEST_FLAG(BRUSH_JITTER_PRESSURE);
   BR_TEST_FLAG(BRUSH_SPACING_PRESSURE);
-  BR_TEST_FLAG(BRUSH_ANCHORED);
   BR_TEST_FLAG(BRUSH_DIR_IN);
-  BR_TEST_FLAG(BRUSH_SPACE);
   BR_TEST_FLAG(BRUSH_SMOOTH_STROKE);
   BR_TEST_FLAG(BRUSH_PERSISTENT);
   BR_TEST_FLAG(BRUSH_ACCUMULATE);
@@ -830,7 +827,6 @@ void BKE_brush_debug_print_state(Brush *br)
   BR_TEST_FLAG(BRUSH_ADAPTIVE_SPACE);
   BR_TEST_FLAG(BRUSH_LOCK_SIZE);
   BR_TEST_FLAG(BRUSH_EDGE_TO_EDGE);
-  BR_TEST_FLAG(BRUSH_DRAG_DOT);
   BR_TEST_FLAG(BRUSH_INVERSE_SMOOTH_PRESSURE);
   BR_TEST_FLAG(BRUSH_PLANE_TRIM);
   BR_TEST_FLAG(BRUSH_FRONTFACE);
@@ -1817,7 +1813,7 @@ bool supports_plane_depth(const Brush &brush)
 }
 bool supports_jitter(const Brush &brush)
 {
-  return !(brush.flag & BRUSH_ANCHORED) && !(brush.flag & BRUSH_DRAG_DOT) &&
+  return !(ELEM(brush.stroke_method, BRUSH_STROKE_ANCHORED, BRUSH_STROKE_DRAG_DOT)) &&
          !ELEM(brush.sculpt_brush_type,
                SCULPT_BRUSH_TYPE_GRAB,
                SCULPT_BRUSH_TYPE_ROTATE,
@@ -1892,8 +1888,11 @@ bool supports_secondary_cursor_color(const Brush &brush)
 }
 bool supports_smooth_stroke(const Brush &brush)
 {
-  return !(brush.flag & BRUSH_ANCHORED) && !(brush.flag & BRUSH_DRAG_DOT) &&
-         !(brush.flag & BRUSH_LINE) && !(brush.flag & BRUSH_CURVE) &&
+  return !(ELEM(brush.stroke_method,
+                BRUSH_STROKE_ANCHORED,
+                BRUSH_STROKE_DRAG_DOT,
+                BRUSH_STROKE_LINE,
+                BRUSH_STROKE_CURVE)) &&
          !ELEM(brush.sculpt_brush_type,
                SCULPT_BRUSH_TYPE_GRAB,
                SCULPT_BRUSH_TYPE_ROTATE,
@@ -1902,7 +1901,7 @@ bool supports_smooth_stroke(const Brush &brush)
 }
 bool supports_space_attenuation(const Brush &brush)
 {
-  return brush.flag & (BRUSH_SPACE | BRUSH_LINE | BRUSH_CURVE) &&
+  return ELEM(brush.stroke_method, BRUSH_STROKE_SPACE, BRUSH_STROKE_LINE, BRUSH_STROKE_CURVE) &&
          !ELEM(brush.sculpt_brush_type,
                SCULPT_BRUSH_TYPE_GRAB,
                SCULPT_BRUSH_TYPE_ROTATE,

source/blender/blenloader/intern/versioning_510.cc
@@ -9,6 +9,8 @@
 #define DNA_DEPRECATED_ALLOW
 
 #include "DNA_ID.h"
+#include "DNA_brush_enums.h"
+#include "DNA_brush_types.h"
 #include "DNA_light_types.h"
 #include "DNA_material_types.h"
 #include "DNA_mesh_types.h"
@@ -566,6 +568,37 @@ static void free_compositor_forward_compatibility_storage(bNode &node)
   node.storage = nullptr;
 }
 
+static void convert_brush_flags_to_type(Brush &brush)
+{
+  if (brush.flag & BRUSH_UNUSED_1) {
+    brush.flag &= ~BRUSH_UNUSED_1;
+    brush.stroke_method = BRUSH_STROKE_AIRBRUSH;
+  }
+  else if (brush.flag & BRUSH_UNUSED_2) {
+    brush.flag &= ~BRUSH_UNUSED_2;
+    brush.stroke_method = BRUSH_STROKE_ANCHORED;
+  }
+  else if (brush.flag & BRUSH_UNUSED_3) {
+    brush.flag &= ~BRUSH_UNUSED_3;
+    brush.stroke_method = BRUSH_STROKE_SPACE;
+  }
+  else if (brush.flag & BRUSH_UNUSED_4) {
+    brush.flag &= ~BRUSH_UNUSED_4;
+    brush.stroke_method = BRUSH_STROKE_DRAG_DOT;
+  }
+  else if (brush.flag & BRUSH_UNUSED_5) {
+    brush.flag &= ~BRUSH_UNUSED_5;
+    brush.stroke_method = BRUSH_STROKE_LINE;
+  }
+  else if (brush.flag & BRUSH_UNUSED_6) {
+    brush.flag &= ~BRUSH_UNUSED_6;
+    brush.stroke_method = BRUSH_STROKE_CURVE;
+  }
+  else {
+    brush.stroke_method = BRUSH_STROKE_DOTS;
+  }
+}
+
 void do_versions_after_linking_510(FileData * /*fd*/, Main *bmain)
 {
   /* Some blend files were saved with an invalid active viewer key, possibly due to a bug that was
@@ -801,6 +834,12 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       }
     }
     FOREACH_NODETREE_END;
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 23)) {
+    for (Brush &brush : bmain->brushes) {
+      convert_brush_flags_to_type(brush);
+    }
   }
 
   /**

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -348,8 +348,10 @@ static bool image_paint_2d_clone_poll(bContext *C)
 
 bool paint_use_opacity_masking(const Paint *paint, const Brush *brush)
 {
-  return ((brush->flag & BRUSH_AIRBRUSH) || (brush->flag & BRUSH_DRAG_DOT) ||
-                  (brush->flag & BRUSH_ANCHORED) ||
+  return ((ELEM(brush->stroke_method,
+                BRUSH_STROKE_AIRBRUSH,
+                BRUSH_STROKE_DRAG_DOT,
+                BRUSH_STROKE_ANCHORED)) ||
                   ELEM(brush->image_brush_type,
                        IMAGE_PAINT_BRUSH_TYPE_SMEAR,
                        IMAGE_PAINT_BRUSH_TYPE_SOFTEN) ||

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -751,7 +751,7 @@ static void brush_painter_2d_refresh_cache(ImagePaintState *s,
     else if (brush->mtex.brush_map_mode == MTEX_MAP_MODE_RANDOM) {
       do_random = true;
     }
-    else if (!((brush->flag & BRUSH_ANCHORED) || update_color)) {
+    else if (!((brush->stroke_method == BRUSH_STROKE_ANCHORED) || update_color)) {
       do_partial_update = true;
     }
 
@@ -769,7 +769,7 @@ static void brush_painter_2d_refresh_cache(ImagePaintState *s,
     else if (brush->mask_mtex.brush_map_mode == MTEX_MAP_MODE_RANDOM) {
       renew_maxmask = true;
     }
-    else if (!(brush->flag & BRUSH_ANCHORED)) {
+    else if (!(brush->stroke_method == BRUSH_STROKE_ANCHORED)) {
       do_partial_update_mask = true;
       renew_maxmask = true;
     }

source/blender/editors/sculpt_paint/mesh/paint_image_ops_paint.cc
@@ -410,7 +410,7 @@ void ImagePaintStroke::update_step(wmOperator *op, PointerRNA *itemptr)
     BKE_brush_alpha_set(paint, brush, max_ff(0.0f, startalpha * alphafac));
   }
 
-  if ((brush->flag & BRUSH_DRAG_DOT) || (brush->flag & BRUSH_ANCHORED)) {
+  if (ELEM(brush->stroke_method, BRUSH_STROKE_DRAG_DOT, BRUSH_STROKE_ANCHORED)) {
     UndoStack *ustack = CTX_wm_manager(this->evil_C)->runtime->undo_stack;
     ED_image_undo_restore(ustack->step_init);
   }

FrenkyBojler/blender-ios

commit c08f745cc4b73835f0d68a6a63295c1c03492cb9

Fix #154877: Stabilize Stroke can draw with Line stroke method
Caused by 29c9353429

Just correct the enum usage in ELEM(), was still using bitflags.

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -1065,7 +1065,7 @@ bool paint_supports_smooth_stroke(const Brush &brush,
     return true;
   }
   if (!(brush.flag & BRUSH_SMOOTH_STROKE) ||
-      ELEM(brush.stroke_method, BRUSH_STROKE_ANCHORED | BRUSH_STROKE_DRAG_DOT | BRUSH_STROKE_LINE))
+      ELEM(brush.stroke_method, BRUSH_STROKE_ANCHORED, BRUSH_STROKE_DRAG_DOT, BRUSH_STROKE_LINE))
   {
     return false;
   }