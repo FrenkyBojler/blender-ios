98524beb1f22ad34f144edf0583c2dc17cd7139c
Philipp Oeser <philipp@blender.org>
2026-02-25 15:44:16 +0100

    Fix #154854: Grease Pencil trace adaptions to new fill workflow
    Give the operator to option to use "Stroke", "Fill" or "Both", just like
    the new workflow is followed elsewhere when adding or deleting. As per
    the comment in #154854 the default was set to "Fill" (previous behavior
    would create "Both") even though that is up for debate I think.
    
    Also clarified the "Radius" option (since that caused confusion in
    #154854)

--- source/blender/editors/sculpt_paint/grease_pencil/trace.cc
+++ source/blender/editors/sculpt_paint/grease_pencil/trace.cc
@@ -63,6 +63,12 @@ enum class TargetObjectMode : int8_t {
   Selected = 1,
 };
 
+enum class StrokeType : int8_t {
+  Stroke = 0,
+  Fill = 1,
+  Both = 2,
+};
+
 enum class TraceMode : int8_t {
   Single = 0,
   Sequence = 1,
@@ -98,6 +104,7 @@ struct TraceJob {
   float radius;
   TurnPolicy turnpolicy;
   TraceMode mode;
+  StrokeType stroke_type;
   /* Custom source frame, allows overriding the default scene frame. */
   int frame_number;
   int material_index;
@@ -197,10 +204,18 @@ static bke::CurvesGeometry grease_pencil_trace_image(TraceJob &trace_job, const 
   material_indices.span.fill(trace_job.material_index);
   material_indices.finish();
 
-  /* Combine strokes into a single fill with the same fill ID. */
-  bke::SpanAttributeWriter<int> fill_ids = attributes.lookup_or_add_for_write_span<int>(
-      "fill_id", bke::AttrDomain::Curve, bke::AttributeInitValue(1));
-  fill_ids.finish();
+  if (ELEM(trace_job.stroke_type, StrokeType::Fill, StrokeType::Both)) {
+    /* Combine strokes into a single fill with the same fill ID. */
+    bke::SpanAttributeWriter<int> fill_ids = attributes.lookup_or_add_for_write_span<int>(
+        "fill_id", bke::AttrDomain::Curve, bke::AttributeInitValue(1));
+    fill_ids.finish();
+    if (trace_job.stroke_type == StrokeType::Fill) {
+      /* If we only fill, hide the strokes. */
+      bke::SpanAttributeWriter<bool> hide_stroke = attributes.lookup_or_add_for_write_span<bool>(
+          "hide_stroke", bke::AttrDomain::Curve);
+      hide_stroke.span.fill(true);
+    }
+  }
 
   /* Uniform radius for all trace curves. */
   bke::SpanAttributeWriter<float> radii = attributes.lookup_or_add_for_write_only_span<float>(
@@ -395,6 +410,7 @@ static wmOperatorStatus grease_pencil_trace_image_exec(bContext *C, wmOperator *
   job->radius = RNA_float_get(op->ptr, "radius");
   job->turnpolicy = TurnPolicy(RNA_enum_get(op->ptr, "turnpolicy"));
   job->mode = TraceMode(RNA_enum_get(op->ptr, "mode"));
+  job->stroke_type = StrokeType(RNA_enum_get(op->ptr, "stroke_type"));
   job->frame_number = RNA_int_get(op->ptr, "frame_number");
 
   job->ensure_output_object();
@@ -441,6 +457,13 @@ static wmOperatorStatus grease_pencil_trace_image_invoke(bContext *C,
 static void GREASE_PENCIL_OT_trace_image(wmOperatorType *ot)
 {
   PropertyRNA *prop;
+
+  static const EnumPropertyItem stroke_types[] = {
+      {int(StrokeType::Stroke), "STROKE", 0, "Stroke", ""},
+      {int(StrokeType::Fill), "FILL", 0, "Fill", ""},
+      {int(StrokeType::Both), "BOTH", 0, "Both", ""},
+      {0, nullptr, 0, nullptr, nullptr},
+  };
 
   static const EnumPropertyItem turnpolicy_type[] = {
       {int(TurnPolicy::Foreground),
@@ -505,7 +528,22 @@ static void GREASE_PENCIL_OT_trace_image(wmOperatorType *ot)
                           "Target Grease Pencil");
   RNA_def_property_flag(ot->prop, PROP_SKIP_SAVE);
 
-  RNA_def_float(ot->srna, "radius", 0.01f, 0.001f, 1.0f, "Radius", "", 0.001, 1.0f);
+  RNA_def_enum(ot->srna,
+               "stroke_type",
+               stroke_types,
+               int(StrokeType::Fill),
+               "Stroke Mode",
+               "Mode to use when creating strokes");
+
+  RNA_def_float_distance(ot->srna,
+                         "radius",
+                         0.01f,
+                         0.001f,
+                         1.0f,
+                         "Radius",
+                         "Radius of curve control points",
+                         0.001,
+                         1.0f);
 
   RNA_def_float_factor(ot->srna,
                        "threshold",
