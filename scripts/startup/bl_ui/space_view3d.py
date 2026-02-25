96424f98b6c050cffbf0eefa1ff9f85cb62ace60
Jonas Holzman <jonas@blender.org>
2026-02-25 11:27:11 +0100

    UI: Fix title of now collapsible View3D Shading popover Color subpanel
    Previously the Color section of the View3D shading setting popover was
    made of a "Wireframe Color" subpanel containing Wireframe, Object and
    Background color settings, using its title for the first label.
    
    However since PR #121584, which made subpanel collapsible, the entire
    Color section of the panel now collapses under "Wireframe Color". Fixed
    by renaming the subpanel to "Color", and adding an extra "Wireframe"
    label for the Wireframe subsection. For consistency, the "Object Color"
    subsection was also renammed to "Object".

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -6701,7 +6701,7 @@ class VIEW3D_PT_shading_lighting(Panel):
 class VIEW3D_PT_shading_color(Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'HEADER'
-    bl_label = "Wireframe Color"
+    bl_label = "Color"
     bl_parent_id = "VIEW3D_PT_shading"
 
     def _draw_color_type(self, context):
@@ -6725,13 +6725,14 @@ class VIEW3D_PT_shading_color(Panel):
         layout = self.layout
         shading = VIEW3D_PT_shading.get_shading(context)
 
-        self.layout.row().prop(shading, "wireframe_color_type", expand=True)
-        self.layout.separator()
+        layout.row().label(text="Wireframe")
+        layout.row().prop(shading, "wireframe_color_type", expand=True)
+        layout.separator()
 
         if shading.type == 'SOLID':
-            layout.row().label(text="Object Color")
+            layout.row().label(text="Object")
             self._draw_color_type(context)
-            self.layout.separator()
+            layout.separator()
             self._draw_background_color(context)
         elif shading.type == 'WIREFRAME':
             self._draw_background_color(context)
