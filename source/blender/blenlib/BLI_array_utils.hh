d4440dae5fcc3a7273c2c19693050694ce2b253b
Hans Goudey <hans@blender.org>
2026-02-25 08:48:46 -0500

    "non-negative" in comment

--- source/blender/blenlib/BLI_array_utils.hh
+++ source/blender/blenlib/BLI_array_utils.hh
@@ -359,7 +359,7 @@ inline BooleanMix booleans_mix_calc(const VArray<bool> &varray)
 /** Check if the value exists in the array. */
 bool contains(const VArray<bool> &varray, const IndexMask &indices_to_check, bool value);
 
-/** Return indices in the mask that are positive (including 0). */
+/** Return indices in the mask that are non-negative. */
 IndexMask indices_positive(const IndexMask &universe, Span<int> values, IndexMaskMemory &memory);
 /** Return indices in the mask that are not negative and less than the given size. */
 IndexMask indices_in_range(const IndexMask &universe,
