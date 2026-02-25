e5e203d8ae1c45b72c7d73a9bb6db72a185003c7
Clément Foucault <foucault.clem@gmail.com>
2026-02-25 16:49:01 +0100

    Rename horizon_scan_lib

--- source/blender/draw/engines/eevee/shaders/eevee_horizon_scan_test.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_horizon_scan_test.glsl
@@ -11,7 +11,7 @@
 
 COMPUTE_SHADER_CREATE_INFO(gpu_shader_test)
 
-#include "eevee_horizon_scan_lib.glsl"
+#include "eevee_horizon_scan_lib.bsl.hh"
 #include "gpu_shader_math_vector_lib.glsl"
 #include "gpu_shader_test_lib.glsl"
 #include "gpu_shader_utildefines_lib.glsl"
