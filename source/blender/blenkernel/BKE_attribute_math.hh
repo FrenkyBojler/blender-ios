080ea1ac3a7926331ab46de2dcfe4fb30f6cbeaa
Hans Goudey <hans@blender.org>
2026-02-25 14:45:58 -0500

    Attributes: Utility function to mix indices
    The goal is to start to remove duplication of the many places we
    currently implement mixing propagation of attributes. The API function
    I started with mixes the values for a group of result geometry elements,
    using index and weight spans broken into groups by offset indices.
    Processing a small group of result elements together seems like a
    good balance of peak memory usage and hot loop / SIMD optimization
    opportunities.
    
    There are still many other places mixing attribute values. They often
    have one extra level of indexing, or with the current code structure
    only one result value is computed at a time. It will take more work to
    generalize those places as well.
    
    As is, this PR saves about 240KB of binary size, and that will scale
    better as new attribute types or storage methods are added.

--- source/blender/blenkernel/BKE_attribute_math.hh
+++ source/blender/blenkernel/BKE_attribute_math.hh
@@ -697,6 +697,23 @@ using DefaultPropagationMixer = typename DefaultPropagationMixerStruct<T>::type;
  * mixer for the given type. */
 template<typename T> using DefaultMixer = typename DefaultMixerStruct<T>::type;
 
+void mix_indices(GSpan src,
+                 OffsetIndices<int> groups,
+                 Span<int> all_indices,
+                 std::optional<Span<float>> all_weights,
+                 GMutableSpan dst);
+inline void mix_indices(GSpan src,
+                        OffsetIndices<int> groups,
+                        Span<int> all_indices,
+                        GMutableSpan dst)
+{
+  mix_indices(src, groups, all_indices, std::nullopt, dst);
+}
+inline void mix_indices(GSpan src, GroupedSpan<int> indices, GMutableSpan dst)
+{
+  mix_indices(src, indices.offsets, indices.data, dst);
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
