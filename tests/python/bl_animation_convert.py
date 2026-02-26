c2c95ead5ede5709bc63fe7ad5319f7c2b11e6cb
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-02-26 14:36:49 +0100

    basic unit tests

--- tests/python/bl_animation_convert.py
+++ tests/python/bl_animation_convert.py
@@ -6,13 +6,30 @@ import bpy
 import sys
 import unittest
 import pathlib
+import mathutils
 
 """
 blender -b --factory-startup --python tests/python/bl_animation_convert.py -- --testdir tests/files/animation/
 """
 
+
+def _action_slot_has_rna_path(action: bpy.types.Action, slot: bpy.types.ActionSlot, rna_path: str) -> bool:
+    for layer in action.layers:
+        for strip in layer.strips:
+            channelbag = strip.channelbag(slot)
+            if not channelbag:
+                continue
+            for fcurve in channelbag.fcurves:
+                if fcurve.data_path == rna_path:
+                    return True
+    return False
+
 class ConvertRotationMode(unittest.TestCase):
 
+    action: bpy.types.Action
+    action_slot: bpy.types.ActionSlot
+
+    keyed_frames = [1, 6, 11, 16, 21]
     bone_quat: bpy.types.PoseBone
     bone_axis_angle: bpy.types.PoseBone
     bone_xyz: bpy.types.PoseBone
@@ -23,9 +40,28 @@ class ConvertRotationMode(unittest.TestCase):
 
     bone_no_rotation_keys: bpy.types.PoseBone
 
+    def _assert_almost_equal_rotation(self, a: mathutils.Matrix, b: mathutils.Matrix):
+        equal = True
+        for j in range(3):
+            for i in range(3):
+                if abs(a.row[i][j] - b.row[i][j]) > 0.001:
+                    equal = False
+                    break
+        if not equal:
+            raise AssertionError(f"Rotation part of matrices doesn't match\n{a}\n{b}")
+
+    def _assert_almost_equal_euler(self, a: mathutils.Euler, b: mathutils.Euler):
+        self.assertAlmostEqual(a.x, b.x, 2, f"{a} - {b}")
+        self.assertAlmostEqual(a.y, b.y, 2, f"{a} - {b}")
+        self.assertAlmostEqual(a.z, b.z, 2, f"{a} - {b}")
+
     def setUp(self) -> None:
         bpy.ops.wm.open_mainfile(filepath=str(args.testdir / "rotation_mode_conversion.blend"))
-        pose = bpy.data.objects["Armature"].pose
+        armature_ob = bpy.data.objects["Armature"]
+        self.action = armature_ob.animation_data.action
+        self.action_slot = armature_ob.animation_data.action_slot
+        pose = armature_ob.pose
+
         self.bone_quat = pose.bones["bone_quat"]
         self.bone_axis_angle = pose.bones["bone_axis_angle"]
         self.bone_xyz = pose.bones["bone_xyz"]
@@ -35,29 +71,58 @@ class ConvertRotationMode(unittest.TestCase):
         self.bone_euler_720 = pose.bones["bone_euler_rotation_720"]
 
         self.bone_no_rotation_keys = pose.bones["bone_no_rotation_keys"]
-    
+
     def test_convert_quat_to_euler(self):
-        keyed_frames = [1, 6, 11, 16, 21]
-        self.bone_quat.convert_rotation_mode("XYZ")
+        self.bone_quat.convert_rotation_mode('XYZ')
+        self.assertEqual(self.bone_quat.rotation_mode, 'XYZ')
+        for frame in self.keyed_frames:
+            bpy.context.scene.frame_set(frame)
+            self._assert_almost_equal_euler(self.bone_quat.rotation_euler, self.bone_xyz.rotation_euler)
+
+    def test_convert_xyz_to_zyx(self):
+        self.bone_xyz.convert_rotation_mode('ZYX')
+        self.assertEqual(self.bone_xyz.rotation_mode, 'ZYX')
+        for frame in self.keyed_frames:
+            bpy.context.scene.frame_set(frame)
+            self._assert_almost_equal_euler(self.bone_xyz.rotation_euler, self.bone_zyx.rotation_euler)
 
     def test_convert_360_rotation_limitation(self):
-        """ When converting from euler with deltas >180 degrees between keyframes, 
+        """ When converting from euler with deltas >180 degrees between keyframes,
         the resulting animation loses that information."""
-        pass
+        self.assertEqual(self.bone_euler_360.rotation_mode, 'XYZ')
+        self.bone_euler_360.convert_rotation_mode('XYZ')
+        # Trying to convert to the current rotation mode should be a no op.
 
     def test_convert_360_rotation_bake(self):
-        """ When converting rotations >180 degrees baking has to 
+        """ When converting rotations >180 degrees baking has to
         be used to ensure the interpolation is preserved. """
         pass
 
     def test_convert_keyed_rotation_mode(self):
-        """ When the rotation mode itself is keyed and changes during the animation, 
+        """ When the rotation mode itself is keyed and changes during the animation,
         the conversion has to make sure the animation is preserved. """
         pass
 
     def test_convert_unkeyed_rotation(self):
         """ When converting the rotation mode on a bone that has no keys on its rotation channels,
          the function should just change the rotation mode while preserving the visual rotation. """
+        matrix_before: mathutils.Matrix = self.bone_no_rotation_keys.matrix
+        self.assertFalse(
+            _action_slot_has_rna_path(
+                self.action,
+                self.action_slot,
+                'pose.bones["bone_no_rotation_keys"].rotation_quaternion'))
+        self.bone_no_rotation_keys.convert_rotation_mode('XYZ')
+        matrix_after = self.bone_no_rotation_keys.matrix
+        self._assert_almost_equal_rotation(matrix_before, matrix_after)
+        self.assertFalse(
+            _action_slot_has_rna_path(
+                self.action,
+                self.action_slot,
+                'pose.bones["bone_no_rotation_keys"].rotation_euler'))
+
+    def test_result_is_euler_filtered(self):
+        """ When converting rotation modes we should not have sudden 180 degree jumps in euler mode. """
         pass
 
 
