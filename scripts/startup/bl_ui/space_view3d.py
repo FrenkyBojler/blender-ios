blender/blender

commit 4434a30d4042594ee2fbb95f4b9c46da38ca950a

Paint: Make UnifiedPaintSettings a per-mode instead of scene-wide value
Historically, the `UnifiedPaintSettings` struct has been used to
provide users the ability to set brush values at a scene level. Examples
of such attributes are the brush size, strength, and color, to name a
few.

Instead of these values being shared across all of the grease pencil,
mesh painting, and curves sculpting modes. This commit migrates the
data to the `Paint` struct, meaning that each individual mode (e.g.
Sculpt, Vertex Paint, Grease Pencil Draw) now has the ability to change
these values without affecting other modes.

While this change is large, the majority of the work is simply
refactoring access to the `UnifiedPaintSetting` struct. to ensure the
correct property is being retrieved.

Resolves #134077

Pull Request: https://projects.blender.org/blender/blender/pulls/139766

scripts/startup/bl_ui/space_view3d.py
@@ -253,7 +253,7 @@ class _draw_tool_settings_context_mode:
         tool_settings = context.tool_settings
         capabilities = brush.sculpt_capabilities
 
-        ups = tool_settings.unified_paint_settings
+        ups = paint.unified_paint_settings
 
         if capabilities.has_color:
             row = layout.row(align=True)
@@ -396,7 +396,7 @@ class _draw_tool_settings_context_mode:
         tool_settings = context.tool_settings
         capabilities = brush.sculpt_capabilities
 
-        ups = tool_settings.unified_paint_settings
+        ups = paint.unified_paint_settings
 
         size = "size"
         size_owner = ups if ups.use_unified_size else brush
@@ -487,7 +487,7 @@ class _draw_tool_settings_context_mode:
 
         if brush.gpencil_vertex_brush_type not in {'BLUR', 'AVERAGE', 'SMEAR'}:
             layout.separator(factor=0.4)
-            ups = context.tool_settings.unified_paint_settings
+            ups = paint.unified_paint_settings
             prop_owner = ups if ups.use_unified_color else brush
             layout.prop_with_popover(prop_owner, "color", text="", panel="TOPBAR_PT_grease_pencil_vertex_color")
 
@@ -8491,10 +8491,11 @@ class VIEW3D_PT_greasepencil_sculpt_context_menu(Panel):
 
     def draw(self, context):
         tool_settings = context.tool_settings
-        brush = tool_settings.gpencil_sculpt_paint.brush
+        paint = tool_settings.gpencil_sculpt_paint
+        brush = paint.brush
         layout = self.layout
 
-        ups = tool_settings.unified_paint_settings
+        ups = paint.unified_paint_settings
         size_owner = ups if ups.use_unified_size else brush
         strength_owner = ups if ups.use_unified_strength else brush
         layout.prop(size_owner, "size", text="")
@@ -8528,8 +8529,8 @@ class VIEW3D_PT_greasepencil_vertex_paint_context_menu(Panel):
 
         if brush.gpencil_vertex_brush_type in {'DRAW', 'REPLACE'}:
             split = layout.split(factor=0.1)
-            split.prop(tool_settings.unified_paint_settings, "color", text="")
-            split.template_color_picker(tool_settings.unified_paint_settings, "color", value_slider=True)
+            split.prop(settings.unified_paint_settings, "color", text="")
+            split.template_color_picker(settings.unified_paint_settings, "color", value_slider=True)
 
             col = layout.column()
             col.separator()
@@ -8537,7 +8538,7 @@ class VIEW3D_PT_greasepencil_vertex_paint_context_menu(Panel):
             col.separator()
 
         row = col.row(align=True)
-        row.prop(tool_settings.unified_paint_settings, "size", text="Radius")
+        row.prop(settings.unified_paint_settings, "size", text="Radius")
         row.prop(brush, "use_pressure_size", text="", icon='STYLUS_PRESSURE')
 
         if brush.gpencil_vertex_brush_type in {'DRAW', 'BLUR', 'SMEAR'}:
@@ -8802,7 +8803,8 @@ class VIEW3D_PT_sculpt_context_menu(Panel):
     def draw(self, context):
         layout = self.layout
 
-        brush = context.tool_settings.sculpt.brush
+        paint = context.tool_settings.sculpt
+        brush = paint.brush
         capabilities = brush.sculpt_capabilities
 
         if capabilities.has_color:
@@ -8811,7 +8813,7 @@ class VIEW3D_PT_sculpt_context_menu(Panel):
             UnifiedPaintPanel.prop_unified_color_picker(split, context, brush, "color", value_slider=True)
             layout.prop(brush, "blend", text="")
 
-        ups = context.tool_settings.unified_paint_settings
+        ups = paint.unified_paint_settings
         size = "size"
         size_owner = ups if ups.use_unified_size else brush
         if size_owner.use_locked_size == 'SCENE':
@@ -8895,7 +8897,7 @@ class TOPBAR_PT_grease_pencil_vertex_color(Panel):
             paint = context.scene.tool_settings.gpencil_vertex_paint
         use_unified_paint = (ob.mode != 'PAINT_GREASE_PENCIL')
 
-        ups = context.tool_settings.unified_paint_settings
+        ups = paint.unified_paint_settings
         brush = paint.brush
         prop_owner = ups if use_unified_paint and ups.use_unified_color else brush
 

source/blender/editors/sculpt_paint/paint_image.cc
@@ -348,7 +348,7 @@ static bool image_paint_2d_clone_poll(bContext *C)
 /** \name Paint Operator
  * \{ */
 
-bool paint_use_opacity_masking(const Scene *scene, const Paint *paint, const Brush *brush)
+bool paint_use_opacity_masking(const Paint *paint, const Brush *brush)
 {
   return ((brush->flag & BRUSH_AIRBRUSH) || (brush->flag & BRUSH_DRAG_DOT) ||
                   (brush->flag & BRUSH_ANCHORED) ||
@@ -357,7 +357,7 @@ bool paint_use_opacity_masking(const Scene *scene, const Paint *paint, const Bru
                        IMAGE_PAINT_BRUSH_TYPE_SOFTEN) ||
                   (brush->image_brush_type == IMAGE_PAINT_BRUSH_TYPE_FILL) ||
                   (brush->flag & BRUSH_USE_GRADIENT) ||
-                  (BKE_brush_color_jitter_get_settings(scene, paint, brush)) ||
+                  (BKE_brush_color_jitter_get_settings(paint, brush)) ||
                   (brush->mtex.tex && !ELEM(brush->mtex.brush_map_mode,
                                             MTEX_MAP_MODE_TILED,
                                             MTEX_MAP_MODE_STENCIL,
@@ -366,8 +366,7 @@ bool paint_use_opacity_masking(const Scene *scene, const Paint *paint, const Bru
               true);
 }
 
-void paint_brush_color_get(Scene *scene,
-                           const Paint *paint,
+void paint_brush_color_get(const Paint *paint,
                            Brush *br,
                            std::optional<blender::float3> &initial_hsv_jitter,
                            bool color_correction,
@@ -378,11 +377,11 @@ void paint_brush_color_get(Scene *scene,
                            float r_color[3])
 {
   if (invert) {
-    copy_v3_v3(r_color, BKE_brush_secondary_color_get(scene, paint, br));
+    copy_v3_v3(r_color, BKE_brush_secondary_color_get(paint, br));
   }
   else {
     const std::optional<BrushColorJitterSettings> color_jitter_settings =
-        BKE_brush_color_jitter_get_settings(scene, paint, br);
+        BKE_brush_color_jitter_get_settings(paint, br);
     if (br->flag & BRUSH_USE_GRADIENT) {
       float color_gr[4];
       switch (br->gradient_stroke_mode) {
@@ -409,10 +408,10 @@ void paint_brush_color_get(Scene *scene,
                                            *initial_hsv_jitter,
                                            distance,
                                            pressure,
-                                           BKE_brush_color_get(scene, paint, br)));
+                                           BKE_brush_color_get(paint, br)));
     }
     else {
-      copy_v3_v3(r_color, BKE_brush_color_get(scene, paint, br));
+      copy_v3_v3(r_color, BKE_brush_color_get(paint, br));
     }
   }
   if (color_correction) {
@@ -699,7 +698,6 @@ static wmOperatorStatus sample_color_exec(bContext *C, wmOperator *op)
 
 static wmOperatorStatus sample_color_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
-  Scene *scene = CTX_data_scene(C);
   Paint *paint = BKE_paint_get_active_from_context(C);
   Brush *brush = BKE_paint_brush(paint);
   SampleColorData *data = MEM_new<SampleColorData>("sample color custom data");
@@ -708,7 +706,7 @@ static wmOperatorStatus sample_color_invoke(bContext *C, wmOperator *op, const w
 
   data->launch_event = WM_userdef_event_type_from_keymap_type(event->type);
   data->show_cursor = ((paint->flags & PAINT_SHOW_BRUSH) != 0);
-  copy_v3_v3(data->initcolor, BKE_brush_color_get(scene, paint, brush));
+  copy_v3_v3(data->initcolor, BKE_brush_color_get(paint, brush));
   data->sample_palette = false;
   op->customdata = data;
   paint->flags &= ~PAINT_SHOW_BRUSH;
@@ -737,7 +735,6 @@ static wmOperatorStatus sample_color_invoke(bContext *C, wmOperator *op, const w
 
 static wmOperatorStatus sample_color_modal(bContext *C, wmOperator *op, const wmEvent *event)
 {
-  Scene *scene = CTX_data_scene(C);
   SampleColorData *data = static_cast<SampleColorData *>(op->customdata);
   Paint *paint = BKE_paint_get_active_from_context(C);
   Brush *brush = BKE_paint_brush(paint);
@@ -748,7 +745,7 @@ static wmOperatorStatus sample_color_modal(bContext *C, wmOperator *op, const wm
     }
 
     if (data->sample_palette) {
-      BKE_brush_color_set(scene, paint, brush, data->initcolor);
+      BKE_brush_color_set(paint, brush, data->initcolor);
       RNA_boolean_set(op->ptr, "palette", true);
       WM_event_add_notifier(C, NC_BRUSH | NA_EDITED, brush);
     }
@@ -870,9 +867,9 @@ static blender::float3 paint_init_pivot_grease_pencil(Object *ob, const int fram
   return float3(0.0f);
 }
 
-void paint_init_pivot(Object *ob, Scene *scene)
+void paint_init_pivot(Object *ob, Scene *scene, Paint *paint)
 {
-  UnifiedPaintSettings *ups = &scene->toolsettings->unified_paint_settings;
+  UnifiedPaintSettings *ups = &paint->unified_paint_settings;
 
   blender::float3 location;
   switch (ob->type) {
@@ -951,7 +948,7 @@ void ED_object_texture_paint_mode_enter_ex(Main &bmain,
 
   /* Set pivot to bounding box center. */
   Object *ob_eval = DEG_get_evaluated(&depsgraph, &ob);
-  paint_init_pivot(ob_eval ? ob_eval : &ob, &scene);
+  paint_init_pivot(ob_eval ? ob_eval : &ob, &scene, &imapaint.paint);
 
   WM_main_add_notifier(NC_SCENE | ND_MODE, &scene);
 }
@@ -1057,13 +1054,11 @@ void PAINT_OT_texture_paint_toggle(wmOperatorType *ot)
 
 static wmOperatorStatus brush_colors_flip_exec(bContext *C, wmOperator * /*op*/)
 {
-  Scene &scene = *CTX_data_scene(C);
-
   Paint *paint = BKE_paint_get_active_from_context(C);
   Brush *br = BKE_paint_brush(paint);
 
-  if (BKE_paint_use_unified_color(scene.toolsettings, paint)) {
-    UnifiedPaintSettings &ups = scene.toolsettings->unified_paint_settings;
+  if (BKE_paint_use_unified_color(paint)) {
+    UnifiedPaintSettings &ups = paint->unified_paint_settings;
     swap_v3_v3(ups.rgb, ups.secondary_rgb);
   }
   else if (br) {

FrenkyBojler/blender-ios

commit 80525c726bd5ace60d97eba8ef17bb4206499845

Fix: Image Paint keymap active in Image Editor outside of 'PAINT' mode
The report was actually about an error spawning the
`VIEW3D_PT_paint_texture_context_menu` context menu in 'VIEW' mode
(which was caused by 4434a30d4042), but even before, you could call that
context menu for brush properties (even if the Image Editor is only in
when in only 'VIEW' or 'MASK' mode).

The root cause here is that the whole "Image Paint" / `km_image_paint`
keymap is accessible/used from 2D painting in the Image Editor as well
as in the 3D viewport, so items in that keymap usually have their own
polls, just not all of the are sufficient.

An example of this is af601e01fab3 and it can turn out cumbersome to
maintain (even though not wrong at all).

So in the case of the report, we could very well add a poll to
`VIEW3D_PT_paint_texture_context_menu` [which, as stated, is not only
used in the 3DView, but in the Image Editor as well] checking we are
actually in 'PAINT' mode when in the Image Editor.

But it turns out that other items are still active as well (radial
control [which might be fixable], but also other entries involving
`wm.context_toggle` [e.g. toggle `brush.use_smooth_stroke`].

So the proposed solution in this PR is to be stricter on the whole
keymap `poll`.

polls involved:

- `image_texture_paint_poll` (the general keymap one)
- `texture_paint_poll` (checks `texture_paint_toggle_poll` and
`OB_MODE_TEXTURE_PAINT`)
- `texture_paint_toggle_poll` (checks valid active object)
- `ED_image_tools_paint_poll` (checks `image_paint_poll_ex`)
- `image_paint_poll_ex` (checks `image_paint_brush` and others [see
below])
- `image_paint_brush` (checks a valid brush)

All of them still succeed in the Image Editor in modes outside of
'PAINT' which is wrong.

Since `texture_paint_poll` and `texture_paint_toggle_poll` only check
the **Object**, they have renamed for clarity. And since these two polls
are really not relevant for Image Editor painting (in fact they are

scripts/startup/bl_ui/space_view3d.py
@@ -8726,10 +8726,18 @@ class VIEW3D_PT_paint_vertex_context_menu(Panel):
 
 
 class VIEW3D_PT_paint_texture_context_menu(Panel):
+    # Used in both the 3DView as well as the Image Editor
     # Only for popover, these are dummy values.
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'WINDOW'
     bl_label = "Texture Paint"
+
+    @classmethod
+    def poll(cls, context):
+        # Since bl_space_type is ignored for popovers, we need to check if the Image Editor is in the right mode
+        if context.space_data.type == 'IMAGE_EDITOR' and context.space_data.mode != 'PAINT':
+            return False
+        return True
 
     def draw(self, context):
         layout = self.layout

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -279,32 +279,30 @@ static Brush *image_paint_brush(bContext *C)
 
 static bool image_paint_poll_ex(bContext *C, bool check_tool)
 {
-  Object *obact;
-
   if (!image_paint_brush(C)) {
     return false;
   }
 
-  obact = CTX_data_active_object(C);
-  if ((obact && obact->mode & OB_MODE_TEXTURE_PAINT) && CTX_wm_region_view3d(C)) {
-    if (!check_tool || WM_toolsystem_active_tool_is_brush(C)) {
-      return true;
+  if (SpaceImage *sima = CTX_wm_space_image(C)) {
+    if (sima->image != nullptr &&
+        (!ID_IS_EDITABLE(sima->image) || ID_IS_OVERRIDE_LIBRARY(sima->image)))
+    {
+      return false;
+    }
+    if (sima->mode == SI_MODE_PAINT) {
+      const ARegion *region = CTX_wm_region(C);
+      if (region->regiontype == RGN_TYPE_WINDOW) {
+        if (!check_tool || WM_toolsystem_active_tool_is_brush(C)) {
+          return true;
+        }
+      }
     }
   }
   else {
-    SpaceImage *sima = CTX_wm_space_image(C);
-
-    if (sima) {
-      if (sima->image != nullptr &&
-          (!ID_IS_EDITABLE(sima->image) || ID_IS_OVERRIDE_LIBRARY(sima->image)))
-      {
-        return false;
-      }
-      if (sima->mode == SI_MODE_PAINT) {
-        const ARegion *region = CTX_wm_region(C);
-        if (region->regiontype == RGN_TYPE_WINDOW) {
-          return true;
-        }
+    Object *obact = CTX_data_active_object(C);
+    if ((obact && obact->mode & OB_MODE_TEXTURE_PAINT) && CTX_wm_region_view3d(C)) {
+      if (!check_tool || WM_toolsystem_active_tool_is_brush(C)) {
+        return true;
       }
     }
   }
@@ -794,7 +792,7 @@ void ED_object_texture_paint_mode_exit(bContext *C)
   ED_object_texture_paint_mode_exit_ex(bmain, scene, ob);
 }
 
-static bool texture_paint_toggle_poll(bContext *C)
+static bool texture_paint_object_toggle_poll(bContext *C)
 {
   Object *ob = CTX_data_active_object(C);
   if (ob == nullptr || ob->type != OB_MESH) {
@@ -847,7 +845,7 @@ void PAINT_OT_texture_paint_toggle(wmOperatorType *ot)
 
   /* API callbacks. */
   ot->exec = texture_paint_toggle_exec;
-  ot->poll = texture_paint_toggle_poll;
+  ot->poll = texture_paint_object_toggle_poll;
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
@@ -946,9 +944,9 @@ void ED_imapaint_bucket_fill(bContext *C, float const color[3], wmOperator *op, 
   }
 }
 
-static bool texture_paint_poll(bContext *C)
+static bool texture_paint_object_poll(bContext *C)
 {
-  if (texture_paint_toggle_poll(C)) {
+  if (texture_paint_object_toggle_poll(C)) {
     if (CTX_data_active_object(C)->mode & OB_MODE_TEXTURE_PAINT) {
       return true;
     }
@@ -965,7 +963,11 @@ float3 seed_hsv_jitter()
 
 bool image_texture_paint_poll(bContext *C)
 {
-  return (texture_paint_poll(C) || ED_image_tools_paint_poll(C));
+  if (CTX_wm_space_image(C)) {
+    return ED_image_tools_paint_poll(C);
+  }
+
+  return (texture_paint_object_poll(C) || ED_image_tools_paint_poll(C));
 }
 
 bool facemask_paint_poll(bContext *C)