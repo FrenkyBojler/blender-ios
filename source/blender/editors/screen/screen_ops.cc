6315c9ecfc4cd858e351509d0b1316344275c8cd
Harley Acheson <harley.acheson@gmail.com>
2026-02-25 17:34:40 -0800

    Now properly reapplying limits when changing extend

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -1784,7 +1784,7 @@ struct sAreaMoveData {
   eScreenAxis dir_axis;
   AreaMoveSnapType snap_type;
   bool can_extend;
-  bool extend;
+  bool extending;
   bScreen *screen;
   ScrArea *area1, *area2;
   double start_time;
@@ -1906,28 +1906,40 @@ static void area_move_out_draw_cb(const wmWindow *win, void *userdata)
 static bool area_move_reinit(bContext *C, wmOperator *op, bool extend, const int xy[2])
 {
   sAreaMoveData *md = static_cast<sAreaMoveData *>(op->customdata);
-  md->extend = extend && md->can_extend;
-
   bScreen *screen = CTX_wm_screen(C);
   wmWindow *win = CTX_wm_window(C);
-
-  /* setup */
-  ScrEdge *actedge = screen_geom_find_active_scredge(win, screen, xy[0], xy[1]);
-
-  if (actedge == nullptr) {
-    return false;
-  }
 
   ED_screen_verts_iter(win, screen, v1)
   {
     v1->editflag = 0;
   }
 
-  if (md->extend) {
+  /* setup */
+  ScrEdge *actedge = screen_geom_find_active_scredge(win, screen, xy[0], xy[1]);
+
+  if (actedge == nullptr) {
+    md->can_extend = false;
+    md->extending = false;
+    return false;
+  }
+
+  if (extend) {
+    if (md->dir_axis == SCREEN_AXIS_H) {
+      md->origval = actedge->v1->vec.y;
+    }
+    else {
+      md->origval = actedge->v1->vec.x;
+    }
+  }
+
+  md->can_extend = screen_geom_edge_can_extend(win, actedge);
+  if (md->can_extend && extend) {
     screen_geom_select_extended_edge(win, actedge);
+    md->extending = true;
   }
   else {
     screen_geom_select_connected_edge(win, actedge);
+    md->extending = false;
   }
 
   /* now all vertices with 'flag == 1' are the ones that can be moved. Move this to editflag */
@@ -1935,6 +1947,10 @@ static bool area_move_reinit(bContext *C, wmOperator *op, bool extend, const int
   {
     v1->editflag = v1->flag;
   }
+
+  bool use_bigger_smaller_snap = false;
+  area_move_set_limits(
+      win, screen, md->dir_axis, &md->bigger, &md->smaller, &use_bigger_smaller_snap);
 
   md->start_time = BLI_time_now_seconds();
   md->end_time = md->start_time + AREA_MOVE_LINE_FADEIN;
@@ -1987,9 +2003,11 @@ static bool area_move_init(bContext *C, wmOperator *op, bool extend)
   md->can_extend = screen_geom_edge_can_extend(win, actedge);
   if (md->can_extend && extend) {
     screen_geom_select_extended_edge(win, actedge);
+    md->extending = true;
   }
   else {
     screen_geom_select_connected_edge(win, actedge);
+    md->extending = false;
   }
 
   /* now all vertices with 'flag == 1' are the ones that can be moved. Move this to editflag */
@@ -2011,7 +2029,6 @@ static bool area_move_init(bContext *C, wmOperator *op, bool extend)
 
   md->win = win;
   md->screen = screen;
-  md->extend = extend;
   md->start_time = BLI_time_now_seconds();
   md->end_time = md->start_time + AREA_MOVE_LINE_FADEIN;
   md->draw_callback = WM_draw_cb_activate(CTX_wm_window(C), area_move_draw_cb, op);
@@ -2224,14 +2241,6 @@ static int area_snap_calc_location(sAreaMoveData *md, const int delta)
 /* Moves selected screen edge amount of delta. */
 static void area_move_apply_do(bContext *C, int delta, sAreaMoveData *md)
 {
-  WorkspaceStatus status(C);
-  status.item(IFACE_("Confirm"), ICON_MOUSE_LMB);
-  status.item(IFACE_("Cancel"), ICON_EVENT_ESC);
-  status.item_bool(IFACE_("Snap"), md->snap_type == SNAP_FRACTION_AND_ADJACENT, ICON_EVENT_CTRL);
-  if (md->can_extend) {
-    status.item_bool(IFACE_("Extend"), md->extend, ICON_EVENT_SHIFT);
-  }
-
   short final_loc = -1;
   bool doredraw = false;
 
@@ -2296,6 +2305,14 @@ static void area_move_apply_do(bContext *C, int delta, sAreaMoveData *md)
     /* Update preview thumbnail */
     BKE_icon_changed(md->screen->id.icon_id);
   }
+
+  WorkspaceStatus status(C);
+  status.item(IFACE_("Confirm"), ICON_MOUSE_LMB);
+  status.item(IFACE_("Cancel"), ICON_EVENT_ESC);
+  status.item_bool(IFACE_("Snap"), md->snap_type == SNAP_FRACTION_AND_ADJACENT, ICON_EVENT_CTRL);
+  if (md->can_extend) {
+    status.item_bool(IFACE_("Extend"), md->extending, ICON_EVENT_SHIFT);
+  }
 }
 
 static void area_move_apply(bContext *C, wmOperator *op)
@@ -2355,7 +2372,7 @@ static wmOperatorStatus area_move_invoke(bContext *C, wmOperator *op, const wmEv
   status.item(IFACE_("Cancel"), ICON_EVENT_ESC);
   status.item_bool(IFACE_("Snap"), md->snap_type == SNAP_FRACTION_AND_ADJACENT, ICON_EVENT_CTRL);
   if (md->can_extend) {
-    status.item_bool(IFACE_("Extend"), md->extend, ICON_EVENT_SHIFT);
+    status.item_bool(IFACE_("Extend"), md->extending, ICON_EVENT_SHIFT);
   }
 
   /* add temp handler */
@@ -2384,8 +2401,11 @@ static wmOperatorStatus area_move_modal(bContext *C, wmOperator *op, const wmEve
     case EVT_LEFTSHIFTKEY: {
       if (md->can_extend) {
         area_move_reinit(C, op, event->val == KM_PRESS, event->xy);
-        WM_event_add_notifier(C, NC_SCREEN | NA_EDITED, nullptr);
       }
+      else {
+        md->extending = false;
+      }
+      WM_event_add_notifier(C, NC_SCREEN | NA_EDITED, nullptr);
       break;
     }
     case MOUSEMOVE: {
@@ -2399,8 +2419,11 @@ static wmOperatorStatus area_move_modal(bContext *C, wmOperator *op, const wmEve
 
       wmWindow *win = CTX_wm_window(C);
       bScreen *screen = CTX_wm_screen(C);
-      ScrEdge *actedge = screen_geom_find_active_scredge(win, screen, event->xy[0], event->xy[1]);
-      md->can_extend = actedge && screen_geom_edge_can_extend(md->win, actedge);
+      if (!md->extending) {
+        ScrEdge *actedge = screen_geom_find_active_scredge(
+            win, screen, event->xy[0], event->xy[1]);
+        md->can_extend = actedge && screen_geom_edge_can_extend(md->win, actedge);
+      }
       break;
     }
     case RIGHTMOUSE: {
@@ -2441,7 +2464,7 @@ static wmOperatorStatus area_move_modal(bContext *C, wmOperator *op, const wmEve
   status.item(IFACE_("Cancel"), ICON_EVENT_ESC);
   status.item_bool(IFACE_("Snap"), md->snap_type == SNAP_FRACTION_AND_ADJACENT, ICON_EVENT_CTRL);
   if (md->can_extend) {
-    status.item_bool(IFACE_("Extend"), md->extend, ICON_EVENT_SHIFT);
+    status.item_bool(IFACE_("Extend"), md->extending, ICON_EVENT_SHIFT);
   }
   return OPERATOR_RUNNING_MODAL;
 }
