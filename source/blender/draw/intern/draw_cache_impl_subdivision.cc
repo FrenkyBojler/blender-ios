blender/blender

commit 4478dd3f9a484e9faa09fc676b2d776505eaaec5

Refactor: BLI: Deduplicate reverse index group creation
Currently there are a few places taking an index map and reversing it
to group the indices of each group. This PR moves that logic to the
offset indices header and also uses it in the Grease Pencil layer
merging code, causing some deduplication there as well. The motivation
for the change was to make it easier to use GroupedSpan in those
Grease Pencil functions in the first place.

Pull Request: https://projects.blender.org/blender/blender/pulls/154924

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -721,33 +721,20 @@ static gpu::VertBuf *gpu_vertbuf_create_from_format(const GPUVertFormat &format,
  * be used for computing normals if limit surfaces are unavailable. */
 static void build_vert_face_adjacency_maps(DRWSubdivCache &cache)
 {
-  /* +1 so that we do not require a special case for the last vertex, this extra offset will
-   * contain the total number of adjacent faces. */
+  const Span<int> loop_to_vert(cache.subdiv_loop_subdiv_vert_index, cache.num_subdiv_loops);
+
   cache.subdiv_vert_face_adjacency_offsets = gpu_vertbuf_create_from_format(
       get_origindex_format(), cache.num_subdiv_verts + 1);
-
   MutableSpan<int> vert_offsets = cache.subdiv_vert_face_adjacency_offsets->data<int>();
-  vert_offsets.fill(0);
 
-  offset_indices::build_reverse_offsets(
-      {cache.subdiv_loop_subdiv_vert_index, cache.num_subdiv_loops}, vert_offsets);
+  vert_offsets.fill(0);
+  const OffsetIndices offsets = offset_indices::build_reverse_offsets(loop_to_vert, vert_offsets);
 
   cache.subdiv_vert_face_adjacency = gpu_vertbuf_create_from_format(get_origindex_format(),
                                                                     cache.num_subdiv_loops);
   MutableSpan<int> adjacent_faces = cache.subdiv_vert_face_adjacency->data<int>();
-  int *tmp_set_faces = MEM_new_array_zeroed<int>(cache.num_subdiv_verts,
-                                                 "tmp subdiv vertex offset");
 
-  for (int i = 0; i < cache.num_subdiv_loops / 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      const int subdiv_vert = cache.subdiv_loop_subdiv_vert_index[i * 4 + j];
-      int first_face_offset = vert_offsets[subdiv_vert] + tmp_set_faces[subdiv_vert];
-      adjacent_faces[first_face_offset] = i;
-      tmp_set_faces[subdiv_vert] += 1;
-    }
-  }
-
-  MEM_delete(tmp_set_faces);
+  offset_indices::reverse_indices_in_groups(loop_to_vert, offsets, adjacent_faces);
 }
 
 static bool draw_subdiv_build_cache(DRWSubdivCache &cache,

source/blender/blenkernel/intern/mesh_mapping.cc
@@ -270,45 +270,6 @@ static Array<int> create_reverse_offsets(const Span<int> indices, const int item
   return offsets;
 }
 
-static void sort_small_groups(const OffsetIndices<int> groups,
-                              const int grain_size,
-                              MutableSpan<int> indices)
-{
-  threading::parallel_for(groups.index_range(), grain_size, [&](const IndexRange range) {
-    for (const int64_t index : range) {
-      MutableSpan<int> group = indices.slice(groups[index]);
-      std::ranges::sort(group);
-    }
-  });
-}
-
-static Array<int> reverse_indices_in_groups(const Span<int> group_indices,
-                                            const OffsetIndices<int> offsets)
-{
-  if (group_indices.is_empty()) {
-    return {};
-  }
-  BLI_assert(*std::max_element(group_indices.begin(), group_indices.end()) < offsets.size());
-  BLI_assert(*std::min_element(group_indices.begin(), group_indices.end()) >= 0);
-
-  /* `counts` keeps track of how many elements have been added to each group, and is incremented
-   * atomically by many threads in parallel. `calloc` can be measurably faster than a parallel fill
-   * of zero. Alternatively the offsets could be copied and incremented directly, but the cost of
-   * the copy is slightly higher than the cost of `calloc`. */
-  int *counts = MEM_new_array_zeroed<int>(size_t(offsets.size()), __func__);
-  BLI_SCOPED_DEFER([&]() { MEM_delete(counts); })
-  Array<int> results(group_indices.size());
-  threading::parallel_for(group_indices.index_range(), 1024, [&](const IndexRange range) {
-    for (const int64_t i : range) {
-      const int group_index = group_indices[i];
-      const int index_in_group = atomic_fetch_and_add_int32(&counts[group_index], 1);
-      results[offsets[group_index][index_in_group]] = int(i);
-    }
-  });
-  sort_small_groups(offsets, 1024, results);
-  return results;
-}
-
 /* A version of #reverse_indices_in_groups that stores face indices instead of corner indices. */
 static void reverse_group_indices_in_groups(const OffsetIndices<int> groups,
                                             const Span<int> group_to_elem,
@@ -325,17 +286,7 @@ static void reverse_group_indices_in_groups(const OffsetIndices<int> groups,
       }
     }
   });
-  sort_small_groups(offsets, 1024, results);
-}
-
-static GroupedSpan<int> gather_groups(const Span<int> group_indices,
-                                      const int groups_num,
-                                      Array<int> &r_offsets,
-                                      Array<int> &r_indices)
-{
-  r_offsets = create_reverse_offsets(group_indices, groups_num);
-  r_indices = reverse_indices_in_groups(group_indices, r_offsets.as_span());
-  return {OffsetIndices<int>(r_offsets), r_indices};
+  offset_indices::sort_small_groups(offsets, results);
 }
 
 Array<int> build_corner_to_face_map(const OffsetIndices<int> faces)
@@ -365,7 +316,7 @@ GroupedSpan<int> build_vert_to_edge_map(const Span<int2> edges,
       }
     }
   });
-  sort_small_groups(offsets, 1024, r_indices);
+  offset_indices::sort_small_groups(offsets, r_indices);
   return {offsets, r_indices};
 }
 
@@ -392,7 +343,9 @@ GroupedSpan<int> build_vert_to_face_map(const OffsetIndices<int> faces,
 Array<int> build_vert_to_corner_indices(const Span<int> corner_verts,
                                         const OffsetIndices<int> offsets)
 {
-  return reverse_indices_in_groups(corner_verts, offsets);
+  Array<int> results(offsets.total_size());
+  offset_indices::reverse_indices_in_groups(corner_verts, offsets, results);
+  return results;
 }
 
 GroupedSpan<int> build_vert_to_corner_map(const Span<int> corner_verts,
@@ -400,7 +353,7 @@ GroupedSpan<int> build_vert_to_corner_map(const Span<int> corner_verts,
                                           Array<int> &r_offsets,
                                           Array<int> &r_indices)
 {
-  return gather_groups(corner_verts, verts_num, r_offsets, r_indices);
+  return offset_indices::build_groups_from_indices(corner_verts, verts_num, r_offsets, r_indices);
 }
 
 GroupedSpan<int> build_edge_to_corner_map(const Span<int> corner_edges,
@@ -408,7 +361,7 @@ GroupedSpan<int> build_edge_to_corner_map(const Span<int> corner_edges,
                                           Array<int> &r_offsets,
                                           Array<int> &r_indices)
 {
-  return gather_groups(corner_edges, edges_num, r_offsets, r_indices);
+  return offset_indices::build_groups_from_indices(corner_edges, edges_num, r_offsets, r_indices);
 }
 
 GroupedSpan<int> build_edge_to_face_map(const OffsetIndices<int> faces,

source/blender/blenlib/BLI_offset_indices.hh
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <optional>
 
+#include "BLI_array.hh"
 #include "BLI_index_mask_fwd.hh"
 #include "BLI_index_range.hh"
 #include "BLI_span.hh"
@@ -194,7 +195,34 @@ void build_reverse_map(OffsetIndices<int> offsets, MutableSpan<int> r_map);
 /**
  * Build offsets to group the elements of \a indices pointing to the same index.
  */
-void build_reverse_offsets(Span<int> indices, MutableSpan<int> offsets);
+OffsetIndices<int> build_reverse_offsets(Span<int> indices, MutableSpan<int> offsets);
+
+/**
+ * Used as a final step for parallel creation of grouped indices, to make results deterministic as
+ * each group's indices are potentially filled from multiple threads.
+ */
+void sort_small_groups(OffsetIndices<int> groups, MutableSpan<int> indices);
+
+/**
+ * Where the `group_indices` argument maps elements into buckets, and the `offsets` argument
+ * describes the size of each bucket, this function fills `results` with the indices in each bucket
+ * grouped by `offsets`. The `sort` argument makes the results deterministic (i.e. the indices in
+ * each bucket are sorted), otherwise internal parallelism makes this non-deterministic.
+ */
+void reverse_indices_in_groups(Span<int> group_indices,
+                               OffsetIndices<int> offsets,
+                               MutableSpan<int> results,
+                               bool sort = true);
+
+/**
+ * With `indices` divided in a certain number of unique groups, reverse the index mapping so that
+ * the indices in each group can be index by the group index. Similar to
+ * #reverse_indices_in_groups, but also creates the offsets.
+ */
+GroupedSpan<int> build_groups_from_indices(const Span<int> indices,
+                                           const int groups_num,
+                                           Array<int> &offset_data,
+                                           Array<int> &index_data);
 
 }  // namespace offset_indices
 

source/blender/blenlib/intern/offset_indices.cc
@@ -10,6 +10,8 @@
 #include "BLI_offset_indices.hh"
 #include "BLI_task.hh"
 
+#include "atomic_ops.h"
+
 namespace blender::offset_indices {
 
 OffsetIndices<int> accumulate_counts_to_offsets(MutableSpan<int> counts_to_offsets,
@@ -149,11 +151,64 @@ void build_reverse_map(OffsetIndices<int> offsets, MutableSpan<int> r_map)
   });
 }
 
-void build_reverse_offsets(const Span<int> indices, MutableSpan<int> offsets)
+OffsetIndices<int> build_reverse_offsets(const Span<int> indices, MutableSpan<int> offsets)
 {
   BLI_assert(std::all_of(offsets.begin(), offsets.end(), [](int value) { return value == 0; }));
   array_utils::count_indices(indices, offsets);
-  offset_indices::accumulate_counts_to_offsets(offsets);
+  return offset_indices::accumulate_counts_to_offsets(offsets);
+}
+
+void sort_small_groups(const OffsetIndices<int> groups, MutableSpan<int> indices)
+{
+  threading::parallel_for(groups.index_range(), 1024, [&](const IndexRange range) {
+    for (const int64_t index : range) {
+      MutableSpan<int> group = indices.slice(groups[index]);
+      std::ranges::sort(group);
+    }
+  });
+}
+
+void reverse_indices_in_groups(const Span<int> group_indices,
+                               const OffsetIndices<int> offsets,
+                               MutableSpan<int> results,
+                               const bool sort)
+{
+  if (group_indices.is_empty()) {
+    return;
+  }
+  BLI_assert(results.size() == group_indices.size());
+  BLI_assert(results.size() == offsets.total_size());
+  BLI_assert(*std::max_element(group_indices.begin(), group_indices.end()) < offsets.size());
+  BLI_assert(*std::min_element(group_indices.begin(), group_indices.end()) >= 0);
+
+  /* `counts` keeps track of how many elements have been added to each group, and is incremented
+   * atomically by many threads in parallel. `calloc` can be measurably faster than a parallel fill
+   * of zero. Alternatively the offsets could be copied and incremented directly, but the cost of
+   * the copy is slightly higher than the cost of `calloc`. */
+  int *counts = MEM_new_array_zeroed<int>(size_t(offsets.size()), __func__);
+  BLI_SCOPED_DEFER([&]() { MEM_delete(counts); })
+  threading::parallel_for(group_indices.index_range(), 1024, [&](const IndexRange range) {
+    for (const int64_t i : range) {
+      const int group_index = group_indices[i];
+      const int index_in_group = atomic_fetch_and_add_int32(&counts[group_index], 1);
+      results[offsets[group_index][index_in_group]] = int(i);
+    }
+  });
+  if (sort) {
+    sort_small_groups(offsets, results);
+  }
+}
+
+GroupedSpan<int> build_groups_from_indices(const Span<int> indices,
+                                           const int groups_num,
+                                           Array<int> &offset_data,
+                                           Array<int> &index_data)
+{
+  offset_data = Array<int>(groups_num + 1, 0);
+  const OffsetIndices offsets = build_reverse_offsets(indices, offset_data.as_mutable_span());
+  index_data.reinitialize(offsets.total_size());
+  reverse_indices_in_groups(indices, offsets, index_data);
+  return {OffsetIndices<int>(offsets), index_data};
 }
 
 }  // namespace blender::offset_indices

source/blender/blenlib/tests/BLI_offset_indices_test.cc
@@ -24,4 +24,20 @@ TEST(offset_indices, SumSizes)
   EXPECT_EQ(sum_group_sizes(offsets, IndexMask(1)), 3);
 }
 
+TEST(offset_indices, build_groups_from_indices)
+{
+  Vector<int> data = {3, 2, 1, 3, 4, 1, 1, 6, 8, 1, 8, 0};
+  const int groups_num = 10;
+
+  Array<int> offset_data;
+  Array<int> index_data;
+  const GroupedSpan<int> groups = build_groups_from_indices(
+      data, groups_num, offset_data, index_data);
+
+  EXPECT_EQ(groups.size(), groups_num);
+  EXPECT_EQ(groups.offsets.total_size(), data.size());
+  EXPECT_EQ_SPAN(groups[1], {2, 5, 6, 9});
+  EXPECT_TRUE(groups[5].is_empty());
+}
+
 }  // namespace blender::offset_indices::tests

source/blender/editors/grease_pencil/intern/grease_pencil_utils.cc
@@ -1978,16 +1978,8 @@ void apply_eval_grease_pencil_data(const GreasePencil &eval_grease_pencil,
 
   /* Ensure that the layer names are unique by merging layers with the same name. */
   const int old_layers_num = eval_grease_pencil.layers().size();
-  Vector<Vector<int>> layers_map;
-  Map<StringRef, int> new_layer_index_by_name;
-  for (const int layer_i : IndexRange(old_layers_num)) {
-    const Layer &layer = eval_grease_pencil.layer(layer_i);
-    const int new_layer_index = new_layer_index_by_name.lookup_or_add_cb(
-        layer.name(), [&]() { return layers_map.append_and_get_index_as(); });
-    layers_map[new_layer_index].append(layer_i);
-  }
-  GreasePencil &merged_layers_grease_pencil = *geometry::merge_layers(
-      eval_grease_pencil, layers_map, {});
+  GreasePencil &merged_layers_grease_pencil = *geometry::merge_layers_by_name(
+      eval_grease_pencil, VArray<bool>::from_single(true, old_layers_num), {});
 
   Map<const Layer *, const Layer *> eval_to_orig_layer_map;
   {

source/blender/geometry/GEO_merge_layers.hh
@@ -2,7 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BLI_vector.hh"
+#include "BLI_offset_indices.hh"
+#include "BLI_virtual_array.hh"
 
 #include "BKE_attribute_filter.hh"
 
@@ -19,8 +20,12 @@ namespace geometry {
  *   have at least one source layer.
  */
 GreasePencil *merge_layers(const GreasePencil &src_grease_pencil,
-                           Span<Vector<int>> layers_to_merge,
+                           GroupedSpan<int> layers_to_merge,
                            const bke::AttributeFilter &attribute_filter);
+
+GreasePencil *merge_layers_by_name(const GreasePencil &src_grease_pencil,
+                                   const VArray<bool> &selection,
+                                   const bke::AttributeFilter &attribute_filter);
 
 }  // namespace geometry
 }  // namespace blender

source/blender/geometry/intern/merge_layers.cc
@@ -41,7 +41,7 @@ static bke::CurvesGeometry join_curves(const GreasePencil &src_grease_pencil,
 }
 
 GreasePencil *merge_layers(const GreasePencil &src_grease_pencil,
-                           const Span<Vector<int>> layers_to_merge,
+                           const GroupedSpan<int> layers_to_merge,
                            const bke::AttributeFilter &attribute_filter)
 {
   using namespace bke::greasepencil;
@@ -109,17 +109,6 @@ GreasePencil *merge_layers(const GreasePencil &src_grease_pencil,
     }
   });
 
-  /* TODO: Use GroupedSpan for the function argument instead of Span<Vector>. */
-  Vector<int> offsets;
-  Vector<int> all_indices;
-  offsets.reserve(layers_to_merge.size());
-  offsets.append(0);
-  for (const Span<int> indices : layers_to_merge) {
-    all_indices.extend(indices);
-    offsets.append(indices.size());
-  }
-  const GroupedSpan<int> src_layers_by_dst(OffsetIndices<int>(offsets), all_indices);
-
   const bke::AttributeAccessor src_attributes = src_grease_pencil.attributes();
   bke::MutableAttributeAccessor new_attributes = new_grease_pencil->attributes_for_write();
   src_attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
@@ -130,11 +119,39 @@ GreasePencil *merge_layers(const GreasePencil &src_grease_pencil,
     bke::GSpanAttributeWriter new_attribute = new_attributes.lookup_or_add_for_write_only_span(
         iter.name, bke::AttrDomain::Layer, iter.data_type);
     bke::attribute_math::mix_groups(
-        GVArraySpan(src_attribute.varray), src_layers_by_dst, new_attribute.span);
+        GVArraySpan(src_attribute.varray), layers_to_merge, new_attribute.span);
     new_attribute.finish();
   });
 
   return new_grease_pencil;
+}
+
+GreasePencil *merge_layers_by_name(const GreasePencil &src_grease_pencil,
+                                   const VArray<bool> &selection,
+                                   const bke::AttributeFilter &attribute_filter)
+{
+  using namespace bke::greasepencil;
+  const int old_layers_num = src_grease_pencil.layers().size();
+
+  Array<int> layer_to_group(old_layers_num);
+  Map<StringRef, int> name_to_group_index;
+  int groups_num = 0;
+  for (const int i : IndexRange(old_layers_num)) {
+    if (selection[i]) {
+      const Layer &layer = src_grease_pencil.layer(i);
+      layer_to_group[i] = name_to_group_index.lookup_or_add_cb(layer.name(),
+                                                               [&]() { return groups_num++; });
+    }
+    else {
+      layer_to_group[i] = groups_num++;
+    }
+  }
+  Array<int> offset_data;
+  Array<int> index_data;
+  const GroupedSpan<int> src_groups = offset_indices::build_groups_from_indices(
+      layer_to_group, groups_num, offset_data, index_data);
+
+  return merge_layers(src_grease_pencil, src_groups, attribute_filter);
 }
 
 }  // namespace blender::geometry

FrenkyBojler/blender-ios

commit 7aeda64a0a220e63b366b66e575bd84dfa5ef494

Fix: Incorrect face adjacency indices in subdivision draw cache
The refactoring in 4478dd3f9a48 incorrectly used loop indices instead
of face indices for the subdivision vertex-face adjacency map. This
caused the normals accumulation shader to read from incorrect memory
locations.

This issue was introduced in 4478dd3f9a48.

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -735,6 +735,12 @@ static void build_vert_face_adjacency_maps(DRWSubdivCache &cache)
   MutableSpan<int> adjacent_faces = cache.subdiv_vert_face_adjacency->data<int>();
 
   offset_indices::reverse_indices_in_groups(loop_to_vert, offsets, adjacent_faces);
+
+  /* The shader expects face indices, but `reverse_indices_in_groups` stored loop indices.
+   * Since this is a quad mesh, divide the loop index by 4 to get the face index. */
+  for (int &face_index : adjacent_faces) {
+    face_index /= 4;
+  }
 }
 
 static bool draw_subdiv_build_cache(DRWSubdivCache &cache,