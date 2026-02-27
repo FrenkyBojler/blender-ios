daf304d86463912fcb3fdd99fa6bad3592765c7c
Pratik Borhade <pratikborhade302@gmail.com>
2026-02-27 09:57:38 +0530

    Fix: Redundant collapsible dopesheet overlay panel
    Caused by c31dcbc0fa. Fix is same as !154299

--- scripts/startup/bl_ui/space_dopesheet.py
+++ scripts/startup/bl_ui/space_dopesheet.py
@@ -1071,6 +1071,7 @@ class DOPESHEET_PT_dopesheet_overlay(Panel):
     bl_region_type = 'HEADER'
     bl_parent_id = "DOPESHEET_PT_overlay"
     bl_label = "Dope Sheet Overlays"
+    bl_options = {'HIDE_HEADER'}
 
     def draw(self, context):
         st = context.space_data
