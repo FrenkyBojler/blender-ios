blender/blender

commit 25773a8bc611a61566bc469d975dc7f63ab39075

Attributes: Utility function to mix indices
The goal is to start to remove duplication of the many places we
currently implement mixing propagation of attributes. The API function
I started with mixes the values for a group of result geometry elements,
using index and weight spans broken into groups by offset indices.
Processing a small group of result elements together seems like a
good balance of peak memory usage and hot loop / SIMD optimization
opportunities.

There are still many other places mixing attribute values. They often
have one extra level of indexing, or with the current code structure
only one result value is computed at a time. It will take more work to
generalize those places as well.

As is, this PR saves about 240KB of binary size, and that will scale
better as new attribute types or storage methods are added.

Pull Request: https://projects.blender.org/blender/blender/pulls/154888

source/blender/geometry/intern/add_curves_on_mesh.cc
@@ -57,8 +57,11 @@ static void calc_straight_curve_positions(const float3 &a,
   }
 }
 
-static Array<NeighborCurves> find_curve_neighbors(const Span<float3> root_positions,
-                                                  const KDTree_3d &old_roots_kdtree)
+static void find_curve_neighbors(const Span<float3> root_positions,
+                                 const KDTree_3d &old_roots_kdtree,
+                                 Vector<int> &offset_data,
+                                 Vector<int> &index_data,
+                                 Vector<float> &weight_data)
 {
   const int tot_added_curves = root_positions.size();
   Array<NeighborCurves> neighbors_per_curve(tot_added_curves);
@@ -81,30 +84,23 @@ static Array<NeighborCurves> find_curve_neighbors(const Span<float3> root_positi
       }
     }
   });
-  return neighbors_per_curve;
-}
-
-template<typename T, typename GetValueF>
-void interpolate_from_neighbor_curves(const Span<NeighborCurves> neighbors_per_curve,
-                                      const T &fallback,
-                                      const GetValueF &get_value_from_neighbor,
-                                      MutableSpan<T> r_interpolated_values)
-{
-  bke::attribute_math::DefaultMixer<T> mixer{r_interpolated_values};
-  threading::parallel_for(r_interpolated_values.index_range(), 512, [&](const IndexRange range) {
+  offset_data.resize(tot_added_curves + 1);
+  threading::parallel_for(neighbors_per_curve.index_range(), 4096, [&](const IndexRange range) {
     for (const int i : range) {
-      const NeighborCurves &neighbors = neighbors_per_curve[i];
-      if (neighbors.is_empty()) {
-        mixer.mix_in(i, fallback, 1.0f);
-      }
-      else {
-        for (const NeighborCurve &neighbor : neighbors) {
-          const T neighbor_value = get_value_from_neighbor(neighbor.index);
-          mixer.mix_in(i, neighbor_value, neighbor.weight);
-        }
+      offset_data[i] = neighbors_per_curve[i].size();
+    }
+  });
+  const OffsetIndices offsets = offset_indices::accumulate_counts_to_offsets(offset_data);
+  index_data.resize(offsets.total_size());
+  weight_data.resize(offsets.total_size());
+  threading::parallel_for(offsets.index_range(), 2048, [&](const IndexRange range) {
+    for (const int dst_i : range) {
+      const IndexRange neighbor_range = offsets[dst_i];
+      for (const int i : neighbor_range.index_range()) {
+        index_data[neighbor_range[i]] = neighbors_per_curve[dst_i][i].index;
+        weight_data[neighbor_range[i]] = neighbors_per_curve[dst_i][i].weight;
       }
     }
-    mixer.finalize(range);
   });
 }
 
@@ -136,7 +132,9 @@ static void calc_position_without_interpolation(CurvesGeometry &curves,
 
 static void calc_position_with_interpolation(CurvesGeometry &curves,
                                              const Span<float3> root_positions_cu,
-                                             const Span<NeighborCurves> neighbors_per_curve,
+                                             const OffsetIndices<int> neighbor_offsets,
+                                             const Span<int> neighbor_indices,
+                                             const Span<float> neighbor_weights,
                                              const int old_curves_num,
                                              const Span<float> new_lengths_cu,
                                              const Span<float3> new_normals_su,
@@ -153,7 +151,7 @@ static void calc_position_with_interpolation(CurvesGeometry &curves,
 
   threading::parallel_for(IndexRange(added_curves_num), 256, [&](const IndexRange range) {
     for (const int added_curve_i : range) {
-      const NeighborCurves &neighbors = neighbors_per_curve[added_curve_i];
+      const IndexRange neighbors = neighbor_offsets[added_curve_i];
       const int curve_i = old_curves_num + added_curve_i;
       const IndexRange points = points_by_curve[curve_i];
 
@@ -173,8 +171,9 @@ static void calc_position_with_interpolation(CurvesGeometry &curves,
 
       positions_cu.slice(points).fill(root_cu);
 
-      for (const NeighborCurve &neighbor : neighbors) {
-        const int neighbor_curve_i = neighbor.index;
+      for (const int neighbor_i : neighbors) {
+        const int neighbor_curve_i = neighbor_indices[neighbor_i];
+        const int neighbor_weight = neighbor_weights[neighbor_i];
         const float2 neighbor_uv = uv_coords[neighbor_curve_i];
         const ReverseUVSampler::Result result = reverse_uv_sampler.sample(neighbor_uv);
         if (result.type != ReverseUVSampler::ResultType::Ok) {
@@ -228,7 +227,7 @@ static void calc_position_with_interpolation(CurvesGeometry &curves,
           const float3 relative_to_root_cu = sample_cu - neighbor_root_cu;
           float3 rotated_relative_coord = relative_to_root_cu;
           mul_m3_v3(normal_rotation_cu, rotated_relative_coord);
-          positions_cu[points[i]] += neighbor.weight * rotated_relative_coord;
+          positions_cu[points[i]] += neighbor_weight * rotated_relative_coord;
         }
       }
     }
@@ -247,7 +246,9 @@ static void calc_radius_with_interpolation(CurvesGeometry &curves,
                                            const int old_curves_num,
                                            const float radius,
                                            const Span<float> new_lengths_cu,
-                                           const Span<NeighborCurves> neighbors_per_curve)
+                                           const OffsetIndices<int> neighbor_offsets,
+                                           const Span<int> neighbor_indices,
+                                           const Span<float> neighbor_weights)
 {
   const int added_curves_num = new_lengths_cu.size();
   const OffsetIndices points_by_curve = curves.points_by_curve();
@@ -262,7 +263,7 @@ static void calc_radius_with_interpolation(CurvesGeometry &curves,
 
   threading::parallel_for(IndexRange(added_curves_num), 256, [&](const IndexRange range) {
     for (const int i : range) {
-      const NeighborCurves &neighbors = neighbors_per_curve[i];
+      const IndexRange neighbors = neighbor_offsets[i];
       const float length_cu = new_lengths_cu[i];
       const int curve_i = old_curves_num + i;
       const IndexRange points = points_by_curve[curve_i];
@@ -275,8 +276,9 @@ static void calc_radius_with_interpolation(CurvesGeometry &curves,
 
       radii_cu.slice(points).fill(0.0f);
 
-      for (const NeighborCurve &neighbor : neighbors) {
-        const int neighbor_curve_i = neighbor.index;
+      for (const int neighbor_i : neighbors) {
+        const int neighbor_curve_i = neighbor_indices[neighbor_i];
+        const int neighbor_weight = neighbor_weights[neighbor_i];
         const IndexRange neighbor_points = points_by_curve[neighbor_curve_i];
         const Span<float3> neighbor_positions_cu = positions_cu.slice(neighbor_points);
         const Span<float> neighbor_radii_cu = radius_attr.span.slice(neighbor_points);
@@ -301,7 +303,7 @@ static void calc_radius_with_interpolation(CurvesGeometry &curves,
           const float sample_cu = math::interpolate(
               neighbor_radii_cu[indices[i]], neighbor_radii_cu[indices[i] + 1], factors[i]);
 
-          radii_cu[points[i]] += neighbor.weight * sample_cu;
+          radii_cu[points[i]] += neighbor_weight * sample_cu;
         }
       }
     }
@@ -346,10 +348,16 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
     used_uvs.append(uv);
   }
 
-  Array<NeighborCurves> neighbors_per_curve;
+  Vector<int> curve_neighbor_offset_data;
+  Vector<int> curve_neighbor_index_data;
+  Vector<float> curve_neighbor_weight_data;
   if (use_interpolation) {
     BLI_assert(inputs.old_roots_kdtree != nullptr);
-    neighbors_per_curve = find_curve_neighbors(root_positions_cu, *inputs.old_roots_kdtree);
+    find_curve_neighbors(root_positions_cu,
+                         *inputs.old_roots_kdtree,
+                         curve_neighbor_offset_data,
+                         curve_neighbor_index_data,
+                         curve_neighbor_weight_data);
   }
 
   const int added_curves_num = root_positions_cu.size();
@@ -368,11 +376,13 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
   Array<int> new_point_counts_per_curve(added_curves_num);
   if (inputs.interpolate_point_count && old_curves_num > 0) {
     const OffsetIndices<int> old_points_by_curve{curve_offsets.take_front(old_curves_num + 1)};
-    interpolate_from_neighbor_curves<int>(
-        neighbors_per_curve,
-        inputs.fallback_point_count,
-        [&](const int curve_i) { return old_points_by_curve[curve_i].size(); },
-        new_point_counts_per_curve);
+    Array<int> sizes(old_curves_num);
+    offset_indices::copy_group_sizes(old_points_by_curve, sizes.index_range(), sizes);
+    bke::attribute_math::mix_groups(sizes.as_span(),
+                                    OffsetIndices(curve_neighbor_offset_data.as_span()),
+                                    curve_neighbor_index_data.as_span(),
+                                    curve_neighbor_weight_data.as_span(),
+                                    new_point_counts_per_curve.as_mutable_span());
   }
   else {
     new_point_counts_per_curve.fill(inputs.fallback_point_count);
@@ -401,20 +411,24 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
   Span<float3> positions_cu = curves.positions();
   Array<float> new_lengths_cu(added_curves_num);
   if (inputs.interpolate_length) {
-    interpolate_from_neighbor_curves<float>(
-        neighbors_per_curve,
-        inputs.fallback_curve_length,
-        [&](const int curve_i) {
-          const IndexRange points = points_by_curve[curve_i];
-          float length = 0.0f;
-          for (const int segment_i : points.drop_back(1)) {
-            const float3 &p1 = positions_cu[segment_i];
-            const float3 &p2 = positions_cu[segment_i + 1];
-            length += math::distance(p1, p2);
-          }
-          return length;
-        },
-        new_lengths_cu);
+    Array<float> lengths(old_curves_num);
+    threading::parallel_for(IndexRange(old_curves_num), 256, [&](const IndexRange range) {
+      for (const int curve_i : range) {
+        const IndexRange points = points_by_curve[curve_i];
+        float length = 0.0f;
+        for (const int segment_i : points.drop_back(1)) {
+          const float3 &p1 = positions_cu[segment_i];
+          const float3 &p2 = positions_cu[segment_i + 1];
+          length += math::distance(p1, p2);
+        }
+        lengths[curve_i] = length;
+      }
+    });
+    bke::attribute_math::mix_groups(lengths.as_span(),
+                                    OffsetIndices(curve_neighbor_offset_data.as_span()),
+                                    curve_neighbor_index_data.as_span(),
+                                    curve_neighbor_weight_data.as_span(),
+                                    new_lengths_cu.as_mutable_span());
   }
   else {
     new_lengths_cu.fill(inputs.fallback_curve_length);
@@ -433,7 +447,9 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
   if (inputs.interpolate_shape) {
     calc_position_with_interpolation(curves,
                                      root_positions_cu,
-                                     neighbors_per_curve,
+                                     OffsetIndices(curve_neighbor_offset_data.as_span()),
+                                     curve_neighbor_index_data.as_span(),
+                                     curve_neighbor_weight_data.as_span(),
                                      old_curves_num,
                                      new_lengths_cu,
                                      new_normals_su,
@@ -453,8 +469,13 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
 
   /* Initialize radius attribute */
   if (inputs.interpolate_radius) {
-    calc_radius_with_interpolation(
-        curves, old_curves_num, inputs.fallback_curve_radius, new_lengths_cu, neighbors_per_curve);
+    calc_radius_with_interpolation(curves,
+                                   old_curves_num,
+                                   inputs.fallback_curve_radius,
+                                   new_lengths_cu,
+                                   OffsetIndices(curve_neighbor_offset_data.as_span()),
+                                   curve_neighbor_index_data.as_span(),
+                                   curve_neighbor_weight_data.as_span());
   }
   else {
     calc_radius_without_interpolation(
@@ -469,11 +490,11 @@ AddCurvesOnMeshOutputs add_curves_on_mesh(CurvesGeometry &curves,
           "resolution"))
   {
     if (inputs.interpolate_resolution) {
-      interpolate_from_neighbor_curves(
-          neighbors_per_curve,
-          12,
-          [&](const int curve_i) { return resolution.span[curve_i]; },
-          resolution.span.take_back(added_curves_num));
+      bke::attribute_math::mix_groups(resolution.span,
+                                      OffsetIndices(curve_neighbor_offset_data.as_span()),
+                                      curve_neighbor_index_data.as_span(),
+                                      curve_neighbor_weight_data.as_span(),
+                                      resolution.span.take_back(added_curves_num));
       resolution.finish();
     }
     else {

source/blender/blenkernel/BKE_attribute_math.hh
@@ -697,6 +697,23 @@ using DefaultPropagationMixer = typename DefaultPropagationMixerStruct<T>::type;
  * mixer for the given type. */
 template<typename T> using DefaultMixer = typename DefaultMixerStruct<T>::type;
 
+void mix_groups(GSpan src,
+                OffsetIndices<int> groups,
+                Span<int> all_indices,
+                std::optional<Span<float>> all_weights,
+                GMutableSpan dst);
+inline void mix_groups(GSpan src,
+                       OffsetIndices<int> groups,
+                       Span<int> all_indices,
+                       GMutableSpan dst)
+{
+  mix_groups(src, groups, all_indices, std::nullopt, dst);
+}
+inline void mix_groups(GSpan src, GroupedSpan<int> indices, GMutableSpan dst)
+{
+  mix_groups(src, indices.offsets, indices.data, dst);
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */

source/blender/blenkernel/intern/attribute_math.cc
@@ -256,6 +256,50 @@ void float4x4Mixer::finalize(const IndexMask &mask)
   });
 }
 
+template<typename T>
+void mix_groups(const Span<T> src,
+                const OffsetIndices<int> groups,
+                const Span<int> all_indices,
+                const std::optional<Span<float>> all_weights,
+                MutableSpan<T> dst)
+{
+  DefaultPropagationMixer<T> mixer(dst);
+  if (all_weights) {
+    for (const int dst_i : groups.index_range()) {
+      const Span<int> indices = all_indices.slice(groups[dst_i]);
+      for (const int i : indices.index_range()) {
+        const int src_i = indices[i];
+        mixer.mix_in(dst_i, src[src_i], (*all_weights)[i]);
+      }
+    }
+  }
+  else {
+    for (const int dst_i : groups.index_range()) {
+      for (const int src_i : all_indices.slice(groups[dst_i])) {
+        mixer.mix_in(dst_i, src[src_i]);
+      }
+    }
+  }
+  mixer.finalize();
+}
+
+void mix_groups(const GSpan src,
+                const OffsetIndices<int> groups,
+                const Span<int> all_indices,
+                const std::optional<Span<float>> all_weights,
+                GMutableSpan dst)
+{
+  BLI_assert(groups.size() == dst.size());
+  BLI_assert(groups.total_size() == all_indices.size());
+  BLI_assert(!all_weights || groups.total_size() == all_weights->size());
+
+  to_static_type(src.type(), [&]<typename T>() {
+    if constexpr (!std::is_void_v<DefaultMixer<T>>) {
+      mix_groups(src.typed<T>(), groups, all_indices, all_weights, dst.typed<T>());
+    }
+  });
+}
+
 void gather(const GSpan src, const Span<int> map, GMutableSpan dst)
 {
   gather(GVArray::from_span(src), map, IndexRange(dst.size()), dst);

source/blender/editors/grease_pencil/intern/grease_pencil_geom.cc
@@ -320,32 +320,16 @@ bke::CurvesGeometry curves_merge_by_distance(const bke::CurvesGeometry &src_curv
       }
     }
 
-    bke::attribute_math::to_static_type(src_attribute.varray.type(), [&]<typename T>() {
-      if constexpr (!std::is_void_v<bke::attribute_math::DefaultMixer<T>>) {
-        bke::SpanAttributeWriter<T> dst_attribute =
-            dst_attributes.lookup_or_add_for_write_only_span<T>(iter.name, bke::AttrDomain::Point);
-        BLI_assert(dst_attribute);
-        VArraySpan<T> src = src_attribute.varray.typed<T>();
-
-        threading::parallel_for(dst_curves.points_range(), 1024, [&](IndexRange range) {
-          for (const int dst_point_i : range) {
-            /* Create a separate mixer for every point to avoid allocating temporary buffers
-             * in the mixer the size of the result curves and to improve memory locality. */
-            bke::attribute_math::DefaultMixer<T> mixer{dst_attribute.span.slice(dst_point_i, 1)};
-
-            Span<int> src_merge_indices = merge_map_indices.as_span().slice(
-                map_offsets[dst_point_i]);
-            for (const int src_point_i : src_merge_indices) {
-              mixer.mix_in(0, src[src_point_i]);
-            }
-
-            mixer.finalize();
-          }
-        });
-
-        dst_attribute.finish();
-      }
+    bke::GSpanAttributeWriter dst_attribute = dst_attributes.lookup_or_add_for_write_only_span(
+        iter.name, bke::AttrDomain::Point, iter.data_type);
+    threading::parallel_for(dst_curves.points_range(), 1024, [&](IndexRange range) {
+      bke::attribute_math::mix_groups(GVArraySpan(src_attribute.varray),
+                                      map_offsets.slice(range),
+                                      merge_map_indices,
+                                      std::nullopt,
+                                      dst_attribute.span.slice(range));
     });
+    dst_attribute.finish();
   });
 
   if (dst_curves.nurbs_has_custom_knots()) {

source/blender/editors/grease_pencil/intern/grease_pencil_merge.cc
@@ -348,6 +348,7 @@ void merge_layers(const GreasePencil &src_grease_pencil,
       return;
     }
 
+    /* Note: Doesn't use #bke::attribute_math::mix_groups because of `old_to_new_index_map`. */
     const CPPType &type = dst_attribute.span.type();
     bke::attribute_math::to_static_type(type, [&]<typename T>() {
       if constexpr (!std::is_void_v<bke::attribute_math::DefaultMixer<T>>) {

source/blender/geometry/intern/merge_layers.cc
@@ -109,39 +109,28 @@ GreasePencil *merge_layers(const GreasePencil &src_grease_pencil,
     }
   });
 
+  /* TODO: Use GroupedSpan for the function argument instead of Span<Vector>. */
+  Vector<int> offsets;
+  Vector<int> all_indices;
+  offsets.reserve(layers_to_merge.size());
+  offsets.append(0);
+  for (const Span<int> indices : layers_to_merge) {
+    all_indices.extend(indices);
+    offsets.append(indices.size());
+  }
+  const GroupedSpan<int> src_layers_by_dst(OffsetIndices<int>(offsets), all_indices);
+
   const bke::AttributeAccessor src_attributes = src_grease_pencil.attributes();
   bke::MutableAttributeAccessor new_attributes = new_grease_pencil->attributes_for_write();
   src_attributes.foreach_attribute([&](const bke::AttributeIter &iter) {
-    if (iter.data_type == bke::AttrType::String) {
-      return;
-    }
     if (attribute_filter.allow_skip(iter.name)) {
       return;
     }
     bke::GAttributeReader src_attribute = iter.get();
     bke::GSpanAttributeWriter new_attribute = new_attributes.lookup_or_add_for_write_only_span(
         iter.name, bke::AttrDomain::Layer, iter.data_type);
-
-    const CPPType &type = new_attribute.span.type();
-
-    bke::attribute_math::to_static_type(type, [&]<typename T>() {
-      if constexpr (!std::is_void_v<bke::attribute_math::DefaultMixer<T>>) {
-        const VArraySpan<T> src_span = src_attribute.varray.typed<T>();
-        MutableSpan<T> new_span = new_attribute.span.typed<T>();
-
-        bke::attribute_math::DefaultMixer<T> mixer(new_span);
-        for (const int new_layer_i : IndexRange(new_layers_num)) {
-          const Span<int> src_layer_indices = layers_to_merge[new_layer_i];
-          for (const int src_layer_i : src_layer_indices) {
-            const T &src_value = src_span[src_layer_i];
-            mixer.mix_in(new_layer_i, src_value);
-          }
-        }
-
-        mixer.finalize();
-      }
-    });
-
+    bke::attribute_math::mix_groups(
+        GVArraySpan(src_attribute.varray), src_layers_by_dst, new_attribute.span);
     new_attribute.finish();
   });
 

source/blender/geometry/intern/mesh_merge_by_distance.cc
@@ -1449,32 +1449,6 @@ static void copy_first_from_src(const Span<T> src,
   }
 }
 
-static void mix_src_indices(const GSpan src_attr,
-                            const GroupedSpan<int> dst_to_src,
-                            GMutableSpan dst_attr)
-{
-  bke::attribute_math::to_static_type(src_attr.type(), [&]<typename T>() {
-    if constexpr (!std::is_void_v<bke::attribute_math::DefaultMixer<T>>) {
-      const Span<T> src = src_attr.typed<T>();
-      MutableSpan<T> dst = dst_attr.typed<T>();
-      threading::parallel_for(dst.index_range(), 2048, [&](const IndexRange range) {
-        for (const int dst_index : range) {
-          const Span<int> src_indices = dst_to_src[dst_index];
-          if (src_indices.size() == 1) {
-            dst[dst_index] = src[src_indices.first()];
-            continue;
-          }
-          bke::attribute_math::DefaultMixer<T> mixer({&dst[dst_index], 1});
-          for (const int src_index : src_indices) {
-            mixer.mix_in(0, src[src_index]);
-          }
-          mixer.finalize();
-        }
-      });
-    }
-  });
-}
-
 static void mix_attributes(const bke::AttributeAccessor src_attributes,
                            const GroupedSpan<int> dst_to_src,
                            const bke::AttrDomain domain,
@@ -1496,10 +1470,9 @@ static void mix_attributes(const bke::AttributeAccessor src_attributes,
         return;
       }
     }
-    const GVArraySpan src_span = src_attr;
     bke::GSpanAttributeWriter dst_attr = dst_attributes.lookup_or_add_for_write_only_span(
         iter.name, iter.domain, iter.data_type);
-    mix_src_indices(src_span, dst_to_src, dst_attr.span);
+    bke::attribute_math::mix_groups(GVArraySpan(src_attr), dst_to_src, dst_attr.span);
     dst_attr.finish();
   });
 }

source/blender/geometry/intern/point_merge_by_distance.cc
@@ -141,30 +141,11 @@ PointCloud *point_merge_by_distance(const PointCloud &src_points,
       }
     }
 
-    bke::attribute_math::to_static_type(type, [&]<typename T>() {
-      if constexpr (!std::is_void_v<bke::attribute_math::DefaultMixer<T>>) {
-        bke::SpanAttributeWriter<T> dst_attribute =
-            dst_attributes.lookup_or_add_for_write_only_span<T>(id, bke::AttrDomain::Point);
-        VArraySpan<T> src = src_attribute.varray.typed<T>();
-
-        threading::parallel_for(IndexRange(dst_size), 1024, [&](IndexRange range) {
-          for (const int i_dst : range) {
-            /* Create a separate mixer for every point to avoid allocating temporary buffers
-             * in the mixer the size of the result point cloud and to improve memory locality. */
-            bke::attribute_math::DefaultMixer<T> mixer{dst_attribute.span.slice(i_dst, 1)};
-
-            Span<int> src_merge_indices = merge_map_indices.as_span().slice(map_offsets[i_dst]);
-            for (const int i_src : src_merge_indices) {
-              mixer.mix_in(0, src[i_src]);
-            }
-
-            mixer.finalize();
-          }
-        });
-
-        dst_attribute.finish();
-      }
-    });
+    bke::GSpanAttributeWriter dst_attribute = dst_attributes.lookup_or_add_for_write_only_span(
+        id, bke::AttrDomain::Point, type);
+    bke::attribute_math::mix_groups(
+        GVArraySpan(src_attribute.varray), map_offsets, merge_map_indices, dst_attribute.span);
+    dst_attribute.finish();
   }
 
   debug_randomize_point_order(dst_pointcloud);

FrenkyBojler/blender-ios

commit ffb492cea792ec2678da565bea26cb733c688cbc

Fix: Add Curves on Mesh: precision loss in interpolation
The 'neighbor_weight' was erroneously truncated to 'int' during
the refactoring, leading to incorrect interpolation results.

This issue was introduced in 25773a8bc61.

source/blender/geometry/intern/add_curves_on_mesh.cc
@@ -173,7 +173,7 @@ static void calc_position_with_interpolation(CurvesGeometry &curves,
 
       for (const int neighbor_i : neighbors) {
         const int neighbor_curve_i = neighbor_indices[neighbor_i];
-        const int neighbor_weight = neighbor_weights[neighbor_i];
+        const float neighbor_weight = neighbor_weights[neighbor_i];
         const float2 neighbor_uv = uv_coords[neighbor_curve_i];
         const ReverseUVSampler::Result result = reverse_uv_sampler.sample(neighbor_uv);
         if (result.type != ReverseUVSampler::ResultType::Ok) {
@@ -278,7 +278,7 @@ static void calc_radius_with_interpolation(CurvesGeometry &curves,
 
       for (const int neighbor_i : neighbors) {
         const int neighbor_curve_i = neighbor_indices[neighbor_i];
-        const int neighbor_weight = neighbor_weights[neighbor_i];
+        const float neighbor_weight = neighbor_weights[neighbor_i];
         const IndexRange neighbor_points = points_by_curve[neighbor_curve_i];
         const Span<float3> neighbor_positions_cu = positions_cu.slice(neighbor_points);
         const Span<float> neighbor_radii_cu = radius_attr.span.slice(neighbor_points);