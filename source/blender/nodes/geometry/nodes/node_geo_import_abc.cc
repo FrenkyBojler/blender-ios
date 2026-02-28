8d8198896012a4e9f1d27235905b3ca735755ba7
Cartesian Caramel <cartesiancaramel@gmail.com>
2026-02-27 21:38:59 -0500

    adapt obj import

--- source/blender/nodes/geometry/nodes/node_geo_import_abc.cc
+++ source/blender/nodes/geometry/nodes/node_geo_import_abc.cc
@@ -57,15 +57,55 @@ class LoadAbcCache : public memory_cache::CachedValue {
 
 static void node_geo_exec(GeoNodeExecParams params)
 {
+  bool is_sequence = params.extract_input<bool>("Sequence");
+  bool override_frame = params.extract_input<bool>("Override Frame");
+  float frame = params.extract_input<float>("Override Frame F");
+  float frame_offset = params.extract_input<float>("Frame Offset");
+  std::string velocity_name = params.extract_input<std::string>("Velocity Attribute");
+  float velocity_scale = params.extract_input<float>("Velocity Scale");
+
 #ifdef WITH_ALEMBIC
-  /*
+  const std::optional<std::string> path = params.ensure_absolute_path(
+      params.extract_input<std::string>("Path"));
+  if (!path) {
+    params.set_default_remaining_outputs();
+    return;
+  }
 
-    for (const geo_eval_log::NodeWarning &warning : cached_value->warnings) {
-      params.error_message_add(warning.type, warning.message);
-    }
+  std::shared_ptr<const LoadAbcCache> cached_value = memory_cache::get_loaded<LoadAbcCache>(
+      GenericStringKey{"import_abc_node"}, {StringRefNull(*path)}, [&]() {
+        AlembicImportParams import_params;
+        // STRNCPY(import_params.paths, path->c_str());
+        import_params.is_sequence = is_sequence;
+        import_params.global_scale = 1.0f;
 
-    params.set_output("Instances", cached_value->geometry);
-*/
+        ABCReadParams params;
+        params.time = time;
+        params.velocity_name = velocity_name.c_str();
+        params.velocity_scale = velocity_scale;
+
+        Vector<bke::GeometrySet> geometries;
+        // OBJ_import_geometries(&import_params, geometries);
+
+        auto instances = std::make_unique<bke::Instances>(geometries.size());
+        MutableSpan<int> handles = instances->reference_handles_for_write();
+        instances->transforms_for_write().fill(float4x4::identity());
+        for (const int i : geometries.index_range()) {
+          handles[i] = instances->add_reference(bke::InstanceReference{std::move(geometries[i])});
+        }
+
+        auto cached_value = std::make_unique<LoadAbcCache>();
+        cached_value->geometry = GeometrySet::from_instances(std::move(instances));
+
+        return cached_value;
+      });
+
+  for (const geo_eval_log::NodeWarning &warning : cached_value->warnings) {
+    params.error_message_add(warning.type, warning.message);
+  }
+
+  params.set_output("Instances", cached_value->geometry);
+
   else params.error_message_add(NodeWarningType::Error,
                                 TIP_("Disabled, Blender was compiled without ABC I/O"));
   params.set_default_remaining_outputs();
