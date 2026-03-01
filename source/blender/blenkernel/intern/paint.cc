blender/blender

commit a9ee06a8eb7fce3c2b1c37f076cdcbd9a45bbba2

Refactor: Simplify `deform_cos` assignment in `sculpt_update_object`
With the refactor in 5fdf7deb57e809dea2d7aeff3a9036bca0d5aa51,
specialized handling of the "deformed" BVH no longer exists and the code
can be simplified.

Pull Request: https://projects.blender.org/blender/blender/pulls/152958

source/blender/blenkernel/intern/paint.cc
@@ -2628,20 +2628,8 @@ static void sculpt_update_object(Depsgraph *depsgraph,
   if (ss.shapekey_active != nullptr && ss.deform_cos.is_empty()) {
     ss.deform_cos = Span(static_cast<const float3 *>(ss.shapekey_active->data),
                          mesh_orig->verts_num);
-  }
-
-  /* if pbvh is deformed, key block is already applied to it */
-  if (ss.shapekey_active) {
-    if (ss.deform_cos.is_empty()) {
-      const Span key_data(static_cast<const float3 *>(ss.shapekey_active->data),
-                          mesh_orig->verts_num);
-
-      if (key_data.data() != nullptr) {
-        BKE_pbvh_vert_coords_apply(pbvh, key_data);
-        if (ss.deform_cos.is_empty()) {
-          ss.deform_cos = key_data;
-        }
-      }
+    if (!ss.deform_cos.is_empty()) {
+      BKE_pbvh_vert_coords_apply(pbvh, ss.deform_cos);
     }
   }
 

FrenkyBojler/blender-ios

commit 90c791516402121416aca52cebbac24a46cfdb20

Fix #155018: Crash when activating dyntopo on mesh with shape key
Introduced with a9ee06a8eb7fce3c2b1c37f076cdcbd9a45bbba2

Prior to the above commit, the BVH coordinates were not updated during
`sculpt_update_object`. `BKE_pbvh_vert_coords_apply` is only valid for
Mesh sculpting, resulting in a bad variant exception if called when
dyntopo is active.

To fix this, this commit makes the following changes:
* Only assign shape key information to `ss.deform_cos` if dyntopo is not
  active
* Clear `ss.deform_cos` and `ss.deform_imats` when entering dyntopo.

This is the rather minimal change to fix the crash for the release
branch. Further work should be done here to ensure `ss.shapekey_active`
is nullptr when Dyntopo is active, but that change has a larger surface
area.

source/blender/blenkernel/intern/paint.cc
@@ -2624,7 +2624,7 @@ static void sculpt_update_object(Depsgraph *depsgraph,
     BKE_sculptsession_free_deformMats(&ss);
   }
 
-  if (ss.shapekey_active != nullptr && ss.deform_cos.is_empty()) {
+  if (ss.bm == nullptr && ss.shapekey_active != nullptr && ss.deform_cos.is_empty()) {
     ss.deform_cos = Span(static_cast<const float3 *>(ss.shapekey_active->data),
                          mesh_orig->verts_num);
     if (!ss.deform_cos.is_empty()) {

source/blender/editors/sculpt_paint/mesh/sculpt_dyntopo.cc
@@ -65,6 +65,7 @@ void enable_ex(Main &bmain, Depsgraph &depsgraph, Object &ob)
   const BMAllocTemplate allocsize = BMALLOC_TEMPLATE_FROM_ME(mesh);
 
   BKE_sculptsession_free_pbvh(ob);
+  BKE_sculptsession_free_deformMats(&ss);
 
   /* Dynamic topology doesn't ensure selection state is valid, so remove #36280. */
   BKE_mesh_mselect_clear(mesh);