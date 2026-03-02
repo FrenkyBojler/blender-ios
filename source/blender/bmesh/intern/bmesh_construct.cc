8d2632823cafbddb3d1c1b4df3b12b9f0a16a2cc
Philipp Oeser <philipp@blender.org>
2026-03-02 15:01:09 +0100

    Fix #155039: "Separate" may create a second Basis shape key
    Somewhat exposed by 43d1685bc318 (in "Separate by Selection").
    Actually present in 67c7fe6ad62e as well though (in "Separate by loose
    parts").
    
    In order to not loose custom normals in the
    `BM_custom_loop_normals_to_vector_layer` /
    `BM_custom_loop_normals_from_vector_layer` dance, we are using
    `BM_mesh_copy_init_customdata_all_layers` (instead of
    `BM_mesh_copy_init_customdata`) to get the `CD_NORMAL` layer over [which
    is flagged `CD_FLAG_TEMPORARY` and thus `CD_FLAG_NOCOPY`, so that is why
    `BM_mesh_copy_init_customdata` wont do anything for that layer].
    
    We actually need that layer to be copied though.
    
    What `BM_mesh_copy_init_customdata_all_layers` does **not** do (in
    comparison to `BM_mesh_copy_init_customdata`) is copy over (next to
    flags, `active`, `active_rnd`) the layers `uid`. If that is missing,
    `bm_to_mesh_shape_layer_index_from_kb` cannot function properly and as a
    consequence, code follows that tries to handle "shape-key but no
    CD_SHAPEKEY layers" and all goes downhill from there.
    
    What could be done about this?
    We could try to find a way to make `CD_NORMAL` even by
    `BM_mesh_copy_init_customdata` (getting rid of `CD_FLAG_NOCOPY` somehow)
    OR let `BM_mesh_copy_init_customdata_all_layers` also bring over (at
    least) the layers `uid`. Another alternative could be to use
    `BM_mesh_copy_init_customdata` and additionally explicitly add a
    `CD_NORMAL` layer?
    
    This PR goes the the route of bringing over the layers `uid` in
    `BM_mesh_copy_init_customdata_all_layers`.

--- source/blender/bmesh/intern/bmesh_construct.cc
+++ source/blender/bmesh/intern/bmesh_construct.cc
@@ -578,6 +578,7 @@ void BM_mesh_copy_init_customdata_all_layers(BMesh *bm_dst,
     for (int l = 0; l < src->totlayer; l++) {
       CustomData_add_layer_named(
           dst, eCustomDataType(src->layers[l].type), CD_SET_DEFAULT, 0, src->layers[l].name);
+      dst->layers[l].uid = src->layers[l].uid;
     }
     CustomData_bmesh_init_pool(dst, size, htypes[i]);
   }
