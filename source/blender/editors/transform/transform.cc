blender/blender

commit 1df4a09539c68947341b64d1bb4b74a749e798d3

Fix #146262: Transform constraints shown when overlays off
The check to draw transform constraints was returning too early and
skipping the later check for the `V3D_HIDE_OVERLAYS` flag.

This meant that transform constraints (e.g. when moving over X axis)
were always shown even when all overlays were turned off, which can
be very distracting.

Fix it by switching the check, so it returns false if the region is
not the same, but continues happily if it is.

See PR for details and screenshots.

Pull Request: https://projects.blender.org/blender/blender/pulls/147132

source/blender/editors/transform/transform.cc
@@ -1546,8 +1546,8 @@ bool calculateTransformCenter(bContext *C, int centerMode, float cent3d[3], floa
 static bool transinfo_show_overlay(TransInfo *t, ARegion *region)
 {
   /* Don't show overlays when not the active view and when overlay is disabled: #57139 */
-  if (region == t->region) {
-    return true;
+  if (region != t->region) {
+    return false;
   }
 
   if (t->spacetype == SPACE_VIEW3D) {

FrenkyBojler/blender-ios

commit eed061202fb07f9c56a6c957c1311a40caddbefd

Fix #155031: Proportional edit overlay not shown in clip editor mask mode
Caused by 1df4a09539
Missing check for overlays of clip editor

source/blender/editors/transform/transform.cc
@@ -1571,11 +1571,14 @@ static bool transinfo_show_overlay(TransInfo *t, ARegion *region)
       const SpaceAction *sact = static_cast<const SpaceAction *>(t->area->spacedata.first);
       return (sact->overlays.flag & ADS_OVERLAY_SHOW_OVERLAYS) != 0;
     }
-
     case SPACE_GRAPH: {
       /* There is no overlay flag defined yet for the Graph Editor. But there is the proportional
        * editing drawing that will not happen if we return false here. */
       return true;
+    }
+    case SPACE_CLIP : {
+      const SpaceClip *sclip = static_cast<const SpaceClip *>(t->area->spacedata.first);
+      return (sclip->overlay.flag & SC_SHOW_OVERLAYS) != 0;
     }
   }
   return false;