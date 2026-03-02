46f01e00f41d0230fe1a7f16d4c7ef93cda3cb8c
Pratik Borhade <pratikborhade302@gmail.com>
2026-03-02 16:06:36 +0530

    Increase width

--- scripts/startup/bl_ui/space_dopesheet.py
+++ scripts/startup/bl_ui/space_dopesheet.py
@@ -1060,7 +1060,7 @@ class DOPESHEET_PT_overlay(Panel):
     bl_space_type = 'DOPESHEET_EDITOR'
     bl_region_type = 'HEADER'
     bl_label = "Overlays"
-    bl_ui_units_x = 8
+    bl_ui_units_x = 10
 
     def draw(self, _context):
         pass
