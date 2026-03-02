c64d2a47336f17d59ce3611b3ee7a732ce40a38d
quackarooni <alfonsomartzii@gmail.com>
2026-03-02 21:13:06 +0800

    Split Align Nodes Operator to subfunctions

--- scripts/addons_core/node_wrangler/operators/align_selected.py
+++ scripts/addons_core/node_wrangler/operators/align_selected.py
@@ -34,28 +34,24 @@ class NODE_OT_align_selected(Operator, NWBase):
     @classmethod
     def poll(cls, context):
         return nw_check(cls, context) and nw_check_not_empty(cls, context) and nw_check_selected(cls, context)
-
-    def execute(self, context):
-        nodes, links = get_nodes_links(context)
+    
+    @staticmethod
+    def parent_depth(node):        
+        for i in range(100_000_000):
+            if node is None:
+                return i
+            
+            node = node.parent
+            
+        raise Exception("This should be unreachable.")
+    
+    def arrange_nodes(self, nodes):
         margin = self.margin
-
-        selection = []
-        for node in nodes:
-            if node.select and node.type != 'FRAME':
-                selection.append(node)
-
-        if not selection:
-            self.report({'WARNING'}, "No nodes to arrange in selection.")
-            return {'CANCELLED'}
-
-        active_loc = None
-        if nodes.active in selection:
-            active_loc = copy(nodes.active.location_absolute)  # make a copy, not a reference
 
         # Check if nodes should be laid out horizontally or vertically
         # use dimension to get center of node, not corner
-        x_locs = [n.location_absolute.x + (n.dimensions.x / 2) for n in selection]
-        y_locs = [n.location_absolute.y - (n.dimensions.y / 2) for n in selection]
+        x_locs = [n.location_absolute.x + (n.dimensions.x / 2) for n in nodes]
+        y_locs = [n.location_absolute.y - (n.dimensions.y / 2) for n in nodes]
         x_range = max(x_locs) - min(x_locs)
         y_range = max(y_locs) - min(y_locs)
         mid_x = (max(x_locs) + min(x_locs)) / 2
@@ -64,13 +60,16 @@ class NODE_OT_align_selected(Operator, NWBase):
 
         # Sort selection by location of node mid-point
         if horizontal:
-            selection = sorted(selection, key=lambda n: n.location_absolute.x + (n.dimensions.x / 2))
+            nodes = sorted(nodes, key=lambda n: n.location_absolute.x + (n.dimensions.x / 2))
         else:
-            selection = sorted(selection, key=lambda n: n.location_absolute.y - (n.dimensions.y / 2), reverse=True)
+            nodes = sorted(nodes, key=lambda n: n.location_absolute.y - (n.dimensions.y / 2), reverse=True)
 
         # Alignment
         current_pos = 0
-        for node in selection:
+        for node in nodes:
+            if node.bl_idname == "NodeFrame":
+                continue
+
             current_margin = margin
 
             # Use a smaller margin for hidden nodes.
@@ -92,16 +91,53 @@ class NODE_OT_align_selected(Operator, NWBase):
 
                 node.location_absolute.x = mid_x - (node.dimensions.x / 2)
 
+    def execute(self, context):
+        nodes = context.selected_nodes
+        parent_map = {}
+
+        for node in nodes:
+            children = parent_map.get(node.parent, None)
+            if children is None:
+                parent_map[node.parent] = []
+
+            parent_map[node.parent].append(node)
+
+        if not nodes:
+            self.report({'WARNING'}, "No nodes to arrange in selection.")
+            return {'CANCELLED'}
+
+        active_loc = None
+        if context.active_node:
+            active_loc = copy(context.active_node.location_absolute)  # make a copy, not a reference
+
+        # Check if nodes should be laid out horizontally or vertically
+        # use dimension to get center of node, not corner
+        x_locs = [n.location_absolute.x + (n.dimensions.x / 2) for n in nodes]
+        y_locs = [n.location_absolute.y - (n.dimensions.y / 2) for n in nodes]
+        x_range = max(x_locs) - min(x_locs)
+        y_range = max(y_locs) - min(y_locs)
+        mid_x = (max(x_locs) + min(x_locs)) / 2
+        mid_y = (max(y_locs) + min(y_locs)) / 2
+        horizontal = x_range > y_range
+
+        sorted_keys = sorted(parent_map.keys(), key=self.parent_depth, reverse=True)
+        for parent in sorted_keys:
+            children = parent_map[parent]
+            for child in children:
+                child.label = str(self.parent_depth(parent) + 1)
+
+            self.arrange_nodes(children)
+
         # If active node is selected, center nodes around it
         if active_loc is not None:
-            active_loc_diff = active_loc - nodes.active.location_absolute
-            for node in selection:
+            active_loc_diff = active_loc - context.active_node.location_absolute
+            for node in nodes:
                 node.location_absolute += active_loc_diff
         else:  # Position nodes centered around where they used to be
-            locs = ([n.location_absolute.x + (n.dimensions.x / 2) for n in selection]
-                    ) if horizontal else ([n.location_absolute.y - (n.dimensions.y / 2) for n in selection])
+            locs = ([n.location_absolute.x + (n.dimensions.x / 2) for n in nodes]
+                    ) if horizontal else ([n.location_absolute.y - (n.dimensions.y / 2) for n in nodes])
             new_mid = (max(locs) + min(locs)) / 2
-            for node in selection:
+            for node in nodes:
                 if horizontal:
                     node.location_absolute.x += (mid_x - new_mid)
                 else:
