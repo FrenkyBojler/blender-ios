blender/blender

commit 342f37566f001b06d14c36805a519197c4fd79af

UI: Add option to disable saving renders
Ref: https://projects.blender.org/blender/blender/issues/133489

A new toggle in the properties panel is implemented to disable scene
render saving:

When enabled, saving behaves the same way, i.e.:
- Still renders (F12) are not saved
- Animation (ctrl+F12) are saved

When disabled, the following was implemented:
- Render animation:
  - If sequencer active: `Render output disabled in output properties
    and no compositing file output nodes.`
  - Else: use File Output nodes in compositor to save results if
    compositing is enabled, error otherwise `Render output disabled in
    Output properties and no active compositing File Output nodes.`
- Render Playblast: Error `Render output disabled in output properties`
- Render Playblast on Keyframes: Error `Render output disabled in output
  properties`

The File Output node still inherits the 'File Extension' parameter from
the scene render settings. This will be addressed in a followup PR

Pull Request: https://projects.blender.org/blender/blender/pulls/153668

source/blender/render/intern/pipeline.cc
@@ -46,6 +46,7 @@
 #include "BKE_callbacks.hh"
 #include "BKE_camera.h"
 #include "BKE_colortools.hh"
+#include "BKE_compositor.hh"
 #include "BKE_global.hh"
 #include "BKE_image.hh"
 #include "BKE_image_format.hh"
@@ -173,8 +174,12 @@ static void render_callback_exec_id(Render *re, Main *bmain, ID *id, eCbEvent ev
 /** \name Allocation & Free
  * \{ */
 
-static bool do_write_image_or_movie(
-    Render *re, Main *bmain, Scene *scene, const int totvideos, const char *filepath_override);
+static bool do_write_image_or_movie(Render *re,
+                                    Main *bmain,
+                                    Scene *scene,
+                                    const int totvideos,
+                                    const char *filepath_override,
+                                    const bool write_anim);
 
 /* default callbacks, set in each new render */
 static void result_rcti_nothing(void * /*arg*/, RenderResult * /*rr*/, rcti * /*rect*/) {}
@@ -1673,34 +1678,17 @@ static int check_valid_camera(Scene *scene, Object *camera_override, ReportList 
   return true;
 }
 
-static bool node_tree_has_file_output(const bNodeTree *node_tree)
-{
-  node_tree->ensure_topology_cache();
-  for (const bNode *node : node_tree->nodes_by_type("CompositorNodeOutputFile")) {
-    if (!node->is_muted()) {
-      return true;
-    }
-  }
-
-  for (const bNode *node : node_tree->group_nodes()) {
-    if (node->is_muted() || !node->id) {
-      continue;
-    }
-
-    if (node_tree_has_file_output(reinterpret_cast<const bNodeTree *>(node->id))) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
 static bool scene_has_compositor_output(Scene *scene)
 {
+  if (scene->compositing_node_group == nullptr) {
+    return false;
+  }
+
   if (node_tree_has_group_output(scene->compositing_node_group)) {
     return true;
   }
-  return node_tree_has_file_output(scene->compositing_node_group);
+
+  return bke::compositor::node_tree_has_linked_file_output(scene->compositing_node_group);
 }
 
 /* Identify if the compositor can run on the GPU. Currently, this only checks if the compositor is
@@ -1989,7 +1977,7 @@ void RE_RenderFrame(Render *re,
             nullptr);
 
         if (errors.is_empty()) {
-          do_write_image_or_movie(re, bmain, scene, 0, filepath_override);
+          do_write_image_or_movie(re, bmain, scene, 0, filepath_override, false);
         }
         else {
           BKE_report_path_template_errors(re->reports, RPT_ERROR, rd.pic, errors);
@@ -2182,8 +2170,12 @@ bool RE_WriteRenderViewsMovie(ReportList *reports,
   return ok;
 }
 
-static bool do_write_image_or_movie(
-    Render *re, Main *bmain, Scene *scene, const int totvideos, const char *filepath_override)
+static bool do_write_image_or_movie(Render *re,
+                                    Main *bmain,
+                                    Scene *scene,
+                                    const int totvideos,
+                                    const char *filepath_override,
+                                    const bool write_anim)
 {
   char filepath[FILE_MAX];
   RenderResult rres;
@@ -2192,8 +2184,9 @@ static bool do_write_image_or_movie(
   RenderEngineType *re_type = RE_engines_find(re->r.engine);
 
   /* Only disable file writing if postprocessing is also disabled. */
-  const bool do_write_file = !(re_type->flag & RE_USE_NO_IMAGE_SAVE) ||
-                             (re_type->flag & RE_USE_POSTPROCESS);
+  const bool do_write_file = (!(re_type->flag & RE_USE_NO_IMAGE_SAVE) ||
+                              (re_type->flag & RE_USE_POSTPROCESS)) &&
+                             write_anim;
 
   if (do_write_file) {
     RE_AcquireResultImageViews(re, &rres);
@@ -2357,10 +2350,13 @@ void RE_RenderAnim(Render *re,
   const bool is_movie = BKE_imtype_is_movie(image_format.imtype);
   const bool is_multiview_name = ((rd.scemode & R_MULTIVIEW) != 0 &&
                                   (image_format.views_format == R_IMF_VIEWS_INDIVIDUAL));
+  const bool write_anim = (scene->r.mode & R_SAVE_OUTPUT);
 
-  /* Only disable file writing if postprocessing is also disabled. */
-  const bool do_write_file = !(re_type->flag & RE_USE_NO_IMAGE_SAVE) ||
-                             (re_type->flag & RE_USE_POSTPROCESS);
+  /* Disable file writing if postprocessing is also disabled or if it's explicitly disabled by the
+   * user. */
+  const bool do_write_file = (!(re_type->flag & RE_USE_NO_IMAGE_SAVE) ||
+                              (re_type->flag & RE_USE_POSTPROCESS)) &&
+                             write_anim;
 
   render_init_depsgraph(re);
 
@@ -2533,7 +2529,7 @@ void RE_RenderAnim(Render *re,
     const bool should_write = !(re->flag & R_SKIP_WRITE);
     if (re->display->test_break() == 0) {
       if (!G.is_break && should_write) {
-        if (!do_write_image_or_movie(re, bmain, scene, totvideos, nullptr)) {
+        if (!do_write_image_or_movie(re, bmain, scene, totvideos, nullptr, write_anim)) {
           G.is_break = true;
         }
       }

scripts/startup/bl_ui/properties_output.py
@@ -296,6 +296,10 @@ class RENDER_PT_output(RenderOutputButtonsPanel, Panel):
         'BLENDER_WORKBENCH',
     }
 
+    def draw_header(self, context):
+        rd = context.scene.render
+        self.layout.prop(rd, "save_output", text="")
+
     def draw(self, context):
         layout = self.layout
         layout.use_property_split = False
@@ -303,6 +307,7 @@ class RENDER_PT_output(RenderOutputButtonsPanel, Panel):
 
         rd = context.scene.render
         image_settings = rd.image_settings
+        layout.active = rd.save_output
 
         layout.prop(rd, "filepath", text="")
 
@@ -356,10 +361,12 @@ class RENDER_PT_output_color_management(RenderOutputButtonsPanel, Panel):
     def draw(self, context):
         scene = context.scene
         image_settings = scene.render.image_settings
+        rd = scene.render
 
         layout = self.layout
         layout.use_property_split = True
         layout.use_property_decorate = False  # No animation.
+        layout.active = rd.save_output
 
         layout.row().prop(image_settings, "color_management", text=" ", expand=True)
 
@@ -437,6 +444,7 @@ class RENDER_PT_output_pixel_density(RenderOutputButtonsPanel, Panel):
         pixeldensity_label_text, show_pixeldensity = RENDER_PT_output_pixel_density._draw_pixeldensity_label(*args)
 
         layout.prop(rd, "ppm_factor", text="Pixels")
+        layout.active = rd.save_output
 
         row = layout.split(factor=0.4)
         row.alignment = 'RIGHT'

source/blender/blenkernel/BKE_blender_version.h
@@ -31,7 +31,7 @@ namespace blender {
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
 
-#define BLENDER_FILE_SUBVERSION 0
+#define BLENDER_FILE_SUBVERSION 1
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_compositor.hh
@@ -17,6 +17,7 @@ namespace blender {
 struct Scene;
 struct ViewLayer;
 struct bContext;
+struct bNodeTree;
 
 namespace bke::compositor {
 
@@ -28,6 +29,10 @@ Set<std::string> get_used_passes(const Scene &scene, const ViewLayer *view_layer
 /* Checks if the viewport compositor is currently being used. This is similar to
  * DRWContext::is_viewport_compositor_enabled but checks all 3D views. */
 bool is_viewport_compositor_used(const bContext &context);
+
+/* Note: Links to the File Output node do not guarantee it will write a result to disk, e.g. if
+ * Menu Switch nodes exists but it's a good estimation without evaluating the node tree. */
+bool node_tree_has_linked_file_output(const bNodeTree *node_tree);
 
 }  // namespace bke::compositor
 }  // namespace blender

source/blender/blenkernel/intern/compositor.cc
@@ -215,4 +215,34 @@ bool is_viewport_compositor_used(const bContext &context)
   return false;
 }
 
+bool node_tree_has_linked_file_output(const bNodeTree *node_tree)
+{
+  if (node_tree == nullptr) {
+    return false;
+  }
+
+  node_tree->ensure_topology_cache();
+  for (const bNode *node : node_tree->nodes_by_type("CompositorNodeOutputFile")) {
+    if (!node->is_muted()) {
+      for (const bNodeSocket &input : node->inputs) {
+        if (input.is_directly_linked()) {
+          return true;
+        }
+      }
+    }
+  }
+
+  for (const bNode *node : node_tree->group_nodes()) {
+    if (node->is_muted() || !node->id) {
+      continue;
+    }
+
+    if (node_tree_has_linked_file_output(reinterpret_cast<const bNodeTree *>(node->id))) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 }  // namespace blender::bke::compositor

source/blender/blenloader/intern/versioning_520.cc
@@ -10,6 +10,7 @@
 
 #include "DNA_ID.h"
 
+#include "BLI_listbase_iterator.hh"
 #include "BLI_sys_types.h"
 
 #include "BKE_main.hh"
@@ -34,8 +35,13 @@ void do_versions_after_linking_520(FileData * /*fd*/, Main * /*bmain*/)
    */
 }
 
-void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main * /*bmain*/)
+void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
 {
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 1)) {
+    for (Scene &scene : bmain->scenes) {
+      scene.r.mode |= R_SAVE_OUTPUT;
+    }
+  }
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.

source/blender/editors/render/render_internal.cc
@@ -28,6 +28,7 @@
 #include "DNA_view3d_types.h"
 
 #include "BKE_colortools.hh"
+#include "BKE_compositor.hh"
 #include "BKE_context.hh"
 #include "BKE_global.hh"
 #include "BKE_image.hh"
@@ -326,6 +327,37 @@ static void get_render_operator_frame_range(wmOperator *render_operator,
   }
 }
 
+/* When rendering an animation, saving files is required, either through scene saving or through
+ * a compositor File Output node. */
+static bool disable_save_output_allowed(const bool is_animation, Scene &scene, ReportList *reports)
+{
+  const bool save_output = (scene.r.mode & R_SAVE_OUTPUT) != 0;
+  const bool do_compositing = (scene.r.scemode & R_DOCOMP) != 0;
+  const bool do_sequencer = RE_seq_render_active(&scene, &scene.r);
+
+  if (is_animation && do_sequencer && !save_output) {
+    BKE_report(reports, RPT_ERROR, "Render output disabled in Output properties");
+    return false;
+  }
+
+  if (is_animation && !save_output && !do_compositing) {
+    BKE_report(reports, RPT_ERROR, "Render output and compositing disabled in Output properties");
+    return false;
+  }
+
+  if (is_animation && !save_output && do_compositing) {
+    if (!bke::compositor::node_tree_has_linked_file_output(scene.compositing_node_group)) {
+      BKE_report(reports,
+                 RPT_ERROR,
+                 "Render output disabled in Output properties and no active compositing File "
+                 "Output nodes");
+      return false;
+    }
+  }
+
+  return true;
+}
+
 /* executes blocking render */
 static wmOperatorStatus screen_render_exec(bContext *C, wmOperator *op)
 {
@@ -381,6 +413,10 @@ static wmOperatorStatus screen_render_exec(bContext *C, wmOperator *op)
   if (!is_animation && is_write_still && BKE_imtype_is_movie(scene->r.im_format.imtype)) {
     BKE_report(
         op->reports, RPT_ERROR, "Cannot write a single file with an animation format selected");
+    return OPERATOR_CANCELLED;
+  }
+
+  if (!disable_save_output_allowed(is_animation, *scene, op->reports)) {
     return OPERATOR_CANCELLED;
   }
 
@@ -1075,6 +1111,10 @@ static wmOperatorStatus screen_render_invoke(bContext *C, wmOperator *op, const 
 
   /* only one render job at a time */
   if (WM_jobs_test(CTX_wm_manager(C), scene, WM_JOB_TYPE_RENDER)) {
+    return OPERATOR_CANCELLED;
+  }
+
+  if (!disable_save_output_allowed(is_animation, *scene, op->reports)) {
     return OPERATOR_CANCELLED;
   }
 

source/blender/editors/render/render_opengl.cc
@@ -967,6 +967,11 @@ static bool screen_opengl_render_anim_init(wmOperator *op)
   OGLRender *oglrender = static_cast<OGLRender *>(op->customdata);
   Scene *scene = oglrender->scene;
 
+  if (!(scene->r.mode & R_SAVE_OUTPUT)) {
+    BKE_report(op->reports, RPT_ERROR, "Render output disabled in Output properties");
+    return false;
+  }
+
   ImageFormatData image_format;
   BKE_image_format_init_for_write(&image_format, scene, nullptr, true);
 

FrenkyBojler/blender-ios

commit 486fad8e68c44db5f32fd56469f014e635955ca3

Fix: bpy.ops.render.render(write_still=True) broken after recent changes
Caused by 342f37566f00.

source/blender/render/intern/pipeline.cc
@@ -179,7 +179,7 @@ static bool do_write_image_or_movie(Render *re,
                                     Scene *scene,
                                     const int totvideos,
                                     const char *filepath_override,
-                                    const bool write_anim);
+                                    const bool write_anim_or_still);
 
 /* default callbacks, set in each new render */
 static void result_rcti_nothing(void * /*arg*/, RenderResult * /*rr*/, rcti * /*rect*/) {}
@@ -1977,7 +1977,7 @@ void RE_RenderFrame(Render *re,
             nullptr);
 
         if (errors.is_empty()) {
-          do_write_image_or_movie(re, bmain, scene, 0, filepath_override, false);
+          do_write_image_or_movie(re, bmain, scene, 0, filepath_override, write_still);
         }
         else {
           BKE_report_path_template_errors(re->reports, RPT_ERROR, rd.pic, errors);
@@ -2175,7 +2175,7 @@ static bool do_write_image_or_movie(Render *re,
                                     Scene *scene,
                                     const int totvideos,
                                     const char *filepath_override,
-                                    const bool write_anim)
+                                    const bool write_anim_or_still)
 {
   char filepath[FILE_MAX];
   RenderResult rres;
@@ -2186,7 +2186,7 @@ static bool do_write_image_or_movie(Render *re,
   /* Only disable file writing if postprocessing is also disabled. */
   const bool do_write_file = (!(re_type->flag & RE_USE_NO_IMAGE_SAVE) ||
                               (re_type->flag & RE_USE_POSTPROCESS)) &&
-                             write_anim;
+                             write_anim_or_still;
 
   if (do_write_file) {
     RE_AcquireResultImageViews(re, &rres);