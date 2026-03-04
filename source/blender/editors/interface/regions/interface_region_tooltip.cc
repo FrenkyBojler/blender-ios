0e2bb7064ed907510666b020514a0e828ed1d523
Dalai Felinto <dalai@blender.org>
2026-03-04 15:40:30 +0100

    Long live c++

--- source/blender/editors/interface/regions/interface_region_tooltip.cc
+++ source/blender/editors/interface/regions/interface_region_tooltip.cc
@@ -978,11 +978,11 @@ void tooltip_uibut_python_add(TooltipData &data,
 
 static const char *icon_id_to_name(BIFIconID icon_value)
 {
-  const char *identifier = NULL;
+  const char *identifier = nullptr;
   if (RNA_enum_identifier(rna_enum_icon_items, icon_value, &identifier)) {
     return identifier;
   }
-  return NULL;
+  return nullptr;
 }
 
 static void tooltip_uibut_icon_add(TooltipData &data, Button &but)
