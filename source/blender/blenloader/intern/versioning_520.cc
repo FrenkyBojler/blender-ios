7b34da0fa8183c14bc6ced23d49bcc85a37a16e9
John Kiril Swenson <kirilswenson@gmail.com>
2026-03-04 00:07:16 -0600

    Fix linting

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -98,7 +98,6 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 6)) {
     for (Scene &scene : bmain->scenes) {
       SequencerToolSettings *sequencer_tool_settings = seq::tool_settings_ensure(&scene);
