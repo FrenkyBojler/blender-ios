blender/blender

commit c9f4795638739b48ca0468c3fc1cde9f0942128f

Cleanup: Attributes: Remove DNA backing type for RNA types
This isn't correct anymore; the RNA type might correspond to the
runtime bke::Attribute type now, or CustomDataLayer for meshes.
Apply the same change to mesh layers for consistency, and to
simplify future changes.

Part of #122398

source/blender/makesrna/intern/rna_attribute.cc
@@ -347,7 +347,7 @@ blender::StringRefNull rna_Attribute_name_get(const PointerRNA &ptr)
   return attr->name();
 }
 
-static void rna_Attribute_name_get(PointerRNA *ptr, char *value)
+void rna_Attribute_name_get(PointerRNA *ptr, char *value)
 {
   rna_Attribute_name_get(*ptr).copy_unsafe(value);
 }
@@ -1283,7 +1283,6 @@ static void rna_def_attribute_float(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "FloatAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "Float Attribute", "Geometry attribute that stores floating-point values");
 
@@ -1317,7 +1316,6 @@ static void rna_def_attribute_float_vector(BlenderRNA *brna)
 
   /* Float Vector Attribute */
   srna = RNA_def_struct(brna, "FloatVectorAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "Float Vector Attribute", "Geometry attribute that stores floating-point 3D vectors");
 
@@ -1355,7 +1353,6 @@ static void rna_def_attribute_float_color(BlenderRNA *brna)
 
   /* Float Color Attribute */
   srna = RNA_def_struct(brna, "FloatColorAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna,
                          "Float Color Attribute",
                          "Geometry attribute that stores RGBA colors as floating-point values "
@@ -1404,7 +1401,6 @@ static void rna_def_attribute_byte_color(BlenderRNA *brna)
 
   /* Byte Color Attribute */
   srna = RNA_def_struct(brna, "ByteColorAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna,
                          "Byte Color Attribute",
                          "Geometry attribute that stores RGBA colors as positive integer values "
@@ -1456,7 +1452,6 @@ static void rna_def_attribute_int(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "IntAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "Integer Attribute", "Geometry attribute that stores integer values");
 
@@ -1488,7 +1483,6 @@ static void rna_def_attribute_string(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "StringAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna, "String Attribute", "Geometry attribute that stores strings");
 
   prop = RNA_def_property(srna, "data", PROP_COLLECTION, PROP_NONE);
@@ -1523,7 +1517,6 @@ static void rna_def_attribute_bool(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "BoolAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna, "Bool Attribute", "Geometry attribute that stores booleans");
 
   prop = RNA_def_property(srna, "data", PROP_COLLECTION, PROP_NONE);
@@ -1554,7 +1547,6 @@ static void rna_def_attribute_int8(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "ByteIntAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "8-bit Integer Attribute", "Geometry attribute that stores 8-bit integers");
 
@@ -1587,7 +1579,6 @@ static void rna_def_attribute_short2(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "Short2Attribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna,
                          "2D 16-Bit Integer Vector Attribute",
                          "Geometry attribute that stores 2D integer vectors");
@@ -1624,7 +1615,6 @@ static void rna_def_attribute_int2(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "Int2Attribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "2D Integer Vector Attribute", "Geometry attribute that stores 2D integer vectors");
 
@@ -1660,7 +1650,6 @@ static void rna_def_attribute_quaternion(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "QuaternionAttribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(srna, "Quaternion Attribute", "Geometry attribute that stores rotation");
 
   prop = RNA_def_property(srna, "data", PROP_COLLECTION, PROP_NONE);
@@ -1695,7 +1684,6 @@ static void rna_def_attribute_float4x4(BlenderRNA *brna)
   PropertyRNA *prop;
 
   srna = RNA_def_struct(brna, "Float4x4Attribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "4x4 Matrix Attribute", "Geometry attribute that stores a 4 by 4 float matrix");
 
@@ -1731,7 +1719,6 @@ static void rna_def_attribute_float2(BlenderRNA *brna)
 
   /* Float2 Attribute */
   srna = RNA_def_struct(brna, "Float2Attribute", "Attribute");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_ui_text(
       srna, "Float2 Attribute", "Geometry attribute that stores floating-point 2D vectors");
 

source/blender/makesrna/intern/rna_internal.hh
@@ -234,6 +234,8 @@ void rna_Attribute_data_begin(CollectionPropertyIterator *iter, PointerRNA *ptr)
 int rna_Attribute_data_length(PointerRNA *ptr);
 
 blender::StringRefNull rna_Attribute_name_get(const PointerRNA &ptr);
+void rna_Attribute_name_get(PointerRNA *ptr, char *value);
+int rna_Attribute_name_length(PointerRNA *ptr);
 void rna_Attribute_name_set(PointerRNA *ptr, const char *value);
 
 void rna_AttributeGroup_iterator_begin(CollectionPropertyIterator *iter,

source/blender/makesrna/intern/rna_mesh.cc
@@ -1931,10 +1931,10 @@ static PointerRNA rna_Mesh_vertex_color_new(Mesh *mesh,
   return RNA_pointer_create_discrete(&mesh->id, &RNA_MeshLoopColorLayer, cdl);
 }
 
-static void rna_Mesh_vertex_color_remove(Mesh *mesh, ReportList *reports, CustomDataLayer *layer)
+static void rna_Mesh_vertex_color_remove(Mesh *mesh, ReportList *reports, PointerRNA *ptr)
 {
   AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
-  BKE_attribute_remove(owner, layer->name, reports);
+  BKE_attribute_remove(owner, rna_Attribute_name_get(*ptr), reports);
 }
 
 static PointerRNA rna_Mesh_uv_layers_new(Mesh *mesh,
@@ -1955,25 +1955,26 @@ static PointerRNA rna_Mesh_uv_layers_new(Mesh *mesh,
   return ptr;
 }
 
-static void rna_Mesh_uv_layers_remove(Mesh *mesh, ReportList *reports, CustomDataLayer *layer)
+static void rna_Mesh_uv_layers_remove(Mesh *mesh, ReportList *reports, PointerRNA *uv_ptr)
 {
   using namespace blender;
   AttributeOwner owner = AttributeOwner::from_id(&mesh->id);
+  const StringRefNull name = rna_Attribute_name_get(*uv_ptr);
   if (mesh->runtime->edit_mesh) {
     BMesh &bm = *mesh->runtime->edit_mesh->bm;
-    if (!CustomData_has_layer_named(&bm.ldata, CD_PROP_FLOAT2, layer->name)) {
-      BKE_reportf(reports, RPT_ERROR, "UV map '%s' not found", layer->name);
+    if (!CustomData_has_layer_named(&bm.ldata, CD_PROP_FLOAT2, name)) {
+      BKE_reportf(reports, RPT_ERROR, "UV map '%s' not found", name.c_str());
       return;
     }
   }
   else {
     bke::AttributeAccessor attributes = *owner.get_accessor();
-    if (!attributes.contains(layer->name)) {
-      BKE_reportf(reports, RPT_ERROR, "UV map '%s' not found", layer->name);
+    if (!attributes.contains(name)) {
+      BKE_reportf(reports, RPT_ERROR, "UV map '%s' not found", name.c_str());
       return;
     }
   }
-  BKE_attribute_remove(owner, layer->name, reports);
+  BKE_attribute_remove(owner, name, reports);
 }
 
 static bool rna_Mesh_is_editmode_get(PointerRNA *ptr)
@@ -2414,7 +2415,6 @@ static void rna_def_mloopuv(BlenderRNA *brna)
   PropertyRNA *prop, *parm;
 
   srna = RNA_def_struct(brna, "MeshUVLoopLayer", nullptr);
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_path_func(srna, "rna_MeshUVLoopLayer_path");
 
   prop = RNA_def_property(srna, "data", PROP_COLLECTION, PROP_NONE);
@@ -2436,7 +2436,8 @@ static void rna_def_mloopuv(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "name", PROP_STRING, PROP_NONE);
   RNA_def_struct_name_property(srna, prop);
-  RNA_def_property_string_funcs(prop, nullptr, nullptr, "rna_Attribute_name_set");
+  RNA_def_property_string_funcs(
+      prop, "rna_Attribute_name_get", "rna_Attribute_name_length", "rna_Attribute_name_set");
   RNA_def_property_string_maxlength(prop, MAX_CUSTOMDATA_LAYER_NAME_NO_PREFIX);
   RNA_def_property_ui_text(prop, "Name", "Name of UV map");
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data_legacy_deg_tag_all");
@@ -2449,7 +2450,6 @@ static void rna_def_mloopuv(BlenderRNA *brna)
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data_legacy_deg_tag_all");
 
   prop = RNA_def_property(srna, "active_render", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "active_rnd", 0);
   RNA_def_property_boolean_funcs(
       prop, "rna_MeshUVLoopLayer_active_render_get", "rna_MeshUVLoopLayer_active_render_set");
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
@@ -2457,7 +2457,6 @@ static void rna_def_mloopuv(BlenderRNA *brna)
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data_legacy_deg_tag_all");
 
   prop = RNA_def_property(srna, "active_clone", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "active_clone", 0);
   RNA_def_property_boolean_funcs(
       prop, "rna_MeshUVLoopLayer_clone_get", "rna_MeshUVLoopLayer_clone_set");
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
@@ -2523,13 +2522,13 @@ static void rna_def_mloopcol(BlenderRNA *brna)
   srna = RNA_def_struct(brna, "MeshLoopColorLayer", nullptr);
   RNA_def_struct_ui_text(
       srna, "Mesh Vertex Color Layer", "Layer of vertex colors in a Mesh data-block");
-  RNA_def_struct_sdna(srna, "CustomDataLayer");
   RNA_def_struct_path_func(srna, "rna_MeshLoopColorLayer_path");
   RNA_def_struct_ui_icon(srna, ICON_GROUP_VCOL);
 
   prop = RNA_def_property(srna, "name", PROP_STRING, PROP_NONE);
   RNA_def_struct_name_property(srna, prop);
-  RNA_def_property_string_funcs(prop, nullptr, nullptr, "rna_Attribute_name_set");
+  RNA_def_property_string_funcs(
+      prop, "rna_Attribute_name_get", "rna_Attribute_name_length", "rna_Attribute_name_set");
   RNA_def_property_string_maxlength(prop, MAX_CUSTOMDATA_LAYER_NAME_NO_PREFIX);
   RNA_def_property_ui_text(prop, "Name", "Name of Vertex color layer");
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data_legacy_deg_tag_all");
@@ -2541,7 +2540,6 @@ static void rna_def_mloopcol(BlenderRNA *brna)
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data_legacy_deg_tag_all");
 
   prop = RNA_def_property(srna, "active_render", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "active_rnd", 0);
   RNA_def_property_boolean_funcs(
       prop, "rna_mesh_color_active_render_get", "rna_mesh_color_active_render_set");
   RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
@@ -2778,7 +2776,7 @@ static void rna_def_loop_colors(BlenderRNA *brna, PropertyRNA *cprop)
   RNA_def_function_ui_description(func, "Remove a vertex color layer");
   RNA_def_function_flag(func, FUNC_USE_REPORTS);
   parm = RNA_def_pointer(func, "layer", "MeshLoopColorLayer", "", "The layer to remove");
-  RNA_def_parameter_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED);
+  RNA_def_parameter_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED | PARM_RNAPTR);
   RNA_def_property_clear_flag(parm, PROP_THICK_WRAP);
 
   prop = RNA_def_property(srna, "active", PROP_POINTER, PROP_NONE);
@@ -2833,7 +2831,7 @@ static void rna_def_uv_layers(BlenderRNA *brna, PropertyRNA *cprop)
   RNA_def_function_ui_description(func, "Remove a UV map layer");
   RNA_def_function_flag(func, FUNC_USE_REPORTS);
   parm = RNA_def_pointer(func, "layer", "MeshUVLoopLayer", "", "The layer to remove");
-  RNA_def_parameter_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED);
+  RNA_def_parameter_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED | PARM_RNAPTR);
 
   prop = RNA_def_property(srna, "active", PROP_POINTER, PROP_NONE);
   RNA_def_property_struct_type(prop, "MeshUVLoopLayer");

FrenkyBojler/blender-ios

commit 21a7065b369e2bd883d13ce581ec9de942998a92

Fix #155159: attribute value lookup by index is very slow
Caused by c9f479563873

Without defining with `RNA_def_struct_sdna` ,
`rna_def_property_lookup_int_func` wont create a `_lookup_int` (making
the the lookup iterate all values until the index matches).

Two possible solutions:
- [1] remove the check in `rna_def_property_lookup_int_func` that
prevents generation of a nice "automatic" function
- [2] create a dedicated `rna_Attribute_data_lookup_int` function

Atm. I first want to check which way to go (so both solutions are in
this PR)

source/blender/makesrna/intern/makesrna.cc
@@ -1601,6 +1601,9 @@ static char *rna_def_property_lookup_int_func(FILE *f,
   }
 
   if (!manualfunc) {
+    /* Solution [2]: get rid of the check, dont return here.
+     * This will make sure we get a proper "lookup_int" function even for RNA types lacking DNA
+     * backing type [so defined without #RNA_def_struct_sdna()]. */
     if (!dp->dnastructname || !dp->dnaname) {
       return nullptr;
     }

source/blender/makesrna/intern/rna_attribute.cc
@@ -329,6 +329,40 @@ static StructRNA *srna_by_custom_data_layer_type(const eCustomDataType type)
   }
 }
 
+static StructRNA *srna_value_by_custom_data_layer_type(const eCustomDataType type)
+{
+  switch (type) {
+    case CD_PROP_FLOAT:
+      return RNA_FloatAttributeValue;
+    case CD_PROP_INT32:
+      return RNA_IntAttributeValue;
+    case CD_PROP_FLOAT3:
+      return RNA_FloatVectorAttributeValue;
+    case CD_PROP_COLOR:
+      return RNA_FloatColorAttributeValue;
+    case CD_PROP_BYTE_COLOR:
+      return RNA_ByteColorAttributeValue;
+    case CD_PROP_STRING:
+      return RNA_StringAttributeValue;
+    case CD_PROP_BOOL:
+      return RNA_BoolAttributeValue;
+    case CD_PROP_FLOAT2:
+      return RNA_Float2AttributeValue;
+    case CD_PROP_INT8:
+      return RNA_ByteIntAttributeValue;
+    case CD_PROP_INT16_2D:
+      return RNA_Short2AttributeValue;
+    case CD_PROP_INT32_2D:
+      return RNA_Int2AttributeValue;
+    case CD_PROP_QUATERNION:
+      return RNA_QuaternionAttribute;
+    case CD_PROP_FLOAT4X4:
+      return RNA_Float4x4AttributeValue;
+    default:
+      return nullptr;
+  }
+}
+
 static StructRNA *rna_Attribute_refine(PointerRNA *ptr)
 {
   if (GS(ptr->owner_id->name) == ID_ME) {
@@ -578,6 +612,49 @@ int rna_Attribute_data_length(PointerRNA *ptr)
   const bke::Attribute *attr = ptr->data_as<bke::Attribute>();
   const bke::AttributeAccessor accessor = *owner.get_accessor();
   return accessor.domain_size(attr->domain());
+}
+
+/* Solution [1]: introduce a dedicated "lookup_int()" function for attribute data. Would have to be
+ * used in many more places. */
+bool rna_Attribute_data_lookup_int(PointerRNA *ptr, int index, PointerRNA *r_ptr)
+{
+  bool found = false;
+  CollectionPropertyIterator iter;
+
+  rna_Attribute_data_begin(&iter, ptr);
+
+  if (iter.valid) {
+    ArrayIterator *internal = &iter.internal.array;
+    if (index < 0 || index >= internal->length) {
+#  ifdef __GNUC__
+      printf("Array iterator out of range: %s (index %d)\n", __func__, index);
+#  else
+      printf("Array iterator out of range: (index %d)\n", index);
+#  endif
+    }
+    else if (internal->skip) {
+      while (index-- > 0 && iter.valid) {
+        rna_iterator_array_next(&iter);
+      }
+      found = (index == -1 && iter.valid);
+    }
+    else {
+      internal->ptr += internal->itemsize * index;
+      found = 1;
+    }
+
+    if (found) {
+      bke::Attribute *attr = ptr->data_as<bke::Attribute>();
+      const eCustomDataType data_type = *bke::attr_type_to_custom_data_type(attr->data_type());
+      StructRNA *type = srna_value_by_custom_data_layer_type(data_type);
+
+      *r_ptr = RNA_pointer_create_with_parent(iter.parent, type, rna_iterator_array_get(&iter));
+    }
+  }
+
+  rna_iterator_array_end(&iter);
+
+  return found;
 }
 
 /**
@@ -1453,13 +1530,15 @@ static void rna_def_attribute_byte_color(BlenderRNA *brna)
   prop = RNA_def_property(srna, "data", PROP_COLLECTION, PROP_NONE);
   RNA_def_property_struct_type(prop, "ByteColorAttributeValue");
   RNA_def_property_override_flag(prop, PROPOVERRIDE_IGNORE);
+  /* Solution [1]: use new "rna_Attribute_data_lookup_int" (but have to be added to many more
+   * places). */
   RNA_def_property_collection_funcs(prop,
                                     "rna_Attribute_data_begin",
                                     "rna_iterator_array_next",
                                     "rna_iterator_array_end",
                                     "rna_iterator_array_get",
                                     "rna_Attribute_data_length",
-                                    nullptr,
+                                    "rna_Attribute_data_lookup_int",
                                     nullptr,
                                     nullptr);
   RNA_def_property_update(prop, 0, "rna_Attribute_update_data");

source/blender/makesrna/intern/rna_internal.hh
@@ -237,6 +237,7 @@ void rna_def_attributes_common(StructRNA *srna, AttributeOwnerType type);
 
 void rna_Attribute_data_begin(CollectionPropertyIterator *iter, PointerRNA *ptr);
 int rna_Attribute_data_length(PointerRNA *ptr);
+bool rna_Attribute_data_lookup_int(PointerRNA *ptr, int index, PointerRNA *r_ptr);
 
 StringRefNull rna_Attribute_name_get(const PointerRNA &ptr);
 void rna_Attribute_name_get(PointerRNA *ptr, char *value);