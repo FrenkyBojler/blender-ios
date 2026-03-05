71c9a8c53c4c4bb45ab1bfc916ada1126cb7c19f
Hans Goudey <hans@blender.org>
2026-03-05 15:27:22 -0500

    Parent for properties RNA pointer

--- source/blender/makesrna/intern/rna_modifier.cc
+++ source/blender/makesrna/intern/rna_modifier.cc
@@ -1958,7 +1958,7 @@ static PointerRNA rna_NodesModifierProperties_get(PointerRNA *ptr)
   if (!nmd->node_group) {
     return PointerRNA_NULL;
   }
-  return RNA_pointer_create_discrete(ptr->owner_id, RNA_NodesModifierProperties, nmd);
+  return RNA_pointer_create_with_parent(*ptr, RNA_NodesModifierProperties, nmd);
 }
 
 static nodes::geo_eval_log::GeoTreeLog *get_nodes_modifier_log(const Object &object,
