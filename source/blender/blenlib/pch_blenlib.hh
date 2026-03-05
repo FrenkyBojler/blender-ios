bed6d9e90abdfa47ef577b8ca131f3ab1295e694
Jorge Reyes Paniagua <joreyesp@microsoft.com>
2026-03-05 12:03:11 -0800

    add licence header

--- source/blender/blenlib/pch_blenlib.hh
+++ source/blender/blenlib/pch_blenlib.hh
@@ -1,9 +1,10 @@
-/* Precompiled header for heavy blenlib templates.
- * Keep this stable and minimal to maximize reuse across many translation units.
- */
-
+/* SPDX-FileCopyrightText: 2023 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
+/* Precompiled header for heavy blenlib templates.
+ * Keep this stable and minimal to maximize reuse across many translation units. */
 #include "BLI_generic_virtual_array.hh"
 #include "BLI_virtual_array.hh"
 #include "BLI_index_mask.hh"
