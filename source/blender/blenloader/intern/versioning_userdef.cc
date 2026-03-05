761427ba078509da42de2aa07c784e9734352670
Guillermo Venegas <guillermovcra@gmail.com>
2026-03-05 16:24:29 -0600

    Fix versioning and undo now unused change

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -431,7 +431,7 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     FROM_DEFAULT_V4_UCHAR(space_view3d.gp_wire_edit);
   }
 
-  if (!USER_VERSION_ATLEAST(502, 6)) {
+  if (!USER_VERSION_ATLEAST(502, 7)) {
     FROM_DEFAULT_V4_UCHAR(tui.link);
   }
 
