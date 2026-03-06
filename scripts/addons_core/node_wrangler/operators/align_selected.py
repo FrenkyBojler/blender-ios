blender/blender

commit 23f446a881725218c6b15115a58b933bd65749fe

Node Wrangler: Do nothing in Align Nodes operator if selection is empty
The previous behavior wasto arrange all nodes if no nodes are selected.
This can lead to confusing behavior since frames are filtered, leading
to the selection being treated as "empty" if it contains all frames.

This is unintuitive and not consistent with how other parts of Blender
handle selections. The operator now will do nothing if there are no
valid nodes selected.

Pull Request: https://projects.blender.org/blender/blender/pulls/154942

scripts/addons_core/node_wrangler/operators/align_selected.py
@@ -32,6 +32,10 @@ class NODE_OT_align_selected(Operator, NWBase):
 
     @classmethod
     def poll(cls, context):
+        if len(context.selected_nodes) <= 0:
+            cls.poll_message_set("No nodes selected.")
+            return False
+
         return nw_check(cls, context) and nw_check_not_empty(cls, context)
 
     def execute(self, context):
@@ -43,12 +47,13 @@ class NODE_OT_align_selected(Operator, NWBase):
             if node.select and node.type != 'FRAME':
                 selection.append(node)
 
-        # If no nodes are selected, align all nodes
-        active_loc = None
         if not selection:
-            selection = nodes
-        elif nodes.active in selection:
-            active_loc = copy(nodes.active.location)  # make a copy, not a reference
+            self.report({'WARNING'}, "No nodes to arrange in selection.")
+            return {'CANCELLED'}
+
+        active_loc = None
+        if nodes.active in selection:
+            active_loc = copy(nodes.active.location_absolute)  # make a copy, not a reference
 
         # Check if nodes should be laid out horizontally or vertically
         # use dimension to get center of node, not corner

FrenkyBojler/blender-ios

commit 1e776d1c078092943394fc49e7313809f808db51

Fix #155312: Node Wrangler error spawning operator search
Caused by 23f446a88172

To resolve, first check the correct context, then the stuff added in the
culprit commit (for which we already have a utility it seems, so use
that instead)

scripts/addons_core/node_wrangler/operators/align_selected.py
@@ -12,6 +12,7 @@ from ..utils.nodes import (
     NWBase,
     nw_check,
     nw_check_not_empty,
+    nw_check_selected,
     get_nodes_links,
 )
 
@@ -32,11 +33,7 @@ class NODE_OT_align_selected(Operator, NWBase):
 
     @classmethod
     def poll(cls, context):
-        if len(context.selected_nodes) <= 0:
-            cls.poll_message_set("No nodes selected.")
-            return False
-
-        return nw_check(cls, context) and nw_check_not_empty(cls, context)
+        return nw_check(cls, context) and nw_check_not_empty(cls, context) and nw_check_selected(cls, context)
 
     def execute(self, context):
         nodes, links = get_nodes_links(context)