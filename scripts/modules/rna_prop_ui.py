a9126cf0836a476dc3de538c5c8a6ea40b274577
Pratik Borhade <pratikborhade302@gmail.com>
2026-03-06 15:44:05 +0530

    Fix #155263: Regression: A python error when assigning custom properties defaults
    Calling `id_properties_ui` triggers error for idprops that don't support `ui_data`.
    Since `rna_idprop_ui_prop_default_set` only cares about assigning "default" value,
    add extra check in that function so that only `id_properties_ui` only called for supported
    types with default_value.

--- scripts/modules/rna_prop_ui.py
+++ scripts/modules/rna_prop_ui.py
@@ -69,6 +69,10 @@ def rna_idprop_value_item_type(value):
 
 
 def rna_idprop_ui_prop_default_set(item, prop, value):
+    prop_type, _ = rna_idprop_value_item_type(value)
+    if prop_type not in {bool, int, float, str}:
+        # Other IDprop types (ID, python, etc.) doesn't have a default value
+        return
     ui_data = item.id_properties_ui(prop)
     ui_data.update(default=value)
 
