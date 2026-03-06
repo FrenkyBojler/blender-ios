fc0b749bff7e5b9d0f3f1689d46412d01b1447cc
Jesse Yurkovich <jesse.y@gmail.com>
2026-03-05 23:21:29 -0800

    Fix: USD: Allow primvars to contain ':' characters

--- source/blender/io/usd/intern/usd_writer_material.cc
+++ source/blender/io/usd/intern/usd_writer_material.cc
@@ -672,7 +672,7 @@ static void create_uvmap_shader(const USDExporterContext &usd_export_context,
     uv_name = usdtokens::st;
   }
   /* We need to make valid, same as was done when exporting UV primvar. */
-  uv_name = make_safe_name(uv_name, usd_export_context.export_params.allow_unicode);
+  uv_name = make_safe_primvar_name(uv_name, usd_export_context.export_params.allow_unicode);
 
   uv_shader.CreateInput(usdtokens::varname, pxr::SdfValueTypeNames->String).Set(uv_name);
   usd_input.ConnectToSource(uv_shader.ConnectableAPI(), usdtokens::result);
