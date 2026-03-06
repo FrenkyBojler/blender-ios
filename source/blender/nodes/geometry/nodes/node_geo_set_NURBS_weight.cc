5799ad72f40037fb95c6636eb26ee4776588b541
Romain Guimbal <39882829+RomainGuimbal@users.noreply.github.com>
2026-03-06 18:13:42 +0100

    filename low cap

--- source/blender/nodes/geometry/nodes/node_geo_set_NURBS_weight.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_NURBS_weight.cc
@@ -9,7 +9,7 @@
 
 #include "node_geometry_util.hh"
 
-namespace blender::nodes::node_geo_set_NURBS_weight_cc {
+namespace blender::nodes::node_geo_set_nurbs_weight_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
@@ -97,4 +97,4 @@ static void node_register()
 }
 NOD_REGISTER_NODE(node_register)
 
-}  // namespace blender::nodes::node_geo_set_NURBS_weight_cc
+}  // namespace blender::nodes::node_geo_set_nurbs_weight_cc
