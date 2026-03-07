54f8c6a1ce90e08157f3dd13cbdf268b10d154c2
XxAbdo <abdulrahmanmohamed420@gmail.com>
2026-03-07 08:46:30 +0300

    Added an explanatory comment

--- source/blender/editors/space_view3d/view3d_select.cc
+++ source/blender/editors/space_view3d/view3d_select.cc
@@ -2468,6 +2468,7 @@ static Base *ed_view3d_give_base_under_cursor_ex(bContext *C,
   BKE_object_update_select_id(CTX_data_main(C));
 
   ViewContext vc = ED_view3d_viewcontext_init(C, depsgraph);
+  /* Signal for #view3d_gpu_select to skip edit-mode objects. */
   vc.obedit = nullptr;
 
   const bool do_nearest = !XRAY_ACTIVE(vc.v3d);
