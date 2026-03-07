36f45cdc3ffb573885d93eb001b589b87b99336c
quackarooni <alfonsomartzii@gmail.com>
2026-03-07 22:09:12 +0800

    Fix for link-drag search

--- source/blender/editors/space_node/link_drag_search.cc
+++ source/blender/editors/space_node/link_drag_search.cc
@@ -98,6 +98,12 @@ static void add_group_input_node_fn(nodes::LinkSearchOpParams &params)
   /* Add a group input based on the connected socket, and add a new group input node. */
   bNodeTreeInterfaceSocket *socket_iface = bke::node_interface::add_interface_socket_from_node(
       params.node_tree, params.node, params.socket, params.socket.name);
+
+  params.node_tree.tree_interface.foreach_item([&](bNodeTreeInterfaceItem &item) {
+    item.set_selected(false);
+    return true;
+  });
+
   params.node_tree.tree_interface.active_item_set(&socket_iface->item);
 
   bNode &group_input = params.add_node("NodeGroupInput"_ustr);
