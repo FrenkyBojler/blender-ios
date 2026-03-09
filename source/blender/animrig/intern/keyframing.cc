blender/blender

commit 7264f7797c23acdc697bd8f41bb60333622292a8

Animation: Parallelize check of existence keyframe at frame for data block with many actions
This improves the playback speed of actions with a high FCurve count
by using threading to find out if an Action has a key on a given frame.

This is used in the UI to display the font of the selected object
in a different color when it is keyed on the current frame.
Since that is checked during playback, the playback speed improves.

Improvement for `assigned_action_has_keyframe_at`
with the "high_bone_count_armature" test file

| main | PR |
| - | - |
| 1.23 ms | 0.62 ms |

Pull Request: https://projects.blender.org/blender/blender/pulls/150828

source/blender/animrig/intern/keyframing.cc
@@ -33,6 +33,7 @@
 #include "BLI_bit_vector.hh"
 #include "BLI_dynstr.h"
 #include "BLI_math_base.h"
+#include "BLI_task.hh"
 #include "BLI_utildefines.h"
 #include "BLT_translation.hh"
 
@@ -271,13 +272,25 @@ static bool assigned_action_has_keyframe_at(AnimData &adt, const float frame)
     return false;
   }
 
-  for (FCurve *fcu : animrig::fcurves_for_assigned_action(&adt)) {
-    if (fcurve_frame_has_keyframe(fcu, frame)) {
-      return true;
-    }
-  }
-
-  return false;
+  const Span<FCurve *> fcurves = animrig::fcurves_for_assigned_action(&adt);
+  /* 1024 is a common value for memory bandwidth limited tasks. The number isn't critical: 512
+   * works fine here, but 128 and 4096 seem to work equally well in testing. */
+  return threading::parallel_reduce<bool>(
+      fcurves.index_range(),
+      512,
+      false,
+      [&](const IndexRange range, const bool is_found) {
+        if (is_found) {
+          return true;
+        }
+        for (FCurve *fcu : fcurves.slice(range)) {
+          if (fcurve_frame_has_keyframe(fcu, frame)) {
+            return true;
+          }
+        }
+        return false;
+      },
+      std::logical_or<bool>());
 }
 
 /* Checks whether an Object has a keyframe for a given frame. */

FrenkyBojler/blender-ios

commit d30608afec8edcd03e2f5153b9e1ec2eb9897dae

Fix: Animation: Fix use-after-free in parallelized keyframe existence check
The Span was initialized from a temporary Vector returned by
fcurves_for_assigned_action, which led to a dangling pointer
during parallel_reduce.

This issue was introduced in 7264f7797c2.

source/blender/animrig/intern/keyframing.cc
@@ -272,7 +272,8 @@ static bool assigned_action_has_keyframe_at(AnimData &adt, const float frame)
     return false;
   }
 
-  const Span<FCurve *> fcurves = animrig::fcurves_for_assigned_action(&adt);
+  const Vector<FCurve *> fcurves_storage = animrig::fcurves_for_assigned_action(&adt);
+  const Span<FCurve *> fcurves = fcurves_storage;
   /* 1024 is a common value for memory bandwidth limited tasks. The number isn't critical: 512
    * works fine here, but 128 and 4096 seem to work equally well in testing. */
   return threading::parallel_reduce<bool>(