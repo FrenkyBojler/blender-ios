blender/blender

Fix #152087: Crash when undoing object.subdivision_set
When memfile undo is used inside Sculpt Mode, the `ob->sculpt`
`SculptSession` pointer is reset and regenerated when the memfile data
is read. Typically, this is also accompanied by a depsgraph update
which causes `sculpt_update_object` to be called. `sculpt_update_object`
is ultimately responsible for ensuring and re-populating data on the
`SculptSession`.

Prior to the pointer stability change in 4f3affd53d, there are certain
cases where a depsgraph update would not have been triggered with two
identical memfile steps. This leads to an invalid state where the BVH
would be generated as part of drawing the paint cursor, but the
`SubdivCCG` would not exist, leading to a crash in the draw code.

This commit is a mitigation for 4.5 which does not have the
aforementioned pointer change. To avoid crashing, we check for the
object being in Sculpt mode and manually tag the mesh for an
`ID_RECALC_GEOMETRY` update so that `sculpt_update_object` is stil
called.

Further changes are planned for `main`, as the previously linked commit
would be expected to *reduce* the amount of depsgraph updates, not
*increase* them. However, such changes will be too invasive to backport
to the LTS branch.

Pull Request: https://projects.blender.org/blender/blender/pulls/152648

source/blender/editors/undo/memfile_undo.cc
@@ -15,6 +15,7 @@
 
 #include "DNA_ID.h"
 #include "DNA_collection_types.h"
+#include "DNA_mesh_types.h"
 #include "DNA_node_types.h"
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
@@ -288,6 +289,20 @@ static void memfile_undosys_step_decode(
         Scene *scene = (Scene *)id;
         if (scene->master_collection != nullptr) {
           scene->master_collection->id.recalc_after_undo_push = 0;
+        }
+      }
+      else if (GS(id->name) == ID_OB) {
+        /* In some cases when using memfile undo in sculpt mode, the object but not the
+         * corresponding mesh will be tagged for an update, leading to invalid data and crashes.
+         *
+         * This is a band-aid mitigation for the 4.5 LTS release, not a proper fix of the
+         * underlying problem.
+         *
+         * See #152087 for more details. */
+        Object *object = reinterpret_cast<Object *>(id);
+        Mesh *mesh = static_cast<Mesh *>(object->data);
+        if (object->mode == OB_MODE_SCULPT && mesh) {
+          DEG_id_tag_update_ex(bmain, &mesh->id, ID_RECALC_GEOMETRY);
         }
       }
     }

FrenkyBojler/blender-ios

source/blender/editors/undo/memfile_undo.cc
@@ -15,6 +15,7 @@
 
 #include "DNA_ID.h"
 #include "DNA_collection_types.h"
+#include "DNA_mesh_types.h"
 #include "DNA_node_types.h"
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
@@ -294,6 +295,22 @@ static void memfile_undosys_step_decode(
         Scene *scene = id_cast<Scene *>(id);
         if (scene->master_collection != nullptr) {
           scene->master_collection->id.recalc_after_undo_push = 0;
+        }
+      }
+      else if (GS(id->name) == ID_OB) {
+        /* In some cases when using memfile undo in sculpt mode, the object but not the
+         * corresponding mesh will be tagged for an update, leading to invalid data and crashes.
+         *
+         * This is a band-aid mitigation for the 5.1 release, not a proper fix of the underlying
+         * problem.
+         *
+         * See #152087 for more details. */
+        Object *object = reinterpret_cast<Object *>(id);
+        if (object->type == OB_MESH) {
+          Mesh *mesh = id_cast<Mesh *>(object->data);
+          if (object->mode == OB_MODE_SCULPT && mesh) {
+            DEG_id_tag_update_ex(bmain, &mesh->id, ID_RECALC_GEOMETRY);
+          }
         }
       }
     }