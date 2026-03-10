ee516aa6330881b84d01514ccc06db0f80a440c9
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-03-10 11:02:05 +0100

    the change

--- source/blender/animrig/ANIM_action.hh
+++ source/blender/animrig/ANIM_action.hh
@@ -137,6 +137,15 @@ class Action : public bAction {
   bool layer_remove(Layer &layer_to_remove);
 
   /**
+   * Sets the given layer to be the active layer of the action.
+   */
+  void layer_set_active(Layer &layer);
+  /**
+   * Return the active layer or a nullptr if there is none.
+   */
+  Layer *layer_get_active();
+
+  /**
    * Ensure that there is at least one layer with the infinite keyframe strip.
    *
    * \note Within the limits of Project Baklava Phase 1, this means that there
