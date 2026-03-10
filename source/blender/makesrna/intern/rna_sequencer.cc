blender/blender

commit d8fd1385fc1299a11a3013b8d4fcc3ffcb6cfcfa

Fix #154634: VSE: Crash editing text from both properties and preview
Whenever text is modified from the properties panel, the text buffer is
deleted and re-allocated from RNA. However, `text->cursor_offset` would
remain unchanged and go out of sync with actual text. This led to
crashes/asserts if the resulting text length was less than the cursor
offset.

Pull Request: https://projects.blender.org/blender/blender/pulls/154999

source/blender/makesrna/intern/rna_sequencer.cc
@@ -937,6 +937,12 @@ static void rna_Strip_text_set(PointerRNA *ptr, const char *value)
   }
   text->text_ptr = BLI_strdup(value);
   text->text_len_bytes = strlen(text->text_ptr);
+  /* We cannot know where the user's cursor is if they edit text from the properties panel,
+   * so just reset it to the end to avoid the cursor getting out of sync with text length. */
+  text->cursor_offset = text->text_len_bytes;
+  /* Also clear any selection to avoid weird behavior. */
+  text->selection_start_offset = 0;
+  text->selection_end_offset = 0;
 }
 
 static StructRNA *rna_Strip_refine(PointerRNA *ptr)

FrenkyBojler/blender-ios

commit 62cc592e76bcdde9dff091b3c995596618af8aa4

Fix: VSE: Incorrect unicode cursor offset setting Strip text
text->cursor_offset is the character count, not byte length.

This issue was introduced in d8fd1385fc1.

source/blender/makesrna/intern/rna_sequencer.cc
@@ -939,7 +939,7 @@ static void rna_Strip_text_set(PointerRNA *ptr, const char *value)
   text->text_len_bytes = strlen(text->text_ptr);
   /* We cannot know where the user's cursor is if they edit text from the properties panel,
    * so just reset it to the end to avoid the cursor getting out of sync with text length. */
-  text->cursor_offset = text->text_len_bytes;
+  text->cursor_offset = BLI_strlen_utf8(text->text_ptr);
   /* Also clear any selection to avoid weird behavior. */
   text->selection_start_offset = 0;
   text->selection_end_offset = 0;