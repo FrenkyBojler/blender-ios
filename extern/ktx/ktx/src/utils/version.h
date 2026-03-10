882f0289f9845d2e504cb914fd10000b1830021c
Julien Duroure <julien.duroure@gmail.com>
2026-03-10 01:11:20 +0100

    WIP - initial libktx import in extern

---
+++ extern/ktx/ktx/src/utils/version.h
@@ -0,0 +1,11 @@
+/* SPDX-FileCopyrightText: 2026 KhronosGroup
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Manually written for Blender integration (upstream file is CMake-generated). */
+
+#pragma once
+
+#define LIBKTX_VERSION "4.4.2"
+#define LIBKTX_DEFAULT_VERSION "4.4.2"
