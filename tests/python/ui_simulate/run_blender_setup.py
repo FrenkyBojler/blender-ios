55568612526805736bac431453d16e8a8aa6afef
Christoph Neuhauser <christoph.neuhauser@intel.com>
2026-03-11 17:13:17 +0100

    Remove UI Vulkan tests AMD Windows blocklist

--- tests/python/ui_simulate/run_blender_setup.py
+++ tests/python/ui_simulate/run_blender_setup.py
@@ -96,9 +96,6 @@ def main():
         if sys.platform == "win32" and gpu_device == "INTEL" and gpu_backend == "OPENGL":
             # See #149084 for the tracking issue
             BLOCKLIST = ["test_workspace"]
-        if sys.platform == "win32" and gpu_device == "AMD" and gpu_backend == "VULKAN":
-            # See #151411 for the tracking issue
-            BLOCKLIST = ["test_sculpt", "test_tools", "test_undo", "test_workspace"]
 
     is_first = True
     for test_id in args.tests:
