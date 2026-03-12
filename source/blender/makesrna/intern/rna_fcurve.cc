blender/blender

commit 5a882c76281da040076eb5e084eed7ac4eb249ca

Fix: FCurve Settings RNA paths incomplete
Fill the missing gaps by going over the action data until we find the
corresponding FCurve in Layer < Slots > Channebags > FCurves.
Channelbads have their `_path` function already, so we can piggyback on
that, just adding the proper FCurve index to the mix.

Part of #137407

Pull Request: https://projects.blender.org/blender/blender/pulls/137956

source/blender/makesrna/intern/rna_fcurve.cc
@@ -24,6 +24,8 @@
 
 #include "ED_keyframing.hh"
 
+#include <fmt/format.h>
+
 const EnumPropertyItem rna_enum_fmodifier_type_items[] = {
     {FMODIFIER_TYPE_NULL, "NULL", 0, "Invalid", ""},
     {FMODIFIER_TYPE_GENERATOR,
@@ -549,6 +551,34 @@ static void rna_FKeyframe_ctrlpoint_ui_set(PointerRNA *ptr, const float *values)
 }
 
 /* ****************************** */
+
+static std::optional<std::string> rna_FCurve_path(const PointerRNA *ptr)
+{
+  using namespace blender;
+  FCurve *fcurve = reinterpret_cast<FCurve *>(ptr->data);
+  animrig::Action &action = reinterpret_cast<bAction *>(ptr->owner_id)->wrap();
+
+  for (animrig::Layer *layer : action.layers()) {
+    for (animrig::Strip *strip : layer->strips()) {
+      if (strip->type() != animrig::Strip::Type::Keyframe) {
+        continue;
+      }
+
+      animrig::StripKeyframeData &strip_data = strip->data<animrig::StripKeyframeData>(action);
+      for (animrig::Channelbag *channelbag : strip_data.channelbags()) {
+        const int fcurve_index = channelbag->fcurves().first_index_try(fcurve);
+        if (fcurve_index != -1) {
+          PointerRNA channelbag_ptr = RNA_pointer_create_discrete(
+              &action.id, &RNA_ActionChannelbag, channelbag);
+          const std::optional<std::string> channelbag_path = rna_Channelbag_path(&channelbag_ptr);
+          return fmt::format("{}.fcurves[{}]", *channelbag_path, fcurve_index);
+        }
+      }
+    }
+  }
+
+  return std::nullopt;
+}
 
 static void rna_FCurve_RnaPath_get(PointerRNA *ptr, char *value)
 {
@@ -2538,6 +2568,7 @@ static void rna_def_fcurve(BlenderRNA *brna)
   srna = RNA_def_struct(brna, "FCurve", nullptr);
   RNA_def_struct_ui_text(srna, "F-Curve", "F-Curve defining values of a period of time");
   RNA_def_struct_ui_icon(srna, ICON_ANIM_DATA);
+  RNA_def_struct_path_func(srna, "rna_FCurve_path");
 
   /* Enums */
   prop = RNA_def_property(srna, "extrapolation", PROP_ENUM, PROP_NONE);

source/blender/makesrna/intern/rna_action.cc
@@ -585,7 +585,7 @@ static bool rna_ActionStrip_key_insert(ID *dna_action_id,
   return ok;
 }
 
-static std::optional<std::string> rna_Channelbag_path(const PointerRNA *ptr)
+std::optional<std::string> rna_Channelbag_path(const PointerRNA *ptr)
 {
   animrig::Action &action = rna_action(ptr);
   animrig::Channelbag &cbag_to_find = rna_data_channelbag(ptr);

source/blender/makesrna/intern/rna_internal.hh
@@ -395,6 +395,8 @@ std::optional<std::string> rna_ColorManagedDisplaySettings_path(const PointerRNA
 std::optional<std::string> rna_ColorManagedViewSettings_path(const PointerRNA *ptr);
 std::optional<std::string> rna_ColorManagedInputColorspaceSettings_path(const PointerRNA *ptr);
 
+std::optional<std::string> rna_Channelbag_path(const PointerRNA *ptr);
+
 /* Node socket subtypes for group interface. */
 void rna_def_node_socket_interface_subtypes(BlenderRNA *brna);
 

FrenkyBojler/blender-ios

commit a499476e0e2982e95c84b6ff25ecd5028761918d

Fix: FCurve Settings RNA paths broken
This was implemented in 5a882c76281d but broken by a followup fix
54a7b0bc6144

`GS()` works with the ID **name** not the ID itself.

(this will also make the nested `.keyframe_points` collection return a
valid path)

This came up in #155468

source/blender/makesrna/intern/rna_fcurve.cc
@@ -588,7 +588,7 @@ static std::optional<std::string> rna_FCurve_path(const PointerRNA *ptr)
 
   /* If the F-Curve is not owned by an Action, bail out early. It could be a driver, NLA control
    * curve, or stored in some place that's yet unknown at the time of writing of this code. */
-  if (GS(ptr->owner_id) != ID_AC) {
+  if (GS(ptr->owner_id->name) != ID_AC) {
     return {};
   }
 