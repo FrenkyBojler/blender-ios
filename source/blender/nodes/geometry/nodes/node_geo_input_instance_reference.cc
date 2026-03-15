ed4c8062174db85dba769a120484c3912828fd7d
Cartesian Caramel <cartesiancaramel@gmail.com>
2026-03-15 12:09:11 -0400

    rename to Instance Reference

--- source/blender/nodes/geometry/nodes/node_geo_input_instance_reference.cc
+++ source/blender/nodes/geometry/nodes/node_geo_input_instance_reference.cc
@@ -4,7 +4,7 @@
 
 #include "node_geometry_util.hh"
 
-namespace blender::nodes::node_geo_input_instance_reference_index_cc {
+namespace blender::nodes::node_geo_input_instance_reference_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
@@ -21,8 +21,8 @@ static void node_register()
 {
   static bke::bNodeType ntype;
 
-  geo_node_type_base(&ntype, "GeometryNodeInputInstanceReferenceIndex");
-  ntype.ui_name = "Instance Reference Index";
+  geo_node_type_base(&ntype, "GeometryNodeInputInstanceReference");
+  ntype.ui_name = "Instance Reference";
   ntype.ui_description = "Output the reference index of the instance";
   ntype.nclass = NODE_CLASS_INPUT;
   ntype.geometry_node_execute = node_geo_exec;
@@ -31,4 +31,4 @@ static void node_register()
 }
 NOD_REGISTER_NODE(node_register)
 
-}  // namespace blender::nodes::node_geo_input_instance_reference_index_cc
+}  // namespace blender::nodes::node_geo_input_instance_reference_cc
