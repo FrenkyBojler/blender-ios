aeaf8a782d1f2f3bab9a3e645c909d041d19daf5
Jeroen Bakker <jeroen@blender.org>
2026-03-16 15:19:10 +0100

    Use is_undefined()

--- source/blender/nodes/shader/node_shader_tree.cc
+++ source/blender/nodes/shader/node_shader_tree.cc
@@ -301,10 +301,10 @@ static void ntree_shader_unlink_script_nodes(bNodeTree *ntree)
   }
 }
 
-static void ntree_shader_unlink_custom_nodes(bNodeTree *ntree)
+static void ntree_shader_unlink_undefined_nodes(bNodeTree *ntree)
 {
   for (bNodeLink &link : ntree->links.items_mutable()) {
-    if ((link.tonode->type_legacy == NODE_CUSTOM) || (link.fromnode->type_legacy == NODE_CUSTOM)) {
+    if (link.tonode->is_undefined() || link.fromnode->is_undefined()) {
       bke::node_remove_link(ntree, link);
     }
   }
@@ -985,7 +985,7 @@ void ntreeGPUMaterialNodes(bNodeTree *localtree, GPUMaterial *mat)
   bNodeTreeExec *exec;
 
   ntree_shader_unlink_script_nodes(localtree);
-  ntree_shader_unlink_custom_nodes(localtree);
+  ntree_shader_unlink_undefined_nodes(localtree);
   bNode *output = ntreeShaderOutputNode(localtree, SHD_OUTPUT_EEVEE);
 
   /* Tree is valid if it contains no undefined implicit socket type cast. */
