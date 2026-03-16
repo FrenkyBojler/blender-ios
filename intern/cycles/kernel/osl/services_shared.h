45c858285c70bbf937e763576163fa1397d408e4
Brecht Van Lommel <brecht@blender.org>
2026-03-16 19:07:00 +0100

    Fix now unnecessary OSL derivative flipping

--- intern/cycles/kernel/osl/services_shared.h
+++ intern/cycles/kernel/osl/services_shared.h
@@ -717,8 +717,7 @@ ccl_device bool osl_shared_texture(KernelGlobals kg,
 
   switch (type) {
     case OSLTextureHandleType::IMAGE: {
-      const dual2 uv({s, t}, {dsdx, -dtdx}, {dsdy, -dtdy});
-
+      const dual2 uv({s, t}, {dsdx, dtdx}, {dsdy, dtdy});
       const float4 rgba = kernel_image_interp_with_udim(kg, sd, image_texture_or_udim_id, uv);
 
       rgba_to_nchannels(rgba, nchannels, result);
