02038e42a9a95a4081c7a0f12038917c602e826c
Habib Gahbiche <habib@blender.org>
2026-03-16 10:37:29 +0100

    Move inside ED_space_image_set

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -1981,13 +1981,7 @@ static void rna_SpaceImageEditor_image_set(PointerRNA *ptr,
 {
   BLI_assert(BKE_id_is_in_global_main(static_cast<ID *>(value.data)));
   SpaceImage *sima = static_cast<SpaceImage *>(ptr->data);
-  Image *ima = static_cast<Image *>(value.data);
-  if (ima) {
-    sima->xof = ima->runtime->view_offset[0];
-    sima->yof = ima->runtime->view_offset[1];
-    sima->zoom = ima->runtime->view_zoom;
-  }
-  ED_space_image_set(G_MAIN, sima, ima, false);
+  ED_space_image_set(G_MAIN, sima, static_cast<Image *>(value.data), false);
 }
 
 static void rna_SpaceImageEditor_mask_set(PointerRNA *ptr,
@@ -2477,8 +2471,8 @@ static void rna_ConsoleLine_body_set(PointerRNA *ptr, const char *value)
   ConsoleLine *ci = static_cast<ConsoleLine *>(ptr->data);
   size_t len = strlen(value);
 
-  if ((len >= size_t(ci->len_alloc)) || (len * 2 < size_t(ci->len_alloc)))
-  { /* allocate a new string */
+  if ((len >= size_t(ci->len_alloc)) ||
+      (len * 2 < size_t(ci->len_alloc))) { /* allocate a new string */
     MEM_delete(ci->line);
     ci->line = MEM_new_array_uninitialized<char>(len + 1, "rna_consoleline");
     ci->len_alloc = int(len + 1);
