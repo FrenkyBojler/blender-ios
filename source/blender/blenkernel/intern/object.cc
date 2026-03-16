blender/blender

commit c0baf6ad65d938b04cc6230eb8010a0ca5495ecd

Fix: correct path for foreach_path callback of object's particle systems
Correctly process objects' particle systems in `object_foreach_path()`.
The `//blendcache_particle` directory is now reported.

Previously the code reported the `cache->path` property, for each
cache in the particle system. However, that path is only to be used
when the 'External' checkbox is checked.

Pull Request: https://projects.blender.org/blender/blender/pulls/155720

source/blender/blenkernel/intern/object.cc
@@ -54,6 +54,7 @@
 #include "BLI_math_matrix.h"
 #include "BLI_math_rotation.h"
 #include "BLI_math_vector_types.hh"
+#include "BLI_path_utils.hh"
 #include "BLI_string.h"
 #include "BLI_string_utf8.h"
 #include "BLI_threads.h"
@@ -522,6 +523,62 @@ static void object_foreach_path_pointcache(ListBaseT<PointCache> *ptcache_list,
   }
 }
 
+static void object_foreach_path_particles(Object *ob, BPathForeachPathData *bpath_data)
+{
+  /* Particle systems are stored on disk in a different way than the generic pointcache files.
+   * Or at least not in the path stored in cache->path. So that's why there's no call to
+   * object_foreach_path_pointcache() here. */
+
+  const Library *lib = ob->id.lib;
+  const char *blendfile_path = lib ? BKE_main_blendfile_path_from_library(*lib) :
+                                     BKE_main_blendfile_path(bpath_data->bmain);
+
+  for (ParticleSystem &psys : ob->particlesystem) {
+    bool all_caches_external = true;
+
+    if (psys.part->type == PART_HAIR && (psys.part->flag & PSYS_HAIR_DYNAMICS) == 0) {
+      /* Hair system without dynamics, this means it doesn't use its particle cache.
+       * NOTE: the PSYS_HAIR_DYNAMICS flag can be animated, so technically this is only correct for
+       * the current frame. */
+      continue;
+    }
+
+    for (PointCache &cache : psys.ptcaches) {
+      /* When the 'External' checkbox is checked, the regular pointcache path is used. */
+      if (cache.flag & PTCACHE_EXTERNAL) {
+        BKE_bpath_foreach_path_fixed_process(bpath_data, cache.path, sizeof(cache.path));
+      }
+      else {
+        all_caches_external = false;
+      }
+    }
+
+    if (all_caches_external) {
+      /* If all caches use the 'External' flag, the calls above have covered this particle system,
+       * and the code below can be skipped. */
+      continue;
+    }
+
+    PTCacheID pid;
+    BKE_ptcache_id_from_particles(&pid, ob, &psys);
+
+    /* Just report the directory that contains the pointcache files. */
+    char ptcache_path[FILE_MAX];
+    const int ptcache_path_len = BKE_ptcache_path(&pid, ptcache_path);
+
+    /* The path can be modified by BKE_bpath_foreach_path_fixed_process(), for example when using
+     * "Save As..." to write the blend file to another directory. In that case blendfile-relative
+     * paths are updated, so that they still point to the same physical location on disk. This
+     * rewriting is not supported here, as the particle system cache is always in the same
+     * directory as the blend file itself.
+     *
+     * And because it's always in the same directory, it makes sense to convert the path to a
+     * blendfile-relative path. */
+    BLI_path_rel(ptcache_path, blendfile_path);
+    BKE_bpath_foreach_path_fixed_process(bpath_data, ptcache_path, ptcache_path_len);
+  }
+}
+
 static void object_foreach_path(ID *id, BPathForeachPathData *bpath_data)
 {
   Object *ob = reinterpret_cast<Object *>(id);
@@ -569,9 +626,7 @@ static void object_foreach_path(ID *id, BPathForeachPathData *bpath_data)
     object_foreach_path_pointcache(&ob->soft->shared->ptcaches, bpath_data);
   }
 
-  for (ParticleSystem &psys : ob->particlesystem) {
-    object_foreach_path_pointcache(&psys.ptcaches, bpath_data);
-  }
+  object_foreach_path_particles(ob, bpath_data);
 }
 
 static void object_foreach_cache(ID *id,

source/blender/blenkernel/BKE_pointcache.h
@@ -319,6 +319,15 @@ int BKE_ptcache_object_reset(struct Scene *scene, struct Object *ob, int mode);
 
 void BKE_ptcache_update_info(PTCacheID *pid);
 
+/**
+ * Obtain the file path for this ptcache.
+ *
+ * \param dirname: pointer to write the cache path to, should be at least MAX_PTCACHE_PATH bytes
+ * (see pointcache.cc, at the moment of writing this is equal to FILE_MAX).
+ * \returns: strlen(dirname).
+ */
+int BKE_ptcache_path(PTCacheID *pid, char *dirname);
+
 /*********** General cache reading/writing ******************/
 
 /**

source/blender/blenkernel/intern/pointcache.cc
@@ -1305,7 +1305,7 @@ static int ptcache_frame_from_filename(const char *filename, const char *ext)
 #define MAX_PTCACHE_PATH FILE_MAX
 #define MAX_PTCACHE_FILE (FILE_MAX * 2)
 
-static int ptcache_path(PTCacheID *pid, char dirname[MAX_PTCACHE_PATH])
+int BKE_ptcache_path(PTCacheID *pid, char dirname[MAX_PTCACHE_PATH])
 {
   const char *blendfile_path = BKE_main_blendfile_path_from_global();
   Library *lib = (pid->owner_id) ? pid->owner_id->lib : nullptr;
@@ -1417,7 +1417,7 @@ static int ptcache_filepath(PTCacheID *pid,
 
   /* start with temp dir */
   if (do_path) {
-    len = ptcache_path(pid, filepath);
+    len = BKE_ptcache_path(pid, filepath);
     newname += len;
   }
   if (pid->cache->name[0] == '\0' && (pid->cache->flag & PTCACHE_EXTERNAL) == 0) {
@@ -2547,7 +2547,7 @@ void BKE_ptcache_id_clear(PTCacheID *pid, int mode, uint cfra)
     case PTCACHE_CLEAR_BEFORE:
     case PTCACHE_CLEAR_AFTER:
       if (pid->cache->flag & PTCACHE_DISK_CACHE) {
-        ptcache_path(pid, path);
+        BKE_ptcache_path(pid, path);
 
         dir = opendir(path);
         if (dir == nullptr) {
@@ -2753,7 +2753,7 @@ void BKE_ptcache_id_time(
       char ext[MAX_PTCACHE_FILE];
       uint len; /* store the length of the string */
 
-      ptcache_path(pid, path);
+      BKE_ptcache_path(pid, path);
 
       len = ptcache_filepath(pid, filepath, int(cfra), false, false); /* no path */
 
@@ -3470,7 +3470,7 @@ void BKE_ptcache_disk_cache_rename(PTCacheID *pid, const char *name_src, const c
 
   len = ptcache_filepath(pid, old_filepath, 0, false, false); /* no path */
 
-  ptcache_path(pid, path);
+  BKE_ptcache_path(pid, path);
   dir = opendir(path);
   if (dir == nullptr) {
     STRNCPY(pid->cache->name, old_name);
@@ -3521,7 +3521,7 @@ void BKE_ptcache_load_external(PTCacheID *pid)
     return;
   }
 
-  ptcache_path(pid, path);
+  BKE_ptcache_path(pid, path);
 
   len = ptcache_filepath(pid, filepath, 1, false, false); /* no path */
 

FrenkyBojler/blender-ios

commit 0248342383c98189eb2538bf89712db6578ef066

Fix: incorrect buffer size in object_foreach_path_particles
The BKE_bpath_foreach_path_fixed_process function expects the size of
the buffer, not the current length of the string.

This issue was introduced in c0baf6ad65d.

source/blender/blenkernel/intern/object.cc
@@ -564,7 +564,7 @@ static void object_foreach_path_particles(Object *ob, BPathForeachPathData *bpat
 
     /* Just report the directory that contains the pointcache files. */
     char ptcache_path[FILE_MAX];
-    const int ptcache_path_len = BKE_ptcache_path(&pid, ptcache_path);
+    BKE_ptcache_path(&pid, ptcache_path);
 
     /* The path can be modified by BKE_bpath_foreach_path_fixed_process(), for example when using
      * "Save As..." to write the blend file to another directory. In that case blendfile-relative
@@ -575,7 +575,7 @@ static void object_foreach_path_particles(Object *ob, BPathForeachPathData *bpat
      * And because it's always in the same directory, it makes sense to convert the path to a
      * blendfile-relative path. */
     BLI_path_rel(ptcache_path, blendfile_path);
-    BKE_bpath_foreach_path_fixed_process(bpath_data, ptcache_path, ptcache_path_len);
+    BKE_bpath_foreach_path_fixed_process(bpath_data, ptcache_path, sizeof(ptcache_path));
   }
 }
 