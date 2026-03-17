blender/blender

commit 40177a8426fca6ab75ed5fcd9f532c939195b759

UI: Tree View: support drag&drop below last element
Previously, when reordering a tree view item to the end, one had to move it drop
it fairly precisely on the lower half of the last item in the list. This patch
makes it possible to just drop it below the last item too. The outliner has the
same functionality.

Pull Request: https://projects.blender.org/blender/blender/pulls/139025

source/blender/editors/interface/views/interface_view.cc
@@ -284,6 +284,24 @@ std::unique_ptr<DropTargetInterface> region_views_find_drop_target_at(const AReg
     }
   }
 
+  if (AbstractView *view = UI_region_view_find_at(region, xy, 0)) {
+    /* If we are above a tree, but not hovering any specific element, dropping something should
+     * insert it after the last item. */
+    if (AbstractTreeView *tree_view = dynamic_cast<AbstractTreeView *>(view)) {
+      /* Find the last item which we want to drop below. */
+      AbstractTreeViewItem *last_item = nullptr;
+      tree_view->foreach_root_item([&](AbstractTreeViewItem &item) {
+        if (!item.is_interactive()) {
+          return;
+        }
+        last_item = &item;
+      });
+      if (last_item) {
+        return last_item->create_item_drop_target();
+      }
+    }
+  }
+
   return nullptr;
 }
 

source/blender/editors/include/UI_tree_view.hh
@@ -136,6 +136,7 @@ class AbstractTreeView : public AbstractView, public TreeViewItemContainer {
   void draw_overlays(const ARegion &region, const uiBlock &block) const override;
 
   void foreach_item(ItemIterFn iter_fn, IterOptions options = IterOptions::None) const;
+  void foreach_root_item(ItemIterFn iter_fn) const;
 
   bool is_fully_visible() const override;
   void scroll(ViewScrollDirection direction) override;

source/blender/editors/interface/views/tree_view.cc
@@ -110,6 +110,13 @@ void AbstractTreeView::foreach_item(ItemIterFn iter_fn, IterOptions options) con
   this->foreach_item_recursive(iter_fn, options);
 }
 
+void AbstractTreeView::foreach_root_item(ItemIterFn iter_fn) const
+{
+  for (const auto &child : children_) {
+    iter_fn(*child);
+  }
+}
+
 AbstractTreeViewItem *AbstractTreeView::find_hovered(const ARegion &region, const int2 &xy)
 {
   AbstractTreeViewItem *hovered_item = nullptr;
@@ -414,6 +421,9 @@ std::optional<DropLocation> TreeViewItemDropTarget::choose_drop_location(
           3;
   const float segment_height = item_height / segment_count;
 
+  if (event.xy[1] < win_rect->ymin) {
+    return DropLocation::After;
+  }
   if (event.xy[1] - win_rect->ymin > (item_height - segment_height)) {
     return DropLocation::Before;
   }

FrenkyBojler/blender-ios

commit 54bae2147d34e0dbfc5a0f0b52453d7df15c42bb

Fix: Assert during drag operation near edges of tree view
Caused by 40177a8426fca6ab75ed5fcd9f532c939195b759.
`region_views_find_drop_target_at` returns last element when function fails
to find view item below the mouse. This would work when mouse is over
non-interactible view item but triggers assert near edges of the view.
Add extra check inside `region_views_find_drop_target_at` i.e. only return
last element when all items are visible and mouse coords are below the the last
item.

source/blender/editors/interface/views/interface_view.cc
@@ -279,6 +279,9 @@ std::unique_ptr<DropTargetInterface> region_views_find_drop_target_at(const AReg
     /* If we are above a tree, but not hovering any specific element, dropping something should
      * insert it after the last item. */
     if (AbstractTreeView *tree_view = dynamic_cast<AbstractTreeView *>(view)) {
+      if (!tree_view->is_fully_visible()) {
+        return nullptr;
+      }
       /* Find the last item which we want to drop below. */
       AbstractTreeViewItem *last_item = nullptr;
       tree_view->foreach_root_item([&](AbstractTreeViewItem &item) {
@@ -288,7 +291,10 @@ std::unique_ptr<DropTargetInterface> region_views_find_drop_target_at(const AReg
         last_item = &item;
       });
       if (last_item) {
-        return last_item->create_item_drop_target();
+        std::optional<rctf> rct = last_item->get_win_rect(*region);
+        if (rct && xy[1] < rct->ymin) {
+          return last_item->create_item_drop_target();
+        }
       }
     }
   }