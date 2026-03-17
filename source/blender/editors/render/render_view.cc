561d4930718638960d2dd61aff137fcd9b4cc9a5
Harley Acheson <harley.acheson@gmail.com>
2026-03-17 09:45:58 -0700

    Use StringRef for Image Editor window key

--- source/blender/editors/render/render_view.cc
+++ source/blender/editors/render/render_view.cc
@@ -155,7 +155,7 @@ ScrArea *render_view_open(bContext *C, int mx, int my, ReportList *reports)
 
     WM_window_dpi_set_userdef(CTX_wm_window(C));
 
-    std::string key = "IMAGE_EDITOR";
+    const StringRef key = "IMAGE_EDITOR";
     std::vector<float> bounds = ui_memory::memory.open("temp.window.dimensions")[key];
 
     const bool bounds_valid = (bounds.size() == 4 && (bounds[1] - bounds[0] > 150.0f) &&
