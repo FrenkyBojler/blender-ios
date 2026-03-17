f49fe5b8bd51f5cb23599869b21110de28d95285
Richard Antalik <richardantalik@gmail.com>
2026-03-17 20:29:21 +0100

    Fix #155775: mov_av_codec_id_get reaches unreachable code
    Caused by loading old, mildly broken file.
    
    Commit 62e25e315a decoupled values of codec enum used by FFmpeg and
    Blender, but that was done during development of Blender 4.5.
    
    Files saved in previous Blender versions may use different values. In
    this paricular case value for FFMPEG_CODEC_ID_H264 was changed from 28 to
    27 in Blender version 2.8, but no versioning code was added.
    
    Versioning code is added to 5.2 version only, because there is no
    previous versioning for codec value.

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -111,6 +111,14 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     }
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 9)) {
+    for (Scene &scene : bmain->scenes) {
+      if (scene.r.ffcodecdata.codec == 28) {
+        scene.r.ffcodecdata.codec = 27;
+      }
+    }
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
