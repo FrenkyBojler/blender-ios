ca43ce5d0911b79669d3ae14545ce93f5b60487d
Sean Kim <SeanCTKim@protonmail.com>
2026-03-18 15:15:40 -0700

    Fix: Failing UI tests after recent change
    Introduced with afc6d7a463e6bb01b54e207d18ba41c4eeb17c03

--- tests/python/ui_simulate/test_tools.py
+++ tests/python/ui_simulate/test_tools.py
@@ -87,7 +87,7 @@ def sculpt_mode_toolbar():
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.cloth_filter")
 
     yield e.shift.space()
-    yield e.ctrl.eight()
+    yield e.ctrl.x()
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.color_filter")
 
     yield e.shift.space()
@@ -95,19 +95,19 @@ def sculpt_mode_toolbar():
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.face_set_edit")
 
     yield e.shift.space()
-    yield e.ctrl.nine()
+    yield e.ctrl.eight()
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.mask_by_color")
 
     yield e.shift.space()
-    yield e.ctrl.zero()
+    yield e.ctrl.nine()
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.move")
 
     yield e.shift.space()
-    yield e.alt.one()
+    yield e.ctrl.zero()
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.rotate")
 
     yield e.shift.space()
-    yield e.alt.two()
+    yield e.alt.one()
     t.assertEqual(window.workspace.tools.from_space_view3d_mode('SCULPT').idname, "builtin.scale")
 
     yield e.shift.space()
