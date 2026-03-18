50b88f5b6f56feebcd2403b5f6800c1e11a7efc6
Lukas Tönne <lukas@blender.org>
2026-03-18 14:02:49 +0100

    Fix #147793: Quaternion slerp implementation for large mix factors
    The `interpolate` function for quaternions uses the geometric slerp
    implementation (https://en.wikipedia.org/wiki/Spherical_linear_interpolation#Geometric_slerp)
    which requires division. For small Omega angles it falls back to a
    linear approximation to avoid numerical errors. This only works if
    the `t` factor is in the [0, 1] interval, which is expected but not
    enforced. The shader/geometry _Mix_ node in particular allows arbitrary
    mix factors, which results in extrapolation and scaling instead of
    rotation.
    
    Use the quaternion slerp implementation for `t` outside of [0,1].
    This does not suffer from the extrapolation problem and works for
    arbitrary angles.
    
    - [ ] Find a way to always use the geometric method in cases where
      `t` is guaranteed to be in [0,1]. This is difficult to determine
      because it's not easy to find all code paths using this `interpolate`
      function implementation.

--- source/blender/blenlib/BLI_math_quaternion.hh
+++ source/blender/blenlib/BLI_math_quaternion.hh
@@ -262,7 +262,7 @@ template<typename T> [[nodiscard]] inline QuaternionBase<T> normalize(const Quat
 }
 
 /**
- * Generic function for implementing slerp
+ * Generic function for implementing slerp on the [0, 1] interval
  * (quaternions and spherical vector coords).
  *
  * \param t: factor in [0..1]
@@ -274,6 +274,7 @@ template<typename T>
 {
   const T eps = T(1e-4);
 
+  BLI_assert(IN_RANGE(t, 0.0, 1.0));
   BLI_assert(IN_RANGE_INCL(cosom, T(-1.0001), T(1.0001)));
 
   VecBase<T, 2> w;
@@ -307,8 +308,13 @@ template<typename T>
   using Vec4T = VecBase<T, 4>;
   BLI_assert(is_unit_scale(a));
   BLI_assert(is_unit_scale(b));
-  VecBase<T, 2> w = interpolate_dot_slerp(t, dot(a, b));
-  return QuaternionBase<T>(w[0] * Vec4T(a) + w[1] * Vec4T(b));
+  if (IN_RANGE(t, 0.0, 1.0)) {
+    VecBase<T, 2> w = interpolate_dot_slerp(t, dot(a, b));
+    return QuaternionBase<T>(w[0] * Vec4T(a) + w[1] * Vec4T(b));
+  }
+
+  /* Quaternion slerp for arbitrary interpolation factors. */
+  return a * math::pow(math::invert_normalized(a) * b, t);
 }
 
 template<typename T>
