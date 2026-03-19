ea63b2074efab4076b50e5e8c19dbceb4c1bb01e
Sean Kim <SeanCTKim@protonmail.com>
2026-03-18 18:10:05 -0700

    Fix: GP Draw mode gets incorrect size & strength values via BKE API
    GP Draw mode does not allow the user to set or toggle the unified paint
    settings and values for size, strength, and color. This commit disables
    the mode-specific flags and also ensures that the accessor methods
    respect this exclusion.

--- source/blender/blenkernel/intern/paint.cc
+++ source/blender/blenkernel/intern/paint.cc
@@ -596,6 +596,24 @@ PaintMode BKE_paintmode_get_from_tool(const bToolRef *tref)
   return PaintMode::Invalid;
 }
 
+bool BKE_paint_use_unified_size(const Paint *paint)
+{
+  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
+    return false;
+  }
+
+  return paint->unified_paint_settings.flag & UNIFIED_PAINT_SIZE;
+}
+
+bool BKE_paint_use_unified_strength(const Paint *paint)
+{
+  if (paint->runtime->ob_mode == OB_MODE_PAINT_GREASE_PENCIL) {
+    return false;
+  }
+
+  return paint->unified_paint_settings.flag & UNIFIED_PAINT_ALPHA;
+}
+
 bool BKE_paint_use_unified_color(const Paint *paint)
 {
   /* Grease pencil draw mode never uses unified paint. */
