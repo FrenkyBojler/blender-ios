bc52600f0cbd48853ff8609f0171d880c7d3e8aa
Philipp Oeser <philipp@blender.org>
2026-03-19 15:38:47 +0100

    from review WIP
    make this backward compatible by adding a couple of nodes to restore the
    old behavior
    
    TODO:
    - fix tests
    - actually use the cached link

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -10,10 +10,13 @@
 
 #include "DNA_ID.h"
 #include "DNA_brush_types.h"
+#include "DNA_node_types.h"
 
 #include "BLI_listbase_iterator.hh"
+#include "BLI_math_vector.h"
 #include "BLI_sys_types.h"
 
+#include "BKE_attribute.hh"
 #include "BKE_main.hh"
 #include "BKE_mesh_legacy_convert.hh"
 #include "BKE_node.hh"
@@ -52,6 +55,160 @@ static void do_version_file_output_use_file_extension_recursive(bNodeTree &node_
   }
 }
 
+static void do_version_geometry_node_primitive_uvmaps(bNodeTree *node_tree)
+{
+  using bke::AttrDomain;
+
+  Map<bNodeSocket *, bNodeLink *> links_done;
+
+  for (bNodeLink &link : node_tree->links) {
+    if (!ELEM(link.fromnode->type_legacy,
+              GEO_NODE_MESH_PRIMITIVE_UV_SPHERE,
+              GEO_NODE_MESH_PRIMITIVE_CYLINDER))
+    {
+      continue;
+    }
+    if (!STREQ(link.fromsock->identifier, "UV Map")) {
+      continue;
+    }
+    /* TODO: actually reuse existing link */
+    bNodeLink *existing_link = links_done.lookup_default(link.fromsock, nullptr);
+    if (existing_link != nullptr) {
+      continue;
+    }
+    /* TODO: split up in two functions? */
+    if (link.fromnode->type_legacy == GEO_NODE_MESH_PRIMITIVE_UV_SPHERE) {
+      /* Multiply node. */
+      bNode *multiply_node = bke::node_add_node(nullptr, *node_tree, "ShaderNodeVectorMath");
+      multiply_node->parent = link.fromnode->parent;
+      multiply_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 20.0f;
+      multiply_node->location[1] = link.fromnode->location[1];
+      multiply_node->custom1 = NODE_VECTOR_MATH_MULTIPLY;
+      bNodeSocket *multiply_a_input = bke::node_find_socket(*multiply_node, SOCK_IN, "Vector");
+      bNodeSocket *multiply_b_input = bke::node_find_socket(*multiply_node, SOCK_IN, "Vector_001");
+      bNodeSocket *multiply_output = bke::node_find_socket(*multiply_node, SOCK_OUT, "Vector");
+      copy_v3_v3(static_cast<bNodeSocketValueVector *>(multiply_b_input->default_value)->value,
+                 float3(1.0f, -1.0f, 0.0f));
+      version_node_add_link(
+          *node_tree, *link.fromnode, *link.fromsock, *multiply_node, *multiply_a_input);
+
+      /* Wire up to original socket. */
+      bNodeLink *new_link = &version_node_add_link(
+          *node_tree, *multiply_node, *multiply_output, *link.tonode, *link.tosock);
+
+      /* Add the new link to the cache. */
+      links_done.add_new(link.fromsock, new_link);
+
+      bke::node_remove_link(node_tree, link);
+    }
+    else if (link.fromnode->type_legacy == GEO_NODE_MESH_PRIMITIVE_CYLINDER) {
+
+      /* Primitive node. */
+      bNodeSocket *cylinder_geometry = bke::node_find_socket(*link.fromnode, SOCK_OUT, "Mesh");
+      bNodeSocket *cylinder_side = bke::node_find_socket(*link.fromnode, SOCK_OUT, "Side");
+      bNodeSocket *cylinder_uv = bke::node_find_socket(*link.fromnode, SOCK_OUT, "UV Map");
+
+      /* Attribute Statistic node. */
+      bNode *statistics_node = bke::node_add_node(
+          nullptr, *node_tree, "GeometryNodeAttributeStatistic");
+      statistics_node->parent = link.fromnode->parent;
+      statistics_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 20.0f;
+      statistics_node->location[1] = link.fromnode->location[1];
+      statistics_node->custom1 = CD_PROP_FLOAT3;
+      statistics_node->custom2 = int16_t(AttrDomain::Corner);
+      bNodeSocket *statistics_geometry = bke::node_find_socket(
+          *statistics_node, SOCK_IN, "Geometry");
+      bNodeSocket *statistics_selection = bke::node_find_socket(
+          *statistics_node, SOCK_IN, "Selection");
+      bNodeSocket *statistics_attribute = bke::node_find_socket(
+          *statistics_node, SOCK_IN, "Attribute");
+      bNodeSocket *statistics_mean = bke::node_find_socket(*statistics_node, SOCK_OUT, "Mean");
+      version_node_add_link(
+          *node_tree, *link.fromnode, *cylinder_geometry, *statistics_node, *statistics_geometry);
+      version_node_add_link(
+          *node_tree, *link.fromnode, *cylinder_side, *statistics_node, *statistics_selection);
+      version_node_add_link(
+          *node_tree, *link.fromnode, *cylinder_uv, *statistics_node, *statistics_attribute);
+
+      /* Subtract node. */
+      bNode *subtract_node = bke::node_add_node(nullptr, *node_tree, "ShaderNodeVectorMath");
+      subtract_node->parent = link.fromnode->parent;
+      subtract_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 40.0f;
+      subtract_node->location[1] = link.fromnode->location[1];
+      subtract_node->custom1 = NODE_VECTOR_MATH_SUBTRACT;
+      bNodeSocket *subtract_a_input = bke::node_find_socket(*subtract_node, SOCK_IN, "Vector");
+      bNodeSocket *subtract_b_input = bke::node_find_socket(*subtract_node, SOCK_IN, "Vector_001");
+      bNodeSocket *subtract_output = bke::node_find_socket(*subtract_node, SOCK_OUT, "Vector");
+      version_node_add_link(
+          *node_tree, *link.fromnode, *cylinder_uv, *subtract_node, *subtract_a_input);
+      version_node_add_link(
+          *node_tree, *statistics_node, *statistics_mean, *subtract_node, *subtract_b_input);
+
+      /* Multiply node. */
+      bNode *multiply_node = bke::node_add_node(nullptr, *node_tree, "ShaderNodeVectorMath");
+      multiply_node->parent = link.fromnode->parent;
+      multiply_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 60.0f;
+      multiply_node->location[1] = link.fromnode->location[1];
+      multiply_node->custom1 = NODE_VECTOR_MATH_MULTIPLY;
+      bNodeSocket *multiply_a_input = bke::node_find_socket(*multiply_node, SOCK_IN, "Vector");
+      bNodeSocket *multiply_b_input = bke::node_find_socket(*multiply_node, SOCK_IN, "Vector_001");
+      bNodeSocket *multiply_output = bke::node_find_socket(*multiply_node, SOCK_OUT, "Vector");
+      copy_v3_v3(static_cast<bNodeSocketValueVector *>(multiply_b_input->default_value)->value,
+                 float3(1.0f, -1.0f, 0.0f));
+      version_node_add_link(
+          *node_tree, *subtract_node, *subtract_output, *multiply_node, *multiply_a_input);
+
+      /* Add node. */
+      bNode *add_node = bke::node_add_node(nullptr, *node_tree, "ShaderNodeVectorMath");
+      add_node->parent = link.fromnode->parent;
+      add_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 80.0f;
+      add_node->location[1] = link.fromnode->location[1];
+      add_node->custom1 = NODE_VECTOR_MATH_ADD;
+      bNodeSocket *add_a_input = bke::node_find_socket(*add_node, SOCK_IN, "Vector");
+      bNodeSocket *add_b_input = bke::node_find_socket(*add_node, SOCK_IN, "Vector_001");
+      bNodeSocket *add_output = bke::node_find_socket(*add_node, SOCK_OUT, "Vector");
+      version_node_add_link(
+          *node_tree, *statistics_node, *statistics_mean, *add_node, *add_a_input);
+      version_node_add_link(*node_tree, *multiply_node, *multiply_output, *add_node, *add_b_input);
+
+      /* Switch node. */
+      bNode *switch_node = bke::node_add_node(nullptr, *node_tree, "GeometryNodeSwitch");
+      switch_node->parent = link.fromnode->parent;
+      switch_node->location[0] = link.fromnode->location[0] + link.fromnode->width + 100.0f;
+      switch_node->location[1] = link.fromnode->location[1];
+      NodeSwitch *data = reinterpret_cast<NodeSwitch *>(switch_node->storage);
+      data->input_type = SOCK_VECTOR;
+      bNodeSocket *switch_condition = bke::node_find_socket(*switch_node, SOCK_IN, "Switch");
+      bNodeSocket *switch_false = bke::node_find_socket(*switch_node, SOCK_IN, "False");
+      bNodeSocket *switch_true = bke::node_find_socket(*switch_node, SOCK_IN, "True");
+      bNodeSocket *switch_output = bke::node_find_socket(*switch_node, SOCK_OUT, "Output");
+      version_node_add_link(*node_tree, *add_node, *add_output, *switch_node, *switch_true);
+      version_node_add_link(*node_tree, *link.fromnode, *cylinder_uv, *switch_node, *switch_false);
+      version_node_add_link(
+          *node_tree, *link.fromnode, *cylinder_side, *switch_node, *switch_condition);
+
+      /* Wire up to original socket. */
+      bNodeLink *new_link = &version_node_add_link(
+          *node_tree, *switch_node, *switch_output, *link.tonode, *link.tosock);
+
+      /* Add the new link to the cache. */
+      links_done.add_new(link.fromsock, new_link);
+
+      bke::node_remove_link(node_tree, link);
+    }
+  }
+
+  /* Recurse into nodegroups. */
+  for (bNode &node : node_tree->nodes) {
+    if (node.type_legacy == NODE_GROUP) {
+      bNodeTree *ngroup = id_cast<bNodeTree *>(node.id);
+      if (ngroup) {
+        do_version_geometry_node_primitive_uvmaps(ngroup);
+      }
+    }
+  }
+}
+
 void do_versions_after_linking_520(FileData * /*fd*/, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 2)) {
@@ -63,6 +220,17 @@ void do_versions_after_linking_520(FileData * /*fd*/, Main *bmain)
       do_version_file_output_use_file_extension_recursive(*node_tree, scene);
     }
   }
+
+  /* Restore old "UV Map" behavior of geometry nodes Cylinder and UV Sphere primitives. */
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 12)) {
+    FOREACH_NODETREE_BEGIN (bmain, node_tree, id_owner) {
+      if (node_tree->type == NTREE_GEOMETRY) {
+        do_version_geometry_node_primitive_uvmaps(node_tree);
+      }
+    }
+    FOREACH_NODETREE_END;
+  }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
