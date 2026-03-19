cbd885b8efe88a0363194736f82a5a486df6dcdc
yazan-saideh <yazan.saideh11@gmail.com>
2026-03-19 20:35:11 +0200

    Compositor: Use Glare node mode as title #142981 FIX

--- source/blender/nodes/composite/nodes/node_composite_glare.cc
+++ source/blender/nodes/composite/nodes/node_composite_glare.cc
@@ -63,6 +63,51 @@ static const EnumPropertyItem quality_items[] = {
     {CMP_NODE_GLARE_QUALITY_LOW, "LOW", 0, N_("Low"), ""},
     {0, nullptr, 0, nullptr, nullptr},
 };
+static const char *glare_type_enum_label(CMPNodeGlareType type)
+{
+  switch (type) {
+    case CMP_NODE_GLARE_BLOOM:
+      return N_("Bloom");
+    case CMP_NODE_GLARE_GHOST:
+      return N_("Ghosts");
+    case CMP_NODE_GLARE_STREAKS:
+      return N_("Streaks");
+    case CMP_NODE_GLARE_FOG_GLOW:
+      return N_("Fog Glow");
+    case CMP_NODE_GLARE_SIMPLE_STAR:
+      return N_("Simple Star");
+    case CMP_NODE_GLARE_SUN_BEAMS:
+      return N_("Sun Beams");
+    case CMP_NODE_GLARE_KERNEL:
+      return N_("Kernel");
+  }
+
+  return N_("Glare");
+}
+
+void node_composite_glare_label(const bNodeTree * /*ntree*/,
+                                       const bNode *node,
+                                       char *label,
+                                       int label_maxncpy)
+{
+  /* Prefer the current value of the \"Type\" menu input so that the label
+   * updates immediately when the user changes the node mode. */
+  CMPNodeGlareType type = CMP_NODE_GLARE_FOG_GLOW;
+  if (label_maxncpy <= 0) {
+    return;
+  }
+  for (const bNodeSocket *sock = (const bNodeSocket*)(node)->inputs.first; sock; sock = sock->next)
+  {
+    if (STREQ(sock->identifier, "Type") && sock->default_value != nullptr) {
+      const bNodeSocketValueMenu *menu = (const bNodeSocketValueMenu *)(sock->default_value);
+      type = CMPNodeGlareType(menu->value);
+      break;
+    }
+  }
+  const char *name = glare_type_enum_label(type);
+  BLI_strncpy_utf8(label, CTX_IFACE_(BLT_I18NCONTEXT_ID_NODETREE, name), label_maxncpy);
+}
+
 
 static const char *glare_type_enum_label(CMPNodeGlareType type)
 {
@@ -2845,6 +2890,7 @@ static void node_register()
   ntype.declare = node_declare;
   ntype.labelfunc = node_composite_glare_label;
   ntype.initfunc = node_init;
+  ntype.labelfunc = node_composite_glare_label;
   ntype.gather_link_search_ops = gather_link_searches;
   bke::node_type_storage(
       ntype, "NodeGlare", node_free_standard_storage, node_copy_standard_storage);
