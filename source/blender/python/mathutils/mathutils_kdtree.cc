a3d2d5790c707ac92f9510c9dcf607707007ff46
илья _ <modormoder@gmail.com>
2026-03-21 22:01:01 +0300

    progress

--- source/blender/python/mathutils/mathutils_kdtree.cc
+++ source/blender/python/mathutils/mathutils_kdtree.cc
@@ -249,7 +249,9 @@ static PyObject *py_kdtree_find(PyKDTree *self, PyObject *args, PyObject *kwargs
     data.py_filter = py_filter;
     data.is_error = false;
 
-    kdtree_3d_find_nearest_cb(self->obj, co, py_find_nearest_cb, &data, &nearest);
+    kdtree_find_nearest_cb<float3>(self->obj, co, &nearest, [&](int index, const float3 &co, float dist_sq) {
+      py_find_nearest_cb(&data, index, co, dist_sq);
+    });
 
     if (data.is_error) {
       return nullptr;
