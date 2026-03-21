2fd6d5fb5c7e438868ab695ad3f319ed2abba1e9
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-03-21 11:54:41 -0700

    Fix: Merge error.

--- source/blender/geometry/intern/offset_curves.cc
+++ source/blender/geometry/intern/offset_curves.cc
@@ -22,8 +22,8 @@ bke::CurvesGeometry offset_curves(const bke::CurvesGeometry &src_curves,
   const OffsetIndices src_points_by_curve = src_curves.offsets();
   const VArray<bool> src_cyclic = src_curves.cyclic();
   const Span<float3> src_positions = src_curves.positions();
-  const Span<float3> src_handles_left = *src_curves.handle_positions_left();
-  const Span<float3> src_handles_right = *src_curves.handle_positions_right();
+  const std::optional<Span<float3>> src_handles_left = src_curves.handle_positions_left();
+  const std::optional<Span<float3>> src_handles_right = src_curves.handle_positions_right();
   const VArray<int8_t> src_curve_types = src_curves.curve_types();
 
   const float miter_limit = cos(miter_angle);
@@ -43,102 +43,110 @@ bke::CurvesGeometry offset_curves(const bke::CurvesGeometry &src_curves,
   Vector<float3> offset_handles_right;
   Vector<int> offset_old_by_new_map;
 
-  curve_selection.foreach_index(GrainSize(1024), [&](const int64_t curve_i) {
-    const IndexRange src_points = src_points_by_curve[curve_i];
-    Span<float3> src_pos = src_positions.slice(src_points);
-    const int8_t curve_type = src_curve_types[curve_i];
-    const bool cyclic = src_cyclic[curve_i];
+  curve_selection.foreach_index(
+      [&](const int64_t curve_i) {
+        const IndexRange src_points = src_points_by_curve[curve_i];
+        Span<float3> src_pos = src_positions.slice(src_points);
+        const int8_t curve_type = src_curve_types[curve_i];
+        const bool cyclic = src_cyclic[curve_i];
 
-    const int offset_pos_size = offset_pos.size();
-    const float3 plane_norm = normals[curve_i];
+        const int offset_pos_size = offset_pos.size();
+        const float3 plane_norm = normals[curve_i];
 
-    for (const int i : src_pos.index_range()) {
-      float3 A = src_pos[(i - 1 + src_pos.size()) % src_pos.size()];
-      const float3 B = src_pos[i];
-      float3 C = src_pos[(i + 1) % src_pos.size()];
+        for (const int i : src_pos.index_range()) {
+          float3 A = src_pos[(i - 1 + src_pos.size()) % src_pos.size()];
+          const float3 B = src_pos[i];
+          float3 C = src_pos[(i + 1) % src_pos.size()];
 
-      if (curve_type == CURVE_TYPE_BEZIER) {
-        A = src_handles_left[src_points[i]];
-        C = src_handles_right[src_points[i]];
-      }
-
-      const float3 BA = math::normalize(B - A);
-      const float3 CB = math::normalize(C - B);
-
-      const float3 BA_tan = math::normalize(math::cross(BA, plane_norm));
-      const float3 CB_tan = math::normalize(math::cross(CB, plane_norm));
-
-      const float cos_theta = math::dot(BA_tan, CB_tan);
-
-      float3 offset = float3(0.0);
-
-      if (!cyclic && (i == 0 || i == src_pos.size() - 1)) {
-        if (i == 0) {
-          offset = CB_tan * offset_distance;
-        }
-        if (i == src_pos.size() - 1) {
-          offset = BA_tan * offset_distance;
-        }
-      }
-      else {
-        if (corner_type == OffsetCornerType::Miter && cos_theta < miter_limit) {
-          const float sin_theta = sqrt(1 - cos_theta * cos_theta);
-          const float S = (sqrt(2 * (cos_theta + 1)) - cos_theta - 1) / sin_theta;
-
-          const float3 norm_dir = math::normalize(BA + CB);
-          const float3 tan_dir = math::normalize(BA_tan + CB_tan);
-
-          const float3 pos_1 = B + (tan_dir - norm_dir * S) * offset_distance;
-          const float3 pos_2 = B + (tan_dir + norm_dir * S) * offset_distance;
-          offset_pos.append(pos_1);
-          offset_pos.append(pos_2);
-          offset_old_by_new_map.append(src_points[i]);
-          offset_old_by_new_map.append(src_points[i]);
-
-          if (!src_handles_left.is_empty()) {
-            /* TODO: Use a better approximation. */
-            offset_handles_left.append(
-                (A - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + pos_1);
-
-            offset_handles_right.append(pos_1 + (pos_2 - pos_1) / 3.0f);
-            offset_handles_left.append(pos_2 + (pos_1 - pos_2) / 3.0f);
-
-            offset_handles_right.append(
-                (C - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + pos_2);
+          if (curve_type == CURVE_TYPE_BEZIER) {
+            A = (*src_handles_left)[src_points[i]];
+            C = (*src_handles_right)[src_points[i]];
           }
 
-          continue;
+          const float3 BA = math::normalize(B - A);
+          const float3 CB = math::normalize(C - B);
+
+          const float3 BA_tan = math::normalize(math::cross(BA, plane_norm));
+          const float3 CB_tan = math::normalize(math::cross(CB, plane_norm));
+
+          const float cos_theta = math::dot(BA_tan, CB_tan);
+
+          float3 offset = float3(0.0);
+
+          if (!cyclic && (i == 0 || i == src_pos.size() - 1)) {
+            if (i == 0) {
+              offset = CB_tan * offset_distance;
+            }
+            if (i == src_pos.size() - 1) {
+              offset = BA_tan * offset_distance;
+            }
+          }
+          else {
+            if (corner_type == OffsetCornerType::Miter && cos_theta < miter_limit) {
+              const float sin_theta = sqrt(1 - cos_theta * cos_theta);
+              const float S = (sqrt(2 * (cos_theta + 1)) - cos_theta - 1) / sin_theta;
+
+              const float3 norm_dir = math::normalize(BA + CB);
+              const float3 tan_dir = math::normalize(BA_tan + CB_tan);
+
+              const float3 pos_1 = B + (tan_dir - norm_dir * S) * offset_distance;
+              const float3 pos_2 = B + (tan_dir + norm_dir * S) * offset_distance;
+              offset_pos.append(pos_1);
+              offset_pos.append(pos_2);
+              offset_old_by_new_map.append(src_points[i]);
+              offset_old_by_new_map.append(src_points[i]);
+
+              if (src_handles_left) {
+                /* TODO: Use a better approximation. */
+                offset_handles_left.append(
+                    (A - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) +
+                    pos_1);
+
+                offset_handles_right.append(pos_1 + (pos_2 - pos_1) / 3.0f);
+                offset_handles_left.append(pos_2 + (pos_1 - pos_2) / 3.0f);
+
+                offset_handles_right.append(
+                    (C - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) +
+                    pos_2);
+              }
+
+              continue;
+            }
+
+            offset = math::safe_divide(BA_tan + CB_tan, 1.0f + cos_theta) * offset_distance;
+          }
+
+          offset_pos.append(B + offset);
+          offset_old_by_new_map.append(src_points[i]);
+
+          if (src_handles_left) {
+            /* TODO: Use a better approximation. */
+            offset_handles_left.append(
+                (A - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + B +
+                offset);
+            offset_handles_right.append(
+                (C - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + B +
+                offset);
+          }
         }
 
-        offset = math::safe_divide(BA_tan + CB_tan, 1.0f + cos_theta) * offset_distance;
-      }
-
-      offset_pos.append(B + offset);
-      offset_old_by_new_map.append(src_points[i]);
-
-      if (!src_handles_left.is_empty()) {
-        /* TODO: Use a better approximation. */
-        offset_handles_left.append(
-            (A - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + B + offset);
-        offset_handles_right.append(
-            (C - B) * (1.0f + math::safe_divide(offset_distance, 1.0f + cos_theta)) + B + offset);
-      }
-    }
-
-    dst_curve_sizes[curve_i] = offset_pos.size() - offset_pos_size;
-  });
+        dst_curve_sizes[curve_i] = offset_pos.size() - offset_pos_size;
+      },
+      exec_mode::grain_size(1024));
 
   const OffsetIndices dst_points_by_curve = offset_indices::accumulate_counts_to_offsets(
       dst_curve_sizes);
   dst_curves.resize(dst_curves.offsets().last(), dst_curves.curves_num());
 
   Array<int> old_by_new_map(dst_curves.points_num());
-  unselected_curves.foreach_index(GrainSize(1024), [&](const int64_t curve_i) {
-    const IndexRange src_points = src_points_by_curve[curve_i];
-    const IndexRange dst_points = dst_points_by_curve[curve_i];
-    array_utils::fill_index_range<int>(old_by_new_map.as_mutable_span().slice(dst_points),
-                                       src_points.start());
-  });
+  unselected_curves.foreach_index(
+      [&](const int64_t curve_i) {
+        const IndexRange src_points = src_points_by_curve[curve_i];
+        const IndexRange dst_points = dst_points_by_curve[curve_i];
+        array_utils::fill_index_range<int>(old_by_new_map.as_mutable_span().slice(dst_points),
+                                           src_points.start());
+      },
+      exec_mode::grain_size(1024));
 
   bke::MutableAttributeAccessor dst_attributes = dst_curves.attributes_for_write();
   const bke::AttributeAccessor src_attributes = src_curves.attributes();
@@ -153,35 +161,37 @@ bke::CurvesGeometry offset_curves(const bke::CurvesGeometry &src_curves,
 
   array_utils::copy_group_to_group(
       src_points_by_curve, dst_points_by_curve, unselected_curves, src_positions, dst_positions);
-  if (!src_handles_left.is_empty()) {
+  if (src_handles_left) {
     array_utils::copy_group_to_group(src_points_by_curve,
                                      dst_points_by_curve,
                                      unselected_curves,
-                                     src_handles_left,
+                                     *src_handles_left,
                                      dst_handles_left);
     array_utils::copy_group_to_group(src_points_by_curve,
                                      dst_points_by_curve,
                                      unselected_curves,
-                                     src_handles_right,
+                                     *src_handles_right,
                                      dst_handles_right);
   }
 
   int index = 0;
-  curve_selection.foreach_index(GrainSize(1024), [&](const int64_t curve_i) {
-    const IndexRange dst_points = dst_points_by_curve[curve_i];
-    const IndexRange off_points = dst_points.index_range().shift(index);
-    MutableSpan<int> map_range = old_by_new_map.as_mutable_span().slice(dst_points);
-    map_range.copy_from(offset_old_by_new_map.as_span().slice(off_points));
+  curve_selection.foreach_index(
+      [&](const int64_t curve_i) {
+        const IndexRange dst_points = dst_points_by_curve[curve_i];
+        const IndexRange off_points = dst_points.index_range().shift(index);
+        MutableSpan<int> map_range = old_by_new_map.as_mutable_span().slice(dst_points);
+        map_range.copy_from(offset_old_by_new_map.as_span().slice(off_points));
 
-    dst_positions.slice(dst_points).copy_from(offset_pos.as_span().slice(off_points));
-    if (!offset_handles_left.is_empty()) {
-      dst_handles_left.slice(dst_points)
-          .copy_from(offset_handles_left.as_span().slice(off_points));
-      dst_handles_right.slice(dst_points)
-          .copy_from(offset_handles_right.as_span().slice(off_points));
-    }
-    index += dst_points.size();
-  });
+        dst_positions.slice(dst_points).copy_from(offset_pos.as_span().slice(off_points));
+        if (!offset_handles_left.is_empty()) {
+          dst_handles_left.slice(dst_points)
+              .copy_from(offset_handles_left.as_span().slice(off_points));
+          dst_handles_right.slice(dst_points)
+              .copy_from(offset_handles_right.as_span().slice(off_points));
+        }
+        index += dst_points.size();
+      },
+      exec_mode::grain_size(1024));
 
   bke::gather_attributes(
       src_curves.attributes(),
