8057316e256399aa7a045403715702cbd8b6708b
Andrej730 <azhilenkov@gmail.com>
2026-03-21 09:38:54 +0500

    Store original color as private attribute

--- scripts/templates_py/Operator/modal_timer.py
+++ scripts/templates_py/Operator/modal_timer.py
@@ -7,7 +7,7 @@ class ModalTimerOperator(bpy.types.Operator):
     bl_label = "Modal Timer Operator"
 
     _timer = None
-    original_color: bpy.props.FloatVectorProperty(name="Original Color", default=(1.0, 0.0, 0.0), subtype="COLOR")
+    _original_color = None
 
     def modal(self, context, event):
         if event.type in {'RIGHTMOUSE', 'ESC'}:
@@ -24,7 +24,8 @@ class ModalTimerOperator(bpy.types.Operator):
 
     def execute(self, context):
         # Store the original color to avoid altering user settings
-        self.original_color = context.preferences.themes[0].view_3d.space.gradients.high_gradient
+        color = context.preferences.themes[0].view_3d.space.gradients.high_gradient
+        self._original_color = color[:]
         wm = context.window_manager
         self._timer = wm.event_timer_add(0.1, window=context.window)
         wm.modal_handler_add(self)
@@ -33,7 +34,7 @@ class ModalTimerOperator(bpy.types.Operator):
     def cancel(self, context):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
-        context.preferences.themes[0].view_3d.space.gradients.high_gradient = self.original_color
+        context.preferences.themes[0].view_3d.space.gradients.high_gradient = self._original_color
 
 
 def menu_func(self, context):
