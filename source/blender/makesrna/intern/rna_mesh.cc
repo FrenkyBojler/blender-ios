blender/blender

commit b81d8cb88d619cb5de1f312642d3b692b2fcda8c

Fix #155858: accessing UV vectors in Python is extremely slow
Caused by c9f4795638

There were two fixes already:
- 3f908d7634c7
- 2342a2198f19

This PR is fixing #155858 in 5.1 (so should be backported)

In 5.2, this is actually "fixed" already with 6f7ae3372a
(but it was decided to have all attribute related lookup
functions being the same)

Pull Request: https://projects.blender.org/blender/blender/pulls/155867

source/blender/makesrna/intern/rna_mesh.cc
@@ -2331,7 +2331,7 @@ static void rna_def_mloopuv(BlenderRNA *brna)
                                     "rna_iterator_array_end",
                                     "rna_iterator_array_get",
                                     "rna_Attribute_data_length",
-                                    nullptr,
+                                    "rna_Attribute_data_lookup_int",
                                     nullptr,
                                     nullptr);
 
@@ -2374,7 +2374,7 @@ static void rna_def_mloopuv(BlenderRNA *brna)
                                     "rna_iterator_array_end",
                                     "rna_iterator_array_get",
                                     "rna_Attribute_data_length",
-                                    nullptr,
+                                    "rna_Attribute_data_lookup_int",
                                     nullptr,
                                     nullptr);
 
@@ -2389,7 +2389,7 @@ static void rna_def_mloopuv(BlenderRNA *brna)
                                     "rna_iterator_array_end",
                                     "rna_iterator_array_get",
                                     "rna_MeshUVLoopLayer_pin_length",
-                                    nullptr,
+                                    "rna_Attribute_data_lookup_int",
                                     nullptr,
                                     nullptr);
 

FrenkyBojler/blender-ios

commit 09c78e45d1078d516d5566e41161c840835a5471

Fix #156087: Wrong lookup_int for legacy uv loop layer "data"
Caused by b81d8cb88d

Originally, c9f4795638 made attribute lookup slow

Specific lookup_int for attributes was then implemented in 3f908d7634
(usage expanded in 2342a2198f & 25cebfa62445)

6f7ae3372a solved this more generally (not only for attributes) in 5.2
(but it was still decided to have attribute related lookups have their
own dedicated function[s],even though not strictly necessary in 5.2).

Now for the legacy "data" of a `MeshUVLoopLayer` (which uses attributes
internally, but still relies on the `MeshUVLoop` structure), we cannot
return a `Float2AttributeValue`, but said `MeshUVLoop` instead. This is
what this PR does.

source/blender/makesrna/intern/rna_mesh.cc
@@ -1056,6 +1056,29 @@ static void rna_MeshUVLoopLayer_clone_set(PointerRNA *ptr, bool value)
   BKE_mesh_tessface_clear(mesh);
 }
 
+bool rna_MeshUVLoopLayer_data_lookup_int(PointerRNA *ptr, int index, PointerRNA *r_ptr)
+{
+  CollectionPropertyIterator iter;
+  rna_Attribute_data_begin(&iter, ptr);
+  if (!iter.valid) {
+    *r_ptr = PointerRNA_NULL;
+    return false;
+  }
+
+  ArrayIterator *internal = &iter.internal.array;
+  if (index < 0 || index >= internal->length) {
+    *r_ptr = PointerRNA_NULL;
+    return false;
+  }
+
+  internal->ptr += internal->itemsize * index;
+
+  *r_ptr = RNA_pointer_create_with_parent(
+      iter.parent, RNA_MeshUVLoop, rna_iterator_array_get(&iter));
+  rna_iterator_array_end(&iter);
+  return true;
+}
+
 static void rna_Mesh_vertex_colors_begin(CollectionPropertyIterator *iter, PointerRNA *ptr)
 {
   rna_AttributeGroup_iterator_begin(
@@ -2331,7 +2354,7 @@ static void rna_def_mloopuv(BlenderRNA *brna)
                                     "rna_iterator_array_end",
                                     "rna_iterator_array_get",
                                     "rna_Attribute_data_length",
-                                    "rna_Attribute_data_lookup_int",
+                                    "rna_MeshUVLoopLayer_data_lookup_int",
                                     nullptr,
                                     nullptr);
 