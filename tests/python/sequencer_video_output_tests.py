c813e96304b9ce85d9216df3599a5235efb296b7
Aras Pranckevicius <aras@nesnausk.org>
2026-03-24 22:17:46 +0200

    Tests: update test expectations

--- tests/python/sequencer_video_output_tests.py
+++ tests/python/sequencer_video_output_tests.py
@@ -114,14 +114,8 @@ def main():
 
     report = VideoOutputReport("Sequencer", args.outdir, args.oiiotool)
     report.set_pixelated(True)
-    if platform.system() == "Linux" and platform.machine().lower() == "aarch64":
-        # On Linux arm64 compression looks different, raise tolerances.
-        report.set_fail_threshold(10.0 / 255.0)
-        report.set_fail_percent(1.0)
-    else:
-        # Default error tolerances are quite large, lower them.
-        report.set_fail_threshold(2.0 / 255.0)
-        report.set_fail_percent(0.01)
+    report.set_fail_threshold(10.0 / 255.0)
+    report.set_fail_percent(1.0)
     report.set_reference_dir("reference")
 
     ok = report.run(args.testdir, args.blender, get_arguments, batch=args.batch)
