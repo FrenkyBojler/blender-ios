6f2a1adcc613a2c8449e0c11ef97e02d1ede6188
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-03-23 18:20:20 -0700

    Cleanup: Use `u`

--- source/blender/draw/intern/shaders/draw_grease_pencil_lib.glsl
+++ source/blender/draw/intern/shaders/draw_grease_pencil_lib.glsl
@@ -243,32 +243,32 @@ float gpencil_decode_miter_limit(int packed_data)
 
 float gpencil_decode_random_size(uint4 random_packed)
 {
-  return float(random_packed.x & 0x0000FFFF) / 65535.0f;
+  return float(random_packed.x & 0x0000FFFFu) / 65535.0f;
 }
 
 float gpencil_decode_random_strength(uint4 random_packed)
 {
-  return float((random_packed.x & 0xFFFF0000) >> 16u) / 65535.0f;
+  return float((random_packed.x & 0xFFFF0000u) >> 16u) / 65535.0f;
 }
 
 float gpencil_decode_random_rotation(uint4 random_packed)
 {
-  return float(random_packed.y & 0x0000FFFF) / 65535.0f;
+  return float(random_packed.y & 0x0000FFFFu) / 65535.0f;
 }
 
 float gpencil_decode_random_hue(uint4 random_packed)
 {
-  return float((random_packed.y & 0xFFFF0000) >> 16u) / 65535.0f;
+  return float((random_packed.y & 0xFFFF0000u) >> 16u) / 65535.0f;
 }
 
 float gpencil_decode_random_saturation(uint4 random_packed)
 {
-  return float(random_packed.z & 0x0000FFFF) / 65535.0f;
+  return float(random_packed.z & 0x0000FFFFu) / 65535.0f;
 }
 
 float gpencil_decode_random_value(uint4 random_packed)
 {
-  return float((random_packed.z & 0xFFFF0000) >> 16u) / 65535.0f;
+  return float((random_packed.z & 0xFFFF0000u) >> 16u) / 65535.0f;
 }
 
 float2 gpencil_project_to_screenspace(float4 v, float4 viewport_res)
