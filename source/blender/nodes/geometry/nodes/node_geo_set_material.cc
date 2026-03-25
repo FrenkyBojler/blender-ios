f836d48e84c289a76c5dccad5878d7d163145d2b
Hans Goudey <hans@blender.org>
2026-03-25 13:24:18 -0400

    Include order

--- source/blender/nodes/geometry/nodes/node_geo_set_material.cc
+++ source/blender/nodes/geometry/nodes/node_geo_set_material.cc
@@ -2,7 +2,6 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BKE_mesh_types.hh"
 #include "node_geometry_util.hh"
 
 #include "DNA_curves_types.h"
@@ -13,6 +12,7 @@
 #include "BKE_curves.hh"
 #include "BKE_grease_pencil.hh"
 #include "BKE_material.hh"
+#include "BKE_mesh_types.hh"
 
 #include "GEO_foreach_geometry.hh"
 
