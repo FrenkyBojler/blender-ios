2ea7c96f3250414ba069835fb3f6ec7c517a5cbc
Jacques Lucke <jacques@blender.org>
2026-03-26 00:33:16 +0100

    add comments

--- source/blender/functions/FN_multi_function_registry.hh
+++ source/blender/functions/FN_multi_function_registry.hh
@@ -10,14 +10,24 @@
 
 namespace blender::fn::multi_function::registry {
 
+/**
+ * Add a new multi-function to the registry. The #MultiFunction::name is used as identifier.
+ * This multi-function is expected to have static storage duration.
+ */
 void add_new(const MultiFunction &fn);
 
+/**
+ * Utility to create a multi-function with static storage duration that is added to the registry.
+ */
 template<typename CreateFn> inline void add_new_cb(CreateFn &&create_fn)
 {
   static auto fn = create_fn();
   registry::add_new(fn);
 }
 
+/**
+ * Find the multi-function with the given identifier. The multi-function is expected to exist.
+ */
 const MultiFunction &lookup(UString id);
 
 }  // namespace blender::fn::multi_function::registry
