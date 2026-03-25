e0a81567f99d509bfcf1809df189aad48890e2bd
Jacques Lucke <jacques@blender.org>
2026-03-25 15:10:53 +0100

    typo

--- source/blender/blenkernel/BKE_sound.hh
+++ source/blender/blenkernel/BKE_sound.hh
@@ -266,7 +266,7 @@ bool sound_mixdown(AUD_Sequence sequence,
 namespace bke {
 
 /**
- * Used by the Sample Sound node in Geometry Nodes. It allows efficiently sampling the an arbitrary
+ * Used by the Sample Sound node in Geometry Nodes. It allows efficiently sampling an arbitrary
  * frequency range at an arbitrary point in time. This is achieved by caching the result of the
  * fourier transform for various windows and interpolating between the cached values.
  */
