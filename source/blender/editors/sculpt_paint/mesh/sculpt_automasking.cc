blender/blender

commit 73bc3f054f39d7eb7cec09c6321dcb19eee9b068

Cleanup: Migrate all of `sculpt.cc` into `ed::sculpt_paint` namespace
* Removes old `SCULPT_` prefix
* Removes `using namespace <...> ` where possible
* Removes unnecessary namespacing

Pull Request: https://projects.blender.org/blender/blender/pulls/156329

source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -1116,7 +1116,7 @@ static void fill_topology_automasking_factors_mesh(const Depsgraph &depsgraph,
   flood.add_initial(find_symm_verts_mesh(depsgraph, ob, active_vert, radius));
 
   const bool use_radius = ss.cache && is_constrained_by_radius(brush);
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(ob);
 
   float3 location = vert_positions[active_vert];
 
@@ -1124,8 +1124,7 @@ static void fill_topology_automasking_factors_mesh(const Depsgraph &depsgraph,
     flood.execute(ob, vert_to_face_map, [&](int from_v, int to_v) {
       factors[from_v] = 1.0f;
       factors[to_v] = 1.0f;
-      return SCULPT_is_vertex_inside_brush_radius_symm(
-          vert_positions[to_v], location, radius, symm);
+      return is_vertex_inside_brush_radius_symm(vert_positions[to_v], location, radius, symm);
     });
   }
   else {
@@ -1157,7 +1156,7 @@ static void fill_topology_automasking_factors_grids(const Sculpt &sd,
   flood.add_initial(key, find_symm_verts_grids(ob, active_vert, radius));
 
   const bool use_radius = ss.cache && is_constrained_by_radius(brush);
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(ob);
 
   float3 location = positions[active_vert];
 
@@ -1166,7 +1165,7 @@ static void fill_topology_automasking_factors_grids(const Sculpt &sd,
         ob, subdiv_ccg, [&](SubdivCCGCoord from_v, SubdivCCGCoord to_v, bool /*is_duplicate*/) {
           factors[from_v.to_index(key)] = 1.0f;
           factors[to_v.to_index(key)] = 1.0f;
-          return SCULPT_is_vertex_inside_brush_radius_symm(
+          return is_vertex_inside_brush_radius_symm(
               positions[to_v.to_index(key)], location, radius, symm);
         });
   }
@@ -1196,7 +1195,7 @@ static void fill_topology_automasking_factors_bmesh(const Sculpt &sd,
   flood.add_initial(*ss.bm, find_symm_verts_bmesh(ob, BM_elem_index_get(active_vert), radius));
 
   const bool use_radius = ss.cache && is_constrained_by_radius(brush);
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(ob);
 
   float3 location = active_vert->co;
 
@@ -1204,7 +1203,7 @@ static void fill_topology_automasking_factors_bmesh(const Sculpt &sd,
     flood.execute(ob, [&](BMVert *from_v, BMVert *to_v) {
       factors[BM_elem_index_get(from_v)] = 1.0f;
       factors[BM_elem_index_get(to_v)] = 1.0f;
-      return SCULPT_is_vertex_inside_brush_radius_symm(to_v->co, location, radius, symm);
+      return is_vertex_inside_brush_radius_symm(to_v->co, location, radius, symm);
     });
   }
   else {
@@ -1584,7 +1583,7 @@ static void normal_occlusion_automasking_fill(const Depsgraph &depsgraph,
                                               eAutomasking_flag mode,
                                               MutableSpan<float> factors)
 {
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
   /* No need to build original data since this is only called at the beginning of strokes. */
   switch (bke::object::pbvh_get(ob)->type()) {
     case bke::pbvh::Type::Mesh: {
@@ -1688,7 +1687,7 @@ std::unique_ptr<Cache> cache_init(const Depsgraph &depsgraph,
     }
   }
 
-  const int verts_num = SCULPT_vertex_count_get(ob);
+  const int verts_num = vertex_count_get(ob);
 
   if ((mode & BRUSH_AUTOMASKING_VIEW_OCCLUSION) && (mode & BRUSH_AUTOMASKING_VIEW_NORMAL)) {
     automasking->occlusion = Array<Cache::OcclusionValue>(verts_num,

source/blender/editors/sculpt_paint/mesh/sculpt_expand.cc
@@ -350,7 +350,7 @@ static BitVector<> enabled_state_to_bitmap(const Depsgraph &depsgraph,
                                            const Cache &expand_cache)
 {
   const SculptSession &ss = *object.runtime->sculpt_session;
-  const int totvert = SCULPT_vertex_count_get(object);
+  const int totvert = vertex_count_get(object);
   BitVector<> enabled_verts(totvert);
   if (expand_cache.all_enabled) {
     if (!expand_cache.invert) {
@@ -566,7 +566,7 @@ Vector<int> find_symm_verts_mesh(const Depsgraph &depsgraph,
                                  const int original_vert,
                                  const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 
@@ -600,7 +600,7 @@ Vector<int> find_symm_verts_grids(const Object &object,
                                   const int original_vert,
                                   const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 
@@ -633,7 +633,7 @@ Vector<int> find_symm_verts_bmesh(const Object &object,
                                   const int original_vert,
                                   const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 
@@ -865,7 +865,7 @@ static void calc_topology_falloff_from_verts(Object &ob,
   const Mesh &mesh = *id_cast<const Mesh *>(ob.data);
   const GroupedSpan<int> vert_to_face_map = mesh.vert_to_face_map();
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(ob);
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
 
   switch (pbvh.type()) {
     case bke::pbvh::Type::Mesh: {
@@ -927,7 +927,7 @@ static Array<float> topology_falloff_create(const Depsgraph &depsgraph,
   IndexMaskMemory memory;
   const IndexMask mask = IndexMask::from_indices(symm_verts.as_span(), memory);
 
-  Array<float> dists(SCULPT_vertex_count_get(ob), 0.0f);
+  Array<float> dists(vertex_count_get(ob), 0.0f);
   calc_topology_falloff_from_verts(ob, mask, dists);
   return dists;
 }
@@ -945,7 +945,7 @@ static Array<float> normals_falloff_create(const Depsgraph &depsgraph,
 {
   SculptSession &ss = *ob.runtime->sculpt_session;
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(ob);
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
   Array<float> dists(totvert, 0.0f);
   Array<float> edge_factors(totvert, 1.0f);
 
@@ -1040,7 +1040,7 @@ static Array<float> spherical_falloff_create(const Depsgraph &depsgraph,
 {
   SculptSession &ss = *object.runtime->sculpt_session;
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
-  Array<float> dists(SCULPT_vertex_count_get(object));
+  Array<float> dists(vertex_count_get(object));
 
   const Vector<int> symm_verts = find_symm_verts(depsgraph, object, vert);
 
@@ -1118,7 +1118,7 @@ static Array<float> boundary_topology_falloff_create(const Depsgraph &depsgraph,
 {
   const Vector<int> symm_verts = find_symm_verts(depsgraph, ob, initial_vert);
 
-  BitVector<> boundary_verts(SCULPT_vertex_count_get(ob));
+  BitVector<> boundary_verts(vertex_count_get(ob));
   for (const int vert : symm_verts) {
     if (std::unique_ptr<boundary::SculptBoundary> boundary = boundary::data_init(
             depsgraph, ob, nullptr, vert, FLT_MAX))
@@ -1132,7 +1132,7 @@ static Array<float> boundary_topology_falloff_create(const Depsgraph &depsgraph,
   IndexMaskMemory memory;
   const IndexMask boundary_mask = IndexMask::from_bits(boundary_verts, memory);
 
-  Array<float> dists(SCULPT_vertex_count_get(ob), 0.0f);
+  Array<float> dists(vertex_count_get(ob), 0.0f);
   calc_topology_falloff_from_verts(ob, boundary_mask, dists);
   return dists;
 }
@@ -1151,7 +1151,7 @@ static Array<float> diagonals_falloff_create(const Depsgraph &depsgraph,
   const OffsetIndices<int> faces = mesh.faces();
   const Span<int> corner_verts = mesh.corner_verts();
   const GroupedSpan<int> vert_to_face_map = mesh.vert_to_face_map();
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
   Array<float> dists(totvert, 0.0f);
 
   /* This algorithm uses mesh data (faces and loops), so this falloff type can't be initialized for
@@ -1203,7 +1203,7 @@ static void update_max_vert_falloff_value(const Object &object, Cache &expand_ca
 {
   SculptSession &ss = *object.runtime->sculpt_session;
   expand_cache.max_vert_falloff = threading::parallel_reduce(
-      IndexRange(SCULPT_vertex_count_get(object)),
+      IndexRange(vertex_count_get(object)),
       4096,
       std::numeric_limits<float>::lowest(),
       [&](const IndexRange range, float max) {
@@ -1343,7 +1343,7 @@ static void topology_from_state_boundary(Object &ob,
 {
   expand_cache.face_falloff = {};
 
-  expand_cache.vert_falloff.reinitialize(SCULPT_vertex_count_get(ob));
+  expand_cache.vert_falloff.reinitialize(vertex_count_get(ob));
   expand_cache.vert_falloff.fill(0);
 
   IndexMaskMemory memory;
@@ -1402,7 +1402,7 @@ static void init_from_face_set_boundary(const Depsgraph &depsgraph,
                                         const bool internal_falloff)
 {
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(ob);
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
 
   Array<bool> vert_has_face_set(totvert);
   Array<bool> vert_has_unique_face_set(totvert);
@@ -1709,13 +1709,13 @@ static void restore_original_state(bContext *C, Object &ob, Cache &expand_cache)
       write_mask_data(ob, expand_cache.original_mask);
       flush_update_step(C, UpdateType::Mask);
       flush_update_done(C, ob, UpdateType::Mask);
-      SCULPT_tag_update_overlays(C);
+      tag_update_overlays(C);
       break;
     case TargetType::FaceSets:
       restore_face_set_data(ob, expand_cache);
       flush_update_step(C, UpdateType::FaceSet);
       flush_update_done(C, ob, UpdateType::FaceSet);
-      SCULPT_tag_update_overlays(C);
+      tag_update_overlays(C);
       break;
     case TargetType::Colors:
       restore_color_data(ob, expand_cache);
@@ -1981,7 +1981,7 @@ static bool colors_update_task(const Depsgraph &depsgraph,
 static void original_state_store(Object &ob, Cache &expand_cache)
 {
   Mesh &mesh = *id_cast<Mesh *>(ob.data);
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
 
   face_set::create_face_sets_mesh(ob);
 
@@ -2171,7 +2171,7 @@ static std::optional<int> target_vert_update_and_get(bContext *C, Object &ob, co
 static void reposition_pivot(bContext *C, Object &ob, Cache &expand_cache)
 {
   SculptSession &ss = *ob.runtime->sculpt_session;
-  const char symm = SCULPT_mesh_symmetry_xyz_get(ob);
+  const char symm = mesh_symmetry_xyz_get(ob);
   const Depsgraph &depsgraph = *CTX_data_depsgraph_pointer(C);
 
   const bool initial_invert_state = expand_cache.invert;
@@ -2202,7 +2202,7 @@ static void reposition_pivot(bContext *C, Object &ob, Cache &expand_cache)
           return;
         }
         const float3 &position = positions[vert];
-        if (!SCULPT_check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
+        if (!check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
           return;
         }
         average += double3(position);
@@ -2219,7 +2219,7 @@ static void reposition_pivot(bContext *C, Object &ob, Cache &expand_cache)
           return;
         }
         const float3 position = positions[vert];
-        if (!SCULPT_check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
+        if (!check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
           return;
         }
         average += double3(position);
@@ -2235,7 +2235,7 @@ static void reposition_pivot(bContext *C, Object &ob, Cache &expand_cache)
           return;
         }
         const float3 position = BM_vert_at_index(&bm, vert)->co;
-        if (!SCULPT_check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
+        if (!check_vertex_pivot_symmetry(position, expand_init_co, symm)) {
           return;
         }
         average += double3(position);
@@ -2288,7 +2288,7 @@ static void find_active_connected_components_from_vert(const Depsgraph &depsgrap
     expand_cache.active_connected_islands[i] = EXPAND_ACTIVE_COMPONENT_NONE;
   }
 
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(ob);
 
   const Vector<int> symm_verts = find_symm_verts(depsgraph, ob, initial_vertex);
 
@@ -2559,7 +2559,7 @@ static wmOperatorStatus sculpt_expand_modal(bContext *C, wmOperator *op, const w
         copy_v2_v2(expand_cache.initial_mouse_move, mval_fl);
         copy_v2_v2(expand_cache.original_mouse_move, expand_cache.initial_mouse);
         if (expand_cache.falloff_type == FalloffType::Geodesic &&
-            SCULPT_vertex_count_get(ob) > expand_cache.max_geodesic_move_preview)
+            vertex_count_get(ob) > expand_cache.max_geodesic_move_preview)
         {
           /* Set to spherical falloff for preview in high poly meshes as it is the fastest one.
            * In most cases it should match closely the preview from geodesic. */
@@ -2883,7 +2883,7 @@ static wmOperatorStatus sculpt_expand_invoke(bContext *C, wmOperator *op, const 
 
     if (RNA_boolean_get(op->ptr, "use_auto_mask")) {
       if (any_nonzero_mask(ob)) {
-        write_mask_data(ob, Array<float>(SCULPT_vertex_count_get(ob), 1.0f));
+        write_mask_data(ob, Array<float>(vertex_count_get(ob), 1.0f));
       }
     }
   }
@@ -2898,7 +2898,7 @@ static wmOperatorStatus sculpt_expand_invoke(bContext *C, wmOperator *op, const 
   }
 
   /* Do nothing when the mesh has 0 vertices. */
-  const int totvert = SCULPT_vertex_count_get(ob);
+  const int totvert = vertex_count_get(ob);
   if (totvert == 0) {
     expand_cache_free(ss);
     return OPERATOR_CANCELLED;
@@ -3082,7 +3082,7 @@ void SCULPT_OT_expand(wmOperatorType *ot)
   ot->invoke = sculpt_expand_invoke;
   ot->modal = sculpt_expand_modal;
   ot->cancel = sculpt_expand_cancel;
-  ot->poll = SCULPT_mode_poll;
+  ot->poll = sculpt_mode_poll;
 
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_DEPENDS_ON_CURSOR;
 

source/blender/editors/sculpt_paint/mesh/brushes/clay_thumb.cc
@@ -161,14 +161,14 @@ void do_clay_thumb_brush(const Depsgraph &depsgraph,
   }
 
   /* Delay the first daub because grab delta is not setup. */
-  if (SCULPT_stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
+  if (stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
     ss.cache->clay_thumb_brush.front_angle = 0.0f;
     return;
   }
 
   /* Simulate the clay accumulation by increasing the plane angle as more samples are added to the
    * stroke. */
-  if (SCULPT_stroke_is_main_symmetry_pass(*ss.cache)) {
+  if (stroke_is_main_symmetry_pass(*ss.cache)) {
     ss.cache->clay_thumb_brush.front_angle += 0.8f;
     ss.cache->clay_thumb_brush.front_angle = std::clamp(
         ss.cache->clay_thumb_brush.front_angle, 0.0f, 60.0f);

source/blender/editors/sculpt_paint/mesh/brushes/draw_vector_displacement.cc
@@ -113,7 +113,7 @@ static void calc_faces(const Depsgraph &depsgraph,
   calc_brush_texture_colors(ss, brush, position_data.eval, verts, translations);
   scale_translations(translations, factors);
   for (const int i : verts.index_range()) {
-    SCULPT_calc_vertex_displacement(ss, brush, translations[i]);
+    calc_vertex_displacement(ss, brush, translations[i]);
   }
 
   clip_and_lock_translations(sd, ss, position_data.eval, verts, translations);
@@ -156,7 +156,7 @@ static void calc_grids(const Depsgraph &depsgraph,
   calc_brush_texture_colors(ss, brush, positions, translations);
   scale_translations(translations, factors);
   for (const int i : positions.index_range()) {
-    SCULPT_calc_vertex_displacement(ss, brush, translations[i]);
+    calc_vertex_displacement(ss, brush, translations[i]);
   }
 
   clip_and_lock_translations(sd, ss, positions, translations);
@@ -198,7 +198,7 @@ static void calc_bmesh(const Depsgraph &depsgraph,
   calc_brush_texture_colors(ss, brush, positions, translations);
   scale_translations(translations, factors);
   for (const int i : positions.index_range()) {
-    SCULPT_calc_vertex_displacement(ss, brush, translations[i]);
+    calc_vertex_displacement(ss, brush, translations[i]);
   }
 
   clip_and_lock_translations(sd, ss, positions, translations);

source/blender/editors/sculpt_paint/mesh/brushes/enhance_details.cc
@@ -207,7 +207,7 @@ void do_enhance_details_brush(const Depsgraph &depsgraph,
   bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
 
   if (ss.cache->detail_directions.is_empty()) {
-    ss.cache->detail_directions.reinitialize(SCULPT_vertex_count_get(object));
+    ss.cache->detail_directions.reinitialize(vertex_count_get(object));
     IndexMaskMemory memory;
     const IndexMask effective_nodes = bke::pbvh::search_nodes(
         pbvh, memory, [&](const bke::pbvh::Node &node) {

source/blender/editors/sculpt_paint/mesh/brushes/layer.cc
@@ -420,8 +420,7 @@ void do_layer_brush(const Depsgraph &depsgraph,
 
       if (displacement.is_empty()) {
         if (ss.cache->layer_displacement_factor.is_empty()) {
-          ss.cache->layer_displacement_factor = Array<float>(SCULPT_vertex_count_get(object),
-                                                             0.0f);
+          ss.cache->layer_displacement_factor = Array<float>(vertex_count_get(object), 0.0f);
         }
         displacement = ss.cache->layer_displacement_factor;
       }
@@ -498,7 +497,7 @@ void do_layer_brush(const Depsgraph &depsgraph,
     }
     case bke::pbvh::Type::BMesh: {
       if (ss.cache->layer_displacement_factor.is_empty()) {
-        ss.cache->layer_displacement_factor = Array<float>(SCULPT_vertex_count_get(object), 0.0f);
+        ss.cache->layer_displacement_factor = Array<float>(vertex_count_get(object), 0.0f);
       }
       const MutableSpan<float> displacement = ss.cache->layer_displacement_factor;
       MutableSpan<bke::pbvh::BMeshNode> nodes = pbvh.nodes<bke::pbvh::BMeshNode>();

source/blender/editors/sculpt_paint/mesh/brushes/multiplane_scrape.cc
@@ -570,7 +570,7 @@ void do_multiplane_scrape_brush(const Depsgraph &depsgraph,
   }
 
   /* Delay the first daub because grab delta is not setup. */
-  if (SCULPT_stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
+  if (stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
     ss.cache->multiplane_scrape_angle = 0.0f;
     return;
   }

source/blender/editors/sculpt_paint/mesh/brushes/pinch.cc
@@ -184,7 +184,7 @@ void do_pinch_brush(const Depsgraph &depsgraph,
   calc_brush_plane(depsgraph, brush, object, node_mask, area_no, area_co);
 
   /* delay the first daub because grab delta is not setup */
-  if (SCULPT_stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
+  if (stroke_is_first_brush_step_of_symmetry_pass(*ss.cache)) {
     return;
   }
 

FrenkyBojler/blender-ios

commit 6d7b3732ac30159551bafc878c9158c4375ae818

Fix: Compile error in recent commit
Introduced in 73bc3f054f39d7eb7cec09c6321dcb19eee9b068

source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -1675,7 +1675,7 @@ std::unique_ptr<Cache> cache_init(const Depsgraph &depsgraph,
 
     std::array<int, PAINT_SYMM_AREAS> symm_verts = find_all_symm_verts(
         depsgraph, ob, ss.active_vert_index());
-    const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+    const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(ob);
 
     for (int symm_it = 0; symm_it < PAINT_SYMM_AREAS; symm_it++) {
       if (!is_symmetry_iteration_valid(symm_it, symm)) {

source/blender/editors/sculpt_paint/mesh/sculpt_expand.cc
@@ -702,7 +702,7 @@ std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_mesh(const Depsgraph &deps
                                                            const int original_vert,
                                                            const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 
@@ -736,7 +736,7 @@ std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_grids(const Object &object
                                                             const int original_vert,
                                                             const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 
@@ -769,7 +769,7 @@ std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_bmesh(const Object &object
                                                             const int original_vert,
                                                             const float max_distance)
 {
-  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const ePaintSymmetryFlags symm = mesh_symmetry_xyz_get(object);
   const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
   const bool use_original = false;
 