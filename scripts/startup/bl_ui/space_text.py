3a064239bcba5bc46df35cfdf5edc4325d77d1e6
Pratik Borhade <pratikborhade302@gmail.com>
2026-03-26 11:49:13 +0530

    Rename to recursive_paths

--- scripts/startup/bl_ui/space_text.py
+++ scripts/startup/bl_ui/space_text.py
@@ -290,7 +290,7 @@ class TEXT_MT_templates_py(Menu):
             props_default={"internal": True},
             filter_ext=lambda ext: (ext.lower() == ".py"),
             translate=False,
-            include_subdir=True,
+            recursive_paths=True,
         )
 
 
