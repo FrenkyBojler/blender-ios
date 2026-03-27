blender/blender

commit d8716f070775e5d412f5eee86c9fe64b55458c97

Fix #106817: Sculpt symmetry doesn't work with topology automasking
Sculpt symmetry was not working for a mesh that has disconnected parts
across a symmetry axis while also having topology auto masking active.

To fix this, pre-calculate the mapping of topology islands to symmetry
passes, and use the symmetry pass to look up the correct topology
island.

A new method, `find_all_symm_verts`, was also added for this usecase.

Pull Request: https://projects.blender.org/blender/blender/pulls/155359

source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -684,7 +684,8 @@ void calc_vert_factors(const Depsgraph &depsgraph,
 
     if (!automasking.settings.topology_use_brush_limit &&
         automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
-        islands::vert_id_get(ss, vert) != automasking.settings.initial_island_nr)
+        islands::vert_id_get(ss, vert) !=
+            automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
     {
       factors[i] = 0.0f;
       continue;
@@ -795,7 +796,8 @@ void calc_face_factors(const Depsgraph &depsgraph,
 
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
-          islands::vert_id_get(ss, vert) != automasking.settings.initial_island_nr)
+          islands::vert_id_get(ss, vert) !=
+              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
       {
         factor = 0.0f;
         continue;
@@ -923,7 +925,8 @@ void calc_grids_factors(const Depsgraph &depsgraph,
 
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
-          islands::vert_id_get(ss, vert) != automasking.settings.initial_island_nr)
+          islands::vert_id_get(ss, vert) !=
+              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
       {
         factors[node_vert] = 0.0f;
         continue;
@@ -1045,7 +1048,8 @@ void calc_vert_factors(const Depsgraph &depsgraph,
 
     if (!automasking.settings.topology_use_brush_limit &&
         automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
-        islands::vert_id_get(ss, vert_i) != automasking.settings.initial_island_nr)
+        islands::vert_id_get(ss, vert_i) !=
+            automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
     {
       factors[i] = 0.0f;
       continue;
@@ -1669,7 +1673,19 @@ std::unique_ptr<Cache> cache_init(const Depsgraph &depsgraph,
   vert_random_access_ensure(ob);
   if (mode & BRUSH_AUTOMASKING_TOPOLOGY && ss.active_vert_index() != -1) {
     islands::ensure_cache(ob);
-    automasking->settings.initial_island_nr = islands::vert_id_get(ss, ss.active_vert_index());
+
+    std::array<int, PAINT_SYMM_AREAS> symm_verts = find_all_symm_verts(
+        depsgraph, ob, ss.active_vert_index());
+    const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(ob);
+
+    for (int symm_it = 0; symm_it < PAINT_SYMM_AREAS; symm_it++) {
+      if (!is_symmetry_iteration_valid(symm_it, symm)) {
+        continue;
+      }
+      BLI_assert(symm_verts[symm_it] != -1);
+      automasking->settings.initial_island_nr[symm_it] = islands::vert_id_get(ss,
+                                                                              symm_verts[symm_it]);
+    }
   }
 
   const int verts_num = SCULPT_vertex_count_get(ob);

source/blender/editors/sculpt_paint/mesh/sculpt_automask.hh
@@ -9,6 +9,8 @@
 
 #include <memory>
 
+#include "BKE_paint.hh"
+
 #include "BLI_array.hh"
 #include "BLI_offset_indices.hh"
 #include "BLI_set.hh"
@@ -36,7 +38,7 @@ struct Settings {
   /* eAutomasking_flag. */
   int flags;
   int initial_face_set;
-  int initial_island_nr;
+  std::array<int, PAINT_SYMM_AREAS> initial_island_nr;
 
   float cavity_factor;
   int cavity_blur_steps;

source/blender/editors/sculpt_paint/mesh/sculpt_expand.cc
@@ -679,6 +679,124 @@ Vector<int> find_symm_verts(const Depsgraph &depsgraph,
   return {};
 }
 
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts(const Depsgraph &depsgraph,
+                                                      const Object &object,
+                                                      const int original_vert,
+                                                      const float max_distance)
+{
+  const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
+  switch (pbvh.type()) {
+    case bke::pbvh::Type::Mesh:
+      return find_all_symm_verts_mesh(depsgraph, object, original_vert, max_distance);
+    case bke::pbvh::Type::Grids:
+      return find_all_symm_verts_grids(object, original_vert, max_distance);
+    case bke::pbvh::Type::BMesh:
+      return find_all_symm_verts_bmesh(object, original_vert, max_distance);
+  }
+  BLI_assert_unreachable();
+  return {};
+}
+
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_mesh(const Depsgraph &depsgraph,
+                                                           const Object &object,
+                                                           const int original_vert,
+                                                           const float max_distance)
+{
+  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
+  const bool use_original = false;
+
+  std::array<int, PAINT_SYMM_AREAS> symm_verts;
+  symm_verts.fill(-1);
+  symm_verts[0] = original_vert;
+
+  const Mesh &mesh = *id_cast<const Mesh *>(object.data);
+  const Span<float3> positions = bke::pbvh::vert_positions_eval(depsgraph, object);
+  const bke::AttributeAccessor attributes = mesh.attributes();
+  const VArraySpan hide_vert = *attributes.lookup<bool>(".hide_vert", bke::AttrDomain::Point);
+
+  const float3 location = positions[original_vert];
+  for (int symm_it = 1; symm_it <= PAINT_SYMM_AREAS; symm_it++) {
+    if (!is_symmetry_iteration_valid(symm_it, symm)) {
+      continue;
+    }
+    const float3 symm_location = symmetry_flip(location, ePaintSymmetryFlags(symm_it));
+    const std::optional<int> nearest = nearest_vert_calc_mesh(
+        pbvh, positions, hide_vert, symm_location, max_distance, use_original);
+    if (!nearest) {
+      continue;
+    }
+    symm_verts[symm_it] = *nearest;
+  }
+
+  return symm_verts;
+}
+
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_grids(const Object &object,
+                                                            const int original_vert,
+                                                            const float max_distance)
+{
+  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
+  const bool use_original = false;
+
+  std::array<int, PAINT_SYMM_AREAS> symm_verts;
+  symm_verts.fill(-1);
+  symm_verts[0] = original_vert;
+
+  const SculptSession &ss = *object.runtime->sculpt_session;
+  const SubdivCCG &subdiv_ccg = *ss.subdiv_ccg;
+  const CCGKey key = BKE_subdiv_ccg_key_top_level(subdiv_ccg);
+  const Span<float3> positions = subdiv_ccg.positions;
+  const float3 location = positions[original_vert];
+  for (int symm_it = 1; symm_it <= PAINT_SYMM_AREAS; symm_it++) {
+    if (!is_symmetry_iteration_valid(symm_it, symm)) {
+      continue;
+    }
+    const float3 symm_location = symmetry_flip(location, ePaintSymmetryFlags(symm_it));
+    const std::optional<SubdivCCGCoord> nearest = nearest_vert_calc_grids(
+        pbvh, subdiv_ccg, symm_location, max_distance, use_original);
+    if (!nearest) {
+      continue;
+    }
+    symm_verts[symm_it] = nearest->to_index(key);
+  }
+
+  return symm_verts;
+}
+
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_bmesh(const Object &object,
+                                                            const int original_vert,
+                                                            const float max_distance)
+{
+  const ePaintSymmetryFlags symm = SCULPT_mesh_symmetry_xyz_get(object);
+  const bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(object);
+  const bool use_original = false;
+
+  std::array<int, PAINT_SYMM_AREAS> symm_verts;
+  symm_verts.fill(-1);
+  symm_verts[0] = original_vert;
+
+  const SculptSession &ss = *object.runtime->sculpt_session;
+  BMesh &bm = *ss.bm;
+  const BMVert *original_bm_vert = BM_vert_at_index(&bm, original_vert);
+  const float3 location = original_bm_vert->co;
+  for (int symm_it = 1; symm_it <= PAINT_SYMM_AREAS; symm_it++) {
+    if (!is_symmetry_iteration_valid(symm_it, symm)) {
+      continue;
+    }
+    const float3 symm_location = symmetry_flip(location, ePaintSymmetryFlags(symm_it));
+    const std::optional<BMVert *> nearest = nearest_vert_calc_bmesh(
+        pbvh, symm_location, max_distance, use_original);
+    if (!nearest) {
+      continue;
+    }
+    symm_verts[symm_it] = BM_elem_index_get(*nearest);
+  }
+
+  return symm_verts;
+}
+
 }  // namespace ed::sculpt_paint
 
 namespace ed::sculpt_paint::expand {

source/blender/editors/sculpt_paint/mesh/sculpt_intern.hh
@@ -763,6 +763,30 @@ Vector<int> find_symm_verts(const Depsgraph &depsgraph,
                             int original_vert,
                             float max_distance = std::numeric_limits<float>::max());
 
+/**
+ * Similar to `find_symm_verts`, but returns an unsorted set of all vertex indices.
+ *
+ * \note If a symmetry pass is invalid, the corresponding vertex index is set to -1
+ */
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_mesh(
+    const Depsgraph &depsgraph,
+    const Object &object,
+    int original_vert,
+    float max_distance = std::numeric_limits<float>::max());
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_grids(
+    const Object &object,
+    int original_vert,
+    float max_distance = std::numeric_limits<float>::max());
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts_bmesh(
+    const Object &object,
+    int original_vert,
+    float max_distance = std::numeric_limits<float>::max());
+std::array<int, PAINT_SYMM_AREAS> find_all_symm_verts(
+    const Depsgraph &depsgraph,
+    const Object &object,
+    int original_vert,
+    float max_distance = std::numeric_limits<float>::max());
+
 bool node_fully_masked_or_hidden(const bke::pbvh::Node &node);
 bool node_in_sphere(const bke::pbvh::Node &node,
                     const float3 &location,

FrenkyBojler/blender-ios

commit f6aaa252e40b0a7ad2a0428710262659c15eb4f9

Fix #156423: Crash when using sculpt filter tool & topology automasking
Introduced with d8716f070775e5d412f5eee86c9fe64b55458c97

It is incorrect to assume the `ss.cache` is always available when
calculating the automasking factor. If a filter tool is being used, use
the '0th' symmetry pass to index into the initial island id array.

source/blender/editors/sculpt_paint/mesh/sculpt_automasking.cc
@@ -794,10 +794,13 @@ void calc_face_factors(const Depsgraph &depsgraph,
         }
       }
 
+      /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+       * symmetry pass instead */
+      const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
           islands::vert_id_get(ss, vert) !=
-              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+              automasking.settings.initial_island_nr[current_symmetry_pass])
       {
         factor = 0.0f;
         continue;
@@ -923,10 +926,13 @@ void calc_grids_factors(const Depsgraph &depsgraph,
         }
       }
 
+      /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+       * symmetry pass instead */
+      const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
       if (!automasking.settings.topology_use_brush_limit &&
           automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
           islands::vert_id_get(ss, vert) !=
-              automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+              automasking.settings.initial_island_nr[current_symmetry_pass])
       {
         factors[node_vert] = 0.0f;
         continue;
@@ -1046,10 +1052,13 @@ void calc_vert_factors(const Depsgraph &depsgraph,
       }
     }
 
+    /* The filter tools do not take symmetry into consideration, use the "generic" (i.e. 0)
+     * symmetry pass instead */
+    const int current_symmetry_pass = ss.cache ? ss.cache->mirror_symmetry_pass : 0;
     if (!automasking.settings.topology_use_brush_limit &&
         automasking.settings.flags & BRUSH_AUTOMASKING_TOPOLOGY &&
         islands::vert_id_get(ss, vert_i) !=
-            automasking.settings.initial_island_nr[ss.cache->mirror_symmetry_pass])
+            automasking.settings.initial_island_nr[current_symmetry_pass])
     {
       factors[i] = 0.0f;
       continue;