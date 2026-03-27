f4e68e90d4dd40c0024f5d49df3c67ce69815eac
Shane Ambler <Graphics@Sheeky.biz>
2026-03-27 18:37:26 +1030

    add support for Eigen >= 3.4

--- intern/dualcon/intern/octree.cpp
+++ intern/dualcon/intern/octree.cpp
@@ -2184,7 +2184,11 @@ void Octree::countIntersection(Node *node, int height, int &nedge, int &ncell, i
 static void pseudoInverse(const Eigen::Matrix3f &a, Eigen::Matrix3f &result, float tolerance)
 {
   const int Options = Eigen::ComputeFullU | Eigen::ComputeFullV;
+#if EIGEN_VERSION_AT_LEAST(3, 4, 0)
+  auto svd = a.jacobiSvd(Options);
+#else
   Eigen::JacobiSVD<Eigen::Matrix3f, Options> svd = a.jacobiSvd<Options>();
+#endif
 
   result = svd.matrixV() *
            Eigen::Vector3f((svd.singularValues().array().abs() > tolerance)
