7c207a0b42cf7d5ea838b85f6ba9e6bbb31fd7d7
Jacques Lucke <jacques@blender.org>
2026-03-27 23:07:10 +0100

    use api to access geometry set name

--- source/blender/editors/space_spreadsheet/spreadsheet_data_source_geometry.cc
+++ source/blender/editors/space_spreadsheet/spreadsheet_data_source_geometry.cc
@@ -1301,9 +1301,11 @@ static std::unique_ptr<DataSource> data_source_from_geometry(
                                                   layer_index);
     }
     case SPREADSHEET_GEOMETRY_ITEM_TYPE_BUNDLE: {
-      const nodes::BundlePtr &bundle_ptr = geometry.bundle_ptr();
-      bke::SocketValueVariant value = lookup_bundle_path(bundle_ptr,
-                                                         geometry_id.geometry_bundle_path);
+      const nodes::BundlePtr *bundle_ptr = geometry.bundle_ptr();
+      bke::SocketValueVariant value = bundle_ptr ?
+                                          lookup_bundle_path(*bundle_ptr,
+                                                             geometry_id.geometry_bundle_path) :
+                                          bke::SocketValueVariant{};
       return data_source_from_socket_value(
           value,
           SpreadsheetClosureInputOutput(geometry_id.geometry_bundle_path.closure_input_output));
