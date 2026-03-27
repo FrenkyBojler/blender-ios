89a8b8add4cdbe8f7a4caef8d4c563c5ee00ceb4
ethan <ethan64682@gmail.com>
2026-03-27 00:49:46 -0500

    updated bmesh path to follow the same idea as mesh was updated to

--- source/blender/editors/sculpt_paint/mesh/sculpt_expand.cc
+++ source/blender/editors/sculpt_paint/mesh/sculpt_expand.cc
@@ -1714,13 +1714,13 @@ static bool update_mask_grids(const SculptSession &ss,
 
 static float calc_new_mask_bmesh(const SculptSession &ss,
                                  const Cache &expand_cache,
-                                 const Span<float> old_mask,
+                                 const float old_mask,
                                  const BitSpan enabled_verts,
                                  const BMVert *vert)
 {
   const int vert_index = BM_elem_index_get(vert);
   if (expand_cache.check_islands && !is_vert_in_active_component(ss, expand_cache, vert_index)) {
-    return old_mask[vert_index];
+    return old_mask;
   }
   float new_mask;
   if (enabled_verts[vert_index]) {
@@ -1749,21 +1749,22 @@ static bool update_mask_bmesh(SculptSession &ss,
   const Cache &expand_cache = *ss.expand_cache;
 
   bool any_changed = false;
+  int i = 0;
   for (BMVert *vert : BKE_pbvh_bmesh_node_unique_verts(node)) {
-    float new_mask = calc_new_mask_bmesh(ss, expand_cache, old_mask, enabled_verts, vert);
-    if (new_mask != old_mask[BM_elem_index_get(vert)]) {
+    float new_mask = calc_new_mask_bmesh(ss, expand_cache, old_mask[i], enabled_verts, vert);
+    if (new_mask != old_mask[i]) {
       any_changed = true;
     }
     BM_ELEM_CD_SET_FLOAT(vert, mask_offset, new_mask);
+    i++;
   }
   if (!any_changed) {
     for (BMVert *vert : BKE_pbvh_bmesh_node_other_verts(node)) {
-      if (calc_new_mask_bmesh(ss, expand_cache, old_mask, enabled_verts, vert) !=
-          old_mask[BM_elem_index_get(vert)])
-      {
+      if (calc_new_mask_bmesh(ss, expand_cache, old_mask[i], enabled_verts, vert) != old_mask[i]) {
         any_changed = true;
         break;
       }
+      i++;
     }
   }
   if (!any_changed) {
@@ -1987,18 +1988,28 @@ static void update_for_vert(bContext *C, Object &ob, const std::optional<int> ve
               &ss.bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
           MutableSpan<bke::pbvh::BMeshNode> nodes = pbvh.nodes<bke::pbvh::BMeshNode>();
 
-          BMVert *vert;
-          BMIter iter;
-          Vector<float> init_mask(ss.bm->totvert);
-          BM_ITER_MESH (vert, &iter, ss.bm, BM_VERTS_OF_MESH) {
-            init_mask[BM_elem_index_get(vert)] = BM_ELEM_CD_GET_FLOAT(vert, mask_offset);
-          }
-
+          Array<Vector<float>> old_masks(node_mask.min_array_size());
+          node_mask.foreach_index(
+              [&](const int i) {
+                const Set<BMVert *, 0> &unique = BKE_pbvh_bmesh_node_unique_verts(&nodes[i]);
+                const Set<BMVert *, 0> &other = BKE_pbvh_bmesh_node_other_verts(&nodes[i]);
+                old_masks[i].resize(unique.size() + other.size());
+                int j = 0;
+                for (BMVert *vert : unique) {
+                  old_masks[i][j] = BM_ELEM_CD_GET_FLOAT(vert, mask_offset);
+                  j++;
+                }
+                for (BMVert *vert : other) {
+                  old_masks[i][j] = BM_ELEM_CD_GET_FLOAT(vert, mask_offset);
+                  j++;
+                }
+              },
+              exec_mode::grain_size(1));
           Array<bool> node_changed(node_mask.min_array_size(), false);
           node_mask.foreach_index(
               [&](const int i) {
                 node_changed[i] = update_mask_bmesh(
-                    ss, enabled_verts, mask_offset, init_mask.as_span(), &nodes[i]);
+                    ss, enabled_verts, mask_offset, old_masks[i].as_span(), &nodes[i]);
               },
               exec_mode::grain_size(1));
 
