8b815e433faa47f46a669fa1e861a33ea9974b23
Cartesian Caramel <cartesiancaramel@gmail.com>
2026-03-29 14:04:50 -0400

    setup

--- scripts/startup/bl_ui/node_add_menu_geometry.py
+++ scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -576,6 +576,7 @@ class NODE_MT_gn_input_import_base(node_add_menu.NodeMenu):
     def draw(self, _context):
         layout = self.layout
         self.node_operator(layout, "GeometryNodeImportCSV", label="CSV (.csv)")
+        self.node_operator(layout, "GeometryNodeImportImage")
         self.node_operator(layout, "GeometryNodeImportOBJ", label="Wavefront (.obj)")
         self.node_operator(layout, "GeometryNodeImportPLY", label="Stanford PLY (.ply)")
         self.node_operator(layout, "GeometryNodeImportSTL", label="STL (.stl)")
--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -10429,6 +10429,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define("GeometryNode", "GeometryNodeImageInfo");
   define("GeometryNode", "GeometryNodeImageTexture", def_geo_image_texture);
   define("GeometryNode", "GeometryNodeImportCSV");
+  define("GeometryNode", "GeometryNodeImportImage");
   define("GeometryNode", "GeometryNodeImportOBJ");
   define("GeometryNode", "GeometryNodeImportPLY");
   define("GeometryNode", "GeometryNodeImportSTL");
