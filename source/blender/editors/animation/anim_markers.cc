cd7a14e3254b89df6ca89c0bbaedeb0fd87e2d74
Sybren A. Stüvel <sybren@blender.org>
2026-03-30 14:12:39 +0200

    One more `const`

--- source/blender/editors/animation/anim_markers.cc
+++ source/blender/editors/animation/anim_markers.cc
@@ -1519,7 +1519,7 @@ static wmOperatorStatus ed_marker_select_exec(bContext *C, wmOperator *op)
   const TimeMarker *marker = ED_markers_find_nearest_marker(markers, marker_frame);
   const bool is_over_marker = region_position_is_over_marker(v2d, markers, mval[0]);
 
-  bool camera = RNA_boolean_get(op->ptr, "camera");
+  const bool camera = RNA_boolean_get(op->ptr, "camera");
 
   if (camera && marker->camera && CTX_data_mode_enum(C) != CTX_MODE_OBJECT) {
     /* Supporting mode switching from this operator doesn't seem so useful.
