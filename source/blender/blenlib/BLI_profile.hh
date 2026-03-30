2d8816bd9f25f844f23389e2f3ae4d77c2fef322
Jonas Holzman <jonas@blender.org>
2026-03-30 12:47:44 +0200

    Complete and re-organize BLI_profile_* API

--- source/blender/blenlib/BLI_profile.hh
+++ source/blender/blenlib/BLI_profile.hh
@@ -6,8 +6,8 @@
  * \ingroup bli
  *
  * A tiny wrapper around the TracyClient library profiling API which takes care of including the
- * Tracy header and wrapping its macros inside of BLI_PROFILE_* macros.
- * When building without TracyClient enabled the macros are evaluated to no-op.
+ * Tracy header and exposing it via BLI_profile_* macros. When building without Tracy enabled
+ * the macros are evaluated to no-op.
  */
 
 #pragma once
@@ -16,21 +16,37 @@
 #  include <tracy/tracy/Tracy.hpp>
 #endif
 
+/* TODO: WIP incomplete API. See Tracy.hpp header for a complete list of implementable macros. */
+
 #ifdef WITH_TRACY_CLIENT
+/* Frame markers. */
+#  define BLI_profile_frame_mark FrameMark
+#  define BLI_profile_frame_mark_start(name) FrameMarkStart(name)
+#  define BLI_profile_frame_mark_end(name) FrameMarkEnd(name)
+
 /* Scoped zones, create a profiling zone lasting until end of current scope. */
 #  define BLI_profile_zone_scoped ZoneScoped
 #  define BLI_profile_zone_scoped_n(name) ZoneScopedN(name)
 
-/* Set dynamic zone name, text. TODO: Add color / value. */
+/* Named zones, zones attached to a specific string, allowing multiple zones in a single scope. */
+#  define BLI_profile_zone_named(zone) ZoneNamed(zone)
+#  define BLI_profile_zone_named_n(zone, ui_name) ZoneNamedN(zone_name, ui_name)
+
+/* Set dynamic zone name, text, color, and value. */
 #  define BLI_profile_zone_set_name(text, size) ZoneName(text, size)
 #  define BLI_profile_zone_set_name_fmt(fmt, ...) ZoneNameF(fmt, ##__VA_ARGS__)
 #  define BLI_profile_zone_set_text(text, size) ZoneText(text, size)
 #  define BLI_profile_zone_set_text_fmt(fmt, ...) ZoneTextF(fmt, ##__VA_ARGS__)
+#  define BLI_profile_zone_set_color(color) ZoneColor(color)
+#  define BLI_profile_zone_set_value(value) ZoneValue(value)
 
-/* Frame markers to delimit profiler frames. */
-#  define BLI_profile_frame_mark FrameMark
-#  define BLI_profile_frame_mark_start(name) FrameMarkStart(name)
-#  define BLI_profile_frame_mark_end(name) FrameMarkEnd(name)
+/* Named zone variants, taking zone name as first argument. */
+#  define BLI_profile_zone_set_name_z(zone, text, size) ZoneNameV(zone, text, size)
+#  define BLI_profile_zone_set_name_fmt_z(zone, fmt, ...) ZoneNameVF(zone, fmt, ##__VA_ARGS__)
+#  define BLI_profile_zone_set_text_z(zone, text, size) ZoneTextV(zone, text, size)
+#  define BLI_profile_zone_set_text_fmt_z(zone, fmt, ...) ZoneTextVF(zone, fmt, ##__VA_ARGS__)
+#  define BLI_profile_zone_set_color_z(zone, color) ZoneColorV(zone, color)
+#  define BLI_profile_zone_set_value_z(zone, value) ZoneValueV(zone, value)
 
 /* Memory allocation profiling. */
 #  define BLI_profile_memory_alloc(ptr, size) TracyAlloc(ptr, size)
@@ -38,17 +54,30 @@
 #  define BLI_profile_memory_free(ptr) TracyFree(ptr)
 #  define BLI_profile_memory_free_n(ptr, name) TracyFreeN(ptr, name)
 
-/* Thread naming for identify threads in the profiler UI. */
+/* Set current thread name. */
 #  define BLI_profile_set_thread_name(name) tracy::SetThreadName(name)
 #  define BLI_profile_set_thread_name_with_hint(name, hint) tracy::SetThreadNameWithHint(name, hint)
+
 #else
 #  define BLI_profile_zone_scoped
 #  define BLI_profile_zone_scoped_n(name)
+
+#  define BLI_profile_zone_named(zone_name)
+#  define BLI_profile_zone_named_n(zone_name, ui_name)
 
 #  define BLI_profile_zone_set_name(text, size)
 #  define BLI_profile_zone_set_name_fmt(fmt, ...)
 #  define BLI_profile_zone_set_text(text, size)
 #  define BLI_profile_zone_set_text_fmt(fmt, ...)
+#  define BLI_profile_zone_set_color(color)
+#  define BLI_profile_zone_set_value(value)
+
+#  define BLI_profile_zone_set_name_z(zone, text, size)
+#  define BLI_profile_zone_set_name_fmt_z(zone, fmt, ...)
+#  define BLI_profile_zone_set_text_z(zone, text, size)
+#  define BLI_profile_zone_set_text_fmt_z(zone, fmt, ...)
+#  define BLI_profile_zone_set_color_z(zone, color)
+#  define BLI_profile_zone_set_value_z(zone, value)
 
 #  define BLI_profile_frame_mark
 #  define BLI_profile_frame_mark_start(name)
