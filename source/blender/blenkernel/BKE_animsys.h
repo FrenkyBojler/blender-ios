0f4d28a89d425f94cc1bb063f7fc83074a98f5ac
Hans Goudey <hans@blender.org>
2026-03-30 08:21:34 -0400

    Double period

--- source/blender/blenkernel/BKE_animsys.h
+++ source/blender/blenkernel/BKE_animsys.h
@@ -176,7 +176,7 @@ void BKE_animdata_fix_paths_rename(struct ID *owner_id,
  * Fix all RNA-Paths throughout the database (directly access the #Global.main version).
  *
  * \param old_infix,new_infix The path secion immediately following the `prefix`. If
- * `infix_is_name` is true, this is processed as a name..
+ * `infix_is_name` is true, this is processed as a name.
  *
  * \param infix_is_name If true, old_infix and new_infix are treated as names and padded with
  * [""] so that only exact matches are made. For example, the structure we're replacing is
