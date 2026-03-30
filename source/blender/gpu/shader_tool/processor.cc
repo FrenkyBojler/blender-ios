351b656eb8f667ce1b56dffe99cf9b2515c02aed
Clément Foucault <foucault.clem@gmail.com>
2026-03-30 15:11:50 +0200

    Fix lower_namesless_parameters

--- source/blender/gpu/shader_tool/processor.cc
+++ source/blender/gpu/shader_tool/processor.cc
@@ -475,12 +475,22 @@ void SourceProcessor::lint_pragma_once(Parser &parser, const string &filename)
 
 void SourceProcessor::lower_namesless_parameters(Parser &parser)
 {
-  parser().foreach_function([&](bool, Token, Token, Scope fn_args, bool, Scope) {
+  parser().foreach_token(ParOpen, [&](Token tok) {
+    if (tok.scope().type() != ScopeType::FunctionArgs) {
+      return;
+    }
+    if (tok.prev(2).str().starts_with("Pipeline")) {
+      return;
+    }
     int i = 0;
-    fn_args.foreach_scope(ScopeType::FunctionArg, [&](Scope arg) {
-      if (arg.token_count() == 1 || arg.back().prev() == Const || arg.back() == '&') {
+    tok.scope().foreach_scope(ScopeType::FunctionArg, [&](Scope arg) {
+      if (arg.token_count() == 1 || arg.back().prev() == Const || arg.back() == '&' ||
+          arg.back() == '>')
+      {
         /* Append a name for nameless argument. */
-        parser.insert_after(arg.back(), "unused" + std::to_string(i++));
+        parser.replace(arg.back().str_index_last_no_whitespace() + 1,
+                       arg.back().str_index_last(),
+                       " _" + std::to_string(i++));
       }
     });
   });
