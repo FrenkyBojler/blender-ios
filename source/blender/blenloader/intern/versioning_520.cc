9103b1d9603d44ac12b4dbe49999c13de5d7a66c
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-03-30 07:11:49 -0700

    Cleanup: Move to `do_versions_after_linking_520`

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -100,6 +100,15 @@ void do_versions_after_linking_520(FileData * /*fd*/, Main *bmain)
       do_version_file_output_use_file_extension_recursive(*node_tree, scene);
     }
   }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 16)) {
+    for (Brush &brush : bmain->brushes) {
+      if (brush.gpencil_settings != nullptr) {
+        brush.gpencil_settings->curve_type = CURVE_TYPE_POLY;
+        brush.gpencil_settings->conversion_threshold = 0.001f;
+      }
+    }
+  }
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
@@ -200,15 +209,6 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 15)) {
     for (Scene &scene : bmain->scenes) {
       scene.r.scemode |= R_USE_TEXTURE_CACHE;
-    }
-  }
-
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 16)) {
-    for (Brush &brush : bmain->brushes) {
-      if (brush.gpencil_settings != nullptr) {
-        brush.gpencil_settings->curve_type = CURVE_TYPE_POLY;
-        brush.gpencil_settings->conversion_threshold = 0.001f;
-      }
     }
   }
   /**
