348a082601b606c1e2c89bedaf5e0b7b0049609c
Pratik Borhade <pratikborhade302@gmail.com>
2026-03-30 10:28:23 +0530

    Fix #156414: UI: Area dock cursor doesn't update when executed from hotkey
    When operator invoked from main region of one area, cursor doesn't update once
    operator is finished. This is because previon and current active regions are same
    after docking, results in swin_changed=false for `region_cursor_set_ex`. To update
    the cursor in such case, set `win.tag_cursor_refresh=true`.

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -5022,6 +5022,8 @@ static wmOperatorStatus area_join_modal(bContext *C, wmOperator *op, const wmEve
         else if (jd->sa1 && jd->sa2 && jd->dir != SCREEN_DIR_NONE) {
           /* Join to neighbor. */
           area_join_apply(C, op);
+          wmWindow *win = CTX_wm_window(C);
+          win->tag_cursor_refresh = true;
         }
         else {
           area_join_cancel(C, op);
