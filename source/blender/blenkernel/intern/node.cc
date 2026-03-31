a5c1a57f04acdceb5fb215b6d8b354afe0bc876a
илья _ <modormoder@gmail.com>
2026-04-01 02:00:34 +0300

    cleanup

--- source/blender/blenkernel/intern/node.cc
+++ source/blender/blenkernel/intern/node.cc
@@ -4235,9 +4235,6 @@ static void *node_static_value_storage_for(bNode &node, const bNodeSocket &socke
   if (STREQ(node.idname, "ShaderNodeRGB")) {
     return &node.output_socket(0).default_value_typed<bNodeSocketValueRGBA>()->value;
   }
-  if (STREQ(node.idname, "ShaderNodeRGB")) {
-    return &node.output_socket(0).default_value_typed<bNodeSocketValueRGBA>()->value;
-  }
   if (STREQ(node.idname, "ShaderNodeValue")) {
     return &node.output_socket(0).default_value_typed<bNodeSocketValueFloat>()->value;
   }
