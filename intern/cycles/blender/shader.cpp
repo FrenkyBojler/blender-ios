8cccecfcb92434d38f0b8991df522f9006206116
илья _ <modormoder@gmail.com>
2026-04-01 00:10:18 +0300

    cleanup

--- intern/cycles/blender/shader.cpp
+++ intern/cycles/blender/shader.cpp
@@ -362,12 +362,8 @@ static ShaderNode *add_node(Scene *scene,
   }
   else if (b_node.is_type("FunctionNodeInputVector")) {
     ColorNode *value = graph->create_node<ColorNode>();
-    auto &storage = *static_cast<blender::NodeInputVector *>(b_node.storage);
-    float3 stored_value;
-    stored_value[0] = storage.vector[0];
-    stored_value[1] = storage.vector[1];
-    stored_value[2] = storage.vector[2];
-    value->set_value(stored_value);
+    const auto &storage = *static_cast<const blender::NodeInputVector *>(b_node.storage);
+    value->set_value(make_float3(storage.vector[0], storage.vector[1], storage.vector[2]));
     node = value;
   }
   else if (b_node.is_type("ShaderNodeValue")) {
@@ -377,13 +373,13 @@ static ShaderNode *add_node(Scene *scene,
   }
   else if (b_node.is_type("FunctionNodeInputBool")) {
     ValueNode *value = graph->create_node<ValueNode>();
-    auto &storage = *static_cast<blender::NodeInputBool *>(b_node.storage);
+    const auto &storage = *static_cast<const blender::NodeInputBool *>(b_node.storage);
     value->set_value(bool(storage.boolean));
     node = value;
   }
   else if (b_node.is_type("FunctionNodeInputInt")) {
     ValueNode *value = graph->create_node<ValueNode>();
-    auto &storage = *static_cast<blender::NodeInputInt *>(b_node.storage);
+    const auto &storage = *static_cast<const blender::NodeInputInt *>(b_node.storage);
     value->set_value(storage.integer);
     node = value;
   }
