886bbc58d9cf3bdd186220453c325a7060b0fc65
Weizhen Huang <weizhen@blender.org>
2026-03-31 10:56:04 +0200

    Address reviews

--- source/blender/gpu/GPU_material.hh
+++ source/blender/gpu/GPU_material.hh
@@ -318,30 +318,30 @@ struct GPUNodeStack {
 
   bool socket_not_zero() const
   {
-    return this->link || (saturatef(this->vec[0]) > NEAR_ZERO);
+    return this->link || (saturate_f(this->vec[0]) > near_zero);
   }
 
   bool socket_not_one() const
   {
-    return this->link || (saturatef(this->vec[0]) < NEAR_ONE);
+    return this->link || (saturate_f(this->vec[0]) < near_one);
   }
 
   bool socket_not_black() const
   {
-    return this->link || saturatef(this->vec[0]) > NEAR_ZERO ||
-           saturatef(this->vec[1]) > NEAR_ZERO || saturatef(this->vec[2]) > NEAR_ZERO;
+    return this->link || saturate_f(this->vec[0]) > near_zero ||
+           saturate_f(this->vec[1]) > near_zero || saturate_f(this->vec[2]) > near_zero;
   }
 
   bool socket_not_white() const
   {
-    return this->link || saturatef(this->vec[0]) < NEAR_ONE ||
-           saturatef(this->vec[1]) < NEAR_ONE || saturatef(this->vec[2]) < NEAR_ONE;
+    return this->link || saturate_f(this->vec[0]) < near_one ||
+           saturate_f(this->vec[1]) < near_one || saturate_f(this->vec[2]) < near_one;
   }
 
  private:
-  static constexpr float NEAR_ZERO = 1e-5f;
-  static constexpr float NEAR_ONE = 1.0f - 1e-5f;
-  float saturatef(const float f) const
+  static constexpr float near_zero = 1e-5f;
+  static constexpr float near_one = 1.0f - 1e-5f;
+  float saturate_f(const float f) const
   {
     return clamp_f(f, 0.0f, 1.0f);
   }
