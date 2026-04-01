blender/blender

commit d086b596402ff2c7891b3f8b68fa3bef70e0eed0

VSE: Lazy colorspace conversions (#119501)
VSE by default uses sRGB color space, and was immediately converting
everything into it. In some cases that created redundant colorspace
conversions.

Now, all images are converted into sequencer (default sRGB) color space
only "when needed", which is these cases:
- Strip has non-default color multiplier or color saturation setting,
- Strip has "make float" option on, and input is not already float,
- Image is an input to a non-compositor effect,
- Image needs to be blended with result of lower channels,
- Non-compositor modifier *without* using "linear modifiers" option.

Note: this is not 100% backwards compatible change, because strip scale
or rotation does *not* cause it to get converted to sequencer color
space. This only affects strips where images are not already sRGB; now
the pixels will be filtered in their current color space. Arguably when
that color space is linear, that is *more correct* result than
previously, but it can change the result ever so slightly
(example image in the PR).

Performance (Ryzen 5950X / RTX 3080Ti, Linux):

Scenario A: 100 4K resolution EXR images (1.8GB), just render out this
sequence as H.264 movie, no effects, transforms, nothing: 51sec -> 48sec.

Scenario B: several simple compositor effects stacked on a strip, plus
that is blended with a compositor based procedural texture strip.
Playback FPS with GPU compositor: 5.5FPS -> 6.2FPS.

Pull Request: https://projects.blender.org/blender/blender/pulls/156281

source/blender/render/intern/pipeline.cc
@@ -1367,47 +1367,10 @@ static bool seq_result_needs_float(const ImageFormatData &im_format)
   return ELEM(im_format.depth, R_IMF_CHAN_DEPTH_10, R_IMF_CHAN_DEPTH_12);
 }
 
-static ImBuf *seq_process_render_image(ImBuf *src,
-                                       const ImageFormatData &im_format,
-                                       const Scene *scene)
-{
-  if (src == nullptr) {
-    return nullptr;
-  }
-
-  ImBuf *dst = nullptr;
-  if (seq_result_needs_float(im_format) && src->float_buffer.data == nullptr) {
-    /* If render output needs >8-BPP input and we only have 8-BPP, convert to float. */
-    dst = IMB_allocImBuf(src->x, src->y, src->planes, 0);
-    IMB_alloc_float_pixels(dst, src->channels, false);
-    /* Transform from sequencer space to scene linear. */
-    const char *from_colorspace = IMB_colormanagement_get_byte_colorspace(src);
-    const char *to_colorspace = IMB_colormanagement_role_colorspace_name_get(
-        COLOR_ROLE_SCENE_LINEAR);
-    IMB_colormanagement_transform_byte_to_float(dst->float_buffer.data,
-                                                src->byte_buffer.data,
-                                                src->x,
-                                                src->y,
-                                                src->channels,
-                                                from_colorspace,
-                                                to_colorspace);
-  }
-  else {
-    /* Duplicate sequencer output and ensure it is in needed color space. */
-    dst = IMB_dupImBuf(src);
-    seq::render_imbuf_from_sequencer_space(scene, dst);
-  }
-  IMB_metadata_copy(dst, src);
-  IMB_freeImBuf(src);
-
-  return dst;
-}
-
 /* Render sequencer strips into render result. */
 static void do_render_sequencer(Render *re)
 {
   static int recurs_depth = 0;
-  ImBuf *out;
   RenderResult *rr; /* don't assign re->result here as it might change during give_ibuf_seq */
   int cfra = re->r.cfra;
   seq::RenderData context;
@@ -1448,8 +1411,13 @@ static void do_render_sequencer(Render *re)
 
   for (view_id = 0; view_id < tot_views; view_id++) {
     context.view_id = view_id;
-    out = render_give_ibuf(&context, cfra, 0);
-    ibuf_arr[view_id] = seq_process_render_image(out, re->r.im_format, re->pipeline_scene_eval);
+    ImBuf *out = render_give_ibuf(&context, cfra, 0);
+    if (out != nullptr) {
+      bool make_float = seq_result_needs_float(re->r.im_format);
+      out = IMB_makeSingleUser(out);
+      seq::ensure_ibuf_is_linear_space(out, make_float);
+      ibuf_arr[view_id] = out;
+    }
   }
 
   rr = re->result;

source/blender/editors/util/ed_util_imbuf.cc
@@ -338,9 +338,9 @@ static void sequencer_sample_apply(bContext *C, wmOperator *op, const wmEvent *e
       info->colf[3] = fp[3];
       info->colfp = info->colf;
 
-      /* sequencer's image buffers are in non-linear space, need to make them linear */
       copy_v4_v4(info->linearcol, info->colf);
-      seq::render_pixel_from_sequencer_space_v4(scene, info->linearcol);
+      IMB_colormanagement_colorspace_to_scene_linear_v4(
+          info->linearcol, true, ibuf->float_buffer.colorspace);
 
       info->color_manage = true;
     }

source/blender/sequencer/SEQ_render.hh
@@ -76,8 +76,10 @@ void render_new_render_data(Main *bmain,
                             RenderData *r_context);
 StripElem *render_give_stripelem(const Scene *scene, const Strip *strip, int timeline_frame);
 
-void render_imbuf_from_sequencer_space(const Scene *scene, ImBuf *ibuf);
-void render_pixel_from_sequencer_space_v4(const Scene *scene, float pixel[4]);
+/* Converts image into scene linear, if needed.
+ * Note: if make_float is false and input image contains byte pixels, it is *NOT* modified. */
+void ensure_ibuf_is_linear_space(ImBuf *ibuf, bool make_float);
+
 /**
  * Check if `strip` is muted for rendering.
  * This function also checks `SeqTimelineChannel` flag.

source/blender/sequencer/intern/cache/thumbnail_cache.cc
@@ -220,8 +220,7 @@ static ImBuf *make_thumb_for_image(const Scene *scene, const ThumbnailCache::Req
     IMB_free_byte_pixels(ibuf);
   }
 
-  seq_imbuf_to_sequencer_space(scene, ibuf, false);
-  seq_imbuf_assign_spaces(scene, ibuf);
+  ensure_ibuf_is_sequencer_space(scene, ibuf, false);
   return ibuf;
 }
 

source/blender/sequencer/intern/compositor.cc
@@ -8,7 +8,6 @@
 
 #include "BKE_node_runtime.hh"
 #include "COM_realize_on_domain_operation.hh"
-#include "IMB_colormanagement.hh"
 #include "IMB_imbuf.hh"
 
 #include "compositor.hh"
@@ -122,12 +121,6 @@ void CompositorContext::set_output_refcount(const bNodeTree &node_group,
     const bool is_color = output_result.type() == ResultType::Color;
     output_result.set_reference_count(is_first_output && is_color ? 1 : 0);
   }
-}
-
-bool is_linear_float_buffer(const ImBuf *image_buffer)
-{
-  return image_buffer->float_buffer.data &&
-         IMB_colormanagement_space_is_scene_linear(image_buffer->float_buffer.colorspace);
 }
 
 }  // namespace blender::seq

source/blender/sequencer/intern/compositor.hh
@@ -74,6 +74,4 @@ class CompositorContext : public compositor::Context {
                            compositor::NodeGroupOperation &node_group_operation);
 };
 
-bool is_linear_float_buffer(const ImBuf *image_buffer);
-
 }  // namespace blender::seq

source/blender/sequencer/intern/effects/effect_compositor.cc
@@ -22,7 +22,6 @@
 #include "cache/compositor_cache.hh"
 #include "compositor.hh"
 #include "effects.hh"
-#include "render.hh"
 
 namespace blender::seq {
 
@@ -118,49 +117,6 @@ class CompositorEffectContext : public CompositorContext {
   }
 };
 
-static ImBuf *make_linear_float_buffer(ImBuf *src)
-{
-  if (!src) {
-    return nullptr;
-  }
-
-  /* Already have scene linear float pixels, return same buffer. */
-  if (is_linear_float_buffer(src)) {
-    return src;
-  }
-
-  ImBuf *dst = IMB_allocImBuf(
-      src->x, src->y, src->planes, IB_float_data | IB_uninitialized_pixels);
-  const char *to_colorspace = IMB_colormanagement_role_colorspace_name_get(
-      COLOR_ROLE_SCENE_LINEAR);
-  if (src->float_buffer.data == nullptr) {
-    const char *from_colorspace = IMB_colormanagement_get_byte_colorspace(src);
-    IMB_colormanagement_transform_byte_to_float(dst->float_buffer.data,
-                                                src->byte_buffer.data,
-                                                src->x,
-                                                src->y,
-                                                src->channels,
-                                                from_colorspace,
-                                                to_colorspace);
-  }
-  else {
-    const char *from_colorspace = IMB_colormanagement_get_float_colorspace(src);
-    //@TODO: src->dst transform would be faster instead of copy + transform in-place
-    memcpy(dst->float_buffer.data,
-           src->float_buffer.data,
-           IMB_get_pixel_count(src) * src->channels * sizeof(float));
-    IMB_colormanagement_transform_float(dst->float_buffer.data,
-                                        dst->x,
-                                        dst->y,
-                                        dst->channels,
-                                        from_colorspace,
-                                        to_colorspace,
-                                        true);
-  }
-  IMB_colormanagement_assign_float_colorspace(dst, to_colorspace);
-  return dst;
-}
-
 static ImBuf *do_compositor_effect(const RenderData *context,
                                    SeqRenderState * /*state*/,
                                    Strip *strip,
@@ -180,18 +136,16 @@ static ImBuf *do_compositor_effect(const RenderData *context,
     IMB_rectfill(out, float4(0, 0, 0, 1));
   }
   else {
-    ImBuf *linear_src1 = make_linear_float_buffer(src1);
-    ImBuf *linear_src2 = make_linear_float_buffer(src2);
+    if (src1) {
+      ensure_ibuf_is_linear_space(src1, true);
+    }
+    if (src2) {
+      ensure_ibuf_is_linear_space(src2, true);
+    }
 
     CompositorCache &com_cache = context->scene->ed->runtime->ensure_compositor_cache();
-    CompositorEffectContext com_context(com_cache.get_cache_manager(),
-                                        *context,
-                                        data->node_group,
-                                        linear_src1,
-                                        linear_src2,
-                                        out,
-                                        fac,
-                                        *strip);
+    CompositorEffectContext com_context(
+        com_cache.get_cache_manager(), *context, data->node_group, src1, src2, out, fac, *strip);
 
     const bool use_gpu = com_context.use_gpu();
     if (use_gpu) {
@@ -204,14 +158,6 @@ static ImBuf *do_compositor_effect(const RenderData *context,
     if (use_gpu) {
       render_end_gpu(*context);
     }
-
-    if (linear_src1 != src1) {
-      IMB_freeImBuf(linear_src1);
-    }
-    if (linear_src2 != src2) {
-      IMB_freeImBuf(linear_src2);
-    }
-    seq_imbuf_to_sequencer_space(context->scene, out, true);
   }
   return out;
 }

source/blender/sequencer/intern/effects/effects.cc
@@ -52,14 +52,12 @@ ImBuf *prepare_effect_imbufs(const RenderData *context,
   }
 
   if (out->float_buffer.data) {
-    if (ibuf1 && !ibuf1->float_buffer.data) {
-      seq_imbuf_to_sequencer_space(scene, ibuf1, true);
+    if (ibuf1) {
+      ensure_ibuf_is_sequencer_space(scene, ibuf1, true);
     }
-
-    if (ibuf2 && !ibuf2->float_buffer.data) {
-      seq_imbuf_to_sequencer_space(scene, ibuf2, true);
+    if (ibuf2) {
+      ensure_ibuf_is_sequencer_space(scene, ibuf2, true);
     }
-
     IMB_colormanagement_assign_float_colorspace(out, scene->sequencer_colorspace_settings.name);
   }
   else {

FrenkyBojler/blender-ios

commit e7cff10bc6ad5c196b7eb059ba379a23cdc597ba

Fix uninitialized memory use in the sequencer
Resolve regression in d086b596402ff2c7891b3f8b68fa3bef70e0eed0

source/blender/render/intern/pipeline.cc
@@ -1416,8 +1416,8 @@ static void do_render_sequencer(Render *re)
       bool make_float = seq_result_needs_float(re->r.im_format);
       out = IMB_makeSingleUser(out);
       seq::ensure_ibuf_is_linear_space(out, make_float);
-      ibuf_arr[view_id] = out;
     }
+    ibuf_arr[view_id] = out;
   }
 
   rr = re->result;