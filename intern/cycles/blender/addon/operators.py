6ebd884797eed6545e90c9dfcdeae1724abd1edb
Martijn Versteegh <blender@aaltjegron.nl>
2026-04-01 21:43:55 +0200

    Simplify, just add 'denoising'

--- intern/cycles/blender/addon/operators.py
+++ intern/cycles/blender/addon/operators.py
@@ -29,9 +29,8 @@ class CYCLES_OT_use_shading_nodes(Operator):
 
 class CYCLES_OT_denoise_animation(Operator):
     "Denoise rendered animation sequence using current scene and view " \
-        "layer settings. Requires denoising data passes and output to " \
-        "OpenEXR multilayer files. Uses the same denoiser settings as"  \
-        "per frame denoising."
+        "layer denoising settings. Requires denoising data passes and " \
+        "output to OpenEXR multilayer files."
     bl_idname = "cycles.denoise_animation"
     bl_label = "Denoise Animation"
 
