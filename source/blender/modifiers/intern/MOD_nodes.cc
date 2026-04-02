zNightlord/Bforartists

commit 1561c1ea4ab652010e2cbff4976f4444cd5a5b7c

Geometry Nodes: Runtime RNA for node tree interface
This PR implements the changes described in #132129. The main goal is to
use actual RNA types to store node group inputs on the Geometry Nodes
modifier and node tools, rather than using custom properties. This makes
the API much friendlier, as well as resolving long-standing technical
debt.

The API change looks like this:
```python
# Before
modifier["identifier"] = 5.0
modifier["identifier_use_attribute"] = True
modifier["identifier_attribute_name"] = "some_input_attribute"
modifier["identifier_attribute_name"] = "some_output_attribute"
# After
modifier.inputs.identifier.value = 5.0
modifier.inputs.identifier.type = "ATTRIBUTE"
modifier.inputs.identifier.attribute_name = "some_input_attribute"
modifier.outputs.identifier.attribute_name = "some_output_attribute"
```

The `identifier` is the identifier of the interface socket (e.g.
`Socket_2` currently). This PR does not allow changing this property
name, that can be done as separately later.

Another benefit of this change is that combined with the work from
#148763, now the node tool inputs are remembered between invocations
like properties from any other operator. To get node tool registration
working, we have to include all the information necessary for the RNA
property registration in the asset meta-data.

Backward and forward compatibility are supported (though animation paths
are only backward compatible), but this does break the current Python API
for accessing Geometry Nodes modifier properties.

Co-authored-by: Hans Goudey <hans@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/138117

source/blender/modifiers/intern/MOD_nodes.cc
@@ -69,6 +69,7 @@
 
 #include "RNA_access.hh"
 #include "RNA_enum_types.hh"
+#include "RNA_path.hh"
 #include "RNA_prototypes.hh"
 
 #include "DEG_depsgraph_build.hh"
@@ -89,6 +90,7 @@
 #include "NOD_geometry_nodes_execute.hh"
 #include "NOD_geometry_nodes_gizmos.hh"
 #include "NOD_geometry_nodes_lazy_function.hh"
+#include "NOD_geometry_nodes_srna.hh"
 #include "NOD_node_declaration.hh"
 #include "NOD_socket_usage_inference.hh"
 
@@ -108,14 +110,15 @@ static void init_data(ModifierData *md)
   nmd->runtime->cache = std::make_shared<bake::ModifierCache>();
 }
 
-static void find_dependencies_from_settings(const NodesModifierSettings &settings,
+static void find_dependencies_from_settings(const NodesModifierData &nmd,
                                             nodes::EvalDependencies &deps)
 {
-  IDP_foreach_property(settings.properties, IDP_TYPE_FILTER_ID, [&](IDProperty *property) {
-    if (ID *id = IDP_ID_get(property)) {
-      deps.add_generic_id_full(id);
-    }
-  });
+  IDP_foreach_property(
+      nmd.modifier.system_properties, IDP_TYPE_FILTER_ID, [&](IDProperty *property) {
+        if (ID *id = IDP_ID_get(property)) {
+          deps.add_generic_id_full(id);
+        }
+      });
 }
 
 /* We don't know exactly what attributes from the other object we will need. */
@@ -178,7 +181,7 @@ static void update_depsgraph(ModifierData *md, const ModifierUpdateDepsgraphCont
   nodes::EvalDependencies eval_deps = nodes::gather_eval_dependencies_recursive(*nmd->node_group);
 
   /* Create dependencies to data-blocks referenced by the settings in the modifier. */
-  find_dependencies_from_settings(nmd->settings, eval_deps);
+  find_dependencies_from_settings(*nmd, eval_deps);
 
   if (ctx->object->type == OB_CURVES) {
     Curves *curves_id = id_cast<Curves *>(ctx->object->data);
@@ -265,9 +268,18 @@ static void foreach_ID_link(ModifierData *md, Object *ob, IDWalkFunc walk, void 
   NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
   walk(user_data, ob, reinterpret_cast<ID **>(&nmd->node_group), IDWALK_CB_USER);
 
-  IDP_foreach_property(nmd->settings.properties, IDP_TYPE_FILTER_ID, [&](IDProperty *id_prop) {
-    walk(user_data, ob, reinterpret_cast<ID **>(&id_prop->data.pointer), IDWALK_CB_USER);
-  });
+  if (nmd->settings.properties) {
+    /* Also walk legacy properties because they are still used at runtime before post linking
+     * versioning. */
+    IDP_foreach_property(nmd->settings.properties, IDP_TYPE_FILTER_ID, [&](IDProperty *id_prop) {
+      walk(user_data, ob, reinterpret_cast<ID **>(&id_prop->data.pointer), IDWALK_CB_USER);
+    });
+  }
+
+  IDP_foreach_property(
+      nmd->modifier.system_properties, IDP_TYPE_FILTER_ID, [&](IDProperty *id_prop) {
+        walk(user_data, ob, reinterpret_cast<ID **>(&id_prop->data.pointer), IDWALK_CB_USER);
+      });
 
   for (NodesModifierBake &bake : MutableSpan(nmd->bakes, nmd->bakes_num)) {
     for (NodesModifierDataBlock &data_block : MutableSpan(bake.data_blocks, bake.data_blocks_num))
@@ -304,29 +316,6 @@ static bool logging_enabled(const ModifierEvalContext *ctx)
     return false;
   }
   return true;
-}
-
-static void update_id_properties_from_node_group(NodesModifierData *nmd)
-{
-  if (nmd->node_group == nullptr || ID_MISSING(nmd->node_group)) {
-    if (nmd->settings.properties) {
-      IDP_FreeProperty(nmd->settings.properties);
-      nmd->settings.properties = nullptr;
-    }
-    return;
-  }
-
-  IDProperty *old_properties = nmd->settings.properties;
-  nmd->settings.properties = bke::idprop::create_group("Nodes Modifier Settings").release();
-  IDProperty *new_properties = nmd->settings.properties;
-
-  nodes::update_input_properties_from_node_tree(*nmd->node_group, old_properties, *new_properties);
-  nodes::update_output_properties_from_node_tree(
-      *nmd->node_group, old_properties, *new_properties);
-
-  if (old_properties != nullptr) {
-    IDP_FreeProperty(old_properties);
-  }
 }
 
 static void remove_outdated_bake_caches(NodesModifierData &nmd)
@@ -451,9 +440,23 @@ static void update_panels_from_node_group(NodesModifierData &nmd)
   nmd.panels_num = interface_panels.size();
 }
 
+static void update_system_properties(Object &object, NodesModifierData &nmd)
+{
+  if (!nmd.modifier.system_properties) {
+    nmd.modifier.system_properties =
+        bke::idprop::create_group("NodesModifierProperties").release();
+  }
+  if (!nmd.node_group || ID_MISSING(nmd.node_group)) {
+    return;
+  }
+  PointerRNA properties_ptr = RNA_pointer_create_discrete(
+      &object.id, RNA_NodesModifierProperties, &nmd);
+  RNA_sync_system_properties(properties_ptr, *nmd.modifier.system_properties);
+}
+
 void MOD_nodes_update_interface(Object *object, NodesModifierData *nmd)
 {
-  update_id_properties_from_node_group(nmd);
+  update_system_properties(*object, *nmd);
   update_bakes_from_node_group(*nmd);
   update_panels_from_node_group(*nmd);
   nmd->runtime->usage_cache.reset();
@@ -902,64 +905,6 @@ static void find_verbose_log_contexts(const NodesModifierData &nmd,
           r_socket_log_contexts.add(hash);
         }
       }
-    }
-  }
-}
-
-/**
- * \note This could be done in #initialize_group_input, though that would require adding the
- * the object as a parameter, so it's likely better to this check as a separate step.
- */
-static void check_property_socket_sync(const Object *ob,
-                                       const IDProperty *properties,
-                                       ModifierData *md)
-{
-  NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-
-  int geometry_socket_count = 0;
-
-  nmd->node_group->ensure_interface_cache();
-  const Span<nodes::StructureType> input_structure_types =
-      nmd->node_group->runtime->structure_type_interface->inputs;
-  for (const int i : nmd->node_group->interface_inputs().index_range()) {
-    const bNodeTreeInterfaceSocket *socket = nmd->node_group->interface_inputs()[i];
-    const bke::bNodeSocketType *typeinfo = socket->socket_typeinfo();
-    const eNodeSocketDatatype type = typeinfo ? typeinfo->type : SOCK_CUSTOM;
-    if (type == SOCK_GEOMETRY) {
-      geometry_socket_count++;
-    }
-    /* The first socket is the special geometry socket for the modifier object. */
-    if (i == 0 && type == SOCK_GEOMETRY) {
-      continue;
-    }
-    if (ELEM(input_structure_types[i], nodes::StructureType::Grid, nodes::StructureType::List)) {
-      continue;
-    }
-
-    IDProperty *property = IDP_GetPropertyFromGroup_null(properties, socket->identifier);
-    if (property == nullptr) {
-      if (!ELEM(type, SOCK_GEOMETRY, SOCK_MATRIX, SOCK_BUNDLE, SOCK_CLOSURE)) {
-        BKE_modifier_set_error(
-            ob, md, "Missing property for input socket \"%s\"", socket->name ? socket->name : "");
-      }
-      continue;
-    }
-
-    if (!nodes::id_property_type_matches_socket(*socket, *property)) {
-      BKE_modifier_set_error(ob,
-                             md,
-                             "Property type does not match input socket \"(%s)\"",
-                             socket->name ? socket->name : "");
-      continue;
-    }
-  }
-
-  if (geometry_socket_count == 1) {
-    const bNodeTreeInterfaceSocket *first_socket = nmd->node_group->interface_inputs()[0];
-    const bke::bNodeSocketType *typeinfo = first_socket->socket_typeinfo();
-    const eNodeSocketDatatype type = typeinfo ? typeinfo->type : SOCK_CUSTOM;
-    if (type != SOCK_GEOMETRY) {
-      BKE_modifier_set_error(ob, md, "Node group's geometry input must be the first");
     }
   }
 }
@@ -1835,7 +1780,6 @@ static void modifyGeometry(ModifierData *md,
   }
 
   const bNodeTree &tree = *nmd->node_group;
-  check_property_socket_sync(ctx->object, nmd->settings.properties, md);
 
   tree.ensure_topology_cache();
   const bNode *output_node = tree.group_output_node();
@@ -1904,11 +1848,11 @@ static void modifyGeometry(ModifierData *md,
   bke::DataBlockComputeContext data_block_compute_context{nullptr, ctx->object->id};
   bke::ModifierComputeContext modifier_compute_context{&data_block_compute_context, *nmd};
 
-  geometry_set = nodes::execute_geometry_nodes_on_geometry(tree,
-                                                           nmd->settings.properties,
-                                                           modifier_compute_context,
-                                                           call_data,
-                                                           std::move(geometry_set));
+  PointerRNA md_ptr = RNA_pointer_create_discrete(&ctx->object->id, RNA_NodesModifier, md);
+  PointerRNA properties_ptr = RNA_pointer_get(&md_ptr, "properties");
+
+  geometry_set = nodes::execute_geometry_nodes_on_geometry(
+      tree, properties_ptr, modifier_compute_context, call_data, std::move(geometry_set));
 
   if (logging_enabled(ctx)) {
     nmd_orig->runtime->eval_log = std::move(eval_log);
@@ -1964,7 +1908,7 @@ static void modify_geometry_set(ModifierData *md,
   modifyGeometry(md, ctx, *geometry_set);
 }
 
-void NodesModifierUsageInferenceCache::ensure(const NodesModifierData &nmd)
+void NodesModifierUsageInferenceCache::ensure(const Object &object, const NodesModifierData &nmd)
 {
   if (!nmd.node_group || ID_MISSING(nmd.node_group)) {
     this->reset();
@@ -1973,9 +1917,14 @@ void NodesModifierUsageInferenceCache::ensure(const NodesModifierData &nmd)
   const bNodeTree &tree = *nmd.node_group;
   tree.ensure_interface_cache();
   tree.ensure_topology_cache();
+
+  PointerRNA nmd_ptr = RNA_pointer_create_discrete(
+      const_cast<ID *>(&object.id), RNA_NodesModifier, const_cast<NodesModifierData *>(&nmd));
+  PointerRNA properties_ptr = RNA_pointer_get(&nmd_ptr, "properties");
+
   ResourceScope scope;
   const Vector<nodes::InferenceValue> group_input_values =
-      nodes::get_geometry_nodes_input_inference_values(tree, nmd.settings.properties, scope);
+      nodes::get_geometry_nodes_input_inference_values(tree, properties_ptr, scope);
 
   /* Compute the hash of the input values. This has to be done every time currently, because there
    * is no reliable callback yet that is called any of the modifier properties changes. */
@@ -2037,24 +1986,9 @@ static void blend_write(BlendWriter *writer, const ID * /*id_owner*/, const Modi
 
   writer->write_string(nmd->bake_directory);
 
-  Map<IDProperty *, IDPropertyUIDataBool *> boolean_props;
   if (nmd->settings.properties != nullptr) {
-    if (!BLO_write_is_undo(writer)) {
-      /* Boolean properties are added automatically for boolean node group inputs. Integer
-       * properties are automatically converted to boolean sockets where applicable as well.
-       * However, boolean properties will crash old versions of Blender, so convert them to integer
-       * properties for writing. The actual value is stored in the same variable for both types */
-      for (IDProperty &prop : nmd->settings.properties->data.group) {
-        if (prop.type == IDP_BOOLEAN) {
-          boolean_props.add_new(&prop, reinterpret_cast<IDPropertyUIDataBool *>(prop.ui_data));
-          prop.type = IDP_INT;
-          prop.ui_data = nullptr;
-        }
-      }
-    }
-
-    /* Note that the property settings are based on the socket type info
-     * and don't necessarily need to be written, but we can't just free them. */
+    /* Write legacy settings for forward compatibility. Created by
+     * #create_legacy_geometry_nodes_properties. */
     IDP_BlendWrite(writer, nmd->settings.properties);
   }
 
@@ -2090,28 +2024,14 @@ static void blend_write(BlendWriter *writer, const ID * /*id_owner*/, const Modi
     }
   }
   writer->write_struct_array(nmd->panels_num, nmd->panels);
-
-  if (nmd->settings.properties) {
-    if (!BLO_write_is_undo(writer)) {
-      for (IDProperty &prop : nmd->settings.properties->data.group) {
-        if (prop.type == IDP_INT) {
-          if (IDPropertyUIDataBool **ui_data = boolean_props.lookup_ptr(&prop)) {
-            prop.type = IDP_BOOLEAN;
-            if (ui_data) {
-              prop.ui_data = reinterpret_cast<IDPropertyUIData *>(*ui_data);
-            }
-          }
-        }
-      }
-    }
-  }
 }
 
 static void blend_read(BlendDataReader *reader, ModifierData *md)
 {
   NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
   BLO_read_string(reader, &nmd->bake_directory);
-  if (nmd->node_group == nullptr) {
+  if (nmd->node_group == nullptr || nmd->modifier.system_properties) {
+    /* Don't bother reading old settings when modifier system properties are available. */
     nmd->settings.properties = nullptr;
   }
   else {
@@ -2234,10 +2154,6 @@ static void copy_data(const ModifierData *md, ModifierData *target, const int fl
     /* Clear the bake path when duplicating. */
     tnmd->bake_directory = nullptr;
   }
-
-  if (nmd->settings.properties != nullptr) {
-    tnmd->settings.properties = IDP_CopyProperty_ex(nmd->settings.properties, flag);
-  }
 }
 
 void nodes_modifier_packed_bake_free(NodesModifierPackedBake *packed_bake)
@@ -2274,11 +2190,6 @@ void nodes_modifier_bake_destruct(NodesModifierBake *bake, const bool do_id_user
 static void free_data(ModifierData *md)
 {
   NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-  if (nmd->settings.properties != nullptr) {
-    IDP_FreeProperty_ex(nmd->settings.properties, false);
-    nmd->settings.properties = nullptr;
-  }
-
   for (NodesModifierBake &bake : MutableSpan(nmd->bakes, nmd->bakes_num)) {
     nodes_modifier_bake_destruct(&bake, false);
   }

source/blender/blenkernel/BKE_idprop.hh
@@ -9,6 +9,7 @@
  */
 
 #include <memory>
+#include <optional>
 
 #include "DNA_ID.h"
 #include "DNA_ID_enums.h"
@@ -375,6 +376,14 @@ const IDProperty *_IDP_assert_type_mask(const IDProperty *prop, int ty_mask);
 /* No `IDP_string_set` needed. */
 #define IDP_ID_get(prop) ((void)0, ((ID *)_IDP_assert_type(prop, IDP_ID)->data.pointer))
 /* No `IDP_ID_set` needed. */
+
+std::optional<StringRefNull> IDP_group_lookup_string(const IDProperty &group, StringRef name);
+std::optional<float> IDP_group_lookup_float(const IDProperty &group, StringRef name);
+std::optional<int> IDP_group_lookup_int(const IDProperty &group, StringRef name);
+std::optional<bool> IDP_group_lookup_bool(const IDProperty &group, StringRef name);
+std::optional<Span<float>> IDP_group_lookup_float_array(const IDProperty &group,
+                                                        StringRef name,
+                                                        int required_size);
 
 /**
  * Return an int from an #IDProperty with a compatible type. This should be avoided, but

source/blender/blenkernel/BKE_idprop_hash.hh
@@ -1,0 +1,26 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/** \file
+ * \ingroup bke
+ */
+
+#include <xxhash.h>
+
+namespace blender {
+
+struct IDProperty;
+
+namespace bke::idprop {
+
+/**
+ * Hash the value of the given IDProperty recursively. This only hashes the actual values, not e.g.
+ * the ui data.
+ */
+void hash(const IDProperty &base_prop, XXH3_state_t *hash_state);
+
+}  // namespace bke::idprop
+}  // namespace blender

source/blender/blenkernel/BKE_node.hh
@@ -58,6 +58,7 @@ struct bNodeTreeExec;
 
 class CPPType;
 namespace nodes {
+struct GeneratedTreeSrnaData;
 class NodeMultiFunctionBuilder;
 class GeoNodeExecParams;
 class NodeDeclaration;
@@ -149,6 +150,12 @@ using NodeBlendWriteFunction = void (*)(const bNodeTree &tree,
                                         BlendWriter &writer);
 using NodeBlendDataReadFunction = void (*)(bNodeTree &tree, bNode &node, BlendDataReader &reader);
 
+using SocketMakeGeometryNodesInputSrnaFunction =
+    void (*)(const bNodeTree &tree,
+             StructRNA &srna,
+             const bNodeTreeInterfaceSocket &io_socket,
+             nodes::GeneratedTreeSrnaData &r_generated);
+
 /**
  * \brief Defines a socket type.
  *
@@ -208,6 +215,8 @@ struct bNodeSocketType {
   SocketGetGeometryNodesCPPValueFunction get_geometry_nodes_cpp_value = nullptr;
   /* Default value for this socket type. */
   const SocketValueVariant *geometry_nodes_default_value = nullptr;
+
+  SocketMakeGeometryNodesInputSrnaFunction make_geometry_nodes_input_srna = nullptr;
 };
 
 using NodeInitExecFunction = void *(*)(bNodeExecContext * context,
@@ -928,6 +937,7 @@ void node_tree_free_local_node(bNodeTree &ntree, bNode &node);
 void node_tree_update_all_new(Main &main);
 
 /** Update asset meta-data cache of data-block properties. */
+IDProperty *node_create_asset_meta_data_properties(const bNodeTree &node_tree);
 void node_update_asset_metadata(bNodeTree &node_tree);
 
 void node_tree_node_flag_set(bNodeTree &ntree, int flag, bool enable);

source/blender/blenkernel/BKE_node_runtime.hh
@@ -36,6 +36,7 @@ struct bNodeTree;
 namespace nodes {
 struct FieldInferencingInterface;
 struct EvalDependencies;
+struct GeneratedTreeSrnaData;
 class NodeDeclaration;
 struct GeometryNodesLazyFunctionGraphInfo;
 struct StructureTypeInterface;
@@ -159,6 +160,9 @@ class bNodeTreeRuntime : NonCopyable, NonMovable {
    * tree execution instructions and its evaluation.
    */
   bNodeTreeExec *execdata = nullptr;
+
+  /** Contains RNA types generated for the geometry nodes modifier interface. */
+  std::shared_ptr<nodes::GeneratedTreeSrnaData> geometry_nodes_srna_data;
 
   /** Information about how inputs and outputs of the node group interact with fields. */
   std::unique_ptr<nodes::FieldInferencingInterface> field_inferencing_interface;

source/blender/blenkernel/CMakeLists.txt
@@ -417,6 +417,7 @@ set(SRC
   BKE_icons.hh
   BKE_id_hash.hh
   BKE_idprop.hh
+  BKE_idprop_hash.hh
   BKE_idtype.hh
   BKE_image.hh
   BKE_image_format.hh

source/blender/blenkernel/intern/idprop.cc
@@ -13,6 +13,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <xxhash.h>
+
 #include <fmt/format.h>
 
 #include "BLI_listbase.h"
@@ -22,6 +24,7 @@
 #include "BLI_utildefines.h"
 
 #include "BKE_idprop.hh"
+#include "BKE_idprop_hash.hh"
 #include "BKE_lib_id.hh"
 
 #include "CLG_log.h"
@@ -791,6 +794,56 @@ static void IDP_FreeGroup(IDProperty *prop, const bool do_id_user)
     IDP_FreePropertyContent_ex(&loop, do_id_user);
   }
   BLI_freelistN(&prop->data.group);
+}
+
+std::optional<StringRefNull> IDP_group_lookup_string(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_STRING) {
+    return std::nullopt;
+  }
+  return IDP_string_get(prop);
+}
+
+std::optional<float> IDP_group_lookup_float(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_FLOAT) {
+    return std::nullopt;
+  }
+  return IDP_float_get(prop);
+}
+
+std::optional<int> IDP_group_lookup_int(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_INT) {
+    return std::nullopt;
+  }
+  return IDP_int_get(prop);
+}
+
+std::optional<bool> IDP_group_lookup_bool(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_BOOLEAN) {
+    return std::nullopt;
+  }
+  return IDP_bool_get(prop);
+}
+
+std::optional<Span<float>> IDP_group_lookup_float_array(const IDProperty &group,
+                                                        StringRef name,
+                                                        int required_size)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_FLOAT) {
+    return std::nullopt;
+  }
+  if (prop->len != required_size) {
+    return std::nullopt;
+  }
+  return Span(IDP_array_float_get(prop), prop->len);
 }
 
 /** \} */
@@ -1984,6 +2037,59 @@ IDPropertyUIData *IDP_TryConvertUIData(IDPropertyUIData *src,
   ui_data_free(src, src_type);
   return nullptr;
 }
+
+namespace bke::idprop {
+
+void hash(const IDProperty &base_prop, XXH3_state_t *hash_state)
+{
+  IDP_foreach_property(&const_cast<IDProperty &>(base_prop), 0, [&](const IDProperty *prop) {
+    XXH3_64bits_update(hash_state, prop->name, strlen(prop->name));
+    XXH3_64bits_update(hash_state, &prop->type, sizeof(prop->type));
+
+    switch (eIDPropertyType(prop->type)) {
+      case IDP_INT:
+      case IDP_FLOAT:
+      case IDP_BOOLEAN: {
+        XXH3_64bits_update(hash_state, &prop->data.val, sizeof(prop->data.val));
+        break;
+      }
+      case IDP_DOUBLE: {
+        const double val = IDP_double_get(prop);
+        XXH3_64bits_update(hash_state, &val, sizeof(double));
+        break;
+      }
+      case IDP_STRING: {
+        if (prop->data.pointer) {
+          XXH3_64bits_update(hash_state, prop->data.pointer, size_t(prop->len));
+        }
+        break;
+      }
+      case IDP_ARRAY: {
+        if (prop->data.pointer) {
+          XXH3_64bits_update(hash_state, &prop->subtype, sizeof(prop->subtype));
+          const size_t elem_size = idp_size_table[int(prop->subtype)];
+          XXH3_64bits_update(hash_state, prop->data.pointer, elem_size * size_t(prop->len));
+        }
+        break;
+      }
+      case IDP_ID: {
+        /* NOTE: Hashing the session_uid of the ID makes the hash session-specific. An alternative
+         * hash wouldn't be complete though, because data-block names aren't necessarily unique. */
+        if (const ID *id = static_cast<const ID *>(prop->data.pointer)) {
+          XXH3_64bits_update(hash_state, &id->session_uid, sizeof(ID::session_uid));
+        }
+        break;
+      }
+      case IDP_GROUP:
+      case IDP_IDPARRAY: {
+        /* Handled by the recursion in #IDP_foreach_property. */
+        break;
+      }
+    }
+  });
+}
+
+}  // namespace bke::idprop
 
 /** \} */
 

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -50,6 +50,8 @@
 #include "BKE_node_runtime.hh"
 #include "BKE_node_tree_update.hh"
 
+#include "NOD_geometry_nodes_srna.hh"
+
 #include "BLT_translation.hh"
 
 namespace blender {
@@ -2265,15 +2267,22 @@ static ModifierData *create_auto_smooth_modifier(
   md->node_group = get_node_group(object.id.lib);
   id_us_plus(&md->node_group->id);
 
-  md->settings.properties = idprop::create_group("Nodes Modifier Settings").release();
-  IDProperty *angle_prop = idprop::create("Socket_2", angle).release();
-  auto *ui_data = reinterpret_cast<IDPropertyUIDataFloat *>(IDP_ui_data_ensure(angle_prop));
-  ui_data->base.rna_subtype = PROP_ANGLE;
-  ui_data->soft_min = 0.0f;
-  ui_data->soft_max = DEG2RADF(180.0f);
-  IDP_AddToGroup(md->settings.properties, angle_prop);
-  IDP_AddToGroup(md->settings.properties, idprop::create("Socket_2_use_attribute", 0).release());
-  IDP_AddToGroup(md->settings.properties, idprop::create("Socket_2_attribute_name", "").release());
+  IDProperty *system_props = bke::idprop::create_group("NodesModifierProperties").release();
+
+  IDProperty *inputs = bke::idprop::create_group("inputs").release();
+  IDP_AddToGroup(system_props, inputs);
+
+  IDProperty *angle_prop_group = bke::idprop::create_group("Socket_2").release();
+  IDP_AddToGroup(inputs, angle_prop_group);
+
+  IDProperty *type_prop =
+      idprop::create("type", int(nodes::GeometryNodesInputType::Value)).release();
+  IDP_AddToGroup(angle_prop_group, type_prop);
+
+  IDProperty *angle_prop = idprop::create("value", angle).release();
+  IDP_AddToGroup(angle_prop_group, angle_prop);
+
+  md->modifier.system_properties = system_props;
 
   BKE_modifiers_persistent_uid_init(object, md->modifier);
   return &md->modifier;

FrenkyBojler/blender-ios

commit 4a7a5daff303b2e58a62768e9ecf80d54c0a2bcf

Fix #156748: Geometry Nodes modifiers lose pointer property on save/reload
Commit 1561c1ea4a introduced a double-walk bug during `lib_link`.
`md.system_properties` was being walked both in `BKE_modifiers_foreach_ID_link` (`modifier.cc`) and in `foreach_ID_link` (`MOD_nodes.cc`).
Walking an ID pointer twice causes the second pass to look up an already-resolved memory address, failing the lookup and nullifying the pointer.

This removes the redundant walk in `MOD_nodes.cc`.

source/blender/modifiers/intern/MOD_nodes.cc
@@ -276,11 +276,6 @@ static void foreach_ID_link(ModifierData *md, Object *ob, IDWalkFunc walk, void 
     });
   }
 
-  IDP_foreach_property(
-      nmd->modifier.system_properties, IDP_TYPE_FILTER_ID, [&](IDProperty *id_prop) {
-        walk(user_data, ob, reinterpret_cast<ID **>(&id_prop->data.pointer), IDWALK_CB_USER);
-      });
-
   for (NodesModifierBake &bake : MutableSpan(nmd->bakes, nmd->bakes_num)) {
     for (NodesModifierDataBlock &data_block : MutableSpan(bake.data_blocks, bake.data_blocks_num))
     {