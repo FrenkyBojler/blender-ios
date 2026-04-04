blender/blender

commit 1724e92dd777a05a84d25796a2708925d625bb29

Geometry Nodes: Optimize material index handling
- Support joining single values in the realize instances
- Sometimes eagerly calculate max material index in realize instances
- Store single value attribute in set material node with full selection
- Eagerly set max index cache in set material for last material

Pull Request: https://projects.blender.org/blender/blender/pulls/156233

source/blender/geometry/intern/realize_instances.cc
@@ -1673,7 +1673,6 @@ static void execute_realize_mesh_task(const RealizeInstancesOptions &options,
                                       MutableSpan<int> all_dst_corner_verts,
                                       MutableSpan<int> all_dst_corner_edges,
                                       MutableSpan<int> all_dst_vert_ids,
-                                      MutableSpan<int> all_dst_material_indices,
                                       GSpanAttributeWriter &all_dst_custom_normals,
                                       MutableSpan<int> all_dst_origindex_vert,
                                       MutableSpan<int> all_dst_origindex_edge,
@@ -1723,31 +1722,6 @@ static void execute_realize_mesh_task(const RealizeInstancesOptions &options,
       dst_face_offsets[i] = src_faces[i].start() + task.start_indices.corner;
     }
   });
-  if (!all_dst_material_indices.is_empty()) {
-    const Span<int> material_index_map = mesh_info.material_index_map;
-    MutableSpan<int> dst_material_indices = all_dst_material_indices.slice(dst_face_range);
-    if (mesh.totcol == 0) {
-      /* The material index map contains the index of the null material in the result. */
-      dst_material_indices.fill(material_index_map.first());
-    }
-    else {
-      if (mesh_info.material_indices.is_single()) {
-        const int src_index = mesh_info.material_indices.get_internal_single();
-        const bool valid = IndexRange(mesh.totcol).contains(src_index);
-        dst_material_indices.fill(valid ? material_index_map[src_index] : 0);
-      }
-      else {
-        VArraySpan<int> indices_span(mesh_info.material_indices);
-        threading::parallel_for(src_faces.index_range(), 1024, [&](const IndexRange face_range) {
-          for (const int i : face_range) {
-            const int src_index = indices_span[i];
-            const bool valid = IndexRange(mesh.totcol).contains(src_index);
-            dst_material_indices[i] = valid ? material_index_map[src_index] : 0;
-          }
-        });
-      }
-    }
-  }
 
   if (!all_dst_vert_ids.is_empty()) {
     create_result_ids(options,
@@ -1874,6 +1848,129 @@ static void copy_vertex_group_names(Mesh &dst_mesh,
   }
 }
 
+static int get_mapped_material_index(const MeshRealizeInfo &info, const int index)
+{
+  const bool valid = IndexRange(info.mesh->totcol).contains(index);
+  return valid ? info.material_index_map[index] : 0;
+}
+
+/**
+ * If the max material index can be easily fetched from the inputs from the inputs without much
+ * work, return the common maximum.
+ */
+static std::optional<int> calc_material_index_max_hint(const Span<MeshRealizeInfo> infos)
+{
+  int final_max = std::numeric_limits<int>::min();
+  for (const MeshRealizeInfo &info : infos) {
+    if (info.mesh->runtime->max_material_index.is_cached()) {
+      if (const std::optional<int> max = info.mesh->runtime->max_material_index.data()) {
+        final_max = std::max(final_max, get_mapped_material_index(info, *max));
+      }
+      continue;
+    }
+    if (const std::optional<int> single = info.material_indices.get_if_single()) {
+      final_max = std::max(final_max, get_mapped_material_index(info, *single));
+      continue;
+    }
+    return std::nullopt;
+  }
+  return final_max;
+}
+
+static std::optional<int> calc_single_value_material_index(const AllMeshesInfo &all_meshes_info)
+{
+  const Span<MeshRealizeInfo> infos = all_meshes_info.realize_info;
+  const std::optional<int> first_opt = infos[0].material_indices.get_if_single();
+  if (!first_opt) {
+    return std::nullopt;
+  }
+  const int first = get_mapped_material_index(infos[0], *first_opt);
+  const bool all_equal = threading::parallel_reduce(
+      infos.index_range().drop_front(1),
+      32,
+      true,
+      [&](const IndexRange range, bool value) {
+        if (!value) {
+          return false;
+        }
+        for (const int i : range) {
+          const MeshRealizeInfo &info = infos[i];
+          const std::optional<int> value = info.material_indices.get_if_single();
+          if (!value) {
+            return false;
+          }
+          if (get_mapped_material_index(info, *value) != first) {
+            return false;
+          }
+        }
+        return true;
+      },
+      std::logical_and<bool>());
+  if (!all_equal) {
+    return std::nullopt;
+  }
+  return first;
+}
+
+static void join_mesh_material_indices(const AllMeshesInfo &all_meshes_info,
+                                       const Span<RealizeMeshTask> tasks,
+                                       Mesh &dst_mesh)
+{
+  if (all_meshes_info.materials.size() <= 1) {
+    dst_mesh.runtime->max_material_index.ensure([&](std::optional<int> &data) { data = 0; });
+    return;
+  }
+  if (const std::optional<int> max = calc_material_index_max_hint(all_meshes_info.realize_info)) {
+    dst_mesh.runtime->max_material_index.ensure([&](std::optional<int> &data) { data = max; });
+    if (max == 0) {
+      return;
+    }
+  }
+
+  bke::MutableAttributeAccessor dst_attributes = dst_mesh.attributes_for_write();
+  if (const std::optional<int> single = calc_single_value_material_index(all_meshes_info)) {
+    dst_attributes.add<int>(
+        "material_index", bke::AttrDomain::Face, bke::AttributeInitValue(*single));
+    return;
+  }
+
+  bke::SpanAttributeWriter dst_attr = dst_attributes.lookup_or_add_for_write_only_span<int>(
+      "material_index", bke::AttrDomain::Face);
+
+  threading::parallel_for(tasks.index_range(), 100, [&](const IndexRange range) {
+    for (const int task_i : range) {
+      const RealizeMeshTask &task = tasks[task_i];
+      const MeshRealizeInfo &mesh_info = *task.mesh_info;
+      const Mesh &mesh = *mesh_info.mesh;
+      const IndexRange dst_face_range(task.start_indices.face, mesh.faces_num);
+      MutableSpan<int> dst_material_indices = dst_attr.span.slice(dst_face_range);
+      if (mesh.totcol == 0) {
+        /* The material index map contains the index of the null material in the result. */
+        dst_material_indices.fill(get_mapped_material_index(mesh_info, 0));
+      }
+      else {
+        if (const std::optional<int> src_index = mesh_info.material_indices.get_if_single()) {
+          dst_material_indices.fill(get_mapped_material_index(mesh_info, *src_index));
+        }
+        else {
+          const VArraySpan<int> src_span(mesh_info.material_indices);
+          const Span<int> map = mesh_info.material_index_map;
+          const int src_mat_num = mesh.totcol;
+          threading::parallel_for(src_span.index_range(), 1024, [&](const IndexRange face_range) {
+            for (const int i : face_range) {
+              const int src_index = src_span[i];
+              const bool valid = IndexRange(src_mat_num).contains(src_index);
+              dst_material_indices[i] = valid ? map[src_index] : 0;
+            }
+          });
+        }
+      }
+    }
+  });
+
+  dst_attr.finish();
+}
+
 static void execute_realize_mesh_tasks(const RealizeInstancesOptions &options,
                                        const GatherOffsets &offsets,
                                        const AllMeshesInfo &all_meshes_info,
@@ -1962,12 +2059,6 @@ static void execute_realize_mesh_tasks(const RealizeInstancesOptions &options,
   if (all_meshes_info.create_id_attribute) {
     vert_ids = dst_attributes.lookup_or_add_for_write_only_span<int>("id", bke::AttrDomain::Point);
   }
-  /* Prepare material indices. */
-  SpanAttributeWriter<int> material_indices;
-  if (all_meshes_info.create_material_index_attribute) {
-    material_indices = dst_attributes.lookup_or_add_for_write_only_span<int>(
-        "material_index", bke::AttrDomain::Face);
-  }
 
   GSpanAttributeWriter custom_normals;
   switch (all_meshes_info.custom_normal_info.result_type) {
@@ -2024,7 +2115,6 @@ static void execute_realize_mesh_tasks(const RealizeInstancesOptions &options,
                                 dst_corner_verts,
                                 dst_corner_edges,
                                 vert_ids.span,
-                                material_indices.span,
                                 custom_normals,
                                 dst_origindex_vert,
                                 dst_origindex_edge,
@@ -2032,12 +2122,13 @@ static void execute_realize_mesh_tasks(const RealizeInstancesOptions &options,
     }
   });
 
+  join_mesh_material_indices(all_meshes_info, tasks, *dst_mesh);
+
   /* Tag modified attributes. */
   for (GSpanAttributeWriter &dst_attribute : dst_attribute_writers) {
     dst_attribute.finish();
   }
   vert_ids.finish();
-  material_indices.finish();
   custom_normals.finish();
 
   if (all_meshes_info.no_loose_edges_hint) {

source/blender/blenkernel/intern/mesh.cc
@@ -1915,9 +1915,7 @@ std::optional<int> Mesh::material_index_max() const
       return;
     }
     value = bounds::max<int>(
-        this->attributes()
-            .lookup_or_default<int>("material_index", bke::AttrDomain::Face, 0)
-            .varray);
+        *this->attributes().lookup_or_default<int>("material_index", bke::AttrDomain::Face, 0));
     if (value.has_value()) {
       value = std::clamp(*value, 0, MAXMAT);
     }
@@ -1948,10 +1946,8 @@ const VectorSet<int> &Mesh::material_indices_used() const
         used_indices[clamp_material_index(efa->mat_nr)] = true;
       }
     }
-    else if (const VArray<int> material_indices =
-                 this->attributes()
-                     .lookup_or_default<int>("material_index", bke::AttrDomain::Face, 0)
-                     .varray)
+    else if (const VArray<int> material_indices = *this->attributes().lookup_or_default<int>(
+                 "material_index", bke::AttrDomain::Face, 0))
     {
       if (const std::optional<int> single_material_index = material_indices.get_if_single()) {
         used_indices[clamp_material_index(*single_material_index)] = true;
@@ -1959,7 +1955,7 @@ const VectorSet<int> &Mesh::material_indices_used() const
       else {
         VArraySpan<int> material_indices_span = material_indices;
         threading::parallel_for(
-            material_indices_span.index_range(), 1024, [&](const IndexRange range) {
+            material_indices_span.index_range(), 8096, [&](const IndexRange range) {
               for (const int i : range) {
                 used_indices[clamp_material_index(material_indices_span[i])] = true;
               }

source/blender/editors/mesh/mesh_join.cc
@@ -436,7 +436,7 @@ static VectorSet<Material *> join_materials(const Span<const Object *> objects_t
 
   bke::MutableAttributeAccessor dst_attributes = dst_mesh.attributes_for_write();
   if (materials.size() <= 1) {
-    dst_attributes.remove("material_index");
+    BLI_assert(!dst_attributes.contains("material_index"));
     return materials;
   }
 

source/blender/nodes/geometry/nodes/node_geo_set_material.cc
@@ -12,6 +12,7 @@
 #include "BKE_curves.hh"
 #include "BKE_grease_pencil.hh"
 #include "BKE_material.hh"
+#include "BKE_mesh_types.hh"
 
 #include "GEO_foreach_geometry.hh"
 
@@ -40,7 +41,8 @@ static void assign_material_to_id_geometry(ID *id,
                                            const Field<bool> &selection_field,
                                            MutableAttributeAccessor &attributes,
                                            const AttrDomain domain,
-                                           Material *material)
+                                           Material *material,
+                                           SharedCache<std::optional<int>> &max_index_cache)
 {
   const int domain_size = attributes.domain_size(domain);
   fn::FieldEvaluator selection_evaluator{field_context, domain_size};
@@ -48,7 +50,7 @@ static void assign_material_to_id_geometry(ID *id,
   selection_evaluator.evaluate();
   const IndexMask selection = selection_evaluator.get_evaluated_selection_as_mask();
 
-  if (selection.size() != attributes.domain_size(domain)) {
+  if (selection.size() != domain_size) {
     /* If the entire geometry isn't selected, and there is no material slot yet, add an empty
      * slot so that the faces that aren't selected can still refer to the default material. */
     BKE_id_material_eval_ensure_default_slot(id);
@@ -66,10 +68,20 @@ static void assign_material_to_id_geometry(ID *id,
     BKE_id_material_eval_assign(id, new_index + 1, material);
   }
 
-  SpanAttributeWriter<int> indices = attributes.lookup_or_add_for_write_span<int>("material_index",
-                                                                                  domain);
-  index_mask::masked_fill(indices.span, new_index, selection);
-  indices.finish();
+  if (selection.size() == domain_size) {
+    attributes.remove("material_index");
+    attributes.add<int>("material_index", domain, bke::AttributeInitValue(new_index));
+    max_index_cache.ensure([&](std::optional<int> &data) { data = new_index; });
+  }
+  else {
+    SpanAttributeWriter<int> indices = attributes.lookup_or_add_for_write_span<int>(
+        "material_index", domain);
+    index_mask::masked_fill(indices.span, new_index, selection);
+    indices.finish();
+    if (new_index == orig_materials_num) {
+      max_index_cache.ensure([&](std::optional<int> &data) { data = new_index; });
+    }
+  }
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -95,8 +107,13 @@ static void node_geo_exec(GeoNodeExecParams params)
       else {
         const bke::MeshFieldContext field_context{*mesh, AttrDomain::Face};
         MutableAttributeAccessor attributes = mesh->attributes_for_write();
-        assign_material_to_id_geometry(
-            &mesh->id, field_context, selection_field, attributes, AttrDomain::Face, material);
+        assign_material_to_id_geometry(&mesh->id,
+                                       field_context,
+                                       selection_field,
+                                       attributes,
+                                       AttrDomain::Face,
+                                       material,
+                                       mesh->runtime->max_material_index);
       }
     }
     if (Volume *volume = geometry_set.get_volume_for_write()) {
@@ -137,7 +154,8 @@ static void node_geo_exec(GeoNodeExecParams params)
                                        selection_field,
                                        attributes,
                                        AttrDomain::Curve,
-                                       material);
+                                       material,
+                                       curves.runtime->max_material_index_cache);
       }
     }
   });

FrenkyBojler/blender-ios

commit a411235e5928158dab962907a40841355c50bfb5

Fix: Geometry nodes realize instances material index mapping
Meshes with no material slots were incorrectly assigned to material
index 0 (the first real material) instead of their actual position in
the combined materials list.

This issue was introduced in 1724e92dd77.

source/blender/geometry/intern/realize_instances.cc
@@ -1850,8 +1850,11 @@ static void copy_vertex_group_names(Mesh &dst_mesh,
 
 static int get_mapped_material_index(const MeshRealizeInfo &info, const int index)
 {
+  if (info.mesh->totcol == 0) {
+    return info.material_index_map.first();
+  }
   const bool valid = IndexRange(info.mesh->totcol).contains(index);
-  return valid ? info.material_index_map[index] : 0;
+  return valid ? info.material_index_map[index] : info.material_index_map.first();
 }
 
 /**