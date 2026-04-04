blender/blender

commit 40672ae4add563017322206548eddb0b42242de6

GPU: Shader: Add overlapping resource check
This is needed to duplicate this check because
BSL allows to discard resources based on compilation
constants. The check needs to apply after the
conditional removal of these resources.

source/blender/gpu/intern/gpu_shader_create_info.cc
@@ -431,6 +431,74 @@ std::string ShaderCreateInfo::check_error() const
     }
   }
 
+  /* Check same bind-points usage. */
+  Set<int> images, samplers, ubos, ssbos;
+
+  auto register_resource = [&](const Resource &res) -> bool {
+    switch (res.bind_type) {
+      case Resource::BindType::UNIFORM_BUFFER:
+        return images.add(res.slot);
+      case Resource::BindType::STORAGE_BUFFER:
+        return samplers.add(res.slot);
+      case Resource::BindType::SAMPLER:
+        return ubos.add(res.slot);
+      case Resource::BindType::IMAGE:
+        return ssbos.add(res.slot);
+      default:
+        return false;
+    }
+  };
+
+  auto print_error_msg = [&](const Resource &res, const Vector<Resource> &resources) {
+    auto print_resource_name = [&](const Resource &res) {
+      switch (res.bind_type) {
+        case Resource::BindType::UNIFORM_BUFFER:
+          error += "Uniform Buffer " + res.uniformbuf.name;
+          break;
+        case Resource::BindType::STORAGE_BUFFER:
+          error += "Storage Buffer " + res.storagebuf.name;
+          break;
+        case Resource::BindType::SAMPLER:
+          error += "Sampler " + res.sampler.name;
+          break;
+        case Resource::BindType::IMAGE:
+          error += "Image " + res.image.name;
+          break;
+        default:
+          error += "Unknown Type";
+          break;
+      }
+    };
+
+    for (const Resource &_res : resources) {
+      if (&res != &_res && res.bind_type == _res.bind_type && res.slot == _res.slot) {
+        error += name_ + ": Validation failed : Overlapping ";
+        print_resource_name(res);
+        error += " and ";
+        print_resource_name(_res);
+        error += " at binding location " + std::to_string(res.slot) + "\n";
+      }
+    }
+  };
+
+  for (const auto &res : batch_resources_) {
+    if (register_resource(res) == false) {
+      print_error_msg(res, resources_get_all_());
+    }
+  }
+
+  for (const auto &res : pass_resources_) {
+    if (register_resource(res) == false) {
+      print_error_msg(res, resources_get_all_());
+    }
+  }
+
+  for (const auto &res : geometry_resources_) {
+    if (register_resource(res) == false) {
+      print_error_msg(res, resources_get_all_());
+    }
+  }
+
   return error;
 }
 

source/blender/gpu/intern/gpu_shader.cc
@@ -818,7 +818,7 @@ Shader *ShaderCompiler::compile(const shader::ShaderCreateInfo &orig_info, bool 
   const std::string error = specialized_info.check_error();
   if (!error.empty()) {
     std::cerr << error << "\n";
-    BLI_assert(false);
+    return nullptr;
   }
 
   const shader::ShaderCreateInfo &info = shader->patch_create_info(specialized_info);

FrenkyBojler/blender-ios

commit 6f926acab22dff99e24386ec9dd7baae20bd30dc

Fix: GPU shader resource check had mismatched variable names
The register_resource lambda was using the wrong sets for each
resource type (e.g. adding uniform buffers to the images set).

This issue was introduced in 40672ae4add.

source/blender/gpu/intern/gpu_shader_create_info.cc
@@ -437,13 +437,13 @@ std::string ShaderCreateInfo::check_error() const
   auto register_resource = [&](const Resource &res) -> bool {
     switch (res.bind_type) {
       case Resource::BindType::UNIFORM_BUFFER:
-        return images.add(res.slot);
-      case Resource::BindType::STORAGE_BUFFER:
-        return samplers.add(res.slot);
-      case Resource::BindType::SAMPLER:
         return ubos.add(res.slot);
-      case Resource::BindType::IMAGE:
+      case Resource::BindType::STORAGE_BUFFER:
         return ssbos.add(res.slot);
+      case Resource::BindType::SAMPLER:
+        return samplers.add(res.slot);
+      case Resource::BindType::IMAGE:
+        return images.add(res.slot);
       default:
         return false;
     }