d39cc30be658c63db6ba1c534d66bf0311aa66d3
илья _ <modormoder@gmail.com>
2026-04-05 22:03:24 +0300

    fix build

--- source/blender/functions/intern/field_recognize.cc
+++ source/blender/functions/intern/field_recognize.cc
@@ -2,6 +2,8 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include <variant>
+
 #include "BLI_map.hh"
 #include "BLI_stack.hh"
 
@@ -13,14 +15,18 @@ namespace blender::fn {
 
 std::optional<Polynom<int>> field_as_polynom_try(const Field<int> &entry_field)
 {
-  const std::shared_ptr<const fn::FieldInputs> &dependencys = entry_field.node().field_inputs();
+  const fn::FieldInputsPtr &dependencys = GField(entry_field).field_inputs();
   if (!dependencys) {
     /* TODO: Extract constant. */
     return std::nullopt;
   }
-  if (dependencys->deduplicated_nodes.size() != 1) {
+  if (dependencys->inputs.size() != 1) {
     return std::nullopt;
   }
+
+  /* Multiplication basiacally double degree, so it is easy to make exponential bomb from repeat
+   * zone and multiply. */
+  constexpr int max_degree = 120;
 
   static const mf::MultiFunction &minus_func = multi_function::registry::lookup("-int"_ustr);
   static const mf::MultiFunction &add_func = multi_function::registry::lookup("int + int"_ustr);
@@ -38,86 +44,107 @@ std::optional<Polynom<int>> field_as_polynom_try(const Field<int> &entry_field)
   while (!fields_to_check.is_empty()) {
     const GFieldRef field = fields_to_check.pop();
     if (const Polynom<int> *value = known_fields.lookup_ptr(field)) {
-      if (value->size() > 120) {
+      if (value->size() > max_degree) {
         return std::nullopt;
       }
       continue;
     }
 
-    const FieldNode &field_node = field.node();
-    const FieldNodeType node_type = field_node.node_type();
-    if (node_type == FieldNodeType::Input) {
-      known_fields.add(field, Polynom<int>::from_degree_variable(1, 1));
-      continue;
+    enum class LoopState {
+      Continue,
+      Terminate,
+    };
+
+    const auto state = std::visit(
+        [&]<typename T>(const T field_data) -> LoopState {
+          if constexpr (std::is_same_v<T, GFieldRef::Value>) {
+            BLI_assert(field_data.type->template is<int>());
+            /* Currently any constant values are evaluated in place.
+             * So any conversion was made before field construction.
+             * See #execute_multi_function_on_value_variant for more info. */
+            /* Have to use implicit cast to void ptr in case value is buffer object. */
+            const int constant = *static_cast<const int *>(field_data.value);
+            known_fields.add(field, Polynom<int>::from_const(constant));
+            return LoopState::Continue;
+          }
+          else
+
+              if constexpr (std::is_same_v<T, GFieldRef::Input>)
+          {
+            known_fields.add(field, Polynom<int>::from_degree_variable(1, 1));
+            return LoopState::Continue;
+          }
+          else {
+
+            static_assert(std::is_same_v<T, GFieldRef::MultiFn>);
+            const fn::FieldOperation &operation = *field_data.node;
+            BLI_assert(field_data.output_i == 0);
+            const mf::MultiFunction &node_function = operation.multi_function();
+            if (!ELEM(&node_function, &minus_func, &add_func, &sub_func, &mul_func, &mul_add_func))
+            {
+              return LoopState::Terminate;
+            }
+
+            const Span<GField> inputs = operation.inputs();
+            const bool all_known = std::all_of(
+                inputs.begin(), inputs.end(), [&](const GFieldRef input_field) {
+                  return known_fields.contains(input_field);
+                });
+
+            if (!all_known) {
+              fields_to_check.push(field);
+
+              for (const GFieldRef input_field : inputs) {
+                if (!known_fields.contains(input_field)) {
+                  fields_to_check.push(input_field);
+                }
+              }
+
+              return LoopState::Continue;
+            }
+
+            const Polynom<int> &first_arg = known_fields.lookup(inputs[0]);
+            if (&node_function == &minus_func) {
+              known_fields.add(field, -first_arg);
+              return LoopState::Continue;
+            }
+
+            const Polynom<int> &second_arg = known_fields.lookup(inputs[1]);
+            if (&node_function == &add_func) {
+              known_fields.add(field, first_arg + second_arg);
+              return LoopState::Continue;
+            }
+
+            if (&node_function == &sub_func) {
+              known_fields.add(field, first_arg - second_arg);
+              return LoopState::Continue;
+            }
+
+            if (first_arg.size() + second_arg.size() > max_degree) {
+              return LoopState::Terminate;
+            }
+
+            if (&node_function == &mul_func) {
+              known_fields.add(field, first_arg * second_arg);
+              return LoopState::Continue;
+            }
+
+            BLI_assert(&node_function == &mul_add_func);
+            const Polynom<int> &third_arg = known_fields.lookup(inputs[2]);
+            known_fields.add(field, first_arg * second_arg + third_arg);
+
+            return LoopState::Continue;
+          }
+        },
+        field.variant());
+
+    switch (state) {
+      using enum LoopState;
+      case Continue:
+        break;
+      case Terminate:
+        return std::nullopt;
     }
-
-    if (node_type == FieldNodeType::Constant) {
-      /* Currently any constant values are evaluated in place.
-       * So any conversion was made before field construction.
-       * See #execute_multi_function_on_value_variant for more info. */
-      const int offset = *dynamic_cast<const FieldConstant &>(field_node).value().get<int>();
-      known_fields.add(field, Polynom<int>::from_const(offset));
-      continue;
-    }
-
-    BLI_assert(node_type == FieldNodeType::Operation);
-    const FieldOperation &operation = dynamic_cast<const FieldOperation &>(field_node);
-    const mf::MultiFunction &node_function = operation.multi_function();
-    if (!ELEM(&node_function, &minus_func, &add_func, &sub_func, &mul_func, &mul_add_func)) {
-      return std::nullopt;
-    }
-
-    const Span<GField> inputs = operation.inputs();
-    const bool all_known = std::all_of(
-        inputs.begin(), inputs.end(), [&](const GFieldRef input_field) {
-          return known_fields.contains(input_field);
-        });
-
-    if (!all_known) {
-      fields_to_check.push(field);
-
-      for (const GFieldRef input_field : inputs) {
-        if (!known_fields.contains(input_field)) {
-          fields_to_check.push(input_field);
-        }
-      }
-
-      continue;
-    }
-
-    const Polynom<int> &first_arg = known_fields.lookup(inputs[0]);
-    if (&node_function == &minus_func) {
-      known_fields.add(field, -first_arg);
-      continue;
-    }
-
-    const Polynom<int> &second_arg = known_fields.lookup(inputs[1]);
-    if (&node_function == &add_func) {
-      known_fields.add(field, first_arg + second_arg);
-      continue;
-    }
-
-    if (&node_function == &sub_func) {
-      known_fields.add(field, first_arg - second_arg);
-      continue;
-    }
-
-    if (first_arg.size() + second_arg.size() > 120) {
-      return std::nullopt;
-    }
-
-    if (&node_function == &mul_func) {
-      known_fields.add(field, first_arg * second_arg);
-      continue;
-    }
-
-    BLI_assert(&node_function == &mul_add_func);
-    const Polynom<int> &third_arg = known_fields.lookup(inputs[2]);
-    known_fields.add(field, first_arg * second_arg + third_arg);
-
-    /* This simple implementation does not support non-linear polynomial even as temporarily
-     * values`. */
-    return std::nullopt;
   }
 
   return known_fields.lookup(entry_field);
