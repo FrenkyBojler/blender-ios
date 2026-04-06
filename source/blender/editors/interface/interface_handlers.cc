2c3dbac9e1246942baefb5c2dd8fa77994621128
paulhanson2000 <paulhanson2000@gmail.com>
2026-04-06 02:53:04 -0400

    UI: Pie menu triggers immediately past confirm threshold
    Functions of a pie menu now trigger immediately when dragging past
    the confirm theshold, instead of waiting for mouse movement to end.

--- source/blender/editors/interface/interface_handlers.cc
+++ source/blender/editors/interface/interface_handlers.cc
@@ -11933,12 +11933,9 @@ static int pie_handler(bContext *C, const wmEvent *event, PopupBlockHandle *menu
       if (block->pie_data->flags & PIE_GESTURE_END_WAIT) {
         float len_sq = 10;
 
-        /* use a time threshold to ensure we leave time to the mouse to move */
-        if (duration - block->pie_data->duration_gesture > 0.02) {
-          len_sq = len_squared_v2v2(event_xy, block->pie_data->last_pos);
-          copy_v2_v2(block->pie_data->last_pos, event_xy);
-          block->pie_data->duration_gesture = duration;
-        }
+        len_sq = len_squared_v2v2(event_xy, block->pie_data->last_pos);
+        copy_v2_v2(block->pie_data->last_pos, event_xy);
+        block->pie_data->duration_gesture = duration;
 
         if (len_sq < 1.0f) {
           Button *but = region_find_active_but(menu->region);
