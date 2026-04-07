f5869a5749057e28f51a7fb370b39c9990408f8c
Tibo Stans <192132910+stanzerelli@users.noreply.github.com>
2026-04-07 12:04:44 +0200

    Update input declaration to separate UI name and Python identifier

--- source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
+++ source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
@@ -163,15 +163,14 @@ static void node_declare(NodeDeclarationBuilder &b)
           "reflection at near-grazing incidence to simulate complex index of refraction")
       .translation_context(BLT_I18NCONTEXT_ID_NODETREE);
 #define SOCK_SPECULAR_TINT_ID 14
-  spec.add_input<decl::Float>("Anisotropic"_ustr)
+  spec.add_input<decl::Float>("Anisotropy"_ustr, "Anisotropic"_ustr)
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR)
       .description(
           "Amount of anisotropy for specular reflection. "
-          "Higher values give elongated highlights along the tangent direction")
-      .short_label("Anisotropy");
+                   "Higher values give elongated highlights along the tangent direction");
 #define SOCK_ANISOTROPIC_ID 15
   spec.add_input<decl::Float>("Anisotropic Rotation"_ustr)
       .default_value(0.0f)
