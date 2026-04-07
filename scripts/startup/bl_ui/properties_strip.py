971b213dc5efb6a48d426327ec1d0b1bb4af14a2
Falk David <falk@blender.org>
2026-04-07 17:16:45 +0200

    VSE: UI: Add Compositor effect node group errors
    Related to !156706.
    When using a node tree that doesn't match the expected interface,
    display the errors in the interface below the node group selector.
    
    This is consistent with other types of errors, e.g. in mesh modifiers.
    
    Note that we cannot (and should not!) display these warnings in the
    node tree, because different users of the same node tree might
    expect a different interface.

--- scripts/startup/bl_ui/properties_strip.py
+++ scripts/startup/bl_ui/properties_strip.py
@@ -138,6 +138,32 @@ class STRIP_PT_adjust_crop(StripButtonsPanel, Panel):
         col.prop(strip.crop, "min_y")
 
 
+def draw_compositor_effect_node_group_errors(layout, node_tree):
+    if not node_tree or not node_tree.interface:
+        return
+    input_sockets = []
+    output_sockets = []
+    for socket in node_tree.interface.items_tree:
+        if socket.item_type == 'SOCKET':
+            if socket.in_out == 'INPUT':
+                input_sockets.append(socket)
+            elif socket.in_out == 'OUTPUT':
+                output_sockets.append(socket)
+
+    if len(input_sockets) < 3:
+        layout.label(text="Node group must have three inputs.", icon='ERROR')
+    else:
+        if all(map(lambda x: x.socket_type != 'NodeSocketFloat', input_sockets)):
+            layout.label(text="Node group must have an input with Float type.", icon='ERROR')
+        if sum(map(lambda x: x.socket_type == 'NodeSocketColor', input_sockets)) < 2:
+            layout.label(text="Node group must have two inputs with Color type.", icon='ERROR')
+
+    if len(output_sockets) < 1:
+        layout.label(text="Node group must have an output.", icon='ERROR')
+    elif output_sockets[0].socket_type != 'NodeSocketColor':
+        layout.label(text="The first node group output must have the Color type.", icon='ERROR')
+
+
 class STRIP_PT_effect(StripButtonsPanel, Panel):
     bl_label = "Effect Strip"
 
@@ -184,6 +210,7 @@ class STRIP_PT_effect(StripButtonsPanel, Panel):
 
         if strip_type == 'COMPOSITOR':
             layout.template_ID(strip, "node_group", new="node.new_compositor_sequencer_node_group")
+            draw_compositor_effect_node_group_errors(layout, strip.node_group)
 
         if strip.input_count > 0:
             col = layout.column()
