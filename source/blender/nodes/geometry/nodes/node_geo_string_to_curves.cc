blender/blender

commit 624f454e08b9e33464e662307ab3d34157b90030

Geometry Nodes: new Font socket and turn Curve to String node inputs to sockets
Until now, the String to Curves node had a significant limitation: the used font
and some other options could only be selected on the node itself. It was not
possible to expose them to a group node or modifier. This made it practically
impossible to build high level motion graphics templates that required the end
user to select a font.

This patch adds a new font socket to Geometry Nodes. Most boilerplate for the
new socket was already added in #151429. Now it is exposed in Geometry Nodes. It
is very similar to other existing data-block socket types. The most similar one
is the image socket, because it also has a special ui template that allows
opening a file etc.

Furthermore, this patch also exposes the remaining menu properties of the String
to Curves node as input sockets and adds panels to make it look cleaner by
default.

The depsgraph integration of `VFont` has been slightly modified so that it
actually works, i.e. when the filepath of a font is changed, it triggers a
reevaluation now.

Pull Request: https://projects.blender.org/blender/blender/pulls/151544

source/blender/nodes/geometry/nodes/node_geo_string_to_curves.cc
@@ -10,6 +10,8 @@
 #include "BKE_instances.hh"
 #include "BKE_vfont.hh"
 
+#include "DNA_vfont_types.h"
+
 #include "BLI_bounds.hh"
 #include "BLI_math_matrix.hh"
 #include "BLI_string_utf8.h"
@@ -28,67 +30,165 @@ namespace blender::nodes::node_geo_string_to_curves_cc {
 
 NODE_STORAGE_FUNCS(NodeGeometryStringToCurves)
 
+static const EnumPropertyItem rna_node_geometry_string_to_curves_overflow_items[] = {
+    {GEO_NODE_STRING_TO_CURVES_MODE_OVERFLOW,
+     "OVERFLOW",
+     ICON_NONE,
+     "Overflow",
+     "Let the text use more space than the specified height"},
+    {GEO_NODE_STRING_TO_CURVES_MODE_SCALE_TO_FIT,
+     "SCALE_TO_FIT",
+     ICON_NONE,
+     "Scale To Fit",
+     "Scale the text size to fit inside the width and height"},
+    {GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE,
+     "TRUNCATE",
+     ICON_NONE,
+     "Truncate",
+     "Only output curves that fit within the width and height. Output the remainder to the "
+     "\"Remainder\" output."},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
+static const EnumPropertyItem rna_node_geometry_string_to_curves_align_x_items[] = {
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_X_LEFT,
+     "LEFT",
+     ICON_ALIGN_LEFT,
+     "Left",
+     "Align text to the left"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_X_CENTER,
+     "CENTER",
+     ICON_ALIGN_CENTER,
+     "Center",
+     "Align text to the center"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_X_RIGHT,
+     "RIGHT",
+     ICON_ALIGN_RIGHT,
+     "Right",
+     "Align text to the right"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_X_JUSTIFY,
+     "JUSTIFY",
+     ICON_ALIGN_JUSTIFY,
+     "Justify",
+     "Align text to the left and the right"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_X_FLUSH,
+     "FLUSH",
+     ICON_ALIGN_FLUSH,
+     "Flush",
+     "Align text to the left and the right, with equal character spacing"},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
+static const EnumPropertyItem rna_node_geometry_string_to_curves_align_y_items[] = {
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_Y_TOP, "TOP", ICON_ALIGN_TOP, "Top", "Align text to the top"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_Y_TOP_BASELINE,
+     "TOP_BASELINE",
+     ICON_ALIGN_TOP,
+     "Top Baseline",
+     "Align text to the top line's baseline"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_Y_MIDDLE,
+     "MIDDLE",
+     ICON_ALIGN_MIDDLE,
+     "Middle",
+     "Align text to the middle"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_Y_BOTTOM_BASELINE,
+     "BOTTOM_BASELINE",
+     ICON_ALIGN_BOTTOM,
+     "Bottom Baseline",
+     "Align text to the bottom line's baseline"},
+    {GEO_NODE_STRING_TO_CURVES_ALIGN_Y_BOTTOM,
+     "BOTTOM",
+     ICON_ALIGN_BOTTOM,
+     "Bottom",
+     "Align text to the bottom"},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
+static const EnumPropertyItem rna_node_geometry_string_to_curves_pivot_mode[] = {
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_MIDPOINT, "MIDPOINT", 0, "Midpoint", "Midpoint"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_TOP_LEFT, "TOP_LEFT", 0, "Top Left", "Top Left"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_TOP_CENTER, "TOP_CENTER", 0, "Top Center", "Top Center"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_TOP_RIGHT, "TOP_RIGHT", 0, "Top Right", "Top Right"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_BOTTOM_LEFT,
+     "BOTTOM_LEFT",
+     0,
+     "Bottom Left",
+     "Bottom Left"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_BOTTOM_CENTER,
+     "BOTTOM_CENTER",
+     0,
+     "Bottom Center",
+     "Bottom Center"},
+    {GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_BOTTOM_RIGHT,
+     "BOTTOM_RIGHT",
+     0,
+     "Bottom Right",
+     "Bottom Right"},
+    {0, nullptr, 0, nullptr, nullptr},
+};
+
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::String>("String").optional_label();
-  b.add_input<decl::Float>("Size").default_value(1.0f).min(0.0f).subtype(PROP_DISTANCE);
-  b.add_input<decl::Float>("Character Spacing").default_value(1.0f).min(0.0f);
-  b.add_input<decl::Float>("Word Spacing").default_value(1.0f).min(0.0f);
-  b.add_input<decl::Float>("Line Spacing").default_value(1.0f).min(0.0f);
-  b.add_input<decl::Float>("Text Box Width").default_value(0.0f).min(0.0f).subtype(PROP_DISTANCE);
-  auto &height = b.add_input<decl::Float>("Text Box Height")
-                     .default_value(0.0f)
-                     .min(0.0f)
-                     .subtype(PROP_DISTANCE)
-                     .make_available([](bNode &node) {
-                       node_storage(node).overflow = GEO_NODE_STRING_TO_CURVES_MODE_SCALE_TO_FIT;
-                     });
+  b.use_custom_socket_order();
+  b.allow_any_socket_order();
+
   b.add_output<decl::Geometry>("Curve Instances");
-  auto &remainder = b.add_output<decl::String>("Remainder").make_available([](bNode &node) {
-    node_storage(node).overflow = GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE;
-  });
+  b.add_output<decl::String>("Remainder")
+      .usage_by_menu("Overflow", GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE);
   b.add_output<decl::Int>("Line").field_on_all().translation_context(BLT_I18NCONTEXT_ID_TEXT);
   b.add_output<decl::Vector>("Pivot Point").field_on_all();
 
-  const bNode *node = b.node_or_null();
-  if (node != nullptr) {
-    const NodeGeometryStringToCurves &storage = node_storage(*node);
-    const GeometryNodeStringToCurvesOverflowMode overflow = GeometryNodeStringToCurvesOverflowMode(
-        storage.overflow);
-
-    remainder.available(overflow == GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE);
-    height.available(overflow != GEO_NODE_STRING_TO_CURVES_MODE_OVERFLOW);
+  b.add_input<decl::String>("String").optional_label();
+  b.add_input<decl::Float>("Size").default_value(1.0f).min(0.0f).subtype(PROP_DISTANCE);
+  b.add_input<decl::Font>("Font")
+      .default_value_fn(
+          [](const bNode & /*node*/) { return id_cast<ID *>(BKE_vfont_builtin_ensure()); })
+      .optional_label();
+  {
+    auto &p = b.add_panel("Alignment").default_closed(true);
+    p.add_input<decl::Menu>("Align X")
+        .static_items(rna_node_geometry_string_to_curves_align_x_items)
+        .optional_label();
+    p.add_input<decl::Menu>("Align Y")
+        .static_items(rna_node_geometry_string_to_curves_align_y_items)
+        .optional_label();
+    p.add_input<decl::Menu>("Pivot Point")
+        .static_items(rna_node_geometry_string_to_curves_pivot_mode);
   }
-}
-
-static void node_layout(ui::Layout &layout, bContext *C, PointerRNA *ptr)
-{
-  layout.use_property_split_set(true);
-  layout.use_property_decorate_set(false);
-  template_id(&layout, C, ptr, "font", nullptr, "FONT_OT_open", "FONT_OT_unlink");
-  layout.prop(ptr, "overflow", UI_ITEM_NONE, "", ICON_NONE);
-  layout.prop(ptr, "align_x", UI_ITEM_NONE, "", ICON_NONE);
-  layout.prop(ptr, "align_y", UI_ITEM_NONE, "", ICON_NONE);
-  layout.prop(ptr, "pivot_mode", UI_ITEM_NONE, IFACE_("Pivot Point"), ICON_NONE);
+  {
+    auto &p = b.add_panel("Spacing").default_closed(true);
+    p.add_input<decl::Float>("Character Spacing").default_value(1.0f).min(0.0f);
+    p.add_input<decl::Float>("Word Spacing").default_value(1.0f).min(0.0f);
+    p.add_input<decl::Float>("Line Spacing").default_value(1.0f).min(0.0f);
+  }
+  {
+    auto &p = b.add_panel("Text Box").default_closed(true);
+    p.add_input<decl::Menu>("Overflow")
+        .static_items(rna_node_geometry_string_to_curves_overflow_items)
+        .optional_label();
+    p.add_input<decl::Float>("Text Box Width")
+        .default_value(0.0f)
+        .min(0.0f)
+        .subtype(PROP_DISTANCE);
+    p.add_input<decl::Float>("Text Box Height")
+        .default_value(0.0f)
+        .min(0.0f)
+        .subtype(PROP_DISTANCE)
+        .usage_by_menu("Overflow", GEO_NODE_STRING_TO_CURVES_MODE_SCALE_TO_FIT);
+  }
 }
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
+  /* Still used for forward compatibility. */
   NodeGeometryStringToCurves *data = MEM_new_for_free<NodeGeometryStringToCurves>(__func__);
-
-  data->overflow = GEO_NODE_STRING_TO_CURVES_MODE_OVERFLOW;
-  data->align_x = GEO_NODE_STRING_TO_CURVES_ALIGN_X_LEFT;
-  data->align_y = GEO_NODE_STRING_TO_CURVES_ALIGN_Y_TOP_BASELINE;
-  data->pivot_mode = GEO_NODE_STRING_TO_CURVES_PIVOT_MODE_BOTTOM_LEFT;
   node->storage = data;
-  node->id = reinterpret_cast<ID *>(BKE_vfont_builtin_ensure());
 }
 
 static float3 get_pivot_point(GeoNodeExecParams &params, bke::CurvesGeometry &curves)
 {
-  const NodeGeometryStringToCurves &storage = node_storage(params.node());
-  const GeometryNodeStringToCurvesPivotMode pivot_mode = (GeometryNodeStringToCurvesPivotMode)
-                                                             storage.pivot_mode;
+  const GeometryNodeStringToCurvesPivotMode pivot_mode =
+      params.get_input<GeometryNodeStringToCurvesPivotMode>("Pivot Point");
 
   const std::optional<Bounds<float3>> bounds = bounds::min_max(curves.positions());
 
@@ -143,7 +243,7 @@ struct TextLayout {
 
 static std::optional<TextLayout> get_text_layout(GeoNodeExecParams &params)
 {
-  VFont *vfont = reinterpret_cast<VFont *>(params.node().id);
+  VFont *vfont = params.get_input<VFont *>("Font");
   if (!vfont) {
     params.error_message_add(NodeWarningType::Error, TIP_("Font not specified"));
     return std::nullopt;
@@ -155,13 +255,9 @@ static std::optional<TextLayout> get_text_layout(GeoNodeExecParams &params)
     return std::nullopt;
   }
 
-  const NodeGeometryStringToCurves &storage = node_storage(params.node());
-  const GeometryNodeStringToCurvesOverflowMode overflow = (GeometryNodeStringToCurvesOverflowMode)
-                                                              storage.overflow;
-  const GeometryNodeStringToCurvesAlignXMode align_x = (GeometryNodeStringToCurvesAlignXMode)
-                                                           storage.align_x;
-  const GeometryNodeStringToCurvesAlignYMode align_y = (GeometryNodeStringToCurvesAlignYMode)
-                                                           storage.align_y;
+  const auto overflow = params.get_input<GeometryNodeStringToCurvesOverflowMode>("Overflow");
+  const auto align_x = params.get_input<GeometryNodeStringToCurvesAlignXMode>("Align X");
+  const auto align_y = params.get_input<GeometryNodeStringToCurvesAlignYMode>("Align Y");
 
   const float font_size = std::max(params.extract_input<float>("Size"), 0.0f);
   const float char_spacing = params.extract_input<float>("Character Spacing");
@@ -265,7 +361,7 @@ static Map<int, int> create_curve_instances(GeoNodeExecParams &params,
                                             TextLayout &layout,
                                             bke::Instances &instances)
 {
-  VFont *vfont = reinterpret_cast<VFont *>(params.node().id);
+  VFont *vfont = params.get_input<VFont *>("Font");
   Map<int, int> handles;
   bool pivot_required = params.anonymous_attribute_output_is_required("Pivot Point");
 
@@ -373,9 +469,8 @@ static void node_geo_exec(GeoNodeExecParams params)
     return;
   }
 
-  const NodeGeometryStringToCurves &storage =
-      *(const NodeGeometryStringToCurves *)params.node().storage;
-  if (storage.overflow == GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE) {
+  const auto overflow = params.get_input<GeometryNodeStringToCurvesOverflowMode>("Overflow");
+  if (overflow == GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE) {
     params.set_output("Remainder", std::move(layout->truncated_text));
   }
 
@@ -411,7 +506,6 @@ static void node_register()
   bke::node_type_size(ntype, 190, 120, 700);
   bke::node_type_storage(
       ntype, "NodeGeometryStringToCurves", node_free_standard_storage, node_copy_standard_storage);
-  ntype.draw_buttons = node_layout;
   bke::node_register_type(ntype);
 }
 NOD_REGISTER_NODE(node_register)

source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ namespace blender {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 17
+#define BLENDER_FILE_SUBVERSION 18
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/intern/node.cc
@@ -592,7 +592,7 @@ static void update_node_location_legacy(bNodeTree &ntree)
   }
 }
 
-static void write_legacy_properties(bNodeTree &ntree)
+static void write_legacy_properties(bNodeTree &ntree, Map<ID **, ID *> &r_ids_to_restore)
 {
   switch (ntree.type) {
     case NTREE_GEOMETRY: {
@@ -689,6 +689,25 @@ static void write_legacy_properties(bNodeTree &ntree)
         else if (node->is_type("FunctionNodeMatchString")) {
           const bNodeSocket *socket = node_find_socket(*node, SOCK_IN, "Operation");
           node->custom1 = socket->default_value_typed<bNodeSocketValueMenu>()->value;
+        }
+        else if (node->type_legacy == GEO_NODE_STRING_TO_CURVES) {
+          auto &storage = *static_cast<NodeGeometryStringToCurves *>(node->storage);
+          storage.overflow = node_find_socket(*node, SOCK_IN, "Overflow")
+                                 ->default_value_typed<bNodeSocketValueMenu>()
+                                 ->value;
+          storage.align_x = node_find_socket(*node, SOCK_IN, "Align X")
+                                ->default_value_typed<bNodeSocketValueMenu>()
+                                ->value;
+          storage.align_y = node_find_socket(*node, SOCK_IN, "Align Y")
+                                ->default_value_typed<bNodeSocketValueMenu>()
+                                ->value;
+          storage.pivot_mode = node_find_socket(*node, SOCK_IN, "Pivot Point")
+                                   ->default_value_typed<bNodeSocketValueMenu>()
+                                   ->value;
+          r_ids_to_restore.add(&node->id, node->id);
+          node->id = id_cast<ID *>(node_find_socket(*node, SOCK_IN, "Font")
+                                       ->default_value_typed<bNodeSocketValueFont>()
+                                       ->value);
         }
       }
       break;
@@ -1207,9 +1226,11 @@ void node_tree_blend_write(BlendWriter *writer, bNodeTree *ntree)
   BKE_id_blend_write(writer, &ntree->id);
   BLO_write_string(writer, ntree->description);
 
+  /* Restore IDs overridden for forward compatibility. Otherwise their user count becomes wrong. */
+  Map<ID **, ID *> ids_to_restore;
   if (!BLO_write_is_undo(writer)) {
     forward_compat::update_node_location_legacy(*ntree);
-    forward_compat::write_legacy_properties(*ntree);
+    forward_compat::write_legacy_properties(*ntree, ids_to_restore);
   }
 
   for (bNode *node : ntree->all_nodes()) {
@@ -1278,6 +1299,9 @@ void node_tree_blend_write(BlendWriter *writer, bNodeTree *ntree)
   if (!BLO_write_is_undo(writer)) {
     for (bNode *node : ntree->all_nodes()) {
       forward_compat::free_legacy_socket_storage(*node);
+    }
+    for (const auto &item : ids_to_restore.items()) {
+      *item.key = item.value;
     }
   }
 }

source/blender/blenloader/intern/versioning_510.cc
@@ -324,6 +324,39 @@ static void version_clear_unused_strip_flags(Main &bmain)
   }
 }
 
+static void version_string_to_curves_node_inputs(bNodeTree &tree, bNode &node)
+{
+  if (!node.storage) {
+    return;
+  }
+  auto &storage = *reinterpret_cast<NodeGeometryStringToCurves *>(node.storage);
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Font")) {
+    bNodeSocket &socket = version_node_add_socket(tree, node, SOCK_IN, "NodeSocketFont", "Font");
+    socket.default_value_typed<bNodeSocketValueFont>()->value = reinterpret_cast<VFont *>(node.id);
+    node.id = nullptr;
+  }
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Overflow")) {
+    bNodeSocket &socket = version_node_add_socket(
+        tree, node, SOCK_IN, "NodeSocketMenu", "Overflow");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.overflow;
+  }
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Align X")) {
+    bNodeSocket &socket = version_node_add_socket(
+        tree, node, SOCK_IN, "NodeSocketMenu", "Align X");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.align_x;
+  }
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Align Y")) {
+    bNodeSocket &socket = version_node_add_socket(
+        tree, node, SOCK_IN, "NodeSocketMenu", "Align Y");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.align_y;
+  }
+  if (!blender::bke::node_find_socket(node, SOCK_IN, "Pivot Point")) {
+    bNodeSocket &socket = version_node_add_socket(
+        tree, node, SOCK_IN, "NodeSocketMenu", "Pivot Point");
+    socket.default_value_typed<bNodeSocketValueMenu>()->value = storage.pivot_mode;
+  }
+}
+
 static const char *legacy_pass_name_to_new_name(const char *name)
 {
   if (STREQ(name, "DiffDir")) {
@@ -626,6 +659,19 @@ void blo_do_versions_510(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
               BKE_tracking_distortion_free(static_cast<MovieDistortion *>(node.storage));
             }
             node.storage = nullptr;
+          }
+        }
+      }
+    }
+    FOREACH_NODETREE_END;
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 501, 17)) {
+    FOREACH_NODETREE_BEGIN (bmain, tree, id) {
+      if (tree->type == NTREE_GEOMETRY) {
+        for (bNode &node : tree->nodes) {
+          if (node.type_legacy == GEO_NODE_STRING_TO_CURVES) {
+            version_string_to_curves_node_inputs(*tree, node);
           }
         }
       }

source/blender/depsgraph/DEG_depsgraph_build.hh
@@ -30,6 +30,7 @@ struct Main;
 struct Object;
 struct Scene;
 struct bNodeTree;
+struct VFont;
 
 /* Graph Building -------------------------------- */
 
@@ -152,6 +153,7 @@ void DEG_add_object_cache_relation(DepsNodeHandle *handle,
                                    CacheFile *cache_file,
                                    eDepsObjectComponentType component,
                                    const char *description);
+void DEG_add_vfont_relation(DepsNodeHandle *handle, VFont *vfont, const char *description);
 /**
  * Adds relation from #DEG_OPCODE_GENERIC_DATABLOCK_UPDATE of a given ID.
  * Is used for such entities as textures and images.

source/blender/depsgraph/intern/builder/deg_builder_nodes.cc
@@ -653,10 +653,12 @@ void DepsgraphNodeBuilder::build_id(ID *id, const bool force_be_visible)
     case ID_PA:
       build_particle_settings(id_cast<ParticleSettings *>(id));
       break;
+    case ID_VF:
+      build_vfont((VFont *)id);
+      break;
 
     case ID_LI:
     case ID_SCR:
-    case ID_VF:
     case ID_BR:
     case ID_WM:
     case ID_PAL:
@@ -2332,8 +2334,6 @@ void DepsgraphNodeBuilder::build_vfont(VFont *vfont)
   build_parameters(&vfont->id);
   build_idproperties(vfont->id.properties);
   build_idproperties(vfont->id.system_properties);
-  add_operation_node(
-      &vfont->id, NodeType::GENERIC_DATABLOCK, OperationCode::GENERIC_DATABLOCK_UPDATE);
 }
 
 static bool strip_node_build_cb(Strip *strip, void *user_data)

source/blender/depsgraph/intern/builder/deg_builder_relations.cc
@@ -587,10 +587,12 @@ void DepsgraphRelationBuilder::build_id(ID *id)
     case ID_PA:
       build_particle_settings(id_cast<ParticleSettings *>(id));
       break;
+    case ID_VF:
+      build_vfont((VFont *)id);
+      break;
 
     case ID_LI:
     case ID_SCR:
-    case ID_VF:
     case ID_BR:
     case ID_WM:
     case ID_PAL:

source/blender/depsgraph/intern/depsgraph_build.cc
@@ -18,6 +18,7 @@
 #include "DNA_node_types.h"
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
+#include "DNA_vfont_types.h"
 
 #include "BKE_collection.hh"
 #include "BKE_global.hh"
@@ -193,6 +194,14 @@ void DEG_add_bone_relation(DepsNodeHandle *node_handle,
   deg::ComponentKey comp_key(&object->id, type, bone_name);
   deg::DepsNodeHandle *deg_node_handle = get_node_handle(node_handle);
   deg_node_handle->builder->add_node_handle_relation(comp_key, deg_node_handle, description);
+}
+
+void DEG_add_vfont_relation(DepsNodeHandle *handle, VFont *vfont, const char *description)
+{
+  deg::OperationKey operation_key(
+      &vfont->id, deg::NodeType::PARAMETERS, deg::OperationCode::PARAMETERS_EXIT);
+  deg::DepsNodeHandle *deg_node_handle = get_node_handle(handle);
+  deg_node_handle->builder->add_node_handle_relation(operation_key, deg_node_handle, description);
 }
 
 void DEG_add_object_pointcache_relation(DepsNodeHandle *node_handle,

FrenkyBojler/blender-ios

commit 2d6acdc19776ac83c8ef2fbcad1d7e1a09f2f7ea

Fix #156996: String To Curves node Truncate mode has no Height input
Regression in 624f454e08 which only displayed the Height input if the
overflow mode is "Scale To Fit", when it needs to also be displayed if
the mode is "Truncate".

Co-Authored By: Omar Emara

source/blender/nodes/geometry/nodes/node_geo_string_to_curves.cc
@@ -175,7 +175,9 @@ static void node_declare(NodeDeclarationBuilder &b)
         .default_value(0.0f)
         .min(0.0f)
         .subtype(PROP_DISTANCE)
-        .usage_by_menu("Overflow"_ustr, GEO_NODE_STRING_TO_CURVES_MODE_SCALE_TO_FIT);
+        .usage_by_menu("Overflow"_ustr,
+                       {GEO_NODE_STRING_TO_CURVES_MODE_SCALE_TO_FIT,
+                        GEO_NODE_STRING_TO_CURVES_MODE_TRUNCATE});
   }
 }
 