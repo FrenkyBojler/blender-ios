b8ec3d91baa20a01b0d367c040ef13101469c090
quackarooni <alfonsomartzii@gmail.com>
2026-04-09 23:22:48 +0800

    Run `make format`

--- scripts/modules/rna_keymap_ui.py
+++ scripts/modules/rna_keymap_ui.py
@@ -70,7 +70,7 @@ def draw_km(display_keymaps, kc, km, children, layout, level):
     row = subcol.row(align=True)
     row.prop(km, "show_expanded_children", text="", emboss=False)
     row.label(text=km.name, text_ctxt=i18n_contexts.id_windowmanager)
-    
+
     if km.is_user_modified or km.is_modal:
         subrow = row.row()
         subrow.alignment = 'RIGHT'
