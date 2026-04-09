9967c968dc5a04494fe82d43c50f20c435adbf85
Falk David <falk@blender.org>
2026-04-09 17:37:13 +0200

    Inline `rna_*_node_group_update` functions

--- source/blender/makesrna/intern/rna_sequencer.cc
+++ source/blender/makesrna/intern/rna_sequencer.cc
@@ -1900,24 +1900,31 @@ static bool rna_Compositor_node_group_poll(PointerRNA * /*ptr*/, PointerRNA valu
   return true;
 }
 
-static void strip_compositor_effect_node_group_update(Main *bmain, PointerRNA *ptr)
+static void rna_CompositorEffect_node_group_update(Main *bmain, Scene *scene, PointerRNA *ptr)
 {
+  rna_Strip_invalidate_raw_update(bmain, scene, ptr);
+
   /* Tag depsgraph relations for an update since the strip could now be referencing a different
    * node tree. */
   DEG_relations_tag_update(bmain);
 
   /* Strips from other scenes could be modified, so use the scene of the strip as opposed to the
    * active scene argument. */
-  Scene *strip_scene = reinterpret_cast<Scene *>(ptr->owner_id);
-  Editing *ed = seq::editing_get(strip_scene);
+  Scene *sequencer_scene = reinterpret_cast<Scene *>(ptr->owner_id);
+  Editing *ed = seq::editing_get(sequencer_scene);
+  if (!ed) {
+    return;
+  }
 
   /* The sequencer stores a cached mapping between compositor node trees and strips that use them,
    * so we need to invalidate the cache since the node tree changed. */
   seq::strip_lookup_invalidate(ed);
 }
 
-static void strip_compositor_modifier_node_group_update(Main *bmain, PointerRNA *ptr)
+static void rna_CompositorModifier_node_group_update(Main *bmain, Scene *scene, PointerRNA *ptr)
 {
+  rna_StripModifier_update(bmain, scene, ptr);
+
   /* Tag depsgraph relations for an update since the strip could now be referencing a different
    * node tree. */
   DEG_relations_tag_update(bmain);
@@ -1936,18 +1943,6 @@ static void strip_compositor_modifier_node_group_update(Main *bmain, PointerRNA 
 
   auto *cmd = ptr->data_as<SequencerCompositorModifierData>();
   seq::compositor_nodes_update_interface(*sequencer_scene, *cmd);
-}
-
-static void rna_CompositorEffect_node_group_update(Main *bmain, Scene *scene, PointerRNA *ptr)
-{
-  rna_Strip_invalidate_raw_update(bmain, scene, ptr);
-  strip_compositor_effect_node_group_update(bmain, ptr);
-}
-
-static void rna_CompositorModifier_node_group_update(Main *bmain, Scene *scene, PointerRNA *ptr)
-{
-  rna_StripModifier_update(bmain, scene, ptr);
-  strip_compositor_modifier_node_group_update(bmain, ptr);
 }
 
 static StructRNA *rna_SequencerCompositorModifierProperties_refine(PointerRNA *ptr)
