zNightlord/Bforartists

commit 646199c89a3747fbfa5e61653817269fe21f7879

BPY: Expose some library info to `bpy.data.libraries.load()` API.
This commit adds a list of Library IDs present in the loaded blendfile
library, exposing:
  - Their stored file path (absolute, or relative to the loaded
    blendfile).
  - Whether they are archive libraries (used as namespaces for packed
    linked IDs).

This should address a requirement from Bat v2, and is in general useful
for 'light introspection' of data dependencies in blendfiles, without
requiring to actually open all of them.

Also updated the documentation of `bpy.data.libraries.load()`, it was...
fairly minimal so far.

Pull Request: https://projects.blender.org/blender/blender/pulls/157066

source/blender/blenloader/intern/readfile.cc
@@ -819,6 +819,26 @@ static const IDHash *blo_bhead_id_deep_hash(const FileData *fd, const BHead *bhe
       POINTER_OFFSET(bhead, sizeof(*bhead) + fd->id_deep_hash_offset));
 }
 
+const char *blo_bhead_library_filepath(const FileData *fd, const BHead *bhead)
+{
+  BLI_assert(blo_bhead_is_id(bhead) && (bhead->code & 0xFFFF) == ID_LI);
+  if (fd->library_filepath_offset < 0) {
+    return nullptr;
+  }
+  return reinterpret_cast<const char *>(
+      POINTER_OFFSET(bhead, sizeof(*bhead) + fd->library_filepath_offset));
+}
+
+LibraryFlag blo_bhead_library_flag(const FileData *fd, const BHead *bhead)
+{
+  BLI_assert(blo_bhead_is_id(bhead) && (bhead->code & 0xFFFF) == ID_LI);
+  if (fd->library_flag_offset < 0) {
+    return LibraryFlag(0);
+  }
+  return *reinterpret_cast<const LibraryFlag *>(
+      POINTER_OFFSET(bhead, sizeof(*bhead) + fd->library_flag_offset));
+}
+
 static void read_blender_header(FileData *fd)
 {
   const BlenderHeaderVariant header_variant = BLO_readfile_blender_header_decode(fd->file);
@@ -890,6 +910,11 @@ static bool read_file_dna(FileData *fd, const char **r_error_message)
             fd->filesdna, "ID", "short", "flag");
         fd->id_deep_hash_offset = DNA_struct_member_offset_by_name_with_alias(
             fd->filesdna, "ID", "IDHash", "deep_hash");
+
+        fd->library_filepath_offset = DNA_struct_member_offset_by_name_with_alias(
+            fd->filesdna, "Library", "char", "filepath[]");
+        fd->library_flag_offset = DNA_struct_member_offset_by_name_with_alias(
+            fd->filesdna, "Library", "ushort", "flag");
 
         fd->filesubversion = subversion;
 

source/blender/blenloader/BLO_readfile.hh
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 
+#include "DNA_ID_enums.h"
 #include "DNA_listBase.h"
 
 #include "BLI_compiler_attrs.h"
@@ -231,8 +232,15 @@ void BLO_read_do_version_after_setup(Main *new_bmain,
  * \{ */
 
 struct BLODataBlockInfo {
+  struct Library {
+    const char *filepath = nullptr;
+    LibraryFlag flag = LibraryFlag(0);
+  };
+
   char name[/*MAX_ID_NAME-2*/ 256] = "";
   AssetMetaData *asset_data = nullptr;
+  /** For Library IDs only: specific info, like the stored blendfile path, flags. */
+  BLODataBlockInfo::Library library_data = {};
   /** Ownership over #asset_data above can be "stolen out" of this struct, for more permanent
    * storage. In that case, set this to false to avoid double freeing of the stolen data. */
   bool free_asset_data = false;

source/blender/blenloader/intern/readblenentry.cc
@@ -91,13 +91,18 @@ static bool blendhandle_load_id_data_and_validate(FileData *fd,
                                                   bool use_assets_only,
                                                   const char *&r_idname,
                                                   short &r_idflag,
-                                                  AssetMetaData *&r_asset_meta_data)
+                                                  AssetMetaData *&r_asset_meta_data,
+                                                  BLODataBlockInfo::Library *r_library_data)
 {
   r_idname = blo_bhead_id_name(fd, bhead);
   if (!r_idname || r_idname[0] == '\0') {
     return false;
   }
   r_idflag = blo_bhead_id_flag(fd, bhead);
+  if (r_library_data) {
+    r_library_data->filepath = blo_bhead_library_filepath(fd, bhead);
+    r_library_data->flag = blo_bhead_library_flag(fd, bhead);
+  }
   /* Do not list (and therefore allow direct linking of) packed data.
    * While supporting this is conceptually possible, it would require significant changes in
    * the UI (file browser) and UX (link operation) to convey this concept and handle it
@@ -128,7 +133,7 @@ LinkNode *BLO_blendhandle_get_datablock_names(BlendHandle *bh,
       short idflag;
       AssetMetaData *asset_meta_data;
       if (!blendhandle_load_id_data_and_validate(
-              fd, bhead, use_assets_only, idname, idflag, asset_meta_data))
+              fd, bhead, use_assets_only, idname, idflag, asset_meta_data, nullptr))
       {
         continue;
       }
@@ -155,6 +160,8 @@ LinkNode *BLO_blendhandle_get_datablock_info(BlendHandle *bh,
   BHead *bhead;
   int tot = 0;
 
+  const bool is_library = (ofblocktype == ID_LI);
+
   const int sdna_nr_preview_image = DNA_struct_find_with_alias(fd->filesdna, "PreviewImage");
 
   for (bhead = blo_bhead_first(fd); bhead; bhead = blo_bhead_next(fd, bhead)) {
@@ -167,14 +174,24 @@ LinkNode *BLO_blendhandle_get_datablock_info(BlendHandle *bh,
       const char *idname;
       short idflag;
       AssetMetaData *asset_meta_data;
-      if (!blendhandle_load_id_data_and_validate(
-              fd, id_bhead, use_assets_only, idname, idflag, asset_meta_data))
+      BLODataBlockInfo::Library library_data;
+      if (!blendhandle_load_id_data_and_validate(fd,
+                                                 id_bhead,
+                                                 use_assets_only,
+                                                 idname,
+                                                 idflag,
+                                                 asset_meta_data,
+                                                 is_library ? &library_data : nullptr))
       {
         continue;
       }
 
       const char *name = idname + 2;
       BLODataBlockInfo *info = MEM_new<BLODataBlockInfo>(__func__);
+
+      if (is_library) {
+        info->library_data = library_data;
+      }
 
       /* Lastly, read asset data from the following blocks. */
       if (asset_meta_data) {

source/blender/blenloader/intern/readfile.hh
@@ -121,11 +121,19 @@ struct FileData {
 
   /** Used to retrieve ID names from (bhead+1). */
   int id_name_offset = 0;
-  /** Used to retrieve asset data from (bhead+1). NOTE: This may not be available in old files,
-   * will be -1 then! */
+  /**
+   * Used to retrieve asset data from (bhead+1). NOTE: This may not be available in old files,
+   * will be -1 then!
+   */
   int id_asset_data_offset = 0;
   int id_flag_offset = 0;
   int id_deep_hash_offset = 0;
+  /**
+   * Gives access to libraries' filepath and flag, useful for introspection of blendfiles'
+   * dependencies _without_ having to fully read them.
+   */
+  int library_filepath_offset = 0;
+  int library_flag_offset = 0;
   /** For do_versions patching. */
   int globalf = 0;
   int fileflags = 0;
@@ -271,6 +279,19 @@ short blo_bhead_id_flag(const FileData *fd, const BHead *bhead);
  * Warning! Caller's responsibility to ensure given bhead **is** an ID one!
  */
 AssetMetaData *blo_bhead_id_asset_data_address(const FileData *fd, const BHead *bhead);
+
+/**
+ * Return the stored filepath (may be relative) of a library ID.
+ *
+ * Warning! Caller's responsibility to ensure that the given bhead **is** a Library ID one!
+ */
+const char *blo_bhead_library_filepath(const FileData *fd, const BHead *bhead);
+/**
+ * Return the stored flags of a library ID.
+ *
+ * Warning! Caller's responsibility to ensure that the given bhead **is** a Library ID one!
+ */
+LibraryFlag blo_bhead_library_flag(const FileData *fd, const BHead *bhead);
 
 /* do versions stuff */
 

source/blender/python/intern/bpy_library_load.cc
@@ -22,6 +22,7 @@
 #include "BLI_string.h"
 #include "BLI_utildefines.h"
 
+#include "BKE_asset.hh"
 #include "BKE_blender_version.h"
 #include "BKE_blendfile_link_append.hh"
 #include "BKE_context.hh"
@@ -72,6 +73,53 @@ static uint32_t bool_flag_pair_as_flag(const BoolFlagPair *bool_flags, int bool_
 }
 
 /** \} */
+
+/* Named tuple to store BLODataBlockInfo::Library data. */
+static PyTypeObject bpy_lib_LibraryType;
+
+static PyStructSequence_Field bpy_lib_LibraryType_fields[] = {
+    {"filepath",
+     "Filepath to the library file, may be relative to the blendfile being loaded"
+     "\n\n:type: str"},
+    {"is_archive",
+     "Whether the Library data-block is an archive one (used as namespace for packed linked "
+     "data-blocks)"
+     "\n\n:type: bool"},
+    {nullptr},
+};
+
+static PyStructSequence_Desc bpy_lib_LibraryType_desc = {
+    /*name*/ "bpy_lib_library",
+    /*doc*/ "Contains information about libraries present in the loaded blendfile",
+    /*fields*/ bpy_lib_LibraryType_fields,
+    /*n_in_sequence*/ ARRAY_SIZE(bpy_lib_LibraryType_fields) - 1,
+};
+
+static PyObject *make_library_info(BLODataBlockInfo &id_info)
+{
+  PyObject *library_info;
+  int pos = 0;
+
+  library_info = PyStructSequence_New(&bpy_lib_LibraryType);
+  if (library_info == nullptr) {
+    return nullptr;
+  }
+
+  PyStructSequence_SET_ITEM(
+      library_info, pos++, PyC_UnicodeFromBytes(id_info.library_data.filepath));
+  PyStructSequence_SET_ITEM(
+      library_info,
+      pos++,
+      PyBool_FromLong((id_info.library_data.flag & LIBRARY_FLAG_IS_ARCHIVE) != 0));
+
+  BLI_assert(pos == bpy_lib_LibraryType_desc.n_in_sequence);
+
+  if (UNLIKELY(PyErr_Occurred())) {
+    Py_DECREF(library_info);
+    return nullptr;
+  }
+  return library_info;
+}
 
 /**
  * The size to pre-allocate #BPy_Library::dict Add +1 for the "version".
@@ -219,8 +267,27 @@ PyDoc_STRVAR(
     "reuse_liboverrides=False, "
     "create_liboverrides_runtime=False)\n"
     "\n"
-    "   Returns a context manager which exposes 2 library objects on entering.\n"
-    "   Each object has attributes matching bpy.data which are lists of strings to be linked.\n"
+    "   Returns a context manager which exposes a pair of library objects (input and output)\n"
+    "   on entering.\n"
+    "\n"
+    "   The input contains the data-blocks available in the loaded blend-file library and should\n"
+    "   be treated as read-only.\n"
+    "   Data-block names added to the output are linked/appended when the context exits.\n"
+    "\n"
+    "   Each library object contains:\n"
+    "      - Attributes matching ``bpy.data``, which are lists of strings representing linkable\n"
+    "        data-blocks.\n"
+    "      - A ``libraries`` attribute, which for the input is a list of all other libraries\n"
+    "        used by the loaded one, as named tuples (``filepath``, ``is_archive``). The\n"
+    "        filepath may be absolute, or relative to the loaded blend-file. The output\n"
+    "        ``libraries`` attribute is always ``None``.\n"
+    "      - A ``version`` attribute, representing the version of the loaded library blend-file\n"
+    "        (for the input) or the version of the current Blender (for the output).\n"
+    "\n"
+    "   Notes:\n"
+    "      - Not all data-block types are linkable (e.g. WindowManager, Library, ...).\n"
+    "      - Packed linked data-blocks are not linkable and are not listed in the input\n"
+    "        ``libraries`` attribute.\n"
     "\n"
     "   :param filepath: The path to a blend file.\n"
     "   :type filepath: str | bytes\n"
@@ -451,19 +518,31 @@ static PyObject *bpy_lib_load(BPy_PropertyRNA *self, PyObject *args, PyObject *k
   return reinterpret_cast<PyObject *>(ret);
 }
 
-static PyObject *_bpy_names(BPy_Library *self, int blocktype)
+static PyObject *_bpy_ids_info(BPy_Library *self, int blocktype)
 {
-  int names_num;
-  LinkNode *names = BLO_blendhandle_get_datablock_names(
-      self->blo_handle, blocktype, (self->flag & FILE_ASSETS_ONLY) != 0, &names_num);
-  PyObject *list = PyList_New(names_num);
+  int ids_info_num;
+  LinkNode *ids_info = BLO_blendhandle_get_datablock_info(
+      self->blo_handle, blocktype, (self->flag & FILE_ASSETS_ONLY) != 0, &ids_info_num);
+  PyObject *list = PyList_New(ids_info_num);
 
-  if (names) {
+  if (ids_info) {
     int i = 0;
-    for (LinkNode *l = names; l; l = l->next, i++) {
-      PyList_SET_ITEM(list, i, PyUnicode_FromString((char *)l->link));
+    for (LinkNode *l = ids_info; l; l = l->next, i++) {
+      BLODataBlockInfo *id_info = static_cast<BLODataBlockInfo *>(l->link);
+      if (blocktype == ID_LI) {
+        PyList_SET_ITEM(list, i, make_library_info(*id_info));
+        continue;
+      }
+      PyList_SET_ITEM(list, i, PyUnicode_FromString(id_info->name));
     }
-    BLI_linklist_freeN(names); /* free linklist *and* each node's data */
+    /* free linklist *and* each node's data */
+    BLI_linklist_free(ids_info, [](void *link) -> void {
+      BLODataBlockInfo *id_info = static_cast<BLODataBlockInfo *>(link);
+      if (id_info->free_asset_data) {
+        BKE_asset_metadata_free(&id_info->asset_data);
+      }
+      MEM_delete(id_info);
+    });
   }
 
   return list;
@@ -493,7 +572,8 @@ static PyObject *bpy_lib_enter(BPy_Library *self)
 
   int i = 0, code;
   while ((code = BKE_idtype_idcode_iter_step(&i))) {
-    if (!BKE_idtype_idcode_is_linkable(code)) {
+    const bool is_library = (code == ID_LI);
+    if (!is_library && !BKE_idtype_idcode_is_linkable(code)) {
       dict_num_offset += 1;
       continue;
     }
@@ -501,9 +581,15 @@ static PyObject *bpy_lib_enter(BPy_Library *self)
     PyObject *str = PyUnicode_FromString(name_plural);
     PyObject *item;
 
-    PyDict_SetItem(dict_dst, str, item = PyList_New(0));
-    Py_DECREF(item);
-    PyDict_SetItem(dict_src, str, item = _bpy_names(self, code));
+    /* Do not add 'libraries' entry in output data, this would make no sense currently. */
+    if (is_library) {
+      PyDict_SetItem(dict_dst, str, Py_None);
+    }
+    else {
+      PyDict_SetItem(dict_dst, str, item = PyList_New(0));
+      Py_DECREF(item);
+    }
+    PyDict_SetItem(dict_src, str, item = _bpy_ids_info(self, code));
     Py_DECREF(item);
 
     Py_DECREF(str);
@@ -819,6 +905,8 @@ int BPY_library_load_type_ready()
   if (PyType_Ready(&bpy_lib_Type) < 0) {
     return -1;
   }
+
+  PyStructSequence_InitType(&bpy_lib_LibraryType, &bpy_lib_LibraryType_desc);
 
   return 0;
 }

FrenkyBojler/blender-ios

commit a79889a987fd98ec88ff7663a2e2730109201212

Fix: Bug in library flag reading due to incorrect data size
In source/blender/blenloader/intern/readfile.cc, the function
blo_bhead_library_flag was using a reinterpret_cast to the LibraryFlag
enum to read a 16-bit flag from the file data. Since the enum is 32-bit,
this resulted in reading 4 bytes instead of 2, mixing in adjacent data
and potentially leading to incorrect values or out-of-bounds reads.

This issue was introduced in 646199c89a3.

source/blender/blenloader/intern/readfile.cc
@@ -835,8 +835,8 @@ LibraryFlag blo_bhead_library_flag(const FileData *fd, const BHead *bhead)
   if (fd->library_flag_offset < 0) {
     return LibraryFlag(0);
   }
-  return *reinterpret_cast<const LibraryFlag *>(
-      POINTER_OFFSET(bhead, sizeof(*bhead) + fd->library_flag_offset));
+  return LibraryFlag(*reinterpret_cast<const uint16_t *>(
+      POINTER_OFFSET(bhead, sizeof(*bhead) + fd->library_flag_offset)));
 }
 
 static void read_blender_header(FileData *fd)