d9e65fe4b580da5e0df6ccfa9a95b6669363d67c
Clément Foucault <foucault.clem@gmail.com>
2026-04-10 16:44:56 +0200

    Add texelFetchExtend

--- source/blender/gpu/shaders/gpu_shader_cxx_sampler.hh
+++ source/blender/gpu/shaders/gpu_shader_cxx_sampler.hh
@@ -108,6 +108,7 @@ int4 texelFetch(isamplerBuffer, int) RET;
 uint4 texelFetch(usamplerBuffer, int) RET;
 
 float4 texelFetchExtend(sampler2D /*samp*/, int2 /*texel*/, int /*lvl*/) RET;
+float4 texelFetchExtend(sampler2DDepth /*samp*/, int2 /*texel*/, int /*lvl*/) RET;
 
 /** \} */
 
