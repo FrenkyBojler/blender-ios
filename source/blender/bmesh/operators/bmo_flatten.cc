db369c08e79dd3d561a85b4a5f3f3522a25868a6
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-04-10 06:16:11 +0000

    init

---
+++ source/blender/bmesh/operators/bmo_flatten.cc
@@ -0,0 +1,20 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup bmesh
+ *
+ * Flattens vertices on a best-fitting plane.
+ */
+
+#include "bmesh.hh"
+#include "intern/bmesh_operators_private.hh" /* own include */
+
+namespace blender {
+
+void bmo_flatten_exec(BMesh *bm, BMOperator *op)
+{
+}
+
+}  // namespace blender
