zNightlord/Bforartists

commit 64640aa853cc2970a2d2cfa3dd0a9de1c50efb7c

Refactor: Nodes: Use UString for idnames in some API functions
Followup to e21b9ed558d2cfe7a90c9c27f85924e8f3e12387

Pull Request: https://projects.blender.org/blender/blender/pulls/156853

source/blender/python/intern/bpy_rna.cc
@@ -4437,10 +4437,11 @@ static PyObject *pyrna_struct_bl_rna_get_subclass(PyObject *cls, PyObject *args)
   const StructRNA *srna_base = static_cast<const StructRNA *>(py_srna->ptr->data);
 
   if (srna_base == RNA_Node) {
+    const UString idname(id);
     /* If the given idname is an alias, translate it to the proper idname. */
-    id = bke::node_type_find_alias(UString(id)).c_str();
+    id = bke::node_type_find_alias(idname).c_str();
 
-    bke::bNodeType *nt = bke::node_type_find(id);
+    bke::bNodeType *nt = bke::node_type_find(idname);
     if (nt) {
       PointerRNA ptr = RNA_pointer_create_discrete(nullptr, RNA_Struct, nt->rna_ext.srna);
       return pyrna_struct_CreatePyObject(&ptr);

FrenkyBojler/blender-ios

commit 44f00738417262f94eb294730935a04239c6d66c

Fix: Node type lookup for legacy aliases in Python API
In pyrna_struct_bl_rna_get_subclass, the node idname was correctly
translated via bke::node_type_find_alias, but the result was not
used for the subsequent bke::node_type_find() call.

This regression was introduced in 64640aa853c.

source/blender/python/intern/bpy_rna.cc
@@ -4437,9 +4437,8 @@ static PyObject *pyrna_struct_bl_rna_get_subclass(PyObject *cls, PyObject *args)
   const StructRNA *srna_base = static_cast<const StructRNA *>(py_srna->ptr->data);
 
   if (srna_base == RNA_Node) {
-    const UString idname(id);
     /* If the given idname is an alias, translate it to the proper idname. */
-    id = bke::node_type_find_alias(idname).c_str();
+    const UString idname = bke::node_type_find_alias(UString(id));
 
     bke::bNodeType *nt = bke::node_type_find(idname);
     if (nt) {