d4fae05e601dcca49f9d10d494fb467dbd46907e
илья _ <modormoder@gmail.com>
2026-04-12 02:31:09 +0300

    back

--- source/blender/nodes/geometry/nodes/node_geo_merge_by_distance.cc
+++ source/blender/nodes/geometry/nodes/node_geo_merge_by_distance.cc
@@ -141,7 +141,6 @@ static void node_register()
   ntype.ui_name = "Merge by Distance";
   ntype.ui_description = "Merge vertices or points within a given distance";
   ntype.enum_name_legacy = "MERGE_BY_DISTANCE";
-  ntype.deprecation_notice = N_("Use the \"Merge Geometry\" node instead");
   ntype.nclass = NODE_CLASS_GEOMETRY;
   ntype.initfunc = node_init;
   bke::node_type_storage(ntype,
