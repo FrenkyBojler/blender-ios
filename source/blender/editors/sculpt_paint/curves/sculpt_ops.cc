zNightlord/Bforartists

commit 665a296a9ec6282edc7ddad9991042cfa39e8cd7

Refactor: Adjust paint stroke cleanup methods
Prior to this patch, we had three methods related to cleanup at the end
of a stroke:

* `stroke_done`: A private method called during modal invocation when
  the stroke would finish.
* `done`: A virtual method, called by `stroke_done` to perform
  mode-specific cleanup.
* `free`: A remnant of the old code that performed generic paint cleanup
  as part of `stroke_done`, also public.

This situation is overly complicated in the case of needing to cancel a
stroke outside of the normal modal invocation, i.e. if certain
prerequisites are not maintained and the stroke needs to be cancelled
prior to starting.

To simplify this situation, this patch makes the following changes:
* `stroke_done` renamed to `done`
* `cancel` & `finish` methods exposed, this simply calls `done` with
  necessary arguments
* `free` call removed entirely, existing code put into `done`
* virtual, mode-specific `done` function is no longer only called if the
  stroke has started.

The end state is that users of the `PaintStroke` class only now have to
call either `finish` or `cancel` if necessary inside an operator
`invoke`.

Pull Request: https://projects.blender.org/blender/blender/pulls/157150

source/blender/editors/sculpt_paint/curves/sculpt_ops.cc
@@ -260,7 +260,7 @@ static wmOperatorStatus sculpt_curves_stroke_invoke(bContext *C,
 
   if (retval == OPERATOR_FINISHED) {
     if (op->customdata != nullptr) {
-      op_data->free(C, op);
+      op_data->finish(C);
       MEM_delete(op_data);
     }
     return OPERATOR_FINISHED;
@@ -287,7 +287,7 @@ static void sculpt_curves_stroke_cancel(bContext *C, wmOperator *op)
 {
   if (op->customdata != nullptr) {
     SculptCurvesBrushStroke *op_data = static_cast<SculptCurvesBrushStroke *>(op->customdata);
-    op_data->cancel(C, op);
+    op_data->cancel(C);
     MEM_delete(op_data);
   }
 }

source/blender/editors/sculpt_paint/grease_pencil/draw_ops.cc
@@ -325,7 +325,7 @@ static wmOperatorStatus grease_pencil_brush_stroke_modal(bContext *C,
 static void grease_pencil_brush_stroke_cancel(bContext *C, wmOperator *op)
 {
   GreasePencilPaintStroke *stroke = static_cast<GreasePencilPaintStroke *>(op->customdata);
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 
 static void GREASE_PENCIL_OT_brush_stroke(wmOperatorType *ot)
@@ -434,7 +434,7 @@ static wmOperatorStatus grease_pencil_sculpt_paint_modal(bContext *C,
 static void grease_pencil_sculpt_paint_cancel(bContext *C, wmOperator *op)
 {
   GreasePencilPaintStroke *stroke = static_cast<GreasePencilPaintStroke *>(op->customdata);
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 
 static void GREASE_PENCIL_OT_sculpt_paint(wmOperatorType *ot)
@@ -532,7 +532,7 @@ static wmOperatorStatus grease_pencil_weight_brush_stroke_modal(bContext *C,
 static void grease_pencil_weight_brush_stroke_cancel(bContext *C, wmOperator *op)
 {
   GreasePencilPaintStroke *stroke = static_cast<GreasePencilPaintStroke *>(op->customdata);
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 
 static void GREASE_PENCIL_OT_weight_brush_stroke(wmOperatorType *ot)
@@ -641,7 +641,7 @@ static wmOperatorStatus grease_pencil_vertex_brush_stroke_modal(bContext *C,
 static void grease_pencil_vertex_brush_stroke_cancel(bContext *C, wmOperator *op)
 {
   GreasePencilPaintStroke *stroke = static_cast<GreasePencilPaintStroke *>(op->customdata);
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 
 static void GREASE_PENCIL_OT_vertex_brush_stroke(wmOperatorType *ot)

source/blender/editors/sculpt_paint/mesh/paint_image_ops_paint.cc
@@ -513,7 +513,7 @@ static wmOperatorStatus paint_invoke(bContext *C, wmOperator *op, const wmEvent 
   if (retval == OPERATOR_FINISHED) {
     ImagePaintStroke *stroke = static_cast<ImagePaintStroke *>(op->customdata);
     if (stroke) {
-      stroke->free(C, op);
+      stroke->finish(C);
       MEM_delete(stroke);
     }
     return OPERATOR_FINISHED;
@@ -603,7 +603,7 @@ static void paint_cancel(bContext *C, wmOperator *op)
     ED_image_undo_restore(ustack->step_init);
   }
 
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 }  // namespace ed::sculpt_paint::image::ops::paint
 

source/blender/editors/sculpt_paint/mesh/paint_vertex.cc
@@ -2090,7 +2090,7 @@ static wmOperatorStatus vpaint_invoke(bContext *C, wmOperator *op, const wmEvent
   if (retval == OPERATOR_FINISHED) {
     VertexPaintStroke *stroke = static_cast<VertexPaintStroke *>(op->customdata);
     if (stroke) {
-      stroke->free(C, op);
+      stroke->finish(C);
       MEM_delete(stroke);
     }
     return OPERATOR_FINISHED;

source/blender/editors/sculpt_paint/mesh/paint_weight.cc
@@ -1954,7 +1954,7 @@ static wmOperatorStatus wpaint_invoke(bContext *C, wmOperator *op, const wmEvent
   if (retval == OPERATOR_FINISHED) {
     WeightPaintStroke *stroke = static_cast<WeightPaintStroke *>(op->customdata);
     if (stroke) {
-      stroke->free(C, op);
+      stroke->finish(C);
       MEM_delete(stroke);
     }
     return OPERATOR_FINISHED;

source/blender/editors/sculpt_paint/mesh/sculpt.cc
@@ -5970,15 +5970,13 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
   if (brush_type_is_paint(brush.sculpt_brush_type) &&
       !color_supported_check(scene, ob, op->reports))
   {
-    stroke->done(true);
-    stroke->free(C, op);
+    stroke->cancel(C);
     MEM_delete(stroke);
     return OPERATOR_CANCELLED;
   }
   if (!brush_type_is_attribute_only(brush.sculpt_brush_type) && !shape_key_check(ob, op->reports))
   {
-    stroke->done(true);
-    stroke->free(C, op);
+    stroke->cancel(C);
     MEM_delete(stroke);
     return OPERATOR_CANCELLED;
   }
@@ -5989,8 +5987,7 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
     const bke::pbvh::Tree *pbvh = bke::object::pbvh_get(ob);
     if (!pbvh || pbvh->type() != bke::pbvh::Type::Grids) {
       BKE_report(op->reports, RPT_ERROR, "Only supported in multiresolution mode");
-      stroke->done(true);
-      stroke->free(C, op);
+      stroke->cancel(C);
       MEM_delete(stroke);
       return OPERATOR_CANCELLED;
     }
@@ -6012,8 +6009,7 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
   ignore_background_click = RNA_boolean_get(op->ptr, "ignore_background_click");
   const float mval[2] = {float(event->mval[0]), float(event->mval[1])};
   if (ignore_background_click && !over_mesh(C, op, mval)) {
-    stroke->done(true);
-    stroke->free(C, op);
+    stroke->cancel(C);
     MEM_delete(stroke);
     return OPERATOR_PASS_THROUGH;
   }
@@ -6024,10 +6020,12 @@ static wmOperatorStatus sculpt_brush_stroke_invoke(bContext *C,
   if (ELEM(retval, OPERATOR_FINISHED, OPERATOR_CANCELLED)) {
     SculptPaintStroke *stroke = static_cast<SculptPaintStroke *>(op->customdata);
     if (stroke) {
-      /* We don't need to call `stroke->done` in this case, as it should have happened inside
-       * the modal call */
-      BLI_assert(ob.runtime->sculpt_session->cache == nullptr);
-      stroke->free(C, op);
+      if (retval == OPERATOR_FINISHED) {
+        stroke->finish(C);
+      }
+      else {
+        stroke->cancel(C);
+      }
       MEM_delete(stroke);
     }
     return retval;
@@ -6067,7 +6065,7 @@ static void sculpt_brush_stroke_cancel(bContext *C, wmOperator *op)
   UNUSED_VARS_NDEBUG(brush);
 
   undo::restore_from_undo_step(depsgraph, sd, ob);
-  stroke->cancel(C, op);
+  stroke->cancel(C);
 }
 
 static wmOperatorStatus brush_stroke_modal(bContext *C, wmOperator *op, const wmEvent *event)

source/blender/editors/sculpt_paint/paint_intern.hh
@@ -197,13 +197,19 @@ struct PaintStroke : NonCopyable, NonMovable {
    */
   wmOperatorStatus modal(bContext *C, wmOperator *op, const wmEvent *event);
   wmOperatorStatus exec(bContext *C, wmOperator *op);
-  /** Cancel a stroke and return to the initial state. */
-  void cancel(bContext *C, wmOperator *op);
-  /**
-   * Free internal stroke data, not a destructor due to needed parameters.
-   * TODO: This might not need to be exposed, all internal code paths should end up calling this.
+
+  /** Cancel a stroke and return to the initial state.
+   *
+   * \note Typically handled as part of modal operator actions. Consumers of this API may need
+   * to call this if returning OPERATOR_CANCELLED during the `invoke` operator callback.
    */
-  void free(bContext *C, wmOperator *op);
+  void cancel(bContext *C);
+  /** Finish a stroke, performing any necessary cleanup actions.
+   *
+   * \note Typically handled as part of modal operator actions. Consumers of this API may need
+   * to call this if returning OPERATOR_FINISHED during the `invoke` operator callback.
+   */
+  void finish(bContext *C);
 
   /* TODO: The following accessors should all be parameters passed into various callbacks */
   bool stroke_flipped() const
@@ -273,8 +279,7 @@ struct PaintStroke : NonCopyable, NonMovable {
               bool *r_location_is_set);
 
  private:
-  void stroke_done(bContext *C, wmOperator *op, bool is_cancel);
-
+  void done(bContext *C, bool is_cancel);
   void add_step(bContext *C, wmOperator *op, float2 mval, float pressure);
 
   void add_sample(int input_samples, float x, float y, float pressure);

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -926,28 +926,7 @@ PaintStroke::PaintStroke(bContext *C, wmOperator *op, int event_type) : event_ty
   paint_runtime->start_pixel_radius = BKE_brush_radius_get(this->paint, this->brush);
 }
 
-void PaintStroke::free(bContext *C, wmOperator * /*op*/)
-{
-  if (RegionView3D *rv3d = CTX_wm_region_view3d(C)) {
-    rv3d->rflag &= ~RV3D_PAINTING;
-  }
-
-  BKE_paint_set_overlay_override(eOverlayFlags(0));
-
-  bke::PaintRuntime *paint_runtime = this->paint->runtime;
-  paint_runtime->draw_anchored = false;
-  paint_runtime->stroke_active = false;
-
-  if (timer_) {
-    WM_event_timer_remove(CTX_wm_manager(C), CTX_wm_window(C), timer_);
-  }
-
-  if (stroke_cursor_) {
-    WM_paint_cursor_end(static_cast<wmPaintCursor *>(stroke_cursor_));
-  }
-}
-
-void PaintStroke::stroke_done(bContext *C, wmOperator *op, const bool is_cancel)
+void PaintStroke::done(bContext *C, const bool is_cancel)
 {
   if (print_pressure_status_enabled()) {
     ED_workspace_status_text(C, nullptr);
@@ -965,13 +944,29 @@ void PaintStroke::stroke_done(bContext *C, wmOperator *op, const bool is_cancel)
     }
   }
 
+  /* TODO: Is this stroke_started_ guard necessary? */
   if (stroke_started_) {
     this->redraw(true);
-
-    this->done(is_cancel);
   }
 
-  this->free(C, op);
+  this->done(is_cancel);
+
+  if (RegionView3D *rv3d = CTX_wm_region_view3d(C)) {
+    rv3d->rflag &= ~RV3D_PAINTING;
+  }
+
+  BKE_paint_set_overlay_override(eOverlayFlags(0));
+
+  paint_runtime->draw_anchored = false;
+  paint_runtime->stroke_active = false;
+
+  if (timer_) {
+    WM_event_timer_remove(CTX_wm_manager(C), CTX_wm_window(C), timer_);
+  }
+
+  if (stroke_cursor_) {
+    WM_paint_cursor_end(static_cast<wmPaintCursor *>(stroke_cursor_));
+  }
 }
 
 static bool curves_sculpt_brush_uses_spacing(const eBrushCurvesSculptType tool)
@@ -1355,7 +1350,7 @@ bool PaintStroke::curve_end(bContext *C, wmOperator *op)
     }
   }
 
-  this->stroke_done(C, op, false);
+  this->done(C, false);
 
 #ifdef DEBUG_TIME
   TIMEIT_END_AVERAGED(whole_stroke);
@@ -1400,7 +1395,7 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
   if (paint == nullptr || br == nullptr) {
     /* In some circumstances, the context may change during modal execution. In this case,
      * we need to cancel the operator. See #147544 and related issues for further information. */
-    this->stroke_done(C, op, true);
+    this->done(C, true);
     return OPERATOR_CANCELLED;
   }
   const PaintMode mode = BKE_paintmode_get_active_from_context(C);
@@ -1543,13 +1538,13 @@ wmOperatorStatus PaintStroke::modal(bContext *C, wmOperator *op, const wmEvent *
         paint_stroke_line_constrain(this->last_mouse_position, this->constrained_pos, mouse);
       }
       this->line_end(C, op, mouse);
-      this->stroke_done(C, op, false);
+      this->done(C, false);
       return OPERATOR_FINISHED;
     }
   }
   else if (ELEM(event->type, EVT_RETKEY, EVT_SPACEKEY)) {
     this->line_end(C, op, sample_average.mouse);
-    this->stroke_done(C, op, false);
+    this->done(C, false);
     return OPERATOR_FINISHED;
   }
   else if (br->stroke_method == BRUSH_STROKE_LINE) {
@@ -1681,14 +1676,19 @@ wmOperatorStatus PaintStroke::exec(bContext *C, wmOperator *op)
 
   const bool ok = stroke_started_;
 
-  this->stroke_done(C, op, !ok);
+  this->done(C, !ok);
 
   return ok ? OPERATOR_FINISHED : OPERATOR_CANCELLED;
 }
 
-void PaintStroke::cancel(bContext *C, wmOperator *op)
+void PaintStroke::finish(bContext *C)
 {
-  this->stroke_done(C, op, true);
+  this->done(C, false);
+}
+
+void PaintStroke::cancel(bContext *C)
+{
+  this->done(C, true);
 }
 
 static const bToolRef *brush_tool_get(const ScrArea *area, const ARegion *region)

FrenkyBojler/blender-ios

commit ea20c18ec392a1835f8743abc702b5bb5611048f

Fix #157213: Sculpt: Crash when stroke starts and ends in empty area
Caused by 665a296a9ec6282edc7ddad9991042cfa39e8cd7

Sculpt Mode conditionally initializes its undo stack only once the
stroke has gone over the mesh. Previously, the `done` callback would
only get called if the stroke had started. However, to support using it
as a generic "cleanup" step, it now needs to be called even if the
stroke never goes over the mesh.

To support both of these cases, pass in `stroke_started` to the `done`
callback so we can avoid calling `sculpt_undo_end` in invalid
situations.

Note that this doesn't fix the nullptr access which directly causes the
crash. This will be handled in a follow-up PR, as it has wider
implications than this individual bug.

source/blender/editors/sculpt_paint/curves/sculpt_ops.cc
@@ -185,7 +185,7 @@ struct SculptCurvesBrushStroke final : public PaintStroke {
   void redraw(bool final) override;
   bool test_cancel() override;
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 
  private:
   std::unique_ptr<CurvesSculptStrokeOperation> operation_;
@@ -238,7 +238,7 @@ bool SculptCurvesBrushStroke::test_cancel()
   return false;
 }
 
-void SculptCurvesBrushStroke::done(const bool /*is_cancel*/) {}
+void SculptCurvesBrushStroke::done(const bool /*is_cancel*/, bool /*stroke_started*/) {}
 
 static wmOperatorStatus sculpt_curves_stroke_invoke(bContext *C,
                                                     wmOperator *op,

source/blender/editors/sculpt_paint/grease_pencil/draw_ops.cc
@@ -91,7 +91,7 @@ struct GreasePencilPaintStroke final : public PaintStroke {
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
   void redraw(bool final) override;
   bool test_cancel() override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 };
 
 bool GreasePencilPaintStroke::get_location(float out[3],
@@ -229,7 +229,7 @@ bool GreasePencilPaintStroke::test_cancel()
   return false;
 }
 
-void GreasePencilPaintStroke::done(bool /*is_cancel*/)
+void GreasePencilPaintStroke::done(bool /*is_cancel*/, bool /*stroke_started*/)
 {
   GreasePencilStrokeOperation *operation = static_cast<GreasePencilStrokeOperation *>(
       mode_data_.get());

source/blender/editors/sculpt_paint/mesh/paint_image_ops_paint.cc
@@ -362,7 +362,7 @@ struct ImagePaintStroke final : public PaintStroke {
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
   void redraw(bool final) override;
   bool test_cancel() override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 
   void update_for_exec(bContext *C,
                        const Brush &brush,
@@ -431,7 +431,7 @@ void ImagePaintStroke::redraw(bool final)
   pop->mode->paint_stroke_redraw(this->evil_C, pop->stroke_handle, final);
 }
 
-void ImagePaintStroke::done(const bool is_cancel)
+void ImagePaintStroke::done(const bool is_cancel, const bool /*stroke_started*/)
 {
   Scene *scene = CTX_data_scene(this->evil_C);
   ToolSettings *toolsettings = scene->toolsettings;

source/blender/editors/sculpt_paint/mesh/paint_vertex.cc
@@ -986,7 +986,7 @@ struct VertexPaintStroke final : public PaintStroke {
   void redraw(bool final) override;
   bool test_cancel() override;
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 };
 
 bool VertexPaintStroke::get_location(float out[3], const float mouse[2], bool force_original)
@@ -2060,7 +2060,7 @@ void VertexPaintStroke::update_step(wmOperator * /*op*/, PointerRNA *itemptr)
   DEG_id_tag_update(ob.data, ID_RECALC_GEOMETRY);
 }
 
-void VertexPaintStroke::done(bool /*is_cancel*/)
+void VertexPaintStroke::done(bool /*is_cancel*/, bool /*stroke_started*/)
 {
   VPaintData *vpd = static_cast<VPaintData *>(mode_data_.get());
   Object &ob = *vpd->vc.obact;

source/blender/editors/sculpt_paint/mesh/paint_weight.cc
@@ -892,7 +892,7 @@ struct WeightPaintStroke final : public PaintStroke {
   void redraw(bool final) override;
   bool test_cancel() override;
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 };
 
 bool WeightPaintStroke::get_location(float out[3], const float mouse[2], bool force_original)
@@ -1909,7 +1909,7 @@ void WeightPaintStroke::update_step(wmOperator * /*op*/, PointerRNA *itemptr)
   ED_region_tag_redraw(vc->region);
 }
 
-void WeightPaintStroke::done(bool /*is_cancel*/)
+void WeightPaintStroke::done(bool /*is_cancel*/, bool /*stroke_started*/)
 {
   Object &ob = *this->object;
 

source/blender/editors/sculpt_paint/mesh/sculpt.cc
@@ -4971,7 +4971,7 @@ struct SculptPaintStroke final : public PaintStroke {
   void redraw(bool final) override;
   bool test_cancel() override;
   void update_step(wmOperator *op, PointerRNA *itemptr) override;
-  void done(bool is_cancel) override;
+  void done(bool is_cancel, bool stroke_started) override;
 };
 
 bool SculptPaintStroke::get_location(float out[3], const float mouse[2], bool force_original)
@@ -5875,7 +5875,7 @@ static void brush_exit_tex(Sculpt &sd)
   }
 }
 
-void SculptPaintStroke::done(bool is_cancel)
+void SculptPaintStroke::done(bool is_cancel, bool stroke_started)
 {
   Object &ob = *this->object;
   SculptSession &ss = *ob.runtime->sculpt_session;
@@ -5908,7 +5908,7 @@ void SculptPaintStroke::done(bool is_cancel)
   MEM_delete(ss.cache);
   ss.cache = nullptr;
 
-  if (!is_cancel) {
+  if (!is_cancel && stroke_started) {
     stroke_undo_end(*paint_mode_settings_, *this->object, brush);
   }
 

source/blender/editors/sculpt_paint/paint_intern.hh
@@ -265,8 +265,10 @@ struct PaintStroke : NonCopyable, NonMovable {
    *
    * \param is_cancel: Some paint modes support cancelling a stroke and returning to the initial
    * state. This parameter indicates this case so that appropriate cleanup actions can be taken.
+   * \param stroke_started: Whether the stroke started. Subclasses can use this to determine if
+   * undo steps should be created. See \test_start.
    */
-  virtual void done(bool is_cancel) = 0;
+  virtual void done(bool is_cancel, bool stroke_started) = 0;
 
   /* TODO: This can probably be private, but `paint_image_ops_paint` depends on this */
   bool update(bContext *C,

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -949,7 +949,7 @@ void PaintStroke::done(bContext *C, const bool is_cancel)
     this->redraw(true);
   }
 
-  this->done(is_cancel);
+  this->done(is_cancel, stroke_started_);
 
   if (RegionView3D *rv3d = CTX_wm_region_view3d(C)) {
     rv3d->rflag &= ~RV3D_PAINTING;