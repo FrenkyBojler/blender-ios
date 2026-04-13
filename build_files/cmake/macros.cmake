zNightlord/Bforartists

commit 84dbd9fea9725f201068213461e66bbe84d7603c

GPU: Shader Tool: Emit dependency file
This allow correct update of files whose content
depends on some included file.

This requires CMake 3.21 so that all our
generators support the `DEPFILE` option.

Pull Request: https://projects.blender.org/blender/blender/pulls/156980

build_files/cmake/macros.cmake
@@ -1174,6 +1174,7 @@ function(glsl_to_c
   get_filename_component(_file_meta ${CMAKE_CURRENT_BINARY_DIR}/${file_from}.hh REALPATH)
   get_filename_component(_file_info ${CMAKE_CURRENT_BINARY_DIR}/${file_from}.info  REALPATH)
   get_filename_component(_file_to   ${CMAKE_CURRENT_BINARY_DIR}/${file_from}.c  REALPATH)
+  get_filename_component(_file_dep  ${CMAKE_CURRENT_BINARY_DIR}/${file_from}.d  REALPATH)
 
   # Turn include directories into absolute paths
   set(_inc_list "")
@@ -1188,8 +1189,9 @@ function(glsl_to_c
   set(${list_to_add} ${${list_to_add}} PARENT_SCOPE)
 
   add_custom_command(
-    OUTPUT  ${_file_to} ${_file_meta} ${_file_info}
-    COMMAND "$<TARGET_FILE:shader_tool>" ${_file_from} ${_file_tmp} ${_file_meta} ${_file_info} ${_inc_list}
+    OUTPUT  ${_file_to} ${_file_meta} ${_file_info} ${_file_dep}
+    DEPFILE ${_file_dep}
+    COMMAND "$<TARGET_FILE:shader_tool>" ${_file_from} ${_file_tmp} ${_file_meta} ${_file_info} ${_file_dep} ${_inc_list}
     COMMAND "$<TARGET_FILE:datatoc>" ${_file_tmp} ${_file_to}
     DEPENDS ${_file_from} datatoc shader_tool)
 

source/blender/gpu/shader_tool/shader_tool.cc
@@ -93,9 +93,9 @@ static metadata::Source scan_external_symbols(const std::vector<std::string> &fi
 int main(int argc, char **argv)
 {
   using namespace blender;
-  if (argc < 5) {
+  if (argc < 6) {
     std::cerr << "Usage: shader_tool <data_file_from> <data_file_to> <metadata_file_to> "
-                 "<infos_file_to> <include_dir1> <include_dir2> ..."
+                 "<infos_file_to> <dep_file_to> <include_dir1> <include_dir2> ..."
               << std::endl;
     exit(1);
   }
@@ -104,6 +104,7 @@ int main(int argc, char **argv)
   const char *output_file_name = argv[2];
   const char *metadata_file_name = argv[3];
   const char *infos_file_name = argv[4];
+  const char *dep_file_name = argv[5];
 
   /* Open the input file for reading */
   std::ifstream input_file(input_file_name);
@@ -149,9 +150,17 @@ int main(int argc, char **argv)
     exit(1);
   }
 
+  /* Open the output file for writing */
+  std::ofstream dep_file(dep_file_name, std::ofstream::out | std::ofstream::binary);
+  if (!dep_file) {
+    std::cerr << "Error: Could not open output file " << dep_file_name << std::endl;
+    input_file.close();
+    exit(1);
+  }
+
   /* List of files available for include. */
   std::vector<std::string> file_list;
-  for (int i = 5; i < argc; i++) {
+  for (int i = 6; i < argc; i++) {
     auto list = list_files(std::string(argv[i]));
     /* Extend list. */
     file_list.insert(file_list.end(), list.begin(), list.end());
@@ -173,10 +182,26 @@ int main(int argc, char **argv)
   }
 
   metadata::Source external_symbols;
+  std::vector<std::string> visited_files{input_file_name};
   if (language == Language::BLENDER_GLSL) {
-    std::vector<std::string> visited_files{input_file_name};
     external_symbols = scan_external_symbols(file_list, visited_files, buffer.str(), filename);
   }
+
+  /* Escape path according to the depfile syntax. */
+  auto escape_path = [](std::string filepath) {
+    size_t pos = 0;
+    while ((pos = filepath.find(' ', pos)) != std::string::npos) {
+      filepath.replace(pos, 1, "\\ ");
+      pos += 2;
+    }
+    return filepath;
+  };
+
+  dep_file << output_file_name << " : ";
+  for (const auto &file : visited_files) {
+    dep_file << escape_path(file) << " ";
+  }
+  dep_file << "\n";
 
   SourceProcessor processor(buffer.str(), input_file_name, language);
 
@@ -200,6 +225,7 @@ int main(int argc, char **argv)
   output_file.close();
   metadata_file.close();
   infos_file.close();
+  dep_file.close();
 
   if (error) {
     std::cerr << error.value().full_report << std::endl;

FrenkyBojler/blender-ios

commit 72fc85491de1a21b6561b7d82afaf72af239bfcb

Build: Fix shader_tool continually rebuilding with Ninja
The shader_tool depfile named the intermediate .glsl tmp file as its target,
but Ninja expects it to match the first declared output (.glsl.c), causing
every shader rule to be considered dirty on every build. Avoid this problem
by splitting shader_tool and datatoc into two commands.

Issue introduced in 84dbd9fea9725f2010682.

build_files/cmake/macros.cmake
@@ -1189,11 +1189,15 @@ function(glsl_to_c
   set(${list_to_add} ${${list_to_add}} PARENT_SCOPE)
 
   add_custom_command(
-    OUTPUT  ${_file_to} ${_file_meta} ${_file_info} ${_file_dep}
+    OUTPUT  ${_file_tmp} ${_file_meta} ${_file_info} ${_file_dep}
     DEPFILE ${_file_dep}
     COMMAND "$<TARGET_FILE:shader_tool>" ${_file_from} ${_file_tmp} ${_file_meta} ${_file_info} ${_file_dep} ${_inc_list}
+    DEPENDS ${_file_from} shader_tool)
+
+  add_custom_command(
+    OUTPUT  ${_file_to}
     COMMAND "$<TARGET_FILE:datatoc>" ${_file_tmp} ${_file_to}
-    DEPENDS ${_file_from} datatoc shader_tool)
+    DEPENDS ${_file_tmp} datatoc)
 
   set_source_files_properties(${_file_tmp} PROPERTIES GENERATED TRUE)
   set_source_files_properties(${_file_to}  PROPERTIES GENERATED TRUE)