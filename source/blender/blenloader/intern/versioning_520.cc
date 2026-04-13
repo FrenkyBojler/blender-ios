dc963a66d16563747e1fea9b365939b736cacaee
Weizhen Huang <weizhen@blender.org>
2026-04-13 16:31:13 +0200

    Remove accidental empty line

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -428,7 +428,6 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
       }
     }
   }
-
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
