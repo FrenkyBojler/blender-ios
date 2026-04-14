951562ead22583c93b59afdc5cac59ebbf346051
Falk David <falk@blender.org>
2026-04-14 11:08:48 +0200

    Use strip input number and update error logic

--- scripts/startup/bl_ui/properties_strip.py
+++ scripts/startup/bl_ui/properties_strip.py
@@ -138,25 +138,20 @@ class STRIP_PT_adjust_crop(StripButtonsPanel, Panel):
         col.prop(strip.crop, "min_y")
 
 
-def draw_compositor_effect_node_group_errors(layout, node_tree):
+def draw_compositor_effect_node_group_errors(layout, node_tree, strip_input_num):
     if not node_tree or not node_tree.interface:
         return
-    input_sockets = []
+    color_input_sockets_num = 0
     output_sockets = []
     for socket in node_tree.interface.items_tree:
         if socket.item_type == 'SOCKET':
-            if socket.in_out == 'INPUT':
-                input_sockets.append(socket)
+            if socket.in_out == 'INPUT' and socket.socket_type == 'NodeSocketColor':
+                color_input_sockets_num += 1
             elif socket.in_out == 'OUTPUT':
                 output_sockets.append(socket)
 
-    if len(input_sockets) < 3:
-        layout.label(text="Node group must have three inputs.", icon='ERROR')
-    else:
-        if all(map(lambda x: x.socket_type != 'NodeSocketFloat', input_sockets)):
-            layout.label(text="Node group must have an input with Float type.", icon='ERROR')
-        if sum(map(lambda x: x.socket_type == 'NodeSocketColor', input_sockets)) < 2:
-            layout.label(text="Node group must have two inputs with Color type.", icon='ERROR')
+    if color_input_sockets_num < strip_input_num:
+        layout.label(text="Node group must have at least {strip_input_num} Color inputs.", icon='ERROR')
 
     if len(output_sockets) < 1:
         layout.label(text="Node group must have an output.", icon='ERROR')
@@ -210,7 +205,7 @@ class STRIP_PT_effect(StripButtonsPanel, Panel):
 
         if strip_type == 'COMPOSITOR':
             layout.template_ID(strip, "node_group", new="node.new_compositor_sequencer_node_group")
-            draw_compositor_effect_node_group_errors(layout, strip.node_group)
+            draw_compositor_effect_node_group_errors(layout, strip.node_group, strip.input_count)
 
         if strip.input_count > 0:
             col = layout.column()
