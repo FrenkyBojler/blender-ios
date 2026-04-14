ca217e21ad0fe610abe2d9f3a28cdc201c957c1c
Pratik Borhade <pratikborhade302@gmail.com>
2026-04-14 15:37:01 +0530

    Use booleans_mix_calc

--- source/blender/editors/curves/intern/curves_selection.cc
+++ source/blender/editors/curves/intern/curves_selection.cc
@@ -524,9 +524,12 @@ void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask, bo
         for (const int i : curve_writers) {
           bke::GSpanAttributeWriter &selection = selection_writers[i];
           GMutableSpan selection_curve = selection.span.slice(points);
-          const bool is_curve_selected = !selection_curve.typed<bool>().contains(false);
-          if (has_anything_selected(selection_curve)) {
-            if (select || is_curve_selected) {
+          const array_utils::BooleanMix has_anything_selected = array_utils::booleans_mix_calc(
+              VArray<bool>::from_span(selection_curve.typed<bool>()));
+          const bool all_points_selected = has_anything_selected ==
+                                           array_utils::BooleanMix::AllTrue;
+          if (has_anything_selected != array_utils::BooleanMix::AllFalse) {
+            if (select || all_points_selected) {
               fill_selection(selection_curve, select);
             }
 
@@ -534,7 +537,7 @@ void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask, bo
               if (j == i) {
                 continue;
               }
-              if (select || is_curve_selected) {
+              if (select || all_points_selected) {
                 fill_selection(selection_curve, select);
               }
             }
