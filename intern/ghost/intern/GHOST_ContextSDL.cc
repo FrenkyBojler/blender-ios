c8033e251179c3ea2604007884cf19b2a890ef79
Jonas Holzman <jonas@blender.org>
2026-04-14 15:17:57 +0200

    Revert "Fix: Conditionally use OpenGL backend only `initClearGL` function"
    This reverts commit 11961a1f3e31f0e5000b6d356c8105218d5558f3.

--- intern/ghost/intern/GHOST_ContextSDL.cc
+++ intern/ghost/intern/GHOST_ContextSDL.cc
@@ -139,9 +139,7 @@ GHOST_TSuccess GHOST_ContextSDL::initializeDrawingContext()
       }
     }
 
-#ifdef WITH_OPENGL_BACKEND
     initClearGL();
-#endif
     SDL_GL_SwapWindow(window_);
 
     active_context_ = this;
