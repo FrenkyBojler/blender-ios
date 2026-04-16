524145c837f99c27d0517985d5d47ba8501affc8
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-04-16 04:11:55 +0000

    remove const_cast

--- source/blender/blenlib/intern/math_matrix_c.cc
+++ source/blender/blenlib/intern/math_matrix_c.cc
@@ -2971,7 +2971,7 @@ void BLI_space_transform_invert_normal(const SpaceTransform *data, float no[3])
 
 void BLI_space_transform_invert_normal_non_uniform(const SpaceTransform *data, float no[3])
 {
-  mul_transposed_mat3_m4_v3((const_cast<SpaceTransform *>(data))->local2target, no);
+  mul_transposed_mat3_m4_v3(data->local2target, no);
   normalize_v3(no);
 }
 
