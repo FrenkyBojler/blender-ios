4036c2e8d948dfdd341c842fcec9e29db1037f1e
Pratik Borhade <pratikborhade302@gmail.com>
2026-04-16 09:34:40 +0530

    Fix layout

--- scripts/startup/bl_ui/space_outliner.py
+++ scripts/startup/bl_ui/space_outliner.py
@@ -436,9 +436,10 @@ class OUTLINER_PT_filter(Panel):
             col.prop(space, "use_sort_alpha")
 
         if display_mode != 'LIBRARY_OVERRIDES':
-            row = layout.row(align=True)
+            col = layout.column(align=True)
+            row = col.row(align=True)
             row.prop(space, "use_sync_select", text="Sync Selection")
-            row = layout.row(align=True)
+            row = col.row(align=True)
             row.active = space.use_sync_select
             row.prop(space, "scroll_to_active", text="Scroll to Active")
 
