5688b6247f52f21b6d34db476096c5ab914620e1
Pratik Borhade <pratikborhade302@gmail.com>
2026-04-16 09:47:32 +0530

    Fix format

--- source/blender/makesrna/intern/rna_space.cc
+++ source/blender/makesrna/intern/rna_space.cc
@@ -4400,8 +4400,7 @@ static void rna_def_space_outliner(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "scroll_to_active", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "flag", SO_SCROLL_TO_ACTIVE);
-  RNA_def_property_ui_text(
-      prop, "Scroll to Active", "Scroll outliner to active object");
+  RNA_def_property_ui_text(prop, "Scroll to Active", "Scroll outliner to active object");
   RNA_def_property_update(prop, NC_SPACE | ND_SPACE_OUTLINER, nullptr);
 
   /* Granular restriction column option. */
