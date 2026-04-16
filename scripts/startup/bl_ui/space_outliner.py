dd1d14432eb68507d70dbe41b8624e32106d5381
Pratik Borhade <pratikborhade302@gmail.com>
2026-04-16 09:23:45 +0530

    Outliner: Flter option for scroll to active selection
    As requested in !148563, add a new option in outliner filters panel.
    When enabled along with sync selection, this will scroll outliner such that
    active tree element is in view.

--- scripts/startup/bl_ui/space_outliner.py
+++ scripts/startup/bl_ui/space_outliner.py
@@ -438,6 +438,9 @@ class OUTLINER_PT_filter(Panel):
         if display_mode != 'LIBRARY_OVERRIDES':
             row = layout.row(align=True)
             row.prop(space, "use_sync_select", text="Sync Selection")
+            row = layout.row(align=True)
+            row.active = space.use_sync_select
+            row.prop(space, "scroll_to_active", text="Scroll to Active")
 
             row = layout.row(align=True)
             row.prop(space, "show_mode_column", text="Show Mode Column")
