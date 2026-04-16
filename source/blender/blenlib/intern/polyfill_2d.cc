9931138acc93e4afd7207959990aabffa4ad9447
Brecht Van Lommel <brecht@blender.org>
2026-04-16 15:48:34 +0200

    Change axis_t to uint8_t, doesn't make much sense as bool

--- source/blender/blenlib/intern/polyfill_2d.cc
+++ source/blender/blenlib/intern/polyfill_2d.cc
@@ -103,7 +103,7 @@ using eSign = int8_t;
  * - the KDTree is only built & used when the polygon is concave.
  */
 
-using axis_t = bool;
+using axis_t = uint8_t;
 
 /* use for sorting */
 struct KDTreeNode2D_head {
@@ -296,7 +296,7 @@ static void kdtree2d_init(KDTree2D *tree, const uint32_t coords_num, const PolyI
     if (indices[i].sign != CONVEX) {
       node->neg = node->pos = KDNODE_UNSET;
       node->index = indices[i].index;
-      node->axis = false;
+      node->axis = 0;
       node->flag = 0;
       node++;
     }
@@ -365,7 +365,7 @@ static uint32_t kdtree2d_balance_recursive(KDTreeNode2D *nodes,
 
 static void kdtree2d_balance(KDTree2D *tree)
 {
-  tree->root = kdtree2d_balance_recursive(tree->nodes, tree->node_num, false, tree->coords, 0);
+  tree->root = kdtree2d_balance_recursive(tree->nodes, tree->node_num, 0, tree->coords, 0);
 }
 
 static void kdtree2d_init_mapping(KDTree2D *tree)
