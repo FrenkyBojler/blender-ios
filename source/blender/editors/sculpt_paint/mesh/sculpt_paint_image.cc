blender/blender

commit 1faa192978da49c991b71e8c65e2cf43f8f06e65

Refactor: Paint: Cache `ImBuf` in `StrokeCache`
Prior to this commit, we would acquire and discard all relevant image
buffers during on every brush daub for each major step of the
experimental image painting process, totaling to four acquisitions
per tile per stroke step.

This commit extracts the acquisition of the `ImBuf` pointers to the
beginning of the stroke step, caching it in a `Map` keyed by the
`TileNumber` of the related tile. We use the `UDIMTilePixels` struct
as the canonical owner of the tile numbers that need to be processed.

This `Map` is stored on the `ImageData` helper struct, which is now
no longer recreated on a per-daub basis, but stored as a `unique_ptr` on
the `StrokeCache`. It is also now responsible for releasing the pointers
as part of its destructor.

A side benefit of this change is that we no longer need to loop over
both the `Image->tiles` and `NodeData->tiles` fields to match tile
numbers when processing the overall stroke step.

In a test file with 5 UDIM tiles, this change results in a 0.10ms (10%)
speedup per stroke step, from 1.98ms to 1.88ms.

Part of #155365

Pull Request: https://projects.blender.org/blender/blender/pulls/155598

source/blender/editors/sculpt_paint/mesh/sculpt_paint_image.cc
@@ -40,20 +40,46 @@ namespace ed::sculpt_paint::paint::image {
 using namespace blender::bke::pbvh::pixels;
 using namespace blender::bke::image;
 
-struct ImageData {
-  Image *image = nullptr;
-  ImageUser *image_user = nullptr;
-
-  ~ImageData() = default;
-
-  static bool init_active_image(Object &ob,
-                                ImageData *r_image_data,
-                                PaintModeSettings &paint_mode_settings)
-  {
-    return BKE_paint_canvas_image_get(
-        &paint_mode_settings, &ob, &r_image_data->image, &r_image_data->image_user);
+ImageData::~ImageData()
+{
+  if (!image || !image_user) {
+    return;
   }
-};
+
+  BLI_assert(buffers.size() <= BLI_listbase_count(&image->tiles));
+  for (ImBuf *buffer : buffers.values()) {
+    BKE_image_release_ibuf(image, buffer, nullptr);
+  }
+  buffers.clear();
+}
+std::unique_ptr<ImageData> ImageData::init_active_image(Object &ob,
+                                                        PaintModeSettings &paint_mode_settings)
+{
+  std::unique_ptr<ImageData> image_data = std::make_unique<ImageData>();
+  if (!BKE_paint_canvas_image_get(
+          &paint_mode_settings, &ob, &image_data->image, &image_data->image_user))
+  {
+    return nullptr;
+  }
+
+  BLI_assert(image_data->image);
+  BLI_assert(image_data->image_user);
+
+  return image_data;
+}
+
+static void fetch_image_buffers(ImageData &image_data, bke::pbvh::Node &node)
+{
+  NodeData &node_data = bke::pbvh::pixels::node_data_get(node);
+  for (const UDIMTilePixels &tile : node_data.tiles) {
+    image_data.buffers.lookup_or_add_cb(tile.tile_number, [&]() {
+      ImageUser tile_user = *image_data.image_user;
+      tile_user.tile = tile.tile_number;
+
+      return BKE_image_acquire_ibuf(image_data.image, &tile_user, nullptr);
+    });
+  }
+}
 
 /** Reading and writing to image buffer with 4 float channels. */
 class ImageBufferFloat4 {
@@ -253,7 +279,7 @@ static void do_paint_pixels(const Depsgraph &depsgraph,
                             Object &object,
                             const Paint &paint,
                             const Brush &brush,
-                            ImageData image_data,
+                            ImageData &image_data,
                             bke::pbvh::Node &node)
 {
   SculptSession &ss = *object.runtime->sculpt_session;
@@ -289,69 +315,59 @@ static void do_paint_pixels(const Depsgraph &depsgraph,
   Vector<float> factors;
   Vector<float> distances;
 
-  ImageUser image_user = *image_data.image_user;
   bool pixels_updated = false;
   for (UDIMTilePixels &tile_data : node_data.tiles) {
-    for (ImageTile &tile : image_data.image->tiles) {
-      ImageTileWrapper image_tile(&tile);
-      if (image_tile.get_tile_number() == tile_data.tile_number) {
-        image_user.tile = image_tile.get_tile_number();
+    ImBuf *image_buffer = image_data.buffers.lookup_default(tile_data.tile_number, nullptr);
+    if (image_buffer == nullptr) {
+      continue;
+    }
 
-        ImBuf *image_buffer = BKE_image_acquire_ibuf(image_data.image, &image_user, nullptr);
-        if (image_buffer == nullptr) {
-          continue;
-        }
+    if (image_buffer->float_buffer.data != nullptr) {
+      kernel_float4.init_brush_color(image_buffer, brush_color);
+    }
+    else {
+      kernel_byte4.init_brush_color(image_buffer, brush_color);
+    }
 
-        if (image_buffer->float_buffer.data != nullptr) {
-          kernel_float4.init_brush_color(image_buffer, brush_color);
-        }
-        else {
-          kernel_byte4.init_brush_color(image_buffer, brush_color);
-        }
+    for (const PackedPixelRow &pixel_row : tile_data.pixel_rows) {
+      if (!brush_test[pixel_row.uv_primitive_index]) {
+        continue;
+      }
 
-        for (const PackedPixelRow &pixel_row : tile_data.pixel_rows) {
-          if (!brush_test[pixel_row.uv_primitive_index]) {
-            continue;
-          }
+      pixel_positions.resize(pixel_row.num_pixels);
+      calc_pixel_row_positions(positions,
+                               pbvh_data.vert_tris,
+                               node_data.uv_primitives.tri_indices,
+                               node_data.uv_primitives.delta_barycentric_coords,
+                               pixel_row,
+                               pixel_positions);
 
-          pixel_positions.resize(pixel_row.num_pixels);
-          calc_pixel_row_positions(positions,
-                                   pbvh_data.vert_tris,
-                                   node_data.uv_primitives.tri_indices,
-                                   node_data.uv_primitives.delta_barycentric_coords,
-                                   pixel_row,
-                                   pixel_positions);
+      factors.resize(pixel_positions.size());
+      factors.fill(1.0f);
 
-          factors.resize(pixel_positions.size());
-          factors.fill(1.0f);
+      distances.resize(pixel_positions.size());
+      calc_brush_distances(
+          ss, pixel_positions, eBrushFalloffShape(brush.falloff_shape), distances);
+      filter_distances_with_radius(cache.radius, distances, factors);
+      apply_hardness_to_distances(cache, distances);
+      calc_brush_strength_factors(cache, brush, distances, factors);
+      calc_brush_texture_factors(ss, brush, pixel_positions, factors);
+      scale_factors(factors, cache.bstrength);
 
-          distances.resize(pixel_positions.size());
-          calc_brush_distances(
-              ss, pixel_positions, eBrushFalloffShape(brush.falloff_shape), distances);
-          filter_distances_with_radius(cache.radius, distances, factors);
-          apply_hardness_to_distances(cache, distances);
-          calc_brush_strength_factors(cache, brush, distances, factors);
-          calc_brush_texture_factors(ss, brush, pixel_positions, factors);
-          scale_factors(factors, cache.bstrength);
+      bool pixels_painted = false;
+      if (image_buffer->float_buffer.data != nullptr) {
+        pixels_painted = kernel_float4.paint(brush, pixel_row, factors, image_buffer);
+      }
+      else {
+        pixels_painted = kernel_byte4.paint(brush, pixel_row, factors, image_buffer);
+      }
 
-          bool pixels_painted = false;
-          if (image_buffer->float_buffer.data != nullptr) {
-            pixels_painted = kernel_float4.paint(brush, pixel_row, factors, image_buffer);
-          }
-          else {
-            pixels_painted = kernel_byte4.paint(brush, pixel_row, factors, image_buffer);
-          }
-
-          if (pixels_painted) {
-            tile_data.mark_dirty(pixel_row);
-          }
-        }
-
-        BKE_image_release_ibuf(image_data.image, image_buffer, nullptr);
-        pixels_updated |= tile_data.flags.dirty;
-        break;
+      if (pixels_painted) {
+        tile_data.mark_dirty(pixel_row);
       }
     }
+
+    pixels_updated |= tile_data.flags.dirty;
   }
 
   node_data.flags.dirty |= pixels_updated;
@@ -371,12 +387,12 @@ static void undo_region_tiles(
 static void push_undo(const NodeData &node_data,
                       Image &image,
                       ImageUser &image_user,
-                      const image::ImageTileWrapper &image_tile,
+                      const TileNumber tile_number,
                       ImBuf &image_buffer,
                       ImBuf **tmpibuf)
 {
   for (const UDIMTileUndo &tile_undo : node_data.undo_regions) {
-    if (tile_undo.tile_number != image_tile.get_tile_number()) {
+    if (tile_undo.tile_number != tile_number) {
       continue;
     }
     int tilex, tiley, tilew, tileh;
@@ -408,22 +424,19 @@ static void push_undo(const NodeData &node_data,
   }
 }
 
-static void do_push_undo_tile(Image &image, ImageUser &image_user, bke::pbvh::Node &node)
+static void do_push_undo_tile(ImageData &image_data, bke::pbvh::Node &node)
 {
   NodeData &node_data = bke::pbvh::pixels::node_data_get(node);
 
   ImBuf *tmpibuf = nullptr;
-  ImageUser local_image_user = image_user;
-  for (ImageTile &tile : image.tiles) {
-    image::ImageTileWrapper image_tile(&tile);
-    local_image_user.tile = image_tile.get_tile_number();
-    ImBuf *image_buffer = BKE_image_acquire_ibuf(&image, &local_image_user, nullptr);
-    if (image_buffer == nullptr) {
+  for (const UDIMTilePixels &tile : node_data.tiles) {
+    ImBuf *buffer = image_data.buffers.lookup_default(tile.tile_number, nullptr);
+    if (buffer == nullptr) {
       continue;
     }
 
-    push_undo(node_data, image, image_user, image_tile, *image_buffer, &tmpibuf);
-    BKE_image_release_ibuf(&image, image_buffer, nullptr);
+    push_undo(
+        node_data, *image_data.image, *image_data.image_user, tile.tile_number, *buffer, &tmpibuf);
   }
   if (tmpibuf) {
     IMB_freeImBuf(tmpibuf);
@@ -444,23 +457,21 @@ static Vector<image::TileNumber> collect_dirty_tiles(MutableSpan<bke::pbvh::Mesh
   return dirty_tiles;
 }
 static void fix_non_manifold_seam_bleeding(bke::pbvh::Tree &pbvh,
-                                           Image &image,
-                                           ImageUser &image_user,
+                                           Map<paint::image::TileNumber, ImBuf *> &buffers,
                                            Span<TileNumber> tile_numbers_to_fix)
 {
   for (image::TileNumber tile_number : tile_numbers_to_fix) {
-    bke::pbvh::pixels::copy_pixels(pbvh, image, image_user, tile_number);
+    bke::pbvh::pixels::copy_pixels(pbvh, buffers, tile_number);
   }
 }
 
 static void fix_non_manifold_seam_bleeding(Object &ob,
-                                           Image &image,
-                                           ImageUser &image_user,
+                                           ImageData &image_data,
                                            MutableSpan<bke::pbvh::MeshNode> nodes,
                                            const IndexMask &node_mask)
 {
   Vector<image::TileNumber> dirty_tiles = collect_dirty_tiles(nodes, node_mask);
-  fix_non_manifold_seam_bleeding(*bke::object::pbvh_get(ob), image, image_user, dirty_tiles);
+  fix_non_manifold_seam_bleeding(*bke::object::pbvh_get(ob), image_data.buffers, dirty_tiles);
 }
 
 /** \} */
@@ -468,24 +479,6 @@ static void fix_non_manifold_seam_bleeding(Object &ob,
 }  // namespace ed::sculpt_paint::paint::image
 
 using namespace blender::ed::sculpt_paint::paint::image;
-
-bool SCULPT_paint_image_canvas_get(PaintModeSettings &paint_mode_settings,
-                                   Object &ob,
-                                   Image **r_image,
-                                   ImageUser **r_image_user)
-{
-  *r_image = nullptr;
-  *r_image_user = nullptr;
-
-  ImageData image_data;
-  if (!ImageData::init_active_image(ob, &image_data, paint_mode_settings)) {
-    return false;
-  }
-
-  *r_image = image_data.image;
-  *r_image_user = image_data.image_user;
-  return true;
-}
 
 bool SCULPT_use_image_paint_brush(PaintModeSettings &settings, Object &ob)
 {
@@ -501,33 +494,38 @@ bool SCULPT_use_image_paint_brush(PaintModeSettings &settings, Object &ob)
 }
 
 void SCULPT_do_paint_brush_image(const Depsgraph &depsgraph,
-                                 PaintModeSettings &paint_mode_settings,
                                  const Sculpt &sd,
                                  Object &ob,
                                  const IndexMask &node_mask)
 {
   const Brush *brush = BKE_paint_brush_for_read(&sd.paint);
+  ed::sculpt_paint::StrokeCache &cache = *ob.runtime->sculpt_session->cache;
 
-  ImageData image_data;
-  if (!ImageData::init_active_image(ob, &image_data, paint_mode_settings)) {
+  if (!cache.image_data) {
     return;
   }
+
+  ImageData &image_data = *cache.image_data;
 
   bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(ob);
   MutableSpan<bke::pbvh::MeshNode> nodes = pbvh.nodes<bke::pbvh::MeshNode>();
 
-  node_mask.foreach_index(
-      [&](const int i) { do_push_undo_tile(*image_data.image, *image_data.image_user, nodes[i]); },
-      exec_mode::grain_size(1));
+  /* Explicitly marked as serial due to image buffer fetching being non-threadsafe */
+  node_mask.foreach_index([&](const int i) { fetch_image_buffers(image_data, nodes[i]); },
+                          exec_mode::serial);
+  node_mask.foreach_index([&](const int i) { do_push_undo_tile(image_data, nodes[i]); },
+                          exec_mode::grain_size(1));
   node_mask.foreach_index(
       [&](const int i) { do_paint_pixels(depsgraph, ob, sd.paint, *brush, image_data, nodes[i]); },
       exec_mode::grain_size(1));
 
-  fix_non_manifold_seam_bleeding(ob, *image_data.image, *image_data.image_user, nodes, node_mask);
+  fix_non_manifold_seam_bleeding(ob, image_data, nodes, node_mask);
 
-  node_mask.foreach_index([&](const int i) {
-    bke::pbvh::pixels::mark_image_dirty(nodes[i], *image_data.image, *image_data.image_user);
-  });
+  node_mask.foreach_index(
+      [&](const int i) {
+        bke::pbvh::pixels::mark_image_dirty(nodes[i], *image_data.image, image_data.buffers);
+      },
+      exec_mode::grain_size(1));
 }
 
 }  // namespace blender

source/blender/blenkernel/BKE_paint_bvh_pixels.hh
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "BLI_array.hh"
+#include "BLI_map.hh"
 #include "BLI_math_vector.hh"
 #include "BLI_rect.h"
 #include "BLI_vector.hh"
@@ -42,7 +43,7 @@ struct PackedPixelRow {
  */
 struct UDIMTilePixels {
   /** UDIM Tile number. */
-  short tile_number;
+  image::TileNumber tile_number;
 
   struct {
     bool dirty : 1;
@@ -140,19 +141,21 @@ struct NodeData {
     }
   }
 
-  void mark_region(Image &image, const image::ImageTileWrapper &image_tile, ImBuf &image_buffer)
+  void mark_region(UDIMTilePixels &tile,
+                   Image &image,
+                   const image::ImageTileWrapper &image_tile,
+                   ImBuf &image_buffer)
   {
-    UDIMTilePixels *tile = find_tile_data(image_tile);
-    if (tile && tile->flags.dirty) {
+    if (tile.flags.dirty) {
       if (image_buffer.planes == 8) {
         image_buffer.planes = 32;
         BKE_image_partial_update_mark_full_update(&image);
       }
       else {
         BKE_image_partial_update_mark_region(
-            &image, image_tile.image_tile, &image_buffer, &tile->dirty_region);
+            &image, image_tile.image_tile, &image_buffer, &tile.dirty_region);
       }
-      tile->clear_dirty();
+      tile.clear_dirty();
     }
   }
 
@@ -347,13 +350,14 @@ struct PBVHData {
 };
 
 NodeData &node_data_get(bke::pbvh::Node &node);
-void mark_image_dirty(bke::pbvh::Node &node, Image &image, ImageUser &image_user);
+void mark_image_dirty(bke::pbvh::Node &node,
+                      Image &image,
+                      Map<image::TileNumber, ImBuf *> &buffers);
 PBVHData &data_get(bke::pbvh::Tree &pbvh);
 void collect_dirty_tiles(bke::pbvh::Node &node, Vector<image::TileNumber> &r_dirty_tiles);
 
 void copy_pixels(bke::pbvh::Tree &pbvh,
-                 Image &image,
-                 ImageUser &image_user,
+                 Map<image::TileNumber, ImBuf *> &buffers,
                  image::TileNumber tile_number);
 
 }  // namespace blender::bke::pbvh::pixels

source/blender/blenkernel/intern/pbvh_pixels.cc
@@ -454,22 +454,39 @@ PBVHData &data_get(Tree &pbvh)
   return *data;
 }
 
-void mark_image_dirty(Node &node, Image &image, ImageUser &image_user)
+/* TODO: This is a awkward to have to re-iterate over the image tiles to find the matching tile.
+ * Investigate storing the pointer on the `UDIMTilePixels` struct instead, or storing this as a
+ * second map in `ImageData` */
+static std::optional<image::ImageTileWrapper> find_image_tile(Image &image,
+                                                              const image::TileNumber tile_number)
+{
+  for (ImageTile &image_tile : image.tiles) {
+    image::ImageTileWrapper wrapper = image::ImageTileWrapper(&image_tile);
+    if (wrapper.get_tile_number() == tile_number) {
+      return std::make_optional(wrapper);
+    }
+  }
+  /* Logically, we should be unable to reference a image_tile here without having first gotten it
+   * from the image tile itself. */
+  BLI_assert(0);
+  return std::nullopt;
+}
+
+void mark_image_dirty(bke::pbvh::Node &node,
+                      Image &image,
+                      Map<image::TileNumber, ImBuf *> &buffers)
 {
   BLI_assert(node.pixels_ != nullptr);
   NodeData *node_data = node.pixels_;
   if (node_data->flags.dirty) {
-    ImageUser local_image_user = image_user;
-    for (ImageTile &tile : image.tiles) {
-      image::ImageTileWrapper image_tile(&tile);
-      local_image_user.tile = image_tile.get_tile_number();
-      ImBuf *image_buffer = BKE_image_acquire_ibuf(&image, &local_image_user, nullptr);
-      if (image_buffer == nullptr) {
+    for (UDIMTilePixels &tile : node_data->tiles) {
+      std::optional<image::ImageTileWrapper> image_tile = find_image_tile(image, tile.tile_number);
+      ImBuf *image_buffer = buffers.lookup_default(tile.tile_number, nullptr);
+      if (image_buffer == nullptr || !image_tile) {
         continue;
       }
 
-      node_data->mark_region(image, image_tile, *image_buffer);
-      BKE_image_release_ibuf(&image, image_buffer, nullptr);
+      node_data->mark_region(tile, image, *image_tile, *image_buffer);
     }
     node_data->flags.dirty = false;
   }

source/blender/blenkernel/intern/pbvh_pixels_copy.cc
@@ -545,9 +545,10 @@ void copy_update(bke::pbvh::Tree &pbvh,
   }
 }
 
+/* TODO: Allow passing `ImageData` here, but this requires pulling this entire class out of the
+ * bke namespace. */
 void copy_pixels(bke::pbvh::Tree &pbvh,
-                 Image &image,
-                 ImageUser &image_user,
+                 Map<image::TileNumber, ImBuf *> &buffers,
                  image::TileNumber tile_number)
 {
   PBVHData &pbvh_data = data_get(pbvh);
@@ -558,9 +559,7 @@ void copy_pixels(bke::pbvh::Tree &pbvh,
     return;
   }
 
-  ImageUser tile_user = image_user;
-  tile_user.tile = tile_number;
-  ImBuf *tile_buffer = BKE_image_acquire_ibuf(&image, &tile_user, nullptr);
+  ImBuf *tile_buffer = buffers.lookup_default(tile_number, nullptr);
   if (tile_buffer == nullptr) {
     /* No tile buffer found to copy. */
     return;
@@ -570,8 +569,6 @@ void copy_pixels(bke::pbvh::Tree &pbvh,
   threading::parallel_for(tile.groups.index_range(), THREADING_GRAIN_SIZE, [&](IndexRange range) {
     tile.copy_pixels(*tile_buffer, range);
   });
-
-  BKE_image_release_ibuf(&image, tile_buffer, nullptr);
 }
 
 }  // namespace blender::bke::pbvh::pixels

source/blender/editors/sculpt_paint/mesh/sculpt.cc
@@ -2798,34 +2798,27 @@ static void update_brush_local_mat(const Sculpt &sd, Object &ob)
 /** \name Texture painting
  * \{ */
 
-static bool sculpt_needs_pbvh_pixels(PaintModeSettings &paint_mode_settings,
-                                     const Brush &brush,
-                                     Object &ob)
+static bool sculpt_needs_pbvh_pixels(const Brush &brush, const Object &ob)
 {
   if (brush.sculpt_brush_type == SCULPT_BRUSH_TYPE_PAINT &&
       USER_EXPERIMENTAL_TEST(&U, use_sculpt_texture_paint))
   {
-    Image *image;
-    ImageUser *image_user;
-    return SCULPT_paint_image_canvas_get(paint_mode_settings, ob, &image, &image_user);
+    return ob.runtime->sculpt_session->cache->image_data.get();
   }
 
   return false;
 }
 
-static void sculpt_pbvh_update_pixels(const Depsgraph &depsgraph,
-                                      PaintModeSettings &paint_mode_settings,
-                                      Object &ob)
+static void sculpt_pbvh_update_pixels(const Depsgraph &depsgraph, Object &ob)
 {
   BLI_assert(ob.type == OB_MESH);
 
-  Image *image;
-  ImageUser *image_user;
-  if (!SCULPT_paint_image_canvas_get(paint_mode_settings, ob, &image, &image_user)) {
+  ed::sculpt_paint::StrokeCache &cache = *ob.runtime->sculpt_session->cache;
+  if (!cache.image_data) {
     return;
   }
 
-  bke::pbvh::build_pixels(depsgraph, ob, *image, *image_user);
+  bke::pbvh::build_pixels(depsgraph, ob, *cache.image_data->image, *cache.image_data->image_user);
 }
 
 /** \} */
@@ -3263,10 +3256,10 @@ static void do_brush_action(const Depsgraph &depsgraph,
 
   const bool use_original = brush_type_needs_original(brush.sculpt_brush_type) ? true :
                                                                                  !ss.cache->accum;
-  const bool use_pixels = sculpt_needs_pbvh_pixels(paint_mode_settings, brush, ob);
+  const bool use_pixels = sculpt_needs_pbvh_pixels(brush, ob);
 
-  if (sculpt_needs_pbvh_pixels(paint_mode_settings, brush, ob)) {
-    sculpt_pbvh_update_pixels(depsgraph, paint_mode_settings, ob);
+  if (sculpt_needs_pbvh_pixels(brush, ob)) {
+    sculpt_pbvh_update_pixels(depsgraph, ob);
 
     texnode_mask = pbvh_gather_texpaint(ob, brush, use_original, 1.0f, memory);
 
@@ -5727,6 +5720,9 @@ void SculptPaintStroke::stroke_cache_init(const BrushStrokeMode stroke_mode,
       SCULPT_use_image_paint_brush(*paint_mode_settings_, ob))
   {
     cache->accum = true;
+
+    cache->image_data = paint::image::ImageData::init_active_image(
+        ob, this->scene->toolsettings->paint_mode);
   }
 
   if (BKE_brush_color_jitter_get_settings(this->paint, brush)) {

source/blender/editors/sculpt_paint/mesh/sculpt_intern.hh
@@ -12,6 +12,7 @@
 
 #include "BKE_brush.hh"
 #include "BKE_bvhutils.hh"
+#include "BKE_image_wrappers.hh"
 #include "BKE_paint.hh"
 #include "BKE_paint_bvh.hh"
 #include "BKE_subdiv_ccg.hh"
@@ -159,6 +160,21 @@ struct ProjectBrushTarget {
   bke::BVHTreeFromMesh tree_data;
   float4x4 active_to_target_matrix;
 };
+
+namespace paint::image {
+struct ImageData : NonCopyable {
+  Image *image = nullptr;
+  ImageUser *image_user = nullptr;
+
+  Map<bke::image::TileNumber, ImBuf *> buffers = {};
+
+  ~ImageData();
+
+  static std::unique_ptr<ImageData> init_active_image(Object &ob,
+                                                      PaintModeSettings &paint_mode_settings);
+};
+
+}  // namespace paint::image
 
 /**
  * This structure contains all the temporary data
@@ -430,6 +446,8 @@ struct StrokeCache {
 
   float4x4 stroke_local_mat = float4x4::identity();
   float multiplane_scrape_angle = 0.0f;
+
+  std::unique_ptr<paint::image::ImageData> image_data;
 
   StrokeCache();
   ~StrokeCache();
@@ -931,19 +949,7 @@ float object_space_radius_get(const ViewContext &vc,
 /** \name 3D Texture Paint (Experimental)
  * \{ */
 
-/**
- * \brief Get the image canvas for painting on the given object.
- *
- * \return #true if an image is found. The #r_image and #r_image_user fields are filled with
- * the image and image user. Returns false when the image isn't found. In the later case the
- * r_image and r_image_user are set to NULL.
- */
-bool SCULPT_paint_image_canvas_get(PaintModeSettings &paint_mode_settings,
-                                   Object &ob,
-                                   Image **r_image,
-                                   ImageUser **r_image_user) ATTR_NONNULL();
 void SCULPT_do_paint_brush_image(const Depsgraph &depsgraph,
-                                 PaintModeSettings &paint_mode_settings,
                                  const Sculpt &sd,
                                  Object &ob,
                                  const IndexMask &node_mask);

source/blender/editors/sculpt_paint/mesh/sculpt_paint_color.cc
@@ -563,7 +563,7 @@ void do_paint_brush(const Depsgraph &depsgraph,
                     const IndexMask &texnode_mask)
 {
   if (SCULPT_use_image_paint_brush(paint_mode_settings, ob)) {
-    SCULPT_do_paint_brush_image(depsgraph, paint_mode_settings, sd, ob, texnode_mask);
+    SCULPT_do_paint_brush_image(depsgraph, sd, ob, texnode_mask);
     return;
   }
 

FrenkyBojler/blender-ios

commit 202e864cfc430876111ab71f1b32c95c5ea9edd0

Fix: Experimental texture paint crash / memory leak
Introduced with 1faa192978da49c991b71e8c65e2cf43f8f06e65

`mark_image_dirty` is non-threadsafe, due to the partial image updating
being non-threadsafe.

source/blender/editors/sculpt_paint/mesh/sculpt_paint_image.cc
@@ -535,12 +535,10 @@ void SCULPT_do_paint_brush_image(const Depsgraph &depsgraph,
 
   fix_non_manifold_seam_bleeding(ob, image_data, nodes, pixel_nodes, node_mask);
 
-  node_mask.foreach_index(
-      [&](const int i) {
-        bke::pbvh::pixels::mark_image_dirty(
-            nodes[i], pixel_nodes[i], *image_data.image, image_data.buffers);
-      },
-      exec_mode::grain_size(1));
+  node_mask.foreach_index([&](const int i) {
+    bke::pbvh::pixels::mark_image_dirty(
+        nodes[i], pixel_nodes[i], *image_data.image, image_data.buffers);
+  });
 }
 
 }  // namespace blender