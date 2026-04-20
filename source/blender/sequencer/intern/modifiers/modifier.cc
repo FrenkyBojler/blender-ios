580e715f3d48d3635f68288682626de458670ed5
Philipp Oeser <philipp@blender.org>
2026-04-20 12:10:19 +0200

    Fix #157543: VSE wrong (or no) active modifier after duplication
    When duplicating a modifier, the previous modifier remained active (and
    the duplicate wasnt).
    When duplicating a strip (or when duplicating whole modifierstack from
    active to selected strips), no modifier was active at all.
    
    This actually worked in 5.0 (for the second case) so somewhat caused by
    7777e298a255
    
    The general behavior should be (and is when doing this with object
    modifiers):
    - Duplicating a modifier should make the **duplicated** modifier active
    - Duplicating a modifiers stack should keep the corresponding modifier
    active on the target that was also active on the source
    
    So to achieve that, dont clear the `STRIP_MODIFIER_FLAG_ACTIVE` on a
    duplicate, but instead make the duplicate active (if the source was).

--- source/blender/sequencer/intern/modifiers/modifier.cc
+++ source/blender/sequencer/intern/modifiers/modifier.cc
@@ -553,8 +553,12 @@ StripModifierData *modifier_copy(Strip &strip_dst, StripModifierData *mod_src, c
 {
   const StripModifierTypeInfo *smti = modifier_type_info_get(mod_src->type);
   StripModifierData *mod_new = MEM_dupalloc(mod_src);
-  /* Ensure at most one active modifier at a time. */
-  mod_new->flag &= ~STRIP_MODIFIER_FLAG_ACTIVE;
+
+  /* Make the duplicate active (if the source was), ensuring at most one active modifier at a time.
+   */
+  if (mod_src->flag & STRIP_MODIFIER_FLAG_ACTIVE) {
+    modifier_set_active(&strip_dst, mod_new);
+  }
 
   mod_new->system_properties = nullptr;
   if (mod_src->system_properties) {
