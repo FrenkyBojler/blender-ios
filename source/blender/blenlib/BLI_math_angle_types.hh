86929ef6ff20e12f1b6fd9a10bf29a2d622e3266
Jacques Lucke <jacques@blender.org>
2026-04-20 13:04:14 +0200

    more fixes

--- source/blender/blenlib/BLI_math_angle_types.hh
+++ source/blender/blenlib/BLI_math_angle_types.hh
@@ -358,7 +358,7 @@ template<typename T> struct AngleCartesianBase {
  *
  * It offers the best accuracy for fractions of Pi radian angles. For instance
  * `sin(AngleFraction::tau() * n - AngleFraction::pi() / 2)` will exactly return `-1` for any `n`
- * within [-INT_MAX..INT_MAX]. This holds true even with very high radian values.
+ * within [INT_MIN..INT_MAX]. This holds true even with very high radian values.
  *
  * Arithmetic operators are relatively cheap (4 operations for addition, 2 for multiplication) but
  * not as cheap as a `AngleRadian`. Another nice property is that the `cos()` and `sin()` functions
