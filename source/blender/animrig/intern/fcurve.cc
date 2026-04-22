056a319c20a30105b88eea168ccdb8489ce3ad12
Wayne Dixon <thewaylow@gmail.com>
2026-04-22 12:17:33 +1000

    Allow 'Generated' keyframe type to be overwritten

--- source/blender/animrig/intern/fcurve.cc
+++ source/blender/animrig/intern/fcurve.cc
@@ -176,9 +176,12 @@ int insert_bezt_fcurve(FCurve *fcu, const BezTriple *bezt, eInsertKeyFlags flag)
     if (replace) {
       /* `i` may in rare cases exceed array bounds. */
       if ((i >= 0) && (i < fcu->totvert)) {
-        if (flag & INSERTKEY_OVERWRITE_FULL) {
-          fcu->bezt[i] = *bezt;
-        }
+        /* Allow 'generated' keyframes to be overwritten too. */
+        if (flag & INSERTKEY_OVERWRITE_FULL)
+          || (BEZKEYTYPE(&fcu->bezt[i]) == BEZT_KEYTYPE_GENERATED)
+          {
+            fcu->bezt[i] = *bezt;
+          }
         else {
           replace_bezt_keyframe_ypos(&fcu->bezt[i], bezt);
         }
