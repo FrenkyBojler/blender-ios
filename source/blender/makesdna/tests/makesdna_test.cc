b88b980f4f8090d2d3ff833d160ea9e4cecaf0e4
Brecht Van Lommel <brecht@blender.org>
2026-04-23 02:14:42 +0200

    Fix: Tests: DNA: Typo in my_matrix member name
    A missing '[' caused the member name to be 'my_matrix[4]4]' instead of
    'my_matrix[4][4]', which would lead to a lookup failure in SDNA.
    
    This issue was introduced in cebd27159e1.

--- source/blender/makesdna/tests/makesdna_test.cc
+++ source/blender/makesdna/tests/makesdna_test.cc
@@ -154,7 +154,7 @@ TEST_F(SDNATest, struct_member_offset_by_name_without_alias)
                 sdna, "StructWithVectorMatrixTypes", "float", "my_float3[3]"),
             offsetof(StructWithVectorMatrixTypes, my_float3));
   EXPECT_EQ(DNA_struct_member_offset_by_name_without_alias(
-                sdna, "StructWithVectorMatrixTypes", "float", "my_matrix[4]4]"),
+                sdna, "StructWithVectorMatrixTypes", "float", "my_matrix[4][4]"),
             offsetof(StructWithVectorMatrixTypes, my_matrix));
 }
 
