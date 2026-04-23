0fe8b45c2e00a0efd5e50b7b200886799cd2ccd3
Guillermo Venegas <guillermovcra@gmail.com>
2026-04-23 14:28:27 -0600

    init

--- source/blender/editors/interface/views/interface_view.cc
+++ source/blender/editors/interface/views/interface_view.cc
@@ -293,9 +293,9 @@ AbstractViewItem *region_views_find_item_at(const ARegion &region, const int xy[
   return item_but->view_item;
 }
 
-AbstractViewItem *region_views_find_active_item(const ARegion *region)
+AbstractViewItem *region_views_find_active_item(const ARegion *region, AbstractView *view)
 {
-  auto *item_but = static_cast<ButtonViewItem *>(view_item_find_active(region));
+  auto *item_but = static_cast<ButtonViewItem *>(view_item_find_active(region, view));
   if (!item_but) {
     return nullptr;
   }
@@ -305,7 +305,7 @@ AbstractViewItem *region_views_find_active_item(const ARegion *region)
 
 Button *region_views_find_active_item_but(const ARegion *region)
 {
-  return view_item_find_active(region);
+  return view_item_find_active(region, nullptr);
 }
 
 void region_views_clear_search_highlight(const ARegion *region)
