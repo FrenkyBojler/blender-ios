a3b2221e20ab15c206928e28f9663d988bb7bfcf
Jacques Lucke <jacques@blender.org>
2026-04-23 21:15:08 +0200

    improve comment

--- source/blender/blenlib/BLI_cpp_type.hh
+++ source/blender/blenlib/BLI_cpp_type.hh
@@ -454,7 +454,9 @@ inline bool operator!=(const CPPType &a, const CPPType &b)
 template<typename T> inline const CPPType &CPPType::get()
 {
   const CPPType &type = cpp_type_impl<std::decay_t<T>>.ref();
-  /* Should have been initialized by #BLI_CPP_TYPE_REGISTER. */
+  /* Should have been initialized by #BLI_CPP_TYPE_REGISTER.
+   * When this is hit in test code, make sure the test calls `register_cpp_types` (for blenlib
+   * tests) or `BKE_cpp_types_init` (for general tests). */
   BLI_assert(type.size > 0);
   return type;
 }
