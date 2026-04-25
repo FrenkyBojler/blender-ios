feaa23b2ca09dd86e63b415bc3679382e406ae7d
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-04-25 19:09:08 +0000

    add function to file

---
+++ source/blender/editors/transform/transform_snap_object_lattice.cc
@@ -0,0 +1,18 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup edtransform
+ */
+
+#include "BKE_object.hh"
+#include "BKE_lattice.hh"
+
+#include "ED_transform_snap_object_context.hh"
+
+#include "transform_snap_object.hh"
+
+namespace blender::ed::transform {
+eSnapMode snapLattice(SnapObjectContext *sctx, const Object *ob_eval, const float4x4 &obmat) {}
+} // namespace blender::ed::transform
