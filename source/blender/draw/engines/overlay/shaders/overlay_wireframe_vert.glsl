5533c082da15df944886236495acffc3a981a6f0
Le Gia Bao <legiabao7300@gmail.com>
2026-04-25 15:02:56 -0400

    Fix #155607: Incorrect Point Opacity

--- source/blender/draw/engines/overlay/shaders/overlay_wireframe_vert.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_wireframe_vert.glsl
@@ -154,9 +154,9 @@ void main()
   }
 
 #if defined(POINTS)
-  final_color = wire_col.rgbb;
-  final_color_inner = rim_col.rgbb;
-
+  final_color = float4(wire_col * wire_opacity, wire_opacity);
+  final_color_inner = float4(rim_col * wire_opacity, wire_opacity);
+  
 #else
   /* Convert to screen position [0..sizeVp]. */
   edge_start = ((gl_Position.xy / gl_Position.w) * 0.5f + 0.5f) * uniform_buf.size_viewport;
