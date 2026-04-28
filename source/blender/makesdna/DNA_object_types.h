f216fd9e58d768f53bd88bed57876226ae50cb8e
Colin Basnett <5035660+cmbasnett@users.noreply.github.com>
2026-04-28 13:36:05 -0700

    Fixed missing header rename

--- source/blender/makesdna/DNA_object_types.h
+++ source/blender/makesdna/DNA_object_types.h
@@ -464,7 +464,7 @@ struct Object {
   /** String describing sub-object info. */
   char parsubstr[/*MAX_NAME*/ 64] = "";
   struct Object *parent = nullptr, *track = nullptr;
-  float parent_bone_head_tail = {};
+  float parent_bone_head_tail_factor = {};
   char _pad4[4] = {};
   /* Proxy pointer are deprecated, only kept for conversion to liboverrides. */
   DNA_DEPRECATED struct Object *proxy = nullptr;
