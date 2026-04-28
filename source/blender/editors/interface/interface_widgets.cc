42c6582adadb5ca3b39a36f0a0ddb7f8884cd7e1
Harley Acheson <harley.acheson@gmail.com>
2026-04-28 08:49:35 -0700

    ui: Show Placeholders During Focus When Empty
    When we added input placeholders three years ago it seemed like the
    general consensus was to never show it while in focus. The argument
    at the time was that users might feel a need to clear that text
    before entering theirs. Since that time the consensus has moved to
    only hiding the placeholder when text is entered. This is the current
    recommendation for HTML and is how Chrome, Firefox, and Edge handle the
    placeholder HTML attribute.

--- source/blender/editors/interface/interface_widgets.cc
+++ source/blender/editors/interface/interface_widgets.cc
@@ -2634,7 +2634,7 @@ static void widget_draw_text(const uiFontStyle *fstyle,
   }
 
   /* Show placeholder text if the input is empty and not being edited. */
-  if (!drawstr[0] && !but->editstr && ELEM(but->type, ButtonType::Text, ButtonType::SearchMenu)) {
+  if (ELEM(but->type, ButtonType::Text, ButtonType::SearchMenu) && (!drawstr[0] || !but->editstr)) {
     const char *placeholder = button_placeholder_get(but);
     if (placeholder && placeholder[0]) {
       FontStyleDrawParams params{};
