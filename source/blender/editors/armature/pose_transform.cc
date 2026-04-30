9e5ee65125ecc43953d587e0afe4cec0cdc9f603
Sybren A. Stüvel <sybren@blender.org>
2026-04-30 15:55:45 +0200

    make format

--- source/blender/editors/armature/pose_transform.cc
+++ source/blender/editors/armature/pose_transform.cc
@@ -240,9 +240,9 @@ static void applyarmature_process_selected_recursive(bArmature *arm,
   ApplyArmature_ParentState new_pstate{};
   new_pstate.bone = bone;
 
-  if (std::find_if(selected.begin(),
-                   selected.end(),
-                   [&](const PointerRNA &ptr) { return ptr.data == pchan; }) != selected.end())
+  if (std::find_if(selected.begin(), selected.end(), [&](const PointerRNA &ptr) {
+        return ptr.data == pchan;
+      }) != selected.end())
   {
     /* SELECTED BONE: Snap to final pose transform minus un-applied parent effects.
      *
