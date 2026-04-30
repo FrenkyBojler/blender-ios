05501c6bd874545e15edfdeac3b884d4271ce3d3
Campbell Barton <campbell@blender.org>
2026-04-30 14:48:47 +1000

    Add code-comment elaborating on why the fix is needed

--- source/blender/bmesh/intern/bmesh_walkers_impl.cc
+++ source/blender/bmesh/intern/bmesh_walkers_impl.cc
@@ -1473,6 +1473,8 @@ static void bmw_EdgeringWalker_begin(BMWalker *walker, void *data)
 
   /* Add both sides so both directions are walked. */
   if (lwalk->l->radial_next != lwalk->l) {
+    /* When `no_calc` is true, the walker hit a delimiting edge and can't step further,
+     * so add the alt walker to traverse the ring in the opposite direction. See #157860. */
     if (lwalk->no_calc || (delimit_ngon ? (lwalk->l->f->len != 4) : (lwalk->l->f->len % 2 != 0))) {
       BMwEdgeringWalker *lwalk_alt = static_cast<BMwEdgeringWalker *>(BMW_state_add(walker));
       lwalk_alt->l = lwalk->l->radial_next;
