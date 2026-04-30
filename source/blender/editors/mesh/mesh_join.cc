2dafa25a70200fa460aa0c4983638147e2d45190
Philipp Oeser <philipp@blender.org>
2026-04-30 16:50:17 +0200

    from review:
    only do join preparation when necessary

--- source/blender/editors/mesh/mesh_join.cc
+++ source/blender/editors/mesh/mesh_join.cc
@@ -704,15 +704,15 @@ wmOperatorStatus join_objects_exec(bContext *C, wmOperator *op)
                           *dst_mesh);
 
   /* Copy multires data to the out-of-main mesh. */
-  for (const int i : objects_to_join.index_range().drop_front(1)) {
-    Object &src_object = *objects_to_join[i];
-    multiresModifier_prepare_join(depsgraph, scene, &src_object, active_object);
-    if (MultiresModifierData *mmd = get_multires_modifier(scene, &src_object, true)) {
-      object::iter_other(
-          bmain, &src_object, true, object::multires_update_totlevels, &mmd->totlvl);
-    }
-  }
   if (get_multires_modifier(scene, active_object, true)) {
+    for (const int i : objects_to_join.index_range().drop_front(1)) {
+      Object &src_object = *objects_to_join[i];
+      multiresModifier_prepare_join(depsgraph, scene, &src_object, active_object);
+      if (MultiresModifierData *mmd = get_multires_modifier(scene, &src_object, true)) {
+        object::iter_other(
+            bmain, &src_object, true, object::multires_update_totlevels, &mmd->totlvl);
+      }
+    }
     if (std::any_of(objects_to_join.begin(), objects_to_join.end(), [](const Object *object) {
           const Mesh &src_mesh = *id_cast<const Mesh *>(object->data);
           return CustomData_has_layer(&src_mesh.corner_data, CD_MDISPS);
