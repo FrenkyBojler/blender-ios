c351a29cf59226782c7fc4689ec26b5cab78e3df
Falk David <falk@blender.org>
2026-05-01 18:04:11 +0200

    Fix crash

--- source/blender/editors/object/object_add.cc
+++ source/blender/editors/object/object_add.cc
@@ -4636,7 +4636,9 @@ static wmOperatorStatus object_convert_exec(bContext *C, wmOperator *op)
        * It is not enough to tag only geometry and rely on the curve parenting relations because
        * this relation is lost when curve is converted to mesh. */
       DEG_id_tag_update(&ob->id, ID_RECALC_GEOMETRY | ID_RECALC_TRANSFORM);
-      ob->data->tag &= ~ID_TAG_DOIT; /* flag not to convert this datablock again */
+      if (ob->data) {
+        ob->data->tag &= ~ID_TAG_DOIT; /* flag not to convert this datablock again */
+      }
     }
   }
 
