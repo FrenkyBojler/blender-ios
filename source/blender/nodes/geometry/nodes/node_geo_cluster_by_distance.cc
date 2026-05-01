b97cbf4b909d020ca29527aaa62fe59ea9e903a4
Hans Goudey <hans@blender.org>
2026-05-01 10:40:48 -0400

    Remove distance==0 special case for now

--- source/blender/nodes/geometry/nodes/node_geo_cluster_by_distance.cc
+++ source/blender/nodes/geometry/nodes/node_geo_cluster_by_distance.cc
@@ -133,44 +133,6 @@ class ClusterByDistanceFieldInput final : public bke::GeometryFieldInput {
     const int avg_group_size = domain_size / group_indices.size();
     const int grain_size = std::max(8192 / avg_group_size, 1);
 
-    if (distance_ <= 0.0f) {
-      threading::parallel_for(IndexRange(groups_num), grain_size, [&](const IndexRange range) {
-        for (const int group_i : range) {
-          const IndexMask &group_mask = all_indices_by_group_id[group_i];
-
-          /* Using a map provides better time complexity compared to kdtree, while yet both are not
-           * parallel at the moment, so map is the better choice. */
-          Map<float3, int> clusters;
-          group_mask.foreach_index(
-              [&](const int index) { clusters.add(positions[index], index); });
-
-          if (clusters.size() == group_mask.size()) {
-            group_mask.foreach_index_optimized<int>(
-                [&](const int index) { cluster_ids[index] = index; }, exec_mode::parallel);
-            continue;
-          }
-
-          if (clusters.size() == 1) {
-            const int first_selected = group_mask.first();
-            BLI_assert(clusters.lookup(positions[first_selected]) == first_selected);
-            index_mask::masked_fill<int>(
-                cluster_ids.as_mutable_span(), first_selected, group_mask);
-            continue;
-          }
-
-          group_mask.foreach_index(
-              [&](const int index) { cluster_ids[index] = clusters.lookup(positions[index]); },
-              exec_mode::parallel);
-        }
-      });
-
-#ifndef NDEBUG
-      mask.foreach_index([&](const int i) { BLI_assert(cluster_ids[i] != NO_CLUSTER_VALUE); });
-#endif
-
-      return VArray<int>::from_container(std::move(cluster_ids));
-    }
-
     threading::parallel_for(IndexRange(groups_num), grain_size, [&](const IndexRange range) {
       Vector<int, 64> buffer;
       for (const int group_i : range) {
