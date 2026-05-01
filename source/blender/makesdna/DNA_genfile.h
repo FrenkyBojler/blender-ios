blender/blender

commit 054e10ac135eee0eb6d4a0e3ca5a3e8ae5a94cea

fix [#30090] Cloth modifier related crash when loading old file
own fault when adding int64 support. `void` type was being used to workaround a different bug in the ClothSimSettings struct.

source/blender/makesdna/DNA_genfile.h
@@ -48,13 +48,13 @@ typedef enum eSDNA_Type {
 	SDNA_TYPE_ULONG    = 6,
 	SDNA_TYPE_FLOAT    = 7,
 	SDNA_TYPE_DOUBLE   = 8,
-	SDNA_TYPE_INT64    = 9,
-	SDNA_TYPE_UINT64   = 10
-	/* ,SDNA_TYPE_VOID     = 11 */ /* nothing uses yet */
+	/* ,SDNA_TYPE_VOID = 9 */ /* nothing uses yet */
+	SDNA_TYPE_INT64    = 10,
+	SDNA_TYPE_UINT64   = 11
 } eSDNA_Type;
 
 /* define so switch statements don't complain */
-#define SDNA_TYPE_VOID 11
+#define SDNA_TYPE_VOID 9
 
 struct SDNA *DNA_sdna_from_data(void *data, int datalen, int do_endian_swap);
 void DNA_sdna_free(struct SDNA *sdna);

FrenkyBojler/blender-ios

commit 5e6b5deb82e46fbc38f38be534d51e9ffd441530

Fix: DNA: Wrong versioning of 64 bit integers
The eSDNA_Type enum and type table were out of sync. This casued int64_t
to be interpreted as void, and uint64_t to be interpreted as int64_t.

However as far as I can tell there have been no cases where we changed
such members to a smaller integer type, so no user facing bugs.

Issue introduced in 054e10ac135eee0eb6d4.

source/blender/makesdna/DNA_genfile.h
@@ -42,11 +42,9 @@ enum eSDNA_Type {
   /* SDNA_TYPE_ULONG    = 6, */ /* deprecated (use as int) */
   SDNA_TYPE_FLOAT = 7,
   SDNA_TYPE_DOUBLE = 8,
-/* ,SDNA_TYPE_VOID = 9 */
-/* define so switch statements don't complain */
-#define SDNA_TYPE_VOID 9
-  SDNA_TYPE_INT64 = 10,
-  SDNA_TYPE_UINT64 = 11,
+  SDNA_TYPE_INT64 = 9,
+  SDNA_TYPE_UINT64 = 10,
+  SDNA_TYPE_VOID = 11,
   SDNA_TYPE_INT8 = 12,
   /**
    * Type used for untyped raw bytes buffers (written by #BlendWriter::write_raw and read by

source/blender/makesdna/intern/dna_genfile.cc
@@ -469,12 +469,17 @@ static bool init_structDNA(SDNA *sdna, const char **r_error_message)
   }
 
   {
-    /* second part of gravity problem, setting "gravity" type to void */
+    /* second part of gravity problem, setting "gravity" type to void.
+     *
+     * NOTE: The gravity fix only applies to pre-2011 blend-files, which had `void` at
+     * type index 9 in their SDNA (`int64_t` was inserted before `void` in the `types`
+     * array later, shifting `void` to a later index in current Blender's SDNA, but the
+     * file's SDNA loaded here still has `void` at 9). */
     if (member_index_gravity_fix > -1) {
       for (int struct_index = 0; struct_index < sdna->structs_num; struct_index++) {
         sp = reinterpret_cast<short *>(sdna->structs[struct_index]);
         if (STREQ(sdna->types[sp[0]], "ClothSimSettings")) {
-          sp[10] = SDNA_TYPE_VOID;
+          sp[10] = 9;
         }
       }
     }
@@ -845,8 +850,10 @@ static void cast_primitive_type(const eSDNA_Type old_type,
         old_value_f = double(value);
         break;
       }
+      case SDNA_TYPE_VOID:
       case SDNA_TYPE_RAW_DATA:
-        BLI_assert_msg(false, "Conversion from SDNA_TYPE_RAW_DATA is not supported");
+        BLI_assert_msg(false,
+                       "Conversion from SDNA_TYPE_VOID/SDNA_TYPE_RAW_DATA is not supported");
         break;
     }
 
@@ -887,8 +894,9 @@ static void cast_primitive_type(const eSDNA_Type old_type,
       case SDNA_TYPE_INT8:
         *reinterpret_cast<int8_t *>(new_data) = int8_t(old_value_i);
         break;
+      case SDNA_TYPE_VOID:
       case SDNA_TYPE_RAW_DATA:
-        BLI_assert_msg(false, "Conversion to SDNA_TYPE_RAW_DATA is not supported");
+        BLI_assert_msg(false, "Conversion to SDNA_TYPE_VOID/SDNA_TYPE_RAW_DATA is not supported");
         break;
     }
 
@@ -1687,6 +1695,8 @@ int DNA_elem_type_size(const eSDNA_Type elem_nr)
     case SDNA_TYPE_INT64:
     case SDNA_TYPE_UINT64:
       return 8;
+    case SDNA_TYPE_VOID:
+      return 0;
     case SDNA_TYPE_RAW_DATA:
       BLI_assert_msg(false, "Operations on the size of SDNA_TYPE_RAW_DATA is not supported");
       return 0;
@@ -2171,6 +2181,7 @@ static void print_single_struct_recursive(const SDNA &sdna,
                 fmt::format_to(dst, "{}", *reinterpret_cast<const uint64_t *>(current_data));
                 break;
               }
+              case SDNA_TYPE_VOID:
               case SDNA_TYPE_RAW_DATA: {
                 BLI_assert_unreachable();
                 break;

source/blender/makesdna/intern/makesdna.cc
@@ -41,6 +41,7 @@
 #include "BLI_vector.hh"
 #include "BLI_vector_set.hh"
 
+#include "DNA_genfile.h"
 #include "DNA_sdna_types.h"
 #include "dna_parse.h"
 #include "dna_utils.h"
@@ -128,8 +129,10 @@ struct TypeTable {
   };
   CustomIDVectorSet<TypeInfo, GetIDFn> types;
 
-  void add_builtin(StringRefNull name, short size)
+  void add_builtin(const StringRef name, const short size, const int expected_type_index)
   {
+    BLI_assert(this->types.size() == expected_type_index);
+    UNUSED_VARS_NDEBUG(expected_type_index);
     this->types.add_new({.name = name,
                          .size_native = size,
                          .size_32 = size,
@@ -179,22 +182,22 @@ static TypeTable build_type_table(const Span<dna::ParsedStruct> parsed_structs)
    * \warning uint is not allowed! use in structs an unsigned int.
    * \warning sizes must match #DNA_elem_type_size().
    */
-  table.add_builtin("char", 1);   /* SDNA_TYPE_CHAR */
-  table.add_builtin("uchar", 1);  /* SDNA_TYPE_UCHAR */
-  table.add_builtin("short", 2);  /* SDNA_TYPE_SHORT */
-  table.add_builtin("ushort", 2); /* SDNA_TYPE_USHORT */
-  table.add_builtin("int", 4);    /* SDNA_TYPE_INT */
+  table.add_builtin("char", 1, SDNA_TYPE_CHAR);
+  table.add_builtin("uchar", 1, SDNA_TYPE_UCHAR);
+  table.add_builtin("short", 2, SDNA_TYPE_SHORT);
+  table.add_builtin("ushort", 2, SDNA_TYPE_USHORT);
+  table.add_builtin("int", 4, SDNA_TYPE_INT);
 
   /* NOTE: long isn't supported, these are place-holders to maintain alignment with #eSDNA_Type. */
-  table.add_builtin("long", 4);  /* SDNA_TYPE_LONG */
-  table.add_builtin("ulong", 4); /* SDNA_TYPE_ULONG */
+  table.add_builtin("long", 4, 5 /* SDNA_TYPE_LONG */);
+  table.add_builtin("ulong", 4, 6 /* SDNA_TYPE_ULONG */);
 
-  table.add_builtin("float", 4);    /* SDNA_TYPE_FLOAT */
-  table.add_builtin("double", 8);   /* SDNA_TYPE_DOUBLE */
-  table.add_builtin("int64_t", 8);  /* SDNA_TYPE_INT64 */
-  table.add_builtin("uint64_t", 8); /* SDNA_TYPE_UINT64 */
-  table.add_builtin("void", 0);     /* SDNA_TYPE_VOID */
-  table.add_builtin("int8_t", 1);   /* SDNA_TYPE_INT8 */
+  table.add_builtin("float", 4, SDNA_TYPE_FLOAT);
+  table.add_builtin("double", 8, SDNA_TYPE_DOUBLE);
+  table.add_builtin("int64_t", 8, SDNA_TYPE_INT64);
+  table.add_builtin("uint64_t", 8, SDNA_TYPE_UINT64);
+  table.add_builtin("void", 0, SDNA_TYPE_VOID);
+  table.add_builtin("int8_t", 1, SDNA_TYPE_INT8);
 
   /* Fake place-holder struct definition used to get an identifier for raw, untyped bytes buffers
    * in blend-files.