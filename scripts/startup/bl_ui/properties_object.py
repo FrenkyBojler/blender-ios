4af4ab9504327a81e470cab2cb4d9e12e969cdab
Falk David <falk@blender.org>
2026-05-04 14:19:47 +0200

    Show properties in `OBJECT_PT_display` for empties

--- scripts/startup/bl_ui/properties_object.py
+++ scripts/startup/bl_ui/properties_object.py
@@ -225,7 +225,18 @@ class OBJECT_PT_display(ObjectButtonsPanel, Panel):
 
         obj = context.object
         obj_type = obj.type
-        is_geometry = (obj_type in {'MESH', 'CURVE', 'SURFACE', 'META', 'FONT', 'VOLUME', 'CURVES', 'POINTCLOUD'})
+        # Empties can have evaluated geometry
+        is_geometry = (
+            obj_type in {
+                'EMPTY',
+                'MESH',
+                'CURVE',
+                'SURFACE',
+                'META',
+                'FONT',
+                'VOLUME',
+                'CURVES',
+                'POINTCLOUD'})
         has_bounds = (is_geometry or obj_type in {'LATTICE', 'ARMATURE'})
         is_wire = (obj_type in {'CAMERA', 'EMPTY'})
         is_empty_image = (obj_type == 'EMPTY' and obj.empty_display_type == 'IMAGE')
