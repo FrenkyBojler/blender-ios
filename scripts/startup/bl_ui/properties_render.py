75b783d73dc48dad4684a9529252b9b2406b8aed
Clément Foucault <foucault.clem@gmail.com>
2026-05-04 19:22:12 +0200

    Fix formatting

--- scripts/startup/bl_ui/properties_render.py
+++ scripts/startup/bl_ui/properties_render.py
@@ -442,7 +442,7 @@ class RENDER_PT_eevee_screen_trace(RenderButtonsPanel, Panel):
         col = layout.column()
         col.prop(props, "screen_trace_quality", text="Precision")
         col.prop(props, "screen_trace_thickness", text="Thickness")
-        
+
         col = col.column(align=False, heading="Backface")
         row = col.row(align=True)
         sub = row.row(align=True)
