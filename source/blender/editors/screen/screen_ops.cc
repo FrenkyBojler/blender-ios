18e158526714bfca1713fe4ceed2aa535434d3c6
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-05-04 09:01:41 +0200

    the fix

--- source/blender/editors/screen/screen_ops.cc
+++ source/blender/editors/screen/screen_ops.cc
@@ -6681,7 +6681,7 @@ static wmOperatorStatus screen_animation_play_exec(bContext *C, wmOperator *op)
 
   const wmOperatorStatus status = ED_screen_animation_play(C, sync, mode);
 
-  if (!ED_screen_animation_playing(CTX_wm_manager(C))) {
+  if (!ED_screen_animation_playing(CTX_wm_manager(C)) && !ED_undo_is_valid_redo(C)) {
     /* Only pushing undo step when stopping playback so that there are no undo steps that seemingly
      * do nothing. Creating an undo step is important though so that when undoing an action right
      * after playback stop doesn't also change the current frame. See #144058. */
--- source/blender/editors/undo/ed_undo.cc
+++ source/blender/editors/undo/ed_undo.cc
@@ -400,6 +400,12 @@ bool ED_undo_is_valid(const bContext *C, const char *undoname)
   return BKE_undosys_stack_has_undo(wm->runtime->undo_stack, undoname);
 }
 
+bool ED_undo_is_valid_redo(const bContext *C)
+{
+  const wmWindowManager *wm = CTX_wm_manager(C);
+  return BKE_undosys_stack_has_redo(wm->runtime->undo_stack);
+}
+
 bool ED_undo_is_memfile_compatible(const bContext *C)
 {
   /* Some modes don't co-exist with memfile undo, disable their use: #60593
