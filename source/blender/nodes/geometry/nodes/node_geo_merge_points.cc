621738a465dcdb95880d6c756c199fa230b97c7f
Hans Goudey <hans@blender.org>
2026-05-04 11:04:05 -0400

    Reimplement mesh merge API interface

--- source/blender/nodes/geometry/nodes/node_geo_merge_points.cc
+++ source/blender/nodes/geometry/nodes/node_geo_merge_points.cc
@@ -30,82 +30,6 @@ static void node_declare(NodeDeclarationBuilder &b)
       .description("ID of group of the points to merge");
 }
 
-static std::optional<int> masked_ids_to_merging_roots(const fn::FieldContext &context,
-                                                      const Field<int> &group_id_field,
-                                                      const Field<bool> &selection_field,
-                                                      const int domain_size,
-                                                      Array<int> &r_roots)
-{
-  FieldEvaluator evaluator(context, domain_size);
-  evaluator.add(group_id_field);
-  evaluator.set_selection(selection_field);
-  evaluator.evaluate();
-
-  const IndexMask selection = evaluator.get_evaluated_selection_as_mask();
-  if (selection.is_empty()) {
-    return std::nullopt;
-  }
-
-  const VArray<int> group_id = evaluator.get_evaluated<int>(0);
-  std::optional<VArraySpan<int>> group_id_span;
-  Map<int, int> group_id_to_root;
-  if (group_id.is_single()) {
-    group_id_to_root.add(group_id.get_internal_single(), selection.first());
-  }
-  else {
-    group_id_span.emplace(group_id);
-    selection.foreach_index_optimized<int>(
-        [&](const int index) { group_id_to_root.add((*group_id_span)[index], index); });
-  }
-
-  if ((selection.size() == domain_size) && (group_id_to_root.size() == 1)) {
-    BLI_assert(selection.bounds() == IndexRange(domain_size));
-    r_roots.reinitialize(domain_size);
-    r_roots.fill(0);
-    return domain_size - 1;
-  }
-
-  if (group_id_to_root.size() == domain_size) {
-    BLI_assert(selection.size() == domain_size);
-    return std::nullopt;
-  }
-
-  r_roots.reinitialize(domain_size);
-  /* TODO: Explicitly create groups of indices in merge code and skip unit groups from future
-   * processing... */
-  r_roots.as_mutable_span().fill(-1);
-
-  if (group_id_to_root.size() == 1) {
-    BLI_assert(group_id_to_root.lookup(group_id[selection.first()]) == selection.first());
-    index_mask::masked_fill<int>(r_roots.as_mutable_span(), selection.first(), selection);
-  }
-  else {
-    Array<bool> is_unit_group(domain_size, true);
-    selection.foreach_index_optimized<int>(
-        [&](const int index) {
-          const int group_root = group_id_to_root.lookup((*group_id_span)[index]);
-          if (group_root != index) {
-            is_unit_group[group_root] = false;
-          }
-        },
-        exec_mode::parallel);
-
-    selection.foreach_index_optimized<int>(
-        [&](const int index) {
-          const int group_root = group_id_to_root.lookup((*group_id_span)[index]);
-          if (is_unit_group[group_root]) {
-            BLI_assert(group_root == index);
-            BLI_assert(r_roots[index] == -1);
-            return;
-          }
-          r_roots[index] = group_root;
-        },
-        exec_mode::parallel);
-  }
-
-  return selection.size() - group_id_to_root.size();
-}
-
 static void node_geo_exec(GeoNodeExecParams params)
 {
   GeometrySet geometry_set = params.extract_input<GeometrySet>("Geometry"_ustr);
@@ -137,14 +61,19 @@ static void node_geo_exec(GeoNodeExecParams params)
     }
     if (const Mesh *mesh = geometry_set.get_mesh()) {
       const bke::MeshFieldContext context(*mesh, AttrDomain::Point);
-      Array<int> masked_group_ids;
-      const std::optional<int> total_merge_ops = masked_ids_to_merging_roots(
-          context, group_id_field, selection_field, mesh->verts_num, masked_group_ids);
-      if (total_merge_ops.has_value()) {
-        Mesh *new_mesh = geometry::mesh_merge_verts(
-            *mesh, masked_group_ids.as_mutable_span(), *total_merge_ops, true);
-        geometry_set.replace_mesh(new_mesh);
+      FieldEvaluator evaluator(context, mesh->verts_num);
+      evaluator.add(group_id_field);
+      evaluator.set_selection(selection_field);
+      Array<int> masked_group_ids(mesh->verts_num);
+      evaluator.add_with_destination(group_id_field, masked_group_ids.as_mutable_span());
+      evaluator.evaluate();
+      const IndexMask selection = evaluator.get_evaluated_selection_as_mask();
+      if (selection.is_empty()) {
+        return;
       }
+      Mesh *new_mesh = geometry::mesh_merge_verts(
+          *mesh, selection, masked_group_ids, attribute_filter);
+      geometry_set.replace_mesh(new_mesh);
     }
   });
 
