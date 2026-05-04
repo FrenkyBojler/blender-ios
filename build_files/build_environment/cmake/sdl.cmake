3c151b4aba733e5625006bb008cd092862a83070
Jonas Holzman <jonas@blender.org>
2026-05-04 17:29:19 +0200

    Linux/macOS: Cleanup harvesting after removal of static test libs

--- build_files/build_environment/cmake/sdl.cmake
+++ build_files/build_environment/cmake/sdl.cmake
@@ -38,8 +38,5 @@ else()
   harvest(external_sdl sdl/include sdl/include "*.h")
   # CMake files first because harvest_rpath_lib edits them.
   harvest(external_sdl sdl/lib/cmake/SDL3 sdl/lib/cmake/SDL3 "*.cmake")
-
   harvest_rpath_lib(external_sdl sdl/lib sdl/lib "*${SHAREDLIBEXT}*")
-  # Harvest the extra SDL3_test static libraries to satisfy the CMake Config files targets.
-  harvest(external_sdl sdl/lib sdl/lib "*.a")
 endif()
