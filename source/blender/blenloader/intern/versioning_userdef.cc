e3fbe06a12b6981e867fca60f0d978bbaf0cb559
Guillermo Venegas <guillermovcra@gmail.com>
2026-05-07 08:35:49 -0600

    Update versioning

--- source/blender/blenloader/intern/versioning_userdef.cc
+++ source/blender/blenloader/intern/versioning_userdef.cc
@@ -1765,7 +1765,7 @@ void blo_do_versions_userdef(UserDef *userdef)
     userdef->geometry_nodes_stack_limit = 100;
   }
 
-  if (!USER_VERSION_ATLEAST(502, 26)) {
+  if (!USER_VERSION_ATLEAST(502, 27)) {
     userdef->flag |= USER_BLENDER_UPDATE_LATEST_RELEASE;
     userdef->statusbar_flag |= STATUSBAR_SHOW_BLENDER_UPDATES_DIALOG;
   }
