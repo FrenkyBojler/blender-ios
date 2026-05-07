zNightlord/Bforartists

commit 1561c1ea4ab652010e2cbff4976f4444cd5a5b7c

Geometry Nodes: Runtime RNA for node tree interface
This PR implements the changes described in #132129. The main goal is to
use actual RNA types to store node group inputs on the Geometry Nodes
modifier and node tools, rather than using custom properties. This makes
the API much friendlier, as well as resolving long-standing technical
debt.

The API change looks like this:
```python
# Before
modifier["identifier"] = 5.0
modifier["identifier_use_attribute"] = True
modifier["identifier_attribute_name"] = "some_input_attribute"
modifier["identifier_attribute_name"] = "some_output_attribute"
# After
modifier.inputs.identifier.value = 5.0
modifier.inputs.identifier.type = "ATTRIBUTE"
modifier.inputs.identifier.attribute_name = "some_input_attribute"
modifier.outputs.identifier.attribute_name = "some_output_attribute"
```

The `identifier` is the identifier of the interface socket (e.g.
`Socket_2` currently). This PR does not allow changing this property
name, that can be done as separately later.

Another benefit of this change is that combined with the work from
#148763, now the node tool inputs are remembered between invocations
like properties from any other operator. To get node tool registration
working, we have to include all the information necessary for the RNA
property registration in the asset meta-data.

Backward and forward compatibility are supported (though animation paths
are only backward compatible), but this does break the current Python API
for accessing Geometry Nodes modifier properties.

Co-authored-by: Hans Goudey <hans@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/138117

source/blender/blenkernel/intern/node.cc
@@ -99,6 +99,7 @@
 #include "NOD_geo_simulation.hh"
 #include "NOD_geometry_nodes_gizmos.hh"
 #include "NOD_geometry_nodes_lazy_function.hh"
+#include "NOD_geometry_nodes_srna.hh"
 #include "NOD_menu_value.hh"
 #include "NOD_node_declaration.hh"
 #include "NOD_register.hh"
@@ -239,6 +240,7 @@ static void ntree_copy_data(Main * /*bmain*/,
       }
     }
   }
+  dst_runtime.geometry_nodes_srna_data = ntree_src->runtime->geometry_nodes_srna_data;
 
   if (ntree_src->geometry_node_asset_traits) {
     ntree_dst->geometry_node_asset_traits = MEM_new<GeometryNodeAssetTraits>(
@@ -2096,6 +2098,159 @@ static void ntree_blend_read_after_liblink(BlendLibReader *reader, ID *id)
   }
 }
 
+IDProperty *node_create_asset_meta_data_properties(const bNodeTree &node_tree)
+{
+  auto properties = idprop::create_group("properties");
+  auto inputs = idprop::create_group("inputs");
+  node_tree.ensure_interface_cache();
+  for (const bNodeTreeInterfaceSocket *socket : node_tree.interface_inputs()) {
+    const bNodeSocketType *base_typeinfo = node_socket_type_find(socket->socket_type);
+
+    auto input = idprop::create_group(socket->identifier);
+    IDP_AddToGroup(input.get(),
+                   idprop::create("name", socket->name ? socket->name : "").release());
+    IDP_AddToGroup(input.get(), idprop::create("type", base_typeinfo->type).release());
+    IDP_AddToGroup(
+        input.get(),
+        idprop::create("description", socket->description ? socket->description : "").release());
+    switch (base_typeinfo->type) {
+      case SOCK_FLOAT: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueFloat>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create("subtype", value.subtype).release());
+        IDP_AddToGroup(input.get(), idprop::create("min", value.min).release());
+        IDP_AddToGroup(input.get(), idprop::create("max", value.max).release());
+        IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_VECTOR: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueVector>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create("subtype", value.subtype).release());
+        IDP_AddToGroup(
+            input.get(),
+            idprop::create("default_value", Span(value.value, value.dimensions)).release());
+        if (value.dimensions != 3) {
+          IDP_AddToGroup(input.get(), idprop::create("dimensions", value.dimensions).release());
+        }
+        IDP_AddToGroup(input.get(), idprop::create("min", value.min).release());
+        IDP_AddToGroup(input.get(), idprop::create("max", value.max).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_RGBA: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueRGBA>(*socket);
+        IDP_AddToGroup(input.get(),
+                       idprop::create("default_value", Span(value.value, 4)).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_BOOLEAN: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueBoolean>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create_bool("default_value", value.value).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_ROTATION: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueRotation>(*socket);
+        const float3 default_value = float3(math::EulerXYZ(float3(value.value_euler)));
+        IDP_AddToGroup(input.get(),
+                       idprop::create("default_value", Span(&default_value.x, 3)).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_INT: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueInt>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create("subtype", value.subtype).release());
+        IDP_AddToGroup(input.get(), idprop::create("min", value.min).release());
+        IDP_AddToGroup(input.get(), idprop::create("max", value.max).release());
+        IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
+        if (socket->default_attribute_name) {
+          IDP_AddToGroup(
+              input.get(),
+              idprop::create("default_attribute_name", socket->default_attribute_name).release());
+        }
+        break;
+      }
+      case SOCK_STRING: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueString>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create("subtype", value.subtype).release());
+        IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
+        break;
+      }
+      case SOCK_MENU: {
+        const auto &value = node_interface::get_socket_data_as<bNodeSocketValueMenu>(*socket);
+        IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
+        auto items = idprop::create_group("items");
+        for (const RuntimeNodeEnumItem &enum_item : value.enum_items->items) {
+          auto item = idprop::create_group(std::to_string(enum_item.identifier));
+          IDP_AddToGroup(item.get(), idprop::create("name", enum_item.name).release());
+          IDP_AddToGroup(item.get(),
+                         idprop::create("description", enum_item.description).release());
+        }
+        IDP_AddToGroup(input.get(), items.release());
+        break;
+      }
+      case SOCK_SHADER:
+      case SOCK_MATRIX:
+      case SOCK_OBJECT:
+      case SOCK_IMAGE:
+      case SOCK_GEOMETRY:
+      case SOCK_COLLECTION:
+      case SOCK_TEXTURE:
+      case SOCK_MATERIAL:
+      case SOCK_BUNDLE:
+      case SOCK_CLOSURE:
+      case SOCK_CUSTOM:
+      case SOCK_FONT:
+      case SOCK_SCENE:
+      case SOCK_TEXT_ID:
+      case SOCK_MASK:
+      case SOCK_SOUND:
+      case SOCK_INT_VECTOR:
+        break;
+    }
+    IDP_AddToGroup(inputs.get(), input.release());
+  }
+  IDP_AddToGroup(properties.get(), inputs.release());
+
+  auto panels = idprop::create_group("panels");
+  for (const bNodeTreeInterfaceItem *item : node_tree.interface_items()) {
+    if (item->item_type != NODE_INTERFACE_PANEL) {
+      continue;
+    }
+    const auto &panel = *reinterpret_cast<const bNodeTreeInterfacePanel *>(item);
+    std::string open_identifier = fmt::format("open_{}", panel.identifier);
+    auto panel_props = idprop::create_group(open_identifier);
+    IDP_AddToGroup(
+        panels.get(),
+        idprop::create_bool(open_identifier, panel.flag & NODE_INTERFACE_PANEL_DEFAULT_CLOSED)
+            .release());
+  }
+  IDP_AddToGroup(properties.get(), panels.release());
+
+  return properties.release();
+}
+
 void node_update_asset_metadata(bNodeTree &node_tree)
 {
   AssetMetaData *asset_data = node_tree.id.asset_data;
@@ -2104,22 +2259,14 @@ void node_update_asset_metadata(bNodeTree &node_tree)
   }
 
   BKE_asset_metadata_idprop_ensure(asset_data, idprop::create("type", node_tree.type).release());
-  auto inputs = idprop::create_group("inputs");
   auto outputs = idprop::create_group("outputs");
-  node_tree.ensure_interface_cache();
-  for (const bNodeTreeInterfaceSocket *socket : node_tree.interface_inputs()) {
-    auto *prop = idprop::create(socket->name ? socket->name : "", socket->socket_type).release();
-    if (!IDP_AddToGroup(inputs.get(), prop)) {
-      IDP_FreeProperty(prop);
-    }
-  }
   for (const bNodeTreeInterfaceSocket *socket : node_tree.interface_outputs()) {
     auto *prop = idprop::create(socket->name ? socket->name : "", socket->socket_type).release();
     if (!IDP_AddToGroup(outputs.get(), prop)) {
       IDP_FreeProperty(prop);
     }
   }
-  BKE_asset_metadata_idprop_ensure(asset_data, inputs.release());
+  BKE_asset_metadata_idprop_ensure(asset_data, node_create_asset_meta_data_properties(node_tree));
   BKE_asset_metadata_idprop_ensure(asset_data, outputs.release());
   if (node_tree.geometry_node_asset_traits) {
     auto property = idprop::create("geometry_node_asset_traits_flag",

FrenkyBojler/blender-ios

commit 27e3e206229711dfa712bf28183b62d13125f74c

Fix #158280: Node Tools: Menu inputs broken since recent refactor
Caused by 1561c1ea4ab652010e2cbff4976f4444cd5a5b7c
There were a few problems. First, the enum items generation callback
didn't get the pointer I had expected when writing this. Instead, generate
them when registering the operator. Second, the asset meta-data stored
things in a confusing way, resulting in the Python-accessible enum items
using numbers as the identifiers instead of the names like similar properties
on modifiers. Add a test to keep this all working in the future.

source/blender/blenkernel/intern/node.cc
@@ -10,6 +10,7 @@
 
 #include "MEM_guardedalloc.h"
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -2231,14 +2232,21 @@ IDProperty *node_create_asset_meta_data_properties(const bNodeTree &node_tree)
       }
       case SOCK_MENU: {
         const auto &value = node_interface::get_socket_data_as<bNodeSocketValueMenu>(*socket);
-        IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
         if (value.enum_items) {
+          if (std::ranges::any_of(value.enum_items->items, [&](const RuntimeNodeEnumItem &item) {
+                return item.identifier == value.value;
+              }))
+          {
+            /* Only add the default value property if it's contained in the enum items. */
+            IDP_AddToGroup(input.get(), idprop::create("default_value", value.value).release());
+          }
           auto items = idprop::create_group("items");
           for (const RuntimeNodeEnumItem &enum_item : value.enum_items->items) {
-            auto item = idprop::create_group(std::to_string(enum_item.identifier));
-            IDP_AddToGroup(item.get(), idprop::create("name", enum_item.name).release());
+            auto item = idprop::create_group(enum_item.name);
+            IDP_AddToGroup(item.get(), idprop::create("value", enum_item.identifier).release());
             IDP_AddToGroup(item.get(),
                            idprop::create("description", enum_item.description).release());
+            IDP_AddToGroup(items.get(), item.release());
           }
           IDP_AddToGroup(input.get(), items.release());
         }

source/blender/editors/geometry/node_group_operator.cc
@@ -127,6 +127,7 @@ struct OperatorTypeData : public wmOperatorType::TypeData {
 
   std::unique_ptr<IDProperty, bke::idprop::IDPropertyDeleter> asset_meta_data_properties;
   Vector<StructRNA *> generated_structs;
+  Vector<Array<EnumPropertyItem, 0>> enum_item_storage;
 
   struct LocalRef {
     uint32_t session_uid;
@@ -1115,35 +1116,31 @@ static bool run_node_group_poll(bContext *C, wmOperatorType *ot)
   return true;
 }
 
-static const EnumPropertyItem *enum_input_items_fn(bContext * /*C*/,
-                                                   PointerRNA *ptr,
-                                                   PropertyRNA *prop,
-                                                   bool *r_free)
+static Array<EnumPropertyItem, 0> get_input_enum_items(const IDProperty &input_idprop)
 {
-  const wmOperator *op = ptr->data_as<wmOperator>();
-  const OperatorTypeData &type_data = *static_cast<const OperatorTypeData *>(op->customdata);
-  const IDProperty &inputs_props = *IDP_GetPropertyFromGroup(
-      type_data.asset_meta_data_properties.get(), "inputs");
-  const IDProperty &input_idprop = *IDP_GetPropertyFromGroup(&inputs_props,
-                                                             RNA_property_identifier(prop));
-
   const IDProperty *items_idprop = IDP_GetPropertyFromGroup(&input_idprop, "items");
   if (!items_idprop || items_idprop->type != IDP_GROUP) {
-    return rna_enum_dummy_NULL_items;
+    return {rna_enum_dummy_NULL_items[0]};
   }
 
-  int totitem = 0;
-  EnumPropertyItem *items = nullptr;
-  for (IDProperty &item_idprop : items_idprop->data.group) {
-    EnumPropertyItem item;
-    item.identifier = item_idprop.name;
-    item.name = IDP_group_lookup_string(item_idprop, "name").value_or("").c_str();
-    item.description = IDP_group_lookup_string(item_idprop, "description").value_or("").c_str();
-    item.value = std::stoi(item_idprop.name);
-    RNA_enum_item_add(&items, &totitem, &item);
+  if (!items_idprop->data.children_map || items_idprop->data.children_map->children.is_empty()) {
+    return {rna_enum_dummy_NULL_items[0]};
   }
 
-  *r_free = true;
+  const int items_num = items_idprop->data.children_map->children.size();
+  Array<EnumPropertyItem, 0> items(items_num + 1);
+  for (const auto [i, item_idprop] : items_idprop->data.group.enumerate()) {
+    items[i] = EnumPropertyItem{
+        .value = IDP_group_lookup_int(item_idprop, "value").value_or(0),
+        .identifier = item_idprop.name,
+        .icon = ICON_NONE,
+        .name = item_idprop.name,
+        .description = IDP_group_lookup_string(item_idprop, "description").value_or("").c_str(),
+    };
+  }
+
+  items.last() = {0, nullptr, 0, nullptr, nullptr};
+
   return items;
 }
 
@@ -1188,9 +1185,8 @@ static void make_common_value_props(StructRNA &srna)
 }
 
 static StructRNA *get_input_socket_struct_rna(IDProperty &input_idprop,
-                                              Vector<StructRNA *> &r_generated)
+                                              OperatorTypeData &type_data)
 {
-
   const StringRefNull identifier = input_idprop.name;
   const std::optional<int> type = IDP_group_lookup_int(input_idprop, "type");
   if (!type) {
@@ -1199,7 +1195,7 @@ static StructRNA *get_input_socket_struct_rna(IDProperty &input_idprop,
   StructRNA *srna = RNA_def_struct_ptr(
       &RNA_blender_rna_get(), identifier.c_str(), RNA_PropertyGroup);
   BLI_assert(!RNA_struct_in_public_namespace(srna));
-  r_generated.append(srna);
+  type_data.generated_structs.append(srna);
   // RNA_def_struct_path_func_runtime(srna, rna_NodesModifierPropertyInput_path);
   const StringRefNull name = IDP_group_lookup_string(input_idprop, "name").value_or(identifier);
   const StringRefNull description =
@@ -1333,9 +1329,21 @@ static StructRNA *get_input_socket_struct_rna(IDProperty &input_idprop,
       break;
     }
     case SOCK_MENU: {
-      PropertyRNA *prop = RNA_def_enum(
-          srna, "value", rna_enum_dummy_NULL_items, 0, name.c_str(), description.c_str());
-      RNA_def_enum_funcs(prop, enum_input_items_fn);
+      type_data.enum_item_storage.append_as(get_input_enum_items(input_idprop));
+      int default_value = IDP_group_lookup_int(input_idprop, "default_value").value_or(0);
+      if (std::ranges::none_of(
+              type_data.enum_item_storage.last(),
+              [&](const EnumPropertyItem &item) { return item.value == default_value; }))
+      {
+        /* Default value must be used by one of the enum items. */
+        default_value = 0;
+      }
+      RNA_def_enum(srna,
+                   "value",
+                   type_data.enum_item_storage.last().data(),
+                   IDP_group_lookup_int(input_idprop, "default_value").value_or(0),
+                   name.c_str(),
+                   description.c_str());
       make_common_value_props(*srna);
       break;
     }
@@ -1346,13 +1354,12 @@ static StructRNA *get_input_socket_struct_rna(IDProperty &input_idprop,
   return srna;
 }
 
-static StructRNA *create_inputs_srna(const IDProperty &properties,
-                                     Vector<StructRNA *> &r_generated)
+static StructRNA *create_inputs_srna(const IDProperty &properties, OperatorTypeData &type_data)
 {
   StructRNA *srna = RNA_def_struct_ptr(
       &RNA_blender_rna_get(), "GeometryNodesInterfaceInputs", RNA_PropertyGroup);
   BLI_assert(!RNA_struct_in_public_namespace(srna));
-  r_generated.append(srna);
+  type_data.generated_structs.append(srna);
 
   const IDProperty &inputs_props = *IDP_GetPropertyFromGroup(&properties, "inputs");
 
@@ -1360,7 +1367,7 @@ static StructRNA *create_inputs_srna(const IDProperty &properties,
     if (input_idprop.type != IDP_GROUP) {
       continue;
     }
-    StructRNA *input_srna = get_input_socket_struct_rna(input_idprop, r_generated);
+    StructRNA *input_srna = get_input_socket_struct_rna(input_idprop, type_data);
     if (!input_srna) {
       continue;
     }
@@ -1413,8 +1420,7 @@ static void register_node_tool(wmOperatorType *ot,
     ot->flag |= OPTYPE_DEPENDS_ON_CURSOR;
   }
 
-  StructRNA *inputs_srna = create_inputs_srna(*type_data.asset_meta_data_properties,
-                                              type_data.generated_structs);
+  StructRNA *inputs_srna = create_inputs_srna(*type_data.asset_meta_data_properties, type_data);
   RNA_def_pointer_runtime(ot->srna, "inputs", inputs_srna, "Inputs", "Settings for input sockets");
   if (StructRNA *panels_srna = create_panels_srna(*type_data.asset_meta_data_properties,
                                                   type_data.generated_structs))

tests/python/bl_node_tool.py
@@ -115,6 +115,56 @@ class TestNodeTool(unittest.TestCase):
         self.assertIn("attr_with_default", attribute_names)
         self.assertIn("attr_without_default", attribute_names)
 
+    def test_menu_input(self):
+        from bpy.types import WindowManager
+
+        bpy.ops.mesh.primitive_cube_add()
+        cube = bpy.context.active_object
+
+        tree = create_object_mode_mesh_tool_tree(
+            "TestNodeToolMenu", "geometry.test_node_tool_menu"
+        )
+        tree.interface.new_socket("Geometry", in_out='INPUT', socket_type='NodeSocketGeometry')
+        mode_socket = tree.interface.new_socket(
+            "Mode", in_out='INPUT', socket_type='NodeSocketMenu')
+        tree.interface.new_socket("Geometry", in_out='OUTPUT', socket_type='NodeSocketGeometry')
+
+        group_input = tree.nodes.new("NodeGroupInput")
+        group_output = tree.nodes.new("NodeGroupOutput")
+        menu_switch = tree.nodes.new("GeometryNodeMenuSwitch")
+        menu_switch.data_type = 'VECTOR'
+        transform = tree.nodes.new("GeometryNodeTransform")
+
+        # The menu switch starts with default items "A" and "B". Rename and add a third so the items become X, Y, Z.
+        menu_switch.enum_items[0].name = "X"
+        menu_switch.enum_items[1].name = "Y"
+        menu_switch.enum_items.new("Z")
+
+        menu_switch.inputs["X"].default_value = (10.0, 0.0, 0.0)
+        menu_switch.inputs["Y"].default_value = (0.0, 10.0, 0.0)
+        menu_switch.inputs["Z"].default_value = (0.0, 0.0, 10.0)
+
+        tree.links.new(group_input.outputs["Geometry"], transform.inputs["Geometry"])
+        tree.links.new(group_input.outputs["Mode"], menu_switch.inputs["Menu"])
+        tree.links.new(menu_switch.outputs["Output"], transform.inputs["Translation"])
+        tree.links.new(transform.outputs["Geometry"], group_output.inputs["Geometry"])
+
+        WindowManager.register_node_group_operators()
+
+        bpy.ops.geometry.test_node_tool_menu(
+            'EXEC_DEFAULT',
+            inputs={mode_socket.identifier: {"value": "Y"}},
+        )
+
+        verts = cube.data.vertices
+        n = len(verts)
+        center_x = sum(v.co.x for v in verts) / n
+        center_y = sum(v.co.y for v in verts) / n
+        center_z = sum(v.co.z for v in verts) / n
+        self.assertAlmostEqual(center_x, 0.0, places=4)
+        self.assertAlmostEqual(center_y, 10.0, places=4)
+        self.assertAlmostEqual(center_z, 0.0, places=4)
+
 
 if __name__ == "__main__":
     import sys