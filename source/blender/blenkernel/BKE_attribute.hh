373ecfc04cc02165cfa44f430387f395a955b053
Jacques Lucke <jacques@blender.org>
2026-05-08 20:45:24 +0200

    remove unnecessary change

--- source/blender/blenkernel/BKE_attribute.hh
+++ source/blender/blenkernel/BKE_attribute.hh
@@ -705,22 +705,10 @@ class AttributeAccessor {
                                        const AttrDomain domain,
                                        const T &default_value) const
   {
-    if (AttributeReader<T> attr = this->lookup<T>(name, domain)) {
-      return attr;
+    if (AttributeReader<T> varray = this->lookup<T>(name, domain)) {
+      return varray;
     }
     return {VArray<T>::from_single(default_value, this->domain_size(domain)), domain};
-  }
-
-  template<typename T>
-  AttributeReader<T> lookup_or_default_varray(const StringRef name,
-                                              const AttrDomain domain,
-                                              const VArray<T> &default_varray) const
-  {
-    if (AttributeReader<T> attr = this->lookup<T>(name, domain)) {
-      return attr;
-    }
-    BLI_assert(default_varray.size() == this->domain_size(domain));
-    return {default_varray, domain};
   }
 
   /**
