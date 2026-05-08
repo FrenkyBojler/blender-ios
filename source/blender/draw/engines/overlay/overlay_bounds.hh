18eb5c48dcdcf61608968c16973e604a2a8dd628
Falk David <falk@blender.org>
2026-05-08 12:26:29 +0200

    Fix bounds rendering

--- source/blender/draw/engines/overlay/overlay_bounds.hh
+++ source/blender/draw/engines/overlay/overlay_bounds.hh
@@ -67,9 +67,8 @@ class Bounds : Overlay {
   {
     const Object *ob = ob_ref.object;
     const bool from_dupli = is_from_dupli_or_set(ob);
-    const bool has_bounds =
-        !ELEM(ob->type, OB_LAMP, OB_CAMERA, OB_EMPTY, OB_SPEAKER, OB_LIGHTPROBE) &&
-        (ob->type != OB_MBALL || BKE_mball_is_basis(ob));
+    const bool has_bounds = !ELEM(ob->type, OB_LAMP, OB_CAMERA, OB_SPEAKER, OB_LIGHTPROBE) &&
+                            (ob->type != OB_MBALL || BKE_mball_is_basis(ob));
     const bool show_extras = !from_dupli && state.show_extras();
 
     /* Ignore `show_extras` when the objects draw-type is already bound-box,
