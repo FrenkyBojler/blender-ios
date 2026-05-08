goshtdev/upbge

commit 119dd6e81630b8234f8e4bad5ccbf0e4df65bdc8

EEVEE: Reduce the number of UniformBuf uploads
* Split `SubsurfaceData` into its own buffer, since it's only used by
  `eevee_subsurface_convolve`.
* Convert `FilmData::display_only` into a push constant, since it's
  only used by `eevee_film_frag`.
* Split `PipelineInfo` to its own buffer, since it's updated several
  times along the frame.
* Split `RayTraceData` to its own buffer, since it's updated several
  times along the frame.
* Update `ShadowSceneData` and `VolumesInfoData` together and only once
  per view.

Pull Request: https://projects.blender.org/blender/blender/pulls/158203

source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.glsl
@@ -305,7 +305,7 @@ void raycast_eval([[maybe_unused]] float3 position,
   float thickness_noise_offset = sampling_rng_1D_get(SAMPLING_RAYTRACE_X);
   float thickness_jitter =
       interleaved_gradient_noise(gl_FragCoord.xy, 1.0f, thickness_noise_offset) * 0.5f + 0.5f;
-  float thickness = uniform_buf.raytrace.thickness * thickness_jitter;
+  float thickness = raytrace_buf.thickness * thickness_jitter;
 
   float2 hit_uv = float2(0.0f);
   uint self_id = drw_resource_id() & 0xFFFF;
@@ -314,7 +314,7 @@ void raycast_eval([[maybe_unused]] float3 position,
                                    drw_point_world_to_view(ws_end),
                                    drw_normal_world_to_view(direction),
                                    hiz_tx,
-                                   uniform_buf.raytrace,
+                                   raytrace_buf,
                                    64,
                                    jitter,
                                    object_id_tx,

source/blender/draw/engines/eevee/eevee_defines.hh
@@ -272,15 +272,20 @@
 /* Uniform Buffers. */
 /* Slot 0 is GPU_NODE_TREE_UBO_SLOT. */
 #define UNIFORM_BUF_SLOT 1
+/* Split from the main uniform buffer since they're updated multiple times per frame. */
+#define PIPELINE_BUF_SLOT 2
+#define RAYTRACE_BUF_SLOT 3
 /* Only during surface shading (forward and deferred eval). */
-#define IRRADIANCE_GRID_BUF_SLOT 2
-#define SPHERE_PROBE_BUF_SLOT 3
-#define PLANAR_PROBE_BUF_SLOT 4
+#define IRRADIANCE_GRID_BUF_SLOT 4
+#define SPHERE_PROBE_BUF_SLOT 5
+#define PLANAR_PROBE_BUF_SLOT 6
 /* Only during pre-pass. */
-#define VELOCITY_CAMERA_PREV_BUF 2
-#define VELOCITY_CAMERA_CURR_BUF 3
-#define VELOCITY_CAMERA_NEXT_BUF 4
-#define CLIP_PLANE_BUF 5
+#define VELOCITY_CAMERA_PREV_BUF 4
+#define VELOCITY_CAMERA_CURR_BUF 5
+#define VELOCITY_CAMERA_NEXT_BUF 6
+#define CLIP_PLANE_BUF 7
+/* Only during subsurface scattering */
+#define SUBSURFACE_BUF_SLOT 4
 
 /* Storage Buffers. */
 #define LIGHT_CULL_BUF_SLOT 0

source/blender/draw/engines/eevee/eevee_film.cc
@@ -571,6 +571,7 @@ void Film::sync()
     accumulate_ps_.dispatch(int3(math::divide_ceil(data_.extent, int2(FILM_GROUP_SIZE)), 1));
   }
   else {
+    accumulate_ps_.push_constant("display_only", &display_only_);
     accumulate_ps_.draw_procedural(GPU_PRIM_TRIS, 1, 3);
   }
 
@@ -858,13 +859,9 @@ void Film::accumulate(View &view, gpu::Texture *combined_final_tx)
     GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(data_.offset), UNPACK2(data_.extent));
   }
 
-  update_sample_table();
-
   combined_final_tx_ = combined_final_tx;
 
-  data_.display_only = false;
-  inst_.uniform_data.push_update();
-
+  display_only_ = false;
   inst_.manager->submit(accumulate_ps_, view);
   inst_.manager->submit(copy_ps_, view);
 
@@ -890,11 +887,9 @@ void Film::display()
 
   combined_final_tx_ = inst_.render_buffers.combined_tx;
 
-  data_.display_only = true;
-  inst_.uniform_data.push_update();
-
   draw::View &drw_view = draw::View::default_get();
 
+  display_only_ = true;
   DRW_manager_get()->submit(accumulate_ps_, drw_view);
 
   inst_.render_buffers.release();

source/blender/draw/engines/eevee/eevee_film.hh
@@ -101,6 +101,7 @@ class Film {
   PassSimple cryptomatte_post_ps_ = {"Film.Cryptomatte.Post"};
 
   FilmData &data_;
+  bool32_t display_only_;
   int2 display_extent = int2(-1);
 
   eViewLayerEEVEEPassType enabled_passes_ = eViewLayerEEVEEPassType(0);
@@ -349,14 +350,14 @@ class Film {
     return result;
   }
 
- private:
-  void init_aovs(const Set<std::string> &passes_used_by_viewport_compositor);
-  void sync_mist();
-
   /**
    * Precompute sample weights if they are uniform across the whole film extent.
    */
   void update_sample_table();
+
+ private:
+  void init_aovs(const Set<std::string> &passes_used_by_viewport_compositor);
+  void sync_mist();
 
   void init_pass(PassSimple &pass, gpu::Shader *sh);
 };

source/blender/draw/engines/eevee/eevee_film_shared.hh
@@ -95,8 +95,6 @@ struct [[host_shared]] FilmData {
   int display_id;
   /** Storage type of the render-pass to be displayed. */
   enum ePassStorageType display_storage_type;
-  /** True if we bypass the accumulation and directly output the accumulation buffer. */
-  bool32_t display_only;
   /** Start of AOVs and number of aov. */
   int aov_color_id;
   int aov_color_len;
@@ -124,7 +122,7 @@ struct [[host_shared]] FilmData {
   int samples_len;
   /** Sum of the weights of all samples in the sample table. */
   float samples_weight_total;
-  int _pad2;
+  int _pad0, _pad1;
   struct FilmSample samples[FILM_PRECOMP_SAMPLE_MAX];
 };
 

source/blender/draw/engines/eevee/eevee_instance.cc
@@ -472,8 +472,6 @@ void Instance::end_sync()
   sphere_probes.end_sync();
   planar_probes.end_sync();
 
-  uniform_data.push_update();
-
   depsgraph_last_update_ = DEG_get_update_count(depsgraph);
 }
 
@@ -527,6 +525,7 @@ void Instance::render_sample()
 {
   if (sampling.finished_viewport()) {
     DRW_submission_start();
+    uniform_data.push_update();
     film.display();
     lookdev.display();
     DRW_submission_end();
@@ -554,6 +553,8 @@ void Instance::render_sample()
     DRW_submission_start();
 
     sampling.step();
+    film.update_sample_table();
+    uniform_data.push_update();
 
     capture_view.render_world();
     lookdev.rotate_world();
@@ -914,6 +915,7 @@ void Instance::light_bake_irradiance(
     /* Sampling module needs to be initialized to computing lighting. */
     sampling.init(probe);
     sampling.step();
+    uniform_data.push_update();
 
     {
       /* Critical section. Potential gpu::Shader concurrent usage. */

source/blender/draw/engines/eevee/eevee_instance.hh
@@ -57,19 +57,27 @@
 namespace blender::eevee {
 
 using UniformDataBuf = draw::UniformBuffer<UniformData>;
+using PipelineInfoBuf = draw::UniformBuffer<PipelineInfoData>;
+using RaytraceDataBuf = draw::UniformBuffer<RayTraceData>;
 
 /* Combines data from several modules to avoid wasting binding slots. */
 struct UniformDataModule {
-  UniformDataBuf data = {"UniformDataBuf"};
+  UniformDataBuf data{"UniformDataBuf"};
+  PipelineInfoBuf pipeline{"PipelineInfoBuf"};
+  RaytraceDataBuf raytrace{"RaytraceDataBuf"};
 
   void push_update()
   {
     data.push_update();
+    pipeline.push_update();
+    raytrace.push_update();
   }
 
   template<typename PassType> void bind_resources(PassType &pass)
   {
     pass.bind_ubo(UNIFORM_BUF_SLOT, &data);
+    pass.bind_ubo(PIPELINE_BUF_SLOT, &pipeline);
+    pass.bind_ubo(RAYTRACE_BUF_SLOT, &raytrace);
   }
 };
 
@@ -184,12 +192,12 @@ class Instance : public DrawEngine {
       : shaders(*ShaderModule::module_get()),
         sync(*this),
         materials(*this),
-        subsurface(*this, uniform_data.data.subsurface),
-        pipelines(*this, uniform_data.data.pipeline),
+        subsurface(*this),
+        pipelines(*this, uniform_data.pipeline),
         shadows(*this, uniform_data.data.shadow),
         lights(*this),
         ambient_occlusion(*this, uniform_data.data.ao),
-        raytracing(*this, uniform_data.data.raytrace),
+        raytracing(*this, uniform_data.raytrace),
         velocity(*this),
         motion_blur(*this),
         depth_of_field(*this),

source/blender/draw/engines/eevee/eevee_lightprobe_volume.cc
@@ -1236,11 +1236,13 @@ void IrradianceBake::surfels_create(const Object &probe_object)
 void IrradianceBake::surfels_lights_eval()
 {
   /* Use the last setup view. This should work since the view is orthographic. */
-  /* TODO(fclem): Remove this. It is only present to avoid crash inside `shadows.set_view` */
+  /* TODO(fclem): Remove this. It is only present to avoid crash inside `shadows.render` */
   inst_.render_buffers.acquire(int2(1));
   inst_.hiz_buffer.set_source(&inst_.render_buffers.depth_tx);
-  inst_.lights.set_view(view_z_, grid_pixel_extent_.xy());
   inst_.shadows.set_view(view_z_, grid_pixel_extent_.xy());
+  inst_.uniform_data.data.push_update();
+  inst_.lights.set_view(view_z_, grid_pixel_extent_.xy());
+  inst_.shadows.render(view_z_, grid_pixel_extent_.xy());
   if (GPU_type_matches(GPU_DEVICE_ANY, GPU_OS_MAC, GPU_DRIVER_ANY)) {
     /* There seems to be a synchronization issue with shadow rendering pass. If not waiting, the
      * surfels are lit without shadows. Waiting for sync here shouldn't be a huge bottleneck

FrenkyBojler/blender-ios

commit 26a3d71e8627805c705e4ad228107e08e6c00493

Fix: EEVEE: Broken AO compilation
Caused by 119dd6e81630b8234f8e4bad5ccbf0e4df65bdc8

source/blender/draw/engines/eevee/shaders/eevee_nodetree_lib.glsl
@@ -243,7 +243,7 @@ float ambient_occlusion_eval([[maybe_unused]] float3 normal,
   float4 noise = utility_tx_fetch(utility_tx, float2(texel), UTIL_BLUE_NOISE_LAYER);
   noise = fract(noise + sampling_rng_3D_get(SAMPLING_AO_U).xyzx);
 
-  float result = eevee::fast_gi::eval<float>(uniform_buf.raytrace.fast_gi_thickness,
+  float result = eevee::fast_gi::eval<float>(raytrace_buf.fast_gi_thickness,
                                              hiz_tx,
                                              hiz_tx /* Dummy. */,
                                              hiz_tx /* Dummy. */,