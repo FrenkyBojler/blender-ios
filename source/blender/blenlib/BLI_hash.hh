ce3d9cfe4891baf9b665770b70af088c6e0ea675
Jacques Lucke <jacques@blender.org>
2026-05-08 20:27:19 +0200

    add special hash function for function pointers

--- source/blender/blenlib/BLI_hash.hh
+++ source/blender/blenlib/BLI_hash.hh
@@ -265,4 +265,15 @@ template<typename T1, typename T2> struct DefaultHash<std::pair<T1, T2>> {
   }
 };
 
+/**
+ * Special overload for function pointers to avoid adding const to them which causes a warning
+ * msvc.
+ */
+template<typename Ret, typename... Args> struct DefaultHash<Ret (*)(Args...)> {
+  constexpr uint64_t operator()(Ret (*fn)(Args...)) const
+  {
+    return get_default_hash(static_cast<void *>(fn));
+  }
+};
+
 }  // namespace blender
