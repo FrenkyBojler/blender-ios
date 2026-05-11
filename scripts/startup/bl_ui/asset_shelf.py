blender/blender

commit 60b516f3329515b070039e580aa5e0daaca96962

Refactor: Assets: Make asset shelf filter options UI extendable
Part of the remote asset libraries project, see:
https://projects.blender.org/blender/blender/issues/134495

Instead of each asset shelf adding its own filter options panel, have
one general panel that individual asset shelves can extend with their
own settings. This way general filter options, such as the upcoming
"Include Online Assets" option can be combined with case-specific ones
(like "Filter By Active Tool" in the brush asset shelf).

Pull Request: https://projects.blender.org/blender/blender/pulls/153284

scripts/startup/bl_ui/asset_shelf.py
@@ -29,8 +29,21 @@ class ASSETSHELF_PT_display(Panel):
         return context.asset_shelf is not None
 
 
+class ASSETSHELF_PT_filter(Panel):
+    bl_label = "Filter"
+    # Doesn't actually matter. Panel is instanced through popover only.
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'HEADER'
+    bl_parent_id = "ASSETSHELF_PT_display"
+
+    def draw(self, context):
+        layout = self.layout
+        prefs = context.preferences
+
+
 classes = (
     ASSETSHELF_PT_display,
+    ASSETSHELF_PT_filter,
 )
 
 

scripts/startup/bl_ui/__init__.py
@@ -123,6 +123,7 @@ def register():
             register_class(cls)
 
     space_filebrowser.register_props()
+    properties_paint_common.register()
 
     from bpy.props import (
         EnumProperty,
@@ -185,6 +186,9 @@ def register():
 
 def unregister():
     from bpy.utils import unregister_class
+
+    properties_paint_common.unregister()
+
     for mod in reversed(_modules_loaded):
         for cls in reversed(mod.classes):
             if cls.is_registered:

scripts/startup/bl_ui/properties_paint_common.py
@@ -161,23 +161,14 @@ class BrushAssetShelf:
         )
 
 
-class VIEW3D_PT_brush_asset_shelf_filter(Panel):
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'HEADER'
-    bl_label = "Filter"
-    bl_parent_id = "ASSETSHELF_PT_display"
+def brush_asset_shelf_filter_draw(panel, context):
+    if context.asset_shelf.bl_idname != BrushAssetShelf.get_shelf_name_from_context(context):
+        return
 
-    @classmethod
-    def poll(cls, context):
-        if context.asset_shelf is None:
-            return False
-        return context.asset_shelf.bl_idname == BrushAssetShelf.get_shelf_name_from_context(context)
+    layout = panel.layout
+    prefs = context.preferences
 
-    def draw(self, context):
-        layout = self.layout
-        prefs = context.preferences
-
-        layout.prop(prefs.view, "use_filter_brushes_by_tool", text="By Active Tool")
+    layout.prop(prefs.view, "use_filter_brushes_by_tool", text="By Active Tool")
 
 
 class UnifiedPaintPanel:
@@ -1905,9 +1896,17 @@ def brush_basic_grease_pencil_vertex_settings(layout, context, brush, *, compact
 
 
 classes = (
-    VIEW3D_PT_brush_asset_shelf_filter,
     VIEW3D_MT_tools_projectpaint_clone,
 )
+
+
+def register():
+    bpy.types.ASSETSHELF_PT_filter.append(brush_asset_shelf_filter_draw)
+
+
+def unregister():
+    bpy.types.ASSETSHELF_PT_filter.remove(brush_asset_shelf_filter_draw)
+
 
 if __name__ == "__main__":  # only for live edit.
     from bpy.utils import register_class

FrenkyBojler/blender-ios

commit eaf6b9e125bd51de0e4df5ffff4f83904fe6acb6

Fix: Remove empty filters panel from compositor asset shelf
Caused by 60b516f3329515b070039e580aa5e0daaca96962
Filters panel isn't extended for compositor shelf, that results in
empty asset shelf. Add poll function to fix this.

scripts/startup/bl_ui/asset_shelf.py
@@ -36,6 +36,15 @@ class ASSETSHELF_PT_filter(Panel):
     bl_region_type = 'HEADER'
     bl_parent_id = "ASSETSHELF_PT_display"
 
+    @classmethod
+    def poll(cls, context):
+        if context.preferences.experimental.use_remote_asset_libraries:
+            return True
+        if context.space_data.type == 'NODE_EDITOR':
+            snode = context.space_data
+            return snode.tree_type != 'CompositorNodeTree'
+        return True
+
     def draw(self, context):
         layout = self.layout
         prefs = context.preferences