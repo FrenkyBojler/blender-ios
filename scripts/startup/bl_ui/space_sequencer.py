19fb86993865ba70c3fb85a0dfe043d074ef0997
Pratik Borhade <pratikborhade302@gmail.com>
2026-05-12 14:48:05 +0530

    Cleanup

--- scripts/startup/bl_ui/space_sequencer.py
+++ scripts/startup/bl_ui/space_sequencer.py
@@ -469,7 +469,6 @@ class SEQUENCER_MT_view(Menu):
                 layout.menu("SEQUENCER_MT_preview_zoom")
             layout.prop(st, "use_zoom_to_fit", text="Auto Zoom")
             layout.separator()
-
             layout.menu("SEQUENCER_MT_proxy")
             layout.operator_context = 'INVOKE_DEFAULT'
 
