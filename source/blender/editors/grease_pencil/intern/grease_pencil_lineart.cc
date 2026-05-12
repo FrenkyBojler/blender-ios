bd1defd688c55e779c23420597f2256481a538e4
YimingWu <xp8110@outlook.com>
2026-05-12 21:19:45 +0800

    Fix #158489: LineArt: Use render settings to bake strokes
    It's almost always preferred to use render settings to bake line art
    strokes (for mesh subdivision etc), so instead of evaluating the default
    depsgraph, a new depsgraph with `DAG_EVAL_RENDER` mode is created in
    order to bake all strokes with final render settings.

--- source/blender/editors/grease_pencil/intern/grease_pencil_lineart.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_lineart.cc
@@ -345,6 +345,8 @@ static void lineart_bake_endjob(void *customdata)
     Object *ob = bj->objects[object];
     WM_main_add_notifier(NC_GPENCIL | ND_DATA | NA_EDITED, ob);
   }
+
+  DEG_graph_free(bj->dg);
 }
 
 static void lineart_bake_job_free(void *customdata)
@@ -384,7 +386,7 @@ static wmOperatorStatus lineart_bake_common(bContext *C,
   bj->C = C;
   Scene *scene = CTX_data_scene(C);
   bj->scene = scene;
-  bj->dg = CTX_data_depsgraph_pointer(C);
+  bj->dg = DEG_graph_new(CTX_data_main(C),scene,CTX_data_view_layer(C),DAG_EVAL_RENDER),
   bj->frame_begin = scene->r.sfra;
   bj->frame_end = scene->r.efra;
   bj->frame_orig = scene->r.cfra;
