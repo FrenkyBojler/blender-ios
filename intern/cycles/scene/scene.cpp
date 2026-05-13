f938f2e4277e1289b289167d103b3ef15c692db7
Patrick Mours <pmours@nvidia.com>
2026-05-13 13:39:15 +0200

    Fix denoiser pass tests

--- intern/cycles/scene/scene.cpp
+++ intern/cycles/scene/scene.cpp
@@ -422,7 +422,9 @@ Scene::MotionType Scene::need_motion() const
   const bool denoiser_motion = integrator->get_use_denoise() &&
                                (integrator->get_denoiser_passes() &
                                 (DENOISER_PASS_MOTION | DENOISER_PASS_BACKWARD_MOTION)) != 0;
-  if (denoiser_motion || Pass::contains(passes, PASS_MOTION)) {
+  if (denoiser_motion || (Pass::contains(passes, PASS_MOTION) ||
+                          Pass::contains(passes, PASS_DENOISING_BACKWARD_MOTION)))
+  {
     return params.background ? MOTION_PASS : MOTION_PASS_INTERACTIVE;
   }
   return MOTION_NONE;
