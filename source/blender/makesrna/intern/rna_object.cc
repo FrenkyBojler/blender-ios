dd2ae9cbb8b5e2c4bccca90a735d9e61cd3f97e6
Colin Basnett <5035660+cmbasnett@users.noreply.github.com>
2026-05-13 12:57:22 -0700

    Fixed default value of head/tail factor

--- source/blender/makesrna/intern/rna_object.cc
+++ source/blender/makesrna/intern/rna_object.cc
@@ -3060,7 +3060,7 @@ static void rna_def_object(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "parent_bone_head_tail_factor", PROP_FLOAT, PROP_FACTOR);
   RNA_def_property_float_sdna(prop, nullptr, "parent_bone_head_tail_factor");
-  RNA_def_property_float_default(prop, 0.0f);
+  RNA_def_property_float_default(prop, 1.0f);
   RNA_def_property_range(prop, 0.0f, 1.0f);
   RNA_def_property_ui_range(prop, 0.0, 1.0, 1, 2);
   RNA_def_property_ui_text(prop, "Parent Bone Head/Tail", "Target along length of bone: Head is 0, Tail is 1");
