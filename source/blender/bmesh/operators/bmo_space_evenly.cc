11be8e99cf19982090667cc8605b399efcee08ed
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-05-14 02:03:28 +0000

    update for loop

--- source/blender/bmesh/operators/bmo_space_evenly.cc
+++ source/blender/bmesh/operators/bmo_space_evenly.cc
@@ -126,7 +126,7 @@ static std::optional<SpaceChainData> walk_edges(BMEdge *start_edge, Set<BMEdge *
 
   /* Skip chains where all vertices are at the same location. */
   bool all_stacked = true;
-  for (const int i : IndexRange(chain_data.verts.size() - 1)) {
+  for (const int i : chain_data.verts.index_range().drop_back(1)) {
     if (math::distance(float3(chain_data.verts[i]->co), float3(chain_data.verts[i + 1]->co)) >
         STACKED_THRESHOLD)
     {
