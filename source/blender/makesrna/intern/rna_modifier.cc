blender/blender

commit 5c912bdfdd9a42f5a12356449b2613b39f2215ca

Fix: Missing RNA path for nodes modifier bake data
This also fix liboverride handling of the relevant bake properties
(liboverride UI and operators are broken if the UI code cannot get
valid RNA paths).

This commit:
* Adds a 'rna ancestors'-based RNA path callback for NodesModifierBake
  struct.
* Changes the SpaceNodes context accessor to generate nodes PointerRNA
  with RNA ancestors info.
* Changes the `get_bake_draw_context` to also generate PointerRNA with
  RNA ancestors info.

Discovered while working on #154029 & !158148.

NOTE: There are likely many other places that will have to switch from
generating 'isolated' PointerRNA to ones with ancestors info in the
future...

Pull Request: https://projects.blender.org/blender/blender/pulls/158330

source/blender/makesrna/intern/rna_modifier.cc
@@ -2136,6 +2136,27 @@ static StructRNA *rna_NodesModifierBake_data_block_typef(PointerRNA *ptr)
   return ID_code_to_RNA_type(data_block->id_type);
 }
 
+static std::optional<std::string> rna_NodesModifierBake_path(const PointerRNA *ptr)
+{
+  const std::optional<AncestorPointerRNA> ancestor = RNA_struct_search_closest_ancestor_by_type(
+      ptr, RNA_NodesModifier);
+  if (!ancestor) {
+    return std::nullopt;
+  }
+
+  const ModifierData *md = static_cast<ModifierData *>(ancestor->data);
+  BLI_assert(md->type == eModifierType_Nodes);
+  const NodesModifierData *nmd = reinterpret_cast<const NodesModifierData *>(md);
+  const NodesModifierBake *nmd_bake = ptr->data_as<NodesModifierBake>();
+  Span<NodesModifierBake> bakes = {nmd->bakes, nmd->bakes_num};
+  if (!bakes.contains_ptr(nmd_bake)) {
+    return std::nullopt;
+  }
+  const int64_t idx = nmd_bake - bakes.begin();
+
+  return fmt::format("modifiers[\"{}\"].bakes[{}]", md->name, idx);
+}
+
 bool rna_GreasePencilModifier_material_poll(PointerRNA *ptr, PointerRNA value)
 {
   Object *ob = reinterpret_cast<Object *>(ptr->owner_id);
@@ -7996,6 +8017,7 @@ static void rna_def_modifier_nodes_bake(BlenderRNA *brna)
 
   srna = RNA_def_struct(brna, "NodesModifierBake", nullptr);
   RNA_def_struct_ui_text(srna, "Nodes Modifier Bake", "");
+  RNA_def_struct_path_func(srna, "rna_NodesModifierBake_path");
 
   prop = RNA_def_property(srna, "directory", PROP_STRING, PROP_DIRPATH);
   RNA_def_property_flag(prop, PROP_PATH_SUPPORTS_BLEND_RELATIVE);

source/blender/editors/space_node/space_node.cc
@@ -1347,7 +1347,8 @@ static int /*eContextResult*/ node_context(const bContext *C,
     if (snode->edittree) {
       for (bNode *node : snode->edittree->all_nodes()) {
         if (node->flag & NODE_SELECT) {
-          CTX_data_list_add(result, &snode->edittree->id, RNA_Node, node);
+          PointerRNA ptr = RNA_pointer_create_id_subdata(snode->edittree->id, RNA_Node, node);
+          CTX_data_list_add_ptr(result, &ptr);
         }
       }
     }
@@ -1357,7 +1358,8 @@ static int /*eContextResult*/ node_context(const bContext *C,
   if (CTX_data_equals(member, "active_node")) {
     if (snode->edittree) {
       bNode *node = bke::node_get_active(*snode->edittree);
-      CTX_data_pointer_set(result, &snode->edittree->id, RNA_Node, node);
+      PointerRNA ptr = RNA_pointer_create_id_subdata(snode->edittree->id, RNA_Node, node);
+      CTX_data_pointer_set_ptr(result, &ptr);
     }
 
     CTX_data_type_set(result, ContextDataType::Pointer);

source/blender/nodes/geometry/nodes/node_geo_bake.cc
@@ -605,8 +605,12 @@ bool get_bake_draw_context(const bContext *C, const bNode &node, BakeDrawContext
     return false;
   }
 
-  r_ctx.bake_rna = RNA_pointer_create_discrete(
-      const_cast<ID *>(&r_ctx.object->id), RNA_NodesModifierBake, (void *)r_ctx.bake);
+  PointerRNA modifier_ptr = RNA_pointer_create_id_subdata(
+      const_cast<ID &>(r_ctx.object->id),
+      RNA_NodesModifier,
+      const_cast<NodesModifierData *>(r_ctx.nmd));
+  r_ctx.bake_rna = RNA_pointer_create_with_parent(
+      modifier_ptr, RNA_NodesModifierBake, const_cast<NodesModifierBake *>(r_ctx.bake));
   if (r_ctx.nmd->runtime->cache) {
     const bke::bake::ModifierCache &cache = *r_ctx.nmd->runtime->cache;
     std::lock_guard lock{cache.mutex};

FrenkyBojler/blender-ios

commit bd5bd9b00e258ab3170c58c4436e7b067e899d17

Fix: Missing escaping of modifier name in NodesModifierBake RNA path
The modifier name was being used directly in the RNA path, which would
produce an invalid path if the name contained quotes.

This issue was introduced in 5c912bdfdd9.

source/blender/makesrna/intern/rna_modifier.cc
@@ -2154,7 +2154,7 @@ static std::optional<std::string> rna_NodesModifierBake_path(const PointerRNA *p
   }
   const int64_t idx = nmd_bake - bakes.begin();
 
-  return fmt::format("modifiers[\"{}\"].bakes[{}]", md->name, idx);
+  return fmt::format("modifiers[\"{}\"].bakes[{}]", BLI_str_escape(md->name), idx);
 }
 
 bool rna_GreasePencilModifier_material_poll(PointerRNA *ptr, PointerRNA value)