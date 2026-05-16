d1095315d8a5ea3f6fbde9d1ac356ce701b910e7
Julien Duroure <julien.duroure@gmail.com>
2026-05-16 16:38:05 +0200

    Enable COLOR filter at import

--- scripts/addons_core/io_scene_gltf2/io/imp/gltf2_io_binary_meshopt.py
+++ scripts/addons_core/io_scene_gltf2/io/imp/gltf2_io_binary_meshopt.py
@@ -126,6 +126,7 @@ class MeshoptDecoder:
                 'OCTAHEDRAL': lib.decodeFilterOct,
                 'QUATERNION': lib.decodeFilterQuat,
                 'EXPONENTIAL': lib.decodeFilterExp,
+                'COLOR': lib.decodeFilterExp,
             }.get(filter)
             filter_func(dst_ptr, count, byte_stride)
 
