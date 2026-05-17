ff7dae85b8d634cf572af4f72958b2e5b1fe7068
frogstomp <ales.zurnal@gmail.com>
2026-05-17 10:47:31 +0200

    include glyph in box calc

--- source/blender/sequencer/intern/effects/effect_text.cc
+++ source/blender/sequencer/intern/effects/effect_text.cc
@@ -1029,10 +1029,14 @@ static void apply_text_alignment(const TextVars *data,
                                  const int2 image_size)
 {
   const int box_width = text_box_width_get(runtime->lines);
-  const int box_height = (data->flag & SEQ_TEXT_USE_ABSOLUTE_LINE_SPACING) ?
-                             runtime->lines.size() * data->abs_space_line :
-                             runtime->line_height + (runtime->lines.size() - 1) *
-                                                        runtime->line_height * data->space_line;
+  rctf glyph_bounds_max;
+  BLF_bounds_max(runtime->font, &glyph_bounds_max);
+
+  const int box_height = (runtime->lines.size() - 1) *
+                             ((data->flag & SEQ_TEXT_USE_ABSOLUTE_LINE_SPACING) ?
+                                  data->abs_space_line :
+                                  runtime->line_height * data->space_line) +
+                         math::ceil(BLI_rctf_size_y(&glyph_bounds_max));
 
   const float2 image_center{data->loc[0] * image_size.x, data->loc[1] * image_size.y};
   const float2 line_height_offset{0.0f,
