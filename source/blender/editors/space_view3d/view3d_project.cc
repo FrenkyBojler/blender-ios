b3ba8e8db4638a9df0a0af679347c7ca110df177
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-05-17 11:04:00 -0700

    Fix: View shift problem.

--- source/blender/editors/space_view3d/view3d_project.cc
+++ source/blender/editors/space_view3d/view3d_project.cc
@@ -723,8 +723,8 @@ void ED_view3d_win_to_vector(const ARegion *region, const float mval[2], float r
   RegionView3D *rv3d = static_cast<RegionView3D *>(region->regiondata);
 
   if (rv3d->is_persp) {
-    r_out[0] = (2.0f * (mval[0] / region->winx) - 1.0f) / rv3d->winmat[0][0];
-    r_out[1] = (2.0f * (mval[1] / region->winy) - 1.0f) / rv3d->winmat[1][1];
+    r_out[0] = (2.0f * (mval[0] / region->winx) - 1.0f + rv3d->winmat[2][0]) / rv3d->winmat[0][0];
+    r_out[1] = (2.0f * (mval[1] / region->winy) - 1.0f + rv3d->winmat[2][1]) / rv3d->winmat[1][1];
     r_out[2] = -1.0f;
     mul_transposed_mat3_m4_v3(rv3d->viewmat, r_out);
   }
