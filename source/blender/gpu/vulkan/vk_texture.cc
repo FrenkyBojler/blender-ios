6adb110c26111b409e560016bb1ec37b87350c0f
Christoph Neuhauser <christoph.neuhauser@intel.com>
2026-05-18 11:47:40 +0200

    Fix: Vulkan: Avoid reading from non-host coherent memory without sync
    In `VKTexture::read_sub`, the staging buffer is created with the
    `VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT` flag. While it is only
    accessed sequentially, it is stated that this gave better performance
    with AMD GPUs.
    VMA tries to preferrably find a memory type with the property flag
    `VK_MEMORY_PROPERTY_HOST_CACHED_BIT` in this case.
    
    Unlike most other drivers by Intel, AMD and NVIDIA, the Intel ANV Mesa
    driver does not expose memory that is both host coherent and host
    cached for some Gen12 GPUs (like Arrow Lake and Meteor Lake iGPUs), but
    only memory that is either only host coherent or only host cached.
    This leads to the issue that host cached memory is selected by VMA due
    to using `VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT`, but neither
    `vkFlushMappedMemoryRanges` nor `vkInvalidateMappedMemoryRanges` are
    called, even though they are required for non-coherent memory. This
    leads to race conditions and memory corruptions with these iGPUs.
    
    This issue is fixed by introducing two new functions
    `VKBuffer::flush_mapped_memory` and
    `VKBuffer::invalidate_mapped_memory`, which are used by VKTexture.
    These new functions have no effect when the memory is host coherent.
    
    It was also alternatively evaluated if switching to
    `VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT` fixes the
    issue. While that also fixed the issue, it resulted in slightly worse
    performance (3.9s vs. 3.7s for `GPUVulkanTest` over multiple runs).
    
    Co-Authored-By: Brandon Jones <brandon1.jones@intel.com>

--- source/blender/gpu/vulkan/vk_texture.cc
+++ source/blender/gpu/vulkan/vk_texture.cc
@@ -318,6 +318,7 @@ void VKTexture::read_sub(
     size_t data_offset = full_transfer_region.result_offset(transfer_region.offset,
                                                             transfer_region.layers.start()) *
                          sample_bytesize;
+    staging_buffer.invalidate_mapped_memory();
     convert_device_to_host(static_cast<void *>(static_cast<uint8_t *>(r_data) + data_offset),
                            staging_buffer.mapped_memory_get(),
                            sample_len,
@@ -495,6 +496,7 @@ void VKTexture::update_sub(int mip,
         dst_ptr += dst_row_stride;
       }
     }
+    staging_buffer.flush_mapped_memory();
   }
   else {
     BLI_assert(pixel_buffer);
