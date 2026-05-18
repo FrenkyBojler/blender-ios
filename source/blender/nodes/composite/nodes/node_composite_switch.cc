906f6a97f1f38b7a3b965577e514c6558a4afefc
Omar Emara <mail@OmarEmara.dev>
2026-05-18 15:00:26 +0300

    Change deprecation notice

--- source/blender/nodes/composite/nodes/node_composite_switch.cc
+++ source/blender/nodes/composite/nodes/node_composite_switch.cc
@@ -59,7 +59,7 @@ static void node_register()
   ntype.declare = node_declare;
   bke::node_type_size_preset(ntype, bke::eNodeSizePreset::Default);
   ntype.get_compositor_operation = get_compositor_operation;
-  ntype.deprecation_notice = N_("Use the GeometryNodeSwitch node instead");
+  ntype.deprecation_notice = N_("Use Utilities \u25B8 Switch  node instead");
 
   bke::node_register_type(ntype);
 }
