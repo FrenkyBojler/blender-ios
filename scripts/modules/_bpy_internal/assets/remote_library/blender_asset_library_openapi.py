9df38e15896fbc13401299d2955e7af5d7f03cd7
Sybren A. Stüvel <sybren@blender.org>
2026-05-18 14:59:14 +0200

    Make it work for online assets

--- scripts/modules/_bpy_internal/assets/remote_library/blender_asset_library_openapi.py
+++ scripts/modules/_bpy_internal/assets/remote_library/blender_asset_library_openapi.py
@@ -238,7 +238,7 @@ class AssetMetadataV1:
     .cats.txt file, if that's ever necessary.
     """
 
-    import_meth: str | None = None
+    preferred_import_method: str | None = None
     """The import method preferred by this asset.
 
     For example, base meshes for sculpting can declare they should
--- scripts/modules/_bpy_internal/assets/remote_library/cli_listing_generator_asset_finder.py
+++ scripts/modules/_bpy_internal/assets/remote_library/cli_listing_generator_asset_finder.py
@@ -142,7 +142,7 @@ def _get_asset_meta(asset_data: bpy.types.AssetData) -> api_models.AssetMetadata
     if asset_data.copyright:
         meta.copyright = asset_data.copyright
     if asset_data.use_preferred_import_method:
-        meta.import_meth = asset_data.preferred_import_method
+        meta.preferred_import_method = asset_data.preferred_import_method
 
     # Convert custom properties.
     import rna_prop_ui
