02c14b399ea6455416957a09e288440b26cdb155
Jeroen Bakker <jeroen@blender.org>
2026-05-19 12:26:07 +0200

    Improve the comment

--- source/blender/draw/engines/eevee/shaders/eevee_lightprobe_sphere_remap_comp.glsl
+++ source/blender/draw/engines/eevee/shaders/eevee_lightprobe_sphere_remap_comp.glsl
@@ -134,7 +134,8 @@ void main()
      * loops. Unroll is needed as driver might decide to not unroll in shaders with more
      * complexity. */
     /* Vulkan validation layers detects a data race on `local_radiance[local_index] +=
-     * local_radiance[local_index + stride]`. This is a false positive. */
+     * local_radiance[local_index + stride]`. This is a false positive. Even when doing a manual
+     * unroll or make the variable `shared coherent` doesn't work around it. */
     for (uint i = 0; i < 10; i++) [[unroll]] {
       barrier();
       uint stride = group_size >> (i + 1u);
@@ -158,7 +159,8 @@ void main()
      * loops. Unroll is needed as driver might decide to not unroll in shaders with more
      * complexity. */
     /* Vulkan validation layers detects a data race on `local_direction[local_index] +=
-     * local_direction[local_index + stride]`. This is a false positive. */
+     * local_direction[local_index + stride]`. This is a false positive. Even when doing a manual
+     * unroll or make the variable `shared coherent` doesn't work around it. */
     for (uint i = 0; i < 10; i++) [[unroll]] {
       barrier();
       uint stride = group_size >> (i + 1u);
@@ -181,7 +183,8 @@ void main()
      * loops. Unroll is needed as driver might decide to not unroll in shaders with more
      * complexity. */
     /* Vulkan validation layers detects a data race on `local_radiance[local_index] +=
-     * local_radiance[local_index + stride]`. This is a false positive. */
+     * local_radiance[local_index + stride]`. This is a false positive. Even when doing a manual
+     * unroll or make the variable `shared coherent` doesn't work around it. */
     for (uint i = 0; i < 10; i++) [[unroll]] {
       barrier();
       uint stride = group_size >> (i + 1u);
