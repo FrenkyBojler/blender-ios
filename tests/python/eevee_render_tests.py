a4b0629fd1cc0f57883dade1958ee1a29864ceaa
Clément Foucault <foucault.clem@gmail.com>
2026-05-19 21:31:23 +0200

    Update threshold special cases

--- tests/python/eevee_render_tests.py
+++ tests/python/eevee_render_tests.py
@@ -50,6 +50,8 @@ BLOCKLIST = [
     "hair_ribbon_close_up.blend",
     # Redundant with transparent_shadow.
     "transparent_shadow_limit_.*",
+    # Redundant with transparent_shadow_hair.
+    "transparent_shadow_hair_blur.blend",
 ]
 
 BLOCKLIST_METAL = [
@@ -63,6 +65,8 @@ BLOCKLIST_METAL = [
     "image.blend",
     # Blocked due to subtle differences in DOF
     "osl_camera_advanced.blend",
+    # Blocked due to volume occupancy being broken
+    "texture_coordinate_object.blend",
 ]
 
 BLOCKLIST_VULKAN = [
@@ -251,8 +255,8 @@ def main():
     elif args.gpu_backend == "opengl":
         blocklist += BLOCKLIST_OPENGL
 
+    gpu_vendor = render_report.get_gpu_device_vendor(args.blender)
     if os.getenv("BLENDER_TEST_IGNORE_VENDOR_BLOCKLIST") is None:
-        gpu_vendor = render_report.get_gpu_device_vendor(args.blender)
         if gpu_vendor == "INTEL":
             blocklist += BLOCKLIST_INTEL
         if gpu_vendor == "INTEL" and sys.platform == "win32" and args.gpu_backend == "opengl":
@@ -266,60 +270,36 @@ def main():
 
     report.set_pixelated(True)
     report.set_reference_dir("eevee_renders")
-    report.set_fail_percent(0.06)
-    report.set_fail_threshold(2.0 / 255.0)
+    # Default settings are too lose. EEVEE renders have much less noise than a path tracer.
+    report.set_fail_percent(0.08)
+    report.set_fail_threshold(4.0 / 255.0)
 
-    # test_dir_name = Path(args.testdir).name
-    # if test_dir_name.startswith('image_mapping'):
-    #     # Platform dependent border values. To be fixed
-    #     report.set_fail_threshold(0.2)
-    # elif test_dir_name.startswith('image'):
-    #     report.set_fail_threshold(0.051)
-    # elif test_dir_name.startswith('displacement'):
-    #     # metal shadow and wireframe difference. To be fixed.
-    #     report.set_fail_threshold(0.07)
-    # elif test_dir_name.startswith('bsdf'):
-    #     # metallic thinfilm tests and dithered transparency
-    #     report.set_fail_threshold(0.045)
-    # elif test_dir_name.startswith('principled_bsdf'):
-    #     # principled bsdf transmission test
-    #     report.set_fail_threshold(0.02)
-    # elif test_dir_name.startswith('camera'):
-    #     # Line/rasterization difference (Old AMD/Linux/OpenGL only, see #154515)
-    #     report.set_fail_threshold(0.0375)
-    # elif test_dir_name.startswith('raycast'):
-    #     # Line/rasterization difference (Old AMD/Linux/OpenGL only, see #154516)
-    #     report.set_fail_threshold(0.02)
-
-    # # Noise pattern changes depending on platform. Mostly caused by transparency.
-    # # TODO(fclem): See if we can just increase number of samples per file.
-    # if test_dir_name.startswith('render_layer'):
-    #     # shadow pass, rlayer flag
-    #     report.set_fail_threshold(0.08)
-    # elif test_dir_name.startswith('hair'):
-    #     # hair close up
-    #     report.set_fail_threshold(0.0275)
-    # elif test_dir_name.startswith('integrator'):
-    #     # Noise difference in transparent materials
-    #     report.set_fail_threshold(0.05)
-    # elif test_dir_name.startswith('pointcloud'):
-    #     # points transparent
-    #     report.set_fail_threshold(0.06)
-    # elif test_dir_name.startswith("lightprobe"):
-    #     # Avoid higher threshold of the light case
-    #     report.set_fail_threshold(0.01)
-    # elif test_dir_name.startswith('light_linking'):
-    #     # Noise difference in transparent material
-    #     report.set_fail_threshold(0.05)
-    # elif test_dir_name.startswith('light'):
-    #     # Noise difference in background
-    #     report.set_fail_threshold(0.03)
-    # elif test_dir_name.startswith('texture'):
-    #     # Noise difference in "white noise 256pp" (Old AMD/Linux/OpenGL only, see #154515)
-    #     report.set_fail_threshold(0.02)
-    # elif test_dir_name.startswith('instancing'):
-    #     # Noise difference in "instance_types" on the point-clouds (Metal only, to investigate)
-    #     report.set_fail_threshold(0.02)
+    test_dir_name = Path(args.testdir).name
+    if gpu_vendor == "NVIDIA":
+        # References are supposed to be generated on Nvidia. Do not loosen the threshold for this platform.
+        pass
+    elif test_dir_name.startswith('image_colorspace'):
+        # image_log has hot pixels that result in platform differences.
+        report.set_fail_percent(0.15)
+    elif test_dir_name.startswith('displacement'):
+        # Real & bump displacement use hardware derivatives which results in platform differences.
+        report.set_fail_percent(0.3)
+    elif test_dir_name.startswith('bsdf'):
+        # Dithered transparency uses platform dependent noise pattern. TODO split to another folder
+        report.set_fail_percent(0.22)
+        report.set_fail_threshold(10.0 / 255.0)
+    elif test_dir_name.startswith('hair'):
+        # transparent_shadow_hair uses transparency. TODO increase sample count
+        report.set_fail_threshold(6.0 / 255.0)
+    elif test_dir_name.startswith('instancing'):
+        # Small pointcloud has platform dependent raster pattern
+        report.set_fail_threshold(8.0 / 255.0)
+    elif test_dir_name.startswith('integrator'):
+        # Noise difference in transparent materials (mostly transparent_spatial_splits)
+        report.set_fail_threshold(8.0 / 255.0)
+    elif test_dir_name.startswith('light_linking'):
+        # Noise difference in transparent materials (mostly shadow_link_transparency) and volume
+        report.set_fail_threshold(8.0 / 255.0)
 
     ok = report.run(args.testdir, args.blender, get_arguments, batch=args.batch)
     sys.exit(not ok)
