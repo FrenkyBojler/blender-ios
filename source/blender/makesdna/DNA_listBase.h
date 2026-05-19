5482212a55e02a2736aab3db3f8b1d8ab32d0180
Jacques Lucke <jacques@blender.org>
2026-05-19 19:29:29 +0200

    add is_single accessor

--- source/blender/makesdna/DNA_listBase.h
+++ source/blender/makesdna/DNA_listBase.h
@@ -53,6 +53,11 @@ template<typename T> struct ListBaseT : public ListBase {
     return this->first == nullptr;
   }
 
+  bool is_single() const
+  {
+    return this->first != nullptr && this->last == this->first;
+  }
+
   /* TODO: Add const and non-const iterators. However this will require some refactoring
    * as some places rely on being able to get a mutable list element from a const list. */
   ListBaseTIterator<T> begin() const
