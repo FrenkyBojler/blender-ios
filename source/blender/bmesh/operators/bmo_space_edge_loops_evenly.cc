9e068f3768cde0363517653af1f253b595b30378
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-05-19 21:17:34 +0000

    use const

--- source/blender/bmesh/operators/bmo_space_edge_loops_evenly.cc
+++ source/blender/bmesh/operators/bmo_space_edge_loops_evenly.cc
@@ -220,10 +220,10 @@ static SpaceMeasurements measure_chain(const SpaceChainData &chain)
 /**
  * Compute cubic spline coefficients for one coordinate axis.
  */
-static void calculate_splines_axis(Span<float> distances,
-                                   Span<float> coords,
+static void calculate_splines_axis(const Span<float> distances,
+                                   const Span<float> coords,
                                    const bool is_closed,
-                                   float total_length,
+                                   const float total_length,
                                    Vector<SplineCoeffs> &r_coeffs)
 {
   const int verts_num = coords.size();
@@ -305,7 +305,7 @@ static void calculate_splines_axis(Span<float> distances,
 }
 
 /** Return the index of the spline segment that contains target_distance. */
-static int calc_spline_segment(Span<float> knot_distances, const float target_distance)
+static int calc_spline_segment(const Span<float> knot_distances, const float target_distance)
 {
   const int segment_index = binary_search::last_if(
       knot_distances, [&](const float value) { return value <= target_distance; });
@@ -313,14 +313,14 @@ static int calc_spline_segment(Span<float> knot_distances, const float target_di
 }
 
 /** Evaluates the cubic spline at target_distance. */
-static float3 evaluate_cubic(Span<float> tknots,
-                             Span<SplineCoeffs> coeffs_x,
-                             Span<SplineCoeffs> coeffs_y,
-                             Span<SplineCoeffs> coeffs_z,
+static float3 evaluate_cubic(const Span<float> tknots,
+                             const Span<SplineCoeffs> coeffs_x,
+                             const Span<SplineCoeffs> coeffs_y,
+                             const Span<SplineCoeffs> coeffs_z,
                              const float target_distance)
 {
-  int segment = calc_spline_segment(tknots, target_distance);
-  float dt = target_distance - coeffs_x[segment].x;
+  const int segment = calc_spline_segment(tknots, target_distance);
+  const float dt = target_distance - coeffs_x[segment].x;
 
   const SplineCoeffs &cx = coeffs_x[segment];
   const SplineCoeffs &cy = coeffs_y[segment];
