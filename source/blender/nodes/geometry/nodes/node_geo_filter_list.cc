f1a7feefd0022e46986d12baf799ea91f691f5ad
Hans Goudey <hans@blender.org>
2026-05-20 11:04:37 -0400

    Geometry Nodes: Filter List node
    A simple node to remove some items from a list. The predicate
    for removal is provided in the form of either a boolean field or
    a boolean list.

---
+++ source/blender/nodes/geometry/nodes/node_geo_filter_list.cc
@@ -0,0 +1,171 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BLI_array_utils.hh"
+
+#include "NOD_geometry_nodes_list.hh"
+#include "NOD_geometry_nodes_values.hh"
+#include "NOD_rna_define.hh"
+#include "NOD_socket.hh"
+#include "NOD_socket_search_link.hh"
+
+#include "RNA_enum_types.hh"
+
+#include "UI_interface_layout.hh"
+#include "UI_resources.hh"
+
+#include "list_function_eval.hh"
+#include "node_geometry_util.hh"
+
+namespace blender::nodes::node_geo_filter_list_cc {
+
+static void node_declare(NodeDeclarationBuilder &b)
+{
+  b.use_custom_socket_order();
+  b.allow_any_socket_order();
+  b.add_default_layout();
+  const bNode *node = b.node_or_null();
+  if (!node) {
+    return;
+  }
+  const auto type = eNodeSocketDatatype(node->custom1);
+  b.add_input(type, "List"_ustr).structure_type(StructureType::List).hide_value();
+  b.add_output(type, "List"_ustr)
+      .dependent_field({1})
+      .structure_type(StructureType::List)
+      .align_with_previous();
+  b.add_input<decl::Bool>("Keep"_ustr).hide_value().structure_type(StructureType::Dynamic);
+}
+
+static void node_layout(ui::Layout &layout, bContext * /*C*/, PointerRNA *ptr)
+{
+  layout.prop(ptr, "socket_type", UI_ITEM_NONE, "", ICON_NONE);
+}
+
+class SocketSearchOp {
+ public:
+  UString socket_name;
+  eNodeSocketDatatype socket_type;
+  void operator()(LinkSearchOpParams &params)
+  {
+    bNode &node = params.add_node("GeometryNodeFilterList"_ustr);
+    node.custom1 = socket_type;
+    params.update_and_connect_available_socket(node, socket_name);
+  }
+};
+
+static void node_gather_link_searches(GatherLinkSearchOpParams &params)
+{
+  const eNodeSocketDatatype socket_type = eNodeSocketDatatype(params.other_socket().type);
+  if (params.in_out() == SOCK_IN) {
+    if (params.node_tree().typeinfo->validate_link(socket_type, SOCK_BOOLEAN)) {
+      params.add_item(IFACE_("Keep"), SocketSearchOp{"Keep"_ustr, SOCK_BOOLEAN});
+    }
+    params.add_item(IFACE_("List"), SocketSearchOp{"List"_ustr, socket_type});
+  }
+  else {
+    params.add_item(IFACE_("Value"), SocketSearchOp{"Value"_ustr, socket_type});
+  }
+}
+
+static void node_rna(StructRNA *srna)
+{
+  RNA_def_node_enum(
+      srna,
+      "socket_type",
+      "Socket Type",
+      "Value may be implicitly converted if the type does not match",
+      rna_enum_node_socket_data_type_items,
+      NOD_inline_enum_accessors(custom1),
+      SOCK_FLOAT,
+      [](bContext * /*C*/, PointerRNA *ptr, PropertyRNA * /*prop*/, bool *r_free) {
+        *r_free = true;
+        const bNodeTree &ntree = *reinterpret_cast<bNodeTree *>(ptr->owner_id);
+        bke::bNodeTreeType *ntree_type = ntree.typeinfo;
+        return enum_items_filter(
+            rna_enum_node_socket_data_type_items, [&](const EnumPropertyItem &item) -> bool {
+              bke::bNodeSocketType *socket_type = bke::node_socket_type_find_static(item.value);
+              return ntree_type->valid_socket_type(ntree_type, socket_type);
+            });
+      });
+}
+
+static GListPtr filter_list(const GList &list, const IndexMask &mask)
+{
+  const CPPType &list_type = list.cpp_type();
+  return std::visit(
+      [&]<typename T>(const T &src_data) {
+        if constexpr (std::is_same_v<T, GList::ArrayData>) {
+          GArray<> dst_data(list_type, mask.size(), NoInitialization());
+          array_utils::gather(GSpan(list_type, src_data.data, list.size()), mask, dst_data);
+          return GList::from_garray(std::move(dst_data));
+        }
+        else if constexpr (std::is_same_v<T, GList::SingleData>) {
+          return GList::create(list_type, src_data, mask.size());
+        }
+      },
+      list.data());
+}
+
+static void node_geo_exec(GeoNodeExecParams params)
+{
+  GListPtr list = params.extract_input<GListPtr>("List"_ustr);
+  if (!list) {
+    params.set_default_remaining_outputs();
+    return;
+  }
+  auto filter_value = params.extract_input<bke::SocketValueVariant>("Keep"_ustr);
+  if (filter_value.is_single()) {
+    if (filter_value.get<bool>()) {
+      params.set_output("List"_ustr, std::move(list));
+    }
+    else {
+      params.set_default_remaining_outputs();
+    }
+  }
+  else if (filter_value.is_context_dependent_field()) {
+    ListFieldContext field_context;
+    fn::FieldEvaluator field_evaluator(field_context, list->size());
+    field_evaluator.add(filter_value.extract<Field<bool>>());
+    field_evaluator.evaluate();
+    const IndexMask mask = field_evaluator.get_evaluated_as_mask(0);
+    GListPtr filtered_list = filter_list(*list, mask);
+    params.set_output("List"_ustr, std::move(filtered_list));
+  }
+  else if (filter_value.is_list()) {
+    const GListPtr keep_list = filter_value.get<GListPtr>();
+    const VArray<bool> values = keep_list->varray().typed<bool>();
+    if (values.size() < list->size()) {
+      params.error_message_add(NodeWarningType::Error, "\"Keep\" list is too small");
+      params.set_default_remaining_outputs();
+      return;
+    }
+    IndexMaskMemory memory;
+    const IndexMask mask = IndexMask::from_bools(values, memory);
+    GListPtr filtered_list = filter_list(*list, mask);
+    params.set_output("List"_ustr, std::move(filtered_list));
+  }
+  else {
+    params.error_message_add(NodeWarningType::Warning, "\"Keep\" input must be a field or a list");
+    params.set_output("List"_ustr, std::move(list));
+  }
+}
+
+static void node_register()
+{
+  static bke::bNodeType ntype;
+  geo_node_type_base(&ntype, "GeometryNodeFilterList"_ustr);
+  ntype.ui_name = "Filter List";
+  ntype.ui_description = "Remove items from a list";
+  ntype.nclass = NODE_CLASS_CONVERTER;
+  ntype.geometry_node_execute = node_geo_exec;
+  ntype.draw_buttons = node_layout;
+  ntype.declare = node_declare;
+  ntype.gather_link_search_ops = node_gather_link_searches;
+  bke::node_register_type(ntype);
+  node_rna(ntype.rna_ext.srna);
+}
+NOD_REGISTER_NODE(node_register)
+
+}  // namespace blender::nodes::node_geo_filter_list_cc
