23411f04651bc3320ab4630fcb2c5e7153b24d8d
Hans Goudey <hans@blender.org>
2026-05-20 09:17:20 -0400

    Writing only in comment

--- source/blender/imbuf/IMB_openexr.hh
+++ source/blender/imbuf/IMB_openexr.hh
@@ -32,6 +32,8 @@ ExrHandle *IMB_exr_get_handle(bool write_multipart = false);
  * The number of channels is determined by channelnames.size() with
  * each character a channel name.
  * Layer and pass name, view name and colorspace are all optional.
+ *
+ * Used only for writing files.
  */
 void IMB_exr_add_channels(ExrHandle *handle,
                           StringRefNull layerpassname,
