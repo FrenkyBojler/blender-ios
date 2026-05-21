12a31a9f8bc1c7ee7d8bd432e3cc7d94317201bb
Sybren A. Stüvel <sybren@blender.org>
2026-05-21 12:45:01 +0200

    make format

--- source/blender/editors/asset/intern/asset_indexer.cc
+++ source/blender/editors/asset/intern/asset_indexer.cc
@@ -287,7 +287,7 @@ AssetMetaData *asset_metadata_from_dictionary(const DictionaryValue &entry)
      * that. This is also the only way to support more than a single property. */
     if (properties && (properties->next || properties->type != IDP_GROUP)) {
       asset_data->properties = bke::idprop::create_group("AssetMetaData.properties").release();
-      for (IDProperty *property = properties; property != nullptr; ) {
+      for (IDProperty *property = properties; property != nullptr;) {
         /* Save next before IDP_AddToGroup (via BLI_addtail) overwrites property->next. */
         IDProperty *next = property->next;
         IDP_AddToGroup(asset_data->properties, property);
