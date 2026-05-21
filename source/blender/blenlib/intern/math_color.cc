1e625ad557ef2f9899674805010c085363260bd0
jlampel <jlampel@autotroph.com>
2026-05-21 14:38:05 -0700

    Add rgb to okhsv math for cpu

--- source/blender/blenlib/intern/math_color.cc
+++ source/blender/blenlib/intern/math_color.cc
@@ -475,6 +475,133 @@ void rgb_to_oklch(float r, float g, float b, float *r_l, float *r_c, float *r_h)
   *r_h = atan2(lab_b, lab_a);
 }
 
+float oklab_compute_max_saturation(float a, float b)
+{
+    // Max saturation will be when one of r, g or b goes below zero.
+
+    // Select different coefficients depending on which component goes below zero first
+    float k0, k1, k2, k3, k4, wl, wm, ws;
+
+    if (-1.88170328f * a - 0.80936493f * b > 1)
+    {
+        // Red component
+        k0 = +1.19086277f; k1 = +1.76576728f; k2 = +0.59662641f; k3 = +0.75515197f; k4 = +0.56771245f;
+        wl = +4.0767416621f; wm = -3.3077115913f; ws = +0.2309699292f;
+    }
+    else if (1.81444104f * a - 1.19445276f * b > 1)
+    {
+        // Green component
+        k0 = +0.73956515f; k1 = -0.45954404f; k2 = +0.08285427f; k3 = +0.12541070f; k4 = +0.14503204f;
+        wl = -1.2684380046f; wm = +2.6097574011f; ws = -0.3413193965f;
+    }
+    else
+    {
+        // Blue component
+        k0 = +1.35733652f; k1 = -0.00915799f; k2 = -1.15130210f; k3 = -0.50559606f; k4 = +0.00692167f;
+        wl = -0.0041960863f; wm = -0.7034186147f; ws = +1.7076147010f;
+    }
+
+    // Approximate max saturation using a polynomial:
+    float S = k0 + k1 * a + k2 * b + k3 * a * a + k4 * a * b;
+
+    // Do one step Halley's method to get closer
+    // this gives an error less than 10e6, except for some blue hues where the dS/dh is close to infinite
+    // this should be sufficient for most applications, otherwise do two/three steps
+
+    float k_l = +0.3963377774f * a + 0.2158037573f * b;
+    float k_m = -0.1055613458f * a - 0.0638541728f * b;
+    float k_s = -0.0894841775f * a - 1.2914855480f * b;
+
+    {
+        float l_ = 1.f + S * k_l;
+        float m_ = 1.f + S * k_m;
+        float s_ = 1.f + S * k_s;
+
+        float l = l_ * l_ * l_;
+        float m = m_ * m_ * m_;
+        float s = s_ * s_ * s_;
+
+        float l_dS = 3.f * k_l * l_ * l_;
+        float m_dS = 3.f * k_m * m_ * m_;
+        float s_dS = 3.f * k_s * s_ * s_;
+
+        float l_dS2 = 6.f * k_l * k_l * l_;
+        float m_dS2 = 6.f * k_m * k_m * m_;
+        float s_dS2 = 6.f * k_s * k_s * s_;
+
+        float f  = wl * l     + wm * m     + ws * s;
+        float f1 = wl * l_dS  + wm * m_dS  + ws * s_dS;
+        float f2 = wl * l_dS2 + wm * m_dS2 + ws * s_dS2;
+
+        S = S - f * f1 / (f1*f1 - 0.5f * f * f2);
+    }
+
+    return S;
+}
+
+void oklab_find_cusp(float a, float b, float *r_l, float*r_c)
+{
+	float s_cusp = oklab_compute_max_saturation(a, b);
+  float r_at_max, g_at_max, b_at_max;
+	oklab_to_rgb(1, s_cusp * a, s_cusp * b, &r_at_max, &g_at_max, &b_at_max);
+	*r_l = cbrtf(1.f / fmaxf(fmaxf(r_at_max, g_at_max), b_at_max));
+	*r_c = *r_l * s_cusp;
+}
+
+float oklab_toe(float x)
+{
+	constexpr float k_1 = 0.206f;
+	constexpr float k_2 = 0.03f;
+	constexpr float k_3 = (1.f + k_1) / (1.f + k_2);
+	return 0.5f * (k_3 * x - k_1 + sqrtf((k_3 * x - k_1) * (k_3 * x - k_1) + 4 * k_2 * k_3 * x));
+}
+
+float oklab_toe_inverse(float x)
+{
+	constexpr float k_1 = 0.206f;
+	constexpr float k_2 = 0.03f;
+	constexpr float k_3 = (1.f + k_1) / (1.f + k_2);
+	return (x * x + k_1 * x) / (k_3 * (x + k_2));
+}
+
+void rgb_to_okhsv(float r, float g, float b, float *r_h, float *r_s, float *r_v)
+{
+  float l, a, b;
+  rgb_to_oklab(r, g, b, &l, &a, &b);
+
+  float c = sqrtf(a * a + b * b);
+  float a_ = a / c;
+  float b_ = b / c;
+
+  float l_cusp, c_cusp;
+  oklab_find_cusp(a_, b_, &l_cusp, &c_cusp);
+  float s_max = c_cusp / l_cusp;
+  float t_max = c_cusp / (1 - l_cusp);
+  float s_0 = 0.5f;
+  float k = 1 - s_0 / s_max;
+
+  float t = t_max / (c + l * t_max);
+  float l_v = t * l;
+  float c_v = t * c;
+
+  float l_vt = oklab_toe_inverse(l_v);
+  float c_vt = c_v * l_vt / l_v;
+
+  float r_scale, g_scale, b_scale;
+  oklab_to_rgb(l_vt, a_ * c_vt, b_ * c_vt, &r_scale, &g_scale, &b_scale);
+  float scale_l = cbrtf(1.0f / fmaxf(fmaxf(r_scale, g_scale), fmaxf(b_scale, 0.0f)));
+
+  l = l / scale_l;
+  c = c / scale_l;
+
+  c = c * oklab_toe(l) / l;
+  l = oklab_toe(l);
+
+  *r_h = 0.5f + 0.5f * atan2f(-b, -a) / 3.1415926536f;
+  *r_v = l / l_v;
+  *r_s = (s_0 + t_max) * c_v / ((t_max * s_0) + t_max * k * c_v);
+}
+
 void hsv_clamp_v(float hsv[3], float v_max)
 {
   if (UNLIKELY(hsv[0] < 0.0f || hsv[0] > 1.0f)) {
