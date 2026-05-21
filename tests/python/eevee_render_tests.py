fa625bb71ed0d4bc32cd898444521b9e81321324
Clément Foucault <foucault.clem@gmail.com>
2026-05-21 15:55:15 +0200

    Try removing intel blocklist

--- tests/python/eevee_render_tests.py
+++ tests/python/eevee_render_tests.py
@@ -95,10 +95,6 @@ BLOCKLIST_INTEL = [
 ]
 
 BLOCKLIST_INTEL_WINDOWS_GL = [
-    # Fails sporadically and causes all subsequent volume tests to fail (See #153612).
-    "volume_instance.blend"
-    # Seems to be missing indirect lighting. Could be driver bug.
-    "background_scene.blend"
 ]
 
 
