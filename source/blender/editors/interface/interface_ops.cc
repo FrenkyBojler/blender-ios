d806e89b961e69d2d8f266f9039b6cdd44a9cf00
Hans Goudey <hans@blender.org>
2026-05-22 17:11:33 -0400

    Cleanup: Use enum class for node interface item type, remove casts
    - The type is used directly in DNA now, no need for casting
    - Remove "e" prefix
    - Use enum class and corresponding naming changes

--- source/blender/editors/interface/interface_ops.cc
+++ source/blender/editors/interface/interface_ops.cc
@@ -1056,7 +1056,7 @@ static bool tree_interface_item_can_set_prop(const bNodeTreeInterfaceItem &item,
       prop_id, "socket_type", "description", "optional_label", "hide_value", "hide_in_modifier");
 
   switch (eNodeTreeInterfaceItemType(item.item_type)) {
-    case NODE_INTERFACE_SOCKET: {
+    case NodeTreeInterfaceItemType::Socket: {
       const auto &sock = reinterpret_cast<const bNodeTreeInterfaceSocket &>(item);
       if ((sock.flag & NODE_INTERFACE_SOCKET_SELECT) == 0) {
         return false;
@@ -1079,7 +1079,7 @@ static bool tree_interface_item_can_set_prop(const bNodeTreeInterfaceItem &item,
       const auto &active_sock = reinterpret_cast<const bNodeTreeInterfaceSocket &>(active_item);
       return sock.socket_typeinfo()->type == active_sock.socket_typeinfo()->type;
     }
-    case NODE_INTERFACE_PANEL: {
+    case NodeTreeInterfaceItemType::Panel: {
       const auto &panel = reinterpret_cast<const bNodeTreeInterfacePanel &>(item);
       return panel.flag & NODE_INTERFACE_PANEL_SELECT;
     }
