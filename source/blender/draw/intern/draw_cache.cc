d4cc80c8a23b35e2d32f16145471542ad450bd9e
Miguel Pozo <pragma37@gmail.com>
2026-05-22 17:37:35 +0200

    Ensure the surface VBOs/IBOs are requested, since BLAS relies on them

--- source/blender/draw/intern/draw_cache.cc
+++ source/blender/draw/intern/draw_cache.cc
@@ -213,6 +213,8 @@ gpu::Batch *DRW_cache_mesh_surface_get(Object *ob)
 gpu::BottomLevelAS *DRW_cache_mesh_surface_blas_get(Object *ob)
 {
   BLI_assert(ob->type == OB_MESH);
+  /* Ensure the surface VBOs/IBOs are requested, since BLAS relies on them. */
+  DRW_cache_mesh_surface_get(ob);
   return DRW_mesh_batch_cache_get_surface_blas(DRW_object_get_data_for_drawing<Mesh>(*ob));
 }
 
