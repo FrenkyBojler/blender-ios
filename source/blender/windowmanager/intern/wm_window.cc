e489666709e7018087d2e2c736762265e8621551
Campbell Barton <campbell@blender.org>
2026-05-22 19:41:21 +1000

    Correct the defatul

--- source/blender/windowmanager/intern/wm_window.cc
+++ source/blender/windowmanager/intern/wm_window.cc
@@ -3077,9 +3077,9 @@ uint WM_cursor_preferred_logical_size(const bool hardware_cursor)
     if (hardware_cursor) {
       /* On macOS 21 logical pixels is the expected "default", so follow this here.
        *
-       * NOTE(@ideasman42): visually Blender's cursors do look bigger then the the systems
+       * NOTE(@ideasman42): visually Blender's cursors do look bigger then the systems
        * when set to #WM_CURSOR_DEFAULT_LOGICAL_SIZE, so use macOS's default size. */
-      return WM_CURSOR_DEFAULT_LOGICAL_SIZE;
+      return 21;
     }
   }
 
