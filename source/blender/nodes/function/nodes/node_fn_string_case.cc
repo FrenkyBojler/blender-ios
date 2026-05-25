8a158104cf6c44f5db53860aecd3e8564303ce95
Brady Johnston <brady.johnston@me.com>
2026-05-25 18:41:01 +0100

    address feedback and fix

--- source/blender/nodes/function/nodes/node_fn_string_case.cc
+++ source/blender/nodes/function/nodes/node_fn_string_case.cc
@@ -4,6 +4,7 @@
 
 #include "BLI_array.hh"
 #include "BLI_string_utf8.h"
+#include "BLI_vector.hh"
 
 #include "node_function_util.hh"
 
@@ -39,10 +40,8 @@ static std::string apply_string_case(const std::string &s, const Case mode)
   size_t len_bytes;
   const size_t len_chars = BLI_strlen_utf8_ex(s.c_str(), &len_bytes);
 
-  Array<char32_t> utf32(len_chars + 1);
+  Array<char32_t, 64> utf32(len_chars + 1);
   BLI_str_utf8_as_utf32(utf32.data(), s.c_str(), utf32.size());
-
-  bool word_start = true;
 
   for (size_t i = 0; i < len_chars; i++) {
     const char32_t c = utf32[i];
@@ -54,33 +53,34 @@ static std::string apply_string_case(const std::string &s, const Case mode)
         utf32[i] = BLI_str_utf32_char_to_lower(c);
         break;
     }
-
-    std::vector<char> out(len_chars * 4 + 1);
-    BLI_str_utf32_as_utf8(out.data(), utf32.data(), out.size());
-    return std::string(out.data());
   }
 
-  static void node_build_multi_function(NodeMultiFunctionBuilder & builder)
-  {
-    static auto fn = mf::build::SI2_SO<std::string, MenuValue, std::string>(
-        "String Case", [](const std::string &s, MenuValue mode) -> std::string {
-          return apply_string_case(s, Case(mode.value));
-        });
-    builder.set_matching_fn(&fn);
-  }
+  Vector<char, 64> out(len_chars * 4 + 1);
+  BLI_str_utf32_as_utf8(out.data(), utf32.data(), out.size());
+  return std::string(out.data());
+}
 
-  static void node_register()
-  {
-    static bke::bNodeType ntype;
+static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
+{
+  static auto fn = mf::build::SI2_SO<std::string, MenuValue, std::string>(
+      "String Case", [](const std::string &s, MenuValue mode) -> std::string {
+        return apply_string_case(s, Case(mode.value));
+      });
+  builder.set_matching_fn(&fn);
+}
 
-    fn_cmp_node_type_base(&ntype, "FunctionNodeStringCase"_ustr);
-    ntype.ui_name = "String Case";
-    ntype.ui_description = "Convert the case of a string";
-    ntype.nclass = NODE_CLASS_CONVERTER;
-    ntype.declare = node_declare;
-    ntype.build_multi_function = node_build_multi_function;
-    bke::node_register_type(ntype);
-  }
-  NOD_REGISTER_NODE(node_register)
+static void node_register()
+{
+  static bke::bNodeType ntype;
+
+  fn_cmp_node_type_base(&ntype, "FunctionNodeStringCase"_ustr);
+  ntype.ui_name = "String Case";
+  ntype.ui_description = "Convert the case of a string";
+  ntype.nclass = NODE_CLASS_CONVERTER;
+  ntype.declare = node_declare;
+  ntype.build_multi_function = node_build_multi_function;
+  bke::node_register_type(ntype);
+}
+NOD_REGISTER_NODE(node_register)
 
 }  // namespace blender::nodes::node_fn_string_case_cc
