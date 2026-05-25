blender/blender

commit 961cd55f7143ff1f5fb8ecc12b16b213639d333e

Fix: Update doc string for `render_stats` call back
This is a continuation of !158365 to futher clarify the API usage.

Pull Request: https://projects.blender.org/blender/blender/pulls/158845

source/blender/python/intern/bpy_app_handlers.cc
@@ -45,7 +45,8 @@ static PyTypeObject BlenderAppCbType;
 
 #define RENDER_STATS_ARG \
   "Accepts one argument: " \
-  "the render stats (render/saving time plus in background mode frame/used [peak] memory)."
+  "the render progress as a string contianing current frame, current sample, render time and " \
+  "saving time."
 #define PYDOC_RENDER_STATS_TYPE "\n\n:type: list[Callable[[str], None]]"
 
 #define DEPSGRAPH_UPDATE_ARG \

FrenkyBojler/blender-ios

commit c9d1087594118746e5df4ffbc42719b2cc04ae2a

Fix: Typo in render_stats app handler docstring
Corrects 'contianing' to 'containing'.

This issue was introduced in 961cd55f714.

source/blender/python/intern/bpy_app_handlers.cc
@@ -45,7 +45,7 @@ static PyTypeObject BlenderAppCbType;
 
 #define RENDER_STATS_ARG \
   "Accepts one argument: " \
-  "the render progress as a string contianing current frame, current sample, render time and " \
+  "the render progress as a string containing current frame, current sample, render time and " \
   "saving time."
 #define PYDOC_RENDER_STATS_TYPE "\n\n:type: list[Callable[[str], None]]"
 