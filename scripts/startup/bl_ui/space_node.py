55e297e30128b35e1d3ab547ac8e500b21d18d21
Habib Gahbiche <habib@blender.org>
2026-05-25 17:51:19 +0200

    Remove utility nodes from asset shelf

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -1203,7 +1203,9 @@ class NODE_AST_compositor(bpy.types.AssetShelf):
             "Separate Cylindrical",
             "Separate Spherical",
             "3D to Screen Space",
-            "Screen to 3D Space"
+            "Screen to 3D Space",
+            "Project with Depth",
+            "Transform and Project",
         }
 
         compositor_essentials_path = Path(os.path.join(
