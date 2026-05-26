blender/blender

commit 409012956ffb9cfa8357bc2e79088a94c2e82fb1

Overlay-Next: Enable by default and add option to enable legacy code
The legacy option is turned off by default and will be removed
in the following weeks. It is only there to check for
regression for a short period of time. Afterward checking
for regression will require to use 4.3 as a reference.

Rel #102179

Pull Request: https://projects.blender.org/blender/blender/pulls/130683

source/blender/gpu/intern/gpu_select.cc
@@ -136,7 +136,7 @@ void GPU_select_begin_next(GPUSelectBuffer *buffer,
                            eGPUSelectMode mode,
                            int oldhits)
 {
-  gpu_select_begin_ex(buffer, input, mode, oldhits, U.experimental.enable_overlay_next);
+  gpu_select_begin_ex(buffer, input, mode, oldhits, !U.experimental.enable_overlay_legacy);
 }
 
 void GPU_select_begin(GPUSelectBuffer *buffer, const rcti *input, eGPUSelectMode mode, int oldhits)

scripts/startup/bl_ui/space_userpref.py
@@ -2898,7 +2898,7 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
                 ({"property": "use_new_curves_tools"}, ("blender/blender/issues/68981", "#68981")),
                 ({"property": "use_new_point_cloud_type"}, ("blender/blender/issues/75717", "#75717")),
                 ({"property": "use_sculpt_texture_paint"}, ("blender/blender/issues/96225", "#96225")),
-                ({"property": "enable_overlay_next"}, ("blender/blender/issues/102179", "#102179")),
+                ({"property": "enable_overlay_legacy"}, ("blender/blender/issues/102179", "#102179")),
                 ({"property": "enable_new_cpu_compositor"}, ("/blender/blender/issues/125968", "#125968")),
             ),
         )

source/blender/draw/intern/draw_manager_c.cc
@@ -1195,8 +1195,8 @@ static void drw_engines_enable_from_engine(const RenderEngineType *engine_type, 
 
 static void drw_engines_enable_overlays()
 {
-  use_drw_engine((U.experimental.enable_overlay_next) ? &draw_engine_overlay_next_type :
-                                                        &draw_engine_overlay_type);
+  use_drw_engine((U.experimental.enable_overlay_legacy) ? &draw_engine_overlay_type :
+                                                          &draw_engine_overlay_next_type);
 }
 /**
  * Use for select and depth-drawing.
@@ -1215,8 +1215,8 @@ static void drw_engine_enable_image_editor()
     use_drw_engine(&draw_engine_image_type);
   }
 
-  use_drw_engine((U.experimental.enable_overlay_next) ? &draw_engine_overlay_next_type :
-                                                        &draw_engine_overlay_type);
+  use_drw_engine((U.experimental.enable_overlay_legacy) ? &draw_engine_overlay_type :
+                                                          &draw_engine_overlay_next_type);
 }
 
 static void drw_engines_enable_editors()
@@ -1234,8 +1234,8 @@ static void drw_engines_enable_editors()
     SpaceNode *snode = (SpaceNode *)space_data;
     if ((snode->flag & SNODE_BACKDRAW) != 0) {
       use_drw_engine(&draw_engine_image_type);
-      use_drw_engine((U.experimental.enable_overlay_next) ? &draw_engine_overlay_next_type :
-                                                            &draw_engine_overlay_type);
+      use_drw_engine((U.experimental.enable_overlay_legacy) ? &draw_engine_overlay_type :
+                                                              &draw_engine_overlay_next_type);
     }
   }
 }
@@ -2489,7 +2489,7 @@ void DRW_draw_select_loop(Depsgraph *depsgraph,
   DST.options.is_material_select = do_material_sub_selection;
   drw_task_graph_init();
   /* Get list of enabled engines */
-  if (U.experimental.enable_overlay_next) {
+  if (!U.experimental.enable_overlay_legacy) {
     use_drw_engine(&draw_engine_select_next_type);
   }
   else if (use_obedit) {
@@ -2616,13 +2616,13 @@ void DRW_draw_select_loop(Depsgraph *depsgraph,
     if (!select_pass_fn(DRW_SELECT_PASS_PRE, select_pass_user_data)) {
       break;
     }
-    if (!U.experimental.enable_overlay_next) {
+    if (U.experimental.enable_overlay_legacy) {
       DRW_state_lock(DRW_STATE_WRITE_DEPTH | DRW_STATE_DEPTH_TEST_ENABLED);
     }
 
     drw_engines_draw_scene();
 
-    if (!U.experimental.enable_overlay_next) {
+    if (U.experimental.enable_overlay_legacy) {
       DRW_state_lock(DRWState(0));
     }
 

source/blender/makesdna/DNA_userdef_types.h
@@ -755,7 +755,7 @@ typedef struct UserDef_Experimental {
   char use_sculpt_tools_tilt;
   char use_extended_asset_browser;
   char use_sculpt_texture_paint;
-  char enable_overlay_next;
+  char enable_overlay_legacy;
   char use_new_volume_nodes;
   char use_new_file_import_nodes;
   char use_shader_node_previews;

source/blender/makesrna/intern/rna_userdef.cc
@@ -7562,10 +7562,10 @@ static void rna_def_userdef_experimental(BlenderRNA *brna)
                            "pop-over");
   RNA_def_property_update(prop, 0, "rna_userdef_ui_update");
 
-  prop = RNA_def_property(srna, "enable_overlay_next", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, nullptr, "enable_overlay_next", 1);
+  prop = RNA_def_property(srna, "enable_overlay_legacy", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_property_boolean_sdna(prop, nullptr, "enable_overlay_legacy", 1);
   RNA_def_property_ui_text(
-      prop, "Overlay Next", "Enable the new Overlay codebase, requires restart");
+      prop, "Overlay Legacy", "Enable the legacy Overlay codebase, requires restart");
 
   prop = RNA_def_property(srna, "enable_new_cpu_compositor", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "enable_new_cpu_compositor", 1);

FrenkyBojler/blender-ios

commit 6129dc2ec8a04ad9e30e137a20a104bfc2a1f32f

Fix #159102: Pose bone selection cycling order wrong
It is now dictated by creation order instead of depth.

This was working prior to the switch to Overlay Next (409012956f).
409012956f broke it completely (no cycling whatsoever), then
04a57dd937e8 brought cycling back (but not based on view depth, but on
creation order).

Looking at `GPU_select_end`, we can go two codepaths:
- `gpu_select_pick_end` (legacy) -- this used to sort the hits by view
depth already
- `gpu_select_next_end` (Overlay Next) -- this does not sort the hits
anymore

In a comment in #140614 the preferred way to do this now is let the
"consumer" do the sorting (instead of doing it very low level in
`GPU_select_end` already). And we can see that this is indeed happening
in places (e.g. `mouse_select_eval_buffer` does it --but only for
objectmode).

So this PR adds back depth based sorting for pose bone selection
cycling.

source/blender/editors/armature/armature_select.cc
@@ -166,9 +166,14 @@ static void *ed_armature_pick_bone_from_selectbuffer_impl(const bool is_editmode
   bool takeNext = false;
   int minsel = 0xffffffff, minunsel = 0xffffffff;
 
-  for (const GPUSelectResult &hit_result : hit_results) {
-    uint hit_id = hit_result.id;
+  Vector<GPUSelectResult> hit_results_sorted = hit_results;
+  qsort(hit_results_sorted.data(),
+        hit_results_sorted.size(),
+        sizeof(GPUSelectResult),
+        gpu_select_buffer_depth_id_cmp);
 
+  for (const GPUSelectResult &hit_result : hit_results_sorted) {
+    uint hit_id = hit_result.id;
     if (hit_id & BONESEL_ANY) { /* to avoid including objects in selection */
       Base *base = nullptr;
       bool sel;

source/blender/editors/space_view3d/view3d_select.cc
@@ -2270,37 +2270,6 @@ static int mixed_bones_object_selectbuffer_extended(const ViewContext *vc,
 }
 
 /**
- * Compare result of `GPU_select`: #GPUSelectResult,
- * Needed for stable sorting, so cycling through all items near the cursor behaves predictably.
- */
-static int gpu_select_buffer_depth_id_cmp(const void *sel_a_p, const void *sel_b_p)
-{
-  GPUSelectResult *a = static_cast<GPUSelectResult *>(const_cast<void *>(sel_a_p));
-  GPUSelectResult *b = static_cast<GPUSelectResult *>(const_cast<void *>(sel_b_p));
-
-  if (a->depth < b->depth) {
-    return -1;
-  }
-  if (a->depth > b->depth) {
-    return 1;
-  }
-
-  /* Depths match, sort by id. */
-  /* NOTE: this is endianness-sensitive.
-   * GPUSelectResult values are always expected to be little-endian. */
-  uint sel_a = a->id;
-  uint sel_b = b->id;
-
-  if (sel_a < sel_b) {
-    return -1;
-  }
-  if (sel_a > sel_b) {
-    return 1;
-  }
-  return 0;
-}
-
-/**
  * \param has_bones: When true, skip non-bone hits, also allow bases to be used
  * that are visible but not select-able,
  * since you may be in pose mode with an un-selectable object.

source/blender/gpu/GPU_select.hh
@@ -92,6 +92,11 @@ void GPU_select_cache_end();
  * Note that comparing depth as uint is fine.
  */
 const GPUSelectResult *GPU_select_buffer_near(const Span<GPUSelectResult> hit_results);
+/**
+ * Compare result of `GPU_select`: #GPUSelectResult,
+ * Needed for stable sorting, so cycling through all items near the cursor behaves predictably.
+ */
+int gpu_select_buffer_depth_id_cmp(const void *sel_a_p, const void *sel_b_p);
 uint GPU_select_buffer_remove_by_id(MutableSpan<GPUSelectResult> hit_results, uint select_id);
 /**
  * Part of the solution copied from `rect_subregion_stride_calc`.

source/blender/gpu/intern/gpu_select.cc
@@ -240,6 +240,32 @@ bool GPU_select_is_cached()
 /* -------------------------------------------------------------------- */
 /** \name Utilities
  * \{ */
+int gpu_select_buffer_depth_id_cmp(const void *sel_a_p, const void *sel_b_p)
+{
+  GPUSelectResult *a = static_cast<GPUSelectResult *>(const_cast<void *>(sel_a_p));
+  GPUSelectResult *b = static_cast<GPUSelectResult *>(const_cast<void *>(sel_b_p));
+
+  if (a->depth < b->depth) {
+    return -1;
+  }
+  if (a->depth > b->depth) {
+    return 1;
+  }
+
+  /* Depths match, sort by id. */
+  /* NOTE: this is endianness-sensitive.
+   * GPUSelectResult values are always expected to be little-endian. */
+  uint sel_a = a->id;
+  uint sel_b = b->id;
+
+  if (sel_a < sel_b) {
+    return -1;
+  }
+  if (sel_a > sel_b) {
+    return 1;
+  }
+  return 0;
+}
 
 const GPUSelectResult *GPU_select_buffer_near(const Span<GPUSelectResult> hit_results)
 {