448793bdc2b8f8db925e30c63ce2f67a4907e478
Tariq-Sulley <tariqsulley3c@gmail.com>
2026-05-26 01:50:38 +0000

    updates comment

--- source/blender/bmesh/intern/bmesh_opdefines.cc
+++ source/blender/bmesh/intern/bmesh_opdefines.cc
@@ -706,7 +706,7 @@ static BMOpDefine bmo_weld_verts_def = {
         /* Merge vertices to their centroid position,
          * otherwise use the position of the target vertex. */
         {"use_centroid", BMO_OP_SLOT_BOOL},
-        /* Averages custom data of merged vertices. */
+        /* Whether to average custom data of merged vertices. */
         {"average_vdata", BMO_OP_SLOT_BOOL},
         {{'\0'}},
     },
