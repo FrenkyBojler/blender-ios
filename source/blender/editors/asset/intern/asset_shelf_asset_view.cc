e50b0d5fbfe31800584569030854db0078a742da
Sybren A. Stüvel <sybren@blender.org>
2026-05-26 14:46:37 +0200

    Update asset shelf for the new icon & the ability to re-download

--- source/blender/editors/asset/intern/asset_shelf_asset_view.cc
+++ source/blender/editors/asset/intern/asset_shelf_asset_view.cc
@@ -288,6 +288,12 @@ void AssetViewItem::build_grid_tile(const bContext &C, ui::Layout &layout) const
     button_label_alpha_factor_set(online_icon, 0.6f);
     button_label_draw_icon_border_set(online_icon, true);
   }
+  else if (asset_.needs_download()) {
+    ui::Button *needs_download_icon = uiItemL_ex(
+        &overlay_row, "", ICON_WARNING_LARGE, false, false);
+    button_label_alpha_factor_set(needs_download_icon, 0.6f);
+    button_label_draw_icon_border_set(needs_download_icon, true);
+  }
 }
 
 void AssetViewItem::build_context_menu(bContext &C, ui::Layout &column) const
@@ -297,7 +303,7 @@ void AssetViewItem::build_context_menu(bContext &C, ui::Layout &column) const
 
   bool has_items = false;
 
-  if (asset_.is_online_only()) {
+  if (asset_.needs_download()) {
     column.op("asset.assets_download", IFACE_("Download Asset"), ICON_NONE);
     has_items = true;
   }
