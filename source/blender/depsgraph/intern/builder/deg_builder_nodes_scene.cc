blender/blender

commit 0d77f1e6aa7e6d8717db59eaab07c0c0ce0a98d2

Fix: Missing compositor relations in pipeline depsgraph
Commit b0278b24868 introduced code which was building compositor nodes
for all pipeline depsgraphs (regardless of the R_DOCOMP flag), but it
did not do it for the relations builder.

While it works in simple cases, it could easily lead to issues when
some non-trivial drivers depending on objects exist. Additionally, in
the !152082 we are trying to add sanitization for the depsgraph state,
and such nodes/relations discrepancy does not pass checks. Happens
with the camera_stereo_panoramic.blend render test (open the file, and
press F12 to render).

Pull Request: https://projects.blender.org/blender/blender/pulls/152086

source/blender/depsgraph/intern/builder/deg_builder_relations_scene.cc
@@ -63,6 +63,9 @@ void DepsgraphRelationBuilder::build_scene_parameters(Scene *scene)
   LISTBASE_FOREACH (TimeMarker *, marker, &scene->markers) {
     build_idproperties(marker->prop);
   }
+
+  /* See the comment in the DepsgraphNodeBuilder::build_scene_parameters(). */
+  build_scene_compositor(scene);
 }
 
 void DepsgraphRelationBuilder::build_scene_compositor(Scene *scene)

FrenkyBojler/blender-ios

commit 42e8859a4fa8afe9cdcfa4067c9a6b8458509466

Fix #156105: Keyframe values don't update in the property
The issue comes from the fact that the same data-block is pulled into
multiple dependency graphs that are both active and evaluated (hence,
they both "flush" evaluated values back to the original IDs).

Apparently, this is a regression since 0d77f1e6aa which introduced
more relations, and caused more evaluation to happen.

It is not really clear what the proper solution would be, but it seems
to be possible to avoid regression caused by the aforementioned commit
by simply not forcing compositor build. The original reason why the
compositor node needed to be build is no longer relevant. Since then
the scene compositor is no longer an embedded data-block, so there is
no risk on remapping anything in the original bmain since the remapping
code no longer "goes inside" of the scene's compositor.

source/blender/depsgraph/intern/builder/deg_builder_nodes_scene.cc
@@ -59,18 +59,6 @@ void DepsgraphNodeBuilder::build_scene_parameters(Scene *scene)
 
   add_operation_node(&scene->id, NodeType::SCENE, OperationCode::SCENE_EVAL);
 
-  /* NOTE: This is a bit overkill and can potentially pull a bit too much into the graph, but:
-   *
-   * - We definitely need an ID node for the scene's compositor, otherwise re-mapping will no
-   *   happen correct and we will risk remapping pointers in the main database.
-   * - Alternatively, we should discard compositor tree, but this might cause other headache like
-   *   drivers which are coming from the tree.
-   *
-   * Would be nice to find some reliable way of ignoring compositor here, but it's already pulled
-   * in when building scene from view layer, so this particular case does not make things
-   * marginally worse. */
-  build_scene_compositor(scene);
-
   for (TimeMarker &marker : scene->markers) {
     build_idproperties(marker.prop);
   }

source/blender/depsgraph/intern/builder/deg_builder_relations_scene.cc
@@ -66,9 +66,6 @@ void DepsgraphRelationBuilder::build_scene_parameters(Scene *scene)
   for (TimeMarker &marker : scene->markers) {
     build_idproperties(marker.prop);
   }
-
-  /* See the comment in the DepsgraphNodeBuilder::build_scene_parameters(). */
-  build_scene_compositor(scene);
 }
 
 void DepsgraphRelationBuilder::build_scene_compositor(Scene *scene)

source/blender/depsgraph/intern/eval/deg_eval_copy_on_write.cc
@@ -166,7 +166,6 @@ const ID *nested_id_hack_get_discarded_pointers(void *storage, const ID *id)
       Scene *scene = static_cast<Scene *>(storage);
       *scene = dna::shallow_copy(*id_cast<Scene *>(const_cast<ID *>(id)));
       scene->toolsettings = nullptr;
-      scene->nodetree = nullptr;
       return &scene->id;
     }
 
@@ -483,6 +482,9 @@ int foreach_libblock_remap_callback(LibraryIDLinkCallbackData *cb_data)
   if (*id_p == nullptr) {
     return IDWALK_RET_NOP;
   }
+
+  BLI_assert(cb_data->owner_id);
+  BLI_assert(cb_data->owner_id->tag & ID_TAG_COPIED_ON_EVAL);
 
   RemapCallbackUserData *user_data = static_cast<RemapCallbackUserData *>(cb_data->user_data);
   const Depsgraph *depsgraph = user_data->depsgraph;