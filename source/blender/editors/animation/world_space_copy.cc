2b6ac2f77d55075d1ff7e704fba864136252d6f2
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-05-28 11:18:26 +0200

    create file

---
+++ source/blender/editors/animation/world_space_copy.cc
@@ -0,0 +1,14 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BLI_bounds.hh"
+
+#include "ED_anim_transformable.hh"
+
+namespace blender::ed {
+static void copy_world_space(Span<AnimTransformable> transformables, Bounds<int> range) {
+    
+}
+
+}  // namespace blender::ed
