78cd3f1061355a644ae0b5a838f92d0a572ec0c2
Philipp Oeser <philipp@blender.org>
2026-05-29 11:23:11 +0200

    Fix #159248: Setting Grease Pencil Stroke Type wrongly removes "fill_id"
    The condition to remove the whole `fill_id` attribute was wrong.
    The intention was to check if all strokes have their `fill_id` set to
    zero, but code was actually taking the already available (by
    `std::all_of` going over the `fill_ids.span`) `fill_id` as an index to
    look up in the `fill_ids.span`...
    This could easily lead to code thinking all are zeros.
    
    To resolve, use the value from `std::all_of` as `fill_id` directly.

--- source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -5104,7 +5104,7 @@ static wmOperatorStatus grease_pencil_set_stroke_type_exec(bContext *C, wmOperat
     switch (type) {
       case StrokeType::Stroke: {
         index_mask::masked_fill(hide_stroke.span, false, strokes);
-        index_mask::masked_fill(fill_ids.span, 0, strokes);
+        index_mask::masked_fill(fill_ids.span, 0, strokes);  // other number?
         break;
       }
       case StrokeType::Fill: {
@@ -5128,8 +5128,8 @@ static wmOperatorStatus grease_pencil_set_stroke_type_exec(bContext *C, wmOperat
     fill_ids.finish();
 
     if (type == StrokeType::Stroke) {
-      if (std::all_of(fill_ids.span.begin(), fill_ids.span.end(), [&](const int64_t i) {
-            return fill_ids.span[i] == 0;
+      if (std::all_of(fill_ids.span.begin(), fill_ids.span.end(), [&](const int64_t fill_id) {
+            return fill_id == 0;
           }))
       {
         /* Remove #fill_id attribute if there are no fills left. */
