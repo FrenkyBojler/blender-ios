blender/blender

commit 8cb6fc3034d65af35fde67d53addbca96e9f2bc2

UI: Add unit hint when text editing number values
When text editing a number field, instead of including the unit as part
of the edit string this PR adds a useful unit hint that is not part of
the edit text.

This works for different scene unit defaults, and also deals with pixel
values and percentages separately (as they are not "units" in Blender).

This is especially useful when the unit is set to "Adaptive". When
typing a number, it was not clear what the unit of that number would
become as there was no indication for it.

Pull Request: https://projects.blender.org/blender/blender/pulls/116103

source/blender/python/intern/bpy_interface_run.cc
@@ -250,6 +250,23 @@ bool BPY_run_text(bContext *C, Text *text, ReportList *reports, const bool do_ju
   return python_script_exec(C, nullptr, text, reports, do_jump);
 }
 
+bool BPY_string_compile_check(const char *expr)
+{
+  if (!expr || expr[0] == '\0') {
+    return true;
+  }
+  PyGILState_STATE gilstate = PyGILState_Ensure();
+  if (PyObject *retval = Py_CompileString(expr, "<expression>", Py_eval_input)) {
+    Py_DECREF(retval);
+    return true;
+  }
+  else {
+    PyErr_Clear();
+  }
+  PyGILState_Release(gilstate);
+  return false;
+}
+
 /**
  * \param mode: Passed to #PyRun_String, matches Python's `compile` functions mode argument.
  * #Py_eval_input for `eval`, #Py_file_input for `exec`.

source/blender/blenkernel/BKE_unit.hh
@@ -35,7 +35,8 @@ size_t BKE_unit_value_as_string(char *str,
                                 int prec,
                                 int type,
                                 const UnitSettings &settings,
-                                bool pad);
+                                bool pad,
+                                bool do_unit_suffix);
 
 /**
  * Humanly readable representation of a value in units (used for button drawing).
@@ -49,7 +50,8 @@ size_t BKE_unit_value_as_string_adaptive(char *str,
                                          int system,
                                          int type,
                                          bool split,
-                                         bool pad);
+                                         bool pad,
+                                         bool do_unit_suffix);
 
 /**
  * A version of #BKE_unit_value_as_string with the `value` scaled by #BKE_unit_value_scale.
@@ -62,7 +64,8 @@ size_t BKE_unit_value_as_string_scaled(char *str,
                                        int prec,
                                        int type,
                                        const UnitSettings &settings,
-                                       bool pad);
+                                       bool pad,
+                                       bool do_unit_suffix);
 
 /**
  * Replace units with values, used before python button evaluation.
@@ -128,11 +131,18 @@ double BKE_unit_value_scale(const UnitSettings &settings, int unit_type, double 
 void BKE_unit_system_get(int system, int type, const void **r_usys_pt, int *r_len);
 int BKE_unit_base_get(const void *usys_pt);
 int BKE_unit_base_of_type_get(int system, int type);
+/**
+ * Gets the preffered units of this type if available otherwise returns the
+ * base unit using `BKE_unit_base_of_type_get`.
+ */
+int BKE_preffered_unit_of_type_or_base_get(const UnitSettings &settings, int type);
 const char *BKE_unit_name_get(const void *usys_pt, int index);
 const char *BKE_unit_display_name_get(const void *usys_pt, int index);
+const char *BKE_unit_display_name_short_get(const void *usys_pt, int index);
 const char *BKE_unit_identifier_get(const void *usys_pt, int index);
 double BKE_unit_scalar_get(const void *usys_pt, int index);
 bool BKE_unit_is_suppressed(const void *usys_pt, int index);
+bool BKE_unit_is_adaptive(const UnitSettings &settings, int type);
 
 /** Aligned with #PropertyUnit and `bpyunits_ucategories_items` in `bpy_utils_units.cc`. */
 enum {

source/blender/blenkernel/intern/unit.cc
@@ -1648,7 +1648,8 @@ static size_t unit_as_string(char *str,
                              const bUnitCollection *usys,
                              /* Non exposed options. */
                              const bUnitDef *unit,
-                             char pad)
+                             char pad,
+                             bool do_unit_suffix)
 {
   BLI_assert(prec >= 0);
   if (unit == nullptr) {
@@ -1696,16 +1697,18 @@ static size_t unit_as_string(char *str,
   }
 
   /* Now add a space for all units except foot, inch, degree, arcminute, arcsecond. */
-  if (!(unit->flag & B_UNIT_DEF_NO_SPACE)) {
+  if (!(unit->flag & B_UNIT_DEF_NO_SPACE) && do_unit_suffix) {
     str[++i] = ' ';
   }
 
-  /* Now add the suffix. */
   if (i < str_maxncpy) {
-    int j = 0;
     i++;
-    while (unit->name_short[j] && (i < str_maxncpy)) {
-      str[i++] = unit->name_short[j++];
+    if (do_unit_suffix) {
+      /* Now add the suffix. */
+      int j = 0;
+      while (unit->name_short[j] && (i < str_maxncpy)) {
+        str[i++] = unit->name_short[j++];
+      }
     }
   }
 
@@ -1761,7 +1764,7 @@ static size_t unit_as_string_split_pair(char *str,
   /* Check the 2 is a smaller unit. */
   if (unit_b > unit_a) {
     /* Always strip zeros for the larger unit, since it is truncated and won't ever "jitter". */
-    size_t i = unit_as_string(str, str_maxncpy, value_a, prec, true, usys, unit_a, '\0');
+    size_t i = unit_as_string(str, str_maxncpy, value_a, prec, true, usys, unit_a, '\0', true);
 
     /* Fixed width mode skips this to preserve the exact decimal place count. */
     if (variable_width) {
@@ -1776,7 +1779,7 @@ static size_t unit_as_string_split_pair(char *str,
 
       /* Use low precision since this is a smaller unit. */
       i += unit_as_string(
-          str + i, str_maxncpy - i, value_b, prec, variable_width, usys, unit_b, '\0');
+          str + i, str_maxncpy - i, value_b, prec, variable_width, usys, unit_b, '\0', true);
     }
     return i;
   }
@@ -1789,11 +1792,33 @@ static bool is_valid_unit_collection(const bUnitCollection *usys)
   return usys != nullptr && usys->units[0].name != nullptr;
 }
 
-static const bUnitDef *get_preferred_display_unit_if_used(int type, const PreferredUnits &units)
+static bool is_unit_adaptive(const int type, const PreferredUnits &units)
+{
+  switch (type) {
+    case B_UNIT_LENGTH:
+    case B_UNIT_AREA:
+    case B_UNIT_VOLUME:
+      return units.length == USER_UNIT_ADAPTIVE;
+    case B_UNIT_MASS:
+      return units.mass == USER_UNIT_ADAPTIVE;
+    case B_UNIT_TIME:
+      return units.time == USER_UNIT_ADAPTIVE;
+    case B_UNIT_TEMPERATURE:
+      return units.temperature == USER_UNIT_ADAPTIVE;
+    default:
+      break;
+  }
+  return false;
+}
+
+static int get_preferred_display_unit_index_if_used(int type, const PreferredUnits &units)
 {
   const bUnitCollection *usys = unit_get_system(units.system, type);
   if (!is_valid_unit_collection(usys)) {
-    return nullptr;
+    return -1;
+  }
+  if (is_unit_adaptive(type, units)) {
+    return -1;
   }
 
   int max_offset = usys->length - 1;
@@ -1802,37 +1827,35 @@ static const bUnitDef *get_preferred_display_unit_if_used(int type, const Prefer
     case B_UNIT_LENGTH:
     case B_UNIT_AREA:
     case B_UNIT_VOLUME:
-      if (units.length == USER_UNIT_ADAPTIVE) {
-        return nullptr;
-      }
-      return usys->units + std::min(units.length, max_offset);
+      return std::min(units.length, max_offset);
     case B_UNIT_MASS:
-      if (units.mass == USER_UNIT_ADAPTIVE) {
-        return nullptr;
-      }
-      return usys->units + std::min(units.mass, max_offset);
+      return std::min(units.mass, max_offset);
     case B_UNIT_TIME:
-      if (units.time == USER_UNIT_ADAPTIVE) {
-        return nullptr;
-      }
-      return usys->units + std::min(units.time, max_offset);
+      return std::min(units.time, max_offset);
     case B_UNIT_ROTATION:
       if (units.rotation == 0) {
-        return usys->units + 0;
+        return 0;
       }
       else if (units.rotation == USER_UNIT_ROT_RADIANS) {
-        return usys->units + 3;
+        return 3;
       }
       break;
     case B_UNIT_TEMPERATURE:
-      if (units.temperature == USER_UNIT_ADAPTIVE) {
-        return nullptr;
-      }
-      return usys->units + std::min(units.temperature, max_offset);
+      return std::min(units.temperature, max_offset);
     default:
       break;
   }
-  return nullptr;
+  return -1;
+}
+
+static const bUnitDef *get_preferred_display_unit_if_used(int type, const PreferredUnits &units)
+{
+  const bUnitCollection *usys = unit_get_system(units.system, type);
+  const int index = get_preferred_display_unit_index_if_used(type, units);
+  if (index == -1) {
+    return nullptr;
+  }
+  return &usys->units[index];
 }
 
 /* Return the length of the generated string. */
@@ -1843,6 +1866,7 @@ static size_t unit_as_string_main(char *str,
                                   int type,
                                   bool split,
                                   bool pad,
+                                  bool do_unit_suffix,
                                   const PreferredUnits &units)
 {
   const bUnitCollection *usys = unit_get_system(units.system, type);
@@ -1870,12 +1894,26 @@ static size_t unit_as_string_main(char *str,
     }
   }
 
-  return unit_as_string(
-      str, str_maxncpy, value, prec, variable_width, usys, main_unit, pad ? ' ' : '\0');
+  return unit_as_string(str,
+                        str_maxncpy,
+                        value,
+                        prec,
+                        variable_width,
+                        usys,
+                        main_unit,
+                        pad ? ' ' : '\0',
+                        do_unit_suffix);
 }
 
-size_t BKE_unit_value_as_string_adaptive(
-    char *str, int str_maxncpy, double value, int prec, int system, int type, bool split, bool pad)
+size_t BKE_unit_value_as_string_adaptive(char *str,
+                                         int str_maxncpy,
+                                         double value,
+                                         int prec,
+                                         int system,
+                                         int type,
+                                         bool split,
+                                         bool pad,
+                                         bool do_unit_suffix)
 {
   PreferredUnits units;
   units.system = system;
@@ -1884,7 +1922,8 @@ size_t BKE_unit_value_as_string_adaptive(
   units.mass = USER_UNIT_ADAPTIVE;
   units.time = USER_UNIT_ADAPTIVE;
   units.temperature = USER_UNIT_ADAPTIVE;
-  return unit_as_string_main(str, str_maxncpy, value, prec, type, split, pad, units);
+  return unit_as_string_main(
+      str, str_maxncpy, value, prec, type, split, pad, do_unit_suffix, units);
 }
 
 size_t BKE_unit_value_as_string(char *str,
@@ -1893,11 +1932,13 @@ size_t BKE_unit_value_as_string(char *str,
                                 int prec,
                                 int type,
                                 const UnitSettings &settings,
-                                bool pad)
+                                bool pad,
+                                bool do_unit_suffix)
 {
   bool do_split = (settings.flag & USER_UNIT_OPT_SPLIT) != 0;
   PreferredUnits units = preferred_units_from_UnitSettings(settings);
-  return unit_as_string_main(str, str_maxncpy, value, prec, type, do_split, pad, units);
+  return unit_as_string_main(
+      str, str_maxncpy, value, prec, type, do_split, pad, do_unit_suffix, units);
 }
 
 size_t BKE_unit_value_as_string_scaled(char *str,
@@ -1906,10 +1947,17 @@ size_t BKE_unit_value_as_string_scaled(char *str,
                                        int prec,
                                        int type,
                                        const UnitSettings &settings,
-                                       bool pad)
+                                       bool pad,
+                                       bool do_unit_suffix)
 {
-  return BKE_unit_value_as_string(
-      str, str_maxncpy, BKE_unit_value_scale(settings, type, value), prec, type, settings, pad);
+  return BKE_unit_value_as_string(str,
+                                  str_maxncpy,
+                                  BKE_unit_value_scale(settings, type, value),
+                                  prec,
+                                  type,
+                                  settings,
+                                  pad,
+                                  do_unit_suffix);
 }
 
 double BKE_unit_value_scale(const UnitSettings &settings, const int unit_type, double value)
@@ -2548,6 +2596,16 @@ int BKE_unit_base_of_type_get(int system, int type)
   return unit_get_system(system, type)->base_unit;
 }
 
+int BKE_preffered_unit_of_type_or_base_get(const UnitSettings &settings, int type)
+{
+  PreferredUnits units = preferred_units_from_UnitSettings(settings);
+  const int unit_index = get_preferred_display_unit_index_if_used(type, units);
+  if (unit_index == -1) {
+    return BKE_unit_base_of_type_get(units.system, type);
+  }
+  return unit_index;
+}
+
 const char *BKE_unit_name_get(const void *usys_pt, int index)
 {
   const bUnitCollection *usys = static_cast<const bUnitCollection *>(usys_pt);
@@ -2559,6 +2617,12 @@ const char *BKE_unit_display_name_get(const void *usys_pt, int index)
   const bUnitCollection *usys = static_cast<const bUnitCollection *>(usys_pt);
   BLI_assert(uint(index) < uint(usys->length));
   return usys->units[index].name_display;
+}
+const char *BKE_unit_display_name_short_get(const void *usys_pt, int index)
+{
+  const bUnitCollection *usys = static_cast<const bUnitCollection *>(usys_pt);
+  BLI_assert(uint(index) < uint(usys->length));
+  return usys->units[index].name_short;
 }
 const char *BKE_unit_identifier_get(const void *usys_pt, int index)
 {
@@ -2583,6 +2647,12 @@ bool BKE_unit_is_suppressed(const void *usys_pt, int index)
   const bUnitCollection *usys = static_cast<const bUnitCollection *>(usys_pt);
   BLI_assert(uint(index) < uint(usys->length));
   return (usys->units[index].flag & B_UNIT_DEF_SUPPRESS) != 0;
+}
+
+bool BKE_unit_is_adaptive(const UnitSettings &settings, int type)
+{
+  PreferredUnits units = preferred_units_from_UnitSettings(settings);
+  return is_unit_adaptive(type, units);
 }
 
 }  // namespace blender

source/blender/draw/intern/draw_manager_text.cc
@@ -366,11 +366,16 @@ void DRW_text_edit_mesh_measure_stats(const ARegion *region,
             v2 = ob->object_to_world().view<3, 3>() * v2;
           }
 
-          const size_t numstr_len =
-              unit.system ?
-                  BKE_unit_value_as_string_scaled(
-                      numstr, sizeof(numstr), len_v3v3(v1, v2), 3, B_UNIT_LENGTH, unit, false) :
-                  SNPRINTF_RLEN(numstr, conv_float, len_v3v3(v1, v2));
+          const size_t numstr_len = unit.system ?
+                                        BKE_unit_value_as_string_scaled(numstr,
+                                                                        sizeof(numstr),
+                                                                        len_v3v3(v1, v2),
+                                                                        3,
+                                                                        B_UNIT_LENGTH,
+                                                                        unit,
+                                                                        false,
+                                                                        true) :
+                                        SNPRINTF_RLEN(numstr, conv_float, len_v3v3(v1, v2));
 
           DRW_text_cache_add(dt, co, numstr, numstr_len, 0, edge_tex_sep, txt_flag, col);
         }
@@ -503,7 +508,7 @@ void DRW_text_edit_mesh_measure_stats(const ARegion *region,
 
         const size_t numstr_len =
             unit.system ? BKE_unit_value_as_string_scaled(
-                              numstr, sizeof(numstr), area, 3, B_UNIT_AREA, unit, false) :
+                              numstr, sizeof(numstr), area, 3, B_UNIT_AREA, unit, false, true) :
                           SNPRINTF_RLEN(numstr, conv_float, area);
 
         DRW_text_cache_add(dt, vmid, numstr, numstr_len, 0, 0, txt_flag, col);

source/blender/editors/interface/eyedroppers/eyedropper_depth.cc
@@ -290,7 +290,8 @@ static void depthdropper_depth_sample_pt(bContext *C,
                                    -4,
                                    B_UNIT_LENGTH,
                                    scene->unit,
-                                   false);
+                                   false,
+                                   true);
         }
         else {
           STRNCPY_UTF8(ddr->name, RPT_("Nothing under cursor"));

source/blender/editors/interface/interface.cc
@@ -2991,11 +2991,16 @@ void button_convert_to_unit_alt_name(Button *but, char *str, size_t str_maxncpy)
 /**
  * \param float_precision: Override the button precision.
  */
-static void get_but_string_unit(
-    Button *but, char *str, int str_maxncpy, double value, bool pad, int float_precision)
+static void get_but_string_unit(Button *but,
+                                char *str,
+                                int str_maxncpy,
+                                double value,
+                                bool pad,
+                                int float_precision,
+                                bool do_suffix)
 {
   const UnitSettings *unit = but->block->unit;
-  const int unit_type = button_unit_type_get(but);
+  const int unit_type = RNA_SUBTYPE_UNIT_VALUE(button_unit_type_get(but));
   int precision;
 
   BLI_assert(unit->scale_length > 0.0f);
@@ -3019,9 +3024,10 @@ static void get_but_string_unit(
                            str_maxncpy,
                            get_but_scale_unit(but, value),
                            precision,
-                           RNA_SUBTYPE_UNIT_VALUE(unit_type),
+                           unit_type,
                            *unit,
-                           pad);
+                           pad,
+                           do_suffix);
 }
 
 static float get_but_step_unit(Button *but, float step_default)
@@ -3171,7 +3177,11 @@ void button_string_get_ex(Button *but,
       }
 
       if (button_is_unit(but)) {
-        get_but_string_unit(but, str, str_maxncpy, value, false, prec);
+        /* In case where the unit is adaptive, include the unit in the edit string. Otherwise the
+         * unit is added as an edit hint. */
+        const int unit_type = RNA_SUBTYPE_UNIT_VALUE(button_unit_type_get(but));
+        const bool do_suffix = BKE_unit_is_adaptive(*but->block->unit, unit_type);
+        get_but_string_unit(but, str, str_maxncpy, value, false, prec, do_suffix);
       }
       else if (subtype == PROP_FACTOR) {
         if (U.factor_display_type == USER_FACTOR_AS_FACTOR) {
@@ -4033,7 +4043,7 @@ static void but_build_drawstr_float(Button *but, double value)
   }
   else if (button_is_unit(but)) {
     char new_str[UI_MAX_DRAW_STR];
-    get_but_string_unit(but, new_str, sizeof(new_str), value, true, -1);
+    get_but_string_unit(but, new_str, sizeof(new_str), value, true, -1, true);
     but->drawstr = but->str + new_str;
   }
   else {

source/blender/editors/interface/interface_handlers.cc
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
+#include <optional>
 #include <variant>
 
 #include "MEM_guardedalloc.h"
@@ -56,6 +57,7 @@
 
 #include "IMB_colormanagement.hh"
 
+#include "ED_numinput.hh"
 #include "ED_screen.hh"
 #include "ED_undo.hh"
 
@@ -65,6 +67,8 @@
 #include "UI_string_search.hh"
 
 #include "BLF_api.hh"
+
+#include "BPY_extern_run.hh"
 
 #include "buttons/interface_textbox.hh"
 #include "interface_intern.hh"
@@ -431,6 +435,9 @@ struct HandleButtonData {
   wmTimer *flashtimer = nullptr;
 
   TextEdit text_edit;
+  /** Unit hint drawn faded after the edit string, empty when there is none. */
+  std::string text_edit_unit_hint;
+
   wmTimer *text_select_auto_scroll = nullptr;
 
   double value = 0.0f;
@@ -3660,6 +3667,96 @@ const wmIMEData *button_ime_data_get(Button *but)
 }
 #endif /* WITH_INPUT_IME */
 
+std::optional<StringRef> button_edit_unit_hint_get(const Button &but)
+{
+  const HandleButtonData *data = but.semi_modal_state ? but.semi_modal_state : but.active;
+  if (data == nullptr || data->text_edit_unit_hint.empty()) {
+    return std::nullopt;
+  }
+  return data->text_edit_unit_hint;
+}
+
+/* Currently there are property subtypes that are not considered "units", so handle them
+ * separately here. */
+static std::optional<StringRef> button_edit_unit_hint_get_from_prop_subtype(
+    const PropertySubType subtype)
+{
+  switch (subtype) {
+    case PROP_PIXEL:
+      return "px";
+    case PROP_PERCENTAGE:
+      return "%";
+    default:
+      return {};
+  }
+  return {};
+}
+
+static void button_edit_unit_hint_refresh(bContext *C, Button *but, HandleButtonData *data)
+{
+  /* Unit completion (hint) is only done for buttons with a unit or with a property such as
+   * percentage or pixel for e.g. For everything else, we reset the completion to an empty string.
+   */
+  const PropertySubType subtype = but->rnaprop ? RNA_property_subtype(but->rnaprop) : PROP_NONE;
+  const std::optional<StringRef> subtype_hint = button_edit_unit_hint_get_from_prop_subtype(
+      subtype);
+  if (!button_is_unit(but) && !subtype_hint.has_value()) {
+    data->text_edit_unit_hint.clear();
+    return;
+  }
+
+  /* Set the completion text (hint) to the unit that is used by this value. */
+  std::string name_short;
+  const int unit_type = RNA_SUBTYPE_UNIT_VALUE(button_unit_type_get(but));
+  if (unit_type != PROP_NONE) {
+    /* If the string contains the unit already, don't add it as a hint. */
+    if (BKE_unit_string_contains_unit(data->text_edit.edit_string, unit_type)) {
+      data->text_edit_unit_hint.clear();
+      return;
+    }
+
+    /* If the expression we're entering is not valid, don't show the hint. */
+    if (!BPY_string_compile_check(data->text_edit.edit_string)) {
+      data->text_edit_unit_hint.clear();
+      return;
+    }
+
+    const void *usys;
+    int len;
+    UnitSettings &unit_settings = CTX_data_scene(C)->unit;
+    BKE_unit_system_get(unit_settings.system, unit_type, &usys, &len);
+    const int unit_index = BKE_preffered_unit_of_type_or_base_get(unit_settings, unit_type);
+    name_short = BKE_unit_display_name_short_get(usys, unit_index);
+    BLI_assert(!name_short.empty());
+  }
+  else if (subtype_hint) {
+    /* Special handling for some subtypes. */
+    name_short = *subtype_hint;
+    /* If the string contains the unit already, don't add it as a hint.
+     * Note: This is a simple sub-string check and may fail at times. */
+    const StringRefNull str(data->text_edit.edit_string);
+    BLI_assert(!name_short.empty());
+    if (str.find(name_short) != StringRef::not_found) {
+      data->text_edit_unit_hint.clear();
+      return;
+    }
+
+    /* If the expression we're entering is not valid, don't show the hint. */
+    if (!BPY_string_compile_check(data->text_edit.edit_string)) {
+      data->text_edit_unit_hint.clear();
+      return;
+    }
+  }
+
+  if (name_short.empty()) {
+    data->text_edit_unit_hint.clear();
+    return;
+  }
+
+  /* Add a space before the short unit name. */
+  data->text_edit_unit_hint = " " + name_short;
+}
+
 static void textedit_begin(bContext *C, Button *but, HandleButtonData *data)
 {
   TextEdit &text_edit = data->text_edit;
@@ -3781,6 +3878,9 @@ static void textedit_begin(bContext *C, Button *but, HandleButtonData *data)
   but->flag &= ~BUT_REDALERT;
 
   button_update(but);
+
+  /* Set the edit unit hint if needed. */
+  button_edit_unit_hint_refresh(C, but, data);
 
   /* Make sure the edited button is in view. */
   if (data->searchbox) {
@@ -4411,6 +4511,8 @@ static int do_but_textedit(
     if ((skip_undo_push == false) && (text_edit.undo_stack_text != nullptr)) {
       textedit_undo_push(text_edit.undo_stack_text, text_edit.edit_string, but->pos);
     }
+
+    button_edit_unit_hint_refresh(C, but, data);
 
     /* only do live update when but flag request it (BUT_TEXTEDIT_UPDATE). */
     if (update && data->interactive) {

source/blender/editors/interface/interface_intern.hh
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <functional>
+#include <optional>
 #include <ranges>
 
 #include "BLI_compiler_attrs.h"
@@ -919,6 +920,11 @@ Button *button_drag_multi_edit_get(Button *but);
  * Get the hint that describes the expected value when empty.
  */
 const char *button_placeholder_get(Button *but);
+
+/**
+ * Get the unit hint shown after the text while editing.
+ */
+std::optional<StringRef> button_edit_unit_hint_get(const Button &but);
 
 void def_but_icon(Button *but, int icon, int flag);
 /**

FrenkyBojler/blender-ios

commit d355bc9b44305b34e051d2a328bbdb9d5c812fcc

Fix: Python GIL leak in unit hint expression compilation
The function BPY_string_compile_check acquired the GIL but failed to
release it when Py_CompileString succeeded. This causes Blender to hang
or crash during subsequent operations.

This issue was introduced in 8cb6fc3034d.

source/blender/python/intern/bpy_interface_run.cc
@@ -256,15 +256,16 @@ bool BPY_string_compile_check(const char *expr)
     return true;
   }
   PyGILState_STATE gilstate = PyGILState_Ensure();
+  bool success = false;
   if (PyObject *retval = Py_CompileString(expr, "<expression>", Py_eval_input)) {
     Py_DECREF(retval);
-    return true;
+    success = true;
   }
   else {
     PyErr_Clear();
   }
   PyGILState_Release(gilstate);
-  return false;
+  return success;
 }
 
 /**