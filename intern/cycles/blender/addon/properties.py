bed16eb4e782caa5e1bc479cd66cdde8f5051c57
Nikita Sirgienko <nikita.sirgienko@intel.com>
2026-05-30 14:05:04 +0200

    Fix the missing parenthesis

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -1915,7 +1915,7 @@ class CyclesPreferences(bpy.types.AddonPreferences):
                     driver_version = "XX.X.101.8306"
                     col.label(
                         text=self._format_device_name(
-    rpt_("Requires Intel(R) Arc(TM) GPUs or newer Intel(R) Graphics")),
+                            rpt_("Requires Intel(R) Arc(TM) GPUs or newer Intel(R) Graphics")),
                         icon='BLANK1',
                         translate=False)
                     col.label(text=rpt_("with Windows driver version %s or newer") % driver_version,
@@ -1923,7 +1923,8 @@ class CyclesPreferences(bpy.types.AddonPreferences):
                 elif sys.platform.startswith("linux"):
                     driver_version = "XX.XX.34666.3"
                     col.label(
-                        text=self._format_device_name(rpt_("Requires Intel(R) Arc(TM) GPUs or newer Intel(R) Graphics"),
+                        text=self._format_device_name(
+                            rpt_("Requires Intel(R) Arc(TM) GPUs or newer Intel(R) Graphics")),
                         icon='BLANK1',
                         translate=False)
                     col.label(
