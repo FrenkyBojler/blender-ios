4d3a82989522556083da0b6fd5433b0b64e0e6b2
Cristian Ivana <criss.ivana@gmail.com>
2026-05-30 16:03:18 +0300

    Fix: gpu ops and bool to color conversion

--- source/blender/gpu/shaders/material/gpu_shader_material_boolean_math.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_boolean_math.glsl
@@ -2,55 +2,55 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-[[node]] 
+[[node]]
 void boolean_math_and(float a, float b, out float result)
 {
   result = float(a != 0.0f && b != 0.0f);
 }
 
-[[node]] 
+[[node]]
 void boolean_math_or(float a, float b, out float result)
 {
   result = float(a != 0.0f || b != 0.0f);
 }
 
-[[node]] 
-void boolean_math_not(float a, float b, out float result)
+[[node]]
+void boolean_math_not(float a, out float result)
 {
   result = float(a == 0.0f);
 }
 
-[[node]] 
+[[node]]
 void boolean_math_nand(float a, float b, out float result)
 {
   result = float(!(a != 0.0f && b != 0.0f));
 }
 
-[[node]] 
+[[node]]
 void boolean_math_nor(float a, float b, out float result)
 {
-  result = float(a == 0.0f && b == 0.0f);
+  result = float(!(a != 0.0f || b != 0.0f));
 }
 
-[[node]] 
+[[node]]
 void boolean_math_xnor(float a, float b, out float result)
 {
   result = float((a != 0.0f) == (b != 0.0f));
 }
 
-[[node]] 
+[[node]]
 void boolean_math_xor(float a, float b, out float result)
 {
   result = float((a != 0.0f) != (b != 0.0f));
 }
 
-[[node]] 
+[[node]]
 void boolean_math_imply(float a, float b, out float result)
 {
   result = float(a == 0.0f || b != 0.0f);
 }
 
-[[node]] 
+[[node]]
 void boolean_math_nimply(float a, float b, out float result)
 {
   result = float(a != 0.0f && b == 0.0f);
