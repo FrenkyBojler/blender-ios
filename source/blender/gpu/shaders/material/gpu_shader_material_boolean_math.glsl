04c780c36805397a6c60bdd6aff056df7d3d538d
Cristian Ivana <criss.ivana@gmail.com>
2026-05-31 16:30:14 +0300

    Refactor: conversion to bool helper func

--- source/blender/gpu/shaders/material/gpu_shader_material_boolean_math.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_boolean_math.glsl
@@ -2,56 +2,61 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+bool to_bool(float value)
+{
+  return value > 0.0f;
+}
+
 [[node]]
 void boolean_math_and(float a, float b, out float result)
 {
-  result = float(a != 0.0f && b != 0.0f);
+  result = float(to_bool(a) && to_bool(b));
 }
 
 [[node]]
 void boolean_math_or(float a, float b, out float result)
 {
-  result = float(a != 0.0f || b != 0.0f);
+  result = float(to_bool(a) || to_bool(b));
 }
 
 [[node]]
 void boolean_math_not(float a, out float result)
 {
-  result = float(a == 0.0f);
+  result = float(!to_bool(a));
 }
 
 [[node]]
 void boolean_math_nand(float a, float b, out float result)
 {
-  result = float(!(a != 0.0f && b != 0.0f));
+  result = float(!(to_bool(a) && to_bool(b)));
 }
 
 [[node]]
 void boolean_math_nor(float a, float b, out float result)
 {
-  result = float(!(a != 0.0f || b != 0.0f));
+  result = float(!(to_bool(a) || to_bool(b)));
 }
 
 [[node]]
 void boolean_math_xnor(float a, float b, out float result)
 {
-  result = float((a != 0.0f) == (b != 0.0f));
+  result = float(to_bool(a) == to_bool(b));
 }
 
 [[node]]
 void boolean_math_xor(float a, float b, out float result)
 {
-  result = float((a != 0.0f) != (b != 0.0f));
+  result = float(to_bool(a) != to_bool(b));
 }
 
 [[node]]
 void boolean_math_imply(float a, float b, out float result)
 {
-  result = float(a == 0.0f || b != 0.0f);
+  result = float(!to_bool(a) || to_bool(b));
 }
 
 [[node]]
 void boolean_math_nimply(float a, float b, out float result)
 {
-  result = float(a != 0.0f && b == 0.0f);
+  result = float(to_bool(a) && !to_bool(b));
 }
