641167f237b1ba9330bb4f5fef48c462a42a4273
Howard Trickey <howard.trickey@gmail.com>
2026-06-01 06:54:25 -0400

    Use propagate_all_geometry instead of propagate_geometry.
    On the Mesh socket. Suggested by Jacques else "otherwise
    it would propagate stuff from all the input fields".

--- source/blender/nodes/geometry/nodes/node_geo_mesh_bevel.cc
+++ source/blender/nodes/geometry/nodes/node_geo_mesh_bevel.cc
@@ -31,7 +31,7 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.allow_any_socket_order();
 
   b.add_input<decl::Geometry>("Mesh"_ustr).supported_type(GeometryComponent::Type::Mesh);
-  b.add_output<decl::Geometry>("Mesh"_ustr).propagate_all().align_with_previous();
+  b.add_output<decl::Geometry>("Mesh"_ustr).propagate_all_geometry().align_with_previous();
   b.add_input<decl::Bool>("Selection"_ustr)
       .default_value(true)
       .hide_value()
