bbd8da85cdca92630c0545a5a29ab39bdf13b51b
Miguel Pozo <pragma37@gmail.com>
2026-06-01 17:34:35 +0200

    Partial Revert "Take texture usage into account for recycling textures from the pool"
    This reverts commit 7f952957a998ffbd033761a5bee9973f108c3e10.

--- source/blender/gpu/opengl/gl_texture_pool.cc
+++ source/blender/gpu/opengl/gl_texture_pool.cc
@@ -91,9 +91,6 @@ Texture *GLTexturePool::acquire_texture_impl(int3 extent,
       /* TODO(not_mark): sub-view on mip levels. */
       continue;
     }
-    if (handle.texture->usage_get() != usage) {
-      continue;
-    }
     match_index = i;
     break;
   }
