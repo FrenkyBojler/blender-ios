90aa8f390a7515b9d74d890ba34107afe7d6d380
Miguel Pozo <pragma37@gmail.com>
2026-06-01 17:34:01 +0200

    Revert "Set GPU_TEXTURE_USAGE_FORMAT_VIEW for all backends"
    This reverts commit 8644f79f9b962301a6ac9f1cda6c784eaeb21826.

--- source/blender/gpu/opengl/gl_texture_pool.cc
+++ source/blender/gpu/opengl/gl_texture_pool.cc
@@ -66,7 +66,6 @@ Texture *GLTexturePool::acquire_texture_impl(int3 extent,
                                              eGPUTextureUsage usage,
                                              const char *name)
 {
-  usage |= GPU_TEXTURE_USAGE_FORMAT_VIEW;
   /* Determine format of compatible underlying texture. If there is no
    * compatible format to alias upon, we simply require an exact match
    * for the underlying texture. */
@@ -115,7 +114,7 @@ Texture *GLTexturePool::acquire_texture_impl(int3 extent,
     }
 
     Texture *texture = GPUBackend::get()->texture_alloc(texture_name_str.c_str());
-    texture->usage_set(usage);
+    texture->usage_set(usage | GPU_TEXTURE_USAGE_FORMAT_VIEW);
     bool texture_result = false;
     UNUSED_VARS_NDEBUG(texture_result);
     switch (type) {
