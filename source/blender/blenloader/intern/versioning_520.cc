6a8309313ff119b2488676176142f2c24d996b93
Jonas Holzman <jonas@blender.org>
2026-06-02 17:21:24 +0200

    Disable Viewfinder by default for release

--- source/blender/blenloader/intern/versioning_520.cc
+++ source/blender/blenloader/intern/versioning_520.cc
@@ -809,7 +809,7 @@ void blo_do_versions_520(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
 
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 502, 40)) {
     for (wmWindowManager &wm : bmain->wm) {
-      wm.xr.session_settings.viewfinder_enabled = true;
+      wm.xr.session_settings.viewfinder_enabled = false;
       wm.xr.session_settings.viewfinder_crosshair_enabled = true;
 
       wm.xr.session_settings.viewfinder_hand = XR_VIEWFINDER_HAND_RIGHT;
