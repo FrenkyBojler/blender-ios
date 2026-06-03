820da07d7a93ded35bb039c1037595598e48e172
Jacques Lucke <jacques@blender.org>
2026-06-03 23:56:18 +0200

    add vector hash test

--- source/blender/blenlib/tests/BLI_math_vector_test.cc
+++ source/blender/blenlib/tests/BLI_math_vector_test.cc
@@ -207,4 +207,10 @@ TEST(math_vector, square)
   EXPECT_NEAR(result.z, 9.0f, 1e-6f);
 }
 
+TEST(math_vector, ZeroHash)
+{
+  EXPECT_EQ(get_default_hash(float3(0.0f, 0.0f, 0.0f)),
+            get_default_hash(float3(-0.0f, -0.0f, -0.0f)));
+}
+
 }  // namespace blender::tests
