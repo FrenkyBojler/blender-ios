818f497cce614a147e59e7b2f3f0c09370138abe
Eitan Traurig <eitant13@gmail.com>
2026-06-04 12:33:39 -0400

    Flip dissolve and delete

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -789,9 +789,9 @@ class NODE_MT_context_menu(Menu):
 
         layout.separator()
 
-        layout.operator("node.delete", icon='X')
         layout.operator_context = 'EXEC_REGION_WIN'
         layout.operator("node.delete_reconnect", text="Dissolve")
+        layout.operator("node.delete", icon='X')
 
         if active_node:
             layout.separator()
