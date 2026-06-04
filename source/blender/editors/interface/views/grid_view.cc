a41889595e0855a23f36acc32570f2c24ab1dadf
Pratik Borhade <pratikborhade302@gmail.com>
2026-06-04 13:54:30 +0530

    Code comment

--- source/blender/editors/interface/views/grid_view.cc
+++ source/blender/editors/interface/views/grid_view.cc
@@ -280,6 +280,7 @@ void AbstractGridView::scroll_active_into_view(bContext *C, bool scroll_active_t
       int first_idx_in_view = visible_range.first();
       int last_idx_in_view = visible_range.last();
 
+      /* When button is slightly outside the view, clamp region to button's height, see: !159566 */
       first_idx_in_view += rect.ymax > v2d.cur.ymax ? cols_per_row_ : 0;
       last_idx_in_view -= rect.ymin < v2d.cur.ymin ? cols_per_row_ : 0;
 
