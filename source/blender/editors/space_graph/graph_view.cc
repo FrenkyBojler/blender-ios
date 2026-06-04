blender/blender

commit 297e9281feaaec2454ba97bcf853c39f226d9b47

Anim: Local/Isolate View for Graph Editor
Similar to 3d viewport, implement local/isolate view for graph editor to
isolate selected FCurves. `local_view_bit` member variable of SpaceGraph
tells whether view is isolated or not. Whereas, FCurve member
`local_view_bits` for how many local views this curve is part of.
`find_free_localview_bit ` copied from `view3d_view.cc`.

View is zoomed when entering the local view to fit selected FCurves
in visible range. Likewise, zoom is reset when exiting the local view.
Added `local_view_visible_region_before ` member in SpaceGraph struct to
store view bounds prior to local view.
Operator is mapped to `/` key. This zoom in/out behavior can be avoided
by disabling `frame_selected` operator property from keymap.

Note: Local view is currently not supported in driver editor (could be
done in future).

Design task: #147503

See PR description for video

Pull Request: https://projects.blender.org/blender/blender/pulls/154187

source/blender/editors/space_graph/graph_view.cc
@@ -24,6 +24,7 @@
 
 #include "BKE_context.hh"
 #include "BKE_fcurve.hh"
+#include "BKE_main.hh"
 #include "BKE_nla.hh"
 #include "BKE_scene.hh"
 
@@ -337,6 +338,16 @@ static wmOperatorStatus graphkeys_viewall(bContext *C,
     return OPERATOR_CANCELLED;
   }
 
+  if (ac.regiontype != RGN_TYPE_WINDOW) {
+    /* It is possible that operator is invoked from other regions like the Graph Editor's channel
+     * list. Main region is required here for smooth view function. */
+    for (ARegion &region : ac.area->regionbase) {
+      if (region.regiontype == RGN_TYPE_WINDOW) {
+        ac.region = &region;
+        break;
+      }
+    }
+  }
   /* Set the horizontal range, with an extra offset so that the extreme keys will be in view. */
   get_graph_view_bounds(&ac, cur_new, do_sel_only, include_handles);
 
@@ -621,6 +632,176 @@ void GRAPH_OT_ghost_curves_clear(wmOperatorType *ot)
 
   /* Flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+}
+
+/* Returns the free bit */
+static uint16_t find_free_localview_bit(const Main *bmain)
+{
+  uint16_t local_view_bits = 0;
+  static_assert(std::is_same_v<decltype(SpaceGraph::local_view_bit), decltype(local_view_bits)>);
+
+  /* Check all areas to see which local view bits are already in use. */
+  for (const bScreen &screen : bmain->screens) {
+    for (const ScrArea &area : screen.areabase) {
+      for (const SpaceLink &sl : area.spacedata) {
+        if (sl.spacetype == SPACE_GRAPH) {
+          const SpaceGraph *sipo = reinterpret_cast<const SpaceGraph *>(&sl);
+          local_view_bits |= sipo->local_view_bit;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < sizeof(SpaceGraph::local_view_bit); i++) {
+    if ((local_view_bits & (1 << i)) == 0) {
+      return (1 << i);
+    }
+  }
+
+  return 0;
+}
+
+/**
+ * Returns true if entered in local view.
+ */
+static bool local_view_enter(bContext *C,
+                             SpaceGraph &sipo,
+                             const ARegion &region,
+                             const ListBaseT<bAnimListElem> &anim_data,
+                             const bool frame_selected)
+{
+  bool is_selected = false;
+  /* Find a free bit and set local view for graph editor in current context. */
+  const uint16_t free_bit = find_free_localview_bit(CTX_data_main(C));
+
+  for (bAnimListElem &ale : anim_data) {
+    FCurve *fcu = static_cast<FCurve *>(ale.key_data);
+    if (ale.type != ANIMTYPE_FCURVE) {
+      continue;
+    }
+    if (ale.flag & FCURVE_SELECTED) {
+      /* Set bit for selected Fcurves to draw them in local view. */
+      fcu->local_view_bits |= free_bit;
+      is_selected = true;
+    }
+  }
+
+  /* Only enter local view when Fcurve is selected. */
+  if (!is_selected) {
+    return false;
+  }
+
+  sipo.local_view_bit = free_bit;
+  sipo.local_view_visible_region_before = region.v2d.cur;
+  if (frame_selected) {
+    graphkeys_viewall(C, false, true, 200);
+  }
+
+  return true;
+}
+
+/**
+ * Returns true if exited from the local view.
+ */
+static bool local_view_exit(bContext *C,
+                            SpaceGraph &sipo,
+                            ARegion &region,
+                            const ListBaseT<bAnimListElem> &anim_data,
+                            const bool frame_selected)
+{
+  bool changed = false;
+  for (bAnimListElem &ale : anim_data) {
+    FCurve *fcu = static_cast<FCurve *>(ale.key_data);
+    if (ale.type != ANIMTYPE_FCURVE) {
+      continue;
+    }
+    fcu->local_view_bits &= ~sipo.local_view_bit;
+    changed = true;
+  }
+
+  sipo.local_view_bit = 0;
+  if (frame_selected) {
+    /* Restore view. */
+    ui::view2d_smooth_view(C, &region, &sipo.local_view_visible_region_before, 200);
+  }
+
+  return changed;
+}
+
+static wmOperatorStatus graph_local_view_exec(bContext *C, wmOperator *op)
+{
+  bAnimContext ac;
+  ListBaseT<bAnimListElem> anim_data = {nullptr, nullptr};
+  SpaceGraph *sipo = CTX_wm_space_graph(C);
+  const bool frame_selected = RNA_boolean_get(op->ptr, "frame_selected");
+  const bool enter_local_view = (sipo->local_view_bit == 0);
+
+  if (ANIM_animdata_get_context(C, &ac) == 0) {
+    return OPERATOR_CANCELLED;
+  }
+
+  if (ac.regiontype != RGN_TYPE_WINDOW) {
+    /* It is possible that operator is invoked from other regions like the Graph Editor's channel
+     * list. Main region is required here for current bounds and smooth view function. */
+    for (ARegion &region : ac.area->regionbase) {
+      if (region.regiontype == RGN_TYPE_WINDOW) {
+        ac.region = &region;
+        break;
+      }
+    }
+  }
+
+  const eAnimFilter_Flags filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_LIST_CHANNELS |
+                                    ANIMFILTER_NODUPLIS | ANIMFILTER_FCURVESONLY);
+  ANIM_animdata_filter(&ac, &anim_data, filter, ac.data, ac.datatype);
+
+  const bool changed = enter_local_view ?
+                           local_view_enter(C, *sipo, *ac.region, anim_data, frame_selected) :
+                           local_view_exit(C, *sipo, *ac.region, anim_data, frame_selected);
+
+  ANIM_animdata_freelist(&anim_data);
+
+  if (!changed) {
+    return OPERATOR_CANCELLED;
+  }
+
+  WM_event_add_notifier(C, NC_ANIMATION | ND_ANIMCHAN | NA_EDITED, nullptr);
+  return OPERATOR_FINISHED;
+}
+
+static bool graph_local_view_poll(bContext *C)
+{
+  if (!ED_operator_graphedit_active(C)) {
+    return false;
+  }
+
+  const SpaceGraph *sipo = CTX_wm_space_graph(C);
+  /* Operator is not supported yet in driver editor. */
+  return sipo->mode != SIPO_MODE_DRIVERS;
+}
+
+void GRAPH_OT_local_view(wmOperatorType *ot)
+{
+  /* identifiers */
+  ot->name = "Local View";
+  ot->idname = "GRAPH_OT_local_view";
+  ot->description = "Isolate selected F-Curves in Graph Editor view";
+
+  /* API callbacks. */
+  ot->exec = graph_local_view_exec;
+  ot->poll = graph_local_view_poll;
+
+  /* flags */
+  ot->flag = OPTYPE_UNDO;
+
+  /* Props */
+  PropertyRNA *prop = RNA_def_boolean(
+      ot->srna,
+      "frame_selected",
+      true,
+      "Frame selected",
+      "Zoom current view to draw selected fcurves in visible range");
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
 }
 
 /** \} */

scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -1975,6 +1975,8 @@ def km_graph_editor_generic(params):
         *_template_items_hide_reveal_actions("graph.hide", "graph.reveal"),
         ("screen.space_type_set_or_cycle", {"type": 'TAB', "value": 'PRESS', "ctrl": True},
          {"properties": [("space_type", 'DOPESHEET_EDITOR')]}),
+        ("graph.local_view", {"type": 'SLASH', "value": 'PRESS'}, None),
+        ("graph.local_view", {"type": 'NUMPAD_SLASH', "value": 'PRESS'}, None),
     ])
 
     return keymap

scripts/startup/bl_ui/space_graph.py
@@ -221,6 +221,7 @@ class GRAPH_MT_view(Menu):
 
         layout.operator("graph.view_selected")
         layout.operator("graph.view_all")
+        layout.operator("graph.local_view")
         if context.scene.use_preview_range:
             layout.operator("anim.scene_range_frame", text="Frame Preview Range")
         else:

source/blender/editors/animation/anim_filter.cc
@@ -1446,6 +1446,12 @@ static size_t animfilter_fcurves_span(bAnimContext *ac,
     if (skip_fcurve_selected_data(ac, fcu, animated_id, filter_mode)) {
       continue;
     }
+    if (ac->spacetype == SPACE_GRAPH) {
+      const SpaceGraph *sipo = reinterpret_cast<SpaceGraph *>(ac->sl);
+      if ((sipo->local_view_bit) && (fcu->local_view_bits & sipo->local_view_bit) == 0) {
+        continue;
+      }
+    }
 
     bAnimListElem *ale = make_new_animlistelem(
         ac->bmain, fcu, ANIMTYPE_FCURVE, animated_id, fcurve_owner_id);

source/blender/editors/space_graph/CMakeLists.txt
@@ -32,6 +32,7 @@ set(SRC
 set(LIB
   PRIVATE bf::blenkernel
   PRIVATE bf::blenlib
+  PRIVATE bf::blenfont
   PRIVATE bf::blenloader
   PRIVATE bf::blentranslation
   PRIVATE bf::depsgraph

source/blender/editors/space_graph/graph_intern.hh
@@ -113,6 +113,7 @@ void GRAPH_OT_previewrange_set(wmOperatorType *ot);
 void GRAPH_OT_view_all(wmOperatorType *ot);
 void GRAPH_OT_view_selected(wmOperatorType *ot);
 void GRAPH_OT_view_frame(wmOperatorType *ot);
+void GRAPH_OT_local_view(wmOperatorType *ot);
 
 void GRAPH_OT_click_insert(wmOperatorType *ot);
 void GRAPH_OT_keyframe_insert(wmOperatorType *ot);

source/blender/editors/space_graph/graph_ops.cc
@@ -439,6 +439,7 @@ void graphedit_operatortypes()
 
   WM_operatortype_append(GRAPH_OT_hide);
   WM_operatortype_append(GRAPH_OT_reveal);
+  WM_operatortype_append(GRAPH_OT_local_view);
 
   /* keyframes */
   /* selection */

source/blender/editors/space_graph/space_graph.cc
@@ -22,6 +22,8 @@
 #include "BLI_string_utf8.h"
 #include "BLI_utildefines.h"
 
+#include "BLF_api.hh"
+
 #include "BKE_context.hh"
 #include "BKE_fcurve.hh"
 #include "BKE_lib_query.hh"
@@ -343,6 +345,14 @@ static void graph_main_region_draw(const bContext *C, ARegion *region)
 
   /* reset view matrix */
   ui::view2d_view_restore(C);
+
+  if (sipo->local_view_bit) {
+    const float margin = 25 * UI_SCALE_FAC;
+    const float x = margin;
+    const float y = region->winy - UI_TIME_SCRUB_MARGIN_Y - margin;
+    const std::string name = "Local View";
+    BLF_draw_default(x, y, 0.0f, name.c_str(), name.length());
+  }
 
   /* time-scrubbing */
   int base = round_db_to_int(scene->frames_per_second());
@@ -687,6 +697,29 @@ static void graph_listener(const wmSpaceTypeListenerParams *params)
   }
 }
 
+/* Exit local view when no fcurve channel exists. */
+static void local_view_exit_if_unused(const bContext *C, SpaceGraph *sipo)
+{
+  if (!sipo->local_view_bit) {
+    return;
+  }
+
+  bAnimContext ac;
+  if (!ANIM_animdata_get_context(C, &ac)) {
+    return;
+  }
+
+  ListBaseT<bAnimListElem> anim_data = {nullptr, nullptr};
+  const eAnimFilter_Flags filter = (ANIMFILTER_DATA_VISIBLE | ANIMFILTER_LIST_VISIBLE |
+                                    ANIMFILTER_LIST_CHANNELS | ANIMFILTER_FCURVESONLY);
+  const size_t item_count = ANIM_animdata_filter(&ac, &anim_data, filter, ac.data, ac.datatype);
+  if (item_count == 0) {
+    sipo->local_view_bit = 0;
+  }
+
+  ANIM_animdata_freelist(&anim_data);
+}
+
 /* Update F-Curve colors */
 static void graph_refresh_fcurve_colors(const bContext *C)
 {
@@ -840,6 +873,8 @@ static void graph_refresh(const bContext *C, ScrArea *area)
 
   /* init/adjust F-Curve colors */
   graph_refresh_fcurve_colors(C);
+  /* Exit local view if no F-Curve channels there. */
+  local_view_exit_if_unused(C, sipo);
 }
 
 static void graph_id_remap(ScrArea * /*area*/,

FrenkyBojler/blender-ios

commit ad12eb6715a398b73d8be2c06a56651b57f61028

Fix Local/Isolate View in Graph Editor limited to 2 open spaces
When looking for a free local view bit, the search loop bounds were checked
against the size of the variable in bytes (2) instead of the size in bits (16).
This meant that when 3 or more Graph Editor areas were open, no free local
view bit could be allocated, preventing local view from being entered.
This multiplies the sizeof check by 8 to iterate over bits.

This issue was introduced in 297e9281fea.

source/blender/editors/space_graph/graph_view.cc
@@ -652,7 +652,7 @@ static uint16_t find_free_localview_bit(const Main *bmain)
     }
   }
 
-  for (int i = 0; i < sizeof(SpaceGraph::local_view_bit); i++) {
+  for (int i = 0; i < 8 * sizeof(SpaceGraph::local_view_bit); i++) {
     if ((local_view_bits & (1 << i)) == 0) {
       return (1 << i);
     }