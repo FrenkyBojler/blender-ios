7edb691fb092e47ee47cf930c7812f6c297e7312
Julian Eisel <julian@blender.org>
2026-06-04 13:47:23 +0200

    Show checkbox under asset libraries list when "Essentials" is selected

--- source/blender/editors/space_userpref/userpref_asset_libraries_list.cc
+++ source/blender/editors/space_userpref/userpref_asset_libraries_list.cc
@@ -185,6 +185,15 @@ static void draw_library_list(const bContext &C, ui::Layout &layout)
 static void draw_active_library_settings(ui::Layout &layout,
                                          const AnyAssetLibraryDefinition &library)
 {
+  if (library.type == ASSET_LIBRARY_ESSENTIALS) {
+    PointerRNA prefs_ptr = RNA_pointer_create_discrete(nullptr, RNA_PreferencesAssetLibraries, &U);
+    layout.prop(&prefs_ptr,
+                "use_online_essentials",
+                UI_ITEM_NONE,
+                IFACE_("Include Online Essentials"),
+                ICON_NONE);
+  }
+
   if (library.user_library) {
     PointerRNA library_ptr = RNA_pointer_create_discrete(
         nullptr, RNA_UserAssetLibrary, library.user_library);
