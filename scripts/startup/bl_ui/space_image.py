052a0e27b145b2a335ce97558c7574a3aaeaaa45
Eitan Traurig <eitant13@gmail.com>
2026-06-05 16:48:50 -0400

    Add to UV editor

--- scripts/startup/bl_ui/space_image.py
+++ scripts/startup/bl_ui/space_image.py
@@ -232,8 +232,8 @@ class IMAGE_MT_image(Menu):
             del _ghost_backend
 
         if has_image_clipboard:
-            layout.operator("image.clipboard_copy", text="Copy")
-            layout.operator("image.clipboard_paste", text="Paste")
+            layout.operator("image.clipboard_copy", text="Copy", icon='COPYDOWN')
+            layout.operator("image.clipboard_paste", text="Paste", icon='PASTEDOWN')
             layout.separator()
 
         if ima:
