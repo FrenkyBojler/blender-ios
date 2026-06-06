d17f555fe4895a4fc4ef70b95d44a0efe1b630e1
Jerry Wei <79512937+JerryWeii-27@users.noreply.github.com>
2026-06-06 15:24:36 -0400

    Use correct flag for lock size

--- source/blender/blenkernel/intern/brush.cc
+++ source/blender/blenkernel/intern/brush.cc
@@ -1296,8 +1296,9 @@ bool BKE_brush_use_locked_size(const Paint *paint, const Brush *brush)
 {
   const short us_flag = paint->unified_paint_settings.flag;
 
-  return (us_flag & UNIFIED_PAINT_SIZE) ? (us_flag & UNIFIED_PAINT_BRUSH_LOCK_SIZE) != 0 :
-                                          (brush->flag & BRUSH_LOCK_SIZE) != 0;
+  return (brush->flag2 & BRUSH_USE_UNIFIED_PAINT_SIZE) ?
+             (us_flag & UNIFIED_PAINT_BRUSH_LOCK_SIZE) != 0 :
+             (brush->flag & BRUSH_LOCK_SIZE) != 0;
 }
 
 bool BKE_brush_use_size_pressure(const Brush *brush)
