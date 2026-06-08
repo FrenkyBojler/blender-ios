Zezo-Ai/blender

commit 45711c281ffa9c3a23c3f591a7d54248a39a5248

Refactor: ImBuf: Improve cropping and region copying API
There are several places where code dealing with ImBuf is much more
complex than it needs to be because of limitations in the way the API
for copying rectangular regions of pixels is designed. This commit
replaces that part of the API with more straightforward functions that
can work on buffers that aren't necessarily part of an ImBuf struct.
The main simplification is removal of the need for temporary buffers
in the image undo system. Besides that, the definition of source and
destination rectangles and sizes is now a bit clearer.

Pull Request: https://projects.blender.org/blender/blender/pulls/157425

source/blender/blenkernel/intern/tracking.cc
@@ -2686,7 +2686,25 @@ ImBuf *BKE_tracking_get_search_imbuf(const ImBuf *ibuf,
 
   searchibuf = IMB_allocImBuf(w, h, 32, ibuf->float_data() ? IB_float_data : IB_byte_data);
 
-  IMB_rectcpy(searchibuf, ibuf, 0, 0, x, y, w, h);
+  /* Clamp copy region to image bounds. */
+  int dst_x = 0, dst_y = 0;
+  if (x < 0) {
+    dst_x = -x;
+    w += x;
+    x = 0;
+  }
+  if (y < 0) {
+    dst_y = -y;
+    h += y;
+    y = 0;
+  }
+  if (x + w > ibuf->x) {
+    w = ibuf->x - x;
+  }
+  if (y + h > ibuf->y) {
+    h = ibuf->y - y;
+  }
+  IMB_copy_rect(searchibuf, ibuf, int2(x, y), int2(dst_x, dst_y), int2(w, h));
 
   if (disable_channels) {
     if ((track->flag & TRACK_PREVIEW_GRAYSCALE) || (track->flag & TRACK_DISABLE_RED) ||

source/blender/blenkernel/intern/tracking_util.cc
@@ -699,14 +699,14 @@ static ImBuf *accessor_get_ibuf(TrackingImageAccessor *accessor,
     final_ibuf = IMB_allocImBuf(width, height, 32, IB_float_data);
 
     if (orig_ibuf->float_data() != nullptr) {
-      IMB_rectcpy(final_ibuf,
-                  orig_ibuf,
-                  dst_offset_x,
-                  dst_offset_y,
-                  clamped_origin_x,
-                  clamped_origin_y,
-                  clamped_width,
-                  clamped_height);
+      IMB_copy_rect(final_ibuf->float_data_for_write(),
+                    int2(final_ibuf->x, final_ibuf->y),
+                    orig_ibuf->float_data(),
+                    int2(orig_ibuf->x, orig_ibuf->y),
+                    orig_ibuf->channels,
+                    int2(clamped_origin_x, clamped_origin_y),
+                    int2(dst_offset_x, dst_offset_y),
+                    int2(clamped_width, clamped_height));
     }
     else {
       /* TODO(sergey): We don't do any color space or alpha conversion

source/blender/editors/asset/intern/asset_ops.cc
@@ -1122,7 +1122,9 @@ static ImBuf *take_screenshot_crop(bContext *C, const rcti &crop_rect)
    * least freeing the memory after would cause a crash if ownership isn't taken. */
   IMB_assign_byte_buffer(image_buffer, dumprect, IB_TAKE_OWNERSHIP);
 
-  IMB_rect_crop(image_buffer, &safe_rect);
+  IMB_crop(image_buffer,
+           int2(safe_rect.xmin, safe_rect.ymin),
+           int2(BLI_rcti_size_x(&safe_rect) + 1, BLI_rcti_size_y(&safe_rect) + 1));
   return image_buffer;
 }
 
@@ -1207,10 +1209,12 @@ static wmOperatorStatus screenshot_preview_exec(bContext *C, wmOperator *op)
                             p2.x - area_p1->totrct.xmin,
                             p1.y - area_p1->totrct.ymin,
                             p2.y - area_p1->totrct.ymin};
-    IMB_rect_crop(image_buffer, &crop_rect);
+    IMB_crop(image_buffer,
+             int2(crop_rect.xmin, crop_rect.ymin),
+             int2(BLI_rcti_size_x(&crop_rect) + 1, BLI_rcti_size_y(&crop_rect) + 1));
   }
   else {
-    const rcti crop_rect = {p1.x, p2.x, p1.y, p2.y};
+    const rcti crop_rect = {p1.x, p2.x + 1, p1.y, p2.y + 1};
     image_buffer = take_screenshot_crop(C, crop_rect);
     if (!image_buffer) {
       BKE_report(op->reports, RPT_ERROR, "Invalid screenshot area selection");

source/blender/editors/include/ED_paint.hh
@@ -99,7 +99,6 @@ void *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
 void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
                                Image *image,
                                ImBuf *ibuf,
-                               ImBuf **tmpibuf,
                                ImageUser *iuser,
                                int x_tile,
                                int y_tile,

source/blender/editors/screen/screendump.cc
@@ -13,6 +13,7 @@
 #include "MEM_guardedalloc.h"
 
 #include "BLI_path_utils.hh"
+#include "BLI_rect.h"
 #include "BLI_string_utf8.h"
 #include "BLI_utildefines.h"
 
@@ -121,7 +122,9 @@ static wmOperatorStatus screenshot_exec(bContext *C, wmOperator *op)
 
       /* crop to show only single editor */
       if (use_crop) {
-        IMB_rect_crop(ibuf, &scd->crop);
+        IMB_crop(ibuf,
+                 int2(scd->crop.xmin, scd->crop.ymin),
+                 int2(BLI_rcti_size_x(&scd->crop) + 1, BLI_rcti_size_y(&scd->crop) + 1));
       }
 
       if ((scd->im_format.planes == R_IMF_PLANES_BW) &&

source/blender/editors/sculpt_paint/mesh/paint_image.cc
@@ -118,7 +118,6 @@ void imapaint_region_tiles(
 void ED_imapaint_dirty_region(
     Image *ima, ImBuf *ibuf, ImageUser *iuser, int x, int y, int w, int h, bool find_old)
 {
-  ImBuf *tmpibuf = nullptr;
   int tilex, tiley, tilew, tileh, tx, ty;
   int srcx = 0, srcy = 0;
 
@@ -139,15 +138,11 @@ void ED_imapaint_dirty_region(
   for (ty = tiley; ty <= tileh; ty++) {
     for (tx = tilex; tx <= tilew; tx++) {
       ED_image_paint_tile_push(
-          undo_tiles, ima, ibuf, &tmpibuf, iuser, tx, ty, nullptr, nullptr, false, find_old);
+          undo_tiles, ima, ibuf, iuser, tx, ty, nullptr, nullptr, false, find_old);
     }
   }
 
   BKE_image_mark_dirty(ima, ibuf);
-
-  if (tmpibuf) {
-    IMB_freeImBuf(tmpibuf);
-  }
 }
 
 void imapaint_image_update(

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -1094,7 +1094,7 @@ static void paint_2d_set_region(
 
 static int paint_2d_torus_split_region(ImagePaintRegion region[4],
                                        ImBuf *dbuf,
-                                       ImBuf *sbuf,
+                                       const ImBuf *sbuf,
                                        short paint_tile)
 {
   int destx = region->destx;
@@ -1157,7 +1157,7 @@ static int paint_2d_torus_split_region(ImagePaintRegion region[4],
   return tot;
 }
 
-static void paint_2d_lift_smear(ImBuf *ibuf, ImBuf *ibufb, int *pos, short paint_tile)
+static void paint_2d_lift_smear(const ImBuf *ibuf, ImBuf *ibufb, int *pos, short paint_tile)
 {
   ImagePaintRegion region[4];
   int a, tot;
@@ -1166,23 +1166,11 @@ static void paint_2d_lift_smear(ImBuf *ibuf, ImBuf *ibufb, int *pos, short paint
   tot = paint_2d_torus_split_region(region, ibufb, ibuf, paint_tile);
 
   for (a = 0; a < tot; a++) {
-    IMB_rectblend(ibufb,
-                  ibufb,
+    IMB_copy_rect(ibufb,
                   ibuf,
-                  nullptr,
-                  nullptr,
-                  nullptr,
-                  0,
-                  region[a].destx,
-                  region[a].desty,
-                  region[a].destx,
-                  region[a].desty,
-                  region[a].srcx,
-                  region[a].srcy,
-                  region[a].width,
-                  region[a].height,
-                  IMB_BLEND_COPY,
-                  false);
+                  int2(region[a].srcx, region[a].srcy),
+                  int2(region[a].destx, region[a].desty),
+                  int2(region[a].width, region[a].height));
   }
 }
 

source/blender/editors/sculpt_paint/mesh/paint_image_proj.cc
@@ -533,7 +533,6 @@ struct TileInfo {
   SpinLock *lock;
   bool masked;
   ushort tile_width;
-  ImBuf **tmpibuf;
   ProjPaintImage *pjima;
 };
 
@@ -1828,7 +1827,6 @@ static int project_paint_undo_subtiles(const TileInfo *tinf, int tx, int ty)
       undorect = ED_image_paint_tile_push(undo_tiles,
                                           pjIma->ima,
                                           pjIma->ibuf,
-                                          tinf->tmpibuf,
                                           &pjIma->iuser,
                                           tx,
                                           ty,
@@ -1841,7 +1839,6 @@ static int project_paint_undo_subtiles(const TileInfo *tinf, int tx, int ty)
       undorect = ED_image_paint_tile_push(undo_tiles,
                                           pjIma->ima,
                                           pjIma->ibuf,
-                                          tinf->tmpibuf,
                                           &pjIma->iuser,
                                           tx,
                                           ty,
@@ -3003,8 +3000,7 @@ static void project_paint_face_init(const ProjPaintState *ps,
                                     const int image_index,
                                     const rctf *clip_rect,
                                     const rctf *bucket_bounds,
-                                    ImBuf *ibuf,
-                                    ImBuf **tmpibuf)
+                                    ImBuf *ibuf)
 {
   /* Projection vars, to get the 3D locations into screen space. */
   MemArena *arena = ps->arena_mt[thread_index];
@@ -3016,7 +3012,6 @@ static void project_paint_face_init(const ProjPaintState *ps,
       ps->tile_lock,
       ps->do_masking,
       ushort(ED_IMAGE_UNDO_TILE_NUMBER(ibuf->x)),
-      tmpibuf,
       ps->projImages + image_index,
   };
 
@@ -3536,7 +3531,6 @@ static void project_bucket_init(const ProjPaintState *ps,
   int tri_index, image_index = 0;
   ImBuf *ibuf = nullptr;
   Image *tpage_last = nullptr, *tpage;
-  ImBuf *tmpibuf = nullptr;
   int tile_last = 0;
 
   if (ps->image_tot == 1) {
@@ -3551,8 +3545,7 @@ static void project_bucket_init(const ProjPaintState *ps,
                               0,
                               clip_rect,
                               bucket_bounds,
-                              ibuf,
-                              &tmpibuf);
+                              ibuf);
     }
   }
   else {
@@ -3584,20 +3577,9 @@ static void project_bucket_init(const ProjPaintState *ps,
       }
       /* context switching done */
 
-      project_paint_face_init(ps,
-                              thread_index,
-                              bucket_index,
-                              tri_index,
-                              image_index,
-                              clip_rect,
-                              bucket_bounds,
-                              ibuf,
-                              &tmpibuf);
+      project_paint_face_init(
+          ps, thread_index, bucket_index, tri_index, image_index, clip_rect, bucket_bounds, ibuf);
     }
-  }
-
-  if (tmpibuf) {
-    IMB_freeImBuf(tmpibuf);
   }
 
   ps->bucketFlags[bucket_index] |= PROJ_BUCKET_INIT;

FrenkyBojler/blender-ios

commit 1921c6913fa642b9f5ef26d09b8e7d76a8bfbc3d

Fix: Crash in tracking when search region is outside image bounds
IMB_copy_rect and IMB_crop were changed to assumpe dimensions > 0, but
the tracking code relied on these being silently rejected.

This issue was introduced in 45711c281ff.

source/blender/blenkernel/intern/tracking.cc
@@ -2711,7 +2711,9 @@ ImBuf *BKE_tracking_get_search_imbuf(const ImBuf *ibuf,
   if (y + h > ibuf->y) {
     h = ibuf->y - y;
   }
-  IMB_copy_rect(searchibuf, ibuf, int2(x, y), int2(dst_x, dst_y), int2(w, h));
+  if (w > 0 && h > 0) {
+    IMB_copy_rect(searchibuf, ibuf, int2(x, y), int2(dst_x, dst_y), int2(w, h));
+  }
 
   if (disable_channels) {
     if ((track->flag & TRACK_PREVIEW_GRAYSCALE) || (track->flag & TRACK_DISABLE_RED) ||

source/blender/blenkernel/intern/tracking_util.cc
@@ -699,14 +699,16 @@ static ImBuf *accessor_get_ibuf(TrackingImageAccessor *accessor,
     final_ibuf = IMB_allocImBuf(width, height, ImBufFlags::FloatData);
 
     if (orig_ibuf->float_data() != nullptr) {
-      IMB_copy_rect(final_ibuf->float_data_for_write(),
-                    int2(final_ibuf->x, final_ibuf->y),
-                    orig_ibuf->float_data(),
-                    int2(orig_ibuf->x, orig_ibuf->y),
-                    orig_ibuf->channels,
-                    int2(clamped_origin_x, clamped_origin_y),
-                    int2(dst_offset_x, dst_offset_y),
-                    int2(clamped_width, clamped_height));
+      if (clamped_width > 0 && clamped_height > 0) {
+        IMB_copy_rect(final_ibuf->float_data_for_write(),
+                      int2(final_ibuf->x, final_ibuf->y),
+                      orig_ibuf->float_data(),
+                      int2(orig_ibuf->x, orig_ibuf->y),
+                      orig_ibuf->channels,
+                      int2(clamped_origin_x, clamped_origin_y),
+                      int2(dst_offset_x, dst_offset_y),
+                      int2(clamped_width, clamped_height));
+      }
     }
     else {
       /* TODO(sergey): We don't do any color space or alpha conversion