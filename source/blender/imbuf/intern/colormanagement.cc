blender/blender

commit 65207a7e561c71f4494db31b26c5b3e80686488b

Color Management: Avoid potential buffer copies
Don't request a mutable copy of the image data unless it's actually
going to be modified (basically move the color management no-op check
before the "for_write()" call. And use const pointers to avoid copies
when writing EXR files.

Pull Request: https://projects.blender.org/blender/blender/pulls/158604

source/blender/imbuf/intern/colormanagement.cc
@@ -78,6 +78,14 @@ static CLG_LogRef LOG = {"color_management"};
 /** \name Global declarations
  * \{ */
 
+static void processor_transform_apply_threaded(uchar *byte_buffer,
+                                               float *float_buffer,
+                                               int width,
+                                               int height,
+                                               int channels,
+                                               ColormanageProcessor *cm_processor,
+                                               bool predivide);
+
 static bool g_config_is_custom = false;
 
 /* Lazily init inside function so it gets destructed before guardedalloc leak check. */
@@ -543,13 +551,23 @@ void colormanage_imbuf_make_linear(ImBuf *ibuf,
       }
     }
 
-    IMB_colormanagement_transform_float(ibuf->float_data_for_write(),
-                                        ibuf->x,
-                                        ibuf->y,
-                                        ibuf->channels,
-                                        from_colorspace,
-                                        to_colorspace,
-                                        predivide);
+    if (from_colorspace[0] == '\0') {
+      return;
+    }
+
+    ColormanageProcessor cm_processor = ColormanageProcessor::colorspace_processor_new(
+        from_colorspace, to_colorspace);
+    if (cm_processor.is_noop()) {
+      return;
+    }
+
+    processor_transform_apply_threaded(nullptr,
+                                       ibuf->float_data_for_write(),
+                                       ibuf->x,
+                                       ibuf->y,
+                                       ibuf->channels,
+                                       &cm_processor,
+                                       predivide);
     ibuf->float_buffer.colorspace = nullptr;
   }
 }

source/blender/blenkernel/intern/image_save.cc
@@ -706,13 +706,13 @@ bool BKE_image_save(
 
 /* OpenEXR saving, single and multilayer. */
 
-static float *image_exr_from_scene_linear_to_output(float *rect,
-                                                    const int width,
-                                                    const int height,
-                                                    const int channels,
-                                                    const ImageFormatData *imf,
-                                                    Vector<float *> &tmp_output_rects,
-                                                    StringRefNull &r_colorspace)
+static const float *image_exr_from_scene_linear_to_output(const float *rect,
+                                                          const int width,
+                                                          const int height,
+                                                          const int channels,
+                                                          const ImageFormatData *imf,
+                                                          Vector<float *> &tmp_output_rects,
+                                                          StringRefNull &r_colorspace)
 {
   if (imf == nullptr) {
     return rect;
@@ -723,7 +723,9 @@ static float *image_exr_from_scene_linear_to_output(float *rect,
     return rect;
   }
 
-  float *output_rect = MEM_dupalloc(rect);
+  const size_t size = size_t(width) * size_t(height) * size_t(channels);
+  float *output_rect = MEM_new_array_uninitialized<float>(size, __func__);
+  std::copy_n(rect, size, output_rect);
   tmp_output_rects.append(output_rect);
 
   const char *from_colorspace = IMB_colormanagement_role_colorspace_name_get(
@@ -736,8 +738,11 @@ static float *image_exr_from_scene_linear_to_output(float *rect,
   return output_rect;
 }
 
-static float *image_exr_from_rgb_to_bw(
-    float *input_buffer, int width, int height, int channels, Vector<float *> &temporary_buffers)
+static const float *image_exr_from_rgb_to_bw(const float *input_buffer,
+                                             int width,
+                                             int height,
+                                             int channels,
+                                             Vector<float *> &temporary_buffers)
 {
   float *gray_scale_output = MEM_new_array_uninitialized<float>(size_t(width) * size_t(height),
                                                                 "Gray Scale Buffer For EXR");
@@ -814,7 +819,7 @@ static void add_exr_compositing_result(ExrHandle *exr_handle,
 
     /* Compositing results is always a 4-channel RGBA. */
     const int channels_count_in_buffer = 4;
-    float *output_buffer = render_view.ibuf->float_data_for_write();
+    const float *output_buffer = render_view.ibuf->float_data();
     StringRefNull colorspace = IMB_colormanagement_role_colorspace_name_get(
         COLOR_ROLE_SCENE_LINEAR);
 
@@ -850,11 +855,11 @@ static void add_exr_compositing_result(ExrHandle *exr_handle,
 
     const ImColorMode color_mode = imf ? imf->color_mode : ImColorMode::RGBA;
     if (color_mode == ImColorMode::BW) {
-      float *gray_scale_output = image_exr_from_rgb_to_bw(output_buffer,
-                                                          render_result->rectx,
-                                                          render_result->recty,
-                                                          channels_count_in_buffer,
-                                                          temporary_buffers);
+      const float *gray_scale_output = image_exr_from_rgb_to_bw(output_buffer,
+                                                                render_result->rectx,
+                                                                render_result->recty,
+                                                                channels_count_in_buffer,
+                                                                temporary_buffers);
       IMB_exr_add_channels(exr_handle,
                            "",
                            "V",
@@ -946,7 +951,7 @@ bool BKE_image_render_write_exr(ReportList *reports,
       const bool pass_half_float = half_float && pass_RGBA;
 
       /* Color-space conversion only happens on RGBA passes. */
-      float *output_rect = render_pass.ibuf->float_data_for_write();
+      const float *output_rect = render_pass.ibuf->float_data();
       StringRefNull colorspace = IMB_colormanagement_role_colorspace_name_get(
           (pass_RGBA) ? COLOR_ROLE_SCENE_LINEAR : COLOR_ROLE_DATA);
 
@@ -1007,7 +1012,7 @@ bool BKE_image_render_write_exr(ReportList *reports,
       else if (required_channels == 1) {
         /* In case of a single required channel, we need to do RGB[A] to BW conversion. We know
          * the input is RGB[A] and not single channel because it filed the condition above. */
-        float *gray_scale_output = image_exr_from_rgb_to_bw(
+        const float *gray_scale_output = image_exr_from_rgb_to_bw(
             output_rect, rr->rectx, rr->recty, render_pass.channels, tmp_output_rects);
         IMB_exr_add_channels(exrhandle,
                              "",

source/blender/imbuf/IMB_openexr.hh
@@ -32,6 +32,8 @@ ExrHandle *IMB_exr_get_handle(bool write_multipart = false);
  * The number of channels is determined by channelnames.size() with
  * each character a channel name.
  * Layer and pass name, view name and colorspace are all optional.
+ *
+ * Used only for writing files.
  */
 void IMB_exr_add_channels(ExrHandle *handle,
                           StringRefNull layerpassname,
@@ -40,7 +42,7 @@ void IMB_exr_add_channels(ExrHandle *handle,
                           StringRefNull colorspace,
                           size_t xstride,
                           size_t ystride,
-                          float *rect,
+                          const float *rect,
                           bool use_half_float);
 
 /**

source/blender/imbuf/intern/openexr/openexr_api.cpp
@@ -920,7 +920,7 @@ void IMB_exr_add_channels(ExrHandle *handle,
                           StringRefNull colorspace,
                           size_t xstride,
                           size_t ystride,
-                          float *rect,
+                          const float *rect,
                           bool use_half_float)
 {
   /* For multipart, part name includes view since part names must be unique. */
@@ -971,7 +971,8 @@ void IMB_exr_add_channels(ExrHandle *handle,
 
     echan.xstride = xstride;
     echan.ystride = ystride;
-    echan.rect = rect + channel;
+    /* This is used for writing, the data should not be modified. ????????? */
+    echan.rect = const_cast<float *>(rect + channel);
     echan.use_half_float = use_half_float;
   }
 

FrenkyBojler/blender-ios

commit b280b7f0a7516677061ef8925e227ac959bc61ae

Fix: Making image buffer scene linear may leave wrong colorspace
The new early returns in colormanage_imbuf_make_linear skipped clearing
the colorspace to indicate it's now scene linear.

This issue was introduced in 65207a7e561.

source/blender/imbuf/intern/colormanagement.cc
@@ -551,6 +551,9 @@ void colormanage_imbuf_make_linear(ImBuf *ibuf,
       }
     }
 
+    /* Clear colorspace to indicate it's scene linear. */
+    ibuf->float_buffer.colorspace = nullptr;
+
     if (from_colorspace[0] == '\0') {
       return;
     }
@@ -568,7 +571,6 @@ void colormanage_imbuf_make_linear(ImBuf *ibuf,
                                        ibuf->channels,
                                        &cm_processor,
                                        predivide);
-    ibuf->float_buffer.colorspace = nullptr;
   }
 }
 