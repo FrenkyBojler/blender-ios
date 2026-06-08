d53ba0e3dc5c3b9628510bbe1be151170720283a
Henry Jiang <henryyjiang42@gmail.com>
2026-06-08 12:59:13 -0400

    Resolving float3 issue

--- source/blender/bmesh/operators/bmo_edge_flow.cc
+++ source/blender/bmesh/operators/bmo_edge_flow.cc
@@ -118,33 +118,33 @@ void bmo_edge_flow_exec(BMesh *bm, BMOperator *op)
     if (mode == EDGE_FLOW_LINEAR) {
       if (space_evenly) {
         const int count = int(loop.verts.size()) - 1;
-        float3 dir[3];
+        float3 dir;
         sub_v3_v3v3(dir, p2->co, p1->co);
 
         for (const int i : loop.verts.index_range().drop_front(1).drop_back(1)) {
-          float3 co[3];
+          float3 co;
           madd_v3_v3v3fl(co, p1->co, dir, float(i) / float(count));
 
-          float3 blended[3];
+          float3 blended;
           interp_v3_v3v3(blended, orig_cos[i], co, mix);
           copy_v3_v3(loop.verts[i]->co, blended);
         }
       }
       else { 
         /* space_evenly off flag */
-        float3 dir[3], dir_norm[3];
+        float3 dir, dir_norm;
         sub_v3_v3v3(dir, p2->co, p1->co);
         normalize_v3_v3(dir_norm, dir);
 
         for (const int i : loop.verts.index_range().drop_front(1).drop_back(1)) {
-          float3 co[3];
+          float3 co;
           sub_v3_v3v3(co, orig_cos[i], p1->co);
           float3 dir_scalar = dot_v3v3(co, dir_norm);
 
-          float3 new_co[3];
+          float3 new_co;
           madd_v3_v3v3fl(new_co, p1->co, dir_norm, dir_scalar);
 
-          float3 blended[3];
+          float3 blended;
           interp_v3_v3v3(blended, orig_cos[i], new_co, mix);
           copy_v3_v3(loop.verts[i]->co, blended);
         }
