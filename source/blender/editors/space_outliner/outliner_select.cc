blender/blender

commit efcd6c653f3cc42408e04828e9651bc1540ebd80

Outliner: Draw Shapekeys
Display individual shapekey in outliner, similar to other child data
like vertex group. New types introduced for key (`TSE_SHAPE_KEY_BASE`)
and Keyblock (`TSE_SHAPE_KEY_BLOCK`). Renaming is handled using
`BKE_keyblock_rename` and Base element name is non-editable.

See PR description for images

Pull Request: https://projects.blender.org/blender/blender/pulls/155406

source/blender/editors/space_outliner/outliner_select.cc
@@ -855,6 +855,11 @@ void tree_element_activate(bContext *C,
   }
 }
 
+static void tree_elemment_shapekey_active_set(Object &ob, TreeElement &te)
+{
+  ob.shapenr = te.index + 1;
+}
+
 void tree_element_type_active_set(bContext *C,
                                   const TreeViewContext &tvc,
                                   TreeElement *te,
@@ -916,6 +921,8 @@ void tree_element_type_active_set(bContext *C,
     case TSE_LAYER_COLLECTION:
       tree_element_layer_collection_activate(C, te);
       break;
+    case TSE_SHAPE_KEY_BLOCK:
+      tree_elemment_shapekey_active_set(*tvc.obact, *te);
     default:
       break;
   }
@@ -1190,6 +1197,14 @@ eOLDrawState tree_element_active_state_get(const TreeViewContext &tvc,
   return OL_DRAWSEL_NONE;
 }
 
+static eOLDrawState tree_element_shapekey_state_get(const Object &ob, const TreeElement *te)
+{
+  if (ob.shapenr == te->index + 1) {
+    return OL_DRAWSEL_NORMAL;
+  }
+  return OL_DRAWSEL_NONE;
+}
+
 eOLDrawState tree_element_type_active_state_get(const TreeViewContext &tvc,
                                                 const TreeElement *te,
                                                 const TreeStoreElem *tselem)
@@ -1232,6 +1247,8 @@ eOLDrawState tree_element_type_active_state_get(const TreeViewContext &tvc,
       return tree_element_layer_collection_state_get(tvc.layer_collection, te);
     case TSE_BONE_COLLECTION:
       return tree_element_bone_collection_state_get(te, tselem);
+    case TSE_SHAPE_KEY_BLOCK:
+      return tree_element_shapekey_state_get(*tvc.obact, te);
     default:
       break;
   }

source/blender/blenkernel/BKE_key.hh
@@ -211,6 +211,7 @@ void BKE_keyblock_curve_data_set_with_mat4(Key *key,
  */
 void BKE_keyblock_data_set(Key *key, int shape_index, const void *data);
 
+void BKE_keyblock_rename(const Key *key, KeyBlock *kb, const char *newname);
 /** \} */
 
 }  // namespace blender

source/blender/blenkernel/intern/key.cc
@@ -37,6 +37,7 @@
 #include "DNA_object_types.h"
 
 #include "BKE_anim_data.hh"
+#include "BKE_animsys.h"
 #include "BKE_attribute.hh"
 #include "BKE_curve.hh"
 #include "BKE_customdata.hh"
@@ -1908,4 +1909,24 @@ std::optional<Array<bool>> BKE_keyblock_get_dependent_keys(const Key *key, const
   return marked;
 }
 
+void BKE_keyblock_rename(const Key *key, KeyBlock *kb, const char *newname)
+{
+  char oldname[sizeof(kb->name)];
+
+  /* Make a copy of the old name first. */
+  STRNCPY(oldname, kb->name);
+  /* Copy the new name into the name slot. */
+  STRNCPY_UTF8(kb->name, newname);
+
+  /* Make sure the name is truly unique. */
+  BLI_uniquename(&key->block,
+                 kb,
+                 CTX_DATA_(BLT_I18NCONTEXT_ID_SHAPEKEY, "Key"),
+                 '.',
+                 offsetof(KeyBlock, name),
+                 sizeof(kb->name));
+
+  /* Fix all the animation data which may link to this. */
+  BKE_animdata_fix_paths_rename_all(nullptr, "key_blocks", oldname, kb->name);
+}
 }  // namespace blender

source/blender/editors/space_outliner/CMakeLists.txt
@@ -76,6 +76,7 @@ set(SRC
   tree/tree_element_rna.cc
   tree/tree_element_scene_objects.cc
   tree/tree_element_seq.cc
+  tree/tree_element_shapekey.cc
   tree/tree_element_view_collection.cc
   tree/tree_element_view_layer.cc
   tree/tree_iterator.cc
@@ -122,6 +123,7 @@ set(SRC
   tree/tree_element_rna.hh
   tree/tree_element_scene_objects.hh
   tree/tree_element_seq.hh
+  tree/tree_element_shapekey.hh
   tree/tree_element_view_collection.hh
   tree/tree_element_view_layer.hh
   tree/tree_iterator.hh

source/blender/editors/space_outliner/outliner_draw.cc
@@ -10,6 +10,7 @@
 #include "DNA_collection_types.h"
 #include "DNA_constraint_types.h"
 #include "DNA_gpencil_legacy_types.h"
+#include "DNA_key_types.h"
 #include "DNA_layer_types.h"
 #include "DNA_light_types.h"
 #include "DNA_lightprobe_types.h"
@@ -37,6 +38,7 @@
 #include "BKE_gpencil_legacy.h"
 #include "BKE_grease_pencil.hh"
 #include "BKE_idtype.hh"
+#include "BKE_key.hh"
 #include "BKE_layer.hh"
 #include "BKE_lib_id.hh"
 #include "BKE_lib_override.hh"
@@ -992,6 +994,20 @@ static void namebutton_fn(bContext *C, TreeStoreElem *tselem, const char *oldnam
         case TSE_ACTION_SLOT: {
           WM_event_add_notifier(C, NC_ID | NA_RENAME, nullptr);
           undo_str = CTX_N_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Rename Action Slot");
+          break;
+        }
+        case TSE_SHAPE_KEY_BLOCK: {
+          const Key *key = id_cast<Key *>(tselem->id);
+          KeyBlock *keyblock = static_cast<KeyBlock *>(te->directdata);
+          /* Outliner renaming already sets the new name to the KeyBlock. Restore the old name
+          before calling rename function which will ensure unique name. */
+          char newname[sizeof(keyblock->name)];
+          STRNCPY_UTF8(newname, keyblock->name);
+          STRNCPY_UTF8(keyblock->name, oldname);
+          BKE_keyblock_rename(key, keyblock, newname);
+          WM_event_add_notifier(C, NC_ID | NA_RENAME, nullptr);
+          DEG_id_tag_update(tselem->id, ID_RECALC_SYNC_TO_EVAL);
+          undo_str = CTX_N_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Rename Shape Key");
           break;
         }
         default:
@@ -2908,6 +2924,10 @@ TreeElementIcon tree_element_get_icon(TreeStoreElem *tselem, TreeElement *te)
         data.drag_id = tselem->id;
         data.icon = ICON_SHADERFX;
         break;
+      case TSE_SHAPE_KEY_BLOCK:
+      case TSE_SHAPE_KEY_BASE:
+        data.icon = ICON_SHAPEKEY_DATA;
+        break;
       default:
         data.icon = ICON_DOT;
         break;
@@ -3189,7 +3209,8 @@ static void outliner_draw_iconrow(ui::Block *block,
                 TSE_BONE_COLLECTION,
                 TSE_DEFGROUP,
                 TSE_ACTION_SLOT,
-                TSE_NLA_TRACK))
+                TSE_NLA_TRACK) &&
+          tselem->type != TSE_SHAPE_KEY_BLOCK)
       {
         outliner_draw_iconrow_doit(block, &te, xmax, offsx, ys, alpha_fac, active, 1);
       }

source/blender/editors/space_outliner/outliner_edit.cc
@@ -351,7 +351,11 @@ static void do_item_rename(ARegion *region,
            TSE_RNA_PROPERTY,
            TSE_RNA_ARRAY_ELEM,
            TSE_ID_BASE) ||
-      ELEM(tselem->type, TSE_SCENE_OBJECTS_BASE, TSE_GENERIC_LABEL, TSE_GPENCIL_EFFECT_BASE))
+      ELEM(tselem->type,
+           TSE_SCENE_OBJECTS_BASE,
+           TSE_GENERIC_LABEL,
+           TSE_GPENCIL_EFFECT_BASE,
+           TSE_SHAPE_KEY_BASE))
   {
     BKE_report(reports, RPT_INFO, "Not an editable name");
   }

source/blender/editors/space_outliner/outliner_tree.cc
@@ -353,6 +353,9 @@ TreeElement *AbstractTreeDisplay::add_element(ListBaseT<TreeElement> *lb,
   else if (type == TSE_LINKED_OB) {
     /* pass */
   }
+  else if (type == TSE_SHAPE_KEY_BLOCK) {
+    /* pass */
+  }
   else if (type == TSE_SOME_ID) {
     BLI_assert_msg(te->abstract_element != nullptr,
                    "Expected this ID type to be ported to new Outliner tree-element design");

source/blender/editors/space_outliner/tree/tree_element.cc
@@ -41,6 +41,7 @@
 #include "tree_element_rna.hh"
 #include "tree_element_scene_objects.hh"
 #include "tree_element_seq.hh"
+#include "tree_element_shapekey.hh"
 #include "tree_element_view_collection.hh"
 #include "tree_element_view_layer.hh"
 
@@ -204,7 +205,12 @@ std::unique_ptr<AbstractTreeElement> AbstractTreeElement::create_from_type(const
     case TSE_ACTION_SLOT:
       return std::make_unique<TreeElementActionSlot>(
           legacy_te, *reinterpret_cast<animrig::Slot *>(create_data));
-
+    case TSE_SHAPE_KEY_BLOCK:
+      return std::make_unique<TreeElementShapeKey>(legacy_te,
+                                                   *static_cast<KeyBlock *>(create_data));
+    case TSE_SHAPE_KEY_BASE:
+      return std::make_unique<TreeElementShapeKeyBase>(legacy_te,
+                                                       *reinterpret_cast<Key *>(owner_id));
     default:
       break;
   }

FrenkyBojler/blender-ios

commit 265bcb2ffe730f8ca2789e8ecb50cda28c4b1300

Fix: Clicking shapekey icon in outliner doesn't switch to data properties tab
Caused by efcd6c653f3cc42408e04828e9651bc1540ebd80
Add shapekey condition inside `outliner_set_properties_tab` as non ID.
First find the parent object of the clicked shape key then get the ob.data
id to use it t generate `PointerRNA`

source/blender/editors/space_outliner/outliner_select.cc
@@ -1337,6 +1337,15 @@ static void outliner_set_properties_tab(bContext *C, TreeElement *te, TreeStoreE
   }
   else {
     switch (tselem->type) {
+      case TSE_SHAPE_KEY_BASE:
+      case TSE_SHAPE_KEY_BLOCK:
+        if(TreeElement *parent_te = outliner_search_back_te(te, ID_OB)) {
+          TreeStoreElem *parent_tselem = TREESTORE(parent_te);
+          Object *ob = id_cast<Object *>(parent_tselem->id);
+          ptr = RNA_id_pointer_create(static_cast<ID *>(ob->data));
+          context = BCONTEXT_DATA;
+        }
+        break;
       case TSE_DEFGROUP_BASE:
       case TSE_DEFGROUP:
         ptr = RNA_id_pointer_create(tselem->id);