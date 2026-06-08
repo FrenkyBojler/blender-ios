5d60c8f4fcfba9279fb34ae4163c1aec8e40d06a
Pratik Borhade <pratikborhade302@gmail.com>
2026-06-08 09:15:20 +0530

    Fix format

--- source/blender/editors/space_file/file_ops.cc
+++ source/blender/editors/space_file/file_ops.cc
@@ -1880,10 +1880,12 @@ static wmOperatorStatus file_external_operation_exec(bContext *C, wmOperator *op
                                                                               "operation");
 
   char filepath[FILE_MAX_LIBEXTRA];
-  if (!(fileentry->typeflag & FILE_TYPE_DIR) && (operation == FILE_EXTERNAL_OPERATION_FOLDER_OPEN)) {
+  if (!(fileentry->typeflag & FILE_TYPE_DIR) && (operation == FILE_EXTERNAL_OPERATION_FOLDER_OPEN))
+  {
     const char *root = filelist_dir(sfile->files);
     BLI_strncpy(filepath, root, sizeof(filepath));
-  } else {
+  }
+  else {
     filelist_file_get_full_path(sfile->files, fileentry, filepath);
   }
 
