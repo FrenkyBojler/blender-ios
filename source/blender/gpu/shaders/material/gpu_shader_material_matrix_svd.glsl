b09599c60e1a8e0959aa8b435207beb1db07c948
Cristian Ivana <criss.ivana@gmail.com>
2026-06-10 02:37:49 +0300

    Add node to Compositor & scaffold

---
+++ source/blender/gpu/shaders/material/gpu_shader_material_matrix_svd.glsl
@@ -0,0 +1,9 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+[[node]]
+void matrix_svd(float4x4 matrix, out float4x4 U, out float3 &, out float4x4 V)
+{
+
+}
