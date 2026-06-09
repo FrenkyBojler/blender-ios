8fd71f33bb7647a802d9eb45b7b5275bb4518fb4
Mark van de Ruit <mark@blender.org>
2026-06-09 17:37:28 +0200

    Added TODO for deduplication

--- source/blender/draw/engines/overlay/shaders/overlay_grid_frag.glsl
+++ source/blender/draw/engines/overlay/shaders/overlay_grid_frag.glsl
@@ -8,21 +8,24 @@ FRAGMENT_SHADER_CREATE_INFO(overlay_grid_next)
 
 #include "draw_view_lib.glsl"
 #include "gpu_shader_math_base_lib.glsl"
-#include "gpu_shader_math_vector_compare_lib.glsl"
 #include "gpu_shader_utildefines_lib.glsl"
 #include "overlay_common_lib.glsl"
 #include "overlay_grid_common_lib.glsl"
+
+/* TODO(not_mark): De-duplicate in BSL port. */
+bool is_equal(float2 a, float2 b, float epsilon)
+{
+  return all(lessThanEqual(abs(a - b), float2(epsilon)));
+}
 
 void main()
 {
   /* Test if a vertex output position overlaps with an active axis line. */
   constexpr float axis_epsilon = 2e-7f;
-  bool3 axis_mask = bool3(flag_test(grid_flag, AXIS_X) &&
-                              is_equal<float2>(vertex_out.pos.yz, float2(0.0), axis_epsilon),
-                          flag_test(grid_flag, AXIS_Y) &&
-                              is_equal<float2>(vertex_out.pos.xz, float2(0.0), axis_epsilon),
-                          flag_test(grid_flag, AXIS_Z) &&
-                              is_equal<float2>(vertex_out.pos.xy, float2(0.0), axis_epsilon));
+  bool3 axis_mask = bool3(
+      flag_test(grid_flag, AXIS_X) && is_equal(vertex_out.pos.yz, float2(0.0), axis_epsilon),
+      flag_test(grid_flag, AXIS_Y) && is_equal(vertex_out.pos.xz, float2(0.0), axis_epsilon),
+      flag_test(grid_flag, AXIS_Z) && is_equal(vertex_out.pos.xy, float2(0.0), axis_epsilon));
 
   /* If an axis line overlaps, the fragment can be discarded. */
   if (any(axis_mask) && flag_test(grid_flag, SHOW_GRID)) {
