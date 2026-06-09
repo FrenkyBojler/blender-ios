blender/blender

commit f1dac96cfd2663a90c8b4cda50c27b46c64d3a8c

Fix: Image buffer colorspace lost after scaling
Missing colorspace copy for the buffers.

This issue was introduced in 8317e481083.

Pull Request: https://projects.blender.org/blender/blender/pulls/159668

source/blender/imbuf/intern/scaling.cc
@@ -578,6 +578,10 @@ ImBuf *IMB_scale_into_new(const ImBuf *ibuf,
       break;
     }
   }
+
+  dst->byte_buffer.colorspace = ibuf->byte_buffer.colorspace;
+  dst->float_buffer.colorspace = ibuf->float_buffer.colorspace;
+
   return dst;
 }
 

FrenkyBojler/blender-ios

commit 581640e6d740a6d965b349ff99e0862e562f1a80

Fix: Image buffer colorspace lost with various operations
This issue was introduced in f1dac96cfd2.

source/blender/imbuf/intern/allocimbuf.cc
@@ -293,7 +293,9 @@ void IMB_ensure_host_buffer(ImBuf *ibuf)
   GPU_memory_barrier(GPU_BARRIER_TEXTURE_UPDATE);
   float *output_buffer = static_cast<float *>(
       GPU_texture_read(ibuf->gpu.texture, GPU_DATA_FLOAT, 0));
+  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
   ibuf->assign_float_data(output_buffer);
+  ibuf->float_buffer.colorspace = float_colorspace;
 
   if (need_secondary_context) {
     IMB_deactivate_gpu_context();

source/blender/imbuf/intern/rectop.cc
@@ -474,12 +474,17 @@ void IMB_crop(ImBuf *ibuf, const int2 &rect_pos, const int2 &rect_size)
     return;
   }
 
+  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
+  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
+
   if (const uchar *byte_data = ibuf->byte_data()) {
     ibuf->assign_byte_data(create_cropped_buffer(byte_data, src_size, rect_pos, rect_size));
+    ibuf->byte_buffer.colorspace = byte_colorspace;
   }
   if (const float *float_data = ibuf->float_data()) {
     ibuf->assign_float_data(
         create_cropped_buffer(float_data, src_size, ibuf->channels, rect_pos, rect_size));
+    ibuf->float_buffer.colorspace = float_colorspace;
   }
 
   ibuf->x = rect_size.x;

source/blender/imbuf/intern/rotate.cc
@@ -66,6 +66,9 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
   const int size_x = ibuf->x;
   const int size_y = ibuf->y;
 
+  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
+  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
+
   if (ELEM(degrees, 90, 270)) {
     std::swap(ibuf->x, ibuf->y);
   }
@@ -76,6 +79,7 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
         size_t(channels) * size_t(size_x) * size_t(size_y), __func__);
     rotate_pixels<float>(degrees, size_x, size_y, src_pixels, dst_pixels, ibuf->channels);
     ibuf->assign_float_data(dst_pixels);
+    ibuf->float_buffer.colorspace = float_colorspace;
     if (ibuf->byte_data()) {
       IMB_byte_from_float(ibuf);
     }
@@ -86,6 +90,7 @@ bool IMB_rotate_orthogonal(ImBuf *ibuf, int degrees)
                                                            __func__);
     rotate_pixels<uchar>(degrees, size_x, size_y, src_pixels, dst_pixels, 4);
     ibuf->assign_byte_data(dst_pixels);
+    ibuf->byte_buffer.colorspace = byte_colorspace;
   }
 
   return true;

source/blender/imbuf/intern/scaling.cc
@@ -460,6 +460,9 @@ bool IMB_scale(ImBuf *ibuf, const int2 new_size, IMBScaleFilter filter, bool thr
     return false;
   }
 
+  const ColorSpace *float_colorspace = ibuf->float_buffer.colorspace;
+  const ColorSpace *byte_colorspace = ibuf->byte_buffer.colorspace;
+
   switch (filter) {
     case IMBScaleFilter::Nearest: {
       if (const float *src = ibuf->float_data()) {
@@ -507,6 +510,8 @@ bool IMB_scale(ImBuf *ibuf, const int2 new_size, IMBScaleFilter filter, bool thr
       break;
     }
   }
+  ibuf->float_buffer.colorspace = float_colorspace;
+  ibuf->byte_buffer.colorspace = byte_colorspace;
   ibuf->x = new_size.x;
   ibuf->y = new_size.y;
   return true;