blender/blender

commit 3bd35d9a4b22493519079d808bf70eecaf523fdc

ImBuf: Implicit sharing for CPU buffers
The current mechanisms to avoid copying ImBuf CPU buffers either do not
work in some situations, are fragile, or add too much code complexity
compared to implicit sharing / copy-on-write. This PR implements those
features for the float and byte CPU buffers. The existing "refcounter"
remains, in order to simplify the change, and because it isn't clear how
it should interact with GPU textures. Implicit sharing is added to the
file output compositor node and replaces the need for "externally owned"
semantics in the compositor's "Result" class, at least for the CPU
buffers. In that class, switching to ImplicitSharingPtr for the RAII
features made the change simpler.

Copying `ImBuf` is now quite cheap, and it should be possible to benefit
from this to simplify the code further in the future. For now, this is
used to simplify the image undo tile and paint tile implementations.

Pull Request: https://projects.blender.org/blender/blender/pulls/157645

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -1251,19 +1251,13 @@ static void paint_2d_do_making_brush(ImagePaintState *s,
       int origx = region->destx - tx * ED_IMAGE_UNDO_TILE_SIZE;
       int origy = region->desty - ty * ED_IMAGE_UNDO_TILE_SIZE;
 
+      const ImBuf *data = ED_image_paint_tile_find(
+          undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false);
       if (tile->canvas->float_data()) {
-        IMB_assign_float_buffer(
-            &tmpbuf,
-            static_cast<float *>(ED_image_paint_tile_find(
-                undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false)),
-            IB_DO_NOT_TAKE_OWNERSHIP);
+        tmpbuf.float_buffer = data->float_buffer;
       }
       else {
-        IMB_assign_byte_buffer(
-            &tmpbuf,
-            static_cast<uchar *>(ED_image_paint_tile_find(
-                undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false)),
-            IB_DO_NOT_TAKE_OWNERSHIP);
+        tmpbuf.byte_buffer = data->byte_buffer;
       }
 
       IMB_rectblend(tile->canvas,

source/blender/compositor/cached_resources/intern/cached_image.cc
@@ -200,15 +200,13 @@ static ImBuf *compute_linear_buffer(ImBuf *image_buffer)
   linear_image_buffer->flags = image_buffer->flags;
 
   /* Assign the float buffer if it exists, as well as its number of channels. */
-  IMB_assign_float_buffer(
-      linear_image_buffer, image_buffer->float_buffer, IB_DO_NOT_TAKE_OWNERSHIP);
+  linear_image_buffer->float_buffer = image_buffer->float_buffer;
   linear_image_buffer->channels = image_buffer->channels;
 
   /* If no float buffer exists, assign it then compute a float buffer from it. This is the main
    * call of this function. */
   if (!linear_image_buffer->float_data()) {
-    IMB_assign_byte_buffer(
-        linear_image_buffer, image_buffer->byte_buffer, IB_DO_NOT_TAKE_OWNERSHIP);
+    linear_image_buffer->byte_buffer = image_buffer->byte_buffer;
     IMB_float_from_byte(linear_image_buffer);
   }
 
@@ -340,7 +338,9 @@ CachedImage::CachedImage(Context &context,
   else {
     const int2 size = int2(image_buffer->x, image_buffer->y);
     Result buffer_result(context, float_type(image_buffer->channels), ResultPrecision::Full);
-    buffer_result.share_data(linear_image_buffer->float_data(), size);
+    buffer_result.share_data(linear_image_buffer->float_buffer.data,
+                             size,
+                             linear_image_buffer->float_buffer.sharing_info);
     this->result.allocate_texture(size, false);
 
     if (buffer_result.type() == ResultType::Color && result.type() == ResultType::Float4) {

source/blender/compositor/intern/render_context.cc
@@ -98,9 +98,18 @@ void FileOutput::add_view(const char *view_name, const Result &data)
   }
   render_view->ibuf = IMB_allocImBuf(UNPACK2(data.domain().data_size), ImBufFlags::Zero);
   render_view->ibuf->color_mode = color_mode;
-  IMB_alloc_float_pixels(render_view->ibuf, data.channels_count(), false);
-  std::memcpy(
-      render_view->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  if (data.sharing_info()) {
+    render_view->ibuf->channels = data.channels_count();
+    render_view->ibuf->float_buffer = ImBufFloatBuffer{
+        .data = static_cast<const float *>(data.cpu_data().data()),
+        .sharing_info = data.sharing_info(),
+        .colorspace = nullptr};
+  }
+  else if (data.cpu_data().data() != render_view->ibuf->float_data()) {
+    IMB_alloc_float_pixels(render_view->ibuf, data.channels_count(), false);
+    std::memcpy(
+        render_view->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  }
 }
 
 void FileOutput::add_pass(const char *pass_name,
@@ -131,9 +140,18 @@ void FileOutput::add_pass(const char *pass_name,
   }
   render_pass->ibuf = IMB_allocImBuf(UNPACK2(data.domain().data_size), ImBufFlags::Zero);
   render_pass->ibuf->color_mode = color_mode;
-  IMB_alloc_float_pixels(render_pass->ibuf, data.channels_count(), false);
-  std::memcpy(
-      render_pass->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  if (data.sharing_info()) {
+    render_pass->ibuf->channels = data.channels_count();
+    render_pass->ibuf->float_buffer = ImBufFloatBuffer{
+        .data = static_cast<const float *>(data.cpu_data().data()),
+        .sharing_info = data.sharing_info(),
+        .colorspace = nullptr};
+  }
+  else if (data.cpu_data().data() != render_pass->ibuf->float_data()) {
+    IMB_alloc_float_pixels(render_pass->ibuf, data.channels_count(), false);
+    std::memcpy(
+        render_pass->ibuf->float_data_for_write(), data.cpu_data().data(), data.size_in_bytes());
+  }
   copy_v2_v2_db(render_pass->ibuf->ppm, render_result_->ppm);
 }
 

source/blender/draw/engines/image/image_buffer_cache.hh
@@ -95,7 +95,8 @@ struct FloatBufferCache {
     ImBuf *new_imbuf = IMB_allocImBuf(image_buffer->x, image_buffer->y, ImBufFlags::Zero);
     new_imbuf->color_mode = image_buffer->color_mode;
 
-    new_imbuf->assign_float_data(IMB_steal_float_buffer(image_buffer));
+    new_imbuf->float_buffer = image_buffer->float_buffer;
+    image_buffer->float_buffer = {};
 
     cache_.append(FloatImageBuffer(image_buffer, new_imbuf));
     return new_imbuf;

source/blender/editors/include/ED_paint.hh
@@ -88,24 +88,24 @@ void ED_image_undo_restore(UndoStep *us);
 /** Export for ED_undo_sys. */
 void ED_image_undosys_type(UndoType *ut);
 
-void *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               unsigned short **r_mask,
-                               bool validate);
-void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               unsigned short **r_mask,
-                               bool **r_valid,
-                               bool use_thread_lock,
-                               bool find_prev);
+const ImBuf *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      unsigned short **r_mask,
+                                      bool validate);
+const ImBuf *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      unsigned short **r_mask,
+                                      bool **r_valid,
+                                      bool use_thread_lock,
+                                      bool find_prev);
 void ED_image_paint_tile_lock_init();
 void ED_image_paint_tile_lock_end();
 

source/blender/editors/interface/interface_icons.cc
@@ -911,7 +911,9 @@ static void icon_verify_datatoc(IconImage *iimg)
       IMB_scale(bbuf, iimg->w, iimg->h, IMBScaleFilter::Box, false);
     }
 
-    iimg->rect = IMB_steal_byte_buffer(bbuf);
+    const size_t size_in_bytes = size_t(bbuf->x) * size_t(bbuf->y) * sizeof(uint);
+    iimg->rect = MEM_new_array_uninitialized<uchar>(size_in_bytes, __func__);
+    memcpy(iimg->rect, reinterpret_cast<const uint *>(bbuf->byte_data()), size_in_bytes);
     IMB_freeImBuf(bbuf);
   }
 }
@@ -1368,7 +1370,9 @@ PreviewImage *icon_to_preview(int icon_id)
     if (bbuf) {
       PreviewImage *prv = BKE_previewimg_create();
 
-      prv->rect[0] = reinterpret_cast<uint *>(IMB_steal_byte_buffer(bbuf));
+      const size_t size = size_t(bbuf->x) * size_t(bbuf->y);
+      prv->rect[0] = MEM_new_array_uninitialized<uint>(size, __func__);
+      memcpy(prv->rect[0], reinterpret_cast<const uint *>(bbuf->byte_data()), size * sizeof(uint));
 
       prv->w[0] = bbuf->x;
       prv->h[0] = bbuf->y;

source/blender/editors/sculpt_paint/mesh/paint_image_proj.cc
@@ -218,7 +218,7 @@ struct ProjPaintImage {
   ImBuf *ibuf;
   ImagePaintPartialRedraw *partRedrawRect;
   /** Only used to build undo tiles during painting. */
-  volatile void **undoRect;
+  volatile const void **undoRect;
   /** The mask accumulation must happen on canvas, not on space screen bucket.
    * Here we store the mask rectangle. */
   ushort **maskRect;
@@ -1831,30 +1831,46 @@ static int project_paint_undo_subtiles(const TileInfo *tinf, int tx, int ty)
 
   if (generate_tile) {
     PaintTileMap *undo_tiles = ED_image_paint_tile_map_get();
-    volatile void *undorect;
+    volatile const void *undorect = nullptr;
     if (tinf->masked) {
-      undorect = ED_image_paint_tile_push(undo_tiles,
-                                          pjIma->ima,
-                                          pjIma->ibuf,
-                                          &pjIma->iuser,
-                                          tx,
-                                          ty,
-                                          &pjIma->maskRect[tile_index],
-                                          &pjIma->valid[tile_index],
-                                          true,
-                                          false);
+      if (const ImBuf *ibuf = ED_image_paint_tile_push(undo_tiles,
+                                                       pjIma->ima,
+                                                       pjIma->ibuf,
+                                                       &pjIma->iuser,
+                                                       tx,
+                                                       ty,
+                                                       &pjIma->maskRect[tile_index],
+                                                       &pjIma->valid[tile_index],
+                                                       true,
+                                                       false))
+      {
+        if (ibuf->float_data()) {
+          undorect = ibuf->float_data();
+        }
+        else {
+          undorect = ibuf->byte_data();
+        }
+      }
     }
     else {
-      undorect = ED_image_paint_tile_push(undo_tiles,
-                                          pjIma->ima,
-                                          pjIma->ibuf,
-                                          &pjIma->iuser,
-                                          tx,
-                                          ty,
-                                          nullptr,
-                                          &pjIma->valid[tile_index],
-                                          true,
-                                          false);
+      if (const ImBuf *ibuf = ED_image_paint_tile_push(undo_tiles,
+                                                       pjIma->ima,
+                                                       pjIma->ibuf,
+                                                       &pjIma->iuser,
+                                                       tx,
+                                                       ty,
+                                                       nullptr,
+                                                       &pjIma->valid[tile_index],
+                                                       true,
+                                                       false))
+      {
+        if (ibuf->float_data()) {
+          undorect = ibuf->float_data();
+        }
+        else {
+          undorect = ibuf->byte_data();
+        }
+      }
     }
 
     BKE_image_mark_dirty(pjIma->ima, pjIma->ibuf);
@@ -4371,7 +4387,7 @@ static void project_paint_build_proj_ima(ProjPaintState *ps,
     projIma->partRedrawRect = static_cast<ImagePaintPartialRedraw *>(
         BLI_memarena_alloc(arena, sizeof(ImagePaintPartialRedraw) * PROJ_BOUNDBOX_SQUARED));
     partial_redraw_array_init(projIma->partRedrawRect);
-    projIma->undoRect = static_cast<volatile void **>(BLI_memarena_alloc(arena, size));
+    projIma->undoRect = static_cast<volatile const void **>(BLI_memarena_alloc(arena, size));
     memset(static_cast<void *>(projIma->undoRect), 0, size);
     projIma->maskRect = static_cast<ushort **>(BLI_memarena_alloc(arena, size));
     memset(projIma->maskRect, 0, size);

source/blender/editors/space_image/image_undo.cc
@@ -125,11 +125,7 @@ struct PaintTile {
    * For 3D projection painting this only uses a tile & frame number.
    * The scene pointer must be cleared (or temporarily set it as needed, but leave cleared). */
   ImageUser iuser;
-  union {
-    float *fp = nullptr;
-    uint8_t *byte_ptr;
-    void *pt;
-  } rect;
+  ImBuf *ptile_ibuf = nullptr;
   uint16_t *mask = nullptr;
   bool valid = false;
   bool use_float = false;
@@ -138,9 +134,7 @@ struct PaintTile {
 
 static void ptile_free(PaintTile *ptile)
 {
-  if (ptile->rect.pt) {
-    MEM_delete_void(ptile->rect.pt);
-  }
+  IMB_freeImBuf(ptile->ptile_ibuf);
   if (ptile->mask) {
     MEM_delete(ptile->mask);
   }
@@ -165,14 +159,14 @@ static void ptile_invalidate_map(PaintTileMap *paint_tile_map)
   }
 }
 
-void *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               ushort **r_mask,
-                               bool validate)
+const ImBuf *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      ushort **r_mask,
+                                      bool validate)
 {
   PaintTileKey key;
   key.ibuf = ibuf;
@@ -196,19 +190,19 @@ void *ED_image_paint_tile_find(PaintTileMap *paint_tile_map,
   if (validate) {
     ptile->valid = true;
   }
-  return ptile->rect.pt;
+  return ptile->ptile_ibuf;
 }
 
-void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
-                               Image *image,
-                               ImBuf *ibuf,
-                               ImageUser *iuser,
-                               int x_tile,
-                               int y_tile,
-                               ushort **r_mask,
-                               bool **r_valid,
-                               bool use_thread_lock,
-                               bool find_prev)
+const ImBuf *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
+                                      Image *image,
+                                      ImBuf *ibuf,
+                                      ImageUser *iuser,
+                                      int x_tile,
+                                      int y_tile,
+                                      ushort **r_mask,
+                                      bool **r_valid,
+                                      bool use_thread_lock,
+                                      bool find_prev)
 {
   if (use_thread_lock) {
     BLI_spin_lock(&paint_tiles_lock);
@@ -219,7 +213,7 @@ void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
 
   /* in projective painting we keep accounting of tiles, so if we need one pushed, just push! */
   if (find_prev) {
-    void *data = ED_image_paint_tile_find(
+    const ImBuf *data = ED_image_paint_tile_find(
         paint_tile_map, image, ibuf, iuser, x_tile, y_tile, r_mask, true);
     if (data) {
       if (use_thread_lock) {
@@ -250,9 +244,9 @@ void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
   calc_tile_rect(*ibuf, ptile->x_tile, ptile->y_tile, tile_pos, tile_copy_size);
 
   if (ibuf->float_data()) {
-    ptile->rect.pt = MEM_new_array_zeroed<float[4]>(square_i(ED_IMAGE_UNDO_TILE_SIZE),
-                                                    "PaintTile.rect");
-    IMB_copy_rect(ptile->rect.fp,
+    ptile->ptile_ibuf = IMB_allocImBuf(
+        ED_IMAGE_UNDO_TILE_SIZE, ED_IMAGE_UNDO_TILE_SIZE, ImBufFlags::FloatData);
+    IMB_copy_rect(ptile->ptile_ibuf->float_data_for_write(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   ibuf->float_data(),
                   int2(ibuf->x, ibuf->y),
@@ -262,9 +256,9 @@ void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
                   tile_copy_size);
   }
   else {
-    ptile->rect.pt = MEM_new_array_zeroed<char[4]>(square_i(ED_IMAGE_UNDO_TILE_SIZE),
-                                                   "PaintTile.rect");
-    IMB_copy_rect(ptile->rect.byte_ptr,
+    ptile->ptile_ibuf = IMB_allocImBuf(
+        ED_IMAGE_UNDO_TILE_SIZE, ED_IMAGE_UNDO_TILE_SIZE, ImBufFlags::ByteData);
+    IMB_copy_rect(ptile->ptile_ibuf->byte_data_for_write(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   ibuf->byte_data(),
                   int2(ibuf->x, ibuf->y),
@@ -299,7 +293,7 @@ void *ED_image_paint_tile_push(PaintTileMap *paint_tile_map,
   if (use_thread_lock) {
     BLI_spin_unlock(&paint_tiles_lock);
   }
-  return ptile->rect.pt;
+  return ptile->ptile_ibuf;
 }
 
 static void ptile_restore_runtime_map(PaintTileMap *paint_tile_map)
@@ -315,7 +309,7 @@ static void ptile_restore_runtime_map(PaintTileMap *paint_tile_map)
     if (ibuf->float_data()) {
       IMB_copy_rect(ibuf->float_data_for_write(),
                     int2(ibuf->x, ibuf->y),
-                    ptile->rect.fp,
+                    ptile->ptile_ibuf->float_data(),
                     int2(ED_IMAGE_UNDO_TILE_SIZE),
                     ibuf->channels,
                     int2(0, 0),
@@ -325,7 +319,7 @@ static void ptile_restore_runtime_map(PaintTileMap *paint_tile_map)
     else {
       IMB_copy_rect(ibuf->byte_data_for_write(),
                     int2(ibuf->x, ibuf->y),
-                    ptile->rect.byte_ptr,
+                    ptile->ptile_ibuf->byte_data(),
                     int2(ED_IMAGE_UNDO_TILE_SIZE),
                     int2(0, 0),
                     tile_pos,
@@ -357,25 +351,16 @@ static uint32_t index_from_xy(uint32_t tile_x, uint32_t tile_y, const uint32_t t
 }
 
 struct UndoImageTile {
-  union {
-    float *fp;
-    uint8_t *byte_ptr;
-    void *pt;
-  } rect;
+  ImBuf *ibuf;
   int users;
 };
 
 static UndoImageTile *utile_alloc(bool has_float)
 {
   UndoImageTile *utile = MEM_new_zeroed<UndoImageTile>("ImageUndoTile");
-  if (has_float) {
-    utile->rect.fp = MEM_new_array_uninitialized<float>(4 * square_i(ED_IMAGE_UNDO_TILE_SIZE),
-                                                        __func__);
-  }
-  else {
-    utile->rect.byte_ptr = MEM_new_array_uninitialized<uint8_t>(
-        4 * square_i(ED_IMAGE_UNDO_TILE_SIZE), __func__);
-  }
+  utile->ibuf = IMB_allocImBuf(ED_IMAGE_UNDO_TILE_SIZE,
+                               ED_IMAGE_UNDO_TILE_SIZE,
+                               has_float ? ImBufFlags::FloatData : ImBufFlags::ByteData);
   return utile;
 }
 
@@ -388,7 +373,7 @@ static void utile_init_from_imbuf(UndoImageTile *utile,
   int2 tile_copy_size;
   calc_tile_rect(*ibuf, x_tile, y_tile, tile_pos, tile_copy_size);
   if (ibuf->float_data()) {
-    IMB_copy_rect(utile->rect.fp,
+    IMB_copy_rect(utile->ibuf->float_data_for_write(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   ibuf->float_data(),
                   int2(ibuf->x, ibuf->y),
@@ -398,7 +383,7 @@ static void utile_init_from_imbuf(UndoImageTile *utile,
                   tile_copy_size);
   }
   else {
-    IMB_copy_rect(utile->rect.byte_ptr,
+    IMB_copy_rect(utile->ibuf->byte_data_for_write(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   ibuf->byte_data(),
                   int2(ibuf->x, ibuf->y),
@@ -419,7 +404,7 @@ static void utile_restore(const UndoImageTile *utile,
   if (ibuf->float_data()) {
     IMB_copy_rect(ibuf->float_data_for_write(),
                   int2(ibuf->x, ibuf->y),
-                  utile->rect.fp,
+                  utile->ibuf->float_data(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   ibuf->channels,
                   int2(0, 0),
@@ -429,7 +414,7 @@ static void utile_restore(const UndoImageTile *utile,
   else {
     IMB_copy_rect(ibuf->byte_data_for_write(),
                   int2(ibuf->x, ibuf->y),
-                  utile->rect.byte_ptr,
+                  utile->ibuf->byte_data(),
                   int2(ED_IMAGE_UNDO_TILE_SIZE),
                   int2(0, 0),
                   tile_pos,
@@ -442,7 +427,7 @@ static void utile_decref(UndoImageTile *utile)
   utile->users -= 1;
   BLI_assert(utile->users >= 0);
   if (utile->users == 0) {
-    MEM_delete_void(utile->rect.pt);
+    IMB_freeImBuf(utile->ibuf);
     MEM_delete(utile);
   }
 }
@@ -810,8 +795,7 @@ static bool image_undosys_step_encode(bContext *C, Main * /*bmain*/, UndoStep *u
 
         UndoImageTile *utile = MEM_new_zeroed<UndoImageTile>("UndoImageTile");
         utile->users = 1;
-        utile->rect.pt = ptile->rect.pt;
-        ptile->rect.pt = nullptr;
+        utile->ibuf = IMB_dupImBuf(ptile->ptile_ibuf);
         const uint tile_index = index_from_xy(ptile->x_tile, ptile->y_tile, ubuf_pre->tiles_dims);
 
         BLI_assert(ubuf_pre->tiles[tile_index] == nullptr);

FrenkyBojler/blender-ios

commit 0938a3078059eb2d06a767b67638dc61250c31a4

Fix #159873: Potential crash in 2D image painting
Introduced with 3bd35d9a4b22493519079d808bf70eecaf523fdc

source/blender/editors/sculpt_paint/mesh/paint_image_2d.cc
@@ -1258,13 +1258,15 @@ static void paint_2d_do_making_brush(ImagePaintState *s,
       int origx = region->destx - tx * ED_IMAGE_UNDO_TILE_SIZE;
       int origy = region->desty - ty * ED_IMAGE_UNDO_TILE_SIZE;
 
-      const ImBuf *data = ED_image_paint_tile_find(
-          undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false);
-      if (tile->canvas->float_data()) {
-        tmpbuf.float_buffer = data->float_buffer;
-      }
-      else {
-        tmpbuf.byte_buffer = data->byte_buffer;
+      if (const ImBuf *data = ED_image_paint_tile_find(
+              undo_tiles, s->image, tile->canvas, &tile->iuser, tx, ty, &mask, false))
+      {
+        if (tile->canvas->float_data()) {
+          tmpbuf.float_buffer = data->float_buffer;
+        }
+        else {
+          tmpbuf.byte_buffer = data->byte_buffer;
+        }
       }
 
       IMB_rectblend(tile->canvas,