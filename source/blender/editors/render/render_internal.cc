blender/blender

commit c065cc46602ad3d2dbe13814e1ef27cc8f8b5142

Fix #158318: Ensure `render_stats` app callback is always executed
`render_stats` python app callbacks are executed by
`BKE_callback_exec_string` which currently is only called when blender
is running in headless mode. This is sometimes not desired because it
prevents scripts from getting the status update while blender is running
with normal GUI. This fix ensures status callbacks are always called
regardless of how the blender is started.

Pull Request: https://projects.blender.org/blender/blender/pulls/158365

source/blender/render/intern/pipeline.cc
@@ -184,14 +184,13 @@ static bool do_write_image_or_movie(Render *re,
 /* default callbacks, set in each new render */
 static void result_rcti_nothing(void * /*arg*/, RenderResult * /*rr*/, rcti * /*rect*/) {}
 static void current_scene_nothing(void * /*arg*/, Scene * /*scene*/) {}
-static void stats_nothing(void * /*arg*/, RenderStats * /*rs*/) {}
 static void float_nothing(void * /*arg*/, float /*val*/) {}
 static bool default_break(void * /*arg*/)
 {
   return G.is_break == true;
 }
 
-static void stats_background(void * /*arg*/, RenderStats *rs)
+static void stats_update(void * /*arg*/, RenderStats *rs)
 {
   if (rs->infostr == nullptr) {
     return;
@@ -202,7 +201,9 @@ static void stats_background(void * /*arg*/, RenderStats *rs)
   static Mutex mutex;
   std::scoped_lock lock(mutex);
 
-  const bool show_info = CLOG_CHECK(&LOG, CLG_LEVEL_INFO);
+  /* Only print render process to stdout when blender is running headless, because the same
+   * progress string will be displayed on the UI. */
+  const bool show_info = G.background && CLOG_CHECK(&LOG, CLG_LEVEL_INFO);
   if (show_info) {
     CLOG_INFO(&LOG, "Fra: %d | %s", rs->cfra, rs->infostr);
     /* Flush stdout to be sure python callbacks are printing stuff after blender. */
@@ -942,12 +943,7 @@ void RE_display_init(Render *re)
   re->display->current_scene_update_cb = current_scene_nothing;
   re->display->progress_cb = float_nothing;
   re->display->test_break_cb = default_break;
-  if (G.background) {
-    re->display->stats_draw_cb = stats_background;
-  }
-  else {
-    re->display->stats_draw_cb = stats_nothing;
-  }
+  re->display->stats_draw_cb = stats_update;
 }
 
 void RE_display_ensure_gpu_context(Render *re)

FrenkyBojler/blender-ios

commit a440151a8d05849e83444933f5e7766daf3c8616

Fix: Python render_stats handler used for preview renders
Making it working not only in background mode is useful, but for the
typical uses cases having it run for every preview render gets in the way.

Additionally it was missing from image_renderinfo_cb for interactive
renders.

This issue was introduced in c065cc4660.

source/blender/editors/render/render_internal.cc
@@ -27,6 +27,7 @@
 #include "DNA_userdef_types.h"
 #include "DNA_view3d_types.h"
 
+#include "BKE_callbacks.hh"
 #include "BKE_colortools.hh"
 #include "BKE_compositor.hh"
 #include "BKE_context.hh"
@@ -527,6 +528,8 @@ static void image_renderinfo_cb(void *rjv, RenderStats *rs)
   }
 
   RE_ReleaseResult(rj->re);
+
+  BKE_callback_exec_string(G_MAIN, rs->infostr, BKE_CB_EVT_RENDER_STATS);
 
   /* make jobs timer to send notifier */
   *(rj->do_update) = true;

source/blender/render/intern/pipeline.cc
@@ -190,11 +190,13 @@ static bool default_break(void * /*arg*/)
   return G.is_break == true;
 }
 
-static void stats_update(void * /*arg*/, RenderStats *rs)
+static void stats_update(void *arg, RenderStats *rs)
 {
   if (rs->infostr == nullptr) {
     return;
   }
+
+  Render *re = static_cast<Render *>(arg);
 
   /* Compositor calls this from multiple threads, mutex lock to ensure we don't
    * get garbled output. */
@@ -212,7 +214,7 @@ static void stats_update(void * /*arg*/, RenderStats *rs)
 
   /* NOTE: using G_MAIN seems valid here???
    * Not sure it's actually even used anyway, we could as well pass nullptr? */
-  BKE_callback_exec_string(G_MAIN, rs->infostr, BKE_CB_EVT_RENDER_STATS);
+  render_callback_exec_string(re, G_MAIN, BKE_CB_EVT_RENDER_STATS, rs->infostr);
 
   if (show_info) {
     fflush(stdout);
@@ -944,6 +946,7 @@ void RE_display_init(Render *re)
   re->display->progress_cb = float_nothing;
   re->display->test_break_cb = default_break;
   re->display->stats_draw_cb = stats_update;
+  re->display->sdh = re;
 }
 
 void RE_display_ensure_gpu_context(Render *re)