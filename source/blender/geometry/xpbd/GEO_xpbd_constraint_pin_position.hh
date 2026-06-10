10ff310b8c995330d5ffb1ce165e4854cd6023da
Jacques Lucke <jacques@blender.org>
2026-06-10 12:06:37 +0200

    support pin position warm starting

--- source/blender/geometry/xpbd/GEO_xpbd_constraint_pin_position.hh
+++ source/blender/geometry/xpbd/GEO_xpbd_constraint_pin_position.hh
@@ -63,6 +63,24 @@ class PinPositionConstraintSet : public TemplatedConstraintSet<PinPositionConstr
     updater.add_residual_error(geo_i, result.residual_error_squared * error_scale_);
   }
 
+  template<typename UpdaterT>
+  void warm_start(const ConstraintSetParams &params,
+                  UpdaterT &updater,
+                  const int constraint_i) const
+  {
+    const int geo_i = affected_geo_indices_[0];
+    const int point_i = point_indices_[constraint_i];
+    lambdas_[constraint_i] *= params.warm_start_lambda_factor;
+    const DistanceConstraintWarmStartResult result = evaluate_distance_constraint_warm_start(
+        params.position(geo_i, point_i),
+        pin_positions_[constraint_i],
+        params.inv_mass(geo_i, point_i),
+        0.0f,
+        lambdas_[constraint_i]);
+    updater.update_position(geo_i, point_i, result.offset0);
+    updater.update_position(geo_i, point_i, result.offset1);
+  }
+
   ConstraintColoring color_constraints(LinearAllocator<> &memory) const override
   {
     return color_constraints__unary(point_indices_, memory);
