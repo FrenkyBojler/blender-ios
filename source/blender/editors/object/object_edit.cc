zNightlord/Bforartists

commit 1561c1ea4ab652010e2cbff4976f4444cd5a5b7c

Geometry Nodes: Runtime RNA for node tree interface
This PR implements the changes described in #132129. The main goal is to
use actual RNA types to store node group inputs on the Geometry Nodes
modifier and node tools, rather than using custom properties. This makes
the API much friendlier, as well as resolving long-standing technical
debt.

The API change looks like this:
```python
# Before
modifier["identifier"] = 5.0
modifier["identifier_use_attribute"] = True
modifier["identifier_attribute_name"] = "some_input_attribute"
modifier["identifier_attribute_name"] = "some_output_attribute"
# After
modifier.inputs.identifier.value = 5.0
modifier.inputs.identifier.type = "ATTRIBUTE"
modifier.inputs.identifier.attribute_name = "some_input_attribute"
modifier.outputs.identifier.attribute_name = "some_output_attribute"
```

The `identifier` is the identifier of the interface socket (e.g.
`Socket_2` currently). This PR does not allow changing this property
name, that can be done as separately later.

Another benefit of this change is that combined with the work from
#148763, now the node tool inputs are remembered between invocations
like properties from any other operator. To get node tool registration
working, we have to include all the information necessary for the RNA
property registration in the asset meta-data.

Backward and forward compatibility are supported (though animation paths
are only backward compatible), but this does break the current Python API
for accessing Geometry Nodes modifier properties.

Co-authored-by: Hans Goudey <hans@blender.org>
Pull Request: https://projects.blender.org/blender/blender/pulls/138117

source/blender/editors/object/object_edit.cc
@@ -1973,14 +1973,11 @@ static wmOperatorStatus shade_auto_smooth_exec(bContext *C, wmOperator *op)
         BKE_modifier_unique_name(&object->modifiers, &smooth_by_angle_nmd->modifier);
       }
 
-      IDProperty *angle_prop = IDP_GetPropertyFromGroup(smooth_by_angle_nmd->settings.properties,
-                                                        angle_identifier.c_str());
-      if (angle_prop->type == IDP_FLOAT) {
-        IDP_float_set(angle_prop, angle);
-      }
-      else if (angle_prop->type == IDP_DOUBLE) {
-        IDP_double_set(angle_prop, angle);
-      }
+      PointerRNA nmd_ptr = RNA_pointer_create_discrete(
+          &object->id, RNA_NodesModifier, smooth_by_angle_nmd);
+      PointerRNA properties_ptr = RNA_pointer_get(&nmd_ptr, "properties");
+
+      RNA_float_set(&properties_ptr, angle_identifier.c_str(), angle);
 
       DEG_id_tag_update(&object->id, ID_RECALC_GEOMETRY);
       WM_event_add_notifier(C, NC_OBJECT | ND_MODIFIER, object);

source/blender/blenkernel/BKE_idprop.hh
@@ -9,6 +9,7 @@
  */
 
 #include <memory>
+#include <optional>
 
 #include "DNA_ID.h"
 #include "DNA_ID_enums.h"
@@ -375,6 +376,14 @@ const IDProperty *_IDP_assert_type_mask(const IDProperty *prop, int ty_mask);
 /* No `IDP_string_set` needed. */
 #define IDP_ID_get(prop) ((void)0, ((ID *)_IDP_assert_type(prop, IDP_ID)->data.pointer))
 /* No `IDP_ID_set` needed. */
+
+std::optional<StringRefNull> IDP_group_lookup_string(const IDProperty &group, StringRef name);
+std::optional<float> IDP_group_lookup_float(const IDProperty &group, StringRef name);
+std::optional<int> IDP_group_lookup_int(const IDProperty &group, StringRef name);
+std::optional<bool> IDP_group_lookup_bool(const IDProperty &group, StringRef name);
+std::optional<Span<float>> IDP_group_lookup_float_array(const IDProperty &group,
+                                                        StringRef name,
+                                                        int required_size);
 
 /**
  * Return an int from an #IDProperty with a compatible type. This should be avoided, but

source/blender/blenkernel/BKE_idprop_hash.hh
@@ -1,0 +1,26 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/** \file
+ * \ingroup bke
+ */
+
+#include <xxhash.h>
+
+namespace blender {
+
+struct IDProperty;
+
+namespace bke::idprop {
+
+/**
+ * Hash the value of the given IDProperty recursively. This only hashes the actual values, not e.g.
+ * the ui data.
+ */
+void hash(const IDProperty &base_prop, XXH3_state_t *hash_state);
+
+}  // namespace bke::idprop
+}  // namespace blender

source/blender/blenkernel/BKE_node.hh
@@ -58,6 +58,7 @@ struct bNodeTreeExec;
 
 class CPPType;
 namespace nodes {
+struct GeneratedTreeSrnaData;
 class NodeMultiFunctionBuilder;
 class GeoNodeExecParams;
 class NodeDeclaration;
@@ -149,6 +150,12 @@ using NodeBlendWriteFunction = void (*)(const bNodeTree &tree,
                                         BlendWriter &writer);
 using NodeBlendDataReadFunction = void (*)(bNodeTree &tree, bNode &node, BlendDataReader &reader);
 
+using SocketMakeGeometryNodesInputSrnaFunction =
+    void (*)(const bNodeTree &tree,
+             StructRNA &srna,
+             const bNodeTreeInterfaceSocket &io_socket,
+             nodes::GeneratedTreeSrnaData &r_generated);
+
 /**
  * \brief Defines a socket type.
  *
@@ -208,6 +215,8 @@ struct bNodeSocketType {
   SocketGetGeometryNodesCPPValueFunction get_geometry_nodes_cpp_value = nullptr;
   /* Default value for this socket type. */
   const SocketValueVariant *geometry_nodes_default_value = nullptr;
+
+  SocketMakeGeometryNodesInputSrnaFunction make_geometry_nodes_input_srna = nullptr;
 };
 
 using NodeInitExecFunction = void *(*)(bNodeExecContext * context,
@@ -928,6 +937,7 @@ void node_tree_free_local_node(bNodeTree &ntree, bNode &node);
 void node_tree_update_all_new(Main &main);
 
 /** Update asset meta-data cache of data-block properties. */
+IDProperty *node_create_asset_meta_data_properties(const bNodeTree &node_tree);
 void node_update_asset_metadata(bNodeTree &node_tree);
 
 void node_tree_node_flag_set(bNodeTree &ntree, int flag, bool enable);

source/blender/blenkernel/BKE_node_runtime.hh
@@ -36,6 +36,7 @@ struct bNodeTree;
 namespace nodes {
 struct FieldInferencingInterface;
 struct EvalDependencies;
+struct GeneratedTreeSrnaData;
 class NodeDeclaration;
 struct GeometryNodesLazyFunctionGraphInfo;
 struct StructureTypeInterface;
@@ -159,6 +160,9 @@ class bNodeTreeRuntime : NonCopyable, NonMovable {
    * tree execution instructions and its evaluation.
    */
   bNodeTreeExec *execdata = nullptr;
+
+  /** Contains RNA types generated for the geometry nodes modifier interface. */
+  std::shared_ptr<nodes::GeneratedTreeSrnaData> geometry_nodes_srna_data;
 
   /** Information about how inputs and outputs of the node group interact with fields. */
   std::unique_ptr<nodes::FieldInferencingInterface> field_inferencing_interface;

source/blender/blenkernel/CMakeLists.txt
@@ -417,6 +417,7 @@ set(SRC
   BKE_icons.hh
   BKE_id_hash.hh
   BKE_idprop.hh
+  BKE_idprop_hash.hh
   BKE_idtype.hh
   BKE_image.hh
   BKE_image_format.hh

source/blender/blenkernel/intern/idprop.cc
@@ -13,6 +13,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <xxhash.h>
+
 #include <fmt/format.h>
 
 #include "BLI_listbase.h"
@@ -22,6 +24,7 @@
 #include "BLI_utildefines.h"
 
 #include "BKE_idprop.hh"
+#include "BKE_idprop_hash.hh"
 #include "BKE_lib_id.hh"
 
 #include "CLG_log.h"
@@ -791,6 +794,56 @@ static void IDP_FreeGroup(IDProperty *prop, const bool do_id_user)
     IDP_FreePropertyContent_ex(&loop, do_id_user);
   }
   BLI_freelistN(&prop->data.group);
+}
+
+std::optional<StringRefNull> IDP_group_lookup_string(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_STRING) {
+    return std::nullopt;
+  }
+  return IDP_string_get(prop);
+}
+
+std::optional<float> IDP_group_lookup_float(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_FLOAT) {
+    return std::nullopt;
+  }
+  return IDP_float_get(prop);
+}
+
+std::optional<int> IDP_group_lookup_int(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_INT) {
+    return std::nullopt;
+  }
+  return IDP_int_get(prop);
+}
+
+std::optional<bool> IDP_group_lookup_bool(const IDProperty &group, StringRef name)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_BOOLEAN) {
+    return std::nullopt;
+  }
+  return IDP_bool_get(prop);
+}
+
+std::optional<Span<float>> IDP_group_lookup_float_array(const IDProperty &group,
+                                                        StringRef name,
+                                                        int required_size)
+{
+  const IDProperty *prop = IDP_GetPropertyFromGroup(&group, name);
+  if (!prop || prop->type != IDP_FLOAT) {
+    return std::nullopt;
+  }
+  if (prop->len != required_size) {
+    return std::nullopt;
+  }
+  return Span(IDP_array_float_get(prop), prop->len);
 }
 
 /** \} */
@@ -1984,6 +2037,59 @@ IDPropertyUIData *IDP_TryConvertUIData(IDPropertyUIData *src,
   ui_data_free(src, src_type);
   return nullptr;
 }
+
+namespace bke::idprop {
+
+void hash(const IDProperty &base_prop, XXH3_state_t *hash_state)
+{
+  IDP_foreach_property(&const_cast<IDProperty &>(base_prop), 0, [&](const IDProperty *prop) {
+    XXH3_64bits_update(hash_state, prop->name, strlen(prop->name));
+    XXH3_64bits_update(hash_state, &prop->type, sizeof(prop->type));
+
+    switch (eIDPropertyType(prop->type)) {
+      case IDP_INT:
+      case IDP_FLOAT:
+      case IDP_BOOLEAN: {
+        XXH3_64bits_update(hash_state, &prop->data.val, sizeof(prop->data.val));
+        break;
+      }
+      case IDP_DOUBLE: {
+        const double val = IDP_double_get(prop);
+        XXH3_64bits_update(hash_state, &val, sizeof(double));
+        break;
+      }
+      case IDP_STRING: {
+        if (prop->data.pointer) {
+          XXH3_64bits_update(hash_state, prop->data.pointer, size_t(prop->len));
+        }
+        break;
+      }
+      case IDP_ARRAY: {
+        if (prop->data.pointer) {
+          XXH3_64bits_update(hash_state, &prop->subtype, sizeof(prop->subtype));
+          const size_t elem_size = idp_size_table[int(prop->subtype)];
+          XXH3_64bits_update(hash_state, prop->data.pointer, elem_size * size_t(prop->len));
+        }
+        break;
+      }
+      case IDP_ID: {
+        /* NOTE: Hashing the session_uid of the ID makes the hash session-specific. An alternative
+         * hash wouldn't be complete though, because data-block names aren't necessarily unique. */
+        if (const ID *id = static_cast<const ID *>(prop->data.pointer)) {
+          XXH3_64bits_update(hash_state, &id->session_uid, sizeof(ID::session_uid));
+        }
+        break;
+      }
+      case IDP_GROUP:
+      case IDP_IDPARRAY: {
+        /* Handled by the recursion in #IDP_foreach_property. */
+        break;
+      }
+    }
+  });
+}
+
+}  // namespace bke::idprop
 
 /** \} */
 

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -50,6 +50,8 @@
 #include "BKE_node_runtime.hh"
 #include "BKE_node_tree_update.hh"
 
+#include "NOD_geometry_nodes_srna.hh"
+
 #include "BLT_translation.hh"
 
 namespace blender {
@@ -2265,15 +2267,22 @@ static ModifierData *create_auto_smooth_modifier(
   md->node_group = get_node_group(object.id.lib);
   id_us_plus(&md->node_group->id);
 
-  md->settings.properties = idprop::create_group("Nodes Modifier Settings").release();
-  IDProperty *angle_prop = idprop::create("Socket_2", angle).release();
-  auto *ui_data = reinterpret_cast<IDPropertyUIDataFloat *>(IDP_ui_data_ensure(angle_prop));
-  ui_data->base.rna_subtype = PROP_ANGLE;
-  ui_data->soft_min = 0.0f;
-  ui_data->soft_max = DEG2RADF(180.0f);
-  IDP_AddToGroup(md->settings.properties, angle_prop);
-  IDP_AddToGroup(md->settings.properties, idprop::create("Socket_2_use_attribute", 0).release());
-  IDP_AddToGroup(md->settings.properties, idprop::create("Socket_2_attribute_name", "").release());
+  IDProperty *system_props = bke::idprop::create_group("NodesModifierProperties").release();
+
+  IDProperty *inputs = bke::idprop::create_group("inputs").release();
+  IDP_AddToGroup(system_props, inputs);
+
+  IDProperty *angle_prop_group = bke::idprop::create_group("Socket_2").release();
+  IDP_AddToGroup(inputs, angle_prop_group);
+
+  IDProperty *type_prop =
+      idprop::create("type", int(nodes::GeometryNodesInputType::Value)).release();
+  IDP_AddToGroup(angle_prop_group, type_prop);
+
+  IDProperty *angle_prop = idprop::create("value", angle).release();
+  IDP_AddToGroup(angle_prop_group, angle_prop);
+
+  md->modifier.system_properties = system_props;
 
   BKE_modifiers_persistent_uid_init(object, md->modifier);
   return &md->modifier;

FrenkyBojler/blender-ios

commit 01188ad6397a6572076bb99680dfb99169a49676

Fix #159881: Auto Smooth operator redo UI broken
Caused by 1561c1ea4ab6

To resolve, get the actual RNA pointer for the input (via `properties`
and `inputs`) to `RNA_float_set` the "value" on.

source/blender/editors/object/object_edit.cc
@@ -1929,8 +1929,10 @@ static wmOperatorStatus shade_auto_smooth_exec(bContext *C, wmOperator *op)
       PointerRNA nmd_ptr = RNA_pointer_create_discrete(
           &object->id, RNA_NodesModifier, smooth_by_angle_nmd);
       PointerRNA properties_ptr = RNA_pointer_get(&nmd_ptr, "properties");
+      PointerRNA inputs_ptr = RNA_pointer_get(&properties_ptr, "inputs");
+      PointerRNA input_ptr = RNA_pointer_get(&inputs_ptr, angle_identifier.c_str());
 
-      RNA_float_set(&properties_ptr, angle_identifier.c_str(), angle);
+      RNA_float_set(&input_ptr, "value", angle);
 
       DEG_id_tag_update(&object->id, ID_RECALC_GEOMETRY);
       WM_event_add_notifier(C, NC_OBJECT | ND_MODIFIER, object);