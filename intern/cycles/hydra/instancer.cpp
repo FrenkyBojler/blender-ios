blender/blender

commit 51977de03a351c517e38a34951716f751205e116

Cycles: Hydra: Change render delegate to use Hydra 2.0 API
The 1.0 API is deprecated (though not yet removed) in OpenUSD 26.xx
releases. So it's time to migrate.

Use of the HdSceneDelegate is mostly removed, except for retrieving the
prims. Specific properties of prims are retrieved through schemas rather
than querying the scene delegate.

The full replacement of HdSceneDelegate is still ongoing in USD itself.
In the very latest 26.05 only HdPrman has an experimental option for
using the scene index directly, but for example HdStorm does not use it
yet. So removing it's use with the current 25.08 in Blender is not
expected to be stable enough.

Additionally HdRenderer is supposed to replace HdRenderDelegate and its
handling of tasks. However even in 26.05 that is only a stub
implementation so there is no actual alternative to migrate to.

Note that OpenUSD provides a compatibility layer so 1.0 and 2.0 hosts and
render delegates can work together. Which means ideally everything keeps
working the same, but there are likely some differences anyway. Support for
OpenUSD version 23.xx and older was dropped.

Pull Request: https://projects.blender.org/blender/blender/pulls/158456

intern/cycles/hydra/instancer.cpp
@@ -4,32 +4,27 @@
  * SPDX-License-Identifier: Apache-2.0 */
 
 #include "hydra/instancer.h"
+#include "hydra/util.h"
 
 #include <pxr/base/gf/quatd.h>
+#include <pxr/base/gf/quatf.h>
+#include <pxr/base/gf/quath.h>
+#include <pxr/imaging/hd/instancedBySchema.h>
+#include <pxr/imaging/hd/instancerTopologySchema.h>
+#include <pxr/imaging/hd/primvarSchema.h>
+#include <pxr/imaging/hd/primvarsSchema.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
+#include <pxr/imaging/hd/xformSchema.h>
 
 HDCYCLES_NAMESPACE_OPEN_SCOPE
 
-HdCyclesInstancer::HdCyclesInstancer(HdSceneDelegate *delegate,
-                                     const SdfPath &instancerId
-#if PXR_VERSION <= 2011
-                                     ,
-                                     const SdfPath &parentId
-#endif
-                                     )
-    : HdInstancer(delegate,
-                  instancerId
-#if PXR_VERSION <= 2011
-                  ,
-                  parentId
-#endif
-      )
+HdCyclesInstancer::HdCyclesInstancer(HdSceneDelegate *delegate, const SdfPath &instancerId)
+    : HdInstancer(delegate, instancerId)
 {
 }
 
-HdCyclesInstancer::~HdCyclesInstancer() {}
+HdCyclesInstancer::~HdCyclesInstancer() = default;
 
-#if PXR_VERSION > 2011
 void HdCyclesInstancer::Sync(HdSceneDelegate *sceneDelegate,
                              HdRenderParam * /*renderParam*/,
                              HdDirtyBits *dirtyBits)
@@ -40,66 +35,74 @@ void HdCyclesInstancer::Sync(HdSceneDelegate *sceneDelegate,
     SyncPrimvars();
   }
 }
-#endif
 
 void HdCyclesInstancer::SyncPrimvars()
 {
   HdSceneDelegate *const sceneDelegate = GetDelegate();
-  const HdDirtyBits dirtyBits =
-      sceneDelegate->GetRenderIndex().GetChangeTracker().GetInstancerDirtyBits(GetId());
+  const SdfPath &id = GetId();
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, id);
+  HdPrimvarsSchema primvars = HdPrimvarsSchema::GetFromParent(prim.dataSource);
 
-  for (const HdPrimvarDescriptor &desc :
-       sceneDelegate->GetPrimvarDescriptors(GetId(), HdInterpolationInstance))
-  {
-    if (!HdChangeTracker::IsPrimvarDirty(dirtyBits, GetId(), desc.name)) {
-      continue;
+  auto read_array_primvar = [&](const TfToken &name, auto &out) {
+    using T = std::remove_reference_t<decltype(out)>;
+    if (HdPrimvarSchema pv = primvars.GetPrimvar(name)) {
+      if (auto valueDs = pv.GetPrimvarValue()) {
+        const VtValue v = valueDs->GetValue(0.0f);
+        if (v.IsHolding<T>()) {
+          out = v.UncheckedGet<T>();
+        }
+      }
     }
+  };
 
-    const VtValue value = sceneDelegate->Get(GetId(), desc.name);
-    if (value.IsEmpty()) {
-      continue;
-    }
+  read_array_primvar(HdInstancerTokens->instanceTranslations, _translate);
+  read_array_primvar(HdInstancerTokens->instanceScales, _scale);
+  read_array_primvar(HdInstancerTokens->instanceTransforms, _instanceTransform);
 
-#if PXR_VERSION < 2311
-    if (desc.name == HdInstancerTokens->translate) {
-      _translate = value.Get<VtVec3fArray>();
+  /* Accept different array types for quaternions. */
+  if (HdPrimvarSchema pv = primvars.GetPrimvar(HdInstancerTokens->instanceRotations)) {
+    if (auto valueDs = pv.GetPrimvarValue()) {
+      const VtValue v = valueDs->GetValue(0.0f);
+      if (v.IsHolding<VtVec4fArray>()) {
+        _rotate = v.UncheckedGet<VtVec4fArray>();
+      }
+      else if (v.IsHolding<VtQuatfArray>()) {
+        const VtQuatfArray &src = v.UncheckedGet<VtQuatfArray>();
+        _rotate.reserve(src.size());
+        for (const GfQuatf &q : src) {
+          const GfVec3f &im = q.GetImaginary();
+          _rotate.push_back(GfVec4f(q.GetReal(), im[0], im[1], im[2]));
+        }
+      }
+      else if (v.IsHolding<VtQuathArray>()) {
+        const VtQuathArray &src = v.UncheckedGet<VtQuathArray>();
+        _rotate.reserve(src.size());
+        for (const GfQuath &q : src) {
+          const GfVec3h &im = q.GetImaginary();
+          _rotate.push_back(GfVec4f(q.GetReal(), im[0], im[1], im[2]));
+        }
+      }
     }
-    else if (desc.name == HdInstancerTokens->rotate) {
-      _rotate = value.Get<VtVec4fArray>();
-    }
-    else if (desc.name == HdInstancerTokens->scale) {
-      _scale = value.Get<VtVec3fArray>();
-    }
-    else if (desc.name == HdInstancerTokens->instanceTransform) {
-      _instanceTransform = value.Get<VtMatrix4dArray>();
-    }
-#else
-    if (desc.name == HdInstancerTokens->instanceTranslations) {
-      _translate = value.Get<VtVec3fArray>();
-    }
-    else if (desc.name == HdInstancerTokens->instanceRotations) {
-      _rotate = value.Get<VtVec4fArray>();
-    }
-    else if (desc.name == HdInstancerTokens->instanceScales) {
-      _scale = value.Get<VtVec3fArray>();
-    }
-    else if (desc.name == HdInstancerTokens->instanceTransforms) {
-      _instanceTransform = value.Get<VtMatrix4dArray>();
-    }
-#endif
   }
 
-  sceneDelegate->GetRenderIndex().GetChangeTracker().MarkInstancerClean(GetId());
+  sceneDelegate->GetRenderIndex().GetChangeTracker().MarkInstancerClean(id);
 }
 
 VtMatrix4dArray HdCyclesInstancer::ComputeInstanceTransforms(const SdfPath &prototypeId)
 {
-#if PXR_VERSION <= 2011
-  SyncPrimvars();
-#endif
+  HdSceneDelegate *const sceneDelegate = GetDelegate();
+  const SdfPath &id = GetId();
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, id);
 
-  const VtIntArray instanceIndices = GetDelegate()->GetInstanceIndices(GetId(), prototypeId);
-  const GfMatrix4d instanceTransform = GetDelegate()->GetInstancerTransform(GetId());
+  HdInstancerTopologySchema topology = HdInstancerTopologySchema::GetFromParent(prim.dataSource);
+  const VtIntArray instanceIndices = topology ?
+                                         topology.ComputeInstanceIndicesForProto(prototypeId) :
+                                         VtIntArray();
+
+  GfMatrix4d instanceTransform(1.0);
+  if (auto matrixDs = HdXformSchema::GetFromParent(prim.dataSource).GetMatrix()) {
+    instanceTransform = matrixDs->GetTypedValue(0.0f);
+  }
 
   VtMatrix4dArray transforms;
   transforms.reserve(instanceIndices.size());
@@ -133,22 +136,35 @@ VtMatrix4dArray HdCyclesInstancer::ComputeInstanceTransforms(const SdfPath &prot
     transforms.push_back(transform);
   }
 
+  /* Recurse into the parent instancer. */
   VtMatrix4dArray resultTransforms;
 
-  if (auto *const instancer = static_cast<HdCyclesInstancer *>(
-          GetDelegate()->GetRenderIndex().GetInstancer(GetParentId())))
-  {
-    for (const GfMatrix4d &parentTransform : instancer->ComputeInstanceTransforms(GetId())) {
-      for (const GfMatrix4d &localTransform : transforms) {
-        resultTransforms.push_back(parentTransform * localTransform);
-      }
+  HdInstancedBySchema instancedBy = HdInstancedBySchema::GetFromParent(prim.dataSource);
+  SdfPath parentId;
+  if (auto pathsDs = instancedBy.GetPaths()) {
+    const VtArray<SdfPath> parentPaths = pathsDs->GetTypedValue(0.0f);
+    if (!parentPaths.empty()) {
+      parentId = parentPaths.front();
     }
   }
-  else {
-    resultTransforms = std::move(transforms);
+  if (parentId.IsEmpty()) {
+    parentId = GetParentId();
   }
 
-  return resultTransforms;
+  if (!parentId.IsEmpty()) {
+    auto *const parentInstancer = static_cast<HdCyclesInstancer *>(
+        sceneDelegate->GetRenderIndex().GetInstancer(parentId));
+    if (parentInstancer) {
+      for (const GfMatrix4d &parentTransform : parentInstancer->ComputeInstanceTransforms(id)) {
+        for (const GfMatrix4d &localTransform : transforms) {
+          resultTransforms.push_back(parentTransform * localTransform);
+        }
+      }
+      return resultTransforms;
+    }
+  }
+
+  return transforms;
 }
 
 HDCYCLES_NAMESPACE_CLOSE_SCOPE

intern/cycles/hydra/CMakeLists.txt
@@ -21,7 +21,6 @@ set(LIB
 cycles_external_libraries_append(LIB)
 
 set(SRC_HD_CYCLES_HEADERS
-  attribute.h
   camera.h
   config.h
   curves.h
@@ -40,11 +39,11 @@ set(SRC_HD_CYCLES_HEADERS
   render_delegate.h
   render_pass.h
   session.h
+  util.h
   volume.h
 )
 
 set(SRC_HD_CYCLES
-  attribute.cpp
   curves.cpp
   camera.cpp
   field.cpp
@@ -61,6 +60,7 @@ set(SRC_HD_CYCLES
   render_delegate.cpp
   render_pass.cpp
   session.cpp
+  util.cpp
   volume.cpp
 )
 

intern/cycles/hydra/attribute.h
@@ -1,22 +1,0 @@
-/* SPDX-FileCopyrightText: 2022 NVIDIA Corporation
- * SPDX-FileCopyrightText: 2022 Blender Foundation
- *
- * SPDX-License-Identifier: Apache-2.0 */
-
-#pragma once
-
-#include "hydra/config.h"
-#include "scene/attribute.h"
-
-#include <pxr/base/vt/value.h>
-#include <pxr/imaging/hd/types.h>
-
-HDCYCLES_NAMESPACE_OPEN_SCOPE
-
-void ApplyPrimvars(CCL_NS::AttributeSet &attributes,
-                   const CCL_NS::ustring &name,
-                   PXR_NS::VtValue value,
-                   CCL_NS::AttributeElement elem,
-                   CCL_NS::AttributeStandard std);
-
-HDCYCLES_NAMESPACE_CLOSE_SCOPE

intern/cycles/hydra/camera.cpp
@@ -5,10 +5,13 @@
 
 #include "hydra/camera.h"
 #include "hydra/session.h"
+#include "hydra/util.h"
 #include "scene/camera.h"
 
 #include <pxr/base/gf/frustum.h>
+#include <pxr/imaging/hd/cameraSchema.h>
 #include <pxr/imaging/hd/sceneDelegate.h>
+#include <pxr/imaging/hd/xformSchema.h>
 #include <pxr/usd/usdGeom/tokens.h>
 
 HDCYCLES_NAMESPACE_OPEN_SCOPE
@@ -28,18 +31,8 @@ static Transform convert_camera_transform(const GfMatrix4d &matrix, const float 
   return t;
 }
 
-#if PXR_VERSION < 2102
-// clang-format off
-TF_DEFINE_PRIVATE_TOKENS(_tokens,
-    (projection)
-    (orthographic)
-);
-// clang-format on
-#endif
-
 HdCyclesCamera::HdCyclesCamera(const SdfPath &sprimId) : HdCamera(sprimId)
 {
-#if PXR_VERSION >= 2102
   // Synchronize default values
   _horizontalAperture = _data.GetHorizontalAperture() * GfCamera::APERTURE_UNIT;
   _verticalAperture = _data.GetVerticalAperture() * GfCamera::APERTURE_UNIT;
@@ -49,7 +42,6 @@ HdCyclesCamera::HdCyclesCamera(const SdfPath &sprimId) : HdCamera(sprimId)
   _clippingRange = _data.GetClippingRange();
   _fStop = _data.GetFStop();
   _focusDistance = _data.GetFocusDistance();
-#endif
 }
 
 HdCyclesCamera::~HdCyclesCamera() = default;
@@ -67,183 +59,111 @@ void HdCyclesCamera::Sync(HdSceneDelegate *sceneDelegate,
     return;
   }
 
-  VtValue value;
   const SdfPath &id = GetId();
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, id);
+  const HdContainerDataSourceHandle &primDs = prim.dataSource;
 
-#if PXR_VERSION >= 2102
+  HdCameraSchema cameraSchema = HdCameraSchema::GetFromParent(primDs);
+  HdContainerDataSourceHandle cameraDs = cameraSchema.GetContainer();
+
+  /* Read the shutter window from the camera prim so animated transforms can
+   * be sampled across it for motion blur. */
+  float shutterOpen = 0.0f;
+  float shutterClose = 0.0f;
+  if (auto ds = cameraSchema.GetShutterOpen()) {
+    shutterOpen = float(ds->GetTypedValue(0.0f));
+  }
+  if (auto ds = cameraSchema.GetShutterClose()) {
+    shutterClose = float(ds->GetTypedValue(0.0f));
+  }
+
   if (*dirtyBits & DirtyBits::DirtyTransform) {
-    sceneDelegate->SampleTransform(id, &_transformSamples);
-
-    bool transform_found = false;
-    for (size_t i = 0; i < _transformSamples.count; ++i) {
-      if (_transformSamples.times[i] == 0.0f) {
-        _transform = _transformSamples.values[i];
+    HdXformSchema xformSchema = HdXformSchema::GetFromParent(primDs);
+    if (auto matrixDs = xformSchema.GetMatrix()) {
+      SampleTyped<GfMatrix4d, 2>(matrixDs, shutterOpen, shutterClose, &_transformSamples);
+      bool transform_found = false;
+      for (size_t i = 0; i < _transformSamples.count; ++i) {
+        if (_transformSamples.times[i] == 0.0f) {
+          _transform = _transformSamples.values[i];
+          _data.SetTransform(_transform);
+          transform_found = true;
+          break;
+        }
+      }
+      if (!transform_found && _transformSamples.count) {
+        _transform = _transformSamples.values[0];
         _data.SetTransform(_transform);
-        transform_found = true;
-        break;
       }
     }
-
-    if (!transform_found && _transformSamples.count) {
-      _transform = _transformSamples.values[0];
-      _data.SetTransform(_transform);
-    }
   }
-#else
-  if (*dirtyBits & DirtyBits::DirtyViewMatrix) {
-    sceneDelegate->SampleTransform(id, &_transformSamples);
-
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->worldToViewMatrix);
-    if (!value.IsEmpty()) {
-      _worldToViewMatrix = value.Get<GfMatrix4d>();
-      _worldToViewInverseMatrix = _worldToViewMatrix.GetInverse();
-      _data.SetTransform(_worldToViewInverseMatrix);
-    }
-  }
-#endif
-
-#if PXR_VERSION < 2111
-  if (*dirtyBits & DirtyBits::DirtyProjMatrix) {
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->projectionMatrix);
-    if (!value.IsEmpty()) {
-      _projectionMatrix = value.Get<GfMatrix4d>();
-      const float focalLength = _data.GetFocalLength();  // Get default focal length
-#  if PXR_VERSION >= 2102
-      _data.SetFromViewAndProjectionMatrix(GetViewMatrix(), _projectionMatrix, focalLength);
-#  else
-      if (_projectionMatrix[2][3] < -0.5) {
-        _data.SetProjection(GfCamera::Perspective);
-
-        const float horizontalAperture = (2.0 * focalLength) / _projectionMatrix[0][0];
-        _data.SetHorizontalAperture(horizontalAperture);
-        _data.SetHorizontalApertureOffset(0.5 * horizontalAperture * _projectionMatrix[2][0]);
-        const float verticalAperture = (2.0 * focalLength) / _projectionMatrix[1][1];
-        _data.SetVerticalAperture(verticalAperture);
-        _data.SetVerticalApertureOffset(0.5 * verticalAperture * _projectionMatrix[2][1]);
-
-        _data.SetClippingRange(
-            GfRange1f(_projectionMatrix[3][2] / (_projectionMatrix[2][2] - 1.0),
-                      _projectionMatrix[3][2] / (_projectionMatrix[2][2] + 1.0)));
-      }
-      else {
-        _data.SetProjection(GfCamera::Orthographic);
-
-        const float horizontalAperture = (2.0 / GfCamera::APERTURE_UNIT) / _projectionMatrix[0][0];
-        _data.SetHorizontalAperture(horizontalAperture);
-        _data.SetHorizontalApertureOffset(-0.5 * horizontalAperture * _projectionMatrix[3][0]);
-        const float verticalAperture = (2.0 / GfCamera::APERTURE_UNIT) / _projectionMatrix[1][1];
-        _data.SetVerticalAperture(verticalAperture);
-        _data.SetVerticalApertureOffset(-0.5 * verticalAperture * _projectionMatrix[3][1]);
-
-        const double nearMinusFarHalf = 1.0 / _projectionMatrix[2][2];
-        const double nearPlusFarHalf = nearMinusFarHalf * _projectionMatrix[3][2];
-        _data.SetClippingRange(
-            GfRange1f(nearPlusFarHalf + nearMinusFarHalf, nearPlusFarHalf - nearMinusFarHalf));
-      }
-#  endif
-    }
-  }
-#endif
 
   if (*dirtyBits & DirtyBits::DirtyWindowPolicy) {
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->windowPolicy);
-    if (!value.IsEmpty()) {
-      _windowPolicy = value.Get<CameraUtilConformWindowPolicy>();
-    }
+    /* The window policy is not part of the camera schema; it lives on the
+     * legacy data source. Leave the default of `CameraUtilFit`. */
   }
 
   if (*dirtyBits & DirtyBits::DirtyClipPlanes) {
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->clipPlanes);
-    if (!value.IsEmpty()) {
-      _clipPlanes = value.Get<std::vector<GfVec4d>>();
+    if (auto ds = cameraSchema.GetClippingPlanes()) {
+      const VtArray<GfVec4d> clipPlanes = ds->GetTypedValue(0.0f);
+      _clipPlanes.assign(clipPlanes.cbegin(), clipPlanes.cend());
     }
   }
 
   if (*dirtyBits & DirtyBits::DirtyParams) {
-#if PXR_VERSION >= 2102
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->projection);
-    if (!value.IsEmpty()) {
-      _projection = value.Get<Projection>();
+    if (auto ds = cameraSchema.GetProjection()) {
+      const TfToken projection = ds->GetTypedValue(0.0f);
+      _projection = (projection == HdCameraSchemaTokens->orthographic) ? Orthographic :
+                                                                         Perspective;
       _data.SetProjection(_projection != Orthographic ? GfCamera::Perspective :
                                                         GfCamera::Orthographic);
     }
-#else
-    value = sceneDelegate->GetCameraParamValue(id, _tokens->projection);
-    if (!value.IsEmpty()) {
-      _data.SetProjection(value.Get<TfToken>() != _tokens->orthographic ? GfCamera::Perspective :
-                                                                          GfCamera::Orthographic);
-    }
-#endif
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->horizontalAperture);
-    if (!value.IsEmpty()) {
-      const auto horizontalAperture = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetHorizontalAperture()) {
+      const float horizontalAperture = ds->GetTypedValue(0.0f);
       _horizontalAperture = horizontalAperture;
-#endif
       _data.SetHorizontalAperture(horizontalAperture / GfCamera::APERTURE_UNIT);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->verticalAperture);
-    if (!value.IsEmpty()) {
-      const auto verticalAperture = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetVerticalAperture()) {
+      const float verticalAperture = ds->GetTypedValue(0.0f);
       _verticalAperture = verticalAperture;
-#endif
       _data.SetVerticalAperture(verticalAperture / GfCamera::APERTURE_UNIT);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->horizontalApertureOffset);
-    if (!value.IsEmpty()) {
-      const auto horizontalApertureOffset = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetHorizontalApertureOffset()) {
+      const float horizontalApertureOffset = ds->GetTypedValue(0.0f);
       _horizontalApertureOffset = horizontalApertureOffset;
-#endif
       _data.SetHorizontalApertureOffset(horizontalApertureOffset / GfCamera::APERTURE_UNIT);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->verticalApertureOffset);
-    if (!value.IsEmpty()) {
-      const auto verticalApertureOffset = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetVerticalApertureOffset()) {
+      const float verticalApertureOffset = ds->GetTypedValue(0.0f);
       _verticalApertureOffset = verticalApertureOffset;
-#endif
       _data.SetVerticalApertureOffset(verticalApertureOffset / GfCamera::APERTURE_UNIT);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->focalLength);
-    if (!value.IsEmpty()) {
-      const auto focalLength = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetFocalLength()) {
+      const float focalLength = ds->GetTypedValue(0.0f);
       _focalLength = focalLength;
-#endif
       _data.SetFocalLength(focalLength / GfCamera::FOCAL_LENGTH_UNIT);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->clippingRange);
-    if (!value.IsEmpty()) {
-      const auto clippingRange = value.Get<GfRange1f>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetClippingRange()) {
+      const GfVec2f range = ds->GetTypedValue(0.0f);
+      const GfRange1f clippingRange(range[0], range[1]);
       _clippingRange = clippingRange;
-#endif
       _data.SetClippingRange(clippingRange);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->fStop);
-    if (!value.IsEmpty()) {
-      const auto fStop = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetFStop()) {
+      const float fStop = ds->GetTypedValue(0.0f);
       _fStop = fStop;
-#endif
       _data.SetFStop(fStop);
     }
 
-    value = sceneDelegate->GetCameraParamValue(id, HdCameraTokens->focusDistance);
-    if (!value.IsEmpty()) {
-      const auto focusDistance = value.Get<float>();
-#if PXR_VERSION >= 2102
+    if (auto ds = cameraSchema.GetFocusDistance()) {
+      const float focusDistance = ds->GetTypedValue(0.0f);
       _focusDistance = focusDistance;
-#endif
       _data.SetFocusDistance(focusDistance);
     }
   }
@@ -323,14 +243,10 @@ void HdCyclesCamera::ApplyCameraSettings(HdRenderParam *renderParam,
                                          const std::vector<GfVec4d> & /*clipPlanes*/,
                                          Camera *cam)
 {
-#if PXR_VERSION >= 2102
   GfCamera data;
   data.SetFromViewAndProjectionMatrix(worldToViewMatrix, projectionMatrix);
 
   ApplyCameraSettings(renderParam, data, CameraUtilFit, cam);
-#else
-  TF_CODING_ERROR("Not implemented");
-#endif
 }
 
 HDCYCLES_NAMESPACE_CLOSE_SCOPE

intern/cycles/hydra/curves.cpp
@@ -4,28 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0 */
 
 #include "hydra/curves.h"
-#include "hydra/attribute.h"
 #include "hydra/geometry.inl"
+#include "hydra/util.h"
 #include "scene/hair.h"
 
-#include <pxr/imaging/hd/extComputationUtils.h>
+#include <pxr/imaging/hd/basisCurvesSchema.h>
+#include <pxr/imaging/hd/basisCurvesTopologySchema.h>
 
 HDCYCLES_NAMESPACE_OPEN_SCOPE
 
-HdCyclesCurves::HdCyclesCurves(const SdfPath &rprimId
-#if PXR_VERSION < 2102
-                               ,
-                               const SdfPath &instancerId
-#endif
-                               )
-    : HdCyclesGeometry(rprimId
-#if PXR_VERSION < 2102
-                       ,
-                       instancerId
-#endif
-      )
-{
-}
+HdCyclesCurves::HdCyclesCurves(const SdfPath &rprimId) : HdCyclesGeometry(rprimId) {}
 
 HdCyclesCurves::~HdCyclesCurves() = default;
 
@@ -71,24 +59,9 @@ void HdCyclesCurves::Populate(HdSceneDelegate *sceneDelegate, HdDirtyBits dirtyB
 
 void HdCyclesCurves::PopulatePoints(HdSceneDelegate *sceneDelegate)
 {
-  VtValue value;
-
-  for (const HdExtComputationPrimvarDescriptor &desc :
-       sceneDelegate->GetExtComputationPrimvarDescriptors(GetId(), HdInterpolationVertex))
-  {
-    if (desc.name == HdTokens->points) {
-      auto valueStore = HdExtComputationUtils::GetComputedPrimvarValues({desc}, sceneDelegate);
-      const auto valueStoreIt = valueStore.find(desc.name);
-      if (valueStoreIt != valueStore.end()) {
-        value = std::move(valueStoreIt->second);
-      }
-      break;
-    }
-  }
-
-  if (value.IsEmpty()) {
-    value = GetPrimvar(sceneDelegate, HdTokens->points);
-  }
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, GetId());
+  const HdPrimvarsSchema primvars = HdPrimvarsSchema::GetFromParent(prim.dataSource);
+  const VtValue value = ReadPrimvar(primvars, HdTokens->points);
 
   if (!value.IsHolding<VtVec3fArray>()) {
     TF_WARN("Invalid points data for %s", GetId().GetText());
@@ -109,8 +82,10 @@ void HdCyclesCurves::PopulatePoints(HdSceneDelegate *sceneDelegate)
 
 void HdCyclesCurves::PopulateWidths(HdSceneDelegate *sceneDelegate)
 {
-  const VtValue value = GetPrimvar(sceneDelegate, HdTokens->widths);
-  const HdInterpolation interpolation = GetPrimvarInterpolation(sceneDelegate, HdTokens->widths);
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, GetId());
+  const HdPrimvarsSchema primvars = HdPrimvarsSchema::GetFromParent(prim.dataSource);
+  const VtValue value = ReadPrimvar(primvars, HdTokens->widths);
+  const HdInterpolation interpolation = ReadPrimvarInterpolation(primvars, HdTokens->widths);
 
   if (!value.IsHolding<VtFloatArray>()) {
     TF_WARN("Invalid widths data for %s", GetId().GetText());
@@ -146,6 +121,9 @@ void HdCyclesCurves::PopulatePrimvars(HdSceneDelegate *sceneDelegate)
 {
   Scene *const scene = (Scene *)_geom->get_owner();
 
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, GetId());
+  const HdPrimvarsSchema primvars = HdPrimvarsSchema::GetFromParent(prim.dataSource);
+
   const std::pair<HdInterpolation, AttributeElement> interpolations[] = {
       std::make_pair(HdInterpolationVertex, ATTR_ELEMENT_CURVE_KEY),
       std::make_pair(HdInterpolationVarying, ATTR_ELEMENT_CURVE_KEY),
@@ -154,29 +132,28 @@ void HdCyclesCurves::PopulatePrimvars(HdSceneDelegate *sceneDelegate)
   };
 
   for (const auto &interpolation : interpolations) {
-    for (const HdPrimvarDescriptor &desc :
-         GetPrimvarDescriptors(sceneDelegate, interpolation.first))
-    {
+    for (const TfToken &primvarName : PrimvarNamesAtInterpolation(primvars, interpolation.first)) {
       // Skip special primvars that are handled separately
-      if (desc.name == HdTokens->points || desc.name == HdTokens->widths) {
+      if (primvarName == HdTokens->points || primvarName == HdTokens->widths) {
         continue;
       }
 
-      const VtValue value = GetPrimvar(sceneDelegate, desc.name);
+      const VtValue value = ReadPrimvar(primvars, primvarName);
       if (value.IsEmpty()) {
         continue;
       }
 
-      const ustring name(desc.name.GetString());
+      const TfToken role = ReadPrimvarRole(primvars, primvarName);
+      const ustring name(primvarName.GetString());
 
       AttributeStandard std = ATTR_STD_NONE;
-      if (desc.role == HdPrimvarRoleTokens->textureCoordinate) {
+      if (role == HdPrimvarRoleTokens->textureCoordinate) {
         std = ATTR_STD_UV;
       }
-      else if (desc.name == HdTokens->normals && interpolation.first == HdInterpolationVertex) {
+      else if (primvarName == HdTokens->normals && interpolation.first == HdInterpolationVertex) {
         std = ATTR_STD_VERTEX_NORMAL;
       }
-      else if (desc.name == HdTokens->displayColor &&
+      else if (primvarName == HdTokens->displayColor &&
                interpolation.first == HdInterpolationConstant)
       {
         if (value.IsHolding<VtVec3fArray>() && value.GetArraySize() == 1) {
@@ -204,7 +181,32 @@ void HdCyclesCurves::PopulateTopology(HdSceneDelegate *sceneDelegate)
   // Clear geometry before populating it again with updated topology
   _geom->clear(true);
 
-  const HdBasisCurvesTopology topology = GetBasisCurvesTopology(sceneDelegate);
+  const HdSceneIndexPrim prim = GetPrim(sceneDelegate, GetId());
+  const HdBasisCurvesTopologySchema topoSchema =
+      HdBasisCurvesSchema::GetFromParent(prim.dataSource).GetTopology();
+
+  TfToken curveType = HdTokens->linear;
+  if (auto ds = topoSchema.GetType()) {
+    curveType = ds->GetTypedValue(0.0f);
+  }
+  TfToken curveBasis = HdTokens->bezier;
+  if (auto ds = topoSchema.GetBasis()) {
+    curveBasis = ds->GetTypedValue(0.0f);
+  }
+  TfToken curveWrap = HdTokens->nonperiodic;
+  if (auto ds = topoSchema.GetWrap()) {
+    curveWrap = ds->GetTypedValue(0.0f);
+  }
+  VtIntArray curveVertexCounts;
+  if (auto ds = topoSchema.GetCurveVertexCounts()) {
+    curveVertexCounts = ds->GetTypedValue(0.0f);
+  }
+  VtIntArray curveIndices;
+  if (auto ds = topoSchema.GetCurveIndices()) {
+    curveIndices = ds->GetTypedValue(0.0f);
+  }
+  const HdBasisCurvesTopology topology(
+      curveType, curveBasis, curveWrap, curveVertexCounts, curveIndices);
 
   _geom->resize_curves(topology.GetNumCurves(), topology.CalculateNeededNumberOfControlPoints());
 

intern/cycles/hydra/curves.h
@@ -14,12 +14,7 @@ HDCYCLES_NAMESPACE_OPEN_SCOPE
 
 class HdCyclesCurves final : public HdCyclesGeometry<PXR_NS::HdBasisCurves, CCL_NS::Hair> {
  public:
-  HdCyclesCurves(const PXR_NS::SdfPath &rprimId
-#if PXR_VERSION < 2102
-                 ,
-                 const PXR_NS::SdfPath &instancerId = {}
-#endif
-  );
+  HdCyclesCurves(const PXR_NS::SdfPath &rprimId);
   ~HdCyclesCurves() override;
 
   PXR_NS::HdDirtyBits GetInitialDirtyBitsMask() const override;

FrenkyBojler/blender-ios

commit 6e0479725985bbd7172c67e4aec96314532d1fa2

Fix: Cycles: Hydra instancer rotation not cleared across syncs
This issue was introduced in 51977de03a3.

intern/cycles/hydra/instancer.cpp
@@ -68,6 +68,7 @@ void HdCyclesInstancer::SyncPrimvars()
       }
       else if (v.IsHolding<VtQuatfArray>()) {
         const VtQuatfArray &src = v.UncheckedGet<VtQuatfArray>();
+        _rotate.clear();
         _rotate.reserve(src.size());
         for (const GfQuatf &q : src) {
           const GfVec3f &im = q.GetImaginary();
@@ -76,6 +77,7 @@ void HdCyclesInstancer::SyncPrimvars()
       }
       else if (v.IsHolding<VtQuathArray>()) {
         const VtQuathArray &src = v.UncheckedGet<VtQuathArray>();
+        _rotate.clear();
         _rotate.reserve(src.size());
         for (const GfQuath &q : src) {
           const GfVec3h &im = q.GetImaginary();