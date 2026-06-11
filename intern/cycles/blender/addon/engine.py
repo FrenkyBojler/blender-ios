blender/blender

commit 6fef00cace6603501b419dbe10865cbb9a0a4e51

Fix #159125: Cycles: Disable denoising motion pass with motion blur
The denoising backward motion pass is disabled for Cycles when
motion blur is used. This mirrors what is also done for the regular
vector data pass and avoids crashes.

Pull Request: https://projects.blender.org/blender/blender/pulls/159157

intern/cycles/blender/addon/engine.py
@@ -251,7 +251,8 @@ def list_render_passes(scene, srl):
         yield (n_("Denoising Normal"), "XYZ", 'VECTOR')
         yield (n_("Denoising Roughness"), "X", 'VALUE')
         yield (n_("Denoising Depth"), "Z", 'VALUE')
-        yield (n_("Denoising Backward Motion"), "XYZ", 'VECTOR')
+        if not scene.render.use_motion_blur:
+            yield (n_("Denoising Backward Motion"), "XYZ", 'VECTOR')
 
     # Custom AOV passes.
     for aov in srl.aovs:

intern/cycles/scene/film.cpp
@@ -244,7 +244,9 @@ void Film::device_update(Device *device, DeviceScene *dscene, Scene *scene)
     }
 
     /* Can't do motion pass if no motion vectors are available. */
-    if (pass->get_type() == PASS_MOTION || pass->get_type() == PASS_MOTION_WEIGHT) {
+    if (pass->get_type() == PASS_MOTION || pass->get_type() == PASS_MOTION_WEIGHT ||
+        pass->get_type() == PASS_DENOISING_BACKWARD_MOTION)
+    {
       const Scene::MotionType need_motion = scene->need_motion();
       if (need_motion != Scene::MOTION_PASS && need_motion != Scene::MOTION_PASS_INTERACTIVE) {
         kfilm->pass_stride += pass->get_info().num_components;

FrenkyBojler/blender-ios

commit 355abdcd0b733a9863c76a28fa336cb93e8ddd84

Fix: Cycles: Denoising motion pass does not check view layer filter
It was inconsistent with the C++ code.

This issue was introduced in 6fef00cace6.

intern/cycles/blender/addon/engine.py
@@ -251,7 +251,7 @@ def list_render_passes(scene, srl):
         yield (n_("Denoising Normal"), "XYZ", 'VECTOR')
         yield (n_("Denoising Roughness"), "X", 'VALUE')
         yield (n_("Denoising Depth"), "Z", 'VALUE')
-        if not scene.render.use_motion_blur:
+        if not (scene.render.use_motion_blur and srl.use_motion_blur):
             yield (n_("Denoising Backward Motion"), "XYZ", 'VECTOR')
 
     # Custom AOV passes.