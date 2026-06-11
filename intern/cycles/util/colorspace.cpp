blender/blender

commit 5d2b8f8a529fd76219b0f48834029a38ce459009

Fix #159311: Cycles texture cache issues with different working spaces
The colorspace from the tx file was not properly used, which caused some
wrong colorspace conversions when using working spaces other than Linear
Rec.709.

Pull Request: https://projects.blender.org/blender/blender/pulls/159543

intern/cycles/util/colorspace.cpp
@@ -28,6 +28,9 @@ ustring u_colorspace_scene_linear_srgb("scene_linear_srgb");
 ustring u_colorspace_srgb("__builtin_srgb");
 
 /* Cached data. */
+static thread_mutex cache_xyz_to_scene_linear_mutex;
+static string cache_xyz_to_scene_linear_hash;
+
 #ifdef WITH_OCIO
 static thread_mutex cache_processors_mutex;
 static unordered_map<ustring, OCIO::ConstProcessorRcPtr> cache_processors;
@@ -75,12 +78,13 @@ static void check_invalidate_caches()
       cache_scene_linear_interop_id = "";
       cache_scene_linear_srgb_interop_id = "";
     }
+    {
+      const thread_scoped_lock cache_lock(cache_xyz_to_scene_linear_mutex);
+      cache_xyz_to_scene_linear_hash.clear();
+    }
   }
 }
 #endif
-
-static thread_mutex cache_xyz_to_scene_linear_mutex;
-static string cache_xyz_to_scene_linear_hash;
 
 ColorSpaceProcessor *ColorSpaceManager::get_processor(ustring colorspace)
 {
@@ -789,6 +793,11 @@ Transform ColorSpaceManager::get_xyz_to_scene_linear_rgb()
 
 const std::string &ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
 {
+#ifdef WITH_OCIO
+  /* Clear the cached hash if the scene linear colorspace changed. */
+  check_invalidate_caches();
+#endif
+
   /* NOTE: Be careful not to change existing hashes if at all possible, as this
    * will cause all texture files to be regenerated with significantly increased
    * disk usage. */

intern/cycles/util/image_metadata.cpp
@@ -280,6 +280,12 @@ void ImageMetaData::detect_tiles(ImageInput &input,
   }
   else {
     tile_need_conform = false;
+
+    /* For tx files, use the color space hint to determine if this was encoded
+     * as scene linear, scene linear + sRGB or data. */
+    if (!colorspace_file_hint.empty()) {
+      colorspace = ustring(colorspace_file_hint);
+    }
   }
 
   bool has_tiles = false;

tests/files/render/image_colorspace/cycles_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:515c9b8eead89c3d7731915980b31b01dbb9f31fafc3c86c9b2ae1e9a4e7e265
+size 46255

tests/files/render/image_colorspace/cycles_tx_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c1a191ed021a093318f14d6269722ca7d9f70de92823461d69693d01087cbff
+size 46255

tests/files/render/image_colorspace/eevee_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59b2342e379638be9e89850ea4807db65ac2d6978feae50029e098b7872e286d
+size 43284

tests/files/render/image_colorspace/image_acescg.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b8c58d21a40a1bb35d5dfb2d2f3300d9957b777ce01e24c35143509b3690531
+size 88148

tests/files/render/image_colorspace/storm_hydra_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4b5a06409c2243539cec63727b7a8b1b2996ffe2f31c75e1e8353b067d15a3e
+size 81532

tests/files/render/image_colorspace/storm_usd_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a04d4ac8c6a40c1493a74a8f565bcb56353f968ee404fe3232ded45ebfef88b
+size 83820

FrenkyBojler/blender-ios

commit ac29ea281acb689ccdc33a1af83ab0ba59022014

Cleanup: Cycles: Avoid potentially dangling XYZ-to-scene-linear reference
It's not really possible in practice for OpenColorIO configs to switch
mid render, but better not to return a reference to something that is
mutex protected.

This issue was introduced in 5d2b8f8a529.

intern/cycles/util/colorspace.cpp
@@ -791,7 +791,7 @@ Transform ColorSpaceManager::get_xyz_to_scene_linear_rgb()
   return xyz_to_rgb;
 }
 
-const std::string &ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
+std::string ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
 {
 #ifdef WITH_OCIO
   /* Clear the cached hash if the scene linear colorspace changed. */

intern/cycles/util/colorspace.h
@@ -65,7 +65,7 @@ class ColorSpaceManager {
   static Transform get_xyz_to_rec2020();
   static Transform get_xyz_to_acescg();
   /* Compute unique string for texture cache hashing and metadata. */
-  static const string &get_xyz_to_scene_linear_rgb_string();
+  static string get_xyz_to_scene_linear_rgb_string();
   /* Determine if scene linear is a common known space. */
   static const char *get_scene_linear_interop_id(const bool srgb_encoded = false);
 