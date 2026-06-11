blender/blender

commit 5d2b8f8a529fd76219b0f48834029a38ce459009

Fix #159311: Cycles texture cache issues with different working spaces
The colorspace from the tx file was not properly used, which caused some
wrong colorspace conversions when using working spaces other than Linear
Rec.709.

Pull Request: https://projects.blender.org/blender/blender/pulls/159543

intern/cycles/util/colorspace.cpp
@@ -28,6 +28,9 @@ ustring u_colorspace_scene_linear_srgb("scene_linear_srgb");
 ustring u_colorspace_srgb("__builtin_srgb");
 
 /* Cached data. */
+static thread_mutex cache_xyz_to_scene_linear_mutex;
+static string cache_xyz_to_scene_linear_hash;
+
 #ifdef WITH_OCIO
 static thread_mutex cache_processors_mutex;
 static unordered_map<ustring, OCIO::ConstProcessorRcPtr> cache_processors;
@@ -75,12 +78,13 @@ static void check_invalidate_caches()
       cache_scene_linear_interop_id = "";
       cache_scene_linear_srgb_interop_id = "";
     }
+    {
+      const thread_scoped_lock cache_lock(cache_xyz_to_scene_linear_mutex);
+      cache_xyz_to_scene_linear_hash.clear();
+    }
   }
 }
 #endif
-
-static thread_mutex cache_xyz_to_scene_linear_mutex;
-static string cache_xyz_to_scene_linear_hash;
 
 ColorSpaceProcessor *ColorSpaceManager::get_processor(ustring colorspace)
 {
@@ -789,6 +793,11 @@ Transform ColorSpaceManager::get_xyz_to_scene_linear_rgb()
 
 const std::string &ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
 {
+#ifdef WITH_OCIO
+  /* Clear the cached hash if the scene linear colorspace changed. */
+  check_invalidate_caches();
+#endif
+
   /* NOTE: Be careful not to change existing hashes if at all possible, as this
    * will cause all texture files to be regenerated with significantly increased
    * disk usage. */

intern/cycles/util/image_metadata.cpp
@@ -280,6 +280,12 @@ void ImageMetaData::detect_tiles(ImageInput &input,
   }
   else {
     tile_need_conform = false;
+
+    /* For tx files, use the color space hint to determine if this was encoded
+     * as scene linear, scene linear + sRGB or data. */
+    if (!colorspace_file_hint.empty()) {
+      colorspace = ustring(colorspace_file_hint);
+    }
   }
 
   bool has_tiles = false;

tests/files/render/image_colorspace/cycles_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:515c9b8eead89c3d7731915980b31b01dbb9f31fafc3c86c9b2ae1e9a4e7e265
+size 46255

tests/files/render/image_colorspace/cycles_tx_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c1a191ed021a093318f14d6269722ca7d9f70de92823461d69693d01087cbff
+size 46255

tests/files/render/image_colorspace/eevee_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59b2342e379638be9e89850ea4807db65ac2d6978feae50029e098b7872e286d
+size 43284

tests/files/render/image_colorspace/image_acescg.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b8c58d21a40a1bb35d5dfb2d2f3300d9957b777ce01e24c35143509b3690531
+size 88148

tests/files/render/image_colorspace/storm_hydra_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4b5a06409c2243539cec63727b7a8b1b2996ffe2f31c75e1e8353b067d15a3e
+size 81532

tests/files/render/image_colorspace/storm_usd_renders/image_acescg.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a04d4ac8c6a40c1493a74a8f565bcb56353f968ee404fe3232ded45ebfef88b
+size 83820

FrenkyBojler/blender-ios

commit 00ab9ee7f6eb8bdeb2c5c74ff3db0e2462ffbecd

Fix: Cycles: Build error without OpenColorIO
Not a required dependency for Cycles standalone yet, even if it is for
Blender.

This issue was introduced in 5d2b8f8a529.

intern/cycles/util/colorspace.cpp
@@ -634,6 +634,7 @@ void ColorSpaceManager::to_scene_linear(ustring colorspace,
   (void)y_stride;
   (void)is_rgba;
   (void)compress_as_srgb;
+  (void)ignore_alpha;
 #endif
 }
 
@@ -827,6 +828,7 @@ std::string ColorSpaceManager::get_xyz_to_scene_linear_rgb_string()
 
 const char *ColorSpaceManager::get_scene_linear_interop_id(const bool srgb_encoded)
 {
+#ifdef WITH_OCIO
   check_invalidate_caches();
 
   const thread_scoped_lock cache_lock(cache_scene_linear_interop_id_mutex);
@@ -855,6 +857,10 @@ const char *ColorSpaceManager::get_scene_linear_interop_id(const bool srgb_encod
   }
 
   return (srgb_encoded) ? cache_scene_linear_srgb_interop_id : cache_scene_linear_interop_id;
+#else
+  (void)srgb_encoded;
+  return "lin_rec709_scene";
+#endif
 }
 
 /* Template instantiations so we don't have to inline functions. */