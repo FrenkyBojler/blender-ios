56eea05a71bc5bbf0febfd2b6079c61a2027ded1
Lukas Tönne <lukas@blender.org>
2026-06-11 17:00:23 +0200

    New kernel functions for the Sample Grid and related nodes
    This adds a unified set of "grid samplers" based on kernel functions
    that improve and extend the grid sampling options. They are a
    prerequisite for future physics simulations and point rasterization.
    
    Apart from the existing constant ("nearest"), linear, and quadratic
    sampling options this adds a new quadratic and a new cubic B-spline
    kernels. These functions have advantages over simpler kernels, some of
    which are critical in physics simulations:
    
    - They are _symmetric_ around the sampling interval [0, 1] between
      data points `i` and `i+1`.
      The B-spline kernels use data points `i-1`, `i`, `i+1`, `i+2`.
    
      This is not the case with the existing quadratic mode in particular,
      which only uses data points `i-1`, `i`, `i+1`, which leads to
      asymmetric influence of values on the sampling result. Such a kernel
      may be used with asymmetric operators, such as a forward difference
      gradient operator, but is not very practical for general sampling.
    
    - They are positive everywhere. The quadratic mode, again, is _negative_
      for point `i-1`, which can lead to incorrect inversion of values.
    
    - They have _continuous derivatives_. This is not currently used, but
      for future simulations the derivatives of these kernel functions would
      be used to compute forces, which should not have discontinuities.
      The linear kernel function in particular changes direction around zero,
      which leads to sudden change of force direction.
    
    "Kernels" are one-dimensional functions. This patch also adds a unified
    method for constructing a 3D sampling function from such 1D kernels.
    The built-in OpenVDB samplers have a custom 3D function in each
    sampler implementation.

---
+++ source/blender/geometry/GEO_grid_samplers.hh
@@ -0,0 +1,472 @@
+/* SPDX-FileCopyrightText: 2023 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "BLI_index_range.hh"
+#include "BLI_math_matrix_types.hh"
+#include "BLI_math_vector_types.hh"
+
+#include "BKE_volume_grid_type_traits.hh"
+
+#ifdef WITH_OPENVDB
+#  include <openvdb/openvdb.h>
+#  include <openvdb/tools/Interpolation.h>
+#endif
+
+namespace blender::geometry {
+
+#ifdef WITH_OPENVDB
+
+namespace grid_sampling {
+
+template<int N, class AccessorT>
+bool probe_values(const AccessorT &accessor,
+                  const openvdb::Vec3i &index,
+                  typename AccessorT::ValueType (&data)[N][N][N])
+{
+  constexpr int start_offset = ((N + 1) >> 1) - 1;
+  const openvdb::Vec3i start_index = index -
+                                     openvdb::Vec3i(start_offset, start_offset, start_offset);
+
+  /* Retrieve the values of the voxels surrounding the fractional source coordinates. */
+  bool active = false;
+  for (int dx = 0, ix = start_index.x(); dx < N; ++dx, ++ix) {
+    for (int dy = 0, iy = start_index.y(); dy < N; ++dy, ++iy) {
+      for (int dz = 0, iz = start_index.z(); dz < N; ++dz, ++iz) {
+        if (accessor.probeValue(openvdb::Coord(ix, iy, iz), data[dx][dy][dz])) {
+          active = true;
+        }
+      }
+    }
+  }
+  return active;
+}
+
+template<int N, class AccessorT>
+void get_values(const AccessorT &accessor,
+                const openvdb::Vec3i &index,
+                typename AccessorT::ValueType (&data)[N][N][N])
+{
+  constexpr int start_offset = ((N + 1) >> 1) - 1;
+  const openvdb::Vec3i start_index = index -
+                                     openvdb::Vec3i(start_offset, start_offset, start_offset);
+
+  /* Retrieve the values of the voxels surrounding the fractional source coordinates. */
+  for (int dx = 0, ix = start_index.x(); dx < N; ++dx, ++ix) {
+    for (int dy = 0, iy = start_index.y(); dy < N; ++dy, ++iy) {
+      for (int dz = 0, iz = start_index.z(); dz < N; ++dz, ++iz) {
+        data[dx][dy][dz] = accessor.getValue(openvdb::Coord(ix, iy, iz));
+      }
+    }
+  }
+}
+
+/* Interpolate values in 3D using a function that combines a 1-dimensional array. */
+template<typename ValueT, int N, typename KernelFn>
+void interpolate_value_3d(ValueT (&data)[N][N][N],
+                          const openvdb::Vec3R &uvw,
+                          KernelFn kernel_fn,
+                          ValueT &result)
+{
+  ValueT vx[N];
+  for (int dx = 0; dx < N; ++dx) {
+    ValueT vy[N];
+    for (int dy = 0; dy < N; ++dy) {
+      const ValueT *vz = &data[dx][dy][0];
+      vy[dy] = kernel_fn(vz, uvw.z());
+    }
+    vx[dx] = kernel_fn(vy, uvw.y());
+  }
+  result = kernel_fn(vx, uvw.x());
+}
+
+template<typename Kernel, class AccessorT>
+bool sample_tree(const AccessorT &accessor,
+                 const openvdb::Vec3R &coord,
+                 typename AccessorT::ValueType &result)
+{
+  using ValueT = typename AccessorT::ValueType;
+
+  const openvdb::Vec3i index = openvdb::tools::local_util::floorVec3(coord);
+  const openvdb::Vec3R uvw = coord - index;
+
+  /* Retrieve the values of the voxels surrounding the fractional source coordinates. */
+  constexpr int N = Kernel::size;
+  ValueT data[N][N][N];
+  bool active = probe_values(accessor, index, data);
+  interpolate_value_3d(data, uvw, Kernel::template weight<ValueT>, result);
+
+  return active;
+}
+
+template<typename Kernel, class AccessorT>
+typename AccessorT::ValueType sample_tree(const AccessorT &accessor, const openvdb::Vec3R &coord)
+{
+  using ValueT = typename AccessorT::ValueType;
+
+  const openvdb::Vec3i index = openvdb::tools::local_util::floorVec3(coord);
+  const openvdb::Vec3R uvw = coord - index;
+
+  /* Retrieve the values of the voxels surrounding the fractional source coordinates. */
+  constexpr int N = Kernel::size;
+  ValueT data[N][N][N];
+  get_values(accessor, index, data);
+
+  ValueT result;
+  interpolate_value_3d(data, uvw, Kernel::template weight<ValueT>, result);
+  return result;
+}
+
+/**
+ * Nearest-neighbor kernel function with a nominal (zero) gradient output.
+ */
+struct ConstantKernel {
+  static constexpr float range = 0.5f;
+  static constexpr int size = 1;
+
+  static float weight(float x)
+  {
+    if (x < -0.5f) {
+      return 0.0f;
+    }
+    if (x < 0.5f) {
+      return 1.0f;
+    }
+    return 0.0f;
+  }
+
+  static float derivative(float /*x*/)
+  {
+    return 0.0f;
+  }
+
+  template<class ValueT> static ValueT weight(const ValueT *value, float /*weight*/)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    return value[0];
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+
+  template<class ValueT> static ValueT derivative(const ValueT * /*value*/, float /*weight*/)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    return ValueT(0.0);
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+};
+
+/**
+ * Linear kernel function that also supports gradient output.
+ */
+struct LinearKernel {
+  static constexpr float range = 1.0f;
+  static constexpr int size = 2;
+
+  static float weight(float x)
+  {
+    if (x < -1.0f) {
+      return 0.0f;
+    }
+    if (x < 0.0f) {
+      return x + 1.0f;
+    }
+    if (x < 1.0f) {
+      return -x + 1.0f;
+    }
+    return 0.0f;
+  }
+
+  static float derivative(float x)
+  {
+    if (x < -1.0f) {
+      return 0.0f;
+    }
+    if (x < 0.0f) {
+      return 1.0f;
+    }
+    if (x < 1.0f) {
+      return -1.0f;
+    }
+    return 0.0f;
+  }
+
+  template<class ValueT> static ValueT weight(const ValueT *value, float weight)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    const ValueT lin = static_cast<ValueT>(value[1] - value[0]);
+    const ValueT con = static_cast<ValueT>(value[0]);
+    return static_cast<ValueT>(weight * lin) + con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+
+  template<class ValueT> static ValueT derivative(const ValueT *value, float /*weight*/)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    const ValueT con = static_cast<ValueT>(value[1] - value[0]);
+    return con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+};
+
+/**
+ * Quadratic B-spline kernel function as described in
+ * Steffen et al., "Analysis and reduction of quadrature errors in the material point method (MPM)"
+ *
+ * The kernel is a piece-wise quadratic spline:
+ * f(x) = 0                          for         x < -3/2
+ * f(x) = 1/2*x^2 + 3/2*x + 9/8      for 1/2 <= x < 3/2
+ * f(x) = -x^2 + 3/4                 for -1/2 <= x < 1/2
+ * f(x) = 1/2*x^2 - 3/2*x + 9/8      for 1/2 <= x < 3/2
+ * f(x) = 0                          for 3/2 <= x
+ *
+ * The derivative is a piece-wise linear function:
+ * f(x) = 0                          for         x < -3/2
+ * f(x) = x + 3/2                    for -3/2 <= x < -1/2
+ * f(x) = -2*x                       for    0 <= x < 1/2
+ * f(x) = x - 3/2                    for  1/2 <= x < 3/2
+ * f(x) = 0                          for  3/2 <= x
+ *
+ * This kernel has a range of 1.5 voxels. For sampling in the index space of i <= x <= i+1
+ * the contribution of points [i-1, i, i+1, i+2] must be considered.
+ * Shifting the kernel function to these voxel locations yields these contributions:
+ * v(x) = v[i-1]*f(x+1) +   v[i]*f(x) + v[i+1]*f(x-1) + v[i+2]*f(x-2)
+ *      =      A*f(x+1) +      B*f(x) +      C*f(x-1) +      D*f(x-2)
+ *
+ * This results in the following expressions for sampling in one dimension:
+ * For 0 <= x < 1/2:
+ *   v(x) =   x^2*( 1/2*A     - B + 1/2*C)
+ *          +   x*(-1/2*A         + 1/2*C)
+ *          +     ( 1/8*A + 6/8*B + 1/8*C)
+ * For 1/2 <= x < 1:
+ *   v(x) =   x^2*( 1/2*B     - C + 1/2*D)
+ *          +   x*(-3/2*B   + 2*C - 1/2*D)
+ *          +     ( 9/8*B - 2/8*C + 1/8*D)
+ *
+ * and for the derivative:
+ * For 0 <= x < 1/2:
+ *   dv(x) =    x*(     A   - 2*B     + C)
+ *           +    (-1/2*A         + 1/2*C)
+ * For 1/2 <= x < 1:
+ *   dv(x) =    x*(     B   - 2*C     + D)
+ *           +    (-3/2*B   + 2*C - 1/2*D)
+ */
+struct QuadraticBSplineKernel {
+  static constexpr float range = 1.5f;
+  static constexpr int size = 4;
+
+  static float weight(float x)
+  {
+    if (x < -1.5f) {
+      return 0.0f;
+    }
+    if (x < -0.5f) {
+      return (0.5f * x + 1.5f) * x + 1.125f;
+    }
+    if (x < 0.5f) {
+      return -x * x + 0.75f;
+    }
+    if (x < 1.5f) {
+      return (0.5f * x - 1.5f) * x + 1.125f;
+    }
+    return 0.0f;
+  }
+
+  static float derivative(float x)
+  {
+    if (x < -1.5f) {
+      return 0.0f;
+    }
+    if (x < -0.5f) {
+      return x + 1.5f;
+    }
+    if (x < 0.5f) {
+      return -2.0f * x;
+    }
+    if (x < 1.5f) {
+      return x - 1.5f;
+    }
+    return 0.0f;
+  }
+
+  template<class ValueT> static ValueT weight(const ValueT *value, float weight)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    if (weight < 0.5) {
+      const ValueT sqr = static_cast<ValueT>(0.5 * (value[0] + value[2]) - value[1]);
+      const ValueT lin = static_cast<ValueT>(0.5 * (value[2] - value[0]));
+      const ValueT con = static_cast<ValueT>(0.125 * (value[0] + value[2]) + 0.75 * value[1]);
+      return weight * (weight * sqr + lin) + con;
+    }
+
+    const ValueT sqr = static_cast<ValueT>(0.5 * (value[1] + value[3]) - value[2]);
+    const ValueT lin = static_cast<ValueT>(-1.5 * value[1] + 2 * value[2] - 0.5 * value[3]);
+    const ValueT con = static_cast<ValueT>(1.125 * value[1] - 0.25 * value[2] + 0.125 * value[3]);
+    return weight * (weight * sqr + lin) + con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+
+  template<class ValueT> static ValueT derivative(const ValueT *value, float weight)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    if (weight < 0.5) {
+      const ValueT lin = static_cast<ValueT>(value[0] - 2.0 * value[1] + value[2]);
+      const ValueT con = static_cast<ValueT>(0.5 * (value[2] - value[0]));
+      return weight * lin + con;
+    }
+
+    const ValueT lin = static_cast<ValueT>(value[1] - 2.0 * value[2] + value[3]);
+    const ValueT con = static_cast<ValueT>(-1.5 * value[1] + 2.0 * value[2] - 0.5 * value[3]);
+    return weight * lin + con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+};
+
+/**
+ * Cubic B-spline kernel function as described in
+ * Steffen et al., "Analysis and reduction of quadrature errors in the material point method (MPM)"
+ *
+ * The kernel is a piece-wise cubic spline:
+ * f(x) = 0                                 for       x  < -2
+ * f(x) = 1/6*x^3 + x^2 + 2*x + 4/3         for  -2 <= x < -1
+ * f(x) = -1/2*x^3 - x^2 + 2/3              for  -1 <= x < 0
+ * f(x) = 1/2*x^3 - x^2 + 2/3               for   0 <= x < 1
+ * f(x) = -1/6*x^3 + x^2 - 2*x + 4/3        for   1 <= x < 2
+ * f(x) = 0                                 for   2 <= x
+ *
+ * The derivative is a piece-wise quadratic spline:
+ * f(x) = 0                                 for        x < -2
+ * f(x) = 1/2*x^2 + 2*x + 2                 for  -2 <= x < -1
+ * f(x) = -3/2*x^2 - 2*x                    for  -1 <= x < 1
+ * f(x) = 3/2*x^2 - 2*x                     for   0 <= x < 1
+ * f(x) = -1/2*x^2 + 2*x - 2                for   1 <= x < 2
+ * f(x) = 0                                 for   2 <= x
+ *
+ * This kernel has a range of 2 voxels. For sampling in the index space of i <= x <= i+1
+ * the contribution of points [i-1, i, i+1, i+2] must be considered.
+ * Shifting the kernel function to these voxel locations yields these contributions:
+ * v(x) = v[i-1]*f(x+1) +   v[i]*f(x) + v[i+1]*f(x-1) + v[i+2]*f(x-2)
+ *      =      A*f(x+1) +      B*f(x) +      C*f(x-1) +      D*f(x-2)
+ *
+ * This results in the following expression for sampling in one dimension:
+ * v(x) =   x^3*(-1/6*A + 1/2*B - 1/2*C + 1/6*D)
+ *        + x^2*( 1/2*A     - B + 1/2*C)
+ *        + x  *(-1/2*A         + 1/2*C)
+ *        +     ( 1/6*A + 2/3*B + 1/6*C)
+ *
+ * and for the derivative:
+ * dv(x) =  x^2*(-1/2*A + 3/2*B - 3/2*C + 1/2*D)
+ *        + x  *(     A   - 2*B     + C)
+ *        +     (-1/2*A         + 1/2*C)
+ */
+struct CubicBSplineKernel {
+  static constexpr float range = 2.0f;
+  static constexpr int size = 4;
+
+  static float weight(float x)
+  {
+    if (x < -2.0f) {
+      return 0.0f;
+    }
+    if (x < -1.0f) {
+      return ((x / 6.0f + 1.0f) * x + 2.0f) * x + 4.0f / 3.0f;
+    }
+    if (x < 0.0f) {
+      return (-x * 0.5f - 1.0f) * x * x + 2.0f / 3.0f;
+    }
+    if (x < 1.0f) {
+      return (x * 0.5f - 1.0f) * x * x + 2.0f / 3.0f;
+    }
+    if (x < 2.0f) {
+      return ((-x / 6.0f + 1.0f) * x - 2.0f) * x + 4.0f / 3.0f;
+    }
+    return 0.0f;
+  }
+
+  static float derivative(float x)
+  {
+    if (x < -2.0f) {
+      return 0.0f;
+    }
+    if (x < -1.0f) {
+      return (0.5 * x + 2.0f) * x + 2.0f;
+    }
+    if (x < 0.0f) {
+      return (-1.5f * x - 2.0f) * x;
+    }
+    if (x < 1.0f) {
+      return (1.5f * x - 2.0f) * x;
+    }
+    if (x < 2.0f) {
+      return (-0.5 * x + 2.0f) * x - 2.0f;
+    }
+    return 0.0f;
+  }
+
+  template<class ValueT> static ValueT weight(const ValueT *value, float weight)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    constexpr double inv6 = 1.0 / 6.0;
+    const ValueT cub = static_cast<ValueT>(inv6 * (value[3] - value[0]) +
+                                           0.5 * (value[1] - value[2]));
+    const ValueT sqr = static_cast<ValueT>(0.5 * (value[0] + value[2]) - value[1]);
+    const ValueT lin = static_cast<ValueT>(0.5 * (value[2] - value[0]));
+    const ValueT con = static_cast<ValueT>(inv6 * (value[0] + 4.0 * value[1] + value[2]));
+    return weight * (weight * (weight * cub + sqr) + lin) + con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+
+  template<class ValueT> static ValueT derivative(const ValueT *value, float weight)
+  {
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_BEGIN
+    const ValueT sqr = static_cast<ValueT>(0.5 * (value[3] - value[0]) +
+                                           1.5 * (value[1] - value[2]));
+    const ValueT lin = static_cast<ValueT>(value[0] - 2.0 * value[1] + value[2]);
+    const ValueT con = static_cast<ValueT>(0.5 * (value[2] - value[0]));
+    return weight * (weight * sqr + lin) + con;
+    OPENVDB_NO_TYPE_CONVERSION_WARNING_END
+  }
+};
+
+/**
+ * Grid value sampler using a kernel type.
+ */
+template<typename KernelT> struct SamplerWithKernel {
+  template<class AccessorT>
+  static bool sample(const AccessorT &accessor,
+                     const openvdb::Vec3R &coord,
+                     typename AccessorT::ValueType &result)
+  {
+    return grid_sampling::sample_tree<KernelT>(accessor, coord, result);
+  }
+
+  template<class AccessorT>
+  static typename AccessorT::ValueType sample(const AccessorT &accessor,
+                                              const openvdb::Vec3R &coord)
+  {
+    return grid_sampling::sample_tree<KernelT>(accessor, coord);
+  }
+};
+
+}  // namespace grid_sampling
+
+/**
+ * Grid value sampler using linear kernels.
+ */
+using LinearSampler = grid_sampling::SamplerWithKernel<grid_sampling::LinearKernel>;
+
+/**
+ * Grid value sampler using quadratic B-spline kernels.
+ */
+using QuadraticBSplineSampler =
+    grid_sampling::SamplerWithKernel<grid_sampling::QuadraticBSplineKernel>;
+
+/**
+ * Grid value sampler using cubic B-spline kernels.
+ */
+using CubicBSplineSampler = grid_sampling::SamplerWithKernel<grid_sampling::CubicBSplineKernel>;
+
+#endif
+
+}  // namespace blender::geometry
