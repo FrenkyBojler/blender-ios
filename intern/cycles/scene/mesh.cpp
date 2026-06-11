blender/blender

commit 24b981b1cd03900eca5481f54eaf9b0546e7d826

Fix: Cycles: Incorrect handling of subd for generated coordinates creation
Does not cause an actual bug in Blender integration since these are already
populated at this point.

Pull Request: https://projects.blender.org/blender/blender/pulls/159334

intern/cycles/scene/mesh.cpp
@@ -795,7 +795,7 @@ void Mesh::add_undisplaced(Scene *scene)
 
 void Mesh::update_generated(Scene *scene)
 {
-  if (!num_triangles()) {
+  if (!num_triangles() && !num_subd_faces) {
     return;
   }
 

FrenkyBojler/blender-ios

commit 9e784beba605e72dc228572dbf7e820573b12867

Fix: Cycles: Incorrect generated coordinates for subdivision
This should get the position attribute from the subdivision attributes,
not get_position().. This code does not run in the Blender integration.

This issue was introduced in 24b981b1cd0.

intern/cycles/scene/mesh.cpp
@@ -803,12 +803,15 @@ void Mesh::update_generated(Scene *scene)
 
   /* apply generated attributes if needed or missing */
   if (need_attribute(scene, ATTR_STD_GENERATED) && !attrs.find(ATTR_STD_GENERATED)) {
-    const size_t verts_size = num_verts();
-    const packed_float3 *verts = get_position();
-    Attribute *attr_generated = attrs.add(ATTR_STD_GENERATED);
-    packed_float3 *generated = attr_generated->data_for_write<packed_float3>();
-    for (size_t i = 0; i < verts_size; ++i) {
-      generated[i] = verts[i];
+    const Attribute *attr_P = attrs.find(ATTR_STD_POSITION);
+    if (attr_P) {
+      const size_t verts_size = attr_P->size;
+      const packed_float3 *verts = attr_P->data<packed_float3>();
+      Attribute *attr_generated = attrs.add(ATTR_STD_GENERATED);
+      packed_float3 *generated = attr_generated->data_for_write<packed_float3>();
+      for (size_t i = 0; i < verts_size; ++i) {
+        generated[i] = verts[i];
+      }
     }
   }
 }