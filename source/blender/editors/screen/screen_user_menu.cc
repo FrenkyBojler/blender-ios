blender/blender

commit 1070d2b5a26ba18e4da0e43766f5810f85415a29

Fix #155581: Quick Favorites: some items are not removable from the menu
`ED_screen_user_menu_item_find_operator` fails to return operator due to
`IDP_EqualsProperties/ok_idprop` being false. Buttons defined with
`template_id_def_new_but` doesn't allocate idpropgroup, so apparently
`but->opptr->data` is null. But when added in quick favorites menu,
`layout->op/uiItemFullO_ptr_ex` creates idpropgroup
"wmOperatorProperties". This mismatch case eventually fails to show
"Remove" button in quick favorites. To fix this, pass `is_strict=false`,
that way `IDP_EqualsProperties_ex` returns true when either of idprop
in comparison is null.

Pull Request: https://projects.blender.org/blender/blender/pulls/155609

source/blender/editors/screen/screen_user_menu.cc
@@ -102,7 +102,7 @@ bUserMenuItem_Op *ED_screen_user_menu_item_find_operator(ListBaseT<bUserMenuItem
   for (bUserMenuItem &umi : *lb) {
     if (umi.type == USER_MENU_TYPE_OPERATOR) {
       bUserMenuItem_Op *umi_op = reinterpret_cast<bUserMenuItem_Op *>(&umi);
-      const bool ok_idprop = prop ? IDP_EqualsProperties(prop, umi_op->prop) : true;
+      const bool ok_idprop = IDP_EqualsProperties_ex(prop, umi_op->prop, false);
       const bool ok_prop_enum = (umi_op->op_prop_enum[0] != '\0') ?
                                     STREQ(umi_op->op_prop_enum, op_prop_enum) :
                                     true;

FrenkyBojler/blender-ios

commit cecee16a7e85b3af6db1283305277db08982586d

Fix #160010: Cannot add both Mark sharp and Clear sharp commands in quick favorites
Caused by 1070d2b5a2
set `is_strict=true` when IDProperty on both operator and quick favorite item
exists.

source/blender/editors/screen/screen_user_menu.cc
@@ -102,7 +102,8 @@ bUserMenuItem_Op *ED_screen_user_menu_item_find_operator(ListBaseT<bUserMenuItem
   for (bUserMenuItem &umi : *lb) {
     if (umi.type == USER_MENU_TYPE_OPERATOR) {
       bUserMenuItem_Op *umi_op = reinterpret_cast<bUserMenuItem_Op *>(&umi);
-      const bool ok_idprop = IDP_EqualsProperties_ex(prop, umi_op->prop, false);
+      const bool is_strict = prop && umi_op->prop;
+      const bool ok_idprop = IDP_EqualsProperties_ex(prop, umi_op->prop, is_strict);
       const bool ok_prop_enum = (umi_op->op_prop_enum[0] != '\0') ?
                                     STREQ(umi_op->op_prop_enum, op_prop_enum) :
                                     true;