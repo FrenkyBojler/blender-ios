17b93af1fb78b0088f50102165986c82ba0871bc
W_Cloud <yunkezengren@outlook.com>
2026-06-13 22:57:53 +0800

    fix after merge

--- source/blender/editors/space_node/drawnode.cc
+++ source/blender/editors/space_node/drawnode.cc
@@ -1121,11 +1121,13 @@ static void draw_node_socket_name_editable(ui::Layout *layout,
     if (sock->runtime->declaration->socket_name_rna) {
       layout->alignment_set(ui::LayoutAlign::Expand);
       layout->emboss_set(ui::EmbossType::None);
-      layout->prop((&sock->runtime->declaration->socket_name_rna->owner),
-                   sock->runtime->declaration->socket_name_rna->property_name,
-                   sock->in_out == SOCK_OUT ? ui::eUI_Item_Flag::ITEM_R_TEXT_RIGHT : UI_ITEM_NONE,
-                   "",
-                   ICON_NONE);
+      layout->prop(
+          (&sock->runtime->declaration->socket_name_rna->owner),
+          sock->runtime->declaration->socket_name_rna->property_name,
+          ui::eUI_Item_Flag::ITEM_R_TEXT_BUT_LABEL_STYLE |
+              (sock->in_out == SOCK_OUT ? ui::eUI_Item_Flag::ITEM_R_TEXT_RIGHT : UI_ITEM_NONE),
+          "",
+          ICON_NONE);
       return;
     }
   }
