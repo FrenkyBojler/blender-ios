4d62f42b5d5bd8e01ecfcf56992841d9f4bd8932
Richard Antalik <richardantalik@gmail.com>
2026-06-13 02:45:26 +0200

    Fix #160001, Fix jump speed and walk speed

--- source/blender/editors/space_view3d/view3d_navigate_walk.cc
+++ source/blender/editors/space_view3d/view3d_navigate_walk.cc
@@ -513,6 +513,7 @@ static struct {
   float jump_height;
   /** Only used to detect change. */
   float userdef_jump_height;
+  float grid;
 } g_walk = {
     /*base_speed*/ -1.0f,
     /*userdef_speed*/ -1.0f,
@@ -565,17 +566,20 @@ static bool initWalkInfo(bContext *C, WalkInfo *walk, wmOperator *op, const int 
                    1.0f :
                    1.0f / walk->scene->unit.scale_length;
 
-  const float userdef_jump_height = U.walk_navigation.jump_height;
-  const float userdef_view_height = U.walk_navigation.view_height;
+  const float userdef_jump_height = U.walk_navigation.jump_height / walk->grid;
+  const float userdef_view_height = U.walk_navigation.view_height / walk->grid;
 
   if (fabsf(U.walk_navigation.walk_speed - g_walk.userdef_speed) > 0.1f) {
     g_walk.base_speed = U.walk_navigation.walk_speed;
     g_walk.userdef_speed = U.walk_navigation.walk_speed;
   }
 
-  if (fabsf(U.walk_navigation.jump_height - g_walk.userdef_jump_height) > 0.1f) {
+  if (fabsf(userdef_jump_height - g_walk.userdef_jump_height) > 0.1f ||
+      fabsf(walk->grid - g_walk.grid) > FLT_EPSILON)
+  {
     g_walk.jump_height = userdef_jump_height;
-    g_walk.userdef_jump_height = U.walk_navigation.jump_height;
+    g_walk.userdef_jump_height = userdef_jump_height;
+    g_walk.grid = walk->grid;
   }
 
   walk->jump_height = 0.0f;
