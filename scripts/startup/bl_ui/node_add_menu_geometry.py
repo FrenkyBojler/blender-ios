bee408b5e04c86318e90b1f0043d16720046bfeb
Tibo Stans <192132910+stanzerelli@users.noreply.github.com>
2026-06-14 19:54:41 +0200

    Geometry Nodes: Add MIDI node to add menu

--- scripts/startup/bl_ui/node_add_menu_geometry.py
+++ scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -696,6 +696,7 @@ class NODE_MT_gn_utilities_sound_base(node_add_menu.NodeMenu):
 
     def draw(self, context):
         layout = self.layout
+        self.node_operator(layout, "GeometryNodeSampleMIDINote")
         self.node_operator(layout, "GeometryNodeSampleSoundFrequencies")
 
 
