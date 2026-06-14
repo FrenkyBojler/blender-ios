blender/blender

commit 131c8ec9f8980835ca317a2eb526d96aeab02e7b

Fix: crash running mesh.normals_tools on a vertex without linked faces
Ref !159841

source/blender/editors/mesh/editmesh_tools.cc
@@ -9545,7 +9545,7 @@ static wmOperatorStatus edbm_normals_tools_exec(bContext *C, wmOperator *op)
             }
           }
         }
-        else {
+        else if (lnors_ed_arr->totloop != 0) {
           /* 'Vertex' normal, i.e. common set of loop normals on the same vertex,
            * only if they are all the same. */
           bool are_same_lnors = true;

FrenkyBojler/blender-ios

commit 55d43f7d929980e28def28e2d09eaf22bd3129a7

Fix: custom normal copy flag set even when no copy was performed
Move done_copy = true inside each branch so it only marks a copy as
done when one actually occurred.

This issue was introduced in 131c8ec9f89.

source/blender/editors/mesh/editmesh_tools.cc
@@ -9535,6 +9535,7 @@ static wmOperatorStatus edbm_normals_tools_exec(bContext *C, wmOperator *op)
         }
         if (lnors_ed_arr->totloop == 1) {
           copy_v3_v3(scene->toolsettings->normal_vector, lnors_ed_arr->lnor_editdata->nloc);
+          done_copy = true;
         }
         else if (bm->totfacesel == 1) {
           BMFace *f;
@@ -9544,6 +9545,7 @@ static wmOperatorStatus edbm_normals_tools_exec(bContext *C, wmOperator *op)
               copy_v3_v3(scene->toolsettings->normal_vector, f->no);
             }
           }
+          done_copy = true;
         }
         else if (lnors_ed_arr->totloop != 0) {
           /* 'Vertex' normal, i.e. common set of loop normals on the same vertex,
@@ -9557,8 +9559,8 @@ static wmOperatorStatus edbm_normals_tools_exec(bContext *C, wmOperator *op)
           if (are_same_lnors) {
             copy_v3_v3(scene->toolsettings->normal_vector, lnors_ed_arr->lnor_editdata->nloc);
           }
+          done_copy = true;
         }
-        done_copy = true;
         break;
 
       case EDBM_CLNOR_TOOLS_PASTE: