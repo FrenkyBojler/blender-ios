dd99394ce9c929a50d0c8b1a5685a78898c26900
Guillermo Venegas <guillermovcra@gmail.com>
2026-06-15 08:14:32 -0600

    Fix Typo

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -781,12 +781,12 @@ const char *ED_info_statusbar_string_ex(Main *bmain,
                                   FRA2TIME(frame_count),
                                   scene->frames_per_second(),
                                   U.timecode_style);
-    const std::string scnene_duration = fmt::format(
+    const std::string scene_duration = fmt::format(
         fmt::runtime(IFACE_("Duration: {} (Frame {}/{})")),
         timecode,
         relative_current_frame,
         frame_count);
-    ofs += BLI_strncpy_utf8_rlen(info + ofs, scnene_duration.c_str(), len - ofs);
+    ofs += BLI_strncpy_utf8_rlen(info + ofs, scene_duration.c_str(), len - ofs);
   }
 
   /* Memory status. */
