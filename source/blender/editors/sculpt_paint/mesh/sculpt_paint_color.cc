blender/blender

commit b8ba40e7e0fd1727c9e3364f87b70e070097d5e6

Refactor: Sculpt: Generalize tip roundness helpers
This PR refactors tip roundness logic in prepraration for making the
functionality available to more brushes within Sculpt mode.

The following changes were done:
* Added tip roundness as a brush capability
* Extracted shared helpers for calculating factors for cube tips
* Updated cube tip distance calculation and node gathering.

Pull Request: https://projects.blender.org/blender/blender/pulls/155212

source/blender/editors/sculpt_paint/mesh/sculpt_paint_color.cc
@@ -41,17 +41,6 @@
 
 namespace blender::ed::sculpt_paint::color {
 
-static void calc_local_positions(const float4x4 &mat,
-                                 const Span<int> verts,
-                                 const Span<float3> positions,
-                                 const MutableSpan<float3> local_positions)
-{
-  PRF_scope(ProfileCategory::Editor);
-  for (const int i : verts.index_range()) {
-    local_positions[i] = math::transform_point(mat, positions[verts[i]]);
-  }
-}
-
 template<typename Func> inline void to_static_color_type(const CPPType &type, const Func &func)
 {
   if (type.is<ColorGeometry4f>()) {
@@ -255,6 +244,8 @@ struct ColorPaintLocalData {
   Vector<float> factors;
   Vector<float> auto_mask;
   Vector<float3> positions;
+  Vector<float2> xy_positions;
+  Vector<float> z_positions;
   Vector<float> distances;
   Vector<float4> colors;
   Vector<float4> new_colors;
@@ -384,9 +375,12 @@ static void do_paint_brush_task(const Depsgraph &depsgraph,
   tls.distances.resize(verts.size());
   const MutableSpan<float> distances = tls.distances;
   if (brush.tip_roundness < 1.0f) {
-    tls.positions.resize(verts.size());
-    calc_local_positions(mat, verts, vert_positions, tls.positions);
-    calc_brush_cube_distances<float3>(brush, tls.positions, distances);
+    tls.xy_positions.resize(verts.size());
+    tls.z_positions.resize(verts.size());
+    MutableSpan<float2> xy_positions = tls.xy_positions;
+    MutableSpan<float> z_positions = tls.z_positions;
+    calc_local_positions(vert_positions, verts, mat, tls.xy_positions, tls.z_positions);
+    calc_brush_cube_distances<float2>(brush, tls.xy_positions, distances);
     radius = 1.0f;
   }
   else {

scripts/startup/bl_ui/properties_paint_common.py
@@ -844,6 +844,10 @@ def brush_settings(layout, context, brush, popover=False):
                 row.prop(brush, "invert_hardness_pressure", text="")
                 row.prop(brush, "use_hardness_pressure", text="")
 
+        if capabilities.has_tip_roundness:
+            layout.prop(brush, "tip_roundness", slider=True)
+            layout.prop(brush, "tip_scale_x", slider=True)
+
         # auto_smooth_factor and use_inverse_smooth_pressure
         if capabilities.has_auto_smooth:
             pressure_name = "use_inverse_smooth_pressure" if capabilities.has_auto_smooth_pressure else None
@@ -932,14 +936,7 @@ def brush_settings(layout, context, brush, popover=False):
 
         # Per sculpt tool options.
 
-        if sculpt_brush_type == 'CLAY_STRIPS':
-            row = layout.row()
-            row.prop(brush, "tip_roundness")
-
-            row = layout.row()
-            row.prop(brush, "tip_scale_x")
-
-        elif sculpt_brush_type == 'ELASTIC_DEFORM':
+        if sculpt_brush_type == 'ELASTIC_DEFORM':
             layout.separator()
             layout.prop(brush, "elastic_deform_type")
             layout.prop(brush, "elastic_deform_volume_preservation", slider=True)
@@ -1032,12 +1029,6 @@ def brush_settings(layout, context, brush, popover=False):
             row.prop(brush, "density")
             row.prop(brush, "invert_density_pressure", text="")
             row.prop(brush, "use_density_pressure", text="")
-
-            row = layout.row()
-            row.prop(brush, "tip_roundness")
-
-            row = layout.row()
-            row.prop(brush, "tip_scale_x")
 
         elif sculpt_brush_type == 'SMEAR':
             col = layout.column()

source/blender/blenkernel/BKE_brush.hh
@@ -250,6 +250,7 @@ bool supports_accumulate(const Brush &brush);
 bool supports_topology_rake(const Brush &brush);
 bool supports_auto_smooth(const Brush &brush);
 bool supports_normal_radius(const Brush &brush);
+bool supports_tip_roundness(const Brush &brush);
 bool supports_hardness(const Brush &brush);
 bool supports_height(const Brush &brush);
 bool supports_plane_height(const Brush &brush);

source/blender/blenkernel/intern/brush.cc
@@ -1738,7 +1738,7 @@ bool BKE_brush_has_cube_tip(const Brush *brush, PaintMode paint_mode)
         return true;
       }
 
-      if (ELEM(brush->sculpt_brush_type, SCULPT_BRUSH_TYPE_CLAY_STRIPS, SCULPT_BRUSH_TYPE_PAINT) &&
+      if (bke::brush::supports_tip_roundness(*brush) &&
           (brush->tip_roundness < 1.0f || brush->tip_scale_x != 1.0f))
       {
         return true;
@@ -1858,6 +1858,10 @@ bool supports_normal_radius(const Brush &brush)
   /* TODO: This setting is closely tied to #supports_sculpt_plane, they should be merged in some
    * way. Update after initial commit to avoid confusing PRs. */
   return !ELEM(brush.sculpt_brush_type, SCULPT_BRUSH_TYPE_POSE);
+}
+bool supports_tip_roundness(const Brush &brush)
+{
+  return ELEM(brush.sculpt_brush_type, SCULPT_BRUSH_TYPE_CLAY_STRIPS, SCULPT_BRUSH_TYPE_PAINT);
 }
 bool supports_hardness(const Brush &brush)
 {

source/blender/editors/sculpt_paint/mesh/brushes/clay_strips.cc
@@ -35,8 +35,6 @@
 #include "BLI_math_vector.hh"
 #include "BLI_task.hh"
 
-#include "PRF_profile.hh"
-
 #include "editors/sculpt_paint/mesh/mesh_brush_common.hh"
 #include "editors/sculpt_paint/mesh/sculpt_automask.hh"
 #include "editors/sculpt_paint/mesh/sculpt_intern.hh"
@@ -56,51 +54,6 @@ struct LocalData {
   Vector<float3> translations;
 };
 
-/**
- * Transforms positions from object space positions to brush-local space. Splitting the XY and Z
- * components gives slightly better performance.
- */
-static void calc_local_positions(const Span<float3> vert_positions,
-                                 const Span<int> verts,
-                                 const float4x4 &mat,
-                                 const MutableSpan<float2> xy_positions,
-                                 const MutableSpan<float> z_positions)
-{
-  PRF_scope(ProfileCategory::Editor);
-  BLI_assert(xy_positions.size() == verts.size());
-  BLI_assert(z_positions.size() == verts.size());
-
-  for (const int i : verts.index_range()) {
-    const float3 position = math::transform_point(mat, vert_positions[verts[i]]);
-
-    xy_positions[i] = position.xy();
-    z_positions[i] = position.z;
-  }
-}
-
-static void calc_local_positions(const Span<float3> positions,
-                                 const float4x4 &mat,
-                                 const MutableSpan<float2> xy_positions,
-                                 const MutableSpan<float> z_positions)
-{
-  PRF_scope(ProfileCategory::Editor);
-  BLI_assert(xy_positions.size() == positions.size());
-  BLI_assert(z_positions.size() == positions.size());
-
-  for (const int i : positions.index_range()) {
-    const float3 position = math::transform_point(mat, positions[i]);
-
-    xy_positions[i] = position.xy();
-    z_positions[i] = position.z;
-  }
-}
-
-/**
- * Applies a parabolic factor of the form `z * (1 - z)` to each vertex.
- * Vertices outside of the interval (0, 1) are out of range and their factors are set to zero.
- * Note: The local coordinate system is constructed such that all relevant `z` values
- * are non-negative.
- */
 static void apply_z_axis_factors(const Span<float> z_positions, const MutableSpan<float> factors)
 {
   PRF_scope(ProfileCategory::Editor);
@@ -385,75 +338,6 @@ void do_clay_strips_brush(const Depsgraph &depsgraph,
 
 namespace clay_strips {
 
-/**
- * Checks whether the node's bounding box overlaps with the region affected by the brush.
- * Clay Strips affects only vertices below the brush plane. The brush-local coordinate
- * system is oriented so that vertices below the plane have positive local z-coordinates.
- * Therefore, we only need to check if the node intersects the [-1,1] x [-1,1] x [0,1] volume in
- * local space.
- */
-static bool node_in_box(const float4x4 &mat, const Bounds<float3> &bounds)
-{
-  const float3 brush_center = float3(0.0f, 0.0f, 0.5f);
-  const float3 node_center = math::transform_point(mat, (bounds.max + bounds.min) * 0.5f);
-  const float3 center_diff = brush_center - node_center;
-
-  const float3 brush_half_lengths = float3(1.0f, 1.0f, 0.5f);
-  const float3 node_half_lengths = (bounds.max - bounds.min) * 0.5f;
-
-  const float3 &node_x_axis = mat.x_axis();
-  const float3 &node_y_axis = mat.y_axis();
-  const float3 &node_z_axis = mat.z_axis();
-
-  /* Tests if `axis` separates the boxes. */
-  auto axis_separates_boxes = [&](const float3 &axis) {
-    const float radius1 = math::dot(math::abs(axis), brush_half_lengths);
-    const float radius2 = math::abs(math::dot(axis, node_x_axis)) * node_half_lengths.x +
-                          math::abs(math::dot(axis, node_y_axis)) * node_half_lengths.y +
-                          math::abs(math::dot(axis, node_z_axis)) * node_half_lengths.z;
-
-    const float projection = math::abs(math::dot(center_diff, axis));
-
-    return projection > radius1 + radius2;
-  };
-
-  const std::array<float3, 3> brush_axes = {
-      float3{1.0f, 0.0f, 0.0f}, float3{0.0f, 1.0f, 0.0f}, float3{0.0f, 0.0f, 1.0f}};
-  const std::array<float3, 3> node_axes = {node_x_axis, node_y_axis, node_z_axis};
-
-  /**
-   * Intersection is tested using the Separating Axis Theorem.
-   * Two boxes (not necessarily axis-aligned) intersect if and only if there does not exist an axis
-   * that separates them. In particular, it is necessary and sufficient to:
-   */
-
-  /* 1. Test axes aligned with the region affected by the brush. */
-  for (const float3 &axis : brush_axes) {
-    if (axis_separates_boxes(axis)) {
-      return false;
-    }
-  }
-
-  /* 2. Test axes aligned with the node bounds. */
-  for (const float3 &axis : node_axes) {
-    if (axis_separates_boxes(axis)) {
-      return false;
-    }
-  }
-
-  /* 3. Test all their cross products. */
-  for (const float3 &brush_axis : brush_axes) {
-    for (const float3 &node_axis : node_axes) {
-      if (axis_separates_boxes(math::cross(brush_axis, node_axis))) {
-        return false;
-      }
-    }
-  }
-
-  /* None of the axes separates the boxes: they intersect. */
-  return true;
-}
-
 float4x4 calc_local_matrix(const Brush &brush,
                            const StrokeCache &cache,
                            const float3 &plane_normal,
@@ -524,7 +408,7 @@ CursorSampleResult calc_node_mask(const Depsgraph &depsgraph,
         if (node_fully_masked_or_hidden(node)) {
           return false;
         }
-        return node_in_box(mat, node.bounds());
+        return node_in_box_positive_z(node.bounds(), mat);
       });
 
   return {plane_mask, plane_center, plane_normal};

source/blender/editors/sculpt_paint/mesh/mesh_brush_common.hh
@@ -210,6 +210,26 @@ void calc_factors_common_mesh_indexed(const Depsgraph &depsgraph,
                                       const bke::pbvh::MeshNode &node,
                                       MutableSpan<float> factors,
                                       MutableSpan<float> distances);
+void calc_cube_tip_factors_common_mesh_indexed(const Depsgraph &depsgraph,
+                                               const Brush &brush,
+                                               const Object &object,
+                                               const float4x4 &mat,
+                                               const MeshAttributeData &attribute_data,
+                                               const Span<float3> vert_positions,
+                                               const Span<float3> vert_normals,
+                                               const bke::pbvh::MeshNode &node,
+                                               Vector<float> &r_factors,
+                                               Vector<float> &r_distances);
+void calc_cube_tip_factors_common_mesh_indexed(const Depsgraph &depsgraph,
+                                               const Brush &brush,
+                                               const Object &object,
+                                               const float4x4 &mat,
+                                               const MeshAttributeData &attribute_data,
+                                               Span<float3> vert_positions,
+                                               Span<float3> vert_normals,
+                                               const bke::pbvh::MeshNode &node,
+                                               MutableSpan<float> factors,
+                                               MutableSpan<float> distances);
 void calc_factors_common_grids(const Depsgraph &depsgraph,
                                const Brush &brush,
                                const Object &object,
@@ -224,6 +244,22 @@ void calc_factors_common_bmesh(const Depsgraph &depsgraph,
                                bke::pbvh::BMeshNode &node,
                                Vector<float> &r_factors,
                                Vector<float> &r_distances);
+void calc_cube_tip_factors_common_grids(const Depsgraph &depsgraph,
+                                        const Brush &brush,
+                                        const Object &object,
+                                        const float4x4 &mat,
+                                        Span<float3> positions,
+                                        const bke::pbvh::GridsNode &node,
+                                        Vector<float> &r_factors,
+                                        Vector<float> &r_distances);
+void calc_cube_tip_factors_common_bmesh(const Depsgraph &depsgraph,
+                                        const Brush &brush,
+                                        const Object &object,
+                                        const float4x4 &mat,
+                                        Span<float3> positions,
+                                        bke::pbvh::BMeshNode &node,
+                                        Vector<float> &r_factors,
+                                        Vector<float> &r_distances);
 void calc_factors_common_from_orig_data_mesh(const Depsgraph &depsgraph,
                                              const Brush &brush,
                                              const Object &object,
@@ -529,6 +565,32 @@ void filter_above_plane_factors(Span<float3> vert_positions,
 void filter_above_plane_factors(Span<float3> positions,
                                 const float4 &plane,
                                 MutableSpan<float> factors);
+
+/* Transforms positions from object space positions to brush-local space. */
+void calc_local_positions(const Span<float3> vert_positions,
+                          const Span<int> verts,
+                          const float4x4 &mat,
+                          const MutableSpan<float3> local_positions);
+
+void calc_local_positions(const Span<float3> positions,
+                          const float4x4 &mat,
+                          const MutableSpan<float3> local_positions);
+
+/**
+ * Transforms positions from object space positions to brush-local space. Splitting the XY and Z
+ * components gives slightly better performance. Used by some brushes that only need the XY
+ * components for certain calculations.
+ */
+void calc_local_positions(const Span<float3> vert_positions,
+                          const Span<int> verts,
+                          const float4x4 &mat,
+                          const MutableSpan<float2> xy_positions,
+                          const MutableSpan<float> z_positions);
+
+void calc_local_positions(const Span<float3> positions,
+                          const float4x4 &mat,
+                          const MutableSpan<float2> xy_positions,
+                          const MutableSpan<float> z_positions);
 
 }  // namespace ed::sculpt_paint
 

FrenkyBojler/blender-ios

commit c9df1ae03c3d2c540ee3a4d2b28d92af45ca99fe

Fix: Silent warning
Caused by b8ba40e7e0, variable are not used anywhere.

source/blender/editors/sculpt_paint/mesh/sculpt_paint_color.cc
@@ -377,8 +377,6 @@ static void do_paint_brush_task(const Depsgraph &depsgraph,
   if (brush.tip_roundness < 1.0f) {
     tls.xy_positions.resize(verts.size());
     tls.z_positions.resize(verts.size());
-    MutableSpan<float2> xy_positions = tls.xy_positions;
-    MutableSpan<float> z_positions = tls.z_positions;
     calc_local_positions(vert_positions, verts, mat, tls.xy_positions, tls.z_positions);
     calc_brush_cube_distances<float2>(brush, tls.xy_positions, distances);
     radius = 1.0f;