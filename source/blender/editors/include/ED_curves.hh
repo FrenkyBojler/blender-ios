Zezo-Ai/blender

commit 66ee7f79c9d106a00c72fec6f354f3aeb417d947

Grease Pencil: Deselect linked
Add "deselect" property to "select linked" operator. Property value is
passed to `ed::curves::select_linked`. When `deselect=true` and curve
is completely selected (`is_curve_selected=true`), clear that curve
selection.
Use `Shift + L` to invoke the operator with deselect property.

Resolves #157245

See PR description for the video

Pull Request: https://projects.blender.org/blender/blender/pulls/157260

source/blender/editors/include/ED_curves.hh
@@ -389,8 +389,10 @@ void select_all(bke::CurvesGeometry &curves,
  *
  * \param curves_mask: (optional) The curves that should be affected.
  */
-void select_linked(bke::CurvesGeometry &curves);
-void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask);
+void select_linked(bke::CurvesGeometry &curves, const bool unselect = true);
+void select_linked(bke::CurvesGeometry &curves,
+                   const IndexMask &curves_mask,
+                   const bool unselect = false);
 
 /**
  * Select alternated points in strokes with already selected points

scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -3963,6 +3963,8 @@ def km_grease_pencil_selection(params):
         ("grease_pencil.select_fill", {"type": 'L', "value": 'PRESS', "ctrl": True}, None),
         # Select linked
         ("grease_pencil.select_linked", {"type": 'L', "value": 'PRESS'}, None),
+        ("grease_pencil.select_linked", {"type": 'L', "value": 'PRESS', "shift": True},
+         {"properties": [("deselect", True)]}),
         # Select more/less
         ("grease_pencil.select_more", {"type": 'NUMPAD_PLUS', "value": 'PRESS', "ctrl": True, "repeat": True}, None),
         ("grease_pencil.select_less", {"type": 'NUMPAD_MINUS', "value": 'PRESS', "ctrl": True, "repeat": True}, None),

source/blender/editors/curves/intern/curves_selection.cc
@@ -502,7 +502,7 @@ void select_all(bke::CurvesGeometry &curves, const bke::AttrDomain selection_dom
   select_all(curves, selection, selection_domain, action);
 }
 
-void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask)
+void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask, const bool unselect)
 {
   const OffsetIndices points_by_curve = curves.points_by_curve();
   const VArray<int8_t> curve_types = curves.curve_types();
@@ -521,17 +521,24 @@ void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask)
                                              all_writers :
                                              selection_writer;
         const IndexRange points = points_by_curve[curve];
-
         for (const int i : curve_writers) {
           bke::GSpanAttributeWriter &selection = selection_writers[i];
           GMutableSpan selection_curve = selection.span.slice(points);
-          if (has_anything_selected(selection_curve)) {
-            fill_selection_true(selection_curve);
+          const array_utils::BooleanMix selection_state = array_utils::booleans_mix_calc(
+              VArray<bool>::from_span(selection_curve.typed<bool>()));
+          const bool all_points_selected = selection_state == array_utils::BooleanMix::AllTrue;
+          if (selection_state != array_utils::BooleanMix::AllFalse) {
+            if (unselect == all_points_selected) {
+              fill_selection(selection_curve, !unselect);
+            }
+
             for (const int j : curve_writers) {
               if (j == i) {
                 continue;
               }
-              fill_selection_true(selection_writers[j].span.slice(points));
+              if (unselect == all_points_selected) {
+                fill_selection(selection_curve, !unselect);
+              }
             }
             return;
           }
@@ -541,9 +548,9 @@ void select_linked(bke::CurvesGeometry &curves, const IndexMask &curves_mask)
   finish_attribute_writers(selection_writers);
 }
 
-void select_linked(bke::CurvesGeometry &curves)
+void select_linked(bke::CurvesGeometry &curves, const bool unselect)
 {
-  select_linked(curves, curves.curves_range());
+  select_linked(curves, curves.curves_range(), unselect);
 }
 
 void select_alternate(bke::CurvesGeometry &curves,

source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -430,11 +430,12 @@ static void GREASE_PENCIL_OT_select_less(wmOperatorType *ot)
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 }
 
-static wmOperatorStatus select_linked_exec(bContext *C, wmOperator * /*op*/)
+static wmOperatorStatus select_linked_exec(bContext *C, wmOperator *op)
 {
   Scene *scene = CTX_data_scene(C);
   Object *object = CTX_data_active_object(C);
   GreasePencil &grease_pencil = *id_cast<GreasePencil *>(object->data);
+  const bool unselect = RNA_boolean_get(op->ptr, "deselect");
 
   const Vector<MutableDrawingInfo> drawings = retrieve_editable_drawings(*scene, grease_pencil);
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
@@ -444,7 +445,7 @@ static wmOperatorStatus select_linked_exec(bContext *C, wmOperator * /*op*/)
     if (selectable_strokes.is_empty()) {
       return;
     }
-    ed::curves::select_linked(info.drawing.strokes_for_write(), selectable_strokes);
+    ed::curves::select_linked(info.drawing.strokes_for_write(), selectable_strokes, unselect);
   });
 
   /* Use #ID_RECALC_GEOMETRY instead of #ID_RECALC_SELECT because it is handled as a generic
@@ -465,6 +466,10 @@ static void GREASE_PENCIL_OT_select_linked(wmOperatorType *ot)
   ot->poll = editable_grease_pencil_point_selection_poll;
 
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+
+  PropertyRNA *prop = RNA_def_boolean(
+      ot->srna, "deselect", false, "Deselect linked", "Deselect linked strokes");
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
 }
 
 static wmOperatorStatus select_random_exec(bContext *C, wmOperator *op)

FrenkyBojler/blender-ios

commit 291800bb7a5594120d45ef16c5bd3137582592af

Fix #160071: Select linked doesn't work on curves
Caused by 66ee7f79c9. Wrong default parameter in select_linked() function.

source/blender/editors/include/ED_curves.hh
@@ -389,7 +389,7 @@ void select_all(bke::CurvesGeometry &curves,
  *
  * \param curves_mask: (optional) The curves that should be affected.
  */
-void select_linked(bke::CurvesGeometry &curves, const bool unselect = true);
+void select_linked(bke::CurvesGeometry &curves, const bool unselect = false);
 void select_linked(bke::CurvesGeometry &curves,
                    const IndexMask &curves_mask,
                    const bool unselect = false);