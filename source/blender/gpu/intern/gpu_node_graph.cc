73fcad50acf9a2e9762cfb555a012aa7b547ee50
Brecht Van Lommel <brecht@blender.org>
2026-06-16 16:32:05 +0200

    Address comments

--- source/blender/gpu/intern/gpu_node_graph.cc
+++ source/blender/gpu/intern/gpu_node_graph.cc
@@ -501,9 +501,12 @@ static GPULayerAttr *gpu_node_graph_add_layer_attribute(GPUNodeGraph *graph, con
 
 static bool gpu_image_user_match(const GPUMaterialTexture *tex, const ImageUser *iuser)
 {
-  return (tex->iuser_available == (iuser != nullptr)) ?
-             (tex->iuser_available) ? BKE_image_user_match(tex->iuser, *iuser) : true :
-             false;
+  const bool iuser_available = iuser != nullptr;
+  if (tex->iuser_available != iuser_available) {
+    return false;
+  }
+
+  return (tex->iuser_available) ? BKE_image_user_match(tex->iuser, *iuser) : true;
 }
 
 static GPUMaterialTexture *gpu_node_graph_add_texture(GPUNodeGraph *graph,
