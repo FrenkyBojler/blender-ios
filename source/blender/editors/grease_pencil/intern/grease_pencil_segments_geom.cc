a82a74a1a9f6b347f0b5d7b9deab1290759819dc
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-06-15 22:04:02 -0700

    Fix: Merge error.

--- source/blender/editors/grease_pencil/intern/grease_pencil_segments_geom.cc
+++ source/blender/editors/grease_pencil/intern/grease_pencil_segments_geom.cc
@@ -14,7 +14,7 @@
 #include "BLI_bounds.hh"
 #include "BLI_lasso_2d.hh"
 #include "BLI_math_base.hh"
-#include "BLI_math_geom.h"
+#include "BLI_math_geom.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_offset_indices.hh"
 #include "BLI_sort.hh"
