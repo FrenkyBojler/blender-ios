e213314fedccc30cbdf9185c824dbdf902be6bed
Christoph Neuhauser <christoph.neuhauser@intel.com>
2026-06-16 19:16:18 +0200

    Remove unnecessary scene member variable initialization

--- source/blender/blenloader/intern/versioning_530.cc
+++ source/blender/blenloader/intern/versioning_530.cc
@@ -81,12 +81,6 @@ void blo_do_versions_530(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
-  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 503, 4)) {
-    for (Scene &scene : bmain->scenes) {
-      scene.eevee.time_limit = 0.0f;
-    }
-  }
-
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
