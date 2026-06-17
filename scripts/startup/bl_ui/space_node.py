8f25403dbba927a00d206de4d8f8c0f54682d468
Omar Emara <mail@OmarEmara.dev>
2026-06-17 19:17:56 +0300

    Hide caching for VSE node trees

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -971,9 +971,9 @@ class NODE_PT_quality(Panel):
         if rd.compositor_device == 'GPU':
             col.prop(rd, "compositor_precision", text="Precision")
 
-        col = layout.column(heading="Cache", align=True)
-
-        col.prop(rd, "use_compositor_frames_cache", text="Frames")
+        if snode.node_tree_sub_type == 'SCENE':
+            col = layout.column(heading="Cache", align=True)
+            col.prop(rd, "use_compositor_frames_cache", text="Frames")
 
 
 class NODE_PT_overlay(Panel):
