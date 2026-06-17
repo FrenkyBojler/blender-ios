blender/blender

commit b2535d04f83b3cf02d12d3dc4d162d514b56a74a

Fix: VSE: Show packed sound files as normal even if their path is not valid
To fix, add checks for packed sound files in the drawing code and
draw their path in the UI as `<Packed File>` since it doesn't make too
much sense to show the original path of a packed file in the timeline.

Pull Request: https://projects.blender.org/blender/blender/pulls/158647

source/blender/sequencer/intern/media_presence.cc
@@ -110,6 +110,10 @@ bool media_presence_is_missing(Scene *scene, const Strip *strip)
    * block. Since it can be used by multiple strips. */
   if (strip->type == STRIP_TYPE_SOUND) {
     const bSound *sound = strip->sound;
+    if (sound->packedfile != nullptr) {
+      /* The sound file has been packed, don't look up the path. */
+      return false;
+    }
     const bool *val = presence->map_sound.lookup_ptr(sound);
     if (val != nullptr) {
       missing = *val;

source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -777,7 +777,13 @@ static void draw_seq_text_get_source(const Strip *strip, char *r_source, size_t 
     }
     case STRIP_TYPE_SOUND: {
       if (strip->sound != nullptr) {
-        BLI_strncpy_utf8(r_source, strip->sound->filepath, source_maxncpy);
+        if (strip->sound->packedfile != nullptr) {
+          /* The sound data has been packed, don't display the path. */
+          BLI_strncpy_utf8(r_source, "<Packed File>", source_maxncpy);
+        }
+        else {
+          BLI_strncpy_utf8(r_source, strip->sound->filepath, source_maxncpy);
+        }
       }
       break;
     }

FrenkyBojler/blender-ios

commit 80e850e17fcee2c0fbccc0e026c1de7cd0752515

Fix #160299: crash when unlinking a sound file in the VSE
Regression from b2535d04f83b3cf02d12d3dc4d162d514b56a74a which missed the possibility that the sound could be null, so simply add a null pointer check to prevent the crash.

source/blender/sequencer/intern/media_presence.cc
@@ -110,7 +110,7 @@ bool media_presence_is_missing(Scene *scene, const Strip *strip)
    * block. Since it can be used by multiple strips. */
   if (strip->type == STRIP_TYPE_SOUND) {
     const bSound *sound = strip->sound;
-    if (sound->packedfile != nullptr) {
+    if (sound && sound->packedfile != nullptr) {
       /* The sound file has been packed, don't look up the path. */
       return false;
     }