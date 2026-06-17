ac7efd2dd70bab11ce2b4a0e06dd5232f507df53
Eitan Traurig <eitant13@gmail.com>
2026-06-17 12:00:51 -0400

    Revert "Use previous ordering type when switching ordering modes. When adding in Custom new objects are"
    This reverts commit 39cd60d7afff00ab12d974d1ac226dc4a46c909e.

--- source/blender/makesdna/DNA_space_types.h
+++ source/blender/makesdna/DNA_space_types.h
@@ -212,8 +212,7 @@ struct SpaceOutliner {
   eSpaceOutliner_ShowRestrictFlag show_restrict_flags = {};
   short filter_id_type = 0;
   eSpaceOutliner_SortMethod sort_method = SO_SORT_ALPHA;
-  eSpaceOutliner_SortMethod prev_sort_method = SO_SORT_ALPHA;
-  char _pad1[4] = {};
+  char _pad1[6] = {};
 
   ed::outliner::SpaceOutliner_Runtime *runtime = nullptr;
 };
