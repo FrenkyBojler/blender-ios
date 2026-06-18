a8c36f93f0d87755ca419e81937a27905bbde04b
Mark van de Ruit <mark@blender.org>
2026-06-18 09:50:26 +0200

    Revert "Temp: removed `edit_mesh_edges_ps_` from `draw_color_only`"
    This reverts commit aa367651ab34f3f75397d554c49c8827e160cc1d.

--- source/blender/draw/engines/overlay/overlay_mesh.hh
+++ source/blender/draw/engines/overlay/overlay_mesh.hh
@@ -435,6 +435,7 @@ class Meshes : Overlay {
 
     GPU_framebuffer_bind(framebuffer);
     manager.submit(edit_mesh_normals_ps_, view);
+    manager.submit(edit_mesh_edges_ps_, view);
     manager.submit(edit_mesh_verts_ps_, view);
     manager.submit(edit_mesh_skin_roots_ps_, view);
     manager.submit(edit_mesh_facedots_ps_, view);
