f31e5ed1d5780d20f4067775740eab0b50486d82
Pratik Borhade <pratikborhade302@gmail.com>
2026-06-18 16:07:43 +0530

    Fix format

--- source/blender/editors/interface/interface_panel.cc
+++ source/blender/editors/interface/interface_panel.cc
@@ -757,9 +757,7 @@ Panel *panel_begin(
     }
   }
 
-  if (newpanel &&
-      !(region->regiontype == RGN_TYPE_TOOLS && U.file_space_data.custom_order))
-  {
+  if (newpanel && !(region->regiontype == RGN_TYPE_TOOLS && U.file_space_data.custom_order)) {
     panel->sortorder = (panel_last) ? panel_last->sortorder + 1 : 0;
 
     for (Panel &panel_next : *lb) {
