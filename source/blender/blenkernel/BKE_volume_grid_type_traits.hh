c9d9d4e03e4c76751e6f5b8af99e933df630db5f
Lukas Tönne <lukas@blender.org>
2026-06-19 09:49:57 +0200

    Remove matrix grid type traits, not needed for now

--- source/blender/blenkernel/BKE_volume_grid_type_traits.hh
+++ source/blender/blenkernel/BKE_volume_grid_type_traits.hh
@@ -10,7 +10,6 @@
 
 #ifdef WITH_OPENVDB
 
-#  include "BLI_math_matrix_types.hh"
 #  include "BLI_math_vector_types.hh"
 
 #  include "BKE_volume_enums.hh"
@@ -108,70 +107,6 @@ template<> struct VolumeGridTraits<float3> {
   static float3 to_blender(const openvdb::Vec3f &value)
   {
     return float3(value.asV());
-  }
-};
-
-template<> struct VolumeGridTraits<float3x3> {
-  using BlenderType = float3x3;
-  using PrimitiveType = openvdb::Mat3s;
-  using TreeType = void;
-  static constexpr VolumeGridType EnumType = VOLUME_GRID_UNKNOWN;
-
-  static openvdb::Mat3s to_openvdb(const float3x3 &value)
-  {
-    /* float3x3 accessor returns columns, Mat3s expects values in row-major form. */
-    return openvdb::Mat3s(value[0][0],
-                          value[1][0],
-                          value[2][0],
-                          value[0][1],
-                          value[1][1],
-                          value[2][1],
-                          value[0][2],
-                          value[1][2],
-                          value[2][2]);
-  }
-  static float3x3 to_blender(const openvdb::Mat3s &value)
-  {
-    /* float3x3 constructor takes column vectors. */
-    return float3x3(float3(value(0, 0), value(1, 0), value(2, 0)),
-                    float3(value(0, 1), value(1, 1), value(2, 1)),
-                    float3(value(0, 2), value(1, 2), value(2, 2)));
-  }
-};
-
-template<> struct VolumeGridTraits<float4x4> {
-  using BlenderType = float4x4;
-  using PrimitiveType = openvdb::Mat4s;
-  using TreeType = void;
-  static constexpr VolumeGridType EnumType = VOLUME_GRID_UNKNOWN;
-
-  static openvdb::Mat4s to_openvdb(const float4x4 &value)
-  {
-    /* float4x4 accessor returns columns, Mat4s expects values in row-major form. */
-    return openvdb::Mat4s(value[0][0],
-                          value[1][0],
-                          value[2][0],
-                          value[3][0],
-                          value[0][1],
-                          value[1][1],
-                          value[2][1],
-                          value[3][1],
-                          value[0][2],
-                          value[1][2],
-                          value[2][2],
-                          value[3][2],
-                          value[0][3],
-                          value[1][3],
-                          value[2][3],
-                          value[3][3]);
-  }
-  static float4x4 to_blender(const openvdb::Mat4s &value)
-  {
-    /* float3x3 constructor takes column vectors. */
-    return float4x4(float4(value(0, 0), value(1, 0), value(2, 0), value(3, 0)),
-                    float4(value(0, 1), value(1, 1), value(2, 1), value(3, 1)),
-                    float4(value(0, 2), value(1, 2), value(2, 2), value(3, 2)),
-                    float4(value(0, 3), value(1, 3), value(2, 3), value(3, 3)));
   }
 };
 
