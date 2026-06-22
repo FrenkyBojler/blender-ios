blender/blender

commit 3559feaf5e0b2dd448c0cdf69251e9e9f3bef4db

Fix #149617: Drag-drop asset can double-instantiate in some cases.
Drag-n-drop use regular link/append code, which will generate a default
instantiation or 'orphanned' imported data, and then removes that
instantiation in its dedicated post-operation (like
`OBJECT_OT_collection_external_asset_drop`) to redo the instantiatiation
the way it needs it.

Ideally, this should be prevented (as in, generic link/append should not
perform any instantiation)...

But the fix here is to ensure that the 'undo generic instantiation' part
also uses the correct parent collection, which may be an ancestor of the
active one in case the latter is not editable.

Pull Request: https://projects.blender.org/blender/blender/pulls/159843

source/blender/editors/object/object_add.cc
@@ -2080,15 +2080,16 @@ void OBJECT_OT_collection_instance_add(wmOperatorType *ot)
 static wmOperatorStatus collection_drop_exec(bContext *C, wmOperator *op)
 {
   Main *bmain = CTX_data_main(C);
-  LayerCollection *active_collection = CTX_data_layer_collection(C);
+  Collection *active_collection = BKE_collection_parent_editable_find_recursive(
+      CTX_data_view_layer(C), CTX_data_collection(C));
   std::optional<CollectionAddInfo> add_info = collection_add_info_get_from_op(C, op);
   if (!add_info) {
     return OPERATOR_CANCELLED;
   }
 
   if (RNA_boolean_get(op->ptr, "use_instance")) {
-    BKE_collection_child_remove(bmain, active_collection->collection, add_info->collection);
-    DEG_id_tag_update(&active_collection->collection->id, ID_RECALC_SYNC_TO_EVAL);
+    BKE_collection_child_remove(bmain, active_collection, add_info->collection);
+    DEG_id_tag_update(&active_collection->id, ID_RECALC_SYNC_TO_EVAL);
     DEG_relations_tag_update(bmain);
 
     Object *ob = add_type(C,

FrenkyBojler/blender-ios

commit 8a3f2ce4f5a0769bb229a0d5a4f520fb53a02dc1

Fix: Crash when drag-dropping collection asset without editable collection
Harden for cases where now editable 'active' collection can be found in
`loose_data_instantiate_ensure_active_collection`.

Add a null check for `active_collection` in `collection_drop_exec` to
prevent a crash when there is no editable active collection or parent
in the view layer.

This issue was introduced in 3559feaf5e0.

source/blender/blenkernel/intern/blendfile_link_append.cc
@@ -477,20 +477,31 @@ static void loose_data_instantiate_ensure_active_collection(
   /* Find or add collection as needed. When `active_collection` is non-null, it is assumed to be
    * editable. */
   if (instantiate_context->active_collection == nullptr) {
+    auto add_instantiating_collection =
+        [&bmain, &lapp_context](Collection *parent_collection) -> Collection * {
+      if (lapp_context->params->flag & FILE_LINK) {
+        return BKE_collection_add(bmain, parent_collection, DATA_("Linked Data"));
+      }
+      return BKE_collection_add(bmain, parent_collection, DATA_("Appended Data"));
+    };
+
     if (lapp_context->params->flag & FILE_ACTIVE_COLLECTION) {
       LayerCollection *lc = BKE_layer_collection_get_active(view_layer);
       instantiate_context->active_collection = BKE_collection_parent_editable_find_recursive(
           view_layer, lc->collection);
+      /* In all 'sane' cases, `BKE_collection_parent_editable_find_recursive` should find a valid
+       * parent collection. This is only a minimal backup in case the link/append operation happens
+       * in a very weird, broken context. */
+      if (!instantiate_context->active_collection) {
+        instantiate_context->active_collection = add_instantiating_collection(nullptr);
+      }
     }
     else {
-      if (lapp_context->params->flag & FILE_LINK) {
-        instantiate_context->active_collection = BKE_collection_add(
-            bmain, scene->master_collection, DATA_("Linked Data"));
-      }
-      else {
-        instantiate_context->active_collection = BKE_collection_add(
-            bmain, scene->master_collection, DATA_("Appended Data"));
-      }
+      Collection *parent_collection = BKE_collection_is_content_editable(
+                                          scene->master_collection) ?
+                                          scene->master_collection :
+                                          nullptr;
+      instantiate_context->active_collection = add_instantiating_collection(parent_collection);
     }
   }
 }

source/blender/editors/object/object_add.cc
@@ -2089,9 +2089,15 @@ static wmOperatorStatus collection_drop_exec(bContext *C, wmOperator *op)
   }
 
   if (RNA_boolean_get(op->ptr, "use_instance")) {
-    BKE_collection_child_remove(bmain, active_collection, add_info->collection);
-    DEG_id_tag_update(&active_collection->id, ID_RECALC_SYNC_TO_EVAL);
-    DEG_relations_tag_update(bmain);
+    /* In case no active editable collection is found, link/append code will have added imported
+     * collections/objects to a new collection (see code in
+     * `loose_data_instantiate_ensure_active_collection`). There is no easy way to retrieve these
+     * currently from here, so just leave the data in these for now - this is not a situation that
+     * should happen in 'normal expected use-cases' anyway. */
+    if (active_collection) {
+      BKE_collection_child_remove(bmain, active_collection, add_info->collection);
+      DEG_id_tag_update(&active_collection->id, ID_RECALC_SYNC_TO_EVAL);
+    }
 
     Object *ob = add_type(C,
                           OB_EMPTY,
@@ -2104,6 +2110,7 @@ static wmOperatorStatus collection_drop_exec(bContext *C, wmOperator *op)
     ob->empty_drawsize = U.collection_instance_empty_size;
     ob->transflag |= OB_DUPLICOLLECTION;
     id_us_plus(&add_info->collection->id);
+    DEG_relations_tag_update(bmain);
   }
   else if (ID_IS_EDITABLE(&add_info->collection->id)) {
     ViewLayer *view_layer = CTX_data_view_layer(C);