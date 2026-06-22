blender/blender

commit acb86ced9c8de8026552634288b88fb20f6afb55

LibOverride: Add a new Outliner 'remove override operation' operator/tool.
Active in the 'Overrides' view of the Outliner, it allows to selectively
remove an override from the outliner, even when there is no RNA widget
to display.

This commit also contains two small reltaed changes:
- Removed insertion of 'empty' entries for collection items that have
  no liboverride affecting them, in the Outliner drawing. This was fairly
  weird and confusing to have many 'meaningless' entries.
- Removed the 'asset' part of the contextual menu in a few outliner
  views where thes make little sense (Sequence and LibOverride ones).

NOTE: Side-effect from supporting geonodes modifiers packed bakes in
liboverrides, see also !158148.

Pull Request: https://projects.blender.org/blender/blender/pulls/159527

source/blender/editors/space_outliner/tree/tree_element_overrides.cc
@@ -192,6 +192,13 @@ StringRefNull TreeElementOverridesProperty::get_warning() const
   return {};
 }
 
+IDOverrideLibraryProperty *TreeElementOverridesProperty::get_override_property_from_id(
+    ID &id) const
+{
+  BLI_assert(ID_IS_OVERRIDE_LIBRARY_REAL(&id));
+  return BKE_lib_override_library_property_find(id.override_library, this->rna_path.c_str());
+}
+
 /** \} */
 
 /* -------------------------------------------------------------------- */
@@ -261,6 +268,23 @@ std::optional<BIFIconID> TreeElementOverridesPropertyOperation::get_icon() const
   }
 
   return {};
+}
+
+short TreeElementOverridesPropertyOperation::get_operation_type() const
+{
+  return operation_->operation;
+}
+
+IDOverrideLibraryPropertyOperation *TreeElementOverridesPropertyOperation::
+    get_override_operation_from_id(ID &id, IDOverrideLibraryProperty &override_property) const
+{
+  BLI_assert(ID_IS_OVERRIDE_LIBRARY_REAL(&id));
+  for (IDOverrideLibraryPropertyOperation &opop : override_property.operations) {
+    if (*operation_ == opop) {
+      return &opop;
+    }
+  }
+  return nullptr;
 }
 
 std::optional<PointerRNA> TreeElementOverridesPropertyOperation::get_collection_ptr() const
@@ -449,7 +473,8 @@ void OverrideRNAPathTreeBuilder::ensure_entire_collection(
                                                     index++);
     }
     else {
-      current_te = &ensure_label_element_for_ptr(te_to_expand, coll_item_path, itemptr, index);
+      /* NOTE: Do not generate entries for collection items which are not affected by liboverride,
+       * this is more disturbing than useful. */
     }
 
     MEM_delete(coll_item_path);

scripts/startup/bl_ui/space_outliner.py
@@ -112,10 +112,20 @@ class OUTLINER_MT_context_menu(Menu):
     bl_label = "Outliner"
 
     @staticmethod
-    def draw_common_operators(layout):
-        layout.menu_contents("OUTLINER_MT_asset")
+    def draw_common_operators(space, layout):
+        # Mark/clear asset options does not belongs in certain outliner views.
+        if space.display_mode not in {'SEQUENCE', 'LIBRARY_OVERRIDES'}:
+            layout.menu_contents("OUTLINER_MT_asset")
 
-        layout.separator()
+            layout.separator()
+
+        if space.display_mode in {'LIBRARY_OVERRIDES'}:
+            layout.operator(
+                "outliner.liboverride_property_remove",
+                text="Remove",
+            )
+
+            layout.separator()
 
         layout.menu("OUTLINER_MT_liboverride", icon='LIBRARY_DATA_OVERRIDE')
 
@@ -136,7 +146,7 @@ class OUTLINER_MT_context_menu(Menu):
             OUTLINER_MT_collection_new.draw_without_context_menu(context, layout)
             layout.separator()
 
-        OUTLINER_MT_context_menu.draw_common_operators(layout)
+        OUTLINER_MT_context_menu.draw_common_operators(space, layout)
 
 
 class OUTLINER_MT_context_menu_view(Menu):
@@ -292,7 +302,7 @@ class OUTLINER_MT_collection(Menu):
 
         layout.separator()
 
-        OUTLINER_MT_context_menu.draw_common_operators(layout)
+        OUTLINER_MT_context_menu.draw_common_operators(space, layout)
 
 
 class OUTLINER_MT_collection_new(Menu):
@@ -310,7 +320,7 @@ class OUTLINER_MT_collection_new(Menu):
 
         layout.separator()
 
-        OUTLINER_MT_context_menu.draw_common_operators(layout)
+        OUTLINER_MT_context_menu.draw_common_operators(context.space_data, layout)
 
 
 class OUTLINER_MT_object(Menu):
@@ -349,7 +359,7 @@ class OUTLINER_MT_object(Menu):
 
         layout.separator()
 
-        OUTLINER_MT_context_menu.draw_common_operators(layout)
+        OUTLINER_MT_context_menu.draw_common_operators(space, layout)
 
 
 class OUTLINER_MT_asset(Menu):

source/blender/blenkernel/intern/lib_override.cc
@@ -4583,6 +4583,23 @@ void BKE_lib_override_library_property_operation_delete(
   BLI_freelinkN(&liboverride_property->operations, liboverride_property_operation);
 }
 
+bool IDOverrideLibraryPropertyOperation::operator==(
+    const IDOverrideLibraryPropertyOperation &b) const
+{
+  return (
+      (this->operation == b.operation) && (this->flag == b.flag) &&
+      (this->subitem_reference_id == b.subitem_reference_id) &&
+      (this->subitem_local_id == b.subitem_local_id) &&
+      (this->subitem_reference_index == b.subitem_reference_index) &&
+      (this->subitem_local_index == b.subitem_local_index) &&
+      ((!this->subitem_reference_name && !b.subitem_reference_name) ||
+       (this->subitem_reference_name && b.subitem_reference_name &&
+        StringRefNull(this->subitem_reference_name) == StringRefNull(b.subitem_reference_name))) &&
+      ((!this->subitem_local_name && !b.subitem_local_name) ||
+       (this->subitem_local_name && b.subitem_local_name &&
+        StringRefNull(this->subitem_local_name) == StringRefNull(b.subitem_local_name))));
+}
+
 bool BKE_lib_override_library_property_operation_operands_validate(
     IDOverrideLibraryPropertyOperation *liboverride_property_operation,
     PointerRNA *ptr_dst,

source/blender/editors/space_outliner/outliner_intern.hh
@@ -547,6 +547,7 @@ void OUTLINER_OT_object_operation(wmOperatorType *ot);
 void OUTLINER_OT_lib_operation(wmOperatorType *ot);
 void OUTLINER_OT_liboverride_operation(wmOperatorType *ot);
 void OUTLINER_OT_liboverride_troubleshoot_operation(wmOperatorType *ot);
+void OUTLINER_OT_liboverride_property_remove(wmOperatorType *ot);
 void OUTLINER_OT_id_operation(wmOperatorType *ot);
 void OUTLINER_OT_id_remap(wmOperatorType *ot);
 void OUTLINER_OT_id_copy(wmOperatorType *ot);

source/blender/editors/space_outliner/outliner_ops.cc
@@ -33,6 +33,7 @@ void outliner_operatortypes()
   WM_operatortype_append(OUTLINER_OT_lib_relocate);
   WM_operatortype_append(OUTLINER_OT_liboverride_operation);
   WM_operatortype_append(OUTLINER_OT_liboverride_troubleshoot_operation);
+  WM_operatortype_append(OUTLINER_OT_liboverride_property_remove);
   WM_operatortype_append(OUTLINER_OT_id_operation);
   WM_operatortype_append(OUTLINER_OT_id_delete);
   WM_operatortype_append(OUTLINER_OT_id_remap);

source/blender/editors/space_outliner/outliner_tools.cc
@@ -94,7 +94,9 @@
 #include "SEQ_sequencer.hh"
 
 #include "outliner_intern.hh"
+#include "tree/tree_element.hh"
 #include "tree/tree_element_grease_pencil_node.hh"
+#include "tree/tree_element_overrides.hh"
 #include "tree/tree_element_rna.hh"
 #include "tree/tree_element_seq.hh"
 #include "tree/tree_iterator.hh"
@@ -2108,6 +2110,233 @@ void OUTLINER_OT_liboverride_troubleshoot_operation(wmOperatorType *ot)
                0,
                "Selection Set",
                "Over which part of the tree items to apply the operation");
+}
+
+static void outliner_do_liboverride_property_selection_set(bContext *C,
+                                                           ReportList *reports,
+                                                           Scene *scene,
+                                                           ListBaseT<TreeElement> &subtree,
+                                                           const bool has_parent_selected,
+                                                           outliner_operation_fn operation_fn)
+{
+  for (TreeElement &element : subtree.items_mutable()) {
+    /* Get needed data out in case element gets freed. */
+    TreeStoreElem *tselem = TREESTORE(&element);
+    ListBaseT<TreeElement> subtree = element.subtree;
+
+    const bool is_selected = (tselem->flag & TSE_SELECTED) || has_parent_selected;
+    if (is_selected) {
+      if (ELEM(tselem->type, TSE_LIBRARY_OVERRIDE, TSE_LIBRARY_OVERRIDE_OPERATION)) {
+        TreeStoreElem *tsep = element.parent ? TREESTORE(element.parent) : nullptr;
+        operation_fn(C, reports, scene, &element, tsep, tselem);
+      }
+    }
+    /* Don't access element from now on, it may be freed. Note that the open/collapsed state may
+     * also have been changed in the visitor callback. */
+
+    outliner_do_liboverride_property_selection_set(
+        C, reports, scene, subtree, is_selected, operation_fn);
+  }
+}
+
+static bool outliner_liboverride_property_remove_poll(bContext *C)
+{
+  if (!outliner_operation_tree_element_poll(C)) {
+    return false;
+  }
+  SpaceOutliner *space_outliner = CTX_wm_space_outliner(C);
+  TreeElement *te = get_target_element(space_outliner);
+  TreeStoreElem *tselem = TREESTORE(te);
+
+  return (space_outliner->outlinevis == SO_OVERRIDES_LIBRARY &&
+          ELEM(tselem->type,
+               TSE_GENERIC_LABEL,
+               TSE_LIBRARY_OVERRIDE_BASE,
+               TSE_LIBRARY_OVERRIDE,
+               TSE_LIBRARY_OVERRIDE_OPERATION));
+}
+
+template<typename TreeElementOverridesT>
+static bool outliner_liboverride_property_remove_do(bContext *C,
+                                                    Main &bmain,
+                                                    TreeStoreElem &tselem,
+                                                    TreeElementOverridesT &override_elem,
+                                                    ReportList *reports)
+{
+  ID *current_id = tselem.id;
+  BLI_assert(current_id);
+  BLI_assert(ID_IS_OVERRIDE_LIBRARY_REAL(current_id));
+
+  IDOverrideLibraryProperty *liboverride_property = override_elem.get_override_property_from_id(
+      *current_id);
+  if (!liboverride_property) {
+    BKE_reportf(reports,
+                RPT_ERROR,
+                "Failed to find a matching Library Override property for ID '%s' (Library "
+                "'%s'), path '%s'",
+                current_id->override_library->reference->name,
+                current_id->override_library->reference->lib ?
+                    current_id->override_library->reference->lib->runtime->filepath_abs :
+                    "<NONE>",
+                liboverride_property->rna_path);
+    return false;
+  }
+
+  IDOverrideLibraryPropertyOperation *liboverride_property_operation = nullptr;
+  if constexpr (std::is_same_v<TreeElementOverridesT, const TreeElementOverridesPropertyOperation>)
+  {
+    const eID_OverrideLib_Op override_opcode = eID_OverrideLib_Op(
+        override_elem.get_operation_type());
+    if (ELEM(override_opcode, LIBOVERRIDE_OP_INSERT_AFTER, LIBOVERRIDE_OP_INSERT_BEFORE)) {
+      BKE_reportf(reports,
+                  RPT_WARNING,
+                  "Cannot remove 'Insert' type of library override operations for ID '%s' "
+                  "(library '%s'), RNA path '%s'. Please delete the added data directly",
+                  current_id->name,
+                  current_id->lib ? current_id->lib->runtime->filepath_abs : "<NONE>",
+                  override_elem.rna_path.c_str());
+      return false;
+    }
+
+    liboverride_property_operation = override_elem.get_override_operation_from_id(
+        *current_id, *liboverride_property);
+    if (!liboverride_property_operation) {
+      BKE_reportf(reports,
+                  RPT_ERROR,
+                  "Failed to find a matching Library Override property operation for ID '%s' "
+                  "(Library '%s'), path '%s'",
+                  current_id->override_library->reference->name,
+                  current_id->override_library->reference->lib ?
+                      current_id->override_library->reference->lib->runtime->filepath_abs :
+                      "<NONE>",
+                  liboverride_property->rna_path);
+      return false;
+    }
+  }
+
+  /* The source (i.e. linked data) is required to restore values of deleted overrides. */
+  PropertyRNA *reference_rna_prop;
+  PointerRNA id_refptr = RNA_id_pointer_create(current_id->override_library->reference);
+  PointerRNA reference_rna_ptr;
+  if (!RNA_path_resolve_property(
+          &id_refptr, liboverride_property->rna_path, &reference_rna_ptr, &reference_rna_prop))
+  {
+    BKE_reportf(reports,
+                RPT_ERROR,
+                "Failed to create matching reference (linked data) RNA pointer for ID '%s' "
+                "(Library '%s'), path '%s'",
+                current_id->override_library->reference->name,
+                current_id->override_library->reference->lib ?
+                    current_id->override_library->reference->lib->runtime->filepath_abs :
+                    "<NONE>",
+                liboverride_property->rna_path);
+    return false;
+  }
+
+  RNA_property_copy(&bmain,
+                    const_cast<PointerRNA *>(&override_elem.override_rna_ptr),
+                    &reference_rna_ptr,
+                    &override_elem.override_rna_prop,
+                    liboverride_property_operation ?
+                        liboverride_property_operation->subitem_reference_index :
+                        -1,
+                    liboverride_property,
+                    liboverride_property_operation);
+  if (liboverride_property_operation) {
+    BKE_lib_override_library_property_operation_delete(liboverride_property,
+                                                       liboverride_property_operation);
+    if (BLI_listbase_is_empty(&liboverride_property->operations)) {
+      BKE_lib_override_library_property_delete(current_id->override_library, liboverride_property);
+    }
+  }
+  else {
+    BKE_lib_override_library_property_delete(current_id->override_library, liboverride_property);
+  }
+
+  /* Perform updates required for this property. */
+  RNA_property_update(C,
+                      const_cast<PointerRNA *>(&override_elem.override_rna_ptr),
+                      &override_elem.override_rna_prop);
+
+  return true;
+}
+
+static wmOperatorStatus outliner_liboverride_property_remove_exec(bContext *C, wmOperator *op)
+{
+  Main *bmain = CTX_data_main(C);
+  Scene *scene = CTX_data_scene(C);
+  SpaceOutliner *space_outliner = CTX_wm_space_outliner(C);
+
+  /* check for invalid states */
+  if (space_outliner == nullptr) {
+    return OPERATOR_CANCELLED;
+  }
+
+  bool has_changes = false;
+  auto property_remove_cb = [bmain, &has_changes](bContext *C,
+                                                  ReportList *reports,
+                                                  Scene * /*scene*/,
+                                                  TreeElement *te,
+                                                  TreeStoreElem * /*tsep*/,
+                                                  TreeStoreElem *tselem) -> void {
+    switch (tselem->type) {
+      case TSE_LIBRARY_OVERRIDE_BASE:
+      case TSE_LIBRARY_OVERRIDE:
+        if (const TreeElementOverridesProperty *override_op_elem =
+                tree_element_cast<TreeElementOverridesProperty>(te))
+        {
+          if (outliner_liboverride_property_remove_do(
+                  C, *bmain, *tselem, *override_op_elem, reports))
+          {
+            has_changes = true;
+          }
+        }
+        break;
+      case TSE_LIBRARY_OVERRIDE_OPERATION: {
+        if (const TreeElementOverridesPropertyOperation *override_op_elem =
+                tree_element_cast<TreeElementOverridesPropertyOperation>(te))
+        {
+          if (outliner_liboverride_property_remove_do(
+                  C, *bmain, *tselem, *override_op_elem, reports))
+          {
+            has_changes = true;
+          }
+        }
+        break;
+      }
+      default:
+        BLI_assert_unreachable();
+    }
+  };
+
+  outliner_do_liboverride_property_selection_set(
+      C, op->reports, scene, space_outliner->runtime->tree, false, property_remove_cb);
+
+  if (!has_changes) {
+    return OPERATOR_CANCELLED;
+  }
+
+  WM_event_add_notifier(C, NC_WINDOW, nullptr);
+  WM_event_add_notifier(C, NC_WM | ND_LIB_OVERRIDE_CHANGED, nullptr);
+  WM_event_add_notifier(C, NC_SPACE | ND_SPACE_VIEW3D, nullptr);
+
+  return OPERATOR_FINISHED;
+}
+
+void OUTLINER_OT_liboverride_property_remove(wmOperatorType *ot)
+{
+  /* identifiers */
+  ot->name = "Outliner Library Override Propery Remove";
+  ot->idname = "OUTLINER_OT_liboverride_property_remove";
+  ot->description =
+      "Remove the selected library override properties, and reset the relevant data to the linked "
+      "reference values";
+
+  /* callbacks */
+  ot->exec = outliner_liboverride_property_remove_exec;
+  ot->poll = outliner_liboverride_property_remove_poll;
+
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 }
 
 /** \} */

source/blender/editors/space_outliner/tree/tree_element_overrides.hh
@@ -63,6 +63,9 @@ class TreeElementOverridesProperty : public AbstractTreeElement {
   TreeElementOverridesProperty(TreeElement &legacy_te, TreeElementOverridesData &override_data);
 
   StringRefNull get_warning() const override;
+
+  /** Return the liboverride property matching this tree element, for the given ID. */
+  IDOverrideLibraryProperty *get_override_property_from_id(ID &id) const;
 };
 
 /**
@@ -81,10 +84,25 @@ class TreeElementOverridesPropertyOperation final : public TreeElementOverridesP
   TreeElementOverridesPropertyOperation(TreeElement &legacy_te,
                                         TreeElementOverridesData &override_data);
 
-  /** Return a short string to display in the right column of the properties mode, indicating what
-   * the override operation did (e.g. added or removed a collection item). */
+  /**
+   * Return a short string to display in the right column of the properties mode, indicating what
+   * the override operation did (e.g. added or removed a collection item).
+   *
+   * \note: When returning an empty string, the outliner drawing code assumes that it can draw an
+   * editing widget based on the affected RNA property instead.
+   */
   StringRefNull get_override_operation_label() const;
   std::optional<BIFIconID> get_icon() const override;
+
+  /** Return liboverride operation type (#eID_OverrideLib_Op). */
+  short get_operation_type() const;
+
+  /**
+   * Return the liboverride operation matching this tree element, for the given ID and library
+   * override property.
+   */
+  IDOverrideLibraryPropertyOperation *get_override_operation_from_id(
+      ID &id, IDOverrideLibraryProperty &override_property) const;
 
  private:
   std::optional<PointerRNA> get_collection_ptr() const;

source/blender/makesdna/DNA_ID.h
@@ -259,6 +259,10 @@ struct IDOverrideLibraryPropertyOperation {
    * same name. */
   struct ID *subitem_reference_id = nullptr;
   struct ID *subitem_local_id = nullptr;
+
+#ifdef __cplusplus
+  bool operator==(const IDOverrideLibraryPropertyOperation &b) const;
+#endif
 };
 
 /* IDOverrideLibrary->flag */

FrenkyBojler/blender-ios

commit 7718ed5e320b3efa7e42fbecd161c1f0c019b1cc

Fix: liboverride outliner tree ordering resets insertion point to null.
When a collection item has no override operation and no existing tree
element, `current_te` remains null and `previous_te = current_te` would
reset the insertion point for the next `BLI_insertlinkafter` call,
breaking the sorted order of subsequent elements.

Only advance `previous_te` when a tree element was actually created.

Regression from acb86ced9c8de80.

source/blender/editors/space_outliner/tree/tree_element_overrides.cc
@@ -491,7 +491,9 @@ void OverrideRNAPathTreeBuilder::ensure_entire_collection(
 
     MEM_delete(coll_item_path);
     item_idx++;
-    previous_te = current_te;
+    if (current_te) {
+      previous_te = current_te;
+    }
   }
   RNA_PROP_END;
 }