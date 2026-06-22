blender/blender

commit 329061c23925fff58eb4b7204a6d8bf775e70c97

LibOverride: Add initial support for geonodes packed bakes.
This is especially tricky, since packed bakes themselves are not exposed
through RNA, so generic liboverride diffing and apply code cannot handle
them.

On top of this, there is no way to tell if a packed bake in the
liboverride data comes (has been copied) from the linked reference data,
or if it was effectively generated from the liboverride itself.

This commit introduces a new `CUSTOM` liboverride operation, which is
ignored by default/generic liboverride processing, and requires dedicated
RNA property callbacks.

It is used to generate operations on the 'bakes' collection property. This
encodes the fact that the matching packed bakes from the written
liboverride data should replace the ones from the linked reference data.

It should handle most commons cases well:
- Packed bakes from the override replace the ones from the linked
  reference, in case both are defined.
- Bakes are matched by 'bake id', to ensure that a change in order or
  number of bakes in the linked reference data, does not completely
  break the override bakes.

Fixes #154029.

Pull Request: https://projects.blender.org/blender/blender/pulls/158148

source/blender/makesrna/intern/rna_modifier.cc
@@ -853,6 +853,7 @@ static const EnumPropertyItem grease_pencil_build_time_mode_items[] = {
 #  include "BLI_string.h"
 #  include "BLI_string_utf8.h"
 
+#  include "BKE_bake_geometry_nodes_modifier.hh"
 #  include "BKE_cachefile.hh"
 #  include "BKE_compute_contexts.hh"
 #  include "BKE_context.hh"
@@ -860,6 +861,7 @@ static const EnumPropertyItem grease_pencil_build_time_mode_items[] = {
 #  include "BKE_deform.hh"
 #  include "BKE_fluid.h"
 #  include "BKE_lib_id.hh"
+#  include "BKE_lib_override.hh"
 #  include "BKE_material.hh"
 #  include "BKE_mesh_runtime.hh"
 #  include "BKE_modifier.hh"
@@ -2131,6 +2133,177 @@ static PointerRNA rna_NodesModifierBake_node_get(PointerRNA *ptr)
   BLI_assert(tree != nullptr);
   return RNA_pointer_create_discrete(
       const_cast<ID *>(&tree->id), RNA_Node, const_cast<bNode *>(node));
+}
+
+void rna_NodesModifierBake_override_diff(Main *bmain, RNAPropertyOverrideDiffContext &rnadiff_ctx)
+{
+  /* This diffing code uses the `LIBOVERRIDE_OP_CUSTOM` liboverride operation to encode a 'packed
+   * data is changed into that bake's info. */
+
+  rna_property_override_diff_default(bmain, rnadiff_ctx);
+
+  const bool do_create = rnadiff_ctx.liboverride != nullptr &&
+                         (rnadiff_ctx.liboverride_flags & RNA_OVERRIDE_COMPARE_CREATE) != 0 &&
+                         rnadiff_ctx.rna_path != nullptr;
+
+  if (rnadiff_ctx.comparison && !do_create) {
+    /* Default diffing found a difference, no need to go further. */
+    return;
+  }
+
+  const NodesModifierData *nmd_a = rnadiff_ctx.prop_a->ptr->data_as<NodesModifierData>();
+  const NodesModifierData *nmd_b = rnadiff_ctx.prop_b->ptr->data_as<NodesModifierData>();
+
+  /* In standard context (same nodes in both modifiers), the bake ids and their order should
+   * always match. For now, simply ignore cases where they don't. */
+  if (nmd_a->bakes_num != nmd_b->bakes_num) {
+    return;
+  }
+
+  for (int i : IndexRange(nmd_a->bakes_num)) {
+    const NodesModifierBake *nmd_bake_a = &nmd_a->bakes[i];
+    const NodesModifierBake *nmd_bake_b = &nmd_b->bakes[i];
+
+    if (nmd_bake_a->id != nmd_bake_b->id) {
+      /* Bakes for different nodes, cannot do anything else here, ignore. */
+      /* NOTE: Not sure if this can actually happen? Maybe in case the user assigns a different
+       * nodetree in the overridden version of the modifier, which happens to have exactly the same
+       * amount of bake nodes? */
+      BLI_assert_unreachable();
+      continue;
+    }
+
+    if (!nmd_bake_a->packed && !nmd_bake_b->packed) {
+      /* There are no packed bake data in either, so regular diffing above should be sufficient to
+       * ensure valid diffing and liboverride operations results. */
+      continue;
+    }
+
+    if (nmd_bake_a->packed && nmd_bake_b->packed) {
+      /* Both bakes have packed data, no other solution than doing full byte-wise comparison of the
+       * whole packed data. */
+      /* TODO: #NodesModifierPackedBake could store a hash of its data, in case this full
+       * comparison becomes a performance issue? */
+      bool is_different =
+          ((nmd_bake_a->packed->meta_files_num != nmd_bake_b->packed->meta_files_num) ||
+           (nmd_bake_a->packed->blob_files_num != nmd_bake_b->packed->blob_files_num));
+      if (!is_different) {
+        for (int i : IndexRange(nmd_bake_a->packed->meta_files_num)) {
+          if ((StringRefNull(nmd_bake_a->packed->meta_files[i].name) !=
+               StringRefNull(nmd_bake_b->packed->meta_files[i].name)) ||
+              (nmd_bake_a->packed->meta_files[i].data() !=
+               nmd_bake_b->packed->meta_files[i].data()))
+          {
+            is_different = true;
+            break;
+          }
+        }
+      }
+      if (!is_different) {
+        for (int i : IndexRange(nmd_bake_a->packed->blob_files_num)) {
+          if ((StringRefNull(nmd_bake_a->packed->blob_files[i].name) !=
+               StringRefNull(nmd_bake_b->packed->blob_files[i].name)) ||
+              (nmd_bake_a->packed->blob_files[i].data() !=
+               nmd_bake_b->packed->blob_files[i].data()))
+            is_different = true;
+          break;
+        }
+      }
+      if (!is_different) {
+        continue;
+      }
+    }
+
+    /* Sign doesn't make sense here, these are not orderable data. */
+    rnadiff_ctx.comparison = 1;
+
+    /* The remainder of this function was taken from rna_property_override_diff_default(). It's
+     * just formatted a little differently to allow for early returns. */
+
+    if (!do_create) {
+      /* Not enough info to create an override operation, so bail out. */
+      continue;
+    }
+
+    /* Create the override operation. */
+    bool created = false;
+    IDOverrideLibraryProperty *op = BKE_lib_override_library_property_get(
+        rnadiff_ctx.liboverride, rnadiff_ctx.rna_path, &created);
+    if (!op) {
+      continue;
+    }
+
+    if (created || op->rna_prop_type == 0) {
+      op->rna_prop_type = PROP_COLLECTION;
+    }
+    else {
+      BLI_assert(op->rna_prop_type == PROP_COLLECTION);
+    }
+    IDOverrideLibraryPropertyOperation *opop = BKE_lib_override_library_property_operation_get(
+        op, LIBOVERRIDE_OP_CUSTOM, nullptr, nullptr, {}, {}, i, i, true, nullptr, &created);
+    if (!opop) {
+      continue;
+    }
+
+    const bNodeTree *owner_ntree = nullptr;
+    const bNode *node = nmd_a->node_group->find_nested_node(nmd_bake_a->id, &owner_ntree);
+    owner_ntree = owner_ntree ? owner_ntree : nmd_a->node_group;
+
+    if (node) {
+      BKE_lib_override_library_property_operation_ui_info_set(
+          *opop,
+          node->name,
+          fmt::format(fmt::runtime(DATA_("{}::{}::{}")),
+                      owner_ntree->id.lib ? BKE_id_name(owner_ntree->id.lib->id) : "LOCAL",
+                      BKE_id_name(owner_ntree->id),
+                      node->name));
+    }
+
+    if (created) {
+      rnadiff_ctx.report_flag |= RNA_OVERRIDE_MATCH_RESULT_CREATED;
+    }
+  }
+}
+
+bool rna_NodesModifierBake_override_apply(Main *bmain,
+                                          RNAPropertyOverrideApplyContext &rnaapply_ctx)
+{
+  PointerRNA *ptr_dst = &rnaapply_ctx.ptr_dst;
+  PropertyRNA *prop_dst = rnaapply_ctx.prop_dst;
+  IDOverrideLibraryPropertyOperation *opop = rnaapply_ctx.liboverride_operation;
+  IDOverrideLibraryPropertyOperation *removed_opop = rnaapply_ctx.liboverride_removed_operation;
+
+  /* `REPLACE` operation will be generated by the 'remove liboverride' feature (see
+   * #override_remove_button_exec), to revert the overridden changes. */
+  BLI_assert_msg((((opop->operation == LIBOVERRIDE_OP_CUSTOM) && !removed_opop) ||
+                  ((opop->operation == LIBOVERRIDE_OP_REPLACE) &&
+                   (removed_opop && (removed_opop->operation = LIBOVERRIDE_OP_CUSTOM)))),
+                 "Unsupported RNA override operation on Nodes modifier bakes collection");
+
+  NodesModifierBake *nmd_bake_src = rnaapply_ctx.ptr_item_src.data_as<NodesModifierBake>();
+
+  /* Ignore index-based default 'destination item' defined by the generic liboverride apply code
+   * and stored in RNAPropertyOverrideApplyContext::ptr_item_dst, as changes in source linked
+   * nodetree may have re-ordered its bakes. Instead, lookup by bake id. */
+  NodesModifierData *nmd_dst = ptr_dst->data_as<NodesModifierData>();
+  NodesModifierBake *nmd_bake_dst = nmd_dst->find_bake(nmd_bake_src->id);
+  if (!nmd_bake_dst) {
+    return false;
+  }
+  BLI_assert(nmd_bake_dst->id == nmd_bake_src->id);
+
+  if (nmd_bake_dst->packed) {
+    nodes_modifier_packed_bake_free(nmd_bake_dst->packed);
+    nmd_bake_dst->packed = nullptr;
+  }
+  nodes_modifier_packed_bake_copy(*nmd_bake_dst, *nmd_bake_src);
+  if (nmd_dst->runtime) {
+    nmd_dst->runtime->cache->reset_cache(nmd_bake_dst->id);
+  }
+
+  RNA_property_update_main(bmain, nullptr, ptr_dst, prop_dst);
+  rna_NodesModifier_bake_update(bmain, nullptr, ptr_dst);
+  return true;
 }
 
 static StructRNA *rna_NodesModifierBake_data_block_typef(PointerRNA *ptr)
@@ -8207,8 +8380,14 @@ static void rna_def_modifier_nodes(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "bakes", PROP_COLLECTION, PROP_NONE);
   RNA_def_property_struct_type(prop, "NodesModifierBake");
+  RNA_def_property_ui_text(
+      prop, "Bakes", "All potential bakes, as defined by the assigned Geometry Nodes");
   RNA_def_property_collection_sdna(prop, nullptr, "bakes", "bakes_num");
   RNA_def_property_srna(prop, "NodesModifierBakes");
+  RNA_def_property_override_funcs(prop,
+                                  "rna_NodesModifierBake_override_diff",
+                                  nullptr,
+                                  "rna_NodesModifierBake_override_apply");
 
   prop = RNA_def_property(srna, "panels", PROP_COLLECTION, PROP_NONE);
   RNA_def_property_struct_type(prop, "NodesModifierPanel");

source/blender/blenkernel/BKE_lib_override.hh
@@ -475,6 +475,14 @@ bool BKE_lib_override_library_property_operation_operands_validate(
     PropertyRNA *prop_storage);
 
 /**
+ * Set the label (UI-only 'name') of the given operation.
+ */
+void BKE_lib_override_library_property_operation_ui_info_set(
+    IDOverrideLibraryPropertyOperation &liboverride_property_operation,
+    StringRefNull label,
+    StringRefNull tooltip);
+
+/**
  * Check against potential \a bmain.
  */
 void BKE_lib_override_library_validate(Main *bmain, ID *id, ReportList *reports);

source/blender/blenkernel/intern/lib_id.cc
@@ -2764,6 +2764,12 @@ void BKE_id_blend_write(BlendWriter *writer, ID *id)
         if (opop.subitem_local_name) {
           writer->write_string(opop.subitem_local_name);
         }
+        if (opop.label) {
+          writer->write_string(opop.label);
+        }
+        if (opop.tooltip) {
+          writer->write_string(opop.tooltip);
+        }
       }
     }
   }

source/blender/blenkernel/intern/lib_override.cc
@@ -4563,6 +4563,12 @@ void lib_override_library_property_operation_copy(IDOverrideLibraryPropertyOpera
   if (opop_src->subitem_local_name) {
     opop_dst->subitem_local_name = BLI_strdup(opop_src->subitem_local_name);
   }
+  if (opop_src->label) {
+    opop_dst->label = BLI_strdup(opop_src->label);
+  }
+  if (opop_src->tooltip) {
+    opop_dst->tooltip = BLI_strdup(opop_src->tooltip);
+  }
 }
 
 void lib_override_library_property_operation_clear(IDOverrideLibraryPropertyOperation *opop)
@@ -4573,6 +4579,12 @@ void lib_override_library_property_operation_clear(IDOverrideLibraryPropertyOper
   if (opop->subitem_local_name) {
     MEM_delete(opop->subitem_local_name);
   }
+  if (opop->label) {
+    MEM_delete(opop->label);
+  }
+  if (opop->tooltip) {
+    MEM_delete(opop->tooltip);
+  }
 }
 
 void BKE_lib_override_library_property_operation_delete(
@@ -4581,6 +4593,17 @@ void BKE_lib_override_library_property_operation_delete(
 {
   lib_override_library_property_operation_clear(liboverride_property_operation);
   BLI_freelinkN(&liboverride_property->operations, liboverride_property_operation);
+}
+
+void BKE_lib_override_library_property_operation_ui_info_set(
+    IDOverrideLibraryPropertyOperation &liboverride_property_operation,
+    StringRefNull label,
+    StringRefNull tooltip)
+{
+  MEM_SAFE_DELETE(liboverride_property_operation.label);
+  liboverride_property_operation.label = BLI_strdup(label.c_str());
+  MEM_SAFE_DELETE(liboverride_property_operation.tooltip);
+  liboverride_property_operation.tooltip = BLI_strdup(tooltip.c_str());
 }
 
 bool IDOverrideLibraryPropertyOperation::operator==(
@@ -4611,6 +4634,10 @@ bool BKE_lib_override_library_property_operation_operands_validate(
 {
   switch (liboverride_property_operation->operation) {
     case LIBOVERRIDE_OP_NOOP:
+      return true;
+    case LIBOVERRIDE_OP_CUSTOM:
+      /* No way to validate these here, custom RNA liboverride callbacks have to take care of
+       * validation. */
       return true;
     case LIBOVERRIDE_OP_ADD:
       ATTR_FALLTHROUGH;
@@ -5564,6 +5591,8 @@ StringRefNull BKE_lib_override_operation_as_string(const eID_OverrideLib_Op oper
       return "Insert After";
     case LIBOVERRIDE_OP_INSERT_BEFORE:
       return "Insert Before";
+    case LIBOVERRIDE_OP_CUSTOM:
+      return "Custom";
   }
   BLI_assert_unreachable();
   return "Unknown";

source/blender/blenloader/intern/readfile.cc
@@ -2016,6 +2016,8 @@ static void direct_link_id_override_property(BlendDataReader *reader,
   for (IDOverrideLibraryPropertyOperation &opop : op->operations) {
     BLO_read_string(reader, &opop.subitem_reference_name);
     BLO_read_string(reader, &opop.subitem_local_name);
+    BLO_read_string(reader, &opop.label);
+    BLO_read_string(reader, &opop.tooltip);
 
     opop.tag = {}; /* Runtime only. */
   }

source/blender/editors/interface/interface_ops.cc
@@ -650,16 +650,16 @@ static wmOperatorStatus override_remove_button_exec(bContext *C, wmOperator *op)
         }
       }
     }
+    RNA_property_copy(bmain, &ptr, &src, prop, index, oprop, opop);
     BKE_lib_override_library_property_operation_delete(oprop, opop);
-    RNA_property_copy(bmain, &ptr, &src, prop, index);
     if (oprop->operations.is_empty()) {
       BKE_lib_override_library_property_delete(id->override_library, oprop);
     }
   }
   else {
     /* Just remove whole generic override operation of this property. */
+    RNA_property_copy(bmain, &ptr, &src, prop, -1, oprop);
     BKE_lib_override_library_property_delete(id->override_library, oprop);
-    RNA_property_copy(bmain, &ptr, &src, prop, -1);
   }
 
   /* Outliner e.g. has to be aware of this change. */

source/blender/editors/space_outliner/outliner_draw.cc
@@ -1988,6 +1988,7 @@ static void outliner_draw_overrides_rna_buts(ui::Block *block,
     {
       StringRefNull op_label = override_op_elem->get_override_operation_label();
       if (!op_label.is_empty()) {
+        StringRefNull op_tooltip = override_op_elem->get_override_operation_tooltip();
         uiDefBut(block,
                  ui::ButtonType::Label,
                  op_label,
@@ -1998,7 +1999,7 @@ static void outliner_draw_overrides_rna_buts(ui::Block *block,
                  nullptr,
                  0,
                  0,
-                 "");
+                 op_tooltip);
         continue;
       }
     }

source/blender/editors/space_outliner/tree/tree_element_overrides.cc
@@ -238,7 +238,10 @@ TreeElementOverridesPropertyOperation::TreeElementOverridesPropertyOperation(
 
 StringRefNull TreeElementOverridesPropertyOperation::get_override_operation_label() const
 {
-  switch (operation_->operation) {
+  if (operation_->label) {
+    return operation_->label;
+  }
+  switch (eID_OverrideLib_Op(operation_->operation)) {
     case LIBOVERRIDE_OP_INSERT_AFTER:
     case LIBOVERRIDE_OP_INSERT_BEFORE:
       return RPT_("Added through override");
@@ -255,10 +258,18 @@ StringRefNull TreeElementOverridesPropertyOperation::get_override_operation_labe
       return RPT_("Subtractive override");
     case LIBOVERRIDE_OP_MULTIPLY:
       return RPT_("Multiplicative override");
-    default:
-      BLI_assert_unreachable();
-      return {};
+    case LIBOVERRIDE_OP_CUSTOM:
+      return RPT_("Custom override");
   }
+  return RPT_("Unknown override");
+}
+
+StringRefNull TreeElementOverridesPropertyOperation::get_override_operation_tooltip() const
+{
+  if (operation_->tooltip) {
+    return operation_->tooltip;
+  }
+  return {};
 }
 
 std::optional<BIFIconID> TreeElementOverridesPropertyOperation::get_icon() const

FrenkyBojler/blender-ios

commit a1595b558f5b767397cb06c507e9466120bbc653

Fix: LibOverride geonodes packed bakes comparison mistake.
In `rna_NodesModifierBake_override_diff`, the blob_files comparison
loop had an unconditional break statement outside the if body due to
missing braces, causing it to exit after the first iteration even
when no difference was found. Added braces to make the break
conditional on the difference check, matching the adjacent
meta_files loop.

This issue was introduced in 329061c2392.

source/blender/makesrna/intern/rna_modifier.cc
@@ -2206,8 +2206,10 @@ void rna_NodesModifierBake_override_diff(Main *bmain, RNAPropertyOverrideDiffCon
                StringRefNull(nmd_bake_b->packed->blob_files[i].name)) ||
               (nmd_bake_a->packed->blob_files[i].data() !=
                nmd_bake_b->packed->blob_files[i].data()))
+          {
             is_different = true;
-          break;
+            break;
+          }
         }
       }
       if (!is_different) {