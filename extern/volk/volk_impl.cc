227ecc39ab5d72e7f0f4eb8392a189bf4350addd
Jeroen Bakker <jeroen@blender.org>
2026-06-23 09:34:31 +0200

    Replace MVK with Metal

--- extern/volk/volk_impl.cc
+++ extern/volk/volk_impl.cc
@@ -7,7 +7,7 @@
 #ifdef _WIN32
 #  define VK_USE_PLATFORM_WIN32_KHR
 #elif defined(__APPLE__)
-#  define VK_USE_PLATFORM_MACOS_MVK
+#  define VK_USE_PLATFORM_METAL_EXT
 #else
 #  define VK_USE_PLATFORM_WAYLAND_KHR
 #  define VK_USE_PLATFORM_XLIB_KHR
