fca0eebb2a058326555697b8a568d8ed33cfc58d
Testfiya <Akhater00@outlook.com>
2026-06-24 00:58:13 +0300

    Build: OIDN: refactor executable path, enable metal for ios

--- build_files/build_environment/cmake/openimagedenoise.cmake
+++ build_files/build_environment/cmake/openimagedenoise.cmake
@@ -2,41 +2,34 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+if(WITH_APPLE_CROSSPLATFORM)
+  # Use cross-compiled host ISPC
+  set(ISPC_EXECUTABLE_PATH ${CMAKE_DEPS_CROSSCOMPILE_BUILDDIR}/deps_arm64/Release/ispc/bin/ispc)
+  if(NOT EXISTS ${ISPC_EXECUTABLE_PATH})
+    message(FATAL_ERROR "Could not find cross-compiled ISPC executable at: ${ISPC_EXECUTABLE_PATH}. Please ensure host dependencies are built before attempting to build for ios.")
+  endif()
+else()
+  set(ISPC_EXECUTABLE_PATH ${LIBDIR}/ispc/bin/ispc)
+endif()
+
 set(OIDN_EXTRA_ARGS
   -DCMAKE_BUILD_TYPE=Release
   -DOIDN_APPS=OFF
   -DTBB_ROOT=${LIBDIR}/tbb
-  
+  -DISPC_EXECUTABLE=${ISPC_EXECUTABLE_PATH}
   -DOIDN_FILTER_RTLIGHTMAP=OFF
   -DPython_EXECUTABLE=${PYTHON_BINARY}
 )
 if(APPLE)
-  if(WITH_APPLE_CROSSPLATFORM)
-    # Use cross-compiled host ISPC
-    set(ISPC_EXECUTABLE_PATH ${CMAKE_DEPS_CROSSCOMPILE_BUILDDIR}/deps_arm64/Release/ispc/bin/ispc)
-    if( NOT EXISTS ${ISPC_EXECUTABLE_PATH})
-      message(FATAL_ERROR "Could not find cross-compiled ISPC executable at: ${ISPC_EXECUTABLE_PATH}. Please ensure host dependencies are built before attempting to build for ios.")
-    endif()
-
-    # TODO: Enable OIDN_DEVICE_METAL for cross-platform builds.
-    set(OIDN_EXTRA_ARGS
-      ${OIDN_EXTRA_ARGS}
-      -DISPC_EXECUTABLE=${ISPC_EXECUTABLE_PATH}
-    )
-  else()
-    set(OIDN_EXTRA_ARGS
-      ${OIDN_EXTRA_ARGS}
-      -DISPC_EXECUTABLE=${LIBDIR}/ispc/bin/ispc
-      -DOIDN_DEVICE_METAL=ON
-    )
-  endif()
-
+  set(OIDN_EXTRA_ARGS
+    ${OIDN_EXTRA_ARGS}
+    -DOIDN_DEVICE_METAL=ON
+  )
 else()
   set(OIDN_EXTRA_ARGS
     ${OIDN_EXTRA_ARGS}
     -DOIDN_DEVICE_CPU=ON
     -DLEVEL_ZERO_ROOT=${LIBDIR}/level-zero
-    -DISPC_EXECUTABLE=${LIBDIR}/ispc/bin/ispc
   )
 
   # x64 platforms support SyCL and HIP, ARM64 doesn't
