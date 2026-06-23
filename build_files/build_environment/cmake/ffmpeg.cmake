ac033299beea3e24682fd5664ce112a6245505b7
Testfiya <Akhater00@outlook.com>
2026-06-23 14:02:33 +0300

    Build: FFmpeg: use APPLE_TARGET_DEVICE for target-os instead of hardcoded ios-simulator.

--- build_files/build_environment/cmake/ffmpeg.cmake
+++ build_files/build_environment/cmake/ffmpeg.cmake
@@ -145,7 +145,7 @@ if(APPLE)
       ${FFMPEG_EXTRA_FLAGS}
       --enable-cross-compile
       --arch=aarch64
-      --target-os=ios-simulator
+      --target-os=${APPLE_TARGET_DEVICE}
 
       --sysroot=${CMAKE_OSX_SYSROOT} 
       --extra-ldflags=${PLATFORM_LDFLAGS} 
