c683a40e808bfbdc0b0a707f5f35167f2265b9bc
Brecht Van Lommel <brecht@blender.org>
2026-06-24 14:11:39 +0200

    Fix #160588: EEVEE sampling does not converge with missing image texture
    Images that failed to loaded were continuously detected as requiring
    a deferred image load. Now return the error texture instead of null
    for this detection. This was a regression from 165160de2d.
    
    Additionally, also handle similar issues when GPU texture creation fails
    with a new IMB_GPU_LOAD_FAILED flag. This was partially a pre-existing
    issue.

--- source/blender/imbuf/intern/util_gpu.cc
+++ source/blender/imbuf/intern/util_gpu.cc
@@ -527,8 +527,10 @@ gpu::Texture *IMB_acquire_gpu_texture(const char *name,
   }
   gpu::Texture *tex = IMB_create_gpu_texture(name, ibuf, create_flags);
   if (tex == nullptr) {
+    ibuf->gpu.flag |= IMB_GPU_LOAD_FAILED;
     return nullptr;
   }
+  ibuf->gpu.flag &= ~IMB_GPU_LOAD_FAILED;
 
   GPU_texture_extend_mode(tex, GPU_SAMPLER_EXTEND_MODE_REPEAT);
 
@@ -568,6 +570,16 @@ void IMB_free_gpu_textures(ImBuf *ibuf)
     ibuf->gpu.texture = nullptr;
   }
   ibuf->gpu.flag = ImBufGPUFlag(0);
+}
+
+void IMB_clear_gpu_load_failed(ImBuf *ibuf)
+{
+  if (!ibuf) {
+    return;
+  }
+
+  std::scoped_lock lock(ibuf->gpu.mutex);
+  ibuf->gpu.flag &= ~IMB_GPU_LOAD_FAILED;
 }
 
 void IMB_assign_gpu_texture(ImBuf *ibuf, gpu::Texture *texture)
