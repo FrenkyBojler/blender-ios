5cacc5c0e23ff31a07a3b412cb3659d9e6ef0005
Guillermo Venegas <guillermovcra@gmail.com>
2026-06-25 10:07:08 -0600

    init

--- source/blender/editors/screen/area.cc
+++ source/blender/editors/screen/area.cc
@@ -1775,8 +1775,7 @@ static void region_rect_recursive(
       }
     }
     else {
-      if (region->regiontype == RGN_TYPE_UI &&
-          BKE_regiontype_uses_categories(region->runtime->type))
+      if (BKE_regiontype_uses_category_tabs(region->runtime->type))
       {
         /* Update category tab width when #USER_UIFLAG2_PANEL_TABS_COMPACT flag is set/unset. */
         const float aspect = BLI_rctf_size_y(&region->v2d.cur) /
