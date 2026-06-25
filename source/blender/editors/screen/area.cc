blender/blender

commit 89dff61d3d33f5042dc650aa65865fc8993ec164

Fix #157707: UI: Clipped tab drawing when switching to compact tabs
This updates sidebar region width so they can fit compact tabs on
preference change.

Pull Request: https://projects.blender.org/blender/blender/pulls/160307

source/blender/editors/screen/area.cc
@@ -1775,6 +1775,18 @@ static void region_rect_recursive(
       }
     }
     else {
+      if (region->regiontype == RGN_TYPE_UI &&
+          BKE_regiontype_uses_categories(region->runtime->type))
+      {
+        /* Update category tab width when #USER_UIFLAG2_PANEL_TABS_COMPACT flag is set/unset. */
+        const float aspect = BLI_rctf_size_y(&region->v2d.cur) /
+                             (BLI_rcti_size_y(&region->v2d.mask) + 1);
+        const int tab_auto_snap_width = (UI_PANEL_CATEGORY_MIN_WIDTH + ui::PANEL_MIN_DRAW_WIDTH) *
+                                        UI_SCALE_FAC / aspect;
+        if (prefsizex < tab_auto_snap_width) {
+          prefsizex = UI_PANEL_CATEGORY_MIN_WIDTH * UI_SCALE_FAC / aspect;
+        }
+      }
       int fac = rct_fits(winrct, SCREEN_AXIS_H, prefsizex);
 
       if (fac < 0) {

source/blender/makesrna/intern/rna_userdef.cc
@@ -322,6 +322,19 @@ static void rna_userdef_update(Main * /*bmain*/, Scene * /*scene*/, PointerRNA *
   USERDEF_TAG_DIRTY;
 }
 
+static void rna_userdef_update_compact_tabs(Main *bmain, Scene *scene, PointerRNA *ptr)
+{
+  rna_userdef_update(bmain, scene, ptr);
+  auto *wm = static_cast<wmWindowManager *>(bmain->wm.first);
+  if (!wm) {
+    return;
+  }
+  for (wmWindow &win : wm->windows) {
+    /* Notify regions so they can update its category tab width when showing only tabs. */
+    WM_event_add_notifier_ex(wm, &win, NC_SCREEN | NA_EDITED, nullptr);
+  }
+}
+
 static void rna_userdef_theme_update(Main *bmain, Scene *scene, PointerRNA *ptr)
 {
   /* Recreate gizmos when changing themes. */
@@ -6246,7 +6259,7 @@ static void rna_def_userdef_system(BlenderRNA *brna)
       prop,
       "Compact Sidebar Tabs",
       "Display sidebar tabs in a compact size that shows icons when available");
-  RNA_def_property_update(prop, 0, "rna_userdef_update");
+  RNA_def_property_update(prop, 0, "rna_userdef_update_compact_tabs");
 
   prop = RNA_def_property(srna, "viewport_aa", PROP_ENUM, PROP_NONE);
   RNA_def_property_enum_items(prop, rna_enum_userdef_viewport_aa_items);

FrenkyBojler/blender-ios

commit a3920e921bc1e0cf337a3731698ffa4ff44a3043

Fix: Regression: Wrong size of preferences categories tab
Caused by 89dff61d3d. prefsize fo ui region is infinite due to
division by aspect which is zero at area init. Make aspect=1 explicitly
in such cases to avoid the issue.

source/blender/editors/screen/area.cc
@@ -1779,8 +1779,11 @@ static void region_rect_recursive(
           BKE_regiontype_uses_categories(region->runtime->type))
       {
         /* Update category tab width when #USER_UIFLAG2_PANEL_TABS_COMPACT flag is set/unset. */
-        const float aspect = BLI_rctf_size_y(&region->v2d.cur) /
+        float aspect = BLI_rctf_size_y(&region->v2d.cur) /
                              (BLI_rcti_size_y(&region->v2d.mask) + 1);
+        if (IS_EQF(aspect, 0)) {
+          aspect = 1.0f;
+        }
         const int tab_auto_snap_width = (UI_PANEL_CATEGORY_MIN_WIDTH + ui::PANEL_MIN_DRAW_WIDTH) *
                                         UI_SCALE_FAC / aspect;
         if (prefsizex < tab_auto_snap_width) {