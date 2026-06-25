blender/blender

commit 477336626ed5934bde1ef957c960f6f867d2ace8

Fix: Internal attributes can become active
"Active" is considered UI data, so should not point to internal
attributes (internal ones are filtered out in the UI via the
`XXX_UL_attributes` UI Lists, checking `is_internal`).

When removing or converting attributes, geometry
`attributes_active_index` was just decremented without checking if the
new index would point to an internal attribute.

Resolved by introducing `BKE_attributes_active_set_first_non_internal`
that goes through available attributes and only sets a **non-internal**
attribute active (checking via `allow_procedural_attribute_access`).

Also improves reporting for `BKE_attribute_remove` (will now give the
attribute name in case it fails) and adds an assert in case
`BKE_attributes_active_set` tries to set an internal attribute active.

This came up in #160091.

Pull Request: https://projects.blender.org/blender/blender/pulls/160380

source/blender/blenkernel/intern/attribute.cc
@@ -464,7 +464,10 @@ bool BKE_attribute_remove(AttributeOwner &owner, const StringRef name, ReportLis
     return false;
   }
   if (BKE_attribute_required(owner, name)) {
-    BKE_report(reports, RPT_ERROR, "Attribute is required and cannot be removed");
+    BKE_reportf(reports,
+                RPT_ERROR,
+                "Attribute '%s' is required and cannot be removed",
+                std::string(name).c_str());
     return false;
   }
 
@@ -725,6 +728,7 @@ std::optional<StringRefNull> BKE_attributes_active_name_get(AttributeOwner &owne
 
 void BKE_attributes_active_set(AttributeOwner &owner, const StringRef name)
 {
+  BLI_assert(bke::allow_procedural_attribute_access(name));
   if (owner.type() == AttributeOwnerType::Mesh) {
     const Mesh *mesh = owner.get_mesh();
     if (mesh->runtime->edit_mesh) {
@@ -764,6 +768,42 @@ int *BKE_attributes_active_index_p(AttributeOwner &owner)
     }
   }
   return nullptr;
+}
+
+void BKE_attributes_active_index_validate(AttributeOwner &owner)
+{
+  int *active_index = BKE_attributes_active_index_p(owner);
+
+  if (*active_index < 0) {
+    return;
+  }
+
+  bke::AttributeStorage *attributes = owner.get_storage();
+
+  /* First try downwards. */
+  int index_check = *active_index;
+  while (index_check >= 0) {
+    bke::Attribute attribute_check = attributes->at_index(index_check);
+    if (bke::allow_procedural_attribute_access(attribute_check.name())) {
+      *active_index = index_check;
+      return;
+    }
+    index_check--;
+  }
+
+  /* Still not found? Try upwards. */
+  index_check = *active_index + 1;
+  while (index_check < attributes->count()) {
+    bke::Attribute attribute_check = attributes->at_index(index_check);
+    if (bke::allow_procedural_attribute_access(attribute_check.name())) {
+      *active_index = index_check;
+      return;
+    }
+    index_check++;
+  }
+
+  /* Still not found? Mark none as active. */
+  *active_index = -1;
 }
 
 std::optional<StringRef> BKE_attribute_from_index(AttributeOwner &owner,

source/blender/blenkernel/BKE_attribute.h
@@ -117,6 +117,12 @@ std::optional<StringRefNull> BKE_attributes_active_name_get(AttributeOwner &owne
 void BKE_attributes_active_set(AttributeOwner &owner, StringRef name);
 void BKE_attributes_active_clear(AttributeOwner &owner);
 int *BKE_attributes_active_index_p(AttributeOwner &owner);
+/**
+ * After changing the active attribute index (e.g after removing an attribute) make sure that index
+ * is valid (is pointing to a non-internal attribute), f it is not, then set it to the first
+ * non-internal attribute.
+ */
+void BKE_attributes_active_index_validate(AttributeOwner &owner);
 
 std::optional<StringRef> BKE_attribute_from_index(AttributeOwner &owner,
                                                   int lookup_index,

source/blender/editors/geometry/geometry_attributes.cc
@@ -509,6 +509,7 @@ static wmOperatorStatus geometry_attribute_remove_exec(bContext *C, wmOperator *
   if (*active_index > 0) {
     *active_index -= 1;
   }
+  BKE_attributes_active_index_validate(owner);
 
   DEG_id_tag_update(id, ID_RECALC_GEOMETRY);
   WM_main_add_notifier(NC_GEOM | ND_DATA, id);
@@ -732,10 +733,12 @@ static wmOperatorStatus geometry_attribute_convert_exec(bContext *C, wmOperator 
           }
         }
         BKE_object_defgroup_active_index_set(ob, defgroup_index + 1);
+
         int *active_index = BKE_attributes_active_index_p(owner);
         if (*active_index > 0) {
           *active_index -= 1;
         }
+        BKE_attributes_active_index_validate(owner);
         break;
       }
     }

FrenkyBojler/blender-ios

commit e1195583c4edbf5f4501389d4bf86a8f4e3abfed

Fix #160653: Crash removing attributes in edit-mode
Caused by 477336626ed5

Code was trying to get the an attribute at index zero where no
attributes existed, now accounted for with a special case that returns
early.

Also generally harden that function a bit more:
- set the index to -1 specifically when a negative index is passed
- make the code work if an index greater than the number of attributes
is passed

Also add the editmode case (here, the attributes, attribute length and
indices can be different).

NOTE: this still does dont touch issues with `active_index` and
modeswitching (might be tackled by !160340) or setting internal
attributes active via python/RNA (should be disallowed imo), but will
also look into that later.

source/blender/blenkernel/intern/attribute.cc
@@ -773,9 +773,56 @@ int *BKE_attributes_active_index_p(AttributeOwner &owner)
 void BKE_attributes_active_index_validate(AttributeOwner &owner)
 {
   int *active_index = BKE_attributes_active_index_p(owner);
-
   if (*active_index < 0) {
+    /* Mark none as active. */
+    *active_index = -1;
     return;
+  }
+
+  const int attributes_num = BKE_attributes_length(owner, ATTR_DOMAIN_MASK_ALL, CD_MASK_PROP_ALL);
+  if (attributes_num == 0) {
+    /* Mark none as active. */
+    *active_index = -1;
+    return;
+  }
+
+  if (owner.type() == AttributeOwnerType::Mesh) {
+    const Mesh *mesh = owner.get_mesh();
+    if (mesh->runtime->edit_mesh.get()) {
+      /* First try downwards. */
+      int index_check = *active_index;
+      while (index_check >= 0) {
+        if (index_check < attributes_num) {
+          std::optional<StringRef> attribute_name_check = BKE_attribute_from_index(
+              owner, index_check, ATTR_DOMAIN_MASK_ALL, CD_MASK_PROP_ALL);
+          if (attribute_name_check.has_value() &&
+              bke::allow_procedural_attribute_access(attribute_name_check.value()))
+          {
+            *active_index = index_check;
+            return;
+          }
+        }
+        index_check--;
+      }
+
+      /* Still not found? Try upwards. */
+      index_check = *active_index + 1;
+      while (index_check < attributes_num) {
+        std::optional<StringRef> attribute_name_check = BKE_attribute_from_index(
+            owner, index_check, ATTR_DOMAIN_MASK_ALL, CD_MASK_PROP_ALL);
+        if (attribute_name_check.has_value() &&
+            bke::allow_procedural_attribute_access(attribute_name_check.value()))
+        {
+          *active_index = index_check;
+          return;
+        }
+        index_check++;
+      }
+
+      /* Still not found? Mark none as active. */
+      *active_index = -1;
+      return;
+    }
   }
 
   bke::AttributeStorage *attributes = owner.get_storage();
@@ -783,10 +830,12 @@ void BKE_attributes_active_index_validate(AttributeOwner &owner)
   /* First try downwards. */
   int index_check = *active_index;
   while (index_check >= 0) {
-    bke::Attribute attribute_check = attributes->at_index(index_check);
-    if (bke::allow_procedural_attribute_access(attribute_check.name())) {
-      *active_index = index_check;
-      return;
+    if (index_check < attributes_num) {
+      bke::Attribute attribute_check = attributes->at_index(index_check);
+      if (bke::allow_procedural_attribute_access(attribute_check.name())) {
+        *active_index = index_check;
+        return;
+      }
     }
     index_check--;
   }