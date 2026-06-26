a7b8688144aef84ed5a79a742b7e549ff4bdd5f4
Kévin Dietrich <kevin.dietrich@mailoo.org>
2026-06-26 16:33:12 +0200

    Fix #158751: JPEG export fails with long CryptoMatte manifests
    When writing JPEG files with long CryptoMatte manifests, where long is
    defined as bigger than 65533 bytes, the file is not written and only a
    single error message reading "Bogus marker length" is outputted.
    
    This happens because libjpeg hardcodes a maximum marker length of 65533
    and raises an error if we try to write a marker exceeding that length.
    Raising the error then calls our `jpeg_error` callback which performs a
    longjmp to reset the state of the process to whatever it was set to
    before trying to write the file so nothing is written to disk, and no
    other error message is printed.
    
    As we do not really support reading CryptoMatte manifests from JPEG
    files, and since the manifests (and other comments) are prefixed with
    "Blender:" (which implies that they may not be useful for other
    programs), to fix this, simply write the data truncated to the maximum
    allowed marker length. This also prints a warning that some data is
    written truncated.

--- source/blender/imbuf/intern/format_jpeg.cc
+++ source/blender/imbuf/intern/format_jpeg.cc
@@ -608,6 +608,10 @@ ImBuf *imb_thumbnail_jpeg(const char *filepath,
 /** \name Save JPG Image
  * \{ */
 
+/* libjpeg has a maximum comment/marker legth of 65533, however it does not provide a definition
+ * for that, so we hardcode it ourselves. */
+#define MAX_LIBJPEG_MARKER_LENGTH 65533
+
 static void write_jpeg(jpeg_compress_struct *cinfo, ImBuf *ibuf)
 {
   JSAMPLE *buffer = nullptr;
@@ -659,6 +663,16 @@ static void write_jpeg(jpeg_compress_struct *cinfo, ImBuf *ibuf)
          */
         text_len = BLI_snprintf_utf8_rlen(
             text, text_size, "Blender:%s:%s", prop.name, IDP_string_get(&prop));
+        /* Truncate the data if it does not fit in a single marker. As the data is prefixed with
+         * "Blender:" it may not be useful for other programs anyway and we might not really
+         * support reading it back in Blender. The only type of data that would suffer truncation
+         * is from CryptoMatte manifests. Further, giving a buffer to libjpeg bigger that
+         * MAX_LIBJPEG_MARKER_LENGTH will result in the process being exited. See #158751.
+         */
+        if (text_len > MAX_LIBJPEG_MARKER_LENGTH) {
+          CLOG_WARN(&LOG, "Writing truncated data for \"%s\"", prop.name);
+          text_len = MAX_LIBJPEG_MARKER_LENGTH;
+        }
         /* Don't write the null byte (not expected by the JPEG format). */
         jpeg_write_marker(cinfo, JPEG_COM, reinterpret_cast<JOCTET *>(text), uint(text_len));
 
