daf5383373544e8ef2728b578ae1f7a6d3d225c6
Criss-Ivana <criss.ivana@gmail.com>
2026-06-28 17:34:42 +0300

    Add codegen conversion macros

--- source/blender/gpu/shaders/common/gpu_shader_math_vector_compare_lib.glsl
+++ source/blender/gpu/shaders/common/gpu_shader_math_vector_compare_lib.glsl
@@ -20,6 +20,9 @@ template<typename VecT> bool is_zero(VecT vec)
 template bool is_zero<float2>(float2);
 template bool is_zero<float3>(float3);
 template bool is_zero<float4>(float4);
+template bool is_zero<int2>(int2);
+template bool is_zero<int3>(int3);
+template bool is_zero<int4>(int4);
 
 /**
  * Return true if any component is equal to zero.
