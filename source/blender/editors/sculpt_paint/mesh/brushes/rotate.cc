0f17f25a1332793e29c1ddcb306793715ed6d215
Jerry Wei <79512937+JerryWeii-27@users.noreply.github.com>
2026-06-28 18:20:37 -0400

    Format code

--- source/blender/editors/sculpt_paint/mesh/brushes/rotate.cc
+++ source/blender/editors/sculpt_paint/mesh/brushes/rotate.cc
@@ -184,8 +184,15 @@ static void calc_bmesh(const Depsgraph &depsgraph,
   orig_position_data_gather_bmesh(*ss.bm_log, verts, orig_positions, orig_normals);
 
   if (BKE_brush_has_cube_tip(&brush, PaintMode::Sculpt)) {
-    calc_cube_tip_factors_common_from_orig_data_bmesh(
-        depsgraph, brush, object, mat, orig_positions, orig_normals, node, tls.factors, tls.distances);
+    calc_cube_tip_factors_common_from_orig_data_bmesh(depsgraph,
+                                                      brush,
+                                                      object,
+                                                      mat,
+                                                      orig_positions,
+                                                      orig_normals,
+                                                      node,
+                                                      tls.factors,
+                                                      tls.distances);
   }
   else {
     calc_factors_common_from_orig_data_bmesh(
@@ -230,8 +237,16 @@ void do_rotate_brush(const Depsgraph &depsgraph,
       node_mask.foreach_index(
           [&](const int i) {
             LocalData &tls = all_tls.local();
-            calc_faces(
-                depsgraph, sd, brush, mat, angle, attribute_data, nodes[i], object, tls, position_data);
+            calc_faces(depsgraph,
+                       sd,
+                       brush,
+                       mat,
+                       angle,
+                       attribute_data,
+                       nodes[i],
+                       object,
+                       tls,
+                       position_data);
             bke::pbvh::update_node_bounds_mesh(position_data.eval, nodes[i]);
           },
           exec_mode::grain_size(1));
--- source/blender/editors/sculpt_paint/mesh/brushes/thumb.cc
+++ source/blender/editors/sculpt_paint/mesh/brushes/thumb.cc
@@ -151,8 +151,15 @@ static void calc_bmesh(const Depsgraph &depsgraph,
   orig_position_data_gather_bmesh(*ss.bm_log, verts, orig_positions, orig_normals);
 
   if (BKE_brush_has_cube_tip(&brush, PaintMode::Sculpt)) {
-    calc_cube_tip_factors_common_from_orig_data_bmesh(
-        depsgraph, brush, object, mat, orig_positions, orig_normals, node, tls.factors, tls.distances);
+    calc_cube_tip_factors_common_from_orig_data_bmesh(depsgraph,
+                                                      brush,
+                                                      object,
+                                                      mat,
+                                                      orig_positions,
+                                                      orig_normals,
+                                                      node,
+                                                      tls.factors,
+                                                      tls.distances);
   }
   else {
     calc_factors_common_from_orig_data_bmesh(
