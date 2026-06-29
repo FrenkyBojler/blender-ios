8ea8d6016a33e33abc713d3c18da852a03ae30ed
Clément Foucault <foucault.clem@gmail.com>
2026-06-29 18:12:05 +0200

    Fix #160662: EEVEE: Wrong vertex attribute slot assignment
    The logic was flawed and allowed multiple time the same assignemnt.
    This also fix the assertion on the number of attribute in
    `GPUCodegen::generate_attribs()` by reintroducing the check for
    `GPU_MAX_ATTR` in `gpu_node_graph_add_attribute`.

--- source/blender/gpu/GPU_shader.hh
+++ source/blender/gpu/GPU_shader.hh
@@ -34,8 +34,7 @@ struct ShaderCreateInfo;
 }  // namespace gpu
 
 /** Opaque type hiding #gpu::shader::ShaderCreateInfo */
-/* Hardware limit is 16. Position attribute is always needed so we reduce to 15.
- * This makes sure the GPUVertexFormat name buffer does not overflow. */
+/* Hardware limit is 16 for meshes until we use a vertex pull mechanism. */
 constexpr static int GPU_MAX_ATTR = 15;
 
 /* -------------------------------------------------------------------- */
--- source/blender/gpu/intern/gpu_node_graph.cc
+++ source/blender/gpu/intern/gpu_node_graph.cc
@@ -413,8 +413,7 @@ static GPUMaterialAttribute *gpu_node_graph_add_attribute(GPUNodeGraph *graph,
     num_attributes++;
   }
 
-  /* Add new requested attribute if it's within GPU limits. */
-  if (attr == nullptr) {
+  if (attr == nullptr && num_attributes < GPU_MAX_ATTR) {
     attr = MEM_new_zeroed<GPUMaterialAttribute>(__func__);
     attr->is_default_color = is_default_color;
     attr->is_hair_length = is_hair_length;
@@ -627,7 +626,6 @@ GPUNodeLink *GPU_uniform_attribute(GPUMaterial *mat,
   GPUNodeGraph *graph = gpu_material_node_graph(mat);
   GPUUniformAttr *attr = gpu_node_graph_add_uniform_attribute(graph, name, use_dupli);
 
-  /* Dummy fallback if out of slots. */
   if (attr == nullptr) {
     *r_hash = 0;
     static const float zero_data[GPU_MAX_CONSTANT_DATA] = {0.0f};
