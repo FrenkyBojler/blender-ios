blender/blender

commit d84af2d956a64bf978506866da77674ad00995cd

Fix: Shapekey crash due to active tree element in outliner
There's crash inside `tree_element_shapekey_state_get` due to nullptr
when active item is deleted from the outliner (resolves #160766). Also
crashes in `tree_elemment_shapekey_active_set` when clicked on shapekey
item but their parent object isn't active. To fix this, trace back the
parent `ID_OB` for that shapekey.

Pull Request: https://projects.blender.org/blender/blender/pulls/160793

source/blender/editors/space_outliner/outliner_select.cc
@@ -855,12 +855,18 @@ void tree_element_activate(bContext *C,
   }
 }
 
-static void tree_elemment_shapekey_active_set(bContext *C, Object &ob, TreeElement &te)
+static void tree_elemment_shapekey_active_set(bContext *C, TreeElement *te)
 {
-  PointerRNA object_ptr = RNA_pointer_create_discrete(&ob.id, RNA_Object, &ob);
-  PropertyRNA *prop = RNA_struct_find_property(&object_ptr, "active_shape_key_index");
-  RNA_property_int_set(&object_ptr, prop, te.index);
-  RNA_property_update(C, &object_ptr, prop);
+  TreeElement *parent_te = outliner_search_back_te(te, ID_OB);
+  TreeStoreElem *parent_tselem = TREESTORE(parent_te);
+  Object *ob = id_cast<Object *>(parent_tselem->id);
+
+  if (ob) {
+    PointerRNA object_ptr = RNA_pointer_create_discrete(&ob->id, RNA_Object, ob);
+    PropertyRNA *prop = RNA_struct_find_property(&object_ptr, "active_shape_key_index");
+    RNA_property_int_set(&object_ptr, prop, te->index);
+    RNA_property_update(C, &object_ptr, prop);
+  }
 }
 
 void tree_element_type_active_set(bContext *C,
@@ -925,7 +931,7 @@ void tree_element_type_active_set(bContext *C,
       tree_element_layer_collection_activate(C, te);
       break;
     case TSE_SHAPE_KEY_BLOCK:
-      tree_elemment_shapekey_active_set(C, *tvc.obact, *te);
+      tree_elemment_shapekey_active_set(C, te);
     default:
       break;
   }
@@ -1200,9 +1206,9 @@ eOLDrawState tree_element_active_state_get(const TreeViewContext &tvc,
   return OL_DRAWSEL_NONE;
 }
 
-static eOLDrawState tree_element_shapekey_state_get(const Object &ob, const TreeElement *te)
+static eOLDrawState tree_element_shapekey_state_get(const Object *ob, const TreeElement *te)
 {
-  if (ob.shapenr == te->index + 1) {
+  if (ob && (ob->shapenr == te->index + 1)) {
     return OL_DRAWSEL_NORMAL;
   }
   return OL_DRAWSEL_NONE;
@@ -1251,7 +1257,7 @@ eOLDrawState tree_element_type_active_state_get(const TreeViewContext &tvc,
     case TSE_BONE_COLLECTION:
       return tree_element_bone_collection_state_get(te, tselem);
     case TSE_SHAPE_KEY_BLOCK:
-      return tree_element_shapekey_state_get(*tvc.obact, te);
+      return tree_element_shapekey_state_get(tvc.obact, te);
     default:
       break;
   }

FrenkyBojler/blender-ios

commit 66361b33dc2768b8b5eaa1fe9fec8d005bde4ac0

Fix #160869: Outliner crash activating a shape key without parent object
Caused by d84af2d956

Since different shapekeys can be active on different objects (even if
they share the same mesh), it only really makes sense to try to update
"active_shape_key_index" from the Outliner if we can associate an
specific object with it. This is actually what d84af2d956 did, but it
fails when we expand in the 'Blender File' view:
- Meshes > Mesh > ShapeKeyName
- Shape Keys > ShapeKeyName

So instead of crashing, we just dont do anything when "activating"
shapekeys in these scenarios.

NOTE: I would also argue that marking them as "active" with
distinguishable icons does dont really make sense in these scenarios,
but can handle that in a separate PR

source/blender/editors/space_outliner/outliner_select.cc
@@ -857,15 +857,16 @@ void tree_element_activate(bContext *C,
 
 static void tree_elemment_shapekey_active_set(bContext *C, TreeElement *te)
 {
-  TreeElement *parent_te = outliner_search_back_te(te, ID_OB);
-  TreeStoreElem *parent_tselem = TREESTORE(parent_te);
-  Object *ob = id_cast<Object *>(parent_tselem->id);
+  if (TreeElement *parent_te = outliner_search_back_te(te, ID_OB)) {
+    TreeStoreElem *parent_tselem = TREESTORE(parent_te);
+    Object *ob = id_cast<Object *>(parent_tselem->id);
 
-  if (ob) {
-    PointerRNA object_ptr = RNA_pointer_create_discrete(&ob->id, RNA_Object, ob);
-    PropertyRNA *prop = RNA_struct_find_property(&object_ptr, "active_shape_key_index");
-    RNA_property_int_set(&object_ptr, prop, te->index);
-    RNA_property_update(C, &object_ptr, prop);
+    if (ob) {
+      PointerRNA object_ptr = RNA_pointer_create_discrete(&ob->id, RNA_Object, ob);
+      PropertyRNA *prop = RNA_struct_find_property(&object_ptr, "active_shape_key_index");
+      RNA_property_int_set(&object_ptr, prop, te->index);
+      RNA_property_update(C, &object_ptr, prop);
+    }
   }
 }
 