1814c30feedc6d353b405c4c26446e50a5aa0068
Nika Kutsniashvili <nickberckley@gmail.com>
2026-06-30 16:42:12 +0400

    Keymap and operator for adding strip modifiers

--- scripts/presets/keyconfig/keymap_data/blender_default.py
+++ scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -1299,6 +1299,7 @@ def km_property_editor(_params):
         ("constraint.apply", {"type": 'A', "value": 'PRESS', "ctrl": True}, {"properties": [("report", True)]}),
         # Strip modifiers
         ("sequencer.strip_modifier_duplicate", {"type": 'D', "value": 'PRESS', "shift": True}, None),
+        ("sequencer.add_strip_modifier_menu", {"type": 'A', "value": 'PRESS', "shift": True}, None),
     ])
 
     return keymap
