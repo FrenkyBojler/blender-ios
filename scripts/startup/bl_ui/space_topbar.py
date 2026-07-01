c3d29e656d5e37955b17d1148e2cc00f62d115a5
Jonas Holzman <jonas@blender.org>
2026-07-01 10:49:39 +0200

    UI: Tweak Project Setup opeator topbar layout position

--- scripts/startup/bl_ui/space_topbar.py
+++ scripts/startup/bl_ui/space_topbar.py
@@ -549,8 +549,11 @@ class TOPBAR_MT_edit(Menu):
 
         layout.separator()
 
-        layout.operator("screen.userpref_show", text="Preferences...", icon='PREFERENCES')
         layout.operator("screen.project_setup_show", text="Project Setup...", icon='PROJECT')
+
+        layout.separator()
+
+        layout.operator("screen.userpref_show", text="Preferences...", icon='PREFERENCES')
 
 
 class TOPBAR_MT_window(Menu):
