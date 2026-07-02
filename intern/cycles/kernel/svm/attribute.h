Zezo-Ai/blender

commit dcac328db32cd87461ed5bae4d4c6a57b31e1a22

Fix #157384: Cycles attribute breaks under certain conditions
Caused by 4d6c7718c5c.

Under certain conditions it is possible that the offset logic in the
AttributeTableBuilder::add() assigns offset of -1. This value just
happens to match ATTR_STD_NOT_FOUND, leading to false-positive check
that attribute is not found (for example, svm_node_attr_init() that
checks desc.offset != ATTR_STD_NOT_FOUND.

The simplest solution is to tweak the value of the ATTR_STD_NOT_FOUND
to make it very big negative value.

There is now an utility function to check whether AttributeDescriptor
points to an attribute that is found, to help possibly tweaking this
check in the future, and to make code a bit more semantically clear.

Pull Request: https://projects.blender.org/blender/blender/pulls/157410

intern/cycles/kernel/svm/attribute.h
@@ -29,7 +29,7 @@ ccl_device AttributeDescriptor svm_node_attr_init(KernelGlobals kg,
 
   if (sd->object != OBJECT_NONE) {
     desc = find_attribute(kg, sd, node.attr);
-    if (desc.offset == ATTR_STD_NOT_FOUND) {
+    if (!is_attribute_found(desc)) {
       desc = attribute_not_found();
       desc.offset = 0;
       desc.type = (NodeAttributeType)node.output_type;
@@ -85,7 +85,7 @@ svm_node_attr_surface_eval(KernelGlobals kg,
     return uv;
   }
 
-  if (node.attr == ATTR_STD_GENERATED && desc.element == ATTR_ELEMENT_NONE) {
+  if (node.attr == ATTR_STD_GENERATED && !is_attribute_found(desc)) {
     Float3Type f = shading_position<Float3Type>(sd);
     object_inverse_position_transform_if_object(kg, sd, &f);
     return f;

intern/cycles/kernel/geom/attribute.h
@@ -21,6 +21,11 @@ CCL_NAMESPACE_BEGIN
  * Lookup of attributes is different between OSL and SVM, as OSL is ustring
  * based while for SVM we use integer ids. */
 
+ccl_device_forceinline bool is_attribute_found(const ccl_private AttributeDescriptor &desc)
+{
+  return desc.offset != ATTR_STD_NOT_FOUND;
+}
+
 ccl_device_inline AttributeDescriptor attribute_not_found()
 {
   const AttributeDescriptor desc = {ATTR_ELEMENT_NONE, (NodeAttributeType)0, ATTR_STD_NOT_FOUND};

intern/cycles/kernel/geom/curve.h
@@ -130,7 +130,7 @@ ccl_device float curve_random(KernelGlobals kg, const ccl_private ShaderData *sd
 {
   if (sd->type & PRIMITIVE_CURVE) {
     const AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_CURVE_RANDOM);
-    return (desc.offset != ATTR_STD_NOT_FOUND) ? curve_attribute<float>(kg, sd, desc) : 0.0f;
+    return is_attribute_found(desc) ? curve_attribute<float>(kg, sd, desc) : 0.0f;
   }
   return 0.0f;
 }

intern/cycles/kernel/geom/point.h
@@ -79,7 +79,7 @@ ccl_device float point_random(KernelGlobals kg, const ccl_private ShaderData *sd
 {
   if (sd->type & PRIMITIVE_POINT) {
     const AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_POINT_RANDOM);
-    return (desc.offset != ATTR_STD_NOT_FOUND) ? point_attribute<float>(kg, sd, desc) : 0.0f;
+    return is_attribute_found(desc) ? point_attribute<float>(kg, sd, desc) : 0.0f;
   }
   return 0.0f;
 }

intern/cycles/kernel/geom/primitive.h
@@ -68,7 +68,7 @@ ccl_device void primitive_normal_set_undisplaced(KernelGlobals kg,
 
   if (sd->shader & SHADER_SMOOTH_NORMAL) {
     const AttributeDescriptor ndesc = find_attribute(kg, sd, ATTR_STD_NORMAL_UNDISPLACED);
-    if (ndesc.offset == ATTR_STD_NOT_FOUND) {
+    if (!is_attribute_found(ndesc)) {
       return;
     }
     N = safe_normalize(primitive_surface_attribute<float3>(kg, sd, ndesc));
@@ -112,7 +112,7 @@ ccl_device_forceinline float3 primitive_uv(KernelGlobals kg, const ccl_private S
 {
   const AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_UV);
 
-  if (desc.offset == ATTR_STD_NOT_FOUND) {
+  if (!is_attribute_found(desc)) {
     return make_float3(0.0f, 0.0f, 0.0f);
   }
 
@@ -131,7 +131,7 @@ ccl_device bool primitive_ptex(KernelGlobals kg,
   const AttributeDescriptor desc_face_id = find_attribute(kg, sd, ATTR_STD_PTEX_FACE_ID);
   const AttributeDescriptor desc_uv = find_attribute(kg, sd, ATTR_STD_PTEX_UV);
 
-  if (desc_face_id.offset == ATTR_STD_NOT_FOUND || desc_uv.offset == ATTR_STD_NOT_FOUND) {
+  if (!is_attribute_found(desc_face_id) || !is_attribute_found(desc_uv)) {
     return false;
   }
 
@@ -162,7 +162,7 @@ ccl_device Float3Type primitive_tangent(KernelGlobals kg, ccl_private ShaderData
   /* try to create spherical tangent from generated coordinates */
   const AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_GENERATED);
 
-  if (desc.offset != ATTR_STD_NOT_FOUND) {
+  if (is_attribute_found(desc)) {
     if constexpr (is_dual_v<Float3Type>) {
       dual3 data = primitive_surface_attribute<dual3>(kg, sd, desc);
       data = make_float3(-(data.y() - 0.5f), (data.x() - 0.5f), dual1());
@@ -224,7 +224,7 @@ ccl_device_forceinline float4 primitive_motion_vector(KernelGlobals kg,
   /* deformation motion */
   AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_MOTION_VERTEX_POSITION);
 
-  if (desc.offset != ATTR_STD_NOT_FOUND) {
+  if (is_attribute_found(desc)) {
     /* get motion info */
     const int numverts = kernel_data_fetch(objects, sd->object).numverts;
 

intern/cycles/kernel/geom/volume.h
@@ -35,7 +35,7 @@ ccl_device_inline Float3Type volume_normalized_position(KernelGlobals kg,
 
   object_inverse_position_transform_if_object(kg, sd, &P);
 
-  if (desc.offset != ATTR_STD_NOT_FOUND) {
+  if (is_attribute_found(desc)) {
     const Transform tfm = primitive_attribute_matrix(kg, desc);
     P = transform_point(&tfm, P);
   }

intern/cycles/kernel/integrator/volume_shader.h
@@ -368,7 +368,7 @@ ccl_device_inline void volume_shader_motion_blur(KernelGlobals kg,
   }
 
   const AttributeDescriptor v_desc = find_attribute(kg, sd, ATTR_STD_VOLUME_VELOCITY);
-  kernel_assert(v_desc.offset != ATTR_STD_NOT_FOUND);
+  kernel_assert(is_attribute_found(v_desc));
 
   const float3 P = sd->P;
   const float velocity_scale = kernel_data_fetch(objects, sd->object).velocity_scale;

intern/cycles/kernel/osl/closures.cpp
@@ -72,6 +72,7 @@ void OSLRenderServices::register_closures(OSL::ShadingSystem *ss)
       #lower, OSL_CLOSURE_##Upper##_ID, osl_closure_##lower##_params(), nullptr, nullptr);
 
 #include "closures_template.h"
+
   ss->register_closure(
       "layer", OSL_CLOSURE_LAYER_ID, osl_closure_layer_params(), nullptr, nullptr);
 }
@@ -135,7 +136,7 @@ void osl_eval_nodes_surface(const ThreadKernelGlobalsCPU *kg,
       /* set state as if undisplaced */
       if (sd->flag & SD_HAS_DISPLACEMENT) {
         const AttributeDescriptor desc = find_attribute(kg, sd, ATTR_STD_POSITION_UNDISPLACED);
-        kernel_assert(desc.offset != ATTR_STD_NOT_FOUND);
+        kernel_assert(is_attribute_found(desc));
 
         dual3 P = primitive_surface_attribute<dual3>(kg, sd, desc);
         object_position_transform(kg, sd, &P);

FrenkyBojler/blender-ios

commit 4925549b668ae7c99d1af71baeb5814c30644dd2

Fix #160980: Cycles area light texture using Generated coordinates is broken
Regression from dcac328db32c. svm_node_attr_init() set offset to 0 which
doesn't work with is_attribute_found() that expects ATTR_STD_NOT_FOUND. A
is_attribute_found() test was introduced in that commit in
svm_node_attr_surface_eval() which then didin't work.

I traced all users of this and found they all check either
(element & ATTR_ELEMENT_X) or is_attribute_found() now, which both work.

intern/cycles/kernel/svm/attribute.h
@@ -31,14 +31,12 @@ ccl_device AttributeDescriptor svm_node_attr_init(KernelGlobals kg,
     desc = find_attribute(kg, sd, node.attr);
     if (!is_attribute_found(desc)) {
       desc = attribute_not_found();
-      desc.offset = 0;
       desc.type = (NodeAttributeType)node.output_type;
     }
   }
   else {
     /* background */
     desc = attribute_not_found();
-    desc.offset = 0;
     desc.type = (NodeAttributeType)node.output_type;
   }
 

tests/files/render/light/area_light_generated_texture.blend
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa2564f5ac367d80bf8a198240ebc4f97cd35a3ef306a5afbeecc5f12f5ef4e6
+size 94655

tests/files/render/light/cycles_renders/area_light_generated_texture.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c66502ddf25cfc589461e421f4231b91cfb469de3db1517f8254319f89e9713a
+size 12564

tests/files/render/light/eevee_renders/area_light_generated_texture.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b290a3df4430a652dca8312433b4123a56c0da166a858de1f9e8ede39668bfae
+size 11608

tests/files/render/light/storm_renders/area_light_generated_texture.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7928e89c871725412ee20a94a176c7af5a6503eec8c24ebbd9b1a9f12b6ec9c
+size 6406