95d337fdd3e8238d08f2549b31a7184b47ed6fd1
Cristian Ivana <criss.ivana@gmail.com>
2026-07-02 14:15:34 +0300

    Add first 8 basic shader funcs

--- source/blender/gpu/shaders/material/gpu_shader_material_integer_math.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_integer_math.glsl
@@ -1,3 +1,51 @@
 /* SPDX-FileCopyrightText: 2026 Blender Authors
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
+
+[[node]]
+void integer_math_add(float a, float b, out float result)
+{
+  result = float(int(a) + int(b));
+}
+
+[[node]]
+void integer_math_subtract(float a, float b, out float result)
+{
+  result = float(int(a) - int(b));
+}
+
+[[node]]
+void integer_math_multiply(float a, float b, out float result)
+{
+  result = float(int(a) * int(b));
+}
+
+[[node]]
+void integer_math_divide(float a, float b, out float result)
+{
+  result = float((int(b) != 0) ? (int(a) / int(b)) : 0);
+}
+
+[[node]]
+void integer_math_power(float base, float exponent, out float result)
+{
+  result = float(int(pow(int(base), int(exponent))));
+}
+
+[[node]]
+void integer_math_multiply_add(float a, float b, float c, out float result)
+{
+  result = float(int(a) * int(b) + int(c));
+}
+
+[[node]]
+void integer_math_absolute(float a, out float result)
+{
+  result = float(abs(int(a)));
+}
+
+[[node]]
+void integer_math_negate(float a, out float result)
+{
+  result = float(-int(a));
+}
