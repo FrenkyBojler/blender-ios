4e9e2058328fa5297b263b37af6fb5996a86d2de
Cristian Ivana <criss.ivana@gmail.com>
2026-07-02 14:27:34 +0300

    Add comparison GPU funcs

--- source/blender/gpu/shaders/material/gpu_shader_material_integer_math.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_integer_math.glsl
@@ -23,7 +23,8 @@ void integer_math_multiply(float a, float b, out float result)
 [[node]]
 void integer_math_divide(float a, float b, out float result)
 {
-  result = float((int(b) != 0) ? (int(a) / int(b)) : 0);
+  const int ib = int(b);
+  result = float((ib != 0) ? (int(a) / ib) : 0);
 }
 
 [[node]]
@@ -42,6 +43,25 @@ void integer_math_multiply_add(float a, float b, float c, out float result)
 void integer_math_absolute(float a, out float result)
 {
   result = float(abs(int(a)));
+}
+
+[[node]]
+void integer_math_sign(float a, out float result)
+{
+  const int ia = int(a);
+  result = float(int(ia > 0) - int(ia < 0));
+}
+
+[[node]]
+void integer_math_minimum(float a, float b, out float result)
+{
+  result = float(min(int(a), int(b)));
+}
+
+[[node]]
+void integer_math_maximum(float a, float b, out float result)
+{
+  result = float(max(int(a), int(b)));
 }
 
 [[node]]
