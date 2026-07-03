4a4554023ab36f8748a0310ffb87a1409f354a25
Bart van der Braak <bart@blender.org>
2026-07-03 16:21:31 +0200

    WIP: Try new nuke strategy

--- build_files/build_environment/windows/nuke.cmd
+++ build_files/build_environment/windows/nuke.cmd
@@ -6,60 +6,16 @@ REM scratch.
 if "%1"=="" goto EOF:
 set ROOT=%~dp0\build\
 
-set CurPath=%ROOT%\s\vs1564D\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564D\x64\debug\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564D\x64\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564D\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564R\release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564R\x64\Release\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564R\x64\Release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1564R\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\output\win64_vc15\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+REM walk every subfolder under "s" (vs1764D, vs1564R, x64, debug, build, arm64, future names)
+REM %%D loop var, checks two names each folder: %1 and external_%1
+for /d /r "%ROOT%\s" %%D in (%1 external_%1) do (
+  REM nul trick = confirm folder real, not junk match
+  if exist "%%D\nul" echo removing "%%D" && rd /s /q "%%D"
+)
 
-set CurPath=%ROOT%\s\vs1664D\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664D\x64\debug\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664D\x64\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664D\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664R\release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664R\x64\Release\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664R\x64\Release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1664R\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\output\win64_vc16\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
+REM walk every subfolder under "output" (win64_vc15, winarm64_vc15, future ones)
+for /d /r "%ROOT%\output" %%D in (%1) do (
+  if exist "%%D\nul" echo removing "%%D" && rd /s /q "%%D"
+)
 
-set CurPath=%ROOT%\s\vs1764D\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764D\x64\debug\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764D\x64\debug\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764D\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764R\release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764R\x64\Release\external_%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764R\x64\Release\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\s\vs1764R\build\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-set CurPath=%ROOT%\output\win64_vc17\%1\
-if EXIST %CurPath%\nul ( echo removing "%CurPath%" && rd /s /q "%CurPath%" ) 
-
+:EOF
