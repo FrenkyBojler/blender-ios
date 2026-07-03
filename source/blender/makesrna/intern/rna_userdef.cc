ce3316818e3bcd154b555098b0c1262b685dacfd
golden-dragon <trmhtk2@gmail.com>
2026-07-03 05:31:39 +0300

    make format

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -5921,13 +5921,10 @@ static void rna_def_userdef_edit(BlenderRNA *brna)
       "Connect newly added movie strips by default if they have multiple channels");
 
   prop = RNA_def_property(srna, "default_strip_len", PROP_INT, PROP_TIME);
-  RNA_def_property_int_sdna(
-      prop, nullptr, "sequencer_default_strip_len");
+  RNA_def_property_int_sdna(prop, nullptr, "sequencer_default_strip_len");
   RNA_def_property_range(prop, 1, MAXFRAME);
   RNA_def_property_ui_text(
-      prop,
-      "Default Strip Length",
-      "Default length for newly created strips");
+      prop, "Default Strip Length", "Default length for newly created strips");
 
   /* duplication linking */
   prop = RNA_def_property(srna, "use_duplicate_mesh", PROP_BOOLEAN, PROP_NONE);
