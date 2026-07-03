17596e2f87d27ebf84ae79f6c4a6645c02d383c4
Brecht Van Lommel <brecht@blender.org>
2026-07-03 03:47:07 +0200

    GPU: Mipmaps: Write sRGB texture in place on Apple GPUs
    WIth Metal and Apple GPUs, the mipmap compute shader can write directly
    to an sRGB texture, and the hardware will do the conversion similar to
    read. This lets us update the textures in-place, avoiding a temporary
    texture.

--- source/blender/gpu/intern/gpu_capabilities_private.hh
+++ source/blender/gpu/intern/gpu_capabilities_private.hh
@@ -48,6 +48,9 @@ struct GPUCapabilities {
   bool stencil_export_support = false;
   bool ray_query_support = false;
 
+  /** Can an sRGB texture be written, with the hardware performing conversion to sRGB? */
+  bool srgb_write_direct_support = false;
+
   int max_parallel_compilations = -1;
 
   /* OpenGL related workarounds. */
