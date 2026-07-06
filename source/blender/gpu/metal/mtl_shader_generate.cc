6c8e80f54e1c9a206bf766480d5556dc5651c3b5
Miguel Pozo <pragma37@gmail.com>
2026-07-06 15:26:59 +0200

    Add missing ACCELERATION_STRUCTURE cases

--- source/blender/gpu/metal/mtl_shader_generate.cc
+++ source/blender/gpu/metal/mtl_shader_generate.cc
@@ -743,6 +743,9 @@ static void generate_resource(GeneratedStreams &generated,
                       MTL_SSBO_SLOT_OFFSET + res.slot,
                       stage);
       break;
+    case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+      BLI_assert_unreachable();
+      break;
   }
 }
 
@@ -1464,6 +1467,9 @@ uint32_t available_buffer_slots(const ShaderCreateInfo &info)
       case ShaderCreateInfo::Resource::BindType::SAMPLER:
       case ShaderCreateInfo::Resource::BindType::IMAGE:
         break;
+      case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+        BLI_assert_unreachable();
+        break;
     };
   };
 
@@ -1526,6 +1532,9 @@ void patch_create_info_atomic_workaround(std::unique_ptr<PatchedShaderCreateInfo
         break;
       case ShaderCreateInfo::Resource::BindType::UNIFORM_BUFFER:
       case ShaderCreateInfo::Resource::BindType::STORAGE_BUFFER:
+        break;
+      case ShaderCreateInfo::Resource::BindType::ACCELERATION_STRUCTURE:
+        BLI_assert_unreachable();
         break;
     }
   };
