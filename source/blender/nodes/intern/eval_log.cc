056c6641ac5a49b23d218f05bee0427b9a99db73
Omar Emara <mail@OmarEmara.dev>
2026-07-07 11:44:05 +0300

    Format

--- source/blender/nodes/intern/eval_log.cc
+++ source/blender/nodes/intern/eval_log.cc
@@ -108,16 +108,17 @@ GeometryInfoLog::GeometryInfoLog(const bke::GeometrySet &geometry_set)
    * attributes with the same name but different domains or data types on separate components. */
   Set<StringRef> names;
 
-  geometry_set.attribute_foreach(
-      all_component_types,
-      true,
-      [&](const StringRef name,
-          const bke::AttributeMetaData &meta_data,
-          const bke::GeometryComponent & /*component*/) {
-        if (!bke::attribute_name_is_anonymous(name) && names.add(name)) {
-          this->attributes.append({name, meta_data.domain, meta_data.data_type});
-        }
-      });
+  geometry_set.attribute_foreach(all_component_types,
+                                 true,
+                                 [&](const StringRef name,
+                                     const bke::AttributeMetaData &meta_data,
+                                     const bke::GeometryComponent & /*component*/) {
+                                   if (!bke::attribute_name_is_anonymous(name) && names.add(name))
+                                   {
+                                     this->attributes.append(
+                                         {name, meta_data.domain, meta_data.data_type});
+                                   }
+                                 });
 
   for (const bke::GeometryComponent *component : geometry_set.get_components()) {
     this->component_types.append(component->type());
