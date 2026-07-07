1f3d303a65073b4c7a70bc8f0b3aab5d5e1a07fa
Philipp Oeser <philipp@blender.org>
2026-07-07 13:04:09 +0200

    update comment

--- source/blender/blenkernel/intern/object.cc
+++ source/blender/blenkernel/intern/object.cc
@@ -3982,7 +3982,7 @@ void BKE_object_foreach_display_point(Object *ob,
                                       void (*func_cb)(const float[3], void *),
                                       void *user_data)
 {
-  /* TODO: point-cloud and curves object support. */
+  /* TODO: volume object support. */
   const Mesh *mesh_eval = BKE_object_get_evaluated_mesh(ob);
   float3 co;
 
