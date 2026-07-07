blender/blender

commit 11e0336ed223d38d704fd8760e0ae1a8cdf0e558

Assets: User count asset previews
There may be multiple asset representations for the same underlying
assets. E.g. when multiple asset libraries contain some of the same
assets, or when multiple asset UIs are open (not all of them share
storage yet).

Whenever this happened, multiple assets could refer to the same preview,
because they use the same preview identifier. When one of the assets was
freed (e.g. because an asset browser was closed), the preview would be
freed, even though it is shared by multiple assets.

Fixes #159330.

Pull Request: https://projects.blender.org/blender/blender/pulls/160287

source/blender/blenkernel/intern/preview_image.cc
@@ -86,6 +86,15 @@ static void previewimg_free_or_defer(PreviewImage **prv)
 
   BLI_assert(BLI_thread_is_main());
 
+  /* User counting is only done in few cases. If the count is 0, no counting is being used. */
+  if ((*prv)->runtime->user_count > 0) {
+    (*prv)->runtime->user_count--;
+    if ((*prv)->runtime->user_count > 0) {
+      /* Don't free yet. */
+      return;
+    }
+  }
+
   bool do_delete = true;
 
   /* If a preview is still being rendered, tag it for deferred deletion in
@@ -303,7 +312,8 @@ PreviewImage *BKE_previewimg_cached_ensure(const char *name)
 PreviewImage *BKE_previewimg_cached_thumbnail_read(const char *name,
                                                    const char *filepath,
                                                    const int source,
-                                                   bool force_update)
+                                                   bool force_update,
+                                                   const bool count_users)
 {
   BLI_assert(BLI_thread_is_main());
 
@@ -337,6 +347,10 @@ PreviewImage *BKE_previewimg_cached_thumbnail_read(const char *name,
     force_update = true;
   }
 
+  if (count_users) {
+    prv->runtime->user_count++;
+  }
+
   if (force_update) {
     if (prv_p) {
       *prv_p = prv;
@@ -351,10 +365,11 @@ PreviewImage *BKE_previewimg_cached_thumbnail_read(const char *name,
 
 PreviewImage *BKE_previewimg_online_thumbnail_read(const char *name,
                                                    const char *dst_filepath,
-                                                   const bool force_update)
+                                                   const bool force_update,
+                                                   const bool count_users)
 {
   PreviewImage *preview = BKE_previewimg_cached_thumbnail_read(
-      name, dst_filepath, THB_SOURCE_DIRECT, force_update);
+      name, dst_filepath, THB_SOURCE_DIRECT, force_update, count_users);
   preview->runtime->deferred_loading_data->is_online = true;
 
   return preview;

source/blender/asset_system/intern/asset_representation.cc
@@ -92,6 +92,11 @@ void AssetRepresentation::ensure_previewable(const bContext &C, ReportList *repo
     return;
   }
 
+  /* The asset may be in multiple libraries, so multiple #AssetRepresentation's may refer to the
+   * same preview. Use user counting so the preview is only released with the last representation.
+   */
+  const bool count_preview_users = true;
+
   /* Only use the remote thumbnail when there is no asset file on disk. Otherwise use the on-disk
    * file. */
   if (this->is_online_only()) {
@@ -102,7 +107,7 @@ void AssetRepresentation::ensure_previewable(const bContext &C, ReportList *repo
     const std::string preview_path = remote_library_asset_preview_path(*this);
     /* Doesn't do the actual reading, just allocates and attaches the derived load info. */
     extern_asset.preview_ = BKE_previewimg_online_thumbnail_read(
-        this->full_path().c_str(), preview_path.c_str(), false);
+        this->full_path().c_str(), preview_path.c_str(), false, count_preview_users);
     remote_library_request_preview_download(C, *this, preview_path, reports);
   }
   else {
@@ -111,7 +116,7 @@ void AssetRepresentation::ensure_previewable(const bContext &C, ReportList *repo
 
     /* Doesn't do the actual reading, just allocates and attaches the derived load info. */
     extern_asset.preview_ = BKE_previewimg_cached_thumbnail_read(
-        full_path.c_str(), full_path.c_str(), THB_SOURCE_BLEND, false);
+        full_path.c_str(), full_path.c_str(), THB_SOURCE_BLEND, false, count_preview_users);
   }
 
   BKE_icon_preview_ensure(nullptr, extern_asset.preview_);

source/blender/blenkernel/BKE_preview_image.hh
@@ -37,6 +37,11 @@ struct PreviewImageRuntime {
   int icon_id = 0;
   int16_t tag[NUM_ICON_SIZES] = {};
 
+  /** This is only needed/used in rare cases (asset previews). Multiple libraries may contain the
+   * same asset, so they will request the same preview. In that case, the user count can be used to
+   * avoid freeing too early. */
+  int user_count = 0;
+
   std::array<gpu::Texture *, NUM_ICON_SIZES> gputexture = {};
 
   /** Used to store data to defer the loading of the preview. If empty, loading is not deferred. */
@@ -179,11 +184,15 @@ PreviewImage *BKE_previewimg_cached_ensure(const char *name);
  * Generate a #PreviewImage from given `filepath`, using thumbnails management, if not yet
  * existing. Does not actually generate the preview, #BKE_previewimg_ensure() must be called for
  * that.
+ *
+ * \param count_users: This must be applied consistently to all previews of their kind (for
+ *     example, to all asset previews, to *no* BPY defined previews, etc.).
  */
 PreviewImage *BKE_previewimg_cached_thumbnail_read(const char *name,
                                                    const char *filepath,
                                                    int source,
-                                                   bool force_update);
+                                                   bool force_update,
+                                                   bool count_users = false);
 /* TODO rename this. */
 /**
  * Create a #PreviewImage that is marked as "needs downloading" to \a dst_filepath before loading
@@ -193,10 +202,13 @@ PreviewImage *BKE_previewimg_cached_thumbnail_read(const char *name,
  *
  * \param force_update: Clear the preview's data, so it will be re-loaded once actual loading is
  *   triggered.
+ * \param count_users: This must be applied consistently to all previews of their kind (for
+ *     example, to all asset previews, to *no* BPY defined previews, etc.).
  */
 PreviewImage *BKE_previewimg_online_thumbnail_read(const char *name,
                                                    const char *dst_filepath,
-                                                   bool force_update);
+                                                   bool force_update,
+                                                   bool count_users = false);
 
 void BKE_previewimg_cached_release(const char *name);
 

FrenkyBojler/blender-ios

commit 389e183511be4bf26ce941eaed866e05ff74a19f

Fix: Online previews leak when displaying both asset browser and shelf
11e0336ed2 introduced user counting for asset previews, so the same
preview can be owned by multiple representations of the same asset. This
worked, but whenever there were multiple owners of an asset preview,
the preview memory would leak.

That's because the preview was already removed from the global preview
cache even when there were more users still, meaning the other users
were not able to look it up and free it properly.

source/blender/blenkernel/intern/preview_image.cc
@@ -78,10 +78,14 @@ static PreviewImage *previewimg_deferred_create(const char *filepath, ThumbSourc
   return prv;
 }
 
-static void previewimg_free_or_defer(PreviewImage **prv)
+/**
+ * \return True if the preview image was deleted or marked for deferred deletion. User counting may
+ *     prevent the freeing so this will return false then.
+ */
+static bool previewimg_free_or_defer(PreviewImage **prv)
 {
   if (*prv == nullptr) {
-    return;
+    return false;
   }
 
   BLI_assert(BLI_thread_is_main());
@@ -91,7 +95,7 @@ static void previewimg_free_or_defer(PreviewImage **prv)
     (*prv)->runtime->user_count--;
     if ((*prv)->runtime->user_count > 0) {
       /* Don't free yet. */
-      return;
+      return false;
     }
   }
 
@@ -110,6 +114,7 @@ static void previewimg_free_or_defer(PreviewImage **prv)
     BKE_previewimg_free(prv);
   }
   *prv = nullptr;
+  return true;
 }
 
 PreviewImage *BKE_previewimg_create()
@@ -379,8 +384,12 @@ void BKE_previewimg_cached_release(const char *name)
 {
   BLI_assert(BLI_thread_is_main());
   CachedPreviewMap &cache = get_cached_previews_map();
-  PreviewImage *prv = cache.pop_default_as(name, nullptr);
-  previewimg_free_or_defer(&prv);
+  PreviewImage *prv = cache.lookup_default(name, nullptr);
+  /* The preview may not be freed when there are more users still. In that case, keep the preview
+   * in the cache, so other users can still find it an free it eventually. */
+  if (previewimg_free_or_defer(&prv)) {
+    cache.pop_try(name);
+  }
 }
 
 void BKE_previewimg_ensure(PreviewImage *prv, const int size)