3cf2e8e99d29b03741d435bc8169991c01f38e08
Philipp Oeser <philipp@blender.org>
2026-07-07 12:02:54 +0200

    Fix #161168: context menu "Open File/Location" broken for relative paths
    Was also asserting.
    
    `BLI_exists` (introduced to `but_menu_add_path_operators` in
    e44f500be814) only works for absolute paths, so convert to absolute (if
    possible) prior.

--- source/blender/editors/interface/interface_context_menu.cc
+++ source/blender/editors/interface/interface_context_menu.cc
@@ -25,6 +25,7 @@
 
 #include "BKE_context.hh"
 #include "BKE_idprop.hh"
+#include "BKE_main.hh"
 #include "BKE_screen.hh"
 
 #include "ED_asset.hh"
@@ -526,6 +527,13 @@ static bool but_menu_add_path_operators(Layout &layout, PointerRNA *ptr, Propert
   UNUSED_VARS_NDEBUG(subtype);
 
   RNA_property_string_get(ptr, prop, filepath);
+
+  if (BLI_path_is_rel(filepath) && ptr->owner_id == nullptr) {
+    return false;
+  }
+
+  const char *base_path = ID_BLEND_PATH_FROM_GLOBAL(ptr->owner_id);
+  BLI_path_abs(filepath, base_path);
 
   if (!BLI_exists(filepath)) {
     return false;
