f943c369f1daf9928ce44bfb6bd2ab8c74cda8b2
Jacques Lucke <jacques@blender.org>
2026-07-09 13:26:13 +0200

    jolt setup boilerplate

--- source/blender/nodes/geometry/nodes/node_geo_jolt_solver.cc
+++ source/blender/nodes/geometry/nodes/node_geo_jolt_solver.cc
@@ -4,6 +4,13 @@
 
 #include <Jolt/Jolt.h>
 
+#include <Jolt/Core/JobSystemSingleThreaded.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+#include <Jolt/Physics/Collision/BroadPhase/BroadPhaseLayer.h>
+#include <Jolt/Physics/Collision/ContactListener.h>
+#include <Jolt/Physics/Collision/ObjectLayer.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+
 #include "node_geometry_util.hh"
 
 #include "GEO_jolt.hh"
@@ -38,9 +45,149 @@ static void node_declare(NodeDeclarationBuilder &b)
   }
 }
 
+static JPH::Vec3 convert_vec3(const float3 &v)
+{
+  return JPH::Vec3(v.x, v.y, v.z);
+}
+static float3 convert_vec3(const JPH::Vec3 &v)
+{
+  return float3(v.GetX(), v.GetY(), v.GetZ());
+}
+static JPH::Quat convert_quat(const math::Quaternion &q)
+{
+  return JPH::Quat(q.x, q.y, q.z, q.w);
+}
+static math::Quaternion convert_quat(const JPH::Quat &q)
+{
+  return math::Quaternion(q.GetW(), q.GetX(), q.GetY(), q.GetZ());
+}
+
+namespace ObjectLayers {
+static constexpr JPH::ObjectLayer non_moving(0);
+static constexpr JPH::ObjectLayer moving(1);
+static constexpr int num_layers = 2;
+}  // namespace ObjectLayers
+
+namespace BroadPhaseLayers {
+static constexpr JPH::BroadPhaseLayer non_moving(0);
+static constexpr JPH::BroadPhaseLayer moving(1);
+static constexpr int num_layers = 2;
+}  // namespace BroadPhaseLayers
+
+class BroadPhaseLayerInterfaceImpl : public JPH::BroadPhaseLayerInterface {
+ private:
+  std::array<JPH::BroadPhaseLayer, ObjectLayers::num_layers> object_to_broad_phase_;
+
+ public:
+  BroadPhaseLayerInterfaceImpl()
+  {
+    object_to_broad_phase_[ObjectLayers::non_moving] = BroadPhaseLayers::non_moving;
+    object_to_broad_phase_[ObjectLayers::moving] = BroadPhaseLayers::moving;
+  }
+
+  uint GetNumBroadPhaseLayers() const override
+  {
+    return BroadPhaseLayers::num_layers;
+  }
+
+  JPH::BroadPhaseLayer GetBroadPhaseLayer(const JPH::ObjectLayer object_layer) const override
+  {
+    BLI_assert(object_layer >= 0 && object_layer < ObjectLayers::num_layers);
+    return object_to_broad_phase_[object_layer];
+  }
+};
+
+class ObjectLayerPairFilterImpl : public JPH::ObjectLayerPairFilter {
+ public:
+  bool ShouldCollide(const JPH::ObjectLayer a, const JPH::ObjectLayer b) const override
+  {
+    if (a == ObjectLayers::moving || b == ObjectLayers::moving) {
+      return true;
+    }
+    return false;
+  }
+};
+
+class ObjectVsBroadPhaseLayerFilterImpl : public JPH::ObjectVsBroadPhaseLayerFilter {
+ public:
+  bool ShouldCollide(const JPH::ObjectLayer object_layer,
+                     const JPH::BroadPhaseLayer broad_phase_layer) const override
+  {
+    if (object_layer == ObjectLayers::moving || broad_phase_layer == BroadPhaseLayers::moving) {
+      return true;
+    }
+    return false;
+  }
+};
+
+class ContactListenerImpl : public JPH::ContactListener {
+ public:
+  virtual JPH::ValidateResult OnContactValidate(
+      const JPH::Body & /*body_a*/,
+      const JPH::Body & /*body_b*/,
+      JPH::RVec3Arg /*in_base_offset*/,
+      const JPH::CollideShapeResult & /*in_collision_result*/) override
+  {
+    return JPH::ValidateResult::AcceptAllContactsForThisBodyPair;
+  }
+
+  void OnContactAdded(const JPH::Body & /*body_a*/,
+                      const JPH::Body & /*body_b*/,
+                      const JPH::ContactManifold & /*manifold*/,
+                      JPH::ContactSettings & /*settings*/) override
+  {
+  }
+
+  void OnContactPersisted(const JPH::Body & /*body_a*/,
+                          const JPH::Body & /*body_b*/,
+                          const JPH::ContactManifold & /*manifold*/,
+                          JPH::ContactSettings & /*settings*/) override
+  {
+  }
+
+  void OnContactRemoved(const JPH::SubShapeIDPair & /*sub_shape_pair*/) override {}
+};
+
+class BodyActivationListenerImpl : public JPH::BodyActivationListener {
+ public:
+  void OnBodyActivated(const JPH::BodyID & /*body_id*/, uint64_t /*body_user_data*/) override {}
+
+  void OnBodyDeactivated(const JPH::BodyID & /*body_id*/, uint64_t /*body_user_data*/) override {}
+};
+
 static void node_geo_exec(GeoNodeExecParams params)
 {
   geometry::jolt::ensure_initialization();
+
+  const float delta_time = params.extract_input<float>("Delta Time"_ustr);
+  const int substeps = params.extract_input<int>("Substeps"_ustr);
+
+  BroadPhaseLayerInterfaceImpl broad_phase_layer_interface;
+  ObjectVsBroadPhaseLayerFilterImpl object_vs_broad_phase_layer_filter;
+  ObjectLayerPairFilterImpl object_layer_pair_filter;
+  BodyActivationListenerImpl body_activation_listener;
+  ContactListenerImpl contact_listener;
+  JPH::PhysicsSystem system;
+
+  /* Defaults taken from Jolt's Hello World example, may need to be tweaked/dynamic over
+   * time.*/
+  const int max_bodies = 65536;
+  const int num_body_mutexes = 0;
+  const int max_body_pairs = 65536;
+  const int max_contact_constraints = 10240;
+
+  system.Init(max_bodies,
+              num_body_mutexes,
+              max_body_pairs,
+              max_contact_constraints,
+              broad_phase_layer_interface,
+              object_vs_broad_phase_layer_filter,
+              object_layer_pair_filter);
+
+  JPH::TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
+  JPH::JobSystemSingleThreaded job_system;
+  system.Update(delta_time, substeps, &temp_allocator, &job_system);
+
   params.set_default_remaining_outputs();
 }
 
