6ac19ee1648f2f3985c6d9b26d954593a79ac622
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-07-09 15:06:26 +0200

    test when main is read

--- source/blender/depsgraph/intern/tests/deg_eval_test.cc
+++ source/blender/depsgraph/intern/tests/deg_eval_test.cc
@@ -25,7 +25,27 @@
 
 #include "testing/testing.h"
 
+#include "intern/depsgraph.hh"
+#include "intern/node/deg_node.hh"
+#include "intern/node/deg_node_operation.hh"
+
 namespace blender::deg::tests {
+
+/* Returns true if any node of type NodeType::COPY_ON_EVAL is tagged for evaluation. */
+static bool has_to_read_from_main(blender::Depsgraph *depsgraph)
+{
+  Depsgraph *dg = reinterpret_cast<Depsgraph *>(depsgraph);
+  for (OperationNode *node : dg->operations) {
+    /* Assuming that only COPY_ON_EVAL nodes read from Main. */
+    if (node->opcode != OperationCode::COPY_ON_EVAL) {
+      continue;
+    }
+    if (node->flag & DEPSOP_FLAG_NEEDS_UPDATE) {
+      return true;
+    }
+  }
+  return false;
+}
 
 class DepsgraphTest : public bke::BlenderGTestBase {
 
@@ -33,7 +53,7 @@ class DepsgraphTest : public bke::BlenderGTestBase {
   Main *bmain_ = nullptr;
   Scene *scene_ = nullptr;
   ViewLayer *view_layer_ = nullptr;
-  Depsgraph *depsgraph_ = nullptr;
+  blender::Depsgraph *depsgraph_ = nullptr;
 
   void SetUp() override
   {
@@ -132,10 +152,13 @@ TEST_F(DepsgraphTest, evaluate_objects_after_transforms)
   Object *eval_ob = DEG_get_evaluated(depsgraph_, ob);
 
   ob->loc[0] = 1;
+  /* To get the depsgraph to anything we need to tag the ID for evaluation, only then is the data
+   * copied from Main. */
   DEG_id_tag_update(&ob->id, ID_RECALC_TRANSFORM);
   EXPECT_FALSE(DEG_id_is_fully_evaluated(depsgraph_, &ob->id));
   EXPECT_GT(abs(ob->loc[0] - eval_ob->loc[0]), 0.0001)
       << "Modifying data in Main should not affect the depsgraph automatically.";
+  EXPECT_TRUE(has_to_read_from_main(depsgraph_));
 
   evaluate_at_frame(0);
   EXPECT_TRUE(DEG_id_is_fully_evaluated(depsgraph_, &ob->id));
@@ -146,8 +169,8 @@ TEST_F(DepsgraphTest, evaluate_objects_after_transforms)
       << "The location value should have been copied from Main.";
 }
 
-/* When the object is animated, the current copy can be kept since the original object is not
- * modified by evaluating the action. */
+/* When the object is animated, the object ID does not need to be copied from Main
+ * on every frame. */
 TEST_F(DepsgraphTest, evaluate_animated_object) {}
 
 }  // namespace blender::deg::tests
