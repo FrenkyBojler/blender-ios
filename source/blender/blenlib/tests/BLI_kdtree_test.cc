4ed06960aad791f68eeceeb54d880c9b7c9a8fd9
илья _ <modormoder@gmail.com>
2026-07-12 00:04:06 +0300

    new kdtree tests..

---
+++ source/blender/blenlib/tests/BLI_kdtree_test.cc
@@ -0,0 +1,34 @@
+/* SPDX-FileCopyrightText: 2026 Blender Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0 */
+
+#include "testing/testing.h"
+
+#include "BLI_array.hh"
+#include "BLI_vector.hh"
+#include "BLI_span.hh"
+#include "BLI_kdtree.hh"
+
+namespace blender::tests {
+
+TEST(kdtree, Balance)
+{
+  
+}
+
+TEST(kdtree, ForeachNode)
+{
+  
+}
+
+TEST(kdtree, ForeachNearest)
+{
+  
+}
+
+TEST(kdtree, ForeachRange)
+{
+  
+}
+
+}  // namespace blender::tests
