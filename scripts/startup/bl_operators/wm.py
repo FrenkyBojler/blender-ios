005ee5edb6a3dd09f01b1716bf1274885bd10f67
RedMser <redmser.jj2@gmx.de>
2026-07-13 20:31:27 +0200

    use time icon

--- scripts/startup/bl_operators/wm.py
+++ scripts/startup/bl_operators/wm.py
@@ -2806,7 +2806,7 @@ class WM_OT_batch_rename(Operator):
             ('ACTION_CLIP', "Action Clips", "", 'ACTION', 17),
             None,
             ('SCENE', "Scenes", "", 'SCENE_DATA', 18),
-            ('MARKER', "Timeline Markers", "", 'MARKER_HLT', 19),
+            ('MARKER', "Timeline Markers", "", 'TIME', 19),
             ('BRUSH', "Brushes", "", 'BRUSH_DATA', 20),
         ),
         translation_context=i18n_contexts.id_id,
