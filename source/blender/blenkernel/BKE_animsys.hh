2ee57cecac91990577f5f738bbe7e6ba94b5a8d4
Christoph Lendenfeld <chris.lenden@gmail.com>
2026-07-13 15:08:07 +0200

    loing loing loing

--- source/blender/blenkernel/BKE_animsys.hh
+++ source/blender/blenkernel/BKE_animsys.hh
@@ -167,7 +167,7 @@ DriverMap BKE_animdata_build_driver_target_map();
  * Search and replace `old_infix` with `new_infix` for all rna paths that reference the given ID
  * and match the prefix.
  *
- * \param prefix does not have to be the full prefix of the rna path; it works as loing as it is
+ * \param prefix does not have to be the full prefix of the rna path; it works as long as it is
  * directly preceding `infix`. E.g. if the full path is `pose.bones["foo"]` then passing `bones` as
  * a prefix will still work.
  * \param old_infix, new_infix: the full search and replace string pair.
