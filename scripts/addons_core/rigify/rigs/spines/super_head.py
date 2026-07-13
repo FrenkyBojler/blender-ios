6a38b0f148f0397ccf75fd25f1c1d30bd4192d77
Antônio Froz <ant.7a@hotmail.com>
2026-07-13 14:41:13 +0200

    Atualizar scripts/addons_core/rigify/rigs/spines/super_head.py

--- scripts/addons_core/rigify/rigs/spines/super_head.py
+++ scripts/addons_core/rigify/rigs/spines/super_head.py
@@ -356,7 +356,7 @@ class Rig(BaseHeadTailRig):
     def add_parameters(cls, params):
         super().add_parameters(params)
         params.bbones = bpy.props.IntProperty(
-            name='B-Bone Segments', default=10, min=1,
+            name='B-Bone Segments', default=8, min=1, max=32,
             description='Number of B-Bone segments')
 
     @classmethod
