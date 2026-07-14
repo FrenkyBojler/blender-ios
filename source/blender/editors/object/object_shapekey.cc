72e3a84e8892b9f68709320a280279c5f8f2a3d0
Ahmed-Nassar <AhmedNassar5720@gmail.com>
2026-07-14 12:48:51 -0400

    Formatting: fixed formatting with make format

--- source/blender/editors/object/object_shapekey.cc
+++ source/blender/editors/object/object_shapekey.cc
@@ -155,7 +155,7 @@ static void object_shape_key_add(bContext *C, Object *ob, const bool from_mix)
     /* Explicitly deselect current selection when adding a new key, rather than
      * relying on the tree view's state. This is problematic when the same data is
      * shown in more than one view instance, see #161071. */
-    for (KeyBlock& other : key->block) {
+    for (KeyBlock &other : key->block) {
       SET_FLAG_FROM_TEST(other.flag, &other == kb, KEYBLOCK_SEL);
     }
 
