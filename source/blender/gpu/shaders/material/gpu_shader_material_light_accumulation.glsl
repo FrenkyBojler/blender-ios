bef98940ad3dcddad22ded282dee1b4da98c1c70
Miguel Pozo <pragma37@gmail.com>
2026-07-14 18:12:59 +0200

    node_light_accumulation

--- source/blender/gpu/shaders/material/gpu_shader_material_light_accumulation.glsl
+++ source/blender/gpu/shaders/material/gpu_shader_material_light_accumulation.glsl
@@ -6,5 +6,13 @@
 void node_light_accumulation(
     float4 diffuse, float4 glossy, float4 transmission, float weight, Closure &result)
 {
-  node_light_accumulation_impl(diffuse, glossy, transmission, weight, result);
+  // node_light_accumulation_impl(diffuse, glossy, transmission, weight, result);
+  diffuse = max(diffuse, float4(0.0f));
+  glossy = max(glossy, float4(0.0f));
+  transmission = max(transmission, float4(0.0f));
+
+  ClosureEmission emission_data;
+  emission_data.emission = (diffuse + glossy + transmission).rgb * weight;
+
+  result = closure_eval(emission_data);
 }
