f7cac226cf9ae055bad8fa4d866584e4a9832e03
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-07-16 09:16:59 -0700

    Fix attributes.

--- source/blender/editors/sculpt_paint/grease_pencil/erase.cc
+++ source/blender/editors/sculpt_paint/grease_pencil/erase.cc
@@ -943,7 +943,7 @@ struct EraseOperationExecutor {
     carver::CurveBooleanOpParameters op_params;
     op_params.boolean_mode = carver::Operation::Difference;
     op_params.keep_caps = keep_caps;
-    op_params.skip_clipping_attributes = false;
+    op_params.skip_clipping_attributes = true;
 
     const GroupedSpan<int> shapes = GroupedSpan<int>(shape_offsets.as_span(), shape_map.as_span());
     const IndexRange clipping_shapes = IndexRange::from_single(shapes.size() - 1);
