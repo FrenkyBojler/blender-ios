4e24b1931d6a8a14f27c4c871a381451d5503a72
quackarooni <alfonsomartzii@gmail.com>
2026-07-16 10:21:15 +0800

    Cleanup: Style and comment order

--- source/blender/blenloader/intern/versioning_530.cc
+++ source/blender/blenloader/intern/versioning_530.cc
@@ -183,6 +183,7 @@ void blo_do_versions_530(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     }
     FOREACH_NODETREE_END;
   }
+
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
