54deac49e9e477e50eef63b7980f08d4ae2441f2
Sean Kim <SeanCTKim@protonmail.com>
2026-07-19 16:40:50 -0700

    Revert formatting

--- source/blender/draw/DRW_pbvh.hh
+++ source/blender/draw/DRW_pbvh.hh
@@ -39,7 +39,12 @@ namespace draw::pbvh {
 
 using GenericRequest = std::string;
 
-enum class CustomRequest : int8_t { Position, Normal, Mask, FaceSet };
+enum class CustomRequest : int8_t {
+  Position,
+  Normal,
+  Mask,
+  FaceSet,
+};
 
 using AttributeRequest = std::variant<CustomRequest, GenericRequest>;
 
