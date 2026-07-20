90fa000433ba0037f02cfe245c5de4179cce1993
Clément Foucault <foucault.clem@gmail.com>
2026-07-20 18:00:19 +0200

    Fix #161515: EEVEE: SubSurface Scattering: Uninitialized loads
    The indirect radiance is not part of the LDS cache.
    The cache hit and indirect radiance texture fetch
    can differ slightly and lead to uninitialized reads
    that are not masked by the object ID.
    
    To avoid this, we clear the indirect radiance to 0.
    
    A better fix might be adding a `cached_indirect`
    array in the LDS cache, but this risks having
    performance impact on the regular non-split path
    (because of higher register pressure).
    
    So for the 5.2 backport it is safer to clear.

--- source/blender/draw/engines/eevee/shaders/eevee_subsurface.bsl.hh
+++ source/blender/draw/engines/eevee/shaders/eevee_subsurface.bsl.hh
@@ -92,6 +92,12 @@ void setup_main([[resource_table]] Setup &srt,
     }
   }
   else {
+    if (srt.use_split_radiance) {
+      /* The indirect radiance is not part of the LDS cache. The cache hit and indirect radiance
+       * texture fetch can differ slightly and lead to uninitialized reads that are not masked by
+       * the object ID. To avoid this, we clear the indirect radiance to 0. */
+      imageStoreFast(srt.radiance_img, int3(texel, 1), float4(0));
+    }
     /* No need to write radiance_img since the radiance won't be used at all. */
     imageStore(srt.object_id_img, texel, uint4(0));
   }
