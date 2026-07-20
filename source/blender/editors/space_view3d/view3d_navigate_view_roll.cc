2de10c905e0f86832199b54e7ac343038e523c1b
Casey Bianco-Davis <caseycasey739@gmail.com>
2026-07-20 10:03:14 -0700

    Cleanup: Use `angle_wrap_rad`

--- source/blender/editors/space_view3d/view3d_navigate_view_roll.cc
+++ source/blender/editors/space_view3d/view3d_navigate_view_roll.cc
@@ -72,8 +72,9 @@ static void viewroll_apply(ViewOpsData *vod, int x, int y)
     const bool is_camera_lock = ED_view3d_camera_lock_check(vod->v3d, vod->rv3d);
     if (vod->rv3d->persp == RV3D_CAMOB && !is_camera_lock) {
       vod->rv3d->camroll = vod->init.camroll - angle;
+
       /* Keep angle between -Pi and Pi */
-      vod->rv3d->camroll = math::floored_mod(vod->rv3d->camroll - M_PI, M_PI * 2.0f) - M_PI;
+      vod->rv3d->camroll = angle_wrap_rad(vod->rv3d->camroll);
     }
   }
 
@@ -218,8 +219,9 @@ static wmOperatorStatus viewroll_exec(bContext *C, wmOperator *op)
   const bool is_camera_lock = ED_view3d_camera_lock_check(vod->v3d, vod->rv3d);
   if (vod->rv3d->persp == RV3D_CAMOB && !is_camera_lock) {
     vod->rv3d->camroll = vod->init.camroll - angle;
+
     /* Keep angle between -Pi and Pi */
-    vod->rv3d->camroll = math::floored_mod(vod->rv3d->camroll - M_PI, M_PI * 2.0f) - M_PI;
+    vod->rv3d->camroll = angle_wrap_rad(vod->rv3d->camroll);
   }
 
   V3D_SmoothParams sview_params = {};
