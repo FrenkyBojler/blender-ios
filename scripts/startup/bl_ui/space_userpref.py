3480df13dbb3cff254d43e9b2b20141c8ee93227
Jacques Lucke <jacques@blender.org>
2026-07-21 15:52:32 +0200

    rename geometry_nodes_stack_limit to nodes_stack_limit

--- scripts/startup/bl_ui/space_userpref.py
+++ scripts/startup/bl_ui/space_userpref.py
@@ -877,7 +877,7 @@ class USERPREF_PT_system_memory(SystemPanel, CenterAlignMixIn, Panel):
         layout.separator()
 
         col = layout.column()
-        col.prop(system, "geometry_nodes_stack_limit")
+        col.prop(system, "nodes_stack_limit")
 
 
 class USERPREF_PT_system_video_sequencer(SystemPanel, CenterAlignMixIn, Panel):
