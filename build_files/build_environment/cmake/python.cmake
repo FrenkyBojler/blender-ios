01078c9c4915aacb9df97dbdeb92a7441c5eac80
Testfiya <Akhater00@outlook.com>
2026-07-21 17:54:07 +0300

    Build: Python: fix typo path

--- build_files/build_environment/cmake/python.cmake
+++ build_files/build_environment/cmake/python.cmake
@@ -157,7 +157,7 @@ else()
       ${PYTHON_CONFIGURE_EXTRA_ARGS}
       --with-force-crosscompile=yes
       --with-static-libpython=yes
-      --disable-shared 
+      --disable-shared
       MODULE_BUILDTYPE=static
       --disable-test-modules
       --enable-test-modules=no
@@ -300,7 +300,7 @@ if(WITH_APPLE_CROSSPLATFORM)
   # Copy sysconfigdata to darwin_arm64
   ExternalProject_Add_Step(external_python after_install
     COMMAND ${CMAKE_COMMAND} -E copy
-    ${BUILD_DIR}/python/src/external_python/build/lib.darwin-3.13/_sysconfigdata__darwin_arm64-${APPLE_SDK_CROSSPLATFORM_NAME_LOWER}.py
+    ${BUILD_DIR}/python/src/external_python/build/lib.darwin-arm-3.13/_sysconfigdata__darwin_arm64-${APPLE_SDK_CROSSPLATFORM_NAME_LOWER}.py
     ${CMAKE_DEPS_CROSSCOMPILE_BUILDDIR}/deps_arm64/Release/python/lib/python3.13/_sysconfigdata__darwin_arm64-${APPLE_SDK_CROSSPLATFORM_NAME_LOWER}.py
     DEPENDEES install
   )
