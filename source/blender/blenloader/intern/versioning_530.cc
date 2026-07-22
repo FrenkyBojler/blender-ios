768f89b96dfdfe00ecc452e00e6ef43f8b08dcb5
Jerry Wei <twei79@gatech.edu>
2026-07-22 15:56:47 -0400

    Fix versioning location

--- source/blender/blenloader/intern/versioning_530.cc
+++ source/blender/blenloader/intern/versioning_530.cc
@@ -184,6 +184,16 @@ void blo_do_versions_530(FileData * /*fd*/, Library * /*lib*/, Main *bmain)
     FOREACH_NODETREE_END;
   }
 
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 503, 9)) {
+    for (Brush &brush : bmain->brushes) {
+      if (brush.curve_hardness == nullptr) {
+        brush.curve_hardness = BKE_paint_default_curve();
+      }
+      if (brush.curve_auto_smooth == nullptr) {
+        brush.curve_auto_smooth = BKE_paint_default_curve();
+      }
+    }
+  }
   /**
    * Always bump subversion in BKE_blender_version.h when adding versioning
    * code here, and wrap it inside a MAIN_VERSION_FILE_ATLEAST check.
